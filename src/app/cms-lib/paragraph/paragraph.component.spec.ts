78b8a8c0629f549eefc0d963dce1aa61121a6337
Patrick Labadie <patrick.labadie@sap.com>
2017-12-22 16:01:41 -0500

    SPA-43: Added unit tests for cms paragraph component.

--- src/app/cms-lib/paragraph/paragraph.component.spec.ts
+++ src/app/cms-lib/paragraph/paragraph.component.spec.ts
@@ -1,25 +1,65 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ParagraphComponent } from './paragraph.component';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-describe('CmsParagraphComponent', () => {
-  let component: ParagraphComponent;
+import { ParagraphComponent } from './paragraph.component';
+import { CmsService } from 'app/data/cms.service';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+
+function getParagraphComponentCmsData() {
+  return {
+    uid: '001',
+    typeCode: 'CMSParagraphComponent',
+    modifiedTime: '2017-12-21T18:15:15+0000',
+    name: 'TestCMSParagraphComponent',
+    container: 'false',
+    type: 'Paragraph',
+    content: 'Arbitrary paragraph content'
+  };
+}
+
+class MockCmsService {
+  public getComponentSubscription(key: string) {
+    return new BehaviorSubject<any>(getParagraphComponentCmsData());
+  }
+}
+
+fdescribe('CmsParagraphComponent', () => {
+  let paragraphComponent: ParagraphComponent;
   let fixture: ComponentFixture<ParagraphComponent>;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParagraphComponent ]
+      declarations: [ ParagraphComponent ],
+      providers: [{ provide: CmsService, useClass: MockCmsService }]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ParagraphComponent);
-    component = fixture.componentInstance;
+    paragraphComponent = fixture.componentInstance;
+    el = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(paragraphComponent).toBeTruthy();
+  });
+
+  it('should contain cms data in the component after bootstrap', () => {
+    expect(paragraphComponent).toBeTruthy();
+    expect(paragraphComponent.component).toBeNull();
+    paragraphComponent.bootstrap();
+    expect(paragraphComponent.component).toEqual(getParagraphComponentCmsData());
+  });
+
+  it('should contain cms content in the html rendering after bootstrap', () => {
+    expect(paragraphComponent).toBeTruthy();
+    expect(paragraphComponent.component).toBeNull();
+    paragraphComponent.bootstrap();
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Arbitrary paragraph content');
   });
 });
