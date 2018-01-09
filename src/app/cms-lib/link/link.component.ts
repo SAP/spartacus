a80601691745d7c10b1869d73c3f23767263eade
Kenny Liu <xiao.liu@arvatosystems.com>
2018-01-09 17:54:09 -0500

    SPA-48 LinkModule (CmsLibModule) - Validate, refactor, add tests

--- src/app/cms-lib/link/link.component.ts
+++ src/app/cms-lib/link/link.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractCmsComponent } from '../abstract-cms-component';
+import { AbstractCmsComponent } from '../../newcms/components/abstract-cms-component';
 
 @Component({
   selector: 'y-link',
