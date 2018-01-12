091d124644890602b56167eec63a0ebe54fdac8f
James Sarkar <james9999994@gmail.com>
2018-01-12 09:25:39 -0500

    SPA-44 NavigationModule (CmsLibModule) - Validate, refactor, add tests

--- src/app/cms-lib/navigation/navigation.component.ts
+++ src/app/cms-lib/navigation/navigation.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { AbstractCmsComponent } from '../abstract-cms-component';
+import { AbstractCmsComponent } from '../../newcms/components/abstract-cms-component';
 import { CmsService } from '../../data/cms.service';
 import { NavigationService } from './navigation.service';
 import { Router } from '@angular/router';
+import { ConfigService } from '../../config.service';
+import { Store } from "@ngrx/store";
+import * as fromStore from "../../newcms/store";
 
 @Component({
-  selector: 'y-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'y-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [NavigationService, ConfigService]
 })
-export class NavigationComponent extends AbstractCmsComponent  {
+export class NavigationComponent extends AbstractCmsComponent {
 
     node;
 
     constructor(
         protected cd: ChangeDetectorRef,
-        protected cmsService: CmsService,
-        private navigationService: NavigationService
+        protected navigationService: NavigationService,
+        private store1: Store<fromStore.CmsState>,
+        private config1: ConfigService,
     ) {
-        super(cd, cmsService);
+        super(cd, store1, config1);
     }
 
     protected fetchData() {
         if (!this.component) {
             return;
         }
+
         const data = this.component.navigationNode ? this.component.navigationNode : this.component;
         this.node = this.navigationService.createNode(data);
         this.cd.detectChanges();
