cbffbc96c129a11f866b565cab228ff40c4d7b89
Weizheng Gao <weizheng.gao@sap.com>
2018-01-12 11:56:12 -0500

    feature/SPA-206: add unit tests

--- src/app/product/converters/product-image-converter.service.ts
+++ src/app/product/converters/product-image-converter.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ConfigService } from '../../config.service';
+import { ConfigService } from '../../newocc/config.service';
 
 @Injectable()
 export class ProductImageConverterService {
