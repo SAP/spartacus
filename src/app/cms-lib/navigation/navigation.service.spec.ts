d200593491c95630db5fa84715f408eeca0a6bd4
James Sarkar <James.Sarkar@sap.com>
2018-01-15 09:21:39 -0500

    SPA-44 Added unit test for navigation service

--- src/app/cms-lib/navigation/navigation.service.spec.ts
+++ src/app/cms-lib/navigation/navigation.service.spec.ts
@@ -1,16 +1,45 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { Component, NgModule } from '@angular/core';
+import { TestBed, inject } from '@angular/core/testing';
+import { ConfigService } from '../../config.service';
 import { NavigationService } from './navigation.service';
 
-describe('Service: Navigation', () => {
+@Component({
+  template: 'test'
+})
+export class TestComponent {}
+
+@NgModule({
+  declarations: [TestComponent],
+  entryComponents: [TestComponent],
+  exports: [TestComponent]
+})
+export class TestModule {}
+
+export class MockConfigService {
+  cmsComponentMapping = {
+    CMSTestComponent: 'TestComponent'
+  };
+}
+
+fdescribe('NavigationService', () => {
+  let navigationService: NavigationService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [NavigationService]
+      imports: [TestModule],
+      providers: [
+        NavigationService,
+        { provide: ConfigService, useClass: MockConfigService }
+      ]
     });
+
+    navigationService = TestBed.get(NavigationService);
   });
 
-  it('should ...', inject([NavigationService], (service: NavigationService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should inject NavigationService',
+    inject([NavigationService], (service: NavigationService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
