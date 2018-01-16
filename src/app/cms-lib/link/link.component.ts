ed5fd4185f1b0d7953467c078475d4bb8fcdc964
Weizheng Gao <weizheng.gao@sap.com>
2018-01-16 13:50:22 -0500

    feature/SPA-44

--- src/app/cms-lib/link/link.component.ts
+++ src/app/cms-lib/link/link.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { AbstractCmsComponent } from '../../newcms/components/abstract-cms-component';
 
 @Component({
   selector: 'y-link',
   templateUrl: './link.component.html',
-  styleUrls: ['./link.component.scss']
+  styleUrls: ['./link.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LinkComponent extends AbstractCmsComponent {}
