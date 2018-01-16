14443d6c6fb698a4aa5b9b2988846a7a8b5ca057
James Sarkar <James.Sarkar@sap.com>
2018-01-16 09:22:00 -0500

    SPA-44 Refined unit test for navigation service

--- src/app/cms-lib/navigation/navigation.service.spec.ts
+++ src/app/cms-lib/navigation/navigation.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { ConfigService } from '../../newcms/config.service';
 import { NavigationService } from './navigation.service';
 import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 fdescribe('NavigationService', () => {
   let navigationService: NavigationService;
@@ -52,6 +53,62 @@ fdescribe('NavigationService', () => {
     }
   ];
 
+  const mockedNode = {
+    uid: 'MockNavigationComponent',
+    typeCode: 'NavigationComponent',
+    modifiedTime: '2017-12-21T18:15:15+0000',
+    name: 'TestNavigationComponent',
+    container: 'false',
+    type: 'Navigation Component',
+    styleClass: 'nav-order-tools',
+    navigationNode: [
+      {
+        uid: 'MockNavigationNode001',
+        children: [
+          {
+            uid: 'MockChildNode001',
+            entries: [
+              {
+                item: [
+                  {
+                    external: false,
+                    linkName: 'MockLinkName001',
+                    target: 'SAMEWINDOW',
+                    url: '/mockLinkName001'
+                  }
+                ],
+                itemId: 'MockLink001'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        uid: 'MockNavigationNode002',
+        children: [
+          {
+            uid: 'MockChildNode002',
+            entries: [
+              {
+                item: [
+                  {
+                    external: false,
+                    linkName: 'MockLinkName002',
+                    target: 'SAMEWINDOW',
+                    url: '/mockLinkName002'
+                  }
+                ],
+                itemId: 'MockLink002'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  const mockNodeObservable = Observable.of(mockedNode);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [NavigationService]
@@ -61,7 +118,7 @@ fdescribe('NavigationService', () => {
   });
 
   beforeEach(() => {
-    spyOn(navigationService, 'createNode').and.callThrough();
+    spyOn(navigationService, 'createNode').and.returnValue(of(mockedNode));
   });
 
   it(
@@ -72,7 +129,7 @@ fdescribe('NavigationService', () => {
   );
 
   it('should create a new navigation node', () => {
-    navigationService.createNode(mockedData);
-    expect(navigationService.createNode).toHaveBeenCalled();
+    const node = navigationService.createNode(mockedData);
+    expect(node).toEqual(mockNodeObservable);
   });
 });
