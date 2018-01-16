bed8b612184e35644448442bdc68853a104ca607
James Sarkar <James.Sarkar@sap.com>
2018-01-16 08:46:16 -0500

    SPA-44 Refactored navigation service

--- src/app/cms-lib/navigation/navigation.service.ts
+++ src/app/cms-lib/navigation/navigation.service.ts
@@ -5,18 +5,18 @@ export class NavigationService {
   constructor() {}
 
   public createNode(data) {
-    let node = {};
-    let title = this.getLinkName(data);
+    const node = {};
+    const title = this.getLinkName(data);
     if (title) {
       node['title'] = title;
     }
 
-    let url = this.getUrl(data);
+    const url = this.getUrl(data);
     if (url) {
       node['url'] = url;
     }
 
-    let childs = this.createChilds(data);
+    const childs = this.createChilds(data);
     if (childs) {
       node['childs'] = childs;
     }
@@ -27,24 +27,24 @@ export class NavigationService {
     if (!node.children) {
       return;
     }
-    let childs = [];
-    for (let child of node.children) {
-      let childNode = this.createNode(child);
+    const childs = [];
+    for (const child of node.children) {
+      const childNode = this.createNode(child);
       childs.push(childNode);
     }
     return childs;
   }
 
   private getUrl(child): string {
-    let link = this.getLink(child);
+    const link = this.getLink(child);
     return link ? link.url : '';
   }
 
   private getLinkName(node) {
     let linkName = '';
-    let link = this.getLink(node);
+    const link = this.getLink(node);
     if (link) {
-      linkName = link.linkName ? link.linkName : link.title;
+      linkName = link.linkItem.linkName ? link.linkItem.linkName : link.itemId;
     } else if (node.title) {
       linkName = node.title;
     }
@@ -53,7 +53,7 @@ export class NavigationService {
 
   private getLink(child) {
     if (child.entries && child.entries.length > 0) {
-      return child.entries[0].item;
+      return child.entries[0];
     } else {
       return;
     }
