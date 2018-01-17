6398914e90b65c8ae2559be802ef6357fa91dca0
James Sarkar <James.Sarkar@sap.com>
2018-01-17 12:09:21 -0500

    SPA-39 SearchBoxModule (CmsLibModule) - Validate, refactor, add tests

--- src/app/cms-lib/search-box/search-box.component.spec.ts
+++ src/app/cms-lib/search-box/search-box.component.spec.ts
@@ -1,25 +1,92 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StoreModule, Store, combineReducers } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs/observable/of';
+import * as fromRoot from '../../routing/store';
+import * as fromCmsReducer from '../../newcms/store/reducers';
 import { SearchBoxComponent } from './search-box.component';
+import { ConfigService } from '../../newcms/config.service';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { PictureComponent } from '../../ui/components/media/picture/picture.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('SearchboxComponent', () => {
-  let component: SearchBoxComponent;
+export class UseConfigService {
+  cmsComponentMapping = {
+    CMSParagraphComponent: 'ParagraphComponent'
+  };
+}
+
+fdescribe('CmsParagraphComponent in CmsLib', () => {
+  let store: Store<fromCmsReducer.CmsState>;
+  let searchBoxComponent: SearchBoxComponent;
   let fixture: ComponentFixture<SearchBoxComponent>;
+  let el: DebugElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchBoxComponent ]
+  const mockSearchBoxComponentData = {
+    uid: '001',
+    typeCode: 'SearchBoxComponent',
+    modifiedTime: '2017-12-21T18:15:15+0000',
+    name: 'Mock SearchBox',
+    type: 'SearchBox Component',
+    displayProductImages: 'true',
+    displayProducts: 'true',
+    displaySuggestions: 'true',
+    maxProducts: '5',
+    maxSuggestions: '5',
+    minCharactersBeforeRequest: '3',
+    waitTimeBeforeRequest: '500'
+  };
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          BrowserAnimationsModule,
+          FormsModule,
+          MatAutocompleteModule,
+          MatChipsModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatSelectModule,
+          ReactiveFormsModule,
+          RouterTestingModule,
+          StoreModule.forRoot({
+            ...fromRoot.reducers,
+            cms: combineReducers(fromCmsReducer.reducers)
+          })
+        ],
+        declarations: [SearchBoxComponent, PictureComponent],
+        providers: [{ provide: ConfigService, useClass: UseConfigService }]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    searchBoxComponent = fixture.componentInstance;
+
+    el = fixture.debugElement;
+
+    store = TestBed.get(Store);
+    spyOn(store, 'select').and.returnValue(of(mockSearchBoxComponentData));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(searchBoxComponent).toBeTruthy();
+  });
+
+  it('should contain cms content in the html rendering after bootstrap', () => {
+    expect(searchBoxComponent.component).toBeNull();
+    searchBoxComponent.bootstrap();
+    expect(searchBoxComponent.component).toBe(mockSearchBoxComponentData);
+
+    // TODO: after replacing material with boothstrap4, need some ui test here
   });
 });
