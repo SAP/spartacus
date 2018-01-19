74b5b7dcface4f189ad9285db2c3d6aa2f1c9164
Eric Maxwell <eric.maxwell@softrams.com>
2018-01-19 03:28:10 -0600

    SPA-37: Updates

--- src/app/cms-lib/category-navigation/category-navigation.component.ts
+++ src/app/cms-lib/category-navigation/category-navigation.component.ts
@@ -1,5 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { AbstractCmsComponent } from '../abstract-cms-component';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { AbstractCmsComponent } from '../../newcms/components/abstract-cms-component';
+import { ConfigService } from '../../newcms/config.service';
+import { Store } from '@ngrx/store';
+import * as fromStore from '../../newcms/store';
 
 @Component({
   selector: 'y-category-navigation',
@@ -11,16 +14,24 @@ export class CategoryNavigationComponent extends AbstractCmsComponent {
 
     nodes = [];
 
+  constructor(
+    protected cd: ChangeDetectorRef,
+    protected store: Store<fromStore.CmsState>,
+    protected config: ConfigService
+  ) {
+    super(cd, store, config);
+  }
+
     protected fetchData() {
         // navigationnodes contain children of ordinary type cms components
         // we thread them simmilar and use the cms model service to store them
         if (this.component && this.component.navigationNode && this.component.navigationNode.children) {
             for (const node of this.component.navigationNode.children) {
-                this.cmsService.storeComponent(node.uid, node);
+                // this.cmsService.storeComponent(node.uid, node);
                 this.nodes.push(node);
             }
         }
-        super.fetchData();
+     //   super.fetchData();
     }
 
 
