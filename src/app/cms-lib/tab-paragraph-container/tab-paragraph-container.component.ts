379468620236bade99d5d14b5f1de6c3326d9b7b
Eric Maxwell <eric.maxwell@softrams.com>
2018-01-23 08:41:32 -0600

    SPA-38: Code coverage is at 100%

--- src/app/cms-lib/tab-paragraph-container/tab-paragraph-container.component.ts
+++ src/app/cms-lib/tab-paragraph-container/tab-paragraph-container.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AbstractProductComponent } from '../abstract-product-component';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'y-tab-paragraph-container',
@@ -8,40 +7,5 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
   styleUrls: ['./tab-paragraph-container.component.scss']
 })
 export class TabParagraphContainerComponent extends AbstractProductComponent {
-    // @ViewChild('tabContent') tabContent: Element;
 
-    // fetchData() {
-        
-    //     // console.log(this.contextParameters.tab);
-    //     // this.contextParameters.tab.title = new BehaviorSubject<any>({});
-    //     // this.contextParameters.tab.title.next('test');
-    //     // this.contextParameters.tab.title.complete();
-        
-    //     // const s = new AsyncSubject<any>();
-    //     // this.occProductSearchService.query(query)
-    //     //     .then((pageData) => {
-    //     //         s.next(pageData);
-    //     //         s.complete();
-    //     // });
-    //     // return s;
-
-
-    //     // if (!this.contextParameters.tab) {
-    //     //     this.contextParameters.tab = {};
-    //     // }
-    //     // this.contextParameters.tab.title = this.component.title;
-    //     // console.log(this.contextParameters.tab);
-    //     // this.cd.detectChanges();
-    //     // console.log(this.component.title);
-    //     // console.log(this.tabContent);
-    //     // console.log(this.contextParameters);
-    //     // this.contextParameters);
-    // }
-
-
-    // ngOnInit() {
-    //     this.contextParameters.tabTitle.next('test');
-    //     this.cd.detectChanges();
-    //     // super.fetchData();
-    // }
 }
