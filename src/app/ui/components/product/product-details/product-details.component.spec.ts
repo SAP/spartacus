3f5f8ae68d5d06606cc32967dab9b6006d020d9e
James Sarkar <James.Sarkar@sap.com>
2018-01-23 12:03:15 -0500

    SPA-221 Added unit test for product details component

--- src/app/ui/components/product/product-details/product-details.component.spec.ts
+++ src/app/ui/components/product/product-details/product-details.component.spec.ts
@@ -1,25 +1,59 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductDetailsComponent } from './product-details.component';
+import { MaterialModule } from 'app/material.module';
+import { DynamicSlotComponent } from 'app/newcms/components';
+import { ProductReviewsComponent } from 'app/ui/components/product/product-reviews/product-reviews.component';
+import { StarRatingComponent } from 'app/ui/components/product/star-rating/star-rating.component';
+import { ProductImagesComponent } from 'app/ui/components/product/product-images/product-images.component';
+import { ProductSummaryComponent } from 'app/ui/components/product/product-summary/product-summary.component';
+import { ProductAttributesComponent } from 'app/ui/components/product/product-attributes/product-attributes.component';
+import { PictureComponent } from 'app/ui/components/media/picture/picture.component';
+import { ComponentWrapperComponent } from 'app/cms/component-wrapper/component-wrapper.component';
+import { ProductLoaderService } from 'app/data/product-loader.service';
+import * as fromRoot from '../../../../routing/store';
+import * as fromCmsReducer from '../../../../newcms/store/reducers';
+import { StoreModule, combineReducers } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfigService } from '../../../../newcms/config.service';
 
-describe('ProductDetailsComponent', () => {
-  let component: ProductDetailsComponent;
+fdescribe('ProductDetailsComponent in ui', () => {
+  let productDetailsComponent: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            ...fromRoot.reducers,
+            cms: combineReducers(fromCmsReducer.reducers)
+          }),
+          RouterTestingModule,
+          MaterialModule
+        ],
+        declarations: [
+          ProductDetailsComponent,
+          StarRatingComponent,
+          ProductImagesComponent,
+          ProductSummaryComponent,
+          ProductAttributesComponent,
+          ProductReviewsComponent,
+          DynamicSlotComponent,
+          ComponentWrapperComponent,
+          PictureComponent
+        ],
+        providers: [{ provide: ProductLoaderService }]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    productDetailsComponent = fixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(productDetailsComponent).toBeTruthy();
   });
 });
