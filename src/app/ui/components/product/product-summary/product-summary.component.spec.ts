68428b5bb451c6103bf9cc31451a614f781ee90a
James Sarkar <James.Sarkar@sap.com>
2018-01-24 13:27:37 -0500

    SPA-224 Refactor and add tests to PDP tab Summary component

--- src/app/ui/components/product/product-summary/product-summary.component.spec.ts
+++ src/app/ui/components/product/product-summary/product-summary.component.spec.ts
@@ -3,17 +3,31 @@ import { ProductSummaryComponent } from './product-summary.component';
 import { ProductLoaderService } from 'app/data/product-loader.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'app/material.module';
+import { of } from 'rxjs/observable/of';
+
+class MockProductLoaderService {
+  loadProduct(productCode) {}
+  getSubscription(productCode) {}
+}
 
 fdescribe('ProductSummaryComponent in ui', () => {
   let productSummaryComponent: ProductSummaryComponent;
   let fixture: ComponentFixture<ProductSummaryComponent>;
+  let productLoader: ProductLoaderService;
+
+  const componentData = 'Mock data for product summary component.';
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         imports: [RouterTestingModule, MaterialModule],
         declarations: [ProductSummaryComponent],
-        providers: [{ provide: ProductLoaderService }]
+        providers: [
+          {
+            provide: ProductLoaderService,
+            useClass: MockProductLoaderService
+          }
+        ]
       }).compileComponents();
     })
   );
@@ -21,9 +35,18 @@ fdescribe('ProductSummaryComponent in ui', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductSummaryComponent);
     productSummaryComponent = fixture.componentInstance;
+    productLoader = TestBed.get(ProductLoaderService);
+
+    spyOn(productLoader, 'getSubscription').and.returnValue(of(componentData));
   });
 
   it('should be created', () => {
     expect(productSummaryComponent).toBeTruthy();
+  });
+
+  it('should call ngOnChanges()', () => {
+    productSummaryComponent.productCode = '123456';
+    productSummaryComponent.ngOnChanges();
+    expect(productSummaryComponent.model).toEqual(componentData);
   });
 });
