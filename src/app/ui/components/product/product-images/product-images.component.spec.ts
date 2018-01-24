94376826c9ce3e82da8fe3b6b1f5a017cb3a7eda
James Sarkar <James.Sarkar@sap.com>
2018-01-24 13:33:58 -0500

    SPA-222 Refactor and add tests to PDP tab Images component

--- src/app/ui/components/product/product-images/product-images.component.spec.ts
+++ src/app/ui/components/product/product-images/product-images.component.spec.ts
@@ -1,25 +1,73 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductImagesComponent } from './product-images.component';
+import { PictureComponent } from 'app/ui/components/media/picture/picture.component';
+import { ProductLoaderService } from 'app/data/product-loader.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs/observable/of';
 
-describe('ProductImagesComponent', () => {
-  let component: ProductImagesComponent;
+class MockImages {
+  PRIMARY = ['image1', 'image2'];
+}
+
+class MockModel {
+  images;
+}
+
+class MockProductLoaderService {
+  loadProduct(productCode) {}
+  getSubscription(productCode) {}
+}
+
+fdescribe('ProductImagesComponent in ui', () => {
+  let productImagesComponent: ProductImagesComponent;
   let fixture: ComponentFixture<ProductImagesComponent>;
+  let productLoader: ProductLoaderService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProductImagesComponent ]
+  const mockImages: any[] = ['image1', 'image2'];
+  const componentData = 'Mock data for product images component.';
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [RouterTestingModule],
+        declarations: [ProductImagesComponent, PictureComponent],
+        providers: [
+          { provide: ProductLoaderService, useClass: MockProductLoaderService }
+        ]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductImagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    productImagesComponent = fixture.componentInstance;
+    productLoader = TestBed.get(ProductLoaderService);
+
+    productImagesComponent.model = new MockModel();
+    productImagesComponent.model.images = new MockImages();
+
+    spyOn(productImagesComponent, 'ready').and.callThrough();
+    spyOn(productImagesComponent, 'showImage').and.callThrough();
+    spyOn(productLoader, 'getSubscription').and.returnValue(of(componentData));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(productImagesComponent).toBeTruthy();
+  });
+
+  it('should call showImage(imageContainer)', () => {
+    productImagesComponent.showImage('mockImageContainer');
+    expect(productImagesComponent.mainImage).toBe('mockImageContainer');
+  });
+
+  it('should call ready()', () => {
+    productImagesComponent.ready();
+    expect(productImagesComponent.mainImage).toEqual(mockImages);
+  });
+
+  it('should call ngOnChanges()', () => {
+    productImagesComponent.productCode = '123456';
+    productImagesComponent.ngOnChanges();
+    expect(productImagesComponent.model).toEqual(componentData);
   });
 });
