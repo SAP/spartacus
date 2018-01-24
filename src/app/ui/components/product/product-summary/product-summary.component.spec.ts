8660ad04fafc59cecaac485faec2e29f0468c13e
James Sarkar <James.Sarkar@sap.com>
2018-01-24 10:26:05 -0500

    SPA-224 Refactor and add tests to PDP tab Summary component

--- src/app/ui/components/product/product-summary/product-summary.component.spec.ts
+++ src/app/ui/components/product/product-summary/product-summary.component.spec.ts
@@ -1,25 +1,74 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductSummaryComponent } from './product-summary.component';
+import { MaterialModule } from 'app/material.module';
+import { DynamicSlotComponent } from 'app/newcms/components';
+import { ProductReviewsComponent } from 'app/ui/components/product/product-reviews/product-reviews.component';
+import { StarRatingComponent } from 'app/ui/components/product/star-rating/star-rating.component';
+import { ProductImagesComponent } from 'app/ui/components/product/product-images/product-images.component';
+import { ProductDetailsComponent } from 'app/ui/components/product/product-details/product-details.component';
+import { ProductAttributesComponent } from 'app/ui/components/product/product-attributes/product-attributes.component';
+import { PictureComponent } from 'app/ui/components/media/picture/picture.component';
+import { ComponentWrapperComponent } from 'app/cms/component-wrapper/component-wrapper.component';
+import { ProductLoaderService } from 'app/data/product-loader.service';
+import * as fromRoot from '../../../../routing/store';
+import * as fromCmsReducer from '../../../../newcms/store/reducers';
+import { StoreModule, combineReducers } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfigService } from '../../../../newcms/config.service';
+import { By } from '@angular/platform-browser';
 
-describe('ProductSummaryComponent', () => {
-  let component: ProductSummaryComponent;
+export class MocksStock {
+  stockLevelStatus = 'Dummy Stock Level Status';
+  stockLevel = 'Dummy Stock Level';
+}
+
+export class MockPrice {
+  formattedValue = '$123';
+}
+export class MockModel {
+  price = '123';
+  stock = new MocksStock();
+}
+
+fdescribe('ProductSummaryComponent in ui', () => {
+  let productSummaryComponent: ProductSummaryComponent;
   let fixture: ComponentFixture<ProductSummaryComponent>;
+  const mockModel = new MockModel();
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProductSummaryComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            ...fromRoot.reducers,
+            cms: combineReducers(fromCmsReducer.reducers)
+          }),
+          RouterTestingModule,
+          MaterialModule
+        ],
+        declarations: [
+          ProductDetailsComponent,
+          StarRatingComponent,
+          ProductImagesComponent,
+          ProductSummaryComponent,
+          ProductAttributesComponent,
+          ProductReviewsComponent,
+          DynamicSlotComponent,
+          ComponentWrapperComponent,
+          PictureComponent
+        ],
+        providers: [{ provide: ProductLoaderService }]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductSummaryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    productSummaryComponent = fixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(productSummaryComponent).toBeTruthy();
   });
 });
