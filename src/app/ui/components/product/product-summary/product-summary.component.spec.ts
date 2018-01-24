eeea6a0d554f2e459d526a90d786f5f29f026805
James Sarkar <James.Sarkar@sap.com>
2018-01-24 12:18:47 -0500

    SPA-224 Code cleanup

--- src/app/ui/components/product/product-summary/product-summary.component.spec.ts
+++ src/app/ui/components/product/product-summary/product-summary.component.spec.ts
@@ -1,22 +1,8 @@
-import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductSummaryComponent } from './product-summary.component';
-import { MaterialModule } from 'app/material.module';
-import { DynamicSlotComponent } from 'app/newcms/components';
-import { ProductReviewsComponent } from 'app/ui/components/product/product-reviews/product-reviews.component';
-import { StarRatingComponent } from 'app/ui/components/product/star-rating/star-rating.component';
-import { ProductImagesComponent } from 'app/ui/components/product/product-images/product-images.component';
-import { ProductDetailsComponent } from 'app/ui/components/product/product-details/product-details.component';
-import { ProductAttributesComponent } from 'app/ui/components/product/product-attributes/product-attributes.component';
-import { PictureComponent } from 'app/ui/components/media/picture/picture.component';
-import { ComponentWrapperComponent } from 'app/cms/component-wrapper/component-wrapper.component';
 import { ProductLoaderService } from 'app/data/product-loader.service';
-import * as fromRoot from '../../../../routing/store';
-import * as fromCmsReducer from '../../../../newcms/store/reducers';
-import { StoreModule, combineReducers } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ConfigService } from '../../../../newcms/config.service';
-import { By } from '@angular/platform-browser';
+import { MaterialModule } from 'app/material.module';
 
 fdescribe('ProductSummaryComponent in ui', () => {
   let productSummaryComponent: ProductSummaryComponent;
@@ -26,17 +12,7 @@ fdescribe('ProductSummaryComponent in ui', () => {
     async(() => {
       TestBed.configureTestingModule({
         imports: [RouterTestingModule, MaterialModule],
-        declarations: [
-          ProductDetailsComponent,
-          StarRatingComponent,
-          ProductImagesComponent,
-          ProductSummaryComponent,
-          ProductAttributesComponent,
-          ProductReviewsComponent,
-          DynamicSlotComponent,
-          ComponentWrapperComponent,
-          PictureComponent
-        ],
+        declarations: [ProductSummaryComponent],
         providers: [{ provide: ProductLoaderService }]
       }).compileComponents();
     })
