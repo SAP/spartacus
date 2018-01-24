06c9ee762bd6731357a77e984963f1131da98af2
James Sarkar <James.Sarkar@sap.com>
2018-01-24 09:26:58 -0500

    SPA-221 Refactor and add tests to PDP tab Details component

--- src/app/ui/components/product/product-details/product-details.component.ts
+++ src/app/ui/components/product/product-details/product-details.component.ts
@@ -20,8 +20,8 @@ import { MatTabChangeEvent } from '@angular/material';
 export class ProductDetailsComponent extends AbstractProductComponent {
   selectedIndex = 0;
 
-  // This is for opening the reviews tab when the review button on top
-  // of the product image is clicked
+  // This is for opening the reviews tab when the review button above
+  // the product image is clicked
   selectedIndexChange(val) {
     this.selectedIndex = val;
   }
