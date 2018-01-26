c9a1e48a79243df8ec1dcd20f14e5b664a24a7ba
James Sarkar <James.Sarkar@sap.com>
2018-01-26 13:17:59 -0500

    SPA-221 Refactored unit test

--- src/app/ui/components/product/product-details/product-details.component.spec.ts
+++ src/app/ui/components/product/product-details/product-details.component.spec.ts
@@ -9,25 +9,19 @@ import { ProductSummaryComponent } from 'app/ui/components/product/product-summa
 import { ProductAttributesComponent } from 'app/ui/components/product/product-attributes/product-attributes.component';
 import { PictureComponent } from 'app/ui/components/media/picture/picture.component';
 import { ComponentWrapperComponent } from 'app/cms/component-wrapper/component-wrapper.component';
-import { ProductLoaderService } from 'app/data/product-loader.service';
 import * as fromRoot from '../../../../routing/store';
 import * as fromCmsReducer from '../../../../newcms/store/reducers';
 import { StoreModule, Store, combineReducers } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs/observable/of';
-
-class MockProductLoaderService {
-  loadProduct(productCode) {}
-  getSubscription(productCode) {}
-}
+import { CmsService } from 'app/data/cms.service';
 
 fdescribe('ProductDetailsComponent in ui', () => {
   let store: Store<fromCmsReducer.CmsState>;
   let productDetailsComponent: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
-  let productLoader: ProductLoaderService;
 
-  const componentData = 'Mock data for product details component.';
+  const mockProduct = ['mockProduct'];
 
   beforeEach(
     async(() => {
@@ -50,12 +44,6 @@ fdescribe('ProductDetailsComponent in ui', () => {
           DynamicSlotComponent,
           ComponentWrapperComponent,
           PictureComponent
-        ],
-        providers: [
-          {
-            provide: ProductLoaderService,
-            useClass: MockProductLoaderService
-          }
         ]
       }).compileComponents();
     })
@@ -64,10 +52,9 @@ fdescribe('ProductDetailsComponent in ui', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductDetailsComponent);
     productDetailsComponent = fixture.componentInstance;
-    productLoader = TestBed.get(ProductLoaderService);
     store = TestBed.get(Store);
 
-    spyOn(productLoader, 'getSubscription').and.returnValue(of(componentData));
+    spyOn(store, 'select').and.returnValue(of(mockProduct));
   });
 
   it('should be created', () => {
@@ -77,6 +64,6 @@ fdescribe('ProductDetailsComponent in ui', () => {
   it('should call ngOnChanges()', () => {
     productDetailsComponent.productCode = '123456';
     productDetailsComponent.ngOnChanges();
-    expect(productDetailsComponent.model).toEqual(componentData);
+    expect(productDetailsComponent.model).toEqual(mockProduct);
   });
 });
