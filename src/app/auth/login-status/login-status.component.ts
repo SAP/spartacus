6be701ca54c09e0b5ecddab16b7158efe2109937
James Sarkar <James.Sarkar@sap.com>
2018-02-07 15:51:16 -0500

    SPA-205 Cleaned imports

--- src/app/auth/login-status/login-status.component.ts
+++ src/app/auth/login-status/login-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
@@ -24,7 +24,7 @@ export class LoginStatusComponent {
   }
 
   openLogin() {
-    const dialogRef = this.dialog.open(LoginDialogComponent);
+    this.dialog.open(LoginDialogComponent);
   }
 
   logout() {
