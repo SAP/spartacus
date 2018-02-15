1324b9f4ac39b97c7e20575956f969d58c0a849a
James Sarkar <James.Sarkar@sap.com>
2018-02-15 09:29:05 -0500

    SPA-298 Organized imports

--- src/app/auth/guards/auth.guard.spec.ts
+++ src/app/auth/guards/auth.guard.spec.ts
@@ -1,9 +1,6 @@
-import { of } from 'rxjs/observable/of';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AuthGuard } from './auth.guard';
 
-import * as fromRoot from './../../routing/store';
+import { AuthGuard } from './auth.guard';
 
 const mockUserValidToken = {
     token: {
--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-
 import { Observable } from 'rxjs/Observable';
-
 import { of } from 'rxjs/observable/of';
-import { isEmpty } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
