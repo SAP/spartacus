d3bd01b9396ec039602c1c95443b7b4d91889c1f
James Sarkar <James.Sarkar@sap.com>
2018-02-21 11:16:28 -0500

    SPA-273 / SPA-277 Removed a comment

--- src/app/auth/store/actions/login-logout.action.ts
+++ src/app/auth/store/actions/login-logout.action.ts
@@ -3,8 +3,6 @@ import { Action } from '@ngrx/store';
 export const LOGIN = '[Auth] Login';
 export const LOGOUT = '[Auth] Logout';
 
-// Not sure if we need this action. We already have LoadUserDetails and
-// LoadUserToken for login
 export class Login implements Action {
   readonly type = LOGIN;
   constructor(public payload: { username: string; password: string }) {}
