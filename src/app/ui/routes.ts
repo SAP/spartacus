5befb3e7cf1ae56c9106297ef0ce8b72f6670a58
James Sarkar <James.Sarkar@sap.com>
2018-02-23 16:51:27 -0500

    SPA-234 Cleanup

--- src/app/ui/routes.ts
+++ src/app/ui/routes.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
 import { HomePageComponent } from '../ui/templates/home-page/home-page.component';
 import { CartPageComponent } from '../ui/templates/cart-page/cart-page.component';
