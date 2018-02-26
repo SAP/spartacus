1338deb6e9f79e5b49acde22798b47b7c80d39f1
James Sarkar <James.Sarkar@sap.com>
2018-02-26 14:40:48 -0500

    SPA-301 Added unit test

--- src/app/ui/layout/header/header.component.spec.ts
+++ src/app/ui/layout/header/header.component.spec.ts
@@ -1,21 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import {
+  DynamicSlotComponent,
+  ComponentWrapperComponent
+} from '../../../cms/components';
+import { LoginModule } from '../../../auth/components/login/login.module';
+import { SiteContextModule } from '../../../site-context/site-context.module';
+import { StoreModule, combineReducers, Store } from '@ngrx/store';
+import * as fromRoot from '../../../routing/store';
+import * as fromUserStore from '../../../auth/store';
+import * as fromCMSStore from '../../../cms/store';
 
-describe('HeaderComponent', () => {
+import { EffectsModule } from '@ngrx/effects';
+import { OccSiteService } from '../../../newocc/site-context/occ-site.service';
+import { CurrencySelectorModule } from '../../../site-context/currency-selector/currency-selector.module';
+import { LanguageSelectorModule } from '../../../site-context/language-selector/language-selector.module';
+
+class MockOccSiteService {
+  loadLanguages() {
+    return;
+  }
+
+  loadCurrencies() {
+    return;
+  }
+}
+
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          LoginModule,
+          SiteContextModule,
+          StoreModule.forRoot({
+            ...fromRoot.reducers,
+            cms: combineReducers(fromCMSStore.reducers),
+            user: combineReducers(fromUserStore.reducers)
+          }),
+          EffectsModule.forRoot([])
+        ],
+        declarations: [
+          HeaderComponent,
+          DynamicSlotComponent,
+          ComponentWrapperComponent
+        ],
+        providers: [{ provide: OccSiteService, useClass: MockOccSiteService }]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
