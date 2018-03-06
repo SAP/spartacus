55901e45f507a1ce8ee41da61d28742e73530f13
Weizheng Gao <weizheng.gao@sap.com>
2018-03-06 09:13:44 -0500

    feature/SPA-294: add tests

--- src/app/cms-lib/mini-cart/cart-dialog/cart-dialog.component.spec.ts
+++ src/app/cms-lib/mini-cart/cart-dialog/cart-dialog.component.spec.ts
@@ -1,17 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MaterialModule } from '../../../material.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BannerModule } from '../../banner/banner.module';
 import { CartDialogComponent } from './cart-dialog.component';
 
 describe('CartDialogComponent', () => {
   let component: CartDialogComponent;
   let fixture: ComponentFixture<CartDialogComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CartDialogComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          MaterialModule,
+          FormsModule,
+          BrowserAnimationsModule,
+          RouterModule,
+          BannerModule
+        ],
+        declarations: [CartDialogComponent],
+        providers: [
+          {
+            provide: MatDialogRef
+          },
+          { provide: MAT_DIALOG_DATA, useValue: {} }
+        ]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CartDialogComponent);
