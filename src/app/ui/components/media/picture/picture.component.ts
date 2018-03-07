19fd2a381724335ced88f459ffe708587d95fedc
Weizheng Gao <weizheng.gao@sap.com>
2018-03-07 13:50:58 -0500

    feature/SPA-278

--- src/app/ui/components/media/picture/picture.component.ts
+++ src/app/ui/components/media/picture/picture.component.ts
@@ -3,40 +3,36 @@ import { Component, Input, OnInit } from '@angular/core';
 const DEFAULT_FORMAT = 'product';
 
 @Component({
-    selector: 'y-picture',
-    templateUrl: './picture.component.html',
-    styleUrls: ['./picture.component.scss']
+  selector: 'y-picture',
+  templateUrl: './picture.component.html',
+  styleUrls: ['./picture.component.scss']
 })
 export class PictureComponent implements OnInit {
+  @Input() imageContainer;
+  @Input() imageFormat;
+  @Input() imagePosition;
 
-    @Input() imageContainer;
-    @Input() imageFormat;
-    @Input() imagePosition;
+  mainImage;
+  // imageTypes = {
+  //     xs: 'xs',
+  //     sm: 'thumbnail',
+  //     md: 'md',
+  //     lg: 'lg'
+  // };
 
-    mainImage;
-    // imageTypes = {
-    //     xs: 'xs',
-    //     sm: 'thumbnail',
-    //     md: 'md',
-    //     lg: 'lg'
-    // };
+  // xsImage;
+  // smImage;
+  // mdImage;
+  // lgImage;
 
-    // xsImage;
-    // smImage;
-    // mdImage;
-    // lgImage;
+  constructor() {}
 
-    constructor() {}
-
-    ngOnInit() {
-
-        if (this.imageContainer) {
-            const image = this.imageContainer[this.imageFormat || DEFAULT_FORMAT];
-            if (image && image.url) {
-                this.mainImage = image.url;
-            }
-        }
-
+  ngOnInit() {
+    if (this.imageContainer) {
+      const image = this.imageContainer[this.imageFormat || DEFAULT_FORMAT];
+      if (image && image.url) {
+        this.mainImage = image.url;
+      }
     }
-
+  }
 }
