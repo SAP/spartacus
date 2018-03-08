0b2faea46b474bbbb0f0ea939ae86322273de0df
James Sarkar <James.Sarkar@sap.com>
2018-03-08 08:57:10 -0500

    SPA-278 Fixed an issue and added unit tests

--- src/app/product/components/product-list/container/product-list.component.ts
+++ src/app/product/components/product-list/container/product-list.component.ts
@@ -60,7 +60,17 @@ export class ProductListComponent implements OnChanges, OnInit {
     );
   }
 
-  ngOnChanges() {}
+  ngOnChanges() {
+    if (this.categoryCode) {
+      this.query = ':relevance:category:' + this.categoryCode;
+    }
+    if (this.brandCode) {
+      this.query = ':relevance:brand:' + this.brandCode;
+    }
+    if (this.query) {
+      this.search(this.query);
+    }
+  }
 
   toggleSidenav() {
     this.sidenav.toggle();
