dfbdfe22165b44d1d9d04d9f1a714f038604cac0
Weizheng Gao <weizheng.gao@sap.com>
2018-03-08 09:07:36 -0500

    feature/SPA-278

--- src/app/ui/layout/cart-page-layout/cart-page-layout.component.ts
+++ src/app/ui/layout/cart-page-layout/cart-page-layout.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CartDetailsComponent } from '../../../cart/components/cart-details/cart-details.component';
 
 @Component({
   selector: 'y-cart-page-layout',
   templateUrl: './cart-page-layout.component.html',
   styleUrls: ['./cart-page-layout.component.scss']
 })
-export class CartPageLayoutComponent {}
+export class CartPageLayoutComponent implements AfterViewInit {
+  @ViewChild(CartDetailsComponent) cartDetail: CartDetailsComponent;
+
+  cart: any;
+
+  ngAfterViewInit() {
+    this.cartDetail.cart$.subscribe(data => (this.cart = data));
+  }
+}
