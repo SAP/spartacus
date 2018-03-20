cb0190a54abc3a4579ef98c36f6a1d6f76f4ec98
James Sarkar <James.Sarkar@sap.com>
2018-03-20 12:47:00 -0400

    SPA-351 Some more refactoring

--- src/app/cms-lib/navigation/navigation.service.ts
+++ src/app/cms-lib/navigation/navigation.service.ts
@@ -8,64 +8,25 @@ export class NavigationService {
 
   public createNode(data) {
     const uid = data.uid;
-    const childList = [];
-    // if (data.children !== undefined) {
-    //   this.createChilds(data);
-    // }
-
-    // const itemId = this.getItemId(data);
-    // const itemSuperType = this.getItemSuperType(data);
-
-    // links.push({ superType: itemSuperType, id: itemId });
-
-    // this.store.dispatch(
-    //   new fromStore.LoadNavigationItems({
-    //     nodeId: uid,
-    //     items: links
-    //   })
-    // );
-    // console.log(uid);
-    // console.log(links);
+    const itemsList = [];
 
     if (data.children) {
+      for (const child of data.children) {
+        if (child.entries && child.entries.length > 0) {
+          for (const entry of child.entries) {
+            itemsList.push({
+              superType: entry.itemSuperType,
+              id: entry.itemId
+            });
+          }
+        }
+      }
     }
+    this.store.dispatch(
+      new fromStore.LoadNavigationItems({
+        nodeId: uid,
+        items: itemsList
+      })
+    );
   }
-
-  // private createChilds(node) {
-  //   const childs = [];
-
-  //   for (const child of node.children) {
-  //     this.createNode(child, node.uid);
-  //   }
-  // }
-
-  // private getItemSuperType(node) {
-  //   let superType = '';
-  //   const link = this.getLink(node);
-  //   if (link) {
-  //     superType = link.itemSuperType;
-  //   } else if (node.itemSuperType) {
-  //     superType = node.itemSuperType;
-  //   }
-  //   return superType;
-  // }
-
-  // private getItemId(node) {
-  //   let id = '';
-  //   const link = this.getLink(node);
-  //   if (link) {
-  //     id = link.itemId;
-  //   } else if (node.title) {
-  //     id = node.title;
-  //   }
-  //   return id;
-  // }
-
-  // private getLink(child) {
-  //   if (child && child.entries && child.entries.length > 0) {
-  //     return child.entries[0];
-  //   } else {
-  //     return;
-  //   }
-  // }
 }
