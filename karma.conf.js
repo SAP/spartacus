c6abb3a661147933dd258b4eb4b1364b4b094ff7
James Sarkar <James.Sarkar@sap.com>
2018-04-04 14:12:44 -0400

    SPA-379 Formatted code

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -15,9 +15,7 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
+    files: [{ pattern: './src/test.ts', watched: false }],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
     },
@@ -38,9 +36,10 @@ module.exports = function (config) {
       config: './.angular-cli.json',
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+    reporters:
+      config.angularCli && config.angularCli.codeCoverage
+        ? ['progress', 'coverage-istanbul']
+        : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -49,10 +48,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
-        flags: [
-          '--headless',
-          '--remote-debugging-port=9001',
-        ],
+        flags: ['--headless', '--remote-debugging-port=9001']
       }
     },
     singleRun: false
