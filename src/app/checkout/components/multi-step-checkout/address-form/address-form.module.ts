ff851ff6e02fadf51e261c45582ddbf90b6d6152
James Sarkar <James.Sarkar@sap.com>
2018-04-12 14:00:50 -0400

    SPA-262 Fixed lint error

--- src/app/checkout/components/multi-step-checkout/address-form/address-form.module.ts
+++ src/app/checkout/components/multi-step-checkout/address-form/address-form.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../../material.module';
 import { AddressFormComponent } from './address-form.component';
 import { SuggestedAddressDialogComponent } from './suggested-addresses-dialog/suggested-addresses-dialog.component';
