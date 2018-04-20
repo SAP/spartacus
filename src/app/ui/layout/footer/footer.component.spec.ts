eded9982a00ef2a39a18cffdead19ae5eaa3aaf4
James Sarkar <James.Sarkar@sap.com>
2018-04-20 10:14:55 -0400

    SPA-393 Fixed some additional tests

--- src/app/ui/layout/footer/footer.component.spec.ts
+++ src/app/ui/layout/footer/footer.component.spec.ts
@@ -1,15 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import {
+  DynamicSlotComponent,
+  ComponentWrapperComponent
+} from '../../../cms/components';
+import { StoreModule, combineReducers } from '@ngrx/store';
+import * as fromRoot from '../../../routing/store';
+import * as fromCmsReducer from '../../../cms/store/reducers';
 
-xdescribe('FooterComponent', () => {
+describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [FooterComponent]
+        imports: [
+          StoreModule.forRoot({
+            ...fromRoot.reducers,
+            cms: combineReducers(fromCmsReducer.reducers)
+          })
+        ],
+        declarations: [
+          FooterComponent,
+          DynamicSlotComponent,
+          ComponentWrapperComponent
+        ]
       }).compileComponents();
     })
   );
