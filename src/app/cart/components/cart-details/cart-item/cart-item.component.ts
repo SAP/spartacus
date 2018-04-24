3383876b1cb48c573d3e2dddf8d609a92eb7d461
Weizheng Gao <weizheng.gao@sap.com>
2018-04-24 09:53:38 -0400

    feature/SPA-393: some more refactor

--- src/app/cart/components/cart-details/cart-item/cart-item.component.ts
+++ src/app/cart/components/cart-details/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'y-cart-item',
@@ -7,4 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class CartItemComponent {
   @Input() entry: any;
+  @Input() potentialPromotions: any[];
+  @Input() appliedPromotions: any[];
+
+  @Output() remove = new EventEmitter<any>();
+
+  removeEntry() {
+    this.remove.emit(this.entry);
+  }
 }
--- src/app/cart/components/cart-details/container/cart-details.component.ts
+++ src/app/cart/components/cart-details/container/cart-details.component.ts
@@ -30,4 +30,46 @@ export class CartDetailsComponent implements OnInit {
   removeEntry(entry) {
     this.cartService.removeCartEntry(entry);
   }
+
+  getPotentialPromotionForEntry(cart: any, entry: any): any[] {
+    return this.getPromotionForEntry(cart.potentialProductPromotions, entry);
+  }
+
+  getAppliedPromotionForEntry(cart: any, entry: any): any[] {
+    return this.getPromotionForEntry(cart.appliedProductPromotions, entry);
+  }
+
+  private getPromotionForEntry(promotions: any[], entry: number): any {
+    const entryPromotions = [];
+    if (promotions && promotions.length > 0) {
+      for (const promotion of promotions) {
+        if (
+          promotion.description &&
+          promotion.consumedEntries &&
+          promotion.consumedEntries.length > 0
+        ) {
+          for (const consumedEntry of promotion.consumedEntries) {
+            if (this.isConsumedByEntry(consumedEntry, entry)) {
+              entryPromotions.push(promotion);
+            }
+          }
+        }
+      }
+    }
+    return entryPromotions;
+  }
+
+  private isConsumedByEntry(consumedEntry: any, entry: any): boolean {
+    const consumendEntryNumber = consumedEntry.orderEntryNumber;
+    if (entry.entries && entry.entries.length > 0) {
+      for (const subEntry of entry.entries) {
+        if (subEntry.entryNumber === consumendEntryNumber) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return consumendEntryNumber === entry.entryNumber;
+    }
+  }
 }
