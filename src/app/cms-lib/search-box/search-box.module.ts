c9f83e1b290a1a395e63e95d3dc74ad2e61c6778
Weizheng Gao <weizheng.gao@sap.com>
2018-04-30 15:40:19 -0400

    feature/SPA-420: remove Material from categoryNavigation moduel and search-box module

--- src/app/cms-lib/search-box/search-box.module.ts
+++ src/app/cms-lib/search-box/search-box.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-//import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { MediaModule } from '../../ui/components/media/media.module';
-
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { SearchBoxComponent } from './search-box.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    //ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    MediaModule
-  ],
+  imports: [CommonModule, RouterModule, MediaModule],
   declarations: [SearchBoxComponent],
   entryComponents: [SearchBoxComponent],
   exports: [SearchBoxComponent]
