aa775ea990d8bdae934f48da595bb92f2462386b
Patrick Labadie <patrick.labadie@sap.com>
2018-05-09 16:04:44 -0400

    Generated a new lib.

---
+++ projects/storefrontlib/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of storefrontlib
+ */
+
+export * from './lib/storefrontlib.service';
+export * from './lib/storefrontlib.component';
+export * from './lib/storefrontlib.module';
