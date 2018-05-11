0cc0e9b0f3cc78f8af3476030beb793fed6cda4d
Patrick Labadie <patrick.labadie@sap.com>
2018-05-11 17:02:02 -0400

    SPA-434: Add the existing storefront code in the new Angular 6 app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'y-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
