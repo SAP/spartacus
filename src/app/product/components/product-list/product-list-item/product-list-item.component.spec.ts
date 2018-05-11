bc2f17933c25ce8cf090aea0e0b166d4b9d68a57
Eric Xiao <e.xiao@sap.com>
2018-05-11 12:33:32 -0400

    SPA-460: Removed HttpModule from test files because they are not used

--- src/app/product/components/product-list/product-list-item/product-list-item.component.spec.ts
+++ src/app/product/components/product-list/product-list-item/product-list-item.component.spec.ts
@@ -4,7 +4,6 @@ import { ProductListItemComponent } from './product-list-item.component';
 import { PictureComponent } from '../../../../ui/components/media/picture/picture.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AddToCartComponent } from '../../../../cart/components/add-to-cart/add-to-cart.component';
-import { HttpModule } from '@angular/http';
 import { CartService } from '../../../../cart/services';
 import { StoreModule, combineReducers } from '@ngrx/store';
 import * as fromRoot from '../../../../routing/store';
@@ -15,28 +14,25 @@ describe('ProductListItemComponent in product-list', () => {
   let component: ProductListItemComponent;
   let fixture: ComponentFixture<ProductListItemComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          MaterialModule,
-          HttpModule,
-          RouterTestingModule,
-          StoreModule.forRoot({
-            ...fromRoot.reducers,
-            cart: combineReducers(fromCart.reducers),
-            user: combineReducers(fromUser.reducers)
-          })
-        ],
-        declarations: [
-          ProductListItemComponent,
-          PictureComponent,
-          AddToCartComponent
-        ],
-        providers: [CartService]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MaterialModule,
+        RouterTestingModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          cart: combineReducers(fromCart.reducers),
+          user: combineReducers(fromUser.reducers)
+        })
+      ],
+      declarations: [
+        ProductListItemComponent,
+        PictureComponent,
+        AddToCartComponent
+      ],
+      providers: [CartService]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductListItemComponent);
