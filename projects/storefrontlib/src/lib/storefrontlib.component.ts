ebffd263480999180fc167b5af9b7212fc2c81c9
Patrick Labadie <patrick.labadie@sap.com>
2018-05-11 23:02:31 -0400

    SPA-434: Generated new lib storefrontlib.

---
+++ projects/storefrontlib/src/lib/storefrontlib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-storefrontlib',
+  template: `
+    <p>
+      storefrontlib works!
+    </p>
+  `,
+  styles: []
+})
+export class StorefrontlibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/storefrontlib/src/lib/storefrontlib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { StorefrontlibComponent } from './storefrontlib.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [StorefrontlibComponent],
+  exports: [StorefrontlibComponent]
+})
+export class StorefrontlibModule { }
