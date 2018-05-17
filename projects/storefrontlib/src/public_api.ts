a7e1fa66d47cb4d9a683125712a31448e615980a
Patrick Labadie <patrick.labadie@sap.com>
2018-05-17 07:50:08 -0400

    Removed files that shouldn't be in the shellapp.

--- projects/storefrontlib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of storefrontlib
- */
-
-export * from './lib/storefrontlib.service';
-export * from './lib/storefrontlib.component';
-export * from './lib/storefrontlib.module';
