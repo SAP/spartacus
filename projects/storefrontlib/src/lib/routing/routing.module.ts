92b47e74e09d5446390336664a08e0a97d56de93
Patrick Labadie <patrick.labadie@sap.com>
2018-05-24 14:04:29 -0400

    SPA-442: Removed dead code and identified lines that currently have a workaround applied with a follow-up ticket to resolve it.

--- projects/storefrontlib/src/lib/routing/routing.module.ts
+++ projects/storefrontlib/src/lib/routing/routing.module.ts
@@ -14,9 +14,6 @@ import {
 } from './store';
 import { ConfigService, StorageSyncType } from './config.service';
 
-// Angular CLI environment
-// import { environment } from '../../environments/environment';
-
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { storeFreeze } from 'ngrx-store-freeze';
@@ -28,9 +25,8 @@ export function getMetaReducers(config: ConfigService): MetaReducer<any>[] {
     metaReducers.push(storageSyncReducer);
   }
 
-  // if (!environment.production) {
-     metaReducers.push(storeFreeze);
-  // }
+  metaReducers.push(storeFreeze); // Should not be used in production (SPA-488)
+
   return metaReducers;
 }
 
@@ -39,8 +35,7 @@ export function getMetaReducers(config: ConfigService): MetaReducer<any>[] {
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot(effects),
     StoreRouterConnectingModule,
-    // environment.production ? [] : StoreDevtoolsModule.instrument()
-    StoreDevtoolsModule.instrument()
+    StoreDevtoolsModule.instrument() // Should not be used in production (SPA-488)
   ],
   providers: [
     { provide: RouterStateSerializer, useClass: CustomSerializer },
