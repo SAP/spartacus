410653c4388ece942d55db7ab00a5cebe05a8ba7
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-05-24 15:50:13 -0400

    ACCXRP-106 Failing the build script if one of the steps fail

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+check_error() {
+  if [ "$?" != "0" ]; then
+    echo "${1:-"Unknown Error"}" 1>&2
+    exit 1
+  fi
+}
+
 echo "Starting pipeline for Spartacus project"
 echo "-----"
 echo "Updating dependencies"
@@ -10,14 +17,18 @@ ng lint
 echo "-----"
 echo "Building SPA core lib"
 ng build storefrontlib
+check_error "Building the storefrontlib failed. Aborting"
 echo "-----"
 echo "Running unit tests and checking code coverage for core lib"
 ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless
+check_error "Tests for storefrontlib failed. Aborting"
 echo "-----"
 echo "Building SPA app"
 ng build storefrontapp
+check_error "Building the storefrontapp failed. Aborting"
 echo "-----"
 echo "Running unit tests and checking code coverage for storefront app"
 ng test storefrontapp --watch=false --code-coverage --browsers=ChromeHeadless
+check_error "Tests for storefrontapp failed. Aborting"
 echo "-----"
 echo "Spartacus Pipeline completed"
