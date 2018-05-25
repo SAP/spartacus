f1b9f628bb24964f14882b40b1212399a2e6a3e5
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-05-25 08:57:12 -0400

    ACCXRP-106: Setting -e flag instead of explicit error handling

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-check_error() {
-  if [ "$?" != "0" ]; then
-    echo "${1:-"Unknown Error"}" 1>&2
-    exit 1
-  fi
-}
+set -e
 
 echo "Starting pipeline for Spartacus project"
 echo "-----"
@@ -17,18 +11,14 @@ ng lint
 echo "-----"
 echo "Building SPA core lib"
 ng build storefrontlib
-check_error "Building the storefrontlib failed. Aborting"
 echo "-----"
 echo "Running unit tests and checking code coverage for core lib"
 ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless
-check_error "Tests for storefrontlib failed. Aborting"
 echo "-----"
 echo "Building SPA app"
 ng build storefrontapp
-check_error "Building the storefrontapp failed. Aborting"
 echo "-----"
 echo "Running unit tests and checking code coverage for storefront app"
 ng test storefrontapp --watch=false --code-coverage --browsers=ChromeHeadless
-check_error "Tests for storefrontapp failed. Aborting"
 echo "-----"
 echo "Spartacus Pipeline completed"
