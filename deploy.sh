a05ae78a8ff44fe68aa5c5c53c0ea583b9294b2b
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-05-30 13:57:17 -0400

    ACCXRP-105: Addressing code review changes * Renaming variables * Using full project names as parameters * Subshell to avoid using the directory stack

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,27 @@ set -e
 set -u
 
 if [ $# == 0 ]; then
-  echo "Usage: `basename $0` [app | lib] [major | minor | patch | prerelease]"
-  exit
+    echo "Usage: `basename $0` [project_name] [major | minor | patch | prerelease]"
+    exit
 fi
 
-APP=$1
-VERSION=$2
-APP_DIR="projects/storefront$APP"
-DEPLOY_DIR="dist/storefront$APP"
+PROJECT=$1
+BUMP=$2
+PROJECT_DIR="projects/$PROJECT"
+DEPLOY_DIR="dist/$PROJECT"
 
 pushd .
-cd $APP_DIR
-echo "Bumping to version --$VERSION from $PWD"
-NEW_VERSION=`npm version $VERSION`
+cd $PROJECT_DIR
+echo "Bumping version to $BUMP"
+NEW_VERSION=`npm version $BUMP`
 echo "New version: $NEW_VERSION"
 popd
-echo "Copying $APP_DIR/package.json $DEPLOY_DIR"
-cp $APP_DIR/package.json $DEPLOY_DIR
-pushd .
-cd $DEPLOY_DIR
-echo "publishing version $VERSION from $PWD"
-# npm publish .
-popd
-cd $APP_DIR
+echo "Copying $PROJECT_DIR/package.json $DEPLOY_DIR"
+cp $PROJECT_DIR/package.json $DEPLOY_DIR
+echo "publishing version $BUMP"
+(cd $DEPLOY_DIR && npm publish .)
+cd $PROJECT_DIR
 git commit -am"Bumping version to $NEW_VERSION"
 git tag storefrontlib-$NEW_VERSION
 echo "Pushing from $PWD"
 git push origin develop --tags
-
