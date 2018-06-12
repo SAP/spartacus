19f6774c149c30f6ff098a1b7f80b97f3fca4ea0
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-06-12 10:21:05 -0400

    ACCXRP-109 Adding configuration to run e2e tests in headless mode

--- projects/storefrontapp-e2e/protractor.conf.js
+++ projects/storefrontapp-e2e/protractor.conf.js
@@ -1,28 +1,45 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {
+  SpecReporter
+} = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 25000,
-  specs: ['./src/**/*.e2e-spec.ts'],
-  capabilities: {
-    browserName: 'chrome'
-  },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  allScriptsTimeout: 25000,
+  specs: ['./src/**/*.e2e-spec.ts'],
+
+  capabilities: {
+    browserName: 'chrome',
+    // acceptSslCerts: true,
+    // trustAllSSLCertificates: true,
+    acceptInsecureCerts: true,
+    chromeOptions: {
+      args: [
+        '--headless',
+        '--no-sandbox',
+        '--disable-gpu',
+        '--window-size=800,600'
+      ]
+    }
+  },
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine
-      .getEnv()
-      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        spec: {
+          displayStacktrace: true
+        }
+      })
+    );
   }
 };
