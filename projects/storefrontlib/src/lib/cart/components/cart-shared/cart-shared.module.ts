2601500a04841f8852a9e5921ab3526af5a0e3df
Eric Xiao <xiao_eric@hotmail.com>
2018-06-13 16:31:49 -0400

    SPA-535: moved order-summary back into detail

--- projects/storefrontlib/src/lib/cart/components/cart-shared/cart-shared.module.ts
+++ projects/storefrontlib/src/lib/cart/components/cart-shared/cart-shared.module.ts
@@ -2,19 +2,13 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { ItemCounterComponent } from './item-counter/item-counter.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  declarations: [
-    OrderSummaryComponent,
-    CartItemComponent,
-    ItemCounterComponent
-  ],
+  declarations: [CartItemComponent, ItemCounterComponent],
   exports: [
-    OrderSummaryComponent,
     CartItemComponent,
     ItemCounterComponent,
     CommonModule,
