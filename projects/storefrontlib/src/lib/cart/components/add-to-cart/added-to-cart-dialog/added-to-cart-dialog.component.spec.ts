258303404299fdd389f701e7e9c256310e8eee8e
Eric Xiao <xiao_eric@hotmail.com>
2018-06-15 15:20:53 -0400

    SPA-535: update tests

--- projects/storefrontlib/src/lib/cart/components/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.spec.ts
+++ projects/storefrontlib/src/lib/cart/components/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.spec.ts
@@ -7,12 +7,9 @@ import { of } from 'rxjs';
 import { CartSharedModule } from './../../cart-shared/cart-shared.module';
 import { AddedToCartDialogComponent } from './added-to-cart-dialog.component';
 
-fdescribe('AddedToCartDialogComponent', () => {
+describe('AddedToCartDialogComponent', () => {
   let component: AddedToCartDialogComponent;
   let fixture: ComponentFixture<AddedToCartDialogComponent>;
-  // Update events represent the previous value of the form
-  const mockDecrementUpdateEvent = { quantity: 2 };
-  const mockIncrementUpdateEvent = { quantity: 0 };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -60,19 +57,5 @@ fdescribe('AddedToCartDialogComponent', () => {
   it('should have form quantity control after init and entry observable finished', () => {
     const entryForm = component.form.controls.entryForm as FormGroup;
     expect(entryForm.controls.quantity).toBeDefined();
-  });
-
-  it('should have default value of addedQuantity to be 1', () => {
-    expect(component.addedQuantity).toBe(1);
-  });
-
-  it('should update entries (increment)', () => {
-    component.updateEntry(mockIncrementUpdateEvent);
-    expect(component.addedQuantity).toBe(2);
-  });
-
-  it('should update entries (decrement)', () => {
-    component.updateEntry(mockDecrementUpdateEvent);
-    expect(component.addedQuantity).toBe(0);
   });
 });
