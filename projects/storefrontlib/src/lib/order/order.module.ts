2a5751c0d82bcc5e2242ed4dc6d3bf6902ca8962
Eric Xiao <xiao_eric@hotmail.com>
2018-06-18 12:40:07 -0400

    SPA-562: moved layout component into the order component

--- projects/storefrontlib/src/lib/order/order.module.ts
+++ projects/storefrontlib/src/lib/order/order.module.ts
@@ -1,11 +1,21 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OrderHistoryControlsComponent } from './order-history/order-history-controls/order-history-controls.component';
+import { OrderHistoryPageComponent } from './order-history/container/order-history-page.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [OrderHistoryControlsComponent, OrderDetailsComponent],
-  exports: [OrderHistoryControlsComponent, OrderDetailsComponent]
+  imports: [CommonModule, RouterModule],
+  declarations: [
+    OrderHistoryPageComponent,
+    OrderHistoryControlsComponent,
+    OrderDetailsComponent
+  ],
+  exports: [
+    OrderHistoryPageComponent,
+    OrderHistoryControlsComponent,
+    OrderDetailsComponent
+  ]
 })
 export class OrderModule {}
