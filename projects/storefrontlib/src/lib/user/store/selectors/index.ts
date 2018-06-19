256340191dee7e3acb1036d0fee7421b83197092
Eric Xiao <e.xiao@sap.com>
2018-06-19 11:27:55 -0400

    SPA-562: user-orders-store (selectors)

--- projects/storefrontlib/src/lib/user/store/selectors/index.ts
+++ projects/storefrontlib/src/lib/user/store/selectors/index.ts
@@ -2,3 +2,4 @@ export * from './user-token.selectors';
 export * from './user-details.selectors';
 export * from './user-addresses.selectors';
 export * from './payment-methods.selectors';
+export * from './user-orders.selectors';
