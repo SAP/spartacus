8d9beb0005f14092043ed49738c519393b3f9a14
Eric Xiao <e.xiao@sap.com>
2018-07-09 11:42:51 -0400

    SPA-563: show more/less button per facet

--- projects/storefrontlib/src/lib/product/components/product-list/product-facet-navigation/product-facet-navigation.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-list/product-facet-navigation/product-facet-navigation.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   Input,
   Output,
+  OnInit,
   EventEmitter,
   ChangeDetectionStrategy
 } from '@angular/core';
@@ -12,14 +13,41 @@ import {
   styleUrls: ['./product-facet-navigation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductFacetNavigationComponent {
+export class ProductFacetNavigationComponent implements OnInit {
   @Input() activeFacetValueCode;
   @Input() searchResult;
+  @Input() minPerFacet;
+  @Input() step;
   @Output() filter: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() {}
+  facetDisplayAmountmap: Map<String, number>;
+
+  constructor() {
+    this.facetDisplayAmountmap = new Map<String, number>();
+  }
+
+  ngOnInit() {
+    for (const index of Object.keys(this.searchResult.facets)) {
+      this.facetDisplayAmountmap.set(
+        this.searchResult.facets[index].name,
+        this.minPerFacet
+      );
+    }
+  }
 
   toggleValue(query: string) {
     this.filter.emit(query);
+  }
+
+  showLess(facetName: String) {
+    this.updateFacetAmount(facetName, -this.step);
+  }
+  showMore(facetName: String) {
+    this.updateFacetAmount(facetName, this.step);
+  }
+
+  private updateFacetAmount(facetName: String, change: number) {
+    const currentAmountDisplayed = this.facetDisplayAmountmap.get(facetName);
+    this.facetDisplayAmountmap.set(facetName, currentAmountDisplayed + change);
   }
 }
