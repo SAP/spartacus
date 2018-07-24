a6f1338ce81f75cb1d39ce8517d4eefda7ad2f03
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-07-24 11:04:15 -0400

    ACCXRP-124: Code coverage for storefrontapp was overriding the storefrontlib one

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Validating tsconfig.json integrity"
 LOCAL_ENV_LIB_PATH="projects/storefrontlib/src/public_api"
 LOCAL_ENV_LIB_PATH_OCCURENCES=$(grep -c "projects/storefrontlib/src/public_api" tsconfig.json || true)
 if [ $LOCAL_ENV_LIB_PATH_OCCURENCES \> 0 ];
-then 
+then
     echo "ERROR: tsconfig.json file is invalid. Found [${LOCAL_ENV_LIB_PATH}].";
     echo "The local env configuration should not be pushed to git."
     exit 1
@@ -19,7 +19,7 @@ echo "Validating yarn.lock integrity"
 DEFAULT_REGISTRY_URL="https://registry.yarnpkg.com"
 DEFAULT_REGISTRY_OCCURENCES=$(grep -c "${DEFAULT_REGISTRY_URL}" yarn.lock || true)
 if [ $DEFAULT_REGISTRY_OCCURENCES \> 0 ];
-then 
+then
     echo "ERROR: yarn file is corrupt. Found [${DEFAULT_REGISTRY_URL}] ${DEFAULT_REGISTRY_OCCURENCES} time(s).";
     echo "The dependency urls should all point to the hybris Artifactory.";
     exit 1
@@ -45,7 +45,7 @@ echo "Building SPA app"
 ng build storefrontapp
 echo "-----"
 echo "Running unit tests and checking code coverage for storefront app"
-ng test storefrontapp --watch=false --code-coverage --browsers=ChromeHeadless
+ng test storefrontapp --watch=false --browsers=ChromeHeadless
 echo "-----"
 echo "Setting endpoint with the server to run end to end tests against"
 sed -i -e "s=https://localhost=https://$DEV_SERVER=g" projects/storefrontapp/src/app/config.service.ts
