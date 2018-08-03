4bba6f4d67f56d497604fd7d2041eac292ccc869
Patrick Labadie <patrick.labadie@sap.com>
2018-08-03 13:32:08 -0400

    SPA-585: Added AbstractStorefrontModule unit tests.

---
+++ projects/storefrontlib/src/abstract-storefront-module.spec.ts
@@ -0,0 +1,73 @@
+import { AbstractStorefrontModule } from './abstract-storefront-module';
+import { DefaultConfigService } from './default-config.service';
+import { TestBed } from '@angular/core/testing';
+import { ConfigService } from './config.service';
+
+class TestStorefrontModule extends AbstractStorefrontModule {}
+
+export class TestDefaultConfigService extends DefaultConfigService {
+  propertyA = 'default value A';
+  propertyB = 'default value B';
+}
+export class TestOverridingConfigService {
+  propertyB = 'overriding value B';
+  propertyC = 'overriding value C';
+}
+
+describe('AbstractStorefrontModule Tests', () => {
+  let testDefaultConfigService: TestDefaultConfigService;
+  let testOverridingConfigService: TestOverridingConfigService;
+
+  beforeEach(() => {
+    testDefaultConfigService = new TestDefaultConfigService();
+    testOverridingConfigService = new TestOverridingConfigService();
+  });
+
+  describe('Create AbstractStorefrontModule', () => {
+    it('should be truthy', () => {
+      const storefrontModuleInstance = new TestStorefrontModule();
+      expect(storefrontModuleInstance).toBeTruthy();
+    });
+  });
+
+  describe('Test function configServiceFactory', () => {
+    it('should handle undefined config param', () => {
+      const expectedResult = { ...testDefaultConfigService };
+      const result = AbstractStorefrontModule.configServiceFactory(
+        undefined,
+        testDefaultConfigService
+      );
+      expect(result).toEqual(expectedResult);
+    });
+    it('should handle undefined default config param', () => {
+      const expectedResult = { ...testOverridingConfigService };
+      const result = AbstractStorefrontModule.configServiceFactory(
+        testOverridingConfigService,
+        undefined
+      );
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should properly override properties', () => {
+      const expectedResult = {
+        ...testDefaultConfigService,
+        propertyB: 'overriding value B',
+        propertyC: 'overriding value C'
+      };
+      const result = AbstractStorefrontModule.configServiceFactory(
+        testOverridingConfigService,
+        testDefaultConfigService
+      );
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('Test function getOverriddenConfigProvider', () => {
+    it('should return a provider', () => {
+      const result = AbstractStorefrontModule.getOverriddenConfigProvider(
+        testOverridingConfigService
+      );
+      expect(result).toBeDefined();
+    });
+  });
+});
