79b784770161b199d2b018aca7f7af8a12328c55
Patrick Labadie <patrick.labadie@sap.com>
2018-08-03 01:05:33 -0400

    SPA-585: Remove non overridden configs from the app's config.

--- projects/storefrontapp/src/app/config.service.ts
+++ projects/storefrontapp/src/app/config.service.ts
@@ -1,65 +1,9 @@
 import { Injectable } from '@angular/core';
 
-export enum StorageSyncType {
-  NO_STORAGE,
-  LOCAL_STORAGE,
-  SESSION_STORAGE
-}
-
 @Injectable()
 export class ConfigService {
   server = {
     baseUrl: 'https://localhost:9002',
     occPrefix: '/rest/v2/'
-  };
-
-  lang =
-    sessionStorage.getItem('language') === null
-      ? 'en'
-      : sessionStorage.getItem('language');
-  curr =
-    sessionStorage.getItem('currency') === null
-      ? 'USD'
-      : sessionStorage.getItem('currency');
-
-  site = {
-    baseSite: 'electronics',
-    language: this.lang,
-    currency: this.curr
-  };
-
-  // site = {
-  //     baseSite: 'apparel-uk',
-  //     language: 'en',
-  //     currency: 'GBP'
-  // };
-  storageSyncType = StorageSyncType.SESSION_STORAGE;
-
-  defaultPageIdForType = {
-    ProductPage: ['productDetails'],
-    CategoryPage: ['productList', 'productGrid', 'category']
-  };
-
-  authentication = {
-    client_id: 'mobile_android',
-    client_secret: 'secret'
-  };
-
-  cmsComponentMapping = {
-    CMSLinkComponent: 'LinkComponent',
-    SimpleResponsiveBannerComponent: 'ResponsiveBannerComponent',
-    SimpleBannerComponent: 'BannerComponent',
-    // BreadcrumbComponent:                'BreadcrumbComponent',
-    CMSParagraphComponent: 'ParagraphComponent',
-    NavigationComponent: 'NavigationComponent',
-    FooterNavigationComponent: 'FooterNavigationComponent',
-    CategoryNavigationComponent: 'CategoryNavigationComponent',
-    ProductAddToCartComponent: 'AddToCartComponent',
-    MiniCartComponent: 'MiniCartComponent',
-    ProductCarouselComponent: 'ProductCarouselComponent',
-    SearchBoxComponent: 'SearchBoxComponent',
-    ProductReferencesComponent: 'ProductReferencesComponent',
-    // CMSTabParagraphComponent: 'TabParagraphContainerComponent'
-    CMSTabParagraphComponent: 'ParagraphComponent'
   };
 }
