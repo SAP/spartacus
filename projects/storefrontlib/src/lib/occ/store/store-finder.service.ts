05271d80e992132c0cf0302070a4f3456840e023
Venelin Koulaxazov <venelin.koulaxazov@sap.com>
2018-08-06 15:06:20 -0400

    E2EC-955 first pass of occ service and started setup of boiler plate code for ngrx store

---
+++ projects/storefrontlib/src/lib/occ/store/store-finder.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { SearchConfig } from './../../product/search-config';
+
+import { ConfigService } from '../config.service';
+
+const STORES_ENDPOINT = 'stores';
+const DEFAULT_SEARCH_CONFIG: SearchConfig = {
+    pageSize: 20
+  };
+
+@Injectable()
+export class OccStoreFinderService {
+  constructor(private http: HttpClient, private configService: ConfigService) {}
+
+  listStores(address: string, searchConfig: SearchConfig = DEFAULT_SEARCH_CONFIG): Observable<any> {
+    const url = this.getStoresEndpoint();
+    let params = new HttpParams({
+        fromString:
+          '&fields=' +
+          'pagination(DEFAULT),' +
+          'sorts(DEFAULT)'
+      });
+      params = params.set('query', address);
+      if (searchConfig.pageSize) {
+        params = params.set('pageSize', searchConfig.pageSize.toString());
+      }
+      if (searchConfig.currentPage) {
+        params = params.set('currentPage', searchConfig.currentPage.toString());
+      }
+      if (searchConfig.sortCode) {
+        params = params.set('sort', searchConfig.sortCode);
+      }
+
+    return this.http
+      .get(url, { params: params })
+      .pipe(catchError((error: any) => throwError(error.json())));
+  }
+
+  protected getStoresEndpoint() {
+    return (
+      this.configService.server.baseUrl +
+      this.configService.server.occPrefix +
+      this.configService.site.baseSite +
+      '/' +
+      STORES_ENDPOINT
+    );
+  }
+}
---
+++ projects/storefrontlib/src/lib/store-finder/services/index.ts
@@ -0,0 +1,5 @@
+import { StoreFinderService } from './store-finder.service';
+
+export const services: any[] = [StoreFinderService];
+
+export * from './store-finder.service';
