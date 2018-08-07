6a5f027975e0a84494096b058d84918d3720f298
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-08-07 12:35:22 -0400

    SPA-576 Validating storefrontstyles integrity as part of the pipeline

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 DEV_SERVER='10\.27\.165\.187'
+
+function validatestyles {
+    echo "-----"
+    echo "Validating styles app"
+    pushd projects/storefrontstyles
+    yarn build
+    rm -rf temp-scss
+    popd
+}
 
 echo "Validating tsconfig.json integrity"
 LOCAL_ENV_LIB_PATH="projects/storefrontlib/src/public_api"
@@ -66,6 +75,9 @@ else
     rm prettier.log
     exit 1
 fi
+
+validatestyles
+
 echo "-----"
 echo "Running unit tests and code coverage for core lib"
 ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless 2>&1 |  tee spa_tests.log
