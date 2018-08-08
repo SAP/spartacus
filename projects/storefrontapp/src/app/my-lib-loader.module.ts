44c226e1c6e002d77c17ae669662e66afbc76407
Weizheng Gao <weizheng.gao@sap.com>
2018-08-08 07:52:54 -0400

    feature/SPA-626-1: remove dynamic-component-loader

--- projects/storefrontapp/src/app/my-lib-loader.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OrderHistoryPageModule } from 'storefrontlib';
-
-@NgModule({
-  imports: [CommonModule, OrderHistoryPageModule],
-  declarations: []
-})
-export class MyLibLoaderModule {}
