8234b4b25b9ee64a8e5866ed77e3769d821b1811
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2018-08-09 12:48:22 -0400

    SPA-626: Order imports

--- projects/storefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/pages.module.ts
@@ -7,6 +7,8 @@ import { PageNotFoundModule } from './404/404.module';
 import { CartPageModule } from './cart-page/cart-page.module';
 import { OrderConfirmationPageModule } from './order-confirmation-page/order-confirmation-page.module';
 import { MultiStepCheckoutPageModule } from './multi-step-checkout-page/multi-step-checkout-page.module';
+import { RegisterPageModule } from './register-page/register-page.module';
+
 // my Account Pages
 import { OrderHistoryPageModule } from './order-history-page/order-history-page.module';
 import { OrderDetailsPageModule } from './order-details-page/order-details-page.module';
@@ -16,9 +18,6 @@ import { CategoryPageModule } from './category-page/category-page.module';
 
 // ProductPage
 import { ProductPageModule } from './product-page/product-page.module';
-
-// RegisterPage
-import { RegisterPageModule } from './register-page/register-page.module';
 
 @NgModule({
   imports: [
