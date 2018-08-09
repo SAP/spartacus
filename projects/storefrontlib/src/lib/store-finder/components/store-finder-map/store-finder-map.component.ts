222e3f8ea25d11dd51de8add8826310ad42b6118
Vlasov, Andrey <andrey.vlasov@sap.com>
2018-08-09 16:37:20 -0400

    E2EC-961 stores are plotted on the map

--- projects/storefrontlib/src/lib/store-finder/components/store-finder-map/store-finder-map.component.ts
+++ projects/storefrontlib/src/lib/store-finder/components/store-finder-map/store-finder-map.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { GoogleMapRendererServcie } from '../../services/googleMapRenderer.service';
 
 @Component({
@@ -6,12 +14,33 @@ import { GoogleMapRendererServcie } from '../../services/googleMapRenderer.servi
   templateUrl: './store-finder-map.component.html',
   styleUrls: ['./store-finder-map.component.scss']
 })
-export class StoreFinderMapComponent implements OnInit {
+export class StoreFinderMapComponent implements OnInit, OnChanges {
   @ViewChild('mapElement') private mapElement: ElementRef;
+  @Input() locations: any;
 
-  constructor(private googleMapRendererService: GoogleMapRendererServcie) {}
+  constructor(private googleMapRendererService: GoogleMapRendererServcie) {
+    this.locations = { stores: [{ geoPoint: { latitude: 0, longtitude: 0 } }] };
+  }
 
-  ngOnInit(): void {
-    this.googleMapRendererService.renderMap(this.mapElement.nativeElement);
+  ngOnInit(): void {}
+
+  convert() {
+    return this.locations.stores.map(store => {
+      return {
+        name: store.displayName,
+        latitude: store.geoPoint.latitude,
+        longitude: store.geoPoint.longitude
+      };
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.locations.stores) {
+      console.log(this.locations);
+      this.googleMapRendererService.renderMap(
+        this.mapElement.nativeElement,
+        this.convert()
+      );
+    }
   }
 }
--- projects/storefrontlib/src/lib/store-finder/services/googleMapRenderer.service.ts
+++ projects/storefrontlib/src/lib/store-finder/services/googleMapRenderer.service.ts
@@ -10,22 +10,28 @@ const GOOGLE_MAP_API_URL = 'https://maps.googleapis.com/maps/api/js';
 export class GoogleMapRendererServcie {
   constructor(private externalJsFileLoader: ExternalJsFileLoader) {}
 
-  public renderMap(mapElement: HTMLElement): void {
+  public renderMap(mapElement: HTMLElement, payload: any): void {
     this.externalJsFileLoader.load(
       GOOGLE_MAP_API_URL,
       { key: 'AIzaSyB7KXTrhIgBrBMNNVHWVWVyHfWcV_2Qe0Q' },
-      () => this.initMap(mapElement)
+      () => this.initMap(mapElement, payload)
     );
   }
 
-  protected initMap(mapElement: HTMLElement): void {
+  protected initMap(mapElement: HTMLElement, payload: any): void {
+    console.log(payload);
     const mapProp = {
-      center: new google.maps.LatLng(28.4595, 77.0266),
-      zoom: 21,
+      center: new google.maps.LatLng(payload[0].latitude, payload[0].longitude),
+      zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     const map = new google.maps.Map(mapElement, mapProp);
-    const marker = new google.maps.Marker({ position: mapProp.center });
-    marker.setMap(map);
+    payload.forEach(element => {
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(element.latitude, element.longitude)
+      });
+      console.log(marker);
+      marker.setMap(map);
+    });
   }
 }
