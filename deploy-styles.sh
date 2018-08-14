1a53d6c65ef01dba9bfe29c509309f97f4fffe8c
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-08-14 10:40:35 -0400

    Updates to storefrontstyles deployment * Using npm instead of yarn to keep consistency * No need to build a zip for this (npm can do it for us) * Adding .npmignore to avoid including yarn.lock in npm package * Bumping version to v0.0.7

--- deploy-styles.sh
+++ deploy-styles.sh
@@ -12,6 +12,7 @@ PROJECT="storefrontstyles"
 PROJECT_DIR="projects/$PROJECT"
 
 pushd $PROJECT_DIR
+
 echo "Bumping $PROJECT version to $BUMP"
 
 PROJECT_NEW_VERSION=`npm version $BUMP`
@@ -19,19 +20,12 @@ PROJECT_NEW_VERSION=`npm version $BUMP`
 echo "New version: $PROJECT_NEW_VERSION"
 
 NEW_PACKAGE=$PROJECT-$PROJECT_NEW_VERSION.tgz
-
-echo "Packing $NEW_PACKAGE"
-yarn pack
-
 echo "publishing $NEW_PACKAGE"
-yarn publish $NEW_PACKAGE
-
-echo "Cleaning up"
-rm $NEW_PACKAGE
+npm publish .
 
 popd
-git commit -am"Bumping version to $PROJECT_NEW_VERSION"
 git tag $PROJECT-$PROJECT_NEW_VERSION
+git commit -am"Bumping version to $PROJECT_NEW_VERSION"
 git push origin develop --tags
 
 echo "Deployment of styles successful"
