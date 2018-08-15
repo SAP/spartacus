ef823e41c4d20afa7ea3f86e798f35a4c7c964f6
Regina Kandiah <regina.kandiah@sap.com>
2018-08-15 13:48:19 -0400

    E2EC-840 formatting

--- projects/storefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/pages.module.ts
@@ -12,7 +12,7 @@ import { LoginPageModule } from './login-page/login-page.module';
 // ContentPage: my Account Pages
 import { OrderHistoryPageModule } from './order-history-page/order-history-page.module';
 import { OrderDetailsPageModule } from './order-details-page/order-details-page.module';
-import {StoreFinderPageModule} from './store-finder-page/store-finder-page.module';
+import { StoreFinderPageModule } from './store-finder-page/store-finder-page.module';
 // CategoryPage
 import { CategoryPageModule } from './category-page/category-page.module';
 
