2f7c00c6bcfb75cfadaa09afea6792ea6fb2b60e
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-08-15 15:57:58 -0400

    SPA-848 Running e2e tests in prod mode

--- build.sh
+++ build.sh
@@ -103,6 +103,6 @@ echo "Setting endpoint with the server to run end to end tests against"
 sed -i -e "s=https://localhost=https://$DEV_SERVER=g" projects/storefrontapp/src/app/config.service.ts
 echo "-----"
 echo "Running end to end tests"
-ng e2e --protractor-config=projects/storefrontapp-e2e/protractor.headless.conf.js
+ng e2e --prod --protractor-config=projects/storefrontapp-e2e/protractor.headless.conf.js
 echo "-----"
 echo "Spartacus Pipeline completed"
