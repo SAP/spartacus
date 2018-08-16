cb621100a284c9f70ff30e653f1e41ccb648daed
Nikola Zaric <nikola.zaric@sap.com>
2018-08-16 11:05:18 -0400

    SPA-594 - code review changes

--- projects/storefrontlib/src/lib/auth/services/user-authentication/user-authentication-token.service.ts
+++ projects/storefrontlib/src/lib/auth/services/user-authentication/user-authentication-token.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -14,18 +14,18 @@ export class UserAuthenticationTokenService {
 
   loadToken(userId: string, password: string): Observable<any> {
     const url = this.getOAuthEndpoint();
-    let creds = '';
-    creds += 'client_id=' + this.configService.authentication.client_id;
-    creds +=
-      '&client_secret=' + this.configService.authentication.client_secret;
-    creds += '&grant_type=password'; // authorization_code, client_credentials, password
-    creds += '&username=' + userId + '&password=' + password;
+    const params = new HttpParams()
+      .set('client_id', this.configService.authentication.client_id)
+      .set('client_secret', this.configService.authentication.client_secret)
+      .set('grant_type', 'password') // authorization_code, client_credentials, password
+      .set('username', userId)
+      .set('password', password);
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded'
     });
 
     return this.http
-      .post(url, creds, { headers })
+      .post(url, params, { headers })
       .pipe(catchError((error: any) => throwError(error)));
   }
 
