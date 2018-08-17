7540da607ad066e3a14d8831543805c442673268
Regina Kandiah <regina.kandiah@sap.com>
2018-08-17 12:04:30 -0400

    E2EC-842 fix page layout unit test

--- projects/storefrontlib/src/lib/ui/layout/store-finder-page-layout/store-finder-page-layout.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/layout/store-finder-page-layout/store-finder-page-layout.component.spec.ts
@@ -1,14 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StoreFinderPageLayoutComponent } from './store-finder-page-layout.component';
+import * as fromCmsReducer from '../../../cms/store/reducers';
+import * as fromStore from '../../../store-finder/store';
+import * as fromRoot from '../../../routing/store';
+import { combineReducers, StoreModule } from '@ngrx/store';
+import { StoreFinderPagingComponent } from '../../../store-finder/components/store-finder-paging/store-finder-paging.component';
+import { StoreFinderListItemComponent } from '../../../store-finder/components/store-finder-list/store-finder-list-item/store-finder-list-item.component';
+import { StoreFinderListComponent } from '../../../store-finder/components/store-finder-list/store-finder-list.component';
+import { StoreFinderSearchComponent } from '../../../store-finder/components/store-finder-search/store-finder-search.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../material.module';
+import { CommonModule } from '@angular/common';
+import { FindStoresService } from '../../../store-finder/services';
+import { OccE2eConfigurationService } from '../../../occ/e2e/configuration-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('StoreFinderPageLayoutComponent', () => {
+fdescribe('StoreFinderPageLayoutComponent', () => {
   let component: StoreFinderPageLayoutComponent;
   let fixture: ComponentFixture<StoreFinderPageLayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [StoreFinderPageLayoutComponent]
+      imports: [
+        ReactiveFormsModule,
+        MaterialModule,
+        CommonModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot({
+          ...fromRoot.getReducers(),
+          stores: combineReducers(fromStore.reducers),
+          cms: combineReducers(fromCmsReducer.getReducers())
+        })
+      ],
+      declarations: [
+        StoreFinderPageLayoutComponent,
+        StoreFinderPagingComponent,
+        StoreFinderListItemComponent,
+        StoreFinderListComponent,
+        StoreFinderSearchComponent
+      ],
+      providers: [FindStoresService, OccE2eConfigurationService]
     }).compileComponents();
   }));
 
