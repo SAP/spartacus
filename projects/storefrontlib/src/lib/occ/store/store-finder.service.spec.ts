d0400f27994b58d7963f0aa12f7303e80a2d832b
Venelin Koulaxazov <venelin.koulaxazov@sap.com>
2018-08-17 14:44:41 -0400

    added occ test

--- projects/storefrontlib/src/lib/occ/store/store-finder.service.spec.ts
+++ projects/storefrontlib/src/lib/occ/store/store-finder.service.spec.ts
@@ -12,7 +12,6 @@ import { OccE2eConfigurationService } from '../e2e/e2e-configuration-service';
 const queryText = 'test';
 const searchResults = { stores: [{ name: 'test' }] };
 const mockSearchConfig: SearchConfig = { pageSize: 5 };
-const endpoint = '/stores';
 
 export class MockConfigService {
   server = {
@@ -50,38 +49,21 @@ describe('OccStoreFinderService', () => {
 
   describe('query text search', () => {
     it('should return search results for given query text', () => {
-      service.findStores(queryText, mockSearchConfig).subscribe(result => {
-        expect(result).toEqual(searchResults);
-      });
+      service
+        .findStores(queryText, mockSearchConfig)
+        .toPromise()
+        .then(result => {
+          expect(result).toEqual(searchResults);
+        });
 
-      const mockReq1 = httpMock.expectOne({
+      const mockReq = httpMock.expectOne({
         method: 'GET',
-        url: endpoint
+        url:
+          '/e2econfigurationwebservices/e2econfiguration/e2egoogleservices.storesdisplayed'
       });
 
-      const mockReq2 = httpMock.expectOne({
-        method: 'GET',
-        url: endpoint
-      });
-
-      expect(mockReq2.request.params.get('query')).toEqual(queryText);
-      expect(mockReq2.request.params.get('pageSize')).toEqual(
-        mockSearchConfig.pageSize.toString()
-      );
-      expect(mockReq2.request.params.get('fields')).toEqual(
-        'stores(name,displayName,openingHours(weekDayOpeningList(FULL),specialDayOpeningList(FULL)),' +
-          'geoPoint(latitude,longitude),address(line1,line2,town,region(FULL),postalCode,phone,country) ),' +
-          'pagination(DEFAULT),' +
-          'sorts(DEFAULT)'
-      );
-
-      expect(mockReq1.cancelled).toBeFalsy();
-      expect(mockReq1.request.responseType).toEqual('json');
-      mockReq2.flush(searchResults);
-
-      expect(mockReq2.cancelled).toBeFalsy();
-      expect(mockReq2.request.responseType).toEqual('json');
-      mockReq2.flush(searchResults);
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
     });
   });
 });
