3a0c48afe34ffef17a3a905e4a0b7d073388175e
Nikola Zaric <nikola.zaric@sap.com>
2018-08-20 11:05:36 -0400

    SPA-820 - added test

--- projects/storefrontlib/src/lib/auth/services/client-error/client-error-handling.service.spec.ts
+++ projects/storefrontlib/src/lib/auth/services/client-error/client-error-handling.service.spec.ts
@@ -8,6 +8,7 @@ import { Observable, of } from 'rxjs';
 import * as fromRoot from '../../../routing/store';
 import * as fromStore from '../../store';
 import { ClientErrorHandlingService } from './client-error-handling.service';
+import { ClientAuthenticationToken } from '../../models/token-types.model';
 
 class MockHttpHandler extends HttpHandler {
   handle(_req: HttpRequest<any>): Observable<HttpEvent<any>> {
@@ -15,9 +16,25 @@ class MockHttpHandler extends HttpHandler {
   }
 }
 
+const clientToken: ClientAuthenticationToken = {
+  access_token: 'xxx',
+  token_type: 'bearer',
+  expires_in: 1000,
+  scope: 'xxx'
+};
+
+const newClientToken: ClientAuthenticationToken = {
+  access_token: 'xxx yyy zzz',
+  token_type: 'bearer',
+  expires_in: 1000,
+  scope: 'xxx'
+};
+
 describe('ClientErrorHandlingService', () => {
+  let httpRequest = new HttpRequest('GET', '/');
   let service: ClientErrorHandlingService;
   let store: Store<fromStore.AuthState>;
+  let httpHandler: HttpHandler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -36,9 +53,30 @@ describe('ClientErrorHandlingService', () => {
 
     store = TestBed.get(Store);
     service = TestBed.get(ClientErrorHandlingService);
+    httpHandler = TestBed.get(HttpHandler);
+
+    spyOn(httpHandler, 'handle').and.callThrough();
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe(`handleExpiredClientToken`, () => {
+    it('should get a new client token and resend the request', () => {
+      store.dispatch(new fromStore.LoadClientTokenSuccess(clientToken));
+      service.handleExpiredClientToken(httpRequest, httpHandler).subscribe();
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new fromStore.LoadClientToken()
+      );
+
+      store.dispatch(new fromStore.LoadClientTokenSuccess(newClientToken));
+      httpRequest = httpRequest.clone({
+        setHeaders: {
+          Authorization: `${newClientToken.token_type} ${
+            newClientToken.access_token
+          }`
+        }
+      });
+      expect(httpHandler.handle).toHaveBeenCalledWith(httpRequest);
+    });
   });
 });
