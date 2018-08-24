5ba4ab8470c1e9564908bafcd3654400fc3ffc6b
dunqan <dunqan@w9.pl>
2018-08-24 10:40:55 +0200

    SPA-846: Footer page-object and footer tests

--- projects/storefrontapp-e2e/src/cmslib/footer.po.ts
+++ projects/storefrontapp-e2e/src/cmslib/footer.po.ts
@@ -1,26 +1,26 @@
-import {by} from 'protractor';
+import {by, ElementArrayFinder, ElementFinder} from 'protractor';
 import {E2EUtil} from './../util.po';
 
 export class Footer {
 
-  readonly footerNavigation = E2EUtil.getComponentWithinDynamicSlot('Footer', 'y-footer-navigation');
-  readonly notice = this.footerNavigation.element(by.css('.y-footer-navigation__notice'));
-  readonly linkSections = this.footerNavigation.all(by.css('.y-footer-navigation__container'));
-  readonly linkSectionHeader = (sectionNo: number) => this.linkSections.get(sectionNo).element(by.tagName('h1'));
+  readonly footerNavigation: ElementFinder = E2EUtil.getComponentWithinDynamicSlot('Footer', 'y-footer-navigation');
+  readonly notice: ElementFinder = this.footerNavigation.element(by.css('.y-footer-navigation__notice'));
+  readonly linkSections: ElementArrayFinder = this.footerNavigation.all(by.css('.y-footer-navigation__container'));
+  readonly linkSectionHeader = (sectionNo: number): ElementFinder  => this.linkSections.get(sectionNo).element(by.tagName('h1'));
 
-  async getNoticeText() {
+  async getNoticeText(): Promise<string> {
     return this.notice.getText();
   }
 
-  async getSectionsCount() {
+  async getSectionsCount(): Promise<number> {
     return this.linkSections.count();
   }
 
-  async getSectionHeader(sectionNo: number) {
+  async getSectionHeader(sectionNo: number): Promise<string> {
     return this.linkSectionHeader(sectionNo).getText();
   }
 
-  async getLinkUrlByTitle(title: string) {
+  async getLinkUrlByTitle(title: string): Promise<string> {
     return this.footerNavigation.element(by.linkText(title)).getAttribute('href');
   }
 
