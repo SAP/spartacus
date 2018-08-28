ff43a903956bf41061d363e0d437e57e5e321493
Weizheng Gao <weizheng.gao@sap.com>
2018-08-28 11:06:32 -0400

    feature/SPA-282: fix lint error

--- projects/storefrontlib/src/lib/checkout/index.ts
+++ projects/storefrontlib/src/lib/checkout/index.ts
@@ -2,6 +2,7 @@ export * from './checkout.module';
 export * from './components/multi-step-checkout/multi-step-checkout.module';
 export * from './components/multi-step-checkout/shipping-address/shipping-address.module';
 export * from './components/order-confirmation/order-confirmation.module';
+// tslint:disable-next-line:max-line-length
 export * from './components/multi-step-checkout/shipping-address/address-form/suggested-addresses-dialog/suggested-addresses-dialog.component';
 export * from './components/multi-step-checkout/shipping-address/address-form/address-form.component';
 export * from './components/multi-step-checkout/payment-method/payment-form/payment-form.component';
