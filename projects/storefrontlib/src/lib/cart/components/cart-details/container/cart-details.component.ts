d96cd35571982dc0ae5302ac459064b357f1c9a3
Roman Pylypets <roman.pylypets@hotmail.com>
2018-08-29 13:10:22 +0200

    SPA-920: Update cart-details component

--- projects/storefrontlib/src/lib/cart/components/cart-details/container/cart-details.component.ts
+++ projects/storefrontlib/src/lib/cart/components/cart-details/container/cart-details.component.ts
@@ -28,81 +28,14 @@ export class CartDetailsComponent implements OnInit {
 
     this.cart$ = this.store.select(fromCartStore.getActiveCart);
 
-    this.entries$ = this.store.select(fromCartStore.getEntries).pipe(
-      tap(entries => {
-        for (const entry of entries) {
-          const entryProductCode = entry.product.code;
-
-          if (!this.form.controls[entryProductCode]) {
-            this.form.setControl(
-              entry.product.code,
-              this.createEntryFormGroup(entry)
-            );
-          } else {
-            // update form on entries update
-            const entryForm = this.form.controls[entryProductCode] as FormGroup;
-            entryForm.controls.quantity.setValue(entry.quantity);
-          }
-        }
-      })
-    );
+    this.entries$ = this.store.select(fromCartStore.getEntries);
   }
 
   removeEntry(entry) {
     this.cartService.removeCartEntry(entry);
-    delete this.form.controls[entry.product.code];
   }
 
   updateEntry({ entry, updatedQuantity }) {
     this.cartService.updateCartEntry(entry.entryNumber, updatedQuantity);
-  }
-
-  getPotentialPromotionForEntry(cart: any, entry: any): any[] {
-    return this.getPromotionForEntry(cart.potentialProductPromotions, entry);
-  }
-
-  getAppliedPromotionForEntry(cart: any, entry: any): any[] {
-    return this.getPromotionForEntry(cart.appliedProductPromotions, entry);
-  }
-
-  private createEntryFormGroup(entry) {
-    return this.fb.group({
-      entryNumber: entry.entryNumber,
-      quantity: entry.quantity
-    });
-  }
-
-  private getPromotionForEntry(promotions: any[], entry: any): any {
-    const entryPromotions = [];
-    if (promotions && promotions.length > 0) {
-      for (const promotion of promotions) {
-        if (
-          promotion.description &&
-          promotion.consumedEntries &&
-          promotion.consumedEntries.length > 0
-        ) {
-          for (const consumedEntry of promotion.consumedEntries) {
-            if (this.isConsumedByEntry(consumedEntry, entry)) {
-              entryPromotions.push(promotion);
-            }
-          }
-        }
-      }
-    }
-    return entryPromotions;
-  }
-
-  private isConsumedByEntry(consumedEntry: any, entry: any): boolean {
-    const consumendEntryNumber = consumedEntry.orderEntryNumber;
-    if (entry.entries && entry.entries.length > 0) {
-      for (const subEntry of entry.entries) {
-        if (subEntry.entryNumber === consumendEntryNumber) {
-          return true;
-        }
-      }
-      return false;
-    } else {
-      return consumendEntryNumber === entry.entryNumber;
-    }
   }
 }
