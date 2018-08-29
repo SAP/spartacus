23670bb0eae6acf93223f1903af23ec25d1af087
Roman Pylypets <roman.pylypets@hotmail.com>
2018-08-29 13:10:50 +0200

    SPA-920: Update cart-item component

--- projects/storefrontlib/src/lib/cart/components/cart-shared/cart-item/cart-item.component.ts
+++ projects/storefrontlib/src/lib/cart/components/cart-shared/cart-item/cart-item.component.ts
@@ -1,32 +1,99 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ControlContainer, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: '[y-cart-item]',
+  selector: 'y-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
-  @Input() entry: any;
+  @Input() entries: any[];
   @Input() disableProductLink = false;
-  @Input() potentialPromotions: any[];
-  @Input() appliedPromotions: any[];
-  parent: FormGroup;
+  @Input() isReadOnly = false;
+  @Input() potentialProductPromotions: any[];
+
+  private currentWindowWidth: number;
+  form: FormGroup = this.fb.group({});
 
   timeout: any;
 
   @Output() remove = new EventEmitter<any>();
   @Output() update = new EventEmitter<any>();
-  constructor(private controlContainer: ControlContainer) {}
+  constructor(protected fb: FormBuilder) {}
+
+  @HostListener('window:resize')
+  onResize() {
+      this.currentWindowWidth = window.innerWidth;
+  }
 
   ngOnInit() {
-    this.parent = this.controlContainer.control as FormGroup;
-  }
-  removeEntry() {
-    this.remove.emit(this.entry);
+    this.currentWindowWidth = window.innerWidth;
+    this.entries.forEach(entry => {
+      const entryProductCode = entry.product.code;
+      if (!this.form.controls[entryProductCode]) {
+        this.form.setControl(
+          entry.product.code,
+          this.createEntryFormGroup(entry)
+        );
+      } else {
+        // update form on entries update
+        const entryForm = this.form.controls[entryProductCode] as FormGroup;
+        entryForm.controls.quantity.setValue(entry.quantity);
+      }
+    });
   }
 
-  updateEntry(updatedQuantity: number) {
-    this.update.emit({ entry: this.entry, updatedQuantity });
+  removeEntry(entry) {
+    this.remove.emit(entry);
+    delete this.form.controls[entry.product.code];
+  }
+
+  updateEntry(updatedQuantity: number, entry) {
+    this.update.emit({ entry, updatedQuantity });
+  }
+
+  get isMobile() {
+    return this.currentWindowWidth < 768;
+  }
+
+  getPromotionForEntry(entry: any): any {
+    const entryPromotions = [];
+    if (this.potentialProductPromotions && this.potentialProductPromotions.length > 0) {
+      for (const promotion of this.potentialProductPromotions) {
+        if (
+          promotion.description &&
+          promotion.consumedEntries &&
+          promotion.consumedEntries.length > 0
+        ) {
+          for (const consumedEntry of promotion.consumedEntries) {
+            if (this.isConsumedByEntry(consumedEntry, entry)) {
+              entryPromotions.push(promotion);
+            }
+          }
+        }
+      }
+    }
+    return entryPromotions;
+  }
+
+  private isConsumedByEntry(consumedEntry: any, entry: any): boolean {
+    const consumendEntryNumber = consumedEntry.orderEntryNumber;
+    if (entry.entries && entry.entries.length > 0) {
+      for (const subEntry of entry.entries) {
+        if (subEntry.entryNumber === consumendEntryNumber) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return consumendEntryNumber === entry.entryNumber;
+    }
+  }
+
+  private createEntryFormGroup(entry) {
+    return this.fb.group({
+      entryNumber: entry.entryNumber,
+      quantity: entry.quantity
+    });
   }
 }
