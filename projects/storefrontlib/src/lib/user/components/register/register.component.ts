e17d301f610889af07fa4a3cd1181a38178a5b54
Eric Xiao <e.xiao@sap.com>
2018-08-30 16:05:05 -0400

    SPA-904: inline email validation

--- projects/storefrontlib/src/lib/user/components/register/register.component.ts
+++ projects/storefrontlib/src/lib/user/components/register/register.component.ts
@@ -26,7 +26,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
       titleCode: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email, this.emailEndsWith]],
       password: ['', [Validators.required, this.validatePassword]],
       passwordconf: ['', Validators.required],
       newsletter: [false],
@@ -48,7 +48,6 @@ export class RegisterComponent implements OnInit, OnDestroy {
         }
       })
     );
-
     this.sub = this.store
       .select(fromAuthStore.getUserToken)
       .pipe(
@@ -94,6 +93,12 @@ export class RegisterComponent implements OnInit, OnDestroy {
     )
       ? null
       : { InvalidPassword: true };
+  }
+  private emailEndsWith(fc: FormControl) {
+    const email = fc.value as string;
+    // Enforce domain (e.g: .com )
+
+    return email.match('[.][a-zA-Z]+$') ? null : { InvalidEmail: true };
   }
 
   private matchPassword(ac: AbstractControl) {
