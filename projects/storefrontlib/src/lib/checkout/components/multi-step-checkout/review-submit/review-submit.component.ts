f0ee00351af168d90dfaf8a2b3ec795e6e8e6747
Weizheng Gao <weizheng.gao@sap.com>
2018-09-05 08:31:34 -0400

    feature/SPA-963: finish styling of review page

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/review-submit/review-submit.component.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/review-submit/review-submit.component.ts
@@ -2,19 +2,15 @@ import {
   Component,
   ChangeDetectionStrategy,
   Input,
-  Output,
-  OnInit,
-  EventEmitter
+  OnInit
 } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 import * as fromCheckoutStore from '../../../store';
 import * as fromUserStore from '../../../../user/store';
 import * as fromCartStore from '../../../../cart/store';
-import { CheckoutService } from '../../../services/checkout.service';
 import { Address } from '../../../models/address-model';
 
 @Component({
@@ -26,68 +22,43 @@ import { Address } from '../../../models/address-model';
 export class ReviewSubmitComponent implements OnInit {
   labels = {
     title: 'Review',
-    cartTotal: 'Cart total (3 items): $300.00',
-    orderItems: 'Order Items',
-    btnBack: 'Back',
-    btnPlaceOrder: 'Place Order'
+    orderItems: 'Order Items'
   };
 
   @Input() deliveryAddress: Address;
   @Input() shippingMethod: string;
   @Input() paymentDetails: any;
 
-  //deliveryMode$: Observable<any>;
-  //countryName$: Observable<any>;
-  //titleName$: Observable<any>;
-
   entries$: Observable<any>;
+  cart$: Observable<any>;
+  deliveryMode$: Observable<any>;
+  countryName$: Observable<any>;
 
-  constructor(
-    protected store: Store<fromCheckoutStore.CheckoutState>,
-    private service: CheckoutService
-  ) {}
+  constructor(protected store: Store<fromCheckoutStore.CheckoutState>) {}
 
   ngOnInit() {
+    this.cart$ = this.store.select(fromCartStore.getActiveCart);
     this.entries$ = this.store.select(fromCartStore.getEntries);
 
-    /*(this.deliveryMode$ = this.store
-      .select(fromCheckoutStore.getSelectedDeliveryMode)
-      .pipe(
-        tap(selected => {
-          if (selected === null) {
-            this.service.loadSupportedDeliveryModes();
-          }
-        })
-      );
+    this.deliveryMode$ = this.store.select(
+      fromCheckoutStore.getSelectedDeliveryMode
+    );
 
-    this.countryName$ = this.store
-      .select(
-        fromUserStore.countrySelectorFactory(
-          this.deliveryAddress.country.isocode
-        )
-      )
-      .pipe(
-        tap(country => {
-          if (country === null) {
-            this.store.dispatch(new fromUserStore.LoadDeliveryCountries());
-          }
-        })
-      );
-
-    this.titleName$ = this.store
-      .select(
-        fromUserStore.titleSelectorFactory(this.deliveryAddress.titleCode)
-      )
-      .pipe(
-        tap(title => {
-          if (title === null) {
-            this.store.dispatch(new fromUserStore.LoadTitles());
-          }
-        })
-      );*/
+    this.countryName$ = this.store.select(
+      fromUserStore.countrySelectorFactory(this.deliveryAddress.country.isocode)
+    );
   }
 
-  getAddressCard() {
+  getAddressCard(countryName) {
+    if (!countryName) {
+      countryName = this.deliveryAddress.country.isocode;
+    }
+
+    let region = '';
+    if (this.deliveryAddress.region && this.deliveryAddress.region.isocode) {
+      region = this.deliveryAddress.region.isocode;
+    }
+
     return {
       title: 'Ship To',
       textBold:
@@ -95,21 +66,18 @@ export class ReviewSubmitComponent implements OnInit {
       text: [
         this.deliveryAddress.line1,
         this.deliveryAddress.line2,
-        this.deliveryAddress.town +
-          ', ' +
-          this.deliveryAddress.region.isocode +
-          ', ' +
-          this.deliveryAddress.country.isocode,
+        this.deliveryAddress.town + ', ' + region + ', ' + countryName,
         this.deliveryAddress.postalCode,
         this.deliveryAddress.phone
       ]
     };
   }
 
-  getShippingMethodCard() {
+  getShippingMethodCard(deliveryMode) {
     return {
       title: 'Shipping Method',
-      text: [this.shippingMethod]
+      textBold: [this.shippingMethod],
+      text: [deliveryMode.description]
     };
   }
 
