136ea08cbe8a81189917f026849c838903ea0051
Patrick Labadie <patrick.labadie@sap.com>
2018-09-05 15:53:32 -0400

    SPA-991: Remove the additional providers for SiteContextModuleConfig.  It's already provided by SiteContextModule.

--- projects/storefrontlib/src/lib/site-context/currency-selector/currency-selector.module.ts
+++ projects/storefrontlib/src/lib/site-context/currency-selector/currency-selector.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CurrencySelectorComponent } from './currency-selector.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { SiteContextModuleConfig } from '../site-context-module-config';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
   declarations: [CurrencySelectorComponent],
-  exports: [CurrencySelectorComponent],
-  providers: [SiteContextModuleConfig]
+  exports: [CurrencySelectorComponent]
 })
 export class CurrencySelectorModule {}
--- projects/storefrontlib/src/lib/site-context/language-selector/language-selector.module.ts
+++ projects/storefrontlib/src/lib/site-context/language-selector/language-selector.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LanguageSelectorComponent } from './language-selector.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { SiteContextModuleConfig } from '../site-context-module-config';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
   declarations: [LanguageSelectorComponent],
-  exports: [LanguageSelectorComponent],
-  providers: [SiteContextModuleConfig]
+  exports: [LanguageSelectorComponent]
 })
 export class LanguageSelectorModule {}
