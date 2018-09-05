bb1723b30f3a7c93b9789a08c7b6c0364a8d535c
Patrick Labadie <patrick.labadie@sap.com>
2018-09-05 15:08:09 -0400

    SPA-1008: Replaced dummy test by a simple more relavant test.  Removed the ignored tests.

--- projects/storefrontapp/src/app/app.component.spec.ts
+++ projects/storefrontapp/src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+@Component({ selector: 'y-main', template: '' })
+class MainComponent {}
 
 describe('AppComponent', () => {
-  it('should be always true', async(() => {
-    expect(true).toBeTruthy();
-  }));
-});
-
-xdescribe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent, MainComponent]
     });
     TestBed.compileComponents();
   });
@@ -19,18 +17,5 @@ xdescribe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'spac works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spac works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('spac works!');
   }));
 });
