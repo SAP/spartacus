25a43894afc519d79005f4e9029b91ca0dddfdbc
Mateusz Kasprzak <kasprzak.mat@gmail.com>
2018-09-06 13:26:02 +0200

    SPA-973: Import NgbRating module with provider into form components module

--- projects/storefrontlib/src/lib/ui/components/form-components/form-components.module.ts
+++ projects/storefrontlib/src/lib/ui/components/form-components/form-components.module.ts
@@ -2,14 +2,21 @@ import { MaterialModule } from './../../../material.module';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { NgbRatingModule, NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 /* Components */
 import { StarRatingComponent } from './star-rating/star-rating.component';
 import { ItemCounterComponent } from './item-counter/item-counter.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    NgbRatingModule
+  ],
   declarations: [StarRatingComponent, ItemCounterComponent],
-  exports: [StarRatingComponent, ItemCounterComponent]
+  exports: [StarRatingComponent, ItemCounterComponent],
+  providers: [NgbRatingConfig]
 })
 export class FormComponentsModule {}
