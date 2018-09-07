cce4f5b13a131f2e9eeba3db655813b0b2ac1822
Roman Pylypets <roman.pylypets@hotmail.com>
2018-09-07 16:01:39 +0200

    SPA-920: Style cart item component

--- projects/storefrontlib/src/lib/cart/components/cart-shared/cart-item/cart-item.component.ts
+++ projects/storefrontlib/src/lib/cart/components/cart-shared/cart-item/cart-item.component.ts
@@ -1,12 +1,12 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  HostListener
-} from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, ControlContainer } from '@angular/forms';
+
+export interface Item {
+  product?: any;
+  quantity?: any;
+  basePrice?: any;
+  totalPrice?: any;
+}
 
 @Component({
   selector: 'y-cart-item',
@@ -14,101 +14,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
-  @Input() entries: any[];
-  @Input() disableProductLink = false;
-  @Input() isReadOnly = false;
+  @Input() readonly = false;
+
+  @Input() compact = false;
+
+  @Input() item: Item;
+
   @Input() potentialProductPromotions: any[];
 
-  private currentWindowWidth: number;
-  form: FormGroup = this.fb.group({});
-
-  timeout: any;
+  parent: FormGroup;
 
   @Output() remove = new EventEmitter<any>();
   @Output() update = new EventEmitter<any>();
-  constructor(protected fb: FormBuilder) {}
 
-  @HostListener('window:resize')
-  onResize() {
-    this.currentWindowWidth = window.innerWidth;
-  }
+  constructor(private controlContainer: ControlContainer) {}
 
   ngOnInit() {
-    this.currentWindowWidth = window.innerWidth;
-    this.entries.forEach(entry => {
-      const entryProductCode = entry.product.code;
-      if (!this.form.controls[entryProductCode]) {
-        this.form.setControl(
-          entry.product.code,
-          this.createEntryFormGroup(entry)
-        );
-      } else {
-        // update form on entries update
-        const entryForm = this.form.controls[entryProductCode] as FormGroup;
-        entryForm.controls.quantity.setValue(entry.quantity);
-      }
-    });
+    this.parent = this.controlContainer.control as FormGroup;
   }
 
   isProductOutOfStock(product) {
     // TODO Move stocklevelstatuses across the app to an enum
-    return product && product.stock && product.stock.stockLevelStatus === 'outOfStock';
+    return (
+      product &&
+      product.stock &&
+      product.stock.stockLevelStatus === 'outOfStock'
+    );
   }
 
-  removeEntry(entry) {
-    this.remove.emit(entry);
-    delete this.form.controls[entry.product.code];
+  updateItem(updatedQuantity: number, item) {
+    this.update.emit({ item, updatedQuantity });
   }
 
-  updateEntry(updatedQuantity: number, entry) {
-    this.update.emit({ entry, updatedQuantity });
-  }
-
-  get isMobile() {
-    return this.currentWindowWidth < 768;
-  }
-
-  getPromotionForEntry(entry: any): any {
-    const entryPromotions = [];
-    if (
-      this.potentialProductPromotions &&
-      this.potentialProductPromotions.length > 0
-    ) {
-      for (const promotion of this.potentialProductPromotions) {
-        if (
-          promotion.description &&
-          promotion.consumedEntries &&
-          promotion.consumedEntries.length > 0
-        ) {
-          for (const consumedEntry of promotion.consumedEntries) {
-            if (this.isConsumedByEntry(consumedEntry, entry)) {
-              entryPromotions.push(promotion);
-            }
-          }
-        }
-      }
-    }
-    return entryPromotions;
-  }
-
-  private isConsumedByEntry(consumedEntry: any, entry: any): boolean {
-    const consumendEntryNumber = consumedEntry.orderEntryNumber;
-    if (entry.entries && entry.entries.length > 0) {
-      for (const subEntry of entry.entries) {
-        if (subEntry.entryNumber === consumendEntryNumber) {
-          return true;
-        }
-      }
-      return false;
-    } else {
-      return consumendEntryNumber === entry.entryNumber;
-    }
-  }
-
-  private createEntryFormGroup(entry) {
-    return this.fb.group({
-      entryNumber: entry.entryNumber,
-      quantity: entry.quantity
-    });
+  removeItem() {
+    this.remove.emit(this.item);
   }
 }
