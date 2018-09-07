4cd5d0a0d36bdebbbd80029782514cd8b66eacc4
Roman Pylypets <roman.pylypets@hotmail.com>
2018-09-07 12:32:26 +0200

    SPA-920: Update props for cart-item inside cart-details

--- projects/storefrontlib/src/lib/cart/components/cart-details/container/cart-details.component.ts
+++ projects/storefrontlib/src/lib/cart/components/cart-details/container/cart-details.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs/operators';
 import { CartService } from '../../../services/cart.service';
 import * as fromCartStore from '../../../store';
 
@@ -15,12 +13,9 @@ export class CartDetailsComponent implements OnInit {
   cart$;
   entries$;
 
-  form: FormGroup = this.fb.group({});
-
   constructor(
     protected store: Store<fromCartStore.CartState>,
-    protected cartService: CartService,
-    protected fb: FormBuilder
+    protected cartService: CartService
   ) {}
 
   ngOnInit() {
@@ -31,28 +26,18 @@ export class CartDetailsComponent implements OnInit {
     this.entries$ = this.store.select(fromCartStore.getEntries);
   }
 
-  removeEntry(entry) {
-    this.cartService.removeCartEntry(entry);
-  }
-
-  updateEntry({ entry, updatedQuantity }) {
-    this.cartService.updateCartEntry(entry.entryNumber, updatedQuantity);
-  }
-
   getAllPromotionsForCart(cart) {
     const potentialPromotions = cart.potentialProductPromotions || [];
     const appliedPromotions = cart.appliedProductPromotions || [];
-    return [
-      ...potentialPromotions,
-      ...appliedPromotions,
-    ];
+    return [...potentialPromotions, ...appliedPromotions];
   }
 
   cartHasPromotions(cart) {
-    const hasPotentialPromotions = cart.potentialProductPromotions
-      && cart.potentialProductPromotions.length > 0;
-    const hasAppliedPromotions = cart.appliedProductPromotions
-      && cart.appliedProductPromotions.length > 0;
+    const hasPotentialPromotions =
+      cart.potentialProductPromotions &&
+      cart.potentialProductPromotions.length > 0;
+    const hasAppliedPromotions =
+      cart.appliedProductPromotions && cart.appliedProductPromotions.length > 0;
     return hasPotentialPromotions || hasAppliedPromotions;
   }
 }
