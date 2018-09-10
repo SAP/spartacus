67fed48e75c0f0c49fb7de3ddeeb588ee3f82daf
Krzysztof Platis <kplatis@divante.pl>
2018-09-10 15:46:19 +0200

    SPA-1066: add dummy ContactPage

---
+++ projects/storefrontlib/src/lib/ui/layout/contact-page-layout/contact-page-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'y-contact-page-layout',
+  templateUrl: './contact-page-layout.component.html',
+  styleUrls: ['./contact-page-layout.component.scss']
+})
+export class ContactPageLayoutComponent {
+  constructor() {}
+}
