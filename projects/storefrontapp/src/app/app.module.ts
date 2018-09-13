c64e16df9d4683fad3ae8668f608fa0dd320051a
Patrick Labadie <patrick.labadie@sap.com>
2018-09-13 15:22:00 -0400

    SPA-1104: Remove AppRoutingModule

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { config } from './config';
 
 import { MainComponent, StorefrontModule } from 'storefrontlib';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, StorefrontModule],
+  imports: [BrowserModule, RouterModule.forRoot([]), StorefrontModule],
   bootstrap: [MainComponent],
   providers: [{ provide: 'APP_CONFIG', useValue: config }]
 })
