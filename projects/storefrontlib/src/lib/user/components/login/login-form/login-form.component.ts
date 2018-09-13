6f4c639f86781f0662822fdbfc6283707da9ca29
Krzysztof Platis <kplatis@divante.pl>
2018-09-13 10:12:35 +0200

    SPA-941: add comment with reference to RFC document about email standard used in regex

--- projects/storefrontlib/src/lib/user/components/login/login-form/login-form.component.ts
+++ projects/storefrontlib/src/lib/user/components/login/login-form/login-form.component.ts
@@ -49,6 +49,7 @@ export class LoginFormComponent implements OnInit, OnDestroy {
         [
           Validators.required,
           Validators.pattern(
+            // Email Standard RFC 5322:
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // tslint:disable-line
           )
         ]
