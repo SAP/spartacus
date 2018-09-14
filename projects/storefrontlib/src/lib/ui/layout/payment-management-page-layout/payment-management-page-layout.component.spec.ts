d3709085760d133de240b7d0066875ee36fbd2a4
Krzysztof Platis <kplatis@divante.pl>
2018-09-14 17:02:47 +0200

    SPA-1107 fix unit tests for PageManagement(Layout)Page components

--- projects/storefrontlib/src/lib/ui/layout/payment-management-page-layout/payment-management-page-layout.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/layout/payment-management-page-layout/payment-management-page-layout.component.spec.ts
@@ -1,12 +1,36 @@
 import { PaymentManagementPageLayoutComponent } from './payment-management-page-layout.component';
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { CheckoutService } from '../../../checkout/services/checkout.service';
+
+const mockCheckoutService = {
+  loadUserPaymentMethods: () => {}
+};
+
+class MockStore<T> extends BehaviorSubject<T> {
+  select = () => this;
+}
 
 describe('PaymentManagementPageLayoutComponent', () => {
   let component: PaymentManagementPageLayoutComponent;
   let fixture: ComponentFixture<PaymentManagementPageLayoutComponent>;
+  let mockStore: MockStore<any>;
 
   beforeEach(async(() => {
+    mockStore = new MockStore({});
+
     TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Store,
+          useValue: mockStore
+        },
+        {
+          provide: CheckoutService,
+          useValue: mockCheckoutService
+        }
+      ],
       declarations: [PaymentManagementPageLayoutComponent]
     }).compileComponents();
   }));
--- projects/storefrontlib/src/lib/ui/pages/payment-management-page/payment-management-page.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/pages/payment-management-page/payment-management-page.component.spec.ts
@@ -1,6 +1,14 @@
 import { PaymentManagementPageComponent } from './payment-management-page.component';
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { PaymentManagementPageLayoutModule } from '../../layout/payment-management-page-layout/payment-management-page-layout.module';
+import { Component } from '@angular/core';
+import { PaymentManagementPageLayoutComponent } from '../../layout/payment-management-page-layout/payment-management-page-layout.component';
+
+@Component({
+  selector: 'y-payment-management-page-layout',
+  template: ''
+})
+class MockPaymentManagementPageLayoutComponent {}
 
 describe('PaymentManagementPageLayoutComponent', () => {
   let component: PaymentManagementPageComponent;
@@ -8,8 +16,10 @@ describe('PaymentManagementPageLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [PaymentManagementPageLayoutModule],
-      declarations: [PaymentManagementPageComponent]
+      declarations: [
+        PaymentManagementPageComponent,
+        MockPaymentManagementPageLayoutComponent
+      ]
     }).compileComponents();
   }));
   beforeEach(() => {
