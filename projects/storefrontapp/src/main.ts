75467ef2b8789e3a01630a2fe236142e09a7345d
Filip Rakowski <frakowski@divante.pl>
2018-09-14 14:26:23 +0200

    Web App Manifest support added

--- projects/storefrontapp/src/main.ts
+++ projects/storefrontapp/src/main.ts
@@ -8,4 +8,14 @@ if (environment.production) {
   enableProdMode();
 }
 
+let addToHomeScreenPrompt;
+
+window.addEventListener('beforeinstallprompt', (e: any) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  addToHomeScreenPrompt = e;
+  e.prompt();
+});
+
 platformBrowserDynamic().bootstrapModule(AppModule);
