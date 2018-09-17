5cae454e7c46aca69fda67296db7238324aaa01f
Vlasov, Andrey <andrey.vlasov@sap.com>
2018-09-17 16:19:57 -0400

    E2EC-942 display store features

--- projects/storefrontlib/src/lib/occ/store/store-finder.service.ts
+++ projects/storefrontlib/src/lib/occ/store/store-finder.service.ts
@@ -36,7 +36,7 @@ export class OccStoreFinderService {
     let params: HttpParams = new HttpParams({
       fromString:
         '&fields=stores(name,displayName,openingHours(weekDayOpeningList(FULL),specialDayOpeningList(FULL)),' +
-        'geoPoint(latitude,longitude),address(line1,line2,town,region(FULL),postalCode,phone,country) ),' +
+        'geoPoint(latitude,longitude),address(line1,line2,town,region(FULL),postalCode,phone,country), features),' +
         'pagination(DEFAULT),' +
         'sorts(DEFAULT)'
     });
