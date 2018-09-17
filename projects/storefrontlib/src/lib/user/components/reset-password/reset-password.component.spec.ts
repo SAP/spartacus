35897c5604b193e6966e20a65effb016fc2855c3
Mateusz Kolasa <mateusz.kolasa@sap.com>
2018-09-17 10:18:24 +0200

    SPA-933: Add additional UI unit tests

--- projects/storefrontlib/src/lib/user/components/reset-password/reset-password.component.spec.ts
+++ projects/storefrontlib/src/lib/user/components/reset-password/reset-password.component.spec.ts
@@ -1,21 +1,23 @@
 import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, AbstractControl } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { FormValidationService } from '../../../ui/services/form-validation/form-validation.service';
+import { CustomFormValidators } from '../../../ui/validators/custom-form-validators';
 import { ResetPasswordComponent } from './reset-password.component';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
   let form: DebugElement;
+  let submit: DebugElement;
+  let userId: AbstractControl;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, RouterTestingModule],
       declarations: [ResetPasswordComponent],
-      providers: [FormValidationService]
+      providers: [CustomFormValidators]
     }).compileComponents();
   }));
 
@@ -23,8 +25,10 @@ describe('ResetPasswordComponent', () => {
     fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
     form = fixture.debugElement.query(By.css('form'));
+    submit = fixture.debugElement.query(By.css('[type="submit"]'));
 
     component.ngOnInit();
+    userId = component.form.controls['userId'];
   });
 
   it('should create component', () => {
@@ -36,7 +40,7 @@ describe('ResetPasswordComponent', () => {
   });
 
   it('should form be valid when proper email is set', () => {
-    component.form.controls['userId'].setValue('test@test.com');
+    userId.setValue('test@test.com');
     expect(component.form.valid).toBeTruthy();
   });
 
@@ -46,9 +50,31 @@ describe('ResetPasswordComponent', () => {
 
   it('should call requestPasswordReset() method on submit', () => {
     const request = spyOn(component, 'requestPasswordReset');
-    component.form.controls['userId'].setValue('test@test.com');
+    userId.setValue('test@test.com');
     fixture.detectChanges();
     form.triggerEventHandler('submit', null);
     expect(request).toHaveBeenCalled();
+  });
+
+  it('should submit button be disabled when form is invalid', () => {
+    userId.setValue('test');
+    fixture.detectChanges();
+    expect(component.form.valid).toBeFalsy();
+    expect(submit.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should error message appear when email is invalid', () => {
+    fixture.detectChanges();
+    const input = component.form.controls['userId'];
+    input.setValue('');
+    input.markAsTouched();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const message = fixture.debugElement.nativeElement.querySelector(
+        '.invalid-feedback'
+      );
+      expect(component.form.valid).toBeFalsy();
+      expect(message).toBeTruthy();
+    });
   });
 });
