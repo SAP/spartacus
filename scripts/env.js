65721b9856679b03b209ae4fc61d1af04568e965
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-09-17 16:15:56 -0400

    SPA-1137 Initial version of script to add a meta tag to index.html

---
+++ scripts/env.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+const backend_url = 'http://localhost:9002';
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const fs = require('fs');
+
+const file = 'projects/storefrontapp/src/index.html';
+
+const config = {
+  FetchExternalResources: ['script'],
+  ProcessExternalResources: ['script'],
+  MutationEvents: '2.0',
+  QuerySelector: false
+};
+
+JSDOM.fromFile(file, config).then(dom => {
+  var document = dom.window.document;
+  var meta = document.createElement('meta');
+  meta.content = backend_url;
+  meta.name = 'occ-backend-base-url';
+  document.getElementsByTagName('head')[0].appendChild(meta);
+
+  fs.writeFile(file, document.documentElement.outerHTML, function(error) {
+    if (error) {
+      console.log('Error' + error);
+    }
+  });
+});
