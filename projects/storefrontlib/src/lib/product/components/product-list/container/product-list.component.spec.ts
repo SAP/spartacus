68574b887100d1a30e649dcc74d0d5eea9ab8465
Weizheng Gao <weizheng.gao@sap.com>
2018-09-17 10:10:41 -0400

    feature/SPA-925: fix lint error

--- projects/storefrontlib/src/lib/product/components/product-list/container/product-list.component.spec.ts
+++ projects/storefrontlib/src/lib/product/components/product-list/container/product-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductPagingComponent } from '../product-paging/product-paging.component';
