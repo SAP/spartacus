1d8946df0cc3607cfa7de0316c7b4ec33054f80c
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-09-17 13:17:15 +0200

    SPA-1147: Use commerce backend baseUrl from environment file

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MainComponent, StorefrontModule } from 'storefrontlib';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot([]), StorefrontModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]),
+    StorefrontModule.withConfig({
+      server: {
+        baseUrl: environment.occBaseUrl,
+        occPrefix: '/rest/v2/'
+      }
+    })
+  ],
   bootstrap: [MainComponent]
 })
 export class AppModule {}
