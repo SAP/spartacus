640ed323da120ee3b5cb83317007042a52867619
Weizheng Gao <weizheng.gao@sap.com>
2018-09-18 19:27:26 -0400

    feature/SPA-1169: fix test

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/review-submit/review-submit.module.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/review-submit/review-submit.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CardModule } from '../../../../ui/components/card/card.module';
 import { ReviewSubmitComponent } from './review-submit.component';
-import { CartModule } from '../../../../cart/cart.module';
+import { CartDetailsModule } from '../../../../cart/components/cart-details/cart-details.module';
 
 @NgModule({
-  imports: [CommonModule, CardModule, CartModule],
+  imports: [CommonModule, CardModule, CartDetailsModule],
   declarations: [ReviewSubmitComponent],
   entryComponents: [ReviewSubmitComponent],
   exports: [ReviewSubmitComponent]
