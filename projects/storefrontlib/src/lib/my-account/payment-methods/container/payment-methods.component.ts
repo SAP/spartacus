c310003dbacc5a00c5e9252da127f428b01ae7be
Krzysztof Platis <kplatis@divante.pl>
2018-09-19 12:25:44 +0200

    SPA-1108: style cards in payment methods and perform delete confirmation

--- projects/storefrontlib/src/lib/my-account/payment-methods/container/payment-methods.component.ts
+++ projects/storefrontlib/src/lib/my-account/payment-methods/container/payment-methods.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromUserStore from '../../../user/store';
+import { CheckoutService } from '../../../checkout/services/checkout.service';
+import { tap, map, filter } from 'rxjs/operators';
+import { Card } from '../../../ui/components/card/card.component';
+import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'y-payment-methods',
@@ -6,7 +13,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-methods.component.scss']
 })
 export class PaymentMethodsComponent implements OnInit {
-  constructor() {}
+  paymentMethods$: Observable<any>;
 
-  ngOnInit() {}
+  constructor(
+    protected store: Store<fromUserStore.UserState>,
+    // tslint:disable-next-line:no-unused-variable max-line-length
+    private cartService: CartService, // hack: unused, but it has to be created before CheckoutService, because CartService initializes CartDataService
+    protected checkoutService: CheckoutService
+  ) {}
+
+  ngOnInit() {
+    this.paymentMethods$ = this.store
+      .select(fromUserStore.getPaymentMethods)
+      .pipe(
+        tap(paymentMethods => {
+          if (!paymentMethods.length) {
+            this.checkoutService.loadUserPaymentMethods();
+          }
+        })
+      );
+  }
+
+  getCardContent(payment): Card {
+    let ccImage;
+    if (payment.cardType.code === 'visa') {
+      ccImage = 'assets/visa.png';
+    } else if (payment.cardType.code === 'master') {
+      ccImage = 'assets/masterCard.png';
+    }
+    const card: Card = {
+      header: payment.defaultPayment ? 'DEFAULT' : null,
+      textBold: payment.accountHolderName,
+      text: [
+        payment.cardNumber,
+        'Expires: ' + payment.expiryMonth + '/' + payment.expiryYear
+      ],
+      img: ccImage,
+      actions: [{ name: 'DELETE', event: 'delete' }],
+      deleteMsg: 'Are you sure you want to delete this payment method?'
+    };
+
+    return card;
+  }
+
+  deletePaymentMethod(paymentMethod) {
+    console.log('TODO: delete payment method in backend'); // TODO delete payment method in backend
+  }
 }
--- projects/storefrontlib/src/lib/my-account/payment-methods/payment-methods.module.ts
+++ projects/storefrontlib/src/lib/my-account/payment-methods/payment-methods.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentMethodsComponent } from './container/payment-methods.component';
+import { CardModule } from '../../ui/components/card/card.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule],
   declarations: [PaymentMethodsComponent],
   exports: [PaymentMethodsComponent]
 })
