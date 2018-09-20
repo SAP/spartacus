5fc5339f1bbabcd2467d8e756189454e99bf575d
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-09-20 15:31:02 +0200

    SPA-1172: fix linting

--- projects/storefrontlib/src/lib/cms/services/index.ts
+++ projects/storefrontlib/src/lib/cms/services/index.ts
@@ -1,7 +1,3 @@
-import { OccCmsService } from './occ-cms.service';
-import { ComponentMapperService } from './component-mapper.service';
-import { DefaultPageService } from './default-page.service';
-
 export * from './occ-cms.service';
 export * from './component-mapper.service';
 export * from './default-page.service';
