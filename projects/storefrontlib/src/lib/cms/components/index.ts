ca7c22976f75ec2f73e0adbf046e015b8b8bd1b0
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-09-20 11:06:59 +0200

    SPA-1172: fix export

--- projects/storefrontlib/src/lib/cms/components/index.ts
+++ projects/storefrontlib/src/lib/cms/components/index.ts
@@ -1,4 +1,5 @@
 export * from './dynamic-slot/dynamic-slot.module';
+export * from './dynamic-slot/dynamic-slot.component';
 
 import { DynamicSlotComponent } from './dynamic-slot/dynamic-slot.component';
 import { ComponentWrapperDirective } from './dynamic-slot/component-wrapper.directive';
