09cdc9ed587a5eb8aafd246be3530c8a13169317
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-09-21 15:28:39 +0200

    SPA-1091: export custom templates

---
+++ projects/storefrontlib/src/lib/custom-templates/index.ts
@@ -0,0 +1,5 @@
+export * from './custom-templates.module';
+export * from './custom-templates.model';
+export * from './custom-template.service';
+export * from './override-template.directive';
+export * from './template.directive';
