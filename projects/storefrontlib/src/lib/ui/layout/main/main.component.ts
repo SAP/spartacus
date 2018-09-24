4bb400adec6a3ad537adad32af7cca898b5dac60
Patrick Labadie <patrick.labadie@sap.com>
2018-09-24 14:29:22 -0400

    SPA-1219: Enable the cx component prefix and rename the y-main selector to cx-storefront.

--- projects/storefrontlib/src/lib/ui/layout/main/main.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'y-main',
+  selector: 'cx-storefront',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
