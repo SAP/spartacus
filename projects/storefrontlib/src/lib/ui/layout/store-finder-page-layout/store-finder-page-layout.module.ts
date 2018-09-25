6d4d27b6160d5aa4952ff0488a0b3c16681b65a7
Patrick Labadie <patrick.labadie@sap.com>
2018-09-25 16:11:38 -0400

    SPA-1256: Remove Material from the store finder.

--- projects/storefrontlib/src/lib/ui/layout/store-finder-page-layout/store-finder-page-layout.module.ts
+++ projects/storefrontlib/src/lib/ui/layout/store-finder-page-layout/store-finder-page-layout.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreFinderPageLayoutComponent } from './store-finder-page-layout.component';
 import { CmsModule } from '../../../cms/cms.module';
-import { MaterialModule } from '../../../material.module';
 
 @NgModule({
-  imports: [CommonModule, CmsModule, MaterialModule],
+  imports: [CommonModule, CmsModule],
   declarations: [StoreFinderPageLayoutComponent],
   exports: [StoreFinderPageLayoutComponent]
 })
