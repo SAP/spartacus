8154bc3b1f3967d82d6c3becb499b029ade2cc3a
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2018-10-03 11:42:19 -0400

    SPA-1333: Fixed access denied bug in empty cart

--- projects/storefrontlib/src/lib/ui/layout/cart-page-layout/cart-page-layout.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/cart-page-layout/cart-page-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import * as fromCartStore from '../../../cart/store';
+import * as fromUserStore from '../../../auth/store';
 import { Subscription, Observable } from 'rxjs';
 import { CartService } from '../../../cart/services/cart.service';
 
@@ -19,7 +20,11 @@ export class CartPageLayoutComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.cartService.loadCartDetails();
+    this.store.pipe(select(fromUserStore.getUserToken)).subscribe(token => {
+      if (token.access_token) {
+        this.cartService.loadCartDetails();
+      }
+    });
     this.cart$ = this.store.select(fromCartStore.getActiveCart);
   }
 }
