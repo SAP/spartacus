49dcebc123d5e037cf37ffff9a8d5c6018113652
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-10-03 14:48:33 +0200

    SPA-1297: Typos

--- projects/storefrontlib/src/lib/config/server-config/server-config-validator.spec.ts
+++ projects/storefrontlib/src/lib/config/server-config/server-config-validator.spec.ts
@@ -2,14 +2,14 @@ import { serverConfigValidator } from './server-config-validator';
 import { ServerConfig } from '@spartacus/storefront';
 
 describe('serverConfigValidator', () => {
-  it('should warn about undefined baseUsl', () => {
+  it('should warn about undefined baseUrl', () => {
     const invalidConfig: ServerConfig = {
       server: {}
     };
     expect(serverConfigValidator(invalidConfig)).toBeTruthy();
   });
 
-  it('should not warn about undefined baseUsl', () => {
+  it('should not warn about undefined baseUrl', () => {
     const invalidConfig: ServerConfig = {
       server: {
         baseUrl: ''
