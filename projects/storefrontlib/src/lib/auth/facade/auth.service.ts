873ec900d7403fd96a7bb9f93e6005e36d233544
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-10-08 23:45:27 +0200

    SPA-1377: Basic auth facade

---
+++ projects/storefrontlib/src/lib/auth/facade/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import * as fromAuthStore from '../store';
+import * as fromStore from '../../user/store';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  readonly userToken$ = this.store.pipe(select(fromAuthStore.getUserToken));
+
+  constructor(private store: Store<fromStore.UserState>) {}
+
+  authorize(userId: string, password: string) {
+    this.store.dispatch(
+      new fromAuthStore.LoadUserToken({
+        userId: userId,
+        password: password
+      })
+    );
+  }
+
+}
