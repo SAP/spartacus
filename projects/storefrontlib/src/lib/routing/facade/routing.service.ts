d2d8ee66635ca15933255b0d46554d04d4a3c0dd
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-10-08 23:45:55 +0200

    SPA-1377: Enhance routing facade

--- projects/storefrontlib/src/lib/routing/facade/routing.service.ts
+++ projects/storefrontlib/src/lib/routing/facade/routing.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 
 import * as fromStore from '../store';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoutingService {
+  readonly redirectUrl$ = this.store.pipe(select(fromStore.getRedirectUrl));
+
   constructor(private store: Store<fromStore.RouterState>) {}
 
-  public go(path: string, query: any) {
+  public go(path: string, query?: any) {
     this.store.dispatch(
       new fromStore.Go({
         path: [path, query]
       })
     );
+  }
+
+  clearRedirectUrl() {
+    this.store.dispatch(new fromStore.ClearRedirectUrl());
+  }
+
+  back() {
+    this.store.dispatch(new fromStore.Back());
   }
 }
