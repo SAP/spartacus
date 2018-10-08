cf02448481a107f4480dfae8a7b73a2907bcd88e
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-10-08 12:19:18 +0200

    SPA:1185: Move search suggestions to search service

--- projects/storefrontlib/src/lib/cms-lib/search-box/search-box-component.service.ts
+++ projects/storefrontlib/src/lib/cms-lib/search-box/search-box-component.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CmsComponentData } from '../../cms/components/cms-component-data';
-import { select, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import * as fromStore from '../../cms/store';
 import { ProductSearchService } from '../../product/services/product-search.service';
 import { combineLatest, merge, Observable, of } from 'rxjs';
@@ -11,7 +11,6 @@ import {
   switchMap
 } from 'rxjs/operators';
 import * as fromRouting from '../../routing/store';
-import * as fromProductStore from '../../product/store';
 
 interface SearchBoxConfig {
   maxProducts: number;
@@ -72,7 +71,7 @@ export class SearchBoxComponentService {
   private fetch(text: string, config: SearchBoxConfig): Observable<any[]> {
     this.executeSearch(text, config);
 
-    return this.store.pipe(select(fromProductStore.getProductSuggestions));
+    return this.searchService.searchSuggestions$;
   }
 
   // Uncomment for product search
