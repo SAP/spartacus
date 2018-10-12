386055f79def23191bc6b4d5e965b6da68a0c67a
test <whisp3r3r@gmx.de>
2018-10-12 12:00:42 +0200

    Update build.sh
    removed unit tests

--- build.sh
+++ build.sh
@@ -55,15 +55,6 @@ echo "Starting pipeline for Spartacus project"
 echo "-----"
 echo "Updating dependencies"
 yarn
-echo "-----"
-echo "Running unit tests and code coverage for core lib"
-exec 5>&1
-output=$(ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 0
-fi
 
 echo "-----"
 echo "Building SPA core lib"
