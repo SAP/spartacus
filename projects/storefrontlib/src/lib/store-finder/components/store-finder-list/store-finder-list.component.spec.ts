1b5b30aa6cde9df3158d77f04afbed4aa9d96907
Venelin Koulaxazov <venelin.koulaxazov@sap.com>
2018-10-18 10:33:54 -0400

    add extra test

--- projects/storefrontlib/src/lib/store-finder/components/store-finder-list/store-finder-list.component.spec.ts
+++ projects/storefrontlib/src/lib/store-finder/components/store-finder-list/store-finder-list.component.spec.ts
@@ -3,6 +3,7 @@ import { combineReducers, Store, StoreModule } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { StoreFinderListComponent } from './store-finder-list.component';
 import { PaginationComponent } from '../../../ui/components/pagination-and-sorting/pagination/pagination.component';
@@ -10,11 +11,38 @@ import { PaginationComponent } from '../../../ui/components/pagination-and-sorti
 import * as fromReducers from '../../store';
 import * as fromRoot from '../../../routing/store';
 import * as fromServices from '../../services';
+import { StoreFinderMapComponent } from '../store-finder-map/store-finder-map.component';
+import { OccModuleConfig } from '../../../occ';
+import { OccE2eConfigurationService } from '../../../occ/e2e/e2e-configuration-service';
+import { StoreDataService } from '../../services';
+import { GoogleMapRendererService } from '../../services/google-map-renderer.service';
 
-describe('StoreFinderListComponent', () => {
+const location = {};
+const stores = [location];
+const locations = [];
+
+class StoreDataServiceMock {
+  getStoreLatitude(_location: any): number {
+    return 35.528984;
+  }
+
+  getStoreLongitude(_location: any): number {
+    return 139.700168;
+  }
+}
+
+class GoogleMapRendererServiceMock {
+  centerMap(_latitute: number, _longitude: number) {}
+}
+
+fdescribe('StoreFinderListComponent', () => {
   let component: StoreFinderListComponent;
   let fixture: ComponentFixture<StoreFinderListComponent>;
   let store: Store<fromReducers.StoresState>;
+  let storeMapComponent: StoreFinderMapComponent;
+  let storeDataService: StoreDataService;
+  let googleMapRendererService: GoogleMapRendererService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -26,8 +54,17 @@ describe('StoreFinderListComponent', () => {
         })
       ],
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [StoreFinderListComponent],
-      providers: [...fromServices.services]
+      declarations: [StoreFinderListComponent, StoreFinderMapComponent],
+      providers: [
+        ...fromServices.services,
+        {
+          provide: GoogleMapRendererService,
+          useClass: GoogleMapRendererServiceMock
+        },
+        { provide: StoreDataService, useClass: StoreDataServiceMock },
+        OccE2eConfigurationService,
+        OccModuleConfig
+      ]
     }).compileComponents();
   }));
 
@@ -37,6 +74,16 @@ describe('StoreFinderListComponent', () => {
     store = TestBed.get(Store);
     spyOn(store, 'dispatch');
     fixture.detectChanges();
+    component.locations = locations;
+    component.locations.stores = stores;
+    storeMapComponent = fixture.debugElement.query(By.css('y-store-finder-map'))
+      .componentInstance;
+    spyOn(storeMapComponent, 'centerMap').and.callThrough();
+    storeDataService = TestBed.get(StoreDataService);
+    googleMapRendererService = TestBed.get(GoogleMapRendererService);
+    spyOn(storeDataService, 'getStoreLatitude');
+    spyOn(storeDataService, 'getStoreLongitude');
+    spyOn(googleMapRendererService, 'centerMap');
   });
 
   it('should create', () => {
@@ -56,8 +103,10 @@ describe('StoreFinderListComponent', () => {
   });
 
   it('should center store on map', () => {
-    spyOn(component.storeMap, 'centerMap').and.callThrough();
-    component.centerStoreOnMapByIndex(1);
-    expect(component.storeMap.centerMap).toHaveBeenCalled();
+    component.centerStoreOnMapByIndex(0);
+    expect(storeMapComponent.centerMap).toHaveBeenCalled();
+    expect(storeDataService.getStoreLatitude).toHaveBeenCalled();
+    expect(storeDataService.getStoreLongitude).toHaveBeenCalled();
+    expect(googleMapRendererService.centerMap).toHaveBeenCalled();
   });
 });
