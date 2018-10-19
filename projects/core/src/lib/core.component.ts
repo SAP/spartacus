95ba282f5cb37e8f0e333760b8e23b25ef287aaf
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-19 10:51:54 +0200

    SPA-1400: Introduce core library

---
+++ projects/core/src/lib/core.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cx-core',
+  template: `
+    <p>
+      core works!
+    </p>
+  `,
+  styles: []
+})
+export class CoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/core/src/lib/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CoreComponent } from './core.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [CoreComponent],
+  exports: [CoreComponent]
+})
+export class CoreModule { }
