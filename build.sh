bdf5c7f4440ad7de89a5a976631ed2a1c96e3b8c
Marcin Lasak <mlasak@divante.pl>
2018-10-24 11:30:35 +0200

    SPA-1409: Fix unit test command after splitting core

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ validatestyles
 echo "-----"
 echo "Running unit tests and code coverage for core lib"
 exec 5>&1
-output=$(ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$((ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless && ng test core --watch=false --code-coverage --browsers=ChromeHeadless) | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
