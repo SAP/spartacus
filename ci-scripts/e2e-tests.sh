b9b7de1a9aafa005573339c3a466bff01eaf9e5a
Marcin Lasak <lasakmarcin90@gmail.com>
2018-10-26 12:03:04 +0200

    Split E2E and build tasks

--- ci-scripts/e2e-tests.sh
+++ ci-scripts/e2e-tests.sh
@@ -2,27 +2,6 @@
 set -e
 set -o pipefail
 
-function validatestyles {
-    echo "-----"
-    echo "Validating styles app"
-    pushd projects/storefrontstyles
-    yarn
-    yarn sass
-    rm -rf temp-scss
-    popd
-}
-
-validatestyles
-
-echo "Updating dependencies"
-yarn
-echo "-----"
-echo "Building SPA core lib"
-yarn build:core:lib
-echo "-----"
-echo "Building SPA app"
-yarn build
-echo "-----"
 echo "Running end to end tests"
 yarn e2e:ci
 echo "-----"
