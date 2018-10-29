95148dda5c40860c077845f8994ac71d1ea77378
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-29 12:41:43 +0100

    Move SiteContext dependency to the right module

--- projects/storefrontlib/src/lib/site-context/currency-selector/currency-selector.module.ts
+++ projects/storefrontlib/src/lib/site-context/currency-selector/currency-selector.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurrencySelectorComponent } from './currency-selector.component';
 
+import { SiteContextModule } from '@spartacus/core';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SiteContextModule],
   declarations: [CurrencySelectorComponent],
   exports: [CurrencySelectorComponent]
 })
--- projects/storefrontlib/src/lib/site-context/site-context.module.ts
+++ projects/storefrontlib/src/lib/site-context/site-context.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 
-import { SiteContextModule as SiteContext } from '@spartacus/core';
-
 import { CurrencySelectorModule } from './currency-selector/currency-selector.module';
 import { LanguageSelectorModule } from './language-selector/language-selector.module';
 
 @NgModule({
-  imports: [SiteContext, CurrencySelectorModule, LanguageSelectorModule],
+  imports: [CurrencySelectorModule, LanguageSelectorModule],
   exports: [CurrencySelectorModule, LanguageSelectorModule]
 })
 export class SiteContextModule {}
