960ec212bb8a2096ce8c7fa990f7f99c43c00c25
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-29 16:07:17 +0100

    Export actions and selectors for reuse and extensibility

--- projects/core/src/site-context/index.ts
+++ projects/core/src/site-context/index.ts
@@ -3,3 +3,5 @@ export * from './config/config';
 export * from './occ/occ-site.service';
 
 export * from './store/state';
+export * from './store/actions/index';
+export * from './store/selectors/index';
