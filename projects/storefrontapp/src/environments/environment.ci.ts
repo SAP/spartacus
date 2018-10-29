a443b04be6d29b2ffcdc462427baad85eb15c420
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-10-29 08:54:55 -0400

    SPA-1414 pointing to our new model t for SPA

--- projects/storefrontapp/src/environments/environment.ci.ts
+++ projects/storefrontapp/src/environments/environment.ci.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   occBaseUrl:
-    'https://storefront.c39j2-walkersde1-d2-public.model-t.cc.commerce.ondemand.com'
+    'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com'
 };
