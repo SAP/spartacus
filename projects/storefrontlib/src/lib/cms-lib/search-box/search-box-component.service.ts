d34a6a6d5871adef6a5f73396bccfb865900cb54
Marcin Lasak <lasakmarcin90@gmail.com>
2018-10-30 11:49:46 +0100

    CR fixes

--- projects/storefrontlib/src/lib/cms-lib/search-box/search-box-component.service.ts
+++ projects/storefrontlib/src/lib/cms-lib/search-box/search-box-component.service.ts
@@ -10,7 +10,7 @@ import {
 } from 'rxjs/operators';
 import { RoutingService } from '../../routing/facade/routing.service';
 
-export interface SearchBoxConfig {
+interface SearchBoxConfig {
   maxProducts: number;
   displaySuggestions: boolean;
   maxSuggestions: number;
