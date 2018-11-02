81b801e9a4fda04af053f9a59285ae9d6effd40b
Patrick Labadie <patrick.labadie@sap.com>
2018-11-02 16:46:55 -0400

    #159: Replace ngrx store usage with a mocked facade in the product-list-item component tests.  Replace sub component implementations by mocks.

--- projects/storefrontlib/src/lib/product/components/product-list/product-list-item/product-list-item.component.spec.ts
+++ projects/storefrontlib/src/lib/product/components/product-list/product-list-item/product-list-item.component.spec.ts
@@ -1,19 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductListItemComponent } from './product-list-item.component';
-import { PictureComponent } from '../../../../ui/components/media/picture/picture.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AddToCartComponent } from '../../../../cart/components/add-to-cart/add-to-cart.component';
-import { CartService, CartDataService } from '../../../../cart/services';
-import { StoreModule, combineReducers } from '@ngrx/store';
-import * as fromRoot from '../../../../routing/store';
-import * as fromCart from '../../../../cart/store';
-import * as fromUser from '../../../../user/store';
-import * as fromAuth from '../../../../auth/store';
-import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
-import { StarRatingComponent } from '../../../../ui';
-import { ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-describe('ProductListItemComponent in product-list', () => {
+@Component({
+  selector: 'y-add-to-cart',
+  template: '<button>add to cart</button>'
+})
+export class MockAddToCartComponent {
+  @Input()
+  iconOnly;
+  @Input()
+  productCode;
+  @Input()
+  quantity;
+}
+
+@Component({
+  selector: 'y-star-rating',
+  template: '*****'
+})
+export class MockStarRatingComponent {
+  @Input()
+  rating = 1;
+  @Input()
+  disabled = false;
+  @Input()
+  steps = 1;
+}
+
+@Component({
+  selector: 'y-picture',
+  template: 'mock picture component'
+})
+export class MockPictureComponent {
+  @Input()
+  imageContainer;
+  @Input()
+  imageFormat;
+  @Input()
+  imagePosition;
+  @Input()
+  imageAlt;
+}
+fdescribe('ProductListItemComponent in product-list', () => {
   let component: ProductListItemComponent;
   let fixture: ComponentFixture<ProductListItemComponent>;
 
@@ -35,23 +65,13 @@ describe('ProductListItemComponent in product-list', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        NgbRatingModule,
-        StoreModule.forRoot({
-          ...fromRoot.getReducers(),
-          cart: combineReducers(fromCart.getReducers()),
-          user: combineReducers(fromUser.getReducers()),
-          auth: combineReducers(fromAuth.getReducers())
-        })
-      ],
+      imports: [RouterTestingModule],
       declarations: [
         ProductListItemComponent,
-        PictureComponent,
-        AddToCartComponent,
-        StarRatingComponent
-      ],
-      providers: [CartService, CartDataService]
+        MockPictureComponent,
+        MockAddToCartComponent,
+        MockStarRatingComponent
+      ]
     })
       .overrideComponent(ProductListItemComponent, {
         set: { changeDetection: ChangeDetectionStrategy.Default }
