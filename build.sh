d1f38b8962c77beef58c4ad38b55ffd558b8b020
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-11-02 15:02:09 -0400

    #144 Testing slack notifications

--- build.sh
+++ build.sh
@@ -25,6 +25,8 @@ function validateTsConfigFile {
     fi;
 }
 
+exit 0
+
 LOCAL_ENV_LIB_PATH="projects/storefrontlib/src/public_api"
 TSCONFIGFILE_TO_VALIDATE="projects/storefrontapp/tsconfig.app.prod.json"
 validateTsConfigFile
