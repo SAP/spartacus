1d2fd860bc3913c12332871d1bdec7678878aa1a
Patrick Labadie <patrick.labadie@sap.com>
2018-11-02 14:23:48 -0400

    #159: Replace ngrx store usage with a mocked facade in the product-detail component tests.

--- projects/storefrontlib/src/lib/product/components/product-details/container/product-details.component.spec.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/container/product-details.component.spec.ts
@@ -1,14 +1,12 @@
 import { ComponentsModule } from './../../../../ui/components/components.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { combineReducers, StoreModule } from '@ngrx/store';
 import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { ComponentMapperService } from '../../../../cms/services/component-mapper.service';
 import { ProductService } from '../../../facade/product.service';
 
-import * as fromRoot from '../../../../routing/store';
-import * as fromProduct from '../../../store/reducers';
 import { BootstrapModule } from '../../../../bootstrap.module';
 import { ProductDetailsComponent } from './product-details.component';
 import { OutletDirective } from '../../../../outlet';
@@ -23,6 +21,14 @@ import { ProductReviewsComponent } from '../product-reviews/product-reviews.comp
 import { Component, Input } from '@angular/core';
 
 class MockComponentMapperService {}
+
+const mockProduct = 'mockProduct';
+
+class MockProductService {
+  get(): Observable<any> {
+    return of(mockProduct);
+  }
+}
 
 @Component({
   selector: 'y-add-to-cart',
@@ -42,19 +48,9 @@ describe('ProductDetailsComponent in product', () => {
   let productDetailsComponent: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
 
-  const mockProduct = 'mockProduct';
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        ReactiveFormsModule,
-        BootstrapModule,
-        StoreModule.forRoot({
-          ...fromRoot.getReducers(),
-          products: combineReducers(fromProduct.getReducers())
-        }),
-        ComponentsModule
-      ],
+      imports: [ReactiveFormsModule, BootstrapModule, ComponentsModule],
       declarations: [
         ProductDetailsComponent,
 
@@ -68,7 +64,10 @@ describe('ProductDetailsComponent in product', () => {
         OutletDirective
       ],
       providers: [
-        ProductService,
+        {
+          provide: ProductService,
+          useClass: MockProductService
+        },
         {
           provide: ComponentMapperService,
           useClass: MockComponentMapperService
