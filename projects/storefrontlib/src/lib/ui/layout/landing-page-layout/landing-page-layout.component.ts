6dea965fb90adbae2deb3e5f66336dc97596da29
droshev <mladen.droshev@sap.com>
2018-11-07 08:49:24 -0500

    #10 styling for landing page

--- projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'cx-landing-page-layout',
   templateUrl: './landing-page-layout.component.html',
-  styleUrls: ['./landing-page-layout.component.scss']
+  styleUrls: ['./landing-page-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LandingPageLayoutComponent implements OnInit {
   constructor() {}
