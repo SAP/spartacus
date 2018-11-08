e4c7d12bf4f535b441b6a6fe56b25dd2b7550666
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-11-08 21:18:08 +0100

    #235 Fix barrel import

--- projects/core/src/routing/routing.module.ts
+++ projects/core/src/routing/routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { ConfigModule, Config } from '../config';
+import { ConfigModule, Config } from '../config/index';
 
 import {
   StoreRouterConnectingModule,
