b4fee83ff067d21672d2a8e26dcfc67e17a9f9d6
test <whisp3r3r@gmx.de>
2018-11-08 10:53:42 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o pipefail
 
 echo "Updating dependencies"
 yarn
-echo "-----"
-./ci-scripts/other.sh
-echo "-----"
-./ci-scripts/lint.sh
-echo "-----"
-./ci-scripts/unit-tests.sh
-echo "-----"
-./ci-scripts/e2e-tests.sh
+#echo "-----"
+#./ci-scripts/other.sh
+#echo "-----"
+#./ci-scripts/lint.sh
+#echo "-----"
+#./ci-scripts/unit-tests.sh
+#echo "-----"
+#./ci-scripts/e2e-tests.sh
