c95bd3a5c025f00b59b7da5044d7ecce0603028c
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-08 11:02:30 +0100

    Toggle waiting class to image thumbnails

--- projects/storefrontlib/src/lib/product/components/product-details/product-images/product-images.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/product-images/product-images.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
+const WAITING_CLASS = 'waiting';
+
 @Component({
   selector: 'cx-product-images',
   templateUrl: './product-images.component.html',
@@ -10,17 +12,37 @@ export class ProductImagesComponent implements OnChanges {
   product: any;
   mainImage;
 
+  waiting: HTMLElement;
+
   ngOnChanges() {
     if (this.product && this.product.images) {
       this.mainImage = this.product.images.PRIMARY;
     }
   }
 
-  showImage(imageContainer) {
+  showImage(event: MouseEvent, imageContainer) {
+    this.start(<HTMLElement>event.target);
     this.mainImage = imageContainer;
   }
 
   isMainImage(image) {
     return image.zoom.url === this.mainImage.zoom.url;
+  }
+
+  loadHandler() {
+    this.clearWaitList();
+  }
+
+  private start(el: HTMLElement) {
+    this.clearWaitList();
+    el.classList.add(WAITING_CLASS);
+    this.waiting = el;
+  }
+
+  private clearWaitList() {
+    if (this.waiting) {
+      this.waiting.classList.remove(WAITING_CLASS);
+      delete this.waiting;
+    }
   }
 }
