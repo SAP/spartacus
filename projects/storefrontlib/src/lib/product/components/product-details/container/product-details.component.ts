027bf228987805b05c138b8dd7213431fb9a41b0
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-09 12:52:51 +0100

    Move tab styles to separate file

--- projects/storefrontlib/src/lib/product/components/product-details/container/product-details.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/container/product-details.component.ts
@@ -13,7 +13,10 @@ import { ProductDetailOutlets } from '../../../product-outlets.model';
 @Component({
   selector: 'cx-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss'],
+  styleUrls: [
+    './product-details.component.scss',
+    './product-details-tabs.component.scss'
+  ],
   encapsulation: ViewEncapsulation.None
 })
 export class ProductDetailsComponent implements OnChanges {
