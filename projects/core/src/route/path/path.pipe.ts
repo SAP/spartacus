892eed4b66c129d7aa147b723b394ceb9e87d842
Krzysztof Platis <platonn.git@gmail.com>
2018-11-13 12:11:08 +0100

    feature/#184-pipes: unit tests for path.pipe

--- projects/core/src/route/path/path.pipe.ts
+++ projects/core/src/route/path/path.pipe.ts
@@ -1,22 +1,32 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { PathService } from './path.service';
 
+type PathPipeArgument = string | (string | object)[];
+
 @Pipe({
   name: 'cxPath'
 })
 export class PathPipe implements PipeTransform {
   constructor(private pathService: PathService) {}
 
-  transform(args: (string | object)[] | string): string {
-    let pageName;
-    let parametersObject;
-    if (typeof args === 'string') {
-      pageName = args;
-      parametersObject = {};
-    } else {
-      pageName = args[0];
-      parametersObject = args[1];
-    }
+  transform(argument: PathPipeArgument): string {
+    const { pageName, parametersObject } = this.interpretArgument(argument);
     return this.pathService.transform(pageName, parametersObject);
+  }
+
+  private interpretArgument(
+    argument: PathPipeArgument
+  ): { pageName: string; parametersObject: object } {
+    return typeof argument === 'string'
+      ? {
+          // if argument is string => it's a page name
+          pageName: argument,
+          parametersObject: {}
+        }
+      : {
+          // if argument is array => first element is page name and second element (when present) is parameters object
+          pageName: argument[0] as string,
+          parametersObject: (argument[1] as object) || {}
+        };
   }
 }
