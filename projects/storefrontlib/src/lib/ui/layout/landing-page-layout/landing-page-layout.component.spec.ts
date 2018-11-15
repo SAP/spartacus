63f09d93eaa2ebad7aba2a97c720e36b5d286ad9
Patrick Labadie <patrick.labadie@sap.com>
2018-11-14 23:09:30 -0500

    GH-157: Fix CategoryPageLayoutComponent unit tests.

--- projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.spec.ts
@@ -14,6 +14,7 @@ export class MockDynamicSlotComponent {
   @Input()
   position: string;
 }
+
 describe('LandingPageLayoutComponent', () => {
   let component: LandingPageLayoutComponent;
   let fixture: ComponentFixture<LandingPageLayoutComponent>;
