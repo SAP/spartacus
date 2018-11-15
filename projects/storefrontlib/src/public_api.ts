d9d0a4b60264852dd0d98141cb36d1ea1ff5f045
Stanislaw Stanislawski <stanislaw.stanislawski@sap.com>
2018-11-15 08:49:08 +0100

    GH-255: Expose guards in public API

--- projects/storefrontlib/src/public_api.ts
+++ projects/storefrontlib/src/public_api.ts
@@ -19,3 +19,4 @@ export * from './lib/storefront.module';
 export * from './lib/storefront-config';
 export * from './lib/outlet/index';
 export * from './lib/cx-api/index';
+export * from './lib/guards/index';
