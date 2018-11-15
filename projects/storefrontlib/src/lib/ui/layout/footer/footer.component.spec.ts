046a72634f3d252bba2c48231e0e1473d9660da0
Patrick Labadie <patrick.labadie@sap.com>
2018-11-14 23:33:03 -0500

    GH-157: Fix unit tests for FooterComponent

--- projects/storefrontlib/src/lib/ui/layout/footer/footer.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/layout/footer/footer.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
-import {
-  DynamicSlotComponent,
-  ComponentWrapperDirective
-} from '../../../cms/components';
+import { ComponentWrapperDirective } from '../../../cms/components';
 import { StoreModule } from '@ngrx/store';
 import * as fromCmsReducer from '../../../cms/store/reducers';
 import { OutletDirective } from '../../../outlet';
+import { Input, Component } from '@angular/core';
 
+@Component({
+  selector: 'cx-dynamic-slot',
+  template: 'MockDynamicSlotComponent'
+})
+export class MockDynamicSlotComponent {
+  @Input()
+  position: string;
+}
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
@@ -21,7 +27,7 @@ describe('FooterComponent', () => {
       ],
       declarations: [
         FooterComponent,
-        DynamicSlotComponent,
+        MockDynamicSlotComponent,
         ComponentWrapperDirective,
         OutletDirective
       ]
