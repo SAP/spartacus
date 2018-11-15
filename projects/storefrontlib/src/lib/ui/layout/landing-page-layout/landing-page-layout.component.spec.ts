aa9813062b06419614541dc76b74fcf33b0303eb
Patrick Labadie <patrick.labadie@sap.com>
2018-11-14 23:05:16 -0500

    GH-157: Fix LandingPageLayoutComponent unit tests.

--- projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/layout/landing-page-layout/landing-page-layout.component.spec.ts
@@ -1,13 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageLayoutComponent } from './landing-page-layout.component';
-import {
-  DynamicSlotComponent,
-  ComponentWrapperDirective
-} from '../../../cms/components';
+import { ComponentWrapperDirective } from '../../../cms/components';
 import { StoreModule } from '@ngrx/store';
 import { OutletDirective } from '../../../outlet';
+import { Input, Component } from '@angular/core';
 
+@Component({
+  selector: 'cx-dynamic-slot',
+  template: 'MockDynamicSlotComponent'
+})
+export class MockDynamicSlotComponent {
+  @Input()
+  position: string;
+}
 describe('LandingPageLayoutComponent', () => {
   let component: LandingPageLayoutComponent;
   let fixture: ComponentFixture<LandingPageLayoutComponent>;
@@ -17,7 +23,7 @@ describe('LandingPageLayoutComponent', () => {
       imports: [StoreModule.forRoot({})],
       declarations: [
         LandingPageLayoutComponent,
-        DynamicSlotComponent,
+        MockDynamicSlotComponent,
         ComponentWrapperDirective,
         OutletDirective
       ]
