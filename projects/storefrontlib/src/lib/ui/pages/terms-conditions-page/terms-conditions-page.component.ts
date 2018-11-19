ecfd44131ea97f1d7719bc7b2660babbe31d1c6d
Roman Pylypets <roman.pylypets@hotmail.com>
2018-11-19 13:57:14 +0100

    GH-210: remove style import for terms and conditions page

--- projects/storefrontlib/src/lib/ui/pages/terms-conditions-page/terms-conditions-page.component.ts
+++ projects/storefrontlib/src/lib/ui/pages/terms-conditions-page/terms-conditions-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'cx-terms-conditions-page',
   templateUrl: './terms-conditions-page.component.html',
-  styleUrls: ['./terms-conditions-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TermsConditionsPageComponent implements OnInit {
