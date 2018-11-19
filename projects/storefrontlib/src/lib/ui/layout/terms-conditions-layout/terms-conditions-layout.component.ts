e1285c89dabff3b09bff7acc40e496cdd24bce93
Roman Pylypets <roman.pylypets@hotmail.com>
2018-11-19 13:30:29 +0100

    GH-210: remove empty style files for terms&conditions

--- projects/storefrontlib/src/lib/ui/layout/terms-conditions-layout/terms-conditions-layout.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/terms-conditions-layout/terms-conditions-layout.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cx-terms-conditions-layout',
-  templateUrl: './terms-conditions-layout.component.html',
-  styleUrls: ['./terms-conditions-layout.component.scss']
+  templateUrl: './terms-conditions-layout.component.html'
 })
 export class TermsConditionsLayoutComponent implements OnInit {
   constructor() {}
