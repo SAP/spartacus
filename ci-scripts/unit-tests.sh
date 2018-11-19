a2780f81b9c07fdbcc0ef3313b8f2d3f953fb6f9
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-11-19 11:08:54 -0500

    GH-335 Running sonar after unit tests to send coverage report

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -2,15 +2,6 @@
 set -e
 set -o pipefail
 
-echo "Running unit tests and code coverage for lib"
-exec 5>&1
-output=$(ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
-
 echo "-----"
 echo "Running unit tests and code coverage for core"
 exec 5>&1
@@ -20,3 +11,23 @@ if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
     exit 1
 fi
+
+echo "Running unit tests and code coverage for lib"
+exec 5>&1
+output=$(ng test storefrontlib --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+if [[ -n "$coverage" ]]; then
+    echo "Error: Tests did not meet coverage expectations"
+    exit 1
+fi
+
+echo "Running SonarCloud scan"
+sonar-scanner \
+-Dsonar.projectKey=sap_cloud-commerce-spartacus-storefront \
+-Dsonar.organization=sap \
+-Dsonar.sources=projects/storefrontlib,projects/core,projects/storefrontstyles,projects/storefrontapp-e2e \
+-Dsonar.tests=projects/storefrontlib,projects/core,projects/storefrontstyles,projects/storefrontapp-e2e \
+-Dsonar.host.url=https://sonarcloud.io \
+-Dsonar.login=$SONAR_TOKEN \
+-Dsonar.branch.name=feature/GH-165
+-Dsonar.cfamily.build-wrapper-output.bypass=true
