dabf1877ab1d0b3e58034cbbefa05a070f4d0f81
Roman Pylypets <roman.pylypets@hotmail.com>
2018-11-20 14:47:52 +0100

    GH-313: Remove inner module exports from product module

--- projects/storefrontlib/src/lib/product/product.module.ts
+++ projects/storefrontlib/src/lib/product/product.module.ts
@@ -8,12 +8,8 @@ import { CmsModule } from './../cms/cms.module';
 // guards
 import { guards } from './guards/index';
 
-import { ProductListModule } from './components/product-list/product-list.module';
-import { ProductDetailsModule } from './components/product-details/product-details.module';
-
 @NgModule({
   imports: [CommonModule, RouterModule, MediaModule, CmsModule],
-  exports: [ProductListModule, ProductDetailsModule],
   providers: [...guards]
 })
 export class ProductModule {}
