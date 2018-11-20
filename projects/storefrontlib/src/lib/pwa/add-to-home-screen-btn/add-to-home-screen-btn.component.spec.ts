932cf9598333c5e4104d33e9dfd73a820504fa42
Weizheng Gao <weizheng.gao@sap.com>
2018-11-20 09:31:27 -0500

    feature/GH-167: add more test to add-to-home-screen-btn.component.spec

--- projects/storefrontlib/src/lib/pwa/add-to-home-screen-btn/add-to-home-screen-btn.component.spec.ts
+++ projects/storefrontlib/src/lib/pwa/add-to-home-screen-btn/add-to-home-screen-btn.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import createSpy = jasmine.createSpy;
 
+import { AddToHomeScreenService } from '../services/add-to-home-screen.service';
 import { AddToHomeScreenBtnComponent } from './add-to-home-screen-btn.component';
-import { PWAModuleConfig, defaultPWAModuleConfig } from '../pwa.module-config';
-import { PwaModule } from './../pwa.module';
-import { StoreModule } from '@ngrx/store';
-import { ɵangular_packages_service_worker_service_worker_b as RegistrationOptions } from '@angular/service-worker';
 
 describe('AddToHomeScreenBtnComponent', () => {
   let component: AddToHomeScreenBtnComponent;
   let fixture: ComponentFixture<AddToHomeScreenBtnComponent>;
+  let mockAddToHomeScreenService: any;
 
   beforeEach(async(() => {
+    mockAddToHomeScreenService = {
+      firePrompt: createSpy(),
+      canPrompt$: of(true)
+    };
+
     TestBed.configureTestingModule({
-      imports: [PwaModule, StoreModule.forRoot({})],
       providers: [
-        RegistrationOptions,
         {
-          provide: PWAModuleConfig,
-          useValue: defaultPWAModuleConfig
+          provide: AddToHomeScreenService,
+          useValue: mockAddToHomeScreenService
         }
-      ]
+      ],
+      declarations: [AddToHomeScreenBtnComponent]
     }).compileComponents();
   }));
 
@@ -31,5 +35,17 @@ describe('AddToHomeScreenBtnComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to get canPrompt', () => {
+    component.ngOnInit();
+    let canPrompt;
+    component.canPrompt$.subscribe(value => (canPrompt = value));
+    expect(canPrompt).toBeTruthy();
+  });
+
+  it('should be able to call prompt', () => {
+    component.prompt();
+    expect(mockAddToHomeScreenService.firePrompt).toHaveBeenCalled();
   });
 });
