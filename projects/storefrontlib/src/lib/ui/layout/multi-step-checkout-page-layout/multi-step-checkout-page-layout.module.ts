86bacdc8ce5559c64f6eb454e52a4749e28e0e73
Roman Pylypets <roman.pylypets@hotmail.com>
2018-11-20 13:30:30 +0100

    GH-313: Remove inner module exports from checkout module

--- projects/storefrontlib/src/lib/ui/layout/multi-step-checkout-page-layout/multi-step-checkout-page-layout.module.ts
+++ projects/storefrontlib/src/lib/ui/layout/multi-step-checkout-page-layout/multi-step-checkout-page-layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CmsModule } from '../../../cms/cms.module';
 import { CheckoutModule } from '../../../checkout/checkout.module';
 import { MultiStepCheckoutPageLayoutComponent } from './multi-step-checkout-page-layout.component';
+import { MultiStepCheckoutModule } from '../../../checkout';
 
 @NgModule({
-  imports: [CommonModule, CmsModule, CheckoutModule],
+  imports: [CommonModule, CmsModule, CheckoutModule, MultiStepCheckoutModule],
   declarations: [MultiStepCheckoutPageLayoutComponent],
   exports: [MultiStepCheckoutPageLayoutComponent]
 })
