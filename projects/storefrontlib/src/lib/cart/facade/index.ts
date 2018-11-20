89fb865550aadeac4c8a110d8170e517b67f7d77
Nikola Zaric <nikola.zaric.ns@gmail.com>
2018-11-20 15:05:13 -0500

    #153 - added index.ts

---
+++ projects/storefrontlib/src/lib/cart/facade/index.ts
@@ -0,0 +1,7 @@
+import { CartService } from './cart.service';
+import { CartDataService } from './cart-data.service';
+
+export const services: any[] = [CartService, CartDataService];
+
+export * from './cart.service';
+export * from './cart-data.service';
