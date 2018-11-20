4d9a2a5ee0753479c782234f368e770d81582abd
Marcin Lasak <lasakmarcin90@gmail.com>
2018-11-20 13:10:17 +0100

    ci: Generate changelog from commits (GH-16)

---
+++ scripts/packages.ts
@@ -0,0 +1,79 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import { JsonObject } from '@angular-devkit/core';
+import * as path from 'path';
+
+export interface PackageInfo {
+  name: string;
+  private: boolean;
+  packageJson: JsonObject;
+  version: string;
+}
+export interface PackageMap {
+  [name: string]: PackageInfo;
+}
+
+function loadPackageJson(p: string) {
+  const pkg = require(p);
+
+  for (const key of Object.keys(pkg)) {
+    switch (key) {
+      // Keep the following keys from the package.json of the package itself.
+      case 'bin':
+      case 'description':
+      case 'dependencies':
+      case 'name':
+      case 'main':
+      case 'peerDependencies':
+      case 'optionalDependencies':
+      case 'typings':
+      case 'version':
+      case 'private':
+      case 'workspaces':
+      case 'resolutions':
+        continue;
+
+      // Remove the following keys from the package.json.
+      case 'devDependencies':
+      case 'scripts':
+        delete pkg[key];
+        continue;
+      case 'keywords':
+        pkg[key] = pkg[key] || [];
+        break;
+      default:
+    }
+  }
+
+  return pkg;
+}
+
+// Fixed list of our packages
+const packageJsonPaths = [
+  path.join(__dirname, '..', 'projects', 'core', 'package.json'),
+  path.join(__dirname, '..', 'projects', 'storefrontlib', 'package.json'),
+  path.join(__dirname, '..', 'projects', 'storefrontstyles', 'package.json')
+];
+
+// All the supported packages. Go through the packages directory and create a map of
+// name => PackageInfo. This map is partial as it lacks some information that requires the
+// map itself to finish building.
+export const packages: PackageMap = packageJsonPaths.reduce(
+  (projectPackages: PackageMap, pkgJsonPath: string) => {
+    const packageJson = loadPackageJson(pkgJsonPath);
+    const name = packageJson['name'];
+    if (!name) {
+      // Only build the entry if there's a package name.
+      return projectPackages;
+    }
+
+    projectPackages[name] = {
+      private: packageJson.private,
+      name,
+      packageJson,
+      version: packageJson.version || '0.0.0'
+    };
+
+    return projectPackages;
+  },
+  {}
+);
