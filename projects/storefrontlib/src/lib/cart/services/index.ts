90c73cae7185ccf263219a1552449c5fe4e9ca55
Nikola Zaric <nikola.zaric.ns@gmail.com>
2018-11-20 15:04:50 -0500

    #153 - removed services folder

--- projects/storefrontlib/src/lib/cart/services/index.ts
+++
@@ -1,7 +0,0 @@
-import { CartService } from './cart.service';
-import { CartDataService } from './cart-data.service';
-
-export const services: any[] = [CartService, CartDataService];
-
-export * from './cart.service';
-export * from './cart-data.service';
