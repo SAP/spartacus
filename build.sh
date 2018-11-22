1f9b5a9d3e7de5a5e97f0485b755dd7d4a0eb07d
test <whisp3r3r@gmx.de>
2018-11-22 16:18:24 +0100

    Revert build sh to merge pull request vom original repo

--- build.sh
+++ build.sh
@@ -4,15 +4,11 @@ set -o pipefail
 
 echo "Updating dependencies"
 yarn
-yarn build:core:lib
 echo "-----"
-echo "Building SPA app"
-yarn build
-#echo "-----"
-#./ci-scripts/other.sh
-#echo "-----"
-#./ci-scripts/lint.sh
-#echo "-----"
-#./ci-scripts/unit-tests.sh
-#echo "-----"
-#./ci-scripts/e2e-tests.sh
+./ci-scripts/validate.sh
+echo "-----"
+./ci-scripts/lint.sh
+echo "-----"
+./ci-scripts/unit-tests.sh
+echo "-----"
+./ci-scripts/e2e-tests.sh
