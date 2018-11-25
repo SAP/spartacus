3214944655fb8c6db8a1c55ac7ad1a6e1dab0f13
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-25 21:04:35 +0100

    Introduce cached observables

--- projects/core/src/product/facade/product.service.ts
+++ projects/core/src/product/facade/product.service.ts
@@ -7,15 +7,20 @@ import * as fromStore from '../store/index';
 
 @Injectable()
 export class ProductService {
+  protected products = {};
+
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   get(productCode: string): Observable<any> {
-    return this.store.pipe(
-      select(fromStore.getSelectedProductFactory(productCode))
-    );
+    if (!this.products[productCode]) {
+      this.products[productCode] = this.store.pipe(
+        select(fromStore.getSelectedProductFactory(productCode))
+      );
+    }
+    return this.products[productCode];
   }
 
-  load(productCode: string): void {
+  load(productCode: string) {
     this.store.dispatch(new fromStore.LoadProduct(productCode));
   }
 
