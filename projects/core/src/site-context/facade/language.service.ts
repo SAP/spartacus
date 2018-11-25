aea3f7fc7a107e32f441ec9ebf48537392fd67a2
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-25 19:58:19 +0100

    Add docmentation

--- projects/core/src/site-context/facade/language.service.ts
+++ projects/core/src/site-context/facade/language.service.ts
@@ -10,10 +10,16 @@ import * as actions from '../store/actions';
 
 @Injectable()
 export class LanguageService {
+  /**
+   * Represents all the languages supported by the current store.
+   */
   languages$: Observable<LanguagesEntities> = this.store.pipe(
     select(selectors.getAllLanguages)
   );
 
+  /**
+   * Represents the active language of the current store.
+   */
   activeLanguage$: Observable<string> = this.store.pipe(
     select(selectors.getActiveLanguage)
   );
@@ -26,14 +32,25 @@ export class LanguageService {
     this.initSessionLanguage();
   }
 
+  /**
+   * Selects the active language by isocode.
+   */
   select(isocode: string) {
     this.store.dispatch(new actions.SetActiveLanguage(isocode));
   }
 
+  /**
+   * Loads all the languages of the current store.
+   */
   protected loadAll() {
     this.store.dispatch(new actions.LoadLanguages());
   }
 
+  /**
+   * Initials the active language. The active language is either given
+   * by the last visit (stored in session storage) or by the
+   * default session language of the store.
+   */
   protected initSessionLanguage() {
     if (sessionStorage && !!sessionStorage.getItem('language')) {
       this.select(sessionStorage.getItem('language'));
