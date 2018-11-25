f7a0f6039550129350632e9e02564ef896b2c8f3
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-25 19:33:59 +0100

    Provide minimum documentation for facade

--- projects/core/src/site-context/facade/currency.service.ts
+++ projects/core/src/site-context/facade/currency.service.ts
@@ -10,10 +10,16 @@ import * as selectors from '../store/selectors/index';
 
 @Injectable()
 export class CurrencyService {
+  /**
+   * Represents all the currencies supported by the current store.
+   */
   currencies$: Observable<CurrencyEntities> = this.store.pipe(
     select(selectors.getAllCurrencies)
   );
 
+  /**
+   * Represents the active currency of the current store.
+   */
   activeCurrency$: Observable<string> = this.store.pipe(
     select(selectors.getActiveCurrency)
   );
@@ -26,14 +32,25 @@ export class CurrencyService {
     this.initSessionCurrency();
   }
 
+  /**
+   * Selects the active currency by isocode.
+   */
   select(isocode?: string) {
     this.store.dispatch(new actions.SetActiveCurrency(isocode));
   }
 
+  /**
+   * Loads all the currencies of the current store.
+   */
   protected loadAll() {
     this.store.dispatch(new actions.LoadCurrencies());
   }
 
+  /**
+   * Initials the active currency. The active currency is either given
+   * by the last visit (stored in session storage) or by the
+   * default session currency of the store.
+   */
   protected initSessionCurrency() {
     if (sessionStorage && !!sessionStorage.getItem('currency')) {
       this.select(sessionStorage.getItem('currency'));
