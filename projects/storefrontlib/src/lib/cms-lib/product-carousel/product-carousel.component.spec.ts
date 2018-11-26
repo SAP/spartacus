47229e72c9708f573bb43989697b1e567c9a25d2
Nikola Zaric <nikola.zaric.ns@gmail.com>
2018-11-26 12:53:03 -0500

    fixed the test

--- projects/storefrontlib/src/lib/cms-lib/product-carousel/product-carousel.component.spec.ts
+++ projects/storefrontlib/src/lib/cms-lib/product-carousel/product-carousel.component.spec.ts
@@ -2,12 +2,15 @@ import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { ProductService } from '@spartacus/core';
+
 import { of } from 'rxjs';
 
 import { BootstrapModule } from '../../bootstrap.module';
 import { CmsService } from '../../cms/facade/cms.service';
-import { ProductService } from '@spartacus/core';
 import { PictureComponent } from '../../ui/components/media/picture/picture.component';
+
 import { ProductCarouselComponent } from './product-carousel.component';
 
 describe('ProductCarouselComponent', () => {
@@ -78,7 +81,8 @@ describe('ProductCarouselComponent', () => {
     expect(productCarouselComponent.productCodes).toEqual(productCodeArray);
   });
 
-  it('should have 1 group', () => {
+  fit('should have 1 group', () => {
+    spyOn<any>(productCarouselComponent, 'getItemsPerPage').and.returnValue(4);
     productCarouselComponent.onCmsComponentInit(mockComponentData.uid);
     expect(productCarouselComponent.productGroups.length).toBe(1);
   });
