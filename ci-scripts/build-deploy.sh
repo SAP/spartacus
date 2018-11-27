5dee5848742b9b1c9f8d81f592bae543b715dc47
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-11-27 15:09:32 -0500

    GH-291 Addressing code review comments

--- ci-scripts/build-deploy.sh
+++ ci-scripts/build-deploy.sh
@@ -6,5 +6,5 @@ echo "Building SPA core lib"
 yarn build:core:lib
 echo "-----"
 echo "Building SPA app"
-ng build storefrontapp --prod -c=ci --base-href "https://sap.github.io/cloud-commerce-spartacus-storefront/"
+ng build storefrontapp -c=ci --base-href "https://sap.github.io/cloud-commerce-spartacus-storefront/"
 echo "-----"
