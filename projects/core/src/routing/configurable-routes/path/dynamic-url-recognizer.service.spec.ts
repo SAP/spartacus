c7caa31e06744fc23f4de3c7b1a5d25eafb16f9f
Krzysztof Platis <platonn.git@gmail.com>
2018-11-28 10:43:20 +0100

    feature/GH-184-nested: fix and add unit tests for nested routes in dynamic-url-recognizer.service

--- projects/core/src/routing/configurable-routes/path/dynamic-url-recognizer.service.spec.ts
+++ projects/core/src/routing/configurable-routes/path/dynamic-url-recognizer.service.spec.ts
@@ -2,19 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { RoutesConfigLoader } from '../routes-config-loader';
 import { RoutesTranslations } from '../routes-config';
 import { DynamicUrlRecognizerService } from './dynamic-url-recognizer.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UrlParser } from './url-parser.service';
 
 const mockRoutesConfigLoader = {
   routesConfig: { translations: { default: {} } }
 };
 
-describe('DynamicUrlRecognizerService', () => {
+fdescribe('DynamicUrlRecognizerService', () => {
   let loader: RoutesConfigLoader;
   let service: DynamicUrlRecognizerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [
         DynamicUrlRecognizerService,
+        UrlParser,
         {
           provide: RoutesConfigLoader,
           useValue: mockRoutesConfigLoader
@@ -26,115 +30,295 @@ describe('DynamicUrlRecognizerService', () => {
     service = TestBed.get(DynamicUrlRecognizerService);
   });
 
-  describe('getPageAndParameters', () => {
+  describe('getNestedRoutes', () => {
     interface TestCase {
-      inputUrl: string;
+      debug?: boolean;
+      description: string;
+      url: string;
       defaultTranslations: RoutesTranslations;
       expectedResult: {
-        pageName: string;
-        parameters: object;
+        nestedRoutesNames: string[];
+        nestedRoutesParams: object[];
       };
     }
-
-    function test_getPageAndParameters(
-      { inputUrl, defaultTranslations, expectedResult }: TestCase,
-      index: number
-    ) {
-      it(`should return matching page name and extracted parameters values from input url - test case ${index}`, () => {
+    function test_getNestedRoutes({
+      debug,
+      description,
+      url,
+      defaultTranslations,
+      expectedResult
+    }: TestCase) {
+      it(description, () => {
+        if (debug) {
+          // tslint:disable-next-line:no-debugger
+          debugger;
+        }
         loader.routesConfig.translations.default = defaultTranslations;
-        expect(service.getPageAndParameters(inputUrl)).toEqual(expectedResult);
+        expect(service.getNestedRoutes(url)).toEqual(expectedResult);
       });
     }
 
     const testCases: TestCase[] = [
       {
-        inputUrl: '/path2',
+        description: `should return route name for given absolute url`,
+        url: '/path2',
         defaultTranslations: {
-          page1: ['path1'],
-          page2: ['path2']
-        },
-        expectedResult: { pageName: 'page2', parameters: {} }
-      },
-      {
-        inputUrl: 'path2',
-        defaultTranslations: {
-          page1: ['path1'],
-          page2: ['path2']
-        },
-        expectedResult: { pageName: 'page2', parameters: {} }
-      },
-      {
-        inputUrl: 'unknown-path',
-        defaultTranslations: {
-          page1: ['path1'],
-          page2: ['path2']
-        },
-        expectedResult: { pageName: null, parameters: {} }
-      },
-      {
-        inputUrl: 'path2/value1/value2',
-        defaultTranslations: {
-          page1: ['path1/:param1/:param2'],
-          page2: ['path2/:param1/:param2']
+          page1: { paths: ['path1'] },
+          page2: { paths: ['path2'] }
         },
         expectedResult: {
-          pageName: 'page2',
-          parameters: { param1: 'value1', param2: 'value2' }
+          nestedRoutesNames: ['page2'],
+          nestedRoutesParams: [{}]
         }
       },
       {
-        inputUrl: 'path2/value1/value2',
+        description: `should return route name for given relative url, treating as absolute`,
+        url: 'path2',
         defaultTranslations: {
-          page1: ['path1/:param1/:param2'],
-          page2: ['path2/:param100/:param200']
+          page1: { paths: ['path1'] },
+          page2: { paths: ['path2'] }
         },
         expectedResult: {
-          pageName: 'page2',
-          parameters: { param100: 'value1', param200: 'value2' }
+          nestedRoutesNames: ['page2'],
+          nestedRoutesParams: [{}]
         }
       },
       {
-        inputUrl: 'path2/value1/path2',
+        description: `should return null for unknown url (case 1)`,
+        url: 'unknown-path',
         defaultTranslations: {
-          page1: ['path1/:param1/path1'],
-          page2: ['path2/:param1/path2']
+          page1: { paths: ['path1'] },
+          page2: { paths: ['path2'] }
         },
-        expectedResult: { pageName: 'page2', parameters: { param1: 'value1' } }
+        expectedResult: { nestedRoutesNames: null, nestedRoutesParams: null }
       },
       {
-        inputUrl: 'path/value1/path/value2',
+        description: `should return null for unknown url (case 2)`,
+        url: 'path1/path2/path3/unknown-path4',
         defaultTranslations: {
-          page1: ['path/:param1/path'],
-          page2: ['path/:param1/path/:param2']
+          page1: {
+            paths: ['path1'],
+            children: {
+              page2: {
+                paths: ['path2'],
+                children: {
+                  page3: {
+                    paths: ['path3']
+                  }
+                }
+              }
+            }
+          }
         },
         expectedResult: {
-          pageName: 'page2',
-          parameters: { param1: 'value1', param2: 'value2' }
+          nestedRoutesNames: null,
+          nestedRoutesParams: null
         }
       },
       {
-        inputUrl: 'path/value1/path/value2',
+        description: `should return route name and params for given url (case 1)`,
+        url: 'path2/value1/value2',
         defaultTranslations: {
-          page1: ['path/:param1/path', 'path/:param1/path/:param2'],
-          page2: ['path/:param1/path/:param2/:param3']
+          page1: { paths: ['path1/:param1/:param2'] },
+          page2: { paths: ['path2/:param1/:param2'] }
         },
         expectedResult: {
-          pageName: 'page1',
-          parameters: { param1: 'value1', param2: 'value2' }
+          nestedRoutesNames: ['page2'],
+          nestedRoutesParams: [{ param1: 'value1', param2: 'value2' }]
         }
       },
       {
-        inputUrl: 'path/value1/path/value2/value3',
+        description: `should return route name and params for given url (case 2)`,
+        url: 'path/value1/path/value2',
         defaultTranslations: {
-          page1: ['path/:param1/path', 'path/:param1/path/:param2'],
-          page2: ['path/:param1/path/:param2/:param3']
+          page1: { paths: ['path/:param1/path', 'path/:param1/path/:param2'] },
+          page2: { paths: ['path/:param1/path/:param2/:param3'] }
         },
         expectedResult: {
-          pageName: 'page2',
-          parameters: { param1: 'value1', param2: 'value2', param3: 'value3' }
+          nestedRoutesNames: ['page1'],
+          nestedRoutesParams: [{ param1: 'value1', param2: 'value2' }]
+        }
+      },
+      {
+        description: `should return route name and params for given url (case 3)`,
+        url: 'path/value1/path/value2/value3',
+        defaultTranslations: {
+          page1: { paths: ['path/:param1/path', 'path/:param1/path/:param2'] },
+          page2: { paths: ['path/:param1/path/:param2/:param3'] }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page2'],
+          nestedRoutesParams: [
+            {
+              param1: 'value1',
+              param2: 'value2',
+              param3: 'value3'
+            }
+          ]
+        }
+      },
+      {
+        description: `should return 2 names of nested routes for given url consisting of 2 nested routes (case 1)`,
+        url: 'path1/path2',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1'],
+            children: {
+              page2: {
+                paths: ['path2']
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2'],
+          nestedRoutesParams: [{}, {}]
+        }
+      },
+      {
+        description: `should return 2 names of nested routes for given url consisting of 2 nested routes (case 2)`,
+        url: 'path1/path2',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1'],
+            children: {
+              page2: {
+                paths: ['path2'],
+                children: {
+                  page3: {
+                    paths: ['path3']
+                  }
+                }
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2'],
+          nestedRoutesParams: [{}, {}]
+        }
+      },
+      {
+        description: `should return 3 names of nested routes for given url consisting of 3 nested routes`,
+        url: 'path1/path2/path3',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1'],
+            children: {
+              page2: {
+                paths: ['path2'],
+                children: {
+                  page3: {
+                    paths: ['path3']
+                  }
+                }
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2', 'page3'],
+          nestedRoutesParams: [{}, {}, {}]
+        }
+      },
+      {
+        description: `should return routes names and params objects in relevant order for url consisting of 2 nested routes (case 1)`,
+        url: 'path1/value1/path2/value2',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1/:param1'],
+            children: {
+              page2: {
+                paths: ['path2/:param2']
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2'],
+          nestedRoutesParams: [{ param1: 'value1' }, { param2: 'value2' }]
+        }
+      },
+      {
+        description: `should return routes names and params objects in relevant order for url consisting of 2 nested routes (case 2)`,
+        url: 'path1/value1/path2/value2',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1/:param1'],
+            children: {
+              page2: {
+                paths: ['path2/:param2'],
+                children: {
+                  page3: {
+                    paths: ['path3/:param3']
+                  }
+                }
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2'],
+          nestedRoutesParams: [{ param1: 'value1' }, { param2: 'value2' }]
+        }
+      },
+      {
+        description: `should return routes names and params objects in relevant order for url consisting of 3 nested routes`,
+        url: 'path1/value1/path2/value2/path3/value3',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1/:param1'],
+            children: {
+              page2: {
+                paths: ['path2/:param2'],
+                children: {
+                  page3: {
+                    paths: ['path3/:param3']
+                  }
+                }
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page2', 'page3'],
+          nestedRoutesParams: [
+            { param1: 'value1' },
+            { param2: 'value2' },
+            { param3: 'value3' }
+          ]
+        }
+      },
+      // spike TODO
+      // przeszukiwanie z nawrotami mimo, że coś pasowało
+      // przeszukiwanie z nie do końca
+      {
+        description: `should return routes that exactly match to given url (case 1)`,
+        url: 'path1/value1/path2/value2/path3',
+        defaultTranslations: {
+          page1: {
+            paths: ['path1/:param1'],
+            children: {
+              page2: {
+                paths: ['path2/:param2'],
+                children: {
+                  page3: {
+                    paths: ['path3/:param3']
+                  }
+                }
+              },
+              page20: {
+                paths: ['path2/:param2/path3']
+              }
+            }
+          }
+        },
+        expectedResult: {
+          nestedRoutesNames: ['page1', 'page20'],
+          nestedRoutesParams: [{ param1: 'value1' }, { param2: 'value2' }]
         }
       }
     ];
-    testCases.forEach(test_getPageAndParameters);
+    testCases.forEach(test_getNestedRoutes);
   });
 });
--- projects/core/src/routing/configurable-routes/path/dynamic-url-recognizer.service.ts
+++ projects/core/src/routing/configurable-routes/path/dynamic-url-recognizer.service.ts
@@ -14,8 +14,8 @@ export class DynamicUrlRecognizerService {
   getNestedRoutes(
     url: string
   ): {
-    nestedRouteNames: string[];
-    paramsObjects: object[];
+    nestedRoutesNames: string[];
+    nestedRoutesParams: object[];
   } {
     url = removeLeadingSlash(url); // url will be compared with paths translations which do not have leading slash
     const routesTranslations = this.defaultRoutesTranslations;
@@ -27,15 +27,15 @@ export class DynamicUrlRecognizerService {
     );
 
     if (recognizedNestedRoutes) {
-      const nestedRouteNames: string[] = [];
-      const paramsObjects: object[] = [];
+      const nestedRoutesNames: string[] = [];
+      const nestedRoutesParams: object[] = [];
       recognizedNestedRoutes.forEach(recognizedNestedRoute => {
-        nestedRouteNames.push(recognizedNestedRoute.routeName);
-        paramsObjects.push(recognizedNestedRoute.params);
+        nestedRoutesNames.push(recognizedNestedRoute.routeName);
+        nestedRoutesParams.push(recognizedNestedRoute.params);
       });
-      return { nestedRouteNames, paramsObjects };
+      return { nestedRoutesNames, nestedRoutesParams };
     }
-    return { nestedRouteNames: null, paramsObjects: null };
+    return { nestedRoutesNames: null, nestedRoutesParams: null };
   }
 
   private getNestedRoutesRecursive(
@@ -48,55 +48,39 @@ export class DynamicUrlRecognizerService {
     }
     const routeNames = Object.keys(routesTranslations);
     const routeNamesLength = routeNames.length;
+
     for (let i = 0; i < routeNamesLength; i++) {
       const routeName = routeNames[i];
       const routeTranslation = routesTranslations[routeName];
-
+      const paths = routeTranslation.paths;
       // SPIKE TODO: improve readability here:
-      const {
-        matchingPathSegments,
-        params
-      } = this.extractParamsFromMatchingPath(
-        remainingUrlSegments,
-        routeTranslation.paths
-      );
-      // if some path is matching:
-      if (matchingPathSegments) {
-        return this.getNestedRoutesRecursive(
-          remainingUrlSegments.slice(matchingPathSegments.length),
-          routeTranslation.children,
-          accResult.concat({ routeName, params })
+      const pathsLength = paths.length;
+      for (let j = 0; j < pathsLength; j++) {
+        const path = paths[j];
+        const pathSegments = this.urlParser.getPrimarySegments(path);
+        const params = this.extractParamsIfMatchingStaticSegments(
+          remainingUrlSegments,
+          pathSegments
         );
+        // if some path is matching, try to match remaining segments
+        if (params) {
+          const result = this.getNestedRoutesRecursive(
+            remainingUrlSegments.slice(pathSegments.length),
+            routeTranslation.children,
+            accResult.concat({ routeName, params })
+          );
+          // if remaining segments were successfuly matched, return result. otherwise continue loop for other paths and routes
+          if (result) {
+            return result;
+          }
+        }
       }
     }
 
     return remainingUrlSegments.length ? null : accResult;
   }
 
-  private extractParamsFromMatchingPath(
-    urlSegments: string[],
-    paths: string[]
-  ): {
-    matchingPathSegments: string[];
-    params: object;
-  } {
-    const pathsLength = paths.length;
-    for (let i = 0; i < pathsLength; i++) {
-      const path = paths[i];
-      const pathSegments = this.urlParser.getPrimarySegments(path);
-      const params = this.extractParamsIfMatchingPath(
-        urlSegments,
-        pathSegments
-      );
-      if (params) {
-        return { matchingPathSegments: pathSegments, params };
-      }
-    }
-    return { matchingPathSegments: null, params: null };
-  }
-
-  // returns extracted params when static params are matching or null otherwise
-  private extractParamsIfMatchingPath(
+  private extractParamsIfMatchingStaticSegments(
     urlSegments: string[],
     pathSegments: string[]
   ): object {
@@ -119,6 +103,7 @@ export class DynamicUrlRecognizerService {
   }
 
   private get defaultRoutesTranslations(): RoutesTranslations {
-    return this.routesConfigLoader.routesConfig.translations.default;
+    return this.routesConfigLoader.routesConfig.translations
+      .default as RoutesTranslations;
   }
 }
