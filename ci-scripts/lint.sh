a8a47854a1ba6d2dd6c82f6fc8d720119e683139
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2018-11-28 15:19:46 -0500

    GH-447: using yarn prettier in pipeline

--- ci-scripts/lint.sh
+++ ci-scripts/lint.sh
@@ -6,7 +6,7 @@ echo "Validating code linting"
 ng lint
 echo "-----"
 echo "Validating code formatting (using prettier)"
-./node_modules/.bin/prettier --config ./.prettierrc --list-different "projects/**/*{.ts,.js,.json,.scss,.html}" 2>&1 |  tee prettier.log
+yarn prettier 2>&1 |  tee prettier.log
 results=$(tail -1 prettier.log | grep projects || true)
 if [[ -z "$results" ]]; then
     echo "Success: Codebase has been prettified correctly"
