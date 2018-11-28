67a32b2326cf8f18b31feb7f7099b2b4c04405de
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-11-28 17:41:41 +0100

    Drop refresh product state sincw we have a global meta reducer for this.

--- projects/core/src/product/store/effects/product.effect.ts
+++ projects/core/src/product/store/effects/product.effect.ts
@@ -2,23 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import { EMPTY, Observable, of } from 'rxjs';
-import {
-  map,
-  filter,
-  catchError,
-  mergeMap,
-  switchMap,
-  take
-} from 'rxjs/operators';
+import { map, catchError, mergeMap, switchMap } from 'rxjs/operators';
 
 import * as productActions from '../actions/product.action';
 import { OccProductService } from '../../occ/product.service';
 import { ProductImageConverterService } from '../converters/product-image-converter.service';
 import { ProductReferenceConverterService } from '../converters/product-reference-converter.service';
-// import { RoutingService } from '../../../routing/facade/routing.service';
 
-import { RoutingService } from '../../../routing/index';
-import { PageType } from '../../../occ-models/occ.models';
 import { select, Store } from '@ngrx/store';
 import { LoadProductStart } from '../actions/product.action';
 import { getSelectedProductStateFactory } from '../selectors/product.selectors';
@@ -59,35 +49,11 @@ export class ProductEffects {
     })
   );
 
-  @Effect()
-  refreshProduct$ = this.actions$.pipe(
-    ofType('[Site-context] Language Change', '[Site-context] Currency Change'),
-    switchMap(() =>
-      this.routingService.routerState$.pipe(
-        filter(routerState => routerState !== undefined),
-        map(routerState => routerState.state.context),
-        take(1),
-        filter(pageContext => pageContext.type === PageType.PRODUCT_PAGE),
-        mergeMap(pageContext => {
-          return this.occProductService.loadProduct(pageContext.id).pipe(
-            map(product => {
-              this.productImageConverter.convertProduct(product);
-              this.productReferenceConverterService.convertProduct(product);
-              return new productActions.LoadProductSuccess(product);
-            }),
-            catchError(error => of(new productActions.LoadProductFail(error)))
-          );
-        })
-      )
-    )
-  );
-
   constructor(
     private actions$: Actions,
     private store: Store<StateWithProduct>,
     private occProductService: OccProductService,
     private productImageConverter: ProductImageConverterService,
-    private productReferenceConverterService: ProductReferenceConverterService,
-    private routingService: RoutingService
+    private productReferenceConverterService: ProductReferenceConverterService
   ) {}
 }
