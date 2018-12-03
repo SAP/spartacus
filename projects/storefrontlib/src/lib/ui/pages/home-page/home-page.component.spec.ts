77a9994a5d9fb1360fe9d59fc526873d83d895b0
Weizheng Gao <weizheng.gao@sap.com>
2018-12-03 15:19:30 -0500

    feature/GH-493: add previewServlet route

--- projects/storefrontlib/src/lib/ui/pages/home-page/home-page.component.spec.ts
+++ projects/storefrontlib/src/lib/ui/pages/home-page/home-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 
+import { of } from 'rxjs';
+
+import { RoutingService } from '@spartacus/core';
+
 import { HomePageComponent } from './home-page.component';
 
 @Component({
@@ -9,13 +13,24 @@ import { HomePageComponent } from './home-page.component';
 })
 export class MockLandingPageLayoutComponent {}
 
+const routerState = {
+  state: {
+    queryParams: {
+      cmsTicketId: 'mockCmsTicketId'
+    }
+  }
+};
+const mockRoutingService = {
+  routerState$: of(routerState)
+};
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePageComponent, MockLandingPageLayoutComponent]
+      declarations: [HomePageComponent, MockLandingPageLayoutComponent],
+      providers: [{ provide: RoutingService, useValue: mockRoutingService }]
     }).compileComponents();
   }));
 
