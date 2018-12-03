a72bf2e402a9c493f972c51f73fbf6b00c2752a9
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2018-12-03 14:49:23 -0500

    GH-482: Initial runtime styling

---
+++ projects/storefrontapp/src/loader.js
@@ -0,0 +1,9 @@
+function appendLink() {
+  document.head.innerHTML +=
+    '<link rel="stylesheet" type="text/css" href="styles/colors.css" />';
+}
+
+function addLoadingToPicture() {
+  document.head.innerHTML +=
+    '<link rel="stylesheet" type="text/css" href="styles/picture.css" />';
+}
