fa3c30561302f10313c563ef5e24f141035d3138
Mateusz Kasprzak <kasprzak.mat@gmail.com>
2018-12-04 12:44:17 +0100

    GH-261: Delete from storefrontlib cart module imports that are related to core

---
+++ projects/core/src/cart/index.ts
@@ -0,0 +1,7 @@
+export * from './store/actions/index';
+export * from './store/selectors/index';
+export * from './guards/index';
+
+export * from './facade/index';
+export * from './occ/index';
+export * from './cart.module';
--- projects/storefrontlib/src/lib/cart/cart.module.ts
+++ projects/storefrontlib/src/lib/cart/cart.module.ts
@@ -1,29 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { services } from './facade/index';
-import { guards } from './guards/index';
 import { AddToCartModule } from './components/add-to-cart/add-to-cart.module';
 import { CartDetailsModule } from './components/cart-details/cart-details.module';
 import { CartSharedModule } from './components/cart-shared/cart-shared.module';
-import { effects } from './store/effects/index';
-import { reducerToken, reducerProvider } from './store/reducers/index';
-import { metaReducers } from './store/reducers/index';
 
 @NgModule({
-  imports: [
-    AddToCartModule,
-    CartDetailsModule,
-    CartSharedModule,
-    NgbModule,
-    StoreModule.forFeature('cart', reducerToken, { metaReducers }),
-    EffectsModule.forFeature(effects)
-  ],
-  exports: [AddToCartModule, CartDetailsModule, CartSharedModule],
-  providers: [reducerProvider, ...services, ...guards]
+  imports: [AddToCartModule, CartDetailsModule, CartSharedModule, NgbModule],
+  exports: [AddToCartModule, CartDetailsModule, CartSharedModule]
 })
 export class CartModule {}
