170707172c0657f60a8d58ac38e32a365d434d7e
Marcin Lasak <lasakmarcin90@gmail.com>
2018-12-04 09:07:15 +0100

    GH-78: Remove empty files and improve test readability

--- projects/storefrontapp-e2e/src/suites/payment-methods.e2e-spec.ts
+++ projects/storefrontapp-e2e/src/suites/payment-methods.e2e-spec.ts
@@ -4,7 +4,7 @@ import { LoginHelper } from '../page-objects/login/login.helper';
 import { AddedToCartModal } from '../page-objects/cmslib/added-to-cart-modal.po';
 import { E2EUtil } from '../e2e-util';
 import { ProductDetailsPage } from '../page-objects/product-details.po';
-import { PaymentManagementPage } from '../page-objects/account/payment-managment.po';
+import { PaymentManagementPage } from '../page-objects/account/payment-management.po';
 
 describe('Payment management page', () => {
   const home: HomePage = new HomePage();
@@ -13,23 +13,26 @@ describe('Payment management page', () => {
   const PRODUCT_CODE = '1446509';
 
   beforeAll(async done => {
+    /* To check payment methods management we need 2 cards saved for user */
+    /* Currently we can only do it with 2 orders */
+
+    // Register new user
     await home.navigateTo();
     await home.waitForReady();
     await LoginHelper.registerNewUser();
 
+    // Add product to cart
     let productDetailsPage = new ProductDetailsPage();
     await productDetailsPage.navigateTo(PRODUCT_CODE);
     await productDetailsPage.waitForReady();
-    await productDetailsPage.itemCounterUpButton.click();
     await productDetailsPage.addToCartButton.click();
-
-    // Added-to-Cart modal opens. Close it.
     let atcModal = new AddedToCartModal();
     await atcModal.waitForReady();
 
     let item = atcModal.item;
     await E2EUtil.wait4VisibleElement(item);
 
+    // Fill shipping address data
     await atcModal.goToCheckoutButton.click();
     let shippingAddress = checkoutPage.shippingAddress;
     await shippingAddress.waitForReady();
@@ -39,11 +42,13 @@ describe('Payment management page', () => {
     await addressForm.fillIn();
     await addressForm.nextButton.click();
 
+    // Select delivery method
     let deliveryForm = checkoutPage.deliveryForm;
     await deliveryForm.waitForReady();
     await deliveryForm.setDeliveryMethod();
     await deliveryForm.nextButton.click();
 
+    // Fill payment data
     let paymentMethod = checkoutPage.paymentMethod;
     await paymentMethod.waitForReady();
 
@@ -52,36 +57,38 @@ describe('Payment management page', () => {
     await paymentForm.fillIn();
     await paymentForm.nextButton.click();
 
-    // Review: Select T&C and submit.
+    // Confirm order
     let reviewForm = checkoutPage.reviewForm;
     await reviewForm.waitForReady();
 
     let orderConfirmationPage = await checkoutPage.placeOrder();
     await orderConfirmationPage.waitForReady();
+
+    // Second order
     productDetailsPage = new ProductDetailsPage();
     await productDetailsPage.navigateTo(PRODUCT_CODE);
     await productDetailsPage.waitForReady();
-    await productDetailsPage.itemCounterUpButton.click();
     await productDetailsPage.addToCartButton.click();
-
-    // Added-to-Cart modal opens. Close it.
     atcModal = new AddedToCartModal();
     await atcModal.waitForReady();
 
     item = atcModal.item;
     await E2EUtil.wait4VisibleElement(item);
 
+    // Use existing shipping address data
     await atcModal.goToCheckoutButton.click();
     shippingAddress = checkoutPage.shippingAddress;
     await shippingAddress.waitForReady();
     await shippingAddress.selectDefaultAddress();
     await shippingAddress.nextButton.click();
 
+    // Select delivery method
     deliveryForm = checkoutPage.deliveryForm;
     await deliveryForm.waitForReady();
     await deliveryForm.setDeliveryMethod();
     await deliveryForm.nextButton.click();
 
+    // Add another payment data
     paymentMethod = checkoutPage.paymentMethod;
     await paymentMethod.waitForReady();
     await paymentMethod.addNewPaymentMethodButton.click();
@@ -93,7 +100,7 @@ describe('Payment management page', () => {
     });
     await paymentForm.nextButton.click();
 
-    // Review: Select T&C and submit.
+    // Confirm order
     reviewForm = checkoutPage.reviewForm;
     await reviewForm.waitForReady();
 
@@ -103,14 +110,18 @@ describe('Payment management page', () => {
   }, 150000);
 
   it('should be able to change default payment method', async done => {
-    // Go to my-account and assess that the new order is the newest in the list.
+    // Go to payment management page
     const paymentManagementPage = new PaymentManagementPage();
     await paymentManagementPage.navigateTo();
     await paymentManagementPage.waitForReady();
+
+    // Check if cards exists
     let card1 = await paymentManagementPage.getCard(0);
     let card2 = await paymentManagementPage.getCard(1);
     expect(await card1.expiry.getText()).toContain('Expires: 7/2020');
     expect(await card2.expiry.getText()).toContain('Expires: 7/2023');
+
+    // Change default payment method
     await card2.selectDefaultLink.click();
     await paymentManagementPage.waitForReady();
     card1 = await paymentManagementPage.getCard(0);
@@ -121,13 +132,18 @@ describe('Payment management page', () => {
   });
 
   it('should be able to delete payment method', async done => {
+    // Go to payment management page
     const paymentManagementPage = new PaymentManagementPage();
     await paymentManagementPage.navigateTo();
     await paymentManagementPage.waitForReady();
+
+    // Delete second card
     const card2 = await paymentManagementPage.getCard(1);
     await card2.deleteLink.click();
     await card2.confirmDelete.click();
     await paymentManagementPage.waitForReady();
+
+    // Check if card was removed
     expect(
       (await paymentManagementPage.getCard(1)).card.isPresent()
     ).toBeFalsy();
