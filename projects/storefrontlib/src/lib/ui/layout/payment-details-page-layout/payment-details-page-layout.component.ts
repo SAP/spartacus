e62d4f035d5f7d4edf8753ab5d423ecf2b1aba43
Marcin Lasak <lasakmarcin90@gmail.com>
2018-12-04 09:20:50 +0100

    GH-78: Remove links to empty style files

--- projects/storefrontlib/src/lib/ui/layout/payment-details-page-layout/payment-details-page-layout.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/payment-details-page-layout/payment-details-page-layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cx-payment-details-page-layout',
   templateUrl: './payment-details-page-layout.component.html',
-  styleUrls: ['./payment-details-page-layout.component.scss']
+  styleUrls: []
 })
 export class PaymentDetailsPageLayoutComponent {
   constructor() {}
--- projects/storefrontlib/src/lib/ui/pages/payment-details-page/payment-details-page.component.ts
+++ projects/storefrontlib/src/lib/ui/pages/payment-details-page/payment-details-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cx-payment-details-page',
   templateUrl: './payment-details-page.component.html',
-  styleUrls: ['./payment-details-page.component.scss']
+  styleUrls: []
 })
 export class PaymentDetailsPageComponent {
   constructor() {}
