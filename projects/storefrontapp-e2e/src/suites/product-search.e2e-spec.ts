61aab75aa8fad14fea48a75fda629041e642d9d2
Jim Yang <tian.hao.yang@sap.com>
2018-12-04 10:03:00 -0500

    GH-139: Updating tests

--- projects/storefrontapp-e2e/src/suites/product-search.e2e-spec.ts
+++ projects/storefrontapp-e2e/src/suites/product-search.e2e-spec.ts
@@ -74,7 +74,7 @@ describe('Product search', () => {
       await searchResults.getSingleFilterFacet(0).click();
 
       const text = await searchResults.getHeaderText();
-      expect(text).toContain('77 results for Chiba');
+      expect(text).toContain('79 results for Chiba');
     });
 
     it('should be able to clear active facet', async () => {
