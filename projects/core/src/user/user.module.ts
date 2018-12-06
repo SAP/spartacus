ecfaf5d72ef1a803c6667ae9812d16bfec82da72
Kacper Knapik <kknapik@divante.pl>
2018-12-06 10:43:20 +0100

    GH-263: Fix e2e tests

--- projects/core/src/user/user.module.ts
+++ projects/core/src/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { UserService } from './facade';
+import { UserService } from './facade/index';
 import { UserStoreModule } from './store/user-store.module';
 import { UserOccModule } from './occ/user-occ.module';
 
