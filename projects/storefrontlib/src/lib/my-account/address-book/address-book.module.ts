6bcb1d1cf411be68423aaa06870078c615ab6191
Mateusz Kolasa <mateusz.kolasa@sap.com>
2018-12-06 11:33:51 +0100

    GH-67: Fixed module imports, revert storefrontlib version

--- projects/storefrontlib/src/lib/my-account/address-book/address-book.module.ts
+++ projects/storefrontlib/src/lib/my-account/address-book/address-book.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AddressBookComponent } from './address-book.component';
 import { AddressCardComponent } from './address-card/address-card.component';
 import { CardModule } from '../../ui/components/card/card.module';
 import { AddressFormModule } from '../../checkout/components/multi-step-checkout/shipping-address/address-form/address-form.module';
 import { SpinnerModule } from '../../ui/components/spinner/spinner.module';
-import { GlobalMessageModule } from '../../global-message';
 import { UserService } from '../../user/facade/user.service';
 
 @NgModule({
-  imports: [
-    CardModule,
-    BrowserModule,
-    CommonModule,
-    AddressFormModule,
-    SpinnerModule,
-    GlobalMessageModule
-  ],
+  imports: [CommonModule, CardModule, AddressFormModule, SpinnerModule],
   declarations: [AddressBookComponent, AddressCardComponent],
   exports: [AddressBookComponent, AddressCardComponent],
   providers: [UserService]
