9cb0b5550c18f7db14cb9bd8e7d51bed5029410d
Kacper Knapik <hello@kacperknapik.pl>
2018-12-11 20:19:48 +0100

    GH-263: Fix reducers import

--- projects/core/src/user/index.ts
+++ projects/core/src/user/index.ts
@@ -1,10 +1,6 @@
 export * from './model/user.model';
 export * from './occ/index';
 export * from './occ/user-occ.module';
-
-export * from './store/user-state';
-export * from './store/actions/index';
-export * from './store/selectors/index';
-
+export * from './store/index';
 export * from './facade/index';
 export * from './user.module';
