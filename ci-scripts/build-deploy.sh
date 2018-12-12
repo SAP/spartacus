951d0b9dd3bf9a0bf3a896b88a3e08369ecee02d
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-12-12 16:07:38 -0600

    Revert "GH-649 Need to output the current branch on CI"
    This reverts commit f2bc5c180556ecc49aa773844d70dbc0fa4fa5da.

--- ci-scripts/build-deploy.sh
+++ ci-scripts/build-deploy.sh
@@ -3,7 +3,6 @@ set -e
 set -o pipefail
 
 BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-echo "Building from current branch: $BRANCH"
 
 if [[ $BRANCH != 'develop' ]]; then
   echo "This step only executes on develop branch"
