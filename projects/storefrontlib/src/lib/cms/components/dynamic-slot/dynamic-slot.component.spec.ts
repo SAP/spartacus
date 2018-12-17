30e9d4cbb4335f5faf83661dce4980808cac623e
Weizheng Gao <weizheng.gao@sap.com>
2018-12-17 11:11:43 -0500

    feature/GH-371: add test for content slot contract

--- projects/storefrontlib/src/lib/cms/components/dynamic-slot/dynamic-slot.component.spec.ts
+++ projects/storefrontlib/src/lib/cms/components/dynamic-slot/dynamic-slot.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { CmsService } from '@spartacus/core';
+import { of } from 'rxjs';
+
 import { DynamicSlotComponent } from './dynamic-slot.component';
 import { ComponentWrapperDirective } from './component-wrapper.directive';
 import { OutletDirective } from '../../../outlet';
 
 class MockCmsService {
-  getContentSlot() {}
+  getContentSlot() {
+    return of({
+      uid: 'slot_uid',
+      catalogUuid: 'slot_catalogUuid',
+      uuid: 'slot_uuid'
+    });
+  }
+  isLaunchInSmartEdit(): boolean {
+    return true;
+  }
 }
 
 describe('DynamicSlotComponent', () => {
   let dynamicSlotComponent: DynamicSlotComponent;
   let fixture: ComponentFixture<DynamicSlotComponent>;
+  let cmsService: CmsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -33,11 +45,42 @@ describe('DynamicSlotComponent', () => {
     fixture = TestBed.createComponent(DynamicSlotComponent);
     dynamicSlotComponent = fixture.componentInstance;
     dynamicSlotComponent.position = 'left';
-
     fixture.detectChanges();
+
+    cmsService = TestBed.get(CmsService);
   });
 
   it('should be created', () => {
     expect(dynamicSlotComponent).toBeTruthy();
+  });
+
+  it('should add smart edit slot contract if app launch in smart edit', () => {
+    const native = fixture.debugElement.nativeElement;
+    expect(native.getAttribute('data-smartedit-component-type')).toEqual(
+      'ContentSlot'
+    );
+    expect(native.getAttribute('data-smartedit-component-id')).toEqual(
+      'slot_uid'
+    );
+    expect(native.getAttribute('data-smartedit-catalog-version-uuid')).toEqual(
+      'slot_catalogUuid'
+    );
+    expect(native.getAttribute('data-smartedit-component-uuid')).toEqual(
+      'slot_uuid'
+    );
+    expect(native.classList.contains('smartEditComponent')).toBeTruthy();
+  });
+
+  it('should not add smart edit slot contract if app not launch in smart edit', () => {
+    spyOn(cmsService, 'isLaunchInSmartEdit').and.returnValue(false);
+
+    fixture = TestBed.createComponent(DynamicSlotComponent);
+    dynamicSlotComponent = fixture.componentInstance;
+    dynamicSlotComponent.position = 'left';
+    fixture.detectChanges();
+
+    const native = fixture.debugElement.nativeElement;
+    expect(native.classList.contains('smartEditComponent')).toBeFalsy();
+    expect(native.getAttribute('data-smartedit-component-id')).toEqual(null);
   });
 });
