256c78d249c00d6d1df25cad50301ebd74322854
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2018-12-18 10:09:06 -0600

    GH-649 Quoting backtick commands to get the right output

--- ci-scripts/build-deploy.sh
+++ ci-scripts/build-deploy.sh
@@ -2,6 +2,14 @@
 set -e
 set -o pipefail
 
+BRANCH="`git branch | grep \* | cut -d ' ' -f2`"
+echo "Building from current branch: $BRANCH"
+
+if [[ $BRANCH != 'develop' ]]; then
+  echo "This step only executes on develop branch"
+  exit
+fi
+
 echo "Building SPA core lib"
 yarn build:core:lib
 echo "-----"
