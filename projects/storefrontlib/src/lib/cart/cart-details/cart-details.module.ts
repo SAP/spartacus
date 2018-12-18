cfeb45643aed3dc406705953e68454d9fd763059
Krzysztof Platis <platonn.git@gmail.com>
2018-12-18 13:19:54 +0100

    import RouterModule in CartDetailsModule

--- projects/storefrontlib/src/lib/cart/cart-details/cart-details.module.ts
+++ projects/storefrontlib/src/lib/cart/cart-details/cart-details.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CartSharedModule } from '../cart-shared/cart-shared.module';
 import { CartDetailsComponent } from './container/cart-details.component';
 import { UrlTranslationModule } from '@spartacus/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CartSharedModule, CommonModule, UrlTranslationModule],
+  imports: [CartSharedModule, CommonModule, RouterModule, UrlTranslationModule],
   declarations: [CartDetailsComponent],
   exports: [CartDetailsComponent]
 })
