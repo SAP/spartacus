3b871e4e5eda557b9f431299578aef22851aa8b2
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-01-07 14:11:53 -0600

    GH-815 Parameterizing script to run e2e test suites

--- ci-scripts/e2e-tests.sh
+++ ci-scripts/e2e-tests.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
+SUITE='smoke'
+
+if [ ! -z $1 ]; then
+    if [ $1 == '-h' ]; then
+        echo "Usage: $0 [suite, default:smoke]"
+        exit 1
+    else
+        SUITE=$1
+    fi
+fi
 
 echo "Building SPA core lib"
 yarn build:core:lib
@@ -8,6 +18,7 @@ echo "-----"
 echo "Building SPA app"
 yarn build
 echo "-----"
-echo "Running end to end tests"
-yarn e2e:ci --suite=smoke
+echo "Running end to end tests. Suite: $SUITE"
+yarn e2e:ci --suite=$SUITE
 echo "-----"
+
