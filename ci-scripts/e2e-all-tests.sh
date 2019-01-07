e0d1aaee5ec593bffe5194eab119e5821d0de320
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-01-07 15:23:22 -0600

    GH-815 Script to trigger custom Travis build (all e2e tests)

---
+++ ci-scripts/e2e-all-tests.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+if [ ! -z $1 ]; then
+    if [ $1 == '-h' ]; then
+        echo "Usage: $0 [travis_token]"
+        exit 1
+    else
+        TOKEN=$1
+    fi
+else
+    echo "No Travis token specified. Aborting"
+    exit 1
+fi
+
+echo "Triggering custom Spa build with token $TOKEN"
+
+body='{
+    "request": {
+        "message": "Override the commit message: this is an api request",
+        "branch":"develop",
+        "config": {
+            "script": "./ci-scripts/e2e-tests.sh all"
+        }
+    }
+}'
+
+curl -s -X POST \
+-H "Content-Type: application/json" \
+-H "Accept: application/json" \
+-H "Travis-API-Version: 3" \
+-H "Authorization: token $TOKEN" \
+-d "$body" \
+https://api.travis-ci.com/repo/SAP%2Fcloud-commerce-spartacus-storefront/requests
