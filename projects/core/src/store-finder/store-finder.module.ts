3cc848da731b709cde7e6d53955f5eb600771f8f
Mateusz Kolasa <mateusz.kolasa@sap.com>
2019-01-08 15:58:34 +0100

    GH-151: Branch update

--- projects/core/src/store-finder/store-finder.module.ts
+++ projects/core/src/store-finder/store-finder.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { StoreFinderStoreModule } from './store/store-finder-store.module';
 import { StoreFinderService } from './facade/store-finder.service';
 import { StoreDataService } from './facade';
-import {
-  GoogleMapRendererService,
-  ExternalJsFileLoader,
-  WindowRef
-} from './services';
+import { GoogleMapRendererService, ExternalJsFileLoader } from './services';
+import { WindowRef } from '../window';
 
 @NgModule({
   imports: [StoreFinderStoreModule],
