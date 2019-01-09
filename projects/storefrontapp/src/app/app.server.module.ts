176e0f9c9427d54611c877ddc1c9027dc5c26f00
Stan <dunqan@w9.pl>
2019-01-09 13:29:03 +0100

    feat: Basic SSR suport (#778)
    * basic SSR solution
    * GH-776: Add express engine as dependencies
    * GH-776: Tidy up commands
    * GH-776: Use prod environtment file for ssr build
    
    Closes GH-776

---
+++ projects/storefrontapp/src/app/app.server.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+// import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+
+import { AppModule } from './app.module';
+import { StorefrontComponent } from '@spartacus/storefront';
+
+@NgModule({
+  imports: [
+    // The AppServerModule should import your AppModule followed
+    // by the ServerModule from @angular/platform-server.
+    AppModule,
+    ServerModule
+    // ModuleMapLoaderModule // <-- *Important* to have lazy-loaded routes work
+  ],
+  // Since the bootstrapped component is not inherited from your
+  // imported AppModule, it needs to be repeated here.
+  bootstrap: [StorefrontComponent]
+})
+export class AppServerModule {}
