5a5953a17d5663f3e1d91bcb677fc37e14d56e60
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-11 10:19:24 +0100

    Reorder elements in mobile view

--- projects/storefrontlib/src/lib/ui/layout/header/header.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, HostBinding } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cx-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor() {}
+export class HeaderComponent implements OnDestroy {
+  private subscription: Subscription;
+
+  @HostBinding('class.mobile-nav') showMenu: boolean = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+    if (this.showMenu) {
+      this.subscription = this.router.events.subscribe(() => {
+        this.toggleMenu();
+      });
+    } else {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
