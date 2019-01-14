62d425855f88d5a4af5c48a9719fa3de92daf442
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-14 16:33:40 +0100

    Drop obsolete complexity

--- projects/storefrontlib/src/lib/cms-lib/product-carousel/product-carousel.component.ts
+++ projects/storefrontlib/src/lib/cms-lib/product-carousel/product-carousel.component.ts
@@ -1,8 +1,6 @@
 import {
   Component,
   ChangeDetectionStrategy,
-  OnDestroy,
-  ViewChild,
   OnInit,
   ElementRef
 } from '@angular/core';
@@ -14,14 +12,14 @@ import {
   WindowRef
 } from '@spartacus/core';
 
-import { Subscription, fromEvent, Observable, of } from 'rxjs';
+import { fromEvent, Observable, of } from 'rxjs';
 import {
   debounceTime,
   map,
   distinctUntilChanged,
   startWith
 } from 'rxjs/operators';
-import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+
 import { CmsComponentData } from '../../cms/components/cms-component-data';
 
 const MAX_WIDTH = 360;
@@ -33,21 +31,11 @@ const MAX_ITEM_SIZE = 4;
   styleUrls: ['./product-carousel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductCarouselComponent implements OnDestroy, OnInit {
+export class ProductCarouselComponent implements OnInit {
   items$: Observable<Observable<Product>[]>;
   itemSize$: Observable<number>;
 
-  productGroups: Array<string[]>;
-  products: { [key: string]: Observable<Product> } = {};
-  productCodes: Array<string>;
-
-  resize$: Subscription;
-  dataSubscription$: Subscription;
-
   private window: Window;
-
-  @ViewChild('carousel')
-  carousel: NgbCarousel;
 
   constructor(
     public component: CmsComponentData<CmsProductCarouselComponent>,
@@ -59,13 +47,6 @@ export class ProductCarouselComponent implements OnDestroy, OnInit {
   }
 
   ngOnInit() {
-    if (this.window) {
-      this.subscribeToData();
-      this.resize$ = fromEvent(this.window, 'resize')
-        .pipe(debounceTime(300))
-        .subscribe(() => this.createGroups());
-    }
-
     this.setItemSize();
     this.setItems();
   }
@@ -107,63 +88,7 @@ export class ProductCarouselComponent implements OnDestroy, OnInit {
     );
   }
 
-  prev() {
-    this.carousel.prev();
-  }
+  prev() {}
 
-  next() {
-    this.carousel.next();
-  }
-
-  protected createGroups() {
-    const groups: Array<string[]> = [];
-    if (this.productCodes) {
-      this.productCodes.forEach(product => {
-        const lastGroup: string[] = groups[groups.length - 1];
-        if (lastGroup && lastGroup.length < this.getItemsPerPage()) {
-          lastGroup.push(product);
-        } else {
-          groups.push([product]);
-        }
-      });
-    }
-    this.productGroups = groups;
-  }
-
-  protected getItemsPerPage(): number {
-    const smallScreenMaxWidth = 576;
-    const tabletScreenMaxWidth = 768;
-    const innerWidth = this.window ? window.innerWidth : tabletScreenMaxWidth;
-    let itemsPerPage: number;
-    if (innerWidth < smallScreenMaxWidth) {
-      itemsPerPage = 1;
-    } else if (
-      innerWidth > smallScreenMaxWidth &&
-      innerWidth < tabletScreenMaxWidth
-    ) {
-      itemsPerPage = 2;
-    } else {
-      itemsPerPage = 4;
-    }
-    return itemsPerPage;
-  }
-
-  protected subscribeToData() {
-    this.dataSubscription$ = this.component.data$.subscribe(data => {
-      this.productCodes = data.productCodes.split(' ');
-      this.productCodes.forEach(code => {
-        this.products[code] = this.productService.get(code);
-      });
-      this.createGroups();
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.dataSubscription$) {
-      this.dataSubscription$.unsubscribe();
-    }
-    if (this.resize$) {
-      this.resize$.unsubscribe();
-    }
-  }
+  next() {}
 }
