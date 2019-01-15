98a5728c2fe25e4b74c4c90287c91af64e43ea87
Michał Gruca <pucek9@gmail.com>
2019-01-15 15:17:10 +0100

    added form to dialog

--- projects/storefrontlib/src/lib/cart/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.ts
+++ projects/storefrontlib/src/lib/cart/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.ts
@@ -1,12 +1,14 @@
 import {
-  Component,
-  OnInit,
   AfterViewChecked,
+  Component,
   ElementRef,
+  OnInit,
   ViewChild
 } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CartService } from '@spartacus/core';
 
 @Component({
   selector: 'cx-added-to-cart-dialog',
@@ -24,13 +26,28 @@ export class AddedToCartDialogComponent implements OnInit, AfterViewChecked {
   @ViewChild('dialog', { read: ElementRef })
   dialog: ElementRef;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  form: FormGroup = this.fb.group({});
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    protected cartService: CartService,
+    protected fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.loaded$.subscribe(res => {
       if (this.previousLoadedState !== res) {
         this.finishedLoading = this.previousLoadedState === false;
         this.previousLoadedState = res;
+      }
+    });
+    this.entry$.subscribe(entry => {
+      const { code } = entry.product;
+      if (!this.form.controls[code]) {
+        this.form.setControl(code, this.createEntryFormGroup(entry));
+      } else {
+        const entryForm = this.form.controls[code] as FormGroup;
+        entryForm.controls.quantity.setValue(entry.quantity);
       }
     });
   }
@@ -43,5 +60,12 @@ export class AddedToCartDialogComponent implements OnInit, AfterViewChecked {
       ) as HTMLElement;
       elementToFocus.focus();
     }
+  }
+
+  private createEntryFormGroup(entry) {
+    return this.fb.group({
+      entryNumber: entry.entryNumber,
+      quantity: entry.quantity
+    });
   }
 }
