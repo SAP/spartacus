bb5c515ef3602ba59d35e29bb4e69274fb220120
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-17 13:14:56 +0100

    improve generic selector

--- projects/storefrontlib/src/lib/cms-lib/site-context-selector/site-context-selector.component.ts
+++ projects/storefrontlib/src/lib/cms-lib/site-context-selector/site-context-selector.component.ts
@@ -1,13 +1,13 @@
-import { Component, Injector, HostBinding, OnInit } from '@angular/core';
+import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, filter, switchMap, take } from 'rxjs/operators';
 import {
   CmsSiteContextSelectorComponent,
+  SiteContext,
   LanguageService,
-  CurrencyService,
-  SiteContext
+  CurrencyService
 } from '@spartacus/core';
 import { CmsComponentData } from '../../cms/components/cms-component-data';
-import { Observable } from 'rxjs';
-import { map, filter, tap } from 'rxjs/operators';
 
 const LANGUAGE = 'LANGUAGE';
 const CURRENCY = 'CURRENCY';
@@ -15,53 +15,65 @@ const CURRENCY = 'CURRENCY';
 @Component({
   selector: 'cx-site-context-selector',
   templateUrl: './site-context-selector.component.html',
-  styleUrls: ['./site-context-selector.component.scss']
+  styleUrls: ['./site-context-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SiteContextSelectorComponent implements OnInit {
+export class SiteContextSelectorComponent {
   siteContextService: SiteContext<any>;
-
-  @HostBinding('class.selector') enabled;
 
   constructor(
     public component: CmsComponentData<CmsSiteContextSelectorComponent>,
     private injector: Injector
   ) {}
 
-  ngOnInit() {
-    this.context$
-      .pipe(filter(Boolean))
-      .subscribe((context: string) => (this.service = context))
-      .unsubscribe();
+  get items$(): Observable<any> {
+    return this.service$.pipe(
+      switchMap((service: SiteContext<any>) => service.getAll()),
+      switchMap(items =>
+        this.context$.pipe(
+          switchMap(context => {
+            items.forEach(item => {
+              return (item.label = this.getOptionText(item, context));
+            });
+            return of(items);
+          })
+        )
+      )
+    );
+  }
+
+  get activeItem$(): Observable<string> {
+    return this.service$.pipe(
+      switchMap((service: SiteContext<any>) => service.getActive())
+    );
+  }
+
+  get service$(): Observable<SiteContext<any>> {
+    return this.component.data$.pipe(
+      map(data => data.context),
+      map(context => this.getService(context)),
+      filter(Boolean)
+    );
+  }
+
+  get context$(): Observable<string> {
+    return this.component.data$.pipe(map(data => data.context));
   }
 
   /**
    * Injects the proper `SiteContext` service
+   * based on the given context.
    */
-  protected set service(context: string) {
+  protected getService(context: string) {
     if (context === LANGUAGE) {
-      this.siteContextService = this.injector.get(LanguageService);
+      return this.injector.get(LanguageService);
     }
     if (context === CURRENCY) {
-      this.siteContextService = this.injector.get(CurrencyService);
+      return this.injector.get(CurrencyService);
     }
   }
 
-  protected get items$(): Observable<any> {
-    return this.siteContextService.getAll().pipe(
-      filter(Boolean),
-      tap(l => (this.enabled = l.length > 1))
-    );
-  }
-
-  protected get activeItem$(): Observable<string> {
-    return this.siteContextService.getActive();
-  }
-
-  protected get context$(): Observable<string> {
-    return this.component.data$.pipe(map(data => data.context));
-  }
-
-  protected getOptionText(item: any, context: string) {
+  protected getOptionText(item: any, context?: string) {
     switch (context) {
       case LANGUAGE:
         return item.nativeName;
@@ -69,14 +81,18 @@ export class SiteContextSelectorComponent implements OnInit {
       case CURRENCY:
         return item.symbol + ' ' + item.isocode;
         break;
+      default:
+        return item.isocode;
     }
   }
 
-  protected setActive(value: string) {
-    this.siteContextService.setActive(value);
+  set active(value: string) {
+    this.service$.pipe(take(1)).subscribe(service => {
+      service.setActive(value);
+    });
   }
 
-  protected get label$(): Observable<any> {
+  get label$(): Observable<any> {
     return this.component.data$.pipe(
       map(data => {
         return data.context === LANGUAGE ? 'Language' : 'Currency';
