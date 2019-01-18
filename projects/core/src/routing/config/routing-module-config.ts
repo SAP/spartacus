f117838f931197a8b753076641c88dbb83a6c071
Stan <dunqan@w9.pl>
2019-01-18 22:28:16 +0100

    feat(@spartacus/core): Implement TransferState for SSR (GH-806) (#905)
    * GH-806: Move storage sync reducer to state module
    
    * GH-806: Implement basic transfer state module
    
    * GH-806: transferSsr keys definition as object
    
    * GH-806: Include TransferState modules
    
    * GH-806: Move state config to state modules
    
    * GH-806: Include cms and siteContext in ssrTransferState
    
    * GH-806: Check language and currency before change
    
    * GH-806: Wait for SSR data before app bootstrap
    
    * GH-806: Add withConfigFactory to Config Module
    
    * GH-806: Use config factories for store/state default configs
    
    * GH-806: Formatting fixes
    
    * GH-806: Remove development residues
    
    * GH-806: Fix severTransferState reducer
    
    * GH-806: Fix tests
    
    * GH-806: Remove hard dependency on config and state trasfer from StateModule
    
    * GH-806: Simplyfy auth service tests
    
    * GH-806: Fix formatting, add comments
    
    * GH-806: Add test to withCofigFactory for CofiglModule
    
    * GH-806: Add unit tests for transfer state featurs
    
    * GH-806: Fix lib compilation
    
    * GH-806: Move storage sync reducer to state module
    
    Closes GH-806

--- projects/core/src/routing/config/routing-module-config.ts
+++
@@ -1,9 +0,0 @@
-export enum StorageSyncType {
-  NO_STORAGE = 'NO_STORAGE',
-  LOCAL_STORAGE = 'LOCAL_STORAGE',
-  SESSION_STORAGE = 'SESSION_STORAGE'
-}
-
-export abstract class RoutingModuleConfig {
-  storageSyncType?: StorageSyncType;
-}
