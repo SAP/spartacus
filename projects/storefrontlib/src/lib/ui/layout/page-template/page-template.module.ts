793146fd1fb82ee435ab954f1fb7c2d80dec2e9c
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-18 10:43:50 +0100

    Add outletRef to page templates

--- projects/storefrontlib/src/lib/ui/layout/page-template/page-template.module.ts
+++ projects/storefrontlib/src/lib/ui/layout/page-template/page-template.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTemplateComponent } from './page-template.component';
 import { CmsModule } from '../../../cms';
+import { OutletModule } from '../../../outlet';
 
 @NgModule({
-  imports: [CommonModule, CmsModule],
+  imports: [CommonModule, CmsModule, OutletModule],
   declarations: [PageTemplateComponent],
   exports: [PageTemplateComponent]
 })
