f62698e46efd326d40959ff8dc6c96484a0dc256
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-01-24 13:18:51 -0500

    GH-533 Bringing back mock messages. Adding right type to get method.

--- projects/storefrontlib/src/lib/global-message/global-message.component.spec.ts
+++ projects/storefrontlib/src/lib/global-message/global-message.component.spec.ts
@@ -1,10 +1,24 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { of, Observable } from 'rxjs';
 import createSpy = jasmine.createSpy;
 import { GlobalMessageComponent } from './global-message.component';
-import { GlobalMessageType, GlobalMessageService } from '@spartacus/core';
+import {
+  GlobalMessageType,
+  GlobalMessageService,
+  GlobalMessageEntities
+} from '@spartacus/core';
+
+const mockMessages = {
+  [GlobalMessageType.MSG_TYPE_CONFIRMATION]: ['Confirmation'],
+  [GlobalMessageType.MSG_TYPE_INFO]: ['Info'],
+  [GlobalMessageType.MSG_TYPE_ERROR]: ['Error']
+};
 
 class MockMessageService {
   remove = createSpy();
+  get(): Observable<GlobalMessageEntities> {
+    return of(mockMessages);
+  }
 }
 
 describe('GlobalMessageComponent', () => {
