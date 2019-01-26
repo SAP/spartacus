2871259a8f59c79e7afcdd7daa110f6306011798
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-26 10:47:13 +0100

    delegate component logic to layout service

--- projects/storefrontlib/src/lib/cms/page-layout/page-layout.component.ts
+++ projects/storefrontlib/src/lib/cms/page-layout/page-layout.component.ts
@@ -6,29 +6,7 @@ import {
   OnInit,
   ChangeDetectionStrategy
 } from '@angular/core';
-import { CmsService, Page, CmsConfig, WindowRef } from '@spartacus/core';
-import { Observable, fromEvent } from 'rxjs';
-import {
-  map,
-  filter,
-  debounceTime,
-  startWith,
-  distinctUntilChanged,
-  switchMap
-} from 'rxjs/operators';
-
-export enum BREAKPOINT {
-  xs = 'xs',
-  sm = 'sm',
-  md = 'md',
-  lg = 'lg'
-}
-
-export enum BREAKPOINT_SIZE {
-  xs = 600,
-  sm = 900,
-  md = 1200
-}
+import { PageLayoutService } from './page-layout.service';
 
 @Component({
   selector: 'cx-page-layout',
@@ -36,101 +14,25 @@ export enum BREAKPOINT_SIZE {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageLayoutComponent implements OnInit {
-  @Input() section;
+  @Input() section: string;
 
   constructor(
-    private cms: CmsService,
-    private config: CmsConfig,
     private el: ElementRef,
     private renderer: Renderer2,
-    private winRef: WindowRef
+    private pageLayoutService: PageLayoutService
   ) {}
 
   ngOnInit() {
     if (this.section) {
-      this.cssClass = this.section;
+      this.styleClass = this.section;
     }
   }
 
-  set cssClass(cl) {
-    this.renderer.addClass(this.el.nativeElement, cl);
+  get slots$() {
+    return this.pageLayoutService.getSlots(this.section);
   }
 
-  get slots$(): Observable<any> {
-    return fromEvent(this.window, 'resize').pipe(
-      debounceTime(300),
-      startWith({ target: this.window }),
-      map(event => this.getBreakpoint((<Window>event.target).innerWidth)),
-      distinctUntilChanged(),
-      switchMap(val =>
-        this.page$.pipe(
-          map(page => {
-            return this.getSlotConfiguration(page.template, val);
-          })
-        )
-      )
-    );
-  }
-
-  get page$(): Observable<Page> {
-    return this.cms.getCurrentPage().pipe(filter(Boolean));
-  }
-
-  get templateName$(): Observable<string> {
-    return this.page$.pipe(map((page: Page) => page.template));
-  }
-
-  protected getBreakpoint(windowWidth: number) {
-    const BreakpointSize = Object.keys(BREAKPOINT_SIZE).find(
-      b => windowWidth < BREAKPOINT_SIZE[b]
-    );
-    const breakpoint = BREAKPOINT[BreakpointSize || BREAKPOINT.lg];
-    return breakpoint;
-  }
-
-  /**
-   * load slots from the layout configuration. The breakpoint is used
-   * to load a specific configuration for the given breakpoint. If there's
-   * no configuration available for the given breakpoint the default slot
-   * configuration is returned.
-   */
-  protected getSlotConfiguration(
-    templateUid: string,
-    breakpoint: BREAKPOINT
-  ): string[] {
-    let slots = this.config.layoutSlots[templateUid];
-    if (this.section) {
-      slots = slots[this.section]
-        ? slots[this.section]
-        : this.config.layoutSlots[this.section];
-    }
-    if (!slots) {
-      return [];
-    }
-    return slots[breakpoint]
-      ? <string[]>slots[breakpoint]
-      : <string[]>slots.slots;
-  }
-
-  // protected getSlotConfiguration2(templateUid: string): SlotGroup {
-  //   if (this.section) {
-  //     return this.config.layoutSlots[templateUid] &&
-  //       this.config.layoutSlots[templateUid][this.section] &&
-  //       this.config.layoutSlots[templateUid][this.section].slots
-  //       ? this.config.layoutSlots[templateUid][this.section]
-  //       : this.config.layoutSlots[this.section];
-  //   } else {
-  //     if (
-  //       this.config.layoutSlots[templateUid] &&
-  //       this.config.layoutSlots[templateUid].slots
-  //     ) {
-  //       return this.config.layoutSlots[templateUid];
-  //     }
-  //   }
-  //   return null;
-  // }
-
-  get window() {
-    return this.winRef.nativeWindow;
+  set styleClass(cls: string) {
+    this.renderer.addClass(this.el.nativeElement, cls);
   }
 }
---
+++ projects/storefrontlib/src/lib/cms/page-layout/page-layout.service.ts
@@ -0,0 +1,132 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap, distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { CmsService, CmsConfig, Page, LayoutSlotConfig } from '@spartacus/core';
+import { BreakpointService } from '../../ui/layout/breakpoint/breakpoint.service';
+import { BREAKPOINT } from '../../ui/layout/config/layout-config';
+
+@Injectable()
+export class PageLayoutService {
+  constructor(
+    private cms: CmsService,
+    private config: CmsConfig,
+    private breakpointService: BreakpointService
+  ) {}
+
+  // we track warn messages on missing layout configs
+  //  to not polute the console log
+  private warnLogMessages = {};
+
+  getSlots(section?: string): Observable<string[]> {
+    return this.breakpointService.breakpoint$.pipe(
+      switchMap(breakpoint =>
+        this.page$.pipe(
+          map(page => {
+            return this.getSlotConfig(page.template, section, breakpoint);
+          })
+        )
+      ),
+      distinctUntilChanged()
+    );
+  }
+
+  get page$(): Observable<Page> {
+    return this.cms.getCurrentPage().pipe(filter(Boolean));
+  }
+
+  /**
+   * load slots from the layout configuration. The breakpoint is used
+   * to load a specific configuration for the given breakpoint. If there's
+   * no configuration available for the given breakpoint the default slot
+   * configuration is returned.
+   */
+  protected getSlotConfig(
+    templateUid: string,
+    section?: string,
+    breakpoint?: BREAKPOINT
+  ): string[] {
+    let slotConfig = this.config.layoutSlots[templateUid];
+    if (!slotConfig) {
+      return this.noConfigFound(templateUid);
+    }
+    if (section) {
+      // if there's no section config on the page layout
+      // we fall back to the global section config
+      slotConfig = slotConfig[section]
+        ? slotConfig[section]
+        : this.config.layoutSlots[section];
+
+      if (!slotConfig) {
+        return this.noConfigFound(templateUid, section);
+      }
+    }
+
+    return this.getResponsiveSlotConfig(
+      <LayoutSlotConfig>slotConfig,
+      breakpoint
+    );
+  }
+
+  /**
+   * Returns a list of slots for a breakpoint specific configuratoin
+   * If there's no specific configuration for the breakpoint,
+   * the closest available configuration will be returned.
+   */
+  protected getResponsiveSlotConfig(
+    layoutSlotConfig: LayoutSlotConfig,
+    breakpoint: BREAKPOINT
+  ): string[] {
+    if (!breakpoint) {
+      return <string[]>(layoutSlotConfig.slots || layoutSlotConfig.xs);
+    }
+    if (layoutSlotConfig[breakpoint]) {
+      return <string[]>layoutSlotConfig[breakpoint];
+    }
+    let config = <string[]>layoutSlotConfig.slots;
+
+    if (
+      this.breakpointService.isDown(BREAKPOINT.xs) &&
+      layoutSlotConfig[BREAKPOINT.xs]
+    ) {
+      config = <string[]>layoutSlotConfig[BREAKPOINT.xs];
+    }
+
+    if (
+      this.breakpointService.isUp(BREAKPOINT.xs) &&
+      this.breakpointService.isDown(BREAKPOINT.sm) &&
+      layoutSlotConfig[BREAKPOINT.sm]
+    ) {
+      config = <string[]>layoutSlotConfig[BREAKPOINT.sm];
+    }
+    if (
+      this.breakpointService.isUp(BREAKPOINT.sm) &&
+      this.breakpointService.isDown(BREAKPOINT.md) &&
+      layoutSlotConfig[BREAKPOINT.md]
+    ) {
+      config = <string[]>layoutSlotConfig[BREAKPOINT.md];
+    }
+    if (
+      this.breakpointService.isUp(BREAKPOINT.md) &&
+      layoutSlotConfig[BREAKPOINT.lg]
+    ) {
+      config = <string[]>layoutSlotConfig[BREAKPOINT.lg];
+    }
+
+    return config;
+  }
+
+  private noConfigFound(template: string, section?: string) {
+    if (section) {
+      if (!this.warnLogMessages[section + ':' + template]) {
+        console.warn(
+          `no layout config found for section ${section} of template ${template}`
+        );
+        this.warnLogMessages[section + ':' + template] = true;
+      }
+    } else if (!this.warnLogMessages[template]) {
+      console.warn(`no layout config found for ${template}`);
+      this.warnLogMessages[template] = true;
+    }
+    return [];
+  }
+}
