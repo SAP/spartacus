1eb9a1064caea56b4cb00e8f4861c3e9576daf6b
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-29 01:41:53 +0100

    Drop obsolete page and templates

--- projects/storefrontlib/src/lib/ui/layout/layout.module.ts
+++ projects/storefrontlib/src/lib/ui/layout/layout.module.ts
@@ -4,7 +4,6 @@ import { OutletRefModule } from '../../outlet/index';
 import { StyleRefModule } from '../../outlet/style-ref/style-ref.module';
 
 import { CartPageLayoutModule } from './cart-page-layout/cart-page-layout.module';
-import { CategoryPageLayoutModule } from './category-page-layout/category-page-layout.module';
 import { ContactPageLayoutModule } from './contact-page-layout/contact-page-layout.module';
 import { HelpPageLayoutModule } from './help-page-layout/help-page-layout.module';
 import { LandingPageLayoutModule } from './landing-page-layout/landing-page-layout.module';
@@ -17,7 +16,6 @@ import { AddressBookPageLayoutModule } from './address-book-page-layout/address-
 import { OrderHistoryPageLayoutModule } from './order-history-page-layout/order-history-page-layout.module';
 import { PaymentDetailsPageLayoutModule } from './payment-details-page-layout/payment-details-page-layout.module';
 import { ProductDetailsPageLayoutModule } from './product-details-page-layout/product-details-page-layout.module';
-import { ProductListPageLayoutModule } from './product-list-page-layout/product-list-page-layout.module';
 import { RegisterLayoutModule } from './register-layout/register-layout.module';
 import { SalePageLayoutModule } from './sale-page-layout/sale-page-layout.module';
 import { StoreFinderPageLayoutModule } from './store-finder-page-layout/store-finder-page-layout.module';
@@ -32,8 +30,6 @@ const layoutModules = [
   LandingPageLayoutModule,
   OrderHistoryPageLayoutModule,
   CartPageLayoutModule,
-  CategoryPageLayoutModule,
-  ProductListPageLayoutModule,
   MultiStepCheckoutPageLayoutModule,
   OrderDetailsPageLayoutModule,
   OrderConfirmationPageLayoutModule,
