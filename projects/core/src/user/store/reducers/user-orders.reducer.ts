5899be0e3670f02f35cfae63e03f72eb43bcca69
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-01-31 09:14:42 -0500

    fixed a test

--- projects/core/src/user/store/reducers/user-orders.reducer.ts
+++ projects/core/src/user/store/reducers/user-orders.reducer.ts
@@ -1,4 +1,5 @@
 import * as fromAction from '../actions/index';
+import * as fromUserOrdersAction from '../actions/user-orders.action';
 import { OrderHistoryList } from '../../../occ';
 
 export const initialState: OrderHistoryList = {
@@ -9,9 +10,19 @@ export const initialState: OrderHistoryList = {
 
 export function reducer(
   state = initialState,
-  action: fromAction.MiscsDataAction
+  action: fromUserOrdersAction.UserOrdersAction | fromAction.MiscsDataAction
 ): OrderHistoryList {
   switch (action.type) {
+    case fromUserOrdersAction.LOAD_USER_ORDERS_SUCCESS: {
+      const orders: OrderHistoryList = action.payload;
+      return orders;
+    }
+    case fromUserOrdersAction.LOAD_USER_ORDERS_FAIL: {
+      return {
+        ...state
+      };
+    }
+
     case fromAction.CLEAR_MISCS_DATA: {
       return initialState;
     }
