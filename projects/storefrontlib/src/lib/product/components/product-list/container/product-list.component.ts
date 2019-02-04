4a63910375860577c29884d40315173d72c8da36
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2019-02-04 14:27:22 -0500

    fix(@spartacus/storefront): Changing the currency on product search does not reload the data (GH-879) (#932)
    fix(@spartacus/storefront): Changing the currency on product search does not reload the data (GH-879)
    
    Closes GH-879

--- projects/storefrontlib/src/lib/product/components/product-list/container/product-list.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-list/container/product-list.component.ts
@@ -3,13 +3,17 @@ import {
   Input,
   OnInit,
   OnChanges,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  SimpleChanges
 } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, filter } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
-import { SearchConfig } from '@spartacus/core';
-import { ProductSearchService } from '@spartacus/core';
+import {
+  ProductSearchService,
+  ProductSearchPage,
+  SearchConfig
+} from '@spartacus/core';
 
 @Component({
   selector: 'cx-product-list',
@@ -30,18 +34,19 @@ export class ProductListComponent implements OnChanges, OnInit {
   itemPerPage: number;
 
   grid: any;
-  model$: Observable<any>;
+  model$: Observable<ProductSearchPage>;
   searchConfig: SearchConfig = {};
   categoryTitle: string;
+  options: SearchConfig;
 
   constructor(
     protected productSearchService: ProductSearchService,
     private activatedRoute: ActivatedRoute
   ) {}
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
     const { queryParams } = this.activatedRoute.snapshot;
-    const options = this.createOptionsByUrlParams();
+    this.options = this.createOptionsByUrlParams();
 
     if (this.categoryCode && this.categoryCode !== queryParams.categoryCode) {
       this.query = ':relevance:category:' + this.categoryCode;
@@ -52,8 +57,10 @@ export class ProductListComponent implements OnChanges, OnInit {
     if (!this.query && queryParams.query) {
       this.query = queryParams.query;
     }
-    if (this.query) {
-      this.search(this.query, options);
+
+    // do search only when 'brandCode' or 'categoryCode' or 'query' changed
+    if (Object.keys(changes).length === 1 && !changes['gridMode']) {
+      this.search(this.query, this.options);
     }
   }
 
@@ -74,6 +81,7 @@ export class ProductListComponent implements OnChanges, OnInit {
     if (this.brandCode) {
       options.brandCode = this.brandCode;
     }
+
     return options;
   }
 
@@ -82,21 +90,33 @@ export class ProductListComponent implements OnChanges, OnInit {
       mode: this.gridMode
     };
 
+    // clean previous search result
+    this.productSearchService.clearSearchResults();
+
     this.model$ = this.productSearchService.getSearchResults().pipe(
       tap(searchResult => {
-        if (searchResult.breadcrumbs && searchResult.breadcrumbs.length > 0) {
-          this.categoryTitle = searchResult.breadcrumbs[0].facetValueName;
-        } else if (!this.query.includes(':relevance:')) {
-          this.categoryTitle = this.query;
+        if (Object.keys(searchResult).length === 0) {
+          this.search(this.query, this.options);
+        } else {
+          this.getCategoryTitle(searchResult);
         }
-        if (this.categoryTitle) {
-          this.categoryTitle =
-            searchResult.pagination.totalResults +
-            ' results for ' +
-            this.categoryTitle;
-        }
-      })
+      }),
+      filter(searchResult => Object.keys(searchResult).length > 0)
     );
+  }
+
+  protected getCategoryTitle(data: ProductSearchPage): string {
+    if (data.breadcrumbs && data.breadcrumbs.length > 0) {
+      this.categoryTitle = data.breadcrumbs[0].facetValueName;
+    } else if (!this.query.includes(':relevance:')) {
+      this.categoryTitle = this.query;
+    }
+    if (this.categoryTitle) {
+      this.categoryTitle =
+        data.pagination.totalResults + ' results for ' + this.categoryTitle;
+    }
+
+    return this.categoryTitle;
   }
 
   onFilter(query: string) {
@@ -113,13 +133,16 @@ export class ProductListComponent implements OnChanges, OnInit {
   }
 
   protected search(query: string, options?: SearchConfig) {
-    if (options) {
-      // Overide default options
-      this.searchConfig = {
-        ...this.searchConfig,
-        ...options
-      };
+    if (this.query) {
+      if (options) {
+        // Overide default options
+        this.searchConfig = {
+          ...this.searchConfig,
+          ...options
+        };
+      }
+
+      this.productSearchService.search(query, this.searchConfig);
     }
-    this.productSearchService.search(query, this.searchConfig);
   }
 }
