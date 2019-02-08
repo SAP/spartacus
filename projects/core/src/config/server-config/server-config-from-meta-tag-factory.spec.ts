121772f30608d976d17567800126e44a52f0ffac
Krzysztof Platis <platonn.git@gmail.com>
2019-02-08 12:47:46 +0100

    add unit test for placeholder logic

--- projects/core/src/config/server-config/server-config-from-meta-tag-factory.spec.ts
+++ projects/core/src/config/server-config/server-config-from-meta-tag-factory.spec.ts
@@ -10,14 +10,12 @@ describe('serverConfigMetaTagFactory', () => {
 
   it('should get content of tag "occ-backend-base-url"', () => {
     mockMeta.getTag = jasmine.createSpy();
-
     serverConfigFromMetaTagFactory(mockMeta as Meta);
     expect(mockMeta.getTag).toHaveBeenCalledWith('name="occ-backend-base-url"');
   });
 
   it('should return server config with baseUrl from meta tag', () => {
     mockMeta.getTag = () => ({ content: 'testBaseUrl' } as HTMLMetaElement);
-
     expect(serverConfigFromMetaTagFactory(mockMeta as Meta)).toEqual({
       server: { baseUrl: 'testBaseUrl' }
     });
@@ -25,13 +23,17 @@ describe('serverConfigMetaTagFactory', () => {
 
   it('should return empty object when meta tag contains empty string', () => {
     mockMeta.getTag = () => ({ content: '' } as HTMLMetaElement);
+    expect(serverConfigFromMetaTagFactory(mockMeta as Meta)).toEqual({});
+  });
 
+  it('should return empty object when meta tag contains placeholder "OCC_BACKEND_BASE_URL_VALUE"', () => {
+    mockMeta.getTag = () =>
+      ({ content: 'OCC_BACKEND_BASE_URL_VALUE' } as HTMLMetaElement);
     expect(serverConfigFromMetaTagFactory(mockMeta as Meta)).toEqual({});
   });
 
   it('should return empty object when there is no meta tag', () => {
     mockMeta.getTag = () => null;
-
     expect(serverConfigFromMetaTagFactory(mockMeta as Meta)).toEqual({});
   });
 });
