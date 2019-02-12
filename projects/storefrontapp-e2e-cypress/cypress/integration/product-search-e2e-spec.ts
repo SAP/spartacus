0a78c550dbc4a9c6ccb592cfd9b9959af25a7e8f
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-02-12 14:59:20 -0500

    GH-1120 Migrating product search e2e tests to cypress

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/product-search-e2e-spec.ts
@@ -0,0 +1,51 @@
+const PRODUCTS_PER_PAGE = 10;
+
+context('Product search', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  describe('Search results', () => {
+    it('should be able to search and get results', () => {
+      cy.get('cx-searchbox input').type('camera{enter}');
+      cy.get('header h1').contains('results for camera');
+      cy.get('cx-product-list cx-product-list-item').should(
+        'have.length',
+        PRODUCTS_PER_PAGE
+      );
+      cy.get('cx-product-list cx-product-list-item:first').within(() => {
+        cy.get('a.cx-product-name').should(
+          'contain',
+          'Photosmart E317 Digital Camera'
+        );
+      });
+    });
+  });
+
+  describe('Pagination', () => {
+    it('should navigate to the next page and display results', () => {
+      cy.get('.page-item:last-of-type .page-link:first').click();
+      cy.get('.page-item.active > .page-link').should('contain', '2');
+    });
+
+    it('should be able navigate to the specified page number and display results', () => {
+      cy.get('.page-item:nth-child(4) .page-link:first').click();
+      cy.get('.page-item.active > .page-link').should('contain', '3');
+    });
+
+    it('should navigate to the previous page and display results', () => {
+      cy.get('.page-item:first-of-type .page-link:first').click();
+      cy.get('.page-item.active > .page-link').should('contain', '2');
+    });
+  });
+
+  describe('product list view mode', () => {
+    it('should be able to switch to grid mode', () => {
+      cy.get('cx-product-view > div > div:first').click();
+      cy.get('cx-product-list cx-product-grid-item').should(
+        'have.length',
+        PRODUCTS_PER_PAGE
+      );
+    });
+  });
+});
