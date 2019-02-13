fe7a2769c1ec7b1fdd82348a126320b04fc74d9a
Nikola Zarić <nikola.zaric.ns@gmail.com>
2019-02-13 10:37:39 -0500

    feat(@spartacus/core): remove clearmiscsdata action from user's meta reducer  (#1190)

--- projects/core/src/user/store/effects/order-details.effect.ts
+++ projects/core/src/user/store/effects/order-details.effect.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
+
 import { Actions, Effect, ofType } from '@ngrx/effects';
+
 import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
-import { OccOrderService } from '../../occ/index';
 
 import * as fromOrderDetailsAction from '../actions/order-details.action';
-import { ProductImageConverterService } from '../../../product/store/converters/index';
+import { OccOrderService } from '../../occ/index';
 import { Order } from '../../../occ/occ-models/index';
+import { ProductImageConverterService } from '../../../product/store/converters/index';
 
 @Injectable()
 export class OrderDetailsEffect {
-  constructor(
-    private actions$: Actions,
-    private occOrderService: OccOrderService,
-    private productImageConverter: ProductImageConverterService
-  ) {}
-
   @Effect()
-  loadOrderDetails$: Observable<any> = this.actions$.pipe(
+  loadOrderDetails$: Observable<
+    fromOrderDetailsAction.OrderDetailsAction
+  > = this.actions$.pipe(
     ofType(fromOrderDetailsAction.LOAD_ORDER_DETAILS),
     map((action: fromOrderDetailsAction.LoadOrderDetails) => action.payload),
     switchMap(payload => {
@@ -47,4 +45,10 @@ export class OrderDetailsEffect {
         );
     })
   );
+
+  constructor(
+    private actions$: Actions,
+    private occOrderService: OccOrderService,
+    private productImageConverter: ProductImageConverterService
+  ) {}
 }
