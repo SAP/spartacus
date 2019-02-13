1365cc22b5c06492ba04c701624bae2fbb200d06
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-02-13 17:24:49 -0500

    GH-1120 Adding constants for most common selectors

--- projects/storefrontapp-e2e-cypress/cypress/integration/product-search-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/product-search-e2e-spec.ts
@@ -1,4 +1,7 @@
 const PRODUCTS_PER_PAGE = 10;
+const resultsTitleSelector = 'header h1';
+const productItemSelector = 'cx-product-list cx-product-list-item';
+const firstProductItemSelector = `${productItemSelector}:first`;
 
 context('Product search', () => {
   before(() => {
@@ -8,31 +11,30 @@ context('Product search', () => {
   describe('Search results', () => {
     it('should be able to search and get results', () => {
       cy.get('cx-searchbox input').type('camera{enter}');
-      cy.get('header h1').contains('results for camera');
-      cy.get('cx-product-list cx-product-list-item').should(
-        'have.length',
-        PRODUCTS_PER_PAGE
-      );
-      cy.get('cx-product-list cx-product-list-item:first').within(() => {
+      cy.get(resultsTitleSelector).contains('results for camera');
+      cy.get(productItemSelector).should('have.length', PRODUCTS_PER_PAGE);
+      cy.get(firstProductItemSelector).within(() => {
         cy.get('a.cx-product-name').should('be.visible');
       });
     });
   });
 
   describe('Pagination', () => {
+    const pageLinkSelector = '.page-item.active > .page-link';
+
     it('should navigate to the next page and display results', () => {
       cy.get('.page-item:last-of-type .page-link:first').click();
-      cy.get('.page-item.active > .page-link').should('contain', '2');
+      cy.get(pageLinkSelector).should('contain', '2');
     });
 
     it('should be able navigate to the specified page number and display results', () => {
       cy.get('.page-item:nth-child(4) .page-link:first').click();
-      cy.get('.page-item.active > .page-link').should('contain', '3');
+      cy.get(pageLinkSelector).should('contain', '3');
     });
 
     it('should navigate to the previous page and display results', () => {
       cy.get('.page-item:first-of-type .page-link:first').click();
-      cy.get('.page-item.active > .page-link').should('contain', '2');
+      cy.get(pageLinkSelector).should('contain', '2');
     });
   });
 
@@ -51,63 +53,64 @@ context('Product search', () => {
       cy.get(
         'cx-product-facet-navigation .cx-search-facet-checkbox:first'
       ).click();
-      cy.get('header h1').should('contain', '79 results for Chiba');
+      cy.get(resultsTitleSelector).should('contain', '79 results for Chiba');
     });
 
     it('should be able to clear active facet', () => {
       cy.get(
         'cx-product-facet-navigation .cx-search-facet-filter__pill .close:first'
       ).click();
-      cy.get('header h1').should('contain', 'results for camera');
+      cy.get(resultsTitleSelector).should('contain', 'results for camera');
     });
   });
 
   describe('Sorting', () => {
+    const sortingOptionSelector = 'cx-sorting .ng-select:first';
+    const firstProductPriceSelector = `${firstProductItemSelector} .cx-product-price`;
+    const firstProductNameSelector = `${firstProductItemSelector} a.cx-product-name`;
+
     before(() => {
       cy.visit('/');
       cy.get('cx-searchbox input').type('camera{enter}');
     });
 
     it('should be able to sort by lowest price', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Price (lowest first)');
-      cy.get(
-        'cx-product-list cx-product-list-item:first .cx-product-price'
-      ).should('contain', '$1.58');
+      cy.get(sortingOptionSelector).ngSelect('Price (lowest first)');
+      cy.get(firstProductPriceSelector).should('contain', '$1.58');
     });
 
     it('should be able to sort by highest price', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Price (highest first)');
-      cy.get(
-        'cx-product-list cx-product-list-item:first .cx-product-price'
-      ).should('contain', '$6,030.71');
+      cy.get(sortingOptionSelector).ngSelect('Price (highest first)');
+      cy.get(firstProductPriceSelector).should('contain', '$6,030.71');
     });
 
     it('should be able to sort by name ascending', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Name (ascending)');
-      cy.get(
-        'cx-product-list cx-product-list-item:first a.cx-product-name'
-      ).should('contain', '10.2 Megapixel D-SLR');
+      cy.get(sortingOptionSelector).ngSelect('Name (ascending)');
+      cy.get(firstProductNameSelector).should(
+        'contain',
+        '10.2 Megapixel D-SLR'
+      );
     });
 
     it('should be able to sort by name descending', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Name (descending)');
-      cy.get(
-        'cx-product-list cx-product-list-item:first a.cx-product-name'
-      ).should('contain', 'Wide Strap for EOS 450D');
+      cy.get(sortingOptionSelector).ngSelect('Name (descending)');
+      cy.get(firstProductNameSelector).should(
+        'contain',
+        'Wide Strap for EOS 450D'
+      );
     });
 
     it('should be able to sort by relevance', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Relevance');
-      cy.get(
-        'cx-product-list cx-product-list-item:first a.cx-product-name'
-      ).should('contain', 'CAMERA');
+      cy.get(sortingOptionSelector).ngSelect('Relevance');
+      cy.get(firstProductNameSelector).should('contain', 'CAMERA');
     });
 
     it('should be able to sort by top rated', () => {
-      cy.get('cx-sorting .ng-select:first').ngSelect('Top Rated');
-      cy.get(
-        'cx-product-list cx-product-list-item:first a.cx-product-name'
-      ).should('contain', 'QuickCam for Notebooks Pro');
+      cy.get(sortingOptionSelector).ngSelect('Top Rated');
+      cy.get(firstProductNameSelector).should(
+        'contain',
+        'QuickCam for Notebooks Pro'
+      );
     });
   });
 });
