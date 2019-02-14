5b18b7c44edb867268b50113b241fa0f7a553167
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-02-14 11:24:03 -0500

    GH-635: Fix e2e tests

--- projects/storefrontapp-e2e-cypress/cypress/integration/big-happy-path.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/big-happy-path.ts
@@ -42,7 +42,7 @@ context('Big happy path', () => {
   });
 
   it('should fill in address form', () => {
-    cy.get('.cx-shipping-address__title').should('contain', 'Shipping Address');
+    cy.get('.cx-shipping-address-title').should('contain', 'Shipping Address');
     cy.get('cx-order-summary .cx-summary-partials .cx-summary-row')
       .first()
       .find('.cx-summary-amount')
