21792f1724da722adf1bc9a17ce9c6aae146b3c2
DonMiguel <15113219+developpeurweb@users.noreply.github.com>
2019-02-15 15:46:33 -0500

    Feature/gh 634 (#1242)
    * GH-634 Remove SASS variables by transferring its values to the CSS properties
    
    * GH-634 Add CSS variables and set previous values as fallbacks
    
    * GH-634 Simplify CSS selector names and update HTML markup to reflect new selector names
    
    * GH-634 adjusting prettier format
    
    * GH-634: Fix e2e test
    
    * GH-634 adjusting class names for cards
    
    * GH-634: Fix unit test
    
    * GH-634: Fix e2e test
    
    * GH-634: e2e improvement

--- projects/storefrontapp-e2e-cypress/cypress/integration/big-happy-path.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/big-happy-path.e2e-spec.ts
@@ -67,17 +67,21 @@ context('Big happy path', () => {
   });
 
   it('should review and place order', () => {
-    cy.get('.cx-review__title').should('contain', 'Review');
-    cy.get('cx-review-submit .cx-review__summary-card__address').within(() => {
-      cy.getByText(user.fullName);
-      cy.getByText(user.address.line1);
-      cy.getByText(user.address.line2);
-    });
-    cy.get('cx-review-submit .cx-review__summary-card__shipping-method').within(
-      () => {
+    cy.get('.cx-review-title').should('contain', 'Review');
+    cy.get('.cx-review-summary-card')
+      .contains('cx-card', 'Ship To')
+      .find('.cx-card-body__container')
+      .within(() => {
+        cy.getByText(user.fullName);
+        cy.getByText(user.address.line1);
+        cy.getByText(user.address.line2);
+      });
+    cy.get('.cx-review-summary-card')
+      .contains('cx-card', 'Shipping Method')
+      .find('.cx-card-body__container')
+      .within(() => {
         cy.getByText('standard-gross');
-      }
-    );
+      });
     cy.get('cx-order-summary .cx-summary-total .cx-summary-amount').should(
       'contain',
       cart.total
