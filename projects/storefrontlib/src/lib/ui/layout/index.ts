e56a4b659f4dae5a683841f384d9070fed81579a
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-15 10:33:59 +0100

    Refactor exporting UI folder, introduce distributed index files

---
+++ projects/storefrontlib/src/lib/ui/layout/index.ts
@@ -0,0 +1,14 @@
+export * from './layout.module';
+export * from './main/main.module';
+
+export * from './config/layout-config';
+export * from './config/default-layout-config';
+export * from './breakpoint/breakpoint.service';
+
+// tmp: we remove those once we render them all with page layout component
+export * from './footer/footer.component';
+export * from './multi-step-checkout-page-layout/multi-step-checkout-page-layout.component';
+export * from './order-confirmation-page-layout/order-confirmation-page-layout.component';
+export * from './main/storefront.component';
+export * from './header/header.component';
+export * from './store-finder-page-layout/store-finder-page-layout.component';
