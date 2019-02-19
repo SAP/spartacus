6a171e1fc221259824b6a505d3479384c1c14d17
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-02-19 11:58:20 -0500

    Creating compodocs

--- ci-scripts/build-for-deploy.sh
+++ ci-scripts/build-for-deploy.sh
@@ -9,3 +9,9 @@ yarn build:core:lib
 echo "-----"
 echo "Building SPA app"
 ng build storefrontapp -c=ci --base-href "https://sap.github.io/cloud-commerce-spartacus-storefront/"
+
+echo "-----"
+echo "generating docs"
+yarn doc:generate
+cp -R documentation dist/storefrontapp/docs
+
