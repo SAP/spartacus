29e8263922a1c2d15d6e47151766c5b6744e6271
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-20 09:04:51 +0100

    fix e2e

--- projects/storefrontapp-e2e-cypress/cypress/integration/product-search-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/product-search-e2e-spec.ts
@@ -11,7 +11,7 @@ context('Product search', () => {
   describe('Search results', () => {
     it('should be able to search and get results', () => {
       cy.get('cx-searchbox input').type('camera{enter}');
-      cy.get(resultsTitleSelector).contains('results for camera');
+      cy.get(resultsTitleSelector).contains('results for "camera"');
       cy.get(productItemSelector).should('have.length', PRODUCTS_PER_PAGE);
       cy.get(firstProductItemSelector).within(() => {
         cy.get('a.cx-product-name').should('be.visible');
@@ -51,14 +51,14 @@ context('Product search', () => {
   describe('Facets', () => {
     it('should filter results using facet filtering', () => {
       cy.get('cx-product-facet-navigation .cx-facet-checkbox:first').click();
-      cy.get(resultsTitleSelector).should('contain', '79 results for Chiba');
+      cy.get(resultsTitleSelector).should('contain', '79 results for "Chiba"');
     });
 
     it('should be able to clear active facet', () => {
       cy.get(
         'cx-product-facet-navigation .cx-facet-filter-pill .close:first'
       ).click();
-      cy.get(resultsTitleSelector).should('contain', 'results for camera');
+      cy.get(resultsTitleSelector).should('contain', 'results for "camera"');
     });
   });
 
