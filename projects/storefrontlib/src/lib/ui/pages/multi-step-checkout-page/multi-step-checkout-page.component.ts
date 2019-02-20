b43f56087e0faf22457a9321b480ec61625ad12b
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-20 10:50:59 +0100

    drop obsolete hardcoded module

--- projects/storefrontlib/src/lib/ui/pages/multi-step-checkout-page/multi-step-checkout-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'cx-multi-step-checkout-page',
-  templateUrl: './multi-step-checkout-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class MultiStepCheckoutPageComponent {}
