bb57a72a36dfe03bc79e0fab45c8f9cee287211f
Weizheng Gao <weizheng.gao@sap.com>
2019-02-20 20:09:40 -0500

    feature/GH-1220: add more tests to reset-password-form.component.spec

--- projects/storefrontlib/src/lib/user/reset-password/reset-password-form/reset-password-form.component.spec.ts
+++ projects/storefrontlib/src/lib/user/reset-password/reset-password-form/reset-password-form.component.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule, AbstractControl } from '@angular/forms';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 import { UserService, RoutingService } from '@spartacus/core';
 
@@ -9,8 +11,9 @@ import { ResetPasswordFormComponent } from './reset-password-form.component';
 
 class MockUserService {
   isPasswordReset() {
-    return of(false);
+    return of();
   }
+  resetPassword() {}
 }
 
 const router = {
@@ -23,11 +26,22 @@ class MockRoutingService {
   getRouterState() {
     return of(router);
   }
+  go() {}
 }
 
-fdescribe('ResetPasswordFormComponent', () => {
+describe('ResetPasswordFormComponent', () => {
   let component: ResetPasswordFormComponent;
   let fixture: ComponentFixture<ResetPasswordFormComponent>;
+
+  let userService: MockUserService;
+  let routingService: MockRoutingService;
+
+  let form: DebugElement;
+  let password: AbstractControl;
+  let rePassword: AbstractControl;
+
+  const validPassword = 'test1234Test@';
+  const nonValidPassword = '12341234';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -44,8 +58,12 @@ fdescribe('ResetPasswordFormComponent', () => {
     fixture = TestBed.createComponent(ResetPasswordFormComponent);
     component = fixture.componentInstance;
 
-    component.ngOnInit();
-    fixture.detectChanges();
+    form = fixture.debugElement.query(By.css('form'));
+    password = component.form.controls['password'];
+    rePassword = component.form.controls['repassword'];
+
+    userService = TestBed.get(UserService);
+    routingService = TestBed.get(RoutingService);
   });
 
   it('should be created', () => {
@@ -55,5 +73,53 @@ fdescribe('ResetPasswordFormComponent', () => {
   it('should initialize the form property', () => {
     expect(component.form.controls['repassword'].value).toBe('');
     expect(component.form.controls['password'].value).toBe('');
+  });
+
+  it('should form be valid when password and repassword are set correctly', () => {
+    password.setValue(validPassword);
+    rePassword.setValue(validPassword);
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('should form be invalid when password not valid', () => {
+    password.setValue(nonValidPassword);
+    rePassword.setValue(nonValidPassword);
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should form be invalid when password and repassword are not match', () => {
+    password.setValue(validPassword);
+    rePassword.setValue(nonValidPassword);
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should call resetPassword() method on submit', () => {
+    const request = spyOn(component, 'resetPassword');
+    password.setValue(validPassword);
+    fixture.detectChanges();
+    form.triggerEventHandler('submit', null);
+    expect(request).toHaveBeenCalled();
+  });
+
+  it('should resetPassword on submit when form is valide', () => {
+    spyOn(userService, 'resetPassword');
+
+    password.setValue(validPassword);
+    rePassword.setValue(validPassword);
+    fixture.detectChanges();
+    form.triggerEventHandler('submit', null);
+
+    expect(userService.resetPassword).toHaveBeenCalledWith(
+      'test token',
+      validPassword
+    );
+    expect(component.submited).toBeTruthy();
+  });
+
+  it('should go to login page when password reset successfully', () => {
+    spyOn(routingService, 'go').and.stub();
+    spyOn(userService, 'isPasswordReset').and.returnValue(of(true));
+    component.ngOnInit();
+    expect(routingService.go).toHaveBeenCalledWith({ route: ['login'] });
   });
 });
