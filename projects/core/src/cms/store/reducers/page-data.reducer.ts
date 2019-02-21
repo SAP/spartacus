56657f200609a9d47d34fe91a0fe94026e5f2e97
Stan <dunqan@w9.pl>
2019-02-21 17:35:20 +0100

    Remove add page data action (#1373)
    Fix pageIndexReducer

--- projects/core/src/cms/store/reducers/page-data.reducer.ts
+++ projects/core/src/cms/store/reducers/page-data.reducer.ts
@@ -1,15 +1,17 @@
 import * as fromAction from '../actions';
 import { Page } from '../../model/page.model';
+import { EntityState } from '../../../state/utils/entity/entity-state';
 
-export const initialState: Page = undefined;
+export const initialState: EntityState<Page> = { entities: {} };
 
 export function reducer(
   state = initialState,
-  action: fromAction.AddPageDataSuccess
-): Page {
+  action: fromAction.LoadPageDataSuccess
+): EntityState<Page> {
   switch (action.type) {
-    case fromAction.ADD_PAGEDATA_SUCCESS: {
-      return action.payload;
+    case fromAction.LOAD_PAGE_DATA_SUCCESS: {
+      const page: Page = action.payload;
+      return {...state, entities: {...state.entities, [page.pageId]: page }};
     }
   }
   return state;
