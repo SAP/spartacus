99ee46a606e4189df066ca32f3908534bef2bd9f
Michał Gruca <pucek9@gmail.com>
2019-02-25 11:20:32 +0100

    updated selectors and finished last case

--- projects/storefrontapp-e2e-cypress/cypress/integration/product-details.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/product-details.e2e-spec.ts
@@ -1,4 +1,23 @@
 context('Product details', () => {
+  const productContainer = 'cx-product-details';
+  const summaryContainer = `${productContainer} cx-product-summary`;
+  const tabsContainer = `${productContainer} .details`;
+  const tabsHeaderList = `${tabsContainer} > h3`;
+  const activeTabContainer = `${tabsContainer} .active .container`;
+  const reviewContainer = 'cx-product-reviews';
+  const reviewList = `${reviewContainer} .review`;
+  const writeAReviewButton = `${reviewContainer} .header button`;
+  const writeAReviewForm = `${reviewContainer} form`;
+  const addToCartButton = `${summaryContainer} cx-add-to-cart button`;
+  const atcModal = `cx-added-to-cart-dialog`;
+  const atcModalTitle = `${atcModal} .cx-dialog-title`;
+  const atcModalItem = `${atcModal} cx-cart-item`;
+  const atcModalCloseButton = `${atcModal} [aria-label="Close"]`;
+  const header = `cx-header`;
+  const headerCartButton = `${header} cx-mini-cart`;
+  const itemCounter = 'cx-item-counter';
+  const itemCounterButtons = `${itemCounter} button`;
+
   const PRODUCT_NAME = 'Battery Video Light';
 
   before(() => {
@@ -6,34 +25,34 @@ context('Product details', () => {
   });
 
   it('should contain correct product details', () => {
-    cy.get('cx-product-summary .name').should('contain', PRODUCT_NAME);
-    cy.get('cx-product-details .code').should('contain', 'ID 266685');
-    cy.get('cx-product-details').should(
+    cy.get(`${summaryContainer} .name`).should('contain', PRODUCT_NAME);
+    cy.get(`${summaryContainer} .code`).should('contain', 'ID 266685');
+    cy.get(`${summaryContainer} .description`).should(
       'contain',
       '20-watt video light compatible with InfoLIYHIUM M-series batteries.'
     );
   });
 
   it('should contain correct tabs', () => {
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(0)
       .should('contain', 'Product Details');
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(1)
       .should('contain', 'Specs');
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(2)
       .should('contain', 'Reviews');
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(3)
       .should('contain', 'Shipping');
   });
 
   it('should contain tabs with correct text', () => {
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(0)
       .click();
-    cy.get('.details .active .container')
+    cy.get(activeTabContainer)
       .should(
         'contain',
         '20-watt video light compatible with InfoLIYHIUM M-series batteries.'
@@ -43,96 +62,82 @@ context('Product details', () => {
         'Can be switched to 10-watt or 20-watt settings (NP-FM50 batteries can only be used at 10-watt setting).'
       )
       .should('contain', 'Includes shoe adaptor for increased functionality.');
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(1)
       .click();
-    cy.get('.details .active .container')
+    cy.get(activeTabContainer)
       .should('contain', 'Weight & dimensions')
       .should('contain', 'Colour')
       .should('contain', 'Technical details');
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(2)
       .click();
-    cy.get('.details .active .container').should('contain', 'Overall Rating');
-    cy.get('.details > h3')
+    cy.get(activeTabContainer).should('contain', 'Overall Rating');
+    cy.get(tabsHeaderList)
       .eq(3)
       .click();
-    cy.get('.details .active .container').should(
-      'contain',
-      'Lorem ipsum dolor sit amet,'
-    );
+    cy.get(activeTabContainer).should('contain', 'Lorem ipsum dolor sit amet,');
   });
 
   it('should contain correct review tab', () => {
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(2)
       .click();
-    cy.get('cx-product-reviews .review').should('have.length', 5);
-    cy.get('cx-product-reviews .header button').should('be.visible');
-    cy.get('cx-product-reviews ngb-rating').should('be.visible');
+    cy.get(reviewList).should('have.length', 5);
+    cy.get(writeAReviewButton).should('be.visible');
+    cy.get(`${reviewContainer} ngb-rating`)
+      .eq(0)
+      .should('be.visible');
   });
 
   it('should contain correct review form', () => {
-    cy.get('.details > h3')
+    cy.get(tabsHeaderList)
       .eq(2)
       .click();
-    cy.get('cx-product-reviews .header button').click();
-    cy.get('cx-product-reviews form').should('be.visible');
-    cy.get('cx-product-reviews form')
+    cy.get(writeAReviewButton).click();
+    cy.get(writeAReviewForm).should('be.visible');
+    cy.get(writeAReviewForm)
       .getByText('Cancel')
       .should('be.not.disabled');
-    cy.get('cx-product-reviews form')
+    cy.get(writeAReviewForm)
       .getByText('Submit')
       .should('be.disabled');
-    cy.get('cx-product-reviews form input')
+    cy.get(`${writeAReviewForm} input`)
       .eq(0)
       .type('My review title');
-    cy.get('cx-product-reviews form textarea').type(
+    cy.get(`${writeAReviewForm} textarea`).type(
       'My best comment I have ever posted'
     );
-    cy.get('cx-product-reviews form .star')
+    cy.get(`${writeAReviewForm} .star`)
       .eq(2)
       .click();
-    cy.get('cx-product-reviews form input')
+    cy.get(`${writeAReviewForm} input`)
       .eq(1)
       .type('Me');
-    cy.get('cx-product-reviews form')
+    cy.get(writeAReviewForm)
       .getByText('Submit')
       .should('be.not.disabled');
-    cy.get('cx-product-reviews form')
+    cy.get(writeAReviewForm)
       .getByText('Submit')
       .click();
-    cy.get('cx-product-reviews form').should('be.not.visible');
-    cy.get('cx-product-reviews .review').should('be.visible');
+    cy.get(writeAReviewForm).should('be.not.visible');
+    cy.get(reviewList).should('be.visible');
   });
 
   it('should be able to add different quantities of product to cart', () => {
-    // TODO
-    // await productDetailsPage.addToCartButton.click();
-    // // Added-to-Cart modal opens. Close it.
-    // const atcModal = new AddedToCartModal();
-    // await atcModal.waitForReady();
-    //
-    // const item = await atcModal.item;
-    // await E2EUtil.wait4VisibleElement(item);
-    // expect(await item.getText()).toContain(PRODUCT_NAME);
-    //
-    // // close modal
-    // await atcModal.closeButton.click();
-    //
-    // // There should be 1 item in cart
-    // expect(await header.miniCartButton.getText()).toBe('1');
-    //
-    // // Let's change qty 3 times (from 1 to 4)
-    // for (let i = 0; i <= 2; i++) {
-    //   await productDetailsPage.itemCounterUpButton.click();
-    // }
-    // await productDetailsPage.addToCartButton.click();
-    // await atcModal.waitForReady();
-    // // close modal
-    // await atcModal.closeButton.click();
-    //
-    // // There should be 5 items in cart
-    // expect(await header.miniCartButton.getText()).toBe('5');
+    cy.get(addToCartButton).click();
+    cy.get(atcModal).should('be.visible');
+    cy.get(atcModalTitle).should('contain', 'Item(s) added to your cart');
+    cy.get(`${atcModalItem} .cx-name`).should('contain', PRODUCT_NAME);
+    cy.get(atcModalCloseButton).click();
+    cy.get(headerCartButton).should('contain', '1');
+    for (let i = 0; i <= 2; i++) {
+      cy.get(itemCounterButtons)
+        .getByText('+')
+        .click();
+    }
+    cy.get(addToCartButton).click();
+    cy.get(atcModalCloseButton).click();
+    cy.get(headerCartButton).should('contain', '5');
   });
 });
