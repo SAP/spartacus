301b7709bae2f9ac62fd5bddd24ced318873c678
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-25 16:04:13 +0100

    fix build

--- projects/storefrontlib/src/lib/seo/seo.module.ts
+++ projects/storefrontlib/src/lib/seo/seo.module.ts
@@ -2,10 +2,11 @@ import { NgModule, APP_INITIALIZER, Injector } from '@angular/core';
 import { SeoTitleService } from './seo-title.service';
 
 export function initSeoService(injector: Injector) {
-  return function() {
+  const result = () => {
     const service = injector.get(SeoTitleService);
     service.initPageTitle();
   };
+  return result;
 }
 
 @NgModule({
