da99aef20d999e8086358013e2ad96a9c8fff0de
dunqan <dunqan@w9.pl>
2019-02-26 10:26:49 +0100

    GH-846: Fix imports in content page module

--- projects/storefrontlib/src/lib/ui/pages/content-page/content-page.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/content-page/content-page.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PageLayoutModule } from '../../../cms/page-layout/page-layout.module';
-import { CmsPageGuards, PageLayoutComponent } from '../../../cms';
+import { CmsPageGuards } from '../../../cms/guards/cms-page.guard';
+import { PageLayoutComponent } from '../../../cms/page-layout/page-layout.component';
 
 const routes: Routes = [
   {
