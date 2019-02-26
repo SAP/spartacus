7150cc658805a8d2ee676745e96868f2ad81b39d
Weizheng Gao <weizheng.gao@sap.com>
2019-02-25 20:48:36 -0500

    feature/GH-1127: finish payment-methods-e2e-spec

--- projects/storefrontapp-e2e-cypress/cypress/integration/payment-methods-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/payment-methods-e2e-spec.ts
@@ -3,6 +3,7 @@ import {
   fillShippingAddress,
   fillPaymentDetails
 } from '../helpers/checkout-forms';
+import { standardUser } from '../sample-data/shared-users';
 
 describe('Payment Methods', () => {
   before(() => {
@@ -16,12 +17,18 @@ describe('Payment Methods', () => {
 
   describe('should go to payment details page for login user', () => {
     before(() => {
-      cy.requireLoggedIn();
+      cy.requireLoggedIn(standardUser);
+    });
+
+    it('should see spinner when loading', () => {
+      cy.visit('/my-account/payment-details');
+
+      cy.get('cx-payment-methods .cx-body').then(() =>
+        cy.get('cx-spinner').should('exist')
+      );
     });
 
     it('should see title and some messages', () => {
-      cy.visit('/my-account/payment-details');
-
       cy.get('cx-payment-methods').within(() => {
         cy.get('.cx-payment .cx-header').should('contain', 'Payment methods');
         cy.get('.cx-payment .cx-body').should(
@@ -31,29 +38,49 @@ describe('Payment Methods', () => {
       });
     });
 
-    it('should see spinner when loading', () => {});
-
     it('should see payment method card', () => {
-      // go to product page
-      const productId = '3595723';
-      cy.visit(`/product/${productId}`);
+      cy.get('.cx-payment .cx-body').then($body => {
+        // if there is no payment method
+        if (!$body.find('cx-card').length) {
+          // go to product page
+          const productId = '3595723';
+          cy.visit(`/product/${productId}`);
 
-      // add product to cart and go to checkout
-      cy.get('cx-product-summary cx-add-to-cart button').click();
-      cy.get('cx-added-to-cart-dialog').within(() => {
-        cy.getByText(/proceed to checkout/i).click();
-      });
+          // add product to cart and go to checkout
+          cy.get('cx-product-summary cx-add-to-cart button').click();
+          cy.get('cx-added-to-cart-dialog').within(() => {
+            cy.getByText(/proceed to checkout/i).click();
+          });
 
-      // fill in shipping address and payment methods
-      fillShippingAddress(user);
-      cy.get('#deliveryMode-standard-gross').check();
-      cy.get('button.btn-primary').click();
-      fillPaymentDetails(user);
+          // go to shipping address
+          cy.get('.cx-shipping-address').then($addressBody => {
+            // after loading
+            cy.get('.cx-shipping-address-spinner')
+              .should('not.exist')
+              .then(() => {
+                // address exist, go to next step; otherwise fill in
+                if ($addressBody.find('cx-card').length) {
+                  cy.get('.card-link').click();
+                  cy.get('button.btn-primary').click();
+                } else {
+                  fillShippingAddress(user);
+                }
+              });
+          });
 
-      // go to payment details page
-      cy.get('cx-review-submit', { timeout: 50000 }).then(() => {
-        cy.visit('/my-account/payment-details');
-        cy.get('cx-card').should('exist');
+          // set delivery method
+          cy.get('#deliveryMode-standard-gross').check();
+          cy.get('button.btn-primary').click();
+
+          // fill in payment method
+          fillPaymentDetails(user);
+
+          // go to payment details page
+          cy.get('cx-review-submit', { timeout: 50000 }).then(() => {
+            cy.visit('/my-account/payment-details');
+            cy.get('cx-card').should('exist');
+          });
+        }
       });
     });
   });
