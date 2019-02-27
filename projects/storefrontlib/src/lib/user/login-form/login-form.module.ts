5c1b057e31f0c59830469492e4ac03d9df20a8a2
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-27 09:07:44 +0100

    Move login-form to a separate module

--- projects/storefrontlib/src/lib/user/login-form/login-form.module.ts
+++ projects/storefrontlib/src/lib/user/login-form/login-form.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login.component';
+import { LoginFormComponent } from './login-form.component';
 import { UserModule, UrlTranslationModule } from '@spartacus/core';
 import { CmsModule } from '../../cms/cms.module';
 import { BootstrapModule } from '../../bootstrap.module';
@@ -9,13 +10,16 @@ import { BootstrapModule } from '../../bootstrap.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule,
     CmsModule,
     BootstrapModule,
     UserModule,
     UrlTranslationModule
   ],
-  declarations: [LoginComponent],
-  exports: [LoginComponent]
+  declarations: [LoginFormComponent],
+  exports: [LoginFormComponent],
+  entryComponents: [LoginFormComponent]
 })
-export class LoginModule {}
+export class LoginFormModule {}
