28e3f6d7a6578f32196ba974426ee15f4ace2bdd
Weizheng Gao <weizheng.gao@sap.com>
2019-02-27 11:37:49 -0500

    feature/GH-1127

--- projects/storefrontapp-e2e-cypress/cypress/integration/payment-methods-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/payment-methods-e2e-spec.ts
@@ -3,7 +3,6 @@ import {
   fillShippingAddress,
   fillPaymentDetails
 } from '../helpers/checkout-forms';
-import { standardUser } from '../sample-data/shared-users';
 
 describe('Payment Methods', () => {
   before(() => {
@@ -17,7 +16,7 @@ describe('Payment Methods', () => {
 
   describe('should go to payment details page for login user', () => {
     before(() => {
-      cy.requireLoggedIn(standardUser);
+      cy.requireLoggedIn();
     });
 
     it('should see spinner when loading', () => {
@@ -39,48 +38,60 @@ describe('Payment Methods', () => {
     });
 
     it('should see payment method card', () => {
-      cy.get('.cx-payment .cx-body').then($body => {
-        // if there is no payment method
-        if (!$body.find('cx-card').length) {
-          // go to product page
-          const productId = '3595723';
-          cy.visit(`/product/${productId}`);
+      // go to product page
+      const productId = '3595723';
+      cy.visit(`/product/${productId}`);
 
-          // add product to cart and go to checkout
-          cy.get('cx-product-summary cx-add-to-cart button').click();
-          cy.get('cx-added-to-cart-dialog').within(() => {
-            cy.getByText(/proceed to checkout/i).click();
-          });
+      // add product to cart and go to checkout
+      cy.get('cx-product-summary cx-add-to-cart button').click();
+      cy.get('cx-added-to-cart-dialog').within(() => {
+        cy.getByText(/proceed to checkout/i).click();
+      });
 
-          // go to shipping address
-          cy.get('.cx-shipping-address').then($addressBody => {
-            // after loading
-            cy.get('.cx-shipping-address-spinner')
-              .should('not.exist')
-              .then(() => {
-                // address exist, go to next step; otherwise fill in
-                if ($addressBody.find('cx-card').length) {
-                  cy.get('.card-link').click();
-                  cy.get('button.btn-primary').click();
-                } else {
-                  fillShippingAddress(user);
-                }
-              });
-          });
+      // go to shipping address
+      fillShippingAddress(user);
 
-          // set delivery method
-          cy.get('#deliveryMode-standard-gross').check();
-          cy.get('button.btn-primary').click();
+      // set delivery method
+      cy.get('#deliveryMode-standard-gross').check();
+      cy.get('button.btn-primary').click();
 
-          // fill in payment method
-          fillPaymentDetails(user);
+      // fill in payment method
+      fillPaymentDetails(user);
 
-          // go to payment details page
-          cy.get('cx-review-submit', { timeout: 50000 }).then(() => {
-            cy.visit('/my-account/payment-details');
-            cy.get('cx-card').should('exist');
-          });
-        }
+      // go to payment details page
+      cy.get('cx-review-submit', { timeout: 50000 });
+
+      cy.visit('/my-account/payment-details');
+      cy.get('.cx-payment .cx-body').then(() => {
+        cy.get('cx-card').should('exist');
+      });
+    });
+
+    it('should be able to delete the payment', () => {
+      cy.get('.card-link').should('have.text', 'Delete');
+      cy.get('.card-link').click();
+
+      // should see confirmation message
+      cy.get('cx-card').should(
+        'contain',
+        'Are you sure you want to delete this payment method?'
+      );
+
+      // click cancel
+      cy.get('.btn-secondary').should('contain', 'cancel');
+      cy.get('.btn-secondary').click();
+      cy.get('cx-card').should(
+        'not.contain',
+        'Are you sure you want to delete this payment method?'
+      );
+      cy.get('.card-link').should('exist');
+
+      // delete the payment
+      cy.get('.card-link').click();
+      cy.get('.btn-primary').should('contain', 'delete');
+      cy.get('.btn-primary').click();
+      cy.get('.cx-payment .cx-body').then(() => {
+        cy.get('cx-card').should('not.exist');
       });
     });
   });
