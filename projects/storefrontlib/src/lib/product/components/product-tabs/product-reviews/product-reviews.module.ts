82305d5f339f072715cf7c098f810190fc38d441
Michał Gruca <pucek9@gmail.com>
2019-02-28 16:35:25 +0100

    feat(@spartacus/storefront): refactor product detail page components to reflect the cms components (#1290)
    Closes #1106

---
+++ projects/storefrontlib/src/lib/product/components/product-tabs/product-reviews/product-reviews.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ProductReviewsComponent } from './product-reviews.component';
+import { FormComponentsModule } from './../../../../ui/components/form-components/form-components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FormComponentsModule
+  ],
+  declarations: [ProductReviewsComponent],
+  exports: [ProductReviewsComponent]
+})
+export class ProductReviewsModule {}
