6ee2032f35fa82419cc74260284664c0baea0e42
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-03-06 11:32:33 -0500

    fix production build

--- projects/core/src/global-message/http-interceptors/index.ts
+++ projects/core/src/global-message/http-interceptors/index.ts
@@ -2,43 +2,49 @@ import { Provider } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HttpErrorInterceptor } from './http-error.interceptor';
-import { HttpErrorHandler } from './handlers';
-import * as handlers from './handlers';
+import { HttpErrorHandler } from './handlers/http-error.handler';
+import { UnknownErrorHandler } from './handlers/unknown-error.handler';
+import { BadGatewayHandler } from './handlers/bad-gateway.handler';
+import { BadRequestHandler } from './handlers/bad-request.handler';
+import { ConflictHandler } from './handlers/conflict.handler';
+import { ForbiddenHandler } from './handlers/forbidden.handler';
+import { GatewayTimeoutHandler } from './handlers/gateway-timeout.handler';
+import { NotFoundHandler } from './handlers/not-found.handler';
 
 export const errorHandlers: Provider[] = [
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.UnknownErrorHandler,
+    useExisting: UnknownErrorHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.BadGatewayHandler,
+    useExisting: BadGatewayHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.BadRequestHandler,
+    useExisting: BadRequestHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.ConflictHandler,
+    useExisting: ConflictHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.ForbiddenHandler,
+    useExisting: ForbiddenHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.GatewayTimeoutHandler,
+    useExisting: GatewayTimeoutHandler,
     multi: true
   },
   {
     provide: HttpErrorHandler,
-    useExisting: handlers.NotFoundHandler,
+    useExisting: NotFoundHandler,
     multi: true
   }
 ];
