c79012d0d78abff74c71013020de02e7b8e4b672
Michał Gruca <pucek9@gmail.com>
2019-03-08 13:53:47 +0100

    refactor done

--- projects/storefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/pages.module.ts
@@ -11,7 +11,6 @@ import { StoreFinderPageModule } from './store-finder-page/store-finder-page.mod
 import { ResetNewPasswordPageModule } from './reset-new-password-page/reset-new-password-page.module';
 
 // ContentPage: my Account Pages
-import { PaymentDetailsPageModule } from './myaccount/payment-details-page/payment-details-page.module';
 import { OrderDetailsPageModule } from './myaccount/order-details-page/order-details-page.module';
 
 // CategoryPage
@@ -36,7 +35,6 @@ const pageModules = [
   OrderConfirmationPageModule,
   ProductPageModule,
   RegisterPageModule,
-  PaymentDetailsPageModule,
   ResetPasswordPageModule,
   StoreFinderPageModule,
   ResetNewPasswordPageModule,
@@ -93,6 +91,12 @@ const pageModules = [
           'Open-Catalogue/:category1/:category2/:category3/:category4/p/:productCode',
         redirectTo: null,
         data: { cxRedirectTo: 'product' }
+      },
+      {
+        path: null,
+        canActivate: [AuthGuard, CmsPageGuards],
+        data: { pageLabel: 'payment-details', cxPath: 'paymentManagement' },
+        component: PageLayoutComponent
       },
       // PLEASE ADD ALL ROUTES ABOVE THIS LINE ===============================
       {
