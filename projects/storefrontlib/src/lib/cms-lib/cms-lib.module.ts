ef52869d86c0156ee8d3b325ff2c86d1363d9f86
Michał Gruca <pucek9@gmail.com>
2019-03-08 17:40:19 +0100

    feat(@spartacus/storefront): refactor order-detail component to be CMS-driven (#1597)

--- projects/storefrontlib/src/lib/cms-lib/cms-lib.module.ts
+++ projects/storefrontlib/src/lib/cms-lib/cms-lib.module.ts
@@ -17,6 +17,7 @@ import { OrderHistoryModule } from '../my-account/order/order-history/order-hist
 import { ProductTabsModule } from '../product/components/product-tabs/product-tabs.module';
 import { CartDetailsModule } from '../cart/cart-details/cart-details.module';
 import { CartTotalsModule } from '../cart/cart-totals/cart-totals.module';
+import { OrderDetailsModule } from '../my-account/order/order-details/order-details.module';
 import { PaymentMethodsModule } from '../my-account/payment-methods/payment-methods.module';
 // import { ProductReferencesModule } from './product-references/product-references.module';
 // import { TabParagraphContainerModule } from './tab-paragraph-container/tab-paragraph-container.module';
@@ -43,6 +44,7 @@ import { PaymentMethodsModule } from '../my-account/payment-methods/payment-meth
     ProductTabsModule,
     CartDetailsModule,
     CartTotalsModule,
+    OrderDetailsModule,
     PaymentMethodsModule
   ]
 })
