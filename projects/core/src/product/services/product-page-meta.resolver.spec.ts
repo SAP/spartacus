c13a6c4f5fa7fcbf392266b8d6648c38fc53ecc1
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-03-08 17:38:55 +0100

    fix tslint

--- projects/core/src/product/services/product-page-meta.resolver.spec.ts
+++ projects/core/src/product/services/product-page-meta.resolver.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { PageType, Product } from '../../occ/occ-models/occ.models';
+import { PageType } from '../../occ/occ-models/occ.models';
 import { Observable, of } from 'rxjs';
 import {
   Page,
