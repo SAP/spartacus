72c1779c46d94752180bef214db76036aef14f72
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-03-08 12:57:02 +0100

    resolve the product image and use it in the seo meta service to build the right metatag

--- projects/storefrontlib/src/cms-structure/seo/seo-meta.service.ts
+++ projects/storefrontlib/src/cms-structure/seo/seo-meta.service.ts
@@ -23,6 +23,7 @@ export class SeoMetaService {
   protected set meta(meta: PageMeta) {
     this.title = meta.title;
     this.description = meta.description;
+    this.image = meta.image;
     this.robots = meta.robots || [PageRobotsMeta.INDEX, PageRobotsMeta.FOLLOW];
   }
 
@@ -34,8 +35,16 @@ export class SeoMetaService {
     this.addTag({ name: 'description', content: value });
   }
 
+  protected set image(imageUrl: string) {
+    if (imageUrl) {
+      this.addTag({ name: 'og:image', content: imageUrl });
+    }
+  }
+
   protected set robots(value: PageRobotsMeta[]) {
-    this.addTag({ name: 'robots', content: value.join(', ') });
+    if (value) {
+      this.addTag({ name: 'robots', content: value.join(', ') });
+    }
   }
 
   protected addTag(meta: MetaDefinition) {
