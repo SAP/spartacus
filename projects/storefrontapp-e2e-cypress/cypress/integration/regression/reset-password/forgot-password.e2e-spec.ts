f2ee1523f652e1f33c5820e5ae9336447564d0f5
Patrick Labadie <patrick.labadie@sap.com>
2019-03-12 14:45:06 -0400

    GH-1295: Added the forgot password test with proper assertions.

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/reset-password/forgot-password.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/reset-password/forgot-password.e2e-spec.ts
@@ -3,14 +3,17 @@ context('Forgot Password Page', () => {
     cy.visit('/forgot-password');
   });
 
-  it('should display title', () => {
-    cy.title().should('not.be.empty');
-  });
-
-  it('should submit email', () => {
+  it('should request password reset email', () => {
     cy.get('cx-forgot-password form').within(() => {
       cy.get('[formcontrolname="userEmail"]').type('test@test.com');
       cy.get('button[type="submit"]').click();
     });
+    // After requesting a reset password email, we should be taken back to the login page
+    cy.url().should('match', /\/login/);
+    // ... and display a success message.
+    cy.get('cx-global-message .alert-info').should(
+      'contain',
+      'An email has been sent to you with information on how to reset your password.'
+    );
   });
 });
