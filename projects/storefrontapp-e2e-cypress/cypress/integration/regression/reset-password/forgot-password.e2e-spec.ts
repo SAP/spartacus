cc93a4d16ae53dc90033fbe74be0b78993831edf
Patrick Labadie <patrick.labadie@sap.com>
2019-03-13 12:42:11 -0400

    GH-1295: Added code review imporvements.

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/reset-password/forgot-password.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/reset-password/forgot-password.e2e-spec.ts
@@ -1,9 +1,11 @@
 context('Forgot Password Page', () => {
-  before(() => {
+  beforeEach(() => {
+    // Clear the session to make sure no user is athenticated.
+    cy.window().then(win => win.sessionStorage.clear());
     cy.visit('/forgot-password');
   });
 
-  it('should request password reset email', () => {
+  it('should request password reset email on submit', () => {
     cy.get('cx-forgot-password form').within(() => {
       cy.get('[formcontrolname="userEmail"]').type(getRandomEmailAddress());
       cy.get('button[type="submit"]').click();
@@ -15,6 +17,24 @@ context('Forgot Password Page', () => {
       'contain',
       'An email has been sent to you with information on how to reset your password.'
     );
+  });
+
+  it('should not submit an invalid form', () => {
+    cy.get('cx-forgot-password form').within(() => {
+      cy.get('button[type="submit"]').click();
+    });
+    // Submitting an invalid form should not move on to the next page.
+    // Form validations are covered by unit tests.
+    cy.url().should('match', /\/forgot-password/);
+  });
+
+  it('should go back to the login page on cancel.', () => {
+    // Click 'Cancel'
+    cy.get('cx-forgot-password a')
+      .contains('Cancel')
+      .click();
+    // After requesting a reset password email, we should be taken back to the login page
+    cy.url().should('match', /\/login/);
   });
 
   function getRandomEmailAddress() {
