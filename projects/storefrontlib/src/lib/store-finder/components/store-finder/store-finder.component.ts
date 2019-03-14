56c2e8cc530ff9118f766332b8074af8c1baa2f3
dunqan <dunqan@w9.pl>
2019-03-14 07:07:47 +0100

    Create storefinder component

---
+++ projects/storefrontlib/src/lib/store-finder/components/store-finder/store-finder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cx-store-finder',
+  templateUrl: './store-finder.component.html',
+  styleUrls: ['./store-finder.component.css']
+})
+export class StoreFinderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
