31ef6f2471106a114f9209deb6233dd2d4c12cbc
dunqan <dunqan@w9.pl>
2019-03-14 07:12:23 +0100

    Fix formatting

--- projects/storefrontlib/src/lib/store-finder/components/store-finder/store-finder.component.spec.ts
+++ projects/storefrontlib/src/lib/store-finder/components/store-finder/store-finder.component.spec.ts
@@ -8,9 +8,8 @@ describe('StoreFinderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StoreFinderComponent ]
-    })
-    .compileComponents();
+      declarations: [StoreFinderComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
