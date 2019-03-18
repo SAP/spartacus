cf89d7b5e78788ed5711d19e8a2a6e05954f4ed4
Krzysztof Platis <platonn.git@gmail.com>
2019-03-18 21:43:44 +0100

    add example for plurals

--- projects/storefrontlib/src/translations/en/common.en.ts
+++ projects/storefrontlib/src/translations/en/common.en.ts
@@ -8,6 +8,10 @@ export default {
       http: {
         badGateway: 'A server error occurred. Please try again later.'
       }
+    },
+    search: {
+      numberOfResults: '{{count}} result for {{query}}',
+      numberOfResults_plural: '{{count}} results for {{query}}'
     }
   }
 };
