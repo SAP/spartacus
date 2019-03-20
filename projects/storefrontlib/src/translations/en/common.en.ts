2314cbddf585b24f1f238bdaca1d45803606d583
Krzysztof Platis <platonn.git@gmail.com>
2019-03-20 11:21:53 +0100

    remove example translations

--- projects/storefrontlib/src/translations/en/common.en.ts
+++ projects/storefrontlib/src/translations/en/common.en.ts
@@ -1,17 +1,3 @@
 export default {
-  cx: {
-    user: {
-      greeting: 'Hi, {{name}}',
-      signInOrRegister: 'Sign In / Register'
-    },
-    messages: {
-      http: {
-        badGateway: 'A server error occurred. Please try again later.'
-      }
-    },
-    search: {
-      numberOfResults: '{{count}} result for {{query}}',
-      numberOfResults_plural: '{{count}} results for {{query}}'
-    }
-  }
+  // common translations should go here
 };
