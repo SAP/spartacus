9ab134d116d83e3d95f97305ba96bac8f4110dae
Krzysztof Platis <platonn.git@gmail.com>
2019-03-20 11:32:03 +0100

    add type for translations

--- projects/storefrontlib/src/translations/index.ts
+++ projects/storefrontlib/src/translations/index.ts
@@ -1,6 +1,12 @@
 import common from './en/common.en';
 
-export const translations = {
+export const translations: {
+  [lang: string]: {
+    [namespace: string]: {
+      [key: string]: any;
+    };
+  };
+} = {
   en: {
     common
   }
