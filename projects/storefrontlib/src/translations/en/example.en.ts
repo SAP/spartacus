702563df6f332995769f55c7905c21bfd806fcc1
Krzysztof Platis <platonn.git@gmail.com>
2019-03-22 15:21:42 +0100

    move example translations to separate file

---
+++ projects/storefrontlib/src/translations/en/example.en.ts
@@ -0,0 +1,7 @@
+export default {
+  greeting: 'Hi, {{name}}',
+  categoryPage: {
+    heading: '{{count}} result for {{query}}',
+    heading_plural: '{{count}} results for {{query}}'
+  }
+};
--- projects/storefrontlib/src/translations/index.ts
+++ projects/storefrontlib/src/translations/index.ts
@@ -1,16 +1,10 @@
 import common from './en/common.en';
+import example from './en/example.en';
 import { TranslationResources } from '@spartacus/core';
 
 export const translations: TranslationResources = {
   en: {
     common,
-
-    example: {
-      greeting: 'Hi, {{name}}',
-      categoryPage: {
-        heading: '{{count}} result for {{query}}',
-        heading_plural: '{{count}} results for {{query}}'
-      }
-    }
+    example
   }
 };
