933938afcdb3976d1c20593fa6813ee5be0054e2
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-03-25 14:01:40 -0400

    GH-1765 Adding parameter to run regression tests

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -2,6 +2,8 @@
 set -e
 set -o pipefail
 
+STAGE=$1
+
 yarn
 (cd projects/storefrontapp-e2e-cypress && yarn)
 
@@ -10,5 +12,9 @@ echo 'Building Spartacus libraries'
 yarn build:core:lib
 
 echo '-----'
-echo 'Running Cypress end to end tests'
-yarn e2e:cy:start-run-ci
+echo "Running Cypress end to end tests $STAGE"
+if [ $STAGE == 'regression' ]; then
+    yarn e2e:cy:start-run-regression-ci
+else
+    yarn e2e:cy:start-run-ci
+fi
