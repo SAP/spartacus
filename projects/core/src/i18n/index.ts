6934f765b8bd87e8fa94d20135dc0062ff3e87be
Krzysztof Platis <platonn.git@gmail.com>
2019-03-25 14:54:13 +0100

    feat(core): i18n testing module with mock translate pipe

--- projects/core/src/i18n/index.ts
+++ projects/core/src/i18n/index.ts
@@ -3,4 +3,6 @@ export * from './translate.pipe';
 export * from './translation.service';
 export * from './i18n.module';
 export * from './config/i18n-config';
-// DON'T EXPORT './i18next'
+export * from './i18next/index';
+
+export * from './testing/index';
