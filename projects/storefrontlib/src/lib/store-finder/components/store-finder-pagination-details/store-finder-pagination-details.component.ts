470c0c74d3bb4c4477e8203d221e8f93a90e7511
Mateusz Kolasa <mateuszkolasa@hotmail.com>
2019-03-26 14:10:07 +0100

    fix: storefinder search improvements

---
+++ projects/storefrontlib/src/lib/store-finder/components/store-finder-pagination-details/store-finder-pagination-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { PaginationModel } from '@spartacus/core';
+
+@Component({
+  selector: 'cx-store-finder-pagination-details',
+  templateUrl: './store-finder-pagination-details.component.html',
+  styleUrls: ['./store-finder-pagination-details.component.scss']
+})
+export class StoreFinderPaginationDetailsComponent {
+  @Input()
+  pagination: PaginationModel;
+
+  constructor() {}
+
+  getResultsPerPage(): string {
+    if (this.pagination.totalResults > this.pagination.pageSize) {
+      const firstItem =
+        this.pagination.currentPage * this.pagination.pageSize + 1;
+
+      let resultsPerPage =
+        (this.pagination.currentPage + 1) * this.pagination.pageSize;
+
+      if (resultsPerPage > this.pagination.totalResults) {
+        resultsPerPage = this.pagination.totalResults;
+      }
+
+      return `${firstItem} - ${resultsPerPage}`;
+    } else {
+      return `1 - ${this.pagination.totalResults}`;
+    }
+  }
+}
