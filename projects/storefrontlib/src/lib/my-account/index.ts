cf77ed915f34787af145dc56e46d8740f5efb674
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-03-26 16:20:04 -0400

    removed a module

--- projects/storefrontlib/src/lib/my-account/index.ts
+++ projects/storefrontlib/src/lib/my-account/index.ts
@@ -1,4 +1,3 @@
-export * from './my-account.module';
 export * from './order/order.module';
 export * from './order/order-details/order-details.component';
 export * from './order/order-history/order-history.component';
