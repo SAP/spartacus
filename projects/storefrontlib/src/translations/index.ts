9f5575caed966406654deae07b7a5f3006f91ebb
Michał Gruca <pucek9@gmail.com>
2019-03-27 12:01:16 +0100

    added to cart component + updated index

--- projects/storefrontlib/src/translations/index.ts
+++ projects/storefrontlib/src/translations/index.ts
@@ -1,8 +1,54 @@
-import header from './en/header.en';
 import { TranslationResources } from '@spartacus/core';
+import addToCart from './en/add-to-cart.en';
+import address from './en/address.en';
+import addressBook from './en/address-book.en.en';
+import cart from './en/cart.en.en';
+import cartItems from './en/cart-items.en';
+import checkout from './en/checkout.en';
+import checkoutAddress from './en/checkout-address.en';
+import checkoutOrderConfirmation from './en/checkout-order-confirmation.en';
+import checkoutPayment from './en/checkout-payment.en';
+import checkoutReview from './en/checkout-review.en';
+import checkoutShipping from './en/checkout-shipping.en';
+import forgottenPassword from './en/forgotten-password.en';
+import header from './en/header.en';
+import login from './en/header.en';
+import orderCost from './en/order-cost.en';
+import orderDetails from './en/order-details.en';
+import orderHistory from './en/order-history.en';
+import orderReview from './en/order-review.en';
+import paymentMethods from './en/payment-methods.en';
+import productDetails from './en/product-details.en';
+import productList from './en/product-list.en';
+import productReview from './en/product-review.en';
+import register from './en/register.en';
+import storeFinder from './en/store-finder.en';
 
 export const translations: TranslationResources = {
   en: {
-    header
+    addToCart,
+    address,
+    addressBook,
+    cart,
+    cartItems,
+    checkout,
+    checkoutAddress,
+    checkoutOrderConfirmation,
+    checkoutPayment,
+    checkoutReview,
+    checkoutShipping,
+    forgottenPassword,
+    header,
+    login,
+    orderCost,
+    orderDetails,
+    orderHistory,
+    orderReview,
+    paymentMethods,
+    productDetails,
+    productList,
+    productReview,
+    register,
+    storeFinder
   }
 };
