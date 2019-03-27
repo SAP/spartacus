de0926e5b42144e6b0e27a280ac5803a4354d043
Nikolay Orlov <norlov@lineate.com>
2019-03-27 12:28:49 +0600

    some more fixes according to the review notes

--- projects/vendor/public_api.ts
+++ projects/vendor/public_api.ts
@@ -1,8 +1,1 @@
-/*
- * Public API Surface of vendor
- */
-
-export * from './src/yotpo/yotpo.module';
-export * from './src/yotpo/yotpoconfig/yotpo-config';
-export * from './src/yotpo/yotporeview/yotpo-review.component';
-export * from './src/yotpo/yotpostarrating/yotpo-star-rating.component';
+export * from './src/yotpo/index';
