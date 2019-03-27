3e16f96fec6823d129a2e530346a4285d539d88c
Krzysztof Platis <platonn.git@gmail.com>
2019-03-27 15:05:14 +0100

    fix prod build of core

--- projects/core/src/i18n/date.pipe.ts
+++ projects/core/src/i18n/date.pipe.ts
@@ -2,11 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe as NativeDatePipe } from '@angular/common';
 import { getLocaleId } from '@angular/common';
 import { LanguageService } from '../site-context/facade/language.service';
-import { ServerConfig } from '../config';
+import { I18nConfig } from './config/i18n-config';
 
 @Pipe({ name: 'cxDate' })
 export class DatePipe extends NativeDatePipe implements PipeTransform {
-  constructor(private language: LanguageService, private config: ServerConfig) {
+  constructor(private language: LanguageService, private config: I18nConfig) {
     super(null);
   }
 
