fa56f7c7f669a8ac4b5e7b8d2a45ec5e55f0ec28
Nikolay Orlov <norlov@lineate.com>
2019-03-29 00:50:08 +0600

    dumb but fixed tests

--- projects/vendor/src/yotpo/service/yotpo.service.spec.ts
+++ projects/vendor/src/yotpo/service/yotpo.service.spec.ts
@@ -1,12 +1,66 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  Product,
+  ProductService,
+  RoutingService, PageType} from '@spartacus/core';
+import { Observable, of } from 'rxjs';
 
-import { YotpoService } from './baseyotpo.service';
+import { TestBed } from '@angular/core/testing';
+import { YotpoConfig } from '../yotpoconfig/yotpo-config';
+
+import { YotpoService } from './yotpo.service';
+
+
+class MockYotpoConfig extends YotpoConfig {
+  vendor = {
+    yotpo: {
+      appToken: 'abc'
+    }
+  };
+}
+
+const mockProduct: Product = { code: '123456' };
+
+class MockProductService {
+  get(): Observable<Product> {
+    return of(mockProduct);
+  }
+}
+
+const router = {
+  state: {
+    url: '/',
+    queryParams: {},
+    params: { productCode: '123456' },
+    context: { id: '1', type: PageType.PRODUCT_PAGE },
+    cmsRequired: false
+  }
+};
+
+class MockRoutingService {
+  getRouterState() {
+    return of(router);
+  }
+}
 
 describe('YotpoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: YotpoService;
+  beforeEach(() => { 
+      TestBed.configureTestingModule({ providers: [ YotpoService,
+        { provide: YotpoConfig, useClass: MockYotpoConfig }, 
+        {
+          provide: ProductService,
+          useClass: MockProductService
+        },
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService
+        }
+      ]});
+
+  service = TestBed.get(YotpoService);
+  });
 
   it('should be created', () => {
-    const service: YotpoService = TestBed.get(YotpoService);
     expect(service).toBeTruthy();
   });
 });
