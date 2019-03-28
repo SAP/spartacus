e900e206d2a7b6bf5399ca633dc0b4b7265b35cc
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-03-28 09:36:42 +0100

    I love simple

--- projects/storefrontlib/src/lib/user/login/login.component.ts
+++ projects/storefrontlib/src/lib/user/login/login.component.ts
@@ -1,39 +1,29 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { AuthService, UserToken, User, UserService } from '@spartacus/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService, User, UserService } from '@spartacus/core';
+import { switchMap, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  user$: Observable<User>;
+export class LoginComponent {
   isLogin = false;
-
-  subscription: Subscription;
 
   constructor(private auth: AuthService, private userService: UserService) {}
 
-  ngOnInit() {
-    this.user$ = this.userService.get();
-
-    this.subscription = this.auth
-      .getUserToken()
-      .subscribe((token: UserToken) => {
-        if (token && token.access_token && !this.isLogin) {
+  get user$(): Observable<User> {
+    return this.auth.getUserToken().pipe(
+      filter(data => data && !!data.access_token),
+      tap(token => {
+        if (!this.isLogin) {
           this.isLogin = true;
           this.userService.load(token.userId);
           this.auth.login();
-        } else if (token && !token.access_token && this.isLogin) {
-          this.isLogin = false;
         }
-      });
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+      }),
+      switchMap(() => this.userService.get())
+    );
   }
 }
