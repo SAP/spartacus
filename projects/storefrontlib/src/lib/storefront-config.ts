1b1bf6e3758ce84ac316a07017b14ca7c2f22f66
Nikolay Orlov <norlov@lineate.com>
2019-03-28 23:14:29 +0600

    vendor to @spartacus/vendor

--- projects/storefrontlib/src/lib/storefront-config.ts
+++ projects/storefrontlib/src/lib/storefront-config.ts
@@ -8,7 +8,7 @@ import {
 } from '@spartacus/core';
 import { PWAModuleConfig } from './pwa/pwa.module-config';
 import { LayoutConfig } from './ui/layout/config/layout-config';
-import { YotpoConfig } from 'vendor';
+import { YotpoConfig } from '@spartacus/vendor';
 
 export interface StorefrontModuleConfig
   extends AuthConfig,
