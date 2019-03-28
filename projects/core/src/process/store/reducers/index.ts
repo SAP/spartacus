6b7cfee03b0f36e36329b57291b0e9897d51faaf
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-03-28 14:12:55 -0400

    fixed the reducer

--- projects/core/src/process/store/reducers/index.ts
+++ projects/core/src/process/store/reducers/index.ts
@@ -1,13 +1,14 @@
 import { InjectionToken, Provider } from '@angular/core';
 
-import { ActionReducerMap, combineReducers } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap } from '@ngrx/store';
 
+import { entityLoaderReducer } from 'projects/core/src/state';
+
+import { PROCESS_FEATURE } from '../process-state';
 import { EntityLoaderState } from '../../../state/utils/entity-loader/entity-loader-state';
 
-export function getReducers<T>(): ActionReducerMap<EntityLoaderState<T>> {
-  return {
-    entities: combineReducers({})
-  };
+export function getReducers<T>(): ActionReducer<EntityLoaderState<T>> {
+  return entityLoaderReducer(PROCESS_FEATURE);
 }
 
 export const reducerToken: InjectionToken<
