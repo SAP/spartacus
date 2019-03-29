15bff92fe1a221aa63414b3f42fa578c076f8e63
Gilberto Alvarado <gil@hackergil.com>
2019-03-29 16:20:12 -0400

    test: Fixing e2e regression tests (#1765) (#1783) (#1821)

--- projects/storefrontapp-e2e-cypress/cypress/helpers/product-search-pricing-flow.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/product-search-pricing-flow.ts
@@ -1,6 +1,13 @@
 import { PRODUCT_LISTING } from './data-configuration';
 
 export function productPricingFlow() {
+  cy.server();
+  cy.route('GET', '/rest/v2/electronics-spa/products/search*').as('query');
+  cy.route(
+    'GET',
+    '/rest/v2/electronics-spa/products/search?fields=*&sort=price-asc*'
+  ).as('query_price_asc');
+
   // Click on a Category
   cy.get('header').within(() => {
     cy.get('.nav-link')
@@ -23,26 +30,29 @@ export function productPricingFlow() {
 
   cy.get('cx-product-list-item')
     .first()
-    .should('contain', 'DSC-S930');
+    .should('contain', 'DSC-WX1');
 
   // Navigate to next page
   cy.get('.page-item:last-of-type .page-link:first').click();
   cy.get('.page-item.active > .page-link').should('contain', '2');
 
+  cy.wait('@query');
+
   cy.get('cx-product-list-item:nth-child(1)').should(
     'contain',
-    'Cyber-shot W55'
+    'PowerShot A480'
   );
 
   // Sort by price low to high
   cy.get('cx-sorting .ng-select:first').ngSelect(
     PRODUCT_LISTING.SORTING_TYPES.BY_PRICE_ASC
   );
-  cy.get('.page-item.active > .page-link').should('contain', '2');
-  cy.get('cx-product-list-item').should('contain', 'DSC-W180');
 
-  // Add product to cart from search listing page
-  cy.get('cx-add-to-cart:first button').click({ force: true });
-  cy.get('.cx-dialog-header .close').click();
-  cy.get('cx-mini-cart .count').should('contain', '1');
+  cy.wait('@query_price_asc');
+
+  cy.get('.page-item.active > .page-link').should('contain', '2');
+  cy.get('cx-product-list-item:first .cx-product-name').should(
+    'contain',
+    'DSC-W180'
+  );
 }
