56b6d5ac607d0abda95d7755305ce098d57bf4f0
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-03-29 13:40:56 -0400

    cr: aligned process id

--- projects/core/src/process/store/selectors/process.selectors.spec.ts
+++ projects/core/src/process/store/selectors/process.selectors.spec.ts
@@ -6,7 +6,7 @@ import { PROCESS_FEATURE, StateWithProcess } from '../process-state';
 import * as fromReducers from '../reducers/index';
 import * as fromSelectors from './process.selectors';
 
-const MOCK_PROCESS_NAME = 'mock-process-name';
+const MOCK_PROCESS_ID = 'mock-process-id';
 
 describe('Process selectors', () => {
   let store: Store<StateWithProcess<void>>;
@@ -24,12 +24,12 @@ describe('Process selectors', () => {
   describe('getProcessStateFactory', () => {
     it('should return requested process slice of the state', () => {
       store.dispatch(
-        new fromActions.EntitySuccessAction(PROCESS_FEATURE, MOCK_PROCESS_NAME)
+        new fromActions.EntitySuccessAction(PROCESS_FEATURE, MOCK_PROCESS_ID)
       );
 
       let result: LoaderState<void>;
       store
-        .pipe(select(fromSelectors.getProcessStateFactory(MOCK_PROCESS_NAME)))
+        .pipe(select(fromSelectors.getProcessStateFactory(MOCK_PROCESS_ID)))
         .subscribe(value => (result = value))
         .unsubscribe();
 
@@ -45,12 +45,12 @@ describe('Process selectors', () => {
   describe('getProcessLoadingFactory', () => {
     it('should return loading flag', () => {
       store.dispatch(
-        new fromActions.EntityLoadAction(PROCESS_FEATURE, MOCK_PROCESS_NAME)
+        new fromActions.EntityLoadAction(PROCESS_FEATURE, MOCK_PROCESS_ID)
       );
 
       let result = false;
       store
-        .pipe(select(fromSelectors.getProcessLoadingFactory(MOCK_PROCESS_NAME)))
+        .pipe(select(fromSelectors.getProcessLoadingFactory(MOCK_PROCESS_ID)))
         .subscribe(value => (result = value))
         .unsubscribe();
 
@@ -61,12 +61,12 @@ describe('Process selectors', () => {
   describe('getProcessSuccessFactory', () => {
     it('should return success flag', () => {
       store.dispatch(
-        new fromActions.EntitySuccessAction(PROCESS_FEATURE, MOCK_PROCESS_NAME)
+        new fromActions.EntitySuccessAction(PROCESS_FEATURE, MOCK_PROCESS_ID)
       );
 
       let result = false;
       store
-        .pipe(select(fromSelectors.getProcessSuccessFactory(MOCK_PROCESS_NAME)))
+        .pipe(select(fromSelectors.getProcessSuccessFactory(MOCK_PROCESS_ID)))
         .subscribe(value => (result = value))
         .unsubscribe();
 
@@ -77,12 +77,12 @@ describe('Process selectors', () => {
   describe('getProcessErrorFactory', () => {
     it('should return success flag', () => {
       store.dispatch(
-        new fromActions.EntityFailAction(PROCESS_FEATURE, MOCK_PROCESS_NAME)
+        new fromActions.EntityFailAction(PROCESS_FEATURE, MOCK_PROCESS_ID)
       );
 
       let result = false;
       store
-        .pipe(select(fromSelectors.getProcessErrorFactory(MOCK_PROCESS_NAME)))
+        .pipe(select(fromSelectors.getProcessErrorFactory(MOCK_PROCESS_ID)))
         .subscribe(value => (result = value))
         .unsubscribe();
 
