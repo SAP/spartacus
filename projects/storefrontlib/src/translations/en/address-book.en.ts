30a7c7f890f5613b24b7ed83c7ac8b03091889e8
Michał Gruca <pucek9@gmail.com>
2019-04-01 17:17:28 +0200

    update

--- projects/storefrontlib/src/translations/en/address-book.en.ts
+++ projects/storefrontlib/src/translations/en/address-book.en.ts
@@ -1,6 +1,5 @@
 export default {
   labels: {
-    savedShippingAddress: 'Saved shipping addresses',
     addNewShippingAddress: 'Add a new shipping address',
     editShippingAddress: 'Edit shipping address',
     areYouSureToDeleteAddress: 'Are you sure you want to delete this address?',
