c7555440e5b6f8aa06a2c854b28e2cd582666627
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2019-04-01 11:56:53 +0200

    GH-1734: Add more tests

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/place-order/place-order.component.spec.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/place-order/place-order.component.spec.ts
@@ -1,12 +1,14 @@
-import { PlaceOrderComponent } from './place-order.component';
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { CheckoutService } from '@spartacus/core';
 import { Pipe, PipeTransform } from '@angular/core';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+
+import { PlaceOrderComponent } from './place-order.component';
 
 const checkoutServiceStub = {
   placeOrder(): void {}
-}
+};
 
 @Pipe({
   name: 'cxTranslateUrl'
@@ -22,13 +24,8 @@ fdescribe('PlaceOrderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [
-        MockTranslateUrlPipe,
-        PlaceOrderComponent
-      ],
-      providers: [
-        { provide: CheckoutService, useValue: checkoutServiceStub}
-      ]
+      declarations: [MockTranslateUrlPipe, PlaceOrderComponent],
+      providers: [{ provide: CheckoutService, useValue: checkoutServiceStub }]
     }).compileComponents();
   }));
 
@@ -41,9 +38,27 @@ fdescribe('PlaceOrderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should bla', () => {
-    spyOn(checkoutServiceStub, 'placeOrder');
-    expect(component).toBeTruthy();
-  });
+  describe('UI Place order button', () => {
+    const placeOrderChbx = () =>
+      fixture.debugElement.query(By.css('.form-check-input'));
 
+    const placeOrderBtn = () =>
+      fixture.debugElement.query(By.css('.btn-block'));
+
+    it('should be disabled by default', () => {
+      fixture.detectChanges();
+
+      expect(placeOrderChbx().nativeElement.checked).toBeFalsy();
+      expect(placeOrderBtn().nativeElement.disabled).toBeTruthy();
+    });
+
+    it('should be enabled when TandC checkbox is selected', () => {
+      placeOrderChbx().nativeElement.click();
+
+      fixture.detectChanges();
+
+      expect(placeOrderChbx().nativeElement.checked).toBeTruthy();
+      expect(placeOrderBtn().nativeElement.disabled).toBeFalsy();
+    });
+  });
 });
