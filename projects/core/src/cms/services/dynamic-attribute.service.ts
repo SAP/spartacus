9a5b214cdb5e3164d9c2a7456b6340adf5ee31ec
Weizheng Gao <weizheng.gao@sap.com>
2019-04-02 08:21:52 -0400

    feature/GH-1813

--- projects/core/src/cms/services/dynamic-attribute.service.ts
+++ projects/core/src/cms/services/dynamic-attribute.service.ts
@@ -7,7 +7,7 @@ export class DynamicAttributeService {
   /**
    * Add dynamic attributes to DOM. These attributes are extracted from the properties of cms items received from backend.
    * There can by many different groups of properties, one of them is smaredit. But EC allows addons to create different groups.
-   * For example, personalization may add `script` group etc.
+   * For example, personalization may add 'script' group etc.
    * @param properties: properties in each cms item response data
    * @param element: slot or cms component element
    * @param renderer
