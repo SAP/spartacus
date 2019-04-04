0323d0875d333f0d87328e575092e3ea287a576f
Patrick Labadie <patrick.labadie@sap.com>
2019-04-04 11:27:41 -0400

    GH-1163: Fix formatting issue.

--- projects/storefrontlib/src/lib/my-account/update-password/update-password.component.spec.ts
+++ projects/storefrontlib/src/lib/my-account/update-password/update-password.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UpdatePasswordComponent } from './update-password.component';
 
 describe('UpdatePasswordComponent', () => {
@@ -8,9 +7,8 @@ describe('UpdatePasswordComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UpdatePasswordComponent ]
-    })
-    .compileComponents();
+      declarations: [UpdatePasswordComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
