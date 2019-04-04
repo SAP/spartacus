36323ed8340255b14c2636caef88dab5dd75e22e
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-04-04 18:26:10 +0200

    expose expanded state to style rules

--- projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HamburgerMenuService } from 'projects/storefrontlib/src/layout/header/hamburger-menu/hamburger-menu.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'cx-storefront',
   templateUrl: './storefront.component.html',
   styleUrls: ['./storefront.component.scss'],
 })
 export class StorefrontComponent {
-  constructor() {}
+  constructor(private hamburgerMenuService: HamburgerMenuService) {}
+
+  get isExpanded(): Observable<boolean> {
+    return this.hamburgerMenuService.isExpanded;
+  }
 }
