fa71ab5e2f8456c283a84155c4cf22a86013b5b3
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-04-04 22:39:27 +0200

    Add tests for HamburgerMenuService

--- projects/storefrontlib/src/layout/header/hamburger-menu/hamburger-menu.service.spec.ts
+++ projects/storefrontlib/src/layout/header/hamburger-menu/hamburger-menu.service.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { NavigationStart, Router, RouterEvent } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { NavigationStart, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { HamburgerMenuService } from './hamburger-menu.service';
 
 class MockRouter {
-  events(): Observable<RouterEvent> {
-    return of({ id: 1, url: '' } as NavigationStart);
-  }
+  events = of({ id: 1, url: '' } as NavigationStart);
 }
 
 describe('HamburgerMenuService', () => {
@@ -31,5 +29,14 @@ describe('HamburgerMenuService', () => {
     service.isExpanded.subscribe(result => (expanded = result));
 
     expect(expanded).toBeTruthy();
+  });
+
+  it('should force expand state to false', () => {
+    let expanded: boolean;
+
+    service.toggle(true);
+    service.isExpanded.subscribe(result => (expanded = result));
+
+    expect(expanded).toBeFalsy();
   });
 });
