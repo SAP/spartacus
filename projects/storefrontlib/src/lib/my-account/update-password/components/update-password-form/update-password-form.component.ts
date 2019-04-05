48794aeef96754de26d21bc06d3a55d1e310c1b8
Patrick Labadie <patrick.labadie@sap.com>
2019-04-05 15:46:59 -0400

    GH-1163: Update validations & form submit logic.

--- projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.ts
+++ projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RoutingService, UserService } from '@spartacus/core';
 import { Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { CustomFormValidators } from '../../../../ui/validators/custom-form-validators';
-
+import { FormUtils } from '../../../../utils/forms/form-utils';
 @Component({
   selector: 'cx-update-password-form',
   templateUrl: './update-password-form.component.html',
@@ -18,9 +19,15 @@ import { CustomFormValidators } from '../../../../ui/validators/custom-form-vali
 export class UpdatePasswordFormComponent implements OnInit, OnDestroy {
   token: string;
   subscription = new Subscription();
-  submited = false;
+  submitClicked = false;
   userId: string;
   form: FormGroup;
+
+  @Output()
+  submited = new EventEmitter<{ oldPassword: string; newPassword: string }>();
+
+  @Output()
+  cancelled = new EventEmitter<void>();
 
   constructor(
     private fb: FormBuilder,
@@ -49,24 +56,37 @@ export class UpdatePasswordFormComponent implements OnInit, OnDestroy {
     );
   }
 
+  isNotValid(formControlName: string): boolean {
+    return FormUtils.isNotValidField(
+      this.form,
+      formControlName,
+      this.submitClicked
+    );
+  }
+
+  isPasswordConfirmNotValid(): boolean {
+    return (
+      this.form.hasError('NotEqual') &&
+      (this.submitClicked ||
+        (this.form.get('newPasswordConfirm').touched &&
+          this.form.get('newPasswordConfirm').dirty))
+    );
+  }
+
   ngOnDestroy() {}
 
-  updatePassword() {
-    console.log('UpdatePassword()', this.userId, this.form.value);
-    this.submited = true;
+  onSubmit(): void {
+    this.submitClicked = true;
     if (this.form.invalid) {
       return;
     }
 
-    this.userService.updatePassword(
-      this.userId,
-      this.form.value.oldPassword,
-      this.form.value.newPassword
-    );
-    this.routingService.go({ route: ['home'] });
+    this.submited.emit({
+      oldPassword: this.form.value.oldPassword,
+      newPassword: this.form.value.newPassword,
+    });
   }
-
-  private matchPassword(ac: AbstractControl) {
+  private matchPassword(ac: FormGroup) {
     if (ac.get('newPassword').value !== ac.get('newPasswordConfirm').value) {
       return { NotEqual: true };
     }
