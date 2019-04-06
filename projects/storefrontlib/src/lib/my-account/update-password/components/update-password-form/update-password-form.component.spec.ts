9e53e802b474583594ca6e241afcbd1c26514535
Patrick Labadie <patrick.labadie@sap.com>
2019-04-05 20:40:31 -0400

    GH-1163: Implement unit tests.

--- projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.spec.ts
+++ projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.spec.ts
@@ -5,7 +5,7 @@ import { By } from '@angular/platform-browser';
 import { FormUtils } from '../../../../utils/forms/form-utils';
 import { UpdatePasswordFormComponent } from './update-password-form.component';
 
-fdescribe('UpdatePasswordFormComponent', () => {
+describe('UpdatePasswordFormComponent', () => {
   let component: UpdatePasswordFormComponent;
   let fixture: ComponentFixture<UpdatePasswordFormComponent>;
   let el: DebugElement;
@@ -24,7 +24,9 @@ fdescribe('UpdatePasswordFormComponent', () => {
     fixture = TestBed.createComponent(UpdatePasswordFormComponent);
     component = fixture.componentInstance;
     el = fixture.debugElement;
+    component.ngOnInit();
     fixture.detectChanges();
+
     oldPassword = component.form.controls['oldPassword'];
     newPassword = component.form.controls['newPassword'];
     newPasswordConfirm = component.form.controls['newPasswordConfirm'];
@@ -48,34 +50,34 @@ fdescribe('UpdatePasswordFormComponent', () => {
   describe('Submiting the form ', () => {
     it('should call onSubmit() when submit button is clicked', () => {
       spyOn(component, 'onSubmit').and.stub();
-      fixture.detectChanges();
 
       const submitBtn = el.query(By.css('button[type="submit"]'));
       submitBtn.nativeElement.dispatchEvent(new MouseEvent('click'));
+
       expect(component.onSubmit).toHaveBeenCalled();
     });
 
     it('should NOT emit submited event if the form is not valid', () => {
       spyOn(component, 'onSubmit').and.stub();
       spyOn(component.submited, 'emit').and.stub();
-      component.ngOnInit();
+
       component.onSubmit();
+
+      expect(component.form.valid).toBeFalsy();
       expect(component.onSubmit).toHaveBeenCalled();
       expect(component.submited.emit).not.toHaveBeenCalled();
     });
-
     it('should emit submited event when the form is valid', () => {
-      spyOn(component, 'onSubmit').and.stub();
       spyOn(component.submited, 'emit').and.stub();
 
-      component.ngOnInit();
       oldPassword.setValue('oldPass123!');
       newPassword.setValue('newPass456!');
       newPasswordConfirm.setValue('newPass456!');
-
+      fixture.detectChanges();
       component.onSubmit();
-      expect(component.onSubmit).toHaveBeenCalled();
-      // expect(component.submited.emit).toHaveBeenCalled();
+
+      expect(component.form.valid).toBeTruthy();
+      expect(component.submited.emit).toHaveBeenCalled();
     });
   });
 });
