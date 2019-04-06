064aa7dc3acfe8e34d68ea3803d07f4124589881
Patrick Labadie <patrick.labadie@sap.com>
2019-04-06 00:04:31 -0400

    GH-1163: Implement unit tests.

--- projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.spec.ts
+++ projects/storefrontlib/src/lib/my-account/update-password/components/update-password-form/update-password-form.component.spec.ts
@@ -5,13 +5,15 @@ import { By } from '@angular/platform-browser';
 import { FormUtils } from '../../../../utils/forms/form-utils';
 import { UpdatePasswordFormComponent } from './update-password-form.component';
 
-describe('UpdatePasswordFormComponent', () => {
+fdescribe('UpdatePasswordFormComponent', () => {
   let component: UpdatePasswordFormComponent;
   let fixture: ComponentFixture<UpdatePasswordFormComponent>;
   let el: DebugElement;
   let oldPassword: AbstractControl;
   let newPassword: AbstractControl;
   let newPasswordConfirm: AbstractControl;
+  const validPassword = 'testPass123!';
+  // const invalidPassword = 'invalid';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -47,12 +49,11 @@ describe('UpdatePasswordFormComponent', () => {
     );
   });
 
-  describe('Submiting the form ', () => {
-    it('should call onSubmit() when submit button is clicked', () => {
+  describe('onSubmit() ', () => {
+    it('should be called when submit button is clicked', () => {
       spyOn(component, 'onSubmit').and.stub();
 
-      const submitBtn = el.query(By.css('button[type="submit"]'));
-      submitBtn.nativeElement.dispatchEvent(new MouseEvent('click'));
+      clickSubmit();
 
       expect(component.onSubmit).toHaveBeenCalled();
     });
@@ -70,9 +71,9 @@ describe('UpdatePasswordFormComponent', () => {
     it('should emit submited event when the form is valid', () => {
       spyOn(component.submited, 'emit').and.stub();
 
-      oldPassword.setValue('oldPass123!');
-      newPassword.setValue('newPass456!');
-      newPasswordConfirm.setValue('newPass456!');
+      oldPassword.setValue(validPassword);
+      newPassword.setValue(validPassword);
+      newPasswordConfirm.setValue(validPassword);
       fixture.detectChanges();
       component.onSubmit();
 
@@ -80,4 +81,75 @@ describe('UpdatePasswordFormComponent', () => {
       expect(component.submited.emit).toHaveBeenCalled();
     });
   });
+
+  describe('onCancel', () => {
+    it('should be called call when the Cancel button is clicked', () => {
+      spyOn(component, 'onCancel').and.stub();
+      const cancelBtn = el.query(By.css('button[type="button"]'));
+      cancelBtn.nativeElement.dispatchEvent(new MouseEvent('click'));
+      expect(component.onCancel).toHaveBeenCalled();
+    });
+
+    it('should emit cancelled event', () => {
+      spyOn(component.cancelled, 'emit').and.stub();
+      component.onCancel();
+      expect(component.cancelled.emit).toHaveBeenCalled();
+    });
+  });
+
+  describe('Error messages on form submit', () => {
+    it('should NOT display when displaying the form', () => {
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(formControlDisplaysError('oldPassword')).toBeFalsy();
+        expect(formControlDisplaysError('newPassword')).toBeFalsy();
+        expect(formControlDisplaysError('newPasswordConfirm')).toBeFalsy();
+      });
+    });
+    it('should display when submit an empty form', () => {
+      clickSubmit();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(formControlDisplaysError('oldPassword')).toBeTruthy();
+        expect(formControlDisplaysError('newPassword')).toBeTruthy();
+        expect(formControlDisplaysError('newPasswordConfirm')).toBeFalsy();
+      });
+    });
+
+    it('should NOT display whan all field have valid valiues', () => {
+      oldPassword.setValue(validPassword);
+      newPassword.setValue(validPassword);
+      newPasswordConfirm.setValue(validPassword);
+      clickSubmit();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(formControlDisplaysError('oldPassword')).toBeFalsy();
+        expect(formControlDisplaysError('newPassword')).toBeFalsy();
+        expect(formControlDisplaysError('newPasswordConfirm')).toBeFalsy();
+      });
+    });
+  });
+
+  function formControlDisplaysError(formControlName: string): boolean {
+    const elementWithErrorMessage = fixture.debugElement.query(
+      By.css(
+        `input[formcontrolname="${formControlName}"] + div.invalid-feedback`
+      )
+    );
+
+    if (!elementWithErrorMessage) {
+      return false;
+    }
+
+    const errorMessage: string =
+      elementWithErrorMessage.nativeElement.innerText;
+    return errorMessage && errorMessage.trim().length > 0;
+  }
+
+  function clickSubmit() {
+    const submitBtn = fixture.debugElement.query(
+      By.css('button[type="submit"]')
+    );
+    submitBtn.nativeElement.dispatchEvent(new MouseEvent('click'));
+  }
 });
