3c64613b98867605cf7049615bedf3985253df3a
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-04-07 14:16:48 +0200

    move product grid styles to style lib

--- projects/storefrontlib/src/lib/product/components/product-list/product-grid-item/product-grid-item.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-list/product-grid-item/product-grid-item.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Product } from '@spartacus/core';
 
 @Component({
   selector: 'cx-product-grid-item',
   templateUrl: './product-grid-item.component.html',
-  styleUrls: ['./product-grid-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductGridItemComponent {
-  @Input()
-  product;
+  @Input() product: Product;
 }
