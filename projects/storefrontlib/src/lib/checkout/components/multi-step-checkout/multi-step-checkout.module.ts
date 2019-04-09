e98d957683c3b01d1b1285911b3153b3348ae2e2
Marcin Lasak <lasakmarcin90@gmail.com>
2019-04-09 10:21:46 +0200

    Checkout orchestrator component + checkout guard (#1953)
    Closes #1889

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/multi-step-checkout.module.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/multi-step-checkout.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
 import { ShippingAddressModule } from './shipping-address/shipping-address.module';
 import { DeliveryModeModule } from './delivery-mode/delivery-mode.module';
 import { PaymentMethodModule } from './payment-method/payment-method.module';
 import { ReviewSubmitModule } from './review-submit/review-submit.module';
-
+import { CheckoutOrchestratorModule } from './checkout-orchestrator/checkout-orchestrator.module';
 import { MultiStepCheckoutComponent } from './container/multi-step-checkout.component';
 import { CartSharedModule } from '../../../cart/cart-shared/cart-shared.module';
 import {
@@ -20,13 +19,13 @@ import {
   imports: [
     CommonModule,
     CartSharedModule,
-
     ShippingAddressModule,
     DeliveryModeModule,
     PaymentMethodModule,
     ReviewSubmitModule,
     RouterModule,
     UrlTranslationModule,
+    CheckoutOrchestratorModule,
     ConfigModule.withConfig(<CmsConfig>{
       cmsComponents: {
         MultiStepCheckoutComponent: { selector: 'cx-multi-step-checkout' },
