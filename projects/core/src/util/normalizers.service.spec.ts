2e16522f8584aa5d10e0f5b979c13b82e4a2ba0a
dunqan <dunqan@w9.pl>
2019-04-09 10:18:13 +0200

    Add tests for normalizer service

--- projects/core/src/util/normalizers.service.spec.ts
+++ projects/core/src/util/normalizers.service.spec.ts
@@ -1,12 +1,97 @@
 import { TestBed } from '@angular/core/testing';
 
-import { NormalizersService } from './normalizers.service';
+import { Normalizer, NormalizersService } from './normalizers.service';
+import { InjectionToken } from '@angular/core';
+import { of } from 'rxjs/internal/observable/of';
+
+const TestNormalizerInjectionToken = new InjectionToken(
+  'TestNormalizerInjectionToken'
+);
+const NotProvidedNormalizerInjectionToken = new InjectionToken(
+  'TestNormalizerInjectionToken'
+);
+
+class CopyNormalizer extends Normalizer<any, any> {
+  normalize(source: any, target: any = {}): any {
+    return { ...target, ...source };
+  }
+}
+
+class TestNormalizer extends Normalizer<any, any> {
+  normalize(source: any, target: any = {}): any {
+    return { ...target, source };
+  }
+}
 
 describe('NormalizersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: NormalizersService;
 
-  it('should be created', () => {
-    const service: NormalizersService = TestBed.get(NormalizersService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TestNormalizerInjectionToken,
+          useClass: CopyNormalizer,
+          multi: true,
+        },
+        {
+          provide: TestNormalizerInjectionToken,
+          useClass: TestNormalizer,
+          multi: true,
+        },
+      ],
+    });
+    service = TestBed.get(NormalizersService);
+  });
+
+  describe('hasNormalizers', () => {
+    it('should return true when normalizers are available', () => {
+      expect(service.hasNormalizers(TestNormalizerInjectionToken)).toBe(true);
+    });
+    it('should return false when normalizers are not provided', () => {
+      expect(service.hasNormalizers(NotProvidedNormalizerInjectionToken)).toBe(
+        false
+      );
+    });
+  });
+
+  describe('pipeable', () => {
+    it('should normalize when normalizers are available', () => {
+      const source = { test: 'test' };
+      let target;
+
+      of(source)
+        .pipe(service.pipeable(TestNormalizerInjectionToken))
+        .subscribe(result => (target = result));
+
+      expect(target).toEqual({ test: 'test', source: { test: 'test' } });
+    });
+    it('should pass data if when normalizers are not provided', () => {
+      const source = { test: 'test' };
+      let target;
+
+      of(source)
+        .pipe(service.pipeable(NotProvidedNormalizerInjectionToken))
+        .subscribe(result => (target = result));
+
+      expect(target).toBe(source);
+    });
+  });
+
+  describe('normalize', () => {
+    it('should normalize when normalizers are available', () => {
+      const source = { test: 'test' };
+      const target = service.normalize(source, TestNormalizerInjectionToken);
+      expect(target).toEqual({ test: 'test', source: { test: 'test' } });
+    });
+
+    it('should pass data if when normalizers are not provided', () => {
+      const source = { test: 'test' };
+      const target = service.normalize(
+        source,
+        NotProvidedNormalizerInjectionToken
+      );
+      expect(target).toBe(source);
+    });
   });
 });
