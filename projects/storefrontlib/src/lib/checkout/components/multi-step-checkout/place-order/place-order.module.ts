baa9d759478c4d76aa0f7203d941f50da1492cf5
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2019-04-09 08:49:42 +0200

    GH-1734: Use translations in the place order module

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/place-order/place-order.module.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/place-order/place-order.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { PlaceOrderComponent } from './place-order.component';
-import { UrlTranslationModule, CheckoutModule } from '@spartacus/core';
+import {
+  UrlTranslationModule,
+  CheckoutModule,
+  I18nModule,
+} from '@spartacus/core';
 
 @NgModule({
-  imports: [CommonModule, CheckoutModule, RouterModule, UrlTranslationModule],
+  imports: [
+    CommonModule,
+    CheckoutModule,
+    RouterModule,
+    UrlTranslationModule,
+    I18nModule,
+  ],
   declarations: [PlaceOrderComponent],
-  exports: [PlaceOrderComponent]
+  exports: [PlaceOrderComponent],
 })
 export class PlaceOrderModule {}
