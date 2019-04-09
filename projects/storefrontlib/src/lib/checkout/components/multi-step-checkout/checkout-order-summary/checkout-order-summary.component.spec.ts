ec482aea44548aabc21d205b35e82f2a2a3ab8bc
Miroslaw Grochowski <m.grochowski@sap.com>
2019-04-09 09:59:33 +0200

    add unit test

---
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/checkout-order-summary/checkout-order-summary.component.spec.ts
@@ -0,0 +1,43 @@
+import { CheckoutOrderSummaryComponent } from './checkout-order-summary.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OrderSummaryComponent } from 'projects/storefrontlib/src/lib/cart';
+import { PromotionsComponent } from '../../promotions/promotions.component';
+import { BehaviorSubject } from 'rxjs';
+import { Cart, CartService } from '@spartacus/core';
+
+import createSpy = jasmine.createSpy;
+
+describe('CheckoutOrderSummaryComponent', () => {
+  let component: CheckoutOrderSummaryComponent;
+  let fixture: ComponentFixture<CheckoutOrderSummaryComponent>;
+  let mockCartService: any;
+
+  beforeEach(async(() => {
+    mockCartService = {
+      getActive(): BehaviorSubject<Cart> {
+        return new BehaviorSubject({
+          totalItems: 5141,
+          subTotal: { formattedValue: '11119' },
+        });
+      },
+      loadDetails: createSpy(),
+    };
+    TestBed.configureTestingModule({
+      declarations: [
+        CheckoutOrderSummaryComponent,
+        OrderSummaryComponent,
+        PromotionsComponent,
+      ],
+      providers: [{ provide: CartService, useValue: mockCartService }],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CheckoutOrderSummaryComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
