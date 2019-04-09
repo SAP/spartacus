eb972529c8e359d7708852270d794b6f3f6a6570
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2019-04-09 12:56:00 +0200

    GH-1890: Add guard to the index

--- projects/storefrontlib/src/lib/checkout/guards/index.ts
+++ projects/storefrontlib/src/lib/checkout/guards/index.ts
@@ -1,4 +1,9 @@
 import { OrderConfirmationPageGuard } from './order-confirmation-page.guard';
 import { CheckoutGuard } from './checkout.guard';
+import { EmptyCartGuard } from './empty-cart.guard';
 
-export const guards: any[] = [OrderConfirmationPageGuard, CheckoutGuard];
+export const guards: any[] = [
+  OrderConfirmationPageGuard,
+  CheckoutGuard,
+  EmptyCartGuard,
+];
