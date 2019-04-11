1bb8ee5fa9039f3c048874a9e74a22d80e8fbe50
dunqan <dunqan@w9.pl>
2019-04-11 21:31:12 +0200

    Change converters pattern

--- projects/core/src/product/occ/product-occ.module.ts
+++ projects/core/src/product/occ/product-occ.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
 import { ProductSearchLoaderService } from './product-search.service';
 import { NgModule } from '@angular/core';
 import { ConfigModule } from '../../config/index';
@@ -10,8 +9,6 @@ import { PRODUCT_REVIEWS_NORMALIZER } from '../connectors/reviews/converters';
 import { ProductReviewsAdapter } from '../connectors/reviews/product-reviews.adapter';
 import { OccProductReviewsListNormalizer } from './converters/occ-product-reviews-list-normalizer';
 import { OccProductReviewsAdapter } from './occ-product-reviews.adapter';
-import { ProductSearchLoaderService } from './product-search.service';
-import { ProductLoaderService } from './product.service';
 import { OccProductAdapter } from './occ-product.adapter';
 import { ProductAdapter } from '../connectors/product/product.adapter';
 import { PRODUCT_NORMALIZER } from '../connectors/product/converters';
