543a87887d3f54f86b47d398a4905ba21c6f60b5
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-04-11 09:17:08 -0400

    GH-1844: Fix for login flicker

--- projects/storefrontlib/src/lib/user/login/login.component.ts
+++ projects/storefrontlib/src/lib/user/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService, User, UserService } from '@spartacus/core';
 import { switchMap, filter, tap } from 'rxjs/operators';
+import { LoginComponentService } from './login.component.service';
 
 @Component({
   selector: 'cx-login',
@@ -9,16 +10,18 @@ import { switchMap, filter, tap } from 'rxjs/operators';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  isLogin = false;
-
-  constructor(private auth: AuthService, private userService: UserService) {}
+  constructor(
+    private auth: AuthService,
+    private userService: UserService,
+    private loginService: LoginComponentService
+  ) {}
 
   get user$(): Observable<User> {
     return this.auth.getUserToken().pipe(
       filter(data => data && !!data.access_token),
       tap(token => {
-        if (!this.isLogin) {
-          this.isLogin = true;
+        if (!this.loginService.isLogin) {
+          this.loginService.isLogin = true;
           this.userService.load(token.userId);
           this.auth.login();
         }
