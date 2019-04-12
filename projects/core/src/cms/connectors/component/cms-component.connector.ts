44a4dfe52ec00242346101ae934b780b9e4e0542
dunqan <dunqan@w9.pl>
2019-04-12 13:44:51 +0200

    Support loading list of components from backend

--- projects/core/src/cms/connectors/component/cms-component.connector.ts
+++ projects/core/src/cms/connectors/component/cms-component.connector.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { CmsComponent } from '../../../occ/occ-models/index';
 import { CmsComponentAdapter } from './cms-component.adapter';
 import { CmsStructureConfigService } from '../../services/cms-structure-config.service';
@@ -31,6 +31,32 @@ export class CmsComponentConnector {
   }
 
   getList(ids: string[], pageContext: PageContext): Observable<CmsComponent[]> {
-    return this.adapter.loadList(ids, pageContext);
+    return this.cmsStructureConfigService.getComponentsFromConfig(ids).pipe(
+      switchMap(configuredComponents => {
+        // check if we have some components that are not loaded from configuration
+        const missingIds = configuredComponents.reduce(
+          (acc, component, index) => {
+            if (component === undefined) {
+              acc.push(ids[index]);
+            }
+            return acc;
+          },
+          []
+        );
+
+        if (missingIds.length > 0) {
+          return this.adapter
+            .loadList(missingIds, pageContext)
+            .pipe(
+              map(loadedComponents => [
+                ...configuredComponents.filter(Boolean),
+                ...loadedComponents,
+              ])
+            );
+        } else {
+          return of(configuredComponents);
+        }
+      })
+    );
   }
 }
