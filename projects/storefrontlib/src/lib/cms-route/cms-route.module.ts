91cfc637b106d1ca6e3d0ab412eeba4ceb7f50ed
Krzysztof Platis <platonn.git@gmail.com>
2019-04-15 04:28:55 +0200

    add cms route in app initializer

--- projects/storefrontlib/src/lib/cms-route/cms-route.module.ts
+++ projects/storefrontlib/src/lib/cms-route/cms-route.module.ts
@@ -1,33 +1,13 @@
 import { NgModule, APP_INITIALIZER, Injector } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { CmsPageGuard } from '../cms/guards/cms-page.guard';
-import { PageLayoutComponent } from '../cms/page-layout/page-layout.component';
-import { moveWildcardRouteToEnd } from './move-wildcard-route-to-end';
-
-export function moveRouteToEnd(injector: Injector) {
-  const result = () => {
-    const router = injector.get(Router);
-    moveWildcardRouteToEnd(router);
-  };
-  return result;
-}
+import { addCmsRoute } from './add-cms-route';
 
 @NgModule({
-  imports: [
-    RouterModule.forChild([
-      {
-        path: '**',
-        canActivate: [CmsPageGuard],
-        component: PageLayoutComponent,
-      },
-    ]),
-  ],
   providers: [
     {
       provide: APP_INITIALIZER,
       multi: true,
       deps: [Injector],
-      useFactory: moveRouteToEnd,
+      useFactory: addCmsRoute,
     },
   ],
 })
