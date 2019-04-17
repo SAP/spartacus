fd9e1c4c2109904b416c43a4f1b816884483e825
Stan <dunqan@w9.pl>
2019-04-17 09:19:14 +0200

    Adapt adapter pattern for product search (#1983)
    Use Connectors, Adapters and Normalizers for product search backend calls
    
    Closes GH-1981

--- projects/core/src/product/store/index.ts
+++ projects/core/src/product/store/index.ts
@@ -2,5 +2,4 @@ export * from './reducers/index';
 export * from './actions/index';
 export * from './effects/index';
 export * from './selectors/index';
-export * from '../occ/converters/index';
 export * from './product-state';
--- projects/core/src/product/store/selectors/product.selectors.ts
+++ projects/core/src/product/store/selectors/product.selectors.ts
@@ -1,7 +1,5 @@
 import { createSelector, MemoizedSelector } from '@ngrx/store';
-
 import { ProductsState, StateWithProduct } from '../product-state';
-import { Product } from '../../../occ/occ-models/occ.models';
 import { EntityLoaderState } from '../../../state/utils/entity-loader/entity-loader-state';
 import { entityStateSelector } from '../../../state/utils/entity-loader/entity-loader.selectors';
 import { LoaderState } from '../../../state/utils/loader/loader-state';
@@ -11,12 +9,12 @@ import {
   loaderSuccessSelector,
   loaderValueSelector,
 } from '../../../state/utils/loader/loader.selectors';
-
+import { UIProduct } from '../../model/product';
 import { getProductsState } from './feature.selector';
 
 export const getProductState: MemoizedSelector<
   StateWithProduct,
-  EntityLoaderState<Product>
+  EntityLoaderState<UIProduct>
 > = createSelector(
   getProductsState,
   (state: ProductsState) => state.details
@@ -24,10 +22,10 @@ export const getProductState: MemoizedSelector<
 
 export const getSelectedProductsFactory = (
   codes: string[]
-): MemoizedSelector<StateWithProduct, Product[]> => {
+): MemoizedSelector<StateWithProduct, UIProduct[]> => {
   return createSelector(
     getProductState,
-    (details: EntityLoaderState<Product>) => {
+    (details: EntityLoaderState<UIProduct>) => {
       return codes
         .map(code =>
           details.entities[code] ? details.entities[code].value : undefined
@@ -39,7 +37,7 @@ export const getSelectedProductsFactory = (
 
 export const getSelectedProductStateFactory = (
   code: string
-): MemoizedSelector<StateWithProduct, LoaderState<Product>> => {
+): MemoizedSelector<StateWithProduct, LoaderState<UIProduct>> => {
   return createSelector(
     getProductState,
     details => entityStateSelector(details, code)
@@ -48,7 +46,7 @@ export const getSelectedProductStateFactory = (
 
 export const getSelectedProductFactory = (
   code: string
-): MemoizedSelector<StateWithProduct, Product> => {
+): MemoizedSelector<StateWithProduct, UIProduct> => {
   return createSelector(
     getSelectedProductStateFactory(code),
     productState => loaderValueSelector(productState)
