4991c0cfa1fe97a0ed99554327481e0aa1eec369
Marcin Lasak <lasakmarcin90@gmail.com>
2019-04-17 09:16:34 +0200

    test: Drop test that does no longer make sense + minor fixes

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/language.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/language.e2e-spec.ts
@@ -1,45 +1,31 @@
 const BASE_URL = Cypress.config().baseUrl;
+const CONTENT_CATALOG = 'electronics-spa';
+const CURRENCY = 'USD';
 
 // Use language switcher to change languages to Deutsch
-function switchLanguage() {
-  cy.get('label')
+function switchLanguage(lang: string) {
+  cy.get('.SiteContext label')
     .contains('Language')
     .parent()
     .children('select')
-    .select('de');
+    .select(lang);
 }
 
 context('Language Switcher', () => {
   describe('Product Page', () => {
-    const PRODUCT_URL_EN = '/electronics/en/USD/product/';
-    const PRODUCT_URL_DE = '/electronics/de/USD/product/';
+    const PRODUCT_URL_EN = `/${CONTENT_CATALOG}/en/${CURRENCY}/product/`;
+    const PRODUCT_URL_DE = `/${CONTENT_CATALOG}/de/${CURRENCY}/product/`;
     const PRODUCT_ID = '3595723';
 
     it('switch language should work and language should be persistent in url', () => {
       // Load Product Page in English
-      cy.visit(`/${PRODUCT_URL_EN}${PRODUCT_ID}`);
+      cy.visit(`${PRODUCT_URL_EN}${PRODUCT_ID}`);
 
       // URL should change to contain 'de' as language after language switch
-      switchLanguage();
+      switchLanguage('de');
       cy.url().should('eq', `${BASE_URL}${PRODUCT_URL_DE}${PRODUCT_ID}`);
-    });
-  });
-
-  describe('Login Page', () => {
-    const LOGIN_URL_EN = '/electronics/en/USD/login';
-
-    it('user input should not be removed on language change', () => {
-      const TEST_EMAIL = 'testEmail';
-
-      // Load Login Page in English
-      cy.visit(`/${LOGIN_URL_EN}`);
-
-      // Add email input into email login field
-      cy.get('input[type="email"]').type(TEST_EMAIL);
-
-      // Check email remains in input after language switch
-      switchLanguage();
-      cy.get('input[type="email"]').should('contain', TEST_EMAIL);
+      // clean after test, go back to default english language
+      switchLanguage('en');
     });
   });
 });
