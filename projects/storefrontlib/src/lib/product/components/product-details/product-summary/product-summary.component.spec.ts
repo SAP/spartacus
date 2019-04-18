6254e43db15770ea436216e029cdf1bda9124096
Caine Rotherham <rotherham.c@gmail.com>
2019-04-18 15:57:51 +0200

    Added describes to unit tests

--- projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.spec.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.spec.ts
@@ -25,57 +25,61 @@ describe('ProductSummaryComponent in product', () => {
     expect(productSummaryComponent).toBeTruthy();
   });
 
-  it('should click tab with no classes', () => {
-    const tabElement: HTMLElement = document.createElement('div');
-    spyOn(tabElement, 'click');
-    productSummaryComponent.clickTabIfInactive(tabElement);
-    expect(tabElement.click).toHaveBeenCalled();
+  describe('clickTabIfInactive to click tabs indicated as inactive', () => {
+    it('should click tab with no classes', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).toHaveBeenCalled();
+    });
+
+    it('should not click tab with active class', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      tabElement.classList.add('active');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).not.toHaveBeenCalled();
+    });
+
+    it('should click tab with toggled classes', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      tabElement.classList.add('toggled');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).toHaveBeenCalled();
+    });
+
+    it('should click tab with active and toggled classes', () => {
+      const tab: HTMLElement = document.createElement('div');
+      tab.classList.add('active');
+      tab.classList.add('toggled');
+      spyOn(tab, 'click');
+      productSummaryComponent.clickTabIfInactive(tab);
+      expect(tab.click).toHaveBeenCalled();
+    });
   });
 
-  it('should not click tab with active class', () => {
-    const tabElement: HTMLElement = document.createElement('div');
-    tabElement.classList.add('active');
-    spyOn(tabElement, 'click');
-    productSummaryComponent.clickTabIfInactive(tabElement);
-    expect(tabElement.click).not.toHaveBeenCalled();
-  });
+  describe('getTabByLabel to get tab from tabs component', () => {
+    it('should return correct tab', () => {
+      const tabsComponent: HTMLElement = document.createElement('div');
+      const tab1: HTMLElement = document.createElement('h3');
+      const tab2: HTMLElement = document.createElement('h3');
+      const tab3: HTMLElement = document.createElement('h3');
 
-  it('should click tab with toggled classes', () => {
-    const tabElement: HTMLElement = document.createElement('div');
-    tabElement.classList.add('toggled');
-    spyOn(tabElement, 'click');
-    productSummaryComponent.clickTabIfInactive(tabElement);
-    expect(tabElement.click).toHaveBeenCalled();
-  });
+      tab1.innerText = 'Tab 1';
+      tab2.innerText = 'Tab 2';
+      tab3.innerText = 'Tab 3';
 
-  it('should click tab with active and toggled classes', () => {
-    const tab: HTMLElement = document.createElement('div');
-    tab.classList.add('active');
-    tab.classList.add('toggled');
-    spyOn(tab, 'click');
-    productSummaryComponent.clickTabIfInactive(tab);
-    expect(tab.click).toHaveBeenCalled();
-  });
+      tabsComponent.appendChild(tab1);
+      tabsComponent.appendChild(tab2);
+      tabsComponent.appendChild(tab3);
 
-  it('should return correct tab from tabs component', () => {
-    const tabsComponent: HTMLElement = document.createElement('div');
-    const tab1: HTMLElement = document.createElement('h3');
-    const tab2: HTMLElement = document.createElement('h3');
-    const tab3: HTMLElement = document.createElement('h3');
+      const result = productSummaryComponent.getTabByLabel(
+        'Tab 2',
+        tabsComponent
+      );
 
-    tab1.innerText = 'Tab 1';
-    tab2.innerText = 'Tab 2';
-    tab3.innerText = 'Tab 3';
-
-    tabsComponent.appendChild(tab1);
-    tabsComponent.appendChild(tab2);
-    tabsComponent.appendChild(tab3);
-
-    const result = productSummaryComponent.getTabByLabel(
-      'Tab 2',
-      tabsComponent
-    );
-
-    expect(result).toBe(tab2);
+      expect(result).toBe(tab2);
+    });
   });
 });
