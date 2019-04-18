0debe88f4f0877a03c4244ede516e4e03aee0edc
Caine Rotherham <rotherham.c@gmail.com>
2019-04-18 16:44:46 +0200

    fix: scroll to Tabs on page and Click Reviews tab on click Show Reviews (#1993)
    Closes #1437

--- projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.spec.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.spec.ts
@@ -24,4 +24,62 @@ describe('ProductSummaryComponent in product', () => {
   it('should be created', () => {
     expect(productSummaryComponent).toBeTruthy();
   });
+
+  describe('clickTabIfInactive to click tabs indicated as inactive', () => {
+    it('should click tab with no classes', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).toHaveBeenCalled();
+    });
+
+    it('should not click tab with active class', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      tabElement.classList.add('active');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).not.toHaveBeenCalled();
+    });
+
+    it('should click tab with toggled classes', () => {
+      const tabElement: HTMLElement = document.createElement('div');
+      tabElement.classList.add('toggled');
+      spyOn(tabElement, 'click');
+      productSummaryComponent.clickTabIfInactive(tabElement);
+      expect(tabElement.click).toHaveBeenCalled();
+    });
+
+    it('should click tab with active and toggled classes', () => {
+      const tab: HTMLElement = document.createElement('div');
+      tab.classList.add('active');
+      tab.classList.add('toggled');
+      spyOn(tab, 'click');
+      productSummaryComponent.clickTabIfInactive(tab);
+      expect(tab.click).toHaveBeenCalled();
+    });
+  });
+
+  describe('getTabByLabel to get tab from tabs component', () => {
+    it('should return correct tab', () => {
+      const tabsComponent: HTMLElement = document.createElement('div');
+      const tab1: HTMLElement = document.createElement('h3');
+      const tab2: HTMLElement = document.createElement('h3');
+      const tab3: HTMLElement = document.createElement('h3');
+
+      tab1.innerText = 'Tab 1';
+      tab2.innerText = 'Tab 2';
+      tab3.innerText = 'Tab 3';
+
+      tabsComponent.appendChild(tab1);
+      tabsComponent.appendChild(tab2);
+      tabsComponent.appendChild(tab3);
+
+      const result = productSummaryComponent.getTabByLabel(
+        'Tab 2',
+        tabsComponent
+      );
+
+      expect(result).toBe(tab2);
+    });
+  });
 });
--- projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.ts
+++ projects/storefrontlib/src/lib/product/components/product-details/product-summary/product-summary.component.ts
@@ -1,24 +1,25 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
   Input,
-  Output,
+  OnInit,
 } from '@angular/core';
 import { ProductDetailOutlets } from '../../../product-outlets.model';
+import { TranslatePipe } from '@spartacus/core';
 
 @Component({
   selector: 'cx-product-summary',
   templateUrl: './product-summary.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TranslatePipe],
 })
-export class ProductSummaryComponent {
+export class ProductSummaryComponent implements OnInit {
   static outlets = ProductDetailOutlets;
 
   itemCount = 1;
+  reviewsTabAvailable: boolean;
 
   @Input() product: any;
-  @Output() openReview = new EventEmitter();
 
   get outlets() {
     return ProductSummaryComponent.outlets;
@@ -43,7 +44,65 @@ export class ProductSummaryComponent {
     );
   }
 
-  launchReview() {
-    this.openReview.emit();
+  // NOTE: Does not currently exists as its own component
+  // but part of tabs component. This is likely to change in refactor.
+  private getReviewsComponent(): Element {
+    return document.querySelector('cx-product-reviews');
+  }
+
+  // Get Tabs Component if exists on page
+  private getTabsComponent(): Element {
+    return document.querySelector('cx-product-tabs');
+  }
+
+  // Get Tab by label if exists on page
+  getTabByLabel(label: string, tabsComponent: Element): HTMLElement {
+    if (tabsComponent) {
+      // NOTE: Reads through h3 tags to click on correct tab
+      // There may be a better way of doing this now/after refactor
+      const h3Elements: HTMLCollectionOf<
+        HTMLElement
+      > = tabsComponent.getElementsByTagName('h3');
+
+      // Look through h3 tab elements until finding tab with label
+      for (const h3Element of Array.from(h3Elements)) {
+        if (h3Element.innerHTML.indexOf(label) > -1) {
+          return h3Element;
+        }
+      }
+    }
+  }
+
+  // Click to activate tab if not already active
+  clickTabIfInactive(tab: HTMLElement): void {
+    if (
+      !tab.classList.contains('active') ||
+      tab.classList.contains('toggled')
+    ) {
+      tab.click();
+    }
+  }
+
+  // Scroll to views component on page and click "Reviews" tab
+  showReviews() {
+    // Use translated label for Reviews tab reference
+    const reviewsTabLabel = this.translatePipe.transform(
+      'productDetails.label.reviews'
+    );
+
+    const tabsComponent = this.getTabsComponent();
+    const reviewsTab = this.getTabByLabel(reviewsTabLabel, tabsComponent);
+    const reviewsComponent = this.getReviewsComponent();
+
+    if (reviewsTab && reviewsComponent) {
+      reviewsComponent.scrollIntoView();
+      this.clickTabIfInactive(reviewsTab);
+    }
+  }
+
+  constructor(protected translatePipe: TranslatePipe) {}
+
+  ngOnInit() {
+    this.reviewsTabAvailable = !!this.getReviewsComponent();
   }
 }
