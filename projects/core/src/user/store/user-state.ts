a453f13b4553570975d4ddd4e17e02ef08a64a63
Louis Pierrestiger <louis.pierrestiger@sap.com>
2019-04-23 12:04:17 -0400

    feat: Close account option in my account (GH-1579)
    Closes #1579

--- projects/core/src/user/store/user-state.ts
+++ projects/core/src/user/store/user-state.ts
@@ -14,6 +14,7 @@ export const USER_FEATURE = 'user';
 export const UPDATE_EMAIL_PROCESS_ID = 'updateEmail';
 export const UPDATE_PASSWORD_PROCESS_ID = 'updatePassword';
 export const UPDATE_USER_DETAILS_PROCESS_ID = 'updateUserDetails';
+export const REMOVE_USER_PROCESS_ID = 'removeUser';
 
 export const USER_PAYMENT_METHODS = '[User] User Payment Methods';
 export const USER_ORDERS = '[User] User Orders';
---
+++ projects/storefrontlib/src/lib/my-account/close-account/components/close-account-modal/close-account-modal.component.ts
@@ -0,0 +1,77 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import {
+  GlobalMessageService,
+  GlobalMessageType,
+  RoutingService,
+  UserService,
+  UserToken,
+  AuthService,
+} from '@spartacus/core';
+
+import { Observable, Subscription } from 'rxjs';
+
+import i18next from 'i18next';
+
+@Component({
+  selector: 'cx-close-account-modal',
+  templateUrl: './close-account-modal.component.html',
+  styleUrls: ['./close-account-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CloseAccountModalComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
+  userToken$: Observable<UserToken>;
+  isLoading$: Observable<boolean>;
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private userService: UserService,
+    private authService: AuthService,
+    private globalMessageService: GlobalMessageService,
+    private routingService: RoutingService
+  ) {}
+
+  ngOnInit() {
+    this.userToken$ = this.authService.getUserToken();
+    this.userService.resetRemoveUserProcessState();
+    this.subscription.add(
+      this.userService
+        .getRemoveUserResultSuccess()
+        .subscribe(success => this.onSuccess(success))
+    );
+    this.isLoading$ = this.userService.getRemoveUserResultLoading();
+  }
+
+  onSuccess(success: boolean): void {
+    if (success) {
+      this.closeModal();
+      this.globalMessageService.add({
+        text: `${i18next.t('closeAccount:closeAccount.message.success')}`,
+        type: GlobalMessageType.MSG_TYPE_CONFIRMATION,
+      });
+      this.routingService.go({ route: ['home'] });
+    }
+  }
+
+  closeModal(): void {
+    this.activeModal.dismiss();
+  }
+
+  closeAccount(userId: string) {
+    this.userService.remove(userId);
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
