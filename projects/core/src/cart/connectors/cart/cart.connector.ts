6c908be135aff7e046940595219ab63c7a44046f
Stan <dunqan@w9.pl>
2019-04-24 09:24:15 +0200

    Adapt adapter pattern for cart (#2077)
    Use Connectors, Adapters and Normalizers for product search backend calls
    
    Closes GH-2035

---
+++ projects/core/src/cart/connectors/cart/cart.connector.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Cart } from '../../../occ/occ-models/occ.models';
+import { CartAdapter } from './cart.adapter';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartConnector {
+  constructor(private adapter: CartAdapter) {}
+
+  public loadAll(userId: string, details?: boolean): Observable<Cart[]> {
+    return this.adapter.loadAll(userId, details);
+  }
+
+  public load(
+    userId: string,
+    cartId: string,
+    details?: boolean
+  ): Observable<Cart> {
+    return this.adapter.load(userId, cartId, details);
+  }
+
+  public create(
+    userId: string,
+    oldCartId?: string,
+    toMergeCartGuid?: string
+  ): Observable<Cart> {
+    return this.adapter.create(userId, oldCartId, toMergeCartGuid);
+  }
+}
