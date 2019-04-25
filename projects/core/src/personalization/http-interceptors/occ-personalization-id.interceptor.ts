86269488ee1354d0d185ed1beb14d9f4f761d402
Weizheng Gao <weizheng.gao@sap.com>
2019-04-25 14:06:58 -0400

    feature/GH-1990: get personalization id from response header then set it to each future request

--- projects/core/src/personalization/http-interceptors/occ-personalization-id.interceptor.ts
+++ projects/core/src/personalization/http-interceptors/occ-personalization-id.interceptor.ts
@@ -4,28 +4,53 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { OccEndpointsService } from '../../occ/services/occ-endpoints.service';
 import { PersonalizationConfig } from '../config/personalization-config';
 
 @Injectable()
 export class OccPersonalizationIdInterceptor implements HttpInterceptor {
-  private personalizationId: string = '9739a8b5-ba9c-4431-8da5-28ee1410d7ca';
-  constructor(private config: PersonalizationConfig) {}
+  private personalizationId: string;
+  private requestHeader: string;
+
+  constructor(
+    private config: PersonalizationConfig,
+    private occEndpoints: OccEndpointsService
+  ) {
+    this.requestHeader = this.config.personalization.requestHeader.toLocaleLowerCase();
+  }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log('here');
-    if (this.personalizationId) {
+    if (
+      this.personalizationId &&
+      request.url.indexOf(this.occEndpoints.getBaseEndpoint()) > -1
+    ) {
       request = request.clone({
         setHeaders: {
-          [this.config.personalization.requestHeader]: this.personalizationId,
+          [this.requestHeader]: this.personalizationId,
         },
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      map(event => {
+        if (
+          this.personalizationId === undefined &&
+          event instanceof HttpResponse
+        ) {
+          if (event.headers.keys().indexOf(this.requestHeader) > -1) {
+            this.personalizationId = event.headers.get(this.requestHeader);
+          }
+        }
+        return event;
+      })
+    );
   }
 }
