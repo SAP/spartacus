9b1424fc28060bd3da3f7451bbd61f37e67807bd
Jerify02 <i323629@c02xv131jgh5.dhcp.ctul.sap.corp>
2019-04-25 15:27:11 +0800

    fix conflict

--- projects/storefrontlib/src/lib/cms-lib/cms-lib.module.ts
+++ projects/storefrontlib/src/lib/cms-lib/cms-lib.module.ts
@@ -22,7 +22,6 @@ import { NavigationModule } from './navigation/navigation.module';
 import { CmsParagraphModule } from './paragraph/paragraph.module';
 import { ProductCarouselModule } from './product-carousel/product-carousel.module';
 import { SearchBoxModule } from './search-box/search-box.module';
-import { CloseAccountModule } from '../my-account/close-account/close-account.module';
 import { MyInterestsModule } from '../my-account/my-interests/my-interests.module';
 
 @NgModule({
