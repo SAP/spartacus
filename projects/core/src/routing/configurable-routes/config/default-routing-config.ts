a9dcde7b5247c7b283e38f7b51af3fc1647592e5
Krzysztof Platis <platonn.git@gmail.com>
2019-04-26 17:01:55 +0200

    fix path to default routes config

--- projects/core/src/routing/configurable-routes/config/default-routing-config.ts
+++ projects/core/src/routing/configurable-routes/config/default-routing-config.ts
@@ -1,5 +1,5 @@
 import { RoutingConfig } from './routing-config';
-import { defaultStorefrontRoutesConfig } from './default-storefront-routes-translations';
+import { defaultStorefrontRoutesConfig } from './default-storefront-routes-config';
 
 export const defaultRoutingConfig: RoutingConfig = {
   routing: {
