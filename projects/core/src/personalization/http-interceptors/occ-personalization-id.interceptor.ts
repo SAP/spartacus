9ae66675a24bec139b607dbbb50cc52b0c16c5b1
Weizheng Gao <weizheng.gao@sap.com>
2019-04-26 09:15:38 -0400

    feature/GH-1990

--- projects/core/src/personalization/http-interceptors/occ-personalization-id.interceptor.ts
+++ projects/core/src/personalization/http-interceptors/occ-personalization-id.interceptor.ts
@@ -7,7 +7,7 @@ import {
   HttpResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 import { OccEndpointsService } from '../../occ/services/occ-endpoints.service';
 import { PersonalizationConfig } from '../config/personalization-config';
@@ -21,7 +21,7 @@ export class OccPersonalizationIdInterceptor implements HttpInterceptor {
     private config: PersonalizationConfig,
     private occEndpoints: OccEndpointsService
   ) {
-    this.requestHeader = this.config.personalization.requestHeader.toLocaleLowerCase();
+    this.requestHeader = this.config.personalization.requestHeader.toLowerCase();
   }
 
   intercept(
@@ -40,16 +40,16 @@ export class OccPersonalizationIdInterceptor implements HttpInterceptor {
     }
 
     return next.handle(request).pipe(
-      map(event => {
+      tap(event => {
         if (
           this.personalizationId === undefined &&
           event instanceof HttpResponse
         ) {
           if (event.headers.keys().indexOf(this.requestHeader) > -1) {
             this.personalizationId = event.headers.get(this.requestHeader);
+            console.log(this.personalizationId);
           }
         }
-        return event;
       })
     );
   }
