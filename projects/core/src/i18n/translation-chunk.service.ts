70fb4724615b48a36bea927114373e1a8c124d82
Miroslaw Grochowski <m.grochowski@sap.com>
2019-04-27 11:49:05 +0200

    optimization: getChunkNameForKey

--- projects/core/src/i18n/translation-chunk.service.ts
+++ projects/core/src/i18n/translation-chunk.service.ts
@@ -3,45 +3,27 @@ import { I18nConfig } from './config/i18n-config';
 
 @Injectable()
 export class TranslationChunkService {
+  private chunks: object = {};
   constructor(protected config: I18nConfig) {
-    this.validateConfig(config);
+    Object.keys(config.i18n.chunks).forEach(chunk => {
+      config.i18n.chunks[chunk].map(key => {
+        if (this.chunks.hasOwnProperty(key)) {
+          console.warn(`Duplicated key (${key}) in the i18n config file.`);
+        }
+        this.chunks[key] = chunk;
+      });
+    });
   }
 
   protected readonly KEY_SEPARATOR = '.';
 
   getChunkNameForKey(key: string): string {
     const mainKey = (key || '').split(this.KEY_SEPARATOR)[0];
-    const chunk = this.getChunkFromConfig(mainKey);
+    const chunk = this.chunks && this.chunks[mainKey];
 
     if (!chunk) {
       return mainKey; // fallback to main key as a chunk
     }
     return chunk;
-  }
-
-  private getChunkFromConfig(mainKey: string): string {
-    const config = this.config.i18n;
-    return (
-      config &&
-      config.chunks &&
-      Object.keys(config.chunks)
-        .filter(chunk =>
-          config.chunks[chunk].filter(key => key === mainKey).join()
-        )
-        .join()
-    );
-  }
-
-  private validateConfig(config: I18nConfig): void {
-    let allKeys = [];
-    Object.keys(config.i18n.chunks).forEach(
-      chunk => (allKeys = [...allKeys, ...config.i18n.chunks[chunk]])
-    );
-    const dupes = allKeys.filter((el, i) => allKeys.indexOf(el) !== i);
-    if (dupes.length > 0 && !config.production) {
-      console.warn(
-        `There are duplicated keys (${dupes.join()}) in the i18n config file.`
-      );
-    }
   }
 }
