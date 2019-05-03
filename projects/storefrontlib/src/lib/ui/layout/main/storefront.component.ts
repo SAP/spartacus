124e23e513b3cf39b2007e12c56786f15cf8bf12
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-05-03 06:55:41 +0200

    move load indicator to storefront element

--- projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { RoutingService } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { HamburgerMenuService } from '../../../../layout/header/hamburger-menu/hamburger-menu.service';
@@ -7,14 +7,21 @@ import { HamburgerMenuService } from '../../../../layout/header/hamburger-menu/h
   selector: 'cx-storefront',
   templateUrl: './storefront.component.html',
 })
-export class StorefrontComponent {
+export class StorefrontComponent implements OnInit {
   isExpanded$: Observable<boolean> = this.hamburgerMenuService.isExpanded;
-  isNavigating$: Observable<boolean> = this.routingService.isNavigating();
+
+  @HostBinding('class.is-navigating') isNavigating;
 
   constructor(
     private hamburgerMenuService: HamburgerMenuService,
     private routingService: RoutingService
   ) {}
+
+  ngOnInit(): void {
+    this.routingService
+      .isNavigating()
+      .subscribe(val => (this.isNavigating = !!val));
+  }
 
   collapseMenu(): void {
     this.hamburgerMenuService.toggle(true);
