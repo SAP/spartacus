9b82ecd1f58f19af17c61e375daf8c96a5fab568
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-05-05 16:08:49 +0200

    move directives to new lib layout

--- projects/storefrontlib/src/shared/components/form-components/form-components.module.ts
+++ projects/storefrontlib/src/shared/components/form-components/form-components.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BootstrapModule } from '../../../lib/bootstrap.module';
-import { OnlyNumberDirective } from '../../../lib/ui/directives/only-number/only-number.directive';
+import { OnlyNumberDirective } from '../../directives/only-number/only-number.directive';
 import { ItemCounterComponent } from './item-counter/item-counter.component';
 /* Components */
 import { StarRatingComponent } from './star-rating/star-rating.component';
--- projects/storefrontlib/src/shared/index.ts
+++ projects/storefrontlib/src/shared/index.ts
@@ -1,1 +1,2 @@
 export * from './components/index';
+export * from './directives/index';
