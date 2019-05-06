aea28d5b2c15d75a67089738ca05cddabfa9d697
Krzysztof Platis <platonn.git@gmail.com>
2019-05-06 11:22:53 +0200

    fix build after merging develop

--- projects/core/src/global-message/http-interceptors/handlers/bad-request.handler.ts
+++ projects/core/src/global-message/http-interceptors/handlers/bad-request.handler.ts
@@ -29,10 +29,10 @@ export class BadRequestHandler extends HttpErrorHandler {
     } else if (response.error.errors[0].type === 'PasswordMismatchError') {
       // uses en translation error message instead of backend exception error
       // @todo: this condition could be removed if backend gives better message
-      this.globalMessageService.add({
-        type: GlobalMessageType.MSG_TYPE_ERROR,
-        text: 'Old password incorrect.',
-      });
+      this.globalMessageService.add(
+        'Old password incorrect.',
+        GlobalMessageType.MSG_TYPE_ERROR
+      );
       // text: customError.customError.passwordMismatch,
     } else {
       // this is currently showing up in case we have a page not found. It should be a 404.
