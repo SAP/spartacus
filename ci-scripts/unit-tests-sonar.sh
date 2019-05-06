b3c30a0241f7203a89caa9bb97923a7ed02f3dec
Marcin Lasak <lasakmarcin90@gmail.com>
2019-05-06 14:18:08 +0200

    chore: Add cypress directory to sonarcloud config

--- ci-scripts/unit-tests-sonar.sh
+++ ci-scripts/unit-tests-sonar.sh
@@ -30,8 +30,8 @@ if [[ $1 == '-h' ]]; then
     sonar-scanner \
     -Dsonar.projectKey=sap_cloud-commerce-spartacus-storefront \
     -Dsonar.organization=sap \
-    -Dsonar.sources=projects/storefrontlib,projects/core,projects/storefrontstyles \
-    -Dsonar.tests=projects/storefrontlib,projects/core,projects/storefrontstyles \
+    -Dsonar.sources=projects/storefrontlib,projects/core,projects/storefrontstyles,projects/storefrontapp-e2e-cypress \
+    -Dsonar.tests=projects/storefrontlib,projects/core,projects/storefrontstyles,projects/storefrontapp-e2e-cypress \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.login=$SONAR_TOKEN \
     -Dsonar.cfamily.build-wrapper-output.bypass=true
