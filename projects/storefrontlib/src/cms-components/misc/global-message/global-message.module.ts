f606f86708dde7fd3874edd4506c2607ed2d3362
developpeurweb <developpeur@gmail.com>
2019-05-07 15:16:56 -0400

    GH-2221 replacing closing button for global alerts

--- projects/storefrontlib/src/cms-components/misc/global-message/global-message.module.ts
+++ projects/storefrontlib/src/cms-components/misc/global-message/global-message.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { GlobalMessageModule, I18nModule } from '@spartacus/core';
-
+import { IconModule } from '../../../cms-components/misc/icon/index';
 import { GlobalMessageComponent } from './global-message.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
+    IconModule,
     I18nModule,
     GlobalMessageModule.forRoot(),
   ],
