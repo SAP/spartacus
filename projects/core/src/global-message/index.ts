cf4ea05c9bd6e44611c8b9f7f7cf472b241a6838
Krzysztof Platis <platonn.git@gmail.com>
2019-05-08 16:56:31 +0200

    export parent barrel

--- projects/core/src/global-message/index.ts
+++ projects/core/src/global-message/index.ts
@@ -6,3 +6,4 @@ export * from './facade/index';
 export * from './models/global-message.model';
 export * from './global-message.module';
 export * from './http-interceptors/index';
+export * from './http-interceptors/handlers/index';
