a9da68851197554b7268d5ad9406d66bff8f96a1
Jerify02 <i323629@c02xv131jgh5.dhcp.ctul.sap.corp>
2019-05-08 14:09:10 +0800

    move scss to styles lib

--- projects/storefrontlib/src/cms-components/myaccount/order/order-details/order-detail-items/consignment-tracking/tracking-events.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/order-details/order-detail-items/consignment-tracking/tracking-events.component.ts
@@ -6,7 +6,6 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'cx-tracking-events',
   templateUrl: './tracking-events.component.html',
-  styleUrls: ['./tracking-events.component.scss'],
 })
 export class TrackingEventsComponent {
   tracking$: Observable<ConsignmentTracking>;
