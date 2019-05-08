5a281d94a7ad714c18bd2af870773cc5d87330fc
Jerify02 <i323629@c02xv131jgh5.dhcp.ctul.sap.corp>
2019-05-08 09:48:56 +0800

    fix i18n issue

--- projects/core/src/i18n/config/default-i18n-config.ts
+++ projects/core/src/i18n/config/default-i18n-config.ts
@@ -5,54 +5,11 @@ export const defaultI18nConfig: I18nConfig = {
     fallbackLang: false,
     debug: false,
     chunks: {
-      common: 'common',
-      spinner: 'common',
-      header: 'common',
-      searchBox: 'common',
-
-      cartDetails: 'cart',
-      cartItems: 'cart',
-      orderCost: 'cart',
-
-      addressForm: 'address',
-      addressBook: 'address',
-      addressCard: 'address',
-
-      paymentForm: 'payment',
-      paymentMethods: 'payment',
-
-      checkout: 'checkout',
-      checkoutAddress: 'checkout',
-      checkoutOrderConfirmation: 'checkout',
-      checkoutReview: 'checkout',
-      checkoutShipping: 'checkout',
-
-      orderDetails: 'myAccount',
-      orderHistory: 'myAccount',
-      myInterests: 'myAccount',
-
-      productDetails: 'product',
-      productList: 'product',
-      productFacetNavigation: 'product',
-      productSummary: 'product',
-      productReview: 'product',
-      addToCart: 'product',
-
-      forgottenPassword: 'user',
-      loginForm: 'user',
-      login: 'user',
-      register: 'user',
-      updateEmailForm: 'user',
-      updatePasswordForm: 'user',
-      updateProfileForm: 'user',
-
-      storeFinder: 'storeFinder',
-      pwa: 'pwa',
       common: ['common', 'spinner', 'header', 'searchBox'],
       cart: ['cartDetails', 'cartItems', 'orderCost'],
       address: ['addressForm', 'addressBook', 'addressCard'],
       payment: ['paymentForm', 'paymentMethods'],
-      myAccount: ['orderDetails', 'orderHistory'],
+      myAccount: ['orderDetails', 'orderHistory', 'myInterests'],
       storeFinder: ['storeFinder'],
       pwa: ['pwa'],
       checkout: [
