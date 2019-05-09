ebc527bedbac3d768c50f6b09ee37a15cc66d064
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-05-09 09:44:38 +0200

    drop unused formmodule

--- projects/storefrontlib/src/cms-components/navigation/search-box/search-box.module.ts
+++ projects/storefrontlib/src/cms-components/navigation/search-box/search-box.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import {
   CmsConfig,
@@ -13,11 +13,9 @@ import {
 import { IconModule } from '../../../cms-components/misc/icon/index';
 import { MediaModule } from '../../../shared/components/media/media.module';
 import { SearchBoxComponent } from './search-box.component';
-
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     RouterModule,
     ReactiveFormsModule,
     MediaModule,
