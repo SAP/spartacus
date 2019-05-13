8cc0e5126ca42fa7095fccecd7923737ad7008a7
dunqan <dunqan@w9.pl>
2019-05-13 07:56:01 +0200

    Add base classes for user connector

---
+++ projects/core/src/user/connectors/user.adapter.ts
@@ -0,0 +1,2 @@
+export interface UserAdapter {
+}
---
+++ projects/core/src/user/connectors/user.connector.spec.ts
@@ -0,0 +1,7 @@
+import { UserConnector } from './user.connector';
+
+describe('UserConnector', () => {
+  it('should create an instance', () => {
+    expect(new UserConnector()).toBeTruthy();
+  });
+});
