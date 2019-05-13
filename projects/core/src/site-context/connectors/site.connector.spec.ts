e7addb5e4103ae35f45f1026412db258191054c4
dunqan <dunqan@w9.pl>
2019-05-13 12:48:55 +0200

    Add tests for site connector

--- projects/core/src/site-context/connectors/site.connector.spec.ts
+++ projects/core/src/site-context/connectors/site.connector.spec.ts
@@ -1,7 +1,51 @@
-// import { SiteConnector } from './site.connector';
+import { TestBed } from '@angular/core/testing';
+
+import { SiteConnector } from './site.connector';
+import { of } from 'rxjs/internal/observable/of';
+import { SiteAdapter } from './site.adapter';
+import createSpy = jasmine.createSpy;
+
+const mockLanguages = ['l', 'a', 'n', 'g'];
+const mockCurrencies = ['c', 'u', 'r', 'r'];
+
+class MockSiteAdapter implements SiteAdapter {
+  loadCurrencies = createSpy('SiteAdapter.loadCurrencies').and.callFake(() =>
+    of(mockCurrencies)
+  );
+
+  loadLanguages = createSpy('SiteAdapter.loadLanguages').and.callFake(() =>
+    of(mockLanguages)
+  );
+}
 
 describe('SiteConnector', () => {
-  // it('should create an instance', () => {
-  //   expect(new SiteConnector()).toBeTruthy();
-  // });
+  let service: SiteConnector;
+  let adapter: SiteAdapter;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: SiteAdapter, useClass: MockSiteAdapter }],
+    });
+
+    service = TestBed.get(SiteConnector);
+    adapter = TestBed.get(SiteAdapter);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('getLanguages should call adapter', () => {
+    let result;
+    service.getLanguages().subscribe(res => (result = res));
+    expect(result).toBe(mockLanguages);
+    expect(adapter.loadLanguages).toHaveBeenCalled();
+  });
+
+  it('getCurrencies should call adapter', () => {
+    let result;
+    service.getCurrencies().subscribe(res => (result = res));
+    expect(result).toBe(mockCurrencies);
+    expect(adapter.loadCurrencies).toHaveBeenCalled();
+  });
 });
