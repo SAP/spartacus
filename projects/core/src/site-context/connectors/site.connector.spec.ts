0460137c2d9d5bdeb2c8ff02e5e54268dbe4b65e
dunqan <dunqan@w9.pl>
2019-05-13 07:56:26 +0200

    Add base classes fot site connector

---
+++ projects/core/src/site-context/connectors/site.connector.spec.ts
@@ -0,0 +1,7 @@
+import { SiteConnector } from './site.connector';
+
+describe('SiteConnector', () => {
+  it('should create an instance', () => {
+    expect(new SiteConnector()).toBeTruthy();
+  });
+});
