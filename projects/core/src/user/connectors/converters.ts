908236fec183035cc01e0642b43605cbc69685a3
dunqan <dunqan@w9.pl>
2019-05-13 14:38:00 +0200

    Remove order serializer

--- projects/core/src/user/connectors/converters.ts
+++ projects/core/src/user/connectors/converters.ts
@@ -2,14 +2,10 @@ import { InjectionToken } from '@angular/core';
 import { Converter } from '../../util/converter.service';
 import { Order, OrderHistoryList } from '../../model/order.model';
 
-export const ORDER_SERIALIZER = new InjectionToken<Converter<Order, any>>(
-  'OrderSerializer'
-);
-
 export const ORDER_NORMALIZER = new InjectionToken<Converter<any, Order>>(
   'OrderNormalizer'
 );
 
-export const ORDER_HISTORY_NORMALIZER = new InjectionToken<Converter<any, OrderHistoryList>>(
-  'OrderHistoryNormalizer'
-);
+export const ORDER_HISTORY_NORMALIZER = new InjectionToken<
+  Converter<any, OrderHistoryList>
+>('OrderHistoryNormalizer');
