431f4e2b7db60033f1165e177f3c32889239ecc4
dunqan <dunqan@w9.pl>
2019-05-14 08:26:00 +0200

    Added comments to adapters

--- projects/core/src/site-context/connectors/site.adapter.ts
+++ projects/core/src/site-context/connectors/site.adapter.ts
@@ -2,7 +2,13 @@ import { Observable } from 'rxjs';
 import { Currency, Language } from '../../model/misc.model';
 
 export abstract class SiteAdapter {
+  /**
+   * Abstract method used to load languages.
+   */
   abstract loadLanguages(): Observable<Language[]>;
 
+  /**
+   * Abstract method used to load currencies.
+   */
   abstract loadCurrencies(): Observable<Currency[]>;
 }
