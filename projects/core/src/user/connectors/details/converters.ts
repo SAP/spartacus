3050385fe7dd7da3e4772c94a86a3add57feb38d
dunqan <dunqan@w9.pl>
2019-05-15 14:37:46 +0200

    Implement UserAccountAdapter

--- projects/core/src/user/connectors/details/converters.ts
+++ projects/core/src/user/connectors/details/converters.ts
@@ -1,11 +1,12 @@
 import { InjectionToken } from '@angular/core';
 import { Converter } from '../../../util/converter.service';
 import { User } from '../../../model/misc.model';
+import { UserRegisterFormData } from '../../model/user.model';
 
 export const USER_NORMALIZER = new InjectionToken<Converter<any, User>>(
   'UserNormalizer'
 );
 
-export const USER_SERIALIZER = new InjectionToken<Converter<User, any>>(
-  'UserSerializer'
-);
+export const USER_REGISTER_FORM_SERIALIZER = new InjectionToken<
+  Converter<UserRegisterFormData, any>
+>('UserRegisterFormSerializer');
