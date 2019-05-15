15f93354b31c4eeb3b70d6654aee5562a60c4476
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-05-15 12:57:11 -0400

    Revert "GH-2483 debugging script"
    This reverts commit 139590c35b0a7863aabcc542bcab65947fd23a63.

--- scripts/publish-builds.sh
+++ scripts/publish-builds.sh
@@ -69,6 +69,7 @@ publish_snapshot() {
         git add --all && \
         git commit -m "${COMMIT_MSG}" --quiet && \
         git tag "${BUILD_ID}"
+        git push origin "${PUBLISH_BRANCH}" --tags --force
     )
 }
 
