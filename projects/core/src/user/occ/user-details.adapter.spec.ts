2c193871233338817b7f62ece7f6e57e7b12c20f
dunqan <dunqan@w9.pl>
2019-05-15 13:04:37 +0200

    Add basic user details adapter

---
+++ projects/core/src/user/occ/user-details.adapter.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+// import { UserDetailsAdapter } from './user-details.adapter';
+
+describe('UserDetailsAdapter', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    // const service: UserDetailsAdapter = TestBed.get(UserDetailsAdapter);
+    // expect(service).toBeTruthy();
+  });
+});
