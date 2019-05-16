ca80a44e94c9585865a6e2bb61ccdc0b34f9dd92
Patrick Labadie <patrick.labadie@sap.com>
2019-05-16 13:47:14 -0400

    GH-2497: Store user details in the local storage with other authentication data.

--- projects/core/src/user/store/user-store.module.ts
+++ projects/core/src/user/store/user-store.module.ts
@@ -1,14 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { RouterModule } from '@angular/router';
-
-import { metaReducers, reducerToken, reducerProvider } from './reducers/index';
-import { effects } from './effects/index';
-import { USER_FEATURE } from './user-state';
+import { ConfigModule } from '../../config';
+import { StateConfig, StorageSyncType } from '../../state';
 import { StateModule } from '../../state/state.module';
+import { effects } from './effects/index';
+import { metaReducers, reducerProvider, reducerToken } from './reducers/index';
+import { USER_FEATURE } from './user-state';
+
+export function userStoreConfigFactory(): StateConfig {
+  const config: StateConfig = {
+    state: {
+      storageSync: {
+        keys: {
+          'user.account.details': StorageSyncType.LOCAL_STORAGE,
+        },
+      },
+    },
+  };
+  return config;
+}
 
 @NgModule({
   imports: [
@@ -18,6 +32,7 @@ import { StateModule } from '../../state/state.module';
     StoreModule.forFeature(USER_FEATURE, reducerToken, { metaReducers }),
     EffectsModule.forFeature(effects),
     RouterModule,
+    ConfigModule.withConfigFactory(userStoreConfigFactory),
   ],
   providers: [reducerProvider],
 })
