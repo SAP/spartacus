f120c556261a86e2d1ad8308e0bc3efae4179161
dunqan <dunqan@w9.pl>
2019-05-16 13:56:43 +0200

    Add tests to user account connector

--- projects/core/src/user/connectors/account/user-account.connector.spec.ts
+++ projects/core/src/user/connectors/account/user-account.connector.spec.ts
@@ -1,64 +1,101 @@
-// import { TestBed } from '@angular/core/testing';
-//
-// import { UserConnector } from './user.connector';
-// import { of } from 'rxjs/internal/observable/of';
-// import { UserAdapter } from './user.adapter';
-// import createSpy = jasmine.createSpy;
-//
-// class MockUserAdapter implements UserAdapter {
-//   place = createSpy('UserAdapter.place').and.callFake((userId, cartId) =>
-//     of(`placedUser-${userId}-${cartId}`)
-//   );
-//
-//   load = createSpy('UserAdapter.load').and.callFake((userId, userCode) =>
-//     of(`user-${userId}-${userCode}`)
-//   );
-//
-//   loadHistory = createSpy('UserAdapter.loadHistory').and.callFake(userId =>
-//     of(`userHistory-${userId}`)
-//   );
-// }
-//
-// describe('SiteConnector', () => {
-//   let service: UserConnector;
-//   let adapter: UserAdapter;
-//
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [{ provide: UserAdapter, useClass: MockUserAdapter }],
-//     });
-//
-//     service = TestBed.get(UserConnector);
-//     adapter = TestBed.get(UserAdapter);
-//   });
-//
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-//
-//   it('place should call adapter', () => {
-//     let result;
-//     service.place('user1', 'cart1').subscribe(res => (result = res));
-//     expect(result).toBe('placedUser-user1-cart1');
-//     expect(adapter.place).toHaveBeenCalledWith('user1', 'cart1');
-//   });
-//
-//   it('get should call adapter', () => {
-//     let result;
-//     service.get('user2', 'user2').subscribe(res => (result = res));
-//     expect(result).toBe('user-user2-user2');
-//     expect(adapter.load).toHaveBeenCalledWith('user2', 'user2');
-//   });
-//
-//   it('getHistory should call adapter', () => {
-//     let result;
-//     service.getHistory('user3').subscribe(res => (result = res));
-//     expect(result).toBe('userHistory-user3');
-//     expect(adapter.loadHistory).toHaveBeenCalledWith(
-//       'user3',
-//       undefined,
-//       undefined,
-//       undefined
-//     );
-//   });
-// });
+import { TestBed } from '@angular/core/testing';
+
+import { UserAccountConnector } from './user-account.connector';
+import { of } from 'rxjs/internal/observable/of';
+import { UserAccountAdapter } from './user-account.adapter';
+import { UserRegisterFormData } from '@spartacus/core';
+import createSpy = jasmine.createSpy;
+
+class MockUserAdapter implements UserAccountAdapter {
+  register = createSpy('UserAdapter.place').and.callFake(userId => of(userId));
+  remove = createSpy('UserAdapter.place').and.returnValue(of({}));
+  requestForgotPasswordEmail = createSpy('UserAdapter.place').and.returnValue(
+    of({})
+  );
+  resetPassword = createSpy('UserAdapter.place').and.returnValue(of({}));
+  updateEmail = createSpy('UserAdapter.place').and.returnValue(of({}));
+  updatePassword = createSpy('UserAdapter.place').and.returnValue(of({}));
+}
+
+describe('SiteConnector', () => {
+  let service: UserAccountConnector;
+  let adapter: UserAccountAdapter;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: UserAccountAdapter, useClass: MockUserAdapter }],
+    });
+
+    service = TestBed.get(UserAccountConnector);
+    adapter = TestBed.get(UserAccountAdapter);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('register should call adapter', () => {
+    let result;
+
+    const registerData: UserRegisterFormData = {
+      firstName: 'name',
+      lastName: 'name',
+      password: 'pass',
+      titleCode: 'title',
+      uid: 'uid',
+    };
+
+    service.register(registerData).subscribe(res => (result = res));
+    expect(result).toBe(registerData);
+    expect(adapter.register).toHaveBeenCalledWith(registerData);
+  });
+
+  it('remove should call adapter', () => {
+    let result;
+    service.remove('user-id').subscribe(res => (result = res));
+    expect(result).toEqual({});
+    expect(adapter.remove).toHaveBeenCalledWith('user-id');
+  });
+
+  it('requestForgotPasswordEmail should call adapter', () => {
+    let result;
+    service
+      .requestForgotPasswordEmail('user-id')
+      .subscribe(res => (result = res));
+    expect(result).toEqual({});
+    expect(adapter.requestForgotPasswordEmail).toHaveBeenCalledWith('user-id');
+  });
+
+  it('resetPassword should call adapter', () => {
+    let result;
+    service.resetPassword('token', 'password').subscribe(res => (result = res));
+    expect(result).toEqual({});
+    expect(adapter.resetPassword).toHaveBeenCalledWith('token', 'password');
+  });
+
+  it('updateEmail should call adapter', () => {
+    let result;
+    service
+      .updateEmail('email', 'password', 'new-email')
+      .subscribe(res => (result = res));
+    expect(result).toEqual({});
+    expect(adapter.updateEmail).toHaveBeenCalledWith(
+      'email',
+      'password',
+      'new-email'
+    );
+  });
+
+  it('updatePassword should call adapter', () => {
+    let result;
+    service
+      .updatePassword('email', 'password', 'new-password')
+      .subscribe(res => (result = res));
+    expect(result).toEqual({});
+    expect(adapter.updatePassword).toHaveBeenCalledWith(
+      'email',
+      'password',
+      'new-password'
+    );
+  });
+});
