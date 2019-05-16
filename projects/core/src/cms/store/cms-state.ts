e4d7ba8a82e3ada0370adcd48054265c8b08ae44
Bitcollage <serkan.sipahi@yahoo.de>
2019-05-16 18:02:29 +0200

    Fix typo

--- projects/core/src/cms/store/cms-state.ts
+++ projects/core/src/cms/store/cms-state.ts
@@ -5,7 +5,7 @@ import { EntityLoaderState } from '../../state/utils/entity-loader/entity-loader
 
 export const CMS_FEATURE = 'cms';
 export const NAVIGATION_DETAIL_ENTITY = '[Cms] Navigation Entity';
-export const COMPONENT_ENTITY = '[Cms[ Component Entity';
+export const COMPONENT_ENTITY = '[Cms] Component Entity';
 
 export interface StateWithCms {
   [CMS_FEATURE]: CmsState;
