1b81dd0ca3fc15271406c097dbd30769a132d2d3
dunqan <dunqan@w9.pl>
2019-05-16 15:20:01 +0200

    OccUserPaymentAdapter tests

--- projects/core/src/user/occ/occ-user-payment.adapter.spec.ts
+++ projects/core/src/user/occ/occ-user-payment.adapter.spec.ts
@@ -1,12 +1,129 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { OccConfig } from '../../occ/config/occ-config';
+import { PaymentDetails } from '../../model/cart.model';
+import { Occ } from '../../occ/occ-models/occ.models';
+import { OccUserPaymentAdapter } from './occ-user-payment.adapter';
 
-// import { OccUserPaymentAdapter } from './occ-user-payment.adapter';
+const username = 'mockUsername';
 
-describe('OccUserPaymentAdapter', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+const endpoint = '/users';
+const paymentDetailsEndpoint = '/paymentdetails';
 
-  it('should be created', () => {
-    // const service: OccUserPaymentAdapter = TestBed.get(OccUserPaymentAdapter);
-    // expect(service).toBeTruthy();
+const MockOccModuleConfig: OccConfig = {
+  backend: {
+    occ: {
+      baseUrl: '',
+      prefix: '',
+    },
+  },
+
+  site: {
+    baseSite: '',
+  },
+};
+
+describe('OccUserService', () => {
+  let service: OccUserPaymentAdapter;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        OccUserPaymentAdapter,
+        { provide: OccConfig, useValue: MockOccModuleConfig },
+      ],
+    });
+
+    service = TestBed.get(OccUserPaymentAdapter);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('load user payment methods', () => {
+    it('should load user payment methods for a given user id', () => {
+      const mockPayment1: PaymentDetails = {
+        accountHolderName: 'mockAccountHolderName1',
+      };
+      const mockPayment2: PaymentDetails = {
+        accountHolderName: 'mockAccountHolderName2',
+      };
+      const mockUserPaymentMethods: Occ.PaymentDetailsList = {
+        payments: [mockPayment1, mockPayment2],
+      };
+
+      service.loadList(username).subscribe(result => {
+        expect(result).toEqual(mockUserPaymentMethods.payments);
+      });
+
+      const mockReq = httpMock.expectOne(req => {
+        return (
+          req.method === 'GET' &&
+          req.url ===
+            `${endpoint}/${username}${paymentDetailsEndpoint}?saved=true`
+        );
+      });
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush(mockUserPaymentMethods);
+    });
+  });
+
+  describe('set default user payment method', () => {
+    it('should set default payment method for given user', () => {
+      const mockPayment: PaymentDetails = {
+        defaultPayment: true,
+        id: '123',
+      };
+
+      service.setDefault(username, mockPayment.id).subscribe(result => {
+        expect(result).toEqual('');
+      });
+
+      const mockReq = httpMock.expectOne(req => {
+        return (
+          req.method === 'PATCH' &&
+          req.body.defaultPayment === true &&
+          req.url ===
+            `${endpoint}/${username}${paymentDetailsEndpoint}/${mockPayment.id}`
+        );
+      });
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush('');
+    });
+  });
+
+  describe('delete user payment method', () => {
+    it('should delete payment method for given user', () => {
+      const mockPayment: PaymentDetails = {
+        id: '123',
+      };
+
+      service
+        .delete(username, mockPayment.id)
+        .subscribe(result => expect(result).toEqual(''));
+
+      const mockReq = httpMock.expectOne(req => {
+        return (
+          req.method === 'DELETE' &&
+          req.url ===
+            `${endpoint}/${username}${paymentDetailsEndpoint}/${mockPayment.id}`
+        );
+      });
+
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+      mockReq.flush('');
+    });
   });
 });
