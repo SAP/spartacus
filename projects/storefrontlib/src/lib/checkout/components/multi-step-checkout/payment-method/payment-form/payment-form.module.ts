34205de0161e178eda9b21e7f641011db41c6d75
Kacper Knapik <kknapik@divante.pl>
2019-05-16 12:16:44 +0200

    remove ngb tooltip from payment form

--- projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/payment-method/payment-form/payment-form.module.ts
+++ projects/storefrontlib/src/lib/checkout/components/multi-step-checkout/payment-method/payment-form/payment-form.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { I18nModule } from '@spartacus/core';
 import { CardModule } from '../../../../../../shared/components/card/card.module';
-import { BootstrapModule } from '../../../../../bootstrap.module';
 import { BillingAddressFormModule } from '../billing-address-form/billing-address-form.module';
 import { IconModule } from '../../../../../../cms-components/misc/icon/index';
 import { PaymentFormComponent } from './payment-form.component';
@@ -14,7 +13,6 @@ import { PaymentFormComponent } from './payment-form.component';
     CommonModule,
     ReactiveFormsModule,
     NgSelectModule,
-    BootstrapModule,
     CardModule,
     BillingAddressFormModule,
     I18nModule,
