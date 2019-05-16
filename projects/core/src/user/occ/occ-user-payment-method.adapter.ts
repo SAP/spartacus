cdfc668d2fc6edac40c92b9f69739ee03b55632c
dunqan <dunqan@w9.pl>
2019-05-16 08:23:45 +0200

    Implement OccPaymentDetailsAdapter

--- projects/core/src/user/occ/occ-user-payment-method.adapter.ts
+++ projects/core/src/user/occ/occ-user-payment-method.adapter.ts
@@ -1,7 +1,68 @@
 import { Injectable } from '@angular/core';
+import { UserPaymentMethodAdapter } from '../connectors/payment-method/user-payment-method.adapter';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  Occ,
+  OccEndpointsService,
+  PAYMENT_DETAILS_NORMALIZER,
+} from '@spartacus/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { PaymentDetails } from '../../model/cart.model';
+import { ConverterService } from '../../util/converter.service';
+
+const USER_ENDPOINT = 'users/';
+const PAYMENT_DETAILS_ENDPOINT = '/paymentdetails';
 
 @Injectable()
-export class OccUserPaymentMethodAdapter {
+export class OccUserPaymentMethodAdapter implements UserPaymentMethodAdapter {
+  constructor(
+    protected http: HttpClient,
+    private occEndpoints: OccEndpointsService,
+    protected converter: ConverterService
+  ) {}
 
-  constructor() { }
+  loadList(userId: string): Observable<PaymentDetails[]> {
+    const url = `${this.getUserEndpoint()}${userId}${PAYMENT_DETAILS_ENDPOINT}?saved=true`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.get<Occ.PaymentDetailsList>(url, { headers }).pipe(
+      catchError((error: any) => throwError(error)),
+      map(methodList => methodList.payments),
+      this.converter.pipeableMany(PAYMENT_DETAILS_NORMALIZER)
+    );
+  }
+
+  delete(userId: string, paymentMethodID: string): Observable<{}> {
+    const url = `${this.getUserEndpoint()}${userId}${PAYMENT_DETAILS_ENDPOINT}/${paymentMethodID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .delete(url, { headers })
+      .pipe(catchError((error: any) => throwError(error)));
+  }
+
+  setDefault(userId: string, paymentMethodID: string): Observable<{}> {
+    const url = `${this.getUserEndpoint()}${userId}${PAYMENT_DETAILS_ENDPOINT}/${paymentMethodID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .patch(
+        url,
+        // TODO: Remove billingAddress property
+        { billingAddress: { titleCode: 'mr' }, defaultPayment: true },
+        { headers }
+      )
+      .pipe(catchError((error: any) => throwError(error)));
+  }
+
+  protected getUserEndpoint(): string {
+    return this.occEndpoints.getEndpoint(USER_ENDPOINT);
+  }
 }
