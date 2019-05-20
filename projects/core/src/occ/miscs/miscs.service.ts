8431da2690f7bfb72ad1e49ba0b2af3d6015f5b5
dunqan <dunqan@w9.pl>
2019-05-20 09:33:47 +0200

    Remove loadRegions from OccMiscsService

--- projects/core/src/occ/miscs/miscs.service.ts
+++ projects/core/src/occ/miscs/miscs.service.ts
@@ -1,31 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { Occ } from '../../occ/occ-models/occ.models';
-import { OccEndpointsService } from '../services/occ-endpoints.service';
-
-const ENDPOINT_REGIONS = 'regions';
-const COUNTRIES_ENDPOINT = 'countries';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OccMiscsService {
-  constructor(
-    private http: HttpClient,
-    private occEndpoints: OccEndpointsService
-  ) {}
-
-  loadRegions(countryIsoCode: string): Observable<Occ.RegionList> {
-    return this.http
-      .get<Occ.RegionList>(
-        this.occEndpoints.getEndpoint(this.buildRegionsUrl(countryIsoCode))
-      )
-      .pipe(catchError((error: any) => throwError(error.json())));
-  }
-
-  private buildRegionsUrl(countryIsoCode: string): string {
-    return `${COUNTRIES_ENDPOINT}/${countryIsoCode}/${ENDPOINT_REGIONS}`;
-  }
+  constructor() {}
 }
