794fc7da4e5129f08f71ac53b1b4456bc0c7db78
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-05-21 11:53:25 -0400

    GH-2264: Changes following comments

--- projects/storefrontlib/src/cms-components/content/tab-paragraph-container/tab-paragraph-container.component.ts
+++ projects/storefrontlib/src/cms-components/content/tab-paragraph-container/tab-paragraph-container.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {
-  CMSTabParagraphContainer,
-  CmsService,
-  WindowRef,
-} from '@spartacus/core';
+import { CmsService, CMSTabParagraphContainer } from '@spartacus/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
 import { CmsComponentData } from '../../../cms-structure/page/model/index';
-import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-tab-paragraph-container',
@@ -13,49 +11,31 @@ import { map, take } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabParagraphContainerComponent {
-  activatedElements: HTMLElement[] = [];
+  activeTabNum = 0;
 
   constructor(
     public componentData: CmsComponentData<CMSTabParagraphContainer>,
-    private cmsService: CmsService,
-    private windowRef: WindowRef
+    private cmsService: CmsService
   ) {}
 
-  components$ = this.componentData.data$.pipe(
-    map((data: CMSTabParagraphContainer) => {
-      return data.components.split(' ').map(component =>
-        this.cmsService.getComponentData<any>(component).pipe(
-          map(tab => {
-            if (!tab.flexType) {
-              tab.flexType = tab.typeCode;
-            }
-            return { ...tab, title: `productTabs.${tab.uid}` };
-          })
+  components$: Observable<any[]> = this.componentData.data$.pipe(
+    switchMap(data =>
+      combineLatest(
+        data.components.split(' ').map(component =>
+          this.cmsService.getComponentData<any>(component).pipe(
+            map(tab => {
+              if (!tab.flexType) {
+                tab.flexType = tab.typeCode;
+              }
+              return { ...tab, title: `productTabs.${tab.uid}` };
+            })
+          )
         )
-      );
-    }),
-    take(1)
+      )
+    )
   );
 
-  select(event: MouseEvent, tab: HTMLElement): void {
-    if (this.activatedElements.length === 0) {
-      const activeElements = this.windowRef.document.querySelectorAll(
-        'cx-tab-paragraph-container .active'
-      );
-      activeElements.forEach((node: Element) =>
-        node.classList.remove('active')
-      );
-    }
-
-    if (!this.activatedElements.includes(tab)) {
-      // remove active class on both header and content panel
-      this.activatedElements.forEach(el =>
-        el.classList.remove('active', 'toggled')
-      );
-      this.activatedElements = [<HTMLElement>event.target, tab];
-      this.activatedElements.forEach(el => el.classList.add('active'));
-    } else {
-      this.activatedElements.forEach(el => el.classList.toggle('toggled'));
-    }
+  select(tabNum: number): void {
+    this.activeTabNum = tabNum;
   }
 }
