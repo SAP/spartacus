5bbc354c7ef5363e69e5ec54f81cf5932221c121
Caine Rotherham <rotherham.c@gmail.com>
2019-05-21 10:28:25 +0200

    Move complex logic into functions, removal of magic numbers, unit tests

--- projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.component.spec.ts
+++ projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.component.spec.ts
@@ -3,7 +3,10 @@ import { PaginationComponent } from './pagination.component';
 import { PaginationModel } from '@spartacus/core';
 
 const FIRST_PAGE = 1;
+const MIDDLE_PAGE = 5;
 const TOTAL_PAGES = 10;
+const OUT_OF_RANGE_MIN = 0;
+const OUT_OF_RANGE_MAX = 11;
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
@@ -32,17 +35,41 @@ describe('PaginationComponent', () => {
 
   describe('pageChange function', () => {
     it('should change pages', () => {
-      component.pageChange(5);
+      component.pageChange(MIDDLE_PAGE);
       component.viewPageEvent.subscribe((event: any) => {
-        expect(event).toEqual(5);
+        expect(event).toEqual(MIDDLE_PAGE);
       });
+    });
+  });
+
+  describe('GET functions', () => {
+    it('should return first page', () => {
+      expect(component.getPageFirst()).toEqual(FIRST_PAGE);
+    });
+
+    it('should return last page', () => {
+      expect(component.getPageLast()).toEqual(TOTAL_PAGES);
+    });
+
+    it('should return next page', () => {
+      expect(component.getPageNext()).toEqual(FIRST_PAGE + 2);
+    });
+
+    it('should return previous page', () => {
+      component.pagination.currentPage = MIDDLE_PAGE;
+      expect(component.getPagePrevious()).toEqual(MIDDLE_PAGE);
+    });
+
+    it('should return array of pages', () => {
+      const pageArray = Array(TOTAL_PAGES);
+      expect(component.getPageIndicies()).toEqual(pageArray);
     });
   });
 
   describe('clickPageNo function', () => {
     it('should change pages by index', () => {
-      const newPage = component.clickPageNo(5);
-      expect(newPage).toEqual(5);
+      const newPage = component.clickPageNo(MIDDLE_PAGE);
+      expect(newPage).toEqual(MIDDLE_PAGE);
     });
 
     it('should change pages to first', () => {
@@ -56,12 +83,12 @@ describe('PaginationComponent', () => {
     });
 
     it('should not change pages when index out of max range', () => {
-      const newPage = component.clickPageNo(TOTAL_PAGES + 1);
+      const newPage = component.clickPageNo(OUT_OF_RANGE_MAX);
       expect(newPage).toEqual(FIRST_PAGE);
     });
 
     it('should not change pages when index out of min range', () => {
-      const newPage = component.clickPageNo(FIRST_PAGE - 1);
+      const newPage = component.clickPageNo(OUT_OF_RANGE_MIN);
       expect(newPage).toEqual(FIRST_PAGE);
     });
   });
--- projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.component.ts
+++ projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.component.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/core';
 import { PaginationModel } from '@spartacus/core';
 
+const PAGE_FIRST = 1;
+
 @Component({
   selector: 'cx-pagination',
   templateUrl: './pagination.component.html',
@@ -16,18 +18,70 @@ export class PaginationComponent {
   @Input() pagination: PaginationModel;
   @Output() viewPageEvent: EventEmitter<number> = new EventEmitter<number>();
 
-  Array = Array;
-  Math = Math;
+  getPageFirst(): number {
+    return PAGE_FIRST;
+  }
+
+  getPageLast(): number {
+    return this.pagination.totalPages;
+  }
+
+  getPagePrevious(): number {
+    return this.pagination.currentPage;
+  }
+
+  getPageNext(): number {
+    return this.pagination.currentPage + 2;
+  }
+
+  getPageIndicies(): Array<number> {
+    return Array(this.pagination.totalPages);
+  }
+
+  onFirstPage(): boolean {
+    return this.pagination.currentPage === 0;
+  }
+
+  onLastPage(): boolean {
+    return this.pagination.currentPage === this.pagination.totalPages - 1;
+  }
+
+  onPageIndex(index: number): boolean {
+    return this.pagination.currentPage === index;
+  }
+
+  hideContinuedBack(): boolean {
+    return !(this.pagination.totalPages > 2 && this.hidePageIndex(1));
+  }
+
+  hideContinuedForwards(): boolean {
+    return !(
+      this.pagination.totalPages > 2 &&
+      this.hidePageIndex(this.pagination.totalPages - 2)
+    );
+  }
+
+  hidePageIndex(index: number): boolean {
+    return (
+      index === 0 ||
+      index === this.pagination.totalPages - 1 ||
+      Math.floor(this.pagination.currentPage / 3) * 3 > index ||
+      Math.floor(this.pagination.currentPage / 3) * 3 + 2 < index
+    );
+  }
 
   clickPageNo(page: number): number {
+    // Change page on valid index
     if (
-      page >= 1 &&
+      page >= PAGE_FIRST &&
       page <= this.pagination.totalPages &&
       page !== this.pagination.currentPage + 1
     ) {
       this.pageChange(page);
       return page;
     }
+
+    // Page stays the same on invalid index
     return this.pagination.currentPage;
   }
 
