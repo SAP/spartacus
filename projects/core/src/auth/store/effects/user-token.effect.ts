65ab31dbcf16c54d6b25b7aa84328d79236bd5be
Patrick Labadie <patrick.labadie@sap.com>
2019-05-23 13:36:56 -0400

    GH-2584: Applied code review improvement suggestion: remove userId from the RefreshUserToken Action.

--- projects/core/src/auth/store/effects/user-token.effect.ts
+++ projects/core/src/auth/store/effects/user-token.effect.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
-import { UserToken } from '../../models/token-types.model';
 import { USERID_CURRENT } from '../../../occ/utils/occ-constants';
+import { UserToken } from '../../models/token-types.model';
 import { Login } from '../actions/login-logout.action';
 import { UserTokenAction } from '../actions/user-token.action';
 import { UserAuthenticationTokenService } from './../../services/user-authentication/user-authentication-token.service';
@@ -39,12 +39,12 @@ export class UserTokenEffects {
   refreshUserToken$: Observable<UserTokenAction> = this.actions$.pipe(
     ofType(fromActions.REFRESH_USER_TOKEN),
     map((action: fromActions.RefreshUserToken) => action.payload),
-    switchMap(({ userId, refreshToken }) => {
+    switchMap(({ refreshToken }) => {
       return this.userTokenService.refreshToken(refreshToken).pipe(
         map((token: UserToken) => {
           const date = new Date();
           date.setSeconds(date.getSeconds() + token.expires_in);
-          token.userId = userId;
+          token.userId = USERID_CURRENT;
           token.expiration_time = date;
           return new fromActions.RefreshUserTokenSuccess(token);
         }, catchError(error => of(new fromActions.RefreshUserTokenFail(error))))
