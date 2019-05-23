a9c99714e1093272ccef7108ecad6221b50c5d95
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-05-23 23:26:50 +0200

    move product suffix route to product page module

--- projects/storefrontlib/src/cms-components/product/product-page/product-page.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-page/product-page.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CmsPageGuard } from '../../../cms-structure/guards/cms-page.guard';
 import { OutletRefModule } from '../../../cms-structure/outlet/index';
 import { PageLayoutModule } from '../../../cms-structure/page/page-layout/page-layout.module';
+import { suffixUrlMatcher } from '../../../cms-structure/routing/suffix-routes/suffix-url-matcher';
 import { ProductDetailsModule } from '../product-details/product-details.module';
 import { ProductPageComponent } from './product-page.component';
 
@@ -13,6 +14,17 @@ const routes: Routes = [
     canActivate: [CmsPageGuard],
     component: ProductPageComponent,
     data: { cxRoute: 'product' },
+  },
+  {
+    matcher: suffixUrlMatcher,
+    canActivate: [CmsPageGuard],
+    component: ProductPageComponent,
+    data: {
+      cxSuffixUrlMatcher: {
+        marker: 'p',
+        paramName: 'productCode',
+      },
+    },
   },
 ];
 
--- projects/storefrontlib/src/cms-structure/routing/suffix-routes/suffix-routes.module.ts
+++ projects/storefrontlib/src/cms-structure/routing/suffix-routes/suffix-routes.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ProductPageComponent } from '../../../cms-components/product/product-page/product-page.component';
-import { ProductPageModule } from '../../../cms-components/product/product-page/product-page.module';
 import { CmsPageGuard } from '../../guards/cms-page.guard';
 import { PageLayoutComponent } from '../../page/index';
 import { suffixUrlMatcher } from './suffix-url-matcher';
 
 @NgModule({
   imports: [
-    ProductPageModule,
     RouterModule.forChild([
-      // TODO: consider moving to product page module
-      {
-        matcher: suffixUrlMatcher,
-        canActivate: [CmsPageGuard],
-        component: ProductPageComponent,
-        data: {
-          cxSuffixUrlMatcher: {
-            marker: 'p',
-            paramName: 'productCode',
-          },
-        },
-      },
       {
         matcher: suffixUrlMatcher,
         canActivate: [CmsPageGuard],
