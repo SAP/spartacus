9ec06966944382887c4267e9f0927412b6446af8
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-05-24 11:26:23 +0200

    fix prettier

--- projects/storefrontlib/src/cms-components/navigation/search-box/search-box.component.spec.ts
+++ projects/storefrontlib/src/cms-components/navigation/search-box/search-box.component.spec.ts
@@ -3,7 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { CmsSearchBoxComponent, CmsService, I18nTestingModule, ProductSearchService } from '@spartacus/core';
+import {
+  CmsSearchBoxComponent,
+  CmsService,
+  I18nTestingModule,
+  ProductSearchService,
+} from '@spartacus/core';
 import { of } from 'rxjs';
 import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
 import { SearchBoxComponentService } from './search-box-component.service';
