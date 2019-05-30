2066ae8cde0218f15679f770ba4885775c8b69fa
dunqan <dunqan@w9.pl>
2019-05-30 14:55:35 +0200

    Add setPageFailIndex action

--- projects/core/src/cms/store/reducers/page-index.reducer.ts
+++ projects/core/src/cms/store/reducers/page-index.reducer.ts
@@ -6,20 +6,26 @@ export function reducer(
   entityType: string
 ): (
   state: string,
-  action: fromAction.LoadPageDataSuccess | fromAction.LoadPageDataFail
+  action:
+    | fromAction.LoadPageDataSuccess
+    | fromAction.LoadPageDataFail
+    | fromAction.SetPageFailIndex
 ) => string {
   return (
     state = initialState,
-    action: fromAction.LoadPageDataSuccess | fromAction.LoadPageDataFail
+    action:
+      | fromAction.LoadPageDataSuccess
+      | fromAction.LoadPageDataFail
+      | fromAction.SetPageFailIndex
   ): string => {
     if (action.meta && action.meta.entityType === entityType) {
       switch (action.type) {
-        case fromAction.LOAD_PAGE_DATA_SUCCESS: {
-          return action.payload.pageId;
+        case fromAction.SET_PAGE_FAIL_INDEX: {
+          return action.meta.loader.error;
         }
 
-        case fromAction.LOAD_PAGE_DATA_FAIL: {
-          return initialState;
+        case fromAction.LOAD_PAGE_DATA_SUCCESS: {
+          return action.payload.pageId;
         }
       }
     }
