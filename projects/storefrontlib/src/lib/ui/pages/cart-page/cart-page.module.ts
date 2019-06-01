526751eb38a3e21ca044dc669a0644adaf8989e5
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-02 00:02:04 +0200

    Move checkout to the new storefrontlib layout

--- projects/storefrontlib/src/lib/ui/pages/cart-page/cart-page.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/cart-page/cart-page.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartDetailsModule } from '../../../../cms-components/checkout/cart/cart-details/cart-details.module';
+import { CartDetailsModule } from '../../../../cms-components/cart/cart-details/cart-details.module';
 import { CmsModule, CmsPageGuard } from '../../../../cms-structure/index';
 import { OutletRefModule } from '../../../../cms-structure/outlet/outlet-ref/outlet-ref.module';
 import { PageLayoutModule } from '../../../../cms-structure/page/index';
