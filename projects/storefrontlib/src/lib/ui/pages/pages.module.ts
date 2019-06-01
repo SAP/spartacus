d469a24bf38bc397e3a8f9215d838e4707ffefc0
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-02 00:33:34 +0200

    @tobi-or-not-tobi Move virtual logout route and guard to own module in new lib layout

--- projects/storefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/storefrontlib/src/lib/ui/pages/pages.module.ts
@@ -1,12 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ConfigModule } from '@spartacus/core';
-import { LogoutGuard } from '../../../cms-components/index';
-import {
-  PageLayoutComponent,
-  PageLayoutModule,
-} from '../../../cms-structure/page/index';
+import { PageLayoutModule } from '../../../cms-structure/page/index';
 import { CartPageModule } from './cart-page/cart-page.module';
 import { defaultRoutingConfig } from './default-routing-config';
 import { OrderConfirmationPageModule } from './order-confirmation-page/order-confirmation-page.module';
@@ -19,14 +14,6 @@ const pageModules = [CartPageModule, OrderConfirmationPageModule];
     CommonModule,
     ...pageModules,
     PageLayoutModule,
-    RouterModule.forChild([
-      {
-        path: null,
-        canActivate: [LogoutGuard],
-        component: PageLayoutComponent,
-        data: { cxRoute: 'logout' },
-      },
-    ]),
   ],
 })
 export class PagesModule {}
