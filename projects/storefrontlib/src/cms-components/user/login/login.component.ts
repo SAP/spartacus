bfdd97488d7ef9a11d7351a8298cdf3a7aee8b00
Patrick Labadie <patrick.labadie@sap.com>
2019-06-03 13:36:11 -0400

    GH-2796: Update LoginComponent to wotk well without subscribing to the observable from the template.

--- projects/storefrontlib/src/cms-components/user/login/login.component.ts
+++ projects/storefrontlib/src/cms-components/user/login/login.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { User, UserService } from '@spartacus/core';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cx-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent implements OnInit {
-  user$: Observable<User>;
-  constructor(private userService: UserService) {}
+export class LoginComponent implements OnInit, OnDestroy {
+  userDetails: User = {};
+  private subscription = new Subscription();
+
+  constructor(
+    private userService: UserService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.user$ = this.userService.get();
+    this.subscription.add(
+      this.userService.get().subscribe(userDet => {
+        this.userDetails = userDet;
+        this.cd.markForCheck();
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
