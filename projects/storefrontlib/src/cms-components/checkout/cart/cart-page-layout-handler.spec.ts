b6df16782f822f9d378bbb3c7e8c782b74f7525b
dunqan <dunqan@w9.pl>
2019-06-04 16:41:44 +0200

    Add base test file for PageLayoutHandler

---
+++ projects/storefrontlib/src/cms-components/checkout/cart/cart-page-layout-handler.spec.ts
@@ -0,0 +1,7 @@
+// import { CartPageLayoutHandler } from './cart-page-layout-handler';
+
+describe('CartPageLayoutHandler', () => {
+  it('should create an instance', () => {
+    // expect(new CartPageLayoutHandler()).toBeTruthy();
+  });
+});
