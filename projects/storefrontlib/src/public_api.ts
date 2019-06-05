62e06b40f348a183aa4d0510b01063203b8e2bfb
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-05 13:23:47 +0200

    drop config import as we removed it

--- projects/storefrontlib/src/public_api.ts
+++ projects/storefrontlib/src/public_api.ts
@@ -5,7 +5,6 @@
 export * from './cms-components/index';
 export * from './cms-pages/index';
 export * from './cms-structure/index';
-export * from './config/index';
 export * from './layout/index';
 export * from './recipes/index';
 export * from './shared/index';
