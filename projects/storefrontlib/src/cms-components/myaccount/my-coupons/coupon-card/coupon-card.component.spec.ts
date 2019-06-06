65dbf4693d9525e5f5ec662d260ce973209bba82
YangQingyu <qingyu.yang@sap.com>
2019-06-06 17:19:18 +0800

    PANDA-3176 Add test cases for customer coupon

--- projects/storefrontlib/src/cms-components/myaccount/my-coupons/coupon-card/coupon-card.component.spec.ts
+++ projects/storefrontlib/src/cms-components/myaccount/my-coupons/coupon-card/coupon-card.component.spec.ts
@@ -1,31 +1,98 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CouponCardComponent } from './coupon-card.component';
-import { of } from 'rxjs';
-import { UserToken, I18nTestingModule, AuthService } from '@spartacus/core';
+import { I18nTestingModule, CustomerCoupon } from '@spartacus/core';
+import { By } from '@angular/platform-browser';
+import { ModalService } from '@spartacus/storefront';
 
-describe('CouponCardComponent', () => {
+const mockCoupon: CustomerCoupon = {
+  couponId: 'CustomerCoupon1',
+  description: 'CustomerCoupon1',
+  endDate: new Date('2019-12-30T23:59:59+0000'),
+  name: 'CustomerCoupon1:name',
+  notificationOn: 'false',
+  solrFacets: '%3Arelevance%3Acategory%3A1',
+  startDate: new Date('1970-01-01T00:00:00+0000'),
+  status: 'Effective',
+};
+
+fdescribe('CouponCardComponent', () => {
   let component: CouponCardComponent;
   let fixture: ComponentFixture<CouponCardComponent>;
-
-  const authService = jasmine.createSpyObj('AuthService', ['getUserToken']);
-  authService.getUserToken.and.returnValue(of({ userId: 'test' } as UserToken));
-
+  const modalService = jasmine.createSpyObj('ModalService', ['open']);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CouponCardComponent],
       imports: [I18nTestingModule],
-      providers: [{ provide: AuthService, useValue: authService }],
+      providers: [{ provide: ModalService, useValue: modalService }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CouponCardComponent);
     component = fixture.componentInstance;
+    modalService.open.and.stub();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display coupon data', () => {
+    component.coupon = mockCoupon;
+    fixture.detectChanges();
+
+    const couponName = fixture.debugElement.query(
+      By.css('.cx-coupon-card-name')
+    ).nativeElement.textContent;
+    expect(couponName).toContain('CustomerCoupon1:name');
+
+    const couponStatus = fixture.debugElement.queryAll(
+      By.css('.cx-coupon-card-status')
+    );
+    expect(couponStatus.length).toBe(1);
+
+    const couponEffectiveDate = fixture.debugElement.queryAll(
+      By.css('.cx-coupon-date')
+    );
+    expect(couponEffectiveDate.length).toBe(1);
+
+    const couponNotificationCheckbox = fixture.debugElement.queryAll(
+      By.css('.form-check-input')
+    );
+    expect(couponNotificationCheckbox.length).toBe(1);
+    const couponNotificationLabel = fixture.debugElement.queryAll(
+      By.css('.form-check-label')
+    );
+    expect(couponNotificationLabel.length).toBe(1);
+
+    const readMoreLink = fixture.debugElement.queryAll(By.css('a'));
+    expect(readMoreLink.length).toBe(1);
+
+    const findProductBtn = fixture.debugElement.query(By.css('button'))
+      .nativeElement.textContent;
+    expect(findProductBtn).toContain('myCoupons.findProducts');
+  });
+
+  it('should be able to open coupon detail dialog', () => {
+    const readMoreLink = fixture.debugElement.query(By.css('a'));
+    readMoreLink.nativeElement.click();
+    expect(modalService.open).toHaveBeenCalled();
+  });
+
+  it('should be able to show correct notification status', () => {
+    let couponNotificationCheckbox = fixture.debugElement.queryAll(
+      By.css('.form-check-input')
+    );
+    expect(couponNotificationCheckbox.length).toBe(1);
+    expect(couponNotificationCheckbox[0].nativeElement.checked).toBeFalsy();
+    component.coupon.notificationOn = 'true';
+    fixture.detectChanges();
+    couponNotificationCheckbox = fixture.debugElement.queryAll(
+      By.css('.form-check-input')
+    );
+    expect(couponNotificationCheckbox.length).toBe(1);
+    expect(couponNotificationCheckbox[0].nativeElement.checked).toBeTruthy();
   });
 });
