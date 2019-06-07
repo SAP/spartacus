a8e85a92523ebd74ccf58f95b8116d302c633e35
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-07 11:46:10 +0200

    super bare minimum of tests

--- projects/storefrontlib/src/cms-components/product/carousel/product-references/product-references.component.spec.ts
+++ projects/storefrontlib/src/cms-components/product/carousel/product-references/product-references.component.spec.ts
@@ -1,199 +1,91 @@
-// import {
-//   Component,
-//   DebugElement,
-//   Input,
-//   Pipe,
-//   PipeTransform,
-// } from '@angular/core';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import {
-//   CmsProductReferencesComponent,
-//   ProductReference,
-//   ProductReferenceService,
-//   RoutingService,
-// } from '@spartacus/core';
-// import { Observable, of } from 'rxjs';
-// import { CmsComponentData } from '../../../../cms-structure/page/model/cms-component-data';
-// import { SharedCarouselService } from '../shared-carousel.service';
-// import { ProductReferencesComponent } from './product-references.component';
-// import { ProductReferencesService } from './product-references.component.service';
+import { Component, Input } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CmsProductReferencesComponent, Product } from '@spartacus/core';
+import { CarouselItem } from 'projects/storefrontlib/src/shared/components/carousel/carousel.model';
+import { Observable, of } from 'rxjs';
+import { CmsComponentData } from '../../../../cms-structure/page/model/cms-component-data';
+import { CurrentProductService } from '../../current-product.service';
+import { ProductCarouselService } from '../product-carousel.service';
+import { ProductReferencesComponent } from './product-references.component';
 
-// @Pipe({
-//   name: 'cxUrl',
-// })
-// class MockUrlPipe implements PipeTransform {
-//   transform(): any {}
-// }
+@Component({
+  selector: 'cx-carousel',
+  template: '',
+})
+class MockCarouselComponent {
+  @Input() items: CarouselItem[];
+  @Input() title: string;
+}
 
-// @Component({
-//   selector: 'cx-media',
-//   template: '',
-// })
-// class MockMediaComponent {
-//   @Input() container;
-// }
+const productCode = 'productCode';
+const product = {
+  code: productCode,
+  name: 'testProduct',
+};
 
-// const productCode = 'productCode';
-// const product = {
-//   code: productCode,
-//   name: 'testProduct',
-// };
+const mockComponentData: CmsProductReferencesComponent = {
+  uid: '001',
+  typeCode: 'ProductReferencesComponent',
+  modifiedTime: new Date('2017-12-21T18:15:15+0000'),
+  productReferenceTypes: 'SIMILAR',
+  title: 'Mock Title',
+  name: 'Mock UIProduct References',
+  container: 'false',
+  displayProductTitles: 'true',
+  displayProductPrices: 'true',
+};
 
-// const list: ProductReference[] = [
-//   { referenceType: 'SIMILAR', target: product },
-//   { referenceType: 'ACCESSORIES', target: product },
-// ];
+const MockCmsProductReferencesComponent = <CmsComponentData<any>>{
+  data$: of(mockComponentData),
+};
 
-// const mockComponentData: CmsProductReferencesComponent = {
-//   uid: '001',
-//   typeCode: 'ProductReferencesComponent',
-//   modifiedTime: new Date('2017-12-21T18:15:15+0000'),
-//   productReferenceTypes: 'SIMILAR',
-//   title: 'Mock Title',
-//   name: 'Mock UIProduct References',
-//   container: 'false',
-//   displayProductTitles: 'true',
-//   displayProductPrices: 'true',
-// };
+class MockCurrentProductService {
+  getProduct(): Observable<Product> {
+    return of(product);
+  }
+}
 
-// const MockCmsComponentData = <CmsComponentData<any>>{
-//   data$: of(mockComponentData),
-// };
+class MockProductCarouselService {
+  getProductReferences() {
+    return of([]);
+  }
+}
 
-// const router = {
-//   state: {
-//     url: '/',
-//     queryParams: {},
-//     params: { productCode },
-//   },
-// };
+describe('ProductReferencesComponent', () => {
+  let productReferencesComponent: ProductReferencesComponent;
+  let fixture: ComponentFixture<ProductReferencesComponent>;
+  //   let el: DebugElement;
 
-// class MockRoutingService {
-//   getRouterState(): Observable<any> {
-//     return of(router);
-//   }
-// }
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [ProductReferencesComponent, MockCarouselComponent],
+      providers: [
+        {
+          provide: CmsComponentData,
+          useValue: MockCmsProductReferencesComponent,
+        },
+        {
+          provide: ProductCarouselService,
+          useClass: MockProductCarouselService,
+        },
+        {
+          provide: CurrentProductService,
+          useClass: MockCurrentProductService,
+        },
+      ],
+    }).compileComponents();
+  }));
 
-// class MockProductReferenceService {
-//   get(): Observable<ProductReference[]> {
-//     return of(list);
-//   }
-// }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductReferencesComponent);
+    productReferencesComponent = fixture.componentInstance;
+    fixture.detectChanges();
+    // el = fixture.debugElement;
+  });
 
-// class MockProductReferencesService {
-//   getTitle = jasmine
-//     .createSpy('getTitle')
-//     .and.callFake(() => of(mockComponentData.title));
-//   fetchTitle = jasmine
-//     .createSpy('fetchTitle')
-//     .and.callFake(() => of(mockComponentData.title));
-//   getDisplayProductTitles = jasmine
-//     .createSpy('getDisplayProductTitles')
-//     .and.callFake(() => of(mockComponentData.displayProductTitles));
-//   fetchDisplayProductTitles = jasmine
-//     .createSpy('fetchDisplayProductTitles')
-//     .and.callFake(() => of(mockComponentData.displayProductTitles));
-//   getDisplayProductPrices = jasmine
-//     .createSpy('getDisplayProductPrices')
-//     .and.callFake(() => of(mockComponentData.displayProductPrices));
-//   fetchDisplayProductPrices = jasmine
-//     .createSpy('fetchDisplayProductPrices')
-//     .and.callFake(() => of(mockComponentData.displayProductPrices));
-//   getReferenceList = jasmine
-//     .createSpy('getReferenceList')
-//     .and.callFake(() => of(list));
-//   setReferenceList = jasmine
-//     .createSpy('setReferenceList')
-//     .and.callFake(() => of([list]));
-// }
-
-// class MockSharedCarouselService {
-//   getItemSize = jasmine.createSpy('getItemSize').and.callFake(() => of(4));
-//   setItemSize = jasmine.createSpy('setItemSize');
-//   getItemAsActive = jasmine
-//     .createSpy('getItemAsActive')
-//     .and.callFake(() => of(0));
-//   setItemAsActive = jasmine
-//     .createSpy('setItemAsActive')
-//     .and.callFake(() => of(1));
-//   setPreviousItemAsActive = jasmine.createSpy('setPreviousItemAsActive');
-//   getActiveItemWithDelay = jasmine.createSpy('getActiveItemWithDelay');
-//   setNextItemAsActive = jasmine.createSpy('setNextItemAsActive');
-//   getDelayValue = jasmine.createSpy('getDelayValue').and.callThrough();
-//   getActiveItem = jasmine.createSpy('getActiveItem').and.callFake(() => of(1));
-// }
-
-// describe('ProductReferencesComponent', () => {
-//   let productReferencesComponent: ProductReferencesComponent;
-//   let fixture: ComponentFixture<ProductReferencesComponent>;
-//   let el: DebugElement;
-
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [RouterTestingModule],
-//       declarations: [
-//         ProductReferencesComponent,
-//         MockMediaComponent,
-//         MockUrlPipe,
-//       ],
-//       providers: [
-//         { provide: CmsComponentData, useValue: MockCmsComponentData },
-//         {
-//           provide: ProductReferenceService,
-//           useClass: MockProductReferenceService,
-//         },
-//         {
-//           provide: RoutingService,
-//           useClass: MockRoutingService,
-//         },
-//       ],
-//     })
-//       .overrideComponent(ProductReferencesComponent, {
-//         set: {
-//           providers: [
-//             {
-//               provide: ProductReferencesService,
-//               useClass: MockProductReferencesService,
-//             },
-//             {
-//               provide: SharedCarouselService,
-//               useClass: MockSharedCarouselService,
-//             },
-//           ],
-//         },
-//       })
-//       .compileComponents();
-//   }));
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ProductReferencesComponent);
-//     productReferencesComponent = fixture.componentInstance;
-//     fixture.detectChanges();
-//     el = fixture.debugElement;
-//   });
-
-//   it('should be created', async(() => {
-//     productReferencesComponent.ngOnInit();
-//     expect(productReferencesComponent).toBeTruthy();
-//   }));
-
-//   it('should have product list', async(() => {
-//     let list$: ProductReference[];
-//     productReferencesComponent.productReferencesService.setReferenceList();
-//     productReferencesComponent.productReferencesService
-//       .getReferenceList()
-//       .subscribe(productData$ => {
-//         list$ = productData$;
-//       })
-//       .unsubscribe();
-//     expect(list$).toBe(list);
-//   }));
-
-//   it('should contain cms content in the html rendering after bootstrap', () => {
-//     expect(el.query(By.css('h3')).nativeElement.textContent).toContain(
-//       mockComponentData.title
-//     );
-//   });
-// });
+  it('should be created', async(() => {
+    expect(productReferencesComponent).toBeTruthy();
+  }));
+});
