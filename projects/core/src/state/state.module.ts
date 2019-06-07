1222a263474c861495dc1c2954f461d0959b56ec
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-06-07 12:24:14 -0400

    switched to META_REDUCERS

--- projects/core/src/state/state.module.ts
+++ projects/core/src/state/state.module.ts
@@ -1,9 +1,8 @@
-import { NgModule, Optional } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule, USER_PROVIDED_META_REDUCERS } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { ConfigModule } from '../config/config.module';
 import { defaultStateConfig } from './config/default-state-config';
-import { metaReducersFactory, META_REDUCER } from './meta-reducer';
 import { stateMetaReducers } from './reducers/index';
 
 @NgModule({
@@ -12,13 +11,6 @@ import { stateMetaReducers } from './reducers/index';
     EffectsModule.forRoot([]),
     ConfigModule.withConfig(defaultStateConfig),
   ],
-  providers: [
-    ...stateMetaReducers,
-    {
-      provide: USER_PROVIDED_META_REDUCERS,
-      useFactory: metaReducersFactory,
-      deps: [[new Optional(), META_REDUCER]],
-    },
-  ],
+  providers: [...stateMetaReducers],
 })
 export class StateModule {}
