6b932a3de4cbee686c53f4f6ad1ab9ab5650e995
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-06-07 09:33:49 -0400

    GH-2920: Fix unit test

--- projects/storefrontlib/src/cms-components/product/product-intro/product-intro.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-intro/product-intro.module.ts
@@ -1,20 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {
-  CmsConfig,
-  CmsModule,
-  ConfigModule,
-  I18nModule,
-} from '@spartacus/core';
-import { OutletModule } from '../../../cms-structure/outlet/index';
+import { CmsConfig, ConfigModule, I18nModule } from '@spartacus/core';
 import { StarRatingModule } from '../../../shared/components/star-rating/star-rating.module';
 import { ProductIntroComponent } from './product-intro.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CmsModule,
-    OutletModule,
     I18nModule,
     StarRatingModule,
     ConfigModule.withConfig(<CmsConfig>{
