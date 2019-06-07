9c32a049851eef54f6bb8f063dffc5c9a0ee9a5a
dunqan <dunqan@w9.pl>
2019-06-07 13:15:39 +0200

    Remove META_REDUCER token and use built in fron NGRX 8

--- projects/core/src/state/reducers/transfer-state.reducer.ts
+++ projects/core/src/state/reducers/transfer-state.reducer.ts
@@ -37,7 +37,7 @@ export function getTransferStateReducer(
     }
   }
 
-  return undefined;
+  return reducer => reducer;
 }
 
 export function getServerTransferStateReducer(
--- projects/core/src/state/state.module.ts
+++ projects/core/src/state/state.module.ts
@@ -1,10 +1,9 @@
-import { NgModule, Optional } from '@angular/core';
-import { StoreModule, USER_PROVIDED_META_REDUCERS } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { ConfigModule } from '../config/config.module';
 import { stateMetaReducers } from './reducers/index';
 import { defaultStateConfig } from './config/default-state-config';
-import { META_REDUCER, metaReducersFactory } from './meta-reducer';
 
 @NgModule({
   imports: [
@@ -12,13 +11,6 @@ import { META_REDUCER, metaReducersFactory } from './meta-reducer';
     EffectsModule.forRoot([]),
     ConfigModule.withConfig(defaultStateConfig),
   ],
-  providers: [
-    ...stateMetaReducers,
-    {
-      provide: USER_PROVIDED_META_REDUCERS,
-      useFactory: metaReducersFactory,
-      deps: [[new Optional(), META_REDUCER]],
-    },
-  ],
+  providers: [...stateMetaReducers],
 })
 export class StateModule {}
