5766f99bd5d693ada961e182e8830661674054e3
maggie.lin <maggie.lin@sap.com>
2019-06-13 13:38:12 +0800

    PANDA-3176 update find product title

--- projects/core/src/product/services/coupon-search-page-meta.resolver.ts
+++ projects/core/src/product/services/coupon-search-page-meta.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SearchPageMetaResolver } from './search-page-meta.resolver';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { TranslationService } from '../../i18n/translation.service';
 import { RoutingService } from '../../routing';
 import { ProductSearchService } from '../facade';
@@ -20,7 +20,12 @@ export class FindProductsPageMetaResolver extends SearchPageMetaResolver {
     if (!query.match(':relevance:category:1')) {
       return super.resolveTitle(total, query);
     } else {
-      return of(total + ' available products for this Coupon');
+      return this.translation.translate(
+        'pageMetaResolver.search.findProductTitle',
+        {
+          count: total,
+        }
+      );
     }
   }
 }
