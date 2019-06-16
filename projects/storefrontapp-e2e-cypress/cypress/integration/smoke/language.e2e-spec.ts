4abe14c68c0363c8368a5c2aa14ef4a4ad6d06e3
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2019-06-16 15:05:58 -0400

    feat: Create e2e tests around language/currency (GH-869) (#2887)
    Closes GH-869

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/language.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/language.e2e-spec.ts
@@ -1,41 +1,25 @@
-// Use language switcher to change language
-function switchLanguage(lang: string) {
-  cy.get('.SiteContext label')
-    .contains('Language')
-    .parent()
-    .children('select')
-    .select(lang);
-}
+import * as siteContextSelector from '../../helpers/site-context-selector';
 
 context('Language Switcher', () => {
-  const BASE_URL = Cypress.config().baseUrl;
-  const CONTENT_CATALOG = 'electronics-spa';
-  const CURRENCY = 'USD';
+  const productPath = siteContextSelector.PRODUCT_PATH_1;
 
   beforeEach(() => {
     cy.server();
-    cy.route(
-      `${Cypress.env(
-        'API_URL'
-      )}/rest/v2/electronics-spa/languages?lang=en&curr=USD`
-    ).as('languages');
+    siteContextSelector.createGerericQuery(
+      siteContextSelector.LANGUAGE_REQUEST,
+      siteContextSelector.LANGUAGES
+    );
   });
 
   describe('Product Page', () => {
-    const PRODUCT_URL_EN = `/${CONTENT_CATALOG}/en/${CURRENCY}/product/`;
-    const PRODUCT_URL_DE = `/${CONTENT_CATALOG}/de/${CURRENCY}/product/`;
-    const PRODUCT_ID = '3595723';
-
     it('switch language should work and language should be persistent in url', () => {
-      // Load Product Page in English
-      cy.visit(`${PRODUCT_URL_EN}${PRODUCT_ID}`);
-      cy.wait('@languages');
-
-      // URL should change to contain 'de' as language after language switch
-      switchLanguage('de');
-      cy.url().should('eq', `${BASE_URL}${PRODUCT_URL_DE}${PRODUCT_ID}`);
-      // clean after test, go back to default english language
-      switchLanguage('en');
+      siteContextSelector.verifySiteContextChangeUrl(
+        productPath,
+        siteContextSelector.LANGUAGES,
+        siteContextSelector.LANGUAGE_DE,
+        siteContextSelector.LANGUAGE_LABEL,
+        siteContextSelector.FULL_BASE_URL_DE_USD + productPath
+      );
     });
   });
 });
