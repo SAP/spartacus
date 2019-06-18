4be53f8295a174db8235cd576cf7782d63a8426e
Marcin Lasak <lasakmarcin90@gmail.com>
2019-06-18 12:32:03 +0200

    refacor: Remove duplicate service

--- projects/core/src/cart/facade/index.ts
+++ projects/core/src/cart/facade/index.ts
@@ -1,6 +1,6 @@
 import { CartDataService } from './cart-data.service';
 import { CartService } from './cart.service';
-export const services: any[] = [CartService, CartDataService, CartDataService];
+export const services: any[] = [CartService, CartDataService];
 
 export * from './cart-data.service';
 export * from './cart.service';
