674294c451084521c417ff7b0cc415bec4b70664
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-06-19 09:53:36 -0400

    site-context done

--- projects/core/src/site-context/store/effects/base-site.effect.ts
+++ projects/core/src/site-context/store/effects/base-site.effect.ts
@@ -1,21 +1,23 @@
-import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-
-import { Effect, Actions, ofType } from '@ngrx/effects';
-import { map, catchError, exhaustMap } from 'rxjs/operators';
-
-import * as actions from '../actions/base-site.action';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { makeHttpErrorSerializable } from '../../../util/serialization-utils';
 import { SiteConnector } from '../../connectors/site.connector';
+import * as actions from '../actions/base-site.action';
 
 @Injectable()
 export class BaseSiteEffects {
   @Effect()
-  loadBaseSite$: Observable<any> = this.actions$.pipe(
+  loadBaseSite$: Observable<Action> = this.actions$.pipe(
     ofType(actions.LOAD_BASE_SITE),
     exhaustMap(() => {
       return this.siteConnector.getBaseSite().pipe(
         map(baseSite => new actions.LoadBaseSiteSuccess(baseSite)),
-        catchError(error => of(new actions.LoadBaseSiteFail(error)))
+        catchError(error =>
+          of(new actions.LoadBaseSiteFail(makeHttpErrorSerializable(error)))
+        )
       );
     })
   );
