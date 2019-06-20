27d39d0a37dca5c4fbd892e45bf9346e147a664a
Terry Tan <terry.tan01@sap.com>
2019-06-20 16:18:26 +0800

    PANDA-3191, update the way to use the variable of disableBtn in coupon component.

--- projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.component.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Cart, CartService, Order } from '@spartacus/core';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'cx-cart-coupon',
@@ -9,6 +10,7 @@ import { Cart, CartService, Order } from '@spartacus/core';
 export class CartCouponComponent implements OnInit {
   form: FormGroup;
   disableBtn: boolean;
+  private subscription = new Subscription();
 
   @Input()
   cart: Cart | Order;
@@ -26,18 +28,29 @@ export class CartCouponComponent implements OnInit {
       couponCode: ['', [Validators.required]],
     });
     this.form.valueChanges.subscribe(() => {
-      this.disableBtn = this.form.valid;
+      this.disableBtn = !this.form.valid;
     });
+
+    this.subscription.add(
+      this.cartService
+        .getAddVoucherResultSuccess()
+        .subscribe(success => this.onSuccess(success))
+    );
   }
 
   applyVoucher(): void {
     this.cartService.addVoucher(this.form.value.couponCode);
 
     //check if the apply action success, then we reset the form.
-    this.form.reset();
   }
 
   removeVoucher(voucherId: string) {
     this.cartService.removeVoucher(voucherId);
+  }
+
+  onSuccess(success: boolean): void {
+    if (success) {
+      this.form.reset();
+    }
   }
 }
