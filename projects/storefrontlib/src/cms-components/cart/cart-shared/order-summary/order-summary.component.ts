e905e9e883ec520cd890e0eae89f2ae62df079c0
RadhepS <Radhep.Saba@gmail.com>
2019-06-20 15:06:12 -0400

    Changes

--- projects/storefrontlib/src/cms-components/cart/cart-shared/order-summary/order-summary.component.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-shared/order-summary/order-summary.component.ts
@@ -8,15 +8,4 @@ import { Cart } from '@spartacus/core';
 export class OrderSummaryComponent {
   @Input()
   cart: Cart;
-
-  getAllAppliedPromotions() {
-    let appliedPromotions = [];
-    if (this.cart.appliedOrderPromotions) {
-      appliedPromotions.push(...this.cart.appliedOrderPromotions);
-    }
-    if (this.cart.appliedProductPromotions) {
-      appliedPromotions.push(...this.cart.appliedProductPromotions);
-    }
-    return appliedPromotions;
-  }
 }
