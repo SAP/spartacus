373955db64ef742d076e3df92a5b4c3c57d4b6f6
Terry Tan <terry.tan01@sap.com>
2019-06-20 17:18:28 +0800

    PANDA-3191, update the UT.

--- projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.component.spec.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.component.spec.ts
@@ -1,14 +1,16 @@
-import { Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CartService, I18nTestingModule } from '@spartacus/core';
+import { Cart, CartService, I18nTestingModule, Voucher } from '@spartacus/core';
+import { of } from 'rxjs';
 import { CartCouponComponent } from './cart-coupon.component';
 import createSpy = jasmine.createSpy;
 
-class MockCartCouponComponent {
-  @Input() cart;
-  @Input() cartIsLoading;
-}
+const coupon1: Voucher = { code: 'coupon1' };
+const coupon2: Voucher = { code: 'coupon2' };
+const cart: Cart = {
+  code: 'xxx',
+  appliedVouchers: [coupon1, coupon2],
+};
 
 describe('CartCouponComponent', () => {
   let component: CartCouponComponent;
@@ -19,11 +21,12 @@ describe('CartCouponComponent', () => {
     mockCartService = {
       applyVoucher: createSpy(),
       removeVoucher: createSpy(),
+      getAddVoucherResultSuccess: createSpy().and.returnValue(of({})),
     };
 
     TestBed.configureTestingModule({
       imports: [I18nTestingModule, ReactiveFormsModule],
-      declarations: [CartCouponComponent, MockCartCouponComponent],
+      declarations: [CartCouponComponent],
       providers: [{ provide: CartService, useValue: mockCartService }],
     }).compileComponents();
   }));
@@ -31,6 +34,7 @@ describe('CartCouponComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CartCouponComponent);
     component = fixture.componentInstance;
+    component.cart = cart;
     fixture.detectChanges();
   });
 
