c8b28131ed5433fdcf1a6a15400d5a23e0c027f1
Miroslaw Grochowski <m.grochowski@sap.com>
2019-06-21 14:02:51 +0200

    fix test

--- projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.spec.ts
+++ projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.spec.ts
@@ -1,6 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  OccConfig,
+  Product,
+  RoutingService,
+  UrlCommandRoute,
+} from '@spartacus/core';
+import { CurrentProductService } from '@spartacus/storefront';
+import { Observable, of } from 'rxjs';
 import { ProductVariantSelectorComponent } from './product-variant-selector.component';
+
+const mockProduct: Product = { name: 'mockProduct' };
+
+class MockRoutingService {
+  go = jasmine.createSpy('go');
+  goByUrl = jasmine.createSpy('goByUrl');
+}
+@Pipe({
+  name: 'cxUrl',
+})
+class MockUrlPipe implements PipeTransform {
+  transform(options: UrlCommandRoute): string {
+    return options.cxRoute;
+  }
+}
+class MockCurrentProductService {
+  getProduct(): Observable<Product> {
+    return of(mockProduct);
+  }
+}
 
 describe('ProductVariantSelectorComponent', () => {
   let component: ProductVariantSelectorComponent;
@@ -8,9 +37,20 @@ describe('ProductVariantSelectorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductVariantSelectorComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductVariantSelectorComponent, MockUrlPipe],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService,
+        },
+        {
+          provide: CurrentProductService,
+          useClass: MockCurrentProductService,
+        },
+        { provide: OccConfig, useValue: {} },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
