1d4ddbfb82c9b0cd0f9c9a236cf7ac7c9a488f79
RadhepS <Radhep.Saba@gmail.com>
2019-06-21 10:43:16 -0400

    Removed fdescribe

--- projects/storefrontlib/src/cms-components/cart/cart-details/cart-details.component.spec.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-details/cart-details.component.spec.ts
@@ -43,7 +43,7 @@ class MockCartItemListComponent {
   cartIsLoading: Observable<boolean>;
 }
 
-fdescribe('CartDetailsComponent', () => {
+describe('CartDetailsComponent', () => {
   let component: CartDetailsComponent;
   let fixture: ComponentFixture<CartDetailsComponent>;
 
