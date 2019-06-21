ad75886843dc4653d2796afb511702ac6e14d2d8
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-06-20 22:31:22 -0400

    process done; store and module barrel files are now consistent

--- projects/core/src/routing/store/index.ts
+++ projects/core/src/routing/store/index.ts
@@ -1,3 +1,4 @@
-export * from './reducers';
-export * from './actions';
-export * from './effects';
+export * from './actions/index';
+export * from './effects/index';
+export * from './reducers/index';
+export * from './state';
