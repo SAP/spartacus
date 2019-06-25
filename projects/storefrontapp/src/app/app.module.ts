05a113cf33e392fe8f37bbef865e52b5de556323
Krzysztof Platis <platonn.git@gmail.com>
2019-06-25 11:51:40 +0200

    revert commit of example baseUrl

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -3,16 +3,10 @@ import localeDe from '@angular/common/locales/de';
 import localeJa from '@angular/common/locales/ja';
 import localeZh from '@angular/common/locales/zh';
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  BrowserTransferStateModule,
-} from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { translationChunksConfig, translations } from '@spartacus/assets';
-import {
-  B2cStorefrontModule,
-  StorefrontComponent,
-} from '@spartacus/storefront';
+import { B2cStorefrontModule, StorefrontComponent } from '@spartacus/storefront';
 import { environment } from '../environments/environment';
 
 registerLocaleData(localeDe);
@@ -34,7 +28,7 @@ if (!environment.production) {
       production: environment.production,
       backend: {
         occ: {
-          baseUrl: 'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
+          baseUrl: environment.occBaseUrl,
           legacy: false,
         },
       },
