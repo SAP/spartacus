5c4dcf2478cd005a0c7d1bf1fde11469f85e6550
Jerify02 <i323629@c02xv131jgh5.dhcp.ctul.sap.corp>
2019-06-26 14:32:18 +0800

    e2e tests

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart.e2e-spec.ts
@@ -1,4 +1,5 @@
 import * as cart from '../../helpers/cart';
+import { user } from '../../sample-data/checkout-flow';
 
 describe('Cart', () => {
   before(() => {
@@ -39,4 +40,54 @@ describe('Cart', () => {
   it('should be unable to add out of stock products to cart', () => {
     cart.outOfStock();
   });
+
+
+
 });
+
+describe.only('Coupon Cart', () => {
+  function placeOrder() {
+    cy.window().then(win => {
+      const savedState = JSON.parse(
+        win.localStorage.getItem('spartacus-local-data')
+      );
+      cy.requireProductAddedToCart(savedState.auth).then(resp => {
+        cy.requireShippingAddressAdded(user.address, savedState.auth);
+        cy.requireShippingMethodSelected(savedState.auth);
+        cy.requirePaymentDone(savedState.auth);
+        cy.requirePlacedOrder(savedState.auth, resp.cartId);
+      });
+    });
+  }
+
+  before(() => {
+    cy.window().then(win => win.sessionStorage.clear());
+    cy.requireLoggedIn();
+    cy.visit('/');
+  });
+
+  it('should show the promotion, discount in price and success message when applied a coupon with cart total action successfully', () => {
+    cart.addProductWhenLoggedIn(false);
+    cart.goToCartAndApplyCoupon();
+  })
+
+  it('should show error message when applied a wrong coupon', () => {
+    cart.addProductWhenLoggedIn(false);
+    cart.applyWrongCoupon();
+  });
+
+  it('should be able to place order with discount promotion and show applied coupon ' +
+    'in order confirmation and order history when applied coupon with cart total action', () => {
+      placeOrder();
+      cy.visit('/order-confirmation')
+    });
+
+  it('should be able to remove the coupon when back to cart and place order without coupon', () => {
+    cart.addProductWhenLoggedIn(false);
+    cart.goToCartAndApplyCoupon();
+    cy.visit('/');
+    cart.goToCartAndRemoveCoupon();
+
+  });
+});
+
