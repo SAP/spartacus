9cfe23541809309513ab85f9a8ad7be2c312b4a6
YangQingyu <qingyu.yang@sap.com>
2019-06-26 18:00:57 +0800

    PANDA-3195 Add e2e test cases

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/coupon/coupon.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/coupon/coupon.e2e-spec.ts
@@ -1,9 +1,9 @@
 import * as cartCoupon from '../../../helpers/cart-coupon';
 import * as bigHappyPath from '../../../helpers/checkout-flow';
 
-export const CouponCode1 = 'BUYMORE16';
-export const CouponCode2 = 'WINTER16';
-export const CouponCode3 = 'CHRISTMAS16';
+export const couponCode1 = 'BUYMORE16';
+export const couponCode2 = 'QingyuCoupon';
+export const productCode = '300938';
 
 describe('Cart Coupon', () => {
   before(() => {
@@ -11,28 +11,60 @@ describe('Cart Coupon', () => {
     cy.visit('/');
   });
 
-  it('should remove the coupon when back to cart and place order without coupon', () => {
-    bigHappyPath.registerUser();
+  function addProductToCart() {
     bigHappyPath.goToProductDetailsPage();
     bigHappyPath.addProductToCart();
+  }
 
-    cartCoupon.navigateToCartPage();
-    cartCoupon.applyCoupon(CouponCode1);
-    cartCoupon.removeCoupon(CouponCode1);
+  function checkout() {
     cartCoupon.navigateToCheckoutPage();
-
     bigHappyPath.fillAddressForm();
     bigHappyPath.chooseDeliveryMethod();
     bigHappyPath.fillPaymentForm();
     bigHappyPath.placeOrder();
+  }
 
+  function verifyChecoutResults(couponCode: string, withCoupon: boolean) {
     bigHappyPath.verifyOrderConfirmationPage();
-    cartCoupon.getCouponItemOrderSummary(CouponCode1).should('not.exist');
+    cartCoupon.verifyCouponInOrderSummary(couponCode, withCoupon);
     bigHappyPath.viewOrderHistory();
+    cartCoupon.navigateToOrderDetailsPage();
+    cartCoupon.verifyCouponInOrderSummary(couponCode, withCoupon);
+  }
 
-    bigHappyPath.goToProductDetailsPage();
-    bigHappyPath.addProductToCart();
-    cartCoupon.navigateToCartPage();
-    cartCoupon.applyCoupon(CouponCode1);
+  describe('Gift Product Coupon', () => {
+    it('should place order with gift product and show applied coupon in order confirmation and order history when applied coupon with gift product action', () => {
+      bigHappyPath.registerUser();
+
+      addProductToCart();
+      cartCoupon.navigateToCartPage();
+      cartCoupon.applyCoupon(couponCode2);
+      cartCoupon.verifyGiftProductCoupon(productCode);
+
+      checkout();
+      verifyChecoutResults(couponCode2, true);
+
+      bigHappyPath.signOut();
+    });
+  });
+
+  describe('Remove Coupon', () => {
+    it('should remove the coupon when back to cart and place order without coupon', () => {
+      bigHappyPath.registerUser();
+
+      addProductToCart();
+      cartCoupon.navigateToCartPage();
+      cartCoupon.applyCoupon(couponCode1);
+      cartCoupon.removeCoupon(couponCode1);
+
+      checkout();
+      verifyChecoutResults(couponCode1, false);
+
+      addProductToCart();
+      cartCoupon.navigateToCartPage();
+      cartCoupon.applyCoupon(couponCode1);
+
+      bigHappyPath.signOut();
+    });
   });
 });
