ba8502f8a3ced012f2766342bcfad8c8d5b61808
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-27 20:43:01 +0200

    fix imports

--- projects/storefrontlib/src/cms-components/content/banner-carousel/banner-carousel.module.ts
+++ projects/storefrontlib/src/cms-components/content/banner-carousel/banner-carousel.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CmsConfig, ConfigModule } from '@spartacus/core';
-import { PageComponentModule } from 'projects/storefrontlib/src/cms-structure';
 import { CarouselModule, MediaModule } from 'projects/storefrontlib/src/shared';
+import { PageComponentModule } from '../../../cms-structure/page/index';
 import { BannerCarouselComponent } from './banner-carousel.component';
 
 @NgModule({
