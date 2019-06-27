e57bc2de5f9fc3835db8088a9b4e666577f28844
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-06-27 20:47:21 +0200

    drop outdated experimental docs

--- projects/storefrontlib/src/cms-components/product/product-images/product-images.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-images/product-images.module.ts
@@ -24,5 +24,6 @@ import { ProductImagesComponent } from './product-images.component';
   ],
   declarations: [ProductImagesComponent],
   entryComponents: [ProductImagesComponent],
+  exports: [ProductImagesComponent],
 })
 export class ProductImagesModule {}
