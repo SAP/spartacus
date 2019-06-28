385d4d07c8a92acd1510b130996d4298e0c21861
Alessandro Ruzzon <a.ruzzon@netconomy.net>
2019-06-28 10:50:00 +0200

    Fixed typos

--- projects/core/src/cms/services/dynamic-attribute.service.ts
+++ projects/core/src/cms/services/dynamic-attribute.service.ts
@@ -6,7 +6,7 @@ import { Injectable, Renderer2 } from '@angular/core';
 export class DynamicAttributeService {
   /**
    * Add dynamic attributes to DOM. These attributes are extracted from the properties of cms items received from backend.
-   * There can by many different groups of properties, one of them is smaredit. But EC allows addons to create different groups.
+   * There can by many different groups of properties, one of them is smartedit. But EC allows addons to create different groups.
    * For example, personalization may add 'script' group etc.
    * @param properties: properties in each cms item response data
    * @param element: slot or cms component element
