7c8a194af2403f8aa1479cbfe0432d870ce92edf
Nikola Zaric <nikola.zaric.ns@gmail.com>
2019-07-02 10:08:30 -0400

    added prod check

--- projects/core/src/util/serialization-utils.ts
+++ projects/core/src/util/serialization-utils.ts
@@ -1,10 +1,16 @@
 import { HttpErrorResponse } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
 import { isObject } from '../config/utils/deep-merge';
 import { ErrorModel, HttpErrorModel } from '../model/misc.model';
 
 export function makeErrorSerializable(
   error: HttpErrorResponse | ErrorModel | any
 ): HttpErrorModel | Error | any {
+  // as ngrx's serialization flags are enabled only in development, we can skip unnecessary computing if running in production
+  if (!isDevMode()) {
+    return error;
+  }
+
   if (error instanceof Error) {
     return {
       message: error.message,
