fa0acfa0d219052007eb38a4be809a7011d10d12
dunqan <dunqan@w9.pl>
2019-07-03 11:57:09 +0200

    get default from config in BaseSite service

--- projects/core/src/site-context/providers/context-service-providers.ts
+++ projects/core/src/site-context/providers/context-service-providers.ts
@@ -3,20 +3,14 @@ import { LanguageService } from '../facade/language.service';
 import { CurrencyService } from '../facade/currency.service';
 import { OccConfig } from '../../occ/config/occ-config';
 import { BaseSiteService } from '../facade/base-site.service';
-import { SiteContextConfig } from '../config/site-context-config';
-import { getContextParameterDefault } from '../config/context-config-utils';
-import { BASE_SITE_CONTEXT_ID } from './context-ids';
 
 export function inititializeContext(
-  config: SiteContextConfig,
   baseSiteService: BaseSiteService,
   langService: LanguageService,
   currService: CurrencyService
 ) {
   return () => {
-    baseSiteService.initialize(
-      getContextParameterDefault(config, BASE_SITE_CONTEXT_ID)
-    );
+    baseSiteService.initialize();
     langService.initialize();
     currService.initialize();
   };
