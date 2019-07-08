b32282d8e40af98699e3ab7613099aa368d998b8
Nikola Zarić <nikola.zaric.ns@gmail.com>
2019-07-08 10:14:39 -0400

    reverted back 8e89ffe

--- projects/core/src/state/reducers/storage-sync.reducer.spec.ts
+++ projects/core/src/state/reducers/storage-sync.reducer.spec.ts
@@ -6,7 +6,7 @@ import {
 } from '../config/default-state-config';
 import { StateConfig, StorageSyncType } from '../config/state-config';
 import {
-  exists,
+  exists, getKeysForStorage,
   getStorage,
   getStorageSyncReducer,
   isSsr,
@@ -62,6 +62,13 @@ describe('storage-sync-reducer', () => {
               access_token: StorageSyncType.SESSION_STORAGE,
               refresh_token: StorageSyncType.LOCAL_STORAGE,
               do_not_sync: StorageSyncType.NO_STORAGE,
+              user: StorageSyncType.SESSION_STORAGE,
+            },
+            excludeKeys: {
+              'user.account.details.currency.name':
+                StorageSyncType.SESSION_STORAGE,
+              'user.account.details.currency.symbol':
+                StorageSyncType.SESSION_STORAGE,
             },
           },
         },
@@ -92,7 +99,7 @@ describe('storage-sync-reducer', () => {
     });
 
     describe('when the action type is NOT UPDATE nor INIT', () => {
-      describe('should set the configured keys to configured storage', () => {
+      describe('should set the configured keys to the configured storage', () => {
         beforeEach(() => {
           spyOn(sessionStorageMock, 'setItem').and.stub();
           spyOn(localStorageMock, 'setItem').and.stub();
@@ -147,6 +154,50 @@ describe('storage-sync-reducer', () => {
             })
           );
         });
+      });
+
+      it('should set the configured keys to the configured storage and exclude specified keys', () => {
+        const state = {
+          user: {
+            account: {
+              addresses: {
+                loading: false,
+              },
+              details: {
+                currency: {
+                  name: 'CAD',
+                  symbol: '$',
+                  value: 100,
+                },
+              },
+            },
+          },
+        };
+        spyOn(sessionStorageMock, 'getItem').and.returnValue(
+          JSON.stringify(state)
+        );
+        spyOn(sessionStorageMock, 'setItem').and.stub();
+
+        const result = reducer(state, { type: 'AN-ACTION' });
+        // excluded keys are not stored in the storage, but they're present in the state
+        expect(result).toEqual(state);
+        expect(sessionStorageMock.setItem).toHaveBeenCalledWith(
+          DEFAULT_SESSION_STORAGE_KEY,
+          JSON.stringify({
+            user: {
+              account: {
+                addresses: {
+                  loading: false,
+                },
+                details: {
+                  currency: {
+                    value: 100,
+                  },
+                },
+              },
+            },
+          })
+        );
       });
     });
   });
@@ -240,6 +291,13 @@ describe('storage-sync-reducer', () => {
         expect(exists({ a: { b: {} } })).toEqual(true);
       });
     });
+    describe('when null is provided instead of keys', () => {
+      it('should return an empty array', () => {
+        const result = getKeysForStorage(null, StorageSyncType.NO_STORAGE);
+        expect(result).toEqual([]);
+      });
+    });
+
   });
 
   describe('getStorage', () => {
--- projects/core/src/state/reducers/storage-sync.reducer.ts
+++ projects/core/src/state/reducers/storage-sync.reducer.ts
@@ -2,7 +2,7 @@ import { Action, ActionReducer, INIT, MetaReducer, UPDATE } from '@ngrx/store';
 import { deepMerge } from '../../config/utils/deep-merge';
 import { WindowRef } from '../../window/window-ref';
 import { StateConfig, StorageSyncType } from '../config/state-config';
-import { filterKeysByType, getStateSlice } from '../utils/get-state-slice';
+import { getStateSlice } from '../utils/get-state-slice';
 
 export function getStorageSyncReducer<T>(
   winRef: WindowRef,
@@ -19,14 +19,6 @@ export function getStorageSyncReducer<T>(
   }
 
   const storageSyncConfig = config.state.storageSync;
-  const localStorageKeys = filterKeysByType(
-    storageSyncConfig.keys,
-    StorageSyncType.LOCAL_STORAGE
-  );
-  const sessionStorageKeys = filterKeysByType(
-    storageSyncConfig.keys,
-    StorageSyncType.SESSION_STORAGE
-  );
 
   return (reducer: ActionReducer<T, Action>): ActionReducer<T, Action> => {
     return (state, action): T => {
@@ -39,8 +31,17 @@ export function getStorageSyncReducer<T>(
 
       if (action.type !== INIT) {
         // handle local storage
+        const localStorageKeys = getKeysForStorage(
+          storageSyncConfig.keys,
+          StorageSyncType.LOCAL_STORAGE
+        );
+        const localStorageExclusionKeys = getKeysForStorage(
+          storageSyncConfig.excludeKeys,
+          StorageSyncType.LOCAL_STORAGE
+        );
         const localStorageStateSlices = getStateSlice(
           localStorageKeys,
+          localStorageExclusionKeys,
           newState
         );
         persistToStorage(
@@ -50,8 +51,17 @@ export function getStorageSyncReducer<T>(
         );
 
         // handle session storage
+        const sessionStorageKeys = getKeysForStorage(
+          storageSyncConfig.keys,
+          StorageSyncType.SESSION_STORAGE
+        );
+        const sessionStorageExclusionKeys = getKeysForStorage(
+          storageSyncConfig.excludeKeys,
+          StorageSyncType.SESSION_STORAGE
+        );
         const sessionStorageStateSlices = getStateSlice(
           sessionStorageKeys,
+          sessionStorageExclusionKeys,
           newState
         );
         persistToStorage(
@@ -64,6 +74,16 @@ export function getStorageSyncReducer<T>(
       return newState;
     };
   };
+}
+
+export function getKeysForStorage(
+  keys: { [key: string]: StorageSyncType },
+  storageType: StorageSyncType
+): string[] {
+  if (!keys) {
+    return [];
+  }
+  return Object.keys(keys).filter(key => keys[key] === storageType);
 }
 
 export function rehydrate<T>(config: StateConfig, winRef: WindowRef): T {
