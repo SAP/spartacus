3a10c914585a3c8bb6fe560cbd65adbd978eef07
Patryk Smul <patryksmul@outlook.com>
2019-07-10 17:07:13 +0200

    fix: export for CheckoutConfigService

--- mock-server/proxy.backend.js
+++ mock-server/proxy.backend.js
@@ -1,4 +1,4 @@
-const BACKEND_SERVER = 'https://localhost:9002';
+const BACKEND_SERVER = 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/';
 
 const PROXY_CONFIG = {
   '/rest': {
