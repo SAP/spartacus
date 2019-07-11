42b7f89e3f83d9459f735077163016d771d50743
Patryk Smul <patryksmul@outlook.com>
2019-07-11 10:27:13 +0200

    revert unnecessary backend mock address change

--- mock-server/proxy.backend.js
+++ mock-server/proxy.backend.js
@@ -1,26 +1,26 @@
-const BACKEND_SERVER = 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/';
+const BACKEND_SERVER = 'https://localhost:9002';
 
 const PROXY_CONFIG = {
   '/rest': {
     target: BACKEND_SERVER,
     secure: false,
     changeOrigin: true,
-    logLevel: 'error'
+    logLevel: 'error',
   },
 
   '/authorizationserver': {
     target: BACKEND_SERVER,
     secure: false,
     changeOrigin: true,
-    logLevel: 'error'
+    logLevel: 'error',
   },
 
   '/medias': {
     target: BACKEND_SERVER,
     secure: false,
     changeOrigin: true,
-    logLevel: 'error'
-  }
+    logLevel: 'error',
+  },
 };
 
 module.exports = PROXY_CONFIG;
