0b5aa28a3642fbf4a6abfba8f757733fa2ab8c1c
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-15 09:43:46 +0200

    fix: Add missing configs

--- scripts/api-extractor.sh
+++ scripts/api-extractor.sh
@@ -8,7 +8,7 @@ yarn build:core:lib
 npm i -g @microsoft/api-extractor
 
 # @spartacus/assets
-cp ./api/api-extractor-assets.json ./dist/assets/api-extractor.json
+cp ./scripts/api-extractor-configs/api-extractor-assets.json ./dist/assets/api-extractor.json
 (
   cd ./dist/assets && \
   api-extractor run --local --verbose
@@ -17,14 +17,14 @@ cp ./api/api-extractor-assets.json ./dist/assets/api-extractor.json
 # @spartacus/core
 # Disabled because of the issue with `import * as `
 #
-# cp ./api/api-extractor-core.json ./dist/core/api-extractor.json
+# cp ./scripts/api-extractor-configs/api-extractor-core.json ./dist/core/api-extractor.json
 # (
 #   cd ./dist/core && \
 #   api-extractor run --local --verbose
 # )
 
 # @spartacus/storefront
-cp ./api/api-extractor-storefrontlib.json ./dist/storefrontlib/api-extractor.json
+cp ./scripts/api-extractor-configs/api-extractor-storefrontlib.json ./dist/storefrontlib/api-extractor.json
 (
   cd ./dist/storefrontlib && \
   api-extractor run --local --verbose
