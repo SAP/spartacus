7e86f4f101aaa789e712416e53b235c27271eefa
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-07-22 09:22:41 -0400

    GH-3229: Order imports

--- projects/core/src/i18n/i18next/i18next-translation.service.spec.ts
+++ projects/core/src/i18n/i18next/i18next-translation.service.spec.ts
@@ -1,10 +1,10 @@
+import * as AngularCore from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { I18nextTranslationService } from './i18next-translation.service';
 import i18next from 'i18next';
 import { first, take } from 'rxjs/operators';
 import { I18nConfig } from '../config/i18n-config';
 import { TranslationChunkService } from '../translation-chunk.service';
-import * as AngularCore from '@angular/core';
+import { I18nextTranslationService } from './i18next-translation.service';
 
 const testKey = 'testKey';
 const testOptions = 'testOptions';
