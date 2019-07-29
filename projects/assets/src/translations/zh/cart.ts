f17883e32a1392080aae63d06f532d974e7754f5
Weizheng Gao <44440575+WeizhengSap@users.noreply.github.com>
2019-07-29 09:13:32 -0400

    Feature/gh 3736 (#3748)
    * feature/GH-3736: add script for localization process

---
+++ projects/assets/src/translations/zh/cart.ts
@@ -0,0 +1,28 @@
+export const cart = {
+  cartDetails: {
+    id: 'ID',
+    proceedToCheckout: 'Proceed to Checkout',
+    cartName: 'Cart #{{code}}',
+  },
+  cartItems: {
+    id: 'ID',
+    description: 'Description',
+    item: 'Item',
+    itemPrice: 'Item price',
+    quantity: 'Qty',
+    quantityTitle:
+      'The quantity represents the total number of this item in your cart.',
+    total: 'Total',
+    cartTotal: 'Cart total ({{count}} item)',
+    cartTotal_plural: 'Cart total ({{count}} items)',
+  },
+  orderCost: {
+    orderSummary: 'Order Summary',
+    subtotal: 'Subtotal after discounts:',
+    estimatedShipping: 'Estimated shipping:',
+    discount: 'You saved:',
+    salesTax: 'Sales Tax:',
+    grossTax: 'The order total does not include tax of',
+    total: 'Total:',
+  },
+};
