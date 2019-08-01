bb77090bf669dcbae11a0659ab97ec25771462c2
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-08-01 16:43:27 -0400

    GH-3784 Adding form validation to checkout login form (in progress)

--- projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
+++ projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
@@ -1,7 +1,43 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { CustomFormValidators } from '../../../shared/utils/validators/custom-form-validators';
 
 @Component({
   selector: 'cx-checkout-login',
   templateUrl: './checkout-login.component.html',
 })
-export class CheckoutLoginComponent {}
+export class CheckoutLoginComponent {
+  checkoutEmailForm: FormGroup = this.formBuilder.group(
+    {
+      email: ['', [Validators.required, CustomFormValidators.emailValidator]],
+      emailConfirmation: [
+        '',
+        Validators.required,
+        CustomFormValidators.emailValidator,
+      ],
+      termsAndConditions: ['', Validators.required],
+    },
+    { validator: this.emailsMatch }
+  );
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  checkout() {
+    const userId = this.checkoutEmailForm.controls.email.value.toLowerCase();
+    return userId;
+  }
+
+  private emailsMatch(abstractControl: AbstractControl): { NotEqual: boolean } {
+    if (
+      abstractControl.get('email').value !==
+      abstractControl.get('emailConfirmation').value
+    ) {
+      return { NotEqual: true };
+    }
+  }
+}
--- projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.module.ts
+++ projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CmsConfig, ConfigModule, I18nModule } from '@spartacus/core';
 import { CheckoutLoginComponent } from './checkout-login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     I18nModule,
+    ReactiveFormsModule,
     ConfigModule.withConfig(<CmsConfig>{
       cmsComponents: {
         GuestCheckoutLoginComponent: {
