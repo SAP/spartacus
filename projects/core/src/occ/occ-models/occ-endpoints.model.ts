79df2459227d810805d5c4a86b9c732f8164b228
Patrick Labadie <patrick.labadie@sap.com>
2019-08-01 12:44:06 -0400

    GH-1797: Remove superfluous doc elements.

--- projects/core/src/occ/occ-models/occ-endpoints.model.ts
+++ projects/core/src/occ/occ-models/occ-endpoints.model.ts
@@ -2,103 +2,103 @@ export interface OccEndpoints {
   /**
    * Get product details
    *
-   * @member {string} [product]
+   * @member {string}
    */
   product?: string;
   /**
    * Get reviews for a product
    *
-   * @member {string} [productReviews]
+   * @member {string}
    */
   productReviews?: string;
   /**
    * Get a list of product references
    *
-   * @member {string} [productReferences]
+   * @member {string}
    */
   productReferences?: string;
   /**
    * Get a list of products and additional data
    *
-   * @member {string} [productSearch]
+   * @member {string}
    */
   productSearch?: string;
   /**
    * Get a list of available suggestions
    *
-   * @member {string} [productSuggestings]
+   * @member {string}
    */
   productSuggestions?: string;
   /**
    * Get CMS component details
    *
-   * @member {string} [component]
+   * @member {string}
    */
   component?: string;
   /**
    * Get a list of CMS component details
    *
-   * @member {string} [components]
+   * @member {string}
    */
   components?: string;
   /**
    * Get page data with list of cms content slots
    *
-   * @member {string} [pages]
+   * @member {string}
    */
   pages?: string;
   /**
    * Get page data with list of cms content slots
    *
-   * @member {string} [page]
+   * @member {string}
    */
   page?: string;
   /**
    * Get a list of available languages
    *
-   * @member {string} [languages]
+   * @member {string}
    */
   languages?: string;
   /**
    * Get a list of available currencies
    *
-   * @member {string} [currencies]
+   * @member {string}
    */
   currencies?: string;
   /**
    * Get a list of countries
    *
-   * @member {string} [countries]
+   * @member {string}
    */
   countries?: string;
   /**
    * Fetch the list of regions for the provided country
    *
-   * @member {string} [regions]
+   * @member {string}
    */
   regions?: string;
   /**
    * Get user details
    *
-   * @member {string} [user]
+   * @member {string}
    */
   user?: string;
   /**
    * Register a new user.
    *
-   * @member {string} [userRegister]
+   * @member {string}
    */
   userRegister?: string;
   /**
    * Request an email to reset the password
    *
-   * @member {string} [userForgotPassword]
+   * @member {string}
    */
   userForgotPassword?: string;
   /**
    * Reset the password once the email is recieved.
    *
-   * @member {string} [userResetPassword]
+   * @member {string}
    */
   userResetPassword?: string;
 }
