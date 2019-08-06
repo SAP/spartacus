01e79c75da429f6e6a41e0e72234c469de5a80bb
Michał Gruca <pucek9@gmail.com>
2019-08-06 18:53:08 +0200

    update

--- projects/storefrontlib/src/cms-components/checkout/services/express-checkout.service.ts
+++ projects/storefrontlib/src/cms-components/checkout/services/express-checkout.service.ts
@@ -1,41 +1,52 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 
-import { CheckoutDetailsService } from './checkout-details.service';
-import { map } from 'rxjs/operators';
 import {
   Address,
-  // CheckoutDeliveryService,
-  // CheckoutPaymentService,
+  CheckoutDeliveryService,
   UserAddressService,
+  UserPaymentService,
+  PaymentDetails,
+  DeliveryMode,
 } from '@spartacus/core';
+import { CheckoutDetailsService } from './checkout-details.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExpressCheckoutService {
   shippingAddress$;
-  // paymentMethod$;
-  // deliveryMode$;
+  deliveryMode$;
+  paymentMethod$;
 
   constructor(
-    // private checkoutDeliveryService: CheckoutDeliveryService,
-    private userAddressService: UserAddressService,
-    // private checkoutPaymentService: CheckoutPaymentService,
-    private checkoutDetailsService: CheckoutDetailsService
+    protected userAddressService: UserAddressService,
+    protected checkoutDeliveryService: CheckoutDeliveryService,
+    protected userPaymentService: UserPaymentService,
+    protected checkoutDetailsService: CheckoutDetailsService
   ) {
+    this.setShippingAddress();
+    this.setDeliveryMode();
+    this.setPaymentMethod();
+  }
+
+  private setShippingAddress() {
     this.shippingAddress$ = this.checkoutDetailsService
       .getDeliveryAddress()
       .pipe(
-        map((deliveryAddress: Address) => {
-          console.log('1', deliveryAddress);
+        switchMap((deliveryAddress: Address) => {
           if (deliveryAddress && Object.keys(deliveryAddress).length) {
-            console.log('2', deliveryAddress);
-            return deliveryAddress;
+            return of(deliveryAddress);
           } else {
-            console.log('3', deliveryAddress);
             return this.userAddressService.getAddresses().pipe(
+              tap(
+                (addresses: Address[]) =>
+                  !Boolean(addresses.length) &&
+                  this.userAddressService.loadAddresses()
+              ),
+              filter((addresses: Address[]) => Boolean(addresses.length)),
               map((addresses: Address[]) => {
-                console.log('4', addresses);
                 return (
                   addresses.find(address => address.defaultAddress) ||
                   addresses[0]
@@ -46,4 +57,72 @@ export class ExpressCheckoutService {
         })
       );
   }
+
+  private setPaymentMethod() {
+    this.paymentMethod$ = this.checkoutDetailsService.getPaymentDetails().pipe(
+      switchMap((cartPaymentMethod: PaymentDetails) => {
+        if (cartPaymentMethod && Object.keys(cartPaymentMethod).length) {
+          return of(cartPaymentMethod);
+        } else {
+          return this.userPaymentService.getPaymentMethods().pipe(
+            tap(
+              (paymentMethods: PaymentDetails[]) =>
+                !Boolean(paymentMethods.length) &&
+                this.userPaymentService.loadPaymentMethods()
+            ),
+            filter((paymentMethods: PaymentDetails[]) =>
+              Boolean(paymentMethods.length)
+            ),
+            map((paymentMethods: PaymentDetails[]) => {
+              return (
+                paymentMethods.find(
+                  paymentMethod => paymentMethod.defaultPayment
+                ) || paymentMethods[0]
+              );
+            })
+          );
+        }
+      })
+    );
+  }
+
+  private setDeliveryMode() {
+    this.deliveryMode$ = this.checkoutDetailsService
+      .getSelectedDeliveryModeCode()
+      .pipe(
+        switchMap((code: string) => {
+          if (code && code.length) {
+            return of(code);
+          } else {
+            return this.checkoutDeliveryService
+              .getSupportedDeliveryModes()
+              .pipe(
+                filter((deliveryModes: DeliveryMode[]) =>
+                  Boolean(deliveryModes.length)
+                ),
+                map((deliveryModes: DeliveryMode[]) => {
+                  deliveryModes.sort((deliveryMode1, deliveryMode2) => {
+                    if (
+                      deliveryMode1.deliveryCost > deliveryMode2.deliveryCost
+                    ) {
+                      return 1;
+                    } else if (
+                      deliveryMode1.deliveryCost < deliveryMode2.deliveryCost
+                    ) {
+                      return -1;
+                    } else {
+                      return 0;
+                    }
+                  });
+                  return deliveryModes[0].code;
+                })
+              );
+          }
+        })
+      );
+  }
+
+  // private setPreferredDeliveryMode() {
+  //
+  // }
 }
