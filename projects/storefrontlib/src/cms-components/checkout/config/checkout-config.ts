776475e8fec330510e2e583fdf0dc5fb938405b0
Michał Gruca <pucek9@gmail.com>
2019-08-06 13:01:26 +0200

    Update projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
    Co-Authored-By: Marcin Lasak <lasakmarcin90@gmail.com>

--- projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
+++ projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
@@ -4,7 +4,7 @@ export abstract class CheckoutConfig {
   checkout?: {
     steps: Array<CheckoutStep>;
     /**
-     * Allow for express checkout if default shipping method and payment method are available.
+     * Allow for express checkout when default shipping method and payment method are available.
      */
     express?: boolean;
   };
