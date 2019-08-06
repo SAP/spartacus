7b680c3e4387f431d6f34f18f9ce308ea0f379f1
Michał Gruca <pucek9@gmail.com>
2019-08-06 12:56:29 +0200

    update

--- projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
+++ projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
@@ -3,6 +3,9 @@ import { CheckoutStep } from '../model/checkout-step.model';
 export abstract class CheckoutConfig {
   checkout?: {
     steps: Array<CheckoutStep>;
+    /**
+     * Allow for express checkout if default shipping method and payment method are available.
+     */
     express?: boolean;
   };
 }
