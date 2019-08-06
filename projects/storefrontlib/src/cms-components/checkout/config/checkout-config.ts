de201d5144e54f18edded5a76235caedc0773f88
Michał Gruca <pucek9@gmail.com>
2019-08-06 13:29:37 +0200

    Add Express checkout config (#3937)
    * Add Express checkout config
    
    * Update projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
    
    Co-Authored-By: Marcin Lasak <lasakmarcin90@gmail.com>

--- projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
+++ projects/storefrontlib/src/cms-components/checkout/config/checkout-config.ts
@@ -3,5 +3,9 @@ import { CheckoutStep } from '../model/checkout-step.model';
 export abstract class CheckoutConfig {
   checkout?: {
     steps: Array<CheckoutStep>;
+    /**
+     * Allow for express checkout when default shipping method and payment method are available.
+     */
+    express?: boolean;
   };
 }
