6190880b555b3d4a45801bd0c6daaaa88d6f8752
Michał Gruca <pucek9@gmail.com>
2019-08-08 13:15:36 +0200

    [WIP] Add expresss checkout gurard #3918 (#3976)
    Closes #3918

--- projects/storefrontlib/src/cms-components/checkout/guards/checkout.guard.ts
+++ projects/storefrontlib/src/cms-components/checkout/guards/checkout.guard.ts
@@ -4,24 +4,47 @@ import { CanActivate, Router, UrlTree } from '@angular/router';
 import { CheckoutConfig } from '../config/checkout-config';
 import { Observable, of } from 'rxjs';
 import { RoutingConfigService } from '@spartacus/core';
+import { ExpressCheckoutService } from '../services/express-checkout.service';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckoutGuard implements CanActivate {
+  firstStep$: Observable<UrlTree>;
+
   constructor(
     private router: Router,
     private config: CheckoutConfig,
-    private routingConfigService: RoutingConfigService
-  ) {}
-
-  canActivate(): Observable<boolean | UrlTree> {
-    return of(
+    private routingConfigService: RoutingConfigService,
+    private expressCheckoutService: ExpressCheckoutService
+  ) {
+    this.firstStep$ = of(
       this.router.parseUrl(
         this.routingConfigService.getRouteConfig(
           this.config.checkout.steps[0].routeName
         ).paths[0]
       )
     );
+  }
+
+  canActivate(): Observable<boolean | UrlTree> {
+    if (this.config.checkout.express) {
+      return this.expressCheckoutService.trySetDefaultCheckoutDetails().pipe(
+        switchMap((expressCheckoutPossible: boolean) => {
+          return expressCheckoutPossible
+            ? of(
+                this.router.parseUrl(
+                  this.routingConfigService.getRouteConfig(
+                    this.config.checkout.steps[3].routeName
+                  ).paths[0]
+                )
+              )
+            : this.firstStep$;
+        })
+      );
+    } else {
+      return this.firstStep$;
+    }
   }
 }
