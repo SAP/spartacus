42d29d09293383d25d65a01f265a7002d54c702f
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-08-08 11:15:27 -0400

    GH-3893: Redirection and unit tests

--- projects/storefrontlib/src/cms-components/checkout/guards/checkout-auth.guard.ts
+++ projects/storefrontlib/src/cms-components/checkout/guards/checkout-auth.guard.ts
@@ -27,10 +27,10 @@ export class CheckoutAuthGuard implements CanActivate {
     ).pipe(
       map(([token, user]) => {
         if (!token.access_token) {
-          console.log(user);
-          if (user && user.name === 'guest') {
-            console.log('in', Boolean(user));
+          console.log('out', user);
 
+          if (user && user.name === 'guest') {
+            console.log('in', user);
             return Boolean(user);
           }
           this.routingService.go({ cxRoute: 'login' }, { forced: true });
@@ -39,19 +39,5 @@ export class CheckoutAuthGuard implements CanActivate {
         return !!token.access_token;
       })
     );
-    // return this.authService.getUserToken().pipe(
-    //   map((token: UserToken) => {
-    //     if (!token.access_token) {
-    //       this.cartService.getActive().pipe(
-    //         map((cart: Cart) => {
-    //           return console.log(cart);
-    //         })
-    //       );
-    //       // this.routingService.go({ cxRoute: 'login' }, { forced: true });
-    //       // this.authRedirectService.reportAuthGuard();
-    //     }
-    //     return !!token.access_token;
-    //   })
-    // );
   }
 }
