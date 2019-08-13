d6ed2fcd506ba8dbcb8d7c89075288f364e0c8a1
Patrick Labadie <patrick.labadie@sap.com>
2019-08-13 16:40:24 -0400

    GH-3914: add tests for CSAgentLoginFormComponent

--- projects/storefrontlib/src/cms-components/asm/csagent-login-form/csagent-login-form.component.spec.ts
+++ projects/storefrontlib/src/cms-components/asm/csagent-login-form/csagent-login-form.component.spec.ts
@@ -1,11 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { I18nTestingModule } from '@spartacus/core';
+import { FormUtils } from '../../../shared/utils/forms/form-utils';
 import { CSAgentLoginFormComponent } from './csagent-login-form.component';
 
 describe('CSAgentLoginFormComponent', () => {
   let component: CSAgentLoginFormComponent;
   let fixture: ComponentFixture<CSAgentLoginFormComponent>;
+  let userIdFormControl: AbstractControl;
+  let passwordFormControl: AbstractControl;
+
+  const validUserId = 'asagent';
+  const validPassword = 'testPass123!';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +25,175 @@ describe('CSAgentLoginFormComponent', () => {
     fixture = TestBed.createComponent(CSAgentLoginFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    userIdFormControl = component.form.controls['userId'];
+    passwordFormControl = component.form.controls['password'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('isNotValid() should delegate to FormUtils.isNotValidField()', () => {
+    spyOn(FormUtils, 'isNotValidField').and.stub();
+
+    component.isNotValid('oldPassword');
+    expect(FormUtils.isNotValidField).toHaveBeenCalledWith(
+      component.form,
+      'oldPassword',
+      component['submitClicked']
+    );
+  });
+
+  describe('onSubmit() ', () => {
+    it('should be called when submit button is clicked', () => {
+      spyOn(component, 'onSubmit').and.stub();
+
+      clickSubmit();
+
+      expect(component.onSubmit).toHaveBeenCalled();
+    });
+
+    it('should NOT emit submited event if the form is not valid', () => {
+      spyOn(component, 'onSubmit').and.stub();
+      spyOn(component.submitEvent, 'emit').and.stub();
+
+      component.onSubmit();
+
+      expect(component.form.valid).toBeFalsy();
+      expect(component.onSubmit).toHaveBeenCalled();
+      expect(component.submitEvent.emit).not.toHaveBeenCalled();
+    });
+
+    it('should emit submited event when the form is valid', () => {
+      spyOn(component.submitEvent, 'emit').and.stub();
+
+      userIdFormControl.setValue(validUserId);
+      passwordFormControl.setValue(validPassword);
+      fixture.detectChanges();
+      component.onSubmit();
+
+      expect(component.form.valid).toBeTruthy();
+      expect(component.submitEvent.emit).toHaveBeenCalled();
+    });
+  });
+
+  describe('Error messages on form submit', () => {
+    it('should NOT display when displaying the form', () => {
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeFalsy();
+        expect(isFormControlDisplayingError('password')).toBeFalsy();
+      });
+    });
+    it('should display when submit an empty form', () => {
+      clickSubmit();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeTruthy();
+        expect(isFormControlDisplayingError('password')).toBeTruthy();
+      });
+    });
+
+    it('should NOT display when all field have valid valies', () => {
+      userIdFormControl.setValue(validUserId);
+      passwordFormControl.setValue(validPassword);
+      clickSubmit();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeFalsy();
+        expect(isFormControlDisplayingError('password')).toBeFalsy();
+      });
+    });
+
+    it('should display when the user submits invalid input', () => {
+      userIdFormControl.setValue('');
+      passwordFormControl.setValue('');
+      clickSubmit();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeTruthy();
+        expect(isFormControlDisplayingError('password')).toBeTruthy();
+      });
+    });
+  });
+
+  describe('Error messages without submit', () => {
+    it('should NOT display for empty abandonment', () => {
+      userIdFormControl.setValue('');
+      userIdFormControl.markAsTouched();
+      passwordFormControl.setValue('');
+      passwordFormControl.markAsTouched();
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeFalsy();
+        expect(isFormControlDisplayingError('password')).toBeFalsy();
+      });
+    });
+    it('should NOT display until the user is finished typing', () => {
+      userIdFormControl.setValue('');
+      userIdFormControl.markAsDirty();
+      passwordFormControl.setValue('');
+      passwordFormControl.markAsDirty();
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeFalsy();
+        expect(isFormControlDisplayingError('password')).toBeFalsy();
+      });
+    });
+
+    it('should display when the user is finished typing invalid input', () => {
+      userIdFormControl.setValue('');
+      userIdFormControl.markAsDirty();
+      userIdFormControl.markAsTouched();
+      passwordFormControl.setValue('');
+      passwordFormControl.markAsDirty();
+      passwordFormControl.markAsTouched();
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeTruthy();
+        expect(isFormControlDisplayingError('password')).toBeTruthy();
+      });
+    });
+    it('should NOT display when the user is finished typing valid input', () => {
+      userIdFormControl.setValue(validUserId);
+      userIdFormControl.markAsDirty();
+      userIdFormControl.markAsTouched();
+      passwordFormControl.setValue(validPassword);
+      passwordFormControl.markAsDirty();
+      passwordFormControl.markAsTouched();
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(isFormControlDisplayingError('userId')).toBeFalsy();
+        expect(isFormControlDisplayingError('password')).toBeFalsy();
+      });
+    });
+  });
+
+  function isFormControlDisplayingError(formControlName: string): boolean {
+    const elementWithErrorMessage = fixture.debugElement.query(
+      By.css(
+        `input[formcontrolname="${formControlName}"] + div.invalid-feedback`
+      )
+    );
+
+    if (!elementWithErrorMessage) {
+      return false;
+    }
+
+    const errorMessage: string =
+      elementWithErrorMessage.nativeElement.innerText;
+    return errorMessage && errorMessage.trim().length > 0;
+  }
+
+  function clickSubmit() {
+    const submitBtn = fixture.debugElement.query(
+      By.css('button[type="submit"]')
+    );
+    submitBtn.nativeElement.dispatchEvent(new MouseEvent('click'));
+  }
 });
