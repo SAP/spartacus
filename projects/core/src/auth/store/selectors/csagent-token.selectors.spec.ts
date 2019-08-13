8bd615933c866c83cfa3cb6c77b9900953e68082
Patrick Labadie <patrick.labadie@sap.com>
2019-08-13 12:40:31 -0400

    GH-3914: Add unit test for csagent token selector

---
+++ projects/core/src/auth/store/selectors/csagent-token.selectors.spec.ts
@@ -0,0 +1,49 @@
+import { Type } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { select, Store, StoreModule } from '@ngrx/store';
+import { UserToken } from '../../models/token-types.model';
+import { AuthActions } from '../actions/index';
+import { StateWithAuth } from '../auth-state';
+import * as fromReducers from '../reducers/index';
+import { AuthSelectors } from './index';
+
+fdescribe('Customer Support Agent Token Selectors', () => {
+  let store: Store<StateWithAuth>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}),
+        StoreModule.forFeature('auth', fromReducers.getReducers()),
+      ],
+    });
+
+    store = TestBed.get(Store as Type<Store<StateWithAuth>>);
+    spyOn(store, 'dispatch').and.callThrough();
+  });
+
+  describe('getCustomerSupportAgentToken', () => {
+    it('should return a customer support agent token from the state', () => {
+      let result: UserToken;
+
+      store
+        .pipe(select(AuthSelectors.getCustomerSupportAgentToken))
+        .subscribe(value => (result = value));
+      expect(result).toEqual(<UserToken>{});
+
+      const testToken: UserToken = {
+        access_token: 'xxx',
+        token_type: 'bearer',
+        refresh_token: 'xxx',
+        expires_in: 1000,
+        scope: ['xxx'],
+        userId: 'xxx',
+      };
+      store.dispatch(
+        new AuthActions.LoadCustomerSupportAgentTokenSuccess(testToken)
+      );
+
+      expect(result).toEqual(testToken);
+    });
+  });
+});
