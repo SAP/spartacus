3e69fb244e253760e9ecea892aabb536fa18b5f9
Caine Rotherham <rotherham.c@gmail.com>
2019-08-14 13:56:03 +0200

    Intercept http request to add token

--- projects/core/src/captcha/http-interceptors/http-captcha.interceptor.ts
+++ projects/core/src/captcha/http-interceptors/http-captcha.interceptor.ts
@@ -6,21 +6,34 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CaptchaSelectors, StateWithCaptcha } from '../store';
+import { select, Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class HttpCaptchaInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private store: Store<StateWithCaptcha>) {}
+
+  // private getToken(): any {
+  //   return this.store.pipe(select(CaptchaSelectors.getCaptchaToken));
+  // }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      map(requestToHandle => {
-        // console.log(requestToHandle);
-        return requestToHandle;
-      })
-    );
+    // this.store.pipe(
+    //   select(CaptchaSelectors.getCaptchaToken),
+    //   map(token => {
+    //     console.log(token);
+    //   })
+    // );
+    request = request.clone({
+      setParams: {
+        token: 'hello',
+      },
+    });
+    // console.log(request);
+    return next.handle(request);
   }
 }
