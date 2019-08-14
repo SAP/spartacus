64ce4ff426196eeb8540e8159925b09f6c3f0591
Stan <dunqan@w9.pl>
2019-08-14 17:47:11 +0200

    fix: Storage Sync Does Not Sync ( right away ) (#4052)
    Closes GH-4048

--- projects/core/src/state/reducers/storage-sync.reducer.ts
+++ projects/core/src/state/reducers/storage-sync.reducer.ts
@@ -35,7 +35,10 @@ export function getStorageSyncReducer<T>(
           storageSyncConfig.keys,
           StorageSyncType.LOCAL_STORAGE
         );
-        const localStorageStateSlices = getStateSlice(localStorageKeys, state);
+        const localStorageStateSlices = getStateSlice(
+          localStorageKeys,
+          newState
+        );
         persistToStorage(
           config.state.storageSync.localStorageKeyName,
           localStorageStateSlices,
@@ -49,7 +52,7 @@ export function getStorageSyncReducer<T>(
         );
         const sessionStorageStateSlices = getStateSlice(
           sessionStorageKeys,
-          state
+          newState
         );
         persistToStorage(
           config.state.storageSync.sessionStorageKeyName,
