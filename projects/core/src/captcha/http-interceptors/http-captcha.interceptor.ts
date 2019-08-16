ba58900724611a3607e31a4d075d3d0f281d3638
Caine Rotherham <rotherham.c@gmail.com>
2019-08-16 10:34:28 +0200

    Remove interceptor logic

--- projects/core/src/captcha/http-interceptors/http-captcha.interceptor.ts
+++ projects/core/src/captcha/http-interceptors/http-captcha.interceptor.ts
@@ -6,34 +6,16 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CaptchaSelectors, StateWithCaptcha } from '../store';
-import { select, Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class HttpCaptchaInterceptor implements HttpInterceptor {
-  constructor(private store: Store<StateWithCaptcha>) {}
-
-  // private getToken(): any {
-  //   return this.store.pipe(select(CaptchaSelectors.getCaptchaToken));
-  // }
+  constructor() {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // this.store.pipe(
-    //   select(CaptchaSelectors.getCaptchaToken),
-    //   map(token => {
-    //     console.log(token);
-    //   })
-    // );
-    request = request.clone({
-      setParams: {
-        token: 'hello',
-      },
-    });
-    // console.log(request);
+    // TODO: Implement handler logic to add captcha token to request
     return next.handle(request);
   }
 }
