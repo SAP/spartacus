53ef6704a377b0ffc877eb2c59aa2bdac4cb639a
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-08-19 15:09:56 -0400

    forgot to add mobile

--- projects/storefrontapp-e2e-cypress/cypress/integration/mobile/my-account/payment-methods-mobile.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/mobile/my-account/payment-methods-mobile.e2e-spec.ts
@@ -8,6 +8,11 @@ import { formats } from '../../../sample-data/viewports';
 describe(`${formats.mobile.width + 1}p resolution - Payment Methods`, () => {
   before(() => {
     cy.window().then(win => win.sessionStorage.clear());
+    cy.viewport(formats.mobile.width, formats.mobile.height);
+  });
+
+  beforeEach(() => {
+    cy.viewport(formats.mobile.width, formats.mobile.height);
   });
 
   checkAnonymous();
