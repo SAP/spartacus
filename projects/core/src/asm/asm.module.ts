aa1822dd3c6da82cdeff05caa65c2a12ee829823
Patrick Labadie <patrick.labadie@sap.com>
2019-08-22 17:31:26 -0400

    GH-3959: remove unrequired config

--- projects/core/src/asm/asm.module.ts
+++ projects/core/src/asm/asm.module.ts
@@ -1,9 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { AuthConfig } from '../auth/config/auth-config';
-import { defaultAuthConfig } from '../auth/config/default-auth-config';
-import { Config, provideConfig } from '../config/config.module';
 import { CustomerServices } from './services/index';
 import { AsmStoreModule } from './store/asm-store.module';
 
@@ -14,11 +11,7 @@ export class AsmModule {
   static forRoot(): ModuleWithProviders<AsmModule> {
     return {
       ngModule: AsmModule,
-      providers: [
-        ...CustomerServices,
-        { provide: AuthConfig, useExisting: Config },
-        provideConfig(defaultAuthConfig),
-      ],
+      providers: [...CustomerServices],
     };
   }
 }
