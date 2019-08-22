20cdd71e588742e91678f88c31eb3ba6334cda1e
Patrick Labadie <patrick.labadie@sap.com>
2019-08-22 15:13:24 -0400

    GH-3959: Add test coverage for the customer search selector

--- projects/core/src/asm/store/selectors/customer-search.selectors.spec.ts
+++ projects/core/src/asm/store/selectors/customer-search.selectors.spec.ts
@@ -1,8 +1,25 @@
 import { Type } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { Store, StoreModule } from '@ngrx/store';
+import { select, Store, StoreModule } from '@ngrx/store';
+import { User } from '../../../model/misc.model';
+import { CustomerSearchPage } from '../../models/asm.models';
+import { CustomerActions } from '../actions';
 import { StateWithAsm } from '../asm-state';
 import * as fromReducers from '../reducers/index';
+import { AsmSelectors } from './index';
+
+const mockUser: User = {
+  displayUid: 'Display Uid',
+  firstName: 'First',
+  lastName: 'Last',
+  name: 'First Last',
+  uid: 'user@test.com',
+  customerId: '123456',
+};
+
+const mockCustomerSearchPage: CustomerSearchPage = {
+  entries: [mockUser],
+} as CustomerSearchPage;
 
 describe('Customer Search Results Selectors', () => {
   let store: Store<StateWithAsm>;
@@ -19,7 +36,18 @@ describe('Customer Search Results Selectors', () => {
     spyOn(store, 'dispatch').and.callThrough();
   });
 
-  describe('get Customer Search Results', () => {
-    it('should pass', () => {});
+  it('should return a Customer Search results from the state', () => {
+    let result: CustomerSearchPage;
+
+    store
+      .pipe(select(AsmSelectors.getCustomerSearchResults))
+      .subscribe(value => (result = value));
+    expect(result).toEqual(<CustomerSearchPage>{});
+
+    store.dispatch(
+      new CustomerActions.CustomerSearchSuccess(mockCustomerSearchPage)
+    );
+
+    expect(result).toEqual(mockCustomerSearchPage);
   });
 });
