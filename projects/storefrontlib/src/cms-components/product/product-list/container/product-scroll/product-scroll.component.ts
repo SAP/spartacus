92259a537b285d14209c9bffc4c98a3c5555314b
RadhepS <Radhep.Saba@gmail.com>
2019-08-26 09:46:07 -0400

    changes

--- projects/storefrontlib/src/cms-components/product/product-list/container/product-scroll/product-scroll.component.ts
+++ projects/storefrontlib/src/cms-components/product/product-list/container/product-scroll/product-scroll.component.ts
@@ -26,10 +26,10 @@ export class ProductScrollComponent implements OnDestroy {
   inputViewMode: ViewModes;
   @Input('inputViewMode')
   set setViewMode(inputViewMode: ViewModes) {
+    this.inputViewMode = inputViewMode;
     //If viewMode is already set (meaning it is not the first load)
     //Reset the product list
     if (this.viewMode) {
-      this.inputViewMode = inputViewMode;
       this.resetListOnViewModeChange();
     } else {
       //If viewMode is not set (meaning it is the first load)
