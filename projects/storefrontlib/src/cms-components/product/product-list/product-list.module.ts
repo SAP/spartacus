f4ecbb1bb0893d96975c03f1c3661f2daef59caa
RadhepS <Radhep.Saba@gmail.com>
2019-08-27 13:42:28 -0400

    changes

--- projects/storefrontlib/src/cms-components/product/product-list/product-list.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-list/product-list.module.ts
@@ -24,8 +24,8 @@ import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductScrollComponent } from './container/product-scroll/product-scroll.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { defaultScrollConfig } from '../config/default-scroll-config';
-import { ViewConfigModule } from 'projects/core/src/config/view-config/view-config.module';
 import { ViewConfig } from '../../../view-config';
+import { ViewConfigModule } from '../../../../../core/src/config/view-config/view-config.module';
 
 @NgModule({
   imports: [
