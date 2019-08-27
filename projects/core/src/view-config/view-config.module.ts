b03d33305e574df76d0a31baced1c364ab411a7b
RadhepS <Radhep.Saba@gmail.com>
2019-08-27 17:24:39 -0400

    changes

--- projects/core/src/view-config/view-config.module.ts
+++ projects/core/src/view-config/view-config.module.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ViewConfig } from './config/view-config';
-import { Config } from '../config';
+import { Config, provideConfig } from '../config';
 
 @NgModule({})
 export class ViewConfigModule {
@@ -8,6 +8,7 @@ export class ViewConfigModule {
     return {
       ngModule: ViewConfigModule,
       providers: [
+        provideConfig({}),
         {
           provide: ViewConfig,
           useExisting: Config,
