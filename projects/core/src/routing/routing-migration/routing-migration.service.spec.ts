72438b888d187fd60c12aa81b045707f0fec9401
Krzysztof Platis <platonn.git@gmail.com>
2019-08-28 13:37:45 +0200

    fix unit tests for migration service

--- projects/core/src/routing/routing-migration/routing-migration.service.spec.ts
+++ projects/core/src/routing/routing-migration/routing-migration.service.spec.ts
@@ -1,46 +1,31 @@
-import { InjectionToken, NgZone, Type } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import {
-  RoutingMigrationConfig,
-  RoutingMigrationConfigType,
-} from './routing-migration-config';
+import { UrlMatcherFactoryService } from '../services/url-matcher-factory.service';
+import { RoutingMigrationConfig } from './routing-migration-config';
 import { RoutingMigrationGuard } from './routing-migration.guard';
 import { RoutingMigrationService } from './routing-migration.service';
-
-const InternalRouteGuard = new InjectionToken('InternalRouteGuard');
 
 describe('RoutingMigrationService', () => {
   let service: RoutingMigrationService;
   let router: Router;
-  let zone: NgZone;
-  let internalRouteGuard: CanActivate;
-  let migrationGuard: CanActivate;
 
-  function beforeEachWithConfig(config: RoutingMigrationConfig) {
-    internalRouteGuard = {
-      canActivate: jasmine
-        .createSpy('testGuard.canActivate')
-        .and.returnValue(false),
-    };
-    migrationGuard = {
-      canActivate: jasmine
-        .createSpy('migrationGuard.canActivate')
-        .and.returnValue(false),
+  beforeEach(() => {
+    const mockUrlMatcherFactoryService: Partial<UrlMatcherFactoryService> = {
+      getOppositeUrlMatcher: matcher => `oppositeTo-${matcher}` as any,
+      getGlobUrlMatcher: patterns => `globUrlMatcherFor-${patterns[0]}` as any,
     };
 
     TestBed.configureTestingModule({
       providers: [
         RoutingMigrationService,
-        { provide: InternalRouteGuard, useValue: internalRouteGuard },
-        {
-          provide: RoutingMigrationGuard,
-          useValue: migrationGuard,
-        },
         {
           provide: RoutingMigrationConfig,
-          useValue: config,
+          useValue: { routing: { internal: ['/internal/path/pattern'] } },
+        },
+        {
+          provide: UrlMatcherFactoryService,
+          useValue: mockUrlMatcherFactoryService,
         },
       ],
 
@@ -48,7 +33,6 @@ describe('RoutingMigrationService', () => {
         RouterTestingModule.withRoutes([
           {
             path: '**',
-            canActivate: [InternalRouteGuard],
             component: {} as any,
           },
         ]),
@@ -57,104 +41,21 @@ describe('RoutingMigrationService', () => {
 
     service = TestBed.get(RoutingMigrationService);
     router = TestBed.get(Router);
-    zone = TestBed.get(NgZone as Type<NgZone>);
-  }
-
-  describe('addMigrationRoutes', () => {
-    describe('when configured explicitly external paths', () => {
-      beforeEach(() => {
-        beforeEachWithConfig({
-          routing: {
-            migration: {
-              paths: ['path/with/param/:someParam', 'normal/path', ''],
-              type: RoutingMigrationConfigType.EXTERNAL,
-            },
-          },
-        });
-      });
-
-      it('should prepend the migration route for configured URLs', () => {
-        expect(router.config.length).toBe(1);
-        service.addMigrationRoutes();
-        expect(router.config.length).toBe(2);
-        expect(router.config[0].canActivate[0]).toBe(RoutingMigrationGuard);
-      });
-
-      it('should not redirect for internal routes', async () => {
-        service.addMigrationRoutes();
-        await zone.run(() => router.navigate(['test-route']));
-        expect(internalRouteGuard.canActivate).toHaveBeenCalled();
-        expect(migrationGuard.canActivate).not.toHaveBeenCalled();
-      });
-
-      it('should redirect for external routes - root path', async () => {
-        service.addMigrationRoutes();
-        await zone.run(() => router.navigate(['/']));
-        expect(internalRouteGuard.canActivate).not.toHaveBeenCalled();
-        expect(migrationGuard.canActivate).toHaveBeenCalled();
-      });
-
-      it('should redirect for external routes - normal path', async () => {
-        service.addMigrationRoutes();
-        await zone.run(() => router.navigate(['normal/path']));
-        expect(internalRouteGuard.canActivate).not.toHaveBeenCalled();
-        expect(migrationGuard.canActivate).toHaveBeenCalled();
-      });
-
-      it('should redirect for external routes - path with param', async () => {
-        service.addMigrationRoutes();
-        await zone.run(() => router.navigate(['path/with/param/1234']));
-        expect(internalRouteGuard.canActivate).not.toHaveBeenCalled();
-        expect(migrationGuard.canActivate).toHaveBeenCalled();
-      });
-    });
   });
 
-  describe('when configured explicitly internal paths', () => {
-    beforeEach(() => {
-      beforeEachWithConfig({
-        routing: {
-          migration: {
-            paths: ['path/with/param/:someParam', 'normal/path', ''],
-            type: RoutingMigrationConfigType.INTERNAL,
-          },
-        },
-      });
-    });
-
-    it('should prepend the migration route for configured URLs', () => {
+  describe('addMigrationRoutes', () => {
+    it('should prepend the migration route with a special guard', () => {
       expect(router.config.length).toBe(1);
       service.addMigrationRoutes();
       expect(router.config.length).toBe(2);
       expect(router.config[0].canActivate[0]).toBe(RoutingMigrationGuard);
     });
 
-    it('should redirect for external routes', async () => {
+    it('should prepend the migration route not accepting internal routes', () => {
       service.addMigrationRoutes();
-      await zone.run(() => router.navigate(['test-route']));
-      expect(internalRouteGuard.canActivate).not.toHaveBeenCalled();
-      expect(migrationGuard.canActivate).toHaveBeenCalled();
-    });
-
-    it('should not redirect for internal routes - root path', async () => {
-      service.addMigrationRoutes();
-      await zone.run(() => router.navigate(['/']));
-      expect(internalRouteGuard.canActivate).toHaveBeenCalled();
-      expect(migrationGuard.canActivate).not.toHaveBeenCalled();
-    });
-
-    it('should not redirect for internal routes - normal path', async () => {
-      service.addMigrationRoutes();
-      await zone.run(() => router.navigate(['normal/path']));
-      expect(internalRouteGuard.canActivate).toHaveBeenCalled();
-      expect(migrationGuard.canActivate).not.toHaveBeenCalled();
-    });
-
-    it('should not redirect for external routes - path with param', async () => {
-      service.addMigrationRoutes();
-      await zone.run(() => router.navigate(['path/with/param/1234']));
-      expect(internalRouteGuard.canActivate).toHaveBeenCalled();
-      expect(migrationGuard.canActivate).not.toHaveBeenCalled();
+      expect(router.config[0].matcher).toEqual(
+        'oppositeTo-globUrlMatcherFor-/internal/path/pattern'
+      );
     });
   });
 });
