c2eb46d7e4d88015ee114f997223484ddd63e532
Patrick Labadie <patrick.labadie@sap.com>
2019-08-30 17:08:29 -0400

    GH-3913: implement unit test

--- projects/storefrontlib/src/components/asm/asm-root/asm-root.component.spec.ts
+++ projects/storefrontlib/src/components/asm/asm-root/asm-root.component.spec.ts
@@ -1,25 +1,64 @@
+import {
+  Component,
+  DebugElement,
+  Directive,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { AsmRootComponent } from './asm-root.component';
 
-describe('AsmRootComponent', () => {
+@Component({
+  selector: 'cx-asm-main-ui',
+  template: '',
+})
+class MockAsmMainUiComponent {}
+
+@Directive({
+  selector: '[cxFeature]',
+})
+class MockFeatureLevelDirective {
+  constructor(
+    protected templateRef: TemplateRef<any>,
+    protected viewContainer: ViewContainerRef
+  ) {}
+
+  @Input() set cxFeature(_feature: string) {
+    this.viewContainer.createEmbeddedView(this.templateRef);
+  }
+}
+
+fdescribe('AsmRootComponent', () => {
   let component: AsmRootComponent;
   let fixture: ComponentFixture<AsmRootComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AsmRootComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        AsmRootComponent,
+        MockAsmMainUiComponent,
+        MockFeatureLevelDirective,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AsmRootComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the asm main ui', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(el.query(By.css('cx-asm-main-ui'))).toBeTruthy();
   });
 });
