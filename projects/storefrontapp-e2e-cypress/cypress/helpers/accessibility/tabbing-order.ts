083edd5d124d715bacd9c92c5f3b8beb88388dd0
Caine Rotherham <rotherham.c@gmail.com>
2019-09-02 10:06:58 +0200

    feat: Tabbing Order E2E Tests - Footer (#4159)
    Closes: #4159

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.ts
@@ -1,26 +1,39 @@
 import { TabbingOrderTypes } from './tabbing-order.config';
 
-export function checkElement(value: string, type: TabbingOrderTypes) {
-  if (!(value && value.length)) {
+export interface TabElement {
+  value: string;
+  type: TabbingOrderTypes;
+}
+
+export function checkElement(tabElement: TabElement) {
+  if (!(tabElement.value && tabElement.value.length)) {
     return;
   }
 
-  switch (type) {
+  switch (tabElement.type) {
     case TabbingOrderTypes.FORM_FIELD: {
-      cy.focused().should('have.attr', 'formcontrolname', value);
+      cy.focused().should('have.attr', 'formcontrolname', tabElement.value);
       break;
     }
     case TabbingOrderTypes.LINK: {
-      cy.focused().should('contain', value);
+      cy.focused().should('contain', tabElement.value);
       break;
     }
     case TabbingOrderTypes.BUTTON: {
-      cy.focused().should('contain', value);
+      cy.focused().should('contain', tabElement.value);
       break;
     }
   }
 
   cy.tab(); // after the above check, tab to the next element
+}
+
+export function checkAllElements(tabElements: TabElement[]) {
+  it('should focus elements in correct order when pressing tab key', () => {
+    tabElements.forEach(el => {
+      checkElement(el);
+    });
+  });
 }
 
 export function getFormFieldByValue(value: string) {
