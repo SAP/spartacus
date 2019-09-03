0f42c643e9fff4b814b871657d679b4762b6e1d5
Caine Rotherham <rotherham.c@gmail.com>
2019-09-03 09:01:28 +0200

    Add update email e2e test

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
@@ -26,6 +26,28 @@ export const tabbingOrderConfig = {
       type: TabbingOrderTypes.BUTTON,
     },
   ],
+  updateEmail: [
+    {
+      value: 'email',
+      type: TabbingOrderTypes.FORM_FIELD,
+    },
+    {
+      value: 'confirmEmail',
+      type: TabbingOrderTypes.FORM_FIELD,
+    },
+    {
+      value: 'password',
+      type: TabbingOrderTypes.FORM_FIELD,
+    },
+    {
+      value: 'Cancel',
+      type: TabbingOrderTypes.BUTTON,
+    },
+    {
+      value: 'Save',
+      type: TabbingOrderTypes.BUTTON,
+    },
+  ],
   footer: [
     { value: 'About SAP Commerce Cloud', type: TabbingOrderTypes.LINK },
     { value: 'Frequently Asked Questions', type: TabbingOrderTypes.LINK },
