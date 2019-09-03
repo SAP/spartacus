113fd55c7015bdd998c5dd32f0b7d454bb4886e7
Caine Rotherham <rotherham.c@gmail.com>
2019-09-03 15:07:49 +0200

    Update register.ts

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/register.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/register.ts
@@ -7,7 +7,7 @@ import { fillRegistrationForm } from '../../auth-forms';
 import { user } from '../../../sample-data/checkout-flow';
 
 export function registerTabbingOrder(config: TabElement[]) {
-  cy.visit('/electronics-spa/en/USD/login/register');
+  cy.visit('/login/register');
   fillRegistrationForm(user); // fill form to enable submit button
   getFormFieldByValue(config[0].value).focus(); // focus the first element
   checkAllElements(config);
