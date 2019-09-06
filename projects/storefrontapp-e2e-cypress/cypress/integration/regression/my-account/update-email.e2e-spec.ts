6a9c96346a58d3eefa2bfa3cb121cad19aa3a6f9
KateChuen <45409515+KateChuen@users.noreply.github.com>
2019-09-06 11:26:55 -0400

    adding myaccount tests for mobile part1 (#4251)
    * adding myaccount tests for mobile part1
    
    * my account mobile tests part2
    
    * adding myaccount tests for mobile part1
    
    * resolved conflict

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/update-email.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/update-email.e2e-spec.ts
@@ -1,73 +1,30 @@
-import { login } from '../../../helpers/auth-forms';
-import { checkBanner } from '../../../helpers/homepage';
-import { generateMail, randomString } from '../../../helpers/user';
-import * as alerts from './../../../helpers/global-message';
+import {
+  updateEmailTest,
+  verifyAsAnonymous,
+} from '../../../helpers/update-email';
+import { formats } from '../../../sample-data/viewports';
 
-const UPDATE_EMAIL = '/my-account/update-email';
-const password = 'Password123.';
-const newUid = generateMail(randomString(), true);
+describe('Update Email Address page', () => {
+  before(() =>
+    cy.window().then(win => {
+      win.sessionStorage.clear();
+    })
+  );
 
-describe('Update email', () => {
+  verifyAsAnonymous();
+  updateEmailTest();
+});
+
+describe(`${formats.mobile.width + 1}p resolution - Update Email page`, () => {
   before(() => {
     cy.window().then(win => win.sessionStorage.clear());
-    cy.visit('/');
+    cy.viewport(formats.mobile.width, formats.mobile.height);
   });
 
-  describe('when an anonymous user tries to access the page', () => {
-    it('should be redirected to the login page', () => {
-      cy.visit(UPDATE_EMAIL);
-      cy.location('pathname').should('contain', '/login');
-    });
+  beforeEach(() => {
+    cy.viewport(formats.mobile.width, formats.mobile.height);
   });
 
-  describe('when a user is logged in', () => {
-    before(() => {
-      cy.requireLoggedIn();
-    });
-
-    beforeEach(() => {
-      cy.restoreLocalStorage();
-      cy.visit(UPDATE_EMAIL);
-    });
-
-    afterEach(() => {
-      cy.saveLocalStorage();
-    });
-
-    it('should be able to cancel and go back to home', () => {
-      cy.get('cx-update-email-form button[type="button"]').click();
-      checkBanner();
-
-      cy.location('pathname').should('contain', '/');
-    });
-
-    it('should be able to update its email', () => {
-      cy.get('cx-breadcrumb h1').should('exist');
-
-      cy.get('cx-update-email-form [formcontrolname="email"]').type(newUid);
-      cy.get('cx-update-email-form [formcontrolname="confirmEmail"]').type(
-        newUid
-      );
-      cy.get('cx-update-email-form [formcontrolname="password"]').type(
-        password
-      );
-
-      cy.get('cx-update-email-form button[type="submit"]').click();
-
-      cy.get('cx-login-form').should('exist');
-
-      alerts
-        .getSuccessAlert()
-        .should('contain', `Success. Please sign in with ${newUid}`);
-    });
-  });
-
-  describe('when a user can login with the new email', () => {
-    it('should be able to login with its new email', () => {
-      login(newUid, password);
-      // TODO: uncomment below component and remove update-email assertion when #1957 is implemented
-      cy.get('cx-update-email').should('exist');
-      // checkBanner();
-    });
-  });
+  verifyAsAnonymous();
+  updateEmailTest();
 });
