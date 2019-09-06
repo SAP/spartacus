f3fb9cce8afcd4e6ac95f9b120b6f15c3c114655
Marcin Lasak <lasakmarcin90@gmail.com>
2019-09-06 10:33:32 +0200

    chore: rollback ci script changes

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -18,6 +18,4 @@ if [ $SUITE == 'regression' ]; then
     yarn e2e:cy:start-run-mobile-ci
 else
     yarn e2e:cy:start-run-ci
-    yarn e2e:cy:start-run-mobile-ci
-    yarn e2e:cy:start-run-regression-ci
 fi
