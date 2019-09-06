75eacf2117c990824416186e96d0bdf4dacce980
Weizheng Gao <weizheng.gao@sap.com>
2019-09-06 15:06:57 -0400

    feature/GH-1225: check order code is guid or not

--- projects/core/src/user/connectors/order/user-order.connector.ts
+++ projects/core/src/user/connectors/order/user-order.connector.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserOrderAdapter } from './user-order.adapter';
 import { Order, OrderHistoryList } from '../../../model/order.model';
-import { ANONYMOUS_USERID } from '../../../cart/facade/cart-data.service';
+import { GUID_PATTERN } from '../../../util';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +11,7 @@ export class UserOrderConnector {
   constructor(protected adapter: UserOrderAdapter) {}
 
   public get(userId: string, orderCode: string): Observable<Order> {
-    if (userId === ANONYMOUS_USERID) {
+    if (this.isGuid(orderCode)) {
       return this.adapter.loadByCode(orderCode);
     }
     return this.adapter.load(userId, orderCode);
@@ -24,5 +24,9 @@ export class UserOrderConnector {
     sort?: string
   ): Observable<OrderHistoryList> {
     return this.adapter.loadHistory(userId, pageSize, currentPage, sort);
+  }
+
+  protected isGuid(str: string): boolean {
+    return str.match(GUID_PATTERN) ? true : false;
   }
 }
