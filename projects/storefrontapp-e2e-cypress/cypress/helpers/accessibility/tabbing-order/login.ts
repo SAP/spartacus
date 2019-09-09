f58eb41947dc433817986f5f29ae305f51592793
Kacper Knapik <kknapik@divante.pl>
2019-09-09 11:05:33 +0200

    feat: add second test for login page (#4474)
    Closes #4368

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/login.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/login.ts
@@ -3,9 +3,15 @@ import {
   checkAllElements,
   TabElement,
 } from '../tabbing-order';
+import { fillLoginForm } from '../../auth-forms';
+import { user } from '../../../sample-data/checkout-flow';
 
-export function loginTabbingOrder(config: TabElement[]) {
+export function loginTabbingOrder(config: TabElement[], prefillForm: boolean = false) {
   cy.visit('/login');
+  if (prefillForm) {
+    const { email: username, password } = user;
+    fillLoginForm({ username, password });
+  }
   getFormFieldByValue(config[0].value).focus();
 
   checkAllElements(config);
