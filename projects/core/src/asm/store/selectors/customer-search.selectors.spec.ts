007d8e05d9dbdba9ecd651cec96c0c301ff49429
Patrick Labadie <patrick.labadie@sap.com>
2019-09-09 13:03:58 -0400

    GH-4241: Remove fdescribe.

--- projects/core/src/asm/store/selectors/customer-search.selectors.spec.ts
+++ projects/core/src/asm/store/selectors/customer-search.selectors.spec.ts
@@ -21,7 +21,7 @@ const mockCustomerSearchPage: CustomerSearchPage = {
   entries: [mockUser],
 } as CustomerSearchPage;
 
-fdescribe('Customer Search Results Selectors', () => {
+describe('Customer Search Results Selectors', () => {
   let store: Store<StateWithAsm>;
 
   beforeEach(() => {
