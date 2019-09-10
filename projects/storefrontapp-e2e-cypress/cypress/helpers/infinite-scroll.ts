8b0a9c5c22c9dcfde84132dbfcae3cdbc770829d
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-09-10 15:54:43 -0400

    hold 2

--- projects/storefrontapp-e2e-cypress/cypress/helpers/infinite-scroll.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/infinite-scroll.ts
@@ -1,5 +1,7 @@
 import { createProductQuery } from './product-search';
 
+var defaultProductLimit = 10;
+
 export function scrollConfig(
   active: boolean,
   productLimit: number,
@@ -23,9 +25,26 @@ export function verifyProductListLoaded() {
   cy.wait('@product');
 }
 
-export function scrollToFooter() {
-  cy.get('cx-footer-navigation').scrollIntoView({
-    duration: 1000,
-    easing: 'linear',
+// tslint:disable-next-line: no-shadowed-variable
+export function scrollToFooter(productLimit?: number) {
+  if (productLimit) {
+    defaultProductLimit = productLimit;
+  }
+
+  cy.get('cx-breadcrumb h1').should($breadcrumb => {
+    const breadcrumbQuantityNumber = Number($breadcrumb.text().split(' ')[0]);
+    const numberOfIteration = Math.ceil(
+      breadcrumbQuantityNumber / productLimit
+    );
+    console.log('text', numberOfIteration);
+
+    for (let i = 0; i <= numberOfIteration; i++) {
+      cy.get('cx-footer-navigation').scrollIntoView({
+        duration: 1000,
+        easing: 'linear',
+      });
+    }
   });
+
+  // const numberOfIteration = Math.ceil();
 }
