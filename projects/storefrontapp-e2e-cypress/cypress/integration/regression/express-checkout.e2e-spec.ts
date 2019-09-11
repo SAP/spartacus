a368c16b5e6096a0519deac003cdd7c7f6f519e9
Michał Gruca <pucek9@gmail.com>
2019-09-11 14:59:56 +0200

    created e2e tests

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/express-checkout.e2e-spec.ts
@@ -0,0 +1,66 @@
+import * as checkout from '../../helpers/checkout-flow';
+import { deletePayment } from '../../helpers/payment-methods';
+
+context('Express checkout', () => {
+  before(() => {
+    cy.window().then(win => win.sessionStorage.clear());
+    cy.cxConfig({ checkout: { express: true } });
+    cy.visit('/');
+  });
+
+  it('should go to checkout', () => {
+    checkout.registerUser();
+    checkout.goToCheapProductDetailsPage();
+    checkout.addCheapProductToCartAndLogin();
+  });
+
+  it('should fill address form', () => {
+    checkout.fillAddressFormWithCheapProduct();
+  });
+
+  it('should choose delivery', () => {
+    checkout.chooseDeliveryMethod();
+  });
+
+  it('should fill in payment form with billing address same as shipping address', () => {
+    checkout.fillPaymentFormWithCheapProduct();
+  });
+
+  it('should redirect to review order page', () => {
+    checkout.verifyReviewOrderPage();
+  });
+
+  it('should open cart', () => {
+    cy.get('cx-mini-cart').click();
+  });
+
+  it('should click proceed to checkout', () => {
+    cy.getByText(/proceed to checkout/i).click();
+  });
+
+  it('should redirect to review order page', () => {
+    checkout.verifyReviewOrderPage();
+  });
+
+  it('should delete payment', () => {
+    cy.selectUserMenuOption({
+      option: 'Payment Details',
+    });
+    cy.getAllByText('Delete')
+      .first()
+      .click({ force: true });
+    cy.get('.btn-primary').click({ force: true });
+  });
+
+  it('should open cart', () => {
+    cy.get('cx-mini-cart').click();
+  });
+
+  it('should click proceed to checkout', () => {
+    cy.getByText(/proceed to checkout/i).click();
+  });
+
+  it('should verify Shipping Address', () => {
+    cy.get('.cx-checkout-title').should('contain', 'Shipping Address');
+  });
+});
