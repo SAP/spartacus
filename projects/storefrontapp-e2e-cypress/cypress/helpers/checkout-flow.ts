4fa84acf0bf4fb89ec69123be3058b340286467a
Patrick Labadie <patrick.labadie@sap.com>
2019-09-11 15:43:50 -0400

    GH-4515: Add first asm e2e tests.

--- projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-flow.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-flow.ts
@@ -13,9 +13,14 @@ import {
   PaymentDetails,
 } from './checkout-forms';
 
-export function visitHomePage() {
+export function visitHomePage(invokeAsmUi?: boolean) {
   const homePage = waitForPage('homepage', 'getHomePage');
-  cy.visit('/');
+
+  if (invokeAsmUi) {
+    cy.visit('/?asm=true');
+  } else {
+    cy.visit('/');
+  }
   cy.wait(`@${homePage}`);
 }
 
