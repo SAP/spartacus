81313535d077fd57c9f0faef6d8b111ae560e3d7
Caine Rotherham <rotherham.c@gmail.com>
2019-09-11 13:11:57 +0200

    feat: Login Button should not be disabled (#4483)
    Closes: #4483

--- projects/storefrontapp-e2e-cypress/cypress/integration/accessibility/tabbing-order.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/accessibility/tabbing-order.e2e-spec.ts
@@ -29,10 +29,9 @@ context("Tabbing order - tests don't require user to be logged in", () => {
   });
 
   describe('Login page', () => {
-    // TODO(issue:#4486) Bring back login page's a11y tests
-    // it('should allow to navigate with tab key (empty form)', () => {
-    //   loginTabbingOrder(config.login);
-    // });
+    it('should allow to navigate with tab key (empty form)', () => {
+      loginTabbingOrder(config.login);
+    });
 
     it('should allow to navigate with tab key (filled out form)', () => {
       loginTabbingOrder(config.login, true);
