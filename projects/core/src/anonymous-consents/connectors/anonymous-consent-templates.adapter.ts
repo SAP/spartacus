6380e8824a2c01fe88d39aa4c75f440664d11dbe
Nikola Zarić <nikola.zaric.ns@gmail.com>
2019-09-13 11:25:39 -0400

    added a jsdoc

--- projects/core/src/anonymous-consents/connectors/anonymous-consent-templates.adapter.ts
+++ projects/core/src/anonymous-consents/connectors/anonymous-consent-templates.adapter.ts
@@ -2,5 +2,8 @@ import { Observable } from 'rxjs';
 import { ConsentTemplate } from '../../model/consent.model';
 
 export abstract class AnonymousConsentTemplatesAdapter {
+  /**
+   * Abstract method used to load anonymous consents.
+   */
   abstract loadAnonymousConsentTemplates(): Observable<ConsentTemplate[]>;
 }
