e4ea5095541822a287ecea9fff647aaaa8c2bb09
Patrick Labadie <patrick.labadie@sap.com>
2019-09-16 11:46:29 -0400

    GH-4566: complete asm emulation checkout.

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/asm.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/asm.e2e-spec.ts
@@ -1,8 +1,12 @@
 import * as checkout from '../../helpers/checkout-flow';
 
 context('ASM', () => {
+  let customer: any;
   before(() => {
     cy.window().then(win => win.sessionStorage.clear());
+    checkout.visitHomePage();
+    customer = checkout.registerUser();
+    cy.log('Registered user:', customer.email);
   });
 
   it('should have ASM feature enabled', () => {
@@ -37,16 +41,51 @@ context('ASM', () => {
 
   it('should start cusrtomer emulation.', () => {
     const customerSearchRequestAlias = listenForCusrtomerSearchRequest();
+    const userDetailsRequestAlias = listenForUserDetailsRequest();
+
     cy.get('cx-csagent-login-form').should('not.exist');
     cy.get('cx-customer-selection').should('exist');
     cy.get('cx-customer-selection form').within(() => {
-      cy.get('[formcontrolname="searchTerm"]').type('pl@sap.com');
+      cy.get('[formcontrolname="searchTerm"]').type(customer.email);
       cy.get('button[type="submit"]').click();
     });
     cy.wait(customerSearchRequestAlias);
-    cy.get('cx-asm-main-ui').should('contain', 'ASM - Supporting customer');
+    cy.wait(userDetailsRequestAlias);
+    cy.get('cx-asm-main-ui').should(
+      'contain',
+      `ASM - Supporting customer: ${customer.fullName} (${customer.email})`
+    );
     cy.get('cx-csagent-login-form').should('not.exist');
     cy.get('cx-customer-selection').should('not.exist');
+  });
+
+  it('should add a product to cart and begin checkout.', () => {
+    checkout.clickCheapProductDetailsFromHomePage();
+    checkout.addCheapProductToCartAndBeginCheckout();
+  });
+
+  it('should fill in address form', () => {
+    checkout.fillAddressFormWithCheapProduct();
+  });
+
+  it('should choose delivery', () => {
+    checkout.chooseDeliveryMethod();
+  });
+
+  it('should fill in payment form', () => {
+    checkout.fillPaymentFormWithCheapProduct();
+  });
+
+  it('should review and place order', () => {
+    checkout.placeOrderWithCheapProduct();
+  });
+
+  it('should display summary page', () => {
+    checkout.verifyOrderConfirmationPageWithCheapProduct();
+  });
+
+  it('should be able to check order in order history', () => {
+    checkout.viewOrderHistoryWithCheapProduct();
   });
 
   it('should stop customer emulation.', () => {
@@ -79,6 +118,12 @@ context('ASM', () => {
       'GET',
       `/assistedservicewebservices/customers/search?baseSite=electronics-spa&query=*`
     ).as(aliasName);
+    return `@${aliasName}`;
+  }
+  function listenForUserDetailsRequest(): string {
+    const aliasName = 'userDetails';
+    cy.server();
+    cy.route('GET', '/rest/v2/electronics-spa/users/*').as(aliasName);
     return `@${aliasName}`;
   }
 });
