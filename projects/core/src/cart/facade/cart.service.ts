e3a2a6856e53cc0e03acca34bf7038ba0d69ba86
Marcin Lasak <lasakmarcin90@gmail.com>
2019-09-16 13:18:27 +0200

    revert to old cart service

--- projects/core/src/cart/facade/cart.service.ts
+++ projects/core/src/cart/facade/cart.service.ts
@@ -1,68 +1,77 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, asyncScheduler } from 'rxjs';
 import { AuthService } from '../../auth/index';
 import { Cart } from '../../model/cart.model';
 import { OrderEntry } from '../../model/order.model';
 import { StateWithCart } from '../store/cart-state';
 import { CartSelectors } from '../store/selectors/index';
-import { CartDataService } from './cart-data.service';
+import { CartDataService, ANONYMOUS_USERID } from './cart-data.service';
 import { LowLevelCartService } from './low-level-cart.service';
 import { ActiveCartService } from './active-cart.service';
+import { debounceTime, filter, tap, map, shareReplay, take } from 'rxjs/operators';
+import { CartActions } from '../store/actions';
 
 @Injectable()
 export class CartService {
+  private readonly PREVIOUS_USER_ID_INITIAL_VALUE =
+    'PREVIOUS_USER_ID_INITIAL_VALUE';
+  private previousUserId = this.PREVIOUS_USER_ID_INITIAL_VALUE;
   private _activeCart$: Observable<Cart>;
 
   constructor(
     protected store: Store<StateWithCart>,
     protected cartData: CartDataService,
     protected authService: AuthService,
-    protected lowLevelCart: LowLevelCartService,
-    protected activeCartService: ActiveCartService,
+    protected lowLevelCart?: LowLevelCartService,
+    protected activeCartService?: ActiveCartService,
   ) {
-    // this._activeCart$ = combineLatest([
-    //   this.store.select(CartSelectors.getCartContent),
-    //   this.store.select(CartSelectors.getCartLoading),
-    //   this.authService.getUserToken(),
-    //   this.store.select(CartSelectors.getCartLoaded),
-    // ]).pipe(
-    //   // combineLatest emits multiple times on each property update instead of one emit
-    //   // additionally dispatching actions that changes selectors used here needs to happen in order
-    //   // for this asyncScheduler is used here
-    //   debounceTime(1, asyncScheduler),
-    //   filter(([, loading]) => !loading),
-    //   tap(([cart, , userToken, loaded]) => {
-    //     if (this.isJustLoggedIn(userToken.userId)) {
-    //       this.loadOrMerge();
-    //     } else if (
-    //       (this.isCreated(cart) && this.isIncomplete(cart)) ||
-    //       (this.isLoggedIn(userToken.userId) &&
-    //         !this.isCreated(cart) &&
-    //         !loaded) // try to load current cart for logged in user (loaded flag to prevent infinite loop when user doesn't have cart)
-    //     ) {
-    //       this.load();
-    //     }
+    this._activeCart$ = combineLatest([
+      this.store.select(CartSelectors.getCartContent),
+      this.store.select(CartSelectors.getCartLoading),
+      this.authService.getUserToken(),
+      this.store.select(CartSelectors.getCartLoaded),
+    ]).pipe(
+      // combineLatest emits multiple times on each property update instead of one emit
+      // additionally dispatching actions that changes selectors used here needs to happen in order
+      // for this asyncScheduler is used here
+      debounceTime(1, asyncScheduler),
+      filter(([, loading]) => !loading),
+      tap(([cart, , userToken, loaded]) => {
+        if (this.isJustLoggedIn(userToken.userId)) {
+          this.loadOrMerge();
+        } else if (
+          (this.isCreated(cart) && this.isIncomplete(cart)) ||
+          (this.isLoggedIn(userToken.userId) &&
+            !this.isCreated(cart) &&
+            !loaded) // try to load current cart for logged in user (loaded flag to prevent infinite loop when user doesn't have cart)
+        ) {
+          this.load();
+        }
 
-    //     this.previousUserId = userToken.userId;
-    //   }),
-    //   filter(
-    //     ([cart]) =>
-    //       !this.isCreated(cart) ||
-    //       (this.isCreated(cart) && !this.isIncomplete(cart))
-    //   ),
-    //   map(([cart]) => cart),
-    //   shareReplay({ bufferSize: 1, refCount: true })
-    // );
+        this.previousUserId = userToken.userId;
+      }),
+      filter(
+        ([cart]) =>
+          !this.isCreated(cart) ||
+          (this.isCreated(cart) && !this.isIncomplete(cart))
+      ),
+      map(([cart]) => cart),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
   }
 
   getActive(): Observable<Cart> {
-    return this.activeCartService.getActive();
+    if (this.activeCartService) {
+      return this.activeCartService.getActive();
+    }
     return this._activeCart$;
   }
 
   getEntries(): Observable<OrderEntry[]> {
-    return this.activeCartService.getEntries();
+    if (this.activeCartService) {
+      return this.activeCartService.getEntries();
+    }
     return this.store.pipe(select(CartSelectors.getCartEntries));
   }
 
@@ -71,137 +80,146 @@ export class CartService {
   }
 
   getLoaded(): Observable<boolean> {
-    return this.activeCartService.getLoaded();
+    if (this.activeCartService) {
+      return this.activeCartService.getLoaded();
+    }
     return this.store.pipe(select(CartSelectors.getCartLoaded));
   }
 
-  // private loadOrMerge(): void {
-  //   // for login user, whenever there's an existing cart, we will load the user
-  //   // current cart and merge it into the existing cart
-  //   if (!this.isCreated(this.cartData.cart)) {
-  //     this.store.dispatch(
-  //       new CartActions.LoadCart({
-  //         userId: this.cartData.userId,
-  //         cartId: 'current',
-  //       })
-  //     );
-  //   } else {
-  //     this.store.dispatch(
-  //       new CartActions.MergeCart({
-  //         userId: this.cartData.userId,
-  //         cartId: this.cartData.cart.guid,
-  //       })
-  //     );
-  //   }
-  // }
+  private loadOrMerge(): void {
+    // for login user, whenever there's an existing cart, we will load the user
+    // current cart and merge it into the existing cart
+    if (!this.isCreated(this.cartData.cart)) {
+      this.store.dispatch(
+        new CartActions.LoadCart({
+          userId: this.cartData.userId,
+          cartId: 'current',
+        })
+      );
+    } else {
+      this.store.dispatch(
+        new CartActions.MergeCart({
+          userId: this.cartData.userId,
+          cartId: this.cartData.cart.guid,
+        })
+      );
+    }
+  }
 
-  // private load(): void {
-  //   if (this.cartData.userId !== ANONYMOUS_USERID) {
-  //     this.store.dispatch(
-  //       new CartActions.LoadCart({
-  //         userId: this.cartData.userId,
-  //         cartId: this.cartData.cartId ? this.cartData.cartId : 'current',
-  //       })
-  //     );
-  //   } else {
-  //     this.store.dispatch(
-  //       new CartActions.LoadCart({
-  //         userId: this.cartData.userId,
-  //         cartId: this.cartData.cartId,
-  //       })
-  //     );
-  //   }
-  // }
+  private load(): void {
+    if (this.cartData.userId !== ANONYMOUS_USERID) {
+      this.store.dispatch(
+        new CartActions.LoadCart({
+          userId: this.cartData.userId,
+          cartId: this.cartData.cartId ? this.cartData.cartId : 'current',
+        })
+      );
+    } else {
+      this.store.dispatch(
+        new CartActions.LoadCart({
+          userId: this.cartData.userId,
+          cartId: this.cartData.cartId,
+        })
+      );
+    }
+  }
 
   addEntry(productCode: string, quantity: number): void {
-    return this.activeCartService.addEntry(productCode, quantity);
-    // this.store
-    //   .pipe(
-    //     select(CartSelectors.getActiveCartState),
-    //     tap(cartState => {
-    //       if (!this.isCreated(cartState.value.content) && !cartState.loading) {
-    //         this.lowLevelCart.createCart(this.cartData.userId).subscribe();
-    //         // this.store.dispatch(
-    //         //   new CartActions.CreateCart({ userId: this.cartData.userId })
-    //         // );
-    //       }
-    //     }),
-    //     filter(cartState => this.isCreated(cartState.value.content)),
-    //     take(1)
-    //   )
-    //   .subscribe(_ => {
-    //     this.store.dispatch(
-    //       new CartActions.CartAddEntry({
-    //         userId: this.cartData.userId,
-    //         cartId: this.cartData.cartId,
-    //         productCode: productCode,
-    //         quantity: quantity,
-    //       })
-    //     );
-    //   });
+    if (this.activeCartService) {
+      return this.activeCartService.addEntry(productCode, quantity);
+    }
+    this.store
+      .pipe(
+        select(CartSelectors.getActiveCartState),
+        tap(cartState => {
+          if (!this.isCreated(cartState.value.content) && !cartState.loading) {
+            this.store.dispatch(
+              new CartActions.CreateCart({ userId: this.cartData.userId })
+            );
+          }
+        }),
+        filter(cartState => this.isCreated(cartState.value.content)),
+        take(1)
+      )
+      .subscribe(_ => {
+        this.store.dispatch(
+          new CartActions.CartAddEntry({
+            userId: this.cartData.userId,
+            cartId: this.cartData.cartId,
+            productCode: productCode,
+            quantity: quantity,
+          })
+        );
+      });
   }
 
   removeEntry(entry: OrderEntry): void {
-    return this.activeCartService.removeEntry(entry);
-    // this.store.dispatch(
-    //   new CartActions.CartRemoveEntry({
-    //     userId: this.cartData.userId,
-    //     cartId: this.cartData.cartId,
-    //     entry: entry.entryNumber,
-    //   })
-    // );
+    if (this.activeCartService) {
+      return this.activeCartService.removeEntry(entry);
+    }
+    this.store.dispatch(
+      new CartActions.CartRemoveEntry({
+        userId: this.cartData.userId,
+        cartId: this.cartData.cartId,
+        entry: entry.entryNumber,
+      })
+    );
   }
 
   updateEntry(entryNumber: string, quantity: number): void {
-    return this.activeCartService.updateEntry(parseInt(entryNumber, 10), quantity);
-    // if (quantity > 0) {
-    //   this.store.dispatch(
-    //     new CartActions.CartUpdateEntry({
-    //       userId: this.cartData.userId,
-    //       cartId: this.cartData.cartId,
-    //       entry: entryNumber,
-    //       qty: quantity,
-    //     })
-    //   );
-    // } else {
-    //   this.store.dispatch(
-    //     new CartActions.CartRemoveEntry({
-    //       userId: this.cartData.userId,
-    //       cartId: this.cartData.cartId,
-    //       entry: entryNumber,
-    //     })
-    //   );
-    // }
+    if (this.activeCartService) {
+      return this.activeCartService.updateEntry(parseInt(entryNumber, 10), quantity);
+    }
+    if (quantity > 0) {
+      this.store.dispatch(
+        new CartActions.CartUpdateEntry({
+          userId: this.cartData.userId,
+          cartId: this.cartData.cartId,
+          entry: entryNumber,
+          qty: quantity,
+        })
+      );
+    } else {
+      this.store.dispatch(
+        new CartActions.CartRemoveEntry({
+          userId: this.cartData.userId,
+          cartId: this.cartData.cartId,
+          entry: entryNumber,
+        })
+      );
+    }
   }
 
   getEntry(productCode: string): Observable<OrderEntry> {
-    return this.activeCartService.getEntry(productCode);
-    // return this.store.pipe(
-    //   select(CartSelectors.getCartEntrySelectorFactory(productCode))
-    // );
+    if (this.activeCartService) {
+      return this.activeCartService.getEntry(productCode);
+    }
+    return this.store.pipe(
+      select(CartSelectors.getCartEntrySelectorFactory(productCode))
+    );
   }
 
-  // private isCreated(cart: Cart): boolean {
-  //   return cart && typeof cart.guid !== 'undefined';
-  // }
+  private isCreated(cart: Cart): boolean {
+    return cart && typeof cart.guid !== 'undefined';
+  }
 
   /**
    * Cart is incomplete if it contains only `guid` and `code` properties, which come from local storage.
    * To get cart content, we need to load cart from backend.
    */
-  // private isIncomplete(cart: Cart): boolean {
-  //   return cart && Object.keys(cart).length <= 2;
-  // }
+  private isIncomplete(cart: Cart): boolean {
+    return cart && Object.keys(cart).length <= 2;
+  }
 
-  // private isJustLoggedIn(userId: string): boolean {
-  //   return (
-  //     this.isLoggedIn(userId) &&
-  //     this.previousUserId !== userId && // *just* logged in
-  //     this.previousUserId !== this.PREVIOUS_USER_ID_INITIAL_VALUE // not app initialization
-  //   );
-  // }
+  private isJustLoggedIn(userId: string): boolean {
+    return (
+      this.isLoggedIn(userId) &&
+      this.previousUserId !== userId && // *just* logged in
+      this.previousUserId !== this.PREVIOUS_USER_ID_INITIAL_VALUE // not app initialization
+    );
+  }
 
-  // private isLoggedIn(userId: string): boolean {
-  //   return typeof userId !== 'undefined';
-  // }
+  private isLoggedIn(userId: string): boolean {
+    return typeof userId !== 'undefined';
+  }
 }
