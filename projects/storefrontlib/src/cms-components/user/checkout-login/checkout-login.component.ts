90b1c756df5ae2b4ab8c02bf84aa03d70993b088
Louis Pierrestiger <louis.pierrestiger@sap.com>
2019-09-17 12:22:36 -0400

    fix: Fix form validation (#4590)
    Closes #4555

--- projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
+++ projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
@@ -7,8 +7,8 @@ import {
 } from '@angular/forms';
 import { AuthRedirectService, CartService } from '@spartacus/core';
 import { Subscription } from 'rxjs';
-import { CustomFormValidators } from '../../../shared/utils/validators/custom-form-validators';
 import { FormUtils } from '../../../shared/utils/forms/form-utils';
+import { CustomFormValidators } from '../../../shared/utils/validators/custom-form-validators';
 
 @Component({
   selector: 'cx-checkout-login',
@@ -18,10 +18,7 @@ export class CheckoutLoginComponent implements OnDestroy {
   form: FormGroup = this.formBuilder.group(
     {
       email: ['', [Validators.required, CustomFormValidators.emailValidator]],
-      emailConfirmation: [
-        '',
-        [Validators.required, CustomFormValidators.emailValidator],
-      ],
+      emailConfirmation: ['', [Validators.required]],
     },
     { validator: this.emailsMatch }
   );
@@ -46,7 +43,10 @@ export class CheckoutLoginComponent implements OnDestroy {
 
   isEmailConfirmInvalid(): boolean {
     return (
-      this.isNotValid('emailConfirmation') || this.form.hasError('NotEqual')
+      this.form.hasError('NotEqual') &&
+      (this.submitClicked ||
+        (this.form.get('emailConfirmation').touched &&
+          this.form.get('emailConfirmation').dirty))
     );
   }
 
