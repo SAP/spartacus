66faf702496cdeb34320e92a731855e4f1c59ee4
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-09-17 09:41:01 -0400

    GH-4555: added validation again

--- projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
+++ projects/storefrontlib/src/cms-components/user/checkout-login/checkout-login.component.ts
@@ -18,7 +18,10 @@ export class CheckoutLoginComponent implements OnDestroy {
   form: FormGroup = this.formBuilder.group(
     {
       email: ['', [Validators.required, CustomFormValidators.emailValidator]],
-      emailConfirmation: ['', [Validators.required]],
+      emailConfirmation: [
+        '',
+        [Validators.required, CustomFormValidators.emailValidator],
+      ],
     },
     { validator: this.emailsMatch }
   );
