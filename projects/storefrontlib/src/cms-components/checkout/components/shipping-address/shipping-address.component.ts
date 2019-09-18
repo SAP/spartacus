c0572324d4aa2037df0aee9fd6419381af6856d7
Marcin Lasak <lasakmarcin90@gmail.com>
2019-09-18 07:32:51 +0200

    fix: Change diff in breaking change bot report

--- projects/storefrontlib/src/cms-components/checkout/components/shipping-address/shipping-address.component.ts
+++ projects/storefrontlib/src/cms-components/checkout/components/shipping-address/shipping-address.component.ts
@@ -45,6 +45,7 @@ export class ShippingAddressComponent implements OnInit, OnDestroy {
   cards$: Observable<CardWithAddress[]>;
   checkoutStepUrlNext: string;
   checkoutStepUrlPrevious: string;
+  variableForTest: boolean;
 
   constructor(
     protected userAddressService: UserAddressService,
