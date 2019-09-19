909c9dfc5f290f3e828c7460781e018536fc6041
Krzysztof Platis <platonn.git@gmail.com>
2019-09-19 13:55:16 +0200

    add comments to config

--- projects/core/src/routing/configurable-routes/config/routing-config.ts
+++ projects/core/src/routing/configurable-routes/config/routing-config.ts
@@ -2,7 +2,14 @@ import { RoutesConfig } from '../routes-config';
 
 export abstract class RoutingConfig {
   routing?: {
+    /**
+     * Configuration of semantic routes. Key is route's name. Value is the config specific to this route.
+     */
     routes?: RoutesConfig;
+
+    /**
+     * When true, it closes the storefront for unauthorized users, except from routes that have individual config flag `protected: false`
+     */
     protected?: boolean;
   };
 }
