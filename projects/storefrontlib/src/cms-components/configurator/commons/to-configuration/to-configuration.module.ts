2a791d263dc365b16cd9382bf18ac2868d361a01
Christoph Hinssen <christoph.hinssen@sap.com>
2019-09-20 07:40:25 +0200

    GH-4357 Remove unneeded references from module

--- projects/storefrontlib/src/cms-components/configurator/commons/to-configuration/to-configuration.module.ts
+++ projects/storefrontlib/src/cms-components/configurator/commons/to-configuration/to-configuration.module.ts
@@ -1,30 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {
-  CmsConfig,
-  ConfigModule,
-  I18nModule,
-  UrlModule,
-} from '@spartacus/core';
+import { I18nModule } from '@spartacus/core';
 import { ToConfigurationComponent } from './to-configuration.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-
-    ConfigModule.withConfig(<CmsConfig>{
-      cmsComponents: {
-        ProductAddToCartComponent: {
-          component: ToConfigurationComponent,
-        },
-      },
-    }),
-    UrlModule,
-
-    I18nModule,
-  ],
+  imports: [RouterModule, I18nModule],
   declarations: [ToConfigurationComponent],
   entryComponents: [ToConfigurationComponent],
   exports: [ToConfigurationComponent],
