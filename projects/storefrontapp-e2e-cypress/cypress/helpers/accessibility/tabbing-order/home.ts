c0aa58d8121068e0b390b35e4f03b7550b4b692a
Caine Rotherham <rotherham.c@gmail.com>
2019-09-20 14:55:16 +0200

    Fix test

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/home.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/home.ts
@@ -2,7 +2,12 @@ import { checkAllElements, TabElement } from '../tabbing-order';
 
 export function homeTabbingOrder(config: TabElement[]) {
   cy.visit('/');
-  cy.wait(4000);
+
+  // Ensures carousel products are loaded before running checks
+  cy.get('cx-carousel')
+    .find('img')
+    .should('have.length', 18);
+
   cy.get('.Section1 a')
     .first()
     .focus();
