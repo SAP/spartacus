07f4a27e09736a01a3b803ce26d100784a6d10d2
Caine Rotherham <rotherham.c@gmail.com>
2019-09-24 08:44:29 +0200

    Missed a file

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/header.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order/header.ts
@@ -2,8 +2,9 @@ import { checkAllElements, TabElement } from '../tabbing-order';
 
 export function headerDesktopTabbingOrder(config: TabElement[]) {
   cy.visit('/');
-  // Ensures carousel products are loaded before running checks
+  // Ensures categorynavigation is loaded before running checks
   cy.get('cx-navigation-ui').should('be.visible');
+
   cy.get('header cx-site-context-selector select')
     .first()
     .focus();
