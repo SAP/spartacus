20256cfae7535e5728add5bd80a5dd58082f19a8
Patrick Labadie <patrick.labadie@sap.com>
2019-09-26 15:33:29 -0400

    GH-4558: Remove dead code and i18n labels.

--- projects/assets/src/translations/en/asm.ts
+++ projects/assets/src/translations/en/asm.ts
@@ -8,19 +8,16 @@ export const asm = {
       submit: 'Sign In Agent',
       userId: {
         label: 'Agent ID',
-        placeholder: 'Agent ID',
         required: 'Agent ID is required',
       },
       password: {
         label: 'Password',
-        placeholder: 'Password',
         required: 'Password is required',
       },
     },
     customerSearch: {
       searchTerm: {
         label: 'Customer Email Address',
-        placeholder: 'Customer Email Address',
         required: 'Customer Email Address is required',
       },
       submit: 'Start Customer Session',
