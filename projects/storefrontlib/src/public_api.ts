d67d17d1914b50104f081e6fb1edfa7fa944e07c
Patrick Labadie <patrick.labadie@sap.com>
2019-09-29 09:15:09 -0400

    GH-4756: Refactoring

--- projects/storefrontlib/src/public_api.ts
+++ projects/storefrontlib/src/public_api.ts
@@ -5,7 +5,6 @@
 export * from './cms-components/index';
 export * from './cms-pages/index';
 export * from './cms-structure/index';
-export * from './components/asm/index';
 export * from './layout/index';
 export * from './recipes/index';
 export * from './recipes/storefront.module';
