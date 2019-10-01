2094fdf254ceb53acf288d394cf4e9319b44822a
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-10-01 15:13:15 +0200

    Use form control for the disabled state of the input, rather than custom inputs

--- projects/storefrontlib/src/shared/components/item-counter/item-counter.component.ts
+++ projects/storefrontlib/src/shared/components/item-counter/item-counter.component.ts
@@ -1,175 +1,56 @@
 import {
   Component,
   ElementRef,
-  EventEmitter,
-  forwardRef,
   HostBinding,
   HostListener,
   Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  Output,
-  Renderer2,
   ViewChild,
 } from '@angular/core';
-import {
-  ControlValueAccessor,
-  FormControl,
-  NG_VALUE_ACCESSOR,
-} from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-
-const COUNTER_CONTROL_ACCESSOR = {
-  provide: NG_VALUE_ACCESSOR,
-  /* tslint:disable-next-line */
-  useExisting: forwardRef(() => ItemCounterComponent),
-  multi: true,
-};
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'cx-item-counter',
   templateUrl: './item-counter.component.html',
-  providers: [COUNTER_CONTROL_ACCESSOR],
 })
-export class ItemCounterComponent
-  implements OnInit, ControlValueAccessor, OnChanges, OnDestroy {
-  @ViewChild('itemCounterInput', { static: false }) public input: ElementRef;
-
-  @Input() value = 1;
+export class ItemCounterComponent {
   @Input() min = 1;
   @Input() max: number;
-  @Input() async = false;
-  @Input() cartIsLoading = false;
+  @Input() control: FormControl;
 
-  @HostBinding('class.changeable') @Input() isValueChangeable = true;
+  @HostBinding('class.readonly') @Input() readonly = false;
 
-  @Output() update = new EventEmitter<number>();
-
-  inputControl: FormControl = new FormControl({
-    disabled: this.isValueChangeable,
-  });
-
-  subscription: Subscription;
-
-  @HostListener('keydown', ['$event'])
-  handleKeydown(event: KeyboardEvent): void {
-    if (event.code === 'ArrowUp' || event.code === 'ArrowRight') {
-      this.increment();
-    }
-
-    if (event.code === 'ArrowDown' || event.code === 'ArrowLeft') {
-      this.decrement();
-    }
-  }
+  @ViewChild('qty', { static: false }) private input: ElementRef<HTMLElement>;
 
   @HostListener('click')
   handleClick() {
     this.input.nativeElement.focus();
   }
 
-  ngOnInit() {
-    this.writeValue(this.min || 0);
-    this.subscription = this.inputControl.valueChanges
-      .pipe(debounceTime(300))
-      .subscribe(value => {
-        if (value) {
-          this.manualChange(Number(value));
-        }
-      });
-  }
-
-  ngOnChanges() {
-    if (this.cartIsLoading) {
-      this.inputControl.disable({
-        onlySelf: true,
-        emitEvent: false,
-      });
-    } else {
-      this.inputControl.enable({
-        onlySelf: true,
-        emitEvent: false,
-      });
+  @HostListener('keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (['ArrowUp', 'ArrowRight'].includes(event.code)) {
+      this.increment();
+    }
+    if (['ArrowDown', 'ArrowLeft', 'Minus'].includes(event.code)) {
+      this.decrement();
     }
   }
 
-  constructor(private renderer: Renderer2) {}
-
-  onTouch: Function = () => {};
-  onModelChange: Function = (_rating: number) => {};
-
-  /**
-   * If value is too small it will be set to min, if is too big it will be set to max.
-   */
-  adjustValueInRange(incomingValue: number): number {
-    return incomingValue < this.min || !this.min
-      ? this.min
-      : incomingValue > this.max || !this.max
-      ? this.max
-      : incomingValue;
+  increment() {
+    this.increase(1);
   }
 
-  /**
-   * Update model value and refresh input
-   */
-  manualChange(newValue: number): void {
-    newValue = this.adjustValueInRange(newValue);
-    this.updateValue(newValue);
-    /* We use the value from the input, however, this value
-      is not the correct value that should be displayed. The correct value to display
-      is this.value, which the parent updates if the async call succeed. If the call
-      fails, then the input will need to display this.value, and not what the user
-      recently typed in */
-    this.renderer.setProperty(this.input.nativeElement, 'value', newValue);
+  decrement() {
+    this.increase(-1);
   }
 
-  /**
-   * Verify value that it can be incremented, if yes it does that.
-   */
-  increment(): void {
-    this.manualChange(++this.value);
-  }
-
-  /**
-   * Verify value that it can be decremented, if yes it does that.
-   */
-  decrement(): void {
-    this.manualChange(--this.value);
-  }
-
-  // ControlValueAccessor interface
-
-  registerOnTouched(fn: Function): void {
-    this.onTouch = fn;
-  }
-
-  registerOnChange(fn: Function): void {
-    this.onModelChange = fn;
-  }
-
-  writeValue(value: number): void {
-    this.value = value || this.min || 0;
-    this.onModelChange(this.value);
-  }
-
-  /**
-   * Set up new value for input and emit event outside
-   */
-  updateValue(updatedQuantity: number): void {
-    if (!this.async) {
-      // If the async flag is true, then the parent component is responsible for updating the form
-      this.writeValue(updatedQuantity);
+  private increase(value: number) {
+    let newValue = this.control.value + value;
+    if (this.min && newValue < this.min) {
+      newValue = this.min;
+    } else if (this.max && newValue > this.max) {
+      newValue = this.max;
     }
-
-    // Additionally, we emit a change event, so that users may optionally do something on change
-    this.update.emit(updatedQuantity);
-    this.onTouch();
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.control.setValue(newValue);
   }
 }
