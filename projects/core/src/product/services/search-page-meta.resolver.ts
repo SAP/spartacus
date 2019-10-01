c5e9fadd41bd424aaa9d9f4e26c5440afc26c5c4
Muhammad Azam <muhammad.azam@sap.com>
2019-10-01 14:05:31 -0700

    Fix code formatting issue causing failure with Travis CI on PR

--- projects/core/src/product/services/search-page-meta.resolver.ts
+++ projects/core/src/product/services/search-page-meta.resolver.ts
@@ -33,9 +33,9 @@ export class SearchPageMetaResolver extends PageMetaResolver
 
     const query$: Observable<
       string
-    > = this.routingService.getRouterState().pipe(
-      map(state => state.state.params['query'])
-    );
+    > = this.routingService
+      .getRouterState()
+      .pipe(map(state => state.state.params['query']));
 
     return combineLatest([total$, query$]).pipe(
       switchMap(([total, query]: [number, string]) =>
