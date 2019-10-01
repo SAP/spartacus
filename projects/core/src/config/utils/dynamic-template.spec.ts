048be8793e23849a8df7e41def51ec0d3a224965
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-10-01 17:28:13 -0400

    TM-17: Change templating method

---
+++ projects/core/src/config/utils/dynamic-template.spec.ts
@@ -0,0 +1,37 @@
+import { DynamicTemplate } from './dynamic-template';
+
+fdescribe('DynamicTemplate', () => {
+  describe('No variables in String', () => {
+    it('should return template string', () => {
+      const url = '/xxx/xxx';
+      const result = DynamicTemplate.resolve(url, { id: '123' });
+      expect(result).toEqual(url);
+    });
+  });
+
+  describe('No variables in Values', () => {
+    it('should return template string', () => {
+      const url = '/xxx/xxx/${id}';
+      const result = DynamicTemplate.resolve(url, {});
+      expect(result).toEqual(url);
+    });
+  });
+
+  describe('Correct variables in String and Values', () => {
+    it('should return template string with value', () => {
+      const url = '/xxx/xxx/${id}';
+      const id = 123;
+      const result = DynamicTemplate.resolve(url, { id });
+      expect(result).toEqual(`/xxx/xxx/${id}`);
+    });
+  });
+
+  describe('Wrong variables in String and Values', () => {
+    it('should return template string with value', () => {
+      const url = '/xxx/xxx/${id}';
+      const productId = 123;
+      const result = DynamicTemplate.resolve(url, { productId });
+      expect(result).toEqual(url);
+    });
+  });
+});
--- projects/core/src/config/utils/dynamic-template.ts
+++ projects/core/src/config/utils/dynamic-template.ts
@@ -2,37 +2,30 @@ import { isDevMode } from '@angular/core';
 
 export class DynamicTemplate {
   static resolve(templateString: string, templateVariables: Object) {
-    const keys = Object.keys(templateVariables);
-    // Can't use Object.values as the compilation settings are to es2015 not es2017
-    const values = keys.map(key => templateVariables[key]);
+    const results = templateString.match(/\${(.*?)}/g);
+    let query = templateString;
 
-    let templateFunction = new Function(
-      ...keys,
-      `return \`${templateString}\`;`
-    );
+    // If the templateString is has no "placeholders" or templateVariables is empty
+    if (
+      Boolean(results) &&
+      Boolean(templateVariables) &&
+      Object.keys(templateVariables).length > 0
+    ) {
+      for (const result of results) {
+        let value: string;
+        // Return the value for the key in the template, second one in the array to get the content of the curly brackets
+        value = templateVariables[result.match(/{(.*)}/).pop()];
 
-    try {
-      return templateFunction(...values);
-    } catch (e) {
-      if (isDevMode() && e instanceof ReferenceError) {
-        console.warn(`Key "${e.message.split(' ')[0]}" not found.`);
-      }
-
-      if (templateString.indexOf('?') > -1) {
-        templateFunction = new Function(
-          ...keys,
-          `return \`${templateString.split('?')[0]}\`;`
-        );
-        try {
-          return templateFunction(...values);
-        } catch (e) {
+        // If one of the placeholders is not result return the string as provided
+        if (value === null || value === undefined) {
           if (isDevMode()) {
-            console.warn('Could not resolve endpoint.');
+            console.warn('Value for key "key" not found.');
           }
           return templateString;
         }
+        query = query.replace(result, value);
       }
-      return templateString;
     }
+    return query;
   }
 }
