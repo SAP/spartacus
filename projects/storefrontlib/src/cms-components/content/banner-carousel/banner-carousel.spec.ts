0ccd81631e7318a1f4f771ad37bbc82bb55f0882
qjimmy <quang-jimmy@hotmail.com>
2019-10-08 15:51:18 -0400

    fix: Banner Carousel Unit Tests (GH-4274)

---
+++ projects/storefrontlib/src/cms-components/content/banner-carousel/banner-carousel.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'cx-banner-carousel',
+  template: '',
+})
+class MockBannerCarouselComponent {
+  getItems() {
+    return of();
+  }
+}
+
+fdescribe('CreateComponent', () => {
+  let component: MockBannerCarouselComponent;
+  let fixture: ComponentFixture<MockBannerCarouselComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockBannerCarouselComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockBannerCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get items', () => {
+    expect(component.getItems()).toBeTruthy();
+  });
+});
