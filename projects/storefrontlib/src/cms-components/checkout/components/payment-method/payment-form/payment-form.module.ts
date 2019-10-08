6fece622b70fb3ef25e2cdcb0cdb8cd0f111f8a9
qjimmy <quang-jimmy@hotmail.com>
2019-10-08 10:57:22 -0400

    fix: Should prevent double click when adding new payment info (GH-3102)

--- projects/storefrontlib/src/cms-components/checkout/components/payment-method/payment-form/payment-form.module.ts
+++ projects/storefrontlib/src/cms-components/checkout/components/payment-method/payment-form/payment-form.module.ts
@@ -7,7 +7,7 @@ import { CardModule } from '../../../../../shared/components/card/card.module';
 import { IconModule } from '../../../../misc/icon/icon.module';
 import { BillingAddressFormModule } from '../billing-address-form/billing-address-form.module';
 import { PaymentFormComponent } from './payment-form.component';
-import { SpinnerModule } from 'projects/storefrontlib/src/shared';
+// import { SpinnerModule } from 'projects/storefrontlib/src/shared';
 
 @NgModule({
   imports: [
@@ -18,7 +18,7 @@ import { SpinnerModule } from 'projects/storefrontlib/src/shared';
     BillingAddressFormModule,
     I18nModule,
     IconModule,
-    SpinnerModule,
+    // SpinnerModule,
   ],
   declarations: [PaymentFormComponent],
   entryComponents: [PaymentFormComponent],
