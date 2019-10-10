2283aa0171cb6214e73f40697d2138a2e60d4979
Weizheng Gao <weizheng.gao@sap.com>
2019-10-10 12:19:19 -0400

    feature/GH-4519: update b2b layout config

--- projects/storefrontlib/src/recipes/config/b2b-layout-config.ts
+++ projects/storefrontlib/src/recipes/config/b2b-layout-config.ts
@@ -58,7 +58,6 @@ export const b2bLayoutConfig: LayoutConfig = {
     },
     ProductDetailsPageTemplate: {
       slots: [
-        'TopHeaderSlot',
         'Summary',
         'UpSelling',
         'CrossSelling',
