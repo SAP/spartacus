dff6466a4932aa6a54b2797c1ab421ac41df2163
Michał Gruca <pucek9@gmail.com>
2019-10-10 17:40:49 +0200

    up

--- projects/core/src/organization/store/actions/budget.action.ts
+++ projects/core/src/organization/store/actions/budget.action.ts
@@ -9,6 +9,7 @@ import {
   EntityFailAction,
   EntitySuccessAction,
 } from '../../../state/utils/entity-loader/entity-loader.action';
+import { PROCESS_FEATURE } from '../../../process/store/process-state';
 
 export const LOAD_BUDGET = '[Budget] Load Budget Data';
 export const LOAD_BUDGET_FAIL = '[Budget] Load Budget Data Fail';
@@ -42,21 +43,21 @@ export class LoadBudgetSuccess extends EntitySuccessAction {
 export class LoadBudgets extends EntityLoadAction {
   readonly type = LOAD_BUDGETS;
   constructor(public payload: string) {
-    super(BUDGETS_FEATURE, LOAD_BUDGETS_PROCESS_ID);
+    super(PROCESS_FEATURE, LOAD_BUDGETS_PROCESS_ID);
   }
 }
 
 export class LoadBudgetsFail extends EntityFailAction {
   readonly type = LOAD_BUDGETS_FAIL;
   constructor(public payload: any) {
-    super(BUDGETS_FEATURE, LOAD_BUDGETS_PROCESS_ID, payload);
+    super(PROCESS_FEATURE, LOAD_BUDGETS_PROCESS_ID, payload);
   }
 }
 
 export class LoadBudgetsSuccess extends EntitySuccessAction {
   readonly type = LOAD_BUDGETS_SUCCESS;
   constructor() {
-    super(BUDGETS_FEATURE, LOAD_BUDGETS_PROCESS_ID );
+    super(PROCESS_FEATURE, LOAD_BUDGETS_PROCESS_ID );
   }
 }
 
