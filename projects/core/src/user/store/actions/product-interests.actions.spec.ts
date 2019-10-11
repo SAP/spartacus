a929fa82b5b42b11d424502553e952fe50596765
Qingyu <qingyu.yang@sap.com>
2019-10-11 14:22:32 +0800

    PANDA-3271 action test

--- projects/core/src/user/store/actions/product-interests.actions.spec.ts
+++ projects/core/src/user/store/actions/product-interests.actions.spec.ts
@@ -1,12 +1,24 @@
-import { PRODUCT_INTERESTS } from '../user-state';
+import {
+  PRODUCT_INTERESTS,
+  REMOVE_PRODUCT_INTERESTS_PROCESS_ID,
+} from '../user-state';
 import { UserActions } from './index';
 import {
   loadMeta,
   failMeta,
   successMeta,
+  resetMeta,
 } from '../../../state/utils/loader/loader.action';
+import { NotificationType } from '../../../model/product-interest.model';
+import {
+  entityLoadMeta,
+  entitySuccessMeta,
+  entityFailMeta,
+} from '../../../state/utils/entity-loader/entity-loader.action';
+import { PROCESS_FEATURE } from '../../../process/store/process-state';
 
-const userId = 'jack.ma@hybris.com';
+const userId = 'qingyu@sap.com';
+const productCode = '343898';
 
 fdescribe('Product Interests Actions', () => {
   describe('LoadProductInterests Actions', () => {
@@ -16,6 +28,8 @@ fdescribe('Product Interests Actions', () => {
         pageSize: 1,
         currentPage: 0,
         sort: 'name:asc',
+        productCode: productCode,
+        notificationType: NotificationType.BACK_IN_STOCK,
       };
       const action = new UserActions.LoadProductInterests(payload);
       expect({ ...action }).toEqual({
@@ -32,7 +46,7 @@ fdescribe('Product Interests Actions', () => {
       expect({ ...action }).toEqual({
         type: UserActions.LOAD_PRODUCT_INTERESTS_FAIL,
         payload: error,
-        meta: failMeta(error, PRODUCT_INTERESTS),
+        meta: failMeta(PRODUCT_INTERESTS, error),
       });
     });
   });
@@ -53,52 +67,62 @@ fdescribe('Product Interests Actions', () => {
     });
   });
 
-  // describe('DeleteProductInterests Actions', () => {
-  //   it('should be able to create the action', () => {
-  //     const payload = {
-  //       userId: userId,
-  //       item: {},
-  //     };
-  //     const action = new UserActions.RemoveProductInterests(payload);
-  //     expect({ ...action }).toEqual({
-  //       type: UserActions.REMOVE_PRODUCT_INTERESTS,
-  //       payload: payload,
-  //       meta: loadMeta(PRODUCT_INTERESTS),
-  //     });
-  //   });
-  // });
+  describe('RemoveProductInterests Actions', () => {
+    it('should be able to create the action', () => {
+      const payload = {
+        userId: userId,
+        item: {},
+      };
+      const action = new UserActions.RemoveProductInterests(payload);
+      expect({ ...action }).toEqual({
+        type: UserActions.REMOVE_PRODUCT_INTERESTS,
+        payload: payload,
+        meta: entityLoadMeta(
+          PROCESS_FEATURE,
+          REMOVE_PRODUCT_INTERESTS_PROCESS_ID
+        ),
+      });
+    });
+  });
 
-  // describe('DeleteProductInterestsSuccess Actions', () => {
-  //   it('should be able to create the action', () => {
-  //     const payload = 'del success';
-  //     const action = new UserActions.RemoveProductInterestsSuccess(payload);
-  //     expect({ ...action }).toEqual({
-  //       type: UserActions.REMOVE_PRODUCT_INTERESTS_SUCCESS,
-  //       payload: payload,
-  //       meta: successMeta(PRODUCT_INTERESTS),
-  //     });
-  //   });
-  // });
+  describe('RemoveProductInterestsSuccess Actions', () => {
+    it('should be able to create the action', () => {
+      const payload = 'remove success';
+      const action = new UserActions.RemoveProductInterestsSuccess(payload);
+      expect({ ...action }).toEqual({
+        type: UserActions.REMOVE_PRODUCT_INTERESTS_SUCCESS,
+        payload: payload,
+        meta: entitySuccessMeta(
+          PROCESS_FEATURE,
+          REMOVE_PRODUCT_INTERESTS_PROCESS_ID
+        ),
+      });
+    });
+  });
 
-  // describe('DeleteProductInterestsFail Actions', () => {
-  //   it('should be able to create the action', () => {
-  //     const error = 'del fail';
-  //     const action = new UserActions.RemoveProductInterestsFail(error);
-  //     expect({ ...action }).toEqual({
-  //       type: UserActions.REMOVE_PRODUCT_INTERESTS_FAIL,
-  //       payload: error,
-  //       meta: failMeta(PRODUCT_INTERESTS, error),
-  //     });
-  //   });
-  // });
+  describe('RemoveProductInterestsFail Actions', () => {
+    it('should be able to create the action', () => {
+      const error = 'remove fail';
+      const action = new UserActions.RemoveProductInterestsFail(error);
+      expect({ ...action }).toEqual({
+        type: UserActions.REMOVE_PRODUCT_INTERESTS_FAIL,
+        payload: error,
+        meta: entityFailMeta(
+          PROCESS_FEATURE,
+          REMOVE_PRODUCT_INTERESTS_PROCESS_ID,
+          error
+        ),
+      });
+    });
+  });
 
-  // describe('ClearProductInterests Actions', () => {
-  //   it('should be able to create the action', () => {
-  //     const action = new UserActions.ClearProductInterests();
-  //     expect({ ...action }).toEqual({
-  //       type: UserActions.CLEAR_PRODUCT_INTERESTS,
-  //       meta: failMeta(PRODUCT_INTERESTS),
-  //     });
-  //   });
-  // });
+  describe('ClearProductInterests Actions', () => {
+    it('should be able to create the action', () => {
+      const action = new UserActions.ClearProductInterests();
+      expect({ ...action }).toEqual({
+        type: UserActions.CLEAR_PRODUCT_INTERESTS,
+        meta: resetMeta(PRODUCT_INTERESTS),
+      });
+    });
+  });
 });
