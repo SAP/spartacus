4d7bc968bb3e26dde2d684351279a6e969ad1ed0
Michał Gruca <pucek9@gmail.com>
2019-10-14 13:10:22 +0200

    up

--- projects/core/src/organization/facade/budget.service.ts
+++ projects/core/src/organization/facade/budget.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { StateWithOrganization } from '../store/organization-state';
+import {
+  LOAD_BUDGETS_PROCESS_ID,
+  StateWithOrganization,
+} from '../store/organization-state';
 import { BudgetActions } from '../store/actions/index';
 import {
   getBudgetsState,
@@ -8,30 +11,50 @@ import {
 } from '../store/selectors/budget.selector';
 import { take } from 'rxjs/operators';
 import { AuthService } from '../../auth/facade/auth.service';
+import {
+  getProcessLoadingFactory,
+  getProcessSuccessFactory,
+} from '../../process/store/selectors/process.selectors';
+import { StateWithProcess } from '../../process/store/process-state';
 
 @Injectable()
 export class BudgetService {
   private user$ = this.authService.getOccUserId();
 
   constructor(
-    protected store: Store<StateWithOrganization>,
+    protected store: Store<StateWithOrganization | StateWithProcess<void>>,
     protected authService: AuthService
   ) {}
 
   loadBudget(budgetCode: string) {
-    this.user$.pipe(take(1)).subscribe(uid =>
-      this.store.dispatch(new BudgetActions.LoadBudget({ uid, budgetCode }))
-    );
+    this.user$
+      .pipe(take(1))
+      .subscribe(uid =>
+        this.store.dispatch(new BudgetActions.LoadBudget({ uid, budgetCode }))
+      );
   }
 
   loadBudgets() {
-    this.user$.pipe(take(1)).subscribe(uid =>
-      this.store.dispatch(new BudgetActions.LoadBudgets(uid))
-    );
+    this.user$
+      .pipe(take(1))
+      .subscribe(uid =>
+        this.store.dispatch(new BudgetActions.LoadBudgets(uid))
+      );
   }
 
   getBudgets() {
     return this.store.select(getBudgetsState);
+  }
+  //
+  // getBudgetsProcess() {
+  //   return this.store.select(getProcessStateFactory(LOAD_BUDGETS_PROCESS_ID));
+  // }
+
+  getBudgetsLoading() {
+    return this.store.select(getProcessLoadingFactory(LOAD_BUDGETS_PROCESS_ID));
+  }
+  getBudgetsLoaded() {
+    return this.store.select(getProcessSuccessFactory(LOAD_BUDGETS_PROCESS_ID));
   }
 
   getBudget(budgetCode: string) {
