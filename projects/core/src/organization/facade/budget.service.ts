79d6ff19fe13c314618b7374c9c8c44139f2c876
Michał Gruca <pucek9@gmail.com>
2019-10-14 13:34:46 +0200

    up

--- projects/core/src/organization/facade/budget.service.ts
+++ projects/core/src/organization/facade/budget.service.ts
@@ -9,13 +9,16 @@ import {
   getBudgetsState,
   getBudgetState,
 } from '../store/selectors/budget.selector';
-import { take } from 'rxjs/operators';
+import { debounceTime, filter, map, take, tap } from 'rxjs/operators';
 import { AuthService } from '../../auth/facade/auth.service';
 import {
   getProcessLoadingFactory,
   getProcessSuccessFactory,
 } from '../../process/store/selectors/process.selectors';
 import { StateWithProcess } from '../../process/store/process-state';
+import { combineLatest } from 'rxjs';
+import { LoaderState } from '../../state/utils/loader/loader-state';
+import { Budget } from '../../model/budget.model';
 
 @Injectable()
 export class BudgetService {
@@ -44,6 +47,24 @@ export class BudgetService {
 
   getBudgets() {
     return this.store.select(getBudgetsState);
+  }
+
+  loadAndGetBudgets() {
+    return combineLatest([
+      this.getBudgetsLoading(),
+      this.getBudgetsLoaded(),
+      this.getBudgets(),
+    ]).pipe(
+      debounceTime(0),
+      filter(([loading]: [boolean, boolean, LoaderState<Budget>]) => !loading),
+      tap(([, loaded]: [boolean, boolean, LoaderState<Budget>]) => {
+        if (!loaded) {
+          this.loadBudgets();
+        }
+      }),
+      filter(([, loaded]: [boolean, boolean, LoaderState<Budget>]) => loaded),
+      map(([, , budgets]: [boolean, boolean, LoaderState<Budget>]) => budgets)
+    );
   }
   //
   // getBudgetsProcess() {
