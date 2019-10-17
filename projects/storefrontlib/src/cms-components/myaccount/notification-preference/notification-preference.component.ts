8178a930141b5a52c68bed5e4f093b91888e65da
Jerify02 <i323629@c02xv131jgh5.dhcp.ctul.sap.corp>
2019-10-17 12:29:26 +0800

    code improvement

--- projects/storefrontlib/src/cms-components/myaccount/notification-preference/notification-preference.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/notification-preference/notification-preference.component.ts
@@ -3,7 +3,8 @@ import {
   NotificationPreference,
   UserNotificationPreferenceService,
 } from '@spartacus/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-notification-preference',
@@ -11,8 +12,8 @@ import { Observable } from 'rxjs';
 })
 export class NotificationPreferenceComponent implements OnInit {
   preferences$: Observable<NotificationPreference[]>;
-  getNotificationPreferencesloading: boolean;
-  updateNotificationPreferencesLoading: boolean;
+  isLoading$: Observable<boolean>;
+  preferences: NotificationPreference[] = [];
 
   constructor(
     private notificationPreferenceService: UserNotificationPreferenceService
@@ -20,36 +21,31 @@ export class NotificationPreferenceComponent implements OnInit {
 
   ngOnInit() {
     this.notificationPreferenceService.resetNotificationPreferences();
-
-    this.preferences$ = this.notificationPreferenceService.getPreferences();
-    this.notificationPreferenceService
-      .getPreferencesLoading()
-      .subscribe(loading => (this.getNotificationPreferencesloading = loading));
-    this.notificationPreferenceService
-      .getUpdatePreferencesResultLoading()
-      .subscribe(
-        loading => (this.updateNotificationPreferencesLoading = loading)
-      );
+    this.preferences$ = this.notificationPreferenceService
+      .getPreferences()
+      .pipe(tap(preferences => (this.preferences = preferences)));
     this.notificationPreferenceService.loadPreferences();
+
+    this.isLoading$ = combineLatest([
+      this.notificationPreferenceService.getPreferencesLoading(),
+      this.notificationPreferenceService.getUpdatePreferencesResultLoading(),
+    ]).pipe(
+      map(([prefsLoading, updateLoading]) => prefsLoading || updateLoading)
+    );
   }
 
   updatePreference(preference: NotificationPreference) {
     const updatedPreferences = [];
-    this.notificationPreferenceService
-      .getPreferences()
-      .subscribe(preferences => {
-        preferences.forEach(p => {
-          if (p.channel === preference.channel) {
-            updatedPreferences.push({
-              ...p,
-              enabled: !p.enabled,
-            });
-          } else {
-            updatedPreferences.push(p);
-          }
+    this.preferences.forEach(p => {
+      if (p.channel === preference.channel) {
+        updatedPreferences.push({
+          ...p,
+          enabled: !p.enabled,
         });
-      })
-      .unsubscribe();
+      } else {
+        updatedPreferences.push(p);
+      }
+    });
     this.notificationPreferenceService.updatePreferences(updatedPreferences);
   }
 }
