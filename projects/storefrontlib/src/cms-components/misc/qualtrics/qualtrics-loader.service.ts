96a6077d297aea573cc78212a95fdad9e86d4011
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-10-18 09:53:37 -0400

    add some small refactor

--- projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.ts
+++ projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.ts
@@ -11,22 +11,23 @@ export class QualtricsLoaderService {
   qualtricsLoaded$ = new BehaviorSubject<boolean>(false);
 
   constructor(private winRef: WindowRef, private config: QualtricsConfig) {
-    if (Boolean(this.winRef.nativeWindow) && Boolean(this.winRef.document)) {
+    if (
+      Boolean(this.winRef.nativeWindow) &&
+      Boolean(this.winRef.document) &&
+      this.isQualtricsConfigured()
+    ) {
       this.initialize();
       this.setup();
     }
   }
 
   private initialize(): void {
-    fromEvent(this.winRef.nativeWindow, 'qsi_js_loaded')
-      .pipe(filter(_ => this.isQualtricsConfigured()))
-      .subscribe(_ => this.qualtricsLoaded$.next(true));
+    fromEvent(this.winRef.nativeWindow, 'qsi_js_loaded').subscribe(_ =>
+      this.qualtricsLoaded$.next(true)
+    );
   }
 
   private setup(): void {
-    if (!this.isQualtricsConfigured()) {
-      return;
-    }
     const qualtricsScript = this.winRef.document.createElement('script');
     qualtricsScript.type = 'text/javascript';
     qualtricsScript.defer = true;
