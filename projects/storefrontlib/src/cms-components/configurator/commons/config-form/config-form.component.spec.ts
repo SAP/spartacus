adb8d8dc7ea9b76540367f8ab867dccda493e183
d054488 <lukas.schuppel@sap.com>
2019-10-18 14:08:25 +0200

    GH-4997: Fix unit tests

--- projects/storefrontlib/src/cms-components/configurator/commons/config-form/config-form.component.spec.ts
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-form/config-form.component.spec.ts
@@ -6,6 +6,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import {
   Configurator,
   ConfiguratorCommonsService,
+  ConfiguratorGroupsService,
   I18nTestingModule,
   RoutingService,
 } from '@spartacus/core';
@@ -70,6 +71,11 @@ class MockConfiguratorCommonsService {
     return of(false);
   }
 }
+class MockConfiguratorGroupsService {
+  getCurrentGroup() {
+    return of('');
+  }
+}
 
 describe('ConfigurationFormComponent', () => {
   let component: ConfigFormComponent;
@@ -97,6 +103,11 @@ describe('ConfigurationFormComponent', () => {
         {
           provide: ConfiguratorCommonsService,
           useClass: MockConfiguratorCommonsService,
+        },
+
+        {
+          provide: ConfiguratorGroupsService,
+          useClass: MockConfiguratorGroupsService,
         },
       ],
     })
