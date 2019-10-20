e2b0adbe68e0991f55502f21ec2995485c96f671
dunqan <dunqan@w9.pl>
2019-10-20 22:57:41 +0200

    scoped loader specs

--- projects/core/src/state/utils/scoped-loader/entity-scoped-loader.reducer.ts
+++ projects/core/src/state/utils/scoped-loader/entity-scoped-loader.reducer.ts
@@ -8,8 +8,8 @@ import { EntityLoaderAction } from '../entity-loader/entity-loader.action';
 import { LoaderState } from '../loader/loader-state';
 
 /**
- * Higher order reducer that wraps LoaderReducer and EntityReducer enhancing
- * single state reducer to support multiple entities with generic loading flags
+ * Higher order reducer that wraps scopedLoaderReducer and EntityReducer enhancing
+ * single state reducer to support multiple entities with generic loading flags and scopes
  */
 export function entityScopedLoaderReducer<T>(
   entityType: string,
