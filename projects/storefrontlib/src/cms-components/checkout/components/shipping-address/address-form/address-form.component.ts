76db411528db784c063f5c0191b38978fb15c673
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2019-10-21 12:28:07 -0400

    fixed unwanted test change

--- projects/storefrontlib/src/cms-components/checkout/components/shipping-address/address-form/address-form.component.ts
+++ projects/storefrontlib/src/cms-components/checkout/components/shipping-address/address-form/address-form.component.ts
@@ -26,8 +26,8 @@ import {
   ModalRef,
   ModalService,
 } from '../../../../../shared/components/modal/index';
-import { sortTitles } from '../../../../../shared/utils/forms/title-utils';
 import { SuggestedAddressDialogComponent } from './suggested-addresses-dialog/suggested-addresses-dialog.component';
+import { sortTitles } from '../../../../../shared/utils/forms/title-utils';
 
 @Component({
   selector: 'cx-address-form',
