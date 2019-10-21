95831a67cd4371df165d327523e47c080cd3ae7a
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-10-21 01:56:36 -0400

    first pass for unit test

--- projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.spec.ts
+++ projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.spec.ts
@@ -9,13 +9,19 @@ const isDataLoadedMethod = 'isDataLoaded';
 const initializeMethod = 'initialize';
 const setupMethod = 'setup';
 const isQualtricsConfiguredMethod = 'isQualtricsConfigured';
+const event = new Event('qsi_js_loaded');
 
 const mockQualtricsConfig: QualtricsConfig = {
-  qualtrics: {},
+  qualtrics: {
+    projectId: 'Test123',
+  },
 };
 
 const mockWindowRef = {
   nativeWindow: {
+    dispatchEvent: (e): Event => {
+      return e;
+    },
     QSI: {
       API: {
         unload: (): void => {},
@@ -28,22 +34,28 @@ const mockWindowRef = {
       },
     },
   },
-  document: {},
+  document: {
+    createElement: (): void => {},
+    appendChild: (): void => {},
+  },
 };
 
 describe('QualtricsLoaderService', () => {
   let service: QualtricsLoaderService;
   let winRef: WindowRef;
+  let config: QualtricsConfig;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        QualtricsLoaderService,
         { provide: QualtricsConfig, useValue: mockQualtricsConfig },
         { provide: WindowRef, useValue: mockWindowRef },
       ],
     });
 
     winRef = TestBed.get(WindowRef as Type<WindowRef>);
+    config = TestBed.get(QualtricsConfig as Type<QualtricsConfig>);
     service = TestBed.get(QualtricsLoaderService as Type<
       QualtricsLoaderService
     >);
@@ -63,25 +75,59 @@ describe('QualtricsLoaderService', () => {
 
   describe(initializeMethod, () => {
     it('should call qualtricsLoaded$.next(true) when qsi_js_loaded event is fired', () => {
-      // TODO:
+      //always false no matter what I try
+      //https://stackoverflow.com/questions/45111498/unit-test-angular-2-service-subject
+      winRef.nativeWindow.dispatchEvent(event);
+      service[initializeMethod]();
+
+      service['qualtricsLoaded$'].subscribe(test => {
+        console.log('mreiogmoergmoe', test);
+      });
     });
   });
 
   describe(setupMethod, () => {
     it('should append the script and div element', () => {
-      // TODO:
+      const qualtricsScript = {
+        asymmetricMatch: function(actual) {
+          return (
+            actual.type === 'text/javascript' &&
+            actual.defer === true &&
+            actual.src === 'assets/qualtricsIntegration.js'
+          );
+        },
+      };
+
+      const idScript = {
+        asymmetricMatch: function(actual) {
+          return actual.id === mockQualtricsConfig.qualtrics.projectId;
+        },
+      };
+
+      spyOn(winRef.document, 'appendChild');
+
+      service[setupMethod]();
+
+      expect(winRef.document).toHaveBeenCalledWith(qualtricsScript);
+      expect(winRef.document).toHaveBeenCalledWith(idScript);
     });
   });
 
   describe(isQualtricsConfiguredMethod, () => {
     describe('when both qualtrics and qualtrics.project are defined', () => {
       it('should return true', () => {
-        // TODO:
+        config.qualtrics = mockQualtricsConfig.qualtrics;
+        const result = service[isQualtricsConfiguredMethod]();
+
+        expect(result).toEqual(true);
       });
     });
     describe('when either qualtrics or qualtrics.project are falsy', () => {
       it('should return false', () => {
-        // TODO:
+        config.qualtrics = {};
+        const result = service[isQualtricsConfiguredMethod]();
+
+        expect(result).toEqual(false);
       });
     });
   });
