dff08ddbef9ba13398d23a6ed662aaaa9130bdf8
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-10-22 16:31:52 -0400

    GH-5082: tweaks

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -63,9 +63,6 @@ if (!environment.production) {
         chunks: translationChunksConfig,
         fallbackLang: 'en',
       },
-      anonymousConsents: {
-        requiredConsents: ['MARKETING_NEWSLETTER'],
-      },
       features: {
         level: '1.3',
         anonymousConsents: true,
