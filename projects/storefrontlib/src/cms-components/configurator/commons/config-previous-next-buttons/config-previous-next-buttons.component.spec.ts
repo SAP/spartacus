116ff0cd791194b45f37c5852fe93f582a1a123f
steinsebastian <54309425+steinsebastian@users.noreply.github.com>
2019-10-24 18:25:35 +0200

    Feature/gh 5110 move prev next btns to slot (#5135)
    * GH-5092: Add Group component; remove navigation
    
    * GH-5092: Fix dependencies, add unit tests
    
    * GH-5110: Add prev/next to own CMS-component
    
    * GH-5110: Style component
    
    * GH-5110: Fix prettier issues
    
    * GH-5110: Incorporate feedback

--- projects/storefrontlib/src/cms-components/configurator/commons/config-previous-next-buttons/config-previous-next-buttons.component.spec.ts
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-previous-next-buttons/config-previous-next-buttons.component.spec.ts
@@ -1,18 +1,62 @@
-import { ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Type } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { I18nTestingModule } from '@spartacus/core';
-import { of } from 'rxjs';
+import {
+  ConfiguratorGroupsService,
+  I18nTestingModule,
+  RouterState,
+  RoutingService,
+} from '@spartacus/core';
+import { Observable, of } from 'rxjs';
 import { ConfigPreviousNextButtonsComponent } from './config-previous-next-buttons.component';
+
+const PRODUCT_CODE = 'CONF_LAPTOP';
+
+const mockRouterState: any = {
+  state: {
+    params: {
+      rootProduct: PRODUCT_CODE,
+    },
+  },
+};
+
+class MockRoutingService {
+  getRouterState(): Observable<RouterState> {
+    return of(mockRouterState);
+  }
+}
+
+class MockConfiguratorGroupsService {
+  getCurrentGroup() {
+    return of('');
+  }
+  getNextGroup() {
+    return of('');
+  }
+  getPreviousGroup() {
+    return of('');
+  }
+}
 
 describe('ConfigPreviousNextButtonsComponent', () => {
   let classUnderTest: ConfigPreviousNextButtonsComponent;
   let fixture: ComponentFixture<ConfigPreviousNextButtonsComponent>;
+  let configurationGroupsService: ConfiguratorGroupsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [I18nTestingModule],
       declarations: [ConfigPreviousNextButtonsComponent],
+      providers: [
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService,
+        },
+        {
+          provide: ConfiguratorGroupsService,
+          useClass: MockConfiguratorGroupsService,
+        },
+      ],
     })
       .overrideComponent(ConfigPreviousNextButtonsComponent, {
         set: {
@@ -25,6 +69,9 @@ describe('ConfigPreviousNextButtonsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfigPreviousNextButtonsComponent);
     classUnderTest = fixture.componentInstance;
+    configurationGroupsService = TestBed.get(ConfiguratorGroupsService as Type<
+      ConfiguratorGroupsService
+    >);
     fixture.detectChanges();
   });
 
@@ -32,8 +79,60 @@ describe('ConfigPreviousNextButtonsComponent', () => {
     expect(classUnderTest).toBeTruthy();
   });
 
+  it('isFirstGroup should return true if the previous group of current group is null', () => {
+    classUnderTest.ngOnInit();
+    fixture.detectChanges();
+    spyOn(configurationGroupsService, 'getPreviousGroup').and.returnValue(
+      of(null)
+    );
+    const isFirstGroup = classUnderTest.isFirstGroup();
+    expect(isFirstGroup).toBeDefined();
+    isFirstGroup.subscribe(group => {
+      expect(group).toEqual(true);
+    });
+  });
+
+  it('isFirstGroup should return false if the previous group of current group is not null', () => {
+    classUnderTest.ngOnInit();
+    fixture.detectChanges();
+    spyOn(configurationGroupsService, 'getPreviousGroup').and.returnValue(
+      of('anyGroupId')
+    );
+    const isFirstGroup = classUnderTest.isFirstGroup();
+    expect(isFirstGroup).toBeDefined();
+    isFirstGroup.subscribe(group => {
+      expect(group).toEqual(false);
+    });
+  });
+
+  it('isLastGroup should return true if the next group of current group is null', () => {
+    classUnderTest.ngOnInit();
+    fixture.detectChanges();
+    spyOn(configurationGroupsService, 'getNextGroup').and.returnValue(of(null));
+    const isLastGroup = classUnderTest.isLastGroup();
+    expect(isLastGroup).toBeDefined();
+    isLastGroup.subscribe(group => {
+      expect(group).toEqual(true);
+    });
+  });
+
+  it('isLastGroup should return false if the next group of current group is not null', () => {
+    classUnderTest.ngOnInit();
+    fixture.detectChanges();
+    spyOn(configurationGroupsService, 'getNextGroup').and.returnValue(
+      of('anyGroupId')
+    );
+    const isLastGroup = classUnderTest.isLastGroup();
+    expect(isLastGroup).toBeDefined();
+    isLastGroup.subscribe(group => {
+      expect(group).toEqual(false);
+    });
+  });
+
   it('should display previous button as disabled if it is the first group', () => {
-    classUnderTest.isFirstGroup = of(true);
+    spyOn(configurationGroupsService, 'getPreviousGroup').and.returnValue(
+      of(null)
+    );
     fixture.detectChanges();
     const prevBtn = fixture.debugElement.query(By.css('.btn-action'))
       .nativeElement;
@@ -41,7 +140,9 @@ describe('ConfigPreviousNextButtonsComponent', () => {
   });
 
   it('should display previous button as enabled if it is not the first group', () => {
-    classUnderTest.isFirstGroup = of(false);
+    spyOn(configurationGroupsService, 'getPreviousGroup').and.returnValue(
+      of('anyGroupId')
+    );
     fixture.detectChanges();
     const prevBtn = fixture.debugElement.query(By.css('.btn-action'))
       .nativeElement;
@@ -49,7 +150,7 @@ describe('ConfigPreviousNextButtonsComponent', () => {
   });
 
   it('should display next button as disabled if it is the last group', () => {
-    classUnderTest.isLastGroup = of(true);
+    spyOn(configurationGroupsService, 'getNextGroup').and.returnValue(of(null));
     fixture.detectChanges();
     const lastBtn = fixture.debugElement.query(By.css('.btn-secondary'))
       .nativeElement;
@@ -57,7 +158,9 @@ describe('ConfigPreviousNextButtonsComponent', () => {
   });
 
   it('should display next button as enabled if it is not the last group', () => {
-    classUnderTest.isLastGroup = of(false);
+    spyOn(configurationGroupsService, 'getNextGroup').and.returnValue(
+      of('anyGroupId')
+    );
     fixture.detectChanges();
     const prevBtn = fixture.debugElement.query(By.css('.btn-secondary'))
       .nativeElement;
