82136b11e144268e78a2967f35431cf1f11b86a9
Patrick Labadie <patrick.labadie@sap.com>
2019-10-24 13:19:44 -0400

    GH-4739: Changed the logout sequence in the logout guard

--- projects/storefrontlib/src/cms-components/user/logout/logout-guard.ts
+++ projects/storefrontlib/src/cms-components/user/logout/logout-guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
 import {
   AuthService,
   CmsService,
@@ -8,6 +7,7 @@ import {
   RoutingService,
   SemanticPathService,
 } from '@spartacus/core';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -22,8 +22,6 @@ export class LogoutGuard implements CanActivate {
   ) {}
 
   canActivate(): Observable<any> {
-    this.logout();
-
     return this.cms
       .hasPage({
         id: this.semanticPathService.get('logout'),
@@ -34,6 +32,7 @@ export class LogoutGuard implements CanActivate {
           if (!hasPage) {
             this.routing.go({ cxRoute: 'home' });
           }
+          this.logout();
         })
       );
   }
