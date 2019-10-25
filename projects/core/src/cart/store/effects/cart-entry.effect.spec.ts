f94073ca49229996af62a39ecf2fa2e02c81fa88
Marcin Lasak <lasakmarcin90@gmail.com>
2019-10-25 09:45:10 +0200

    test: Unit test for cart entry effect

--- projects/core/src/cart/store/effects/cart-entry.effect.spec.ts
+++ projects/core/src/cart/store/effects/cart-entry.effect.spec.ts
@@ -30,6 +30,7 @@ class MockCartEntryConnector {
 describe('Cart effect', () => {
   let entryEffects: fromEffects.CartEntryEffects;
   let actions$: Observable<Action>;
+  let cartEntryConnector: CartEntryConnector;
 
   const userId = 'testUserId';
   const cartId = 'testCartId';
@@ -47,6 +48,9 @@ describe('Cart effect', () => {
 
     entryEffects = TestBed.get(fromEffects.CartEntryEffects as Type<
       fromEffects.CartEntryEffects
+    >);
+    cartEntryConnector = TestBed.get(CartEntryConnector as Type<
+      CartEntryConnector
     >);
   });
 
@@ -68,6 +72,37 @@ describe('Cart effect', () => {
       const expected = cold('-b', { b: completion });
 
       expect(entryEffects.addEntry$).toBeObservable(expected);
+    });
+  });
+
+  describe('addEntries$', () => {
+    it('should add multiple entries', () => {
+      const action = new CartActions.CartAddEntries({
+        userId,
+        cartId,
+        products: [
+          { productCode: 'testProductCode', quantity: 1 },
+          { productCode: 'testProductCode2', quantity: 2 },
+        ],
+      });
+      const cartAddEntriesSuccessCompletion = new CartActions.CartAddEntriesSuccess(
+        {}
+      );
+      const loadCartCompletion = new CartActions.LoadCart({
+        userId,
+        cartId,
+        extraData: {
+          addEntries: true,
+        },
+      });
+
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-(bc)', {
+        b: cartAddEntriesSuccessCompletion,
+        c: loadCartCompletion,
+      });
+      expect(entryEffects.addEntries$).toBeObservable(expected);
+      expect(cartEntryConnector.add).toHaveBeenCalledTimes(2);
     });
   });
 
