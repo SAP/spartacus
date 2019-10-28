d1c30f5bc545da9aef97da6eb3ca13f069a4006d
Michał Gruca <pucek9@gmail.com>
2019-10-28 11:48:33 +0100

    up

--- projects/core/src/organization/connectors/budget/budget.connector.spec.ts
+++ projects/core/src/organization/connectors/budget/budget.connector.spec.ts
@@ -5,23 +5,31 @@ import { BudgetAdapter } from './budget.adapter';
 import { BudgetConnector } from './budget.connector';
 import createSpy = jasmine.createSpy;
 
+const userId = 'userId';
+const budgetCode = 'budgetCode';
+
+const budget = {
+  code: budgetCode
+};
+
 class MockBudgetAdapter implements BudgetAdapter {
-  load = createSpy('BudgetAdapter.load').and.callFake(code =>
-    of('product' + code)
+  load = createSpy('BudgetAdapter.load').and.callFake(() =>
+    of(budget)
   );
-  loadList = createSpy('BudgetAdapter.loadList').and.callFake(code =>
-    of('product' + code)
+  loadList = createSpy('BudgetAdapter.loadList').and.callFake(() =>
+    of([budget])
   );
-  create = createSpy('BudgetAdapter.create').and.callFake(code =>
-    of('product' + code)
+  create = createSpy('BudgetAdapter.create').and.callFake(() =>
+    of(budget)
   );
-  update = createSpy('BudgetAdapter.update').and.callFake(code =>
-    of('product' + code)
+  update = createSpy('BudgetAdapter.update').and.callFake(() =>
+    of(budget)
   );
 }
 
 describe('BudgetConnector', () => {
   let service: BudgetConnector;
+  let adapter: BudgetAdapter;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,10 +37,30 @@ describe('BudgetConnector', () => {
     });
 
     service = TestBed.get(BudgetConnector as Type<BudgetConnector>);
+    adapter = TestBed.get(BudgetAdapter as Type<BudgetAdapter>);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should load budget', () => {
+    service.get(userId, budgetCode);
+    expect(adapter.load).toHaveBeenCalledWith(userId, budgetCode);
+  });
+
+  it('should load budgets', () => {
+    service.getList(userId);
+    expect(adapter.loadList).toHaveBeenCalledWith(userId, undefined);
+  });
+
+  it('should create budget', () => {
+    service.create(userId, budget);
+    expect(adapter.create).toHaveBeenCalledWith(userId, budget);
+  });
+
+  it('should update budget', () => {
+    service.update(userId, budget);
+    expect(adapter.update).toHaveBeenCalledWith(userId, budget);
+  });
 });
