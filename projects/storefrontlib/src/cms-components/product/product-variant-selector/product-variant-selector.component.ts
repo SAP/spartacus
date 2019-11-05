af28dfeece46e6897167eab11af63a75ab4700c1
Mateusz Kolasa <mateusz.kolasa@sap.com>
2019-11-05 13:12:41 +0100

    Variants refactoring

--- projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.ts
+++ projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Product, VariantOption } from '@spartacus/core';
+import { Product, BaseOption, VariantType } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CurrentProductService } from '../current-product.service';
 import { tap, filter, distinctUntilChanged } from 'rxjs/operators';
@@ -7,28 +7,31 @@ import { tap, filter, distinctUntilChanged } from 'rxjs/operators';
 @Component({
   selector: 'cx-product-variant-selector',
   templateUrl: './product-variant-selector.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class ProductVariantSelectorComponent {
   constructor(private currentProductService: CurrentProductService) {}
 
-  variants: Array<VariantOption[]> = [];
+  variants: BaseOption[];
+
+  variantType = VariantType;
 
   product$: Observable<Product> = this.currentProductService.getProduct().pipe(
     filter(product => !!product),
     distinctUntilChanged(),
     tap(product => {
+      this.variants = [];
       product.baseOptions.forEach(option => {
         if (option && option.variantType) {
-          this.variants[option.variantType] = option;
+          this.variants.push(option);
         }
       });
 
       if (product.variantType) {
-        this.variants[product.variantType] = {
+        this.variants.push({
           options: product.variantOptions,
           variantType: product.variantType,
-        };
+        });
       }
     })
   );
