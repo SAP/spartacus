3c5e7e6a5308f4386f0392ce1ed2822c9337f56e
corey Aloia <corey.aloia@sap.com>
2019-11-05 10:35:08 +0100

    changed to not subscribe manually

--- projects/cds/src/components/profile-tag/profile-tag.component.ts
+++ projects/cds/src/components/profile-tag/profile-tag.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Event as NgRouterEvent } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ProfileTagInjector } from './profile-tag.injector';
-
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'cx-profiletag',
     template: `
     <ng-container *ngIf="profileTagEnabled$ | async"></ng-container>
   `,
 })
 export class ProfileTagComponent implements OnInit {
-    profileTagEnabled$ = this.profileTagInjector.start();
+    profileTagEnabled$: Observable<Boolean | NgRouterEvent>;
     constructor(private profileTagInjector: ProfileTagInjector) {
+        this.profileTagEnabled$ = this.profileTagInjector.injectScript();
         console.log('starting');
     }
     ngOnInit(): void {
--- projects/cds/src/components/profile-tag/profile-tag.injector.ts
+++ projects/cds/src/components/profile-tag/profile-tag.injector.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { AnonymousConsentsService, BaseSiteService, WindowRef } from '@spartacus/core';
-import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { Event as NgRouterEvent, NavigationEnd, Router } from '@angular/router';
+import { AnonymousConsent, AnonymousConsentsService, BaseSiteService, WindowRef } from '@spartacus/core';
+import { BehaviorSubject, merge, Observable } from 'rxjs';
+import { distinctUntilChanged, filter, map, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { CdsConfig, ProfileTagConfig } from '../../config/config.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileTagInjector {
-  profiletagConfig: ProfileTagConfig
-  bannerVisible$: Observable<boolean>
+  profiletagConfig: ProfileTagConfig;
+  consentChanged$: Observable<boolean>;
+  pageLoaded$: Observable<NgRouterEvent>;
+  w: any;
+  isProfileTagLoaded = new BehaviorSubject<boolean>(false);
+  startTracking$: Observable<Boolean | NgRouterEvent>;
   constructor(
     private winRef: WindowRef,
     private config: CdsConfig,
@@ -18,22 +22,82 @@ export class ProfileTagInjector {
     private router: Router,
     private anonymousConsentsService: AnonymousConsentsService
   ) {
+    this.w = this.winRef.nativeWindow;
     this.profiletagConfig = this.config.cds.profileTag;
-    this.bannerVisible$ = this.anonymousConsentsService.isBannerVisible();
+    this.consentChanged$ = this.consentChanged();
+    this.pageLoaded$ = this.pageLoaded();
+    this.startTracking$ = merge(this.consentChanged$, this.pageLoaded$);
   }
-  start() {
-    this.addTracker();
+
+  injectScript(): Observable<Boolean | NgRouterEvent> {
     this.addScript();
+    return this.addTracker().pipe(
+      switchMap(() => {
+        return this.isProfileTagLoaded.pipe(
+          filter(Boolean),
+          switchMap(() => {
+            return this.startTracking$
+          })
+        )
+      })
+    );
   }
-  private addTracker(): void {
-    this.baseSiteService
+
+  private pageLoaded(): Observable<NgRouterEvent> {
+    return this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        shareReplay(1),
+        tap(() => {
+          this.w.Y_TRACKING.push({ event: 'Navigated' })
+        }),
+        tap(() => 'called ytracking Navigated'),
+      )
+  }
+  private consentChanged(): Observable<Boolean[]> {
+    return this.anonymousConsentsService.getConsents()
+      .pipe(
+        map((anonymousConsents: AnonymousConsent[]) => {
+          return anonymousConsents.map((anonymousConsent) => {
+            return this.anonymousConsentsService.isConsentGiven(anonymousConsent);
+          })
+        }),
+        distinctUntilChanged((prev, curr) => this.areArraysEqual(prev, curr)),
+        filter((consentsGranted: Boolean[]) => {
+          for (const consentGranted of consentsGranted) {
+            if (!consentGranted) {
+              return false;
+            }
+          }
+          return true;
+        }),
+        tap(() => {
+          this.w.Y_TRACKING.push({ event: 'ConsentChanged', granted: true })
+        }),
+        tap(() => { console.log('called ytracking ConsentChanged') }));
+  }
+
+  private areArraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (let i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
+
+  private addTracker(): Observable<string> {
+    return this.baseSiteService
       .getActive()
-      .pipe(filter(Boolean))
-      .subscribe((site: string) => {
-        const newConfig: ProfileTagConfig = { ...this.profiletagConfig };
-        newConfig.siteId = site;
-        this.track(newConfig);
-      });
+      .pipe(
+        filter(Boolean),
+        tap((site: string) => {
+          const newConfig: ProfileTagConfig = { ...this.profiletagConfig };
+          newConfig.siteId = site;
+          this.track(newConfig);
+        }))
   }
 
   private addScript(): void {
@@ -59,23 +123,11 @@ export class ProfileTagInjector {
   private profileTagEventTriggered(profileTagEvent) {
     switch (profileTagEvent.eventName) {
       case 'Loaded':
-        this.addProfileTagListeners();
+        this.isProfileTagLoaded.next(true);
         break;
       default:
         //add logger from spartacus. this.logger.info(`Unsupported Event ${profileTagEvent.eventName}`)
         break;
-    }
-  }
-
-  private addProfileTagListeners() {
-    {
-      const w: any = this.winRef.nativeWindow;
-      this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
-        w.Y_TRACKING.push({ event: 'Navigated' });
-      });
-      this.bannerVisible$.subscribe((visible: Boolean) => {
-        w.Y_TRACKING.push({ event: 'ConsentChanged', granted: !visible });
-      });
     }
   }
 }
