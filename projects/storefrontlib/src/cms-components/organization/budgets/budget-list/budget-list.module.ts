e2f6601223885658df236b088000eed9a3e586a5
Michał Gruca <pucek9@gmail.com>
2019-11-05 11:39:22 +0100

    up

---
+++ projects/storefrontlib/src/cms-components/organization/budgets/budget-list/budget-list.module.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {
+  AuthGuard,
+  CmsConfig,
+  ConfigModule,
+  I18nModule,
+  UrlModule,
+  UserService,
+} from '@spartacus/core';
+import { ListNavigationModule } from '../../../../shared/components/list-navigation/list-navigation.module';
+import { BudgetListComponent } from './order-history.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ConfigModule.withConfig(<CmsConfig>{
+      cmsComponents: {
+        AccountOrderHistoryComponent: {
+          component: BudgetListComponent,
+          guards: [AuthGuard],
+        },
+      },
+    }),
+    RouterModule,
+    FormsModule,
+    NgSelectModule,
+    ListNavigationModule,
+    UrlModule,
+    I18nModule,
+  ],
+  declarations: [BudgetListComponent],
+  exports: [BudgetListComponent],
+  providers: [UserService],
+  entryComponents: [BudgetListComponent],
+})
+export class BudgetListModule {}
