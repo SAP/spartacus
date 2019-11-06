631895df30d46cdfe10fae5b3ccbe4ea0e5c40ff
Weizheng Gao <weizheng.gao@sap.com>
2019-11-06 17:20:50 -0500

    feature/GH-5116

--- projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/cancellation-return-items/cancellation-return-items.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/cancellation-return-items/cancellation-return-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 import { OrderEntry } from '@spartacus/core';
 
@@ -6,8 +7,48 @@ import { OrderEntry } from '@spartacus/core';
   selector: 'cx-cancellation-return-items',
   templateUrl: './cancellation-return-items.component.html',
 })
-export class CancellationReturnItemsComponent {
+export class CancellationReturnItemsComponent implements OnInit {
   @Input() entries: OrderEntry[];
-  @Input() confirmation: boolean;
-  @Input() cancelOrder: boolean;
+  @Input() confirmation = false;
+  @Input() cancelOrder = true;
+
+  form: FormGroup;
+  inputsControl: FormArray;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      entryInput: this.formBuilder.array([]),
+    });
+
+    this.inputsControl = this.form.get('entryInput') as FormArray;
+    this.entries.forEach(entry => {
+      this.inputsControl.push(
+        this.formBuilder.group({
+          orderEntryNumber: entry.entryNumber,
+          quantity: '',
+        })
+      );
+    });
+
+    console.log(this.form.value);
+  }
+
+  setComplete() {
+    console.log('here');
+
+    for (let i = 0; i < this.entries.length; i++) {
+      this.inputsControl.at(i).setValue({
+        orderEntryNumber: this.entries[i].entryNumber,
+        quantity: this.entries[i].quantity,
+      });
+    }
+
+    console.log(this.form.value);
+  }
+
+  confirm() {
+    console.log(this.form.value);
+  }
 }
--- projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/cancellation-return-items/cancellation-return-items.module.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/cancellation-return-items/cancellation-return-items.module.ts
@@ -1,22 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {
-  I18nModule,
-  //UrlModule,
-} from '@spartacus/core';
+import { I18nModule } from '@spartacus/core';
 
 import { MediaModule } from '../../../../../shared/index';
 
 import { CancellationReturnItemsComponent } from './cancellation-return-items.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    //UrlModule,
-    I18nModule,
-    MediaModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, I18nModule, MediaModule],
   declarations: [CancellationReturnItemsComponent],
   exports: [CancellationReturnItemsComponent],
   entryComponents: [CancellationReturnItemsComponent],
