d09298367aa95a29cc43245692d5629590db09f6
matt-williams8 <matthew.e.williams120+github@gmail.com>
2019-11-08 14:43:10 +0000

    Revert "COL-168 Remove cms component from connector/adapter branch"
    This reverts commit 6081fc3901a5f9bf361267202f602b8b5ba810ab.

---
+++ projects/cds/src/cms-components/merchandising/carousel/merchandising-carousel/merchandising-carousel.component.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Product } from '@spartacus/core';
+import { Observable, of } from 'rxjs';
+import { filter, flatMap, map } from 'rxjs/operators';
+import { CmsComponentData } from '../../../../../../storefrontlib/src/cms-structure/page/model/cms-component-data';
+import { CmsMerchandisingCarouselComponent as model } from '../../../../cds-models/cms.model';
+import { StrategyConnector } from './../../../../../../cds/src/merchandising/connectors/strategy/strategy.connector';
+
+@Component({
+  selector: 'cx-merchandising-carousel',
+  templateUrl: './merchandising-carousel.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MerchandisingCarouselComponent {
+  private componentData$: Observable<model> = this.componentData.data$.pipe(
+    filter(Boolean)
+  );
+
+  title$: Observable<string> = this.componentData$.pipe(
+    map(data => data.title)
+  );
+
+  size$: Observable<string> = this.componentData$.pipe(
+    map(data => data.numberToDisplay)
+  );
+
+  backgroundColor$: Observable<string> = this.componentData$.pipe(
+    map(data => data.backgroundColour)
+  );
+
+  textColor$: Observable<string> = this.componentData$.pipe(
+    map(data => data.textColour)
+  );
+
+  items$: Observable<Observable<Product>[]> = this.componentData$.pipe(
+    map(data => data.strategy),
+    flatMap(strategyId =>
+      this.strategyConnector.loadProductsForStrategy(strategyId)
+    ),
+    map(merchandisingProducts => merchandisingProducts.products),
+    map(products => products.map(product => of(product)))
+  );
+
+  constructor(
+    protected componentData: CmsComponentData<model>,
+    protected strategyConnector: StrategyConnector
+  ) {}
+}
---
+++ projects/cds/src/cms-components/merchandising/carousel/merchandising-carousel/merchandising-carousel.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CmsConfig, ConfigModule, UrlModule } from '@spartacus/core';
+import {
+  CarouselModule,
+  MediaModule,
+} from '../../../../../../storefrontlib/src/shared/components/index';
+import { MerchandisingCarouselComponent } from './merchandising-carousel.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CarouselModule,
+    MediaModule,
+    RouterModule,
+    UrlModule,
+    ConfigModule.withConfig(<CmsConfig>{
+      cmsComponents: {
+        MerchandisingCarouselComponent: {
+          component: MerchandisingCarouselComponent,
+        },
+      },
+    }),
+  ],
+  declarations: [MerchandisingCarouselComponent],
+  entryComponents: [MerchandisingCarouselComponent],
+  exports: [MerchandisingCarouselComponent],
+})
+export class MerchandisingCarouselModule {}
