b70448ae93faf87af2ed7fa894b5afb2a92da737
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-11-12 11:54:00 +0100

    introduce dynamic component, abusing cms component here as long as #4441 is not in place

--- projects/storefrontlib/src/cms-components/asm/asm.module.ts
+++ projects/storefrontlib/src/cms-components/asm/asm.module.ts
@@ -2,25 +2,27 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CmsConfig, ConfigModule, I18nModule } from '@spartacus/core';
+import { AsmLoaderModule } from './asm-loader/asm-loader.module';
 import { AsmMainUiComponent } from './asm-main-ui/asm-main-ui.component';
 import { AsmRootComponent } from './asm-root/asm-root.component';
-import { CSAgentLoginFormComponent } from './csagent-login-form/csagent-login-form.component';
-import { CustomerSelectionComponent } from './customer-selection/customer-selection.component';
 import { AsmSessionTimerComponent } from './asm-session-timer/asm-session-timer.component';
 import { FormatTimerPipe } from './asm-session-timer/format-timer.pipe';
+import { CSAgentLoginFormComponent } from './csagent-login-form/csagent-login-form.component';
 import { CustomerEmulationComponent } from './customer-emulation/customer-emulation.component';
+import { CustomerSelectionComponent } from './customer-selection/customer-selection.component';
 @NgModule({
   imports: [
     CommonModule,
+    AsmLoaderModule,
     ReactiveFormsModule,
     I18nModule,
-    ConfigModule.withConfig(<CmsConfig>{
+    ConfigModule.withConfig({
       cmsComponents: {
-        AsmComponent: {
+        asm: {
           component: AsmRootComponent,
         },
       },
-    }),
+    } as CmsConfig),
   ],
   declarations: [
     AsmMainUiComponent,
