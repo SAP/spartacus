14d1f17a33ca5b91a59a14c457f599e55c29f03f
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-11-12 11:49:06 +0100

    simplify asm root component

--- projects/storefrontlib/src/cms-components/asm/asm-root/asm-root.component.ts
+++ projects/storefrontlib/src/cms-components/asm/asm-root/asm-root.component.ts
@@ -1,46 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 import { AsmService, AsmUi } from '@spartacus/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-asm',
   templateUrl: './asm-root.component.html',
 })
-export class AsmRootComponent implements OnInit, OnDestroy {
-  private subscription = new Subscription();
-  asmUi$: Observable<AsmUi>;
+export class AsmRootComponent {
+  state$: Observable<AsmUi> = this.asmService.getAsmUiState();
 
-  constructor(
-    protected asmService: AsmService,
-    protected activatedRoute: ActivatedRoute
-  ) {}
+  constructor(protected asmService: AsmService) {}
 
-  ngOnInit() {
-    this.asmUi$ = this.asmService.getAsmUiState();
-
-    this.subscription.add(
-      this.activatedRoute.queryParamMap.subscribe(queryParams => {
-        if (queryParams.get('asm') === 'true') {
-          this.showUi();
-        }
-      })
-    );
-  }
-
-  expandUi(): void {
-    this.asmService.updateAsmUiState({ expanded: true });
-  }
-
-  collapseUi(): void {
-    this.asmService.updateAsmUiState({ expanded: false });
-  }
-
-  private showUi(): void {
-    this.asmService.updateAsmUiState({ visible: true });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  set collapse(expanded: boolean) {
+    this.asmService.updateAsmUiState({ expanded });
   }
 }
