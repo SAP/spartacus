4774ae675528f8c9a3ac12df8e3d8bb67dc74c25
qjimmy <quang-jimmy@hotmail.com>
2019-11-14 11:28:17 -0500

    public instance

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  occBaseUrl: 'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
+  occBaseUrl:
+    'https://accstorefront.c39j2-walkersde1-d3-public.model-t.cc.commerce.ondemand.com',
 };
