b3c2c1c75c866b05c10ee60d3b987e974f0cb6ed
Michał Gruca <pucek9@gmail.com>
2019-11-14 17:11:15 +0100

    moving components outside

--- projects/storefrontlib/src/shared/components/table/table.component.ts
+++ projects/storefrontlib/src/shared/components/table/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-table',
@@ -7,8 +7,6 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class TableComponent {
   constructor() {
-    this.sortListEvent = new EventEmitter<string>();
-    this.pageChangeEvent = new EventEmitter<number>();
     this.clickDetailsEvent = new EventEmitter<any>();
   }
 
@@ -19,42 +17,13 @@ export class TableComponent {
   columns: Array<{key: string, value: string}>;
 
   @Input()
-  sortLabels: [];
-
-  @Input()
   tableData$: Observable<any>;
-
-  @Input()
-  sortType$: BehaviorSubject<string>;
-
-  @Input()
-  currentPage$: BehaviorSubject<number>;
-
-  @Input()
-  isLoaded$: Observable<boolean>;
 
   @Input()
   cxRoute: string;
 
-  @Input()
-  header: string;
-
-  @Output()
-  sortListEvent: EventEmitter<string>;
-
-  @Output()
-  pageChangeEvent: EventEmitter<number>;
-
   @Output()
   clickDetailsEvent: EventEmitter<any>;
-
-  changeSortCode(sortCode: string): void {
-    this.sortListEvent.emit(sortCode);
-  }
-
-  pageChange(page: number): void {
-    this.pageChangeEvent.emit(page);
-  }
 
   goToDetail(params: any): void {
     this.clickDetailsEvent.emit(params);
