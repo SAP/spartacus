e0a28ddf4e4c7cff9ec5803c8689804dfa4004c3
Weizheng Gao <weizheng.gao@sap.com>
2019-11-14 12:38:52 -0500

    feature/GH-5119: add cancellation-return-request-input.guard

--- projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/guards/cancellation-return-request-input.guard.spec.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/cancellations-returns/guards/cancellation-return-request-input.guard.spec.ts
@@ -1,119 +1,59 @@
-/*import { Type } from '@angular/core';
+import { Type } from '@angular/core';
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
-import { UrlTree } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { RoutesConfig, RoutingConfigService } from '@spartacus/core';
-import { Observable, of } from 'rxjs';
-import { defaultStorefrontRoutesConfig } from '../../../cms-structure/routing/default-routing-config';
-import { CheckoutConfig } from '../config/checkout-config';
-import { defaultCheckoutConfig } from '../config/default-checkout-config';
-import { CheckoutConfigService } from '../services/checkout-config.service';
-import { CheckoutDetailsService } from '../services/checkout-details.service';
-import { DeliveryModeSetGuard } from './delivery-mode-set.guard';
+import { CancellationReturnRequestEntryInput } from '@spartacus/core';
+import { OrderDetailsService } from '../../order-details/order-details.service';
+import { CancellationReturnRequestInputGuard } from './cancellation-return-request-input.guard';
 
-const MockCheckoutConfig: CheckoutConfig = defaultCheckoutConfig;
-const MockRoutesConfig: RoutesConfig = defaultStorefrontRoutesConfig;
-
-const mockDeliveryModeCode = 'test mode code';
-
-class MockCheckoutDetailsService {
-  getSelectedDeliveryModeCode(): Observable<string> {
-    return of();
-  }
+class MockOrderDetailsService {
+  cancellationReturnRequestInputs: CancellationReturnRequestEntryInput[];
 }
 
-class MockRoutingConfigService {
-  getRouteConfig(routeName: string) {
-    return MockRoutesConfig[routeName];
-  }
-}
-class MockCheckoutConfigService {
-  getCheckoutStep() {}
-}
-
-describe(`DeliveryModeSetGuard`, () => {
-  let guard: DeliveryModeSetGuard;
-  let mockCheckoutDetailsService: MockCheckoutDetailsService;
-  let mockCheckoutConfig: CheckoutConfig;
-  let mockRoutingConfigService: RoutingConfigService;
-  let mockCheckoutConfigService: CheckoutConfigService;
+describe(`CancellationReturnRequestInputGuard`, () => {
+  let guard: CancellationReturnRequestInputGuard;
+  let mockOrderDetailsService: MockOrderDetailsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: CheckoutConfig, useValue: MockCheckoutConfig },
-        {
-          provide: CheckoutDetailsService,
-          useClass: MockCheckoutDetailsService,
-        },
-        { provide: RoutingConfigService, useClass: MockRoutingConfigService },
-        { provide: CheckoutConfigService, useClass: MockCheckoutConfigService },
+        { provide: OrderDetailsService, useClass: MockOrderDetailsService },
       ],
       imports: [RouterTestingModule],
     });
 
-    guard = TestBed.get(DeliveryModeSetGuard as Type<DeliveryModeSetGuard>);
-    mockCheckoutDetailsService = TestBed.get(CheckoutDetailsService as Type<
-      CheckoutDetailsService
+    guard = TestBed.get(CancellationReturnRequestInputGuard as Type<
+      CancellationReturnRequestInputGuard
     >);
-    mockCheckoutConfig = TestBed.get(CheckoutConfig as Type<CheckoutConfig>);
-    mockRoutingConfigService = TestBed.get(RoutingConfigService as Type<
-      RoutingConfigService
-    >);
-    mockCheckoutConfigService = TestBed.get(CheckoutConfigService as Type<
-      CheckoutConfigService
+    mockOrderDetailsService = TestBed.get(OrderDetailsService as Type<
+      OrderDetailsService
     >);
   });
 
-  it('should redirect to deliveryMode page when no modes selected', done => {
-    spyOn(
-      mockCheckoutDetailsService,
-      'getSelectedDeliveryModeCode'
-    ).and.returnValue(of(null));
+  describe(`when there is NO cancellationReturnRequestInputs`, () => {
+    it(`should return UrlTree to order return/cancel page`, () => {
+      mockOrderDetailsService.cancellationReturnRequestInputs = undefined;
+      const urlTree = guard.canActivate({
+        url: [
+          { path: 'my-account' },
+          { path: 'order' },
+          { path: 'return' },
+          { path: 'confirmation' },
+        ],
+      } as ActivatedRouteSnapshot);
 
-    spyOn(mockCheckoutConfigService, 'getCheckoutStep').and.returnValue(
-      MockCheckoutConfig.checkout.steps[1]
-    );
-
-    guard.canActivate().subscribe((result: boolean | UrlTree) => {
-      expect(result.toString()).toEqual(
-        `/${
-          mockRoutingConfigService.getRouteConfig(
-            MockCheckoutConfig.checkout.steps[1].routeName
-          ).paths[0]
-        }`
-      );
-      done();
+      expect(urlTree.toString()).toEqual('/my-account/order/return');
     });
   });
 
-  it('should redirect to default page if there is no deliveryMode step', done => {
-    spyOn(
-      mockCheckoutDetailsService,
-      'getSelectedDeliveryModeCode'
-    ).and.returnValue(of(''));
-    spyOn(console, 'warn');
-    mockCheckoutConfig.checkout.steps = [];
-
-    guard.canActivate().subscribe((result: boolean | UrlTree) => {
-      expect(console.warn).toHaveBeenCalledWith(
-        'Missing step with type deliveryMode in checkout configuration.'
-      );
-      expect(result.toString()).toEqual('/');
-      done();
+  describe(`when there is cancellationReturnRequestInputs`, () => {
+    it(`should return true`, () => {
+      mockOrderDetailsService.cancellationReturnRequestInputs = [
+        { orderEntryNumber: 0, quantity: 1 },
+      ];
+      expect(
+        guard.canActivate({ url: [] } as ActivatedRouteSnapshot)
+      ).toBeTruthy();
     });
   });
-
-  it('should not redirect to deliveryMode page when mode is selected', done => {
-    spyOn(
-      mockCheckoutDetailsService,
-      'getSelectedDeliveryModeCode'
-    ).and.returnValue(of(mockDeliveryModeCode));
-
-    guard.canActivate().subscribe((result: boolean | UrlTree) => {
-      expect(result).toEqual(true);
-
-      done();
-    });
-  });
-});*/
+});
