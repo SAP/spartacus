2b6e7245ecbbb85664ce14e74aa94dac6b4b4050
Mirosław Grochowski <m.grochowski@sap.com>
2019-11-15 16:18:52 +0100

    Introduce robots NOINDEX for non purchasable products

--- projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.ts
+++ projects/storefrontlib/src/cms-components/product/product-variant-selector/product-variant-selector.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {
-  RoutingService,
   Product,
   BaseOption,
   VariantType,
-  VariantOption,
 } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { CurrentProductService } from '../current-product.service';
@@ -18,7 +16,6 @@ import { tap, filter, distinctUntilChanged } from 'rxjs/operators';
 export class ProductVariantSelectorComponent implements OnInit {
   constructor(
     private currentProductService: CurrentProductService,
-    private routingService: RoutingService
   ) {}
 
   variants: BaseOption[] = [];
@@ -35,31 +32,7 @@ export class ProductVariantSelectorComponent implements OnInit {
             this.variants[option.variantType] = option;
           }
         });
-
-        if (!product.purchasable) {
-          const purchasableVariant = this.findPurchasableVariant(
-            product.variantOptions
-          );
-
-          if (purchasableVariant) {
-            this.routingService.go(
-              {
-                cxRoute: 'product',
-                params: { code: purchasableVariant.code },
-              },
-              null,
-              { replaceUrl: true }
-            );
-          }
-        }
       })
     );
-  }
-
-  private findPurchasableVariant(variants: VariantOption[]): VariantOption {
-    const results: VariantOption[] = variants.filter(variant => {
-      return variant.stock && variant.stock.stockLevel ? variant : false;
-    });
-    return !results.length && variants.length ? variants[0] : results[0];
   }
 }
