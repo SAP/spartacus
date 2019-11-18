a172fcfb8455f1a0581638ab1f8cfa1a0a887946
dunqan <dunqan@w9.pl>
2019-11-18 16:36:40 +0100

    refactor step 2

---
+++ projects/core/src/asm/http-interceptors/csagent-auth-error.interceptor.ts
@@ -0,0 +1,57 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {
+  InterceptorUtil,
+  USE_CUSTOMER_SUPPORT_AGENT_TOKEN,
+} from '../../occ/utils/interceptor-util';
+import { CustomerSupportAgentErrorHandlingService } from '../../asm/services/csagent-error-handling.service';
+
+@Injectable({ providedIn: 'root' })
+export class CsagentAuthErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private csagentErrorHandlingService: CustomerSupportAgentErrorHandlingService
+  ) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const isCustomerSupportAgentRequest = this.isCustomerSupportAgentRequest(
+      request
+    );
+    if (isCustomerSupportAgentRequest) {
+      request = InterceptorUtil.removeHeader(
+        USE_CUSTOMER_SUPPORT_AGENT_TOKEN,
+        request
+      );
+    }
+
+    return next.handle(request).pipe(
+      catchError((errResponse: any) => {
+        if (errResponse instanceof HttpErrorResponse) {
+          // Unauthorized
+          if (isCustomerSupportAgentRequest && errResponse.status === 401) {
+            this.csagentErrorHandlingService.terminateCustomerSupportAgentExpiredSession();
+          }
+        }
+        return throwError(errResponse);
+      })
+    );
+  }
+
+  private isCustomerSupportAgentRequest(request: HttpRequest<any>): boolean {
+    const isRequestMapping = InterceptorUtil.getInterceptorParam(
+      USE_CUSTOMER_SUPPORT_AGENT_TOKEN,
+      request.headers
+    );
+    return Boolean(isRequestMapping);
+  }
+}
--- projects/core/src/asm/http-interceptors/index.ts
+++ projects/core/src/asm/http-interceptors/index.ts
@@ -1,11 +1,17 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Provider } from '@angular/core';
 import { CustomerSupportAgentTokenInterceptor } from './csagent-token.interceptor';
+import { CsagentAuthErrorInterceptor } from './csagent-auth-error.interceptor';
 
 export const interceptors: Provider[] = [
   {
     provide: HTTP_INTERCEPTORS,
     useExisting: CustomerSupportAgentTokenInterceptor,
+    multi: true,
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useExisting: CsagentAuthErrorInterceptor,
     multi: true,
   },
 ];
