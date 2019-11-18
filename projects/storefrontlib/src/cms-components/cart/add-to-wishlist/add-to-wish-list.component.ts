fd41e23b90d348db2669360154e73388b71a611f
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2019-11-18 17:38:07 -0500

    Initail logic

--- projects/storefrontlib/src/cms-components/cart/add-to-wishlist/add-to-wish-list.component.ts
+++ projects/storefrontlib/src/cms-components/cart/add-to-wishlist/add-to-wish-list.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { OrderEntry, Product } from '@spartacus/core';
+import { WishListService } from 'projects/core/src/cart/facade/wish-list.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CurrentProductService } from '../../product';
 
 @Component({
   selector: 'cx-add-to-wishlist',
   templateUrl: './add-to-wish-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddToWishListComponent implements OnInit {
-  constructor() {}
+export class AddToWishListComponent {
+  product$: Observable<Product> = this.currentProductService.getProduct();
+  wishListEntries$: Observable<
+    OrderEntry[]
+  > = this.wishListService
+    .getWishList()
+    .pipe(map(wishList => wishList.entries));
 
-  ngOnInit() {}
+  constructor(
+    protected wishListService: WishListService,
+    protected currentProductService: CurrentProductService
+  ) {}
 
-  add() {
-    return true;
+  add(product: Product): void {
+    this.wishListService.addEntry(product.code);
   }
 
-  remove() {
-    return false;
+  remove(entry: OrderEntry): void {
+    this.wishListService.removeEntry(entry);
+  }
+
+  getProductInWishList(product: Product, entries: OrderEntry[]): OrderEntry {
+    const item = entries.find(entry => entry.product.code === product.code);
+    return item;
   }
 }
