ab0c368740757bbf404ca3fcf167dc6c5e41c69d
Merryweather, Darrell <darrell.merryweather@sap.com>
2019-11-19 23:16:31 +0000

    COL-163 - Addressed the code review comments

--- projects/cds/src/config/cds-config-validator.spec.ts
+++ projects/cds/src/config/cds-config-validator.spec.ts
@@ -2,6 +2,46 @@ import { CdsConfig } from './cds-config';
 import { cdsConfigValidator } from './cds-config-validator';
 
 describe('cdsConfigValidator', () => {
+  it('should warn about an undefined cds configuration section', () => {
+    const CONFIG: CdsConfig = {};
+    expect(cdsConfigValidator(CONFIG)).toBeTruthy();
+  });
+
+  it('should warn about undefined profile tag configuration url', () => {
+    const CONFIG: CdsConfig = {
+      cds: {
+          profileTag: {
+              configUrl: undefined
+          },
+      }
+    };
+    expect(cdsConfigValidator(CONFIG)).toBeTruthy();
+  })
+
+  it('should warn about undefined profile tag javascript url', () => {
+    const CONFIG: CdsConfig = {
+        cds: {
+            profileTag: {
+                configUrl: 'some config url',
+                javascriptUrl: undefined,
+            },
+        }
+    };
+    expect(cdsConfigValidator(CONFIG)).toBeTruthy();
+  })
+
+  it('should warn about a blank profile tag javascript url', () => {
+    const CONFIG: CdsConfig = {
+        cds: {
+            profileTag: {
+                configUrl: 'some config url',
+                javascriptUrl: '',
+            },
+        }
+    };
+    expect(cdsConfigValidator(CONFIG)).toBeTruthy();
+  })
+
   it('should warn about undefined tenant', () => {
     const CONFIG: CdsConfig = {
       cds: {
