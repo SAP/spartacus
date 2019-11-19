1037bf2eb4702cdf4bd5f3b6fe71c0f5ef001790
dunqan <dunqan@w9.pl>
2019-11-19 12:01:20 +0100

    fix build

--- projects/storefrontlib/src/recipes/storefront.module.ts
+++ projects/storefrontlib/src/recipes/storefront.module.ts
@@ -2,8 +2,15 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { ExternalRoutesModule, OccModule, PersonalizationModule, provideConfig, SiteContextModule, SmartEditModule } from '@spartacus/core';
-import { AsmModule } from '../cms-components/asm';
+import {
+  ExternalRoutesModule,
+  OccModule,
+  PersonalizationModule,
+  provideConfig,
+  SiteContextModule,
+  SmartEditModule,
+} from '@spartacus/core';
+import { AsmModule } from '../cms-components/asm/asm.module';
 import { ProductDetailsPageModule } from '../cms-pages/product-details-page/product-details-page.module';
 import { ProductListingPageModule } from '../cms-pages/product-listing-page/product-listing-page.module';
 import { MainModule } from '../layout/main/main.module';
@@ -40,7 +47,6 @@ import { StorefrontFoundationModule } from './storefront-foundation.module';
 
     SmartEditModule.forRoot(), // should be custom
     PersonalizationModule.forRoot(), // should be custom
-
 
     // opt-in explicitly
     OccModule.forRoot(),
