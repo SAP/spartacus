3b086ccaf2f1882c90a093cd7bd97f75febe3618
Natan portilho <natan.portilho@sap.com>
2019-11-20 15:52:53 -0300

    fix: Longer timeout for info and confirmation messages in GlobalMessageConfig so user can read them.

--- projects/core/src/global-message/config/default-global-message-config.ts
+++ projects/core/src/global-message/config/default-global-message-config.ts
@@ -5,10 +5,10 @@ export function defaultGlobalMessageConfigFactory(): GlobalMessageConfig {
   return {
     globalMessages: {
       [GlobalMessageType.MSG_TYPE_CONFIRMATION]: {
-        timeout: 3000,
+        timeout: 7000,
       },
       [GlobalMessageType.MSG_TYPE_INFO]: {
-        timeout: 3000,
+        timeout: 7000,
       },
       [GlobalMessageType.MSG_TYPE_ERROR]: {
         timeout: 7000,
