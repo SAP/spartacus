a77ebcdb5d3e4421d26054506b8e2140bdaeef2c
Merryweather, Darrell <darrell.merryweather@sap.com>
2019-11-20 14:48:31 +0000

    COL-188 - Added the productId retrieval so that it can be used in the strategy API call

--- projects/cds/src/merchandising/facade/cds-merchandising-product.service.ts
+++ projects/cds/src/merchandising/facade/cds-merchandising-product.service.ts
@@ -1,9 +1,12 @@
-import { Product } from './../../../../core/src/model/product.model';
-import { CurrentProductService } from './../../../../storefrontlib/src/cms-components/product/current-product.service';
 import { Injectable } from '@angular/core';
-import { BaseSiteService, LanguageService } from '@spartacus/core';
-import { Observable } from 'rxjs';
-import { map, switchMap, filter, tap, withLatestFrom } from 'rxjs/operators';
+import {
+  BaseSiteService,
+  LanguageService,
+  RoutingService,
+} from '@spartacus/core';
+import { combineLatest, Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
+import { map, switchMap } from 'rxjs/operators';
 import { StrategyRequest } from './../../cds-models/cds-strategy-request.model';
 import { MerchandisingStrategyConnector } from './../connectors/strategy/merchandising-strategy.connector';
 import { MerchandisingProducts } from './../model/merchandising.products.model';
@@ -16,38 +19,32 @@ export class CdsMerchandisingProductService {
     protected strategyConnector: MerchandisingStrategyConnector,
     protected baseSiteService: BaseSiteService,
     protected languageService: LanguageService,
-    protected currentProductService: CurrentProductService
+    private routingService: RoutingService
   ) {}
 
   loadProductsForStrategy(
     strategyId: string,
     numberToDisplay?: number
   ): Observable<MerchandisingProducts> {
-
-
-  return this.currentProductService
-        .getProduct()
-        .pipe(
-            filter(Boolean),
-            //switchMap(product => product.code),
-            withLatestFrom(
-                this.baseSiteService.getActive(),
-                this.languageService.getActive()
-            ),
-            tap(x => console.log('result is an array [string, string, string]', x)),
-            map(([product, site, language]: [Product, string, string]) => {
-                console.log('*****DEBUG - product code - ', product.code);
-                const strategyRequest: StrategyRequest = {
-                  site,
-                  language,
-                  pageSize: numberToDisplay,
-                };
-                return strategyRequest;
-              }),
-              switchMap(context =>
-                this.strategyConnector.loadProductsForStrategy(strategyId, context)
-              )
-        );
-
+    return combineLatest([
+      this.baseSiteService.getActive(),
+      this.languageService.getActive(),
+      this.routingService
+        .getRouterState()
+        .pipe(switchMap(state => of(state.state.params['productCode']))),
+    ]).pipe(
+      map(([site, language, productId]: [string, string, string]) => {
+        const strategyRequest: StrategyRequest = {
+          site,
+          language,
+          pageSize: numberToDisplay,
+          productId,
+        };
+        return strategyRequest;
+      }),
+      switchMap(context =>
+        this.strategyConnector.loadProductsForStrategy(strategyId, context)
+      )
+    );
   }
 }
