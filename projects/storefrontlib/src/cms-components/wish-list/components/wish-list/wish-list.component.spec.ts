c1971690c16ef24b75a6cf04670b06dcf1ff3899
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-11-21 12:48:45 -0500

    [GH-5242] Removing unnecessary mocks

--- projects/storefrontlib/src/cms-components/wish-list/components/wish-list/wish-list.component.spec.ts
+++ projects/storefrontlib/src/cms-components/wish-list/components/wish-list/wish-list.component.spec.ts
@@ -1,43 +1,7 @@
+import { Component, Input, Type } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { WishListService } from '@spartacus/core';
 import { WishListComponent } from './wish-list.component';
-import { I18nTestingModule, WishListService } from '@spartacus/core';
-import { Component, Input, Pipe, PipeTransform, Type } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MediaComponent } from '@spartacus/storefront';
-
-@Component({
-  selector: 'cx-star-rating',
-  template: '',
-})
-class MockStarRatingComponent {
-  @Input() rating;
-  @Input() disabled;
-}
-
-@Pipe({
-  name: 'cxUrl',
-})
-class MockUrlPipe implements PipeTransform {
-  transform() {}
-}
-
-@Component({
-  selector: 'cx-icon',
-  template: '',
-})
-export class MockCxIconComponent {
-  @Input() type;
-}
-
-@Component({
-  selector: 'cx-add-to-cart',
-  template: '<button>add to cart</button>',
-})
-export class MockAddToCartComponent {
-  @Input() product;
-  @Input() showQuantity;
-}
 
 const MockWishListService = jasmine.createSpyObj('WishListService', [
   'getWishList',
@@ -59,16 +23,7 @@ describe('WishListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        WishListComponent,
-        MockWishListItemComponent,
-        MockStarRatingComponent,
-        MockAddToCartComponent,
-        MockUrlPipe,
-        MockCxIconComponent,
-        MediaComponent,
-      ],
-      imports: [I18nTestingModule, RouterTestingModule],
+      declarations: [WishListComponent, MockWishListItemComponent],
       providers: [
         {
           provide: WishListService,
