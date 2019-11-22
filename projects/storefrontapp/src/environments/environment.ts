c0714574c5f405f55b7f50b79277af79bc6a06ce
Florian Wiegand <florian.wiegand@sap.com>
2019-11-22 15:33:14 +0100

    Add yarn start:ssr to ssr dockerfile

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   // The following is needed for argonauts deployments
   occBaseUrl: 'https://yprofiletest2.yrdci.fra.hybris.com:9002',
   configUrl: 'https://tag.static.stage.context.cloud.sap/config/dfbb97b0-f4d7-11e9-9c99-2125ab7968c6',
-  profileTagUrl: 'http://127.0.0.1:8080/profile-tag.js',
-  authClientId: 'mobile_android222',
-  authClientSecret: 'li0A2SEP41s1CQRUaodiCTDJaXtm76m',
+  profileTagUrl: 'https://tag.static.stage.context.cloud.sap/js/profile-tag.js',
+  authClientId: 'mobile_android',
+  authClientSecret: 'li0A2SEKP41s1CQRUaodiCTDJaXtm76m',
 };
