26de1cd21add8886dfe9dbd9dc3a95d8750393ee
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2019-11-25 12:54:13 +0100

    GH-5523: Fix consumed entries bug in added to cart modal

--- projects/storefrontlib/src/cms-components/cart/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.ts
+++ projects/storefrontlib/src/cms-components/cart/add-to-cart/added-to-cart-dialog/added-to-cart-dialog.component.ts
@@ -74,11 +74,14 @@ export class AddedToCartDialogComponent implements OnInit {
     return appliedOrderPromotions;
   }
 
-  getAppliedProductPromotions(cart: Cart): PromotionResult[] {
+  getAppliedProductPromotions(item: OrderEntry, cart: Cart): PromotionResult[] {
     const appliedProductPromotions = [];
     appliedProductPromotions.push(...(cart.appliedProductPromotions || []));
 
-    return appliedProductPromotions;
+    return this.getProductPromotionForOrderEntry(
+      item,
+      appliedProductPromotions
+    );
   }
 
   private createEntryFormGroup(entry: OrderEntry): FormGroup {
@@ -86,5 +89,42 @@ export class AddedToCartDialogComponent implements OnInit {
       entryNumber: entry.entryNumber,
       quantity: entry.quantity,
     });
+  }
+
+  getProductPromotionForOrderEntry(
+    item: OrderEntry,
+    promotions: PromotionResult[]
+  ): PromotionResult[] {
+    const entryPromotions: PromotionResult[] = [];
+    if (promotions && promotions.length > 0) {
+      for (const promotion of promotions) {
+        if (
+          promotion.description &&
+          promotion.consumedEntries &&
+          promotion.consumedEntries.length > 0
+        ) {
+          for (const consumedEntry of promotion.consumedEntries) {
+            if (this.isConsumedByEntry(consumedEntry, item)) {
+              entryPromotions.push(promotion);
+            }
+          }
+        }
+      }
+    }
+    return entryPromotions;
+  }
+
+  private isConsumedByEntry(consumedEntry: any, entry: any): boolean {
+    const consumedEntryNumber = consumedEntry.orderEntryNumber;
+    if (entry.entries && entry.entries.length > 0) {
+      for (const subEntry of entry.entries) {
+        if (subEntry.entryNumber === consumedEntryNumber) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return consumedEntryNumber === entry.entryNumber;
+    }
   }
 }
