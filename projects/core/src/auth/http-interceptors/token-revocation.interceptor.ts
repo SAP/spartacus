03b74460ce0bd1a12dfae1010ee017359b14eb9e
Patrick Labadie <patrick.labadie@sap.com>
2019-11-26 09:59:34 -0500

    GH-4739: Work in progress.

---
+++ projects/core/src/auth/http-interceptors/token-revocation.interceptor.ts
@@ -0,0 +1,58 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {
+  InterceptorUtil,
+  TOKEN_REVOCATION,
+} from '../../occ/utils/interceptor-util';
+
+@Injectable({ providedIn: 'root' })
+export class TokenRevocationInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const isInterceptingTokenRevocationRequest = this.isTokenRevocationRequest(
+      request
+    );
+    if (isInterceptingTokenRevocationRequest) {
+      request = InterceptorUtil.removeHeader(TOKEN_REVOCATION, request);
+    }
+
+    return next.handle(request).pipe(
+      catchError((response: any) => {
+        if (response instanceof HttpErrorResponse) {
+          if (isInterceptingTokenRevocationRequest) {
+            console.log(
+              'Error during token revokation.  Ending interceptor stream.'
+            );
+            return EMPTY;
+          } else {
+            console.log(
+              'Rethrow Error in TokenRevocationInterceptor',
+              response.status,
+              response.url
+            );
+            return throwError(response);
+          }
+        }
+      })
+    );
+  }
+
+  private isTokenRevocationRequest(request: HttpRequest<any>): boolean {
+    const isTokenRevocationHeaderPresent = InterceptorUtil.getInterceptorParam<
+      string
+    >(TOKEN_REVOCATION, request.headers);
+    return Boolean(isTokenRevocationHeaderPresent);
+  }
+}
