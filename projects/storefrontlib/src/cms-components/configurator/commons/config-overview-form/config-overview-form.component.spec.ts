d9932175ec130e65840b496f6eb0fbfbd0663ad9
Christoph Hinssen <christoph.hinssen@sap.com>
2019-11-27 15:18:19 +0100

    GH-5544 Add skeleton for overview page, component and module

---
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-overview-form/config-overview-form.component.spec.ts
@@ -0,0 +1,148 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterState } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {
+  Configurator,
+  ConfiguratorCommonsService,
+  I18nTestingModule,
+  RoutingService,
+} from '@spartacus/core';
+import { ICON_TYPE } from '@spartacus/storefront';
+import { Observable, of } from 'rxjs';
+import { ConfigAttributeFooterComponent } from '../config-attribute-footer/config-attribute-footer.component';
+import { ConfigAttributeHeaderComponent } from '../config-attribute-header/config-attribute-header.component';
+import { ConfigAttributeDropDownComponent } from '../config-attribute-types/config-attribute-drop-down/config-attribute-drop-down.component';
+import { ConfigAttributeInputFieldComponent } from '../config-attribute-types/config-attribute-input-field/config-attribute-input-field.component';
+import { ConfigAttributeRadioButtonComponent } from '../config-attribute-types/config-attribute-radio-button/config-attribute-radio-button.component';
+import { ConfigAttributeReadOnlyComponent } from '../config-attribute-types/config-attribute-read-only/config-attribute-read-only.component';
+import { ConfigPreviousNextButtonsComponent } from '../config-previous-next-buttons/config-previous-next-buttons.component';
+import { ConfigOverviewFormComponent } from './config-overview-form.component';
+
+@Component({
+  selector: 'cx-icon',
+  template: '',
+})
+class MockCxIconComponent {
+  @Input() type: ICON_TYPE;
+}
+const routerStateObservable = null;
+const configurationObservable = null;
+const configurationCreateObservable = null;
+
+const configCreate: Configurator.Configuration = {
+  configId: '1234-56-7890',
+  groups: [
+    {
+      configurable: true,
+      description: 'Core components',
+      groupType: Configurator.GroupType.ATTRIBUTE_GROUP,
+      id: '1-CPQ_LAPTOP.1',
+      name: '1',
+      attributes: [
+        {
+          label: 'Expected Number',
+          name: 'EXP_NUMBER',
+          required: true,
+          uiType: Configurator.UiType.NOT_IMPLEMENTED,
+          values: [],
+        },
+        {
+          label: 'Processor',
+          name: 'CPQ_CPU',
+          required: true,
+          selectedSingleValue: 'INTELI5_35',
+          uiType: Configurator.UiType.RADIOBUTTON,
+          values: [],
+        },
+      ],
+    },
+    {
+      configurable: true,
+      description: 'Peripherals & Accessories',
+      groupType: Configurator.GroupType.ATTRIBUTE_GROUP,
+      id: '1-CPQ_LAPTOP.2',
+      name: '2',
+      attributes: [],
+    },
+    {
+      configurable: true,
+      description: 'Software',
+      groupType: Configurator.GroupType.ATTRIBUTE_GROUP,
+      id: '1-CPQ_LAPTOP.3',
+      name: '3',
+      attributes: [],
+    },
+  ],
+};
+
+class MockRoutingService {
+  getRouterState(): Observable<RouterState> {
+    return routerStateObservable;
+  }
+}
+
+class MockConfiguratorCommonsService {
+  createConfiguration(
+    productCode: string
+  ): Observable<Configurator.Configuration> {
+    configCreate.productCode = productCode;
+    return of(configCreate);
+  }
+  getConfiguration(): Observable<Configurator.Configuration> {
+    return configurationObservable;
+  }
+  getOrCreateConfiguration(): Observable<Configurator.Configuration> {
+    return configurationCreateObservable;
+  }
+  hasConfiguration(): Observable<boolean> {
+    return of(false);
+  }
+}
+describe('ConfigurationFormComponent', () => {
+  let component: ConfigOverviewFormComponent;
+  let fixture: ComponentFixture<ConfigOverviewFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [I18nTestingModule, ReactiveFormsModule, NgSelectModule],
+      declarations: [
+        ConfigOverviewFormComponent,
+        ConfigAttributeHeaderComponent,
+        ConfigAttributeFooterComponent,
+        ConfigAttributeRadioButtonComponent,
+        ConfigAttributeInputFieldComponent,
+        ConfigAttributeDropDownComponent,
+        ConfigAttributeReadOnlyComponent,
+        ConfigPreviousNextButtonsComponent,
+        MockCxIconComponent,
+      ],
+      providers: [
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService,
+        },
+
+        {
+          provide: ConfiguratorCommonsService,
+          useClass: MockConfiguratorCommonsService,
+        },
+      ],
+    })
+      .overrideComponent(ConfigAttributeHeaderComponent, {
+        set: {
+          changeDetection: ChangeDetectionStrategy.Default,
+        },
+      })
+      .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfigOverviewFormComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create component', () => {
+    expect(component).toBeDefined();
+  });
+});
