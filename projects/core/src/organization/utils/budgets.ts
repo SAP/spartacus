099f82988b34807b67c6150015ae181e2c8078b5
Michał Gruca <pucek9@gmail.com>
2019-12-02 18:12:56 +0100

    up

--- projects/core/src/organization/utils/budgets.ts
+++ projects/core/src/organization/utils/budgets.ts
@@ -1,6 +1,6 @@
 import { BudgetSearchConfig } from '../model/search-config';
 
-// TODO after update typescript to 3.7 it can be replaced by Nullish Coalescing (??)
+// TODO after update typescript to 3.7 it can be replaced by Nullish Coalescing (??) operator
 function nullish(param, defaultValue) {
   return param !== null && param !== undefined ? param : defaultValue;
 }
