6a26f36b869fe25025a0716cfe416d0b4d2c10f3
Christoph Hinssen <christoph.hinssen@sap.com>
2019-12-03 08:22:45 +0100

    GH-5547 Add better error handling to utils service

--- projects/core/src/configurator/commons/utils/config-utils.service.spec.ts
+++ projects/core/src/configurator/commons/utils/config-utils.service.spec.ts
@@ -1,6 +1,10 @@
 import { Type } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
+import { Configurator } from 'projects/core/src/model';
 import { ConfigUtilsService } from './config-utils.service';
+
+const productCode = 'CONF_LAPTOP';
+let owner: Configurator.Owner = null;
 
 describe('ConfigUtilsService', () => {
   let serviceUnderTest: ConfigUtilsService;
@@ -12,9 +16,46 @@ describe('ConfigUtilsService', () => {
     serviceUnderTest = TestBed.get(ConfigUtilsService as Type<
       ConfigUtilsService
     >);
+    owner = {};
   });
 
   it('should create component', () => {
     expect(serviceUnderTest).toBeDefined();
+  });
+
+  it('should set key for product related owner', () => {
+    owner.type = Configurator.OwnerType.PRODUCT;
+    owner.productCode = productCode;
+    serviceUnderTest.setOwnerKey(owner);
+    expect(owner.key.includes(productCode)).toBe(true);
+    expect(owner.key.includes(Configurator.OwnerType.PRODUCT)).toBe(true);
+  });
+
+  it('should set key for document related owner', () => {
+    owner.type = Configurator.OwnerType.CART_ENTRY;
+    owner.documentEntryId = '1';
+    serviceUnderTest.setOwnerKey(owner);
+    expect(owner.key.includes(owner.documentEntryId)).toBe(true);
+    expect(owner.key.includes(Configurator.OwnerType.CART_ENTRY)).toBe(true);
+  });
+
+  it('should throw an error if no owner type is present', () => {
+    expect(function() {
+      serviceUnderTest.setOwnerKey(owner);
+    }).toThrow();
+  });
+
+  it('should throw an error if for owner type PRODUCT if no product code is present', () => {
+    owner.type = Configurator.OwnerType.PRODUCT;
+    expect(function() {
+      serviceUnderTest.setOwnerKey(owner);
+    }).toThrow();
+  });
+
+  it('should throw an error if for owner type CART_ENTRY if no cart entry link is present', () => {
+    owner.type = Configurator.OwnerType.CART_ENTRY;
+    expect(function() {
+      serviceUnderTest.setOwnerKey(owner);
+    }).toThrow();
   });
 });
--- projects/core/src/configurator/commons/utils/config-utils.service.ts
+++ projects/core/src/configurator/commons/utils/config-utils.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { Configurator } from '@spartacus/core';
 
 /**
- * Service to extract the configuration owner key from the current route
+ * Utilities: Creating the key for a configuration owner object
  */
 @Injectable({ providedIn: 'root' })
 export class ConfigUtilsService {
   setOwnerKey(owner: Configurator.Owner) {
     if (owner.type === Configurator.OwnerType.PRODUCT) {
+      if (!owner.productCode) {
+        throw new Error('We expect a product code!');
+      }
       owner.key = owner.type + '/' + owner.productCode;
-    } else {
+    } else if (owner.type === Configurator.OwnerType.CART_ENTRY) {
+      if (!owner.documentEntryId) {
+        throw new Error('We expect a document entry Id!');
+      }
       owner.key = owner.type + '/' + owner.documentEntryId;
+    } else {
+      throw new Error('We expect an owner type!');
     }
   }
 }
