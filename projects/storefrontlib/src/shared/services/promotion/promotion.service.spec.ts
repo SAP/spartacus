ac6630ac49a212b1e20de4d816dba0e4afc28a67
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2019-12-04 15:42:03 +0100

    GH-3796: Promotion service unit tests

--- projects/storefrontlib/src/shared/services/promotion/promotion.service.spec.ts
+++ projects/storefrontlib/src/shared/services/promotion/promotion.service.spec.ts
@@ -5,37 +5,12 @@ import {
   Cart,
   PromotionResult,
   Order,
+  PromotionLocation,
 } from '@spartacus/core';
 import { OrderDetailsService } from 'projects/storefrontlib/src/cms-components';
 import { TestBed } from '@angular/core/testing';
 import { Type } from '@angular/core';
 import { Observable, of } from 'rxjs';
-
-// import { Cart, Order } from '@spartacus/core';
-// import { Observable } from 'rxjs';
-// import { Component, Input } from '@angular/core';
-// import { Item } from '@spartacus/storefront';
-
-// const mockItems = [
-//   {
-//     id: 0,
-//     quantity: 1,
-//     entryNumber: 0,
-//     product: {
-//       id: 0,
-//       code: 'PR0000',
-//     },
-//   },
-//   {
-//     id: 1,
-//     quantity: 5,
-//     entryNumber: 1,
-//     product: {
-//       id: 1,
-//       code: 'PR0001',
-//     },
-//   },
-// ];
 
 const mockAppliedProductPromotions: PromotionResult[] = [
   {
@@ -54,52 +29,47 @@ const mockAppliedProductPromotions: PromotionResult[] = [
   },
 ];
 
-const mockAppliedOrderPromotions: PromotionResult[] = [
+const mockAppliedOrderPromotionsForCart: PromotionResult[] = [
+  {
+    consumedEntries: [
+      {
+        orderEntryNumber: 1,
+      },
+    ],
+    description: 'test applied order promotion for cart',
+  },
+];
+
+const mockAppliedOrderPromotionsForCheckout: PromotionResult[] = [
   {
     consumedEntries: [
       {
         orderEntryNumber: 2,
       },
     ],
-    description: 'test applied order promotion',
+    description: 'test applied order promotion for checkout',
   },
 ];
 
-// const mockPotentialProductPromotions = [
-//   {
-//     description: 'Buy two more and win a trip to the Moon',
-//     consumedEntries: [
-//       {
-//         orderEntryNumber: 1,
-//       },
-//     ],
-//   },
-// ];
+const mockAppliedOrderPromotionsForOrder: PromotionResult[] = [
+  {
+    consumedEntries: [
+      {
+        orderEntryNumber: 3,
+      },
+    ],
+    description: 'test applied order promotion for order',
+  },
+];
 
 const mockCart: Cart = {
   guid: 'test',
   code: 'test',
   deliveryItemsQuantity: 123,
   totalPrice: { formattedValue: '$999.98' },
-  appliedOrderPromotions: mockAppliedOrderPromotions,
+  appliedOrderPromotions: mockAppliedOrderPromotionsForCart,
   appliedProductPromotions: mockAppliedProductPromotions,
 };
-
-// const mockedOrder: Order = {
-//   guid: '1',
-
-// };
-
-// @Component({
-//   template: '',
-//   selector: 'cx-cart-item-list',
-// })
-// class MockCartItemListComponent {
-//   @Input()
-//   items: Item[];
-//   @Input()
-//   cartIsLoading: Observable<boolean>;
-// }
 
 const mockOrder: Order = {
   code: '1',
@@ -142,7 +112,52 @@ const mockOrder: Order = {
     },
   },
   created: new Date('2019-02-11T13:02:58+0000'),
-  appliedOrderPromotions: mockAppliedOrderPromotions,
+  appliedOrderPromotions: mockAppliedOrderPromotionsForOrder,
+  appliedProductPromotions: mockAppliedProductPromotions,
+};
+
+const mockCheckoutDetails: Order = {
+  code: '1',
+  statusDisplay: 'orderDetails.statusDisplay context:Shipped',
+  deliveryAddress: {
+    firstName: 'John',
+    lastName: 'Smith',
+    line1: 'Buckingham Street 5',
+    line2: '1A',
+    phone: '(+11) 111 111 111',
+    postalCode: 'MA8902',
+    town: 'London',
+    country: {
+      isocode: 'UK',
+    },
+  },
+  deliveryMode: {
+    name: 'Standard order-detail-shipping',
+    description: '3-5 days',
+  },
+  paymentInfo: {
+    accountHolderName: 'John Smith',
+    cardNumber: '************6206',
+    expiryMonth: '12',
+    expiryYear: '2026',
+    cardType: {
+      name: 'Visa',
+    },
+    billingAddress: {
+      firstName: 'John',
+      lastName: 'Smith',
+      line1: 'Buckingham Street 5',
+      line2: '1A',
+      phone: '(+11) 111 111 111',
+      postalCode: 'MA8902',
+      town: 'London',
+      country: {
+        isocode: 'UK',
+      },
+    },
+  },
+  created: new Date('2019-02-11T13:02:58+0000'),
+  appliedOrderPromotions: mockAppliedOrderPromotionsForCheckout,
   appliedProductPromotions: mockAppliedProductPromotions,
 };
 
@@ -154,7 +169,7 @@ class MockCartService {
 
 class MockCheckoutService {
   getOrderDetails(): Observable<Order> {
-    return of(mockOrder);
+    return of(mockCheckoutDetails);
   }
 }
 
@@ -164,7 +179,7 @@ class MockOrderDetailsService {
   }
 }
 
-describe('PromotionService', () => {
+fdescribe('PromotionService', () => {
   let promotionService: PromotionService;
 
   beforeEach(() => {
@@ -193,7 +208,74 @@ describe('PromotionService', () => {
     expect(promotionService).toBeTruthy();
   });
 
-  describe('getOrderPromotions', () => {});
+  describe('getOrderPromotions', () => {
+    describe('for cart', () => {
+      const expectedAppliedOrderPromotions = [
+        {
+          consumedEntries: [
+            {
+              orderEntryNumber: 1,
+            },
+          ],
+          description: 'test applied order promotion for cart',
+        },
+      ];
+      const promotionLocation: PromotionLocation = PromotionLocation.Cart;
+      let appliedOrderPromotions: PromotionResult[];
 
-  describe('getProductPromotionForEntry', () => {});
+      it('should return appropriate applied order promotions for cart', () => {
+        promotionService
+          .getOrderPromotions(promotionLocation)
+          .subscribe(promotions => (appliedOrderPromotions = promotions))
+          .unsubscribe();
+        expect(appliedOrderPromotions).toEqual(expectedAppliedOrderPromotions);
+      });
+    });
+
+    describe('for checkout', () => {
+      const expectedAppliedOrderPromotions = [
+        {
+          consumedEntries: [
+            {
+              orderEntryNumber: 2,
+            },
+          ],
+          description: 'test applied order promotion for checkout',
+        },
+      ];
+      const promotionLocation: PromotionLocation = PromotionLocation.Checkout;
+      let appliedOrderPromotions: PromotionResult[];
+
+      it('should return appropriate applied order promotions for checkout', () => {
+        promotionService
+          .getOrderPromotions(promotionLocation)
+          .subscribe(promotions => (appliedOrderPromotions = promotions))
+          .unsubscribe();
+        expect(appliedOrderPromotions).toEqual(expectedAppliedOrderPromotions);
+      });
+    });
+
+    describe('for order', () => {
+      const expectedAppliedOrderPromotions = [
+        {
+          consumedEntries: [
+            {
+              orderEntryNumber: 3,
+            },
+          ],
+          description: 'test applied order promotion for order',
+        },
+      ];
+      const promotionLocation: PromotionLocation = PromotionLocation.Order;
+      let appliedOrderPromotions: PromotionResult[];
+
+      it('should return appropriate applied order promotions for order', () => {
+        promotionService
+          .getOrderPromotions(promotionLocation)
+          .subscribe(promotions => (appliedOrderPromotions = promotions))
+          .unsubscribe();
+        expect(appliedOrderPromotions).toEqual(expectedAppliedOrderPromotions);
+      });
+    });
+  });
 });
