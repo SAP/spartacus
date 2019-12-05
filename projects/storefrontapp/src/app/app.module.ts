7a8d12c57fc49465c15d346e7bf780c87a06621d
Nikola Zarić <nikola.zaric.ns@gmail.com>
2019-12-05 12:59:47 -0500

    dummy

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -35,7 +35,7 @@ if (!environment.production) {
     B2cStorefrontModule.withConfig({
       backend: {
         occ: {
-          baseUrl: environment.occBaseUrl,
+          baseUrl: 'https://dev-com-14.accdemo.b2c.ydev.hybris.com:9002/',
           legacy: false,
         },
       },
@@ -71,12 +71,15 @@ if (!environment.production) {
     CdsModule.forRoot({
       cds: {
         tenant: 'argotest',
-        baseUrl: 'test',
+        baseUrl: 'https://api.stage.context.cloud.sap',
         endpoints: {
-          strategyProducts: 'example',
+          strategyProducts:
+            '/strategy/${tenant}/strategies/${strategyId}/products',
         },
         profileTag: {
-          javascriptUrl: 'http://127.0.0.1:8080/profile-tag.js',
+          allowInsecureCookies: true,
+          javascriptUrl:
+            'http://tag.static.stage.context.cloud.sap/js/profile-tag.js',
           configUrl:
             'https://tag.static.stage.context.cloud.sap/config/dfbb97b0-f4d7-11e9-9c99-2125ab7968c6',
         },
