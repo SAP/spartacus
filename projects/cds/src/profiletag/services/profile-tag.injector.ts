4fa024721650b709f81c059ca8454ac025d51d2a
Shankari Giri <shankari.giri@sap.com>
2019-12-05 16:13:27 +0100

    ARGO-5715: Refactored the tests along the lines of the cds/services

--- projects/cds/src/profiletag/services/profile-tag.injector.ts
+++ projects/cds/src/profiletag/services/profile-tag.injector.ts
@@ -1,15 +1,19 @@
-import { merge, Observable } from 'rxjs';
-import { mapTo, switchMap } from 'rxjs/operators';
-import { SpartacusEventTracker } from './spartacus-events';
-import { ProfileTagEventTracker } from './profiletag-events';
 import { Injectable } from '@angular/core';
+import { merge, Observable } from 'rxjs';
+import { mapTo, switchMap, tap } from 'rxjs/operators';
+import { ProfileTagEventTracker } from './profiletag-events';
+import { SpartacusEventTracker } from './spartacus-events';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileTagInjector {
-  private tracking$;
-  private profileTagEvents$;
+  private profileTagEvents$ = this.profileTagEventTracker.getProfileTagEvent();
+  private injectionsEvents$: Observable<boolean> = merge(
+    this.notifyProfileTagOfConsentGranted(),
+    this.notifyProfileTagOfCartChange(),
+    this.notifyProfileTagOfPageLoaded()
+  );
 
   constructor(
     private profileTagEventTracker: ProfileTagEventTracker,
@@ -17,11 +21,42 @@ export class ProfileTagInjector {
   ) {}
 
   track(): Observable<boolean> {
-    this.profileTagEvents$ = this.profileTagEventTracker.getProfileTagEvent();
-    this.tracking$ = this.spartacusEventTracker.getSpartacusTracking();
     return this.profileTagEventTracker.addTracker().pipe(
-      switchMap(_ => merge(this.tracking$, this.profileTagEvents$)),
+      switchMap(_ => merge(this.injectionsEvents$, this.profileTagEvents$)),
       mapTo(true)
+    );
+  }
+  notifyProfileTagOfConsentGranted() {
+    return this.spartacusEventTracker.consentGranted().pipe(
+      tap(granted => {
+        this.profileTagEventTracker.notifyProfileTagOfEventOccurence({
+          event: 'ConsentChanged',
+          granted,
+        });
+      })
+    );
+  }
+  notifyProfileTagOfCartChange() {
+    return this.spartacusEventTracker.cartChanged().pipe(
+      tap(([entries, cart]) => {
+        const cartSnapshotEvent = {
+          event: 'CartSnapshot',
+          data: { entries, cart },
+        };
+        this.profileTagEventTracker.notifyProfileTagOfEventOccurence(
+          cartSnapshotEvent
+        );
+      }),
+      mapTo(true)
+    );
+  }
+  notifyProfileTagOfPageLoaded() {
+    return this.spartacusEventTracker.navigated().pipe(
+      tap(_ => {
+        this.profileTagEventTracker.notifyProfileTagOfEventOccurence({
+          event: 'Navigated',
+        });
+      })
     );
   }
 }
---
+++ projects/cds/src/profiletag/services/profiletag-events.spec.ts
@@ -0,0 +1,139 @@
+import { PLATFORM_ID } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import {
+  Event as NgRouterEvent,
+  NavigationEnd,
+  NavigationStart,
+} from '@angular/router';
+import { BaseSiteService, WindowRef } from '@spartacus/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CdsConfig } from '../../config/index';
+import { ProfileTagEventNames, ProfileTagWindowObject } from '../model/index';
+import { ProfileTagEventTracker } from './profiletag-events';
+
+const mockCDSConfig: CdsConfig = {
+  cds: {
+    tenant: 'ArgoTest',
+    baseUrl: 'example.com',
+    endpoints: {
+      strategyProducts: 'example',
+    },
+    profileTag: {
+      javascriptUrl: 'https:tag.static.eu.context.cloud.sap',
+      configUrl: 'https:tag.static.us.context.cloud.sap',
+      allowInsecureCookies: false,
+      gtmId: 'test-id-1234567',
+    },
+  },
+};
+
+describe('ProfileTagEventTracker', () => {
+  let profileTagEventTracker: ProfileTagEventTracker;
+  let nativeWindow: ProfileTagWindowObject;
+  let getActiveBehavior;
+  let baseSiteService;
+  let appendChildSpy;
+  let eventListener;
+  let mockedWindowRef;
+  let getConsentBehavior;
+  let routerEventsBehavior;
+  let router;
+
+  function setVariables() {
+    getActiveBehavior = new BehaviorSubject<String>('');
+    appendChildSpy = jasmine.createSpy('appendChildSpy');
+    getConsentBehavior = new BehaviorSubject<Object>([{}]);
+    routerEventsBehavior = new BehaviorSubject<NgRouterEvent>(
+      new NavigationStart(0, 'test.com', 'popstate')
+    );
+    mockedWindowRef = {
+      nativeWindow: {
+        addEventListener: (_, listener) => {
+          eventListener = listener;
+        },
+        removeEventListener: jasmine.createSpy('removeEventListener'),
+        Y_TRACKING: {
+          push: jasmine.createSpy('push'),
+        },
+      },
+      document: {
+        createElement: () => ({}),
+        getElementsByTagName: () => [{ appendChild: appendChildSpy }],
+      },
+    };
+    baseSiteService = {
+      getActive: () => getActiveBehavior,
+    };
+    router = {
+      events: routerEventsBehavior,
+    };
+  }
+  beforeEach(() => {
+    setVariables();
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: CdsConfig, useValue: mockCDSConfig },
+        { provide: WindowRef, useValue: mockedWindowRef },
+        { provide: BaseSiteService, useValue: baseSiteService },
+        { provide: PLATFORM_ID, useValue: 'browser' },
+        { provide: router, useValue: router },
+      ],
+    });
+    profileTagEventTracker = TestBed.get(ProfileTagEventTracker);
+    nativeWindow = TestBed.get(WindowRef).nativeWindow;
+  });
+
+  it('should be created', () => {
+    expect(profileTagEventTracker).toBeTruthy();
+  });
+
+  it('Should first wait for the basesite to be active before adding config parameters to the q array', () => {
+    expect(appendChildSpy).not.toHaveBeenCalled();
+    expect(nativeWindow.Y_TRACKING.push).not.toHaveBeenCalled();
+    expect(nativeWindow.Y_TRACKING.q).not.toBeDefined();
+  });
+
+  it(`Should add config parameters to the q array after the base site is active`, () => {
+    const profileTagLoaded$ = profileTagEventTracker.addTracker();
+    const subscription = profileTagLoaded$.subscribe();
+    const baseSite = 'electronics-test';
+    getActiveBehavior.next(baseSite);
+    subscription.unsubscribe();
+
+    expect(nativeWindow.Y_TRACKING.q[0][0]).toEqual({
+      ...mockCDSConfig.cds.profileTag,
+      tenant: mockCDSConfig.cds.tenant,
+      siteId: baseSite,
+      spa: true,
+    });
+    expect(appendChildSpy).toHaveBeenCalled();
+    expect(nativeWindow.Y_TRACKING.push).not.toHaveBeenCalled();
+  });
+
+  it(`Should not call the push method if the event receiver callback hasn't been called`, () => {
+    const profileTagLoaded$ = profileTagEventTracker.addTracker();
+    const subscription = profileTagLoaded$.subscribe();
+    getActiveBehavior.next('electronics-test');
+    routerEventsBehavior.next(new NavigationEnd(0, 'test', 'test'));
+    getConsentBehavior.next({ consent: 'test' });
+    subscription.unsubscribe();
+
+    expect(nativeWindow.Y_TRACKING.push).not.toHaveBeenCalled();
+  });
+
+  it(`Should call the pageLoaded method if the site is active,
+        and event receiver callback has been called with loaded`, () => {
+    let timesCalled = 0;
+    const subscription = profileTagEventTracker
+      .profileTagLoaded()
+      .pipe(tap(_ => timesCalled++))
+      .subscribe();
+    getActiveBehavior.next('electronics-test');
+    eventListener(new CustomEvent(ProfileTagEventNames.LOADED));
+    routerEventsBehavior.next(new NavigationEnd(0, 'test', 'test'));
+    subscription.unsubscribe();
+
+    expect(timesCalled).toEqual(1);
+  });
+});
