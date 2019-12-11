222f02974752f558858ea23ae6c7e811bd557e0f
d054488 <lukas.schuppel@sap.com>
2019-12-11 15:05:42 +0100

    EPIC/PRODUCT_CONFIGURATION: Fix build

--- projects/storefrontlib/src/cms-components/product/product-list/product-grid-item/product-grid-item.component.spec.ts
+++ projects/storefrontlib/src/cms-components/product/product-list/product-grid-item/product-grid-item.component.spec.ts
@@ -9,7 +9,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { I18nTestingModule } from '@spartacus/core';
 import { ProductGridItemComponent } from './product-grid-item.component';
-import { I18nTestingModule } from '@spartacus/core';
 
 @Component({
   selector: 'cx-add-to-cart',
--- projects/storefrontlib/src/cms-components/product/product-list/product-list-item/product-list-item.component.spec.ts
+++ projects/storefrontlib/src/cms-components/product/product-list/product-list-item/product-list-item.component.spec.ts
@@ -9,7 +9,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { I18nTestingModule } from '@spartacus/core';
 import { ProductListItemComponent } from './product-list-item.component';
-import { I18nTestingModule } from '@spartacus/core';
 
 @Component({
   selector: 'cx-add-to-cart',
