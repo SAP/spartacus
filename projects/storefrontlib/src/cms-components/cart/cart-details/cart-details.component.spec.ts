9a34684612db7ad457c294c37ca2efd3df0cdccc
Qingyu <qingyu.yang@sap.com>
2019-12-11 18:29:51 +0800

    remove f

--- projects/storefrontlib/src/cms-components/cart/cart-details/cart-details.component.spec.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-details/cart-details.component.spec.ts
@@ -94,7 +94,7 @@ class MockCartCouponComponent {
   userId: string;
 }
 
-fdescribe('CartDetailsComponent', () => {
+describe('CartDetailsComponent', () => {
   let component: CartDetailsComponent;
   let fixture: ComponentFixture<CartDetailsComponent>;
 
