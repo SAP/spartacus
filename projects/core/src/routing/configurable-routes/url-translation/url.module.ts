49f646f523b5235acbb7f553c132b609c4583465
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2019-12-11 16:04:16 -0500

    delete changes

--- projects/core/src/routing/configurable-routes/url-translation/url.module.ts
+++ projects/core/src/routing/configurable-routes/url-translation/url.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UrlPipe } from './url.pipe';
-import { ProductURLPipe } from './product-url.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [UrlPipe, ProductURLPipe],
+  declarations: [UrlPipe],
   exports: [UrlPipe],
 })
 export class UrlModule {}
