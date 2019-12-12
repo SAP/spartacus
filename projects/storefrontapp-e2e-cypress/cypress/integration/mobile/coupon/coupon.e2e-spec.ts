0f3da66ab824c684f5fb13f1baf03b4860043bb9
maggie.lin <maggie.lin@sap.com>
2019-12-12 09:58:59 +0800

    fix coupon e2e after merge dev

--- projects/storefrontapp-e2e-cypress/cypress/integration/mobile/coupon/coupon.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/mobile/coupon/coupon.e2e-spec.ts
@@ -56,10 +56,19 @@ describe('Cart Coupon', () => {
     cartCoupon.addProductToCart(cartCoupon.productCode3);
     cartCoupon.applyCoupon(cartCoupon.couponCode3);
     cy.get('cx-searchbox cx-icon[aria-label="search"]').click({ force: true });
-    cartCoupon.verifyGiftProductCoupon(giftProductCode);
-    cartCoupon.verifyCouponAndPromotion(couponCode3, '$1,914.23', '$20');
+    cartCoupon.verifyGiftProductCoupon(cartCoupon.giftProductCode);
+    cartCoupon.verifyCouponAndPromotion(
+      cartCoupon.couponCode3,
+      '$1,914.23',
+      '$20'
+    );
     cartCoupon.placeOrder(stateAuth).then(orderData => {
-    cartCoupon.varifyOrderHistory(orderData, couponCode3, '$1,914.23', '$20');
+      cartCoupon.verifyOrderHistory(
+        orderData,
+        cartCoupon.couponCode3,
+        '$1,914.23',
+        '$20'
+      );
     });
   });
 
