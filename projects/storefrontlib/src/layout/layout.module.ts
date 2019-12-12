3f8ac64d0ddf0dddb47621858457f97e68ae9a0d
Caine Rotherham <rotherham.c@gmail.com>
2019-12-12 09:59:22 +0100

    fix duplicate skippers

--- projects/storefrontlib/src/layout/layout.module.ts
+++ projects/storefrontlib/src/layout/layout.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Config } from '@spartacus/core';
 import { OutletRefModule } from '../cms-structure/outlet/outlet-ref/outlet-ref.module';
-import { SkipLinkModule } from './a11y/index';
 import { LayoutConfig } from './config/layout-config';
 
 @NgModule({
-  imports: [SkipLinkModule, OutletRefModule],
+  imports: [OutletRefModule],
   providers: [{ provide: LayoutConfig, useExisting: Config }],
   exports: [OutletRefModule],
 })
