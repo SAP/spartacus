c41ba8b9997eec9042a0314e1d1d1f6d3f3f3637
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2019-12-13 16:20:58 -0500

    [GH-5785] Script for pre-release steps

---
+++ scripts/pre-release.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Script that updates compodocs
+# Run from the release branch root
+set -e
+
+function cleanup {
+    echo '--> Cleaning up spartacus workspace'
+    delete_file docs.tar.gz
+    delete_file docs.zip
+    delete_file spartacussampledataaddon.zip
+
+    delete_dir dist
+    delete_dir documentation
+    delete_dir node_modules
+}
+
+function delete_file {
+  if [ -a $1 ]; then
+    rm $1
+  fi
+}
+
+function delete_dir {
+  if [ -d $1 ]; then
+    rm -rf $1
+  fi
+}
+
+function build_libs {
+    echo '--> Building Spartacus libraries'
+    yarn build:core:lib
+}
+
+function generate_docs {
+    echo "--> Generating compodocs"
+    yarn generate:docs
+
+    echo "--> Getting dependencies again"
+    yarn
+}
+
+function zipSamplesAddOn {
+    echo "--> Generating Spartacus samples addon zip"
+    delete_dir spartacussampledataaddon
+    git clone https://github.tools.sap/cx-commerce/spartacussampledataaddon.git
+    cd spartacussampledataaddon
+    delete_dir .git
+    delete_file .gitignore
+    cd ..
+    zip -r spartacussampledataaddon.zip spartacussampledataaddon
+    delete_dir spartacussampledataaddon
+}
+
+cleanup
+zipSamplesAddOn
+generate_docs
+build_libs
