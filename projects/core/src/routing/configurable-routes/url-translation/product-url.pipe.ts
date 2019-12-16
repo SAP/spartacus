44e02a8549973f9fdd1179da826e4f4efd41063b
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2019-12-16 10:13:22 -0500

    Custom product URL pipe

--- projects/core/src/routing/configurable-routes/url-translation/product-url.pipe.ts
+++ projects/core/src/routing/configurable-routes/url-translation/product-url.pipe.ts
@@ -5,6 +5,7 @@ import { SemanticPathService } from './semantic-path.service';
 
 import { UrlParsingService } from './url-parsing.service';
 import { isParam, getParamName } from './path-utils';
+import { ParamsMapping } from '../routes-config';
 @Pipe({
   name: 'cxProductUrl',
 })
@@ -14,9 +15,37 @@ export class ProductURLPipe implements PipeTransform {
   
   constructor(private productURLService: RoutingConfigService, private urlParser: UrlParsingService) {}
 
-  transform(item: Product) {
+  transform(item : Product) {
     const path =this.productURLService.getRouteConfig("product").paths[0];
+    const paramMapping= this.productURLService.getRouteConfig("product").paramsMapping;
     console.log(path);
-    return this.urlParser.getPrimarySegments(path).filter(isParam).map(getParamName);
+    console.log(item);
+    console.log(paramMapping);
+    return this.provideParamsValues(path,item, paramMapping);
+  }
+
+  private provideParamsValues(
+    path: string,
+    params: object,
+    paramsMapping: ParamsMapping
+  ): string[] {
+    return this.urlParser.getPrimarySegments(path).map(segment => {
+      if (isParam(segment)) {
+        const paramName = getParamName(segment);
+        const mappedParamName = this.getMappedParamName(
+          paramName,
+          paramsMapping
+        );
+        return params[mappedParamName];
+      }
+      return segment;
+    });
+  }
+
+  private getMappedParamName(paramName: string, paramsMapping: object): string {
+    if (paramsMapping) {
+      return paramsMapping[paramName] || paramName;
+    }
+    return paramName;
   }
 }
