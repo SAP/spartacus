a745a1b78ed24a03e8f92e2001e6fd829baa3584
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-12-16 09:13:18 +0100

    Wire intersection service into outlet directive

--- projects/storefrontlib/src/cms-structure/outlet/outlet.directive.ts
+++ projects/storefrontlib/src/cms-structure/outlet/outlet.directive.ts
@@ -6,6 +6,10 @@ import {
   TemplateRef,
   ViewContainerRef,
 } from '@angular/core';
+import {
+  IntersectionOptions,
+  IntersectionService,
+} from '../../layout/intersection/index';
 import { OutletPosition, USE_STACKED_OUTLETS } from './outlet.model';
 import { OutletService } from './outlet.service';
 
@@ -20,16 +24,38 @@ export class OutletDirective implements OnInit {
   set cxOutletContext(value: any) {
     this._context = value;
   }
+  /**
+   * Defers loading of the templates for this outlet.
+   */
+  @Input() cxOutletDefer: IntersectionOptions = { deferLoading: false };
 
   constructor(
     private vcr: ViewContainerRef,
     private templateRef: TemplateRef<any>,
     private outletService: OutletService<
       TemplateRef<any> | ComponentFactory<any>
-    >
+    >,
+    // TODO: avoid breacking change
+    private intersectionService: IntersectionService
   ) {}
 
   ngOnInit(): void {
+    this.cxOutletDefer.deferLoading ? this.deferRender() : this.render();
+  }
+
+  private deferRender() {
+    const hostElement = this.getHostElement(this.vcr.element.nativeElement);
+
+    // we need to wait a tick to take advantage of the ghost layout
+    setTimeout(() => {}, 0);
+    this.intersectionService
+      .isIntersected(hostElement, this.cxOutletDefer)
+      .subscribe(() => {
+        this.render();
+      });
+  }
+
+  private render() {
     this.renderOutlet(OutletPosition.BEFORE);
     this.renderOutlet(OutletPosition.REPLACE);
     this.renderOutlet(OutletPosition.AFTER);
@@ -63,5 +89,12 @@ export class OutletDirective implements OnInit {
         $implicit: this._context,
       });
     }
+  }
+
+  private getHostElement(element: Element): HTMLElement {
+    if (element instanceof HTMLElement) {
+      return element;
+    }
+    return this.getHostElement(element.parentElement);
   }
 }
