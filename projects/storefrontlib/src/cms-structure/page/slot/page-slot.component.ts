392a26205e4842ea728e3b381bf3d58bfacf38f6
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2019-12-19 14:29:06 +0100

    feat: Deferred and above-the-fold loading (#5847)
    * Intersection Service
    
    * Wire intersection service into outlet directive
    
    * drop unused code
    
    * optimize slot
    
    * defer component rendering in page slots
    
    * defer component rendering in container component
    
    * load consent management component anyways
    
    * defer component rendering in page slots
    
    * Add change detection
    
    * Introduce ghost styles to take advantage of deferred loading
    
    * add min height to ensure components end up in the view port
    
    * change cx-ghost to cx-defer
    
    * Introduce page fold + use CSS configuration for below the fold deverred loading
    
    * whole bunch of small refactors and documentations
    
    * fixed import
    
    * fix existing tests
    
    * Avoid low level renderer API for adding css classes
    
    * drop optional argument in constructuor signature
    
    * introduce defer loader
    
    * fix storefront unit tests
    
    * remove default configuration for deferred loading
    
    * use async EventEmitter
    
    * drop subscription
    
    * Add tests for page fold
    
    * drop outlet mock from test
    
    * Add tests for defer loading
    
    * organize code
    
    * fix test

--- projects/storefrontlib/src/cms-structure/page/slot/page-slot.component.ts
+++ projects/storefrontlib/src/cms-structure/page/slot/page-slot.component.ts
@@ -2,67 +2,127 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
+  HostBinding,
   Input,
+  OnDestroy,
+  OnInit,
   Renderer2,
 } from '@angular/core';
 import {
+  CmsConfig,
   CmsService,
   ContentSlotComponentData,
   ContentSlotData,
+  DeferLoadingStrategy,
   DynamicAttributeService,
 } from '@spartacus/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { IntersectionOptions } from '../../../layout/loading/intersection.model';
 
 @Component({
   selector: 'cx-page-slot',
   templateUrl: './page-slot.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageSlotComponent {
-  @Input() set position(position: string) {
+export class PageSlotComponent implements OnInit, OnDestroy {
+  // need to have this host binding at the top as it will override the entire class
+  @HostBinding('class') @Input() set position(position: string) {
     this.position$.next(position);
-    // add the position name as a css class so that
-    // layout can be applied to it, using the position based class.
-    this.renderer.addClass(this.hostElement.nativeElement, position);
   }
+  get position(): string {
+    return this.position$.value;
+  }
+
+  @HostBinding('class.cx-pending') isPending = true;
+  @HostBinding('class.has-components') hasComponents = false;
+  @HostBinding('class.page-fold') @Input() isPageFold = false;
+
+  private pendingComponentCount: number;
 
   readonly position$ = new BehaviorSubject<string>(undefined);
 
-  /**
-   * observable with `ContentSlotData` for the current position
-   */
-  readonly slot$: Observable<ContentSlotData> = this.position$.pipe(
-    switchMap(position => this.cmsService.getContentSlot(position)),
-    tap(slot => this.addSmartEditSlotClass(slot))
+  components$: Observable<ContentSlotComponentData[]> = this.position$.pipe(
+    switchMap(position =>
+      this.cmsService.getContentSlot(position).pipe(
+        tap(slot => this.addSmartEditSlotClass(slot)),
+        map(slot => (slot && slot.components ? slot.components : [])),
+        distinctUntilChanged(
+          (a, b) =>
+            a.length === b.length &&
+            !a.find((el, index) => el.uid !== b[index].uid)
+        )
+      )
+    )
   );
 
-  /**
-   * observable with components (`ContentSlotComponentData[]`)
-   * for the current slot
-   */
-  readonly components$: Observable<
-    ContentSlotComponentData[]
-  > = this.slot$.pipe(
-    map(slot => (slot && slot.components ? slot.components : [])),
-    distinctUntilChanged(
-      (a, b) =>
-        a.length === b.length && !a.find((el, index) => el.uid !== b[index].uid)
-    ),
-    tap(components => this.addComponentClass(components))
-  );
+  private subscription = new Subscription();
 
+  constructor(
+    cmsService: CmsService,
+    dynamicAttributeService: DynamicAttributeService,
+    renderer: Renderer2,
+    hostElement: ElementRef,
+    // tslint:disable-next-line:unified-signatures
+    config: CmsConfig
+  );
+  /**
+   * @deprecated since version 1.4
+   * Use constructor(cmsService: CmsService, dynamicAttributeService: DynamicAttributeService, renderer: Renderer2, hostElement: ElementRef, config?: CmsConfig) instead
+   */
+  constructor(
+    cmsService: CmsService,
+    dynamicAttributeService: DynamicAttributeService,
+    renderer: Renderer2,
+    hostElement: ElementRef
+  );
   constructor(
     protected cmsService: CmsService,
     protected dynamicAttributeService: DynamicAttributeService,
     protected renderer: Renderer2,
-    protected hostElement: ElementRef
+    protected hostElement: ElementRef,
+    protected config?: CmsConfig
   ) {}
 
-  // add a class to indicate whether the class is empty or not
-  private addComponentClass(components): void {
-    if (components && components.length > 0) {
-      this.renderer.addClass(this.hostElement.nativeElement, 'has-components');
+  ngOnInit() {
+    this.subscription.add(
+      this.components$.subscribe(components => {
+        this.hasComponents = components && components.length > 0;
+        this.pendingComponentCount = components ? components.length : 0;
+        this.isPending = this.pendingComponentCount > 0;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  /**
+   * Is triggered when a component is added to the view.
+   * We use this information to dropthe `is-pending` class from the page slot
+   * when all nested components have been added.
+   */
+  isLoaded(loadState: boolean) {
+    if (loadState) {
+      this.pendingComponentCount--;
+    }
+    this.isPending = this.pendingComponentCount > 0;
+  }
+
+  getComponentDeferOptions(componentType: string): IntersectionOptions {
+    const deferLoading = this.getDeferLoadingStrategy(componentType);
+    return { deferLoading };
+  }
+
+  /**
+   * The `DeferLoadingStrategy` indicates whether component rendering
+   * should be deferred.
+   */
+  private getDeferLoadingStrategy(componentType: string): DeferLoadingStrategy {
+    if (this.config) {
+      return ((this.config as CmsConfig).cmsComponents[componentType] || {})
+        .deferLoading;
     }
   }
 
