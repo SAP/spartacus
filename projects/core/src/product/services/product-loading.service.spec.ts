7c4cdd2b12a6378877427c1d737fe3d091608dae
dunqan <dunqan@w9.pl>
2019-12-20 09:32:09 +0100

    initial test for product loading service

---
+++ projects/core/src/product/services/product-loading.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+// import { ProductLoadingService } from './product-loading.service';
+
+describe('ProductLoadingService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    // const service: ProductLoadingService = TestBed.get(ProductLoadingService);
+    expect(true).toBeTruthy();
+  });
+});
