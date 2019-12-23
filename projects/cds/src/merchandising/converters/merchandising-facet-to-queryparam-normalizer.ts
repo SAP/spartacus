4972550cbd41b7e3026a00e824b7a3826ea89b67
Merryweather, Darrell <darrell.merryweather@sap.com>
2019-12-23 15:48:15 +0000

    Refactored to fix the sonar issue

--- projects/cds/src/merchandising/converters/merchandising-facet-to-queryparam-normalizer.ts
+++ projects/cds/src/merchandising/converters/merchandising-facet-to-queryparam-normalizer.ts
@@ -5,8 +5,8 @@ import { MerchandisingFacet } from '../model/merchandising-facet.model';
 @Injectable({ providedIn: 'root' })
 export class MerchandisingFacetToQueryparamNormalizer
   implements Converter<MerchandisingFacet[], string> {
-  convert(source: MerchandisingFacet[], target?: string): string {
-    target = source
+  convert(source: MerchandisingFacet[]): string {
+    const target = source
       .filter(Boolean)
       .filter((facet: MerchandisingFacet) => facet.code && facet.value)
       .map(facet => `${facet.code}:${facet.value}`)
