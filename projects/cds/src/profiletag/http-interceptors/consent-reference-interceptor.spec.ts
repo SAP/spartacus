47375a9ede45722ead2afc227cc8d5ae6aa15df6
corey Aloia <corey.aloia@sap.com>
2019-12-27 14:36:22 +0100

    fixing formatting error

--- projects/cds/src/profiletag/http-interceptors/consent-reference-interceptor.spec.ts
+++ projects/cds/src/profiletag/http-interceptors/consent-reference-interceptor.spec.ts
@@ -1,5 +1,8 @@
 import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { Type } from '@angular/core';
 import { inject, TestBed } from '@angular/core/testing';
 import { OccEndpointsService } from '@spartacus/core';
@@ -90,7 +93,9 @@ describe('consent reference interceptor', () => {
   it('Should not add the x-consent-reference header if url is not occ', inject(
     [HttpClient, HttpTestingController],
     (http: HttpClient, mock: HttpTestingController) => {
-      const injector = TestBed.get(ProfileTagEventService as Type<ProfileTagEventService>);
+      const injector = TestBed.get(ProfileTagEventService as Type<
+        ProfileTagEventService
+      >);
       injector.profileTagDebug = true;
       let response;
       http
