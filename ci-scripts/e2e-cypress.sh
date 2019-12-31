962e736437f52c739e196f4f42cab73b1bf0f2b4
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2019-12-31 13:30:30 -0500

    chore: match old e2e behavior (#5948)

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -9,7 +9,7 @@ yarn
 
 echo '-----'
 echo 'Building Spartacus libraries'
-yarn build:core:lib:cds && yarn build 2>&1 | tee build.log
+yarn build:core:lib:cds && yarn build:cds 2>&1 | tee build.log
 results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
 if [[ -z "$results" ]]; then
     echo "Success: prod build is fine."
@@ -26,5 +26,8 @@ if [[ $SUITE == 'regression' ]]; then
     # TODO: will fix once cds is out the door (conclusion we had on Friday is to make everything in one)
     yarn e2e:cy:cds:start-run-all-ci
 else
-    yarn e2e:cy:start-run-ci
+    # TODO: will fix once cds is out the door (conclusion we had on Friday is to make everything in one)
+    # Upcoming task to separate them before schematics
+    # This is just a hotfix for everything being together
+    yarn e2e:cy:cds:start-run-ci
 fi
