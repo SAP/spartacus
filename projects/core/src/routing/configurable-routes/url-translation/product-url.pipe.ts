e2ef501a4eb8f4b3951c29d354bbd08d68138167
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2020-01-02 10:24:58 -0500

    Freindly Pipe for PDP

--- projects/core/src/routing/configurable-routes/url-translation/product-url.pipe.ts
+++ projects/core/src/routing/configurable-routes/url-translation/product-url.pipe.ts
@@ -11,46 +11,6 @@ export class ProductURLPipe implements PipeTransform {
 
   constructor(private semanticPath : SemanticPathService){}
   transform(item: Product){
-  //  console.log(new ProductURLCommand("product", {"product":item}));
-
     return this.semanticPath.transform(new ProductURLCommand("product", item));
   }
- /* protected routingConfigService: RoutingConfigService;
-  semanticPath: SemanticPathService;
-  
-  constructor(private productURLService: RoutingConfigService, private urlParser: UrlParsingService) {}
-
-  transform(item : Product) {
-    const path =this.productURLService.getRouteConfig("product").paths[0];
-    const paramMapping= this.productURLService.getRouteConfig("product").paramsMapping;
-    console.log(path);
-    console.log(item);
-    console.log(paramMapping);
-    return this.provideParamsValues(path,item, paramMapping);
-  }
-
-  private provideParamsValues(
-    path: string,
-    params: object,
-    paramsMapping: ParamsMapping
-  ): string[] {
-    return this.urlParser.getPrimarySegments(path).map(segment => {
-      if (isParam(segment)) {
-        const paramName = getParamName(segment);
-        const mappedParamName = this.getMappedParamName(
-          paramName,
-          paramsMapping
-        );
-        return params[mappedParamName];
-      }
-      return segment;
-    });
-  }
-
-  private getMappedParamName(paramName: string, paramsMapping: object): string {
-    if (paramsMapping) {
-      return paramsMapping[paramName] || paramName;
-    }
-    return paramName;
-  }*/
 }
