bcb363823b1232ea16e95acb04b1ce8dc9d5289f
bgambocjaviniar <brian_javiniar@hotmail.com>
2020-01-02 10:21:38 -0500

    Modify the shell script to cater different integrators (GH-5963)

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -3,13 +3,14 @@ set -e
 set -o pipefail
 
 SUITE=$1
+INTEGRATION=$2
 
 yarn
 (cd projects/storefrontapp-e2e-cypress && yarn)
 
 echo '-----'
 echo 'Building Spartacus libraries'
-yarn build:core:lib:cds && yarn build:cds 2>&1 | tee build.log
+yarn build:core:lib:cds 2>&1 | tee build.log
 results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
 if [[ -z "$results" ]]; then
     echo "Success: prod build is fine."
@@ -23,11 +24,11 @@ fi
 echo '-----'
 echo "Running Cypress end to end tests $SUITE"
 if [[ $SUITE == 'regression' ]]; then
-    # TODO: will fix once cds is out the door (conclusion we had on Friday is to make everything in one)
-    yarn e2e:cy:cds:start-run-all-ci
+    if [[ $INTEGRATION == 'cds' ]]; then
+        yarn e2e:cy:cds:start-run-all-ci
+    else 
+        yarn e2e:cy:start-run-all-ci
+    fi
 else
-    # TODO: will fix once cds is out the door (conclusion we had on Friday is to make everything in one)
-    # Upcoming task to separate them before schematics
-    # This is just a hotfix for everything being together
-    yarn e2e:cy:cds:start-run-ci
+    yarn e2e:cy:start-run-ci
 fi
