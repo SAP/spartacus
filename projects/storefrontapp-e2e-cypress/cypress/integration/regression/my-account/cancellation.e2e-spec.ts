cee9d0d25ed50553b78e163c15d1238e92279302
qjimmy <quang-jimmy@hotmail.com>
2020-01-09 11:29:32 -0500

    replaced checkout process by quicker method

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/cancellation.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/cancellation.e2e-spec.ts
@@ -1,7 +1,7 @@
 import * as orderHistory from '../../../helpers/cancellations-returns';
 import * as orderFlow from '../../../helpers/order-history';
 
-describe('Return Request List for Cancellations and Returns', () => {
+describe('Place Order', () => {
   before(() => {
     cy.window().then(win => win.sessionStorage.clear());
     cy.requireLoggedIn();
@@ -15,8 +15,10 @@ describe('Return Request List for Cancellations and Returns', () => {
     cy.saveLocalStorage();
   });
 
-  orderFlow.orderHistoryTest.checkIfOrderIsDisplayed(); // This places order and goes to order history page
+  orderFlow.orderHistoryTest.checkIfOrderIsDisplayed(); // This places 2 orders
+});
 
+describe('Return Request List for Cancellations and Returns', () => {
   it('should have two tabs: 1 order tab and 1 return tab', () => {
     orderHistory.checkTabs();
   });
