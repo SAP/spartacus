bf3d60f96d4ad3fcdec1b13a6dfde3b832c6cdb2
qjimmy <quang-jimmy@hotmail.com>
2020-01-10 15:59:35 -0500

    test: e2e tests for order cancellations (GH-4291)

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/order-cancellations.e2e-spec.ts
@@ -0,0 +1,29 @@
+import * as orderCancellationsReturns from '../../../helpers/order-cancellations-returns';
+import * as orderHistory from '../../../helpers/order-history';
+
+describe.skip('Place Order', () => {
+  before(() => {
+    cy.window().then(win => win.sessionStorage.clear());
+    cy.requireLoggedIn();
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  orderHistory.orderHistoryTest.checkIfOrderIsDisplayed(); // This places 2 orders
+});
+
+describe.skip('Return Request List for Cancellations and Returns', () => {
+  it('should have two tabs: 1 order tab and 1 return tab', () => {
+    orderCancellationsReturns.checkTabs();
+  });
+
+  it('should fully cancel order', () => {
+    orderCancellationsReturns.cancelOrder();
+  });
+});
