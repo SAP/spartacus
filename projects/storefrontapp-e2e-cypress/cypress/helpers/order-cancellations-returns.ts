a7f6ddcb30df65523b8a86e2332d42e750a239a9
qjimmy <quang-jimmy@hotmail.com>
2020-01-10 13:02:50 -0500

    made requested changed

--- projects/storefrontapp-e2e-cypress/cypress/helpers/order-cancellations-returns.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/order-cancellations-returns.ts
@@ -1,28 +1,46 @@
+import { waitForPage } from './checkout-flow';
+
 export function checkTabs() {
   cy.get('cx-tab-paragraph-container > h3')
     .eq(1)
-    .should('contain', 'RETURNS')
+    .contains(/RETURNS \(\d\)/)
     .click();
 
   cy.get('cx-tab-paragraph-container > h3')
     .first()
-    .should('contain', 'ALL ORDERS')
+    .contains(/ALL ORDERS \(\d\)/)
     .click();
 }
 
 export function cancelOrder() {
+  const orderHistoryPage = waitForPage(
+    '/my-account/order/*',
+    'orderHistoryPage'
+  );
+
   cy.get('cx-order-history a.cx-order-history-value')
     .contains('In Process')
     .click({ force: true });
 
-  cy.get('cx-order-details-actions button.btn-primary').click({ force: true });
+  cy.wait(`@${orderHistoryPage}`);
 
-  cy.get('button.cx-counter-action')
-    .contains('+')
+  const cancelOrderPage = waitForPage('/my-account/order/*', 'cancelOrderPage');
+
+  cy.get('cx-order-details-actions button.btn-primary')
+    .contains('Cancel Items')
     .click({ force: true });
+
+  cy.wait(`@${cancelOrderPage}`);
+
+  cy.get('button.cx-action-link')
+    .contains('Set all quantities to maximum')
+    .click({ force: true });
+
+  cy.get('input.cx-counter-value').should('have.value', '1');
 
   cy.get('button.btn-primary')
     .contains('Continue')
+    .should('not.be.disabled')
     .click({ force: true });
 
   cy.get('cx-cancel-order-confirmation button.btn-primary')
