8aa848bb3ea081fca87ce6494058dcee3b3cf99e
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2020-01-10 10:49:21 -0500

    checkout with variants e2e test

--- projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-with-variants.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-with-variants.ts
@@ -1,4 +1,6 @@
-import { addProductToCart } from './checkout-as-persistent-user';
+import { user } from '../sample-data/checkout-flow';
+import { waitForPage } from './checkout-flow';
+import { fillPaymentDetails, fillShippingAddress } from './checkout-forms';
 
 export function loginSuccessfully() {
   cy.login('test-user-cypress@ydev.hybris.com', 'Password123.');
@@ -25,7 +27,49 @@ function configureApparelProduct() {
 }
 
 export function addProductVariantToCart() {
-  // cy.visit('/product/100191');
+  const request = requestPages();
+
+  // go to product page
+  const productId = '100191';
+  cy.visit(`/product/${productId}`);
+
+  // add product to cart and go to checkout
+  cy.get('cx-add-to-cart')
+    .getByText(/Add To Cart/i)
+    .click({ force: true });
+  cy.get('cx-added-to-cart-dialog').within(() => {
+    cy.getByText(/proceed to checkout/i).click({ force: true });
+  });
+
+  cy.wait(`@${request.shippingPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  // go to shipping address
+  fillShippingAddress(user);
+
+  cy.wait(`@${request.deliveryPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  // set delivery method
+  cy.get('#deliveryMode-standard-gross').check({ force: true });
+  cy.get('button.btn-primary').click({ force: true });
+
+  cy.wait(`@${request.paymentPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  // fill in payment method
+  fillPaymentDetails(user);
+
+  // go to payment details page
+  cy.get('cx-review-submit').should('exist');
+
+  cy.visit('/my-account/payment-details');
+  cy.get('.cx-payment .cx-body').then(() => {
+    cy.get('cx-card').should('exist');
+  });
 }
 
 export function checkoutWithVariants() {
@@ -35,4 +79,23 @@ export function checkoutWithVariants() {
   it('should visit product variant page', () => {
     addProductVariantToCart();
   });
+}
+
+function requestPages() {
+  const shippingPage = waitForPage(
+    '/checkout/shipping-address',
+    'getShippingPage'
+  );
+
+  const deliveryPage = waitForPage(
+    '/checkout/delivery-mode',
+    'getDeliveryPage'
+  );
+
+  const paymentPage = waitForPage(
+    '/checkout/payment-details',
+    'getPaymentPage'
+  );
+
+  return { shippingPage, deliveryPage, paymentPage };
 }
