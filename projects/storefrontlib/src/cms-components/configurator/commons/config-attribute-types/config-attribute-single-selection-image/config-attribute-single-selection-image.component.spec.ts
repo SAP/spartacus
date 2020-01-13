9c3dbb83fe33363de3c8d323d86ed846df29c881
Alexander Pilarczyk <alexander.pilarczyk@sap.com>
2020-01-13 13:26:47 +0100

    GH-5850 create attribute single selection Image

---
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-attribute-types/config-attribute-single-selection-image/config-attribute-single-selection-image.component.spec.ts
@@ -0,0 +1,58 @@
+import { ChangeDetectionStrategy } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Configurator } from '@spartacus/core';
+import { ConfigUIKeyGeneratorService } from '../../service/config-ui-key-generator.service';
+import { ConfigAttributeSingleSelectionImageComponent } from './config-attribute-single-selection-image.component';
+
+describe('ConfigAttributeSingleSelectionImageComponent', () => {
+  let component: ConfigAttributeSingleSelectionImageComponent;
+  let fixture: ComponentFixture<ConfigAttributeSingleSelectionImageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ConfigAttributeSingleSelectionImageComponent],
+      imports: [ReactiveFormsModule],
+      providers: [ConfigUIKeyGeneratorService],
+    })
+      .overrideComponent(ConfigAttributeSingleSelectionImageComponent, {
+        set: {
+          changeDetection: ChangeDetectionStrategy.Default,
+        },
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(
+      ConfigAttributeSingleSelectionImageComponent
+    );
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(
+      ConfigAttributeSingleSelectionImageComponent
+    );
+    component = fixture.componentInstance;
+    component.attribute = {
+      name: 'valueName',
+      attrCode: 444,
+      uiType: Configurator.UiType.RADIOBUTTON,
+      selectedSingleValue: 'selectedValue',
+      quantity: 1,
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set selectedSingleValue on init', () => {
+    expect(component.attributeSingleSelectionImage.value).toEqual(
+      'selectedValue'
+    );
+  });
+});
---
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-attribute-types/config-attribute-single-selection-image/config-attribute-single-selection-image.component.ts
@@ -0,0 +1,49 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Configurator } from '@spartacus/core';
+import { ConfigFormUpdateEvent } from '../../config-form/config-form.event';
+import { ConfigUIKeyGeneratorService } from '../../service/config-ui-key-generator.service';
+
+@Component({
+  selector: 'cx-config-attribute-single-selection-image',
+  templateUrl: './config-attribute-single-selection-image.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfigAttributeSingleSelectionImageComponent implements OnInit {
+  constructor(public uiKeyGenerator: ConfigUIKeyGeneratorService) {}
+
+  attributeSingleSelectionImage = new FormControl('');
+
+  @Input() attribute: Configurator.Attribute;
+  @Input() group: string;
+  @Input() ownerKey: string;
+
+  @Output() selectionChange = new EventEmitter<ConfigFormUpdateEvent>();
+
+  ngOnInit() {
+    this.attributeSingleSelectionImage.setValue(
+      this.attribute.selectedSingleValue
+    );
+  }
+
+  onSelect() {
+    const event: ConfigFormUpdateEvent = {
+      productCode: this.ownerKey,
+      changedAttribute: {
+        name: this.attribute.name,
+        selectedSingleValue: this.attributeSingleSelectionImage.value,
+        uiType: this.attribute.uiType,
+      },
+      groupId: this.group,
+    };
+
+    this.selectionChange.emit(event);
+  }
+}
