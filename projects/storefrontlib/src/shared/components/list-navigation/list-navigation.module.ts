f744a3de58a68732cdbb82ee2ba99c9ec6aa0949
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-01-17 17:41:39 +0100

    remove PaginationComponent from exports of wrong module

--- projects/storefrontlib/src/shared/components/list-navigation/list-navigation.module.ts
+++ projects/storefrontlib/src/shared/components/list-navigation/list-navigation.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { PaginationModule } from './pagination/index';
-import { PaginationComponent } from './pagination/pagination.component';
 import { SortingComponent } from './sorting/sorting.component';
 
 @NgModule({
   imports: [CommonModule, NgSelectModule, FormsModule, PaginationModule],
   declarations: [SortingComponent],
-  exports: [PaginationComponent, SortingComponent],
+  exports: [SortingComponent],
 })
 export class ListNavigationModule {}
