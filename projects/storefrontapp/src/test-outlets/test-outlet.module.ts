dca40c5ac96fc30fd75ae250ffc1e58ac58c901c
Marcin Lasak <lasakmarcin90@gmail.com>
2020-01-17 14:19:56 +0100

    Revert test-outlet.module.ts

--- projects/storefrontapp/src/test-outlets/test-outlet.module.ts
+++ projects/storefrontapp/src/test-outlets/test-outlet.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { ConfigModule } from '@spartacus/core';
-import { testOutletPagesCmsContentConfig } from './test-outlet-cms-page.config';
+import { TestOutletTemplateModule } from './test-outlet-template/test-outlet-template.module';
 import { TestOutletComponentModule } from './test-outlet-component/test-outlet-component.module';
 import { TestOutletSlotModule } from './test-outlet-slot/test-outlet-slot.module';
-import { TestOutletTemplateModule } from './test-outlet-template/test-outlet-template.module';
+import { ConfigModule } from '@spartacus/core';
+import { testOutletPagesCmsContentConfig } from './test-outlet-cms-page.config';
 
 @NgModule({
   imports: [
