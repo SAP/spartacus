adcb69077917f458faaa139190349f769700faf5
Michał Gruca <pucek9@gmail.com>
2020-01-20 16:49:59 +0100

    fix

--- projects/core/src/anonymous-consents/facade/anonymous-consents.service.ts
+++ projects/core/src/anonymous-consents/facade/anonymous-consents.service.ts
@@ -202,7 +202,9 @@ export class AnonymousConsentsService {
    * @param consent a consent to test
    */
   isConsentWithdrawn(consent: AnonymousConsent): boolean {
-    return consent.consentState === ANONYMOUS_CONSENT_STATUS.WITHDRAWN;
+    return (
+      consent && consent.consentState === ANONYMOUS_CONSENT_STATUS.WITHDRAWN
+    );
   }
 
   /**
