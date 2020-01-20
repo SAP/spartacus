ebd07c3e79ed770fb5834ebbbacd5593308951c0
Marcin Lasak <lasakmarcin90@gmail.com>
2020-01-20 08:06:41 +0100

    feat: Add MultiCartService to public API

--- projects/core/src/cart/facade/index.ts
+++ projects/core/src/cart/facade/index.ts
@@ -2,4 +2,5 @@ export * from './active-cart.service';
 export * from './cart-data.service';
 export * from './cart-voucher.service';
 export * from './cart.service';
+export * from './multi-cart.service';
 export * from './wish-list.service';
