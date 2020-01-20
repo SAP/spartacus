8ddbace4fd3b49c358ea2057264537b0dbe4bb2d
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-01-20 17:16:47 -0500

    GH-6051 develop PR (#6103)
    Closes GH-6103

---
+++ projects/storefrontlib/src/cms-components/myaccount/order/order-details/order-detail-items/order-consigned-entries/order-consigned-entries.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Consignment, Order, OrderEntry } from '@spartacus/core';
+
+@Component({
+  selector: 'cx-order-consigned-entries',
+  templateUrl: './order-consigned-entries.component.html',
+})
+export class OrderConsignedEntriesComponent {
+  @Input() consignments: Consignment[];
+  @Input() order: Order;
+
+  getConsignmentProducts(consignment: Consignment): OrderEntry[] {
+    const products: OrderEntry[] = [];
+    consignment.entries.forEach(element => {
+      products.push(element.orderEntry);
+    });
+
+    return products;
+  }
+}
