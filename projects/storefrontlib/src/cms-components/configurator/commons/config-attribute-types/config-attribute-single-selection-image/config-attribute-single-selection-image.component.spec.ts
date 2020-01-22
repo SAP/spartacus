58b04135a5e3e81178112853bae6659a24ecfa09
Alexander Pilarczyk <alexander.pilarczyk@sap.com>
2020-01-22 10:47:43 +0100

    GH-5850 update coverage

---
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-attribute-types/config-attribute-single-selection-image/config-attribute-single-selection-image.component.spec.ts
@@ -0,0 +1,76 @@
+import { ChangeDetectionStrategy } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Configurator } from '@spartacus/core';
+import { ConfigUIKeyGeneratorService } from '../../service/config-ui-key-generator.service';
+import { ConfigAttributeSingleSelectionImageComponent } from './config-attribute-single-selection-image.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+describe('ConfigAttributeSingleSelectionImageComponent', () => {
+  let component: ConfigAttributeSingleSelectionImageComponent;
+  let fixture: ComponentFixture<ConfigAttributeSingleSelectionImageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ConfigAttributeSingleSelectionImageComponent],
+      imports: [ReactiveFormsModule, NgSelectModule],
+      providers: [ConfigUIKeyGeneratorService],
+    })
+      .overrideComponent(ConfigAttributeSingleSelectionImageComponent, {
+        set: {
+          changeDetection: ChangeDetectionStrategy.Default,
+        },
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    const value1: Configurator.Value = {
+      valueCode: '1',
+      name: 'val1',
+      selected: true,
+    };
+    const value2: Configurator.Value = {
+      valueCode: '2',
+      name: 'val2',
+      selected: false,
+    };
+    const value3: Configurator.Value = {
+      valueCode: '3',
+      name: 'val3',
+      selected: true,
+    };
+
+    let localvalues: Configurator.Value[];
+    localvalues = [];
+
+    localvalues.push(value1, value2, value3);
+
+    fixture = TestBed.createComponent(
+      ConfigAttributeSingleSelectionImageComponent
+    );
+    component = fixture.componentInstance;
+
+    component.attribute = {
+      name: 'attributeName',
+      attrCode: 444,
+      uiType: Configurator.UiType.SINGLE_SELECTION_IMAGE,
+      values: localvalues,
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should not set selectedSingleValue on init', () => {
+    expect(component.attributeRadioButtonForm.value).toEqual(null);
+  });
+
+  it('should set selectedSingleValue on init', () => {
+    this.onSelect(1);
+    fixture.detectChanges();
+    expect(component.attributeRadioButtonForm.value).toEqual('2');
+  });
+});
