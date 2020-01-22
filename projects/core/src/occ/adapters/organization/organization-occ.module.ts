9d2c0c0d4fe807ffb2dcce49a83a1423a8987c38
Michał Gruca <pucek9@gmail.com>
2020-01-22 19:09:15 +0100

    up

--- projects/core/src/occ/adapters/organization/organization-occ.module.ts
+++ projects/core/src/occ/adapters/organization/organization-occ.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
+import { ConfigModule } from '../../../config/config.module';
 import { BudgetAdapter } from '../../../organization/connectors/budget/budget.adapter';
+import { PermissionAdapter } from '../../../organization/connectors/permission/permission.adapter';
+import { OrgUnitAdapter } from '../../../organization/connectors/org-unit/org-unit.adapter';
 import {
   BUDGET_NORMALIZER,
   BUDGETS_NORMALIZER,
@@ -10,16 +14,23 @@ import {
   B2BUNIT_NORMALIZER,
   B2BUNIT_LIST_NORMALIZER,
 } from '../../../organization/connectors/org-unit/converters';
+import {
+  PERMISSION_NORMALIZER,
+  PERMISSIONS_NORMALIZER,
+} from '../../../organization/connectors/permission/converters';
 
-import { OccBudgetAdapter } from './occ-budget.adapter';
-import { OccBudgetNormalizer } from './converters/occ-budget-normalizer';
-import { OrgUnitAdapter } from '../../../organization/connectors/org-unit/org-unit.adapter';
-import { OccOrgUnitAdapter } from './occ-org-unit.adapter';
-import { OccOrgUnitNormalizer } from './converters/occ-org-unit-normalizer';
 import { defaultOccOrganizationConfig } from './default-occ-organization-config';
-import { ConfigModule } from '../../../config/config.module';
-import { OccBudgetListNormalizer } from './converters/occ-budget-list-normalizer';
-import { OccOrgUnitListNormalizer } from './converters/occ-org-unit-list-normalizer';
+import { OccBudgetAdapter } from './occ-budget.adapter';
+import { OccOrgUnitAdapter } from './occ-org-unit.adapter';
+import { OccPermissionAdapter } from './occ-permission.adapter';
+import {
+  OccBudgetNormalizer,
+  OccBudgetListNormalizer,
+  OccOrgUnitNormalizer,
+  OccOrgUnitListNormalizer,
+  OccPermissionNormalizer,
+  OccPermissionListNormalizer,
+} from './converters/index';
 
 @NgModule({
   imports: [
@@ -50,6 +61,20 @@ import { OccOrgUnitListNormalizer } from './converters/occ-org-unit-list-normali
     {
       provide: B2BUNIT_LIST_NORMALIZER,
       useClass: OccOrgUnitListNormalizer,
+      multi: true,
+    },
+    {
+      provide: PermissionAdapter,
+      useClass: OccPermissionAdapter,
+    },
+    {
+      provide: PERMISSION_NORMALIZER,
+      useClass: OccPermissionNormalizer,
+      multi: true,
+    },
+    {
+      provide: PERMISSIONS_NORMALIZER,
+      useClass: OccPermissionListNormalizer,
       multi: true,
     },
   ],
