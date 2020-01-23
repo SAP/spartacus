520ba3b62a5a3198f092af7c2036077b91deabd2
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-01-23 12:06:42 -0500

    re-using loader reducer

--- projects/core/src/cms/store/reducers/components.reducer.ts
+++ projects/core/src/cms/store/reducers/components.reducer.ts
@@ -1,4 +1,4 @@
-import { initialLoaderState } from '../../../state/utils/loader/loader.reducer';
+import { loaderReducer } from '../../../state/utils/loader/loader.reducer';
 import { serializePageContext } from '../../../util/serialization-utils';
 import { CmsActions } from '../actions/index';
 import { ComponentsContext } from '../cms-state';
@@ -8,52 +8,67 @@ export const initialState: ComponentsContext = {
   pageContext: {},
 };
 
+function componentExistsReducer<T>(
+  state = false,
+  action: CmsActions.CmsComponentAction<T>
+): boolean {
+  switch (action.type) {
+    case CmsActions.LOAD_CMS_COMPONENT_FAIL:
+      return false;
+
+    case CmsActions.CMS_GET_COMPONENET_FROM_PAGE:
+    case CmsActions.LOAD_CMS_COMPONENT_SUCCESS:
+      return true;
+  }
+  return state;
+}
+
 export function reducer<T>(
   state = initialState,
   action: CmsActions.CmsComponentAction<T>
 ): ComponentsContext {
   switch (action.type) {
     case CmsActions.LOAD_CMS_COMPONENT: {
+      const pageContextReducer = loaderReducer<boolean>(
+        action.meta.entityType,
+        componentExistsReducer
+      );
       const context = serializePageContext(action.pageContext);
       return {
         ...state,
         pageContext: {
           ...state.pageContext,
-          [context]: {
-            ...initialLoaderState,
-            loading: true,
-            value: false,
-          },
+          [context]: pageContextReducer(state.pageContext[context], action),
         },
       };
     }
     case CmsActions.LOAD_CMS_COMPONENT_FAIL: {
+      const pageContextReducer = loaderReducer<boolean>(
+        action.meta.entityType,
+        componentExistsReducer
+      );
       const context = serializePageContext(action.pageContext);
       return {
         ...state,
         pageContext: {
           ...state.pageContext,
-          [context]: {
-            ...initialLoaderState,
-            error: true,
-            value: false,
-          },
+          [context]: pageContextReducer(state.pageContext[context], action),
         },
       };
     }
     case CmsActions.CMS_GET_COMPONENET_FROM_PAGE:
     case CmsActions.LOAD_CMS_COMPONENT_SUCCESS: {
+      const pageContextReducer = loaderReducer<boolean>(
+        action.meta.entityType,
+        componentExistsReducer
+      );
       const context = serializePageContext(action.pageContext);
       return {
         ...state,
         component: action.payload as T,
         pageContext: {
           ...state.pageContext,
-          [context]: {
-            ...initialLoaderState,
-            success: true,
-            value: true,
-          },
+          [context]: pageContextReducer(state.pageContext[context], action),
         },
       };
     }
