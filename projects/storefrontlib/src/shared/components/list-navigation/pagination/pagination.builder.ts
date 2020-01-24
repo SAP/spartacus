c6ad9b659323b2ef982a02ed395edf3411d22b8c
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-01-24 17:39:46 +0100

    Refactor pagination builder

--- projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.builder.ts
+++ projects/storefrontlib/src/shared/components/list-navigation/pagination/pagination.builder.ts
@@ -1,28 +1,70 @@
 import { Injectable } from '@angular/core';
-import { PaginationConfigService } from './config/pagination-config.service';
-import { PaginationItem } from './pagination.model';
+import { PaginationConfig } from './config/index';
+import {
+  PaginationItem,
+  PaginationItemType,
+  PaginationNavigationPosition,
+  PaginationOptions,
+} from './pagination.model';
 
+const FALLBACK_PAGINATION_OPTIONS: PaginationOptions = {
+  rangeCount: 3,
+  dotsLabel: '...',
+  startLabel: '«',
+  previousLabel: '‹',
+  nextLabel: '›',
+  endLabel: '»',
+};
+
+/**
+ * Builds a pagination structures based on a pageCount and current page number.
+ * There are various {@link PaginationConfig} options which can be used to configure
+ * the behaviour of the build. Alternatively, CSS can be used to further customise
+ * the pagination.
+ *
+ * Examples:
+ * The full blown pagination items contain the follow elements:
+ *
+ * `« ‹ 1 ... 4 (5) 6 ... 9 › »`
+ *
+ * This includes pagination items to the following pages:
+ * - start page
+ * - previous page
+ * - first page
+ * - page range
+ * - last page
+ * - next page
+ * - end page
+ *
+ * All of those links are configurable, including the size of the page range.
+ * The current page will always be centered in the page range to provide direct access
+ * to the previous and next page.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class PaginationBuilder {
-  constructor(protected config: PaginationConfigService) {}
+  constructor(protected paginationConfig: PaginationConfig) {}
 
   /**
-   * Builds a list of `PaginationItem`, based on teh current page number, and
-   * pagination options.
+   * Builds a list of `PaginationItem`. The give pageCount and current are used
+   * to build out the full pagination. There are various {@link PaginationConfig} options
+   * which can be used to configure the behaviour of the build. Alternatively, CSS
+   * can be used to further specialize visibility of the pagination.
    *
-   * @param pageCount the total number of pages
-   * @param current the current page number
+   * @param pageCount The total number of pages
+   * @param current The current page number, 0-index based
+   * @returns An array of `PaginationItem`
    */
   paginate(pageCount: number, current: number): PaginationItem[] {
+    const pages: PaginationItem[] = [];
     if (pageCount < 2) {
-      return [];
+      return pages;
     }
-
-    const pages = this.buildRange(pageCount, current);
-    this.addDots(pageCount, pages);
-    this.addFirstLast(pageCount, pages);
+    this.addPages(pages, pageCount, current);
+    this.addDots(pages, pageCount);
+    this.addFirstLast(pages, pageCount);
+    this.addNavigation(pages, pageCount, current);
 
     return pages;
   }
@@ -31,90 +73,255 @@ export class PaginationBuilder {
    * Returns the current page with surrounding pages (based on the `config.rangeCount`).
    * The current page is always centered to provide direct access to the previous and next page.
    *
-   * @param current The current page is always centered in the range
+   * @param pages The list of page items that is used to amend
+   * @param pageCount The total number of pages
+   * @param current The current page number, 0-index based
    */
-  buildRange(pageCount: number, current: number): PaginationItem[] {
-    const pages: PaginationItem[] = [];
-
+  protected addPages(
+    pages: PaginationItem[],
+    pageCount: number,
+    current: number
+  ): void {
     const start = this.getStartOfRange(pageCount, current);
     const max = Math.min(this.config.rangeCount, pageCount);
 
     Array.from(Array(max)).forEach((_, i) => {
       pages.push({
         number: i + start,
-        isPrimary: true,
-        isCurrent: i + start === current,
+        label: String(i + start + 1),
+        type: PaginationItemType.PAGE,
       });
     });
-
-    return pages;
   }
 
   /**
-   * Adds dots before and after the page range, if configured (defaults to true).
+   * Adds dots before and after the given pages, if configured (defaults to true).
    * If the dots only represent a single page, the page number is added instead of
    * the dots, unless the configuration requires dots always.
    *
-   * @param start The first page of the page range.
+   * @param pages The list of page items that is used to amend
+   * @param pageCount The total number of pages
    */
-  private addDots(pageCount: number, pages: PaginationItem[]): void {
+  protected addDots(pages: PaginationItem[], pageCount: number): void {
     if (!this.config.addDots) {
       return;
     }
 
-    // before
-    const start = pages[0].number;
-    const firstOccurrence = this.config.addFirst ? 2 : 1;
-    if (start > firstOccurrence) {
-      pages.unshift({
-        number: firstOccurrence,
-        isGap: !(start === 1 + firstOccurrence && this.config.omitDotsForPage),
-      });
-    }
+    const addFirstGap = () => {
+      const firstItemNumber = pages[0].number;
+      const gapNumber = this.config.addFirst ? 1 : 0;
+      if (firstItemNumber > gapNumber) {
+        const isGap =
+          !this.config.substituteDotsForSingularPage ||
+          firstItemNumber !== gapNumber + 1;
+        const isSubstitued =
+          this.config.addFirst &&
+          this.config.substituteDotsForSingularPage &&
+          gapNumber === 0;
+        const type = isGap
+          ? PaginationItemType.GAP
+          : isSubstitued
+          ? PaginationItemType.FIRST
+          : PaginationItemType.PAGE;
+        return [
+          Object.assign(
+            {
+              label: isGap ? this.config.dotsLabel : String(gapNumber + 1),
+              type,
+            },
+            isGap ? null : { number: gapNumber }
+          ),
+        ];
+      } else return [];
+    };
 
-    // after
-    const last = pages[pages.length - 1].number;
-    const gapSizeEnd = this.config.addLast ? 1 : 0;
-    if (last < pageCount) {
-      pages.push({
-        number: last + 1,
-        isGap:
-          pageCount - last > gapSizeEnd + (this.config.omitDotsForPage ? 1 : 0),
-      });
-    }
+    const addLastGap = () => {
+      const nextPageNumber = pages[pages.length - 1].number + 1;
+      const last = pageCount - (this.config.addLast ? 2 : 1);
+      if (nextPageNumber <= last) {
+        const isSubstitued =
+          this.config.addLast &&
+          this.config.substituteDotsForSingularPage &&
+          nextPageNumber === last;
+        const isGap =
+          nextPageNumber <
+          pageCount - (this.config.substituteDotsForSingularPage ? 1 : 0);
+        const type = isGap
+          ? PaginationItemType.GAP
+          : isSubstitued
+          ? PaginationItemType.LAST
+          : PaginationItemType.PAGE;
+        return [
+          Object.assign(
+            {
+              label: isGap ? this.config.dotsLabel : String(nextPageNumber + 1),
+              type,
+            },
+            isGap ? null : { number: nextPageNumber }
+          ),
+        ];
+      } else return [];
+    };
 
-    return;
+    pages.unshift(...addFirstGap());
+    pages.push(...addLastGap());
   }
 
-  private addFirstLast(pageCount: number, pages: PaginationItem[]): void {
-    if (this.config.addFirst && pages[0].number !== 1) {
+  /**
+   * Add links to the first and last page, if configured to do so.
+   *
+   * @param pages The list of page items that is used to amend
+   * @param pageCount The total number of pages
+   *
+   */
+  protected addFirstLast(pages: PaginationItem[], pageCount: number) {
+    if (this.config.addFirst && pages[0].number !== 0) {
       pages.unshift({
-        number: 1,
+        number: 0,
+        label: '1',
+        type: PaginationItemType.FIRST,
       });
     }
-
-    if (this.config.addLast && pages[pages.length - 1].number !== pageCount) {
+    if (
+      this.config.addLast &&
+      pages[pages.length - 1].number !== pageCount - 1
+    ) {
       pages.push({
-        number: pageCount,
+        number: pageCount - 1,
+        label: String(pageCount),
+        type: PaginationItemType.LAST,
       });
     }
   }
 
   /**
+   * Add links to the start, previous, next and last page, if configured to do so.
+   * The order of the links can be configured by using the {@link PaginationConfig},
+   * using the `PaginationNavigationPosition` (`BEFORE` or `AFTER`).
+   * The `PaginationNavigationPosition` allows for 3 flavours:
+   *
+   * - by default the pagination starts with start and previous and ends with the next and end links
+   * - BEFORE – all navigation links are added in the front of the pagination list
+   * - AFTER – all navigation links are pushed to the end of the pagination list
+   *
+   * @param pages The list of page items that is used to amend
+   * @param pageCount The total number of pages
+   * @param current The current page number, 0-index based
+   *
+   */
+  protected addNavigation(
+    pages: PaginationItem[],
+    pageCount: number,
+    current: number
+  ): void {
+    const before = this.getBeforeLinks(current);
+    const after = this.getAfter(pageCount, current);
+    const pos = this.config.navigationPosition;
+    if (!pos || pos === PaginationNavigationPosition.ASIDE) {
+      pages.unshift(...before);
+      pages.push(...after);
+    } else {
+      if (pos === PaginationNavigationPosition.BEFORE) {
+        pages.unshift(...before, ...after);
+      }
+      if (pos === PaginationNavigationPosition.AFTER) {
+        pages.push(...before, ...after);
+      }
+    }
+  }
+
+  /**
+   * Returns the start and previous links, if applicable.
+   */
+  private getBeforeLinks(current: number): PaginationItem[] {
+    const list = [];
+
+    if (this.config.addStart) {
+      const start = () => {
+        return Object.assign(
+          {
+            label: this.config.startLabel,
+            type: PaginationItemType.START,
+          },
+          current > 0 ? { number: 0 } : null
+        );
+      };
+      list.push(start());
+    }
+    if (this.config.addPrevious) {
+      const previous = () => {
+        return Object.assign(
+          {
+            label: this.config.previousLabel,
+            type: PaginationItemType.PREVIOUS,
+          },
+          current > 0 ? { number: current - 1 } : null
+        );
+      };
+      list.push(previous());
+    }
+    return list;
+  }
+
+  /**
+   * Returns the next and end links, if applicable.
+   */
+  private getAfter(pageCount: number, current: number): PaginationItem[] {
+    const list = [];
+
+    if (this.config.addNext) {
+      const next = () => {
+        return Object.assign(
+          {
+            label: this.config.nextLabel,
+            type: PaginationItemType.NEXT,
+          },
+          current < pageCount - 1 ? { number: current + 1 } : null
+        );
+      };
+      list.push(next());
+    }
+    if (this.config.addEnd) {
+      const end = () => {
+        return Object.assign(
+          {
+            label: this.config.endLabel,
+            type: PaginationItemType.END,
+          },
+          current < pageCount - 1 ? { number: pageCount - 1 } : null
+        );
+      };
+      list.push(end());
+    }
+
+    return list;
+  }
+  /**
    * Resolves the first page of the range we need to build.
    * This is the page that is leading up to the range of the
    * current page.
+   *
+   * @param pageCount The total number of pages.
+   * @param current The current page number, 0-index based.
    */
   private getStartOfRange(pageCount: number, current: number): number {
+    const count = this.config.rangeCount - 1;
     // the least number of pages before and after the current
-    const delta = Math.round((this.config.rangeCount - 1) / 2);
+    const delta = Math.round(count / 2);
 
     // ensure that we start with at least the first page
-    const minStart = Math.max(1, current - delta);
+    const minStart = Math.max(0, current - delta);
     // ensures that we start with at least 1 and do not pass the last range
-    const maxStart = Math.max(1, pageCount - (this.config.rangeCount - 1));
+    const maxStart = Math.max(0, pageCount - count - 1);
 
     // ensure that we get at least a full range at the end
     return Math.min(maxStart, minStart);
+  }
+
+  private get config(): PaginationOptions {
+    return Object.assign(
+      FALLBACK_PAGINATION_OPTIONS,
+      this.paginationConfig.pagination
+    );
   }
 }
