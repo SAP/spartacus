5ad21f34ac77731365f3f882eb1646c1eaf8fdb4
Caine Rotherham <rotherham.c@gmail.com>
2020-02-04 10:32:40 +0100

    Focus trap works

--- projects/storefrontlib/src/layout/a11y/focus-trap/service/focus-trap.service.ts
+++ projects/storefrontlib/src/layout/a11y/focus-trap/service/focus-trap.service.ts
@@ -1,64 +1,53 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FocusTrapService {
-  observers = [];
+  focusableSelectors = [
+    `a[href]:not([tabindex='-1'])`,
+    `area[href]:not([tabindex='-1'])`,
+    `input:not([disabled]):not([tabindex='-1'])`,
+    `select:not([disabled]):not([tabindex='-1'])`,
+    `textarea:not([disabled]):not([tabindex='-1'])`,
+    `button:not([disabled]):not([tabindex='-1'])`,
+    `iframe:not([tabindex='-1'])`,
+    `[tabindex]:not([tabindex='-1'])`,
+    `[contentEditable=true]:not([tabindex='-1'])`,
+  ];
 
   constructor() {}
 
   add(target: HTMLElement): void {
-    // const observer = new MutationObserver(mutations => {
-    //   console.log(mutations);
-    // });
-    // observer.observe(target);
-    // this.observers.push({ target: target, observer: observer });
-    // const root = target.getRootNode();
-    // const focusableEls: NodeListOf<HTMLElement> = root.querySelectorAll(
-    //   'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
-    // );
-    // focusableEls.forEach(el => el.setAttribute('tabindex', '-1'));
-    console.log('added');
+    const root: Document = <Document>target.getRootNode();
+    const focusableElements = Array.from(<NodeListOf<HTMLElement>>(
+      target.querySelectorAll(this.focusableSelectors.join(','))
+    )).filter(element => element.offsetParent !== null);
 
-    const focusableEls = target.querySelectorAll(
-      'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
-    );
-    const firstFocusableEl = focusableEls[0];
-    const lastFocusableEl = focusableEls[focusableEls.length - 1];
-    const KEYCODE_TAB = 9;
+    const firstFocusableEl: HTMLElement = focusableElements[0];
+    const lastFocusableEl: HTMLElement =
+      focusableElements[focusableElements.length - 1];
 
-    target.addEventListener('keydown', function(e) {
-      const isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
-
-      if (!isTabPressed) {
-        return;
-      }
-
-      if (e.shiftKey) {
-        /* shift + tab */ if (document.activeElement === firstFocusableEl) {
-          lastFocusableEl.focus();
-          e.preventDefault();
-        }
-      } /* tab */ else {
-        if (document.activeElement === lastFocusableEl) {
-          firstFocusableEl.focus();
-          e.preventDefault();
+    root.addEventListener('keydown', (e: KeyboardEvent) => {
+      if (e.key === 'Tab') {
+        if (e.shiftKey) {
+          // Wrap first to last element on SHIFT+TAB keypress
+          if (root.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          // Wrap last to first element on TAB keypress
+          if (root.activeElement === lastFocusableEl) {
+            firstFocusableEl.focus();
+            e.preventDefault();
+          }
         }
       }
     });
   }
 
   remove(target: HTMLElement): void {
-    // const root = target.getRootNode();
-    // const focusableEls: NodeListOf<HTMLElement> = root.querySelectorAll(
-    //   'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
-    // );
-    // focusableEls.forEach(el => el.removeAttribute('tabindex'));
-    this.observers = this.observers.filter(
-      observer => observer.target === target
-    );
     console.log('removed');
   }
 }
