12a794334ea8cbf63e5c3c0789b353a58b31340c
Agrawal <archit.nitb@gmail.com>
2020-02-04 16:04:51 +0000

    saving changes in index & permission model & prettier

--- projects/core/src/model/index.ts
+++ projects/core/src/model/index.ts
@@ -1,24 +1,21 @@
 export * from './address.model';
+export * from './budget.model';
 export * from './cart.model';
 export * from './cms.model';
 export * from './consent.model';
 export * from './consignment-tracking.model';
+export * from './cost-center.model';
+export * from './customer-coupon.model';
 export * from './image.model';
 export * from './misc.model';
-export * from './order.model';
-export * from './point-of-service.model';
-export * from './product-search.model';
-export * from './product.model';
-export * from './shared.model';
-export * from './store-finder.model';
-export * from './customer-coupon.model';
-export * from './scoped-data';
 export * from './notification-preference.model';
-export * from './product-interest.model';
-export * from './budget.model';
+export * from './order.model';
 export * from './org-unit.model';
 export * from './permission.model';
-<<<<<<< HEAD
-=======
-export * from './cost-center.model';
->>>>>>> 8902e5cfed8bb22e0b4b53d5f2c043a26d1e184d
+export * from './point-of-service.model';
+export * from './product-interest.model';
+export * from './product-search.model';
+export * from './product.model';
+export * from './scoped-data';
+export * from './shared.model';
+export * from './store-finder.model';
