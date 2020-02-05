cc2a0ed8f4090c45a43cc0fe47401e69cc7eaddb
bgambocjaviniar <brian_javiniar@hotmail.com>
2020-02-05 00:03:54 -0500

    fixed first error run and made the test more stronger in timing

--- projects/storefrontapp-e2e-cypress/cypress/integration/vendor/cds/profile-tag.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/vendor/cds/profile-tag.e2e-spec.ts
@@ -1,4 +1,5 @@
 import * as anonymousConsents from '../../../helpers/anonymous-consents';
+import { waitForPage } from '../../../helpers/checkout-flow';
 import { navigation } from '../../../helpers/navigation';
 import { cdsHelper } from '../../../helpers/vendor/cds/cds';
 import { profileTagHelper } from '../../../helpers/vendor/cds/profile-tag';
@@ -6,36 +7,35 @@ describe.skip('Profile-tag component', () => {
   beforeEach(() => {
     cy.server();
     cdsHelper.setUpMocks();
-  });
-  it('should send a Navigated event when a navigation occurs', () => {
-    // navigation.visitHomePage({});
+
+    const homePage = waitForPage('homepage', 'getHomePage');
     navigation.visitHomePage({
       options: {
         onBeforeLoad: profileTagHelper.interceptProfileTagJs,
       },
     });
     cy.get('cx-profiletag');
+    cy.wait(`@${homePage}`)
+      .its('status')
+      .should('eq', 200);
+
     profileTagHelper.triggerLoaded();
-    cy.get('.Section1.has-components')
-      .first()
-      .click();
-    cy.location('pathname', { timeout: 2000 }).should(
-      'include',
-      '/OpenCatalogue'
-    );
+  });
+  it('should send a Navigated event when a navigation occurs', () => {
+    const categoryPage = waitForPage('CategoryPage', 'getCategory');
+    cy.get(
+      'cx-page-slot cx-banner img[alt="Save Big On Select SLR & DSLR Cameras"]'
+    ).click();
+    cy.wait(`@${categoryPage}`)
+      .its('status')
+      .should('eq', 200);
+
+    cy.location('pathname').should('include', '/OpenCatalogue');
     cy.window().then(win => {
       expect((<any>win).Y_TRACKING.eventLayer[0]['name']).to.equal('Navigated');
     });
   });
   it('should wait for a user to accept consent and then send a ConsentChanged event', () => {
-    // navigation.visitHomePage({});
-    navigation.visitHomePage({
-      options: {
-        onBeforeLoad: profileTagHelper.interceptProfileTagJs,
-      },
-    });
-    cy.get('cx-profiletag');
-    profileTagHelper.triggerLoaded();
     anonymousConsents.clickAllowAllFromBanner();
     cy.window().then(win => {
       expect((<any>win).Y_TRACKING.eventLayer[0]).to.have.property('name');
