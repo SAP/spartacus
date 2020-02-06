b011b174c159a89a09c8e2463e20f7c41fd34c9e
Terry Tan <terry.tan01@sap.com>
2020-02-06 19:01:44 +0800

    PANDA-3352, udpte a11y.

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
@@ -174,7 +174,7 @@ export const tabbingOrderConfig: TabbingOrderConfig = {
     { value: '-', type: TabbingOrderTypes.BUTTON },
     { type: TabbingOrderTypes.GENERIC_INPUT },
     { value: '+', type: TabbingOrderTypes.BUTTON },
-    { value: 'Save For Later', type: TabbingOrderTypes.LINK},
+    { value: 'Save For Later', type: TabbingOrderTypes.GENERIC_BUTTON},
     { value: 'Remove', type: TabbingOrderTypes.LINK },
     {
       value: 'Proceed to Checkout',
@@ -186,7 +186,7 @@ export const tabbingOrderConfig: TabbingOrderConfig = {
       value: 'FUN Flash Single Use Camera, 27+12 pic',
       type: TabbingOrderTypes.LINK,
     },
-    { value: 'Move To Cart', type: TabbingOrderTypes.GENERIC_BUTTON},
+    { type: TabbingOrderTypes.GENERIC_BUTTON},
     { value: 'Remove', type: TabbingOrderTypes.GENERIC_BUTTON },
   ],
   changePassword: [
