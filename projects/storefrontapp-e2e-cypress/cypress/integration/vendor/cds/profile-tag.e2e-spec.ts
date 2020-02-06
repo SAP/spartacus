7779c8a41bb4bca13a36d7f9de8bd09ffb606c23
Shankari Giri <shankari.giri@sap.com>
2020-02-06 12:57:38 +0100

    ARGO: Added e2e tests for cart events.

--- projects/storefrontapp-e2e-cypress/cypress/integration/vendor/cds/profile-tag.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/vendor/cds/profile-tag.e2e-spec.ts
@@ -3,7 +3,7 @@ import { waitForPage } from '../../../helpers/checkout-flow';
 import { navigation } from '../../../helpers/navigation';
 import { cdsHelper } from '../../../helpers/vendor/cds/cds';
 import { profileTagHelper } from '../../../helpers/vendor/cds/profile-tag';
-describe.skip('Profile-tag component', () => {
+describe.skip('Profile-tag events', () => {
   beforeEach(() => {
     cy.server();
     cdsHelper.setUpMocks();
@@ -20,6 +20,29 @@ describe.skip('Profile-tag component', () => {
       .should('eq', 200);
 
     profileTagHelper.triggerLoaded();
+  });
+  it('should send a CartChanged event on adding an item to cart', () => {
+    const productCode = 'ProductPage&code=280916';
+    const productPage = waitForPage(productCode, 'getProductPage');
+    cy.get('.Section4 cx-banner')
+      .first()
+      .find('img')
+      .click({ force: true });
+    cy.wait(`@${productPage}`)
+      .its('status')
+      .should('eq', 200);
+    cy.get('cx-add-to-cart button.btn-primary').click();
+    cy.window().then(win => {
+      expect((<any>win).Y_TRACKING.eventLayer.length).to.equal(2);
+      expect((<any>win).Y_TRACKING.eventLayer[1]['name']).to.equal(
+        'CartSnapshot'
+      );
+      expect((<any>win).Y_TRACKING.eventLayer[1]['data']).to.exist;
+      expect((<any>win).Y_TRACKING.eventLayer[1]['data']['cart']).to.exist;
+      expect(
+        JSON.stringify((<any>win).Y_TRACKING.eventLayer[1]['data']['cart'])
+      ).to.include('code');
+    });
   });
   it('should send a Navigated event when a navigation occurs', () => {
     const categoryPage = waitForPage('CategoryPage', 'getCategory');
