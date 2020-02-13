e97e046e7452eb46f69ac5d6f4d04474231f5ea8
corey Aloia <corey.aloia@sap.com>
2020-02-13 12:11:53 +0100

    merged develop

--- projects/storefrontapp-cds-integration/src/environments/environment.ts
+++ projects/storefrontapp-cds-integration/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  occBaseUrl: 'https://dev-com-12.accdemo.b2c.ydev.hybris.com:9002',
+  occBaseUrl: 'https://yprofiletest2.yrdci.fra.hybris.com:9002',
 };
