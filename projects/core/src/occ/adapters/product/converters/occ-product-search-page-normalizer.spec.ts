52a45d2920a3ddd712414ccaa3d4af4315ea7cb4
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-02-17 18:45:53 +0100

    Remove useles facets from the result list

--- projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.spec.ts
+++ projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.spec.ts
@@ -13,6 +13,42 @@ class MockConverterService {
 
 const mockSource: Occ.ProductSearchPage = {
   products: [{ images: [] }, { images: [] }],
+  facets: [
+    {
+      name: 'facet-1',
+      values: [{ count: 1 }, { count: 2 }, { count: 3 }],
+      topValues: [{}, {}],
+    } as Occ.Facet,
+  ],
+};
+
+const mockPlpWithUselessFacets: Occ.ProductSearchPage = {
+  pagination: {
+    totalResults: 2,
+  },
+  facets: [
+    {
+      name: 'useless-facet',
+      values: [{ count: 2 }, { count: 2 }],
+    },
+    {
+      name: 'useful-facet',
+      values: [{ count: 1 }, { count: 2 }, { count: 1 }],
+    },
+  ] as Occ.Facet[],
+};
+
+const mockPlpWithoutPagination: Occ.ProductSearchPage = {
+  facets: [
+    {
+      name: 'useless-facet',
+      values: [{ count: 2 }, { count: 2 }],
+    },
+    {
+      name: 'useful-facet',
+      values: [{ count: 1 }, { count: 2 }, { count: 1 }],
+    },
+  ] as Occ.Facet[],
 };
 
 describe('OccProductSearchPageNormalizer', () => {
@@ -41,10 +77,24 @@ describe('OccProductSearchPageNormalizer', () => {
   it('should apply product image normalizer to products', () => {
     const converter = TestBed.get(ConverterService as Type<ConverterService>);
     const result = normalizer.convert(mockSource);
-    const expected = {
-      products: [{ images: ['images' as any] }, { images: ['images' as any] }],
-    } as any;
-    expect(result).toEqual(expected);
+    const expected = [
+      { images: ['images' as any] },
+      { images: ['images' as any] },
+    ] as any;
+    expect(result.products).toEqual(expected);
     expect(converter.convert).toHaveBeenCalled();
+  });
+
+  describe('normalize facet value count', () => {
+    it('should remove useles facet from facet list', () => {
+      const result = normalizer.convert(mockPlpWithUselessFacets);
+      expect(result.facets.length).toEqual(1);
+      expect(result.facets[0].name).toEqual('useful-facet');
+    });
+
+    it('should not remove useles facet facet list if pagination is not used', () => {
+      const result = normalizer.convert(mockPlpWithoutPagination);
+      expect(result.facets.length).toEqual(2);
+    });
   });
 });
--- projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.ts
+++ projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.ts
@@ -1,11 +1,11 @@
-import { Occ } from '../../../occ-models/occ.models';
+import { Injectable } from '@angular/core';
+import { ProductSearchPage } from '../../../../model/product-search.model';
+import { PRODUCT_NORMALIZER } from '../../../../product/connectors/product/converters';
 import {
   Converter,
   ConverterService,
 } from '../../../../util/converter.service';
-import { Injectable } from '@angular/core';
-import { PRODUCT_NORMALIZER } from '../../../../product/connectors/product/converters';
-import { ProductSearchPage } from '../../../../model/product-search.model';
+import { Occ } from '../../../occ-models/occ.models';
 
 @Injectable({ providedIn: 'root' })
 export class OccProductSearchPageNormalizer
@@ -20,11 +20,40 @@ export class OccProductSearchPageNormalizer
       ...target,
       ...(source as any),
     };
+    this.normalizeUselessFacets(target);
     if (source.products) {
       target.products = source.products.map(product =>
         this.converterService.convert(product, PRODUCT_NORMALIZER)
       );
     }
     return target;
+  }
+
+  /**
+   * The (current) backend returns facets with values that do not contribute
+   * to the facet navigation much, as the number in the result list will not get
+   * affected when they got applied. A ticket has been created to fix this
+   * behaviour, see https://jira.hybris.com/browse/CS-427.
+   *
+   * As long as this is not in place, we manually filter the facet from the list;
+   * any facet that does not have a count < the total results will be dropped from
+   * the facets.
+   */
+  private normalizeUselessFacets(target: ProductSearchPage): void {
+    target.facets = [].concat(
+      target.facets.filter(facet => {
+        return (
+          !target.pagination ||
+          !target.pagination.totalResults ||
+          ((!facet.hasOwnProperty('visible') || facet.visible) &&
+            facet.values &&
+            facet.values.find(value => {
+              return (
+                value.selected || value.count < target.pagination.totalResults
+              );
+            }))
+        );
+      })
+    );
   }
 }
