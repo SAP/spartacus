1c28966c5dd17e86380ed53c96ca0c5a04d089b4
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-02-17 17:36:17 +0100

    normalize topValues facet data into a simplified model

--- projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.ts
+++ projects/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.ts
@@ -1,16 +1,25 @@
-import { Occ } from '../../../occ-models/occ.models';
+import { Injectable } from '@angular/core';
+import {
+  Facet,
+  ProductSearchPage,
+} from '../../../../model/product-search.model';
+import { PRODUCT_NORMALIZER } from '../../../../product/connectors/product/converters';
 import {
   Converter,
   ConverterService,
 } from '../../../../util/converter.service';
-import { Injectable } from '@angular/core';
-import { PRODUCT_NORMALIZER } from '../../../../product/connectors/product/converters';
-import { ProductSearchPage } from '../../../../model/product-search.model';
+import { Occ } from '../../../occ-models/occ.models';
 
 @Injectable({ providedIn: 'root' })
 export class OccProductSearchPageNormalizer
   implements Converter<Occ.ProductSearchPage, ProductSearchPage> {
   constructor(private converterService: ConverterService) {}
+
+  /**
+   * Specifies the minimal number of top values in case
+   * non have been setup by the business.
+   */
+  protected DEFAULT_TOP_VALUES = 6;
 
   convert(
     source: Occ.ProductSearchPage,
@@ -20,11 +29,38 @@ export class OccProductSearchPageNormalizer
       ...target,
       ...(source as any),
     };
+    this.normalizeFacetValues(target);
     if (source.products) {
       target.products = source.products.map(product =>
         this.converterService.convert(product, PRODUCT_NORMALIZER)
       );
     }
     return target;
+  }
+
+  /**
+   *
+   * In case there are so-called `topValues` given for the facet values,
+   * we replace the facet values by the topValues, simple because the
+   * values are obsolete.
+   *
+   * `topValues` is a feature in the adaptive search which can limit a large
+   * amount of facet values to a small set (5 by default). As long as the backend
+   * provides all facet values AND topValues, we normalize the data to not bother
+   * the UI with this specific feature.
+   */
+  private normalizeFacetValues(target: ProductSearchPage): void {
+    if (target.facets) {
+      target.facets.map((facet: Facet) => {
+        if ((<any>facet).topValues) {
+          facet.topValueCount = (<any>facet).topValues.length;
+          // no need to keep the array of topValues, as they duplicate the data.
+          delete (<any>facet).topValues;
+        } else {
+          facet.topValueCount = this.DEFAULT_TOP_VALUES;
+        }
+        return facet;
+      });
+    }
   }
 }
