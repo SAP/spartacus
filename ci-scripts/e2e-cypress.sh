04bd8c0f4d56f0b3d9aab2fa603d72bce33c95ab
Marcin Lasak <lasakmarcin90@gmail.com>
2020-02-20 14:32:48 +0100

    chore: Remove unnecessary builds from e2e script

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -46,7 +46,7 @@ yarn
 
 echo '-----'
 echo 'Building Spartacus libraries'
-yarn build:core:lib"${INTEGRATION}" && yarn build"${INTEGRATION}" 2>&1 | tee build.log
+yarn build:core:lib"${INTEGRATION}" 2>&1 | tee build.log
 
 results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
 if [[ -z "$results" ]]; then
