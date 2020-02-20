cb14c633886f61c039dd40f362225c4d98aaff38
Agrawal <archit.nitb@gmail.com>
2020-02-20 16:54:28 +0000

    up

--- projects/storefrontlib/src/shared/components/checktable/checktable.component.ts
+++ projects/storefrontlib/src/shared/components/checktable/checktable.component.ts
@@ -17,29 +17,11 @@ export class ChecktableComponent {
   @Input()
   tableData: Array<any>;
 
-  @Output() toggle = new EventEmitter<any[]>();
+  @Output() toggle = new EventEmitter<any>();
 
   selectedValues: Array<any>;
 
   onToggle(event, row) {
-    // console.log(event.target.value)
-    if (event.target.value = 'on') {
-      this.toggle.emit(row);
-    }
-    // console.log(row)
-    // const checkedOptions = this.tableData.filter(x => x.options);
-    // console.log(checkedOptions);
-    // this.selectedValues = checkedOptions.map(x => x.code);
-    // console.log(this.selectedValues);
-    // this.toggle.emit(checkedOptions);
+    this.toggle.emit({ value: event.target.value, row });
   }
-
-  // ngOnChanges() {
-  //   this.selectedValues.forEach(value => {
-  //     const element = this.tableData.find(x => x.value === value);
-  //     if (element) {
-  //       element.checked = true;
-  //     }
-  //    });
-  // }
 }
