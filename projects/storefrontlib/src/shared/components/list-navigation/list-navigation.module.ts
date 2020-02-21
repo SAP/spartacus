f518a3df056dcfe2f2e009be5e71afe455571003
Michał Gruca <pucek9@gmail.com>
2020-02-21 13:06:20 +0100

    up

--- projects/storefrontlib/src/shared/components/list-navigation/list-navigation.module.ts
+++ projects/storefrontlib/src/shared/components/list-navigation/list-navigation.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { PaginationComponent, PaginationModule } from './pagination/index';
 import { SortingComponent } from './sorting/sorting.component';
+import { SearchComponent } from './search/search.component';
+import { IconModule } from '../../../cms-components/misc/icon/icon.module';
 
 @NgModule({
-  imports: [CommonModule, NgSelectModule, FormsModule, PaginationModule],
-  declarations: [SortingComponent],
-  exports: [SortingComponent, PaginationComponent],
+  imports: [
+    CommonModule,
+    NgSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IconModule,
+    PaginationModule,
+  ],
+  declarations: [SortingComponent, SearchComponent],
+  exports: [SortingComponent, PaginationComponent, SearchComponent],
 })
 export class ListNavigationModule {}
