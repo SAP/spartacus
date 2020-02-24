ede51d28249009653e2d503814f70e89efd8e972
Marcin Lasak <lasakmarcin90@gmail.com>
2020-02-24 09:32:15 +0100

    refactor: Drop APP_INITIALIZER for PersistanceService

--- projects/core/src/state/state.module.ts
+++ projects/core/src/state/state.module.ts
@@ -1,14 +1,9 @@
-import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Config, provideConfig } from '../config/config.module';
 import { defaultStateConfig } from './config/default-state-config';
 import { StateConfig } from './config/state-config';
 import { stateMetaReducers } from './reducers/index';
 import { PersistanceService } from './services/persistance.service';
-
-export function persistanceFactory(persistanceService): any {
-  const result = () => persistanceService;
-  return result;
-}
 
 @NgModule({})
 export class StateModule {
@@ -17,14 +12,9 @@ export class StateModule {
       ngModule: StateModule,
       providers: [
         ...stateMetaReducers,
+        PersistanceService,
         provideConfig(defaultStateConfig),
         { provide: StateConfig, useExisting: Config },
-        {
-          provide: APP_INITIALIZER,
-          useFactory: persistanceFactory,
-          deps: [PersistanceService],
-          multi: true,
-        },
       ],
     };
   }
