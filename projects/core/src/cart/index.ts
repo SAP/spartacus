416c123f29e1ae05900b1d0325d1159843693a50
Marcin Lasak <lasakmarcin90@gmail.com>
2020-02-24 09:18:33 +0100

    refactor: Add barrel files

--- projects/core/src/cart/index.ts
+++ projects/core/src/cart/index.ts
@@ -1,6 +1,7 @@
 export * from './cart.module';
 export * from './connectors/index';
 export * from './facade/index';
+export * from './services/cart-persistance.service';
 export * from './store/cart-state';
 export * from './store/index';
 export * from './store/selectors/index';
--- projects/core/src/state/index.ts
+++ projects/core/src/state/index.ts
@@ -1,4 +1,5 @@
 export * from './config/default-state-config';
 export * from './config/state-config';
+export * from './services/persistance.service';
 export * from './state.module';
 export * from './utils/index';
