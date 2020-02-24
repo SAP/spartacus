deceecee9de7f2c1f54a0dda2484e2ec482db9ba
dunqan <dunqan@w9.pl>
2020-02-24 23:22:34 +0100

    ssr ng9

--- projects/storefrontapp-cds-integration/src/main.server.ts
+++ projects/storefrontapp-cds-integration/src/main.server.ts
@@ -1,13 +1,9 @@
 import { enableProdMode } from '@angular/core';
-import { ngExpressEngine as engine } from '@nguniversal/express-engine';
-import { NgExpressEngineDecorator } from '@spartacus/core';
 import { environment } from './environments/environment';
+
 if (environment.production) {
   enableProdMode();
 }
-
-export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 export { AppServerModule } from './app/app.server.module';
-export const ngExpressEngine = NgExpressEngineDecorator.get(engine);
 
 export { renderModule, renderModuleFactory } from '@angular/platform-server';
