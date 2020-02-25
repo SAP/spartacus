9666d5ba49ec5ba38d5a118115c8b4981817fb48
Miroslaw Grochowski <m.grochowski@sap.com>
2020-02-25 09:40:42 +0100

    ondition for message display added

--- projects/core/src/global-message/http-interceptors/handlers/bad-request.handler.ts
+++ projects/core/src/global-message/http-interceptors/handlers/bad-request.handler.ts
@@ -50,6 +50,7 @@ export class BadRequestHandler extends HttpErrorHandler {
             error.subjectType === 'cart' &&
             error.reason === 'notFound'
           ) {
+            console.log(error);
             errorMessage = { key: 'httpHandlers.cartNotFound' };
           } else if (error.type === 'ValidationError') {
             // build translation key in case of backend field validation error
@@ -61,10 +62,14 @@ export class BadRequestHandler extends HttpErrorHandler {
             // see https://jira.hybris.com/browse/CMSX-8516
             errorMessage = { raw: error.message || '' };
           }
-          this.globalMessageService.add(
-            errorMessage,
-            GlobalMessageType.MSG_TYPE_ERROR
-          );
+          // @todo: remove this condition once backend is improved, see:
+          // https://github.com/SAP/cloud-commerce-spartacus-storefront/issues/6679
+          if (error.type !== 'JaloObjectNoLongerValidError') {
+            this.globalMessageService.add(
+              errorMessage,
+              GlobalMessageType.MSG_TYPE_ERROR
+            );
+          }
         });
       }
     }
