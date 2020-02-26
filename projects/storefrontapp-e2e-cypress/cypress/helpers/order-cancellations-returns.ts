9af892c5b483e03bc9e2eb5d8594931b3936bcf6
qjimmy <quang-jimmy@hotmail.com>
2020-02-26 15:01:53 -0500

    Merge PR #6048 into develop (GH-4291)

---
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/order-cancellations-returns.ts
@@ -0,0 +1,71 @@
+import { waitForPage } from './checkout-flow';
+import { getSuccessAlert } from './global-message';
+
+export function checkTabs() {
+  cy.get('cx-tab-paragraph-container > h3')
+    .eq(1)
+    .contains('RETURNS (0)')
+    .click();
+
+  cy.get('cx-tab-paragraph-container > h3')
+    .first()
+    .contains('ALL ORDERS (2)')
+    .click();
+}
+
+export function cancelOrder() {
+  const orderHistoryPage = waitForPage('/my-account/order', 'orderHistoryPage');
+
+  cy.get('cx-order-history a.cx-order-history-value')
+    .contains('In Process')
+    .click({ force: true });
+
+  cy.wait(`@${orderHistoryPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  const cancelOrderPage = waitForPage(
+    '/my-account/order/cancel',
+    'cancelOrderPage'
+  );
+
+  cy.get('cx-order-details-actions button.btn-primary')
+    .contains('Cancel Items')
+    .click({ force: true });
+
+  cy.wait(`@${cancelOrderPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  cy.get('button.cx-action-link')
+    .contains('Set all quantities to maximum')
+    .click({ force: true });
+
+  cy.get('input.cx-counter-value').should('have.value', '1');
+
+  const cancelConfirmation = waitForPage(
+    '/my-account/order/cancel/confirmation',
+    'cancelConfirmation'
+  );
+
+  cy.get('button.btn-primary')
+    .contains('Continue')
+    .should('not.be.disabled')
+    .click({ force: true });
+
+  cy.wait(`@${cancelConfirmation}`)
+    .its('status')
+    .should('eq', 200);
+
+  cy.get('cx-cancel-order-confirmation button.btn-primary')
+    .contains('Submit Request')
+    .click({ force: true });
+
+  cy.wait(`@${orderHistoryPage}`)
+    .its('status')
+    .should('eq', 200);
+
+  getSuccessAlert().contains('Your cancellation request was submitted');
+
+  cy.url().should('contain', 'my-account/orders');
+}
