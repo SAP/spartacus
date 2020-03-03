4c341286d7c94fffd22f6d9140777f395763b388
Michał Gruca <pucek9@gmail.com>
2020-03-03 14:35:58 +0100

    unify config with b2b checkout

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -5,8 +5,5 @@
 
 export const environment = {
   production: false,
-  // occBaseUrl: 'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
-  // occBaseUrl: 'https://powertools.dev-com-4.accdemo.b2c.ydev.hybris.com:9002',
-  // occBaseUrl: 'https://powertools.dev-com-7.accdemo.b2c.ydev.hybris.com:9002',
-  occBaseUrl: 'https://dev-com-19.accdemo.b2c.ydev.hybris.com:9002',
+  channel: 'b2c',
 };
