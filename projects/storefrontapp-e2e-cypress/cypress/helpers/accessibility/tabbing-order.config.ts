3bf4c4a516a28dac8747eb0365c851b55fe958e6
Jerify02 <jerify.zhang@sap.com>
2020-03-05 15:34:52 +0800

    add tabbing conifg elements due to cart was updated.

--- projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/accessibility/tabbing-order.config.ts
@@ -193,6 +193,9 @@ export const tabbingOrderConfig: TabbingOrderConfig = {
       value: 'FUN Flash Single Use Camera, 27+12 pic',
       type: TabbingOrderTypes.LINK,
     },
+    {
+      type: TabbingOrderTypes.GENERIC_INPUT,
+    },
     { value: 'Move To Cart', type: TabbingOrderTypes.LINK },
     { value: 'Remove', type: TabbingOrderTypes.LINK },
   ],
