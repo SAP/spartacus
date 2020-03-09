1281e6b167c5c7bd91bdc8f84f90656654b29f12
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-03-09 12:27:12 -0400

    refactored to lessen time for each assertion

--- projects/storefrontapp-e2e-cypress/cypress/helpers/payment-methods.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/payment-methods.ts
@@ -1,23 +1,77 @@
 import { user } from '../sample-data/checkout-flow';
-import { waitForPage } from './checkout-flow';
+import {
+  addProductToCartViaAutoComplete,
+  addProductToCartViaSearchPage,
+  loginRegisteredUser,
+} from './cart';
+import { addPaymentMethod } from './checkout-as-persistent-user';
+import { visitHomePage, waitForPage } from './checkout-flow';
 import {
   fillPaymentDetails,
   fillShippingAddress,
   PaymentDetails,
 } from './checkout-forms';
 
-export const secondPayment: PaymentDetails = {
-  fullName: 'Bar Foo',
-  payment: {
-    card: 'Mastercard',
-    number: '5400540054005400',
-    expires: {
-      month: '12',
-      year: '2025',
+interface PaymentDetail {
+  accountHolderName: string;
+  cardNumber: number;
+  cardType: { code: string };
+  expiryMonth: string;
+  expiryYear: string;
+  defaultPayment: boolean;
+  saved: boolean;
+  billingAddress: {
+    firstName: string;
+    lastName: string;
+    titleCode: string;
+    line1: string;
+    line2?: string;
+    town: string;
+    postalCode: string;
+    country: { isocode: string };
+  };
+}
+
+const testPaymentDetail: PaymentDetail[] = [
+  {
+    accountHolderName: 'test user',
+    cardNumber: 4111111111111111,
+    cardType: { code: 'visa' },
+    expiryMonth: '01',
+    expiryYear: '2125',
+    defaultPayment: true,
+    saved: true,
+    billingAddress: {
+      firstName: 'test',
+      lastName: 'user',
+      titleCode: 'mr',
+      line1: '999 de Maisonneuve',
+      line2: '',
+      town: 'Montreal',
+      postalCode: 'H4B3L4',
+      country: { isocode: 'US' },
     },
-    cvv: '123',
   },
-};
+  {
+    accountHolderName: 'named user',
+    cardNumber: 1234123412341234,
+    cardType: { code: 'visa' },
+    expiryMonth: '03',
+    expiryYear: '2126',
+    defaultPayment: false,
+    saved: true,
+    billingAddress: {
+      firstName: 'named',
+      lastName: 'user',
+      titleCode: 'mr',
+      line1: '999 de Maisonneuve',
+      line2: '',
+      town: 'Montreal',
+      postalCode: 'H4B3L4',
+      country: { isocode: 'US' },
+    },
+  },
+];
 
 export function checkAnonymous() {
   it('should redirect anonymous user to login page', () => {
@@ -33,6 +87,13 @@ export function verifyText() {
       'contain',
       'New payment methods are added during checkout.'
     );
+  });
+}
+
+export function verifyPaymentCard(cardLength: number) {
+  cy.get('.cx-payment .cx-body').then(() => {
+    cy.get('cx-card').should('exist');
+    cy.get('cx-card').should('have.length', cardLength);
   });
 }
 
@@ -128,7 +189,7 @@ export function addSecondaryPaymentCard() {
     .getByText('Add New Payment')
     .click({ force: true });
 
-  fillPaymentDetails(secondPayment);
+  fillPaymentDetails(testPaymentDetail[1]);
 
   cy.wait(`@${waitAliases.reviewPage}`)
     .its('status')
@@ -148,8 +209,8 @@ export function setOtherPaymentToDefault() {
 
   const firstCard = cy.get('.cx-payment-card').first();
   firstCard.should('contain', 'Default Payment Method');
-  firstCard.should('contain', '1111');
-  firstCard.should('contain', `Expires: 7/2022`);
+  firstCard.should('contain', '1234');
+  firstCard.should('contain', `Expires: 03/2126`);
 }
 
 export function deletePayment() {
@@ -185,26 +246,88 @@ export function deletePayment() {
   defaultCard.should('contain', 'Winston Rumfoord');
 }
 
+export function visitPaymentDetailsPage(isMobile: boolean = false) {
+  const paymentDetailPage = waitForPage(
+    '/my-account/payment-detail',
+    'getPaymentDetail'
+  );
+  cy.selectUserMenuOption({
+    option: 'Payment Details',
+    isMobile,
+  });
+  cy.wait(`@${paymentDetailPage}`)
+    .its('status')
+    .should('eq', 200);
+}
+
 export function paymentMethodsTest() {
+  it('should visit payment details page', () => {
+    loginRegisteredUser();
+    visitPaymentDetailsPage();
+  });
+
   it('should render page with no payment methods', () => {
     verifyText();
   });
 
-  it('should render payment method', () => {
-    paymentDetailCard();
+  it('should add product to cart via search', () => {
+    addProductToCartViaAutoComplete(false);
   });
 
-  it('should add a second payment method', () => {
-    addSecondaryPaymentCard();
+  it('should create payment details using POST request', () => {
+    addPaymentMethod(testPaymentDetail[0]);
+  });
+
+  it('should render page with only one payment methods', () => {
+    visitHomePage();
+    visitPaymentDetailsPage();
+    verifyPaymentCard(1);
+  });
+
+  it('should add product a second product via search', () => {
+    addProductToCartViaSearchPage(false);
+  });
+
+  it('should add a second a payment details using POST request', () => {
+    addPaymentMethod(testPaymentDetail[1]);
+  });
+
+  it('should render page with only two payment methods', () => {
+    visitHomePage();
+    visitPaymentDetailsPage();
+    verifyPaymentCard(2);
   });
 
   it('should set additional payment method as default', () => {
     setOtherPaymentToDefault();
   });
 
-  it('should be able to delete payment method', () => {
-    deletePayment();
-  });
+  // it('should be able to delete payment method', () => {
+  //   deletePayment();
+  // });
+}
+
+function addPaymentMethod(paymentDetail: PaymentDetails) {
+  cy.get('.cx-total')
+    .first()
+    .then($cart => {
+      const cartid = $cart.text().match(/[0-9]+/)[0];
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env(
+          'API_URL'
+        )}/rest/v2/electronics-spa/users/current/carts/${cartid}/paymentdetails`,
+        headers: {
+          Authorization: `bearer ${
+            JSON.parse(localStorage.getItem('spartacus-local-data')).auth
+              .userToken.token.access_token
+          }`,
+        },
+        body: paymentDetail,
+      }).then(response => {
+        expect(response.status).to.eq(201);
+      });
+    });
 }
 
 function getWaitAliases() {
