20c3e8d16ead4dd28c6e02a9f2ad6f0448fcea56
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-10 21:20:01 +0100

    remove premature directives

--- projects/storefrontlib/src/layout/a11y/tab-index/tab-index.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ElementRef, QueryList, ViewChildren } from '@angular/core';
-
-@Directive({
-  selector: '[cxOutlet]',
-})
-export class TabindexDirective {
-  @ViewChildren('tabindex') values: QueryList<ElementRef<HTMLElement>>;
-}
--- projects/storefrontlib/src/layout/a11y/tab-index/tab-index.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EscGroupDirective } from './esc-group.directive';
-import { TabindexDirective } from './tab-index.directive';
-
-@NgModule({
-  declarations: [EscGroupDirective, TabindexDirective],
-  exports: [EscGroupDirective, TabindexDirective],
-})
-export class TabIndexModule {}
