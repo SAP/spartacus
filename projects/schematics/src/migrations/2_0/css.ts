01e64a6b6ec114c90077217743a54ae8b174dcc6
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-03-10 09:28:02 +0100

    cr changes

--- projects/schematics/src/migrations/2_0/css.ts
+++ projects/schematics/src/migrations/2_0/css.ts
@@ -1,7 +1,5 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
-const CSS_DOCS_URL =
-  'https://sap.github.io/cloud-commerce-spartacus-storefront-docs/deprecation-guide/css';
+import { CSS_DOCS_URL } from '../../shared/constants';
 
 export function migrate(): Rule {
   return (_tree: Tree, context: SchematicContext) => {
