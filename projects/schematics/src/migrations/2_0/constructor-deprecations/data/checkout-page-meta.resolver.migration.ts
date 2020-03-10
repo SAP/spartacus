848f4361e70115ca3f302b084e42be0146f3875a
Krzysztof Platis <platonn.git@gmail.com>
2020-03-10 17:16:23 +0100

    fix comma

--- projects/schematics/src/migrations/2_0/constructor-deprecations/data/checkout-page-meta.resolver.migration.ts
+++ projects/schematics/src/migrations/2_0/constructor-deprecations/data/checkout-page-meta.resolver.migration.ts
@@ -1,31 +1,36 @@
-import { ACTIVE_CART_SERVICE, CART_SERVICE, CHECKOUT_PAGE_META_RESOLVER, SPARTACUS_CORE, TRANSLATION_SERVICE } from '../../../../shared/constants';
+import {
+  ACTIVE_CART_SERVICE,
+  CART_SERVICE,
+  CHECKOUT_PAGE_META_RESOLVER,
+  SPARTACUS_CORE,
+  TRANSLATION_SERVICE,
+} from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
 export const CHECKOUT_PAGE_META_RESOLVER_MIGRATION: ConstructorDeprecation = {
   // projects/core/src/checkout/services/checkout-page-meta.resolver.ts
-    class: CHECKOUT_PAGE_META_RESOLVER,
-    importPath: SPARTACUS_CORE,
-    deprecatedParams: [
-      {
-        className: CART_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-      {
-        className: TRANSLATION_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-    ],
-    removeParams: [
-      {
-        className: CART_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-    ],
-    addParams: [
-      {
-        className: ACTIVE_CART_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-    ],
-  },
+  class: CHECKOUT_PAGE_META_RESOLVER,
+  importPath: SPARTACUS_CORE,
+  deprecatedParams: [
+    {
+      className: CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: TRANSLATION_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  removeParams: [
+    {
+      className: CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  addParams: [
+    {
+      className: ACTIVE_CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
 };
