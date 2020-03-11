58bf4dfc27e9666be8f522752532734cd1fc95d4
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-03-11 13:54:35 -0400

    removed 3 files that carried over

--- projects/schematics/src/migrations/2_0/component-deprecations/component-deprecations-data.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentData } from '../../../shared/utils/file-utils';
-import { CONSENT_MANAGEMENT_FORM_COMPONENT_MIGRATION } from './data/consent-management-form.component.migration';
-
-export const COMPONENT_DEPRECATION_DATA: ComponentData[] = [
-  CONSENT_MANAGEMENT_FORM_COMPONENT_MIGRATION,
-];
