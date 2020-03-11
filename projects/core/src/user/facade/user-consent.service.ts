173d2e17576ec87d6c282a8c89cfcc39179081f5
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-03-11 14:14:05 -0400

    refactor: Deprecated code since 1.3: ASM Customer Emulation Support (GH-5628)

--- projects/core/src/user/facade/user-consent.service.ts
+++ projects/core/src/user/facade/user-consent.service.ts
@@ -11,7 +11,6 @@ import {
 } from 'rxjs/operators';
 import { AuthService } from '../../auth/facade/auth.service';
 import { Consent, ConsentTemplate } from '../../model/consent.model';
-import { OCC_USER_ID_CURRENT } from '../../occ/index';
 import { StateWithProcess } from '../../process/store/process-state';
 import {
   getProcessErrorFactory,
@@ -31,22 +30,8 @@ import {
 })
 export class UserConsentService {
   constructor(
-    store: Store<StateWithUser | StateWithProcess<void>>,
-    // tslint:disable-next-line:unified-signatures
-    authService: AuthService
-  );
-
-  /**
-   * @deprecated since version 1.3
-   *  Use constructor(store: Store<StateWithUser | StateWithProcess<void>>,
-   *  authService: AuthService) instead
-   *
-   *  TODO(issue:#5628) Deprecated since 1.3.0
-   */
-  constructor(store: Store<StateWithUser | StateWithProcess<void>>);
-  constructor(
     protected store: Store<StateWithUser | StateWithProcess<void>>,
-    protected authService?: AuthService
+    protected authService: AuthService
   ) {}
 
   /**
@@ -291,19 +276,13 @@ export class UserConsentService {
     return updatedTemplateList;
   }
 
-  /**
-   * Utility method to distinquish pre / post 1.3.0 in a convenient way.
-   *
+  /*
+   * Utility method to distinquish user id  in a convenient way
    */
   private withUserId(callback: (userId: string) => void): void {
-    if (this.authService) {
-      this.authService
-        .getOccUserId()
-        .pipe(take(1))
-        .subscribe(userId => callback(userId));
-    } else {
-      // TODO(issue:#5628) Deprecated since 1.3.0
-      callback(OCC_USER_ID_CURRENT);
-    }
+    this.authService
+      .getOccUserId()
+      .pipe(take(1))
+      .subscribe(userId => callback(userId));
   }
 }
