433a04a132a77e4d11c5344b30c6cdd6777ef539
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-11 17:33:59 +0100

    clean up test

--- projects/storefrontlib/src/layout/a11y/keyboard-focus/lock/lock-focus.service.spec.ts
+++ projects/storefrontlib/src/layout/a11y/keyboard-focus/lock/lock-focus.service.spec.ts
@@ -1,22 +1,6 @@
-import { Component } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { SelectFocusUtility } from '../services';
 import { LockFocusService } from './lock-focus.service';
-
-@Component({
-  template: `
-    <div id="a"></div>
-    <div id="b">
-      <button id="b1"></button>
-      <button id="b2"></button>
-      <button id="b3" data-cx-focus="b3"></button>
-      <button id="b4"></button>
-      <button id="b5"></button>
-    </div>
-    <div id="c"></div>
-  `,
-})
-class MockComponent {}
 
 class MockSelectFocusUtility {
   findFirstFocusable() {}
@@ -30,7 +14,6 @@ describe('LockFocusService', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MockComponent],
       providers: [
         LockFocusService,
         {
