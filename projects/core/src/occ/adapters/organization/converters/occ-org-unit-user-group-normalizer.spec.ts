6835b350ed45f74925d6590d9fed89edb5133a28
Caine Rotherham <rotherham.c@gmail.com>
2020-03-12 14:25:40 +0100

    occ unit tests

---
+++ projects/core/src/occ/adapters/organization/converters/occ-org-unit-user-group-normalizer.spec.ts
@@ -0,0 +1,59 @@
+import { Type } from '@angular/core';
+import { inject, TestBed } from '@angular/core/testing';
+import { OrgUnitUserGroup } from '../../../../model/';
+import { OccConfig } from '../../../config/occ-config';
+import { Occ } from '../../../occ-models/occ.models';
+import { OccOrgUnitUserGroupNormalizer } from './occ-org-unit-user-group-normalizer';
+
+const MockOccModuleConfig: OccConfig = {
+  backend: {
+    occ: {
+      baseUrl: '',
+      prefix: '',
+    },
+  },
+};
+
+describe('OrgUnitUserGroupNormalizer', () => {
+  let service: OccOrgUnitUserGroupNormalizer;
+
+  const orgUnitUserGroup: Occ.OrgUnitUserGroup = {
+    name: 'OrgUnitUserGroup1',
+    uid: 'testUid',
+  };
+
+  const convertedOrgUnitUserGroup: OrgUnitUserGroup = {
+    name: 'OrgUnitUserGroup1',
+    uid: 'testUid',
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        OccOrgUnitUserGroupNormalizer,
+        { provide: OccConfig, useValue: MockOccModuleConfig },
+      ],
+    });
+
+    service = TestBed.inject(
+      OccOrgUnitUserGroupNormalizer as Type<OccOrgUnitUserGroupNormalizer>
+    );
+  });
+
+  it('should inject OccOrgUnitUserGroupNormalizer', inject(
+    [OccOrgUnitUserGroupNormalizer],
+    (orgUnitUserGroupNormalizer: OccOrgUnitUserGroupNormalizer) => {
+      expect(orgUnitUserGroupNormalizer).toBeTruthy();
+    }
+  ));
+
+  it('should convert orgUnitUserGroup', () => {
+    const result = service.convert(orgUnitUserGroup);
+    expect(result).toEqual(convertedOrgUnitUserGroup);
+  });
+
+  it('should convert costCenter with applied target', () => {
+    const result = service.convert(orgUnitUserGroup, {});
+    expect(result).toEqual({});
+  });
+});
