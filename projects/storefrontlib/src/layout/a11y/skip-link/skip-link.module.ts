f3727af090f3633d32e825cff101b63d7a5535c2
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-13 14:54:46 +0100

    fix import

--- projects/storefrontlib/src/layout/a11y/skip-link/skip-link.module.ts
+++ projects/storefrontlib/src/layout/a11y/skip-link/skip-link.module.ts
@@ -12,7 +12,7 @@ import {
 } from '@spartacus/core';
 import { OutletPosition } from '../../../cms-structure/outlet/outlet.model';
 import { OutletService } from '../../../cms-structure/outlet/outlet.service';
-import { KeyboardFocusModule } from '../keyboard-focus';
+import { KeyboardFocusModule } from '../keyboard-focus/keyboard-focus.module';
 import { SkipLinkComponent } from './component/skip-link.component';
 import { defaultSkipLinkConfig } from './config/default-skip-link.config';
 import { SkipLinkConfig } from './config/skip-link.config';
