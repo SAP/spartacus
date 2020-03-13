4150b74c89222afd33400cdf6a747a4c295a69f4
Miroslaw Grochowski <m.grochowski@sap.com>
2020-03-13 13:53:13 +0100

    prettier fix

--- projects/schematics/src/migrations/2_0/component-deprecations/data/product-images.component.migration.ts
+++ projects/schematics/src/migrations/2_0/component-deprecations/data/product-images.component.migration.ts
@@ -5,6 +5,9 @@ export const PRODUCT_IMAGES_COMPONENT_MIGRATION: ComponentData = {
   selector: 'cx-product-images',
   componentClassName: PRODUCT_IMAGES_COMPONENT,
   removedProperties: [
-    { name: 'isThumbsEmpty', comment: `'isThumbsEmpty' property has been removed.` },
+    {
+      name: 'isThumbsEmpty',
+      comment: `'isThumbsEmpty' property has been removed.`,
+    },
   ],
 };
