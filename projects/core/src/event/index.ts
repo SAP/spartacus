fcef1f227a590292aaeee157357809a405a8b186
Krzysztof Platis <platonn.git@gmail.com>
2020-03-16 11:26:01 +0100

    remove redundant event module

--- projects/core/src/event/index.ts
+++ projects/core/src/event/index.ts
@@ -1,3 +1,2 @@
 export * from './base-event.model';
-export { EventModule } from './event.module';
 export { EventService } from './event.service';
--- projects/core/src/state/state.module.ts
+++ projects/core/src/state/state.module.ts
@@ -4,7 +4,7 @@ import { defaultStateConfig } from './config/default-state-config';
 import { StateConfig } from './config/state-config';
 import { stateMetaReducers } from './reducers/index';
 
-@NgModule()
+@NgModule({})
 export class StateModule {
   static forRoot(): ModuleWithProviders<StateModule> {
     return {
