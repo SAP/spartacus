3ed66a7c2eb194c6386eb060e00b6f8e29cbec0a
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-03-16 14:58:55 -0400

    removed legacy methods

--- projects/core/src/occ/adapters/cart/occ-cart-entry.adapter.spec.ts
+++ projects/core/src/occ/adapters/cart/occ-cart-entry.adapter.spec.ts
@@ -4,7 +4,6 @@ import {
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { CART_MODIFICATION_NORMALIZER } from '@spartacus/core';
-import { FeatureConfigService } from 'projects/core/src/features-config';
 import { Cart, CartModification } from '../../../model/cart.model';
 import { ConverterService } from '../../../util/converter.service';
 import { OccEndpointsService } from '../../services';
@@ -29,22 +28,11 @@ class MockOccEndpointsService {
   }
 }
 
-class MockFeatureConfigService {
-  isLevel(_featureLevel: string): boolean {
-    return true;
-  }
-}
-
-// Deprecated in 1.1
-const usersEndpoint = 'users';
-const cartsEndpoint = '/carts/';
-
 describe('OccCartEntryAdapter', () => {
   let occCartEntryAdapter: OccCartEntryAdapter;
   let httpMock: HttpTestingController;
   let converterService: ConverterService;
   let occEnpointsService: OccEndpointsService;
-  let featureConfigService: FeatureConfigService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -52,7 +40,6 @@ describe('OccCartEntryAdapter', () => {
       providers: [
         OccCartEntryAdapter,
         { provide: OccEndpointsService, useClass: MockOccEndpointsService },
-        { provide: FeatureConfigService, useClass: MockFeatureConfigService },
       ],
     });
 
@@ -60,7 +47,6 @@ describe('OccCartEntryAdapter', () => {
     httpMock = TestBed.inject(HttpTestingController);
     converterService = TestBed.inject(ConverterService);
     occEnpointsService = TestBed.inject(OccEndpointsService);
-    featureConfigService = TestBed.inject(FeatureConfigService);
 
     spyOn(converterService, 'pipeable').and.callThrough();
     spyOn(occEnpointsService, 'getUrl').and.callThrough();
@@ -182,133 +168,6 @@ describe('OccCartEntryAdapter', () => {
       expect(mockReq.request.responseType).toEqual('json');
       mockReq.flush(cartData);
       expect(result).toEqual(cartData);
-    });
-  });
-
-  /**
-   * @deprecated Since 1.1
-   * Remove when legacy code is removed.
-   */
-  describe('legacy', () => {
-    beforeEach(() => {
-      spyOn(featureConfigService, 'isLevel').and.returnValue(false);
-    });
-
-    describe('add entry to cart', () => {
-      it('should add entry to cart for given user id, cart id, product code and product quantity', () => {
-        let result;
-        occCartEntryAdapter
-          .add(userId, cartId, '147852', 5)
-          .subscribe(res => (result = res));
-
-        const mockReq = httpMock.expectOne(req => {
-          return (
-            req.method === 'POST' &&
-            req.url ===
-              `${usersEndpoint}/${userId}${cartsEndpoint}${cartId}/entries`
-          );
-        });
-
-        expect(mockReq.request.headers.get('Content-Type')).toEqual(
-          'application/x-www-form-urlencoded'
-        );
-
-        expect(mockReq.request.params.get('code')).toEqual('147852');
-
-        expect(mockReq.request.params.get('qty')).toEqual('5');
-
-        expect(mockReq.cancelled).toBeFalsy();
-        expect(mockReq.request.responseType).toEqual('json');
-        mockReq.flush(cartModified);
-        expect(result).toEqual(cartModified);
-        expect(converterService.pipeable).toHaveBeenCalledWith(
-          CART_MODIFICATION_NORMALIZER
-        );
-      });
-    });
-
-    describe('update entry in a cart', () => {
-      it('should update an entry in a cart for given user id, cart id, entryNumber and quantitiy', () => {
-        let result;
-        occCartEntryAdapter
-          .update(userId, cartId, '12345', 5)
-          .subscribe(res => (result = res));
-
-        const mockReq = httpMock.expectOne(req => {
-          return (
-            req.method === 'PATCH' &&
-            req.url ===
-              usersEndpoint +
-                `/${userId}` +
-                cartsEndpoint +
-                cartId +
-                '/entries/12345'
-          );
-        });
-
-        expect(mockReq.request.headers.get('Content-Type')).toEqual(
-          'application/x-www-form-urlencoded'
-        );
-
-        expect(mockReq.request.params.get('qty')).toEqual('5');
-        expect(mockReq.cancelled).toBeFalsy();
-        expect(mockReq.request.responseType).toEqual('json');
-        mockReq.flush(cartModified);
-        expect(result).toEqual(cartModified);
-        expect(converterService.pipeable).toHaveBeenCalledWith(
-          CART_MODIFICATION_NORMALIZER
-        );
-      });
-
-      it(`should handle 'pickupStore'`, () => {
-        const pickupStore =
-          'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France';
-        occCartEntryAdapter
-          .update(userId, cartId, '12345', 5, pickupStore)
-          .subscribe()
-          .unsubscribe();
-
-        const mockReq = httpMock.expectOne(req => {
-          return (
-            req.method === 'PATCH' &&
-            req.url ===
-              usersEndpoint +
-                `/${userId}` +
-                cartsEndpoint +
-                cartId +
-                '/entries/12345'
-          );
-        });
-        expect(mockReq.request.params.get('qty')).toEqual('5');
-        expect(mockReq.request.params.get('pickupStore')).toEqual(pickupStore);
-      });
-    });
-
-    describe('remove an entry from cart', () => {
-      it('should remove entry from cart for given user id, cart id and entry number', () => {
-        let result;
-        occCartEntryAdapter
-          .remove(userId, cartId, '147852')
-          .subscribe(res => (result = res));
-
-        const mockReq = httpMock.expectOne(req => {
-          return (
-            req.method === 'DELETE' &&
-            req.url ===
-              usersEndpoint +
-                `/${userId}` +
-                cartsEndpoint +
-                cartId +
-                '/entries/' +
-                '147852'
-          );
-        });
-
-        expect(mockReq.cancelled).toBeFalsy();
-        expect(mockReq.request.responseType).toEqual('json');
-        mockReq.flush(cartData);
-        expect(result).toEqual(cartData);
-      });
     });
   });
 });
