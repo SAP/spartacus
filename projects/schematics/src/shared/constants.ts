74387f32946e2f1bf82d74292922379da478ad33
Marcin Lasak <lasakmarcin90@gmail.com>
2020-03-16 12:48:11 +0100

    refactor: Remove CartService (#6917)
    Closes #6832

--- projects/schematics/src/shared/constants.ts
+++ projects/schematics/src/shared/constants.ts
@@ -89,6 +89,10 @@ export const ORDER_DETAIL_ITEMS_COMPONENT = 'OrderDetailItemsComponent';
 export const ORDER_CONFIRMATION_ITEMS_COMPONENT =
   'OrderConfirmationItemsComponent';
 export const REVIEW_SUBMIT_COMPONENT = 'ReviewSubmitComponent';
+export const CART_COUPON_COMPONENT = 'CartCouponComponent';
+export const CART_VOUCHER_SERVICE = 'CartVoucherService';
+export const CUSTOMER_COUPON_SERVICE = 'CustomerCouponService';
+export const CART_ITEM_LIST_COMPONENT = 'CartItemListComponent';
 /***** Classes end *****/
 
 /***** Removed public api start *****/
