88b73e6dd62047261a9d873bb9c8fb4037f126aa
Krzysztof Platis <platonn.git@gmail.com>
2020-03-17 12:32:37 +0100

    register method of state event service returns a teardown

--- projects/core/src/state/event/state-event.service.spec.ts
+++ projects/core/src/state/event/state-event.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Action, ActionsSubject } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
+import { Subject } from 'rxjs';
 import { EventService } from '../../event/event.service';
 import { StateEventService } from './state-event.service';
 
@@ -15,27 +15,24 @@ interface ActionWithPayload extends Action {
   payload: any;
 }
 
-describe('StateEventService', () => {
-  let mockActionsSubject$: Observable<ActionWithPayload>;
-  const mockActions: ActionWithPayload[] = [
-    { type: 'A', payload: 1 },
-    { type: 'B', payload: 2 },
-    { type: 'C', payload: 3 },
-    { type: 'A', payload: 4 },
-  ];
-
+fdescribe('StateEventService', () => {
+  let mockActionsSubject$: Subject<ActionWithPayload>;
   let service: StateEventService;
   let eventService: EventService;
+  const mockTearDown = () => {};
 
   beforeEach(() => {
-    mockActionsSubject$ = of(...mockActions);
-
+    mockActionsSubject$ = new Subject();
     TestBed.configureTestingModule({
       providers: [
         { provide: ActionsSubject, useValue: mockActionsSubject$ },
         {
           provide: EventService,
-          useValue: { register: jasmine.createSpy('register') },
+          useValue: {
+            register: jasmine
+              .createSpy('register')
+              .and.returnValue(mockTearDown),
+          },
         },
       ],
     });
@@ -51,11 +48,15 @@ describe('StateEventService', () => {
           action: 'A',
           event: EventA,
         });
-
         const registeredSource$ = eventService.register['calls'].argsFor(0)[1];
         const results = [];
         registeredSource$.subscribe(e => results.push(e));
-        expect(results).toEqual([new EventA(1), new EventA(4)]);
+
+        mockActionsSubject$.next({ type: 'A', payload: 1 });
+        mockActionsSubject$.next({ type: 'B', payload: 2 });
+        mockActionsSubject$.next({ type: 'A', payload: 3 });
+
+        expect(results).toEqual([new EventA(1), new EventA(3)]);
         expect(eventService.register).toHaveBeenCalledWith(
           EventA,
           jasmine.any(Object)
@@ -70,14 +71,24 @@ describe('StateEventService', () => {
         factory: (action: ActionWithPayload) =>
           new EventA(100 + action.payload),
       });
-
       const registeredSource$ = eventService.register['calls'].argsFor(0)[1];
       const results = [];
       registeredSource$.subscribe(e => results.push(e));
-      expect(results).toEqual([new EventA(101), new EventA(104)]);
+
+      mockActionsSubject$.next({ type: 'A', payload: 1 });
+      mockActionsSubject$.next({ type: 'B', payload: 2 });
+      mockActionsSubject$.next({ type: 'A', payload: 3 });
+
+      expect(results).toEqual([new EventA(101), new EventA(103)]);
       expect(eventService.register).toHaveBeenCalledWith(
         EventA,
         jasmine.any(Object)
+      );
+    });
+
+    it('that can be unregistered with a returned teardown function mapped implicitly from action payload', () => {
+      expect(service.register({ action: 'A', event: EventA })).toBe(
+        mockTearDown
       );
     });
   });
