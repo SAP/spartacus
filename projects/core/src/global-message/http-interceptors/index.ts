8eaddc2cdce3b125343b4d2326ebe840b9b0aa15
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-17 14:04:18 +0100

    Add unauthorized error handler

--- projects/core/src/global-message/http-interceptors/index.ts
+++ projects/core/src/global-message/http-interceptors/index.ts
@@ -1,5 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Provider } from '@angular/core';
+import { UnauthorizedErrorHandler } from './handlers';
 import { BadGatewayHandler } from './handlers/bad-gateway.handler';
 import { BadRequestHandler } from './handlers/bad-request.handler';
 import { ConflictHandler } from './handlers/conflict.handler';
@@ -50,6 +51,11 @@ export const errorHandlers: Provider[] = [
   {
     provide: HttpErrorHandler,
     useExisting: NotFoundHandler,
+    multi: true,
+  },
+  {
+    provide: HttpErrorHandler,
+    useExisting: UnauthorizedErrorHandler,
     multi: true,
   },
 ];
