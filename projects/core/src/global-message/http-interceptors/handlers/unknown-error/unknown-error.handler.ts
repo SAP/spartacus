e3f16cd3fa6040fdb28f1c84fa9c0601a32cc13a
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-03-18 08:15:52 +0100

    feat: Add unauthorized error handler (#6938)
    Introduced UnauthorizedErrorHandler for 401 errors.
    
    closes #6936

--- projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
+++ projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
@@ -1,15 +1,11 @@
 import { Injectable, isDevMode } from '@angular/core';
-import { GlobalMessageService } from '../../facade/global-message.service';
-import { HttpResponseStatus } from '../../models/response-status.model';
-import { HttpErrorHandler } from './http-error.handler';
+import { HttpResponseStatus } from '../../../models/response-status.model';
+import { HttpErrorHandler } from '../http-error.handler';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UnknownErrorHandler extends HttpErrorHandler {
-  constructor(protected globalMessageService: GlobalMessageService) {
-    super(globalMessageService);
-  }
   responseStatus = HttpResponseStatus.UNKNOWN;
 
   handleError() {
