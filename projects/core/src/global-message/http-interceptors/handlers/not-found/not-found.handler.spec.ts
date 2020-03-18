06aa5e859fc17bc6f45f3b982771dd0cf1b68b20
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-18 07:41:25 +0100

    finalize tests with 2 missing tests

--- projects/core/src/global-message/http-interceptors/handlers/not-found/not-found.handler.spec.ts
+++ projects/core/src/global-message/http-interceptors/handlers/not-found/not-found.handler.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { GlobalMessageService } from '../../../facade/global-message.service';
+import { HttpResponseStatus } from '../../../models/response-status.model';
 import { NotFoundHandler } from './not-found.handler';
 
 class MockGlobalMessageService {}
@@ -22,5 +23,9 @@ describe('NotFoundHandler', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should register 404 responseStatus ', () => {
+    expect(service.responseStatus).toEqual(HttpResponseStatus.NOT_FOUND);
   });
 });
--- projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.spec.ts
+++ projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { GlobalMessageService } from '../../../facade';
+import { HttpResponseStatus } from '../../../models/response-status.model';
 import { UnknownErrorHandler } from './unknown-error.handler';
 
 class MockGlobalMessageService {}
@@ -21,5 +22,9 @@ describe('UnknownErrorHandler', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should register -1 responseStatus ', () => {
+    expect(service.responseStatus).toEqual(HttpResponseStatus.UNKNOWN);
   });
 });
