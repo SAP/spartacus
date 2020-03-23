32c0fcf4bad1001eca261205b83c0ceaf799c1a0
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-03-23 07:42:50 +0100

    Filter focusalbe child elements by it's visibility

--- projects/storefrontlib/src/layout/a11y/keyboard-focus/services/select-focus.util.ts
+++ projects/storefrontlib/src/layout/a11y/keyboard-focus/services/select-focus.util.ts
@@ -34,23 +34,34 @@ export class SelectFocusUtility {
     );
   }
 
+  /**
+   * Returns the first visible focusable element of the host element. The focusable
+   * element is queried based on the given `AutoFocusConfig`:
+   * - a specific autofocus _selector_ is given (i.e. `{autofocus: 'button[submit]'}`)
+   * - a focusable element that uses the native _autofocus_ attribute
+   * - the first focusuable child element
+   *
+   * The visibility of the focusable child elements is only restricted by `display:none` of
+   * the element or an of it's parents.
+   */
   findFirstFocusable(
     host: HTMLElement,
     config: AutoFocusConfig = { autofocus: true }
   ): HTMLElement {
     const selector =
       typeof config?.autofocus === 'string' ? config.autofocus : '[autofocus]';
-
-    // fallback to first focusable
     return (
-      this.query(host, selector).find(Boolean) ||
-      this.findFocusable(host).find(Boolean)
+      this.query(host, selector).find(el => !this.isHidden(el)) ||
+      this.findFocusable(host).find(el => Boolean(el))
     );
   }
 
   /**
-   * returns all focusable child elements of the host element. The element selectors
+   * Returns all visible focusable child elements of the host element. The element selectors
    * are build from the `focusableSelectors`.
+   *
+   * The visibility of the focusable child elements is only restricted by `display:none` of
+   * the element or an of it's parents.
    *
    * @param host the `HTMLElement` used to query focusable elements
    * @param locked indicates whether inactive (`tabindex="-1"`) focusable elements should be returend as well
@@ -61,6 +72,20 @@ export class SelectFocusUtility {
       suffix += `:not([tabindex='-1'])`;
     }
     const selector = this.focusableSelectors.map(s => (s += suffix)).join(',');
-    return this.query(host, selector);
+    return this.query(host, selector).filter(el => !this.isHidden(el));
+  }
+
+  /**
+   * Indicates whether the element is hidden by CSS. There are various CSS rules and
+   * HTML structures which can lead to an hidden or invisible element. An `offsetParent`
+   * of null indicates that the element or any of it's decendants is hidden (`display:none`).
+   *
+   * Oother techniques use the visibility (`visibility: hidden`), opacity (`opacity`) or
+   * phyisical location on the element itself or any of it's anchestor elements. Those
+   * technique require to work with the _computed styles_, which will cause a performance
+   * downgrade. We don't do this in the standard implementaton.
+   */
+  protected isHidden(el: HTMLElement): boolean {
+    return el.offsetParent === null;
   }
 }
