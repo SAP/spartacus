f5fbbf11c02eee6275833ef030aede4707da7619
Michał Gruca <pucek9@gmail.com>
2020-03-24 13:16:57 +0100

    up

--- projects/core/src/organization/store/reducers/b2b-user.reducer.ts
+++ projects/core/src/organization/store/reducers/b2b-user.reducer.ts
@@ -15,14 +15,12 @@ export function b2bUserEntitiesReducer(
     case OrgUnitActions.ASSIGN_ROLE_SUCCESS:
       return {
         ...state,
-        uid: action.payload.uid,
         selected: action.payload.selected,
         roles: [...state.roles, action.payload.roleId],
       };
     case OrgUnitActions.UNASSIGN_ROLE_SUCCESS:
       return {
         ...state,
-        uid: action.payload.uid,
         selected: action.payload.selected,
         roles: [...state.roles].filter(role => role !== action.payload.roleId),
       };
