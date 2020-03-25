56b9f792d13595c5cb52083be4dcee29c2fd335d
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-03-25 12:45:52 -0400

    delay and timeout functionality

--- projects/storefrontapp-e2e-cypress/cypress/support/utils/order-placed.ts
+++ projects/storefrontapp-e2e-cypress/cypress/support/utils/order-placed.ts
@@ -1,3 +1,11 @@
+const delay = 3000;
+
+// 2 mins in milliseconds
+const timerTimeout = 10000;
+
+// start time
+let startTime = 0;
+
 export function waitForOrderToBePlacedRequest(
   orderNumber: string,
   contentCatalog: string = 'electronics-spa'
@@ -13,16 +21,20 @@ export function waitForOrderToBePlacedRequest(
           .token.access_token
       }`,
     },
-  }).then(res => {
-    if (
-      res.status === 200 &&
-      res.body.orders &&
-      res.body.orders.length &&
-      res.body.orders.filter(order => order.code === orderNumber)
-    ) {
-      return;
-    } else {
-      waitForOrderToBePlacedRequest(orderNumber, contentCatalog);
-    }
-  });
+  })
+    .then(res => new Promise(resolve => setTimeout(_ => resolve(res), delay)))
+    .then((res: Cypress.Response) => {
+      if (
+        startTime > timerTimeout ||
+        (res.status === 200 &&
+          res.body.orders &&
+          res.body.orders.length &&
+          res.body.orders.filter(order => order.code === orderNumber))
+      ) {
+        return;
+      } else {
+        startTime += delay;
+        waitForOrderToBePlacedRequest(orderNumber, contentCatalog);
+      }
+    });
 }
