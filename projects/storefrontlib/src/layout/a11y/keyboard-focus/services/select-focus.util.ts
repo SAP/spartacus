0c475dd505b39baf6a7f4b8464da67002bdbb025
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-03-25 12:19:06 +0100

    Filter focusable child elements by it's visibility (#7001)
    Visibility of focusable elements, missing feature of #6866

--- projects/storefrontlib/src/layout/a11y/keyboard-focus/services/select-focus.util.ts
+++ projects/storefrontlib/src/layout/a11y/keyboard-focus/services/select-focus.util.ts
@@ -40,11 +40,10 @@ export class SelectFocusUtility {
   ): HTMLElement {
     const selector =
       typeof config?.autofocus === 'string' ? config.autofocus : '[autofocus]';
-
     // fallback to first focusable
     return (
-      this.query(host, selector).find(Boolean) ||
-      this.findFocusable(host).find(Boolean)
+      this.query(host, selector).find(el => !this.isHidden(el)) ||
+      this.findFocusable(host).find(el => Boolean(el))
     );
   }
 
@@ -53,14 +52,35 @@ export class SelectFocusUtility {
    * are build from the `focusableSelectors`.
    *
    * @param host the `HTMLElement` used to query focusable elements
-   * @param locked indicates whether inactive (`tabindex="-1"`) focusable elements should be returend as well
+   * @param locked indicates whether inactive (`tabindex="-1"`) focusable elements should be returned
+   * @param invisible indicates whether hidden focusable elements should be returned
    */
-  findFocusable(host: HTMLElement, locked = false): HTMLElement[] {
+  findFocusable(
+    host: HTMLElement,
+    locked = false,
+    invisible = false
+  ): HTMLElement[] {
     let suffix = this.focusableSelectorSuffix;
     if (!locked) {
       suffix += `:not([tabindex='-1'])`;
     }
     const selector = this.focusableSelectors.map(s => (s += suffix)).join(',');
-    return this.query(host, selector);
+    return this.query(host, selector).filter(el =>
+      !invisible ? !this.isHidden(el) : Boolean(el)
+    );
+  }
+
+  /**
+   * Indicates whether the element is hidden by CSS. There are various CSS rules and
+   * HTML structures which can lead to an hidden or invisible element. An `offsetParent`
+   * of null indicates that the element or any of it's decendants is hidden (`display:none`).
+   *
+   * Oother techniques use the visibility (`visibility: hidden`), opacity (`opacity`) or
+   * phyisical location on the element itself or any of it's anchestor elements. Those
+   * technique require to work with the _computed styles_, which will cause a performance
+   * downgrade. We don't do this in the standard implementaton.
+   */
+  protected isHidden(el: HTMLElement): boolean {
+    return el.offsetParent === null;
   }
 }
