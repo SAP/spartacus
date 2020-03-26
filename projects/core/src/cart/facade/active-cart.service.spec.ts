03282aa75348187f82a47ee1705d55ab54e0a972
Marcin Lasak <lasakmarcin90@gmail.com>
2020-03-26 08:19:36 +0100

    refactor: Rename ActiveCartService `getLoaded` to `isStable`

--- projects/core/src/cart/facade/active-cart.service.spec.ts
+++ projects/core/src/cart/facade/active-cart.service.spec.ts
@@ -211,12 +211,12 @@ describe('ActiveCartService', () => {
     });
   });
 
-  describe('getLoaded', () => {
+  describe('isStable', () => {
     it('should return true when isStable returns true', done => {
       spyOn(multiCartService, 'isStable').and.returnValue(of(true));
 
       service
-        .getLoaded()
+        .isStable()
         .pipe(take(1))
         .subscribe(val => {
           expect(val).toBe(true);
@@ -228,7 +228,7 @@ describe('ActiveCartService', () => {
       spyOn(multiCartService, 'isStable').and.returnValue(of(false));
 
       service
-        .getLoaded()
+        .isStable()
         .pipe(take(1))
         .subscribe(val => {
           expect(val).toBe(false);
