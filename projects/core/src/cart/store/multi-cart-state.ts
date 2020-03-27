100fb7eab6750b1b4f0cf5365d6ae9ae8fc9b4f0
Marcin Lasak <lasakmarcin90@gmail.com>
2020-03-27 09:59:47 +0100

    refactor: Change store feature name from carts to cart

--- projects/core/src/cart/store/multi-cart-state.ts
+++ projects/core/src/cart/store/multi-cart-state.ts
@@ -1,7 +1,7 @@
 import { Cart } from '../../model/cart.model';
 import { EntityProcessesLoaderState } from '../../state/utils/entity-processes-loader/entity-processes-loader-state';
 
-export const MULTI_CART_FEATURE = 'carts';
+export const MULTI_CART_FEATURE = 'cart';
 export const MULTI_CART_DATA = '[Multi Cart] Multi Cart Data';
 export const ADD_VOUCHER_PROCESS_ID = 'addVoucher';
 
