e138e51dcc4c458415d7cb1100dc242a4fb97551
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2020-03-27 17:00:09 +0100

    Add new effects

--- projects/core/src/organization/store/effects/b2b-user.effect.ts
+++ projects/core/src/organization/store/effects/b2b-user.effect.ts
@@ -4,10 +4,11 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { EntitiesModel } from '../../../model/misc.model';
 import { makeErrorSerializable } from '../../../util/serialization-utils';
-import { B2BUserActions } from '../actions/index';
+import { B2BUserActions, PermissionActions } from '../actions/index';
 import { normalizeListPage } from '../../utils/serializer';
 import { B2BUser } from '../../../model/org-unit.model';
 import { B2BUserConnector } from '../../connectors/b2b-user/b2b-user.connector';
+import { Permission } from 'projects/core/src/model';
 
 @Injectable()
 export class B2BUserEffects {
@@ -63,6 +64,106 @@ export class B2BUserEffects {
           )
         )
       )
+    )
+  );
+
+  @Effect()
+  loadB2BUserApprovers$: Observable<
+    | B2BUserActions.LoadB2BUserApproversSuccess
+    | B2BUserActions.LoadB2BUserApproversFail
+    | B2BUserActions.LoadB2BUserSuccess
+  > = this.actions$.pipe(
+    ofType(
+      B2BUserActions.LOAD_B2B_USER_APPROVERS
+    ),
+    map(
+      (
+        action: B2BUserActions.LoadB2BUserApprovers
+      ) => action.payload
+    ),
+    switchMap(payload =>
+      this.b2bUserConnector
+        .getApprovers(
+          payload.userId,
+          payload.orgCustomerId,
+          payload.params
+        )
+        .pipe(
+          switchMap((approvers: EntitiesModel<B2BUser>) => {
+            const { values, page } = normalizeListPage(approvers, 'code');
+            return [
+              new B2BUserActions.LoadB2BUserSuccess(values),
+              new B2BUserActions.LoadB2BUserApproversSuccess(
+                {
+                  orgCustomerId: payload.orgCustomerId,
+                  page,
+                  params: payload.params,
+                }
+              ),
+            ];
+          }),
+          catchError(error =>
+            of(
+              new B2BUserActions.LoadB2BUserApproversFail(
+                {
+                  orgCustomerId: payload.orgCustomerId,
+                  params: payload.params,
+                  error: makeErrorSerializable(error),
+                }
+              )
+            )
+          )
+        )
+    )
+  );
+
+  @Effect()
+  loadB2BUserPermissions$: Observable<
+    | B2BUserActions.LoadB2BUserApproversSuccess
+    | B2BUserActions.LoadB2BUserApproversFail
+    | PermissionActions.LoadPermissionSuccess
+  > = this.actions$.pipe(
+    ofType(
+      B2BUserActions.LOAD_B2B_USER_PERMISSIONS
+    ),
+    map(
+      (
+        action: B2BUserActions.LoadB2BUserPermissions
+      ) => action.payload
+    ),
+    switchMap(payload =>
+      this.b2bUserConnector
+        .getPermissions(
+          payload.userId,
+          payload.orgCustomerId,
+          payload.params
+        )
+        .pipe(
+          switchMap((permissions: EntitiesModel<Permission>) => {
+            const { values, page } = normalizeListPage(permissions, 'code');
+            return [
+              new PermissionActions.LoadPermissionSuccess(values),
+              new B2BUserActions.LoadB2BUserApproversSuccess(
+                {
+                  orgCustomerId: payload.orgCustomerId,
+                  page,
+                  params: payload.params,
+                }
+              ),
+            ];
+          }),
+          catchError(error =>
+            of(
+              new B2BUserActions.LoadB2BUserApproversFail(
+                {
+                  orgCustomerId: payload.orgCustomerId,
+                  params: payload.params,
+                  error: makeErrorSerializable(error),
+                }
+              )
+            )
+          )
+        )
     )
   );
 
