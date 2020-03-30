41bb61419173df8b6dcf9a3ca4c5ca9f2136f6f2
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2020-03-30 12:55:07 +0200

    Add missing methods to the b2b user connector

--- projects/core/src/organization/connectors/b2b-user/b2b-user.connector.ts
+++ projects/core/src/organization/connectors/b2b-user/b2b-user.connector.ts
@@ -5,7 +5,7 @@ import { EntitiesModel } from '../../../model/misc.model';
 import { B2BUser } from '../../../model/org-unit.model';
 import { B2BUserAdapter } from './b2b-user.adapter';
 import { Permission } from '../../../model/permission.model';
-import { OrgUnitUserGroup } from '../../../model/user-group.model';
+import { OrgUnitUserGroup } from '../../../model/u';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,21 @@ export class B2BUserConnector {
 
   get(userId: string, orgUnitCustomerId: string): Observable<B2BUser> {
     return this.adapter.load(userId, orgUnitCustomerId);
+  }
+
+  create(
+    userId: string,
+    orgCustomer: B2BUser
+  ): Observable<B2BUser> {
+    return this.adapter.create(userId, orgCustomer);
+  }
+
+  update(
+    userId: string,
+    orgCustomerId: string,
+    orgCustomer: B2BUser
+  ): Observable<B2BUser> {
+    return this.adapter.update(userId, orgCustomerId, orgCustomer);
   }
 
   getList(
@@ -36,6 +51,30 @@ export class B2BUserConnector {
     );
   }
 
+  assignApprover(
+    userId: string,
+    orgCustomerId: string,
+    approverId: string,
+  ): Observable<any> {
+    return this.adapter.assignApprover(
+      userId,
+      orgCustomerId,
+      approverId,
+    );
+  }
+
+  unassignApprover(
+    userId: string,
+    orgCustomerId: string,
+    approverId: string,
+  ): Observable<any> {
+    return this.adapter.unassignApprover(
+      userId,
+      orgCustomerId,
+      approverId,
+    );
+  }
+
   getPermissions(
     userId: string,
     orgUnitCustomerId: string,
@@ -48,6 +87,30 @@ export class B2BUserConnector {
     );
   }
 
+  assignPermission(
+    userId: string,
+    orgCustomerId: string,
+    permissionId: string,
+  ): Observable<any> {
+    return this.adapter.assignPermission(
+      userId,
+      orgCustomerId,
+      permissionId,
+    );
+  }
+
+  unassignPermission(
+    userId: string,
+    orgCustomerId: string,
+    permissionId: string,
+  ): Observable<any> {
+    return this.adapter.unassignPermission(
+      userId,
+      orgCustomerId,
+      permissionId,
+    );
+  }
+
   getUserGroups(
     userId: string,
     orgUnitCustomerId: string,
@@ -57,6 +120,30 @@ export class B2BUserConnector {
       userId,
       orgUnitCustomerId,
       params
+    );
+  }
+
+  assignUserGroup(
+    userId: string,
+    orgCustomerId: string,
+    userGroupId: string,
+  ): Observable<any> {
+    return this.adapter.assignUserGroup(
+      userId,
+      orgCustomerId,
+      userGroupId,
+    );
+  }
+
+  unassignUserGroup(
+    userId: string,
+    orgCustomerId: string,
+    userGroupId: string,
+  ): Observable<any> {
+    return this.adapter.unassignUserGroup(
+      userId,
+      orgCustomerId,
+      userGroupId,
     );
   }
 }
