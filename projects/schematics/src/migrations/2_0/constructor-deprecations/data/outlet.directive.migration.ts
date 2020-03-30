8cb019ea6580af26d72b69f398517b93645ec5f0
Miroslaw Grochowski <m.grochowski@sap.com>
2020-03-30 19:28:39 +0200

    prettier fix

--- projects/schematics/src/migrations/2_0/constructor-deprecations/data/outlet.directive.migration.ts
+++ projects/schematics/src/migrations/2_0/constructor-deprecations/data/outlet.directive.migration.ts
@@ -1,36 +1,36 @@
 import {
-    SPARTACUS_STOREFRONTLIB,
-    OUTLET_DIRECTIVE,
-    VIEW_CONTAINER_REF,
-    SPARTACUS_CORE,
-    TEMPLATE_REF,
-    OUTLET_SERVICE,
-    DEFER_LOADER_SERVICE,
-  } from '../../../../shared/constants';
-  import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
-  
-  export const OUTLET_DIRECTIVE_MIGRATION: ConstructorDeprecation = {
-    // projects/storefrontlib/src/cms-structure/outlet/outlet.directive.ts
-    class: OUTLET_DIRECTIVE,
-    importPath: SPARTACUS_STOREFRONTLIB,
-    deprecatedParams: [
-      {
-        className: VIEW_CONTAINER_REF,
-        importPath: SPARTACUS_CORE,
-      },
-      {
-        className: TEMPLATE_REF,
-        importPath: SPARTACUS_CORE,
-      },
-      {
-        className: OUTLET_SERVICE,
-        importPath: SPARTACUS_STOREFRONTLIB,
-      },
-    ],
-    addParams: [
-      {
-        className: DEFER_LOADER_SERVICE,
-        importPath: SPARTACUS_STOREFRONTLIB,
-      },
-    ],
-  };
+  SPARTACUS_STOREFRONTLIB,
+  OUTLET_DIRECTIVE,
+  VIEW_CONTAINER_REF,
+  SPARTACUS_CORE,
+  TEMPLATE_REF,
+  OUTLET_SERVICE,
+  DEFER_LOADER_SERVICE,
+} from '../../../../shared/constants';
+import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
+
+export const OUTLET_DIRECTIVE_MIGRATION: ConstructorDeprecation = {
+  // projects/storefrontlib/src/cms-structure/outlet/outlet.directive.ts
+  class: OUTLET_DIRECTIVE,
+  importPath: SPARTACUS_STOREFRONTLIB,
+  deprecatedParams: [
+    {
+      className: VIEW_CONTAINER_REF,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: TEMPLATE_REF,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: OUTLET_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+  ],
+  addParams: [
+    {
+      className: DEFER_LOADER_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+  ],
+};
