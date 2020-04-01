8bb45a275d203d21640b6749e7db3bb60aa08c49
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-04-01 06:55:09 +0200

    code readibility and documentation

--- projects/storefrontlib/src/cms-components/user/logout/logout-guard.ts
+++ projects/storefrontlib/src/cms-components/user/logout/logout-guard.ts
@@ -11,6 +11,13 @@ import {
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+/**
+ * Guards the _logout_ route.
+ *
+ * Takes care of routing the user to a logout page (if available) or redirects to
+ * the homepage. If the homepage is protected, the user is redirected
+ * to the login route instead.
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -40,15 +47,23 @@ export class LogoutGuard implements CanActivate {
       );
   }
 
+  /**
+   * Whenever there is no specific "logout" page configured in the CMS,
+   * we redirect after the user is logged out.
+   *
+   * The user gets redirected to the homepage, unless the homepage is protected
+   * (in case of a closed shop). We'll redirect to the login page instead.
+   */
   protected redirect(): void {
-    const cxRoute: string =
-      this.protectedRoutes && this.protectedRoutes.shouldProtect
-        ? 'login'
-        : 'home';
-
+    const cxRoute = this.protectedRoutes.shouldProtect ? 'login' : 'home';
     this.routing.go({ cxRoute });
   }
 
+  /**
+   * Log user out.
+   *
+   * This is delegated to the `AuthService`.
+   */
   protected logout(): void {
     this.auth.logout();
   }
