70f4449f0c7d72633a529bb5971a95c49b18c72d
Louis Pierrestiger <louis.pierrestiger@sap.com>
2020-04-01 16:28:06 -0400

    feat: trigger mechanism to launch components (#6848)
    Closes #6778

---
+++ projects/storefrontlib/src/layout/launch-dialog/services/routing-render-strategy.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { RoutingService } from '@spartacus/core';
+import { LaunchRoute, LAUNCH_CALLER } from '../config/index';
+import { LaunchRenderStrategy } from './launch-render.strategy';
+
+@Injectable({ providedIn: 'root' })
+export class RoutingRenderStrategy extends LaunchRenderStrategy {
+  constructor(protected routingService: RoutingService) {
+    super();
+  }
+  /**
+   * Navigates to the route configured for the caller
+   */
+  render(config: LaunchRoute, _caller: LAUNCH_CALLER) {
+    this.routingService.go(config);
+  }
+
+  match(config: LaunchRoute) {
+    return Boolean(config.cxRoute);
+  }
+}
