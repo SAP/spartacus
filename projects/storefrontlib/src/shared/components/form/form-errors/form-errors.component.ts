cbfdeedf4dae73f509028db1f5fc6a1edb9a1937
Kacper Knapik <hello@kacperknapik.pl>
2020-04-02 17:41:27 +0200

    fix: improve form errors extensibility

--- projects/storefrontlib/src/shared/components/form/form-errors/form-errors.component.ts
+++ projects/storefrontlib/src/shared/components/form/form-errors/form-errors.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  HostBinding,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
@@ -12,10 +17,13 @@ import { startWith, map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormErrorsComponent {
+  _control: FormControl;
   errors$: Observable<string[]>;
 
   @Input()
   set control(control: FormControl) {
+    this._control = control;
+
     this.errors$ = control?.statusChanges.pipe(
       startWith({}),
       map(() => control.errors || {}),
@@ -25,5 +33,19 @@ export class FormErrorsComponent {
           .map((error) => error[0])
       )
     );
+  }
+
+  get control(): FormControl {
+    return this._control;
+  }
+
+  @HostBinding('class.control-invalid') get invalid() {
+    return this.control.invalid;
+  }
+  @HostBinding('class.control-dirty') get dirty() {
+    return this.control.dirty;
+  }
+  @HostBinding('class.control-touched') get touched() {
+    return this.control.touched;
   }
 }
