285fd3c1c3442330650a6094e1e71eb8597df8d2
dunqan <dunqan@w9.pl>
2020-04-06 14:37:39 +0200

    finalize component-launcher.service.ts

--- projects/storefrontlib/src/cms-structure/page/component/launchers/component-launcher.service.ts
+++ projects/storefrontlib/src/cms-structure/page/component/launchers/component-launcher.service.ts
@@ -1,19 +1,11 @@
 import { ComponentRef, ElementRef, Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 
-export interface LauncherResult {
-  componentRef?: ComponentRef<any>;
-  elementRef?: ElementRef;
-  dispose?: () => void;
-}
-
 @Injectable()
 export abstract class ComponentLauncherService {
-
   abstract getLauncher(
     componentType: string,
     uid: string,
     directiveInjector: Injector
   ): Observable<[ElementRef, ComponentRef<any>?]>;
-
 }
