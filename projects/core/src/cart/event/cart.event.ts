0d6b3d0abdcbdfcf5a2e98f0290562b37d7c4b9a
Krzysztof Platis <platonn.git@gmail.com>
2020-04-06 12:48:23 +0200

    rename MultiCartEvent to CartEvent

--- projects/core/src/cart/event/cart.event.ts
+++ projects/core/src/cart/event/cart.event.ts
@@ -1,7 +1,7 @@
 /**
  * Base cart event. Almost all cart events should have `cartId` and `userId`
  */
-export abstract class MultiCartEvent {
+export abstract class CartEvent {
   cartId?: string;
   userId?: string;
 }
