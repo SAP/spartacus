35d78b33b98c7a9b2033a480711019c1466d706c
Krzysztof Platis <platonn.git@gmail.com>
2020-04-06 17:04:06 +0200

    moved logic to cms page guard service

--- projects/storefrontlib/src/cms-structure/guards/cms-page-guard.service.ts
+++ projects/storefrontlib/src/cms-structure/guards/cms-page-guard.service.ts
@@ -5,9 +5,18 @@ import {
   CmsService,
   Page,
   PageContext,
+  PageType,
+  SemanticPathService,
 } from '@spartacus/core';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, take, tap, withLatestFrom } from 'rxjs/operators';
+import {
+  filter,
+  map,
+  switchMap,
+  take,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 import { CmsGuardsService } from '../services/cms-guards.service';
 import { CmsI18nService } from '../services/cms-i18n.service';
 import { CmsRoutesService } from '../services/cms-routes.service';
@@ -20,6 +29,7 @@ import { CmsRoutesService } from '../services/cms-routes.service';
 })
 export class CmsPageGuardService {
   constructor(
+    protected semanticPathService: SemanticPathService,
     private cmsService: CmsService,
     private cmsRoutes: CmsRoutesService,
     private cmsI18n: CmsI18nService,
@@ -27,11 +37,13 @@ export class CmsPageGuardService {
   ) {}
 
   /**
-   * Takes CMS components types on current CMS page, triggers side effects and returns a boolean - whether the route can be activated.
-   * Based on cms mapping config for CMS components:
-   * - evaluates their guards; if one of them returns false or UrlTree - it's returned by this method
-   * - loads i18n chunks
-   * - registers children routes
+   * Takes CMS components types in the current CMS page, triggers (configurable) side effects and returns a boolean - whether the route can be activated.
+   *
+   * Based on `cmsComponents` config for the components in the page:
+   * - Evaluates components' guards; if one of them emits false or UrlTree - the route cannot be activated or redirects to the given UrlTree, respectively.
+   * - If all components' guards emitted true, then the route can be activated
+   * - Then we trigger loading of configured i18n chunks in parallel
+   * - And we register the configured children routes of cms components
    *
    * @param pageContext current cms page context
    * @param pageData cms page data
@@ -40,7 +52,7 @@ export class CmsPageGuardService {
    *
    * @returns boolean observable - whether the route can be activated
    */
-  handlePage(
+  canActivatePage(
     pageContext: PageContext,
     pageData: Page,
     route: CmsActivatedRouteSnapshot,
@@ -48,7 +60,7 @@ export class CmsPageGuardService {
   ): Observable<boolean | UrlTree> {
     return this.cmsService.getPageComponentTypes(pageContext).pipe(
       take(1),
-      switchMap(componentTypes =>
+      switchMap((componentTypes) =>
         this.cmsGuards
           .cmsPageCanActivate(componentTypes, route, state)
           .pipe(withLatestFrom(of(componentTypes)))
@@ -73,6 +85,45 @@ export class CmsPageGuardService {
           );
         }
         return canActivate;
+      })
+    );
+  }
+
+  /**
+   * Activates the "NOT FOUND" cms page.
+   *
+   * It loads cms page data for the "NOT FOUND" page and puts it in the state of the the requested page label.
+   * Then it processes its CMS components with the method `canActivatePage()` of this service. For more, see its docs.
+   */
+  canActivateNotFoundPage(
+    pageContext: PageContext,
+    route: CmsActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> {
+    const notFoundCmsPageContext: PageContext = {
+      type: PageType.CONTENT_PAGE,
+      id: this.semanticPathService.get('notFound'),
+    };
+
+    return this.cmsService.getPage(notFoundCmsPageContext).pipe(
+      switchMap((notFoundPage) => {
+        if (notFoundPage) {
+          return this.cmsService.getPageIndex(notFoundCmsPageContext).pipe(
+            tap((notFoundIndex) => {
+              this.cmsService.setPageFailIndex(pageContext, notFoundIndex);
+            }),
+            switchMap((notFoundIndex) =>
+              this.cmsService.getPageIndex(pageContext).pipe(
+                // we have to wait for page index update
+                filter((index) => index === notFoundIndex)
+              )
+            ),
+            switchMap(() =>
+              this.canActivatePage(pageContext, notFoundPage, route, state)
+            )
+          );
+        }
+        return of(false);
       })
     );
   }
--- projects/storefrontlib/src/cms-structure/guards/cms-page.guard.ts
+++ projects/storefrontlib/src/cms-structure/guards/cms-page.guard.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import {
   CmsActivatedRouteSnapshot,
   CmsService,
-  PageContext,
-  PageType,
+  Config,
+  isFeatureEnabled,
   ProtectedRoutesGuard,
   RoutingService,
-  SemanticPathService,
 } from '@spartacus/core';
 import { Observable, of } from 'rxjs';
-import { filter, first, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { first, switchMap } from 'rxjs/operators';
 import { CmsPageGuardService } from './cms-page-guard.service';
 
 @Injectable({
@@ -22,110 +21,61 @@ export class CmsPageGuard implements CanActivate {
   constructor(
     protected routingService: RoutingService,
     protected cmsService: CmsService,
-    protected semanticPathService: SemanticPathService,
     protected protectedRoutesGuard: ProtectedRoutesGuard,
-    protected service: CmsPageGuardService
+    protected service: CmsPageGuardService,
+    @Inject(Config) protected config: any
   ) {}
 
+  /**
+   * Tries to load the CMS page data for the anticipated route and returns:
+   * - `true` - if it can be activated
+   * - `false` - if it cannot be activated
+   * - `UrlTree` - if user should be redirected to a given `UrlTree`
+   *
+   * If the route can be activated, it fires additional calculations on the CMS components present on this CMS page,
+   * based on their configuration (`cmsComponents` config).
+   *
+   * For more, see docs of the `CmsPageGuardService.canActivatePage`.
+   */
   canActivate(
     route: CmsActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> {
-    return this.protectedRoutesGuard
-      ? this.protectedRoutesGuard
-          .canActivate(route)
-          .pipe(
-            switchMap((result) =>
-              result ? this.getCmsPage(route, state) : of(result)
+    return this.protectedRoutesGuard.canActivate(route).pipe(
+      switchMap((canActivateRoute) =>
+        canActivateRoute
+          ? this.routingService.getNextPageContext().pipe(
+              switchMap((pageContext) =>
+                this.cmsService
+                  .getPage(pageContext, this.shouldReloadCmsData())
+                  .pipe(
+                    first(),
+                    switchMap((pageData) =>
+                      pageData
+                        ? this.service.canActivatePage(
+                            pageContext,
+                            pageData,
+                            route,
+                            state
+                          )
+                        : this.service.canActivateNotFoundPage(
+                            pageContext,
+                            route,
+                            state
+                          )
+                    )
+                  )
+              )
             )
-          )
-      : this.getCmsPage(route, state);
-  }
-
-  protected getCmsPage(
-    route: CmsActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    return this.routingService.getNextPageContext().pipe(
-      switchMap((pageContext) =>
-        this.cmsService
-          .getPage(pageContext, this.cmsGuards.shouldForceRefreshPage())
-          .pipe(first(), withLatestFrom(of(pageContext)))
-      ),
-      switchMap(([pageData, pageContext]) =>
-        pageData
-          ? this.service.handlePage(pageContext, pageData, route, state)
-          : this.handleNotFoundPage(pageContext, route, state)
+          : of(false)
       )
     );
   }
 
-  private resolveCmsPageLogic(
-    pageContext: PageContext,
-    pageData: Page,
-    route: CmsActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    return this.cmsService.getPageComponentTypes(pageContext).pipe(
-      take(1),
-      switchMap((componentTypes) =>
-        this.cmsGuards
-          .cmsPageCanActivate(componentTypes, route, state)
-          .pipe(withLatestFrom(of(componentTypes)))
-      ),
-      tap(([canActivate, componentTypes]) => {
-        if (canActivate === true) {
-          this.cmsI18n.loadChunksForComponents(componentTypes);
-        }
-      }),
-      map(([canActivate, componentTypes]) => {
-        const pageLabel = pageData.label || pageContext.id; // for content pages the page label returned from backend can be different than ID initially assumed from route
-        if (
-          canActivate === true &&
-          !route.data.cxCmsRouteContext &&
-          !this.cmsRoutes.cmsRouteExist(pageLabel)
-        ) {
-          return this.cmsRoutes.handleCmsRoutesInGuard(
-            pageContext,
-            componentTypes,
-            state.url,
-            pageLabel
-          );
-        }
-        return canActivate;
-      })
-    );
-  }
-
-  private handleNotFoundPage(
-    pageContext: PageContext,
-    route: CmsActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    const notFoundCmsPageContext: PageContext = {
-      type: PageType.CONTENT_PAGE,
-      id: this.semanticPathService.get('notFound'),
-    };
-    return this.cmsService.getPage(notFoundCmsPageContext).pipe(
-      switchMap((notFoundPage) => {
-        if (notFoundPage) {
-          return this.cmsService.getPageIndex(notFoundCmsPageContext).pipe(
-            tap((notFoundIndex) => {
-              this.cmsService.setPageFailIndex(pageContext, notFoundIndex);
-            }),
-            switchMap((notFoundIndex) =>
-              this.cmsService.getPageIndex(pageContext).pipe(
-                // we have to wait for page index update
-                filter((index) => index === notFoundIndex)
-              )
-            ),
-            switchMap(() =>
-              this.service.handlePage(pageContext, notFoundPage, route, state)
-            )
-          );
-        }
-        return of(false);
-      })
-    );
+  /**
+   * Returns whether we should reload the CMS page data, even when it was loaded before.
+   */
+  protected shouldReloadCmsData(): boolean {
+    return !isFeatureEnabled(this.config, 'cmsPageLoadOnce');
   }
 }
