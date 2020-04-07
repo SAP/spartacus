639a29eae8de03af670c3dc15bd5fff7707d8a0e
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2020-04-07 13:51:50 +0200

    GH-5971: Prettify

--- projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.module.ts
+++ projects/storefrontlib/src/cms-components/cart/cart-coupon/cart-coupon.module.ts
@@ -1,11 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  CmsConfig,
-  I18nModule,
-  provideDefaultConfig,
-} from '@spartacus/core';
+import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
 import { IconModule } from '../../../cms-components/misc/icon/icon.module';
 import { AppliedCouponsComponent } from './applied-coupons/applied-coupons.component';
 import { CartCouponComponent } from './cart-coupon.component';
