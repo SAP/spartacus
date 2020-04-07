e41707f5e2b9a87982b2321fde8a9d842e130a57
Michał Gruca <pucek9@gmail.com>
2020-04-07 13:48:01 +0200

    up

--- projects/storefrontlib/src/cms-components/organization/units/unit-address-create/unit-address-create.component.ts
+++ projects/storefrontlib/src/cms-components/organization/units/unit-address-create/unit-address-create.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
-import { OrgUnitService, RoutingService, B2BUnitNode } from '@spartacus/core';
+import { OrgUnitService, RoutingService, B2BAddress } from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-unit-address-create',
   templateUrl: './unit-address-create.component.html',
 })
 export class UnitAddressCreateComponent {
+  orgUnitCode$: Observable<string> = this.routingService
+    .getRouterState()
+    .pipe(map(routingData => routingData.state.params['code']));
+
   constructor(
     protected orgUnitService: OrgUnitService,
     protected routingService: RoutingService
   ) {}
 
-  createOrgUnit(orgUnit: B2BUnitNode) {
-    this.orgUnitService.create(orgUnit);
-    this.routingService.go({
-      cxRoute: 'orgUnitAddressDetails',
-      params: orgUnit,
+  createAddress(address: B2BAddress) {
+    this.orgUnitCode$.pipe(take(1)).subscribe(code => {
+      this.orgUnitService.createAddress(address);
+      this.routingService.go({
+        cxRoute: 'orgUnitAddressDetails',
+        params: { ...address, code },
+      });
     });
   }
 }
--- projects/storefrontlib/src/cms-components/organization/units/unit-address-edit/unit-address-edit.component.ts
+++ projects/storefrontlib/src/cms-components/organization/units/unit-address-edit/unit-address-edit.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { map, switchMap, take, tap } from 'rxjs/operators';
+import {
+  filter,
+  map,
+  switchMap,
+  take,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
-import { B2BUnitNode, OrgUnitService, RoutingService } from '@spartacus/core';
+import { B2BAddress, OrgUnitService, RoutingService } from '@spartacus/core';
 
 @Component({
   selector: 'cx-unit-address-edit',
   templateUrl: './unit-address-edit.component.html',
 })
 export class UnitAddressEditComponent implements OnInit {
-  orgUnit$: Observable<B2BUnitNode>;
   orgUnitCode$: Observable<string> = this.routingService
     .getRouterState()
     .pipe(map(routingData => routingData.state.params['code']));
+
+  addressId$: Observable<string> = this.routingService
+    .getRouterState()
+    .pipe(map(routingData => routingData.state.params['id']));
+
+  address$: Observable<B2BAddress>;
 
   constructor(
     protected routingService: RoutingService,
@@ -20,21 +32,27 @@ export class UnitAddressEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.orgUnit$ = this.orgUnitCode$.pipe(
-      tap(code => this.orgUnitsService.loadOrgUnit(code)),
-      switchMap(code => this.orgUnitsService.get(code))
+    this.address$ = this.orgUnitCode$.pipe(
+      withLatestFrom(this.addressId$),
+      tap(([code]) => this.orgUnitsService.loadAddresses(code)),
+      switchMap(([code, id]) =>
+        this.orgUnitsService.getAddress(code, id).pipe(
+          filter(Boolean),
+          map((address: B2BAddress) => ({ ...address, code }))
+        )
+      )
     );
   }
 
-  updateOrgUnit(orgUnit: B2BUnitNode) {
+  updateAddress(address: B2BAddress) {
     this.orgUnitCode$
-      .pipe(take(1))
-      .subscribe(orgUnitCode =>
-        this.orgUnitsService.update(orgUnitCode, orgUnit)
-      );
-    this.routingService.go({
-      cxRoute: 'orgUnitAddressDetails',
-      params: orgUnit,
-    });
+      .pipe(withLatestFrom(this.addressId$), take(1))
+      .subscribe(([code, addressId]) => {
+        this.orgUnitsService.updateAddress(code, addressId, address);
+        this.routingService.go({
+          cxRoute: 'orgUnitAddressDetails',
+          params: { ...address, code },
+        });
+      });
   }
 }
