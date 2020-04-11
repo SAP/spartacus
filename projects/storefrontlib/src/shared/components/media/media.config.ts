c99409a6b263c4f8325909d89118b5d07e156212
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-04-11 11:05:28 +0200

    docs

--- projects/storefrontlib/src/shared/components/media/media.config.ts
+++ projects/storefrontlib/src/shared/components/media/media.config.ts
@@ -5,6 +5,10 @@ import { MediaFormatSize } from './media.model';
  * rendering of the media, SEO and performance.
  */
 export abstract class MediaConfig {
+  /**
+   * Media _format_ configuration holds the size of the media's assigned to
+   * a format.
+   */
   mediaFormats?: {
     /**
      * Represents the media format code, that is the key to distinguish different
