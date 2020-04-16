20bbe25be5bbda143c1a5e71d7748a51c6738452
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-04-16 02:32:38 -0400

    removed constant folder

--- projects/storefrontapp-e2e-cypress/cypress/helpers/constants/backend.ts
+++
@@ -1,9 +0,0 @@
-//TODO: more to come
-export const occ = {
-  apiUrl: Cypress.env('API_URL'),
-  prefix: Cypress.env('OCC_PREFIX'),
-};
-export const newBaseSite = Cypress.env('BASE_SITE');
-export const prefixAndBaseSite = occ.prefix + newBaseSite;
-export const apiUrlAndPrefix = occ.apiUrl + occ.prefix;
-export const baseEndpoint = occ.apiUrl + prefixAndBaseSite;
