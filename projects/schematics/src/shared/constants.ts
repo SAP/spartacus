5a84071d8c226584d6ee7f061609173a0b4860a2
Patryk Smul <psmul@divante.pl>
2020-04-16 14:57:46 +0200

    feat: remove deprecation for save for later feature (#7189)
    Closes: #5958

--- projects/schematics/src/shared/constants.ts
+++ projects/schematics/src/shared/constants.ts
@@ -143,8 +143,8 @@ export const CART_VOUCHER_SERVICE = 'CartVoucherService';
 export const CART_COUPON_COMPONENT = 'CartCouponComponent';
 export const CUSTOMER_COUPON_SERVICE = 'CustomerCouponService';
 export const CART_ITEM_LIST_COMPONENT = 'CartItemListComponent';
+export const CART_ITEM_COMPONENT = 'CartItemComponent';
 export const PROTECTED_ROUTES_SERVICE = 'ProtectedRoutesService';
-
 export const LOGIN_FORM_COMPONENT = 'LoginFormComponent';
 export const REGISTER_COMPONENT = 'RegisterComponent';
 export const ANONYMOUS_CONSENTS_SERVICE = 'AnonymousConsentsService';
