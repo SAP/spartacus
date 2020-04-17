343a8591715c3a9b80e886177f04ea1fc721622c
Kacper Knapik <kknapik@divante.pl>
2020-04-17 12:19:34 +0200

    fix: enable PDP test

--- projects/storefrontapp-e2e-cypress/cypress/integration/accessibility/tabbing-order.flaky-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/accessibility/tabbing-order.flaky-e2e-spec.ts
@@ -202,7 +202,7 @@ describe("Tabbing order - tests don't require user to be logged in", () => {
     });
   });
 
-  context.skip('Product Page', () => {
+  context('Product Page', () => {
     it('should allow to navigate with tab key', () => {
       productPageTabbingOrder(config.productPage);
     });
