2ff0c99506150c83c5e7b5a675b85d12ebe692e6
dunqan <dunqan@w9.pl>
2020-04-18 17:33:16 +0200

    docs update

--- projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
+++ projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
@@ -3,12 +3,19 @@ import { HttpResponseStatus } from '../../../models/response-status.model';
 import { HttpErrorHandler } from '../http-error.handler';
 import { Priority } from '../../../../util/handler';
 
+/**
+ * Unknown Error Handler works as an fallback, to handle errors that were
+ * not handled by any other error handlers
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class UnknownErrorHandler extends HttpErrorHandler {
   responseStatus = HttpResponseStatus.UNKNOWN;
 
+  /**
+   * hasMatch always returns true, to mach all errors
+   */
   hasMatch(_errorResponse): boolean {
     return true;
   }
@@ -19,6 +26,9 @@ export class UnknownErrorHandler extends HttpErrorHandler {
     }
   }
 
+  /**
+   * Fallback priority assures that the handler is used as a last resort
+   */
   getPriority() {
     return Priority.FALLBACK;
   }
