1a78dd1058f410b62d41175517974b713eba8e82
Krzysztof Platis <platonn.git@gmail.com>
2020-04-20 16:31:32 +0200

    add unit tests

--- projects/core/src/site-context/store/effects/currencies.effect.spec.ts
+++ projects/core/src/site-context/store/effects/currencies.effect.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
+import { Store } from '@ngrx/store';
 import { cold, hot } from 'jasmine-marbles';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, NEVER, Observable, of, Subject } from 'rxjs';
 import { ConfigModule } from '../../../config/config.module';
 import { Currency } from '../../../model/misc.model';
 import { OccModule } from '../../../occ/occ.module';
@@ -10,33 +11,35 @@ import { SiteAdapter } from '../../connectors/site.adapter';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
 import * as fromEffects from './currencies.effect';
+import { CurrenciesEffects } from './currencies.effect';
 
 describe('Currencies Effects', () => {
-  let actions$: Observable<SiteContextActions.CurrenciesAction>;
-  let connector: SiteConnector;
-  let effects: fromEffects.CurrenciesEffects;
+  describe('loadCurrencies$', () => {
+    let actions$: Observable<SiteContextActions.CurrenciesAction>;
+    let connector: SiteConnector;
+    let effects: fromEffects.CurrenciesEffects;
 
-  const currencies: Currency[] = [
-    { active: false, isocode: 'USD', name: 'US Dollar', symbol: '$' },
-  ];
+    const currencies: Currency[] = [
+      { active: false, isocode: 'USD', name: 'US Dollar', symbol: '$' },
+    ];
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
-      providers: [
-        fromEffects.CurrenciesEffects,
-        { provide: SiteAdapter, useValue: {} },
-        provideMockActions(() => actions$),
-      ],
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
+        providers: [
+          fromEffects.CurrenciesEffects,
+          { provide: SiteAdapter, useValue: {} },
+          provideMockActions(() => actions$),
+          { provide: Store, useValue: { select: () => NEVER } },
+        ],
+      });
+
+      connector = TestBed.inject(SiteConnector);
+      effects = TestBed.inject(fromEffects.CurrenciesEffects);
+
+      spyOn(connector, 'getCurrencies').and.returnValue(of(currencies));
     });
 
-    connector = TestBed.inject(SiteConnector);
-    effects = TestBed.inject(fromEffects.CurrenciesEffects);
-
-    spyOn(connector, 'getCurrencies').and.returnValue(of(currencies));
-  });
-
-  describe('loadCurrencies$', () => {
     it('should populate all currencies from LoadCurrenciesSuccess', () => {
       const action = new SiteContextActions.LoadCurrencies();
       const completion = new SiteContextActions.LoadCurrenciesSuccess(
@@ -51,14 +54,76 @@ describe('Currencies Effects', () => {
   });
 
   describe('activateCurrency$', () => {
-    it('should change the active currency', () => {
-      const action = new SiteContextActions.SetActiveCurrency('USD');
-      const completion = new SiteContextActions.CurrencyChange();
+    let actions$: Subject<SiteContextActions.CurrenciesAction>;
+    let mockState: BehaviorSubject<string>;
+    let currenciesEffects: CurrenciesEffects;
 
-      actions$ = hot('-a', { a: action });
-      const expected = cold('-b', { b: completion });
+    beforeEach(() => {
+      actions$ = new Subject();
+      mockState = new BehaviorSubject(null);
 
-      expect(effects.activateCurrency$).toBeObservable(expected);
+      const mockStore: Partial<Store<any>> = {
+        select: () => mockState,
+      };
+
+      TestBed.configureTestingModule({
+        imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
+        providers: [
+          fromEffects.CurrenciesEffects,
+          { provide: SiteAdapter, useValue: {} },
+          provideMockActions(() => actions$),
+          { provide: Store, useValue: mockStore },
+        ],
+      });
+
+      currenciesEffects = TestBed.inject(CurrenciesEffects);
+    });
+
+    describe('when currency is set for the first time', () => {
+      it('should NOT dispatch language change action', () => {
+        const results = [];
+        currenciesEffects.activateCurrency$.subscribe(a => results.push(a));
+
+        mockState.next('zh');
+        const setActiveAction = new SiteContextActions.SetActiveCurrency('USD');
+        actions$.next(setActiveAction);
+
+        expect(results).toEqual([]);
+      });
+    });
+
+    describe('when currency is set for the next time', () => {
+      it('should dispatch language change action when language is activated again', () => {
+        const results = [];
+        currenciesEffects.activateCurrency$.subscribe(a => results.push(a));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveCurrency('USD'));
+
+        mockState.next('zh');
+        actions$.next(new SiteContextActions.SetActiveCurrency('GPB'));
+
+        const changeAction = new SiteContextActions.CurrencyChange({
+          previous: 'en',
+          current: 'zh',
+        });
+        expect(results).toEqual([changeAction]);
+      });
+    });
+
+    describe('when the same currency is set for the next time', () => {
+      it('should NOT dispatch language change action', () => {
+        const results = [];
+        currenciesEffects.activateCurrency$.subscribe(a => results.push(a));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveCurrency('USD'));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveCurrency('USD'));
+
+        expect(results).toEqual([]);
+      });
     });
   });
 });
--- projects/core/src/site-context/store/effects/languages.effect.spec.ts
+++ projects/core/src/site-context/store/effects/languages.effect.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
+import { Store } from '@ngrx/store';
 import { cold, hot } from 'jasmine-marbles';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, NEVER, Observable, of, Subject } from 'rxjs';
 import { ConfigModule } from '../../../config/config.module';
 import { Language } from '../../../model/misc.model';
 import { OccModule } from '../../../occ/occ.module';
@@ -10,33 +11,35 @@ import { SiteAdapter } from '../../connectors/site.adapter';
 import { SiteConnector } from '../../connectors/site.connector';
 import { SiteContextActions } from '../actions/index';
 import * as fromEffects from './languages.effect';
+import { LanguagesEffects } from './languages.effect';
 
 describe('Languages Effects', () => {
-  let actions$: Observable<SiteContextActions.LanguagesAction>;
-  let connector: SiteConnector;
-  let effects: fromEffects.LanguagesEffects;
+  describe('loadLanguages$', () => {
+    let actions$: Observable<SiteContextActions.LanguagesAction>;
+    let connector: SiteConnector;
+    let effects: fromEffects.LanguagesEffects;
 
-  const languages: Language[] = [
-    { active: true, isocode: 'ja', name: 'Japanese' },
-  ];
+    let languages: Language[];
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
-      providers: [
-        fromEffects.LanguagesEffects,
-        { provide: SiteAdapter, useValue: {} },
-        provideMockActions(() => actions$),
-      ],
+    beforeEach(() => {
+      languages = [{ active: true, isocode: 'ja', name: 'Japanese' }];
+
+      TestBed.configureTestingModule({
+        imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
+        providers: [
+          fromEffects.LanguagesEffects,
+          { provide: SiteAdapter, useValue: {} },
+          provideMockActions(() => actions$),
+          { provide: Store, useValue: { select: () => NEVER } },
+        ],
+      });
+
+      connector = TestBed.inject(SiteConnector);
+      effects = TestBed.inject(fromEffects.LanguagesEffects);
+
+      spyOn(connector, 'getLanguages').and.returnValue(of(languages));
     });
 
-    connector = TestBed.inject(SiteConnector);
-    effects = TestBed.inject(fromEffects.LanguagesEffects);
-
-    spyOn(connector, 'getLanguages').and.returnValue(of(languages));
-  });
-
-  describe('loadLanguages$', () => {
     it('should populate all languages from LoadLanguagesSuccess', () => {
       const action = new SiteContextActions.LoadLanguages();
       const completion = new SiteContextActions.LoadLanguagesSuccess(languages);
@@ -49,14 +52,76 @@ describe('Languages Effects', () => {
   });
 
   describe('activateLanguage$', () => {
-    it('should change the active language', () => {
-      const action = new SiteContextActions.SetActiveLanguage('zh');
-      const completion = new SiteContextActions.LanguageChange();
+    let actions$: Subject<SiteContextActions.LanguagesAction>;
+    let mockState: BehaviorSubject<string>;
+    let languagesEffects: LanguagesEffects;
 
-      actions$ = hot('-a', { a: action });
-      const expected = cold('-b', { b: completion });
+    beforeEach(() => {
+      actions$ = new Subject();
+      mockState = new BehaviorSubject(null);
 
-      expect(effects.activateLanguage$).toBeObservable(expected);
+      const mockStore: Partial<Store<any>> = {
+        select: () => mockState,
+      };
+
+      TestBed.configureTestingModule({
+        imports: [ConfigModule.forRoot(), HttpClientTestingModule, OccModule],
+        providers: [
+          fromEffects.LanguagesEffects,
+          { provide: SiteAdapter, useValue: {} },
+          provideMockActions(() => actions$),
+          { provide: Store, useValue: mockStore },
+        ],
+      });
+
+      languagesEffects = TestBed.inject(LanguagesEffects);
+    });
+
+    describe('when language is set for the first time', () => {
+      it('should NOT dispatch language change action', () => {
+        const results = [];
+        languagesEffects.activateLanguage$.subscribe(a => results.push(a));
+
+        mockState.next('zh');
+        const setActiveAction = new SiteContextActions.SetActiveLanguage('zh');
+        actions$.next(setActiveAction);
+
+        expect(results).toEqual([]);
+      });
+    });
+
+    describe('when language is set for the next time', () => {
+      it('should dispatch language change action', () => {
+        const results = [];
+        languagesEffects.activateLanguage$.subscribe(a => results.push(a));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveLanguage('en'));
+
+        mockState.next('zh');
+        actions$.next(new SiteContextActions.SetActiveLanguage('zh'));
+
+        const changeAction = new SiteContextActions.LanguageChange({
+          previous: 'en',
+          current: 'zh',
+        });
+        expect(results).toEqual([changeAction]);
+      });
+    });
+
+    describe('when the same language is set for the next time', () => {
+      it('should NOT dispatch language change action', () => {
+        const results = [];
+        languagesEffects.activateLanguage$.subscribe(a => results.push(a));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveLanguage('en'));
+
+        mockState.next('en');
+        actions$.next(new SiteContextActions.SetActiveLanguage('en'));
+
+        expect(results).toEqual([]);
+      });
     });
   });
 });
