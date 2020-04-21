55eb3f27a8db053b286dfbfddf029aa39fcb972c
Michał Gruca <pucek9@gmail.com>
2020-04-21 14:17:34 +0200

    add unit test

--- projects/storefrontlib/src/cms-components/storefinder/components/store-finder-list-item/store-finder-list-item.component.spec.ts
+++ projects/storefrontlib/src/cms-components/storefinder/components/store-finder-list-item/store-finder-list-item.component.spec.ts
@@ -1,4 +1,3 @@
-import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,6 +7,8 @@ import {
   RoutingService,
 } from '@spartacus/core';
 import { StoreFinderListItemComponent } from './store-finder-list-item.component';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const weekday = {
   closingTime: {
@@ -23,7 +24,12 @@ const weekday = {
   closed: false,
 };
 
+const name = 'Tokyu Hotel';
+const displayName = 'Tokio Cerulean Tower Tokyu Hotel';
+
 const sampleStore: any = {
+  name,
+  displayName,
   address: {
     country: { isocode: 'JP' },
     line1: 'Sakuragaokacho Shibuya',
@@ -32,7 +38,6 @@ const sampleStore: any = {
     postalCode: '150-8512',
     town: 'Tokio',
   },
-  displayName: 'Tokio Cerulean Tower Tokyu Hotel',
   geoPoint: {
     latitude: 35.656347,
     longitude: 139.69956,
@@ -85,23 +90,21 @@ const mockRoutingService = {
   go: jasmine.createSpy('go'),
 };
 
-const mockActivatedRoute = {
-  snapshot: {
-    params: {},
-  },
-};
-
 describe('StoreFinderListItemComponent', () => {
   let component: StoreFinderListItemComponent;
   let fixture: ComponentFixture<StoreFinderListItemComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [CommonModule, ReactiveFormsModule, I18nTestingModule],
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        I18nTestingModule,
+        RouterTestingModule,
+      ],
       declarations: [StoreFinderListItemComponent],
       providers: [
         StoreDataService,
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
         { provide: RoutingService, useValue: mockRoutingService },
       ],
     }).compileComponents();
@@ -124,5 +127,15 @@ describe('StoreFinderListItemComponent', () => {
     component.handleStoreItemClick();
     fixture.detectChanges();
     expect(component.storeItemClick.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should prepare proper link', () => {
+    fixture.detectChanges();
+    const encodedName = name.replace(' ', '%20');
+    const link = fixture.debugElement
+      .queryAll(By.css('.cx-store-name > a'))
+      .find((el) => el.nativeElement.innerText === displayName).nativeElement;
+    expect(link.getAttribute('href')).toEqual(`/${encodedName})}`);
+    expect(link.getAttribute('ng-reflect-router-link')).toEqual(name);
   });
 });
