11352f23fb42227dd8d9bd5601bc0a78f2836a73
Marcin Lasak <lasakmarcin90@gmail.com>
2020-04-28 18:09:50 +0200

    Revert constructor-deprecations_spec.ts

--- projects/schematics/src/migrations/2_0/constructor-deprecations/constructor-deprecations_spec.ts
+++ projects/schematics/src/migrations/2_0/constructor-deprecations/constructor-deprecations_spec.ts
@@ -180,7 +180,7 @@ const REMOVE_PARAMETER_EXPECTED_CLASS = `
 import { Dummy } from '@angular/core';
 import {
   CmsService,
-
+  
   PageMetaResolver,
   PageMetaService
 } from '@spartacus/core';
@@ -188,7 +188,7 @@ export class Test extends PageMetaService {
   constructor(
     resolvers: PageMetaResolver[],
     cms: CmsService
-
+    
   ) {
     super(resolvers, cms );
   }
@@ -218,7 +218,7 @@ const REMOVE_PARAMETER_WITH_ADDITIONAL_INJECTED_SERVICE_EXPECTED_CLASS = `
 import { ActionsSubject } from '@ngrx/store';
 import {
   CmsService,
-
+  
   PageMetaResolver,
   PageMetaService
 } from '@spartacus/core';
@@ -313,7 +313,7 @@ const CART_PAGE_LAYOUT_HANDLER_EXPECTED = `
 `;
 
 describe('constructor migrations', () => {
-  let host: TempScopedNodeJsSyncHost;
+  let host = new TempScopedNodeJsSyncHost();
   let appTree = Tree.empty() as UnitTestTree;
   let schematicRunner: SchematicTestRunner;
   let tmpDirPath: string;
@@ -364,7 +364,6 @@ describe('constructor migrations', () => {
   afterEach(() => {
     shx.cd(previousWorkingDir);
     shx.rm('-r', tmpDirPath);
-    console.log('constructor deprecation spec');
   });
 
   describe('when the class does NOT extend a Spartacus class', () => {
