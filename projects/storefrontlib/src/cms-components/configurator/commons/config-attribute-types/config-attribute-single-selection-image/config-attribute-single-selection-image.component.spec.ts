04d24fae7df156af2945383acf51f96a84a3ade8
Larisa Staroverova <larisa.staroverova@sap.com>
2020-05-07 08:00:12 +0200

    Improve test coverage

--- projects/storefrontlib/src/cms-components/configurator/commons/config-attribute-types/config-attribute-single-selection-image/config-attribute-single-selection-image.component.spec.ts
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-attribute-types/config-attribute-single-selection-image/config-attribute-single-selection-image.component.spec.ts
@@ -5,9 +5,10 @@ import { Configurator } from '@spartacus/core';
 import { ConfigUIKeyGeneratorService } from '../../service/config-ui-key-generator.service';
 import { ConfigAttributeSingleSelectionImageComponent } from './config-attribute-single-selection-image.component';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { By } from '@angular/platform-browser';
 
-describe('ConfigAttributeSingleSelectionImageComponent', () => {
-  let classUnderTest: ConfigAttributeSingleSelectionImageComponent;
+fdescribe('ConfigAttributeSingleSelectionImageComponent', () => {
+  let component: ConfigAttributeSingleSelectionImageComponent;
   let fixture: ComponentFixture<ConfigAttributeSingleSelectionImageComponent>;
 
   beforeEach(async(() => {
@@ -24,64 +25,79 @@ describe('ConfigAttributeSingleSelectionImageComponent', () => {
       .compileComponents();
   }));
 
-  beforeEach(() => {
+  function createImage(url: string, altText: string): Configurator.Image {
     const image: Configurator.Image = {
-      url: 'url',
-      altText: 'altText',
+      url: url,
+      altText: altText,
     };
+    return image;
+  }
 
-    let localimages: Configurator.Image[];
-    localimages = [];
-
-    localimages.push(image, image, image);
-
-    const value1: Configurator.Value = {
-      valueCode: '1',
-      name: 'val1',
-      selected: false,
-      images: localimages,
+  function createValue(
+    code: string,
+    name: string,
+    isSelected: boolean,
+    images: Configurator.Image[]
+  ): Configurator.Value {
+    const value: Configurator.Value = {
+      valueCode: code,
+      name: name,
+      selected: isSelected,
+      images: images,
     };
-    const value2: Configurator.Value = {
-      valueCode: '2',
-      name: 'val2',
-      selected: false,
-      images: localimages,
-    };
-    const value3: Configurator.Value = {
-      valueCode: '3',
-      name: 'val3',
-      selected: false,
-      images: localimages,
-    };
+    return value;
+  }
 
-    let localvalues: Configurator.Value[];
-    localvalues = [];
-
-    localvalues.push(value1, value2, value3);
+  beforeEach(() => {
+    const image = createImage('url', 'altText');
+    let images: Configurator.Image[] = [image, image, image];
+    const value1 = createValue('1', 'val1', false, images);
+    const value2 = createValue('2', 'val2', false, images);
+    const value3 = createValue('3', 'val3', false, images);
+    const values: Configurator.Value[] = [value1, value2, value3];
 
     fixture = TestBed.createComponent(
       ConfigAttributeSingleSelectionImageComponent
     );
-    classUnderTest = fixture.componentInstance;
+    component = fixture.componentInstance;
 
-    classUnderTest.attribute = {
+    component.attribute = {
       name: 'attributeName',
       attrCode: 444,
       uiType: Configurator.UiType.SINGLE_SELECTION_IMAGE,
       required: false,
-      selectedSingleValue: 'selectedSingleValue',
-      values: localvalues,
+      selectedSingleValue: values[2].valueCode,
+      values: values,
     };
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(classUnderTest).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it('should init with selectedValue', () => {
-    expect(classUnderTest.attributeRadioButtonForm.value).toEqual(
-      'selectedSingleValue'
+    expect(component.attributeRadioButtonForm.value).toEqual(
+      component.attribute.values[2].valueCode
+    );
+  });
+
+  it('should select a single selection image value', () => {
+    const singleSelectionImageId =
+      '#cx-config--single_selection_image--' +
+      component.attribute.name +
+      '--' +
+      component.attribute.values[1].valueCode +
+      '-input';
+    const valueToSelect = fixture.debugElement.query(
+      By.css(singleSelectionImageId)
+    ).nativeElement;
+    expect(valueToSelect.checked).toBe(false);
+    valueToSelect.click();
+    fixture.detectChanges();
+    expect(valueToSelect.checked).toBe(true);
+    expect(component.attributeRadioButtonForm.value).toEqual(
+      component.attribute.values[1].valueCode
     );
   });
 });
