b0289149561105cc81acd1b569d96d0283199aa8
lohitbhosur <lohit.b.hosur@sap.com>
2020-05-08 11:21:13 +0100

    7441-1 service action

--- projects/core/src/organization/facade/org-unit.service.spec.ts
+++ projects/core/src/organization/facade/org-unit.service.spec.ts
@@ -1,21 +1,29 @@
 import { Type } from '@angular/core';
 import { inject, TestBed } from '@angular/core/testing';
 import { Store, StoreModule } from '@ngrx/store';
+import {
+  AuthService,
+  B2BSearchConfig,
+  CostCenter,
+  EntitiesModel,
+  ListModel,
+  ORGANIZATION_FEATURE,
+  StateWithOrganization,
+} from '@spartacus/core';
 import { of } from 'rxjs';
-import createSpy = jasmine.createSpy;
-
-import { B2BUnit, B2BUnitNode } from '../../model/org-unit.model';
+import {
+  B2BAddress,
+  B2BApprovalProcess,
+  B2BUnit,
+  B2BUnitNode,
+  B2BUser,
+} from '../../model/org-unit.model';
 import { PROCESS_FEATURE } from '../../process/store/process-state';
 import * as fromProcessReducers from '../../process/store/reducers';
 import { OrgUnitActions } from '../store/actions/index';
 import * as fromReducers from '../store/reducers/index';
 import { OrgUnitService } from './org-unit.service';
-import {
-  AuthService,
-  ORGANIZATION_FEATURE,
-  StateWithOrganization,
-  CostCenter,
-} from '@spartacus/core';
+import createSpy = jasmine.createSpy;
 
 const userId = 'current';
 const orgUnitId = 'testOrgUnit';
@@ -251,6 +259,249 @@ describe('OrgUnitService', () => {
 
       expect(service['findUnitChildrenInTree']('Fake ID', mockedTree)).toEqual(
         []
+      );
+    });
+
+    it('getChildUnits()', () => {
+      let unitNode: B2BUnitNode[];
+      service
+        .getChildUnits(orgUnitId)
+        .subscribe((data) => {
+          unitNode = data;
+        })
+        .unsubscribe();
+
+      expect(unitNode).toEqual(undefined);
+    });
+  });
+
+  describe('get Addresses', () => {
+    it('getAddresses() should trigger loadAddresses when they are not present in the store', () => {
+      let fetchedAddress: EntitiesModel<B2BAddress>;
+      service
+        .getAddresses(orgUnitId)
+        .subscribe((data) => {
+          fetchedAddress = data;
+        })
+        .unsubscribe();
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(fetchedAddress).toEqual(undefined);
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.LoadOrgUnit({
+          userId,
+          orgUnitId,
+        })
+      );
+    });
+  });
+
+  describe('create, update & delete Address', () => {
+    const address: B2BAddress = { id: 'adrId' };
+    const addressId = 'testAddressId';
+
+    it('should create address', () => {
+      service.createAddress(orgUnitId, address);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.CreateAddress({ userId, orgUnitId, address })
+      );
+    });
+
+    it('should update address', () => {
+      service.updateAddress(orgUnitId, addressId, address);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.UpdateAddress({
+          userId,
+          orgUnitId,
+          addressId,
+          address,
+        })
+      );
+    });
+
+    it('should delete address', () => {
+      service.deleteAddress(orgUnitId, addressId);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.DeleteAddress({
+          userId,
+          orgUnitId,
+          addressId,
+        })
+      );
+    });
+  });
+
+  describe('assign, unassign role', () => {
+    const orgCustomerId = 'testOrgCustomerId';
+    const roleId = 'testRoleId';
+
+    it('should assign role', () => {
+      service.assignRole(orgUnitId, orgCustomerId, roleId);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.AssignRole({
+          userId,
+          orgUnitId,
+          orgCustomerId,
+          roleId,
+        })
+      );
+    });
+
+    it('should unassign role', () => {
+      service.unassignRole(orgUnitId, orgCustomerId, roleId);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.UnassignRole({
+          userId,
+          orgUnitId,
+          orgCustomerId,
+          roleId,
+        })
+      );
+    });
+  });
+
+  describe('get approvalProcesses', () => {
+    it('getApprovalProcesses() should trigger loadApprovalProcesses when they are not present in the store', () => {
+      let approvalProcesses: B2BApprovalProcess[];
+      service
+        .getApprovalProcesses()
+        .subscribe((data) => {
+          approvalProcesses = data;
+        })
+        .unsubscribe();
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(approvalProcesses).toEqual(undefined);
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.LoadApprovalProcesses({ userId })
+      );
+    });
+
+    it('getApprovalProcesses() should be able to get approvalProcess when they are present in the store', () => {
+      let b2bApprovalProcesses: B2BApprovalProcess[];
+      const b2bMockApprovalProcesses: B2BApprovalProcess[] = [
+        { code: 'testCode', name: 'testName' },
+      ];
+
+      store.dispatch(
+        new OrgUnitActions.LoadApprovalProcessesSuccess(
+          b2bMockApprovalProcesses
+        )
+      );
+
+      service
+        .getApprovalProcesses()
+        .subscribe((data) => {
+          b2bApprovalProcesses = data;
+        })
+        .unsubscribe();
+
+      expect(authService.getOccUserId).not.toHaveBeenCalled();
+      expect(b2bApprovalProcesses).toEqual(b2bMockApprovalProcesses);
+      expect(store.dispatch).not.toHaveBeenCalledWith(
+        new OrgUnitActions.LoadApprovalProcesses({ userId })
+      );
+    });
+  });
+
+  describe('create & update unit', () => {
+    const unit: B2BUnit = { uid: 'testUid' };
+
+    it('should create unit', () => {
+      service.create(unit);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.CreateUnit({
+          userId,
+          unit,
+        })
+      );
+    });
+
+    it('should update unit', () => {
+      const unitCode = 'testUnitCode';
+
+      service.update(unitCode, unit);
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.UpdateUnit({
+          userId,
+          unitCode,
+          unit,
+        })
+      );
+    });
+  });
+
+  describe('get Users', () => {
+    const params: B2BSearchConfig = { sort: 'code' };
+    const roleId = 'testRoleId';
+    const page: ListModel = { ids: [userId], sorts: [{ code: 'code' }] };
+
+    it('getUsers() should trigger loadUsers when they are not present in the store', () => {
+      let users: EntitiesModel<B2BUser>;
+      service
+        .getUsers(orgUnitId, roleId, params)
+        .subscribe((data) => {
+          users = data;
+        })
+        .unsubscribe();
+
+      expect(authService.getOccUserId).toHaveBeenCalled();
+      expect(users).toEqual(undefined);
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new OrgUnitActions.LoadAssignedUsers({
+          userId,
+          orgUnitId,
+          roleId,
+          params,
+        })
+      );
+    });
+
+    it('getUsers() should be able to get users when they are present in the store', () => {
+      let b2bFetchedUsers: EntitiesModel<B2BUser>;
+      const b2bMockUsers: B2BUser[] = [
+        { uid: userId, orgUnit: orgUnit, roles: [roleId] },
+      ];
+
+      store.dispatch(
+        new OrgUnitActions.LoadAssignedUsersSuccess({
+          orgUnitId,
+          roleId,
+          page,
+          params,
+        })
+      );
+
+      service
+        .getUsers(orgUnitId, roleId, params)
+        .subscribe((data) => {
+          b2bFetchedUsers = data;
+        })
+        .unsubscribe();
+
+      expect(authService.getOccUserId).not.toHaveBeenCalled();
+      expect(b2bFetchedUsers.values).toEqual(b2bMockUsers);
+      expect(store.dispatch).not.toHaveBeenCalledWith(
+        new OrgUnitActions.LoadAssignedUsers({
+          userId,
+          orgUnitId,
+          roleId,
+          params,
+        })
       );
     });
   });
--- projects/core/src/organization/store/actions/org-unit.action.spec.ts
+++ projects/core/src/organization/store/actions/org-unit.action.spec.ts
@@ -1,10 +1,16 @@
 import {
-  ORG_UNIT_NODE_LIST,
-  ORG_UNIT_NODES,
-  ORG_UNIT_ENTITIES,
-} from '../organization-state';
-import { B2BUnit, B2BUnitNode } from '../../../model/org-unit.model';
+  B2BApprovalProcess,
+  B2BUnit,
+  B2BUnitNode,
+} from '../../../model/org-unit.model';
 import { StateUtils } from '../../../state/utils/index';
+import {
+  ORG_UNIT_APPROVAL_PROCESSES,
+  ORG_UNIT_APPROVAL_PROCESSES_ENTITIES,
+  ORG_UNIT_ENTITIES,
+  ORG_UNIT_NODES,
+  ORG_UNIT_NODE_LIST,
+} from '../organization-state';
 import { OrgUnitActions } from './index';
 
 const orgUnitId = 'testOrgUnitId';
@@ -14,6 +20,10 @@ const orgUnitNode: Partial<B2BUnitNode> = { id: orgUnitId };
 const orgUnitNode2: Partial<B2BUnitNode> = { id: 'testOrgUnit2' };
 
 const orgUnitList: B2BUnitNode[] = [orgUnitNode, orgUnitNode2];
+
+const approvalProcesses: B2BApprovalProcess[] = [
+  { code: 'testAprovalProcessCode', name: 'testAprovalProcessName' },
+];
 
 const userId = 'xxx@xxx.xxx';
 const error = 'anError';
@@ -103,6 +113,52 @@ describe('OrgUnit Actions', () => {
             ORG_UNIT_NODES
           ),
         });
+      });
+    });
+  });
+
+  describe('LoadApprovalProcesses Actions', () => {
+    it('should execute LoadApprovalProcesses action', () => {
+      const action = new OrgUnitActions.LoadApprovalProcesses({
+        userId,
+      });
+
+      expect({ ...action }).toEqual({
+        type: OrgUnitActions.LOAD_APPROVAL_PROCESSES,
+        payload: { userId },
+        meta: StateUtils.entityLoadMeta(
+          ORG_UNIT_APPROVAL_PROCESSES_ENTITIES,
+          ORG_UNIT_APPROVAL_PROCESSES
+        ),
+      });
+    });
+
+    it('should execute LoadApprovalProcessesFail action', () => {
+      const action = new OrgUnitActions.LoadApprovalProcessesFail({ error });
+
+      expect({ ...action }).toEqual({
+        type: OrgUnitActions.LOAD_APPROVAL_PROCESSES_FAIL,
+        payload: { error },
+        meta: StateUtils.entityFailMeta(
+          ORG_UNIT_APPROVAL_PROCESSES_ENTITIES,
+          ORG_UNIT_APPROVAL_PROCESSES,
+          error
+        ),
+      });
+    });
+
+    it('should execute LoadApprovalProcessesSuccess action', () => {
+      const action = new OrgUnitActions.LoadApprovalProcessesSuccess(
+        approvalProcesses
+      );
+
+      expect({ ...action }).toEqual({
+        type: OrgUnitActions.LOAD_APPROVAL_PROCESSES_SUCCESS,
+        payload: approvalProcesses,
+        meta: StateUtils.entitySuccessMeta(
+          ORG_UNIT_APPROVAL_PROCESSES_ENTITIES,
+          ORG_UNIT_APPROVAL_PROCESSES
+        ),
       });
     });
   });
