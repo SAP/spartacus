50528e71f3fd88885494ea4b21a3741d5fd37a34
Patryk Smul <psmul@divante.pl>
2020-05-14 14:52:59 +0200

    fix: code review suggestion change, bash script for updating template files for outlets

---
+++ projects/dev-schematics/scripts/outlet-template-files.sh
@@ -0,0 +1,27 @@
+
+OUTLETS_PARENT_DIR='./../src/add-test-outlets/files';
+RELATIVE_OUTLETS_SOURCE_DIR='./../../../../storefrontapp/src/test-outlets';
+
+function delete_dir {
+  if [[ -d "$1" ]]; then
+    rm -rf "$1"
+  fi
+}
+
+function copy_dir {
+  if [[ -d "$1" ]]; then
+    mkdir -p $2
+    cp -rf $1/* ./$2
+  fi
+}
+
+function add_template_ext_in_dir {
+  if [[ -d "$1" ]]; then
+    find . -type f -exec mv '{}' '{}'.template \;
+  fi
+}
+
+cd $OUTLETS_PARENT_DIR
+delete_dir ./test-outlets
+copy_dir $RELATIVE_OUTLETS_SOURCE_DIR test-outlets
+add_template_ext_in_dir test-outlets
