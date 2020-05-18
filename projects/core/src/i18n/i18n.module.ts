d4f17ec49f09d4918b08206f9410b6c0922210d9
Christoph Hinssen <christoph.hinssen@sap.com>
2020-05-18 15:19:49 +0200

    GH-7566 Restore file to previous state

--- projects/core/src/i18n/i18n.module.ts
+++ projects/core/src/i18n/i18n.module.ts
@@ -1,18 +1,17 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { Config, provideDefaultConfig } from '../config/config.module';
+import { TranslatePipe } from './translate.pipe';
+import { i18nextProviders } from './i18next/i18next-providers';
 import { defaultI18nConfig } from './config/default-i18n-config';
 import { I18nConfig } from './config/i18n-config';
-import { CxDatePipe } from './date.pipe';
-import { CxDecimalPipe } from './decimal.pipe';
-import { i18nextProviders } from './i18next/i18next-providers';
-import { I18nextTranslationService } from './i18next/i18next-translation.service';
-import { TranslatePipe } from './translate.pipe';
-import { TranslationChunkService } from './translation-chunk.service';
 import { TranslationService } from './translation.service';
+import { Config, provideDefaultConfig } from '../config/config.module';
+import { I18nextTranslationService } from './i18next/i18next-translation.service';
+import { CxDatePipe } from './date.pipe';
+import { TranslationChunkService } from './translation-chunk.service';
 
 @NgModule({
-  declarations: [TranslatePipe, CxDatePipe, CxDecimalPipe],
-  exports: [TranslatePipe, CxDatePipe, CxDecimalPipe],
+  declarations: [TranslatePipe, CxDatePipe],
+  exports: [TranslatePipe, CxDatePipe],
 })
 export class I18nModule {
   static forRoot(): ModuleWithProviders<I18nModule> {
