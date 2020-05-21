0f4a5b8a398894c1a9f93a9a4b712fa532a11386
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-05-21 17:46:31 +0200

    feat: unify shell apps (#7726)
    This PR lays a foundation for one shell app to rule them all.

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,21 +8,22 @@ import {
   BrowserTransferStateModule,
 } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { translationChunksConfig, translations } from '@spartacus/assets';
-import { ConfigModule, TestConfigModule } from '@spartacus/core';
+import { TestConfigModule } from '@spartacus/core';
 import {
-  B2cStorefrontModule,
   JsonLdBuilderModule,
   StorefrontComponent,
 } from '@spartacus/storefront';
+import { b2bFeature } from '../environments/b2c/b2b.feature';
+import { b2cFeature } from '../environments/b2c/b2c.feature';
+import { cdsFeature } from '../environments/cds/cds.feature';
 import { environment } from '../environments/environment';
 import { TestOutletModule } from '../test-outlets/test-outlet.module';
+
 registerLocaleData(localeDe);
 registerLocaleData(localeJa);
 registerLocaleData(localeZh);
 
 const devImports = [];
-
 if (!environment.production) {
   devImports.push(StoreDevtoolsModule.instrument());
 }
@@ -32,54 +33,15 @@ if (!environment.production) {
     BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
     BrowserTransferStateModule,
 
-    B2cStorefrontModule.withConfig({
-      backend: {
-        occ: {
-          baseUrl: environment.occBaseUrl,
-          prefix: '/rest/v2/',
-        },
-      },
-      context: {
-        urlParameters: ['baseSite', 'language', 'currency'],
-        baseSite: [
-          'electronics-spa',
-          'electronics',
-          'apparel-de',
-          'apparel-uk',
-          'apparel-uk-spa',
-        ],
-      },
-
-      // custom routing configuration for e2e testing
-      routing: {
-        routes: {
-          product: {
-            paths: ['product/:productCode/:name', 'product/:productCode'],
-          },
-        },
-      },
-      // we bring in static translations to be up and running soon right away
-      i18n: {
-        resources: translations,
-        chunks: translationChunksConfig,
-        fallbackLang: 'en',
-      },
-      features: {
-        level: '1.5',
-      },
-      cart: {
-        selectiveCart: {
-          enabled: true,
-        },
-      },
-    }),
+    ...(environment.b2b ? b2bFeature.imports : b2cFeature.imports),
     JsonLdBuilderModule,
+
+    ...cdsFeature.imports,
 
     TestOutletModule, // custom usages of cxOutletRef only for e2e testing
     TestConfigModule.forRoot({ cookie: 'cxConfigE2E' }), // Injects config dynamically from e2e tests. Should be imported after other config modules.
 
     ...devImports,
-    ConfigModule,
   ],
 
   bootstrap: [StorefrontComponent],
