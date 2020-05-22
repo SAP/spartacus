2e50c5facab7007565796e882940a19e98ed8470
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-05-22 18:44:09 +0200

    switching the 1905 env to b2b build for CI

--- projects/storefrontapp/src/environments/environment.1905.ts
+++ projects/storefrontapp/src/environments/environment.1905.ts
@@ -1,7 +1,20 @@
+/************* TODO:b2b REVERT THIS FILE BEFORE MERGING **************/
+/** The contents should be:
 import { Environment } from './models/environment.model';
 
 export const environment: Environment = {
   production: true,
   occBaseUrl: 'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
   occApiPrefix: '/rest/v2/',
+};
+ */
+/********************************************************************/
+
+import { Environment } from './models/environment.model';
+
+export const environment: Environment = {
+  production: true,
+  occBaseUrl: 'https://dev-com-7.accdemo.b2c.ydev.hybris.com:9002',
+  occApiPrefix: '/occ/v2/',
+  b2b: true,
 };
