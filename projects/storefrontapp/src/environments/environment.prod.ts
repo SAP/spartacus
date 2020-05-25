68c95ba21d3629681c69a0635dd6415325d3eaa5
Marcin Lasak <lasakmarcin90@gmail.com>
2020-05-25 15:24:45 +0200

    feat: Configure app through environment variables

---
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../../../types/typings.d.ts" />
+
+import { Environment } from './models/environment.model';
+
+export const environment: Environment = {
+  production: true,
+  occBaseUrl:
+    build.process.env.SPARTACUS_BASE_URL ??
+    'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
+  occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/rest/v2/',
+  cds: !!build.process.env.SPARTACUS_CDS,
+  b2b: !!build.process.env.SPARTACUS_B2B,
+};
--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../../types/typings.d.ts" />
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
@@ -7,7 +9,12 @@ import { Environment } from './models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  occBaseUrl: '',
+  occBaseUrl:
+    'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002' ||
+    // 'https://dev-com-7.accdemo.b2c.ydev.hybris.com:9002' ||
+    // 'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com' ||
+    build.process.env.SPARTACUS_BASE_URL,
   occApiPrefix: '/rest/v2/',
   b2b: false,
+  cds: false,
 };
