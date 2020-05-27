3739e9b224988f871647e74ba2b996236a5d2dc0
Krzysztof Platis <platonn.git@gmail.com>
2020-05-27 11:15:55 +0200

    fix: support navigation links containing query params and hash fragments (#7782) (#7793)
    Angular `routerLink` encodes special characters including `?` and `#`. That's why when we passed a raw string URL containing query params or hash fragment to the cx-generic-link component, the output link was broken.
    
    Now the string URL is parsed, the query params and hash fragment extracted and passed into the special inputs of the `routerLink` directive: `[queryParams]` and `[fragment]`.
    
    closes GH-6368

--- projects/storefrontlib/src/shared/components/generic-link/generic-link.component.ts
+++ projects/storefrontlib/src/shared/components/generic-link/generic-link.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Params, Router } from '@angular/router';
+
+// private
+interface RouteParts {
+  /** Path in the Angular-like array format */
+  path?: string[];
+
+  /** Query params */
+  queryParams?: Params;
+
+  /** Hash fragment */
+  fragment?: string;
+}
 
 /**
  * This component navigates using [routerLink] attribute when input 'url' is a relative url. Otherwise (when it's absolute), [href] is used.
@@ -7,8 +20,27 @@ import { Component, Input } from '@angular/core';
   selector: 'cx-generic-link',
   templateUrl: './generic-link.component.html',
 })
-export class GenericLinkComponent {
-  private readonly protocolRegex: RegExp = /^https?:\/\//i;
+export class GenericLinkComponent implements OnChanges {
+  constructor(protected router: Router) {}
+
+  /**
+   * Pattern matching string starting with `http://` or `https://`.
+   */
+  private readonly PROTOCOL_REGEX: RegExp = /^https?:\/\//i;
+
+  /**
+   * Used to split url into 2 parts:
+   * 1. the path
+   * 2. query params + hash fragment
+   */
+  private readonly URL_SPLIT = /(^[^#?]*)(.*)/;
+
+  /**
+   * Parsed parts of the @Input `url`, when it's a local URL.
+   * It should not be used when the `url` is external.
+   * @see `url`
+   */
+  private routeParts: RouteParts = {};
 
   @Input() url: string | any[];
   @Input() target: string;
@@ -17,22 +49,74 @@ export class GenericLinkComponent {
   @Input() style: string;
   @Input() title: string;
 
+  /**
+   * Returns true when the @Input `url` is a string starting with `http://` or `https://`.
+   */
+  isExternalUrl(): boolean {
+    return typeof this.url === 'string' && this.PROTOCOL_REGEX.test(this.url);
+  }
+
   get rel() {
     return this.target === '_blank' ? 'noopener' : null;
   }
 
-  get routerUrl(): any[] {
-    if (typeof this.url === 'string') {
-      return [this.getAbsoluteUrl(this.url)];
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['url']) {
+      this.setUrlParts(changes['url'].currentValue);
     }
-    return this.url;
   }
 
-  isExternalUrl(): boolean {
-    return typeof this.url === 'string' && this.protocolRegex.test(this.url);
+  /**
+   * The part with the path of the local url.
+   */
+  get routerUrl(): any[] {
+    return this.routeParts.path;
   }
 
-  private getAbsoluteUrl(url: string) {
-    return url.startsWith('/') ? this.url : '/' + this.url;
+  /**
+   * The part with the query params of the local url.
+   */
+  get queryParams(): Params {
+    return this.routeParts.queryParams;
+  }
+
+  /**
+   * The part with the hash fragment of the local url.
+   */
+  get fragment(): string {
+    return this.routeParts.fragment;
+  }
+
+  /**
+   * Parses the given url and sets the property `urlParts` accordingly.
+   */
+  private setUrlParts(url: string | any[]) {
+    if (typeof url === 'string') {
+      url = this.getAbsoluteUrl(url); // string links in CMS sometimes don't have the leading slash, so fix it here
+      this.routeParts = this.splitUrl(url as string);
+    } else {
+      this.routeParts = { path: url };
+    }
+  }
+
+  /**
+   * Parses the given string into 3 parts:
+   * - string path (wrapped in an array to be compatible with Angular syntax for the `routerLink`)
+   * - query params (as an object)
+   * - hash fragment (string)
+   */
+  private splitUrl(url: string = ''): RouteParts {
+    const { queryParams, fragment } = this.router.parseUrl(url);
+    const [, path] = url.match(this.URL_SPLIT);
+
+    // wrap path in an array, to have the Angular-like path format
+    return { path: [path], queryParams, fragment };
+  }
+
+  /**
+   * Prepends a leading slash to the given URL string, in case it doesn't have it.
+   */
+  private getAbsoluteUrl(url: string): string {
+    return url.startsWith('/') ? url : '/' + url;
   }
 }
