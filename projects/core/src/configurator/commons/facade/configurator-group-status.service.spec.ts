1d8453fa92ff68bf2a9f81ff7718d0be1bb2e1c6
Larisa Staroverova <larisa.staroverova@sap.com>
2020-05-29 08:47:14 +0200

    Extract test data from tests

--- projects/core/src/configurator/commons/facade/configurator-group-status.service.spec.ts
+++ projects/core/src/configurator/commons/facade/configurator-group-status.service.spec.ts
@@ -1,129 +1,22 @@
 import { async, TestBed } from '@angular/core/testing';
-import { GenericConfigurator } from '../../../model/generic-configurator.model';
-import { Configurator } from './../../../model/configurator.model';
 import { ConfiguratorGroupStatusService } from './configurator-group-status.service';
 import { ConfiguratorGroupUtilsService } from './configurator-group-utils.service';
 import { of } from 'rxjs';
-import { StateWithConfiguration, UiState } from '../store/configuration-state';
+import { StateWithConfiguration } from '../store/configuration-state';
 import { Store, StoreModule } from '@ngrx/store';
 import { Type } from '@angular/core';
 import * as UiActions from '../store/actions/configurator-ui.action';
-
-const PRODUCT_CODE = 'CONF_LAPTOP';
-const GROUP_ID_1 = '1234-56-7891';
-const GROUP_ID_2 = '1234-56-7892';
-const GROUP_ID_3 = '1234-56-7893';
-const GROUP_ID_4 = '1234-56-7894';
-const GROUP_ID_5 = '1234-56-7895';
-const GROUP_ID_6 = '1234-56-7896';
-const GROUP_ID_7 = '1234-56-7897';
-const GROUP_ID_8 = '1234-56-7898';
-const GROUP_ID_9 = '1234-56-7899';
-const GROUP_ID_10 = '1234-56-7900';
-const uiState: UiState = {
-  currentGroup: GROUP_ID_2,
-  menuParentGroup: GROUP_ID_3,
-};
-const CONFIG_ID = '1234-56-7890';
-const productConfiguration: Configurator.Configuration = {
-  configId: CONFIG_ID,
-  productCode: PRODUCT_CODE,
-  groups: [
-    {
-      id: GROUP_ID_1,
-      attributes: [
-        {
-          name: 'ATTRIBUTE_1_CHECKBOX',
-          uiType: Configurator.UiType.CHECKBOX,
-          required: true,
-          incomplete: true,
-        },
-      ],
-      subGroups: [],
-    },
-
-    {
-      id: GROUP_ID_2,
-      attributes: [
-        {
-          name: 'ATTRIBUTE_2_RADIOBUTTON',
-          uiType: Configurator.UiType.RADIOBUTTON,
-          required: false,
-          incomplete: false,
-        },
-      ],
-      subGroups: [],
-    },
-    {
-      id: GROUP_ID_3,
-      attributes: [
-        {
-          name: 'ATTRIBUTE_3_SINGLESELECTIONIMAGE',
-          uiType: Configurator.UiType.SINGLE_SELECTION_IMAGE,
-          required: true,
-          incomplete: true,
-        },
-      ],
-      subGroups: [{ id: GROUP_ID_4, subGroups: [], attributes: [] }],
-    },
-    {
-      id: GROUP_ID_5,
-      attributes: [
-        {
-          name: 'ATTRIBUTE_5_STRING',
-          uiType: Configurator.UiType.STRING,
-          required: true,
-          incomplete: false,
-        },
-        {
-          name: 'ATTRIBUTE_5_DROPDOWN',
-          uiType: Configurator.UiType.DROPDOWN,
-          required: true,
-          incomplete: true,
-        },
-      ],
-
-      subGroups: [
-        { id: GROUP_ID_6, subGroups: [], attributes: [] },
-        {
-          id: GROUP_ID_7,
-          subGroups: [{ id: GROUP_ID_8, subGroups: [], attributes: [] }],
-          attributes: [],
-        },
-      ],
-    },
-
-    {
-      id: GROUP_ID_9,
-      subGroups: [
-        {
-          id: GROUP_ID_10,
-          attributes: [
-            {
-              name: 'ATTRIBUTE_10_DROPDOWN',
-              uiType: Configurator.UiType.DROPDOWN,
-              required: true,
-              incomplete: false,
-            },
-          ],
-          subGroups: [],
-        },
-      ],
-    },
-  ],
-  flatGroups: [
-    { id: GROUP_ID_1 },
-    { id: GROUP_ID_2 },
-    { id: GROUP_ID_4 },
-    { id: GROUP_ID_6 },
-    { id: GROUP_ID_7 },
-    { id: GROUP_ID_10 },
-  ],
-  owner: {
-    id: PRODUCT_CODE,
-    type: GenericConfigurator.OwnerType.PRODUCT,
-  },
-};
+import {
+  GROUP_ID_1,
+  GROUP_ID_3,
+  GROUP_ID_4,
+  GROUP_ID_5,
+  GROUP_ID_6,
+  GROUP_ID_7,
+  GROUP_ID_8,
+  productConfiguration,
+  uiState,
+} from './configuration-test-data';
 
 describe('ConfiguratorGroupStatusService', () => {
   let classUnderTest: ConfiguratorGroupStatusService;
