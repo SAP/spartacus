0c80514db135f5c1ee467c804cd31559be91c31b
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-06-01 00:34:17 -0400

    Update order-detail-headline.component.ts

--- projects/storefrontlib/src/cms-components/myaccount/order/order-details/order-detail-headline/order-detail-headline.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/order-details/order-detail-headline/order-detail-headline.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { Order } from '@spartacus/core';
+
 import { OrderDetailsService } from '../order-details.service';
 
 @Component({
