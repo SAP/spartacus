c18382a7f065944feffeba5bbbe85af1514c238e
Larisa Staroverova <larisa.staroverova@sap.com>
2020-06-02 11:31:38 +0200

    Solve cyclic dependency

--- projects/core/src/configurator/commons/configurator-commons.module.ts
+++ projects/core/src/configurator/commons/configurator-commons.module.ts
@@ -2,13 +2,20 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ConfiguratorCommonsService } from './facade/configurator-commons.service';
 import { ConfiguratorGroupsService } from './facade/configurator-groups.service';
 import { ConfiguratorStoreModule } from './store/configurator-store.module';
+import { ConfiguratorGroupUtilsService } from './facade/configurator-group-utils.service';
+import { ConfiguratorGroupStatusService } from './facade/configurator-group-status.service';
 
 @NgModule({ imports: [ConfiguratorStoreModule] })
 export class ConfiguratorCommonsModule {
   static forRoot(): ModuleWithProviders<ConfiguratorCommonsModule> {
     return {
       ngModule: ConfiguratorCommonsModule,
-      providers: [ConfiguratorCommonsService, ConfiguratorGroupsService],
+      providers: [
+        ConfiguratorCommonsService,
+        ConfiguratorGroupsService,
+        ConfiguratorGroupUtilsService,
+        ConfiguratorGroupStatusService,
+      ],
     };
   }
 }
