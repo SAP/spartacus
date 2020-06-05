fb480b43432f8ee79b10d923050c1d38b9ec0865
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-06-05 12:34:40 -0400

    new requirements for creditcard headline markup

--- projects/assets/src/translations/en/my-account.ts
+++ projects/assets/src/translations/en/my-account.ts
@@ -2,7 +2,7 @@ export const myAccount = {
   orderDetails: {
     orderId: 'Order #',
     purchaseOrderId: 'Purchase Order #',
-    emptyPurchaseOrderId: 'None',
+    none: 'None {{value}}',
     placed: 'Placed',
     placedBy: 'Placed By',
     unit: 'Unit',
@@ -10,6 +10,7 @@ export const myAccount = {
     costCenterAndUnit: 'Cost Center / Unit',
     costCenterAndUnitValue: '{{costCenterName}} / {{unitName}}',
     payByAccount: 'Pay by Account',
+    paidByCreditCard: '(paid by credit card)',
     status: 'Status',
     shippedOn: 'Shipped on',
     deliveryStatus_IN_TRANSIT: 'In Transit',
