223bfa3191e762be651a0d0621a56ec565fc8abf
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-06-05 12:22:15 +0200

    updated script

--- projects/dev-schematics/scripts/test-dev-schematics-local.sh
+++ projects/dev-schematics/scripts/test-dev-schematics-local.sh
@@ -5,9 +5,12 @@ if [ "$1" ]; then
 fi
 
 cd ../../schematics
+yarn
 npm unpublish @spartacus/schematics --registry http://localhost:4873 --force
 yarn build && npm publish --registry http://localhost:4873
+
 cd ../dev-schematics
+yarn
 echo "@spartacus:registry=http://localhost:4873" > .npmrc
 yarn remove @spartacus/schematics
 rm -rf node_modules && yarn
