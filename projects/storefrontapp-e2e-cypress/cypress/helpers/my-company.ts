478073e19a23904932ecab77a87f83e2aa080751
Caine Rotherham <rotherham.c@gmail.com>
2020-06-08 12:21:39 +0200

    push

--- projects/storefrontapp-e2e-cypress/cypress/helpers/my-company.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/my-company.ts
@@ -1,22 +1,18 @@
 import { CONTEXT_URL_EN_USD } from './site-context-selector';
 
-export function testMyCompanyListFromConfig(config) {
+export function testListFromConfig(config) {
   context(`${config.navLink} List`, () => {
-    it('should navigate to list', () => {
-      cy.get('cx-navigation-ui.companyNavComponent').within(() => {
-        cy.get('a').contains(config.navLink).click({ force: true });
-      });
-
+    it('should show list', () => {
       cy.get(config.listSelector).within(() => {
         const defaultSort = config.sorts.find((sort) => sort.default);
-        cy.url().should('contain', `${config.url}${defaultSort.urlParams}`);
+        cy.url().should('contain', `${config.url}s${defaultSort.urlParams}`);
         cy.get('h3').should('contain.text', config.pageTitle);
         cy.get('a')
           .contains(config.createBtn.text)
           .parent()
           .should(
             'contain.html',
-            `href="${config.url}${config.createBtn.link}"`
+            `href="${config.url}s${config.createBtn.link}"`
           );
         cy.get('cx-sorting .ng-select').should(
           'contain.text',
@@ -33,12 +29,366 @@ export function testMyCompanyListFromConfig(config) {
       cy.get(config.listSelector).within(() => {
         config.sorts.forEach((sort) => {
           ngSelect(sort.value);
-          cy.url().should('contain', `${config.url}${sort.urlParams}`);
+          cy.url().should('contain', `${config.url}s${sort.urlParams}`);
           cy.get('cx-table').within(() => {
             checkRowHeaders(config.rowHeaders);
             checkRows(config.rows, sort.rowOrder);
           });
         });
+      });
+    });
+  });
+}
+
+export function testDetailsFromConfig(config) {
+  context(`${config.navLink} Details`, () => {
+    it('should show details', () => {
+      cy.get(config.listSelector).within(() => {
+        cy.get('a')
+          .contains(`${config.details.entity.id}`)
+          .click({ force: true });
+      });
+
+      cy.url().should('contain', `${config.url}/${config.details.entity.id}`);
+
+      cy.get(config.detailsSelector).within(() => {
+        cy.get('h3').should(
+          'contain.text',
+          `Details for ${config.details.entity.id}`
+        );
+        cy.get('div.row')
+          .should('contain.text', 'ID')
+          .and('contain.text', config.details.entity.id);
+        if (config.details.entity.parentUnit) {
+          cy.get('div.row')
+            .should('contain.text', 'Parent Unit')
+            .and('contain.text', config.details.entity.parentUnit)
+            .and(
+              'contain.html',
+              `href="${CONTEXT_URL_EN_USD}/organization/unit/${config.details.entity.parentUnit}"`
+            );
+        }
+        cy.get('div.row')
+          .should('contain.text', 'Name')
+          .and('contain.text', config.details.entity.name);
+
+        cy.get('a')
+          .contains(config.details.editBtn.text)
+          .parent()
+          .should(
+            'contain.html',
+            `href="${config.url}${config.details.editBtn.link}"`
+          );
+
+        cy.get('a')
+          .contains('Back to list')
+          .parent()
+          .should('contain.html', `href="${config.url}s"`);
+
+        cy.get('cx-fake-tabs-desktop').within(() => {
+          config.details.tabs.forEach((tab) => {
+            cy.get('a')
+              .contains(tab.label)
+              .parent()
+              .and('contain.html', `href="${config.url}${tab.link}"`);
+          });
+        });
+      });
+    });
+  });
+}
+
+export function testCreateUpdateFromConfig(config) {
+  context(`${config.navLink} Create / Update`, () => {
+    it(`should create`, () => {
+      cy.get(config.listSelector).within(() => {
+        cy.get('a').contains(config.createBtn.text).click();
+      });
+
+      cy.url().should('contain', `${config.url}s/create`);
+
+      cy.get(config.create.selector).within(() => {
+        cy.get('h3').should('contain.text', config.create.header);
+
+        cy.get(config.form.selector).within(() => {
+          cy.get('label').should('have.length', config.form.inputs.length);
+
+          // cy.get('cx-form-errors')
+          //   .contains('This field is required')
+          //   .should('not.be.visible');
+          // cy.get('button').contains(config.create.header).click();
+          // cy.get('cx-form-errors')
+          //   .should('contain.text', 'This field is required')
+          //   .and('be.visible')
+          //   .and('have.length', config.form.inputs.length);
+
+          config.form.inputs.forEach((input) => {
+            cy.get('label')
+              .contains(input.label)
+              .parent()
+              .within(() => {
+                switch (input.type) {
+                  case 'text':
+                    cy.get('input[type="text"]').type(input.value);
+                    break;
+                  case 'ngSelect':
+                    ngSelect(input.value);
+                    break;
+                }
+              });
+          });
+
+          cy.get('button').contains(config.create.header).click();
+        });
+      });
+
+      cy.url().should(
+        'contain',
+        `${config.url}/${config.form.inputs[0].value}`
+      );
+
+      cy.get(config.detailsSelector).within(() => {
+        cy.get('h3').should(
+          'contain.text',
+          `Details for ${config.form.inputs[0].value}`
+        );
+
+        if (config.details.entity.parentUnit) {
+          cy.get('div.row')
+            .should('contain.text', 'Parent Unit')
+            .and('contain.text', config.form.inputs[2].value)
+            .and(
+              'contain.html',
+              `href="${CONTEXT_URL_EN_USD}/organization/unit/${config.form.inputs[2].link}"`
+            );
+        }
+        cy.get('div.row')
+          .should('contain.text', 'Name')
+          .and('contain.text', config.form.inputs[1].value);
+
+        cy.get('a')
+          .contains(config.details.editBtn.text)
+          .parent()
+          .should(
+            'contain.html',
+            `href="${config.url}/edit/${config.form.inputs[0].value}"`
+          );
+
+        cy.get('a')
+          .contains('Back to list')
+          .parent()
+          .should('contain.html', `href="${config.url}s"`)
+          .click();
+      });
+
+      cy.url().should('contain', `${config.url}`);
+
+      cy.get(config.listSelector).within(() => {
+        // const defaultSort = config.sorts.find((sort) => sort.default);
+        cy.get('h3').should('contain.text', config.pageTitle);
+        cy.get('cx-table').within(() => {
+          cy.get('tr').should('have.length', 5);
+          const rowsWithNewEntity = [
+            ...config.rows,
+            {
+              text: ['test-user-group', 'Test User Group', 'Custom Retail'],
+              links: [
+                '/organization/user-group/test-user-group',
+                null,
+                '/organization/unit/Custom%20Retail',
+              ],
+            },
+          ];
+          const newOrder = [0, 1, 2, 4, 3];
+          checkRowHeaders(config.rowHeaders);
+          checkRows(rowsWithNewEntity, newOrder);
+        });
+      });
+    });
+
+    it(`should update`, () => {
+      cy.get(config.listSelector).within(() => {
+        cy.get('a')
+          .contains(`${config.form.inputs[0].value}`)
+          .click({ force: true });
+      });
+
+      cy.url().should(
+        'contain',
+        `${config.url}/${config.form.inputs[0].value}`
+      );
+
+      cy.get(config.detailsSelector).within(() => {
+        cy.get('h3').should(
+          'contain.text',
+          `Details for ${config.form.inputs[0].value}`
+        );
+
+        cy.get('a').contains(config.details.editBtn.text).click();
+      });
+
+      cy.get(config.edit.selector).within(() => {
+        cy.get('h3').should('contain.text', config.edit.header);
+
+        cy.get(config.form.selector).within(() => {
+          cy.get('label').should('have.length', config.edit.inputs.length);
+
+          // cy.get('cx-form-errors')
+          //   .contains('This field is required')
+          //   .should('not.be.visible');
+          // cy.get('button').contains(config.create.header).click();
+          // cy.get('cx-form-errors')
+          //   .should('contain.text', 'This field is required')
+          //   .and('be.visible')
+          //   .and('have.length', config.form.inputs.length);
+
+          config.edit.inputs.forEach((input) => {
+            cy.get('label')
+              .contains(input.label)
+              .parent()
+              .within(() => {
+                switch (input.type) {
+                  case 'text':
+                    cy.get('input[type="text"]').clear().type(input.value);
+                    break;
+                  case 'ngSelect':
+                    ngSelect(input.value);
+                    break;
+                }
+              });
+          });
+
+          cy.get('button').contains(config.edit.btn).click();
+        });
+      });
+
+      cy.url().should(
+        'contain',
+        `${config.url}/${config.edit.inputs[0].value}`
+      );
+
+      cy.get(config.detailsSelector).within(() => {
+        cy.get('h3').should(
+          'contain.text',
+          `Details for ${config.edit.inputs[0].value}`
+        );
+
+        cy.get('div.row')
+          .should('contain.text', 'ID')
+          .and('contain.text', config.edit.inputs[0].value);
+
+        if (config.details.entity.parentUnit) {
+          cy.get('div.row')
+            .should('contain.text', 'Parent Unit')
+            .and('contain.text', config.edit.inputs[2].value)
+            .and(
+              'contain.html',
+              `href="${CONTEXT_URL_EN_USD}/organization/unit/${config.edit.inputs[2].link}"`
+            );
+        }
+        cy.get('div.row')
+          .should('contain.text', 'Name')
+          .and('contain.text', config.edit.inputs[1].value);
+
+        cy.get('a')
+          .contains(config.details.editBtn.text)
+          .parent()
+          .should(
+            'contain.html',
+            `href="${config.url}/edit/${config.edit.inputs[0].value}"`
+          );
+
+        cy.get('a')
+          .contains('Back to list')
+          .parent()
+          .should('contain.html', `href="${config.url}s"`)
+          .click();
+      });
+
+      cy.url().should('contain', `${config.url}s`);
+
+      cy.get(config.listSelector).within(() => {
+        // const defaultSort = config.sorts.find((sort) => sort.default);
+        cy.get('h3').should('contain.text', config.pageTitle);
+        cy.get('cx-table').within(() => {
+          cy.get('tr').should('have.length', 5);
+          const rowsWithNewEntity = [
+            ...config.rows,
+            {
+              text: [
+                config.edit.inputs[0].value,
+                config.edit.inputs[1].value,
+                config.edit.inputs[2].value,
+              ],
+              links: [
+                `/organization/user-group/{${config.edit.inputs[0].value}`,
+                null,
+                config.edit.inputs[2].link,
+              ],
+            },
+          ];
+          const newOrder = [0, 1, 2, 4, 3];
+          checkRowHeaders(config.rowHeaders);
+          checkRows(rowsWithNewEntity, newOrder);
+        });
+      });
+    });
+
+    after(() => {
+      cleanUp(config);
+    });
+  });
+}
+
+export function testAssignmentFromConfig(config) {
+  context(`${config.navLink} Assignment`, () => {
+    config.details.tabs.forEach((tab) => {
+      it(`should ${tab.label}`, () => {
+        cy.get(config.listSelector).within(() => {
+          cy.get('a')
+            .contains(`${config.details.entity.id}`)
+            .click({ force: true });
+        });
+
+        cy.get('cx-fake-tabs').within(() => {
+          cy.get('a').contains(tab.label).click({ force: true });
+        });
+
+        cy.url().should('contain', tab.link);
+
+        // it('should show list', () => {
+        cy.get(tab.selector).within(() => {
+          if (tab.sorts) {
+            const defaultSort = tab.sorts.find((sort) => sort.default);
+            cy.url().should('contain', `${tab.link}${defaultSort.urlParams}`);
+            cy.get('h3').should('contain.text', tab.label);
+            cy.get('cx-sorting .ng-select').should(
+              'contain.text',
+              defaultSort.value
+            );
+            cy.get('cx-table').within(() => {
+              checkRowHeaders(tab.rowHeaders);
+              checkRows(tab.rows, defaultSort.rowOrder);
+            });
+          }
+        });
+        // });
+
+        // it('should sort table data', () => {
+        cy.get(tab.selector).within(() => {
+          tab.sorts?.forEach((sort) => {
+            ngSelect(sort.value);
+            cy.url().should('contain', `${tab.link}${sort.urlParams}`);
+            cy.get('cx-table').within(() => {
+              checkRowHeaders(tab.rowHeaders);
+              checkRows(tab.rows, sort.rowOrder);
+            });
+          });
+        });
+        // });
+
+        cy.get('a').contains('Back to list').click();
+        cy.url().should('contain', config.url);
       });
     });
   });
@@ -58,14 +408,16 @@ function checkRows(rows, order: number[]): void {
       .eq(j)
       .within(() => {
         for (let i = 0; i < row.text.length; i++) {
-          cy.get('td').eq(i).should('contain.text', row.text[i]);
-          if (row.links && row.links[i]) {
-            cy.get('td')
-              .eq(i)
-              .should(
-                'contain.html',
-                `href="${CONTEXT_URL_EN_USD}${row.links[i]}"`
-              );
+          if (row.text[i]) {
+            cy.get('td').eq(i).should('contain.text', row.text[i]);
+            if (row.links && row.links[i]) {
+              cy.get('td')
+                .eq(i)
+                .should(
+                  'contain.html',
+                  `href="${CONTEXT_URL_EN_USD}${row.links[i]}"`
+                );
+            }
           }
         }
       });
@@ -74,9 +426,27 @@ function checkRows(rows, order: number[]): void {
 }
 
 function ngSelect(sortKey: string): void {
-  cy.get('cx-sorting .ng-select').click();
-  cy.get('cx-sorting .ng-select .ng-dropdown-panel-items')
+  cy.get('.ng-select').click();
+  cy.get('.ng-select .ng-dropdown-panel-items')
     .contains(new RegExp(`^${sortKey}$`, 'g'))
     .click({ force: true });
   cy.wait(1000);
+}
+
+function cleanUp(config) {
+  const userToken = `${
+    JSON.parse(localStorage.getItem('spartacus-local-data')).auth.userToken
+      .token.access_token
+  }`;
+  cy.request({
+    method: 'DELETE',
+    url: `${Cypress.env('API_URL')}${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+      'BASE_SITE'
+    )}/users/current/orgUnitUserGroups/${config.edit.inputs[0].value}`,
+    headers: {
+      Authorization: `bearer ${userToken}`,
+    },
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+  });
 }
--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-company/user-groups.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-company/user-groups.e2e-spec.ts
@@ -1,9 +1,9 @@
-import { testMyCompanyListFromConfig } from '../../../helpers/my-company';
+import * as myCompany from '../../../helpers/my-company';
 import { CONTEXT_URL_EN_USD } from '../../../helpers/site-context-selector';
 
 const config = {
   navLink: 'User Groups',
-  url: `${CONTEXT_URL_EN_USD}/organization/user-groups`,
+  url: `${CONTEXT_URL_EN_USD}/organization/user-group`,
   pageTitle: 'User Group Management',
   createBtn: {
     text: 'Create new user group',
@@ -63,10 +63,228 @@ const config = {
       default: true,
     },
   ],
+  details: {
+    entity: {
+      id: 'limitedPermissions',
+      parentUnit: 'Rustic',
+      name: 'Limited Permissions',
+    },
+    editBtn: {
+      text: 'Edit',
+      link: '/edit/limitedPermissions',
+    },
+    tabs: [
+      {
+        label: 'Details',
+        link: `/limitedPermissions`,
+        selector: 'cx-user-group-details',
+      },
+      {
+        label: 'Purchase limits',
+        link: `/purchase-limits/limitedPermissions`,
+        selector: 'cx-user-group-permissions',
+        rowHeaders: [
+          'Code',
+          'Type',
+          'Threshold Value',
+          'Time Period',
+          'Parent Unit',
+        ],
+        rows: [
+          {
+            text: [
+              'Rustic_0K_USD_ORDER',
+              'Allowed Order Threshold (per order)',
+              '0 $',
+              null,
+              'Rustic',
+            ],
+            links: [
+              '/organization/purchase-limit/Rustic_0K_USD_ORDER',
+              null,
+              null,
+              null,
+              '/organization/unit/Rustic',
+            ],
+          },
+          {
+            text: [
+              'Rustic_10K_USD_MONTH',
+              'Allowed Order Threshold (per timespan)',
+              '10000 $',
+              'MONTH',
+              'Rustic',
+            ],
+            links: [
+              '/organization/purchase-limit/Rustic_10K_USD_MONTH',
+              null,
+              null,
+              null,
+              '/organization/unit/Rustic',
+            ],
+          },
+          {
+            text: [
+              'Rustic_15K_USD_ORDER',
+              'Allowed Order Threshold (per order)',
+              '15000 $',
+              null,
+              'Rustic',
+            ],
+            links: [
+              '/organization/purchase-limit/Rustic_15K_USD_ORDER',
+              null,
+              null,
+              null,
+              '/organization/unit/Rustic',
+            ],
+          },
+          {
+            text: [
+              'Rustic_25K_USD_MONTH',
+              'Allowed Order Threshold (per timespan)',
+              '25000 $',
+              'MONTH',
+              'Rustic',
+            ],
+            links: [
+              '/organization/purchase-limit/Rustic_25K_USD_MONTH',
+              null,
+              null,
+              null,
+              '/organization/unit/Rustic',
+            ],
+          },
+        ],
+        sorts: [
+          {
+            urlParams: '?sort=byUnitName',
+            value: 'Unit Name',
+            rowOrder: [3, 0, 1, 2],
+          },
+          {
+            urlParams: '',
+            value: 'Name',
+            rowOrder: [0, 1, 2, 3],
+            default: true,
+          },
+        ],
+      },
+      {
+        label: 'Users',
+        link: `/users/limitedPermissions`,
+        selector: 'cx-user-group-users',
+        rowHeaders: ['Email', 'Name', 'Unit'],
+        rows: [
+          {
+            text: ['akiro.nakamura@rustic-hw.com', 'Akiro Nakamura', 'Rustic'],
+            links: [
+              '/organization/user/08ecc0b1-16ef-4a74-a1dd-4a244300c974',
+              null,
+              '/organization/unit/Rustic',
+            ],
+          },
+          {
+            text: [
+              'alejandro.navarro@rustic-hw.com',
+              'Alejandro Navarro',
+              'Services East',
+            ],
+            links: [
+              '/organization/user/0db38452-5b78-45af-ba26-6cfa20090d8d',
+              null,
+              '/organization/unit/Services%20East',
+            ],
+          },
+          {
+            text: [
+              'anthony.lombardi@rustic-hw.com',
+              'Anthony Lombardi',
+              'Custom Retail',
+            ],
+            links: [
+              '/organization/user/f455605a-0a14-48aa-a48d-a7842337aba9',
+              null,
+              '/organization/unit/Custom%20Retail',
+            ],
+          },
+          {
+            text: [
+              'william.hunter@rustic-hw.com',
+              'William Hunter',
+              'Custom Retail',
+            ],
+            links: [
+              '/organization/user/2b1d2812-e5e5-47f4-accf-96e67b76d4e7',
+              null,
+              '/organization/unit/Custom%20Retail',
+            ],
+          },
+        ],
+        sorts: [
+          {
+            urlParams: '?sort=byUnit',
+            value: 'Unit Name',
+            rowOrder: [3, 2, 0, 1],
+          },
+          {
+            urlParams: '',
+            value: 'Name',
+            rowOrder: [0, 1, 2, 3],
+            default: true,
+          },
+        ],
+      },
+    ],
+  },
+  detailsSelector: 'cx-user-group-details',
+  create: {
+    selector: 'cx-user-group-create',
+    header: 'Create User Group',
+    entity: {
+      id: 'test-user-group',
+    },
+  },
+  form: {
+    selector: 'cx-user-group-form',
+    inputs: [
+      {
+        label: 'User Group ID',
+        type: 'text',
+        value: 'test-user-group',
+      },
+      { label: 'User Group name', type: 'text', value: 'Test User Group' },
+      {
+        label: 'Parent business unit',
+        type: 'ngSelect',
+        value: 'Custom Retail',
+        link: 'Custom%20Retail',
+      },
+    ],
+  },
+  edit: {
+    selector: 'cx-user-group-edit',
+    header: 'Edit User Group',
+    btn: 'Update User Group',
+    inputs: [
+      {
+        label: 'User Group ID',
+        type: 'text',
+        value: 'edited-user-group',
+      },
+      { label: 'User Group name', type: 'text', value: 'Edited User Group' },
+      {
+        label: 'Parent business unit',
+        type: 'ngSelect',
+        value: 'Rustic',
+        link: 'Rustic',
+      },
+    ],
+  },
 };
 
 describe(`My Company - ${config.navLink}`, () => {
-  before(() => {
+  beforeEach(() => {
     cy.requireLoggedIn({
       user: 'linda.wolf@rustic-hw.com',
       registrationData: {
@@ -77,8 +295,11 @@ describe(`My Company - ${config.navLink}`, () => {
         email: 'linda.wolf@rustic-hw.com',
       },
     });
-    cy.visit(`/`);
+    cy.visit(`${config.url}s`);
   });
 
-  testMyCompanyListFromConfig(config);
+  // myCompany.testListFromConfig(config);
+  // myCompany.testDetailsFromConfig(config);
+  // myCompany.testCreateUpdateFromConfig(config);
+  myCompany.testAssignmentFromConfig(config);
 });
