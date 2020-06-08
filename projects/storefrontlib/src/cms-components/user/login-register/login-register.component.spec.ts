4b11567dc63c7f1cc1c10a70ee393f28a2b4bd25
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-06-08 18:09:11 -0400

    GH-7785 Fixing unit test

--- projects/storefrontlib/src/cms-components/user/login-register/login-register.component.spec.ts
+++ projects/storefrontlib/src/cms-components/user/login-register/login-register.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginRegisterComponent } from './login-register.component';
+import { I18nTestingModule } from '@spartacus/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 describe('LoginRegisterComponent', () => {
   let component: LoginRegisterComponent;
   let fixture: ComponentFixture<LoginRegisterComponent>;
 
+  @Pipe({
+    name: 'cxUrl',
+  })
+  class MockUrlPipe implements PipeTransform {
+    transform() {}
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginRegisterComponent ]
-    })
-    .compileComponents();
+      imports: [
+        I18nTestingModule
+      ],
+      declarations: [LoginRegisterComponent, MockUrlPipe],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +30,7 @@ describe('LoginRegisterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
