a313dafd6035501fd0d878ee959c9ca0c46504c1
Mateusz Kolasa <mateusz.kolasa@sap.com>
2020-06-09 10:25:14 +0200

    Add unit test for mobile and adapt checkbox view

--- projects/storefrontlib/src/shared/components/table/table.component.ts
+++ projects/storefrontlib/src/shared/components/table/table.component.ts
@@ -19,6 +19,7 @@ export class TableComponent {
   tableData: Array<any>;
 
   @Output() check = new EventEmitter<{ key: string; row: any }>();
+
   @Output() uncheck = new EventEmitter<{ key: string; row: any }>();
 
   onToggle(event, row, key) {
