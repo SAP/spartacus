f805c27f91eec4cf826befe391433ed541a894f2
schuppell <51401884+schuppell@users.noreply.github.com>
2020-06-09 13:24:54 +0200

    Feature/gh 6674 (#7875)
    * GH-6674: Add new component for product name
    
    * GH-6674: Add Image
    
    * GH-6674: Add animation to product titlte
    
    * GH-6674: Add details to product title component;
    
    * GH-6674: Show price as fallback when pricerange is undefined
    
    * GH-6674: Fix unit test and add test for pricerange check
    
    * GH-6674: Rename click function
    
    * GH-6674: Remove Prices
    
    Co-authored-by: d054488 <lukas.schuppel@sap.com>

---
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-product-title/config-product-title.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  ConfiguratorCommonsService,
+  Product,
+  ProductService,
+  RoutingService,
+} from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ICON_TYPE } from '../../../misc/icon/index';
+import { ConfigRouterExtractorService } from '../../generic/service/config-router-extractor.service';
+
+@Component({
+  selector: 'cx-config-product-title',
+  templateUrl: './config-product-title.component.html',
+})
+export class ConfigProductTitleComponent implements OnInit {
+  product$: Observable<Product>;
+  showMore = false;
+  iconTypes = ICON_TYPE;
+
+  constructor(
+    private routingService: RoutingService,
+    private configuratorCommonsService: ConfiguratorCommonsService,
+    private configRouterExtractorService: ConfigRouterExtractorService,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.product$ = this.configRouterExtractorService
+      .extractRouterData(this.routingService)
+      .pipe(
+        switchMap((routerData) =>
+          this.configuratorCommonsService.getConfiguration(routerData.owner)
+        ),
+        switchMap((configuration) =>
+          this.productService.get(configuration.productCode)
+        )
+      );
+  }
+
+  triggerDetails() {
+    this.showMore = !this.showMore;
+  }
+
+  getProductImageURL(product: Product): string {
+    return product.images?.PRIMARY?.['thumbnail']?.url;
+  }
+
+  getProductImageAlt(product: Product): string {
+    return product.images?.PRIMARY?.['thumbnail']?.altText;
+  }
+}
