2b94cbc7d5b9f116b584e76c84b71d666ec423e8
Mateusz Kolasa <mateusz.kolasa@sap.com>
2020-06-09 12:27:07 +0200

    Add button template

--- projects/storefrontlib/src/shared/components/table/table.component.ts
+++ projects/storefrontlib/src/shared/components/table/table.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ICON_TYPE } from './../../../cms-components/misc/icon/icon.model';
 
 export interface Column {
   key: string;
   value: string;
   cxRoute?: string;
   checkBox?: boolean;
+  button?: boolean;
 }
 
 @Component({
@@ -12,6 +14,8 @@ export interface Column {
   templateUrl: './table.component.html',
 })
 export class TableComponent {
+  iconTypes = ICON_TYPE;
+
   @Input()
   columns: Array<Column>;
 
@@ -22,11 +26,19 @@ export class TableComponent {
 
   @Output() uncheck = new EventEmitter<{ key: string; row: any }>();
 
+  @Output() buttonClick = new EventEmitter<{ key: string; row: any }>();
+
   onToggle(event, row, key) {
     if (event.target.checked) {
       this.check.emit({ key, row });
     } else {
       this.uncheck.emit({ key, row });
+    }
+  }
+
+  onClick(event, row, key) {
+    if (event.target) {
+      this.buttonClick.emit({ key, row });
     }
   }
 }
