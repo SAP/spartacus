7b9d61c49027796c79db6dfb93b8df7223fc017e
Alfons Bruckl <albrcmon@inf.upv.es>
2020-06-10 14:30:55 +0100

    Fixing id translation issue

--- projects/core/src/organization/facade/organization-meta.resolver.ts
+++ projects/core/src/organization/facade/organization-meta.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, Observable, of } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, map, switchMap, mergeMap } from 'rxjs/operators';
 import { CmsService } from '../../cms/facade/cms.service';
 import { BreadcrumbMeta, Page } from '../../cms/model/page.model';
 import { PageMetaResolver } from '../../cms/page/page-meta.resolver';
@@ -12,6 +12,7 @@ import { TranslationService } from '../../i18n/translation.service';
 import { PageType } from '../../model/cms.model';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { RoutingService } from '../../routing/facade/routing.service';
+
 /**
  * Resolves the page data for Organization Pages.
  *
@@ -23,12 +24,12 @@ import { RoutingService } from '../../routing/facade/routing.service';
 export class OrganizationMetaResolver extends PageMetaResolver
   implements PageTitleResolver, PageBreadcrumbResolver {
   protected organizationPage$: Observable<
-    UrlSegment | Page | string
+    UrlSegment
   > = this.cms.getCurrentPage().pipe(
     filter(Boolean),
     switchMap((page: Page) =>
       // checking the template to make sure it's a 'my company' page
-      this.isCompanyPage(page) ? this.route.snapshot.children[0].url : of(page)
+      this.isCompanyPage(page) ? this.route.snapshot.children[0].url : null
     )
   );
 
@@ -44,11 +45,15 @@ export class OrganizationMetaResolver extends PageMetaResolver
   }
 
   resolveTitle(): Observable<string> {
-    return this.organizationPage$.pipe(
-      filter((urlSegment: UrlSegment) => !!urlSegment),
-      switchMap(
-        (url: UrlSegment) =>
-          this.translation.translate(`breadcrumbs.${url.path}`) || url.path
+    return combineLatest([
+      this.organizationPage$,
+      this.routingService.getRouterState(),
+    ]).pipe(
+      filter(([url, routerState]) => Boolean(url) && Boolean(routerState)),
+      mergeMap(([url, routerState]) =>
+        Object.keys(routerState.state.params).length
+          ? of(url.path)
+          : this.translation.translate(`breadcrumbs.${url.path}`)
       )
     );
   }
