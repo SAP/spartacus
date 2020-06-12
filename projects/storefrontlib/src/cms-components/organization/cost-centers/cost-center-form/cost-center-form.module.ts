2e09f452f19b0212dd278b9f249b4f613596cb67
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-06-12 11:55:35 +0200

    renamed the folder

--- projects/storefrontlib/src/cms-components/organization/cost-centers/cost-center-form/cost-center-form.module.ts
+++ projects/storefrontlib/src/cms-components/organization/cost-centers/cost-center-form/cost-center-form.module.ts
@@ -5,8 +5,8 @@ import { RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import { FormErrorsModule } from '../../../../shared/components/form/form-errors/form-errors.module';
-import { CurrencyCommonFormModule } from '../../common/currency/currency-common-form.module';
-import { UnitCommonFormModule } from '../../common/unit/unit-common-form.module';
+import { CurrencyCommonFormModule } from '../../form-commons/currency/currency-common-form.module';
+import { UnitCommonFormModule } from '../../form-commons/unit/unit-common-form.module';
 import { CostCenterFormComponent } from './cost-center-form.component';
 
 @NgModule({
