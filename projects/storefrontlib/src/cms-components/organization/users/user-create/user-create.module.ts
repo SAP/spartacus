bc67fa8076bcd5c74eed86a46c8c83f981f13155
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2020-06-12 17:20:34 +0200

    GH-6540: Fix user create route config

--- projects/storefrontlib/src/cms-components/organization/users/user-create/user-create.module.ts
+++ projects/storefrontlib/src/cms-components/organization/users/user-create/user-create.module.ts
@@ -8,10 +8,21 @@ import {
 } from '@spartacus/core';
 import { B2BUserCreateComponent } from './user-create.component';
 import { B2BUserFormModule } from '../user-form/user-form.module';
+import { RouterModule } from '@angular/router';
+import { CmsPageGuard } from '../../../../cms-structure/guards/cms-page.guard';
+import { PageLayoutComponent } from '../../../../cms-structure/page/page-layout/page-layout.component'
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule.forChild([
+      {
+        path: null,
+        canActivate: [CmsPageGuard],
+        component: PageLayoutComponent,
+        data: { cxRoute: 'userCreate' },
+      },
+    ]),
     ConfigModule.withConfig(<CmsConfig>{
       cmsComponents: {
         UserCreateComponent: {
