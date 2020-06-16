dd79f8f6e782db37a616e1df5746171282de39cd
Michał Gruca <pucek9@gmail.com>
2020-06-16 22:24:02 +0200

    up

--- projects/storefrontlib/src/cms-components/organization/users/index.ts
+++ projects/storefrontlib/src/cms-components/organization/users/index.ts
@@ -2,7 +2,6 @@ export * from './user-create/user-create.module';
 export * from './user-edit/user-edit.module';
 export * from './user-details/user-details.module';
 export * from './user-list/user-list.module';
-export * from './user-permissions/user-permissions.module';
 export * from './user-approvers/user-approvers.module';
 export * from './user-assign-approvers/user-assign-approvers.module';
 export * from './user-assign-permissions/user-assign-permissions.module';
