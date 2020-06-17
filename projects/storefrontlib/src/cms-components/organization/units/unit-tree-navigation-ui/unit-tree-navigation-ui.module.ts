3ebe47f967c32708fd1726e967600643d9601221
Patryk Smul <psmul@divante.pl>
2020-06-17 14:09:48 +0200

    fix: import

--- projects/storefrontlib/src/cms-components/organization/units/unit-tree-navigation-ui/unit-tree-navigation-ui.module.ts
+++ projects/storefrontlib/src/cms-components/organization/units/unit-tree-navigation-ui/unit-tree-navigation-ui.module.ts
@@ -1,20 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { I18nModule, UrlModule } from '@spartacus/core';
-import { UnitTreeNavigationUIComponent } from './unit-tree-navigation-ui.component';
 import { GenericLinkModule } from '../../../../shared/components/generic-link';
 import { IconModule } from '../../../misc/icon/index';
+import { UnitTreeNavigationUIComponent } from './unit-tree-navigation-ui.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    UrlModule,
-    I18nModule,
-    IconModule,
-    GenericLinkModule,
-  ],
+  imports: [CommonModule, UrlModule, I18nModule, IconModule, GenericLinkModule],
   declarations: [UnitTreeNavigationUIComponent],
   exports: [UnitTreeNavigationUIComponent],
   providers: [],
