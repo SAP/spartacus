232d0caf221b3f975699d229c47e5eddc374c887
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-06-18 08:26:19 +0200

    normalize boolean (string) values ("true" or "false")

--- projects/core/src/occ/adapters/organization/converters/occ-cost-center-normalizer.ts
+++ projects/core/src/occ/adapters/organization/converters/occ-cost-center-normalizer.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Occ } from '../../../occ-models/occ.models';
-import { Converter } from '../../../../util/converter.service';
 import { CostCenter } from '../../../../model/org-unit.model';
+import { Converter } from '../../../../util/converter.service';
+import { Occ } from '../../../occ-models/occ.models';
 
 @Injectable()
 export class OccCostCenterNormalizer
@@ -10,8 +10,17 @@ export class OccCostCenterNormalizer
     if (target === undefined) {
       target = {
         ...(source as any),
+        active: this.normalizeBoolean(source.active),
       };
     }
     return target;
+  }
+
+  /**
+   * Returns the boolean value for a string property that is supposed
+   * to be of type boolean.
+   */
+  protected normalizeBoolean(property: string | boolean): boolean {
+    return typeof property === 'string' ? property === 'true' : property;
   }
 }
