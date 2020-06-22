5cd90f092846b81b8ab2575cf949aa75b6bf54bf
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2020-06-22 15:51:59 -0400

    GH-7953: Styling changes

--- projects/storefrontlib/src/shared/components/image-zoom/dialog/image-zoom-dialog.component.ts
+++ projects/storefrontlib/src/shared/components/image-zoom/dialog/image-zoom-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { ICON_TYPE } from '../../../../cms-components/misc/icon/icon.model';
 import { FocusConfig } from '../../../../layout/a11y/index';
 import { LaunchDialogService } from '../../../../layout/launch-dialog/services/index';
@@ -17,7 +17,18 @@ export class ImageZoomDialogComponent {
     focusOnEscape: true,
   };
 
-  constructor(protected launchDialogService: LaunchDialogService) {}
+  @HostListener('click', ['$event'])
+  handleClick(event: UIEvent): void {
+    // Close on click outside the dialog window
+    if ((event.target as any).tagName === this.el.nativeElement.tagName) {
+      this.close('Cross click');
+    }
+  }
+
+  constructor(
+    protected launchDialogService: LaunchDialogService,
+    protected el: ElementRef
+  ) {}
 
   close(reason = ''): void {
     this.launchDialogService.closeDialog(reason);
