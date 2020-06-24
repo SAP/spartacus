413e78f487c4353b2e3a9cef746dd3ad69819ca7
Alexis Phanor <alexis.phanor@gmail.com>
2020-06-24 10:25:14 +0100

    Added latest changes to my unit test

--- projects/storefrontlib/src/cms-components/organization/units/unit-tree-navigation-ui/unit-tree-navigation-ui.component.spec.ts
+++ projects/storefrontlib/src/cms-components/organization/units/unit-tree-navigation-ui/unit-tree-navigation-ui.component.spec.ts
@@ -9,11 +9,20 @@ import { BreakpointService } from '../../../../layout/breakpoint/breakpoint.serv
 import { BREAKPOINT } from '../../../../layout/config/layout-config';
 import { of, Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
-import { Pipe } from '@angular/core';
+import { I18nTestingModule, CmsNavigationComponent } from '@spartacus/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { NavigationNode } from '../../../navigation/navigation/navigation-node.model';
+import { CmsComponentData } from 'projects/storefrontlib/src/cms-structure';
 
-@Pipe({
-    name: 'cxTranslateUrl',
+@Component({
+    template: '',
+    selector: 'cx-unit-tree-navigation-ui'
 })
+
+class MockNavigationComponent {
+    @Input() node: NavigationNode;
+    @Input() defaultExpandLevel: number;
+}
 
 class MockBreakpointService {
     get breakpoint$(): Observable<BREAKPOINT> {
@@ -39,44 +48,119 @@ class MockBreakpointService {
     }
 }
 
+const mockNode: NavigationNode = {
+    title: 'test',
+    children: [
+      {
+        title: 'Root 1',
+        url: '/root-1',
+        children: [
+          {
+            title: 'Child 1',
+            children: [
+              {
+                title: 'Sub child 1',
+                children: [
+                  {
+                    title: 'Sub sub child 1',
+                    url: '/sub-sub-child-1',
+                  },
+                  {
+                    title: 'Sub sub child 1',
+                    url: '/sub-sub-child-1',
+                  },
+                  {
+                    title: 'Sub sub child 1',
+                    url: '/sub-sub-child-1',
+                  },
+                  {
+                    title: 'Sub sub child 1',
+                    url: '/sub-sub-child-1',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            title: 'Child 2',
+            url: '/child-2',
+          },
+        ],
+      },
+      {
+        title: 'Root 2',
+        url: '/root-2',
+      },
+    ],
+  };
+
 describe('UnitTreeNavigationUIComponent', () => {
     let component : UnitTreeNavigationUIComponent;
     let fixture: ComponentFixture<UnitTreeNavigationUIComponent>;
-    let breakpointService : MockBreakpointService;
+    // let breakpointService : MockBreakpointService;
+
+    const mockCmsComponentData = <CmsNavigationComponent>{
+        styleClass: 'footer-styling',
+    };
+    const MockCmsNavigationComponent = <CmsComponentData<any>>{
+        data$: of(mockCmsComponentData),
+    };
   
     beforeEach(async(() => {
       TestBed.configureTestingModule({
-        declarations: [ UnitTreeNavigationUIComponent ],
+        declarations: [ UnitTreeNavigationUIComponent, MockNavigationComponent ],
+        imports: [I18nTestingModule],
         providers: [
             UnitTreeNavigationUIComponent,
+            {
+                provide: CmsComponentData,
+                useValue: MockCmsNavigationComponent,
+              },
           {
             provide: BreakpointService,
             useClass: MockBreakpointService
           }
-        ]
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
       }).compileComponents();
-      breakpointService = TestBed.inject(BreakpointService);
+    //   breakpointService = TestBed.inject(BreakpointService);
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(UnitTreeNavigationUIComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    });
-  
-    it('should create', () => {
-        expect(component).toBeTruthy();
+
+        component.node = mockNode;
+        component.defaultExpandLevel = 1;
+        component.isMobile = true;
     });
 
-    describe('mobile (md)', () => {
-        beforeEach(() => {
-          spyOnProperty(breakpointService, 'breakpoint$').and.returnValue(
-            of(BREAKPOINT.md)
-          );
-        });
+    // afterEach(() => {
+    //     const subscription = new Subscription();
+    //     spyOn(component, 'ngOnDestroy').and.callFake(() => {
+    //         spyOn(component.subscriptions, 'unsubscribe');
+    //         component.ngOnDestroy();
+    //         expect(component.subscriptions.unsubscribe).toHaveBeenCalled();
+    //     });
+    //     fixture.destroy();
+    // });
 
-        it('breakpoint service should return true', () => {
-            expect(breakpointService.isDown(BREAKPOINT.md)).toBeTruthy();
+    describe('navigtion UI test', () => {
+        it('should create', () => {
+            expect(component).toBeTruthy();
         });
     });
+
+    // describe('mobile (md)', () => {
+    //     beforeEach(() => {
+    //       spyOnProperty(breakpointService, 'breakpoint$').and.returnValue(
+    //         of(BREAKPOINT.md)
+    //       );
+    //     });
+
+    //     it('breakpoint service should return true', () => {
+    //         expect(breakpointService.isDown(BREAKPOINT.md)).toBeTruthy();
+    //     });
+    // });
 });
