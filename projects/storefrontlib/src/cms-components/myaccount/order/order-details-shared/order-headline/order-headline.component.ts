e75b12c7a1f155ac1b4e28fe1dee76c1cc9aa604
Patrick Labadie <patrick.labadie@sap.com>
2020-06-25 15:59:14 -0400

    GH-7856: Small fixes

--- projects/storefrontlib/src/cms-components/myaccount/order/order-details-shared/order-headline/order-headline.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/order-details-shared/order-headline/order-headline.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Order } from '@spartacus/core';
-import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'cx-order-headline',
   templateUrl: './order-headline.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrderHeadlineComponent {
   @Input()
