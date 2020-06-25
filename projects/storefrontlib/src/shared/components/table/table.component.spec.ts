fd6ac4189dfd72cdb34f230895d53b235203e717
Michał Gruca <pucek9@gmail.com>
2020-06-25 02:53:04 +0200

    update test

--- projects/storefrontlib/src/shared/components/table/table.component.spec.ts
+++ projects/storefrontlib/src/shared/components/table/table.component.spec.ts
@@ -1,7 +1,10 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { I18nTestingModule } from '@spartacus/core';
 import { TableComponent } from './table.component';
+import { Table, TableHeader } from './table.model';
+import { of } from 'rxjs';
 
 @Directive({
   selector: '[cxOutlet]',
@@ -11,13 +14,19 @@ class MockAttributesDirective {
   @Input() cxOutletContext: any;
 }
 
-// const mockDataset = {
-//   structure: {
-//     type: 'test-1',
-//     headers: [],
-//   },
-//   data$: of([]),
-// };
+const mockDataset: Table = {
+  structure: {
+    type: 'test-1',
+    headers: [],
+  },
+  data$: of([]),
+};
+
+const headers: TableHeader[] = [
+  { key: 'key1', sortCode: 'sort1' },
+  { key: 'key2', sortCode: 'sort2' },
+  { key: 'key3' },
+];
 
 describe('TableComponent', () => {
   let fixture: ComponentFixture<TableComponent>;
@@ -27,7 +36,11 @@ describe('TableComponent', () => {
     TestBed.configureTestingModule({
       imports: [I18nTestingModule],
       declarations: [TableComponent, MockAttributesDirective],
-    }).compileComponents();
+    })
+      .overrideComponent(TableComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(TableComponent);
     tableComponent = fixture.componentInstance;
@@ -39,24 +52,57 @@ describe('TableComponent', () => {
   });
 
   it('should not render a table if there is no dataset', () => {
-    // TODO
+    tableComponent.dataset = undefined;
+    fixture.detectChanges();
+    const table = fixture.debugElement.query(By.css('table'));
+    expect(table).toBeNull();
   });
 
   it('should not render a table if there is no dataset.structure', () => {
-    // TODO
+    tableComponent.dataset = {} as Table;
+    fixture.detectChanges();
+    const table = fixture.debugElement.query(By.css('table'));
+    expect(table).toBeNull();
+  });
+
+  it('should render table', () => {
+    tableComponent.dataset = mockDataset;
+    fixture.detectChanges();
+    const table = fixture.debugElement.query(By.css('table'));
+    expect(table.nativeElement).toBeTruthy();
   });
 
   it('should add the dataset.structure.type to cx-table element in devMode', () => {
-    // TODO
+    tableComponent.dataset = mockDataset;
+    expect(tableComponent.tableType).toEqual('test-1');
   });
 
   describe('table header', () => {
     it('should not add the thead when hideHeader = true', () => {
-      // TODO
+      tableComponent.dataset = {
+        ...mockDataset,
+        structure: { ...mockDataset.structure, hideHeader: true },
+      };
+      fixture.detectChanges();
+      const table = fixture.debugElement.query(By.css('table thead'));
+      expect(table).toBeNull();
     });
 
     it('should add a th for each tableHeader ', () => {
-      // TODO
+      tableComponent.dataset = {
+        ...mockDataset,
+        structure: {
+          ...mockDataset.structure,
+          headers,
+        },
+      };
+      fixture.detectChanges();
+      const table = fixture.debugElement.query(By.css('table thead'));
+      expect(table.nativeElement).toBeTruthy();
+      const th = fixture.debugElement.queryAll(By.css('table thead tr th'));
+      expect(th[0].nativeElement).toBeTruthy();
+      expect(th[1].nativeElement).toBeTruthy();
+      expect(th[2].nativeElement).toBeTruthy();
     });
 
     it('should add the header.label in the th if available ', () => {
