506f8043dd63c2e825a1dc4f55ea846d45759d71
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-06-29 21:29:12 +0200

    renamed domain to main

--- feature-libs/my-account/public_api.ts
+++ feature-libs/my-account/public_api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of my-account
  */
 export * from '@spartacus/my-account/cost-center';
-export * from '@spartacus/my-account/domain';
+export * from '@spartacus/my-account/main';
 export * from './recipes/index';
