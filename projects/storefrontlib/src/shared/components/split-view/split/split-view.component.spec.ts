efb78310876d2125910af03ff8505a9fb24a79e1
Michał Gruca <pucek9@gmail.com>
2020-06-29 21:02:52 +0200

    unexpected fdescribe -sorry

--- projects/storefrontlib/src/shared/components/split-view/split/split-view.component.spec.ts
+++ projects/storefrontlib/src/shared/components/split-view/split/split-view.component.spec.ts
@@ -10,7 +10,7 @@ class MockSplitViewService {
   }
 }
 
-fdescribe('SplitViewComponent', () => {
+describe('SplitViewComponent', () => {
   let component: SplitViewComponent;
   let fixture: ComponentFixture<SplitViewComponent>;
 
