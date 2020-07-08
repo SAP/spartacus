f743e265fc7ade63b1d4c8883567d58e17a733f0
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-07-08 13:21:06 +0200

    hard-coded cpq config in the prod env

--- projects/storefrontapp/src/environments/environment.prod.ts
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment: Environment = {
   production: true,
   occBaseUrl:
     build.process.env.SPARTACUS_BASE_URL ??
-    'https://dev-com-6.accdemo.b2c.ydev.hybris.com:9002',
+    'https://cpqteamserver.mo.sap.corp:39002',
   occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/occ/v2/',
   cds: build.process.env.SPARTACUS_CDS,
-  b2b: build.process.env.SPARTACUS_B2B,
+  b2b: true,
   productconfig: build.process.env.SPARTACUS_PRODUCTCONFIG,
 };
--- todo.md
+++ todo.md
@@ -9,6 +9,7 @@
 2. Revert:
 
    - `projects/storefrontapp/src/environments/environment.ts`
+   - `projects/storefrontapp/src/environments/environment.prod.ts`
    - `projects/storefrontapp-e2e-cypress/cypress.json`
    - `projects/storefrontapp-e2e-cypress/cypress.ci.2005.json`
 
