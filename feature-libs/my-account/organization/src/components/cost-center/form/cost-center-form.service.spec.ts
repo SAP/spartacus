b01fd60cd3364fad918e0dd2f3ea5074dc42cd78
dunqan <dunqan@w9.pl>
2020-07-10 11:26:37 +0200

    fix cost center from test

--- feature-libs/my-account/organization/src/components/cost-center/form/cost-center-form.service.spec.ts
+++ feature-libs/my-account/organization/src/components/cost-center/form/cost-center-form.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-import { FormGroup } from '@angular/forms';
 import { CostCenterFormService } from './cost-center-form.service';
 
 describe('CostCenterFormService', () => {
@@ -15,17 +14,16 @@ describe('CostCenterFormService', () => {
   });
 
   it('should build the form', () => {
-    const form = new FormGroup({});
-    service.build(form);
+    const form = service.getForm({});
     expect(form.get('code')).toBeDefined();
     expect(form.get('name')).toBeDefined();
     expect(form.get('currency').get('isocode')).toBeDefined();
     expect(form.get('unit').get('uid')).toBeDefined();
   });
 
-  it('should fail building silently', () => {
-    const form = undefined;
-    service.build(form);
-    expect(form).toBeUndefined();
+  it('should apply the model', () => {
+    const form = service.getForm({ code: 'test' });
+    expect(form.get('code')).toBeDefined();
+    expect(form.get('code').value).toEqual('test');
   });
 });
