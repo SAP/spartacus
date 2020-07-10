5718f467bf6acded4de3c0f197bfb4cef22d7999
Krzysztof Platis <platonn.git@gmail.com>
2020-07-10 15:50:52 +0200

    provide Config in root by itself

--- projects/core/src/config/config.module.ts
+++ projects/core/src/config/config.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import {
+  inject,
+  InjectFlags,
   InjectionToken,
   ModuleWithProviders,
   NgModule,
@@ -11,13 +13,20 @@ import { deepMerge } from './utils/deep-merge';
 /**
  * Global Configuration injection token, can be used to inject configuration to any part of the app
  */
-export const Config = new InjectionToken('Configuration');
+export const Config = new InjectionToken('Configuration', {
+  providedIn: 'root',
+  factory: () =>
+    configurationFactory(
+      inject(ConfigChunk, InjectFlags.Optional),
+      inject(DefaultConfigChunk, InjectFlags.Optional)
+    ),
+});
 
 /**
  * Config chunk token, can be used to provide configuration chunk and contribute to the global configuration object.
  * Should not be used directly, use `provideConfig` or import `ConfigModule.withConfig` instead.
  */
-export const ConfigChunk = new InjectionToken('ConfigurationChunk');
+export const ConfigChunk = new InjectionToken<object[]>('ConfigurationChunk');
 
 /**
  * Config chunk token, can be used to provide configuration chunk and contribute to the default configuration.
@@ -25,7 +34,7 @@ export const ConfigChunk = new InjectionToken('ConfigurationChunk');
  *
  * General rule is, that all config provided in libraries should be provided as default config.
  */
-export const DefaultConfigChunk = new InjectionToken(
+export const DefaultConfigChunk = new InjectionToken<object[]>(
   'DefaultConfigurationChunk'
 );
 
@@ -156,6 +165,8 @@ export class ConfigModule {
    * Module with providers, should be imported only once, if possible, at the root of the app.
    *
    * @param config
+   *
+   * @deprecated since 2.1 - see #8183
    */
   static forRoot(config: any = {}): ModuleWithProviders<ConfigModule> {
     return {
