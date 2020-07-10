26ef2d8fd3540b1addb5139d9eafffc6844bd139
Michał Gruca <pucek9@gmail.com>
2020-07-10 12:36:41 +0200

    set [create, edit, assign] views to be full-width on split view

--- feature-libs/my-account/organization/src/components/cost-center/list/cost-center-list.component.ts
+++ feature-libs/my-account/organization/src/components/cost-center/list/cost-center-list.component.ts
@@ -1,20 +1,50 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-import { PaginationModel } from '@spartacus/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { PaginationModel, RoutingService, RouterState } from '@spartacus/core';
 import { Table } from '@spartacus/storefront';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CostCenterListService } from './cost-center-list.service';
+import { map } from 'rxjs/operators';
+
+const BASE_CLASS = 'organization';
 
 @Component({
   selector: 'cx-cost-center-list',
   templateUrl: './cost-center-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CostCenterListComponent {
-  @HostBinding('class') hostClass = 'organization';
+export class CostCenterListComponent implements OnInit, OnDestroy {
+  @HostBinding('class') hostClass = BASE_CLASS;
 
   dataTable$: Observable<Table> = this.costCentersService.getTable();
 
-  constructor(protected costCentersService: CostCenterListService) {}
+  subscription = new Subscription();
+  lastPath$ = this.routingService
+    .getRouterState()
+    .pipe(
+      map((state: RouterState) => state.state?.url.split('/').reverse()[0])
+    );
+
+  constructor(
+    protected costCentersService: CostCenterListService,
+    protected routingService: RoutingService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.lastPath$.subscribe(
+        (path) => (this.hostClass = `${BASE_CLASS} ${path}`)
+      )
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   /**
    * Paginates the cost center list. Pagination is not using query parameters, as we like
