c613a963be8dc227c3fc927040e7d6f59e4d0920
Krzysztof Platis <platonn.git@gmail.com>
2020-07-13 14:36:02 +0200

    test unregistering and re-registering template on cxOutletRef destroy and rereate

--- projects/storefrontlib/src/cms-structure/outlet/outlet-ref/outlet-ref.directive.spec.ts
+++ projects/storefrontlib/src/cms-structure/outlet/outlet-ref/outlet-ref.directive.spec.ts
@@ -1,5 +1,6 @@
 import { Component, TemplateRef } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeaturesConfig } from '@spartacus/core';
 import { of } from 'rxjs';
 import { DeferLoaderService } from '../../../layout/loading/defer-loader.service';
 import { OutletDirective } from '../outlet.directive';
@@ -13,14 +14,29 @@ const CUSTOM_TEXT = 'customized';
 @Component({
   template: `
     <ng-template cxOutletRef="${OUTLET_NAME}">
-      <div id="debugEl">${CUSTOM_TEXT}</div>
+      ${CUSTOM_TEXT}
     </ng-template>
     <ng-container *cxOutlet="'${OUTLET_NAME}'">
-      <div id="debugEl">${STANDARD_TEXT}</div>
+      ${STANDARD_TEXT}
     </ng-container>
   `,
 })
 class TestContainerComponent {}
+
+@Component({
+  template: `
+    <ng-container *ngIf="!destroyed">
+      <ng-template cxOutletRef="${OUTLET_NAME}">
+        ${CUSTOM_TEXT}
+      </ng-template>
+
+      <ng-container *cxOutlet="'${OUTLET_NAME}'"></ng-container>
+    </ng-container>
+  `,
+})
+class TestTogglingComponent {
+  destroyed = false;
+}
 
 class MockDeferLoaderService {
   load(_element: HTMLElement, _options?: any) {
@@ -28,8 +44,11 @@ class MockDeferLoaderService {
   }
 }
 
-describe('OutletDirective', () => {
-  let fixture: ComponentFixture<TestContainerComponent>;
+function getContent(fixture: ComponentFixture<any>): string {
+  return fixture.debugElement.nativeElement.innerText;
+}
+
+describe('OutletRefDirective', () => {
   let service: OutletService;
 
   beforeEach(async(() => {
@@ -37,31 +56,54 @@ describe('OutletDirective', () => {
       imports: [],
       declarations: [
         TestContainerComponent,
+        TestTogglingComponent,
         OutletDirective,
         OutletRefDirective,
       ],
       providers: [
         OutletService,
         { provide: DeferLoaderService, useClass: MockDeferLoaderService },
+        {
+          provide: FeaturesConfig,
+          useValue: { features: { level: '2.1' } } as FeaturesConfig, // deprecated, see #
+        },
       ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestContainerComponent);
     service = TestBed.inject(OutletService);
   });
 
   it('should render custom content', () => {
+    const fixture = TestBed.createComponent(TestContainerComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('#debugEl').textContent).toContain(
-      CUSTOM_TEXT
-    );
+    expect(getContent(fixture)).toContain(CUSTOM_TEXT);
   });
 
   it('should have outlet for given name', () => {
+    const fixture = TestBed.createComponent(TestContainerComponent);
     fixture.detectChanges();
     expect(service.get(OUTLET_NAME) instanceof TemplateRef).toBeTruthy();
+  });
+
+  it('should unregister template on directive destroy', () => {
+    const fixture = TestBed.createComponent(TestTogglingComponent);
+    fixture.componentInstance.destroyed = true;
+    fixture.detectChanges();
+
+    expect(service.get(OUTLET_NAME) instanceof TemplateRef).toBeFalsy();
+    expect(getContent(fixture)).toEqual('');
+  });
+
+  it('should re-register template on directive re-creation', () => {
+    const fixture = TestBed.createComponent(TestTogglingComponent);
+    fixture.componentInstance.destroyed = true;
+    fixture.detectChanges();
+    fixture.componentInstance.destroyed = false;
+    fixture.detectChanges();
+
+    expect(service.get(OUTLET_NAME) instanceof TemplateRef).toBeTruthy();
+    expect(getContent(fixture)).toEqual(CUSTOM_TEXT);
   });
 });
