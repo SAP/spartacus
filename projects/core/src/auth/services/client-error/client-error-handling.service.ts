21741760309429c8f58eabdbd27d6a25f50df1b6
Marcin Lasak <lasakmarcin90@gmail.com>
2020-07-17 11:13:14 +0200

    refactor: Move client token error handling to client token interceptor

--- projects/core/src/auth/services/client-error/client-error-handling.service.ts
+++ projects/core/src/auth/services/client-error/client-error-handling.service.ts
@@ -1,23 +1,21 @@
+import { HttpHandler, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler } from '@angular/common/http';
-
 import { Observable } from 'rxjs';
 import { switchMap, take } from 'rxjs/operators';
-
-import { AuthService } from '../../facade/auth.service';
 import { ClientToken } from '../../models/token-types.model';
+import { ClientTokenService } from '../client-token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientErrorHandlingService {
-  constructor(protected authService: AuthService) {}
+  constructor(protected clientTokenService: ClientTokenService) {}
 
   public handleExpiredClientToken(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<any> {
-    return this.authService.refreshClientToken().pipe(
+    return this.clientTokenService.refreshClientToken().pipe(
       take(1),
       switchMap((token: ClientToken) => {
         return next.handle(this.createNewRequestWithNewToken(request, token));
@@ -31,7 +29,7 @@ export class ClientErrorHandlingService {
   ): HttpRequest<any> {
     request = request.clone({
       setHeaders: {
-        Authorization: `${token.token_type} ${token.access_token}`,
+        Authorization: `${token.token_type || 'Bearer'} ${token.access_token}`,
       },
     });
     return request;
