0055bd2ab5f3b8eff85e829742f766a25ce70264
Patrick Labadie <patrick.labadie@sap.com>
2020-07-17 17:36:57 -0400

    GH-8146: Code review improvements.

--- projects/storefrontlib/src/cms-components/organization/order-approval/order-approval-detail/order-approval-detail-form/order-approval-detail-form.component.ts
+++ projects/storefrontlib/src/cms-components/organization/order-approval/order-approval-detail/order-approval-detail-form/order-approval-detail-form.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   OrderApproval,
