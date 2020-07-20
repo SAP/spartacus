05ac9fad2193d5cc61566277575cc672d3c94e0c
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-07-20 00:41:13 -0400

    Run E2Es against Azure dev servers (GH-8066) - 2.0.x

--- projects/storefrontapp/src/environments/environment.prod.ts
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   occBaseUrl:
     build.process.env.SPARTACUS_BASE_URL ??
-    'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002',
+    'https://spartacus-legacy.eastus.cloudapp.azure.com:9002',
   occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/rest/v2/',
   cds: build.process.env.SPARTACUS_CDS,
   b2b: build.process.env.SPARTACUS_B2B,
