e28b22a70808024af5180cd2cc1d8840a2991e75
Patryk Smul <psmul@divante.pl>
2020-07-21 09:30:44 +0200

    fix: unit tree highlight active link

--- feature-libs/my-account/organization/src/components/units/unit-tree-navigation-ui/unit-tree-navigation-ui.module.ts
+++ feature-libs/my-account/organization/src/components/units/unit-tree-navigation-ui/unit-tree-navigation-ui.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import { UnitTreeNavigationUIComponent } from './unit-tree-navigation-ui.component';
-import { GenericLinkModule, IconModule } from '@spartacus/storefront';
+import { IconModule } from '@spartacus/storefront';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, UrlModule, I18nModule, IconModule, GenericLinkModule],
+  imports: [CommonModule, UrlModule, I18nModule, IconModule, RouterModule],
   declarations: [UnitTreeNavigationUIComponent],
   exports: [UnitTreeNavigationUIComponent],
   providers: [],
