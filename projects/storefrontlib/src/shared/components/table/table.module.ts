c3c4359c9f7295336cbd675847a3ca7816bdfe50
Michał Gruca <pucek9@gmail.com>
2020-07-22 16:49:12 +0200

    cleanup

--- projects/storefrontlib/src/shared/components/table/table.module.ts
+++ projects/storefrontlib/src/shared/components/table/table.module.ts
@@ -2,14 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
 import { OutletModule } from '../../../cms-structure/outlet/outlet.module';
-import { IconModule } from '../../../cms-components/misc/icon/icon.module';
 import { TableComponent } from './table.component';
 
 /**
  * The TableModule provides a table component that is driven by (responsible) configuration.
  */
 @NgModule({
-  imports: [CommonModule, OutletModule, I18nModule, IconModule],
+  imports: [CommonModule, OutletModule, I18nModule],
   declarations: [TableComponent],
   exports: [TableComponent],
 })
