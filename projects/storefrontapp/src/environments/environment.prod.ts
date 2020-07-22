be245497ed0fc4339139a7205f6b34e8153ab98f
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-07-22 17:48:33 -0400

    change 2.0.x to point to 2005 instead of 1905

--- projects/storefrontapp/src/environments/environment.prod.ts
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment: Environment = {
   production: true,
   occBaseUrl:
     build.process.env.SPARTACUS_BASE_URL ??
-    'https://spartacus-legacy.eastus.cloudapp.azure.com:9002',
-  occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/rest/v2/',
+    'https://spartacus-dev0.eastus.cloudapp.azure.com:9002',
+  occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/occ/v2/',
   cds: build.process.env.SPARTACUS_CDS,
   b2b: build.process.env.SPARTACUS_B2B,
 };
