84fd15c81be3fa473695c3faf38c8a292aba1814
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-07-27 14:23:39 +0200

    product tests

--- projects/storefrontapp/src/app/events/product-event.builder.spec.ts
+++ projects/storefrontapp/src/app/events/product-event.builder.spec.ts
@@ -1,112 +1,173 @@
 import { TestBed } from '@angular/core/testing';
 import { Action, ActionsSubject } from '@ngrx/store';
-import { Observable, of, ReplaySubject } from 'rxjs';
-import { EventService } from '../../event/event.service';
-import { ProductSearchPage } from '../../model/product-search.model';
-import { Product } from '../../model/product.model';
-import { ProductSearchService } from '../../product/facade/product-search.service';
-import { ProductService } from '../../product/facade/product.service';
-import { SemanticPathService } from '../../routing/configurable-routes/url-translation/semantic-path.service';
-import { UrlCommands } from '../../routing/configurable-routes/url-translation/url-command';
-import { RoutingEventBuilder } from '../../routing/event/routing-event.builder';
 import {
+  createFrom,
+  EventService,
+  Product,
+  ProductSearchPage,
+  ProductSearchService,
+  ProductService,
+} from '@spartacus/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { ProductEventBuilder } from './product-event.builder';
+import {
+  BrandPageVisited,
   CategoryPageVisited,
   ProductDetailsPageVisited,
-  SearchPageVisited,
+  SearchResultsRetrieved,
 } from './product.events';
+import { PageVisited } from './routing.events';
 
+const productGetBehavior = new BehaviorSubject<Product>(undefined);
 class MockProductService {
-  get(): Observable<Product> {
-    return of();
-  }
-}
-class ProductSearchServiceStub {
-  getResults(): Observable<ProductSearchPage> {
-    return of();
-  }
+  get = () => productGetBehavior;
 }
 
-class MockSemanticPathService {
-  transform(_commands: UrlCommands): any[] {
-    return [];
-  }
-  get(_routeName: string): string {
-    return '';
-  }
+const getResultsBehavior = new BehaviorSubject<ProductSearchPage>(undefined);
+class MockProductSearchService {
+  getResults = () => getResultsBehavior;
 }
 
-describe('Routing-Event Builder', () => {
-  let routingEventBuilder: RoutingEventBuilder;
-  let mockActionsSubject: ReplaySubject<Action>;
+interface ActionWithPayload extends Action {
+  payload: any;
+}
+
+fdescribe('Product-Event Builder', () => {
   let eventService: EventService;
-
-  const mockTearDown = () => {};
-
-  function setVariables() {
-    mockActionsSubject = new ReplaySubject<Action>();
-  }
+  let actions$: Subject<ActionWithPayload>;
 
   beforeEach(() => {
-    setVariables();
+    actions$ = new Subject();
+
     TestBed.configureTestingModule({
       providers: [
-        RoutingEventBuilder,
-        {
-          provide: ActionsSubject,
-          useValue: mockActionsSubject,
-        },
-        {
-          provide: EventService,
-          useValue: {
-            register: jasmine
-              .createSpy('register')
-              .and.returnValue(mockTearDown),
-          },
-        },
-        {
-          provide: ProductService,
-          useClass: MockProductService,
-        },
-        {
-          provide: ProductSearchService,
-          useClass: ProductSearchServiceStub,
-        },
-        {
-          provide: SemanticPathService,
-          useClass: MockSemanticPathService,
-        },
+        { provide: ActionsSubject, useValue: actions$ },
+        { provide: ProductService, useClass: MockProductService },
+        { provide: ProductSearchService, useClass: MockProductSearchService },
       ],
     });
-    routingEventBuilder = TestBed.inject(RoutingEventBuilder);
+
+    TestBed.inject(ProductEventBuilder); // register events
     eventService = TestBed.inject(EventService);
   });
 
-  it('should be created', () => {
-    expect(routingEventBuilder).toBeTruthy();
-  });
+  it('SearchResultsRetrieved', () => {
+    const searchResults: ProductSearchPage = {
+      freeTextSearch: 'camera',
+      pagination: { totalResults: 5 },
+      facets: [{ category: true }],
+    };
 
-  it('should register seven events', () => {
-    expect(eventService.register).toHaveBeenCalledTimes(7);
-  });
+    let result: SearchResultsRetrieved;
+    eventService
+      .get(SearchResultsRetrieved)
+      .pipe(take(1))
+      .subscribe((value) => (result = value));
 
-  it('should register SearchPageVisited', () => {
-    expect(eventService.register).toHaveBeenCalledWith(
-      SearchPageVisited,
-      jasmine.any(Observable)
+    const pageVisitedEvent = createFrom(PageVisited, {
+      context: undefined,
+      semanticRoute: 'search',
+      url: 'search url',
+      params: undefined,
+    });
+    eventService.dispatch(pageVisitedEvent);
+    getResultsBehavior.next(searchResults);
+
+    expect(result).toEqual(
+      jasmine.objectContaining({
+        searchTerm: searchResults.freeTextSearch,
+        numberOfResults: searchResults.pagination.totalResults,
+      } as SearchResultsRetrieved)
     );
   });
 
-  it('should register ProductDetailsPageVisited', () => {
-    expect(eventService.register).toHaveBeenCalledWith(
-      ProductDetailsPageVisited,
-      jasmine.any(Observable)
+  it('CategoryPageVisited', () => {
+    const searchResults: ProductSearchPage = {
+      breadcrumbs: [{ facetValueName: 'Cat1' }],
+    };
+
+    let result: CategoryPageVisited;
+    eventService
+      .get(CategoryPageVisited)
+      .pipe(take(1))
+      .subscribe((value) => (result = value));
+
+    const pageVisitedEvent = createFrom(PageVisited, {
+      context: { id: 'cat1' },
+      semanticRoute: 'category',
+      url: 'category url',
+      params: undefined,
+    });
+    eventService.dispatch(pageVisitedEvent);
+    getResultsBehavior.next(searchResults);
+
+    expect(result).toEqual(
+      jasmine.objectContaining({
+        categoryCode: pageVisitedEvent.context.id,
+        categoryName: searchResults.breadcrumbs[0].facetValueName,
+      } as CategoryPageVisited)
     );
   });
 
-  it('should register CategoryPageVisited', () => {
-    expect(eventService.register).toHaveBeenCalledWith(
-      CategoryPageVisited,
-      jasmine.any(Observable)
+  it('BrandPageVisited', () => {
+    const searchResults: ProductSearchPage = {
+      breadcrumbs: [{ facetValueName: 'Brand1' }],
+    };
+
+    let result: BrandPageVisited;
+    eventService
+      .get(BrandPageVisited)
+      .pipe(take(1))
+      .subscribe((value) => (result = value));
+
+    const pageVisitedEvent = createFrom(PageVisited, {
+      context: { id: 'brand1' },
+      semanticRoute: 'brand',
+      url: 'brand url',
+      params: undefined,
+    });
+    eventService.dispatch(pageVisitedEvent);
+    getResultsBehavior.next(searchResults);
+
+    expect(result).toEqual(
+      jasmine.objectContaining({
+        brandCode: pageVisitedEvent.context.id,
+        brandName: searchResults.breadcrumbs[0].facetValueName,
+      } as BrandPageVisited)
+    );
+  });
+
+  it('ProductDetailsPageVisited', () => {
+    const product: Product = {
+      code: '1234',
+      categories: [{ code: 'cat1', name: 'Cat1' }],
+      name: 'Test Product 1',
+      price: { value: 100 },
+    };
+
+    let result: ProductDetailsPageVisited;
+    eventService
+      .get(ProductDetailsPageVisited)
+      .pipe(take(1))
+      .subscribe((value) => (result = value));
+
+    const productPageVisitedEvent = createFrom(PageVisited, {
+      context: { id: product.code },
+      semanticRoute: 'product',
+      url: 'product url',
+      params: undefined,
+    });
+    eventService.dispatch(productPageVisitedEvent);
+    productGetBehavior.next(product);
+
+    expect(result).toEqual(
+      jasmine.objectContaining({
+        code: product.code,
+        categories: product.categories,
+        name: product.name,
+        price: product.price,
+      } as ProductDetailsPageVisited)
     );
   });
 });
