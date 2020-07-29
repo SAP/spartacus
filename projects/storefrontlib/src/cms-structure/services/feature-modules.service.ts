f8dfb4e9f011178ecb67d467b63bae2fca691fad
dunqan <dunqan@w9.pl>
2020-07-29 16:05:58 +0200

    fixes and polishing

--- projects/storefrontlib/src/cms-structure/services/feature-modules.service.ts
+++ projects/storefrontlib/src/cms-structure/services/feature-modules.service.ts
@@ -39,7 +39,6 @@ import {
 interface FeatureInstance extends FeatureModuleConfig {
   moduleRef?: NgModuleRef<any>;
   depsModules?: any[];
-  injectors?: Injector[];
   componentsMappings?: CMSComponentConfig;
 }
 
@@ -116,29 +115,47 @@ export class FeatureModulesService implements OnDestroy {
   /**
    * Get all injectors for feature and its dependencies
    *
-   * As it's a synchronous method, it works only for already resolved features
+   * As it's a synchronous method, it works only for already resolved features,
+   * returning undefined otherwise
    */
   getInjectors(componentType: string): Injector[] | undefined {
     const feature = this.componentFeatureMap.get(componentType);
-    let injectors = undefined;
+    let injectors;
 
     // we are returning injectors only for already resolved features
     this.features
       .get(feature)
-      ?.subscribe((featureInstance) => (injectors = featureInstance.injectors))
+      ?.subscribe((featureInstance) => {
+        injectors = [
+          // feature module injector
+          featureInstance.moduleRef.injector,
+          // injectors from dependency modules
+          ...featureInstance.depsModules.map(
+            (module) => this.dependencyModules.get(module).injector
+          ),
+        ];
+      })
       .unsubscribe();
     return injectors;
   }
 
+  /**
+   * Resolve feature based on feature name, if feature was not yet resolved
+   *
+   * It will first resolve all module dependencies if defined
+   */
   private resolveFeature(featureName: string): Observable<FeatureInstance> {
     return defer(() => {
       if (!this.features.has(featureName)) {
         const featureConfig = this.featureModulesConfig[featureName];
 
         if (!featureConfig?.module) {
-          throw 'No module defined for Feature Module ' + featureName;
+          throw new Error(
+            'No module defined for Feature Module ' + featureName
+          );
         }
 
+        // resolve dependencies first (if any)
         const depsResolve = featureConfig.dependencies?.length
           ? forkJoin(
               featureConfig.dependencies.map((depModuleFunc) =>
@@ -160,32 +177,21 @@ export class FeatureModulesService implements OnDestroy {
     });
   }
 
+  /**
+   * Initialize feature module by returning feature instance
+   */
   private resolveFeatureModule(
     featureConfig: FeatureModuleConfig,
     depsModules: any[] = []
   ): Observable<FeatureInstance> {
-    return from(featureConfig?.module()).pipe(
-      switchMap((module) =>
-        module instanceof NgModuleFactory
-          ? of(module)
-          : from(this.compiler.compileModuleAsync(module as any))
-      ),
-      observeOn(queueScheduler),
-      map((moduleFactory: NgModuleFactory<any>) => {
+    return this.resolveModuleFactory(featureConfig?.module).pipe(
+      map(([moduleFactory]) => {
         const moduleRef = moduleFactory.create(this.injector);
-
-        const injectors = [
-          moduleRef.injector,
-          ...depsModules.map(
-            (module) => this.dependencyModules.get(module).injector
-          ),
-        ];
 
         const featureInstance: FeatureInstance = {
           ...featureConfig,
           moduleRef,
           depsModules,
-          injectors,
           componentsMappings: {},
         };
 
@@ -228,19 +234,16 @@ export class FeatureModulesService implements OnDestroy {
     ) as CmsConfig;
   }
 
+  /**
+   * Resolves dependency module and initializes single module instance
+   */
   private resolveDependencyModule(
     moduleFunc: () => Promise<any>
   ): Observable<any> {
-    return from(moduleFunc()).pipe(
-      switchMap((module) =>
-        module instanceof NgModuleFactory
-          ? (of([module, module]) as Observable<[NgModuleFactory<any>, any]>)
-          : combineLatest([
-              from(this.compiler.compileModuleAsync(module as any)),
-              of(module),
-            ])
-      ),
-      observeOn(queueScheduler),
+    // We grab moduleFactory symbol from module function and if there is no
+    // such a module created yet, we create it and store it in a
+    // dependencyModules map
+    return this.resolveModuleFactory(moduleFunc).pipe(
       tap(([moduleFactory, module]) => {
         if (!this.dependencyModules.has(module)) {
           const moduleRef = moduleFactory.create(this.injector);
@@ -248,6 +251,27 @@ export class FeatureModulesService implements OnDestroy {
         }
       }),
       pluck(1)
+    );
+  }
+
+  /**
+   * Resolve any Angular module from an function that return module or moduleFactory
+   */
+  private resolveModuleFactory(
+    moduleFunc: () => Promise<any>
+  ): Observable<[NgModuleFactory<any>, any]> {
+    return from(moduleFunc()).pipe(
+      switchMap((module) =>
+        module instanceof NgModuleFactory
+          ? (of([module, module]) as Observable<[NgModuleFactory<any>, any]>)
+          : combineLatest([
+              // using compiler here is for jit compatibility, there is no overhead
+              // for aot production builds as it will be stubbed
+              from(this.compiler.compileModuleAsync(module as any)),
+              of(module),
+            ])
+      ),
+      observeOn(queueScheduler)
     );
   }
 
