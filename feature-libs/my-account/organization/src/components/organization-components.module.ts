6e6d485b0374bb0da9a71904ee5ca6c389914917
Patryk Smul <psmul@divante.pl>
2020-07-29 11:09:06 +0200

    chore: units root folder rename

--- feature-libs/my-account/organization/src/components/organization-components.module.ts
+++ feature-libs/my-account/organization/src/components/organization-components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CostCenterComponentsModule } from './cost-center/cost-center-components.module';
-import { UnitsComponentsModule } from './units/units-components.module';
+import { UnitsComponentsModule } from './unit/units-components.module';
 import { UserGroupComponentsModule } from './user-group/user-group-components.module';
 
 @NgModule({
