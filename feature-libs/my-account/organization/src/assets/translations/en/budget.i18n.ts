dedeffaa20f1787c867c1af43e3a57324a1d2d5c
Alfons Bruckl <albrcmon@inf.upv.es>
2020-07-29 18:01:09 +0100

    adding i18n file

---
+++ feature-libs/my-account/organization/src/assets/translations/en/budget.i18n.ts
@@ -0,0 +1,70 @@
+export const budget = {
+  header: 'Budgets',
+  disabled: '(disabled)',
+  code: 'Code',
+  name: 'Name',
+  currency: 'Currency',
+  unit: 'Parent Unit',
+
+  messages: {
+    enabled:
+      'When you disable the cost center, the related data will be disabled as well. ',
+    disabled: 'You cannot edit a disabled Cost Center.',
+    deactivate: 'Are you sure you want to disable this cost center?',
+    deactivateHeader: 'Disable Cost Center',
+  },
+
+  budget: {
+    header: 'Budgets',
+    assign: 'Assign budgets',
+    name: 'Name',
+    code: 'Code',
+    close: '',
+    cancel: 'Cancel',
+    create: 'Create {{name}}',
+    edit: 'Edit details',
+    save: 'Save {{name}}',
+    back: 'Back',
+    instructions: {
+      check: 'To assign a budget to this cost center, select its check box.',
+      uncheck: 'To unassign a budget, clear its check box.',
+      changes: 'Changes are saved automatically.',
+    },
+  },
+  budgetForm: {
+    save: 'Save',
+    cancel: 'Cancel',
+    create: 'Create Budget',
+    edit: 'Edit Budget',
+    update: 'Update Budget',
+    code: {
+      label: 'Budget ID',
+      placeholder: 'Code',
+    },
+    name: {
+      label: 'Budget name',
+      placeholder: 'Name',
+    },
+    businessUnits: {
+      label: 'Parent business unit',
+      placeholder: 'Select business unit',
+    },
+    startDate: 'Start date',
+    endDate: 'End date',
+    currency: 'Currency',
+    amount: {
+      label: 'Budget amount',
+      placeholder: 'Amount',
+    },
+  },
+  costCenter: {
+    header: 'Cost Centers',
+  },
+};
+
+export const costCenterAssignBudgets = {
+  name: 'Name',
+  code: 'Code',
+  amount: 'Amount',
+  dateRange: 'Start - End',
+};
