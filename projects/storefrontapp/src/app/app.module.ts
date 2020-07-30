a45b47dd30f2ec451f90936d2d0673eebbb943d3
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-07-30 11:08:23 +0200

    revert app module

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,19 +8,9 @@ import {
   BrowserTransferStateModule,
 } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TestConfigModule } from '@spartacus/core';
 import {
-  EventService,
-  OrderPlacedEvent,
-  TestConfigModule,
-} from '@spartacus/core';
-import {
-  CartPageVisitedEvent,
-  CategoryPageResultsEvent,
-  HomePageVisitedEvent,
   JsonLdBuilderModule,
-  PageVisitedEvent,
-  ProductDetailsPageEvent,
-  SearchPageResultsEvent,
   StorefrontComponent,
 } from '@spartacus/storefront';
 import { b2bFeature } from '../environments/b2b/b2b.feature';
@@ -58,28 +48,10 @@ if (environment.b2b) {
     ...additionalImports,
     TestOutletModule, // custom usages of cxOutletRef only for e2e testing
     TestConfigModule.forRoot({ cookie: 'cxConfigE2E' }), // Injects config dynamically from e2e tests. Should be imported after other config modules.
+
     ...devImports,
   ],
 
   bootstrap: [StorefrontComponent],
 })
-export class AppModule {
-  constructor(events: EventService) {
-    events.get(PageVisitedEvent).subscribe((x) => console.log('page: ', x));
-    events.get(HomePageVisitedEvent).subscribe((x) => console.log('home: ', x));
-    events.get(CartPageVisitedEvent).subscribe((x) => console.log('cart: ', x));
-
-    events
-      .get(ProductDetailsPageEvent)
-      .subscribe((x) => console.log('pdp: ', x));
-    events
-      .get(CategoryPageResultsEvent)
-      .subscribe((x) => console.log('cat: ', x));
-    // events.get(BrandPageVisited).subscribe((x) => console.log('brand: ', x));
-    events
-      .get(SearchPageResultsEvent)
-      .subscribe((x) => console.log('search: ', x));
-
-    events.get(OrderPlacedEvent).subscribe((x) => console.log('order: ', x));
-  }
-}
+export class AppModule {}
