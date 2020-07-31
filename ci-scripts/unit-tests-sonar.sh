18b44d348f28971961ae8978ece56f5c2975c737
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-07-31 11:45:15 +0200

    running only gigya

--- ci-scripts/unit-tests-sonar.sh
+++ ci-scripts/unit-tests-sonar.sh
@@ -4,50 +4,50 @@ set -o pipefail
 
 echo "-----"
 
-echo "Running unit tests and code coverage for cds"
-exec 5>&1
-output=$(ng test cds --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
+# echo "Running unit tests and code coverage for cds"
+# exec 5>&1
+# output=$(ng test cds --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+# coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+# if [[ -n "$coverage" ]]; then
+#     echo "Error: Tests did not meet coverage expectations"
+#     exit 1
+# fi
 
-echo "Running unit tests and code coverage for core"
-exec 5>&1
-output=$(ng test core --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
+# echo "Running unit tests and code coverage for core"
+# exec 5>&1
+# output=$(ng test core --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+# coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+# if [[ -n "$coverage" ]]; then
+#     echo "Error: Tests did not meet coverage expectations"
+#     exit 1
+# fi
 
-echo "Running unit tests and code coverage for storefrontlib"
-exec 5>&1
-output=$(ng test storefrontlib --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
+# echo "Running unit tests and code coverage for storefrontlib"
+# exec 5>&1
+# output=$(ng test storefrontlib --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+# coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+# if [[ -n "$coverage" ]]; then
+#     echo "Error: Tests did not meet coverage expectations"
+#     exit 1
+# fi
 
-echo "Running unit tests and code coverage for product"
-exec 5>&1
-output=$(ng test product --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
+# echo "Running unit tests and code coverage for product"
+# exec 5>&1
+# output=$(ng test product --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+# coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+# if [[ -n "$coverage" ]]; then
+#     echo "Error: Tests did not meet coverage expectations"
+#     exit 1
+# fi
 
-echo "Running unit tests and code coverage for my-account"
-exec 5>&1
-output=$(ng test my-account --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
+# echo "Running unit tests and code coverage for my-accunt"
+# exec 5>&1
+# output=$(ng test my-account --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+# coverage=$(echo $output | grep -i "does not meet global threshold" || true)
+# if [[ -n "$coverage" ]]; then
+#     echo "Error: Tests did not meet coverage expectations"
+#     exit 1
+# fi
 
 echo "Running unit tests and code coverage for gigya"
 exec 5>&1
@@ -58,11 +58,11 @@ if [[ -n "$coverage" ]]; then
     exit 1
 fi
 
-echo "Running unit tests for schematics"
-cd projects/schematics
-yarn
-yarn test
-cd ../..
+# echo "Running unit tests for schematics"
+# cd projects/schematics
+# yarn
+# yarn test
+# cd ../..
 
 if [[ $1 == '-h' ]]; then
     echo "Usage: $0 [sonar (to run sonar scan)]"
