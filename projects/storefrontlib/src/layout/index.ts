49254fa28e7f29cafe6838c35c1e1abe10f04eca
Krzysztof Platis <platonn.git@gmail.com>
2020-08-03 11:39:20 +0200

    export direciton service

--- projects/storefrontlib/src/layout/index.ts
+++ projects/storefrontlib/src/layout/index.ts
@@ -2,6 +2,7 @@ export * from './a11y/index';
 export * from './breakpoint/breakpoint.service';
 export * from './config/direction.model';
 export * from './config/layout-config';
+export * from './direction/index';
 export * from './header/index';
 export * from './launch-dialog/index';
 export * from './layout.module';
