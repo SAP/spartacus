feb9991adf26f2cb40a2bb6077d68a515d39e5ca
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-08-03 12:35:13 +0200

    Drop defaults to avoid breaking change

--- projects/storefrontlib/src/layout/direction/default-direction.config.ts
+++ projects/storefrontlib/src/layout/direction/default-direction.config.ts
@@ -1,13 +1,14 @@
-import { DirectionMode } from '../config/direction.model';
 import { LayoutConfig } from '../config/layout-config';
 
 export const defaultDirectionConfig: LayoutConfig = {
   direction: {
     detect: true,
-    default: DirectionMode.LTR,
+
+    // we hold back with default direction during minor release
+    // default: DirectionMode.LTR,
+
     // we're not polluting the system with all defaults for rtl or ltr.
-    // rtlLanguages: [ 'ar', 'arc','dv','fa','ha','he','khw','ks','ku','ps','ur', 'yi',],
     // see https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
-    rtlLanguages: ['ar', 'he'],
+    // rtlLanguages: [ 'ar','arc','dv','fa','ha','he','khw','ks','ku','ps','ur','yi',],
   },
 };
