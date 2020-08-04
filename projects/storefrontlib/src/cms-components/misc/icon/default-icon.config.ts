bc6cfa60eeac6a0c31a263af2a734db7e4b58d72
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-08-04 15:06:22 +0200

    drop icon flipping for gradient direction

--- projects/storefrontlib/src/cms-components/misc/icon/default-icon.config.ts
+++ projects/storefrontlib/src/cms-components/misc/icon/default-icon.config.ts
@@ -4,7 +4,6 @@ import { IconConfig } from './icon.model';
 export const defaultIconConfig: IconConfig = {
   icon: {
     flipDirection: {
-      STAR: DirectionMode.RTL,
       CARET_RIGHT: DirectionMode.RTL,
       CARET_LEFT: DirectionMode.RTL,
     },
