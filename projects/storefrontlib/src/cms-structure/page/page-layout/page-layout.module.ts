ebc9fbbad14a54cb01f807cf39eaae4dec3c9665
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-08-05 16:55:11 +0200

    Change directive implementation to APP_BOOTSTRAP_LISTENER

--- projects/storefrontlib/src/cms-structure/page/page-layout/page-layout.module.ts
+++ projects/storefrontlib/src/cms-structure/page/page-layout/page-layout.module.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { APP_BOOTSTRAP_LISTENER, ComponentRef, NgModule } from '@angular/core';
+import { FeatureConfigService } from '@spartacus/core';
 import { PageSlotModule } from '../../../cms-structure/page/slot/page-slot.module';
 import { OutletModule } from '../../outlet/outlet.module';
 import { PageLayoutComponent } from './page-layout.component';
-import { PageTemplateDirective } from './page-template.directive';
+import { PageTemplateStyleService } from './page-template-style.service';
 
 @NgModule({
   imports: [CommonModule, OutletModule, PageSlotModule],
-  declarations: [PageLayoutComponent, PageTemplateDirective],
-  exports: [PageLayoutComponent, PageTemplateDirective],
+  declarations: [PageLayoutComponent],
+  exports: [PageLayoutComponent],
+
+  providers: [
+    {
+      provide: APP_BOOTSTRAP_LISTENER,
+      multi: true,
+      useFactory: (
+        service: PageTemplateStyleService,
+        featureConfigService: FeatureConfigService
+      ) => (componentRef: ComponentRef<any>) => {
+        if (featureConfigService.isLevel('2.1')) {
+          service.initialize(componentRef);
+        }
+      },
+      deps: [PageTemplateStyleService, FeatureConfigService],
+    },
+  ],
 })
 export class PageLayoutModule {}
