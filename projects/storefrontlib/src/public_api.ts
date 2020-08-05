9879f5b5a4e062ab1bc42491eb0bd50000f5c0fb
Krzysztof Platis <platonn.git@gmail.com>
2020-08-05 18:11:46 +0200

    add util for form's deep updateValueAndValidity

--- projects/storefrontlib/src/public_api.ts
+++ projects/storefrontlib/src/public_api.ts
@@ -10,3 +10,4 @@ export * from './recipes/index';
 export * from './recipes/storefront.module';
 export * from './shared/index';
 export * from './storefront-config';
+export * from './utils/index';
