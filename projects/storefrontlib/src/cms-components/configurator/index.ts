4d4261bdf6b6571e63962bd2f22a371af991dab9
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2020-08-11 17:54:17 +0200

    Scrolling is not working with prev/next buttons (#8539)
    Scrolling is not working with prev/next buttons

--- projects/storefrontlib/src/cms-components/configurator/index.ts
+++ projects/storefrontlib/src/cms-components/configurator/index.ts
@@ -3,7 +3,6 @@ export * from './commons/config-conflict-suggestion/config-conflict-suggestion.c
 export * from './commons/config-group-menu/config-group-menu.component';
 export * from './commons/config/default-message-config';
 export * from './commons/config/message-config';
-export * from './commons/service/config-utils.service';
 export * from './generic/configure-cart-entry/configure-cart-entry.component';
 export * from './generic/configure-issues-notification/configure-issues-notification.component';
 export * from './generic/configure-product/configure-product.component';
