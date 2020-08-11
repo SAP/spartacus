90fcb1c154563c3ec7b2b0faa9b6b8620d87e8af
dunqan <dunqan@w9.pl>
2020-08-11 08:32:39 +0200

    remove index.d.ts from builder in repo

--- tools/build-lib/augmented-types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { JsonObject } from '@angular-devkit/core';
-import { NgPackagrBuilderOptions } from '@angular-devkit/build-ng-packagr';
-declare const _default: import("@angular-devkit/architect/src/internal").Builder<NgPackagrBuilderOptions & JsonObject>;
-export default _default;
