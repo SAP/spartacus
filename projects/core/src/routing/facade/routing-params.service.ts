f196415141fa2b74634558643fd040279072dbdf
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-08-14 17:00:59 +0200

    RoutingParamsService

---
+++ projects/core/src/routing/facade/routing-params.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map, shareReplay, startWith } from 'rxjs/operators';
+
+/**
+ * Service to expose all parameters for the router, including child routes.
+ * This is convenient in case the parent route (component) requires awareness
+ * of child routes parameters.
+ */
+@Injectable({ providedIn: 'root' })
+export class RoutingParamsService {
+  protected routerEvent$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    startWith(this.router)
+  );
+
+  protected readonly params$: Observable<{
+    [key: string]: string;
+  }> = this.routerEvent$.pipe(
+    map(() => this.findAllParam(this.router.routerState.snapshot.root)),
+    shareReplay({ refCount: true, bufferSize: 1 })
+  );
+
+  constructor(protected router: Router) {}
+
+  getParams(): Observable<{ [key: string]: string }> {
+    return this.params$;
+  }
+
+  protected findAllParam(
+    route: ActivatedRouteSnapshot
+  ): { [key: string]: string } {
+    let params = {};
+    route.children.forEach((c) =>
+      c.paramMap.keys.forEach((key) => (params[key] = c.paramMap.get(key)))
+    );
+    route.children.forEach(
+      (c) => (params = { ...params, ...this.findAllParam(c) })
+    );
+    return params;
+  }
+}
