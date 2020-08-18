1812e41d910b824ded573ad4f73edc6e6452427b
benmarcotte <55809994+benmarcotte@users.noreply.github.com>
2020-08-18 13:51:58 -0400

    Update search-box-component.service.spec.ts

--- projects/storefrontlib/src/cms-components/navigation/search-box/search-box-component.service.spec.ts
+++ projects/storefrontlib/src/cms-components/navigation/search-box/search-box-component.service.spec.ts
@@ -23,17 +23,6 @@ const searchBoxConfig: SearchBoxConfig = {
   maxProducts: 5,
   maxSuggestions: 5,
   displayProductImages: false,
-  exactMatchEnabled: true,
-};
-
-const searchBoxConfigNoExactMatch: SearchBoxConfig = {
-  minCharactersBeforeRequest: 1,
-  displayProducts: true,
-  displaySuggestions: true,
-  maxProducts: 5,
-  maxSuggestions: 5,
-  displayProductImages: false,
-  exactMatchEnabled: false,
 };
 
 class MockSearchboxService {
@@ -187,7 +176,7 @@ describe('SearchBoxComponentService', () => {
       expect(result.suggestions.length).toEqual(0);
     });
 
-    it('should have exact match suggestion when there are no suggestions but at least one product and config.exactMatchEnabled = true', () => {
+    it('should have exact match suggestion when there are no suggestions but at least one product', () => {
       spyOn(searchBoxservice, 'getSuggestionResults').and.returnValue(of([]));
 
       service
@@ -196,15 +185,6 @@ describe('SearchBoxComponentService', () => {
       expect(result.suggestions).toEqual([
         'searchBox.help.exactMatch{"term":"query"}',
       ]);
-    });
-
-    it('should not have exact match suggestion when config.exactMatchEnabled = false', () => {
-      spyOn(searchBoxservice, 'getSuggestionResults').and.returnValue(of([]));
-
-      service
-        .getResults(searchBoxConfigNoExactMatch)
-        .subscribe((results) => (result = results));
-      expect(result.suggestions).toEqual([]);
     });
 
     it('should not get an exact match suggestion when there are suggestions returned', () => {
