38027cd9526f508f7cca0822f0f1929183ecee5e
dunqan <dunqan@w9.pl>
2020-08-19 08:20:53 +0200

    create outlet context data model

--- projects/storefrontlib/src/cms-structure/outlet/outlet.model.ts
+++ projects/storefrontlib/src/cms-structure/outlet/outlet.model.ts
@@ -6,3 +6,12 @@ export enum OutletPosition {
 
 export const AVOID_STACKED_OUTLETS = false;
 export const USE_STACKED_OUTLETS = true;
+
+/**
+ * Token for injecting outlet related context to the component rendered in the outlet
+ */
+export abstract class OutletContextData<T = any> {
+  name: string;
+  position: OutletPosition;
+  context: T;
+}
