d963fc814e362b1dfbf7176b90c02cbe62082bda
Krzysztof Platis <platonn.git@gmail.com>
2020-08-19 14:24:51 +0200

    import missing StorefrontModule

--- feature-libs/my-account/organization/src/recipes/b2b-storefront.module.ts
+++ feature-libs/my-account/organization/src/recipes/b2b-storefront.module.ts
@@ -4,19 +4,23 @@ import {
   provideDefaultConfig,
   provideDefaultConfigFactory,
 } from '@spartacus/core';
-import { CmsLibModule, StorefrontConfig } from '@spartacus/storefront';
-
+import {
+  CmsLibModule,
+  StorefrontConfig,
+  StorefrontModule,
+} from '@spartacus/storefront';
+import { OrganizationModule } from '../organization.module';
 import {
   b2bLayoutConfig,
   defaultB2bOccConfig,
   mediaConfig,
 } from './config/index';
 import { defaultCmsContentConfig } from './config/static-cms-structure/default-cms-content.config';
-import { OrganizationModule } from '../organization.module';
 
 @NgModule({
   imports: [
     OrganizationModule,
+    StorefrontModule,
     // the cms lib module contains all components that added in the bundle
     CmsLibModule,
   ],
