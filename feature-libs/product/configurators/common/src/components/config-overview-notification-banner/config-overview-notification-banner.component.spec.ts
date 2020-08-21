3b21a41409f1afc88438762d51b1b0d585de7e15
Christoph Hinssen <christoph.hinssen@sap.com>
2020-08-21 12:32:05 +0200

    GH-8584 OV banner component does not depend on entry

--- feature-libs/product/configurators/common/src/components/config-overview-notification-banner/config-overview-notification-banner.component.spec.ts
+++ feature-libs/product/configurators/common/src/components/config-overview-notification-banner/config-overview-notification-banner.component.spec.ts
@@ -1,47 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
-  ActiveCartService,
+  Configurator,
+  ConfiguratorCommonsService,
   GenericConfigurator,
-  OrderEntry,
 } from '@spartacus/core';
 import {
   ConfigRouterExtractorService,
   ConfigurationRouter,
 } from '@spartacus/storefront';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ConfigComponentTestUtilsService } from '../../shared/testing/config-component-test-utils.service';
 import * as ConfigurationTestData from '../../shared/testing/configuration-test-data';
 import { ConfigOverviewNotificationBannerComponent } from './config-overview-notification-banner.component';
 
-const cartEntries: OrderEntry[] = [
-  {
-    entryNumber: 1,
-    product: {
-      configurable: true,
-      configuratorType: 'CPQCONFIGURATOR',
-    },
-  },
-  {
-    entryNumber: 2,
-    product: {
-      configurable: false,
-    },
-  },
-  {
-    entryNumber: 3,
-    product: {
-      configurable: true,
-      configuratorType: 'CPQCONFIGURATOR',
-    },
-  },
-  {
-    entryNumber: 4,
-    product: {
-      configurable: true,
-      configuratorType: 'some other configurator',
-    },
-  },
-];
+@Pipe({
+  name: 'cxTranslate',
+})
+class MockTranslatePipe implements PipeTransform {
+  transform(): any {}
+}
+
+@Pipe({
+  name: 'cxUrl',
+})
+class MockUrlPipe implements PipeTransform {
+  transform(): any {}
+}
 
 const productConfiguration = ConfigurationTestData.productConfiguration;
 productConfiguration.owner.type = GenericConfigurator.OwnerType.CART_ENTRY;
@@ -55,57 +40,82 @@ const routerData: ConfigurationRouter.Data = {
   owner: productConfiguration.owner,
 };
 
-class MockActiveCartService {
-  getEntries() {
-    return of(cartEntries);
-  }
-}
-
+let routerObs;
 class MockConfigRouterExtractorService {
   extractRouterData() {
-    return of(routerData);
+    return routerObs;
   }
 }
 
+let configurationObs;
+class MockConfiguratorCommonsService {
+  getOrCreateConfiguration(): Observable<Configurator.Configuration> {
+    return configurationObs;
+  }
+  getConfigurationWithOverview(): Observable<Configurator.Configuration> {
+    return configurationObs;
+  }
+  removeConfiguration(): void {}
+}
+let component: ConfigOverviewNotificationBannerComponent;
+let fixture: ComponentFixture<ConfigOverviewNotificationBannerComponent>;
+let htmlElem: HTMLElement;
+function initialize() {
+  routerObs = of(routerData);
+  fixture = TestBed.createComponent(ConfigOverviewNotificationBannerComponent);
+  component = fixture.componentInstance;
+  htmlElem = fixture.nativeElement;
+  fixture.detectChanges();
+}
 describe('ConfigOverviewNotificationBannerComponent', () => {
-  let component: ConfigOverviewNotificationBannerComponent;
-  let fixture: ComponentFixture<ConfigOverviewNotificationBannerComponent>;
-  let htmlElem: HTMLElement;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ConfigOverviewNotificationBannerComponent],
+      declarations: [
+        ConfigOverviewNotificationBannerComponent,
+        MockTranslatePipe,
+        MockUrlPipe,
+      ],
       providers: [
-        {
-          provide: ActiveCartService,
-          useClass: MockActiveCartService,
-        },
         {
           provide: ConfigRouterExtractorService,
           useClass: MockConfigRouterExtractorService,
+        },
+        {
+          provide: ConfiguratorCommonsService,
+          useClass: MockConfiguratorCommonsService,
         },
       ],
     }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(
-      ConfigOverviewNotificationBannerComponent
-    );
-    component = fixture.componentInstance;
-    htmlElem = fixture.nativeElement;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    configurationObs = of(productConfiguration);
+    initialize();
     expect(component).toBeTruthy();
   });
 
-  it('should display banner for error notification', () => {
+  it('should display no banner in case there are no issues', () => {
+    configurationObs = of(productConfiguration);
+    initialize();
+    ConfigComponentTestUtilsService.expectElementNotPresent(
+      expect,
+      htmlElem,
+      '.cx-error-container'
+    );
+  });
+
+  it('should display banner in case there are issues', () => {
+    //create a configuration with a conflict
+    const conflictingConfiguration: Configurator.Configuration = {
+      configId: productConfiguration.configId,
+      flatGroups: [{ groupType: Configurator.GroupType.CONFLICT_GROUP }],
+    };
+    configurationObs = of(conflictingConfiguration);
+    initialize();
     ConfigComponentTestUtilsService.expectElementPresent(
       expect,
       htmlElem,
-      'cx-configure-issues-notification'
+      '.cx-error-container'
     );
   });
 });
