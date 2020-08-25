505a6310b75de53f263e6c5be79773a576519a3a
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-08-25 10:36:08 +0200

    drop test component

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,18 +8,16 @@ import {
   BrowserTransferStateModule,
 } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { ConfigModule, TestConfigModule } from '@spartacus/core';
+import { TestConfigModule } from '@spartacus/core';
 import {
   JsonLdBuilderModule,
   StorefrontComponent,
-  TableConfig,
 } from '@spartacus/storefront';
 import { b2bFeature } from '../environments/b2b/b2b.feature';
 import { b2cFeature } from '../environments/b2c/b2c.feature';
 import { cdcFeature } from '../environments/cdc/cdc.feature';
 import { environment } from '../environments/environment';
 import { TestOutletModule } from '../test-outlets/test-outlet.module';
-import { TestComponent } from './test.component';
 
 registerLocaleData(localeDe);
 registerLocaleData(localeJa);
@@ -42,7 +40,6 @@ if (environment.cdc) {
 }
 
 @NgModule({
-  declarations: [TestComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
     BrowserTransferStateModule,
@@ -52,47 +49,6 @@ if (environment.cdc) {
     TestConfigModule.forRoot({ cookie: 'cxConfigE2E' }), // Injects config dynamically from e2e tests. Should be imported after other config modules.
 
     ...devImports,
-
-    // app mod
-    ConfigModule.withConfig({
-      table: {
-        budget: {
-          options: {
-            fields: {
-              // name: {
-              //   dataRenderer: TestComponent,
-              // },
-            },
-          },
-        },
-        //   mockTable: {
-        //     fields: ['first', 'second'],
-        //     options: {
-        //       first: {
-        //         // headerRenderer: 'comp',
-        //         // dataRenderer: 'comp',
-        //         // label: 'FFF',
-        //       },
-        //     },
-        //     md: {
-        //       options: {
-        //         hideHeader: false,
-        //       },
-        //     },
-        //     xs: {
-        //       fields: ['name'],
-        //       options: {
-        //         hideHeader: true,
-        //         second: {
-        //           label: {
-        //             i18nKey: 'prop.second',
-        //           },
-        //         },
-        //       },
-        //     },
-        //   },
-      },
-    } as TableConfig),
   ],
 
   bootstrap: [StorefrontComponent],
--- projects/storefrontapp/src/app/test.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { Budget } from '@spartacus/my-account';
-import { OrganizationLinkComponent } from 'feature-libs/my-account/organization/src/components/shared/property-renderers/organization-link.component';
-
-@Component({
-  template: ` {{ model.name }} `,
-})
-export class TestComponent extends OrganizationLinkComponent<Budget> {}
