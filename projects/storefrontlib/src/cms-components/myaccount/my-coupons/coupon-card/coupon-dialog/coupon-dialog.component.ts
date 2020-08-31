2a5cdf9058145ff75814583b791c95644784ac45
Christoph Hinssen <christoph.hinssen@sap.com>
2020-08-31 15:33:05 +0200

    fix unintended formatting

--- projects/storefrontlib/src/cms-components/myaccount/my-coupons/coupon-card/coupon-dialog/coupon-dialog.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/my-coupons/coupon-card/coupon-dialog/coupon-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ModalService } from '../../../../../shared/components/modal/index';
 import { ICON_TYPE } from '../../../../../cms-components/misc/icon/index';
 import { CustomerCoupon } from '@spartacus/core';
