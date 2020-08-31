9d2cd557b22344eb0bd4625d2ce625ea5336cf93
Christoph Hinssen <christoph.hinssen@sap.com>
2020-08-31 15:46:50 +0200

    Fix unintended formatting

--- projects/storefrontlib/src/cms-components/product/product-variants/variant-size-selector/variant-size-selector.component.spec.ts
+++ projects/storefrontlib/src/cms-components/product/product-variants/variant-size-selector/variant-size-selector.component.spec.ts
@@ -2,10 +2,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
   I18nTestingModule,
-  Product,
-  ProductService,
   RoutingService,
   UrlCommands,
+  Product,
+  ProductService,
 } from '@spartacus/core';
 import { VariantSizeSelectorComponent } from './variant-size-selector.component';
 import { NavigationExtras } from '@angular/router';
