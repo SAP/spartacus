69d066ff80352a90d0b5abdbcecd43af8e09b301
dunqan <dunqan@w9.pl>
2020-09-03 00:09:14 +0200

    emit event when lazy loading module

--- projects/storefrontlib/src/feature-toggles.ts
+++ projects/storefrontlib/src/feature-toggles.ts
@@ -7,5 +7,18 @@ export interface FeatureToggles {
      */
     level?: string;
     consignmentTracking?: boolean;
+
+    /**
+     * With 3.0 we introduced an unified configuration that emits new configuration
+     * every time lazy loaded module is instantiated. To ease transition to this new
+     * mechanism we are also publishing those changes to global Config token under
+     * the hood which can cause issues in some implementations.
+     *
+     *This option allows to disable this mechanism, i.e.:
+     *  - configuration won't change after app will be bootstrapped
+     *  - if any service would like to take into account configuration from lazy loaded
+     *    modules, it will have to use ConfigurationService.unifiedConfig$
+     */
+    disableConfigUpdates?: boolean;
   };
 }
