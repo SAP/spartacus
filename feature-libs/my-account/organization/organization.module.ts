82d9eb15666dc78860e699d0ec76bd4fdfbd214a
dunqan <dunqan@w9.pl>
2020-09-03 15:34:32 +0200

    optimize imports

--- feature-libs/my-account/organization/organization.module.ts
+++ feature-libs/my-account/organization/organization.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OrganizationComponentsModule } from '@spartacus/my-account/organization/components';
 import { OrganizationCoreModule } from '@spartacus/my-account/organization/core';
@@ -6,7 +5,6 @@ import { OrganizationOccModule } from '@spartacus/my-account/organization/occ';
 
 @NgModule({
   imports: [
-    CommonModule,
     OrganizationCoreModule.forRoot(),
     OrganizationOccModule,
     OrganizationComponentsModule,
