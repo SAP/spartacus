3de9251bc4b0f94b3854e5d694d77404154a3335
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-09-03 13:22:20 -0400

    GH-7999 Using yarn to run tests

--- ci-scripts/unit-tests-sonar.sh
+++ ci-scripts/unit-tests-sonar.sh
@@ -6,7 +6,7 @@ echo "-----"
 
 echo "Running unit tests and code coverage for cds"
 exec 5>&1
-output=$(ng test cds --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(yarn test cds --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
@@ -15,7 +15,7 @@ fi
 
 echo "Running unit tests and code coverage for Spartacus core"
 exec 5>&1
-output=$(ng test core --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(yarn test core --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
@@ -24,7 +24,7 @@ fi
 
 echo "Running unit tests and code coverage for storefront library"
 exec 5>&1
-output=$(ng test storefrontlib --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(yarn test storefrontlib --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
@@ -33,7 +33,7 @@ fi
 
 echo "Running unit tests and code coverage for CDC"
 exec 5>&1
-output=$(ng test cdc --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(yarn test cdc --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
