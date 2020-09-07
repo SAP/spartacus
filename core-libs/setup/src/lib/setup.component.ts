8cf933029706ec5e36a1660933171c6efc399490
dunqan <dunqan@w9.pl>
2020-09-07 13:02:21 +0200

    base lib

---
+++ core-libs/setup/src/lib/setup.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-setup',
+  template: `
+    <p>
+      setup works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ core-libs/setup/src/lib/setup.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SetupComponent } from './setup.component';
+
+
+
+@NgModule({
+  declarations: [SetupComponent],
+  imports: [
+  ],
+  exports: [SetupComponent]
+})
+export class SetupModule { }
