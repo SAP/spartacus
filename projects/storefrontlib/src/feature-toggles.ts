03304e3b1bc3373bd2e61ace88b7a9d701a6d185
dunqan <dunqan@w9.pl>
2020-09-08 14:07:16 +0200

    fix docs after CR

--- projects/storefrontlib/src/feature-toggles.ts
+++ projects/storefrontlib/src/feature-toggles.ts
@@ -9,10 +9,10 @@ export interface FeatureToggles {
     consignmentTracking?: boolean;
 
     /**
-     * With 3.0 we introduced an unified configuration that emits new configuration
+     * The 3.0 version introduced an unified configuration that emits new configuration
      * every time lazy loaded module is instantiated. To ease transition to this new
-     * mechanism we are also publishing those changes to global Config token under
-     * the hood which can cause issues in some implementations.
+     * mechanism, the same changes are also published to global Config token under
+     * the hood, which could cause issues in some implementations.
      *
      *This option allows to disable this mechanism, i.e.:
      *  - configuration won't change after app will be bootstrapped
