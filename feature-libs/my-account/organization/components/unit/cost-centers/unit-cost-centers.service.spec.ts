e76bd8e1e54b9e216493c798c0b9e93af9d59fda
Patryk Smul <psmul@divante.pl>
2020-09-08 12:47:43 +0200

    chore: provide missing unit test files for list services (#8776)
    Closes: #8417

---
+++ feature-libs/my-account/organization/components/unit/cost-centers/unit-cost-centers.service.spec.ts
@@ -0,0 +1,67 @@
+import { OrgUnitService } from '@spartacus/my-account/organization/core';
+import { Observable, of } from 'rxjs';
+import { CostCenter, EntitiesModel } from '@spartacus/core';
+import { Injectable } from '@angular/core';
+import { Table, TableService, TableStructure } from '@spartacus/storefront';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UnitCostCentersService } from './unit-cost-centers.service';
+
+const mockUnitCostCenterEntities: EntitiesModel<CostCenter> = {
+  values: [
+    {
+      name: 'first',
+    },
+    {
+      name: 'second',
+    },
+    {
+      name: 'third',
+    },
+  ],
+};
+
+class MockOrgUnitService implements Partial<OrgUnitService> {
+  getCostCenters(): Observable<EntitiesModel<CostCenter>> {
+    return of(mockUnitCostCenterEntities);
+  }
+}
+
+@Injectable()
+export class MockTableService {
+  buildStructure(type): Observable<TableStructure> {
+    return of({ type });
+  }
+}
+
+describe('UnitCostCentersService', () => {
+  let service: UnitCostCentersService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        UnitCostCentersService,
+        {
+          provide: OrgUnitService,
+          useClass: MockOrgUnitService,
+        },
+        {
+          provide: TableService,
+          useClass: MockTableService,
+        },
+      ],
+    });
+    service = TestBed.inject(UnitCostCentersService);
+  });
+
+  it('should inject service', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get cost center list', () => {
+    let result: Table<CostCenter>;
+    service.getTable().subscribe((table) => (result = table));
+    expect(result.data.length).toEqual(3);
+  });
+});
