952e26691eb3bb43d3d27f321233e4fbb43fb629
Christoph Hinssen <christoph.hinssen@sap.com>
2020-09-08 07:06:40 +0200

    GH-8805 Don't expose default config, state module
    And modules that cannot be used with first release

--- feature-libs/product/configurators/common/src/core/state/index.ts
+++ feature-libs/product/configurators/common/src/core/state/index.ts
@@ -1,6 +1,6 @@
 export * from './actions/index';
 export * from './configurator-state';
-export * from './configurator-state.module';
+export { ConfiguratorStateModule as ɵConfiguratorStateModule } from './configurator-state.module';
 export { ConfiguratorBasicEffects as ɵConfiguratorBasicEffects } from './effects/configurator-basic.effect';
 export { ConfiguratorCartEffects as ɵConfiguratorCartEffects } from './effects/configurator-cart.effect';
 export { ConfiguratorPlaceOrderHookEffects as ɵConfiguratorPlaceOrderHookEffects } from './effects/configurator-place-order-hook.effect';
