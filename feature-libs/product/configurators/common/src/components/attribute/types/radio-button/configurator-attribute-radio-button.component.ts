b3fa417317eac314305098737a0302fc46d57d07
Sebastian Stein <sebastian.stein@sap.com>
2020-09-10 10:21:41 +0200

    GH-8812: Use focusout and mousedown events

--- feature-libs/product/configurators/common/src/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
+++ feature-libs/product/configurators/common/src/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
@@ -26,15 +26,59 @@ export class ConfiguratorAttributeRadioButtonComponent implements OnInit {
   ngOnInit() {
     this.attributeRadioButtonForm.setValue(this.attribute.selectedSingleValue);
   }
+
+  onMouseDown(valueCode: string) {
+    //event.preventDefault();
+    console.log('submitMouse: ' + this.attribute.name + ':' + valueCode);
+    //this.submitValue(valueCode);
+  }
+
+  onFocusOut(event: FocusEvent) {
+    if (
+      this.attributeLostFocus(event) &&
+      this.attributeRadioButtonForm.value !== null
+    ) {
+      const attributeToSubmit =
+        this.attribute.name + ':' + this.attributeRadioButtonForm.value;
+      console.log('submitFocus: ' + attributeToSubmit);
+      //this.submitValue(this.attributeRadioButtonForm.value);
+    }
+  }
+
   /**
-   * Hit when value has been selected (from available domain)
+   * Returns true if the focus changed from one attribute to any other element.
+   * Returns false if the focus change occured inside the same attribute (i.e. from one value to another of the same attribute)
+   * @param event FocusEvent to be checked
    */
-  onSelect(): void {
+  attributeLostFocus(event: FocusEvent): boolean {
+    let attributeLostFocus = false;
+    const from: HTMLElement = event.target as HTMLElement;
+    const to: HTMLElement = event.relatedTarget as HTMLElement;
+    if (this.getAttributeId(from?.id) !== this.getAttributeId(to?.id)) {
+      attributeLostFocus = true;
+    }
+    return attributeLostFocus;
+  }
+
+  /**
+   * Removes the value Id from the element id to get the attribute id.
+   * @param id HTMLElment id
+   */
+  getAttributeId(id: string): string {
+    const index = id?.lastIndexOf('--');
+    return id?.substring(0, index);
+  }
+
+  /**
+   * Submits the value.
+   */
+  submitValue(valueCode: string): void {
+    console.log('the value to submit: ' + valueCode);
     const event: ConfigFormUpdateEvent = {
       productCode: this.ownerKey,
       changedAttribute: {
         name: this.attribute.name,
-        selectedSingleValue: this.attributeRadioButtonForm.value,
+        selectedSingleValue: valueCode,
         uiType: this.attribute.uiType,
         groupId: this.attribute.groupId,
       },
