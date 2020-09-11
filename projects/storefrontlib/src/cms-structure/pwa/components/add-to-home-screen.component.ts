a16e15f9d456280e62cd8452b40b7b6d941821a0
Mirosław Grochowski <admin@mirkosoft.pl>
2020-09-11 12:43:02 +0200

    fix lint

--- projects/storefrontlib/src/cms-structure/pwa/components/add-to-home-screen.component.ts
+++ projects/storefrontlib/src/cms-structure/pwa/components/add-to-home-screen.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { AddToHomeScreenService } from '../services/add-to-home-screen.service';
 
 @Directive()
+// tslint:disable-next-line:directive-class-suffix
 export abstract class AddToHomeScreenComponent implements OnInit {
   canPrompt$: Observable<boolean>;
   constructor(protected addToHomeScreenService: AddToHomeScreenService) {}
