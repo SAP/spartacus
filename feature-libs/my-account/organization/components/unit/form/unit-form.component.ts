d8ac0d7e4c6dd10427c46e31ff51dfea84b41fb4
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-09-11 11:36:44 +0200

    parent unit in form

--- feature-libs/my-account/organization/components/unit/form/unit-form.component.ts
+++ feature-libs/my-account/organization/components/unit/form/unit-form.component.ts
@@ -3,7 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { B2BUnit, B2BUnitNode } from '@spartacus/core';
 import { OrgUnitService } from '@spartacus/my-account/organization/core';
 import { Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { OrganizationItemService } from '../../shared/organization-item.service';
 import { UnitItemService } from '../services/unit-item.service';
 
@@ -20,32 +20,11 @@ import { UnitItemService } from '../services/unit-item.service';
 export class UnitFormComponent implements OnInit {
   form: FormGroup = this.itemService.getForm();
 
-  units$: Observable<B2BUnitNode[]> = this.unitService.getActiveUnitList().pipe(
-    map((units) => units.filter((unit) => unit.id !== this.form?.value.uid)),
-    // filter(
-    //   (units) =>
-    //     !units.find((unit) => {
-    //       console.log(unit.id, this.form?.value.parentOrgUnit?.uid);
-    //       return unit.id === this.form?.value.parentOrgUnit?.uid;
-    //     })
-    // ),
-    tap(console.log)
-  );
-
-  // units$: Observable<B2BUnitNode[]> = this.itemService.current$.pipe(
-  //   switchMap((item) =>
-  //     this.unitService.getActiveUnitList().pipe(
-  //       tap((units) => {
-  //         console.log(units);
-  //         console.log(item);
-  //       }),
-  //       // filter((units) => !item || !units.find((unit) => unit.id === item.uid)),
-  //       map((units) =>
-  //         units.filter((unit) => unit.id === item?.parentOrgUnit?.uid)
-  //       )
-  //     )
-  //   )
-  // );
+  units$: Observable<B2BUnitNode[]> = this.unitService
+    .getActiveUnitList()
+    .pipe(
+      map((units) => units.filter((unit) => unit.id !== this.form?.value.uid))
+    );
 
   approvalProcess$ = this.unitService
     .getApprovalProcesses()
@@ -60,63 +39,3 @@ export class UnitFormComponent implements OnInit {
     this.unitService.loadList();
   }
 }
-
-// import {
-//   ChangeDetectionStrategy,
-//   Component,
-//   Input,
-//   OnDestroy,
-//   OnInit,
-// } from '@angular/core';
-// import { FormGroup } from '@angular/forms';
-// import { B2BApprovalProcess, B2BUnitNode } from '@spartacus/core';
-// import { OrgUnitService } from '@spartacus/my-account/organization/core';
-// import { Observable, Subscription } from 'rxjs';
-// import { filter, map } from 'rxjs/operators';
-// import { AbstractFormComponent } from '../../abstract-component/abstract-form.component';
-// import { CurrentUnitService } from '../current-unit.service';
-
-// @Component({
-//   selector: 'cx-unit-form',
-//   templateUrl: './unit-form.component.html',
-//   changeDetection: ChangeDetectionStrategy.OnPush,
-// })
-// export class UnitFormComponent extends AbstractFormComponent
-//   implements OnInit, OnDestroy {
-//   @Input() form: FormGroup;
-
-//   businessUnits$: Observable<B2BUnitNode[]>;
-//   approvalProcesses$: Observable<B2BApprovalProcess[]>;
-
-//   subscription = new Subscription();
-//   parentUnit$ = this.currentUnitService.b2bUnit$.pipe(filter(Boolean));
-
-//   constructor(
-//     protected orgUnitService: OrgUnitService,
-//     protected currentUnitService: CurrentUnitService
-//   ) {
-//     super();
-//   }
-
-//   ngOnInit() {
-//     this.approvalProcesses$ = this.orgUnitService.getApprovalProcesses();
-//     this.orgUnitService.loadList();
-
-//     // filter out currently edited entity from list of possible parent units to assign
-//     this.businessUnits$ = this.orgUnitService
-//       .getActiveUnitList()
-//       .pipe(
-//         map((units) => units.filter((unit) => unit.id !== this.form?.value.uid))
-//       );
-
-//     this.subscription.add(
-//       this.parentUnit$.subscribe(() => {
-//         this.form.get('parentOrgUnit').disable();
-//       })
-//     );
-//   }
-
-//   ngOnDestroy(): void {
-//     this.subscription.unsubscribe();
-//   }
-// }
--- feature-libs/my-account/organization/components/unit/form/unit-form.service.ts
+++ feature-libs/my-account/organization/components/unit/form/unit-form.service.ts
@@ -7,41 +7,12 @@ import { OrganizationFormService } from '../../shared/organization-form';
   providedIn: 'root',
 })
 export class UnitFormService extends OrganizationFormService<B2BUnit> {
-  // getForm(model?: B2BUnit): FormGroup {
-  //   const form = new FormGroup({});
-  //   this.build(form);
-  //   if (model) {
-  //     form.patchValue(model);
-  //   }
-  //   // disable parent unit select for root units
-  //   if (model?.active && !model.parentOrgUnit?.uid) {
-  //     form.removeControl('parentOrgUnit');
-  //   }
-  //   return form;
-  // }
-
-  getForm(item?: B2BUnit): FormGroup {
-    const form = super.getForm(item);
-
-    if (item && !item.parentOrgUnit) {
-      // form.removeControl('parentOrgUnit');
-      form.get('parentOrgUnit').disable();
-    } else {
-    }
-
-    // disable parent unit select for root units
-
-    // if (!item || !item.parentOrgUnit) {
-    //   form.get('parentOrgUnit').enable();
-    //   // form.get('parentOrgUnit')?.get('uid')?.setValidators(Validators.required);
-    // } else {
-    //   form.get('parentOrgUnit').disable();
-    // }
-
-    return form;
+  protected patchData(item?: B2BUnit) {
+    this.toggleParentUnit(item);
+    super.patchData(item);
   }
 
-  protected build() {
+  protected build(item?: B2BUnit) {
     const form = new FormGroup({});
     form.setControl('uid', new FormControl('', Validators.required));
     form.setControl('name', new FormControl('', Validators.required));
@@ -53,14 +24,20 @@ export class UnitFormService extends OrganizationFormService<B2BUnit> {
       })
     );
 
-    form.setControl(
-      'parentOrgUnit',
-      new FormGroup({
-        uid: new FormControl(null),
-        // , Validators.required
-      })
-    );
-
     this.form = form;
+    this.toggleParentUnit(item);
+  }
+
+  protected toggleParentUnit(item?: B2BUnit): void {
+    if (item && !item.parentOrgUnit) {
+      this.form.removeControl('parentOrgUnit');
+    } else if (!this.form.get('parentOrgUnit')) {
+      this.form.setControl(
+        'parentOrgUnit',
+        new FormGroup({
+          uid: new FormControl(null, Validators.required),
+        })
+      );
+    }
   }
 }
