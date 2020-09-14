da58539c92f955e50918d1c209cfc5567f728966
steinsebastian <54309425+steinsebastian@users.noreply.github.com>
2020-09-14 12:14:16 +0200

    Feature/gh 8812 (#8900)
    Update configuration from radio button attribute only after unfocus

--- feature-libs/product/configurators/common/src/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
+++ feature-libs/product/configurators/common/src/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
@@ -17,6 +17,7 @@ import { Configurator } from './../../../../core/model/configurator.model';
 })
 export class ConfiguratorAttributeRadioButtonComponent implements OnInit {
   attributeRadioButtonForm = new FormControl('');
+  changeTriggeredByKeyboard = false;
 
   @Input() attribute: Configurator.Attribute;
   @Input() ownerKey: string;
@@ -26,21 +27,71 @@ export class ConfiguratorAttributeRadioButtonComponent implements OnInit {
   ngOnInit() {
     this.attributeRadioButtonForm.setValue(this.attribute.selectedSingleValue);
   }
+
+  onMouseDown(valueCode: string) {
+    this.submitValue(valueCode);
+  }
+
+  onFocusOut(event: FocusEvent) {
+    if (
+      this.attributeLostFocus(event) &&
+      this.attributeRadioButtonForm.value !== null &&
+      this.changeTriggeredByKeyboard === true
+    ) {
+      this.submitValue(this.attributeRadioButtonForm.value);
+    }
+  }
+
+  onKeyUp() {
+    this.changeTriggeredByKeyboard = true;
+  }
+
   /**
-   * Hit when value has been selected (from available domain)
+   * Returns true if the focus changed from one attribute to any other element.
+   * Returns false if the focus change occured inside the same attribute (i.e. from one value to another of the same attribute)
+   * @param event FocusEvent to be checked
    */
-  onSelect(): void {
+  attributeLostFocus(event: FocusEvent): boolean {
+    let attributeLostFocus = false;
+    const from: HTMLElement = event.target as HTMLElement;
+    const to: HTMLElement = event.relatedTarget as HTMLElement;
+    // Avoid submit on round-trip (in this case event.relatedTarget is null)
+    if (to === null) {
+      return attributeLostFocus;
+    }
+    if (this.getAttributeId(from?.id) !== this.getAttributeId(to?.id)) {
+      attributeLostFocus = true;
+    }
+
+    return attributeLostFocus;
+  }
+
+  /**
+   * Removes the value Id from the element id to get the attribute id.
+   * @param id HTMLElment id
+   */
+  getAttributeId(id: string): string {
+    const index = id?.lastIndexOf('--');
+    return id?.substring(0, index);
+  }
+
+  /**
+   * Submits the value.
+   */
+  submitValue(valueCode: string): void {
+    //console.log('the value to submit: ' + valueCode);
     const event: ConfigFormUpdateEvent = {
       productCode: this.ownerKey,
       changedAttribute: {
         name: this.attribute.name,
-        selectedSingleValue: this.attributeRadioButtonForm.value,
+        selectedSingleValue: valueCode,
         uiType: this.attribute.uiType,
         groupId: this.attribute.groupId,
       },
     };
 
     this.selectionChange.emit(event);
+    this.changeTriggeredByKeyboard = false;
   }
   createAttributeValueIdForConfigurator(
     attribute: Configurator.Attribute,
