d2966ddc2f82ed839bcf2337e0edcb9d4741404d
Kacper Knapik <kknapik@divante.pl>
2020-09-15 11:57:03 +0200

    feat: add budget reducer unit tests

--- feature-libs/my-account/organization/core/store/reducers/budget.reducer.spec.ts
+++ feature-libs/my-account/organization/core/store/reducers/budget.reducer.spec.ts
@@ -1,4 +1,4 @@
-import { BudgetActions } from '../actions/index';
+import { BudgetActions, CostCenterActions } from '../actions/index';
 import {
   budgetInitialState,
   budgetsInitialState,
@@ -7,39 +7,146 @@ import {
 } from './budget.reducer';
 import { Budget } from '../../model';
 
-fdescribe('hehe', () => {
-  describe('Budget Entities Reducer', () => {
-    describe('no action', () => {
-      it('should return the default state', () => {
-        const action = { type: '' };
-        const state = budgetsEntitiesReducer(undefined, action);
+const payloadBudget: Budget = {
+  code: 'testCode',
+  name: 'testBudget',
+};
 
-        expect(state).toBe(budgetInitialState);
+describe('Budget Entities Reducer', () => {
+  describe('no action', () => {
+    it('should return the default state', () => {
+      const action = { type: '' };
+      const state = budgetsEntitiesReducer(undefined, action);
+
+      expect(state).toBe(budgetInitialState);
+    });
+  });
+
+  describe('LOAD_BUDGET_SUCCESS action', () => {
+    it('should return the payload', () => {
+      const action = new BudgetActions.LoadBudgetSuccess(payloadBudget);
+      const result = budgetsEntitiesReducer(budgetInitialState, action);
+
+      expect(result).toEqual(payloadBudget);
+    });
+  });
+
+  describe('CREATE_BUDGET_SUCCESS action', () => {
+    it('should return the payload', () => {
+      const action = new BudgetActions.CreateBudgetSuccess(payloadBudget);
+      const result = budgetsEntitiesReducer(budgetInitialState, action);
+
+      expect(result).toEqual(payloadBudget);
+    });
+  });
+
+  describe('UPDATE_BUDGET_SUCCESS action', () => {
+    it('should return the payload', () => {
+      const action = new BudgetActions.UpdateBudgetSuccess(payloadBudget);
+      const result = budgetsEntitiesReducer(budgetInitialState, action);
+
+      expect(result).toEqual(payloadBudget);
+    });
+  });
+
+  describe('UNASSIGN_BUDGET_SUCCESS action', () => {
+    describe('without passed state', () => {
+      it('should return the payload', () => {
+        const payload = {
+          code: 'testCode',
+          selected: true,
+        };
+
+        const action = new CostCenterActions.UnassignBudgetSuccess(payload);
+        const result = budgetsEntitiesReducer(budgetInitialState, action);
+
+        expect(result).toEqual(payload);
       });
     });
 
-    describe('LOAD_BUDGET_SUCCESS action', () => {
-      it('should return the budget', () => {
-        const budget: Budget = {
-          name: 'testBudget',
+    describe('with passed state', () => {
+      it('should merge state and return the payload', () => {
+        const payload = {
+          code: 'testCode',
+          selected: true,
         };
 
-        const action = new BudgetActions.LoadBudgetSuccess([budget]);
-        const result = budgetsEntitiesReducer(budgetInitialState, action);
+        const expected = {
+          ...payloadBudget,
+          ...payload,
+        };
 
-        expect(result).toEqual(budget);
+        const action = new CostCenterActions.UnassignBudgetSuccess(payload);
+        const result = budgetsEntitiesReducer(payloadBudget, action);
+
+        expect(result).toEqual(expected);
       });
     });
   });
 
-  describe('Budget List Reducer', () => {
-    describe('no action', () => {
-      it('should return the default state', () => {
-        const action = { type: '' };
-        const state = budgetsListReducer(undefined, action);
+  describe('ASSIGN_BUDGET_SUCCESS action', () => {
+    describe('without passed state', () => {
+      it('should return the payload', () => {
+        const payload = {
+          code: 'testCode',
+          selected: true,
+        };
 
-        expect(state).toBe(budgetsInitialState);
+        const action = new CostCenterActions.AssignBudgetSuccess(payload);
+        const result = budgetsEntitiesReducer(budgetInitialState, action);
+
+        expect(result).toEqual(payload);
       });
+    });
+
+    describe('with passed state', () => {
+      it('should merge state and return the payload', () => {
+        const payload = {
+          code: 'testCode',
+          selected: true,
+        };
+
+        const expected = {
+          ...payloadBudget,
+          ...payload,
+        };
+
+        const action = new CostCenterActions.AssignBudgetSuccess(payload);
+        const result = budgetsEntitiesReducer(payloadBudget, action);
+
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+});
+
+describe('Budget List Reducer', () => {
+  describe('no action', () => {
+    it('should return the default state', () => {
+      const action = { type: '' };
+      const state = budgetsListReducer(undefined, action);
+
+      expect(state).toBe(budgetsInitialState);
+    });
+  });
+
+  describe('LOAD_BUDGETS_SUCCESS action', () => {
+    it('should return the page object', () => {
+      const payload = {
+        page: {
+          ids: ['testId'],
+        },
+        params: {
+          pageSize: 2,
+          currentPage: 1,
+          sort: 'yes',
+        },
+      };
+
+      const action = new BudgetActions.LoadBudgetsSuccess(payload);
+      const result = budgetsListReducer(budgetsInitialState, action);
+
+      expect(result).toEqual(payload.page);
     });
   });
 });
