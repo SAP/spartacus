18f3a4d4f59cc86577c295fa228eccaa630d258f
Christoph Hinssen <christoph.hinssen@sap.com>
2020-09-15 14:43:48 +0200

    Park stare

--- projects/storefrontapp/src/environments/productconfig/productconfig.feature.ts
+++ projects/storefrontapp/src/environments/productconfig/productconfig.feature.ts
@@ -1,11 +1,28 @@
+import { translationChunksConfig, translations } from '@spartacus/assets';
 import { CommonConfiguratorModule } from '@spartacus/product/configurators/common';
+import {
+  configuratorTranslationChunksConfig,
+  configuratorTranslations,
+} from '@spartacus/product/configurators/common/assets';
 import { TextfieldConfiguratorModule } from '@spartacus/product/configurators/textfield';
 import { VariantConfiguratorModule } from '@spartacus/product/configurators/variant';
 import { FeatureEnvironment } from '../models/feature.model';
 export const productConfigFeature: FeatureEnvironment = {
   imports: [
-    TextfieldConfiguratorModule,
-    CommonConfiguratorModule,
-    VariantConfiguratorModule,
+    { ngModule: TextfieldConfiguratorModule },
+    CommonConfiguratorModule.withConfig({
+      i18n: {
+        resources: {
+          en: { ...translations.en, ...configuratorTranslations.en },
+        },
+        chunks: {
+          ...translationChunksConfig,
+          ...configuratorTranslationChunksConfig,
+        },
+
+        fallbackLang: 'en',
+      },
+    }),
+    { ngModule: VariantConfiguratorModule },
   ],
 };
