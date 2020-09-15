b6a0a96b83629403b3fae7d207d934e2435d9866
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-09-15 10:20:42 +0200

    add unit to public api

--- feature-libs/my-account/organization/components/public_api.ts
+++ feature-libs/my-account/organization/components/public_api.ts
@@ -1,9 +1,8 @@
-// export * from './unit/index';
 export * from './budget/index';
 export * from './cost-center/index';
 export * from './order-approval/index';
 export * from './organization-components.module';
 export * from './permission/index';
 export * from './shared/index';
+export * from './unit/index';
 export * from './user-group/index';
-export * from './user/index';
