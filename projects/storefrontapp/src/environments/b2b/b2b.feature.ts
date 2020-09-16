9a94c79e38364b4112f98b90b19278836c2fe76c
dunqan <dunqan@w9.pl>
2020-09-16 18:26:06 +0200

    after merge fix

--- projects/storefrontapp/src/environments/b2b/b2b.feature.ts
+++ projects/storefrontapp/src/environments/b2b/b2b.feature.ts
@@ -1,16 +1,12 @@
 import { translationChunksConfig, translations } from '@spartacus/assets';
-import { B2bStorefrontModule } from '@spartacus/storefront';
 import { environment } from '../environment';
 import { FeatureEnvironment } from '../models/feature.model';
-import { translationChunksConfig, translations } from '@spartacus/assets';
 import {
   organizationTranslationChunksConfig,
   organizationTranslations,
 } from '@spartacus/my-account/organization/assets';
 import { OrganizationModule } from '@spartacus/my-account/organization';
 import { B2bStorefrontModule } from '@spartacus/my-account';
-import { environment } from '../environment';
-import { FeatureEnvironment } from '../models/feature.model';
 
 export const b2bFeature: FeatureEnvironment = {
   imports: [
@@ -35,17 +31,14 @@ export const b2bFeature: FeatureEnvironment = {
         },
       },
       // we bring in static translations to be up and running soon right away
-      i18n: { {
-      resources: {
-        en: { ...translations.en, ...organizationTranslations.en },
-      },
-      chunks: {
-        ...translationChunksConfig,
-        ...organizationTranslationChunksConfig,
-      },
-
-      resources: translations,
-        chunks: translationChunksConfig,
+      i18n: {
+        resources: {
+          en: { ...translations.en, ...organizationTranslations.en },
+        },
+        chunks: {
+          ...translationChunksConfig,
+          ...organizationTranslationChunksConfig,
+        },
         fallbackLang: 'en',
       },
 
