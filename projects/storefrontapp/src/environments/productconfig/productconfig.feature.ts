c2bdde637b572d224ded9bf253b2d164541e3292
Christoph Hinssen <christoph.hinssen@sap.com>
2020-09-16 11:29:52 +0200

    GH-8899 Get rid of 'src' folder

--- projects/storefrontapp/src/environments/productconfig/productconfig.feature.ts
+++ projects/storefrontapp/src/environments/productconfig/productconfig.feature.ts
@@ -1,15 +1,14 @@
 import { translationChunksConfig, translations } from '@spartacus/assets';
 import { CommonConfiguratorModule } from '@spartacus/product/configurators/common';
+import { TextfieldConfiguratorModule } from '@spartacus/product/configurators/textfield';
+import { VariantConfiguratorModule } from '@spartacus/product/configurators/variant';
 import {
   configuratorTranslationChunksConfig,
   configuratorTranslations,
-} from '@spartacus/product/configurators/common/assets';
-import { TextfieldConfiguratorModule } from '@spartacus/product/configurators/textfield';
-import { VariantConfiguratorModule } from '@spartacus/product/configurators/variant';
+} from 'feature-libs/product/configurators/common/assets/public_api';
 import { FeatureEnvironment } from '../models/feature.model';
 export const productConfigFeature: FeatureEnvironment = {
   imports: [
-    { ngModule: TextfieldConfiguratorModule },
     CommonConfiguratorModule.withConfig({
       i18n: {
         resources: {
@@ -24,5 +23,6 @@ export const productConfigFeature: FeatureEnvironment = {
       },
     }),
     { ngModule: VariantConfiguratorModule },
+    { ngModule: TextfieldConfiguratorModule },
   ],
 };
