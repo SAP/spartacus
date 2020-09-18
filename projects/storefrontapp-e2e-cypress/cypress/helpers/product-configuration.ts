58860e7c5068eeffe844df6e2a9e26f0cecfad20
Sebastian Stein <sebastian.stein@sap.com>
2020-09-18 12:45:12 +0200

    GH-8897: Change test user

--- projects/storefrontapp-e2e-cypress/cypress/helpers/product-configuration.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/product-configuration.ts
@@ -7,8 +7,9 @@ const previousBtnSelector =
   'cx-configurator-previous-next-buttons div div:first button';
 const addToCartButtonSelector = 'cx-configurator-add-to-cart-button button';
 
-const email = 'cpq02@sap.com';
-const password = 'welcome';
+const email = 'test-user-for-variant-configuration@ydev.hybris.com';
+const password = 'Password123.';
+const user = 'Variant Configuration';
 
 const conflictDetectedMsgSelector =
   '.cx-configurator-attribute-conflict-container';
@@ -840,7 +841,6 @@ export function login(): void {
   authentication.login(email, password);
   // Verify whether the user logged in successfully,
   // namely the logged in user should be greeted
-  const user = email.split('@')[0];
   cy.get('.cx-login-greet').should('contain', user);
 }
 
