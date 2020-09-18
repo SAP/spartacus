4d88214dd83457064d574becbdee2a6d6e182bc0
Krzysztof Platis <platonn.git@gmail.com>
2020-09-18 12:42:26 +0200

    move common config for b2b and b2c to app.module

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -3,16 +3,11 @@ import localeDe from '@angular/common/locales/de';
 import localeJa from '@angular/common/locales/ja';
 import localeZh from '@angular/common/locales/zh';
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  BrowserTransferStateModule,
-} from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { TestConfigModule } from '@spartacus/core';
-import {
-  JsonLdBuilderModule,
-  StorefrontComponent,
-} from '@spartacus/storefront';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { ConfigModule, TestConfigModule } from '@spartacus/core';
+import { JsonLdBuilderModule, StorefrontComponent } from '@spartacus/storefront';
 import { b2bFeature } from '../environments/b2b/b2b.feature';
 import { b2cFeature } from '../environments/b2c/b2c.feature';
 import { cdcFeature } from '../environments/cdc/cdc.feature';
@@ -50,10 +45,38 @@ if (environment.cdc) {
     BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
     BrowserTransferStateModule,
     JsonLdBuilderModule,
+    ConfigModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: environment.occBaseUrl,
+          prefix: environment.occApiPrefix,
+        },
+      },
+      
+      // custom routing configuration for e2e testing
+      routing: {
+        routes: {
+          product: {
+            paths: ['product/:productCode/:name', 'product/:productCode'],
+          },
+        },
+      },
+
+      // we bring in static translations to be up and running soon right away
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+      
+      features: {
+        level: '2.1',
+      },
+    }),
     ...additionalImports,
     TestOutletModule, // custom usages of cxOutletRef only for e2e testing
     TestConfigModule.forRoot({ cookie: 'cxConfigE2E' }), // Injects config dynamically from e2e tests. Should be imported after other config modules.
-
+    
     ...devImports,
   ],
 
