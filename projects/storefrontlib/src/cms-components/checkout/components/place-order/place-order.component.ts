01e188a1dd80feb7bcbce330e45a8f9ac7be786c
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-09-20 00:31:51 -0400

    Disabled place order when 'weekly' has no days checked during it's view for replenishment (GH-8958)

--- projects/storefrontlib/src/cms-components/checkout/components/place-order/place-order.component.ts
+++ projects/storefrontlib/src/cms-components/checkout/components/place-order/place-order.component.ts
@@ -1,5 +1,6 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   OnDestroy,
   OnInit,
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   CheckoutService,
   ORDER_TYPE,
+  recurrencePeriod,
   RoutingService,
   ScheduleReplenishmentForm,
 } from '@spartacus/core';
@@ -29,6 +31,7 @@ export class PlaceOrderComponent implements OnInit, OnDestroy {
 
   currentOrderType: ORDER_TYPE;
   scheduleReplenishmentFormData: ScheduleReplenishmentForm;
+  daysOfWeekNotChecked: Boolean;
 
   checkoutSubmitForm: FormGroup = this.fb.group({
     termsAndConditions: [false, Validators.requiredTrue],
@@ -44,7 +47,8 @@ export class PlaceOrderComponent implements OnInit, OnDestroy {
     protected routingService: RoutingService,
     protected launchDialogService: LaunchDialogService,
     protected fb: FormBuilder,
-    protected vcr: ViewContainerRef
+    protected vcr: ViewContainerRef,
+    protected cd: ChangeDetectorRef
   ) {}
 
   submitForm(): void {
@@ -96,7 +100,15 @@ export class PlaceOrderComponent implements OnInit, OnDestroy {
     this.subscription.add(
       this.checkoutReplenishmentFormService
         .getScheduleReplenishmentFormData()
-        .subscribe((data) => (this.scheduleReplenishmentFormData = data))
+        .subscribe((data) => {
+          this.scheduleReplenishmentFormData = data;
+
+          this.daysOfWeekNotChecked =
+            data.daysOfWeek.length === 0 &&
+            data.recurrencePeriod === recurrencePeriod.WEEKLY;
+
+          this.cd.markForCheck();
+        })
     );
   }
 
@@ -110,10 +122,10 @@ export class PlaceOrderComponent implements OnInit, OnDestroy {
 
         case ORDER_TYPE.SCHEDULE_REPLENISHMENT_ORDER: {
           this.routingService.go({ cxRoute: 'replenishmentConfirmation' });
-          this.checkoutReplenishmentFormService.resetScheduleReplenishmentFormData();
           break;
         }
       }
+      this.checkoutReplenishmentFormService.resetScheduleReplenishmentFormData();
     }
   }
 
