d511bc7a1b9d8f84761c1ff9f5afb6a13370c979
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-09-24 23:30:24 -0400

    fix prettier

--- projects/storefrontlib/src/cms-components/myaccount/order/replenishment-order-details/replenishment-order-cancellation/replenishment-order-cancellation-launch-dialog.service.spec.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/replenishment-order-details/replenishment-order-cancellation/replenishment-order-cancellation-launch-dialog.service.spec.ts
@@ -55,11 +55,13 @@ describe('ReplenishmentOrderCancellationLaunchDialogService', () => {
     it('should call LaunchDialogService launch', () => {
       service.openDialog(null, component.vcr, { test: 123 });
 
-      expect(
-        launchDialogService.launch
-      ).toHaveBeenCalledWith(LAUNCH_CALLER.REPLENISHMENT_ORDER, component.vcr, {
-        test: 123,
-      });
+      expect(launchDialogService.launch).toHaveBeenCalledWith(
+        LAUNCH_CALLER.REPLENISHMENT_ORDER,
+        component.vcr,
+        {
+          test: 123,
+        }
+      );
     });
 
     it('should call LaunchDialogService clear on close', () => {
