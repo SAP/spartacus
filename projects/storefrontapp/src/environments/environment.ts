93d133c39e14e3eff23eed2457cf8265bf100bad
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-09-28 11:48:23 +0200

    fix prettier

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment: Environment = {
   occBaseUrl:
     build.process.env.SPARTACUS_BASE_URL ??
     'https://spartacus-dev0.eastus.cloudapp.azure.com:9002',
-    // 'https://spartacus-dev3.eastus.cloudapp.azure.com:9002',
-    // 'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com',
+  // 'https://spartacus-dev3.eastus.cloudapp.azure.com:9002',
+  // 'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com',
   occApiPrefix: build.process.env.SPARTACUS_API_PREFIX ?? '/occ/v2/',
   b2b: false,
   cds: false,
