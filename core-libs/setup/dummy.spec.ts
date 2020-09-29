5a98767a26e80b8bc15e194960b66b7a75cf30c2
dunqan <dunqan@w9.pl>
2020-09-29 21:27:17 +0200

    improve tests

--- core-libs/setup/dummy.spec.ts
+++
@@ -1,7 +0,0 @@
-import { B2bStorefrontModule } from '@spartacus/setup';
-
-describe('B2bStorefrontModule', () => {
-  it('should be defined', () => {
-    expect(B2bStorefrontModule).toBeTruthy();
-  });
-});
