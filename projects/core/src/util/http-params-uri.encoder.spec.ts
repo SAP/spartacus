daf27c780be95cbd25ca65df761449e6aa9a7c39
i042416 <jerry.wang@sap.com>
2020-09-29 19:28:28 +0800

    improve unit test spec description

--- projects/core/src/util/http-params-uri.encoder.spec.ts
+++ projects/core/src/util/http-params-uri.encoder.spec.ts
@@ -4,29 +4,29 @@ describe('HttpParamsURIEncoder', () => {
   const httpParamsURIEncoder = new HttpParamsURIEncoder();
   const stringDecoded = 'Hello Spartacus!';
   const stringEncoded = 'Hello%20Spartacus!';
-  describe(`test for encodeKey`, () => {
-    it('should equal to result of encodeURIComponent', () => {
+  describe(`encodeKey()`, () => {
+    it('should return encoded key', () => {
       const result = httpParamsURIEncoder.encodeKey(stringDecoded);
       expect(result).toEqual(stringEncoded);
     });
   });
 
-  describe(`test for encodeValue`, () => {
-    it('should equal to result of encodeURIComponent', () => {
+  describe(`encodeValue()`, () => {
+    it('should return encoded value', () => {
       const result = httpParamsURIEncoder.encodeValue(stringDecoded);
       expect(result).toEqual(stringEncoded);
     });
   });
 
-  describe(`test for decodeKey`, () => {
-    it('should equal to result of decodeURIComponent', () => {
+  describe(`decodeKey()`, () => {
+    it('should return decoded key', () => {
       const result = httpParamsURIEncoder.decodeKey(stringEncoded);
       expect(result).toEqual(stringDecoded);
     });
   });
 
-  describe(`test for decodeValue`, () => {
-    it('should equal to result of decodeURIComponent', () => {
+  describe(`decodeValue()`, () => {
+    it('should return decoded value', () => {
       const result = httpParamsURIEncoder.decodeValue(stringEncoded);
       expect(result).toEqual(stringDecoded);
     });
