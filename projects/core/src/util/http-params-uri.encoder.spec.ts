df7929e0e9bf76901ba9d8b13e138aa3b9e0a327
i042416 <jerry.wang@sap.com>
2020-09-29 19:05:25 +0800

    fix bad variable name

--- projects/core/src/util/http-params-uri.encoder.spec.ts
+++ projects/core/src/util/http-params-uri.encoder.spec.ts
@@ -2,33 +2,33 @@ import { HttpParamsURIEncoder } from './http-params-uri.encoder';
 
 describe('HttpParamsURIEncoder', () => {
   const httpParamsURIEncoder = new HttpParamsURIEncoder();
-  const VALUE_FOR_ENCODE = 'Hello Spartacus!';
-  const VALUE_FOR_DECODE = 'Hello%20Spartacus!';
+  const stringDecoded = 'Hello Spartacus!';
+  const stringEncoded = 'Hello%20Spartacus!';
   describe(`test for encodeKey`, () => {
     it('should equal to result of encodeURIComponent', () => {
-      const result = httpParamsURIEncoder.encodeKey(VALUE_FOR_ENCODE);
-      expect(result).toEqual(VALUE_FOR_DECODE);
+      const result = httpParamsURIEncoder.encodeKey(stringDecoded);
+      expect(result).toEqual(stringEncoded);
     });
   });
 
   describe(`test for encodeValue`, () => {
     it('should equal to result of encodeURIComponent', () => {
-      const result = httpParamsURIEncoder.encodeValue(VALUE_FOR_ENCODE);
-      expect(result).toEqual(VALUE_FOR_DECODE);
+      const result = httpParamsURIEncoder.encodeValue(stringDecoded);
+      expect(result).toEqual(stringEncoded);
     });
   });
 
   describe(`test for decodeKey`, () => {
     it('should equal to result of decodeURIComponent', () => {
-      const result = httpParamsURIEncoder.decodeKey(VALUE_FOR_DECODE);
-      expect(result).toEqual(VALUE_FOR_ENCODE);
+      const result = httpParamsURIEncoder.decodeKey(stringEncoded);
+      expect(result).toEqual(stringDecoded);
     });
   });
 
   describe(`test for decodeValue`, () => {
     it('should equal to result of decodeURIComponent', () => {
-      const result = httpParamsURIEncoder.decodeValue(VALUE_FOR_DECODE);
-      expect(result).toEqual(VALUE_FOR_ENCODE);
+      const result = httpParamsURIEncoder.decodeValue(stringEncoded);
+      expect(result).toEqual(stringDecoded);
     });
   });
 });
