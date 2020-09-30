b5a08b7a4deb490fd595d6d997b69789b029843e
i042416 <jerry.wang@sap.com>
2020-09-30 16:35:22 +0800

    follow newline practice

--- projects/core/src/util/http-params-uri.encoder.spec.ts
+++ projects/core/src/util/http-params-uri.encoder.spec.ts
@@ -1,33 +1,39 @@
 import { HttpParamsURIEncoder } from './http-params-uri.encoder';
 
+const stringDecoded = 'Hello Spartacus!';
+const stringEncoded = 'Hello%20Spartacus!';
+
 describe('HttpParamsURIEncoder', () => {
-  const httpParamsURIEncoder = new HttpParamsURIEncoder();
-  const stringDecoded = 'Hello Spartacus!';
-  const stringEncoded = 'Hello%20Spartacus!';
+  const encoder = new HttpParamsURIEncoder();
+
   describe(`encodeKey()`, () => {
     it('should return encoded key', () => {
-      const result = httpParamsURIEncoder.encodeKey(stringDecoded);
+      const result = encoder.encodeKey(stringDecoded);
+
       expect(result).toEqual(stringEncoded);
     });
   });
 
   describe(`encodeValue()`, () => {
     it('should return encoded value', () => {
-      const result = httpParamsURIEncoder.encodeValue(stringDecoded);
+      const result = encoder.encodeValue(stringDecoded);
+
       expect(result).toEqual(stringEncoded);
     });
   });
 
   describe(`decodeKey()`, () => {
     it('should return decoded key', () => {
-      const result = httpParamsURIEncoder.decodeKey(stringEncoded);
+      const result = encoder.decodeKey(stringEncoded);
+
       expect(result).toEqual(stringDecoded);
     });
   });
 
   describe(`decodeValue()`, () => {
     it('should return decoded value', () => {
-      const result = httpParamsURIEncoder.decodeValue(stringEncoded);
+      const result = encoder.decodeValue(stringEncoded);
+
       expect(result).toEqual(stringDecoded);
     });
   });
