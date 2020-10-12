c120d37f3f9eeb28b135c7a806e6f60ca5251edd
Marcin Lasak <lasakmarcin90@gmail.com>
2020-10-12 15:37:24 +0200

    fix: Fix merge conflict resolution

--- projects/core/src/auth/user-auth/models/user-token.model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserToken {
-  access_token: string;
-  refresh_token: string;
-  expires_at: string;
-  granted_scopes: string[];
-  access_token_stored_at: string;
-  token_type?: string;
-  [custom_param: string]: any;
-}
