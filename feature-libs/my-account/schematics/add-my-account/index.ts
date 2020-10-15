ae19b98d9561a708ac19b17050bba76aba6c62ee
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-10-15 15:51:48 +0200

    improved build; using common api

--- feature-libs/my-account/schematics/add-my-account/index.ts
+++ feature-libs/my-account/schematics/add-my-account/index.ts
@@ -1,4 +1,3 @@
-import { experimental, JsonParseMode, parseJson } from '@angular-devkit/core';
 import {
   chain,
   Rule,
@@ -8,31 +7,31 @@ import {
 } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
-  addSymbolToNgModuleMetadata,
-  insertImport,
-  isImported,
-} from '@schematics/angular/utility/ast-utils';
-import {
-  Change,
-  InsertChange,
-  RemoveChange,
-  ReplaceChange,
-} from '@schematics/angular/utility/change';
-import { getWorkspace as getWorkspaceAngular } from '@schematics/angular/utility/config';
-import {
   addPackageJsonDependency,
   NodeDependency,
   NodeDependencyType,
 } from '@schematics/angular/utility/dependencies';
 import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
 import {
-  ProjectType,
-  WorkspaceProject,
-  WorkspaceSchema,
-  WorkspaceTargets,
-} from '@schematics/angular/utility/workspace-models';
-import * as ts from 'typescript';
-import { version } from '../../package.json';
+  addImport,
+  addToModuleImportsAndCommitChanges,
+  addToModuleProviders,
+  commitChanges,
+  DEFAULT_B2B_OCC_CONFIG,
+  getDefaultProjectNameFromWorkspace,
+  getProjectTargets,
+  getSourceRoot,
+  getSpartacusSchematicsVersion,
+  getTsSourceFile,
+  getWorkspace,
+  ORGANIZATION_MODULE,
+  PROVIDE_DEFAULT_CONFIG,
+  SPARTACUS_CORE,
+  SPARTACUS_MY_ACCOUNT,
+  SPARTACUS_ORGANIZATION,
+  SPARTACUS_SETUP,
+  UTF_8,
+} from '@spartacus/schematics';
 import { Schema as MyAccountOptions } from './schema';
 
 export function addSpartacusMyAccount(options: MyAccountOptions): Rule {
@@ -53,25 +52,21 @@ function addPackageJsonDependencies(packageJson: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const spartacusVersion = `^${getSpartacusSchematicsVersion()}`;
 
-    // TODO: create constant
     const spartacusMyAccountDependency: NodeDependency = {
       type: NodeDependencyType.Default,
       version: spartacusVersion,
-      // TODO: create constant
-      name: `@spartacus/my-account`,
+      name: SPARTACUS_MY_ACCOUNT,
     };
     addPackageJsonDependency(tree, spartacusMyAccountDependency);
     context.logger.info(
       `✅️ Added '${spartacusMyAccountDependency.name}' into ${spartacusMyAccountDependency.type}`
     );
 
-    if (!packageJson.dependencies.hasOwnProperty(`@spartacus/setup`)) {
-      // TODO: create constant
+    if (!packageJson.dependencies.hasOwnProperty(SPARTACUS_SETUP)) {
       const spartacusSetupDependency: NodeDependency = {
         type: NodeDependencyType.Default,
         version: spartacusVersion,
-        // TODO: create constant
-        name: `@spartacus/setup`,
+        name: SPARTACUS_SETUP,
       };
 
       addPackageJsonDependency(tree, spartacusSetupDependency);
@@ -85,8 +80,7 @@ function addPackageJsonDependencies(packageJson: any): Rule {
 }
 
 function validateSpartacusInstallation(packageJson: any): void {
-  // TODO: use constants
-  if (!packageJson.dependencies.hasOwnProperty(`@spartacus/core`)) {
+  if (!packageJson.dependencies.hasOwnProperty(SPARTACUS_CORE)) {
     throw new SchematicsException(
       `Spartacus is not detected. Please first install Spartacus by running: 'ng add @spartacus/schematics'.
     To see more options, please check our documentation.`
@@ -99,7 +93,7 @@ function addStyles(): Rule {
     const myAccountScssPath = `${getSourceRoot(
       tree
     )}/styles/spartacus-my-account.scss`;
-    tree.create(myAccountScssPath, `@import "@spartacus/my-account";`);
+    tree.create(myAccountScssPath, `@import "${SPARTACUS_MY_ACCOUNT}";`);
 
     const { path, workspace: angularJson } = getWorkspace(tree);
     const defaultProject = getDefaultProjectNameFromWorkspace(tree);
@@ -173,7 +167,7 @@ function readPackageJson(tree: Tree): any {
 }
 
 function updateAppModule(options: MyAccountOptions): Rule {
-  return (host: Tree, context: SchematicContext) => {
+  return (host: Tree, _context: SchematicContext) => {
     const projectTargets = getProjectTargets(host, options.project);
 
     if (!projectTargets.build) {
@@ -182,260 +176,20 @@ function updateAppModule(options: MyAccountOptions): Rule {
 
     const mainPath = projectTargets.build.options.main;
     const modulePath = getAppModulePath(host, mainPath);
-    context.logger.debug(`main module path: ${modulePath}`);
     const moduleSource = getTsSourceFile(host, modulePath);
 
-    const providersChanges = addToMetadata(
+    const providersChanges = addToModuleProviders(
       host,
       modulePath,
-      `provideDefaultConfig(defaultB2bOccConfig)`,
-      'providers',
+      `${PROVIDE_DEFAULT_CONFIG}(${DEFAULT_B2B_OCC_CONFIG})`,
       moduleSource
     );
-    commitChanges(host, modulePath, providersChanges, InsertDirection.RIGHT);
+    commitChanges(host, modulePath, providersChanges);
 
-    // TODO: create constants
-    addImport(host, modulePath, 'provideDefaultConfig', `@spartacus/core`);
-    addImport(host, modulePath, 'defaultB2bOccConfig', `@spartacus/setup`);
-    addImport(
-      host,
-      modulePath,
-      'OrganizationModule',
-      `@spartacus/my-account/organization`
-    );
+    addImport(host, modulePath, PROVIDE_DEFAULT_CONFIG, SPARTACUS_CORE);
+    addImport(host, modulePath, DEFAULT_B2B_OCC_CONFIG, SPARTACUS_SETUP);
+    addImport(host, modulePath, ORGANIZATION_MODULE, SPARTACUS_ORGANIZATION);
 
-    addToModuleImportsAndCommitChanges(host, modulePath, `OrganizationModule`);
+    addToModuleImportsAndCommitChanges(host, modulePath, ORGANIZATION_MODULE);
   };
-}
-
-// TODO: use from `@spartacus/schematics`
-function getProjectTargets(
-  projectOrHost: WorkspaceProject | Tree | WorkspaceSchema,
-  projectName = ''
-): WorkspaceTargets {
-  const project = isWorkspaceProject(projectOrHost)
-    ? projectOrHost
-    : getProject(projectOrHost, projectName);
-
-  const projectTargets = project.targets || project.architect;
-  if (!projectTargets) {
-    throw new Error('Project target not found.');
-  }
-
-  return projectTargets;
-}
-
-// TODO: use from `@spartacus/schematics`
-function isWorkspaceProject(project: any): project is WorkspaceProject {
-  return !!(project && (project as WorkspaceProject).projectType);
-}
-
-// TODO: use from `@spartacus/schematics`
-function getProject<TProjectType extends ProjectType = ProjectType.Application>(
-  workspaceOrHost: WorkspaceSchema | Tree,
-  projectName: string
-): WorkspaceProject<TProjectType> {
-  const workspace = isWorkspaceSchema(workspaceOrHost)
-    ? workspaceOrHost
-    : getWorkspaceAngular(workspaceOrHost);
-
-  return workspace.projects[projectName] as WorkspaceProject<TProjectType>;
-}
-
-// TODO: use from `@spartacus/schematics`
-function isWorkspaceSchema(workspace: any): workspace is WorkspaceSchema {
-  return !!(workspace && (workspace as WorkspaceSchema).projects);
-}
-
-// TODO: use from `@spartacus/schematics`
-const UTF_8 = 'utf-8';
-// TODO: use from `@spartacus/schematics`
-export function getTsSourceFile(tree: Tree, path: string): ts.SourceFile {
-  const buffer = tree.read(path);
-  if (!buffer) {
-    throw new SchematicsException(`Could not read file (${path}).`);
-  }
-  const content = buffer.toString(UTF_8);
-  const source = ts.createSourceFile(
-    path,
-    content,
-    ts.ScriptTarget.Latest,
-    true
-  );
-
-  return source;
-}
-
-// TODO: use from `@spartacus/schematics`
-function addImport(
-  host: Tree,
-  filePath: string,
-  importText: string,
-  importPath: string
-): void {
-  const moduleSource = getTsSourceFile(host, filePath);
-  if (!isImported(moduleSource, importText, importPath)) {
-    const change = insertImport(moduleSource, filePath, importText, importPath);
-    commitChanges(host, filePath, [change], InsertDirection.LEFT);
-  }
-}
-
-// TODO: use from `@spartacus/schematics`
-enum InsertDirection {
-  LEFT,
-  RIGHT,
-}
-
-// TODO: use from `@spartacus/schematics`
-function commitChanges(
-  host: Tree,
-  path: string,
-  changes: Change[] | null,
-  insertDirection: InsertDirection = InsertDirection.RIGHT
-): void {
-  if (!changes || changes.length === 0) {
-    return;
-  }
-
-  const recorder = host.beginUpdate(path);
-  changes.forEach((change) => {
-    if (change instanceof InsertChange) {
-      const pos = change.pos;
-      const toAdd = change.toAdd;
-      if (insertDirection === InsertDirection.LEFT) {
-        recorder.insertLeft(pos, toAdd);
-      } else {
-        recorder.insertRight(pos, toAdd);
-      }
-    } else if (change instanceof ReplaceChange) {
-      const pos = change['pos'];
-      const oldText = change['oldText'];
-      const newText = change['newText'];
-
-      recorder.remove(pos, oldText.length);
-      if (insertDirection === InsertDirection.LEFT) {
-        recorder.insertLeft(pos, newText);
-      } else {
-        recorder.insertRight(pos, newText);
-      }
-    } else if (change instanceof RemoveChange) {
-      const pos = change['pos'];
-      const length = change['toRemove'].length;
-      recorder.remove(pos, length);
-    }
-  });
-  host.commitUpdate(recorder);
-}
-
-// TODO: use from `@spartacus/schematics`
-function addToModuleImportsAndCommitChanges(
-  host: Tree,
-  modulePath: string,
-  importText: string
-): void {
-  const metadataChanges = addToModuleImports(host, modulePath, importText);
-  commitChanges(host, modulePath, metadataChanges, InsertDirection.RIGHT);
-}
-
-// TODO: use from `@spartacus/schematics`
-function addToModuleImports(
-  host: Tree,
-  modulePath: string,
-  importText: string,
-  moduleSource?: ts.SourceFile
-): InsertChange[] {
-  return addToMetadata(host, modulePath, importText, 'imports', moduleSource);
-}
-// TODO: use from `@spartacus/schematics`
-function addToMetadata(
-  host: Tree,
-  modulePath: string,
-  importText: string,
-  metadataType:
-    | 'imports'
-    | 'declarations'
-    | 'entryComponents'
-    | 'exports'
-    | 'providers',
-  moduleSource?: ts.SourceFile
-): InsertChange[] {
-  moduleSource = moduleSource || getTsSourceFile(host, modulePath);
-  return addSymbolToNgModuleMetadata(
-    moduleSource,
-    modulePath,
-    metadataType,
-    importText
-  ) as InsertChange[];
-}
-
-// TODO: use from `@spartacus/schematics`
-export function getSpartacusSchematicsVersion(): string {
-  return version;
-}
-
-// TODO: use from `@spartacus/schematics`
-export function getSourceRoot(
-  host: Tree,
-  options: { project?: string | undefined; path?: string | undefined } = {}
-): string {
-  const workspace = getWorkspace(host).workspace;
-
-  if (!options.project) {
-    options.project = getDefaultProjectNameFromWorkspace(host);
-  }
-
-  const sourceRoot = workspace.projects[options.project].sourceRoot;
-  if (!sourceRoot) {
-    throw new SchematicsException('No default project found');
-  }
-
-  return sourceRoot;
-}
-
-// TODO: use from `@spartacus/schematics`
-const DEFAULT_POSSIBLE_PROJECT_FILES = ['/angular.json', '/.angular.json'];
-// TODO: use from `@spartacus/schematics`
-export function getWorkspace(
-  host: Tree,
-  files = DEFAULT_POSSIBLE_PROJECT_FILES
-): { path: string; workspace: experimental.workspace.WorkspaceSchema } {
-  const angularJson = getAngularJsonFile(host, files);
-  const path = files.filter((filePath) => host.exists(filePath))[0];
-
-  return {
-    path,
-    workspace: angularJson,
-  };
-}
-// TODO: use from `@spartacus/schematics`
-function getAngularJsonFile(
-  tree: Tree,
-  possibleProjectFiles = DEFAULT_POSSIBLE_PROJECT_FILES
-): experimental.workspace.WorkspaceSchema {
-  const path = possibleProjectFiles.filter((filePath) =>
-    tree.exists(filePath)
-  )[0];
-  if (!path) {
-    throw new SchematicsException(`Could not find Angular`);
-  }
-
-  const configBuffer = tree.read(path);
-  if (configBuffer === null) {
-    throw new SchematicsException(`Could not find (${path})`);
-  }
-
-  const angularJsonContent = configBuffer.toString();
-  return (parseJson(
-    angularJsonContent,
-    JsonParseMode.Loose
-  ) as unknown) as experimental.workspace.WorkspaceSchema;
-}
-
-// TODO: use from `@spartacus/schematics`
-export function getDefaultProjectNameFromWorkspace(tree: Tree): string {
-  const workspace = getWorkspace(tree).workspace;
-
-  return workspace.defaultProject !== undefined
-    ? workspace.defaultProject
-    : Object.keys(workspace.projects)[0];
 }
