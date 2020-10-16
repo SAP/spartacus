90bf39231b8267be4fd22564af4bf6e9f81eb3d8
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-10-16 12:26:26 +0200

    renamed organization to administration

---
+++ feature-libs/my-account/administration/components/unit/links/addresses/list/unit-address-list.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { I18nTestingModule } from '@spartacus/core';
+import { OrganizationSubListTestingModule } from 'feature-libs/my-account/administration/components/shared/organization-sub-list/organization-sub-list.testing.module';
+import { UrlTestingModule } from 'projects/core/src/routing/configurable-routes/url-translation/testing/url-testing.module';
+import { UnitAddressListComponent } from './unit-address-list.component';
+import { UnitAddressListService } from './unit-address-list.service';
+
+class MockUnitAddressListService {}
+
+describe('UnitAddressListComponent', () => {
+  let component: UnitAddressListComponent;
+  let fixture: ComponentFixture<UnitAddressListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        OrganizationSubListTestingModule,
+        UrlTestingModule,
+        I18nTestingModule,
+      ],
+      providers: [
+        {
+          provide: UnitAddressListService,
+          useClass: MockUnitAddressListService,
+        },
+      ],
+      declarations: [UnitAddressListComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UnitAddressListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ feature-libs/my-account/administration/components/unit/links/users/list/unit-user-list.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { I18nTestingModule } from '@spartacus/core';
+import { OrganizationSubListTestingModule } from 'feature-libs/my-account/administration/components/shared/organization-sub-list/organization-sub-list.testing.module';
+import { UrlTestingModule } from 'projects/core/src/routing/configurable-routes/url-translation/testing/url-testing.module';
+import { UnitUserListService } from '../services/unit-user-list.service';
+import { UnitUserListComponent } from './unit-user-list.component';
+
+class MockUnitUserListService {}
+
+describe('UnitUserListComponent', () => {
+  let component: UnitUserListComponent;
+  let fixture: ComponentFixture<UnitUserListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        OrganizationSubListTestingModule,
+        UrlTestingModule,
+        I18nTestingModule,
+      ],
+      providers: [
+        {
+          provide: UnitUserListService,
+          useClass: MockUnitUserListService,
+        },
+      ],
+      declarations: [UnitUserListComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UnitUserListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
