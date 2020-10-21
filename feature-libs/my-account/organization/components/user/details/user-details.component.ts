71e65cea30b36567b2cc433fb888d39febce140d
C5282366 <michal.szczepaniak01@sap.com>
2020-10-21 12:12:34 +0200

    GH-8973: Refactor user details

--- feature-libs/my-account/organization/components/user/details/user-details.component.ts
+++ feature-libs/my-account/organization/components/user/details/user-details.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { B2BUser } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { shareReplay, switchMap } from 'rxjs/operators';
@@ -8,11 +8,15 @@ import { OrganizationItemService } from '../../shared/organization-item.service'
   templateUrl: './user-details.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserDetailsComponent {
-  model$: Observable<B2BUser> = this.itemService.key$.pipe(
-    switchMap((code) => this.itemService.load(code)),
-    shareReplay({ bufferSize: 1, refCount: true })
-  );
+export class UserDetailsComponent implements OnInit {
+  model$: Observable<B2BUser>;
+
+  ngOnInit() {
+    this.model$ = this.itemService.key$.pipe(
+      switchMap((code) => this.itemService.load(code)),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
 
   constructor(protected itemService: OrganizationItemService<B2BUser>) {}
 }
