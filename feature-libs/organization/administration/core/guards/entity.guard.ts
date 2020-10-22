09b84b14e1e6c24ef7a817ccbaf21c87f6d58bfc
C5282366 <michal.szczepaniak01@sap.com>
2020-10-22 15:41:35 +0200

    GH-8973: Fix template

--- feature-libs/organization/administration/core/guards/entity.guard.ts
+++ feature-libs/organization/administration/core/guards/entity.guard.ts
@@ -73,7 +73,6 @@ export class EntityGuard {
           item: entityData[0].charAt(0).toUpperCase() + entityData[0].slice(1),
         },
       },
-      type: GlobalMessageType.MSG_TYPE_ERROR,
     });
   }
 }
