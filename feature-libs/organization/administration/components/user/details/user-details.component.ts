21d1c0c3f556168da47b6a128b11c9433fad592a
C5282366 <michal.szczepaniak01@sap.com>
2020-10-23 16:13:00 +0200

    WIP

--- feature-libs/organization/administration/components/user/details/user-details.component.ts
+++ feature-libs/organization/administration/components/user/details/user-details.component.ts
@@ -1,18 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
 import { B2BUser } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { shareReplay, switchMap } from 'rxjs/operators';
 import { OrganizationItemService } from '../../shared/organization-item.service';
+import { ExistUserGuard } from '../guards';
 
 @Component({
   templateUrl: './user-details.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ExistUserGuard],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements AfterViewInit {
   model$: Observable<B2BUser> = this.itemService.key$.pipe(
     switchMap((code) => this.itemService.load(code)),
     shareReplay({ bufferSize: 1, refCount: true })
   );
 
-  constructor(protected itemService: OrganizationItemService<B2BUser>) {}
+  ngAfterViewInit() {
+    console.log('guard check!');
+    setTimeout(() => {
+      this.existUserGuard.canActivate();
+    }, 500);
+  }
+
+  constructor(
+    protected itemService: OrganizationItemService<B2BUser>,
+    protected existUserGuard: ExistUserGuard
+  ) {}
 }
--- feature-libs/organization/administration/components/user/guards/exist-user.guard.ts
+++ feature-libs/organization/administration/components/user/guards/exist-user.guard.ts
@@ -1,29 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Router, UrlTree } from '@angular/router';
+import { ActivatedRoute, Router, UrlTree } from '@angular/router';
 import {
   B2BUser,
   GlobalMessageService,
   GlobalMessageType,
+  RoutingService,
   SemanticPathService,
 } from '@spartacus/core';
 import { B2BUserService } from '@spartacus/organization/administration/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ROUTE_PARAMS } from '../../constants';
-import { ExistOrganizationItemGuard } from '../../shared/exist-organization-item.guard';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ExistUserGuard extends ExistOrganizationItemGuard<B2BUser> {
+export class ExistUserGuard {
   protected code = ROUTE_PARAMS.userCode;
 
   constructor(
     protected userService: B2BUserService,
     protected router: Router,
     protected semanticPathService: SemanticPathService,
-    protected globalMessageService: GlobalMessageService
-  ) {
-    super();
+    protected globalMessageService: GlobalMessageService,
+    protected route: ActivatedRoute,
+    protected routingService: RoutingService
+  ) {}
+
+  canActivate() {
+    const urlParams = {
+      code:
+        this.route.params[this.code] ?? this.route.parent?.params[this.code],
+    };
+
+    return this.getItem(urlParams.code).pipe(
+      map((item) => {
+        if (item && this.isValid(item)) {
+          return;
+        }
+        this.showErrorMessage();
+        this.routingService.go({
+          cxRoute: `userDetails`,
+          params: { userCode: urlParams.code },
+        });
+      })
+    );
+  }
+
+  protected isValid(item): boolean {
+    // change it, since the reducer initial state is undefined now
+    return Object.keys(item).length !== 0;
   }
 
   protected getItem(code: string): Observable<B2BUser> {
