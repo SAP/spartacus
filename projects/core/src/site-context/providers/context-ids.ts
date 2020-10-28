e6660ee9c71fbf455221da366e1f4d84d0ae469d
Weizheng Gao <weizheng.gao@sap.com>
2020-10-28 16:06:43 -0400

    feature/GH-8459

--- projects/core/src/site-context/providers/context-ids.ts
+++ projects/core/src/site-context/providers/context-ids.ts
@@ -1,4 +1,4 @@
 export const LANGUAGE_CONTEXT_ID = 'language';
 export const CURRENCY_CONTEXT_ID = 'currency';
 export const BASE_SITE_CONTEXT_ID = 'baseSite';
-export const BASE_SITES_CONTEXT_ID = 'baseSite';
+export const BASE_SITES_CONTEXT_ID = 'baseSites';
