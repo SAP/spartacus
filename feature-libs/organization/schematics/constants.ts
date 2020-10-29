fc5ce2ff4d7ba386aadb63b683820c6b86ab687a
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-10-29 17:13:49 +0100

    moved constants

---
+++ feature-libs/organization/schematics/constants.ts
@@ -0,0 +1,10 @@
+export const ORGANIZATION_ADMINISTRATION_FEATURE_NAME =
+  'organizationAdministration';
+export const ADMINISTRATION_MODULE = 'AdministrationModule';
+export const ADMINISTRATION_ROOT_MODULE = 'AdministrationRootModule';
+export const ORDER_APPROVAL_MODULE = 'OrderApprovalModule';
+export const ORDER_APPROVAL_ROOT_MODULE = 'OrderApprovalRootModule';
+
+export const SPARTACUS_ORGANIZATION = '@spartacus/organization';
+export const SPARTACUS_ADMINISTRATION = `${SPARTACUS_ORGANIZATION}/administration`;
+export const SPARTACUS_ADMINISTRATION_ROOT = `${SPARTACUS_ADMINISTRATION}/root`;
