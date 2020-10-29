4bd85e4d55aaee7130a357ccba8d6205422ca437
Weizheng Gao <weizheng.gao@sap.com>
2020-10-29 10:51:57 -0400

    feature/GH-9493: format

--- projects/storefrontlib/src/cms-components/myaccount/order/order-details/index.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/order-details/index.ts
@@ -4,4 +4,3 @@ export * from './order-detail-shipping/order-detail-shipping.component';
 export * from './order-detail-totals/order-detail-totals.component';
 export * from './order-details.module';
 export * from './order-details.service';
-
