f71cb102f002cd5f9f870e90f6f8948bd88c6207
Michał Gruca <pucek9@gmail.com>
2020-10-31 01:45:45 +0100

    up

--- feature-libs/organization/administration/components/user-group/users/user-group-user-list.component.spec.ts
+++ feature-libs/organization/administration/components/user-group/users/user-group-user-list.component.spec.ts
@@ -17,6 +17,7 @@ const mockKey = 'mock';
 class MockCurrentUserGroupService {
   key$ = of(mockKey);
 }
+
 class MockUserGroupUserListService {
   unassignAllMembers(): Observable<OrganizationItemStatus<UserGroup>> {
     return of({ status: LoadStatus.SUCCESS, item: {} });
@@ -27,6 +28,7 @@ describe('UserGroupUserListComponent', () => {
   let component: UserGroupUserListComponent;
   let fixture: ComponentFixture<UserGroupUserListComponent>;
   let userGroupUserListService: UserGroupUserListService;
+  let messageService: MessageService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -49,6 +51,7 @@ describe('UserGroupUserListComponent', () => {
     }).compileComponents();
 
     userGroupUserListService = TestBed.inject(UserGroupUserListService);
+    messageService = TestBed.inject(MessageService);
     fixture = TestBed.createComponent(UserGroupUserListComponent);
     component = fixture.componentInstance;
   });
@@ -58,11 +61,25 @@ describe('UserGroupUserListComponent', () => {
   });
 
   it('should unassign all members', () => {
-    spyOn(userGroupUserListService, 'unassignAllMembers');
+    spyOn(userGroupUserListService, 'unassignAllMembers').and.callThrough();
 
     component.unassignAll();
     expect(userGroupUserListService.unassignAllMembers).toHaveBeenCalledWith(
       mockKey
     );
+  });
+
+  it('should notify after unassign all members', () => {
+    spyOn(messageService, 'add').and.callThrough();
+
+    component.unassignAll();
+    expect(messageService.add).toHaveBeenCalledWith({
+      message: {
+        key: `userGroupUsers.unassignAllConfirmation`,
+        params: {
+          item: {},
+        },
+      },
+    });
   });
 });
