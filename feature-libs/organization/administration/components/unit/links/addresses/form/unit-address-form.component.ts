9ef8ff91dea1bfd7f6a4c9b7014bed818765079c
Michał Gruca <pucek9@gmail.com>
2020-11-02 17:26:21 +0100

    fix address form animation

--- feature-libs/organization/administration/components/unit/links/addresses/form/unit-address-form.component.ts
+++ feature-libs/organization/administration/components/unit/links/addresses/form/unit-address-form.component.ts
@@ -20,6 +20,7 @@ import { UnitAddressFormService } from './unit-address-form.service';
 export class UnitAddressFormComponent implements OnInit {
   form: FormGroup = this.itemService.getForm();
 
+  key$ = this.itemService.key$;
   countries$: Observable<Country[]> = this.formService.getCountries();
   titles$: Observable<Title[]> = this.formService.getTitles();
   regions$: Observable<Region[]> = this.formService.getRegions();
