17dc4865e636bcb77c7fde017618bd9c89a9818b
Weizheng Gao <weizheng.gao@sap.com>
2020-11-03 15:40:02 -0500

    feature/GH-9544

--- projects/schematics/src/migrations/3_0/constructor-deprecations/data/routing.service.migration.ts
+++ projects/schematics/src/migrations/3_0/constructor-deprecations/data/routing.service.migration.ts
@@ -9,30 +9,28 @@ import {
 } from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
-export const ROUTING_SERVICE_MIGRATIONS: ConstructorDeprecation[] = [
+export const ROUTING_SERVICE_MIGRATION: ConstructorDeprecation = {
   // projects/core/src/routing/facade/routing.service.ts
-  {
-    class: ROUTING_SERVICE,
-    importPath: SPARTACUS_CORE,
-    deprecatedParams: [
-      {
-        className: STORE,
-        importPath: NGRX_STORE,
-      },
-      {
-        className: WINDOW_REF,
-        importPath: SPARTACUS_CORE,
-      },
-      {
-        className: SEMANTIC_PATH_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-    ],
-    addParams: [
-      {
-        className: ROUTING_PARAMS_SERVICE,
-        importPath: SPARTACUS_CORE,
-      },
-    ],
-  },
-];
+  class: ROUTING_SERVICE,
+  importPath: SPARTACUS_CORE,
+  deprecatedParams: [
+    {
+      className: STORE,
+      importPath: NGRX_STORE,
+    },
+    {
+      className: WINDOW_REF,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: SEMANTIC_PATH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  addParams: [
+    {
+      className: ROUTING_PARAMS_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+};
