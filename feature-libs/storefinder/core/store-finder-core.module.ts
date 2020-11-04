00356e2fb4052fd5437edb6495e79ae0bc0e76c9
dunqan <dunqan@w9.pl>
2020-11-04 12:41:23 +0100

    fix connector providers

--- feature-libs/storefinder/core/store-finder-core.module.ts
+++ feature-libs/storefinder/core/store-finder-core.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { provideDefaultConfig } from '@spartacus/core';
 import { defaultStoreFinderConfig } from './config/default-store-finder-config';
 import { StoreFinderStoreModule } from './store/store-finder-store.module';
+import { StoreFinderConnector } from './connectors/store-finder.connector';
 
 @NgModule({
   imports: [StoreFinderStoreModule],
-  providers: [provideDefaultConfig(defaultStoreFinderConfig)],
+  providers: [
+    provideDefaultConfig(defaultStoreFinderConfig),
+    StoreFinderConnector,
+  ],
 })
 export class StoreFinderCoreModule {}
