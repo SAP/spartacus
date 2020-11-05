18bbb8a40eef7ef4a01f90769efa2b6882e1727f
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-11-05 15:42:35 -0500

    GH-9553 Forgot to add this one

--- projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
+++ projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
@@ -5,12 +5,14 @@ import { CHECKOUT_ADAPTER_MIGRATION } from './data/checkout.adapter.migration';
 import { CHECKOUT_CONNECTOR_MIGRATION } from './data/checkout.connector.migration';
 import { CHECKOUT_SERVICE_MIGRATION } from './data/checkout.service.migration';
 import { CMS_COMPONENTS_SERVICE_MIGRATION } from './data/cms-components.service.migration';
+import { OCC_CHECKOUT_ADAPTER_MIGRATION } from './data/occ-checkout.adapter.migration';
 
 export const METHOD_PROPERTY_DATA: MethodPropertyDeprecation[] = [
   ...CHECKOUT_CONNECTOR_MIGRATION,
   ...CHECKOUT_ADAPTER_MIGRATION,
   ...CHECKOUT_SERVICE_MIGRATION,
   ...CMS_COMPONENTS_SERVICE_MIGRATION,
+  ...OCC_CHECKOUT_ADAPTER_MIGRATION,
 ];
 
 export function migrate(): Rule {
