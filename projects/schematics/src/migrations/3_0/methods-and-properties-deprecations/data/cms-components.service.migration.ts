b8533b00298e6ca7d13c7b0e611e99f450986cf7
Krzysztof Platis <platonn.git@gmail.com>
2020-11-05 12:13:46 +0100

    fix build

--- projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/data/cms-components.service.migration.ts
+++ projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/data/cms-components.service.migration.ts
@@ -1,12 +1,8 @@
 import {
   CMS_COMPONENTS_SERVICE,
-
   GET_CHILD_ROUTES,
-
-
-
   SPARTACUS_STOREFRONTLIB,
-  TODO_SPARTACUS
+  TODO_SPARTACUS,
 } from '../../../../shared/constants';
 import { MethodPropertyDeprecation } from '../../../../shared/utils/file-utils';
 
@@ -17,6 +13,6 @@ export const CMS_COMPONENTS_SERVICE_MIGRATION: MethodPropertyDeprecation[] = [
     importPath: SPARTACUS_STOREFRONTLIB,
     deprecatedNode: GET_CHILD_ROUTES,
     newNode: GET_CHILD_ROUTES,
-    comment: `// ${TODO_SPARTACUS} Method '${GET_CHILD_ROUTES}' changed the return type from 'Route[]' to 'CmsComponentChildRoutesConfig',
+    comment: `// ${TODO_SPARTACUS} Method '${GET_CHILD_ROUTES}' changed the return type from 'Route[]' to 'CmsComponentChildRoutesConfig'`,
   },
 ];
