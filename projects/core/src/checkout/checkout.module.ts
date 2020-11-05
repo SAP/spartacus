93c1cb2f05c98dd2e6576f1cc80187c45cf636dc
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2020-11-05 09:02:17 -0500

    GH-5652: fix build

--- projects/core/src/checkout/checkout.module.ts
+++ projects/core/src/checkout/checkout.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { PageMetaResolver } from '../cms/page/page-meta.resolver';
 import { CheckoutEventModule } from './events/checkout-event.module';
-import { interceptors } from './http-interceptors';
+import { interceptors } from './http-interceptors/index';
 import { CheckoutPageMetaResolver } from './services/checkout-page-meta.resolver';
 import { CheckoutStoreModule } from './store/checkout-store.module';
 
