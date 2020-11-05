fd257136da5139ed4a61981e7abbc9645caac43f
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-11-05 17:18:38 -0500

    code review changes

--- projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/data/store-finder-group.actions.migration.ts
+++ projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/data/store-finder-group.actions.migration.ts
@@ -2,14 +2,15 @@ import {
   FIND_STORES_CLASS,
   SEARCH_CONFIG,
   SPARTACUS_CORE,
-  StoreFinderActions,
+  STORE_FINDER_ACTIONS,
   TODO_SPARTACUS,
 } from '../../../../shared/constants';
 import { MethodPropertyDeprecation } from '../../../../shared/utils/file-utils';
 
-export const STORE_FINDER_GROUP_ACTIONS_MIGRATION: MethodPropertyDeprecation[] = [
+// projects/core/src/store-finder/store/actions/index.ts
+export const STORE_FINDER_ACTIONS_MIGRATION: MethodPropertyDeprecation[] = [
   {
-    class: StoreFinderActions,
+    class: STORE_FINDER_ACTIONS,
     importPath: SPARTACUS_CORE,
     deprecatedNode: FIND_STORES_CLASS,
     comment: `// ${TODO_SPARTACUS} please change the property type of 'searchConfig' to '${SEARCH_CONFIG}' for '${FIND_STORES_CLASS}' action`,
