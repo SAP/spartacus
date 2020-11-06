fdc02a98310ac03cdbdbca2b780b9cfc029012ba
Louis Pierrestiger <louis.pierrestiger@sap.com>
2020-11-06 16:08:54 -0500

    chore: breakpoint service deprecation migration (#9669)
    Closes #9564

--- projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
+++ projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
@@ -2,6 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { MethodPropertyDeprecation } from '../../../shared/utils/file-utils';
 import { migrateMethodPropertiesDeprecation } from '../../mechanism/methods-and-properties-deprecations/methods-and-properties-deprecations';
 import { BASE_SITE_SERVICE_MIGRATION } from './data/base-site.service.migration';
+import { BREAKPOINT_SERVICE_MIGRATION } from './data/breakpoint.service.migration';
 import { CHECKOUT_AUTH_GUARD_MIGRATION } from './data/checkout-auth.guard.migration';
 import { CHECKOUT_CONFIG_SERVICE_MIGRATION } from './data/checkout-config.service.migration';
 import { CHECKOUT_GROUP_ACTIONS_MIGRATION } from './data/checkout-group.actions.migration';
@@ -15,6 +16,7 @@ export const METHOD_PROPERTY_DATA: MethodPropertyDeprecation[] = [
   ...CHECKOUT_CONFIG_SERVICE_MIGRATION,
   ...CHECKOUT_AUTH_GUARD_MIGRATION,
   ...CMS_COMPONENTS_SERVICE_MIGRATION,
+  ...BREAKPOINT_SERVICE_MIGRATION,
   ...ITEM_COUNTER_COMPONENT_MIGRATION,
   ...STORE_FINDER_ACTIONS_MIGRATION,
   ...UPDATE_EMAIL_COMPONENT_MIGRATION,
