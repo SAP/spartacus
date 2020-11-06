3c458fd7abc9238e1aee18e0e1c278924b52dab2
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2020-11-06 10:53:24 -0500

    update method_property_data with correct variable name

--- projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
+++ projects/schematics/src/migrations/3_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
@@ -3,13 +3,13 @@ import { MethodPropertyDeprecation } from '../../../shared/utils/file-utils';
 import { migrateMethodPropertiesDeprecation } from '../../mechanism/methods-and-properties-deprecations/methods-and-properties-deprecations';
 import { CMS_COMPONENTS_SERVICE_MIGRATION } from './data/cms-components.service.migration';
 import { ITEM_COUNTER_COMPONENT_MIGRATION } from './data/item-counter.component.migration';
-import { STORE_FINDER_GROUP_ACTIONS_MIGRATION } from './data/store-finder-group.actions.migration';
+import { STORE_FINDER_ACTIONS_MIGRATION } from './data/store-finder-group.actions.migration';
 import { UPDATE_EMAIL_COMPONENT_MIGRATION } from './data/update-email.component.migration';
 
 export const METHOD_PROPERTY_DATA: MethodPropertyDeprecation[] = [
   ...CMS_COMPONENTS_SERVICE_MIGRATION,
   ...ITEM_COUNTER_COMPONENT_MIGRATION,
-  ...STORE_FINDER_GROUP_ACTIONS_MIGRATION,
+  ...STORE_FINDER_ACTIONS_MIGRATION,
   ...UPDATE_EMAIL_COMPONENT_MIGRATION,
 ];
 
