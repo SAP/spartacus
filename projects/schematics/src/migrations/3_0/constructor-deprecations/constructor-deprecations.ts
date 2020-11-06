022761147425f601de3cf5b2bf22594a99429166
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2020-11-06 08:52:03 -0500

    GH-9683: added missing ctr migration

--- projects/schematics/src/migrations/3_0/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/3_0/constructor-deprecations/constructor-deprecations.ts
@@ -1,12 +1,14 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { ConstructorDeprecation } from '../../../shared/utils/file-utils';
 import { migrateConstructorDeprecation } from '../../mechanism/constructor-deprecations/constructor-deprecations';
+import { CONTENT_PAGE_META_RESOLVER_MIGRATION } from './data/content-page-meta.resolver.migration';
 import { MULTI_CART_SERVICE_MIGRATION } from './data/multi-cart.service.migration';
 import { ROUTING_SERVICE_MIGRATION } from './data/routing.service.migration';
 
 export const CONSTRUCTOR_DEPRECATION_DATA: ConstructorDeprecation[] = [
   ROUTING_SERVICE_MIGRATION,
   MULTI_CART_SERVICE_MIGRATION,
+  CONTENT_PAGE_META_RESOLVER_MIGRATION,
 ];
 
 export function migrate(): Rule {
