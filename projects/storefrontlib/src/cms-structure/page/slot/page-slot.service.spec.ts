e81a369dc1c76ce845a6c7ada0a710e01fe8bf9c
dunqan <dunqan@w9.pl>
2020-11-06 13:58:34 +0100

    add tests

---
+++ projects/storefrontlib/src/cms-structure/page/slot/page-slot.service.spec.ts
@@ -0,0 +1,61 @@
+import { TestBed } from '@angular/core/testing';
+import { PageSlotService } from './page-slot.service';
+import { DOCUMENT } from '@angular/common';
+
+function createSlotElementStub(slotName, top) {
+  return {
+    classList: [slotName],
+    getBoundingClientRect: () => ({ top }),
+  };
+}
+
+const documentStub = {
+  querySelectorAll: () => [
+    createSlotElementStub('test', 20),
+    createSlotElementStub('test-2', 100),
+  ],
+
+  documentElement: {
+    clientHeight: 80,
+  },
+};
+
+describe('PageSlotService', () => {
+  let pageSlotService: PageSlotService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: DOCUMENT,
+          useValue: documentStub,
+        },
+      ],
+    });
+
+    pageSlotService = TestBed.inject(PageSlotService);
+  });
+
+  it('should inject service', () => {
+    expect(pageSlotService).toBeTruthy();
+  });
+
+  describe('shouldNotDefer', () => {
+    describe('for slot that is visible in SSR viewport', () => {
+      it('should return true', () => {
+        expect(pageSlotService.shouldNotDefer('test')).toBeTrue();
+      });
+
+      it('should return true only once', () => {
+        expect(pageSlotService.shouldNotDefer('test')).toBeTrue();
+        expect(pageSlotService.shouldNotDefer('test')).toBeFalse();
+      });
+    });
+
+    describe('for slot that is visible in SSR viewport', () => {
+      it('should return false', () => {
+        expect(pageSlotService.shouldNotDefer('test-2')).toBeFalse();
+      });
+    });
+  });
+});
--- projects/storefrontlib/src/cms-structure/page/slot/page-slot.service.ts
+++ projects/storefrontlib/src/cms-structure/page/slot/page-slot.service.ts
@@ -8,18 +8,24 @@ export class PageSlotService {
   protected instantSsrSlots: string[] | undefined;
 
   constructor(
-    @Inject(PLATFORM_ID) platformId: any,
-    @Inject(DOCUMENT) document
+    @Inject(PLATFORM_ID) protected platformId: any,
+    @Inject(DOCUMENT) protected document
   ) {
-    if (isPlatformBrowser(platformId)) {
-      // Find all slots visible in the SSR pre-rendered DOM
+    this.discoverInstantSsrSlots();
+  }
+
+  /**
+   * Finds all slots visible in the SSR pre-rendered DOM
+   */
+  protected discoverInstantSsrSlots() {
+    if (isPlatformBrowser(this.platformId)) {
       this.instantSsrSlots = Array.from(
-        document.querySelectorAll('cx-page-slot')
+        this.document.querySelectorAll('cx-page-slot')
       )
         .filter(
           (el: Element) =>
             el.getBoundingClientRect().top <
-            document.documentElement.clientHeight
+            this.document.documentElement.clientHeight
         )
         .map((el: Element) => el.classList[0]);
     }
