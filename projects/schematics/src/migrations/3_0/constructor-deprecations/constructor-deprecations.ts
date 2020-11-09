1891181655466ee86d753a046d8db4a6dcd429fc
dunqan <dunqan@w9.pl>
2020-11-09 13:36:21 +0100

    add migrations

--- projects/schematics/src/migrations/3_0/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/3_0/constructor-deprecations/constructor-deprecations.ts
@@ -17,6 +17,7 @@ import { REVIEW_SUBMIT_COMPONENT_MIGRATION } from './data/review-submit.componen
 import { ROUTING_SERVICE_MIGRATION } from './data/routing.service.migration';
 import { SHIPPING_ADDRESS_SET_GUARD_MIGRATION } from './data/shipping-address-set.guard.migration';
 import { SHIPPING_ADDRESS_COMPONENT_MIGRATION } from './data/shipping-address.component.migration';
+import { PAGE_SLOT_COMPONENT_MIGRATION } from './data/page-slot.component.migration';
 
 export const CONSTRUCTOR_DEPRECATION_DATA: ConstructorDeprecation[] = [
   CHECKOUT_PROGRESS_MOBILE_BOTTOM_COMPONENT_MIGRATION,
@@ -35,6 +36,7 @@ export const CONSTRUCTOR_DEPRECATION_DATA: ConstructorDeprecation[] = [
   CHECKOUT_AUTH_GUARD_MIGRATION,
   ORDER_DETAILS_SHIPPING_COMPONENT_MIGRATION,
   ROUTING_SERVICE_MIGRATION,
+  PAGE_SLOT_COMPONENT_MIGRATION,
 ];
 
 export function migrate(): Rule {
