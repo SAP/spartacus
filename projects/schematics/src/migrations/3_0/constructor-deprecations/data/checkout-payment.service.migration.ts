ce8f711f4001c4660a66e50548765f545d0ec7e1
Patryk Smul <psmul@divante.pl>
2020-11-12 09:50:33 +0100

    Add checkout-payment.service.migration.ts

---
+++ projects/schematics/src/migrations/3_0/constructor-deprecations/data/checkout-payment.service.migration.ts
@@ -0,0 +1,31 @@
+import {
+  AUTH_SERVICE,
+  SPARTACUS_CORE,
+  CHECKOUT_PAYMENT_SERVICE,
+  USER_ID_SERVICE,
+} from '../../../../shared/constants';
+import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
+
+export const CHECKOUT_PAYMENT_SERVICE_MIGRATION: ConstructorDeprecation = {
+  // projects/core/src/checkout/facade/checkout-payment.service.ts
+  class: CHECKOUT_PAYMENT_SERVICE,
+  importPath: SPARTACUS_CORE,
+  deprecatedParams: [
+    {
+      className: AUTH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  removeParams: [
+    {
+      className: AUTH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  addParams: [
+    {
+      className: USER_ID_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+};
