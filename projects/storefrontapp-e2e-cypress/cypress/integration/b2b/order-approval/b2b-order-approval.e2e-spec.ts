9bf3556df12aa0094ccfb2f8af7d6b786dc9081d
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-11-13 15:41:24 -0500

    Gh-9504 Adding approval dashboard test specs

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/order-approval/b2b-order-approval.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/order-approval/b2b-order-approval.e2e-spec.ts
@@ -120,6 +120,59 @@ describe('B2B - Order Approval', () => {
       assertPermissionResults(sampleData.rejectedOrderDetails.order);
     });
   });
+
+  describe('B2B - Order Approval dashboard', () => {
+    const APPROVAL_DASHBOARD_ROUTE =
+      'powertools-spa/en/USD/my-account/approval-dashboard';
+
+    describe('Anonymous user', () => {
+      it('should NOT be allowed to access order approval dashboard directly', () => {
+        cy.visit(APPROVAL_DASHBOARD_ROUTE);
+        cy.location('pathname').should('contain', '/login');
+      });
+    });
+
+    describe('Unauthorized user (regular)', () => {
+      before(() => {
+        orderApproval.loginB2bUser();
+      });
+
+      it('should NOT display order approval option in menu', () => {
+        cy.visit('/');
+        orderApproval.checkApprovalDashboardMenuOptionExistence(false);
+        cy.saveLocalStorage();
+      });
+
+      it('should NOT be allowed to access order approval dashboard directly', () => {
+        cy.restoreLocalStorage();
+        cy.visit(APPROVAL_DASHBOARD_ROUTE);
+        cy.location('pathname').should('contain', '/login');
+      });
+    });
+
+    describe('Authorized user (approver)', () => {
+      before(() => {
+        orderApproval.loginB2bApprover();
+      });
+
+      it('should display order approval option in menu', () => {
+        cy.visit('/');
+        orderApproval.checkApprovalDashboardMenuOptionExistence(true);
+        cy.saveLocalStorage();
+      });
+
+      it('should BE allowed to access order approval dashboard directly', () => {
+        cy.restoreLocalStorage();
+        cy.visit(APPROVAL_DASHBOARD_ROUTE);
+        cy.location('pathname').should('contain', 'approval-dashboard');
+        cy.get('cx-breadcrumb > h1').should(
+          'contain',
+          'Order Approval Dashboard'
+        );
+        cy.get('cx-order-approval-list > table').should('exist');
+      });
+    });
+  });
 });
 
 function assertButtons() {
