c802510bc0aadd02d36a11a6299e1eac4948402c
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-11-16 15:09:40 +0100

    added i18n support

--- feature-libs/organization/schematics/constants.ts
+++ feature-libs/organization/schematics/constants.ts
@@ -6,6 +6,10 @@ export const ADMINISTRATION_ROOT_MODULE = 'AdministrationRootModule';
 export const ADMINISTRATION_MODULE = 'AdministrationModule';
 export const SPARTACUS_ADMINISTRATION = `${SPARTACUS_ORGANIZATION}/administration`;
 export const SPARTACUS_ADMINISTRATION_ROOT = `${SPARTACUS_ADMINISTRATION}/root`;
+export const SPARTACUS_ADMINISTRATION_ASSETS = `${SPARTACUS_ADMINISTRATION}/assets`;
+export const ORGANIZATION_TRANSLATIONS = 'organizationTranslations';
+export const ORGANIZATION_TRANSLATION_CHUNKS_CONFIG =
+  'organizationTranslationChunksConfig';
 
 export const ORGANIZATION_ORDER_APPROVAL_FEATURE_NAME =
   'organizationOrderApproval';
@@ -13,6 +17,10 @@ export const ORDER_APPROVAL_MODULE = 'OrderApprovalModule';
 export const ORDER_APPROVAL_ROOT_MODULE = 'OrderApprovalRootModule';
 export const SPARTACUS_ORDER_APPROVAL = `${SPARTACUS_ORGANIZATION}/order-approval`;
 export const SPARTACUS_ORDER_APPROVAL_ROOT = `${SPARTACUS_ORDER_APPROVAL}/root`;
+export const SPARTACUS_ORDER_APPROVAL_ASSETS = `${SPARTACUS_ORDER_APPROVAL}/assets`;
+export const ORDER_APPROVAL_TRANSLATIONS = 'orderApprovalTranslations';
+export const ORDER_APPROVAL_TRANSLATION_CHUNKS_CONFIG =
+  'orderApprovalTranslationChunksConfig';
 
 export const CLI_ADMINISTRATION_FEATURE = 'Administration';
 export const CLI_ORDER_APPROVAL_FEATURE = 'Order-approval';
