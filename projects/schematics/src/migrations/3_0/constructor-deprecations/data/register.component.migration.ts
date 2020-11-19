3768c10573f83562afdea87bae6f1d4e70108b53
C5282366 <michal.szczepaniak01@sap.com>
2020-11-19 14:18:24 +0100

    GH-9947

---
+++ projects/schematics/src/migrations/3_0/constructor-deprecations/data/register.component.migration.ts
@@ -0,0 +1,42 @@
+import {
+  REGISTER_COMPONENT,
+  SPARTACUS_STOREFRONTLIB,
+  ANONYMOUS_CONSENTS_SERVICE,
+  ANONYMOUS_CONSENTS_CONFIG,
+  AUTH_CONFIG_SERVICE,
+  FORM_BUILDER,
+  ANGULAR_FORMS,
+  ROUTING_SERVICE,
+  SPARTACUS_CORE
+} from '../../../../shared/constants';
+import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
+
+export const REGISTER_COMPONENT_MIGRATION: ConstructorDeprecation = {
+  // projects/storefrontlib/src/cms-components/user/register/review-submit.component.ts
+  class: REGISTER_COMPONENT,
+  importPath: SPARTACUS_STOREFRONTLIB,
+  deprecatedParams: [
+    {
+      className: FORM_BUILDER,
+      importPath: ANGULAR_FORMS,
+    },
+    {
+      className: ROUTING_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ANONYMOUS_CONSENTS_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ANONYMOUS_CONSENTS_CONFIG,
+      importPath: SPARTACUS_CORE,
+    },
+  ],
+  addParams: [
+    {
+      className: AUTH_CONFIG_SERVICE,
+      importPath: SPARTACUS_CORE,
+    }
+  ],
+};
