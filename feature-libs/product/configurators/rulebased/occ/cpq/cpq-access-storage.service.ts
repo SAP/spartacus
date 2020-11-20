7ac43521d3f3cc82de8b0b2ab26a094b6d7905ff
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2020-11-20 16:34:42 +0100

    remove some unused stuff

--- feature-libs/product/configurators/rulebased/occ/cpq/cpq-access-storage.service.ts
+++ feature-libs/product/configurators/rulebased/occ/cpq/cpq-access-storage.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, InjectionToken } from '@angular/core';
-import { BehaviorSubject, Observable, timer } from 'rxjs';
-import { expand, filter, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { expand, filter, shareReplay, switchMap } from 'rxjs/operators';
 import { Cpq } from '../../cpq/cpq.models';
 import { CpqAccessLoaderService } from './cpq-access-loader.service';
 
@@ -24,8 +24,6 @@ export class CpqAccessStorageService {
   ) {}
 
   protected cpqAccessData: Observable<Cpq.AccessData>;
-  protected currentCpqAccessData: Cpq.AccessData;
-  protected cache: BehaviorSubject<Cpq.AccessData>;
 
   private static defaultValue() {
     return 1000;
@@ -35,21 +33,14 @@ export class CpqAccessStorageService {
     if (!this.cpqAccessData) {
       this.cpqAccessData = this.cpqAccessLoaderService.getCpqAccessData().pipe(
         // schedule an auto refresh of token, when it expires
-        tap((data) => console.log(`fetched first token ${data.accessToken}`)),
         expand((data) =>
           timer(this.fetchNextTokenIn(data)).pipe(
-            tap(() =>
-              console.log(`refreshing token data ${data.accessToken} now`)
-            ),
             switchMap(() => this.cpqAccessLoaderService.getCpqAccessData())
           )
         ),
-        tap((data) => console.log(`received a token ${data.accessToken} now`)),
-        tap((data) => (this.currentCpqAccessData = data)), // store current data
         shareReplay(1),
         //token might be expired in the meantime and a new one was not feched, yet,
-        filter((data) => !this.isTokenExpired(data)),
-        tap((data) => console.log(`sending token ${data.accessToken} now`))
+        filter((data) => !this.isTokenExpired(data))
       );
     }
     return this.cpqAccessData;
