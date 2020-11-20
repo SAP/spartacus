2251ae4bd3839e8662a9a154046c3e731fcc94bb
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-11-20 16:43:19 +0100

    config cleanup

--- feature-libs/organization/jest.base.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/test.ts'],
-  collectCoverage: true,
-  coverageReporters: ['html'],
-};
--- feature-libs/organization/jest.schematics.config.js
+++ feature-libs/organization/jest.schematics.config.js
@@ -1,13 +1,28 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig.schematics.spec');
-const baseConfig = require('./jest.base.config');
 
 module.exports = {
-  ...baseConfig,
+  setupFilesAfterEnv: ['<rootDir>/test.ts'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage/organization/schematics',
+  coverageThreshold: {
+    global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+    }
+  },
+
   roots: ['<rootDir>/schematics'],
   modulePaths: ['<rootDir>/../../projects/schematics'],
   testMatch: ['**/+(*_)+(spec).+(ts)'],
-  coverageDirectory: 'coverage/organization/schematics',
+  moduleFileExtensions: [ "js", "ts", "json" ],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/'
   })
