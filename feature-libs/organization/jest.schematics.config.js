89bf732fc417c12ffd15675f41d88734eb68ffdd
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-11-20 22:54:32 +0100

    prettier fix

--- feature-libs/organization/jest.schematics.config.js
+++ feature-libs/organization/jest.schematics.config.js
@@ -4,7 +4,7 @@ const { compilerOptions } = require('./tsconfig.schematics.spec');
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.ts'],
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    '^.+\\.ts?$': 'ts-jest',
   },
 
   collectCoverage: true,
@@ -12,18 +12,18 @@ module.exports = {
   coverageDirectory: 'coverage/organization/schematics',
   coverageThreshold: {
     global: {
-        branches: 70,
-        functions: 80,
-        lines: 80,
-        statements: 80
-    }
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
   },
 
   roots: ['<rootDir>/schematics'],
   modulePaths: ['<rootDir>/../../projects/schematics'],
   testMatch: ['**/+(*_)+(spec).+(ts)'],
-  moduleFileExtensions: [ "js", "ts", "json" ],
+  moduleFileExtensions: ['js', 'ts', 'json'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: '<rootDir>/'
-  })
+    prefix: '<rootDir>/',
+  }),
 };
