e561aaee994f7f3f7639e1c112270834ae2e9bd1
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-11-21 12:47:55 +0100

    Overload method for stricter type checking

--- projects/core/src/i18n/testing/mock-date.pipe.ts
+++ projects/core/src/i18n/testing/mock-date.pipe.ts
@@ -1,9 +1,11 @@
-import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'cxDate' })
 export class MockDatePipe extends DatePipe implements PipeTransform {
-  transform(value: any, format?: string, timezone?: string): string {
+  // Overload to support stricter type check from angular 11 onwards
+  transform(value: null | undefined, args?: string): null;
+  transform(value: any, format?: string, timezone?: string): string | null {
     return super.transform(value, format, timezone, 'en');
   }
 }
