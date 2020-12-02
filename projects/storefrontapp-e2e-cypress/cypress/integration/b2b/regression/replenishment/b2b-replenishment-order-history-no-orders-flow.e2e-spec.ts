1a6c491940642d0520f48b9df14a1a3df05fb1b8
Krzysztof Platis <platonn.git@gmail.com>
2020-12-02 15:36:39 +0100

    split b2b folder in to smoke and regression

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/replenishment/b2b-replenishment-order-history-no-orders-flow.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/replenishment/b2b-replenishment-order-history-no-orders-flow.e2e-spec.ts
@@ -1,11 +1,11 @@
-import { replenishmentOrderHistoryUrl } from '../../../helpers/b2b/b2b-replenishment-order-history';
-import { orderHistoryTest } from '../../../helpers/order-history';
+import { replenishmentOrderHistoryUrl } from '../../../../helpers/b2b/b2b-replenishment-order-history';
+import { orderHistoryTest } from '../../../../helpers/order-history';
 import {
   b2bAccountShipToUser,
   ORDER_REQUEST_ENDPOINT,
   POWERTOOLS_BASESITE,
   USER_REQUEST_ENDPOINT,
-} from '../../../sample-data/b2b-checkout';
+} from '../../../../sample-data/b2b-checkout';
 
 describe('Order History with no orders', () => {
   before(() => {
