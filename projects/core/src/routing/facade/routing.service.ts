73d8dc0a50b81bfcb0b9a2fe5e417be42e8ce6af
dunqan <dunqan@w9.pl>
2020-12-03 07:41:48 +0100

    routing service test

--- projects/core/src/routing/facade/routing.service.ts
+++ projects/core/src/routing/facade/routing.service.ts
@@ -52,6 +52,15 @@ export class RoutingService {
   }
 
   /**
+   * Allow to change next page context for the ongoing navigation
+   *
+   * @param pageContext
+   */
+  changeNextPageContext(pageContext: PageContext) {
+    this.store.dispatch(new RoutingActions.ChangeNextPageContext(pageContext));
+  }
+
+  /**
    * Get the `isNavigating` info from the state
    */
   isNavigating(): Observable<boolean> {
@@ -118,14 +127,5 @@ export class RoutingService {
         extras,
       })
     );
-  }
-
-  /**
-   * Allow to change next page context for the ongoing navigation
-   *
-   * @param pageContext
-   */
-  changeNextPageContext(pageContext: PageContext) {
-    this.store.dispatch(new RoutingActions.ChangeNextPageContext(pageContext));
   }
 }
