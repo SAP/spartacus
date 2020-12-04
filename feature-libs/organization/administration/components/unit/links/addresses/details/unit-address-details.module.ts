f45c4466e06a1e7b3548add4ecc3542566773fab
Michał Gruca <pucek9@gmail.com>
2020-12-04 16:18:29 +0100

    fix: Add missing possibility for remove user group #10053 + delete ad… (#10230)
    Closes #10053

--- feature-libs/organization/administration/components/unit/links/addresses/details/unit-address-details.module.ts
+++ feature-libs/organization/administration/components/unit/links/addresses/details/unit-address-details.module.ts
@@ -4,9 +4,17 @@ import { RouterModule } from '@angular/router';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import { CardModule } from '../../../../shared/card/card.module';
 import { UnitAddressDetailsComponent } from './unit-address-details.component';
+import { DeleteItemModule } from '../../../../shared/detail/delete-item-action/delete-item.module';
 
 @NgModule({
-  imports: [CommonModule, CardModule, RouterModule, UrlModule, I18nModule],
+  imports: [
+    CommonModule,
+    CardModule,
+    RouterModule,
+    UrlModule,
+    I18nModule,
+    DeleteItemModule,
+  ],
   declarations: [UnitAddressDetailsComponent],
 })
 export class UnitAddressDetailsModule {}
