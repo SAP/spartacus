885cc90b8a3c2932990264409c44913489374f6a
Larisa Staroverova <larisa.staroverova@sap.com>
2020-12-04 15:22:20 +0100

    Merge epic/product-configuration

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/product-configuration.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/product-configuration.e2e-spec.ts
@@ -1,23 +1,30 @@
 import * as configuration from '../../helpers/product-configuration';
+import * as login from '../../helpers/login';
 import * as productSearch from '../../helpers/product-search';
+//import {signOutUser} from '../../helpers/checkout-flow';
+//import { user } from '../../sample-data/checkout-flow';
 
 const testProductMultiLevel = 'CONF_HOME_THEATER_ML';
 
 context('Product Configuration', () => {
   beforeEach(() => {
     cy.visit('/');
+    login.registerUser();
   });
 
   describe('Configuration process', () => {
-    xit('should support the product configuration aspect in product search, cart, checkout and order history', () => {
-      configuration.login();
+    it('should support the product configuration aspect in product search, cart, checkout and order history', () => {
+      //configuration.login();
+      login.loginUser();
       productSearch.searchForProduct(testProductMultiLevel);
       configuration.clickOnAddToCartBtnOnPD();
       configuration.clickOnProceedToCheckoutBtnOnPD();
       configuration.checkout();
       configuration.navigateToOrderDetails();
       configuration.selectOrderByOrderNumberAlias();
-      configuration.signOutUser();
+      //configuration.signOutUser();
+      //signOutUser(user);
+      login.signOutUser();
     });
   });
 });
