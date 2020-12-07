00cdb17c96561975a64ab343be8f8921fee22178
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2020-12-07 15:39:32 +0100

    How to do e2e order test without providing test user in sampledata (#10229)
    Refactor the available cypress test for the complete order propess

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/product-configuration.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/product-configuration.e2e-spec.ts
@@ -1,4 +1,5 @@
 import * as configuration from '../../helpers/product-configuration';
+import * as login from '../../helpers/login';
 import * as productSearch from '../../helpers/product-search';
 
 const testProductMultiLevel = 'CONF_HOME_THEATER_ML';
@@ -6,18 +7,19 @@ const testProductMultiLevel = 'CONF_HOME_THEATER_ML';
 context('Product Configuration', () => {
   beforeEach(() => {
     cy.visit('/');
+    login.registerUser();
   });
 
   describe('Configuration process', () => {
-    xit('should support the product configuration aspect in product search, cart, checkout and order history', () => {
-      configuration.login();
+    it('should support the product configuration aspect in product search, cart, checkout and order history', () => {
+      login.loginUser();
       productSearch.searchForProduct(testProductMultiLevel);
       configuration.clickOnAddToCartBtnOnPD();
       configuration.clickOnProceedToCheckoutBtnOnPD();
       configuration.checkout();
       configuration.navigateToOrderDetails();
       configuration.selectOrderByOrderNumberAlias();
-      configuration.signOutUser();
+      login.signOutUser();
     });
   });
 });
