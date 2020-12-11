be219d0fbe02d672252bf3375cfbae1cd95424d1
luklus <ll_code@outlook.com>
2020-12-11 12:16:43 +0100

    Update configurator.model.ts

--- feature-libs/product/configurators/rulebased/core/model/configurator.model.ts
+++ feature-libs/product/configurators/rulebased/core/model/configurator.model.ts
@@ -159,6 +159,7 @@ export namespace Configurator {
     NUMERIC = 'numeric',
     AUTO_COMPLETE_CUSTOM = 'input_autocomplete',
     MULTI_SELECTION_IMAGE = 'multi_selection_image',
+    MULTI_SELECTION_BUNDLE = 'multi_selection_bundle',
     SINGLE_SELECTION_IMAGE = 'single_selection_image',
     SINGLE_SELECTION_BUNDLE = 'single_selection_bundle',
   }
