232dbf55a162bcfb390146f26d7536f8b8854c2d
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-17 11:54:53 +0100

    chore: Fix eq lint rule

--- projects/core/src/auth/user-auth/config/base-url-config-validator.ts
+++ projects/core/src/auth/user-auth/config/base-url-config-validator.ts
@@ -6,7 +6,7 @@ export function baseUrlConfigValidator(config: OccConfig & AuthConfig) {
     typeof config?.authentication?.baseUrl === 'undefined' &&
     typeof config?.backend?.occ?.baseUrl === 'undefined' &&
     // Don't show warning when user tries to work around the issue.
-    config?.authentication?.OAuthLibConfig?.requireHttps != false
+    config?.authentication?.OAuthLibConfig?.requireHttps !== false
   ) {
     return 'Authentication might not work correctly without setting either authentication.baseUrl or backend.occ.baseUrl configuration option! Workaround: To support relative urls in angular-oauth2-oidc library you can try setting authentication.OAuthLibConfig.requireHttps to false.';
   }
