ae5ee905e262f7bd904790591aa82b9775598574
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2020-12-18 10:03:36 +0100

    return always an array of items

--- projects/storefrontlib/src/cms-components/product/carousel/product-carousel/product-carousel.component.ts
+++ projects/storefrontlib/src/cms-components/product/carousel/product-carousel/product-carousel.component.ts
@@ -22,7 +22,7 @@ export class ProductCarouselComponent {
   );
 
   /**
-   * returns an Obervable string for the title.
+   * returns an Observable string for the title.
    */
   title$: Observable<string> = this.componentData$.pipe(
     map((data) => data.title)
@@ -34,8 +34,7 @@ export class ProductCarouselComponent {
    * in the viewpoint.
    */
   items$: Observable<Observable<Product>[]> = this.componentData$.pipe(
-    filter((data) => Boolean(data.productCodes)),
-    map((data) => data.productCodes.trim().split(' ')),
+    map((data) => data.productCodes?.trim().split(' ') ?? []),
     map((codes) =>
       codes.map((code) => this.productService.get(code, this.PRODUCT_SCOPE))
     )
