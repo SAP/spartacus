cdb155869fbf0dd4e2abff93489f48c4d14571b0
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-22 14:07:36 +0100

    chore: Test new actions tookit

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -1,121 +1,142 @@
-const { Toolkit } = require('actions-toolkit');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
+const core = require('@actions/core');
+const fs = require('fs');
 const diff = require('diff-lines');
 const normalizeNewline = require('normalize-newline');
 
-Toolkit.run(
-  async (tools) => {
-    const owner = tools.context.payload.repository.owner.login;
-    const repo = tools.context.payload.repository.name;
+async function run() {
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const gh = github.getOctokit(GITHUB_TOKEN);
 
-    const response = await tools.github.pulls.list({
+  const context = github.context;
+
+  console.log(context);
+
+  const owner = context.payload.repository.owner.login;
+  const repo = context.payload.repository.name;
+
+  const response = await gh.pulls.list({
+    owner,
+    repo,
+    head: 'SAP:' + context.payload.ref.replace('refs/heads/', ''),
+  });
+
+  console.log(response);
+
+  const relatedPullRequests = response.data;
+
+  if (relatedPullRequests.length === 0) {
+    return;
+  }
+
+  const relatedPR = relatedPullRequests[0];
+
+  const issueNumber = relatedPR.number;
+  const targetBranch = relatedPR.base.ref;
+  const reportHeader = 'Public API change detection bot';
+
+  // function extractSnippetFromFile(filename) {
+  //   const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
+  //   return regexForTSSnippetInMarkdown
+  //     .exec(normalizeNewline(fs.readFileSync(filename, 'utf-8')))[1]
+  //     .trim();
+  // }
+
+  // await exec.exec('sh', [
+  //   './.github/api-extractor-action/api-extractor-for-branch.sh',
+  // ]);
+  // await exec.exec('sh', [
+  //   './.github/api-extractor-action/api-extractor-for-branch.sh',
+  //   targetBranch,
+  //   'target',
+  // ]);
+
+  // const libraries = ['assets', 'storefront', 'cds', 'product', 'setup'];
+
+  // const libsDiffs = libraries.map((library) => {
+  //   const sourceBranchReportDirectory = `etc`;
+  //   const targetBranchReportDirectory = `target-branch-clone/etc`;
+  //   const sourceBranchSnippet = extractSnippetFromFile(
+  //     `${sourceBranchReportDirectory}/${library}.api.md`
+  //   );
+  //   const targetBranchSnippet = extractSnippetFromFile(
+  //     `${targetBranchReportDirectory}/${library}.api.md`
+  //   );
+  //   return {
+  //     library,
+  //     diff: diff(targetBranchSnippet, sourceBranchSnippet, {
+  //       n_surrounding: 2,
+  //     }),
+  //   };
+  // });
+
+  // function generateCommentBody(libsDiffs) {
+  //   return (
+  //     '## ' +
+  //     reportHeader +
+  //     '\n' +
+  //     libsDiffs
+  //       .map(
+  //         (libDiff) =>
+  //           '### @spartacus/' +
+  //           libDiff.library +
+  //           ' public API diff\n' +
+  //           (!libDiff.diff
+  //             ? 'nothing changed ;)'
+  //             : '``` diff\n' + libDiff.diff + '\n```')
+  //       )
+  //       .join('\n') +
+  //     '\n' +
+  //     '### @spartacus/core public API diff\n' +
+  //     'unable to analyze this library :(\n' +
+  //     'Please check changes in public API manually.'
+  //   );
+  // }
+
+  function generateCommentBody() {
+    return (
+      '## ' +
+      reportHeader +
+      '\n' +
+      '\n' +
+      '### @spartacus/core public API diff\n' +
+      'unable to analyze this library :(\n' +
+      'Please check changes in public API manually.'
+    );
+  }
+
+  async function printReport(body) {
+    const comments = await gh.issues.listComments({
+      issue_number: issueNumber,
       owner,
       repo,
-      head: 'SAP:' + tools.context.payload.ref.replace('refs/heads/', ''),
     });
 
-    const relatedPullRequests = response.data;
+    console.log(comments);
 
-    if (relatedPullRequests.length === 0) {
-      return;
-    }
+    const botComment = comments.data.filter((comment) =>
+      comment.body.includes(reportHeader)
+    );
 
-    const relatedPR = relatedPullRequests[0];
-
-    const issueNumber = relatedPR.number;
-    const targetBranch = relatedPR.base.ref;
-    const reportHeader = 'Public API change detection bot';
-
-    function extractSnippetFromFile(filename) {
-      const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
-      return regexForTSSnippetInMarkdown
-        .exec(normalizeNewline(tools.getFile(filename)))[1]
-        .trim();
-    }
-
-    await exec.exec('sh', [
-      './.github/api-extractor-action/api-extractor-for-branch.sh',
-    ]);
-    await exec.exec('sh', [
-      './.github/api-extractor-action/api-extractor-for-branch.sh',
-      targetBranch,
-      'target',
-    ]);
-
-    const libraries = ['assets', 'storefront', 'cds', 'product', 'setup'];
-
-    const libsDiffs = libraries.map((library) => {
-      const sourceBranchReportDirectory = `etc`;
-      const targetBranchReportDirectory = `target-branch-clone/etc`;
-      const sourceBranchSnippet = extractSnippetFromFile(
-        `${sourceBranchReportDirectory}/${library}.api.md`
-      );
-      const targetBranchSnippet = extractSnippetFromFile(
-        `${targetBranchReportDirectory}/${library}.api.md`
-      );
-      return {
-        library,
-        diff: diff(targetBranchSnippet, sourceBranchSnippet, {
-          n_surrounding: 2,
-        }),
-      };
-    });
-
-    function generateCommentBody(libsDiffs) {
-      return (
-        '## ' +
-        reportHeader +
-        '\n' +
-        libsDiffs
-          .map(
-            (libDiff) =>
-              '### @spartacus/' +
-              libDiff.library +
-              ' public API diff\n' +
-              (!libDiff.diff
-                ? 'nothing changed ;)'
-                : '``` diff\n' + libDiff.diff + '\n```')
-          )
-          .join('\n') +
-        '\n' +
-        '### @spartacus/core public API diff\n' +
-        'unable to analyze this library :(\n' +
-        'Please check changes in public API manually.'
-      );
-    }
-
-    async function printReport(body) {
-      const comments = await tools.github.issues.listComments({
+    if (botComment && botComment.length) {
+      await gh.issues.updateComment({
+        comment_id: botComment[0].id,
+        owner,
+        repo,
+        body,
+      });
+    } else {
+      await gh.issues.createComment({
         issue_number: issueNumber,
         owner,
         repo,
+        body,
       });
-
-      const botComment = comments.data.filter((comment) =>
-        comment.body.includes(reportHeader)
-      );
-
-      if (botComment && botComment.length) {
-        await tools.github.issues.updateComment({
-          comment_id: botComment[0].id,
-          owner,
-          repo,
-          body,
-        });
-      } else {
-        await tools.github.issues.createComment({
-          issue_number: issueNumber,
-          owner,
-          repo,
-          body,
-        });
-      }
     }
-
-    printReport(generateCommentBody(libsDiffs));
-  },
-  {
-    event: ['push'],
-    secrets: ['GITHUB_TOKEN'],
   }
-);
+
+  printReport(generateCommentBody());
+}
+
+run();
