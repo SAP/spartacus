c9406cfa01dd6c356e937162576717d3ea997519
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2020-12-22 12:32:26 -0500

    Attempt to make checkout test less flaky

--- projects/storefrontapp-e2e-cypress/cypress/integration/smoke/checkout-flow.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/smoke/checkout-flow.e2e-spec.ts
@@ -14,35 +14,14 @@ context('Checkout flow', () => {
     cy.saveLocalStorage();
   });
 
-  it('should register successfully', () => {
+  it('should perform checkout', () => {
     checkout.registerUser();
-  });
-
-  it('should go to product page from category page', () => {
     checkout.goToCheapProductDetailsPage();
-  });
-
-  it('should add product to cart and go to checkout', () => {
     checkout.addCheapProductToCartAndLogin();
-  });
-
-  it('should fill in address form', () => {
     checkout.fillAddressFormWithCheapProduct();
-  });
-
-  it('should choose delivery', () => {
     checkout.verifyDeliveryMethod();
-  });
-
-  it('should fill in payment form', () => {
     checkout.fillPaymentFormWithCheapProduct();
-  });
-
-  it('should review and place order', () => {
     checkout.placeOrderWithCheapProduct();
-  });
-
-  it('should display summary page', () => {
     checkout.verifyOrderConfirmationPageWithCheapProduct();
   });
 
