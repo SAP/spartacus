ab02200bf1f8ee7232e658fd80fa08239ded1e10
luklus <ll_code@outlook.com>
2020-12-23 12:54:21 +0100

    clean

--- feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.ts
@@ -9,13 +9,7 @@ import {
 import { Configurator } from '../../../core/model/configurator.model';
 import { FormControl } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
-import { ConfiguratorCommonsService } from '../../../core/facade/configurator-commons.service';
 import { Product, ProductService } from '@spartacus/core';
-import {
-  CommonConfigurator,
-  ConfiguratorRouterExtractorService,
-} from '@spartacus/product-configurator/common';
 
 @Component({
   selector: 'cx-configurator-attribute-product-card',
@@ -33,11 +27,7 @@ export class ConfiguratorAttributeProductCardComponent
   @Output() handleSelect = new EventEmitter<string>();
   @Output() handleDeselect = new EventEmitter<string>();
 
-  constructor(
-    protected configuratorCommonsService: ConfiguratorCommonsService,
-    protected configRouterExtractorService: ConfiguratorRouterExtractorService,
-    protected productService: ProductService
-  ) {}
+  constructor(protected productService: ProductService) {}
 
   ngOnInit() {
     this.product$ = this.productService.get(this.product.productSystemId);
