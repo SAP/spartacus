2a97ba1770f0d07e8525757c0fc986aa00623c37
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-28 16:35:39 +0100

    chore: Fix script

--- .github/api-extractor-action/api-extractor.sh
+++ .github/api-extractor-action/api-extractor.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-PATH='.'
-if [ "$1" != "" ] ; then
-  PATH=$1
-fi
+PATH=$1
 
 npm i -g @microsoft/api-extractor@^7.12.0
-cd $PATH
+cd "$PATH"
 api-extractor run --local --verbose
-
