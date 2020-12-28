e0021346a3470752b34fffc8eae390fac089c002
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-28 16:57:54 +0100

    chore: Change name of path var

--- .github/api-extractor-action/api-extractor.sh
+++ .github/api-extractor-action/api-extractor.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PATH=$1
+LIB_PATH=$1
 
-npm i -g @microsoft/api-extractor@^7.12.0
-cd "$PATH"
-api-extractor run --local --verbose
+cd "$LIB_PATH"
+npx @microsoft/api-extractor@7.12.0 run --local --verbose
