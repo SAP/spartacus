28fe739ea4103920ca4594a664f3b01d77e921ba
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-28 18:59:31 +0100

    chore: Test running 2 builds and extractors

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -30,11 +30,13 @@ async function run() {
   core.endGroup();
 
   // Prepare target branch libs for api-extractor
-  // await exec.exec('sh', [
-  //   './.github/api-extractor-action/prepare-repo-for-api-extractor.sh',
-  //   targetBranch,
-  //   'target',
-  // ]);
+  core.startGroup('Prepare target branch for extractor');
+  await exec.exec('sh', [
+    './.github/api-extractor-action/prepare-repo-for-api-extractor.sh',
+    targetBranch,
+    'target',
+  ]);
+  core.endGroup();
 
   let globber = await glob.create('dist/**/package.json', {});
   const files = await globber.glob();
@@ -55,20 +57,50 @@ async function run() {
       );
 
       const directory = path.substring(0, path.length - `/package.json`.length);
-      core.startGroup(`api extractor for ${directory}`);
 
       await io.cp(apiExtractorConfigPath, directory);
       await exec.exec('sh', [
         './.github/api-extractor-action/api-extractor.sh',
         directory,
       ]);
-      core.endGroup();
     })
   );
 
   globber = await glob.create('etc/**/*.md', {});
   const raports = await globber.glob();
   console.log(raports);
+
+  globber = await glob.create('target-branch-clone/dist/**/package.json', {});
+  const files2 = await globber.glob();
+  globber = await glob.create(
+    '.github/api-extractor-action/api-extractor.json',
+    {}
+  );
+  const apiExtractorConfigPath = (await globber.glob())[0];
+
+  await Promise.all(
+    files2.map(async (path) => {
+      const packageContent = JSON.parse(fs.readFileSync(path, 'utf-8'));
+      const name = packageContent.name;
+      const newName = name.replace(/\//g, '_').replace(/\_/, '/');
+      fs.writeFileSync(
+        path,
+        JSON.stringify({ ...packageContent, name: newName }, undefined, 2)
+      );
+
+      const directory = path.substring(0, path.length - `/package.json`.length);
+
+      await io.cp(apiExtractorConfigPath, directory);
+      await exec.exec('sh', [
+        './.github/api-extractor-action/api-extractor.sh',
+        directory,
+      ]);
+    })
+  );
+
+  globber = await glob.create('target-branch-clone/etc/**/*.md', {});
+  const reports = await globber.glob();
+  console.log(reports);
 
   // function extractSnippetFromFile(filename) {
   //   const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
