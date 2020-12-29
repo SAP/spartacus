85a17a50f93ac6ad75e5e427894d0f2ed4703358
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-29 15:07:47 +0100

    chore: Add comment functionality

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -4,8 +4,8 @@ const core = require('@actions/core');
 const glob = require('@actions/glob');
 const io = require('@actions/io');
 const fs = require('fs');
-// const diff = require('diff-lines');
-// const normalizeNewline = require('normalize-newline');
+const diff = require('diff-lines');
+const normalizeNewline = require('normalize-newline');
 
 async function prepareRepositoryForApiExtractor(branch, baseCommit) {
   core.startGroup('Prepare branches for extractor');
@@ -55,7 +55,6 @@ async function run() {
     Unknown: 'unknown',
     Success: 'success',
     Failed: 'failed',
-    Missing: 'missing',
   };
 
   let entryPoints = {};
@@ -66,20 +65,20 @@ async function run() {
   for (let path of files) {
     const packageContent = JSON.parse(fs.readFileSync(path, 'utf-8'));
     const name = packageContent.name;
+    const newName = name.replace(/\//g, '_').replace(/\_/, '/');
+    fs.writeFileSync(
+      path,
+      JSON.stringify({ ...packageContent, name: newName }, undefined, 2)
+    );
 
     if (!entryPoints[name]) {
       entryPoints[name] = {
         name: name,
         head: { status: Status.Unknown },
         base: { status: Status.Unknown },
+        file: `${nameName.split('/')[1]}.api.md`,
       };
     }
-
-    const newName = name.replace(/\//g, '_').replace(/\_/, '/');
-    fs.writeFileSync(
-      path,
-      JSON.stringify({ ...packageContent, name: newName }, undefined, 2)
-    );
 
     const directory = path.substring(0, path.length - `/package.json`.length);
 
@@ -124,20 +123,20 @@ async function run() {
     files2.map(async (path) => {
       const packageContent = JSON.parse(fs.readFileSync(path, 'utf-8'));
       const name = packageContent.name;
-
-      if (!entryPoints[name]) {
-        entryPoints[name] = {
-          name: name,
-          head: { status: Status.Missing },
-          base: { status: Status.Unknown },
-        };
-      }
-
       const newName = name.replace(/\//g, '_').replace(/\_/, '/');
       fs.writeFileSync(
         path,
         JSON.stringify({ ...packageContent, name: newName }, undefined, 2)
       );
+
+      if (!entryPoints[name]) {
+        entryPoints[name] = {
+          name: name,
+          head: { status: Status.Unknown },
+          base: { status: Status.Unknown },
+          file: `${nameName.split('/')[1]}.api.md`,
+        };
+      }
 
       const directory = path.substring(0, path.length - `/package.json`.length);
 
@@ -171,102 +170,104 @@ async function run() {
 
   console.log(entryPoints);
 
-  // globber = await glob.create('branch-clone/etc/**/*.md', {});
-  // const reports = await globber.glob();
-  // console.log(reports);
+  const comment = entryPoints
+    .map((entry) => {
+      if (
+        entry.head.status === Status.Success &&
+        entry.base.status === Status.Success
+      ) {
+        // prepare diff
+        const diff = getDiff(entry.file);
+        if (diff) {
+          return `### ${entry.name}\n\`\`\`diff\n${diff}\n\`\`\``;
+        }
+        return '';
+      } else if (
+        entry.head.status === Status.Failed &&
+        entry.base.status === Status.Success
+      ) {
+        return `### ${entry.name}
+Library no longer can be analyzed with api-extractor. Please check the errors below\n
+${entry.head.errors.join('\n')}`;
+      } else if (
+        entry.head.status === Status.Success &&
+        entry.base.status === Status.Failed
+      ) {
+        return `### ${entry.name}\nLibrary can now by analyzed with api-extractor.`;
+      } else if (
+        entry.head.status === Status.Failed &&
+        entry.base.status === Status.Failed
+      ) {
+        if (entry.head.errors[0] !== entry.base.errors[0]) {
+          return `### ${entry.name}\nNew error: ${entry.head.errors[0]}\nPrevious error: ${entry.base.errors[0]}`;
+        }
+      } else if (entry.head.status === Status.Unknown) {
+        return `### ${entry.name}\nEntry point removed. Are you sure it was intentional?`;
+        // entry removed -> add info
+      } else if (entry.base.status === Status.Unknown) {
+        // entry added -> add whole diff
+        const publicApi = extractSnippetFromFile(entry.file);
+        return `### ${entry.name}\nNew entry point. Initial public api:\`\`\`ts\n${publicApi}\n\`\`\``;
+      }
+      return '';
+    })
+    .filter((comment) => !!comment)
+    .join('\n');
 
-  // function extractSnippetFromFile(filename) {
-  //   const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
-  //   return regexForTSSnippetInMarkdown
-  //     .exec(normalizeNewline(fs.readFileSync(filename, 'utf-8')))[1]
-  //     .trim();
-  // }
+  function extractSnippetFromFile(filename) {
+    const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
+    return regexForTSSnippetInMarkdown
+      .exec(normalizeNewline(fs.readFileSync(filename, 'utf-8')))[1]
+      .trim();
+  }
 
-  // const libraries = ['assets', 'storefront', 'cds', 'product', 'setup'];
+  function getDiff(file) {
+    const sourceBranchReportDirectory = `etc`;
+    const targetBranchReportDirectory = `branch-clone/etc`;
+    const sourceBranchSnippet = extractSnippetFromFile(
+      `${sourceBranchReportDirectory}/${file}`
+    );
+    const targetBranchSnippet = extractSnippetFromFile(
+      `${targetBranchReportDirectory}/${file}`
+    );
+    return diff(targetBranchSnippet, sourceBranchSnippet, {
+      n_surrounding: 3,
+    });
+  }
 
-  // const libsDiffs = libraries.map((library) => {
-  //   const sourceBranchReportDirectory = `etc`;
-  //   const targetBranchReportDirectory = `branch-clone/etc`;
-  //   const sourceBranchSnippet = extractSnippetFromFile(
-  //     `${sourceBranchReportDirectory}/${library}.api.md`
-  //   );
-  //   const targetBranchSnippet = extractSnippetFromFile(
-  //     `${targetBranchReportDirectory}/${library}.api.md`
-  //   );
-  //   return {
-  //     library,
-  //     diff: diff(targetBranchSnippet, sourceBranchSnippet, {
-  //       n_surrounding: 2,
-  //     }),
-  //   };
-  // });
+  function generateCommentBody(comment) {
+    return '## ' + reportHeader + '\n' + comment;
+  }
 
-  // function generateCommentBody(libsDiffs) {
-  //   return (
-  //     '## ' +
-  //     reportHeader +
-  //     '\n' +
-  //     libsDiffs
-  //       .map(
-  //         (libDiff) =>
-  //           '### @spartacus/' +
-  //           libDiff.library +
-  //           ' public API diff\n' +
-  //           (!libDiff.diff
-  //             ? 'nothing changed ;)'
-  //             : '``` diff\n' + libDiff.diff + '\n```')
-  //       )
-  //       .join('\n') +
-  //     '\n' +
-  //     '### @spartacus/core public API diff\n' +
-  //     'unable to analyze this library :(\n' +
-  //     'Please check changes in public API manually.'
-  //   );
-  // }
+  async function printReport(body) {
+    const comments = await gh.issues.listComments({
+      issue_number: issueNumber,
+      owner,
+      repo,
+    });
 
-  // function generateCommentBody() {
-  //   return (
-  //     '## ' +
-  //     reportHeader +
-  //     '\n' +
-  //     '\n' +
-  //     '### @spartacus/core public API diff\n' +
-  //     'unable to analyze this library :(\n' +
-  //     'Please check changes in public API manually.'
-  //   );
-  // }
+    const botComment = comments.data.filter((comment) =>
+      comment.body.includes(reportHeader)
+    );
 
-  // async function printReport(body) {
-  //   const comments = await gh.issues.listComments({
-  //     issue_number: issueNumber,
-  //     owner,
-  //     repo,
-  //   });
+    if (botComment && botComment.length) {
+      await gh.issues.updateComment({
+        comment_id: botComment[0].id,
+        owner,
+        repo,
+        body,
+      });
+    } else {
+      await gh.issues.createComment({
+        issue_number: issueNumber,
+        owner,
+        repo,
+        body,
+      });
+    }
+  }
 
-  //   console.log(comments);
-
-  //   const botComment = comments.data.filter((comment) =>
-  //     comment.body.includes(reportHeader)
-  //   );
-
-  //   if (botComment && botComment.length) {
-  //     await gh.issues.updateComment({
-  //       comment_id: botComment[0].id,
-  //       owner,
-  //       repo,
-  //       body,
-  //     });
-  //   } else {
-  //     await gh.issues.createComment({
-  //       issue_number: issueNumber,
-  //       owner,
-  //       repo,
-  //       body,
-  //     });
-  //   }
-  // }
-
-  // await printReport(generateCommentBody());
+  await printReport(generateCommentBody(comment));
 }
 
 run();
