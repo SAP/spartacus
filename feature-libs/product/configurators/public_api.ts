c3a5cecf90c7522cf41fa912e618f575f1e73ac8
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-29 11:22:13 +0100

    chore: Fix cpq lib

--- feature-libs/product/configurators/public_api.ts
+++ feature-libs/product/configurators/public_api.ts
@@ -3,6 +3,6 @@
  */
 
 export * from '@spartacus/product/configurators/common';
-export * from '@spartacus/product/configurators/cpq';
+export * from '@spartacus/product/configurators/cpd';
 export * from '@spartacus/product/configurators/textfield';
 export * from '@spartacus/product/configurators/variant';
