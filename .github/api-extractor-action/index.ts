e4eb34d8a3e935bd5bf2e8168b0d52821b5e37b9
Marcin Lasak <lasakmarcin90@gmail.com>
2020-12-31 10:50:06 +0100

    chore: Further API extractor refactor

--- .github/api-extractor-action/index.ts
+++ .github/api-extractor-action/index.ts
@@ -15,6 +15,17 @@ const fs = require('fs');
 const diff = require('diff-lines');
 const normalizeNewline = require('normalize-newline');
 
+const BASE_BRANCH_DIR = 'branch-clone';
+const BUILD_DIR = 'dist';
+const REPORT_DIR = 'etc';
+const COMMENT_HEADER = 'Public API changes';
+
+enum Status {
+  Unknown = 'unknown',
+  Success = 'success',
+  Failed = 'failed',
+}
+
 async function prepareRepositoryForApiExtractor(
   branch: string,
   baseCommit: string
@@ -26,34 +37,103 @@ async function prepareRepositoryForApiExtractor(
 
   // Try to restore builded libraries for base branch
   // Builded libraries are cached by `cache-builded-libs` action
-  const paths = ['dist'];
+  const paths = [BUILD_DIR];
   const key = `dist-${baseCommit}`;
   const cacheKey = await cache.restoreCache(paths, key, []);
   if (cacheKey) {
     // We create `etc` directory for api-extractor files
-    await io.mkdirP('branch-clone/etc');
+    await io.mkdirP(`${BASE_BRANCH_DIR}/${REPORT_DIR}`);
     // Cache restores files in the same location, so we need to move them manually
-    await io.cp('dist', 'branch-clone/dist', { recursive: true, force: false });
-    await io.rmRF('dist');
+    await io.cp(BUILD_DIR, `${BASE_BRANCH_DIR}/${BUILD_DIR}`, {
+      recursive: true,
+      force: false,
+    });
+    await io.rmRF(BUILD_DIR);
   } else {
     // When we don't have cache let's clone the base branch (with particular commit)
     await exec.exec('sh', [
       './.github/api-extractor-action/prepare-repo-for-api-extractor.sh',
       branch,
-      'branch-clone',
+      BASE_BRANCH_DIR,
       baseCommit,
     ]);
   }
 
   // Build the libraries
   // TODO: We can parallel these builds, when schematics builds won't trigger yarn install
-  await exec.exec('yarn', ['build:libs']);
+  const BUILD_COMMAND = 'build:libs';
+  await exec.exec('yarn', [BUILD_COMMAND]);
   // If we didn't restored builded libs, we need to also build base branch
   if (!cacheKey) {
-    await exec.exec('yarn', ['--cwd', 'branch-clone', 'build:libs']);
+    await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR, BUILD_COMMAND]);
   }
 
   core.endGroup();
+}
+
+/**
+ * Generate github comment for pull request with bot results
+ *
+ * @param analyzedComments List of comments for each analysed entry point
+ * @param notAnalyzableEntryPoints List of entry points that couldn't be analyzed
+ */
+function generateCommentBody(
+  analyzedComments: string[],
+  notAnalyzableEntryPoints: string[]
+) {
+  return `## ${COMMENT_HEADER}\n${generateCommentForAnalyzed(
+    analyzedComments
+  )}${generateCommentForNotAnalyzed(notAnalyzableEntryPoints)}`;
+}
+
+function generateCommentForAnalyzed(comments: string[]) {
+  const changedEntryPoints = comments.filter((comment) => !!comment);
+  if (!changedEntryPoints.length) {
+    return '### :heavy_check_mark: Nothing changed in analyzed entry points.';
+  } else {
+    return changedEntryPoints.join('\n');
+  }
+}
+
+function generateCommentForNotAnalyzed(notAnalyzableEntryPoints: string[]) {
+  const listOfEntryPoints = notAnalyzableEntryPoints
+    .map((entryPoint: string) => `- ${entryPoint}`)
+    .join('\n');
+  return `\n\n### :warning: Impossible to analyze\n${listOfEntryPoints}`;
+}
+
+async function printReport(
+  body: string,
+  gh: any,
+  issueNumber: string,
+  owner: string,
+  repo: string
+) {
+  const comments = await gh.issues.listComments({
+    issue_number: issueNumber,
+    owner,
+    repo,
+  });
+
+  const botComment = comments.data.filter((comment: any) =>
+    comment.body.includes(COMMENT_HEADER)
+  );
+
+  if (botComment && botComment.length) {
+    await gh.issues.updateComment({
+      comment_id: botComment[0].id,
+      owner,
+      repo,
+      body,
+    });
+  } else {
+    await gh.issues.createComment({
+      issue_number: issueNumber,
+      owner,
+      repo,
+      body,
+    });
+  }
 }
 
 async function run() {
@@ -70,7 +150,6 @@ async function run() {
   const issueNumber = relatedPR.number;
   const baseBranch = relatedPR.base.ref;
   const baseCommit = relatedPR.base.sha;
-  const reportHeader = 'Public API changes';
 
   let globber = await glob.create(
     '.github/api-extractor-action/api-extractor.json',
@@ -80,15 +159,9 @@ async function run() {
 
   await prepareRepositoryForApiExtractor(baseBranch, baseCommit);
 
-  const Status = {
-    Unknown: 'unknown',
-    Success: 'success',
-    Failed: 'failed',
-  };
-
   let entryPoints: any = {};
 
-  globber = await glob.create('dist/**/package.json', {});
+  globber = await glob.create(`${BUILD_DIR}/**/package.json`, {});
   const files = await globber.glob();
 
   core.info('\u001b[35mAPI extractor for head branch');
@@ -112,7 +185,6 @@ async function run() {
 
     const directory = path.substring(0, path.length - `/package.json`.length);
 
-    // Run api extractor for entrypoint
     await io.cp(apiExtractorConfigPath, directory);
     const options: ExecOptions = {
       ignoreReturnCode: true,
@@ -141,7 +213,10 @@ async function run() {
     core.endGroup();
   }
 
-  globber = await glob.create('branch-clone/dist/**/package.json', {});
+  globber = await glob.create(
+    `${BASE_BRANCH_DIR}/${BUILD_DIR}/**/package.json`,
+    {}
+  );
   const files2 = await globber.glob();
 
   core.info('\u001b[35mAPI extractor for base branch');
@@ -195,50 +270,47 @@ async function run() {
 
   let notAnalyzableEntryPoints: any = [];
 
-  const comment = Object.values(entryPoints)
-    .map((entry: any) => {
-      if (
-        entry.head.status === Status.Success &&
-        entry.base.status === Status.Success
-      ) {
-        // prepare diff
-        const diff = getDiff(entry.file);
-        if (diff) {
-          return `### :warning: ${entry.name}\n\`\`\`diff\n${diff}\n\`\`\``;
-        }
-        return '';
-      } else if (
-        entry.head.status === Status.Failed &&
-        entry.base.status === Status.Success
-      ) {
-        return `### :boom: ${entry.name}
-Library no longer can be analyzed with api-extractor. Please check the errors:\n\`\`\`
-${entry.head.errors.join('\n')}\n\`\`\``;
-      } else if (
-        entry.head.status === Status.Success &&
-        entry.base.status === Status.Failed
-      ) {
-        return `### :green_heart: ${entry.name}\nLibrary can now by analyzed with api-extractor.`;
-      } else if (
-        entry.head.status === Status.Failed &&
-        entry.base.status === Status.Failed
-      ) {
-        notAnalyzableEntryPoints.push(entry.name);
-        if (entry.head.errors[0] !== entry.base.errors[0]) {
-          return `### :boom: ${entry.name}\nNew error: \`${entry.head.errors[0]}\`\nPrevious error: \`${entry.base.errors[0]}\``;
-        }
-      } else if (entry.head.status === Status.Unknown) {
-        return `### :boom: ${entry.name}\nEntry point removed. Are you sure it was intentional?`;
-      } else if (entry.base.status === Status.Unknown) {
-        const publicApi = extractSnippetFromFile(`etc/${entry.file}`);
-        return `### :warning: ${entry.name}\nNew entry point. Initial public api:\n\`\`\`ts\n${publicApi}\n\`\`\``;
+  const comment = Object.values(entryPoints).map((entry: any) => {
+    if (
+      entry.head.status === Status.Success &&
+      entry.base.status === Status.Success
+    ) {
+      // prepare diff
+      const diff = getDiff(entry.file);
+      if (diff) {
+        return `### :warning: ${entry.name}\n\`\`\`diff\n${diff}\n\`\`\``;
       }
       return '';
-    })
-    .filter((comment) => !!comment)
-    .join('\n');
+    } else if (
+      entry.head.status === Status.Failed &&
+      entry.base.status === Status.Success
+    ) {
+      return `### :boom: ${entry.name}
+Library no longer can be analyzed with api-extractor. Please check the errors:\n\`\`\`
+${entry.head.errors.join('\n')}\n\`\`\``;
+    } else if (
+      entry.head.status === Status.Success &&
+      entry.base.status === Status.Failed
+    ) {
+      return `### :green_heart: ${entry.name}\nLibrary can now by analyzed with api-extractor.`;
+    } else if (
+      entry.head.status === Status.Failed &&
+      entry.base.status === Status.Failed
+    ) {
+      notAnalyzableEntryPoints.push(entry.name);
+      if (entry.head.errors[0] !== entry.base.errors[0]) {
+        return `### :boom: ${entry.name}\nNew error: \`${entry.head.errors[0]}\`\nPrevious error: \`${entry.base.errors[0]}\``;
+      }
+    } else if (entry.head.status === Status.Unknown) {
+      return `### :boom: ${entry.name}\nEntry point removed. Are you sure it was intentional?`;
+    } else if (entry.base.status === Status.Unknown) {
+      const publicApi = extractSnippetFromFile(`etc/${entry.file}`);
+      return `### :warning: ${entry.name}\nNew entry point. Initial public api:\n\`\`\`ts\n${publicApi}\n\`\`\``;
+    }
+    return '';
+  });
 
-  function extractSnippetFromFile(filename: string) {
+  function extractSnippetFromFile(filename: string): string {
     const regexForTSSnippetInMarkdown = /```ts([\s\S]*)```/ms;
     const result = regexForTSSnippetInMarkdown.exec(
       normalizeNewline(fs.readFileSync(filename, 'utf-8'))
@@ -246,12 +318,12 @@ ${entry.head.errors.join('\n')}\n\`\`\``;
     if (result) {
       return result[1].trim();
     }
-    return null;
+    return '';
   }
 
   function getDiff(file: string) {
-    const sourceBranchReportDirectory = `etc`;
-    const targetBranchReportDirectory = `branch-clone/etc`;
+    const sourceBranchReportDirectory = `${REPORT_DIR}`;
+    const targetBranchReportDirectory = `${BASE_BRANCH_DIR}/${REPORT_DIR}`;
     const sourceBranchSnippet = extractSnippetFromFile(
       `${sourceBranchReportDirectory}/${file}`
     );
@@ -263,50 +335,8 @@ ${entry.head.errors.join('\n')}\n\`\`\``;
     });
   }
 
-  function generateCommentBody(comment: string) {
-    return (
-      '## ' +
-      reportHeader +
-      '\n' +
-      (comment.length
-        ? comment
-        : '### :heavy_check_mark: Nothing changed in analyzed entry points.') +
-      '\n\n ### :warning: Impossible to analyze\n' +
-      notAnalyzableEntryPoints
-        .map((entryPoint: string) => `- ${entryPoint}`)
-        .join('\n')
-    );
-  }
-
-  async function printReport(body: string) {
-    const comments = await gh.issues.listComments({
-      issue_number: issueNumber,
-      owner,
-      repo,
-    });
-
-    const botComment = comments.data.filter((comment: any) =>
-      comment.body.includes(reportHeader)
-    );
-
-    if (botComment && botComment.length) {
-      await gh.issues.updateComment({
-        comment_id: botComment[0].id,
-        owner,
-        repo,
-        body,
-      });
-    } else {
-      await gh.issues.createComment({
-        issue_number: issueNumber,
-        owner,
-        repo,
-        body,
-      });
-    }
-  }
-
-  await printReport(generateCommentBody(comment));
+  const commentBody = generateCommentBody(comment, notAnalyzableEntryPoints);
+  await printReport(commentBody, gh, issueNumber, owner, repo);
 }
 
 run();
