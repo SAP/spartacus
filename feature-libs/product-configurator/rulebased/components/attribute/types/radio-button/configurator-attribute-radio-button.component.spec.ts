227f062c789dd788cf319c4aea6705ffdf2c2311
luklus <ll_code@outlook.com>
2021-01-04 09:06:34 +0100

    clean

--- feature-libs/product-configurator/rulebased/components/attribute/types/radio-button/configurator-attribute-radio-button.component.spec.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/radio-button/configurator-attribute-radio-button.component.spec.ts
@@ -19,7 +19,7 @@ export class MockFocusDirective {
   @Input('cxFocus') protected config;
 }
 
-fdescribe('ConfigAttributeRadioButtonComponent', () => {
+describe('ConfigAttributeRadioButtonComponent', () => {
   let component: ConfiguratorAttributeRadioButtonComponent;
   let fixture: ComponentFixture<ConfiguratorAttributeRadioButtonComponent>;
   const ownerKey = 'theOwnerKey';
