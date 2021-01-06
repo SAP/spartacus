6a1fff447b9ec15cdfe06ac0147e7458edf05e0b
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-01-06 17:19:34 -0500

    rename old lib with alias as deprecated qualtrics component

--- projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics.module.ts
+++ projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics.module.ts
@@ -1,25 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CmsConfig, provideDefaultConfig } from '@spartacus/core';
-import { defaultQualtricsConfig } from './config/default-qualtrics-config';
-import { QualtricsComponent } from './qualtrics.component';
+import { defaultQualtricsConfig as deprecatedDefaultQualtricsConfig } from './config/default-qualtrics-config';
+import { QualtricsComponent as DeprecatedQualtricsComponent } from './qualtrics.component';
 
 /**
  * @deprecated since 3.1 - moved to feature-lib
  */
 @NgModule({
   imports: [CommonModule],
-  declarations: [QualtricsComponent],
-  entryComponents: [QualtricsComponent],
+  declarations: [DeprecatedQualtricsComponent],
+  entryComponents: [DeprecatedQualtricsComponent],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
-        aa: {
-          component: QualtricsComponent,
+        QualtricsComponent: {
+          component: DeprecatedQualtricsComponent,
         },
       },
     }),
-    provideDefaultConfig(defaultQualtricsConfig),
+    provideDefaultConfig(deprecatedDefaultQualtricsConfig),
   ],
 })
 export class QualtricsModule {}
