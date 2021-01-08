29370e451fa73ed3da4ff7585c626d8d35e8ea3c
Weizheng Gao <weizheng.gao@sap.com>
2021-01-08 12:00:34 -0500

    feature/GH-10569: add unit tests for smartedit element decorators

---
+++ projects/core/src/smart-edit/decorators/smart-edit-component-decorator.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed } from '@angular/core/testing';
+import { SmartEditService } from '../services/smart-edit.service';
+import { SmartEditComponentDecorator } from './smart-edit-component-decorator';
+
+class MockSmartEditService {
+  isLaunchedInSmartEdit() {
+    return true;
+  }
+  addSmartEditContract() {}
+}
+describe('SmartEditComponentDecorator', () => {
+  let decorator: SmartEditComponentDecorator;
+  let smartEditService: SmartEditService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SmartEditComponentDecorator,
+        {
+          provide: SmartEditService,
+          useClass: MockSmartEditService,
+        },
+      ],
+    });
+    decorator = TestBed.inject(SmartEditComponentDecorator);
+    smartEditService = TestBed.inject(SmartEditService);
+  });
+
+  it('should be created', () => {
+    expect(decorator).toBeTruthy();
+  });
+
+  it('should match when smarteidt is lunch ', () => {
+    expect(decorator.hasMatch()).toEqual(
+      smartEditService.isLaunchedInSmartEdit()
+    );
+  });
+
+  it('should call addSmartEditContract', () => {
+    const component = { properties: { smartedit: { uuid: 'test-id' } } };
+    spyOn(smartEditService, 'addSmartEditContract');
+    decorator.decorate(null, null, component);
+    expect(smartEditService.addSmartEditContract).toHaveBeenCalledWith(
+      null,
+      null,
+      component.properties
+    );
+  });
+});
---
+++ projects/core/src/smart-edit/decorators/smart-edit-html-body-decorator.spec.ts
@@ -0,0 +1,66 @@
+import { RendererFactory2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { WindowRef } from '../../window/window-ref';
+import { SmartEditService } from '../services/smart-edit.service';
+import { SmartEditHtmlBodyDecorator } from './smart-edit-html-body-decorator';
+
+class MockSmartEditService {
+  isLaunchedInSmartEdit() {
+    return true;
+  }
+  addSmartEditContract() {}
+}
+
+class MockRendererFactory2 {
+  createRenderer() {
+    return {};
+  }
+}
+describe('SmartEditHtmlBodyDecorator', () => {
+  let decorator: SmartEditHtmlBodyDecorator;
+  let smartEditService: SmartEditService;
+  let winRef: WindowRef;
+  let rendererFactory: RendererFactory2;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SmartEditHtmlBodyDecorator,
+        WindowRef,
+        {
+          provide: SmartEditService,
+          useClass: MockSmartEditService,
+        },
+        {
+          provide: RendererFactory2,
+          useClass: MockRendererFactory2,
+        },
+      ],
+    });
+    decorator = TestBed.inject(SmartEditHtmlBodyDecorator);
+    smartEditService = TestBed.inject(SmartEditService);
+    winRef = TestBed.inject(WindowRef);
+    rendererFactory = TestBed.inject(RendererFactory2);
+  });
+
+  it('should be created', () => {
+    expect(decorator).toBeTruthy();
+  });
+
+  it('should match when smarteidt is lunch ', () => {
+    expect(decorator.hasMatch()).toEqual(
+      smartEditService.isLaunchedInSmartEdit()
+    );
+  });
+
+  it('should call addSmartEditContract', () => {
+    const page = { properties: { smartedit: { uuid: 'test-id' } } };
+    spyOn(smartEditService, 'addSmartEditContract');
+    decorator.decorate(page);
+    expect(smartEditService.addSmartEditContract).toHaveBeenCalledWith(
+      winRef.document.body,
+      rendererFactory.createRenderer(null, null),
+      page.properties
+    );
+  });
+});
