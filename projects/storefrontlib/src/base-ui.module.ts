d17d7dfe883bb6cca9ebbbb7384560a69b326ad7
dunqan <dunqan@w9.pl>
2021-01-11 13:16:40 +0100

    fix imports

--- projects/storefrontlib/src/base-ui.module.ts
+++ projects/storefrontlib/src/base-ui.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { GlobalMessageComponentModule } from './cms-components';
-import {
-  OutletModule,
-  OutletRefModule,
-  PageLayoutModule,
-  PageSlotModule,
-  PwaModule,
-  RoutingModule,
-  SeoModule,
-} from './cms-structure';
-import { KeyboardFocusModule, LayoutModule, SkipLinkModule } from './layout';
-import { MediaModule } from './shared';
+import { GlobalMessageComponentModule } from './cms-components/misc/global-message/global-message.module';
+import { OutletModule } from './cms-structure/outlet/outlet.module';
+import { OutletRefModule } from './cms-structure/outlet/outlet-ref/outlet-ref.module';
+import { PageLayoutModule } from './cms-structure/page/page-layout/page-layout.module';
+import { PageSlotModule } from './cms-structure/page/slot/page-slot.module';
+import { PwaModule } from './cms-structure/pwa/pwa.module';
+import { RoutingModule } from './cms-structure/routing/routing.module';
+import { SeoModule } from './cms-structure/seo/seo.module';
+import { LayoutModule } from './layout/layout.module';
+import { SkipLinkModule } from './layout/a11y/skip-link/skip-link.module';
+import { KeyboardFocusModule } from './layout/a11y/keyboard-focus/keyboard-focus.module';
+import { MediaModule } from './shared/components/media/media.module';
 
 @NgModule({
   declarations: [],
