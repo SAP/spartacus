a864f8b8ab0908072e4153445ad1987c77850e7b
dunqan <dunqan@w9.pl>
2021-01-11 12:59:51 +0100

    Fix and deprecate ViewConfigModule

--- projects/storefrontlib/src/shared/config/view-config.module.ts
+++ projects/storefrontlib/src/shared/config/view-config.module.ts
@@ -1,6 +1,11 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { provideDefaultConfig } from '@spartacus/core';
 
+/**
+ * @deprecated since 3.2
+ *
+ * Remove, don't use
+ */
 @NgModule({})
 export class ViewConfigModule {
   static forRoot(): ModuleWithProviders<ViewConfigModule> {
