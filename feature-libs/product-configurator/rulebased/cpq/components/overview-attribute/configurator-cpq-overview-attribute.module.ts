46666c7bb74694765deb52c2ef7726a3b9ab3fac
Kacper Knapik <kknapik@divante.pl>
2021-01-12 12:22:23 +0100

    fix: improve html

--- feature-libs/product-configurator/rulebased/cpq/components/overview-attribute/configurator-cpq-overview-attribute.module.ts
+++ feature-libs/product-configurator/rulebased/cpq/components/overview-attribute/configurator-cpq-overview-attribute.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { I18nModule } from '@spartacus/core';
 import { MediaModule } from '@spartacus/storefront';
 import { ConfiguratorCPQOverviewAttributeComponent } from './configurator-cpq-overview-attribute.component';
 
 @NgModule({
-  imports: [CommonModule, MediaModule],
+  imports: [CommonModule, MediaModule, I18nModule],
   declarations: [ConfiguratorCPQOverviewAttributeComponent],
   exports: [ConfiguratorCPQOverviewAttributeComponent],
   entryComponents: [ConfiguratorCPQOverviewAttributeComponent],
