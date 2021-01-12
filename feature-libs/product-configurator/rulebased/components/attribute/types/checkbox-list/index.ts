9e77bf88d44b4bd20a9810ecf7af655d3f4022fd
Lukas Lusiak <ll_code@outlook.com>
2021-01-12 14:05:35 +0100

    feat: add quantity for checkbox list type (#10590)
    Closes #10373

--- feature-libs/product-configurator/rulebased/components/attribute/types/checkbox-list/index.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/checkbox-list/index.ts
@@ -1,2 +1,3 @@
+export * from './checkbox-quantity/configurator-attribute-checkbox-quantity.component';
 export * from './configurator-attribute-checkbox-list.component';
 export * from './configurator-attribute-checkbox-list.module';
