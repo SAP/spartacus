b1cc48e2d62977b31d187f4cf533597d25386ae1
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-01-13 13:02:12 -0500

    Forgot this

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -54,21 +54,21 @@ fi
 echo '-----'
 echo "Building Spartacus libraries"
 
-# yarn install
+yarn install
 
-# (cd projects/storefrontapp-e2e-cypress && yarn install)
+(cd projects/storefrontapp-e2e-cypress && yarn install)
 
-# yarn build:libs && yarn build"${INTEGRATION}" 2>&1 | tee build.log
+yarn build:libs && yarn build"${INTEGRATION}" 2>&1 | tee build.log
 
-# results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
-# if [[ -z "${results}" ]]; then
-#     echo "Success: Spartacus production build was successful."
-#     rm build.log
-# else
-#     echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
-#     rm build.log
-#     exit 1
-# fi
+results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
+if [[ -z "${results}" ]]; then
+    echo "Success: Spartacus production build was successful."
+    rm build.log
+else
+    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
+    rm build.log
+    exit 1
+fi
 
 echo '-----'
 echo "Running Cypress end to end tests"
