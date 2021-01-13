01c1d83f1cc79cbbb93dc7051327bd8bd183061c
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-01-13 12:41:29 -0500

    first stab at running b2b tests grouped

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -7,17 +7,17 @@ POSITIONAL=()
 readonly help_display="Usage: $0 [ command_options ] [ param ]
 
     command options:
-        --suite, -s                             choose an e2e suite to run. Default: regression
-        --integration, -i                       run the correct e2e integration suite. Default: "" for smoke tests
-        --environment, --env                    [1905 | 2005 | ccv2]. Default: 1905
-        --help, -h                              show this message and exit
+        --suite, -s                             choose an e2e suite to run. Default: b2c
+        --integration, -i                       run an additional e2e integration suite (cds, cdc, etc)
+        --environment, --env                    [ 2005 | 2011 | ccv2]. Default: 2005
+        --help, -h                              show help
 "
 
 while [ "${1:0:1}" == "-" ]
 do
     case "$1" in
         '--suite' | '-s' )
-            SUITE=$2
+            SUITE=":$2"
             shift
             shift
             ;;
@@ -47,41 +47,32 @@ done
 
 set -- "${POSITIONAL[@]}"
 
-
 if [[ -z "${CI_ENV}" ]]; then
     CI_ENV=":2005"
 fi
 
-yarn
-(cd projects/storefrontapp-e2e-cypress && yarn)
+echo '-----'
+echo "Building Spartacus libraries"
+
+# yarn install
+
+# (cd projects/storefrontapp-e2e-cypress && yarn install)
+
+# yarn build:libs && yarn build"${INTEGRATION}" 2>&1 | tee build.log
+
+# results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
+# if [[ -z "${results}" ]]; then
+#     echo "Success: Spartacus production build was successful."
+#     rm build.log
+# else
+#     echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
+#     rm build.log
+#     exit 1
+# fi
 
 echo '-----'
-echo 'Building Spartacus libraries'
-# Currently for our unified app you have to build all libraries to run it
-yarn build:libs && yarn build"${INTEGRATION}" 2>&1 | tee build.log
+echo "Running Cypress end to end tests"
 
-results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
-if [[ -z "${results}" ]]; then
-    echo "Success: Spartacus production build was successful."
-    rm build.log
-else
-    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
-    rm build.log
-    exit 1
-fi
+yarn e2e:cy"${INTEGRATION}":start-run-ci"${CI_ENV}${SUITE}"
 
-
-# Hardcoded 2005 becuase cypress.ci.b2b.json currently supports only 2005.
-# TODO: The condition should be removed and logic here simplified, when fixing https://github.com/SAP/spartacus/issues/10160
-SHOULD_RUN_B2B=false;
-
-echo '-----'
-echo "Running Cypress end to end tests for suite: $SUITE"
-if [[ $SUITE == 'regression' ]]; then
-    yarn e2e:cy"${INTEGRATION}":start-run-ci"${CI_ENV}"
-else
-    yarn e2e:cy"${INTEGRATION}":start-run-smoke-ci"${CI_ENV}"
-    if [[ $SHOULD_RUN_B2B ]]; then
-        yarn e2e:cy"${INTEGRATION}":start-run-smoke-ci"${CI_ENV}":b2b
-    fi
-fi
+echo "Running Cypress end to end tests finished"
