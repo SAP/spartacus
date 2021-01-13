0de0c57592b5f9c2b452a8adba7c021c70a1738c
Kacper Knapik <kknapik@divante.pl>
2021-01-13 18:55:09 +0100

    feat: use overview price component in cpq overview

--- feature-libs/product-configurator/rulebased/cpq/components/overview-attribute/configurator-cpq-overview-attribute.module.ts
+++ feature-libs/product-configurator/rulebased/cpq/components/overview-attribute/configurator-cpq-overview-attribute.module.ts
@@ -2,10 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
 import { MediaModule } from '@spartacus/storefront';
+import { ConfiguratorOverviewPriceModule } from '../../../components/overview-price/index';
 import { ConfiguratorCPQOverviewAttributeComponent } from './configurator-cpq-overview-attribute.component';
 
 @NgModule({
-  imports: [CommonModule, MediaModule, I18nModule],
+  imports: [
+    CommonModule,
+    MediaModule,
+    I18nModule,
+    ConfiguratorOverviewPriceModule,
+  ],
   declarations: [ConfiguratorCPQOverviewAttributeComponent],
   exports: [ConfiguratorCPQOverviewAttributeComponent],
   entryComponents: [ConfiguratorCPQOverviewAttributeComponent],
