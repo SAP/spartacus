2470d624da6c480397bd8ec901c87cde8a4f2979
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-01-13 14:55:38 +0100

    configured tms feature

--- projects/storefrontapp/src/environments/tms/tms.feature.ts
+++ projects/storefrontapp/src/environments/tms/tms.feature.ts
@@ -1,5 +1,33 @@
+import {
+  CartAddEntryEvent,
+  CartAddEntryFailEvent,
+  CartAddEntrySuccessEvent,
+  CartRemoveEntrySuccessEvent,
+  CartUpdateEntrySuccessEvent,
+} from '@spartacus/core';
+import { HomePageEvent, PageEvent } from '@spartacus/storefront';
 import { TmsModule } from '@spartacus/tms';
 import { FeatureEnvironment } from '../models/feature.model';
+
 export const tmsFeature: FeatureEnvironment = {
-  imports: [TmsModule.forRoot({ tms: { gtm: true, adobeLaunch: true } })],
+  imports: [
+    TmsModule.forRoot({
+      tms: {
+        gtm: {
+          enabled: true,
+          events: [
+            CartAddEntryEvent,
+            CartAddEntrySuccessEvent,
+            CartAddEntryFailEvent,
+            CartRemoveEntrySuccessEvent,
+            CartUpdateEntrySuccessEvent,
+          ],
+        },
+        adobeLaunch: {
+          enabled: true,
+          events: [PageEvent, HomePageEvent],
+        },
+      },
+    }),
+  ],
 };
