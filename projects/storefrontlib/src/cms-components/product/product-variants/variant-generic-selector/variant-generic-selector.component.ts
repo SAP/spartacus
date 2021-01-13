b5de84c0aa97d4ac44a6c20ed4d4ce96ebbc5a94
Michal Dydo <mdydo@divante.pl>
2021-01-13 09:33:16 +0100

    GH-10615

--- projects/storefrontlib/src/cms-components/product/product-variants/variant-generic-selector/variant-generic-selector.component.ts
+++ projects/storefrontlib/src/cms-components/product/product-variants/variant-generic-selector/variant-generic-selector.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Product, ProductScope, ProductService, RoutingService } from '@spartacus/core';
+import {
+  Product,
+  ProductScope,
+  ProductService,
+  RoutingService,
+  VariantMatrixElement,
+} from '@spartacus/core';
 import { filter, take } from 'rxjs/operators';
-// import { product } from 'projects/assets/src/translations/en/product';
 
 @Component({
   selector: 'cx-variant-generic-selector',
   templateUrl: './variant-generic-selector.component.html',
-  styleUrls: ['./variant-generic-selector.component.css']
+  styleUrls: ['./variant-generic-selector.component.css'],
 })
 export class VariantGenericSelectorComponent implements OnInit {
-
   @Input()
   product: Product;
 
@@ -18,10 +22,13 @@ export class VariantGenericSelectorComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private routingService: RoutingService
-    ) { }
+  ) {}
 
   ngOnInit() {
-    const levels = Array.from({length: this.product.categories.length},(_,k) => k + 1);
+    const levels = Array.from(
+      { length: this.product.categories.length },
+      (_, k) => k + 1
+    );
 
     let selectedIndex = 0;
     let productMatrix = this.product.variantMatrix;
@@ -31,31 +38,32 @@ export class VariantGenericSelectorComponent implements OnInit {
       }
       this.variants.push(productMatrix);
 
-      productMatrix.forEach(variantCategory => {
+      productMatrix.forEach((variantCategory) => {
         if (variantCategory.variantOption.code === this.product.code) {
           selectedIndex = index;
         }
       });
     });
-
   }
 
   changeVariant(code: string): void {
     if (code) {
       this.productService
         .get(code, ProductScope.VARIANTS)
-        .pipe(
-          filter(Boolean),
-          take(1)
-        )
+        .pipe(filter(Boolean), take(1))
         .subscribe((product: Product) => {
           this.routingService.go({
             cxRoute: 'product',
             params: product,
-          })
-        })
+          });
+        });
     }
     return null;
   }
 
+  variantHasImages(variants: VariantMatrixElement[]): boolean {
+    return variants.some(
+      (variant: VariantMatrixElement) => variant.parentVariantCategory.hasImage
+    );
+  }
 }
