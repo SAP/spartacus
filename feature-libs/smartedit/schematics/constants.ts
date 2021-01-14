e818d4542a70738738c1ad6955aaeec7568dea1b
Weizheng Gao <weizheng.gao@sap.com>
2021-01-14 11:31:19 -0500

    feature/GH-10570: add schematics into smartedit lib

---
+++ feature-libs/smartedit/schematics/constants.ts
@@ -0,0 +1,7 @@
+export const SMARTEDIT_MODULE = 'SmartEditModule';
+export const SMARTEDIT_FEATURE_NAME = 'smartEdit';
+export const SMARTEDIT_ROOT_MODULE = 'SmartEditRootModule';
+export const SPARTACUS_SMARTEDIT = '@spartacus/smartedit';
+export const SPARTACUS_SMARTEDIT_ROOT = `${SPARTACUS_SMARTEDIT}/root`;
+
+export const CLI_SMARTEDIT_FEATURE = 'SmartEdit';
