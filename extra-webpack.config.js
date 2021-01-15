de08f03138b1341d64be85ccd3278a80351fd596
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-01-15 11:09:35 -0500

    Handling booleans correctly

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,14 +2,16 @@ const webpack = require('webpack');
 const path = require('path');
 
 const keyPrefix = 'CX_';
-
-let env = {};
+const env = {};
 
 const keys = Object.keys(process.env).filter((key) =>
   key.startsWith(keyPrefix)
 );
 
-keys.forEach((key) => (env[key] = process.env[key]));
+keys.forEach((key) => {
+  const value = process.env[key];
+  env[key] = value === 'true' ? true : value === 'false' ? false : value;
+});
 
 console.log('env=', env);
 
