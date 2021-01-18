276601095613038ab27a2f48ab1472bc45ce104a
Thambirajah <renuchan.thambirajah@sap.com>
2021-01-18 12:00:13 -0500

    PriceTier observable handled by template view. Removed unused classes.

--- feature-libs/product/bulk-pricing/components/bulk-pricing-table/bulk-pricing-table.component.ts
+++ feature-libs/product/bulk-pricing/components/bulk-pricing-table/bulk-pricing-table.component.ts
@@ -1,29 +1,21 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { BulkPricesService } from '../../core/services/bulk-prices.service';
 import { RoutingService } from '@spartacus/core';
 import { BulkPrice } from '../../core/model/bulk-price.model';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-bulk-pricing-table',
   templateUrl: './bulk-pricing-table.component.html',
 })
-export class BulkPricingTableComponent implements OnInit, OnDestroy {
-  pricingTiers: BulkPrice[];
-  pricesSubscription: Subscription;
+export class BulkPricingTableComponent {
+  priceTiers$: Observable<BulkPrice[]> = this.getPrices();
 
   constructor(
     private routingService: RoutingService,
     private bulkPrices: BulkPricesService
   ) {}
-
-  // TODO: once we are done with the template, remove the subscribe/unsubscribe code 
-  ngOnInit(): void {
-    this.pricesSubscription = this.getPrices().subscribe((prices) => {
-      this.pricingTiers = prices;
-    });
-  }
 
   formatQuantity(tier: BulkPrice): string {
     let formattedQuantityRange = '';
@@ -37,16 +29,11 @@ export class BulkPricingTableComponent implements OnInit, OnDestroy {
 
   getPrices(): Observable<BulkPrice[]> {
     const productCodeKey = 'productCode';
-
     return this.routingService.getRouterState().pipe(
       switchMap((state) => {
         const productCode = state.state.params[productCodeKey];
         return this.bulkPrices.getBulkPrices(productCode);
       })
     );
-  }
-
-  ngOnDestroy(): void {
-    this.pricesSubscription.unsubscribe();
   }
 }
