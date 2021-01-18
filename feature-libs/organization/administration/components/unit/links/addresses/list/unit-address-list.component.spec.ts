4782d51510a1dc9bf1ed17349d02584c247f298d
Caine Rotherham <rotherham.c@gmail.com>
2021-01-18 11:53:39 +0100

    revert addresses

--- feature-libs/organization/administration/components/unit/links/addresses/list/unit-address-list.component.spec.ts
+++ feature-libs/organization/administration/components/unit/links/addresses/list/unit-address-list.component.spec.ts
@@ -2,13 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { I18nTestingModule } from '@spartacus/core';
 import { SubListTestingModule } from 'feature-libs/organization/administration/components/shared/sub-list/sub-list.testing.module';
 import { UrlTestingModule } from 'projects/core/src/routing/configurable-routes/url-translation/testing/url-testing.module';
-import { CurrentUnitService } from '../../../services/current-unit.service';
 import { UnitAddressListComponent } from './unit-address-list.component';
 import { UnitAddressListService } from './unit-address-list.service';
 
 class MockUnitAddressListService {}
-
-class MockCurrentUnitService {}
 
 describe('UnitAddressListComponent', () => {
   let component: UnitAddressListComponent;
@@ -21,10 +18,6 @@ describe('UnitAddressListComponent', () => {
         {
           provide: UnitAddressListService,
           useClass: MockUnitAddressListService,
-        },
-        {
-          provide: CurrentUnitService,
-          useClass: MockCurrentUnitService,
         },
       ],
       declarations: [UnitAddressListComponent],
--- feature-libs/organization/administration/components/unit/links/addresses/list/unit-address-list.component.ts
+++ feature-libs/organization/administration/components/unit/links/addresses/list/unit-address-list.component.ts
@@ -1,9 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { B2BUnit } from '@spartacus/core';
-import { Observable } from 'rxjs';
 import { ROUTE_PARAMS } from '../../../../constants';
 import { ListService } from '../../../../shared/list/list.service';
-import { CurrentUnitService } from '../../../services/current-unit.service';
 import { UnitAddressListService } from './unit-address-list.service';
 
 @Component({
@@ -20,8 +17,4 @@ import { UnitAddressListService } from './unit-address-list.service';
 })
 export class UnitAddressListComponent {
   routerKey = ROUTE_PARAMS.addressCode;
-
-  unit$: Observable<B2BUnit> = this.currentUnitService.item$;
-
-  constructor(private currentUnitService: CurrentUnitService) {}
 }
