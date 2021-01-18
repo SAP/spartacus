07b3fc8e4961b4b0e05f59794aa245fe8020709a
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-01-18 22:21:52 +0100

    added jsdocs

--- feature-libs/tms/core/gtm/gtm.service.ts
+++ feature-libs/tms/core/gtm/gtm.service.ts
@@ -8,6 +8,9 @@ export interface GtmWindow extends Window {
   dataLayer?: any[];
 }
 
+/**
+ * This service dispatches events to the GTM's `Window.dataLayer`.
+ */
 @Injectable({ providedIn: 'root' })
 export class GoogleTagManagerService implements OnDestroy {
   protected subscription = new Subscription();
@@ -18,8 +21,11 @@ export class GoogleTagManagerService implements OnDestroy {
     protected tmsConfig: TmsConfig
   ) {}
 
+  /**
+   * This method is called only once to start collecting and dispatching events to GTM
+   */
   collect(): void {
-    // prep data layer
+    // prep the data layer
     if (this.window) {
       this.window.dataLayer = this.window.dataLayer || [];
     }
@@ -30,17 +36,28 @@ export class GoogleTagManagerService implements OnDestroy {
       ) || [];
     this.subscription = merge(...events)
       .pipe(tap((x) => console.log('gtm event: ', x)))
-      .subscribe((event) => this.pushToTms(event));
+      .subscribe((event) => this.pushToDataLayer(event));
   }
 
-  protected pushToTms<T extends CxEvent>(event: T): void {
+  /**
+   * Called every time an event fires.
+   *
+   * @param event Spartacus event to dispatch
+   */
+  protected pushToDataLayer<T extends CxEvent>(event: T): void {
     this.window?.dataLayer?.push(event);
   }
 
+  /**
+   * Returns a Window object (if not in SSR) enriched with the GTM's data layer property.
+   */
   get window(): GtmWindow | undefined {
     return this.windowRef.nativeWindow;
   }
 
+  /**
+   * Angular's lifecycle hook
+   */
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
