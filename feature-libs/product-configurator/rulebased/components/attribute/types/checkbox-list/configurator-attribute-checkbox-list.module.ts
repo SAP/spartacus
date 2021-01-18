17fc55ce536536edb95e858d69a5a103e62018bd
luklus <ll_code@outlook.com>
2021-01-18 07:35:04 +0100

    feat: add quantity to checkboxlist attribute level

--- feature-libs/product-configurator/rulebased/components/attribute/types/checkbox-list/configurator-attribute-checkbox-list.module.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/checkbox-list/configurator-attribute-checkbox-list.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { I18nModule } from '@spartacus/core';
-import { ItemCounterModule, KeyboardFocusModule } from '@spartacus/storefront';
+import { KeyboardFocusModule } from '@spartacus/storefront';
 import { ConfiguratorAttributeCheckBoxListComponent } from './configurator-attribute-checkbox-list.component';
-import { ConfiguratorAttributeCheckBoxQuantityComponent } from './checkbox-quantity/configurator-attribute-checkbox-quantity.component';
+import { ConfiguratorAttributeQuantityModule } from '../../quantity/configurator-attribute-quantity.module';
 
 @NgModule({
   imports: [
@@ -13,12 +13,9 @@ import { ConfiguratorAttributeCheckBoxQuantityComponent } from './checkbox-quant
     ReactiveFormsModule,
     CommonModule,
     I18nModule,
-    ItemCounterModule,
+    ConfiguratorAttributeQuantityModule,
   ],
-  declarations: [
-    ConfiguratorAttributeCheckBoxListComponent,
-    ConfiguratorAttributeCheckBoxQuantityComponent,
-  ],
+  declarations: [ConfiguratorAttributeCheckBoxListComponent],
   exports: [ConfiguratorAttributeCheckBoxListComponent],
   entryComponents: [ConfiguratorAttributeCheckBoxListComponent],
 })
