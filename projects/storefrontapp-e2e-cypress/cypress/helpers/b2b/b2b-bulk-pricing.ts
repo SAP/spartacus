e8b9a335ed4e07039f00bab44d3560bfe1f23199
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-01-19 15:14:12 -0500

    fix e2e request to solidify e2e

--- projects/storefrontapp-e2e-cypress/cypress/helpers/b2b/b2b-bulk-pricing.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/b2b/b2b-bulk-pricing.ts
@@ -1,33 +1,47 @@
 import * as sampleData from '../../sample-data/b2b-bulk-pricing';
+import { waitForPage } from '../checkout-flow';
 
-function visitProduct(productCode){
-    cy.server();
-    const aliasName = 'bulkPrices';
-    cy.route('GET', `*orgProducts/${productCode}?fields=*price(DEFAULT),volumePrices(FULL)*`).as(
-        aliasName
-    );
-    cy.visit(`/product/${productCode}`);
-    cy.wait(`@${aliasName}`).its('status').should('eq', 200);
+function visitProduct(productCode) {
+  cy.server();
+
+  const productPageWithBulkPricingAlias = waitForPage(
+    productCode,
+    'productPage'
+  );
+  const bulkPricingAlias = 'bulkPrices';
+
+  cy.route(
+    'GET',
+    `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+      'BASE_SITE'
+    )}/orgProducts/${productCode}*`
+  ).as(bulkPricingAlias);
+
+  cy.visit(`/product/${productCode}`);
+
+  cy.wait(`@${productPageWithBulkPricingAlias}`)
+    .its('status')
+    .should('eq', 200);
+  cy.wait(`@${bulkPricingAlias}`).its('status').should('eq', 200);
 }
 
-export function visitProductWithBulkPrices() {    
-    visitProduct(sampleData.PRODUCT);
-    checkTableData();
+export function visitProductWithBulkPrices() {
+  visitProduct(sampleData.PRODUCT);
+  checkTableData();
 }
 
-export function checkTableData(){
-    const selector = "cx-bulk-pricing-table .table";
-    cy.get(selector).contains('td', sampleData.expectedData[0].quantity);
-    cy.get(selector).contains('td', sampleData.expectedData[0].price);
-    cy.get(selector).contains('td', sampleData.expectedData[0].discount);
+export function checkTableData() {
+  const selector = 'cx-bulk-pricing-table .table';
+  cy.get(selector).contains('td', sampleData.expectedData[0].quantity);
+  cy.get(selector).contains('td', sampleData.expectedData[0].price);
+  cy.get(selector).contains('td', sampleData.expectedData[0].discount);
 }
 
-export function visitProductWithNoBulkPrices(){
-    //cy.visit(`/product/${sampleData.PRODUCT_NO_PRICING}`);
-    visitProduct(sampleData.PRODUCT_NO_PRICING);
+export function visitProductWithNoBulkPrices() {
+  //cy.visit(`/product/${sampleData.PRODUCT_NO_PRICING}`);
+  visitProduct(sampleData.PRODUCT_NO_PRICING);
 }
 
-
-export function checkNoTableRendered(){
-    cy.get('cx-bulk-pricing-table .container').should('not.exist');
+export function checkNoTableRendered() {
+  cy.get('cx-bulk-pricing-table .container').should('not.exist');
 }
