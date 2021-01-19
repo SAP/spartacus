f0f5bce86b52715e99701f47f87a465837b2bebf
Renuchan Thambirajah <71661414+ren-tham@users.noreply.github.com>
2021-01-19 07:45:26 -0500

    Create data layer for bulk price information #10560
    Closes #10560

--- feature-libs/product/bulk-pricing/components/bulk-pricing-table/bulk-pricing-table.component.ts
+++ feature-libs/product/bulk-pricing/components/bulk-pricing-table/bulk-pricing-table.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BulkPricesService } from '../../core/services/bulk-prices.service';
+import { RoutingService } from '@spartacus/core';
+import { BulkPrice } from '../../core/model/bulk-price.model';
+import { Observable, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-bulk-pricing-table',
   templateUrl: './bulk-pricing-table.component.html',
 })
-export class BulkPricingTableComponent implements OnInit {
-  constructor() {}
-  testString = 'bulk-pricing-table works!';
+export class BulkPricingTableComponent implements OnInit, OnDestroy {
+  pricingTiers: BulkPrice[];
+  pricesSubscription: Subscription;
+  testString;
 
-  ngOnInit(): void {}
+  protected readonly PRODUCT_KEY = 'productCode';
+
+  constructor(
+    protected routingService: RoutingService,
+    protected bulkPrices: BulkPricesService
+  ) {
+    // TODO: once we are done with the template, remove testString
+    this.testString = 'bulk-pricing-table works!';
+  }
+
+  // TODO: once we are done with the template, remove the subscribe/unsubscribe code
+  ngOnInit(): void {
+    this.pricesSubscription = this.getPrices().subscribe((prices) => {
+      this.pricingTiers = prices;
+    });
+  }
+
+  getPrices(): Observable<BulkPrice[]> {
+    return this.routingService.getRouterState().pipe(
+      switchMap((state) => {
+        const productCode = state.state.params[this.PRODUCT_KEY];
+        return this.bulkPrices.getBulkPrices(productCode);
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.pricesSubscription.unsubscribe();
+  }
 }
---
+++ feature-libs/product/bulk-pricing/core/services/bulk-prices.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Price, Product, ProductScope, ProductService } from '@spartacus/core';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { BulkPrice } from '../../core/model/bulk-price.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BulkPricesService {
+  protected readonly PRODUCT_SCOPE = ProductScope.PRICES;
+
+  constructor(protected productService: ProductService) {}
+
+  getBulkPrices(productCode: string): Observable<BulkPrice[]> {
+    return this.productService.get(productCode, this.PRODUCT_SCOPE).pipe(
+      switchMap((productPriceScope) => {
+        return of(this.convert(productPriceScope));
+      })
+    );
+  }
+
+  protected convert(productPriceScope: Product): BulkPrice[] {
+    let bulkPrices = [];
+
+    if (productPriceScope && productPriceScope !== {}) {
+      const basePrice = productPriceScope.price?.value;
+      const volumePrices = productPriceScope.volumePrices;
+
+      bulkPrices = volumePrices.map((volumePrice) =>
+        this.parsePrice(volumePrice, basePrice)
+      );
+    }
+
+    return bulkPrices;
+  }
+
+  protected parsePrice(priceTier: Price, basePrice: number): BulkPrice {
+    const bulkPriceTemplate: BulkPrice = {
+      currencyIso: priceTier.currencyIso,
+      formattedValue: priceTier.formattedValue,
+      maxQuantity: priceTier.maxQuantity,
+      minQuantity: priceTier.minQuantity,
+      priceType: priceTier.priceType,
+      value: priceTier.value,
+      formattedDiscount: '',
+      discount: 0,
+    };
+
+    return this.calculateDiscount(bulkPriceTemplate, basePrice);
+  }
+
+  protected calculateDiscount(
+    bulkPriceTemplate: BulkPrice,
+    basePrice: number
+  ): BulkPrice {
+    const bulkPrice = Object.assign({}, bulkPriceTemplate);
+
+    const tierPrice = bulkPriceTemplate.value;
+    const discount = Math.round(100.0 - (tierPrice / basePrice) * 100);
+    const formatted = `-${discount}%`;
+    bulkPrice.formattedDiscount = formatted;
+    bulkPrice.discount = discount;
+
+    return bulkPrice;
+  }
+}
