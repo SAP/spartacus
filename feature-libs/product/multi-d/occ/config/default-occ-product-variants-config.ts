e503df0f37e118cbf44453f0decfd3a2b693145a
Patryk Smul <psmul@divante.pl>
2021-01-22 13:39:20 +0100

    chore: removed unnecessary occ configuration

--- feature-libs/product/multi-d/occ/config/default-occ-product-variants-config.ts
+++ feature-libs/product/multi-d/occ/config/default-occ-product-variants-config.ts
@@ -5,13 +5,6 @@ export const defaultOccProductVariantsMultidimensionalConfig: OccConfig = {
     occ: {
       endpoints: {
         product: {
-          default:
-            'orgProducts/${productCode}?fields=DEFAULT,averageRating,images(FULL),classifications,manufacturer,numberOfReviews,categories(FULL),baseOptions,baseProduct,variantOptions,variantType',
-          list:
-            'orgProducts/${productCode}?fields=code,name,summary,price(formattedValue),images(DEFAULT,galleryIndex)',
-          details:
-            'orgProducts/${productCode}?fields=averageRating,stock(DEFAULT),description,availableForPickup,code,url,price(DEFAULT),numberOfReviews,manufacturer,categories(FULL),priceRange,multidimensional,configuratorType,configurable,tags,images(FULL)',
-          attributes: 'orgProducts/${productCode}?fields=classifications',
           variants:
             'orgProducts/${productCode}?fields=name,categories,multidimensional,variantMatrix,purchasable,baseOptions(DEFAULT),baseProduct,variantOptions(DEFAULT),variantType',
         },
