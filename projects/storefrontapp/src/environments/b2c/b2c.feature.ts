0880da55410bd14ad8f391b99deeb9bb7b84cb52
Krzysztof Platis <platonn.git@gmail.com>
2021-01-22 12:25:32 +0100

    move product configuration imports from app.module to b2c.feature

--- projects/storefrontapp/src/environments/b2c/b2c.feature.ts
+++ projects/storefrontapp/src/environments/b2c/b2c.feature.ts
@@ -1,10 +1,14 @@
-import { B2cStorefrontModule } from '@spartacus/storefront';
-import { FeatureEnvironment } from '../models/feature.model';
-import { StoreFinderRootModule } from '@spartacus/storefinder/root';
+import { ConfigModule } from '@spartacus/core';
+import { configuratorTranslations } from '@spartacus/product-configurator/common/assets';
+import { RulebasedConfiguratorRootModule } from '@spartacus/product-configurator/rulebased/root';
+import { TextfieldConfiguratorRootModule } from '@spartacus/product-configurator/textfield/root';
 import {
   storeFinderTranslationChunksConfig,
   storeFinderTranslations,
 } from '@spartacus/storefinder/assets';
+import { StoreFinderRootModule } from '@spartacus/storefinder/root';
+import { B2cStorefrontModule, StorefrontConfig } from '@spartacus/storefront';
+import { FeatureEnvironment } from '../models/feature.model';
 
 export const b2cFeature: FeatureEnvironment = {
   imports: [
@@ -37,5 +41,27 @@ export const b2cFeature: FeatureEnvironment = {
       },
     }),
     StoreFinderRootModule,
+
+    // PRODUCT CONFIGURATOR
+    ConfigModule.withConfig(<StorefrontConfig>{
+      i18n: { resources: configuratorTranslations },
+      featureModules: {
+        rulebased: {
+          module: () =>
+            import('@spartacus/product-configurator/rulebased').then(
+              (m) => m.RulebasedConfiguratorModule
+            ),
+        },
+        textfield: {
+          module: () =>
+            import('@spartacus/product-configurator/textfield').then(
+              (m) => m.TextfieldConfiguratorModule
+            ),
+        },
+      },
+    }),
+    RulebasedConfiguratorRootModule,
+    TextfieldConfiguratorRootModule,
+    // PRODUCT CONFIGURATOR END
   ],
 };
