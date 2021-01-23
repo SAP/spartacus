8feff907f25f4862d54b2c868edeed531884f253
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-01-22 19:22:14 -0500

    functions to build and deploy spa

--- ci-scripts/deploy-hs.sh
+++ ci-scripts/deploy-hs.sh
@@ -3,6 +3,7 @@
 APP="upp-cli"
 
 function download_cli {
+    echo "-----"
     echo "Downloading cli"
 
     curl -u $GH_USER:$GH_TOKEN -L -H "Accept: application/octet-stream" \
@@ -10,11 +11,24 @@ function download_cli {
 }
 
 function install_cli {
+    echo "-----"
     echo "Installing cli"
 
     unzip -o ${APP}.zip -d ${APP}
     cd ${APP}
     npm run install-cli
+}
+
+function build_spa {
+    echo "-----"
+    echo "Building Spartacus"
+    yarn install && yarn build:libs && yarn build:prod
+}
+
+function deploy_spa {
+    echo "-----"
+    echo "Deploying Spartacus"
+    upp application deploy -s ./dist -e stage
 }
 
 download_cli
