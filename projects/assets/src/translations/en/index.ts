46d7492883e2435ce6020f3b90cd0afbbb88c046
Patrick Labadie <patrick.labadie@sap.com>
2021-01-25 12:20:59 -0500

    GH-10047: Bring back asm i18n labels in assets lib for backwards compatibility

--- projects/assets/src/translations/en/index.ts
+++ projects/assets/src/translations/en/index.ts
@@ -1,4 +1,5 @@
 import { address } from './address';
+import { asm } from './asm';
 import { cart } from './cart';
 import { checkout } from './checkout';
 import { common } from './common';
@@ -10,6 +11,7 @@ import { user } from './user';
 
 export const en = {
   address,
+  asm, // @deprecated since 3.2, use asm lib instead
   cart,
   checkout,
   common,
