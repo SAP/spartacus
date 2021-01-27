771d9ed2e2851b93fe21dd648df1e7c3576ffc48
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2021-01-27 09:24:48 +0100

    Fix display of required message (#10879)
    * Fix display of required message
    
    * Add check  for user input
    
    * Align required message styling

--- feature-libs/product-configurator/rulebased/components/attribute/footer/configurator-attribute-footer.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/footer/configurator-attribute-footer.component.ts
@@ -39,16 +39,19 @@ export class ConfiguratorAttributeFooterComponent implements OnInit {
   /**
    * Checks if attribute is a user input typed attribute with empty value.
    * Method will return false for domain based attributes
-   * @param input
+   * @param {string} input - user input
    */
   isUserInputEmpty(input: string): boolean {
     return input !== undefined && (!input.trim() || 0 === input.length);
   }
 
   protected needsUserInputMessage(): boolean {
+    const uiType = this.attribute.uiType;
     return (
       this.attribute.required &&
       this.attribute.incomplete &&
+      (uiType === Configurator.UiType.STRING ||
+        uiType === Configurator.UiType.NUMERIC) &&
       this.isUserInputEmpty(this.attribute.userInput)
     );
   }
