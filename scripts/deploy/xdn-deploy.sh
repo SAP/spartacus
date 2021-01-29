d444de4c578e3405c917052f9cce84c4158947c4
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-01-29 13:55:03 -0500

    Do not forget to install angular cli

--- scripts/deploy/xdn-deploy.sh
+++ scripts/deploy/xdn-deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 ENDPOINT="gilberto-alvarado-spartacus-default.moovweb-edge.io"
+ANGULAR_CLI_VERSION='~10.1.0'
 
 function deploy_spa {
+    npm i -g @angular/cli@${ANGULAR_CLI_VERSION}
+
     echo "-----"
     echo "Installing xdn cli"
     npm i -g @xdn/cli@latest
