788a7c42c34db551bb149cf5786e6c712c95737e
Caine Rotherham <rotherham.c@gmail.com>
2021-02-01 16:49:49 +0100

    test: Disable units e2e test

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/units.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/units.e2e-spec.ts
@@ -1,4 +1,7 @@
-import { unitConfig } from '../../../../helpers/b2b/my-company/config/unit';
-import { testMyCompanyFeatureFromConfig } from '../../../../helpers/b2b/my-company/my-company.utils';
+// TODO: Reenable e2e test when unit clean up procedure is in place.
+// https://github.com/SAP/spartacus/issues/10958
 
-testMyCompanyFeatureFromConfig(unitConfig);
+// import { unitConfig } from '../../../../helpers/b2b/my-company/config/unit';
+// import { testMyCompanyFeatureFromConfig } from '../../../../helpers/b2b/my-company/my-company.utils';
+
+// testMyCompanyFeatureFromConfig(unitConfig);
