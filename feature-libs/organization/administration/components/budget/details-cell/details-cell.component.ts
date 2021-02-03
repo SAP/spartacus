340794cc87cd95493ba821d2e0db82c229623461
Mateusz Kolasa <mateusz.kolasa@sap.com>
2021-02-03 15:58:52 +0100

    Changes

--- feature-libs/organization/administration/components/budget/details-cell/details-cell.component.ts
+++ feature-libs/organization/administration/components/budget/details-cell/details-cell.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CellComponent } from '../../shared';
 
 @Component({
@@ -7,6 +9,8 @@ import { CellComponent } from '../../shared';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BudgetDetailsCellComponent extends CellComponent {
+  test$ = of('hello world').pipe(tap((data) => console.log(data)));
+
   public get key() {
     return this.model.code;
   }
