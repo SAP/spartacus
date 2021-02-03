560d2a56bcd652bd630040a5871b6ec5b46320d5
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-02-03 13:33:45 +0100

    jsdoc update

--- feature-libs/tracking/tms/config/tms-config.ts
+++ feature-libs/tracking/tms/config/tms-config.ts
@@ -1,25 +1,28 @@
 import { AbstractType, Injectable } from '@angular/core';
 import { Config, CxEvent, WindowRef } from '@spartacus/core';
 
+/**
+ * Collector configuration
+ */
 export interface TmsCollectorConfig {
   /**
-   * This function will initialize the data layer before the events start being pushed to it.
+   * Initializes the data layer.
    */
   dataLayerInit?: (winRef: WindowRef) => void;
   /**
-   * The function that pushes the events to the event layer
+   * Pushes the event to the configured data layer.
    */
   dataLayerPush?: <T extends CxEvent>(event: T, winRef: WindowRef) => void;
   /** Should be enabled in development mode only */
   debug?: boolean;
   /**
-   * An array of events to send to the configured TMS.
+   * Events to send to the configured TMS.
    */
   events: AbstractType<CxEvent>[];
 }
 
 /**
- * TMS configuration.
+ * TMS configuration
  */
 @Injectable({
   providedIn: 'root',
