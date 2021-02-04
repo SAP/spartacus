cdf8b80f9dd3d17367bb9428013a18bb6821d8e9
Caine Rotherham <rotherham.c@gmail.com>
2021-02-04 12:18:45 +0100

    correct naming

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/user-groups.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/user-groups.e2e-spec.ts
@@ -2,6 +2,6 @@ import { userGroupConfig } from '../../../../helpers/b2b/my-company/config/user-
 import { testMyCompanyFeatureFromConfig } from '../../../../helpers/b2b/my-company/my-company.utils';
 
 // TODO: Reenable e2e test when new backend server is in place (replacing dev0 with devci)
-describe.skip(`My Company - Units`, () => {
+describe.skip(`My Company - User Groups`, () => {
   testMyCompanyFeatureFromConfig(userGroupConfig);
 });
