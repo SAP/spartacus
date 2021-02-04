2abf16095273ee18016bd4a7c4dbbed875c08ba7
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-02-05 00:59:45 +0100

    injectable mappers example

--- projects/storefrontapp/src/app/spartacus/features/tracking-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/tracking-feature.module.ts
@@ -1,19 +1,37 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import {
   CartAddEntrySuccessEvent,
   CartRemoveEntrySuccessEvent,
+  CxEvent,
 } from '@spartacus/core';
 import { NavigationEvent } from '@spartacus/storefront';
-import { TmsModule } from '@spartacus/tracking/tms';
+import { TmsMapper, TmsModule } from '@spartacus/tracking/tms';
+
+@Injectable({ providedIn: 'root' })
+export class GtmMapper implements TmsMapper {
+  map<T extends CxEvent>(event: T): object | T {
+    console.log('gtm mapper');
+    return event;
+  }
+}
+@Injectable({ providedIn: 'root' })
+export class AepMapper implements TmsMapper {
+  map<T extends CxEvent>(event: T): object | T {
+    console.log('aep mapper');
+    return event;
+  }
+}
 
 @NgModule({
   imports: [
     TmsModule.forRoot({
       tagManager: {
         gtm: {
+          eventMapper: GtmMapper,
           events: [NavigationEvent, CartAddEntrySuccessEvent],
         },
         aep: {
+          eventMapper: AepMapper,
           events: [NavigationEvent, CartRemoveEntrySuccessEvent],
         },
       },
