36a79ca1ffd614f1882641ecd6345b5e9ef2c661
Caine Rotherham <rotherham.c@gmail.com>
2021-02-04 17:12:12 +0100

    test: Disable units and user groups B2B e2e tests

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/units.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/units.e2e-spec.ts
@@ -1,4 +1,8 @@
 import { unitConfig } from '../../../../helpers/b2b/my-company/config/unit';
 import { testMyCompanyFeatureFromConfig } from '../../../../helpers/b2b/my-company/my-company.utils';
 
-testMyCompanyFeatureFromConfig(unitConfig);
+// TODO: Reenable e2e test when unit clean up procedure is in place.
+// https://github.com/SAP/spartacus/issues/10958
+describe.skip(`My Company - Units`, () => {
+  testMyCompanyFeatureFromConfig(unitConfig);
+});
