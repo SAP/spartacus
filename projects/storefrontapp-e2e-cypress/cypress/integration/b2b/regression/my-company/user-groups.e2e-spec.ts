82649fa5e079557632a5c9ab2a7c0912ba1494e3
Caine Rotherham <rotherham.c@gmail.com>
2021-02-05 12:36:56 +0100

    test: Reenable user groups e2e (#11019)

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/user-groups.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/my-company/user-groups.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { userGroupConfig } from '../../../../helpers/b2b/my-company/config/user-group';
 import { testMyCompanyFeatureFromConfig } from '../../../../helpers/b2b/my-company/my-company.utils';
 
-// TODO: Reenable e2e test when new backend server is in place (replacing dev0 with devci)
-describe.skip(`My Company - User Groups`, () => {
+describe(`My Company - User Groups`, () => {
   testMyCompanyFeatureFromConfig(userGroupConfig);
 });
