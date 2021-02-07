6346228f81687bc3ebc6da44297b1dc3e3ccf3e1
Patrick Labadie <patrick.labadie@sap.com>
2021-02-07 16:11:01 -0500

    GH-10965: Add module initializer functionality.

--- projects/core/src/lazy-loading/tokens.ts
+++ projects/core/src/lazy-loading/tokens.ts
@@ -1,5 +1,13 @@
 import { InjectionToken } from '@angular/core';
 
+/**
+ * The MODULE_INITIALIZER is used as a multi provider that returns
+ * a function that should be executed when the module is lazy loaded.
+ * It is, in a way, an APP_INITIALIZER for lazy loaded modules.
+ *
+ * If the module is eagerly loaded, the MODULE_INITIALIZER functions
+ * run when the app is initialized.
+ */
 export const MODULE_INITIALIZER: InjectionToken<
   (() => any)[]
 > = new InjectionToken<(() => any)[]>('MODULE_INITIALIZER');
