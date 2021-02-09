b43c173a93be7845be058bc1ba29707b6fa51ae5
Patryk Smul <psmul@divante.pl>
2021-02-09 12:01:02 +0100

    fix: default product occ

--- projects/core/src/occ/adapters/product/default-occ-product-config.ts
+++ projects/core/src/occ/adapters/product/default-occ-product-config.ts
@@ -7,14 +7,14 @@ export const defaultOccProductConfig: OccConfig = {
       endpoints: {
         product: {
           default:
-            'orgProducts/${productCode}?fields=DEFAULT,averageRating,images(FULL),classifications,manufacturer,numberOfReviews,categories(FULL),baseOptions,baseProduct,variantOptions,variantType',
+            'products/${productCode}?fields=DEFAULT,averageRating,images(FULL),classifications,manufacturer,numberOfReviews,categories(FULL),baseOptions,baseProduct,variantOptions,variantType',
           list:
-            'orgProducts/${productCode}?fields=code,name,summary,price(formattedValue),images(DEFAULT,galleryIndex)',
+            'products/${productCode}?fields=code,name,summary,price(formattedValue),images(DEFAULT,galleryIndex)',
           details:
-            'orgProducts/${productCode}?fields=averageRating,stock(DEFAULT),description,availableForPickup,code,url,price(DEFAULT),numberOfReviews,manufacturer,categories(FULL),priceRange,multidimensional,tags,images(FULL)',
-          attributes: 'orgProducts/${productCode}?fields=classifications',
+            'products/${productCode}?fields=averageRating,stock(DEFAULT),description,availableForPickup,code,url,price(DEFAULT),numberOfReviews,manufacturer,categories(FULL),priceRange,multidimensional,tags,images(FULL)',
+          attributes: 'products/${productCode}?fields=classifications',
           variants:
-            'orgProducts/${productCode}?fields=name,categories,multidimensional,variantMatrix,purchasable,baseOptions(DEFAULT),baseProduct,variantOptions(DEFAULT),variantType',
+            'products/${productCode}?fields=name,purchasable,baseOptions(DEFAULT),baseProduct,variantOptions(DEFAULT),variantType',
         },
         productReviews: 'products/${productCode}/reviews',
         // Uncomment this when occ gets configured
