d2ed680ddcc1d39d9b4ccfe92b7685738f612fd3
Louis Pierrestiger <louis.pierrestiger@sap.com>
2021-02-16 15:58:58 -0500

    fix: fix auth event issue with ASM (#11119)
    A logout event use to fire when a support agent authenticated even if the was no session in progress.
    
    Closes #11120

--- projects/core/src/auth/user-auth/events/user-auth.events.ts
+++ projects/core/src/auth/user-auth/events/user-auth.events.ts
@@ -1,8 +1,14 @@
 /**
  * Indicates that the user has logged out
+ *
+ * * The event is fired even for customer emulation
+ * * The event is NOT fired for a ASM support agent authentication
  */
 export class LogoutEvent {}
 /**
  * Indicates that the user has logged in
+ *
+ * * The event is fired even for customer emulation
+ * * The event is NOT fired for a ASM support agent authentication
  */
 export class LoginEvent {}
