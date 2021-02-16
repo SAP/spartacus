43e1a46854dfdbe3b37fe580f365fd4c0f039263
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-02-16 16:15:57 -0500

    Getting URL for deployed app

--- ci-scripts/deploy-hs.js
+++ ci-scripts/deploy-hs.js
@@ -5,16 +5,22 @@ const process = exec(
   'upp application deploy -b a0000000000000000000000000000001 -s ./dist/storefrontapp -e stage'
 );
 
+let exp = /https\:\/\/\w+\.cloudfront\.net/;
 let output = '';
 
 process.stdout.on('data', (data) => {
-  console.log(data.toString());
+  line = data.toString();
+  console.log(line);
+  const match = line.match(exp);
+  if (match && match.length > 0) {
+    console.log('--> Deployment done to ' + match);
+  }
   output += data.toString();
-  //INFO - Deployment done. You can access the application at https://ddr1pf6lomx90.cloudfront.net
+  //INFO - Deployment done. You can access the application at https://ddr1pf6lomx90.cloudfront.net [Request id:3660df82-55fa-4c60-9017-15b22e344196]
 });
 
 process.on('close', (data) => {
-  console.log('Process done. Output --> ');
+  console.log('Process done. Output ommitted for now --> ');
   // console.log(output);
 });
 
