5512b3fd5dbddf3752f6f90a945fe0e35daeebef
Marcin Lasak <lasakmarcin90@gmail.com>
2021-02-17 09:38:00 +0100

    test: Drop explicit mobile flags

--- projects/storefrontapp-e2e-cypress/cypress/integration/mobile/product-search/product-search-store-flow-mobile.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/mobile/product-search/product-search-store-flow-mobile.e2e-spec.ts
@@ -1,22 +1,22 @@
-import * as productSearchStoreFlow from '../../../helpers/product-search-store-flow';
 import { clickSearchIcon } from '../../../helpers/product-search';
+import * as productSearchStoreFlow from '../../../helpers/product-search-store-flow';
 import { formats } from '../../../sample-data/viewports';
 
 context(
-  `${formats.mobile.width + 1}p resolution - Product search store flow`,
+  `${formats.mobile.width}p resolution - Product search store flow`,
+  {
+    viewportWidth: formats.mobile.width,
+    viewportHeight: formats.mobile.height,
+  },
   () => {
     before(() => {
       cy.visit('/');
     });
 
-    beforeEach(() => {
-      cy.viewport(formats.mobile.width, formats.mobile.height);
-    });
-
     describe('Product search', () => {
       it('should be able to search with store filtering', () => {
         clickSearchIcon();
-        productSearchStoreFlow.productStoreFlow('.cx-facet-mobile');
+        productSearchStoreFlow.productStoreFlow();
       });
     });
   }
