be1618e1fc131f938d54a63069d30c88813f3d72
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-02-17 17:13:57 -0500

    added default unit test to work on when core is ready

---
+++ feature-libs/cart/saved-cart/components/list/saved-cart-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SavedCartListComponent } from './saved-cart-list.component';
+
+describe('SavedCartListComponent', () => {
+  let component: SavedCartListComponent;
+  let fixture: ComponentFixture<SavedCartListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SavedCartListComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SavedCartListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
