59c7b76e5e2b64d60d2e6b2a169563233672248e
Michał Gruca <pucek9@gmail.com>
2021-02-17 03:10:50 +0100

    budget details uses row model

--- feature-libs/organization/administration/components/budget/details-cell/budget-details-cell.component.ts
+++ feature-libs/organization/administration/components/budget/details-cell/budget-details-cell.component.ts
@@ -1,29 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Observable } from 'rxjs';
 import { CellComponent } from '../../shared';
-import { BudgetItemService } from '../services/budget-item.service';
-import {
-  OutletContextData,
-  TableDataOutletContext,
-} from '@spartacus/storefront';
-import { Budget } from '../../../core/model/budget.model';
 
 @Component({
   selector: 'cx-org-budget-details-cell',
   templateUrl: './budget-details-cell.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BudgetDetailsCellComponent extends CellComponent {
-  model$: Observable<Budget>;
-
-  constructor(
-    protected outlet: OutletContextData<TableDataOutletContext>,
-    protected itemService: BudgetItemService
-  ) {
-    super(outlet);
-  }
-
-  openPopover() {
-    this.model$ = this.itemService.load(this.model.code);
-  }
-}
+export class BudgetDetailsCellComponent extends CellComponent {}
