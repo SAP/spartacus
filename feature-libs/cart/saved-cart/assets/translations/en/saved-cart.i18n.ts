0d5d2766022d0f6870631a2f4f1ad0584cefb854
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-02-16 19:27:09 -0500

    feat: Saved carts list page UI #9194

--- feature-libs/cart/saved-cart/assets/translations/en/saved-cart.i18n.ts
+++ feature-libs/cart/saved-cart/assets/translations/en/saved-cart.i18n.ts
@@ -1,6 +1,17 @@
 export const savedCartDetails = {};
 
-export const savedCartList = {};
+export const savedCartList = {
+  savedCarts: 'Saved Carts ({{count}})',
+  cartName: 'Name',
+  cartId: 'Id',
+  dateSaved: 'Date Saved',
+  cartDescription: 'Description',
+  quantity: 'Qty',
+  total: 'Total',
+  actions: 'Actions',
+  makeCartActive: 'Make cart active',
+  notFound: 'No Saved Carts Found',
+};
 
 export const savedCartCartPage = {};
 
