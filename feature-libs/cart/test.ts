c02219f304b71e91817d5a8c5642a45ddccb4f30
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-02-17 17:14:56 -0500

    add the filter in test.ts in cart feature-lib

--- feature-libs/cart/test.ts
+++ feature-libs/cart/test.ts
@@ -1,12 +1,12 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
 
 declare const require: {
   context(
@@ -27,4 +27,8 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context
+  .keys()
+  // filter tests from node_modules
+  .filter((key) => !key.startsWith('@'))
+  .forEach(context);
