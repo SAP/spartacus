905a08ee86ac6cc13513efc064708403e0b42596
Sagar Pawar <sagar.pawar.dev@gmail.com>
2021-02-17 17:48:26 +0530

    test: fix test initialization (GH-11166)

--- integration-libs/digital-payments/src/test.ts
+++ integration-libs/digital-payments/src/test.ts
@@ -1,24 +1,14 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
-import '@angular/localize/init';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
+declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -26,6 +16,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const CONTEXT = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+CONTEXT.keys().map(CONTEXT);
