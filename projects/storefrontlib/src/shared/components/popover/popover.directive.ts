7a04ff152089784cba309421051c05d1f8413ab1
Mateusz Kolasa <mateusz.kolasa@sap.com>
2021-02-18 09:47:48 +0100

    Popover update

--- projects/storefrontlib/src/shared/components/popover/popover.directive.ts
+++ projects/storefrontlib/src/shared/components/popover/popover.directive.ts
@@ -33,6 +33,10 @@ export class PopoverDirective implements OnInit {
 
   /**
    * The preferred placement of the popover. Default popover position is 'auto'.
+   *
+   *  Allowed popover positions: 'auto', 'top', 'bottom', 'left', 'right',
+   * 'top-left', 'top-right', 'bottom-left', 'bottom-right',
+   * 'left-top', 'left-bottom', 'right-top', 'right-bottom'.
    */
   @Input() placement?: PopoverPosition;
 
@@ -40,6 +44,11 @@ export class PopoverDirective implements OnInit {
    * Flag used to prevent firing popover open function.
    */
   @Input() disablePopover?: boolean;
+
+  /**
+   * Custom class name for popover component wrapper.
+   */
+  @Input() customClass? = 'cx-popover';
 
   /**
    * Append popover component into 'body' selector.
@@ -68,29 +77,16 @@ export class PopoverDirective implements OnInit {
   popoverContainer: ComponentRef<PopoverComponent>;
 
   /**
+   * Callback used to unlisten keydown event when is not necessary anymore.
+   */
+  keydownEventUnlistner: () => void;
+
+  /**
    * Method performs open action for popover component.
    */
   open() {
     if (!this.disablePopover) {
-      const containerFactory = this.componentFactoryResolver.resolveComponentFactory(
-        PopoverComponent
-      );
-      this.popoverContainer = this.viewContainer.createComponent(
-        containerFactory
-      );
-      if (this.popoverContainer && this.popoverContainer.instance) {
-        this.popoverContainer.instance.content = this.cxPopover;
-        this.popoverContainer.instance.triggerElement = this.element;
-        this.popoverContainer.instance.popoverInstance = this.popoverContainer;
-        this.popoverContainer.instance.position = this.placement;
-        if (this.appendToBody) {
-          this.popoverContainer.instance.appendToBody = this.appendToBody;
-          this.renderer.appendChild(
-            this.document.body,
-            this.popoverContainer.location.nativeElement
-          );
-        }
-      }
+      this.renderPopover();
       this.openPopover.emit();
       this.changeDetectorRef.markForCheck();
     }
@@ -102,9 +98,41 @@ export class PopoverDirective implements OnInit {
   close() {
     this.viewContainer.clear();
     this.closePopover.emit();
+    this.keydownEventUnlistner();
   }
 
-  ngOnInit(): void {
+  /**
+   * Method creates instance and pass parameters to popover component.
+   */
+  renderPopover() {
+    const containerFactory = this.componentFactoryResolver.resolveComponentFactory(
+      PopoverComponent
+    );
+    this.popoverContainer = this.viewContainer.createComponent(
+      containerFactory
+    );
+    if (this.popoverContainer && this.popoverContainer.instance) {
+      this.popoverContainer.instance.content = this.cxPopover;
+      this.popoverContainer.instance.triggerElement = this.element;
+      this.popoverContainer.instance.popoverInstance = this.popoverContainer;
+      this.popoverContainer.instance.position = this.placement;
+      this.popoverContainer.instance.customClass = this.customClass;
+      if (this.appendToBody) {
+        this.popoverContainer.instance.appendToBody = this.appendToBody;
+        this.renderer.appendChild(
+          this.document.body,
+          this.popoverContainer.location.nativeElement
+        );
+      }
+    }
+    this.triggerEscKeydownEvent();
+  }
+
+  /**
+   * Method uses `Renderer2` service to listen every click event.
+   * If binded to directive element was clicked popover changes the state.
+   */
+  triggerClickEvent() {
     this.renderer.listen(this.winRef.nativeWindow, 'click', (e: Event) => {
       if (e.target === this.element.nativeElement && !this.isOpen) {
         this.isOpen = true;
@@ -116,6 +144,29 @@ export class PopoverDirective implements OnInit {
         }
       }
     });
+  }
+
+  /**
+   * Method uses `Renderer2` service to listen every keydown event.
+   * If 'Escape' key was clicked popover component performs close action.
+   */
+  triggerEscKeydownEvent() {
+    this.keydownEventUnlistner = this.renderer.listen(
+      this.winRef.nativeWindow,
+      'keydown',
+      (e: KeyboardEvent) => {
+        if (e.code === 'Escape') {
+          if (this.isOpen) {
+            this.isOpen = false;
+            this.close();
+          }
+        }
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.triggerClickEvent();
   }
 
   constructor(
