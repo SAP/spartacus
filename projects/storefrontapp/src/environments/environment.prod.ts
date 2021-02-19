f6be72fe65315646c16353341ae210489b7181f7
Patrick Labadie <patrick.labadie@sap.com>
2021-02-19 11:31:42 -0500

    GH-11203: Test devci3 stability

--- projects/storefrontapp/src/environments/environment.prod.ts
+++ projects/storefrontapp/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   occBaseUrl:
     buildProcess.env.CX_BASE_URL ??
-    'https://spartacus-devci767.eastus.cloudapp.azure.com:9002',
+    'https://spartacus-devci7673.eastus.cloudapp.azure.com:9002',
   occApiPrefix: '/occ/v2/',
   cds: buildProcess.env.CX_CDS,
   b2b: buildProcess.env.CX_B2B,
