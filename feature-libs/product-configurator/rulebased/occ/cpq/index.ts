cf59b501762a74bb56630ff7773d46df4ef06d80
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2021-02-19 17:51:56 +0100

    rename #2

--- feature-libs/product-configurator/rulebased/occ/cpq/index.ts
+++ feature-libs/product-configurator/rulebased/occ/cpq/index.ts
@@ -1,4 +1,4 @@
 export * from './converters/index';
 export * from './cpq-configurator-occ.module';
 export * from './cpq-configurator-occ.service';
-export { defaultOccCpqConfiguratorConfigFactory as ɵdefaultOccCpqConfiguratorConfigFactory } from './default-occ-cpq-configurator-config';
+export { defaultOccCpqConfiguratorConfigFactory as ɵdefaultOccCpqConfiguratorConfigFactory } from './default-occ-configurator-cpq-config';
