0ab4888cd173aa5f63582183b4b2760aa4625773
Weizheng Gao <weizheng.gao@sap.com>
2021-02-21 17:08:25 -0500

    feature/GH-10880: move personalization into tracking lib

--- feature-libs/personalization/README.md
+++
@@ -1,5 +0,0 @@
-# Spartacus Personalization Library
-
-Spartacus Personalization can be added to the existing Spartacus application by running `ng add @spartacus/personalization`. For more information about Spartacus schematics, visit the [official Spartacus schematics documentation page](https://sap.github.io/spartacus-docs/schematics/).
-
-For more information, see [Spartacus](https://github.com/SAP/spartacus).
--- scripts/packages.ts
+++ scripts/packages.ts
@@ -62,7 +62,6 @@ const packageJsonPaths = [
   path.join(__dirname, '..', 'feature-libs', 'smartedit', 'package.json'),
   path.join(__dirname, '..', 'feature-libs', 'tracking', 'package.json'),
   path.join(__dirname, '..', 'feature-libs', 'qualtrics', 'package.json'),
-  path.join(__dirname, '..', 'feature-libs', 'personalization', 'package.json'),
   path.join(__dirname, '..', 'integration-libs', 'cdc', 'package.json'),
 ];
 
