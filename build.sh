9edfcfdc8416fd4678f912d12cf58b4bdd4a9f1c
Marcin Lasak <lasakmarcin90@gmail.com>
2021-02-21 14:38:23 +0100

    chore: Do not build app 2 times in Travis

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ SONAR=$1
 
 ./ci-scripts/validate-lint.sh
 ./ci-scripts/unit-tests-sonar.sh $SONAR
-./ci-scripts/build-for-deploy.sh
+# Build is also checked in the e2e job
+# ./ci-scripts/build-for-deploy.sh
