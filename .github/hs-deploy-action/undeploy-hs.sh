1dd80899d7d1f5862b95bfad5d48c6f412f79aa1
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-02-23 16:32:59 -0500

    Github action for undeploy apps from Hosting service

---
+++ .github/hs-deploy-action/undeploy-hs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "-----"
+echo "Undeploying issue ${GITHUB_HEAD_REF}"
+
+ISSUE_NO=`echo "${GITHUB_HEAD_REF}" | cut -d'-' -f 2`
+upp application undeploy -b ${ISSUE_NO} -e stage
+
+echo "-----"
+echo "App Undeployed successfully"
+
+
