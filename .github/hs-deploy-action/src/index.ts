cfeae10d7fa6ba64f33ce1b8da7aa8be66d50630
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-02-23 13:28:30 -0500

    wrong object

--- .github/hs-deploy-action/src/index.ts
+++ .github/hs-deploy-action/src/index.ts
@@ -30,7 +30,7 @@ async function run() {
 
   // await deploy(github, octoKit);
 
-  await addComment(github, octoKit, 'Sample comment');
+  await addComment(context, octoKit, 'Sample comment');
 }
 
 run();
