046c4c4ade00070219ce1baf2d0e1d9143acc9cd
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-02-25 03:03:22 -0500

    feat: Saved carts list page UI #9194

---
+++ feature-libs/cart/saved-cart/components/list/saved-cart-list.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  Cart,
+  EntitiesModel,
+  RoutingService,
+  TranslationService,
+} from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { SavedCartService } from '../../core/services/saved-cart.service';
+
+@Component({
+  selector: 'cx-saved-cart-list',
+  templateUrl: './saved-cart-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SavedCartListComponent {
+  savedCarts$: Observable<
+    EntitiesModel<Cart>
+  > = this.savedCartService.getList();
+
+  constructor(
+    protected routing: RoutingService,
+    protected translation: TranslationService,
+    protected savedCartService: SavedCartService
+  ) {}
+
+  goToSavedCartDetails(cart: Cart): void {
+    this.routing.go({
+      cxRoute: 'savedCartsDetails',
+      params: { savedCartId: cart?.code },
+    });
+  }
+}
