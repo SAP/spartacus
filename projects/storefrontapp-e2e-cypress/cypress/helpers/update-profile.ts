71f4a5cdc892a942a80e7f5d5fbee6903cd2ceec
Marcin Lasak <lasakmarcin90@gmail.com>
2021-03-01 19:30:02 +0100

    chore: Update password/profile viewport aware

--- projects/storefrontapp-e2e-cypress/cypress/helpers/update-profile.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/update-profile.ts
@@ -1,23 +1,9 @@
 import * as alerts from './global-message';
 import { checkBanner } from './homepage';
-export const UPDATE_PROFILE_URL = '/my-account/update-profile';
+
 export const newTitle = 'dr';
 export const newFirstName = 'N';
 export const newLastName = 'Z';
-
-export function accessPageAsAnonymous() {
-  cy.visit(UPDATE_PROFILE_URL);
-  cy.location('pathname').should('contain', '/login');
-}
-
-export function cancelUpdateProfileAction() {
-  cy.get('cx-update-profile-form').within(() => {
-    cy.get('button[type="button"]').click();
-  });
-  checkBanner();
-
-  cy.location('pathname').should('contain', '/');
-}
 
 export function updateProfile() {
   cy.get('cx-update-profile-form').within(() => {
@@ -48,39 +34,5 @@ export function verifyUpdatedProfile() {
       .should('have.value', newTitle);
     cy.get('[formcontrolname="firstName"]').should('have.value', newFirstName);
     cy.get('[formcontrolname="lastName"]').should('have.value', newLastName);
-  });
-}
-
-export function verifyAsAnonymous() {
-  it('should redirect to login page for anonymous user', () => {
-    accessPageAsAnonymous();
-  });
-}
-
-export function validateUpdateProfileForm(
-  title: string,
-  firstName: string,
-  lastName: string
-) {
-  cy.get('cx-update-profile-form').within(() => {
-    cy.get('[formcontrolname="titleCode"]')
-      .find(':selected')
-      .should('have.value', title);
-    cy.get('[formcontrolname="firstName"]').should('have.value', firstName);
-    cy.get('[formcontrolname="lastName"]').should('have.value', lastName);
-  });
-}
-
-export function updateProfileTest() {
-  it('should be able to cancel and go back to home', () => {
-    cancelUpdateProfileAction();
-  });
-
-  it('should be able to update profile details', () => {
-    updateProfile();
-  });
-
-  it('should be able to see the new profile info', () => {
-    verifyUpdatedProfile();
   });
 }
