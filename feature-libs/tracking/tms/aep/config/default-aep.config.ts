b6377c7b95a97c324b6889f23a0dee36d0e3faa9
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-03-02 16:47:46 +0100

    example - to be reverted

--- feature-libs/tracking/tms/aep/config/default-aep.config.ts
+++ feature-libs/tracking/tms/aep/config/default-aep.config.ts
@@ -4,7 +4,12 @@ import { AepCollectorService } from '../services/aep-collector.service';
 export const defaultAdobeExperiencePlatformConfig: TmsConfig = {
   tagManager: {
     aep: {
+      debug: true,
       collector: AepCollectorService,
+      script: {
+        url:
+          '//assets.adobedtm.com/ccc66c06b30b/3d6ad0fe69f4/launch-2243563c1764-development.min.js',
+      },
     },
   },
 };
--- feature-libs/tracking/tms/gtm/config/default-gtm.config.ts
+++ feature-libs/tracking/tms/gtm/config/default-gtm.config.ts
@@ -4,7 +4,11 @@ import { GtmCollectorService } from '../services/gtm-collector.service';
 export const defaultGoogleTagManagerConfig: TmsConfig = {
   tagManager: {
     gtm: {
+      debug: true,
       collector: GtmCollectorService,
+      script: {
+        url: 'https://www.googletagmanager.com/gtm.js?id=GTM-NFCR3XV',
+      },
     },
   },
 };
