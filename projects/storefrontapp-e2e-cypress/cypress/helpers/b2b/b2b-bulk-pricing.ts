37678b32cf5e01fe5954d61a1c1e7029c31c283d
Renuchan Thambirajah <71661414+ren-tham@users.noreply.github.com>
2021-03-02 04:59:24 -0500

    feature: bulk-pricing (#10158)
    Closes #10158

---
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/b2b/b2b-bulk-pricing.ts
@@ -0,0 +1,21 @@
+import { waitForPage } from '../checkout-flow';
+
+export function visitProduct(productCode) {
+  const page = `/product/${productCode}`;
+  const bulkPricingAlias = 'bulkPrices';
+  const productPageAlias = 'productPage';
+
+  waitForPage(productCode, productPageAlias);
+
+  cy.intercept(
+    'GET',
+    `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+      'BASE_SITE'
+    )}/orgProducts/${productCode}*`
+  ).as(bulkPricingAlias);
+
+  cy.visit(page);
+
+  cy.wait(`@${productPageAlias}`);
+  cy.wait(`@${bulkPricingAlias}`).its('response.statusCode').should('eq', 200);
+}
