ef98c5365c35a927325d4ac6fcbfdeb9696407e6
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-03-02 14:25:48 -0500

    Adding viewport context for desktop

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-flow.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-flow.e2e-spec.ts
@@ -1,30 +1,27 @@
 import * as checkout from '../../../helpers/checkout-flow';
+import { viewportContext } from '../../../helpers/viewport-context';
 
 context('Checkout flow', () => {
-  before(() => {
-    cy.window().then((win) => win.sessionStorage.clear());
-    checkout.visitHomePage();
-  });
+  viewportContext(['desktop'], () => {
+    before(() => {
+      cy.window().then((win) => {
+        win.sessionStorage.clear();
+        checkout.visitHomePage();
+      });
+    });
 
-  beforeEach(() => {
-    cy.restoreLocalStorage();
-  });
-
-  afterEach(() => {
-    cy.saveLocalStorage();
-  });
-
-  it('should perform checkout', () => {
-    checkout.registerUser();
-    checkout.goToCheapProductDetailsPage();
-    checkout.addCheapProductToCartAndLogin();
-    checkout.fillAddressFormWithCheapProduct();
-    checkout.verifyDeliveryMethod();
-    checkout.fillPaymentFormWithCheapProduct();
-    checkout.placeOrderWithCheapProduct();
-    checkout.verifyOrderConfirmationPageWithCheapProduct();
-    cy.waitForOrderToBePlacedRequest();
-    checkout.viewOrderHistoryWithCheapProduct();
-    checkout.signOut();
+    it('should perform checkout', () => {
+      checkout.registerUser();
+      checkout.goToCheapProductDetailsPage();
+      checkout.addCheapProductToCartAndLogin();
+      checkout.fillAddressFormWithCheapProduct();
+      checkout.verifyDeliveryMethod();
+      checkout.fillPaymentFormWithCheapProduct();
+      checkout.placeOrderWithCheapProduct();
+      checkout.verifyOrderConfirmationPageWithCheapProduct();
+      cy.waitForOrderToBePlacedRequest();
+      checkout.viewOrderHistoryWithCheapProduct();
+      checkout.signOut();
+    });
   });
 });
