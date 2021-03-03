9a46a55845f06a3a509e54b05acb61e87bd1edb6
Larisa Staroverova <larisa.staroverova@sap.com>
2021-03-03 15:26:55 +0100

    Fix failing component tests

--- feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.spec.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.spec.ts
@@ -80,6 +80,7 @@ describe('ConfiguratorAttributeProductCardComponent', () => {
   };
 
   const createValue = (
+    valueCode: string,
     description: string,
     images: Configurator.Image[],
     quantity: number,
@@ -88,6 +89,7 @@ describe('ConfiguratorAttributeProductCardComponent', () => {
     valueDisplay: string
   ): Configurator.Value => {
     const value: Configurator.Value = {
+      valueCode,
       description,
       images,
       quantity,
@@ -138,6 +140,7 @@ describe('ConfiguratorAttributeProductCardComponent', () => {
     component = fixture.componentInstance;
 
     value = createValue(
+      '888',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       [createImage('url', 'alt')],
       1,
