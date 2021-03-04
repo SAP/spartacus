1d0224ebaabba441f525bfd9f9dd18468b0c8681
Mateusz Kolasa <mateusz.kolasa@sap.com>
2021-03-04 12:28:20 +0100

    Add unit tests for popover component

--- projects/storefrontlib/src/shared/components/popover/popover.component.spec.ts
+++ projects/storefrontlib/src/shared/components/popover/popover.component.spec.ts
@@ -1,1 +1,109 @@
-// TODO: Add unit tests
+import { ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IconModule } from 'projects/storefrontlib/src/cms-components';
+import { KeyboardFocusTestingModule } from 'projects/storefrontlib/src/layout/a11y/keyboard-focus/focus-testing.module';
+import { take } from 'rxjs/operators';
+import { PopoverComponent } from './popover.component';
+import { PopoverEvent, PopoverPosition } from './popover.model';
+import { PositioningService } from './positioning.service';
+
+const mockPopoverPosition = 'top';
+
+const mockPopoverProperties = {
+  content: 'Test content',
+  customClass: 'test-class',
+  triggerElement: new ElementRef('a'),
+};
+
+class MockPositionService {
+  positionElements(): PopoverPosition {
+    return mockPopoverPosition;
+  }
+}
+
+describe('PopoverComponent', () => {
+  let component: PopoverComponent;
+  let fixture: ComponentFixture<PopoverComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, IconModule, KeyboardFocusTestingModule],
+      declarations: [PopoverComponent],
+      providers: [
+        { provide: PositioningService, useClass: MockPositionService },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PopoverComponent);
+
+    component = fixture.componentInstance;
+    component.popoverInstance = fixture.componentRef;
+
+    component.content = mockPopoverProperties.content;
+    component.customClass = mockPopoverProperties.customClass;
+    component.triggerElement = mockPopoverProperties.triggerElement;
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render `content` property', () => {
+    expect(fixture.debugElement.nativeNode.innerText).toContain(
+      mockPopoverProperties.content
+    );
+  });
+
+  it('should contain custom class', () => {
+    expect(fixture.debugElement.nativeNode.classList).toContain(
+      mockPopoverProperties.customClass
+    );
+  });
+
+  it('should contain position class', () => {
+    expect(fixture.debugElement.nativeNode.classList).toContain(
+      mockPopoverPosition
+    );
+  });
+
+  it('should contain `opened` class', () => {
+    expect(fixture.debugElement.nativeNode.classList).toContain('opened');
+  });
+
+  it('should contain close button', () => {
+    expect(fixture.debugElement.query(By.css('button.close'))).toBeTruthy();
+  });
+
+  it('should emit `insideClick` event', () => {
+    component.eventSubject.pipe(take(1)).subscribe((event: PopoverEvent) => {
+      expect(event).toBe(PopoverEvent.INSIDE_CLICK);
+    });
+
+    fixture.debugElement.query(By.css('.popover-body')).nativeElement.click();
+    fixture.detectChanges();
+  });
+
+  it('should emit `closeButtonClick` event', () => {
+    component.eventSubject.pipe(take(1)).subscribe((event: PopoverEvent) => {
+      expect(event).toBe(PopoverEvent.CLOSE_BUTTON_CLICK);
+    });
+
+    fixture.debugElement.query(By.css('button.close')).nativeElement.click();
+    fixture.detectChanges();
+  });
+
+  it('should emit `outsideClick` event', () => {
+    component.eventSubject.pipe(take(1)).subscribe((event: PopoverEvent) => {
+      expect(event).toBe(PopoverEvent.OUTSIDE_CLICK);
+    });
+
+    document.body.click();
+    fixture.detectChanges();
+  });
+});
