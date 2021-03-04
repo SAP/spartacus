1b14e8f92473b558f9a5bc0c03a6d125b6c2381e
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-03-04 12:29:01 -0500

    Removing extra contexts and hooks

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-as-guest.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-as-guest.e2e-spec.ts
@@ -12,18 +12,13 @@ context('Checkout as guest', () => {
   viewportContext(['mobile'], () => {
     before(() => {
       cy.window().then((win) => win.sessionStorage.clear());
-      cy.cxConfig({ checkout: { guest: true } } as CheckoutConfig);
     });
 
     beforeEach(() => {
-      cy.restoreLocalStorage();
+      cy.cxConfig({ checkout: { guest: true } } as CheckoutConfig);
     });
 
-    afterEach(() => {
-      cy.saveLocalStorage();
-    });
-
-    it('should perform checkout as guest', () => {
+    it('should perform checkout as guest and create an account after', () => {
       checkout.goToCheapProductDetailsPage();
       checkout.addCheapProductToCartAndProceedToCheckout();
 
@@ -36,84 +31,74 @@ context('Checkout as guest', () => {
       checkout.fillPaymentFormWithCheapProduct();
       checkout.placeOrderWithCheapProduct();
       checkout.verifyOrderConfirmationPageWithCheapProduct();
+
+      guestCheckout.createAccountFromGuest(user.password);
+
+      cy.selectUserMenuOption({
+        option: 'Address Book',
+      });
+
+      assertAddressForm(
+        {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          phone: '',
+          address: user.address,
+        },
+        'US-CA'
+      );
+
+      cy.selectUserMenuOption({
+        option: 'Payment Details',
+      });
+
+      cy.get('.cx-payment .cx-body').then(() => {
+        cy.get('cx-card').should('exist');
+      });
+
+      cy.selectUserMenuOption({
+        option: 'Personal Details',
+      });
+
+      validateUpdateProfileForm('mr', user.firstName, user.lastName);
+      checkout.signOut();
     });
 
-    describe('Create account', () => {
-      it('should create an account and display existing information from guest', () => {
-        guestCheckout.createAccountFromGuest(user.password);
+    it('should keep products in guest cart and restart checkout', () => {
+      checkout.goToCheapProductDetailsPage();
+      checkout.addCheapProductToCartAndProceedToCheckout();
 
-        cy.selectUserMenuOption({
-          option: 'Address Book',
-        });
+      guestCheckout.loginAsGuest();
 
-        assertAddressForm(
-          {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            phone: '',
-            address: user.address,
-          },
-          'US-CA'
-        );
+      checkout.fillAddressFormWithCheapProduct();
 
-        cy.selectUserMenuOption({
-          option: 'Payment Details',
-        });
+      const shippingPage = checkout.waitForPage(
+        '/checkout/shipping-address',
+        'getShippingPage'
+      );
 
-        cy.get('.cx-payment .cx-body').then(() => {
-          cy.get('cx-card').should('exist');
-        });
-
-        cy.selectUserMenuOption({
-          option: 'Personal Details',
-        });
-
-        validateUpdateProfileForm('mr', user.firstName, user.lastName);
-        checkout.signOut();
+      cy.onMobile(() => {
+        cy.get('cx-hamburger-menu [aria-label="Menu"]').click();
       });
-    });
+      const loginPage = waitForPage('/login', 'getLoginPage');
+      cy.findByText(/Sign in \/ Register/i).click();
+      cy.wait(`@${loginPage}`).its('status').should('eq', 200);
 
-    describe('Guest cart merge', () => {
-      before(() => {
-        cy.cxConfig({ checkout: { guest: true } } as CheckoutConfig);
-      });
-      it('should keep guest cart content and restart checkout', () => {
-        cy.clearLocalStorage();
-        checkout.goToCheapProductDetailsPage();
-        checkout.addCheapProductToCartAndProceedToCheckout();
+      login(user.email, user.password);
+      cy.wait(`@${shippingPage}`).its('status').should('eq', 200);
 
-        guestCheckout.loginAsGuest();
+      cy.get('cx-mini-cart .count').contains('1');
 
-        checkout.fillAddressFormWithCheapProduct();
+      const cartPage = waitForPage('/cart', 'getCartPage');
+      cy.get('cx-mini-cart').click();
+      cy.wait(`@${cartPage}`).its('status').should('eq', 200);
 
-        const shippingPage = checkout.waitForPage(
-          '/checkout/shipping-address',
-          'getShippingPage'
-        );
-
-        cy.onMobile(() => {
-          cy.get('cx-hamburger-menu [aria-label="Menu"]').click();
+      cy.get('cx-cart-item-list')
+        .contains('cx-cart-item', cheapProduct.code)
+        .within(() => {
+          cy.get('cx-item-counter input').should('have.value', '1');
         });
-        const loginPage = waitForPage('/login', 'getLoginPage');
-        cy.findByText(/Sign in \/ Register/i).click();
-        cy.wait(`@${loginPage}`).its('status').should('eq', 200);
-
-        login(user.email, user.password);
-        cy.wait(`@${shippingPage}`).its('status').should('eq', 200);
-
-        cy.get('cx-mini-cart .count').contains('1');
-
-        const cartPage = waitForPage('/cart', 'getCartPage');
-        cy.get('cx-mini-cart').click();
-        cy.wait(`@${cartPage}`).its('status').should('eq', 200);
-
-        cy.get('cx-cart-item-list')
-          .contains('cx-cart-item', cheapProduct.code)
-          .within(() => {
-            cy.get('cx-item-counter input').should('have.value', '1');
-          });
-        loginHelper.signOutUser();
-      });
+      loginHelper.signOutUser();
     });
   });
 });
