f1ba41861c2bcd3b225313a2ea7186aafac55720
Michal Dydo <mdydo@divante.pl>
2021-03-05 15:09:03 +0100

    Added occ adapter spec working version

--- feature-libs/cart/saved-cart/occ/adapters/occ-saved-cart.adapter.spec.ts
+++ feature-libs/cart/saved-cart/occ/adapters/occ-saved-cart.adapter.spec.ts
@@ -1,39 +1,40 @@
-// TODO: unit test
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ConverterService, OccEndpointsService } from '@spartacus/core';
+import { OccSavedCartAdapter } from './occ-saved-cart.adapter';
 
-// import {
-//   HttpClientTestingModule,
-//   HttpTestingController,
-// } from '@angular/common/http/testing';
-// import { TestBed } from '@angular/core/testing';
-// import { ConverterService, OccEndpointsService } from '@spartacus/core';
-// import { OccSavedCartAdapter } from './occ-saved-cart.adapter';
+class MockOccEndpointsService implements Partial<OccEndpointsService> {}
 
-// describe('OccSavedCartAdapter', () => {
-//   let adapter: OccSavedCartAdapter;
-//   let converter: ConverterService;
-//   let httpMock: HttpTestingController;
+describe('OccSavedCartAdapter', () => {
+  let adapter: OccSavedCartAdapter;
+  let httpMock: HttpTestingController;
+  let converterService: ConverterService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule],
-//       providers: [
-//         OccSavedCartAdapter,
-//         {
-//           provide: OccEndpointsService,
-//         },
-//       ],
-//     });
-//     converter = TestBed.inject(ConverterService);
-//     adapter = TestBed.inject(OccSavedCartAdapter);
-//     httpMock = TestBed.inject(HttpTestingController);
-//     spyOn(converter, 'pipeable').and.callThrough();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        OccSavedCartAdapter,
+        {
+          provide: OccEndpointsService,
+          useClass: MockOccEndpointsService,
+        },
+      ],
+    });
+    converterService = TestBed.inject(ConverterService);
+    adapter = TestBed.inject(OccSavedCartAdapter);
+    httpMock = TestBed.inject(HttpTestingController);
+    spyOn(converterService, 'pipeable').and.callThrough();
+  });
 
-//   afterEach(() => {
-//     httpMock.verify();
-//   });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
-//   it('should be created', () => {
-//     expect(adapter).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(adapter).toBeTruthy();
+  });
+});
