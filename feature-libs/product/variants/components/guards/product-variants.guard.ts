afa51ad98a616dba12eea37b0fe92779fee511e8
Patryk Smul <psmul@divante.pl>
2021-03-05 14:57:34 +0100

    fix: variant guard invalid routing

--- feature-libs/product/variants/components/guards/product-variants.guard.ts
+++ feature-libs/product/variants/components/guards/product-variants.guard.ts
@@ -34,27 +34,26 @@ export class ProductVariantsGuard implements CanActivate {
     }
 
     return this.productService.get(productCode, ProductScope.VARIANTS).pipe(
+      filter((p) => !!p),
       switchMap((product: Product) => {
-        if (product && !product.purchasable) {
+        if (!product.purchasable && product.variantOptions) {
           const variant = this.findVariant(product.variantOptions);
-          if (variant && variant.code) {
-            return this.productService
-              .get(variant.code, ProductScope.LIST)
-              .pipe(
-                filter((p) => Boolean(p)),
-                take(1),
-                map((_product: Product) => {
-                  return this.router.createUrlTree(
-                    this.semanticPathService.transform({
-                      cxRoute: 'product',
-                      params: _product,
-                    })
-                  );
+          // below call might looks redundant but in fact this data is going to be loaded anyways
+          // we're just calling it earlier and storing
+
+          // eslint-disable-next-line
+          return this.productService.get(variant.code!, ProductScope.LIST).pipe(
+            filter((p) => !!p),
+            take(1),
+            map((_product: Product) => {
+              return this.router.createUrlTree(
+                this.semanticPathService.transform({
+                  cxRoute: 'product',
+                  params: _product,
                 })
               );
-          } else {
-            return of(true);
-          }
+            })
+          );
         } else {
           return of(true);
         }
@@ -62,16 +61,10 @@ export class ProductVariantsGuard implements CanActivate {
     );
   }
 
-  findVariant(
-    variants: VariantOption[] | undefined
-  ): VariantOption | undefined {
-    if (!variants) {
-      return;
-    } else {
-      const results: VariantOption[] = variants.filter((variant) => {
-        return variant.stock && variant.stock.stockLevel ? variant : false;
-      });
-      return !results.length && variants.length ? variants[0] : results[0];
-    }
+  findVariant(variants: VariantOption[]): VariantOption {
+    const results: VariantOption[] = variants.filter((variant) => {
+      return variant.stock && variant.stock.stockLevel ? variant : false;
+    });
+    return !results.length && variants.length ? variants[0] : results[0];
   }
 }
