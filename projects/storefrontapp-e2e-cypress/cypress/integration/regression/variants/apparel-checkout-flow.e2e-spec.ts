e39fc9df511cfc5bfad3edeaf31c67248a21841b
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-03-05 09:48:43 -0500

    Adding viewports for mobile and desktop - Creating sample user - Addressing test changes

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/variants/apparel-checkout-flow.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/variants/apparel-checkout-flow.e2e-spec.ts
@@ -8,60 +8,60 @@ import {
   configureProductWithVariants,
   visitProductWithoutVariantPage,
 } from '../../../helpers/variants/apparel-checkout-flow';
+import { viewportContext } from '../../../helpers/viewport-context';
 import {
   cartWithTotalVariantProduct,
+  getApparelCheckoutUser,
   products,
-  variantUser,
 } from '../../../sample-data/apparel-checkout-flow';
 
 context('Apparel - checkout flow', () => {
-  before(() => {
-    cy.window().then((win) => win.sessionStorage.clear());
-    Cypress.env('BASE_SITE', APPAREL_BASESITE);
-  });
+  let variantUser;
+  viewportContext(['mobile', 'desktop'], () => {
+    before(() => {
+      cy.window().then((win) => win.sessionStorage.clear());
+      Cypress.env('BASE_SITE', APPAREL_BASESITE);
+      variantUser = getApparelCheckoutUser();
+    });
 
-  beforeEach(() => {
-    configureProductWithVariants();
-    cy.restoreLocalStorage();
-  });
+    beforeEach(() => {
+      configureProductWithVariants();
+    });
 
-  afterEach(() => {
-    cy.saveLocalStorage();
-  });
+    it('should perform checkout with a registered user', () => {
+      checkout.visitHomePage();
+      cy.onMobile(() => {
+        cy.get('cx-hamburger-menu [aria-label="Menu"]').click();
+      });
 
-  it('should perform checkout with a registered user', () => {
-    checkout.visitHomePage();
-    checkout.registerUser(false, variantUser);
-    checkout.goToCheapProductDetailsPage(products[0]);
-    addVariantOfSameProductToCart();
-    visitProductWithoutVariantPage();
-    addMutipleProductWithoutVariantToCart();
-    checkout.goToCheapProductDetailsPage(products[0]);
-    checkout.addCheapProductToCartAndLogin(variantUser, products[0]);
-    checkout.fillAddressFormWithCheapProduct(
-      variantUser,
-      cartWithTotalVariantProduct
-    );
-    checkout.verifyDeliveryMethod(APPAREL_DEFAULT_DELIVERY_MODE);
-    checkout.fillPaymentFormWithCheapProduct(
-      variantUser,
-      undefined,
-      cartWithTotalVariantProduct
-    );
-    checkout.placeOrderWithCheapProduct(
-      variantUser,
-      cartWithTotalVariantProduct,
-      APPAREL_CURRENCY
-    );
-    checkout.verifyOrderConfirmationPageWithCheapProduct(
-      variantUser,
-      products[0],
-      cartWithTotalVariantProduct,
-      true
-    );
-  });
-
-  after(() => {
-    checkout.signOut();
+      checkout.registerUser(false, variantUser);
+      checkout.goToCheapProductDetailsPage(products[0]);
+      addVariantOfSameProductToCart();
+      visitProductWithoutVariantPage();
+      addMutipleProductWithoutVariantToCart();
+      checkout.goToCheapProductDetailsPage(products[0]);
+      checkout.addCheapProductToCartAndLogin(variantUser, products[0]);
+      checkout.fillAddressFormWithCheapProduct(
+        variantUser,
+        cartWithTotalVariantProduct
+      );
+      checkout.verifyDeliveryMethod(APPAREL_DEFAULT_DELIVERY_MODE);
+      checkout.fillPaymentFormWithCheapProduct(
+        variantUser,
+        undefined,
+        cartWithTotalVariantProduct
+      );
+      checkout.placeOrderWithCheapProduct(
+        variantUser,
+        cartWithTotalVariantProduct,
+        APPAREL_CURRENCY
+      );
+      checkout.verifyOrderConfirmationPageWithCheapProduct(
+        variantUser,
+        products[0],
+        cartWithTotalVariantProduct,
+        true
+      );
+    });
   });
 });
