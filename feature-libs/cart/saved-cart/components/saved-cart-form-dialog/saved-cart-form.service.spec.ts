8a56720ae61160f2c98627593a490c05a6a33cd5
Michal Dydo <mdydo@divante.pl>
2021-03-05 12:30:51 +0100

    Added unit tests for form service

--- feature-libs/cart/saved-cart/components/saved-cart-form-dialog/saved-cart-form.service.spec.ts
+++ feature-libs/cart/saved-cart/components/saved-cart-form-dialog/saved-cart-form.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { SavedCartFormService } from './saved-cart-form.service';
 
+const mockNameMaxLength = 50;
+const mockDescriptionMaxLength = 50;
+
 describe('SavedCartFormService', () => {
   let service: SavedCartFormService;
 
@@ -11,5 +14,22 @@ describe('SavedCartFormService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should build the form', () => {
+    const form = service.getForm(mockNameMaxLength, mockDescriptionMaxLength);
+    expect(form.get('name')).not.toBeNull();
+    expect(form.get('description')).not.toBeNull();
+  });
+
+  it('should apply the values', () => {
+    const form = service.getForm(mockNameMaxLength, mockDescriptionMaxLength, {
+      name: 'testName',
+      description: 'testDescription',
+    });
+    expect(form.get('name')).not.toBeNull();
+    expect(form.get('name').value).toEqual('testName');
+    expect(form.get('description')).not.toBeNull();
+    expect(form.get('description').value).toEqual('testDescription');
   });
 });
