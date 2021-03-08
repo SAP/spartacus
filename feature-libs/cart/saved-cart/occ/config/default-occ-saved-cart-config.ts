9e823714158de31a997985d2fcb80d9e9754bf92
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-03-07 22:12:27 -0500

    occ saved cart adapter unit test..

--- feature-libs/cart/saved-cart/occ/config/default-occ-saved-cart-config.ts
+++ feature-libs/cart/saved-cart/occ/config/default-occ-saved-cart-config.ts
@@ -5,7 +5,7 @@ export const defaultOccSavedCartConfig: OccConfig = {
     occ: {
       endpoints: {
         savedCarts:
-          'users/${userId}/carts?savedCartsOnly=true&fields=carts(DEFAULT,potentialProductPromotions,appliedProductPromotions,potentialOrderPromotions,appliedOrderPromotions,entries(totalPrice(formattedValue),product(images(FULL),stock(FULL)),basePrice(formattedValue,value),updateable),totalPrice(formattedValue),totalItems,totalPriceWithTax(formattedValue),totalDiscounts(value,formattedValue),subTotal(formattedValue),deliveryItemsQuantity,deliveryCost(formattedValue),totalTax(formattedValue, value),pickupItemsQuantity,net,appliedVouchers,productDiscounts(formattedValue),saveTime,user,name,description)',
+          '/users/${userId}/carts?savedCartsOnly=true&fields=carts(DEFAULT,potentialProductPromotions,appliedProductPromotions,potentialOrderPromotions,appliedOrderPromotions,entries(totalPrice(formattedValue),product(images(FULL),stock(FULL)),basePrice(formattedValue,value),updateable),totalPrice(formattedValue),totalItems,totalPriceWithTax(formattedValue),totalDiscounts(value,formattedValue),subTotal(formattedValue),deliveryItemsQuantity,deliveryCost(formattedValue),totalTax(formattedValue, value),pickupItemsQuantity,net,appliedVouchers,productDiscounts(formattedValue),saveTime,user,name,description)',
         saveCart:
           '/users/${userId}/carts/${cartId}/save?saveCartName=${saveCartName}&saveCartDescription=${saveCartDescription}',
         savedCart: '/users/${userId}/carts/${cartId}/savedcart',
