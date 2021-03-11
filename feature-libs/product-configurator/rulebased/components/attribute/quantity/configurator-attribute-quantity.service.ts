a412a9b370c7ef9ac04fb97ee809029737a12130
Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
2021-03-11 06:24:25 +0100

    GH-11445 fix review issues in qty component, service and tests (#11446)
    * GH-11445 fix review issues in qty component, service and tests
    
    * GH-11445 Change assertion using .not.

--- feature-libs/product-configurator/rulebased/components/attribute/quantity/configurator-attribute-quantity.service.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/quantity/configurator-attribute-quantity.service.ts
@@ -1,14 +1,27 @@
-import { Configurator } from '../../../core/model/configurator.model';
 import { Injectable } from '@angular/core';
+import { Configurator } from '../../../core/model/configurator.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfiguratorAttributeQuantityService {
-  disableQuantityActions(value): boolean {
+  /**
+   * Checks if the interaction with the quantity control needs
+   * to be disabled
+   * @param value Selected value
+   * @returns Quantity actions disabled?
+   */
+  disableQuantityActions(value: any): boolean {
     return !value || value === '0';
   }
 
+  /**
+   * Checks if an attribute needs to be equipped with the option to select
+   * a quantity
+   * @param dataType Attribute data type
+   * @param uiType Attribute ui type, refers to how an attribute must be rendered
+   * @returns Render a quantity component?
+   */
   withQuantity(
     dataType: Configurator.DataType,
     uiType: Configurator.UiType
