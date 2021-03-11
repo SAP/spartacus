1a71376bb973a11f6bf8d1abf41961d4a5c610d2
Alexander Kuschner <alexander.kuschner@sap.com>
2021-03-11 08:34:23 +0100

    GH-11410 add line item model and fill it in the service class

---
+++ feature-libs/product-configurator/common/components/configurator-cart-entry-bundle-info/configurator-cart-entry-bundle-info.model.ts
@@ -0,0 +1,8 @@
+/**
+ * An interface representing the CPQ Line Item from order entry.
+ */
+export interface LineItem {
+  name?: string;
+  formattedQuantity?: string;
+  formattedPrice?: string;
+}
---
+++ feature-libs/product-configurator/common/components/configurator-cart-entry-bundle-info/configurator-cart-entry-bundle-info.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { OrderEntry } from '@spartacus/core';
+import { LineItem } from './configurator-cart-entry-bundle-info.model';
+import { ConfigurationInfo } from '../../core/model/common-configurator.model';
+
+/**
+ * Service for mapping of the CPQ line items from order entry
+ */
+@Injectable({ providedIn: 'root' })
+export class ConfiguratorCartEntryBundleInfoService {
+  /**
+   * Retrieves the numer of CPQ line items for an order entry
+   * @param entry order entry
+   * @returns number of line items
+   */
+  retrieveNumberOfLineItems(entry: OrderEntry): number {
+    const numberOfLineItems: number = entry?.configurationInfos
+      ? entry.configurationInfos.filter(
+          (configurationInfo) =>
+            configurationInfo.configurationLabel ||
+            configurationInfo.configurationValue
+        ).length
+      : 0;
+    return numberOfLineItems;
+  }
+
+  /**
+   * Retrieves the CPQ line items for an order entry
+   * @param entry order entry
+   * @returns line item array
+   */
+  retrieveLineItems(entry: OrderEntry): LineItem[] {
+    const lineItems: LineItem[] = [];
+    if (entry?.configurationInfos) {
+      entry.configurationInfos
+        .filter(
+          (configurationInfo) =>
+            configurationInfo.configurationLabel ||
+            configurationInfo.configurationValue
+        )
+        .forEach((configurationInfo) =>
+          lineItems.push(this.prepareLineItem(configurationInfo))
+        );
+    }
+    return lineItems;
+  }
+
+  protected prepareLineItem(configurationInfo: ConfigurationInfo): LineItem {
+    const quantityAndPrice: string[] = configurationInfo.configurationValue
+      ? configurationInfo.configurationValue.split('x')
+      : [];
+    return {
+      name: configurationInfo.configurationLabel
+        ? configurationInfo.configurationLabel
+        : '',
+      formattedQuantity:
+        quantityAndPrice.length >= 1 ? quantityAndPrice[0] : '',
+      formattedPrice: quantityAndPrice.length >= 2 ? quantityAndPrice[0] : '',
+    };
+  }
+}
