f0d33b433d7f8278352d2b775b0ee7649ea3e420
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2021-03-11 09:21:48 +0100

    imort from file, not index

--- feature-libs/product-configurator/rulebased/components/overview-bundle-attribute/configurator-overview-bundle-attribute.module.ts
+++ feature-libs/product-configurator/rulebased/components/overview-bundle-attribute/configurator-overview-bundle-attribute.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
 import { MediaModule } from '@spartacus/storefront';
-import { ConfiguratorPriceModule } from '../../components/price/index';
+import { ConfiguratorPriceModule } from '../price/configurator-price.module';
 import { ConfiguratorOverviewBundleAttributeComponent } from './configurator-overview-bundle-attribute.component';
 
 @NgModule({
