b7eb81362c7d92f1bfe68e3dd358034a8da42fea
Sebastian Stein <sebastian.stein@sap.com>
2021-03-11 13:05:59 +0100

    GH-11463: Review component

---
+++ feature-libs/misc/schematics/add-misc/index.d.ts
@@ -0,0 +1,3 @@
+import { Rule } from '@angular-devkit/schematics';
+import { LibraryOptions as SpartacusMiscOptions } from '@spartacus/schematics';
+export declare function addMiscFeatures(options: SpartacusMiscOptions): Rule;
--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -15,11 +15,10 @@ import { Environment } from './models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  occBaseUrl:
-    buildProcess.env.CX_BASE_URL ??
-    'https://api.cpce-teamtiger1-d1-public.model-t.cc.commerce.ondemand.com',
+  occBaseUrl: buildProcess.env.CX_BASE_URL ?? 'https://localhost:9002',
+  //'https://api.cpce-teamtiger1-d1-public.model-t.cc.commerce.ondemand.com',
   occApiPrefix: '/occ/v2/',
   cds: buildProcess.env.CX_CDS ?? false,
-  b2b: buildProcess.env.CX_B2B ?? false,
+  b2b: buildProcess.env.CX_B2B ?? true,
   cdc: buildProcess.env.CX_CDC ?? false,
 };
