b8707ee89fe12cfef8f6a8aff8fc7a28715778b3
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-03-12 18:44:37 -0500

    Introducing viewports

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-coupons/my-coupons.flaky-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-coupons/my-coupons.flaky-e2e-spec.ts
@@ -1,68 +1,71 @@
 import * as cartCoupon from '../../../helpers/cart-coupon';
 import { visitHomePage } from '../../../helpers/checkout-flow';
 import * as myCoupons from '../../../helpers/my-coupons';
+import { viewportContext } from '../../../helpers/viewport-context';
 
-describe('My coupons - Anonymous user', () => {
-  it('should redirect to login page', () => {
-    cy.visit('/my-account/coupons');
-    cy.location('pathname').should('contain', '/login');
-  });
-
-  it('should apply customer coupon that fails for anonymous user', () => {
-    cartCoupon.applyMyCouponAsAnonymous();
-  });
-});
-
-describe('My coupons - Authenticated user', () => {
-  beforeEach(() => {
-    cy.window().then((win) => {
-      win.sessionStorage.clear();
-    });
-    cy.requireLoggedIn();
-  });
-
-  describe('Claim customer coupon', () => {
-    it('should claim customer coupon successfully', () => {
-      myCoupons.verifyClaimCouponSuccess(myCoupons.validCouponCode);
-      cy.saveLocalStorage();
+viewportContext(['mobile', 'desktop'], () => {
+  describe('My coupons - Anonymous user', () => {
+    it('should redirect to login page', () => {
+      cy.visit('/my-account/coupons');
+      cy.location('pathname').should('contain', '/login');
     });
 
-    it('should not claim invalid customer coupon', () => {
-      cy.restoreLocalStorage();
-      myCoupons.verifyClaimCouponFail(myCoupons.invalidCouponCode);
+    it('should apply customer coupon that fails for anonymous user', () => {
+      cartCoupon.applyMyCouponAsAnonymous();
     });
   });
 
-  it('should claim customer coupon, switch notification button and find products', () => {
-    visitHomePage();
-    cy.selectUserMenuOption({
-      option: 'My Coupons',
+  describe('My coupons - Authenticated user', () => {
+    beforeEach(() => {
+      cy.window().then((win) => {
+        win.sessionStorage.clear();
+      });
+      cy.requireLoggedIn();
     });
-    myCoupons.verifyMyCoupons();
-  });
 
-  it('should list customer coupons and apply in cart', () => {
-    cartCoupon.verifyOrderPlacingWithCouponAndCustomerCoupon();
-  });
+    describe('Claim customer coupon', () => {
+      it('should claim customer coupon successfully', () => {
+        myCoupons.verifyClaimCouponSuccess(myCoupons.validCouponCode);
+        cy.saveLocalStorage();
+      });
 
-  it('should remove customer coupon from cart', () => {
-    cartCoupon.verifyCustomerCouponRemoving();
-  });
-});
-
-describe('My coupons test for pagination and sort', () => {
-  before(() => {
-    cy.window().then((win) => {
-      win.sessionStorage.clear();
+      it('should not claim invalid customer coupon', () => {
+        cy.restoreLocalStorage();
+        myCoupons.verifyClaimCouponFail(myCoupons.invalidCouponCode);
+      });
     });
-    cy.login(myCoupons.testUser, myCoupons.testPassword);
-    visitHomePage();
+
+    it('should claim customer coupon, switch notification button and find products', () => {
+      visitHomePage();
+      cy.selectUserMenuOption({
+        option: 'My Coupons',
+      });
+      myCoupons.verifyMyCoupons();
+    });
+
+    it('should list customer coupons and apply in cart', () => {
+      cartCoupon.verifyOrderPlacingWithCouponAndCustomerCoupon();
+    });
+
+    it('should remove customer coupon from cart', () => {
+      cartCoupon.verifyCustomerCouponRemoving();
+    });
   });
 
-  it('should page and sort my coupon list', () => {
-    cy.selectUserMenuOption({
-      option: 'My Coupons',
+  describe('My coupons test for pagination and sort', () => {
+    before(() => {
+      cy.window().then((win) => {
+        win.sessionStorage.clear();
+      });
+      cy.login(myCoupons.testUser, myCoupons.testPassword);
+      visitHomePage();
     });
-    myCoupons.verifyPagingAndSorting();
+
+    it('should page and sort my coupon list', () => {
+      cy.selectUserMenuOption({
+        option: 'My Coupons',
+      });
+      myCoupons.verifyPagingAndSorting();
+    });
   });
 });
