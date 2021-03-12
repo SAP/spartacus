b285d6a8a3a14c34524fa47f080217e239b529f5
Christoph Hinssen <christoph.hinssen@sap.com>
2021-03-12 08:05:35 +0100

    GH-11217 Add 2 tests

--- feature-libs/product-configurator/common/shared/utils/common-configurator-model-utils.spec.ts
+++ feature-libs/product-configurator/common/shared/utils/common-configurator-model-utils.spec.ts
@@ -1,11 +1,34 @@
 import { CommonConfigurator } from './../../core/model/common-configurator.model';
-import { getOwnerKey } from './common-configurator-model-utils';
+import {
+  createInitialOwner,
+  createOwner,
+  getOwnerKey,
+} from './common-configurator-model-utils';
 describe('CommonConfiguratorModelUtils', () => {
   const PRODUCT_CODE = 'CONF_LAPTOP';
 
-  it('should check defined string successfully', () => {
+  it('should compile owner key', () => {
     expect(
       getOwnerKey(CommonConfigurator.OwnerType.PRODUCT, PRODUCT_CODE)
     ).toBe(CommonConfigurator.OwnerType.PRODUCT + '/' + PRODUCT_CODE);
+  });
+
+  it('should create initial owner with only key defined', () => {
+    const owner = createInitialOwner();
+    expect(owner.key).toBe('INITIAL');
+    expect(owner.type).toBeUndefined();
+    expect(owner.id).toBeUndefined();
+  });
+
+  it('should create owner from attributes', () => {
+    const owner = createOwner(
+      CommonConfigurator.OwnerType.CART_ENTRY,
+      PRODUCT_CODE
+    );
+    expect(owner.key).toBe(
+      getOwnerKey(CommonConfigurator.OwnerType.CART_ENTRY, PRODUCT_CODE)
+    );
+    expect(owner.type).toBe(CommonConfigurator.OwnerType.CART_ENTRY);
+    expect(owner.id).toBe(PRODUCT_CODE);
   });
 });
