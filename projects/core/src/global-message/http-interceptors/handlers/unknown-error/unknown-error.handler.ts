66e72612a000fc483b8a1934213c82c7f0112c2e
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2021-03-15 10:41:55 +0100

    feat: improve unknown error handling on ssr or in dev mode (#11486)
    Previously we've seen some `unknown error -1` in log files on the server (SSR), which didn't give a lot of feedback on the specific error. With this change, we provide a bit more detail to the error at hand. 
    
    closes #11485

--- projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
+++ projects/core/src/global-message/http-interceptors/handlers/unknown-error/unknown-error.handler.ts
@@ -1,7 +1,8 @@
+import { HttpErrorResponse, HttpRequest } from '@angular/common/http';
 import { Injectable, isDevMode } from '@angular/core';
+import { Priority } from '../../../../util/applicable';
 import { HttpResponseStatus } from '../../../models/response-status.model';
 import { HttpErrorHandler } from '../http-error.handler';
-import { Priority } from '../../../../util/applicable';
 
 /**
  * Unknown Error Handler works as an fallback, to handle errors that were
@@ -16,13 +17,13 @@ export class UnknownErrorHandler extends HttpErrorHandler {
   /**
    * hasMatch always returns true, to mach all errors
    */
-  hasMatch(_errorResponse): boolean {
+  hasMatch(_errorResponse: HttpErrorResponse): boolean {
     return true;
   }
 
-  handleError() {
-    if (isDevMode()) {
-      console.warn(`Unknown http response error: ${this.responseStatus}`);
+  handleError(_request: HttpRequest<any>, errorResponse: HttpErrorResponse) {
+    if (isDevMode() || this.isSsr()) {
+      console.warn(`An unknown http error occurred\n`, errorResponse.message);
     }
   }
 
