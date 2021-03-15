a30601f00769a42937266910b0305aff6924ddf2
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2021-03-15 16:20:39 +0100

    proper unsubscribe

--- feature-libs/product-configurator/rulebased/components/attribute/quantity/configurator-attribute-quantity.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/quantity/configurator-attribute-quantity.component.ts
@@ -3,11 +3,12 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnDestroy,
   OnInit,
   Output,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, timer } from 'rxjs';
+import { Observable, Subscription, timer } from 'rxjs';
 import { debounce, distinct, take } from 'rxjs/operators';
 import { ConfiguratorUISettings } from '../../config/configurator-ui-settings';
 
@@ -26,8 +27,10 @@ export interface ConfiguratorAttributeQuantityComponentOptions {
   templateUrl: './configurator-attribute-quantity.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfiguratorAttributeQuantityComponent implements OnInit {
+export class ConfiguratorAttributeQuantityComponent
+  implements OnDestroy, OnInit {
   quantity = new FormControl(1);
+  managedSubscriptions: Subscription = new Subscription();
   @Input() quantityOptions: ConfiguratorAttributeQuantityComponentOptions;
   @Output() changeQuantity = new EventEmitter<Quantity>();
 
@@ -36,23 +39,31 @@ export class ConfiguratorAttributeQuantityComponent implements OnInit {
   ngOnInit(): void {
     this.quantity.setValue(this.quantityOptions?.initialQuantity?.quantity);
     // max to 2 changes are expected: 1) gets active, 2) gets disabled before update ==> take(2)
-    this.quantityOptions.disableQuantityActions
-      ?.pipe(distinct(), take(2))
-      .subscribe((disable) => {
-        if (disable) {
-          this.quantity.disable();
-        } else {
-          this.quantity.enable();
-        }
-      });
-    this.quantity.valueChanges
-      .pipe(
-        debounce(() =>
-          timer(this.config?.rulebasedConfigurator?.quantityDebounceTime)
-        ),
-        take(1)
-      )
-      .subscribe(() => this.onChangeQuantity());
+    this.managedSubscriptions.add(
+      this.quantityOptions.disableQuantityActions
+        ?.pipe(distinct(), take(2))
+        .subscribe((disable) => {
+          if (disable) {
+            this.quantity.disable();
+          } else {
+            this.quantity.enable();
+          }
+        })
+    );
+    this.managedSubscriptions.add(
+      this.quantity.valueChanges
+        .pipe(
+          debounce(() =>
+            timer(this.config?.rulebasedConfigurator?.quantityDebounceTime)
+          ),
+          take(1)
+        )
+        .subscribe(() => this.onChangeQuantity())
+    );
+  }
+
+  ngOnDestroy() {
+    this.managedSubscriptions.unsubscribe();
   }
 
   onChangeQuantity(): void {
