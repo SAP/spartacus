8f1574809dfc6c103ab06a87f60a627debf63569
Thambirajah <renuchan.thambirajah@sap.com>
2021-03-16 16:26:36 -0400

    Implemented sort for saved cart list page.

--- feature-libs/cart/saved-cart/components/list/saved-cart-list.component.ts
+++ feature-libs/cart/saved-cart/components/list/saved-cart-list.component.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/core';
 import { Cart, RoutingService, TranslationService } from '@spartacus/core';
 import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SavedCartService } from '../../core/services/saved-cart.service';
 
 @Component({
@@ -16,8 +17,19 @@ import { SavedCartService } from '../../core/services/saved-cart.service';
 export class SavedCartListComponent implements OnInit, OnDestroy {
   private subscription = new Subscription();
 
-  savedCarts$: Observable<Cart[]> = this.savedCartService.getList();
-
+  savedCarts$: Observable<Cart[]> = this.savedCartService.getList().pipe(
+    map((lists) =>
+      lists.sort((a: Cart, b: Cart) => {
+        let date1: number = a.saveTime
+          ? new Date(a.saveTime).getTime()
+          : new Date().getTime();
+        let date2: number = b.saveTime
+          ? new Date(b.saveTime).getTime()
+          : new Date().getTime();
+        return date2 - date1;
+      })
+    )
+  );
   constructor(
     protected routing: RoutingService,
     protected translation: TranslationService,
