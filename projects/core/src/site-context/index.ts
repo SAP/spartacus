9b8cd1cde26133cb5159305107b319ac9a5a5e08
Krzysztof Platis <platonn.git@gmail.com>
2021-03-16 13:23:16 +0100

    remove new factories from public API

--- projects/core/src/site-context/index.ts
+++ projects/core/src/site-context/index.ts
@@ -3,7 +3,7 @@ export * from './connectors/index';
 export * from './events/index';
 export * from './facade/index';
 export * from './providers/index';
-export * from './site-context.module';
+export { SiteContextModule } from './site-context.module';
 export * from './store/actions/index';
 export * from './store/selectors/index';
 export * from './store/state';
