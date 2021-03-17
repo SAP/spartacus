f1700aa8e4482a3682b05235257e53db0356661e
Marcin Lasak <lasakmarcin90@gmail.com>
2021-03-17 13:02:12 +0100

    perf: Replace importes more efficiently

--- projects/schematics/src/migrations/mechanism/rename-symbol/rename-symbol.ts
+++ projects/schematics/src/migrations/mechanism/rename-symbol/rename-symbol.ts
@@ -1,63 +1,60 @@
 import { Tree } from '@angular-devkit/schematics';
+import { ImportDeclarationStructure } from 'ts-morph';
+import { RenamedSymbol } from '../../../shared/utils/file-utils';
 // import { getSourceNodes } from '@schematics/angular/utility/ast-utils';
 import { createProgram } from '../../../shared/utils/program';
 import { getProjectTsConfigPaths } from '../../../shared/utils/project-tsconfig-paths';
 import { getDefaultProjectNameFromWorkspace } from '../../../shared/utils/workspace-utils';
-import { RenamedSymbol } from '../../../shared/utils/file-utils';
-import { ImportDeclarationStructure } from 'ts-morph';
-
 
 export function migrateRenamedSymbols(
   tree: Tree,
   renamedSymbols: RenamedSymbol[]
 ): Tree {
-    const project = getDefaultProjectNameFromWorkspace(tree);
+  const project = getDefaultProjectNameFromWorkspace(tree);
 
-    const { buildPaths } = getProjectTsConfigPaths(tree, project);
-    const basePath = process.cwd();
+  const { buildPaths } = getProjectTsConfigPaths(tree, project);
+  const basePath = process.cwd();
 
-    for (const tsconfigPath of buildPaths) {
+  for (const tsconfigPath of buildPaths) {
+    const { appSourceFiles } = createProgram(tree, basePath, tsconfigPath);
 
-        const { appSourceFiles } = createProgram(tree, basePath, tsconfigPath);
-
-        appSourceFiles.map((sourceFile) => {
-            const importDeclarationStructures: ImportDeclarationStructure[] = [];
-            sourceFile.getImportDeclarations().forEach((id) => {
-                id.getImportClause().getNamedImports().forEach((namedImport) => {
-                    if (renamedSymbols.some(rs => namedImport.getName() === rs.previousNode)) {
-                        const renamedSymbol = renamedSymbols.find(_ => _.previousNode === namedImport.getName()); // &&
-                                                                    //    _.previousImportPath === id.getModuleSpecifier().getText())
-                        importDeclarationStructures.push({
-                            namedImports: [renamedSymbol.newNode],
-                            moduleSpecifier: renamedSymbol.newImportPath
-                        } as ImportDeclarationStructure);
-                    } else {
-                        importDeclarationStructures.push({
-                            namedImports: [namedImport.getName()],
-                            moduleSpecifier: id.getModuleSpecifier().getText()
-                        } as ImportDeclarationStructure);
-                    }
-                });
+    appSourceFiles.map((sourceFile) => {
+      const importDeclarationStructures: ImportDeclarationStructure[] = [];
+      sourceFile.getImportDeclarations().forEach((id) => {
+        id.getImportClause()
+          .getNamedImports()
+          .forEach((namedImport) => {
+            const renamedSymbol = renamedSymbols.find(
+              (_) =>
+                _.previousNode === namedImport.getName() &&
+                _.previousImportPath ===
+                  id
+                    .getModuleSpecifier()
+                    .getText()
+                    .substr(1, id.getModuleSpecifier().getText().length - 2)
+            );
+            if (renamedSymbol) {
+              importDeclarationStructures.push({
+                namedImports: [renamedSymbol.newNode],
+                moduleSpecifier: renamedSymbol.newImportPath,
+              } as ImportDeclarationStructure);
+              if (id.getImportClause().getNamedImports().length > 1) {
+                namedImport.remove();
+              } else {
                 id.remove();
+              }
+            }
+          });
+      });
 
-            })
+      if (importDeclarationStructures.length) {
+        sourceFile.addImportDeclarations(importDeclarationStructures);
 
-            // importDeclarationStructures.forEach((ids) => {
-            //     sourceFile.addImportDeclaration({
-            //         namedImports: ids.namedImports,
-            //         moduleSpecifier: ids.moduleSpecifier
-            //     });
-            // })
-            sourceFile.addImportDeclarations(importDeclarationStructures);
+        sourceFile.organizeImports();
+        sourceFile.saveSync();
+      }
+    });
+  }
 
-            sourceFile.saveSync();
-        });
-
-        for (const sourceFile of appSourceFiles) {
-            sourceFile.organizeImports();
-            sourceFile.saveSync();
-        }
-    }
-
-    return tree;
+  return tree;
 }
