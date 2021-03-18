93677a4d12638d03ac89d27befe38ca54c190cf2
Mateusz Kolasa <mateusz.kolasa@sap.com>
2021-03-18 12:08:58 +0100

    Add export from cart component

---
+++ projects/storefrontlib/src/cms-components/cart/export-from-cart/export-from-cart.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cx-export-from-cart',
+  templateUrl: './export-from-cart.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ExportFromCartComponent {}
