106abbbb9ee7db8726c37ddbb20b9a88b9338b77
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2021-03-22 11:34:33 +0100

    deprecations

--- projects/storefrontlib/src/cms-components/myaccount/update-email/update-email-form/update-email-form.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/update-email/update-email-form/update-email-form.component.ts
@@ -3,7 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomFormValidators } from '../../../../shared/utils/validators/custom-form-validators';
 /**
  * @deprecated since 3.2, replaced with the `UpdateEmailComponent` in the  @spartacus/user package.
- */ @Component({
+ */
+@Component({
   selector: 'cx-update-email-form',
   templateUrl: './update-email-form.component.html',
 })
