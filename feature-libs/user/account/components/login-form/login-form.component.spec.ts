9ecd729cd5537b1b31d4df70825936c2ef1f6f83
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2021-03-22 11:40:36 +0100

    revert test

--- feature-libs/user/account/components/login-form/login-form.component.spec.ts
+++ feature-libs/user/account/components/login-form/login-form.component.spec.ts
@@ -87,7 +87,7 @@ describe('LoginFormComponent', () => {
 
   it('should init the form - prefilled', () => {
     const email = 'test@email.com';
-    windowRef.nativeWindow?.history.pushState(
+    windowRef.nativeWindow.history.pushState(
       {
         newUid: email,
       },
@@ -100,7 +100,7 @@ describe('LoginFormComponent', () => {
     expect(component.loginForm.controls['userId'].value).toBe(email);
 
     // reset the state
-    windowRef.nativeWindow?.history.replaceState(null, null);
+    windowRef.nativeWindow.history.replaceState(null, null);
   });
 
   describe('login()', () => {
