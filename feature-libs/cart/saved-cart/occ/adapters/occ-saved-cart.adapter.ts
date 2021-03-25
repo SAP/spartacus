9f8890b8f3d23cf7300d1d8ae8c84322fa62ae5b
Michał D <mdydo@divante.pl>
2021-03-25 11:49:24 +0100

    Update feature-libs/cart/saved-cart/occ/adapters/occ-saved-cart.adapter.ts
    Co-authored-by: Marcin Lasak <lasakmarcin90@gmail.com>

--- feature-libs/cart/saved-cart/occ/adapters/occ-saved-cart.adapter.ts
+++ feature-libs/cart/saved-cart/occ/adapters/occ-saved-cart.adapter.ts
@@ -44,8 +44,8 @@ export class OccSavedCartAdapter implements SavedCartAdapter {
   saveCart(
     userId: string,
     cartId: string,
-    saveCartName?: string,
-    saveCartDescription?: string
+    saveCartName: string,
+    saveCartDescription: string
   ): Observable<Cart> {
     return this.http
       .patch<Occ.Cart>(
