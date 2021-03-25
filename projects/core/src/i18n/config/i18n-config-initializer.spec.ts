0fbf9ad2c9fcca16eb488add748ff72bfb0472d6
Krzysztof Platis <platonn.git@gmail.com>
2021-03-25 11:19:28 +0100

    fix: set fallbackLang to context.langauge[0] by default unless customer explicitly sets fallbackLang to string or false

--- projects/core/src/i18n/config/i18n-config-initializer.spec.ts
+++ projects/core/src/i18n/config/i18n-config-initializer.spec.ts
@@ -4,10 +4,6 @@ import { ConfigInitializerService } from '../../config/config-initializer/config
 import { I18nConfig } from './i18n-config';
 import { I18nConfigInitializer } from './i18n-config-initializer';
 
-class MockI18nConfig {
-  i18n = { fallbackLang: undefined };
-}
-
 class MockConfigInitializerService
   implements Partial<ConfigInitializerService> {
   getStable() {
@@ -26,7 +22,7 @@ describe(`I18nConfigInitializer`, () => {
           provide: ConfigInitializerService,
           useClass: MockConfigInitializerService,
         },
-        { provide: I18nConfig, useClass: MockI18nConfig },
+        { provide: I18nConfig, useValue: {} },
       ],
     });
 
@@ -34,24 +30,35 @@ describe(`I18nConfigInitializer`, () => {
     config = TestBed.inject(I18nConfig);
   });
 
-  describe(`resolveConfig - fallbackLang was already configured statically`, () => {
-    it(`should return empty object`, async () => {
-      config.i18n = {
-        fallbackLang: 'en',
-      };
+  describe(`resolveConfig`, () => {
+    describe(`- fallbackLang was already configured statically`, () => {
+      it(`should return empty object`, async () => {
+        config.i18n = { fallbackLang: 'en' };
 
-      const result = await initializer.configFactory();
-      expect(result).toEqual({});
+        const result = await initializer.configFactory();
+        expect(result).toEqual({});
+      });
     });
-  });
 
-  describe(`resolveConfig - fallbackLang was not already configured statically`, () => {
-    it(`should return config based on context.language`, async () => {
-      const result = await initializer.configFactory();
-      expect(result).toEqual({
-        i18n: {
-          fallbackLang: 'testLang',
-        },
+    describe(`- fallbackLang was already configured statically to false (disabled)`, () => {
+      it(`should return empty object`, async () => {
+        config.i18n = { fallbackLang: false };
+
+        const result = await initializer.configFactory();
+        expect(result).toEqual({});
+      });
+    });
+
+    describe(`- fallbackLang was not already configured statically`, () => {
+      it(`should return config based on context.language`, async () => {
+        config.i18n = { fallbackLang: undefined };
+
+        const result = await initializer.configFactory();
+        expect(result).toEqual({
+          i18n: {
+            fallbackLang: 'testLang',
+          },
+        });
       });
     });
   });
