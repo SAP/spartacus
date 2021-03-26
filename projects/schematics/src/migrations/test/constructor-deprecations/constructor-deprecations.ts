3cb11f31d8ff1db38bfd25f0fd32ca3f7f9689d1
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-03-26 13:23:04 +0100

    cleaned unnecessary files

--- projects/schematics/src/migrations/test/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/test/constructor-deprecations/constructor-deprecations.ts
@@ -6,19 +6,12 @@ import { CART_PAGE_LAYOUT_HANDLER_MIGRATIONS } from './data/cart-page-layout-han
 import { CHECKOUT_SERVICE_MIGRATION } from './data/checkout.service.migration';
 import { CURRENT_PRODUCT_SERVICE_MIGRATION } from './data/current-product-service.migration';
 import { DYNAMIC_ATTRIBUTE_SERVICE_MIGRATION } from './data/dynamic-attribute.service.migration';
-import { FOOTER_NAVIGATION_COMPONENT_MIGRATION } from './data/footer-navigation.component.migration';
-import { LOGIN_FORM_COMPONENT_MIGRATION } from './data/login-form.component.migration';
 import { CATEGORY_PAGE_META_RESOLVER_MIGRATION } from './data/page-resolvers/category-page-meta.resolver.migration';
 import { CHECKOUT_PAGE_META_RESOLVER_MIGRATION } from './data/page-resolvers/checkout-page-meta.resolver.migration';
 import { PAGE_META_SERVICE_MIGRATION } from './data/page-resolvers/page-meta.service.migration';
 import { PRODUCT_PAGE_META_RESOLVER_MIGRATION } from './data/page-resolvers/product-page-meta.resolver.migration';
 import { PAGE_SLOT_COMPONENT_MIGRATION } from './data/page-slot.component.migration';
-import { PRODUCT_FACET_NAVIGATION_COMPONENT_MIGRATION } from './data/product-facet-navigation-component.migration';
-import { PRODUCT_LIST_COMPONENT_MIGRATION } from './data/product-list.component.migration';
-import { PRODUCT_REVIEWS_COMPONENT_MIGRATION } from './data/product-reviews.component.migration';
-import { PRODUCT_SERVICE_MIGRATION } from './data/product-service.migration';
 import { SELECTIVE_CART_SERVICE_MIGRATION } from './data/selective-cart.service.migration';
-import { STOREFRONT_COMPONENT_MIGRATION } from './data/storefront-component.migration';
 import { USER_ADDRESS_SERVICE_MIGRATION } from './data/user-address.service.migration';
 
 export const CONSTRUCTOR_DEPRECATION_DATA: ConstructorDeprecation[] = [
@@ -30,17 +23,10 @@ export const CONSTRUCTOR_DEPRECATION_DATA: ConstructorDeprecation[] = [
   CHECKOUT_PAGE_META_RESOLVER_MIGRATION,
   ADD_TO_CART_COMPONENT_MIGRATION,
   ...CART_PAGE_LAYOUT_HANDLER_MIGRATIONS,
-  LOGIN_FORM_COMPONENT_MIGRATION,
   CURRENT_PRODUCT_SERVICE_MIGRATION,
-  STOREFRONT_COMPONENT_MIGRATION,
-  PRODUCT_SERVICE_MIGRATION,
-  PRODUCT_LIST_COMPONENT_MIGRATION,
-  FOOTER_NAVIGATION_COMPONENT_MIGRATION,
   ...PAGE_SLOT_COMPONENT_MIGRATION,
-  PRODUCT_REVIEWS_COMPONENT_MIGRATION,
   DYNAMIC_ATTRIBUTE_SERVICE_MIGRATION,
   SELECTIVE_CART_SERVICE_MIGRATION,
-  PRODUCT_FACET_NAVIGATION_COMPONENT_MIGRATION,
 ];
 
 export function migrate(): Rule {
