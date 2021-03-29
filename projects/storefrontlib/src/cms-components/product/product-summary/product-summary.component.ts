798935d8885e12a732407d4f84d90f91608c9301
Caine Rotherham <rotherham.c@gmail.com>
2021-03-29 15:43:15 +0200

    update summary page to get bundles using bundle service

--- projects/storefrontlib/src/cms-components/product/product-summary/product-summary.component.ts
+++ projects/storefrontlib/src/cms-components/product/product-summary/product-summary.component.ts
@@ -1,28 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Product, SelectiveCartService } from '@spartacus/core';
 import {
-  Product,
-  SelectiveCartService,
-} from '@spartacus/core';
-import { BundleStarter, CartBundleService } from '@spartacus/cart/bundle/core';
+  BundleStarter,
+  BundleTemplate,
+  CartBundleService,
+} from '@spartacus/cart/bundle/core';
 import { Observable } from 'rxjs';
 import { CurrentProductService } from '../current-product.service';
 import { ProductDetailOutlets } from '../product-outlets.model';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-product-summary',
   templateUrl: './product-summary.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductSummaryComponent {
+export class ProductSummaryComponent implements OnInit {
   outlets = ProductDetailOutlets;
 
   product$: Observable<Product> = this.currentProductService.getProduct();
+  bundleTemplates$: Observable<BundleTemplate[]>;
 
   constructor(
     protected currentProductService: CurrentProductService,
     protected selectiveCartService: SelectiveCartService,
     protected cartBundleService: CartBundleService
-  ) { }
+  ) {}
+
+  ngOnInit(): void {
+    this.product$
+      .pipe(
+        filter((product) => !!product),
+        take(1)
+      )
+      .subscribe((product: Product) => {
+        this.bundleTemplates$ = this.cartBundleService.getBundleTemplates(
+          product.code
+        );
+      });
+  }
 
   startBundle(product: Product, template: any) {
     this.cartBundleService.startBundle(<BundleStarter>{
