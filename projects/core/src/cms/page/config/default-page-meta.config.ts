9198bb5ebc85a72a0a31486df9503610d5503d3f
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2021-03-30 12:28:29 +0200

    drop feature that should only be introduced with next major release

--- projects/core/src/cms/page/config/default-page-meta.config.ts
+++ projects/core/src/cms/page/config/default-page-meta.config.ts
@@ -19,7 +19,6 @@ export const defaultPageMetaConfig: PageMetaConfig = {
       {
         property: 'description',
         method: 'resolveDescription',
-        disabledInCsr: true,
       },
       {
         property: 'image',
