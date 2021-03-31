addcfa09e4914023d93b38eb6edd51053048ece3
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2021-03-31 16:58:05 +0200

    revert unintended change

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -16,13 +16,11 @@ import { Environment } from './models/environment.model';
 export const environment: Environment = {
   production: false,
 
-  //'https://localhost:9002',
   occBaseUrl:
-    //buildProcess.env.CX_BASE_URL ??
-    'https://localhost:9002',
-  //'https://api.cpce-teamtiger1-d1-public.model-t.cc.commerce.ondemand.com',
+    buildProcess.env.CX_BASE_URL ??
+    'https://api.cpce-teamtiger1-d1-public.model-t.cc.commerce.ondemand.com',
   occApiPrefix: '/occ/v2/',
   cds: buildProcess.env.CX_CDS ?? false,
-  b2b: buildProcess.env.CX_B2B ?? true,
+  b2b: buildProcess.env.CX_B2B ?? false,
   cdc: buildProcess.env.CX_CDC ?? false,
 };
