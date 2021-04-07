0332b24839fa28c113289957bd4b763ea4483414
Michał Gruca <pucek9@gmail.com>
2021-04-07 17:20:29 +0200

    up

--- feature-libs/cart/import-export/core/import-export-core.module.ts
+++ feature-libs/cart/import-export/core/import-export-core.module.ts
@@ -1,4 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { provideDefaultConfig } from '@spartacus/core';
+import { defaultImportExportConfig } from './config/default-import-export-config';
 
 @NgModule({
   providers: [provideDefaultConfig(defaultImportExportConfig)],
