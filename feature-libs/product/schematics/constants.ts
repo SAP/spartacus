da8ea4f9f8d7c924ceb4b4c2b40ac0ed5006afa7
Marcin Lasak <lasakmarcin90@gmail.com>
2021-04-07 13:13:30 +0200

    chore: Schematics update for new app structure (#11060)
    Co-authored-by: Marcin Lasak <lasakmarcin90@gmail.com>
    Co-authored-by: Nikola Zarić <nikola.zaric.ns@gmail.com>
    Co-authored-by: Patryk Smul <psmul@divante.pl>
    
    Part 1 of #10744
    
    Closes #11582

--- feature-libs/product/schematics/constants.ts
+++ feature-libs/product/schematics/constants.ts
@@ -1,8 +1,9 @@
-export const SPARTACUS_PRODUCT = '@spartacus/product';
-export const PRODUCT_SCSS_FILE_NAME = 'product.scss';
+import { SPARTACUS_PRODUCT } from '@spartacus/schematics';
 
+export const PRODUCT_FOLDER_NAME = 'product';
+export const PRODUCT_SCSS_FILE_NAME = 'product.scss';
 export const BULK_PRICING_MODULE = 'BulkPricingModule';
-export const BULK_PRICING_FEATURE_NAME = 'bulkPricing';
+export const BULK_PRICING_FEATURE_NAME = 'productBulkPricing';
 export const BULK_PRICING_ROOT_MODULE = 'BulkPricingRootModule';
 export const SPARTACUS_BULK_PRICING = `${SPARTACUS_PRODUCT}/bulk-pricing`;
 export const SPARTACUS_BULK_PRICING_ROOT = `${SPARTACUS_BULK_PRICING}/root`;
