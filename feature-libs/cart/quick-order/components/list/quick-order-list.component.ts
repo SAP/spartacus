348db6e3ae46d9ce7e0d27938b660ca2b96e49b7
Michal Dydo <mdydo@divante.pl>
2021-04-12 08:59:03 +0200

    quick order list basic implementation

--- feature-libs/cart/quick-order/components/list/quick-order-list.component.ts
+++ feature-libs/cart/quick-order/components/list/quick-order-list.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { OrderEntry } from 'projects/core/src/model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-quick-order-list',
   templateUrl: './quick-order-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuickOrderListComponent {}
+export class QuickOrderListComponent {
+  entries$: Observable<OrderEntry>;
+}
--- feature-libs/cart/quick-order/components/list/quick-order-list.module.ts
+++ feature-libs/cart/quick-order/components/list/quick-order-list.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthGuard, CmsConfig, provideDefaultConfig } from '@spartacus/core';
+import { CardModule } from '@spartacus/storefront';
 import { QuickOrderListComponent } from './quick-order-list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
