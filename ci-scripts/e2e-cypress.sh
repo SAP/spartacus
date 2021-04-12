ef1fa9237e0ea1dba4e1861e95811bb41d99a622
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-04-12 15:34:47 -0400

    Running e2es against QA server only

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -42,30 +42,6 @@ done
 set -- "${POSITIONAL[@]}"
 
 echo '-----'
-echo "Building Spartacus libraries"
-
-yarn install
-
-(cd projects/storefrontapp-e2e-cypress && yarn install)
-
-yarn build:libs 2>&1 | tee build.log
-
-results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
-if [[ -z "${results}" ]]; then
-    echo "Success: Spartacus production build was successful."
-    rm build.log
-else
-    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
-    rm build.log
-    exit 1
-fi
-echo '-----'
-echo "Building Spartacus storefront app"
-yarn build
-
-yarn start:pwa &
-
-echo '-----'
 echo "Running Cypress end to end tests"
 
 yarn e2e:run:ci"${SUITE}"
