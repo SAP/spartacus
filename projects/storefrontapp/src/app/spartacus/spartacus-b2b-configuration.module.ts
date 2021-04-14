9c51a68279bc9e9dce5e43d99258a46d9e6e69e1
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-04-14 15:24:39 +0200

    split configuration in our shell app

--- projects/storefrontapp/src/app/spartacus/spartacus-b2b-configuration.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-b2b-configuration.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { provideConfig } from '@spartacus/core';
 import {
+  defaultB2bCheckoutConfig,
+  defaultB2bOccConfig,
+} from '@spartacus/setup';
+import {
   defaultCmsContentProviders,
   layoutConfig,
   mediaConfig,
 } from '@spartacus/storefront';
-import {
-  defaultB2bCheckoutConfig,
-  defaultB2bOccConfig,
-} from '@spartacus/setup';
 
 @NgModule({
   providers: [
@@ -24,6 +24,8 @@ import {
         urlParameters: ['baseSite', 'language', 'currency'],
         baseSite: ['powertools-spa'],
       },
+    }),
+    provideConfig({
       pwa: {
         enabled: true,
         addToHomeScreen: true,
