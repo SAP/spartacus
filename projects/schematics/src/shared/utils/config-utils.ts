5606c5ad1ddb83e44e5f481e34d127ba10403bdf
Marcin Lasak <lasakmarcin90@gmail.com>
2021-04-14 16:16:25 +0200

    feat: New util for grabbing all spartacus providers

--- projects/schematics/src/shared/utils/config-utils.ts
+++ projects/schematics/src/shared/utils/config-utils.ts
@@ -17,7 +17,7 @@ import {
   B2C_STOREFRONT_MODULE,
   SPARTACUS_CORE,
 } from '../constants';
-import { isImportedFrom } from './import-utils';
+import { isImportedFrom, isImportedFromSpartacusLibs } from './import-utils';
 import { getModule } from './new-module-utils';
 
 /**
@@ -348,4 +348,41 @@ export function getConfigs(sourceFile: SourceFile): Node[] {
     });
   }
   return configs;
+}
+
+export function getSpartacusProviders(sourceFile: SourceFile): Node[] {
+  const module = getModule(sourceFile);
+  if (!module) {
+    return [];
+  }
+  const literal = module.getFirstDescendantByKind(
+    tsMorph.SyntaxKind.ObjectLiteralExpression
+  );
+  const providers: Node[] = [];
+  if (literal) {
+    const properties =
+      literal.getChildrenOfKind(tsMorph.SyntaxKind.PropertyAssignment) ?? [];
+    properties.forEach((property) => {
+      if (
+        property.getNameNode().getText() === 'providers' &&
+        property.getInitializerIfKind(tsMorph.SyntaxKind.ArrayLiteralExpression)
+      ) {
+        const initializer = property.getInitializerIfKind(
+          tsMorph.SyntaxKind.ArrayLiteralExpression
+        );
+        initializer?.getElements().forEach((element) => {
+          if (Node.isCallExpression(element) || Node.isSpreadElement(element)) {
+            const expression = element.getExpression();
+            if (
+              Node.isIdentifier(expression) &&
+              isImportedFromSpartacusLibs(expression)
+            ) {
+              providers.push(element);
+            }
+          }
+        });
+      }
+    });
+  }
+  return providers;
 }
