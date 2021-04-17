bc995ea96fb70bc37915d2162eb7408e8423c35e
Patrick Labadie <patrick.labadie@sap.com>
2021-04-17 11:19:14 -0400

    GH-10821:  Fix import

--- feature-libs/checkout/root/checkout-root.module.ts
+++ feature-libs/checkout/root/checkout-root.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { provideDefaultConfig } from '@spartacus/core';
-import { defaultCheckoutConfig } from '@spartacus/storefront';
+import { defaultCheckoutConfig } from './config/default-checkout-config';
 
 @NgModule({
   imports: [],
