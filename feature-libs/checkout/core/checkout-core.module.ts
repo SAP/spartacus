89aad110760766e28d6018e952c80bf223ed5568
Patrick Labadie <patrick.labadie@sap.com>
2021-04-20 09:19:40 -0400

    GH-10821: Remove CheckoutCoreModule.forRoot()

--- feature-libs/checkout/core/checkout-core.module.ts
+++ feature-libs/checkout/core/checkout-core.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { PageMetaResolver } from '@spartacus/core';
 import { CheckoutEventModule } from './events/checkout-event.module';
 import { CheckoutPageMetaResolver } from './services/checkout-page-meta.resolver';
@@ -6,18 +6,12 @@ import { CheckoutStoreModule } from './store/checkout-store.module';
 
 @NgModule({
   imports: [CheckoutStoreModule, CheckoutEventModule],
+  providers: [
+    {
+      provide: PageMetaResolver,
+      useExisting: CheckoutPageMetaResolver,
+      multi: true,
+    },
+  ],
 })
-export class CheckoutCoreModule {
-  static forRoot(): ModuleWithProviders<CheckoutCoreModule> {
-    return {
-      ngModule: CheckoutCoreModule,
-      providers: [
-        {
-          provide: PageMetaResolver,
-          useExisting: CheckoutPageMetaResolver,
-          multi: true,
-        },
-      ],
-    };
-  }
-}
+export class CheckoutCoreModule {}
