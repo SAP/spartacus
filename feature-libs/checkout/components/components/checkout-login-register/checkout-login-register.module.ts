422e7fc0760e84bc089f8e75cdc6b3585fe0edba
Patrick Labadie <patrick.labadie@sap.com>
2021-04-20 14:50:10 -0400

    10821: Adjust imports in CheckoutLoginRegisterModule

--- feature-libs/checkout/components/components/checkout-login-register/checkout-login-register.module.ts
+++ feature-libs/checkout/components/components/checkout-login-register/checkout-login-register.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {
-  CheckoutModule,
   CmsConfig,
   I18nModule,
   NotAuthGuard,
@@ -13,14 +12,7 @@ import { PageSlotModule } from '@spartacus/storefront';
 import { CheckoutLoginRegisterComponent } from './checkout-login-register.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    UrlModule,
-    PageSlotModule,
-    I18nModule,
-    CheckoutModule,
-  ],
+  imports: [CommonModule, RouterModule, UrlModule, PageSlotModule, I18nModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
