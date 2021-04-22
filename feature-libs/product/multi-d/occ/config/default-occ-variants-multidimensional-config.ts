6c9d66f5ef726098d79a0b449e782d6666f8516b
Michal Dydo <mdydo@divante.pl>
2021-04-22 11:11:33 +0200

    Fix occ config and styles for multi-d selectors

--- feature-libs/product/multi-d/occ/config/default-occ-variants-multidimensional-config.ts
+++ feature-libs/product/multi-d/occ/config/default-occ-variants-multidimensional-config.ts
@@ -5,7 +5,7 @@ export const defaultOccProductVariantsMultidimensionalConfig: OccConfig = {
     occ: {
       endpoints: {
         product:
-          'orgProducts/${productCode}?fields=name,categories,multidimensional,variantMatrix,purchasable,baseOptions(DEFAULT),baseProduct,variantOptions(DEFAULT),variantMatrix(DEFAULT),variantType',
+          'products/${productCode}?fields=name,averageRating,stock(DEFAULT),description,summary,availableForPickup,code,url,price(DEFAULT),numberOfReviews,manufacturer,categories(FULL),priceRange,multidimensional,tags,images(FULL),purchasable,baseOptions(DEFAULT),baseProduct,variantOptions(DEFAULT),variantMatrix(DEFAULT)',
       },
     },
   },
