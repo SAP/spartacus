148a074ae4c30b912ae3b0dcaed441e2af9d5cc5
Caine Rotherham <rotherham.c@gmail.com>
2021-04-22 09:22:37 +0200

    add config comment

--- projects/storefrontlib/src/layout/a11y/keyboard-focus/config/keyboard-focus.config.ts
+++ projects/storefrontlib/src/layout/a11y/keyboard-focus/config/keyboard-focus.config.ts
@@ -7,6 +7,9 @@ import { Config } from '@spartacus/core';
 })
 export abstract class KeyboardFocusConfig {
   keyboardFocus?: {
+    /**
+     *  Resets the focus back to the root `<cx-storefront>` element when a navigation is started.
+     */
     enableResetFocusOnNavigate?: boolean;
   };
 }
