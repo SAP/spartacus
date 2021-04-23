a76ac317bd79faaadb6af0ea97cdca45acb18aa2
d026976 <christoph.hinssen@sap.com>
2021-04-23 07:59:16 +0200

    Add optional scope attributes

--- feature-libs/product-configurator/rulebased/cpq/occ/model/occ-cpq-configurator-endpoints.model.ts
+++ feature-libs/product-configurator/rulebased/cpq/occ/model/occ-cpq-configurator-endpoints.model.ts
@@ -1,3 +1,4 @@
+import { OccEndpoint } from '@spartacus/core';
 declare module '@spartacus/core' {
   interface OccEndpoints {
     /**
@@ -5,24 +6,24 @@ declare module '@spartacus/core' {
      *
      * @member {string}
      */
-    getCpqAccessData?: string;
+    getCpqAccessData?: string | OccEndpoint;
     /**
      * Endpoint to add a cpq configuration to the current cart
      *
      * @member {string}
      */
-    addCpqConfigurationToCart?: string;
+    addCpqConfigurationToCart?: string | OccEndpoint;
     /**
      * Endpoint for updating a cpq configuration attached to the given cart entry
      */
-    updateCpqConfigurationForCartEntry?: string;
+    updateCpqConfigurationForCartEntry?: string | OccEndpoint;
     /**
      * Endpoint for reading a CPQ configuration attached to the given cart entry
      */
-    readCpqConfigurationForCartEntry?: string;
+    readCpqConfigurationForCartEntry?: string | OccEndpoint;
     /**
      * Endpoint for reading a CPQ configuration attached to a given order entry
      */
-    readCpqConfigurationForOrderEntry?: string;
+    readCpqConfigurationForOrderEntry?: string | OccEndpoint;
   }
 }
