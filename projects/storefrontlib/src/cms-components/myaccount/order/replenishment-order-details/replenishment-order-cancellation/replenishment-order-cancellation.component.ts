38970819f0506e903c9b3c667937e1f47edd9fed
Michał Gruca <pucek9@gmail.com>
2021-04-28 15:55:17 +0200

    add deprecation for usage ReplenishmentOrderCancellationLaunchDialogService

--- projects/storefrontlib/src/cms-components/myaccount/order/replenishment-order-details/replenishment-order-cancellation/replenishment-order-cancellation.component.ts
+++ projects/storefrontlib/src/cms-components/myaccount/order/replenishment-order-details/replenishment-order-cancellation/replenishment-order-cancellation.component.ts
@@ -9,6 +9,7 @@ import {
   ReplenishmentOrder,
   UserReplenishmentOrderService,
 } from '@spartacus/core';
+import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
 import { Observable, Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { ReplenishmentOrderCancellationLaunchDialogService } from './replenishment-order-cancellation-launch-dialog.service';
@@ -24,20 +25,56 @@ export class ReplenishmentOrderCancellationComponent implements OnDestroy {
 
   replenishmentOrder$: Observable<ReplenishmentOrder> = this.userReplenishmentOrderService.getReplenishmentOrderDetails();
 
+  // TODO(#12167): make launchDialogService a required dependency instead of replenishmentOrderCancellationLaunchDialogService and remove deprecated constructor
+  /**
+   * Default constructor
+   *
+   * @param {UserReplenishmentOrderService} userReplenishmentOrderService
+   * @param {ViewContainerRef} vcr
+   * @param {LaunchDialogService} launchDialogService
+   */
+  constructor(
+    userReplenishmentOrderService: UserReplenishmentOrderService,
+    vcr: ViewContainerRef,
+    launchDialogService: LaunchDialogService
+  );
+
+  /**
+   * @deprecated since 3.3
+   */
+  constructor(
+    userReplenishmentOrderService: UserReplenishmentOrderService,
+    vcr: ViewContainerRef,
+    replenishmentOrderCancellationLaunchDialogService: ReplenishmentOrderCancellationLaunchDialogService
+  );
   constructor(
     protected userReplenishmentOrderService: UserReplenishmentOrderService,
     protected replenishmentOrderCancellationLaunchDialogService: ReplenishmentOrderCancellationLaunchDialogService,
-    protected vcr: ViewContainerRef
+    protected vcr: ViewContainerRef,
+    protected launchDialogService?: LaunchDialogService
   ) {}
 
   openDialog() {
-    const dialog = this.replenishmentOrderCancellationLaunchDialogService.openDialog(
-      this.element,
-      this.vcr
-    );
+    // TODO(#12167): use launchDialogService only
+    if (this.launchDialogService) {
+      const dialog = this.launchDialogService.openDialog(
+        LAUNCH_CALLER.REPLENISHMENT_ORDER,
+        this.element,
+        this.vcr
+      );
 
-    if (dialog) {
-      this.subscription.add(dialog.pipe(take(1)).subscribe());
+      if (dialog) {
+        this.subscription.add(dialog.pipe(take(1)).subscribe());
+      }
+    } else {
+      const dialog = this.replenishmentOrderCancellationLaunchDialogService.openDialog(
+        this.element,
+        this.vcr
+      );
+
+      if (dialog) {
+        this.subscription.add(dialog.pipe(take(1)).subscribe());
+      }
     }
   }
 
