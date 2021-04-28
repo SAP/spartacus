de7e1dea871a5b97073fe99a2ee029445afb2c89
Michał Gruca <pucek9@gmail.com>
2021-04-28 15:33:45 +0200

    add deprecation for usage AnonymousConsentLaunchDialogService

--- projects/storefrontlib/src/cms-components/anonymous-consent-management/banner/anonymous-consent-management-banner.component.ts
+++ projects/storefrontlib/src/cms-components/anonymous-consent-management/banner/anonymous-consent-management-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, ViewContainerRef } from '@angular/core';
 import { AnonymousConsentsService } from '@spartacus/core';
+import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
 import { Observable, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AnonymousConsentLaunchDialogService } from '../anonymous-consent-launch-dialog.service';
@@ -13,20 +14,55 @@ export class AnonymousConsentManagementBannerComponent implements OnDestroy {
 
   bannerVisible$: Observable<boolean> = this.anonymousConsentsService.isBannerVisible();
 
+  // TODO(#12167): make launchDialogService a required dependency instead of anonymousConsentLaunchDialogService and remove deprecated constructor
+  /**
+   * Default constructor
+   *
+   * @param {AnonymousConsentsService} anonymousConsentsService
+   * @param {ViewContainerRef} vcr
+   * @param {LaunchDialogService} launchDialogService
+   */
+  constructor(
+    anonymousConsentsService: AnonymousConsentsService,
+    vcr: ViewContainerRef,
+    launchDialogService: LaunchDialogService
+  );
+
+  /**
+   * @deprecated since 3.3
+   */
+  constructor(
+    anonymousConsentsService: AnonymousConsentsService,
+    anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService,
+    vcr: ViewContainerRef
+  );
   constructor(
     protected anonymousConsentsService: AnonymousConsentsService,
     protected anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService,
-    protected vcr: ViewContainerRef
+    protected vcr: ViewContainerRef,
+    protected launchDialogService?: LaunchDialogService
   ) {}
 
   viewDetails(): void {
     this.hideBanner();
-    const dialog = this.anonymousConsentLaunchDialogService.openDialog(
-      null,
-      this.vcr
-    );
-    if (dialog) {
-      this.subscriptions.add(dialog.subscribe());
+    // TODO(#12167): use launchDialogService only
+    if (this.launchDialogService) {
+      const dialog = this.launchDialogService.openDialog(
+        LAUNCH_CALLER.ANONYMOUS_CONSENT,
+        null,
+        this.vcr
+      );
+      if (dialog) {
+        this.subscriptions.add(dialog.subscribe());
+      }
+    } else {
+      const dialog = this.anonymousConsentLaunchDialogService.openDialog(
+        null,
+        this.vcr
+      );
+      if (dialog) {
+        this.subscriptions.add(dialog.subscribe());
+      }
     }
   }
 
--- projects/storefrontlib/src/cms-components/anonymous-consent-management/open-dialog/anonymous-consent-open-dialog.component.ts
+++ projects/storefrontlib/src/cms-components/anonymous-consent-management/open-dialog/anonymous-consent-open-dialog.component.ts
@@ -4,6 +4,7 @@ import {
   ViewChild,
   ViewContainerRef,
 } from '@angular/core';
+import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
 import { take } from 'rxjs/operators';
 import { AnonymousConsentLaunchDialogService } from '../anonymous-consent-launch-dialog.service';
 
@@ -14,18 +15,46 @@ import { AnonymousConsentLaunchDialogService } from '../anonymous-consent-launch
 export class AnonymousConsentOpenDialogComponent {
   @ViewChild('open') openElement: ElementRef;
 
+  // TODO(#12167): make launchDialogService a required dependency instead of anonymousConsentLaunchDialogService and remove deprecated constructor
+  /**
+   * Default constructor
+   *
+   * @param {ViewContainerRef} vcr
+   * @param {LaunchDialogService} launchDialogService
+   */
+  constructor(vcr: ViewContainerRef, launchDialogService: LaunchDialogService);
+  /**
+   * @deprecated since 3.3
+   */
+  constructor(
+    vcr: ViewContainerRef,
+    anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService
+  );
   constructor(
     protected vcr: ViewContainerRef,
-    protected anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService
+    protected anonymousConsentLaunchDialogService: AnonymousConsentLaunchDialogService,
+    protected launchDialogService?: LaunchDialogService
   ) {}
 
   openDialog(): void {
-    const dialog = this.anonymousConsentLaunchDialogService.openDialog(
-      this.openElement,
-      this.vcr
-    );
-    if (dialog) {
-      dialog.pipe(take(1)).subscribe();
+    // TODO(#12167): use launchDialogService only
+    if (this.launchDialogService) {
+      const dialog = this.launchDialogService.openDialog(
+        LAUNCH_CALLER.ANONYMOUS_CONSENT,
+        this.openElement,
+        this.vcr
+      );
+      if (dialog) {
+        dialog.pipe(take(1)).subscribe();
+      }
+    } else {
+      const dialog = this.anonymousConsentLaunchDialogService.openDialog(
+        this.openElement,
+        this.vcr
+      );
+      if (dialog) {
+        dialog.pipe(take(1)).subscribe();
+      }
     }
   }
 }
