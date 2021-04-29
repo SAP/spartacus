cbf5be059836838186ba1a25d86e2000adfad4f4
Patrick Labadie <patrick.labadie@sap.com>
2021-04-29 11:27:36 -0400

    GH-10821: Move the promotions component out of checkout.

--- projects/storefrontlib/src/cms-components/misc/index.ts
+++ projects/storefrontlib/src/cms-components/misc/index.ts
@@ -1,4 +1,5 @@
 export * from './global-message/index';
 export * from './icon/index';
+export * from './promotions/index';
 export * from './qualtrics/index';
 export * from './site-context-selector/index';
