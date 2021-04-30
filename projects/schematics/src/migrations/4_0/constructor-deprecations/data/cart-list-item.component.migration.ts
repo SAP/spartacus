0eb33488fd1057d9cb1c06cbc8069ccb15411064
Caine Rotherham <rotherham.c@gmail.com>
2021-04-30 07:57:33 +0200

    Update projects/schematics/src/migrations/4_0/constructor-deprecations/data/cart-list-item.component.migration.ts
    Co-authored-by: Marcin Lasak <lasakmarcin90@gmail.com>

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/cart-list-item.component.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/cart-list-item.component.migration.ts
@@ -36,57 +36,6 @@ export const CART_LIST_ITEM_COMPONENT_MIGRATION_V1: ConstructorDeprecation = {
   ],
 };
 
-export const CART_LIST_ITEM_COMPONENT_MIGRATION_V2: ConstructorDeprecation = {
-  // projects/storefrontlib/src/cms-components/cart/cart-shared/cart-item-list/cart-item-list.component.ts
-  class: CART_ITEM_LIST_COMPONENT,
-  importPath: SPARTACUS_STOREFRONTLIB,
-  deprecatedParams: [
-    {
-      className: ACTIVE_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-    {
-      className: SELECTIVE_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-    {
-      className: USER_ID_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-  ],
-  addParams: [
-    {
-      className: MULTI_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-  ],
-};
-
-export const CART_LIST_ITEM_COMPONENT_MIGRATION_V3: ConstructorDeprecation = {
-  // projects/storefrontlib/src/cms-components/cart/cart-shared/cart-item-list/cart-item-list.component.ts
-  class: CART_ITEM_LIST_COMPONENT,
-  importPath: SPARTACUS_STOREFRONTLIB,
-  deprecatedParams: [
-    {
-      className: ACTIVE_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-    {
-      className: SELECTIVE_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-    {
-      className: MULTI_CART_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-  ],
-  addParams: [
-    {
-      className: USER_ID_SERVICE,
-      importPath: SPARTACUS_CORE,
-    },
-  ],
-};
 
 export const CART_LIST_ITEM_COMPONENT_MIGRATION_V4: ConstructorDeprecation = {
   // projects/storefrontlib/src/cms-components/cart/cart-shared/cart-item-list/cart-item-list.component.ts
