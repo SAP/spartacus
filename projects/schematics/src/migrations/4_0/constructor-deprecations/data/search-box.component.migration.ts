505fd987dcfde837b2f463a50b93e7722976c3aa
Michal Dydo <mdydo@divante.pl>
2021-05-04 09:32:11 +0200

    Add deprecated params

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/search-box.component.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/search-box.component.migration.ts
@@ -1,8 +1,11 @@
 import {
+  CMS_COMPONENT_DATA_CLASS,
   ROUTING_SERVICE,
   SEARCH_BOX_COMPONENT,
+  SEARCH_BOX_COMPONENT_SERVICE,
   SPARTACUS_CORE,
   SPARTACUS_STOREFRONTLIB,
+  WINDOW_REF,
 } from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
@@ -10,7 +13,14 @@ export const SEARCH_BOX_COMPONENT_MIGRATION: ConstructorDeprecation = {
   // storefrontlibs\scr\cms-components\navigation\search-box\search-box.component.ts
   class: SEARCH_BOX_COMPONENT,
   importPath: SPARTACUS_STOREFRONTLIB,
-  deprecatedParams: [],
+  deprecatedParams: [
+    { className: SEARCH_BOX_COMPONENT_SERVICE, importPath: SPARTACUS_CORE },
+    {
+      className: CMS_COMPONENT_DATA_CLASS,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    { className: WINDOW_REF, importPath: SPARTACUS_CORE },
+  ],
   addParams: [
     {
       className: ROUTING_SERVICE,
