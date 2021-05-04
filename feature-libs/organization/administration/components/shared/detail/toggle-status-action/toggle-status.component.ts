40d25087c6676771f6c3879414b35e58bad287bd
wangzixi-diablo <cle.ee@163.com>
2021-05-04 14:32:40 +0800

    deprecation cleanup done for deactivate in toggle component

--- feature-libs/organization/administration/components/shared/detail/toggle-status-action/toggle-status.component.ts
+++ feature-libs/organization/administration/components/shared/detail/toggle-status-action/toggle-status.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnDestroy } from '@angular/core';
-import { FeatureConfigService } from '@spartacus/core';
 import { LoadStatus } from '@spartacus/organization/administration/core';
 import { Observable, Subject, Subscription } from 'rxjs';
 import { filter, first, take } from 'rxjs/operators';
@@ -57,22 +56,9 @@ export class ToggleStatusComponent<T extends BaseItem> implements OnDestroy {
     messageService: MessageService<ConfirmationMessageData>
   );
 
-  /**
-   * @deprecated since version 3.2
-   * Use constructor( protected itemService: ItemService<T>, protected messageService: MessageService<ConfirmationMessageData>, protected featureConfigService: FeatureConfigService) {} instead
-   */
-  // TODO(#11387): Remove deprecated constructors
-  constructor(
-    itemService: ItemService<T>,
-    messageService: MessageService<ConfirmationMessageData>,
-    // eslint-disable-next-line @typescript-eslint/unified-signatures
-    featureConfigService: FeatureConfigService
-  );
-
   constructor(
     protected itemService: ItemService<T>,
-    protected messageService: MessageService<ConfirmationMessageData>,
-    protected featureConfigService?: FeatureConfigService
+    protected messageService: MessageService<ConfirmationMessageData>
   ) {}
 
   toggle(item: T) {
@@ -83,12 +69,7 @@ export class ToggleStatusComponent<T extends BaseItem> implements OnDestroy {
       if (!this.confirmation) {
         this.confirmation = this.messageService.add({
           message: {
-            key: `${this.i18nRoot}${
-              //TODO(#11387): Replace deactivateBody with deactivate
-              this.featureConfigService?.isLevel('3.2')
-                ? '.messages.deactivateBody'
-                : '.messages.deactivate'
-            }`,
+            key: this.i18nRoot + '.messages.deactivate',
             params: { item },
           },
           messageTitle: {
