b6b995dfe473a43ef3dc733b4a83b8131b6365c1
Patrick Labadie <patrick.labadie@sap.com>
2021-05-04 15:45:44 -0400

    GH-12185: Move the event class to checkout/root

--- feature-libs/checkout/core/events/index.ts
+++ feature-libs/checkout/core/events/index.ts
@@ -1,3 +1,2 @@
 export * from './checkout-event.builder';
 export * from './checkout-event.module';
-export * from './checkout.events';
--- feature-libs/checkout/root/index.ts
+++ feature-libs/checkout/root/index.ts
@@ -1,3 +1,4 @@
 export * from './checkout-root.module';
 export * from './config/index';
+export * from './events/index';
 export * from './model/index';
