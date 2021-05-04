19e467f36704d5604f46bc63b73c3d433b1ecd16
Patrick Labadie <patrick.labadie@sap.com>
2021-05-04 08:02:11 -0400

    GH-12184: Enable lazy loading

--- projects/storefrontapp/src/app/spartacus/features/checkout-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/checkout-feature.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CheckoutModule } from '@spartacus/checkout';
 import {
   checkoutTranslationChunksConfig,
   checkoutTranslations,
 } from '@spartacus/checkout/assets';
-import { CheckoutRootModule } from '@spartacus/checkout/root';
+import { CheckoutRootModule, CHECKOUT_FEATURE } from '@spartacus/checkout/root';
 import { provideConfig } from '@spartacus/core';
 
 @NgModule({
-  imports: [CheckoutRootModule, CheckoutModule],
+  imports: [CheckoutRootModule],
   providers: [
     provideConfig({
-      // featureModules: {
-      //   [CHECKOUT_FEATURE]: {
-      //     module: () =>
-      //       import('@spartacus/checkout').then((m) => m.CheckoutModule),
-      //   },
-      // },
+      featureModules: {
+        [CHECKOUT_FEATURE]: {
+          module: () =>
+            import('@spartacus/checkout').then((m) => m.CheckoutModule),
+        },
+      },
       i18n: {
         resources: checkoutTranslations,
         chunks: checkoutTranslationChunksConfig,
