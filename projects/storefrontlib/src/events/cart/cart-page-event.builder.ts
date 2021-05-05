81644017de79038dbdf897bef9e44107847e5c13
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-05-05 20:14:12 +0200

    Revert "chore: TMS migration (#12249)" (#12306)
    This reverts commit d6aebf535d53902f94960b569fcd648fa5960acc.

--- projects/storefrontlib/src/events/cart/cart-page-event.builder.ts
+++ projects/storefrontlib/src/events/cart/cart-page-event.builder.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { createFrom, EventService } from '@spartacus/core';
+import { ActionsSubject } from '@ngrx/store';
+import {
+  createFrom,
+  EventService,
+  FeatureConfigService,
+} from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { NavigationEvent } from '../navigation/navigation.event';
+import { PageEvent } from '../page/page.events';
 import { CartPageEvent } from './cart-page.events';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartPageEventBuilder {
-  constructor(protected eventService: EventService) {
+  constructor(
+    // TODO: #10896 - remove this
+    /** @deprecated @since 3.1 - this will be removed in 4.0 */
+    protected actions: ActionsSubject,
+    protected eventService: EventService,
+    // TODO: #10896 - remove this
+    /** @deprecated @since 3.1 - this will be removed in 4.0 */ protected featureConfigService?: FeatureConfigService
+  ) {
     this.register();
   }
 
@@ -22,9 +35,25 @@ export class CartPageEventBuilder {
       filter((navigationEvent) => navigationEvent.semanticRoute === 'cart'),
       map((navigationEvent) =>
         createFrom(CartPageEvent, {
-          navigation: navigationEvent,
+          ...this.createDeprecatedPageEvent(navigationEvent),
+          navigation: {
+            ...navigationEvent,
+          },
         })
       )
     );
+  }
+
+  // TODO: #10896 - remove this method
+  private createDeprecatedPageEvent(
+    navigationEvent: NavigationEvent
+  ): PageEvent | undefined {
+    if (
+      !this.featureConfigService ||
+      this.featureConfigService.isLevel('!3.1')
+    ) {
+      return { ...navigationEvent };
+    }
+    return undefined;
   }
 }
