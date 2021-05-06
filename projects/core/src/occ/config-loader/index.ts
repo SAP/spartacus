e6a6bcb0216501229d0e3eabe4b7311b635dfdfa
Thambirajah <renuchan.thambirajah@sap.com>
2021-05-06 13:06:55 -0400

    Removed all config-loader classes.

--- projects/core/src/occ/config-loader/index.ts
+++
@@ -1,5 +0,0 @@
-export { OccConfigLoaderModule } from './occ-config-loader.module';
-export * from './occ-config-loader.service';
-export * from './occ-loaded-config';
-export * from './occ-loaded-config-converter';
-export * from './occ-sites-config-loader';
