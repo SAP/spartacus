4ededdbcea5154763fa83de89f15c5a5e932fc98
Patrick Labadie <patrick.labadie@sap.com>
2021-05-07 09:38:59 -0400

    GH-12183: Fix schematics

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/login-register.component.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/login-register.component.migration.ts
@@ -1,4 +1,5 @@
 import {
+  ACTIVATED_ROUTE,
   CHECKOUT_CONFIG_SERVICE,
   LOGIN_REGISTER_COMPONENT,
   SPARTACUS_CORE,
@@ -10,7 +11,10 @@ export const LOGIN_REGISTER_COMPONENT_MIGRATION: ConstructorDeprecation = {
   // feature-libs/user/account/components/login-register/login-register.component.ts
   class: LOGIN_REGISTER_COMPONENT,
   importPath: SPARTACUS_USER,
-  deprecatedParams: [],
+  deprecatedParams: [
+    { className: CHECKOUT_CONFIG_SERVICE, importPath: SPARTACUS_CORE },
+    { className: CHECKOUT_CONFIG_SERVICE, importPath: ACTIVATED_ROUTE },
+  ],
   removeParams: [
     { className: CHECKOUT_CONFIG_SERVICE, importPath: SPARTACUS_CORE },
   ],
