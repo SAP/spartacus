0d444f60b4c84f85af8e84ec61220c5761ddd909
Michał Gruca <pucek9@gmail.com>
2021-05-10 01:04:27 +0200

    add removed-public-api mechanism

--- projects/schematics/src/migrations/4_0/removed-public-api-deprecations/removed-public-api-deprecation.ts
+++ projects/schematics/src/migrations/4_0/removed-public-api-deprecations/removed-public-api-deprecation.ts
@@ -1,8 +1,30 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { DeprecatedNode } from '../../../shared/utils/file-utils';
 import { removedPublicApiDeprecation } from '../../mechanism/removed-public-api-deprecations/removed-public-api-deprecation';
+import {
+  ANONYMOUS_CONSENT_LAUNCH_DIALOG_SERVICE,
+  REPLENISHMENT_ORDER_CANCELLATION_LAUNCH_DIALOG_SERVICE,
+  SAVED_CART_FORM_LAUNCH_DIALOG_SERVICE,
+  SPARTACUS_CART_SAVED_CART_COMPONENTS,
+  SPARTACUS_STOREFRONTLIB,
+} from '@spartacus/schematics';
 
 export const REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
+  {
+    node: SAVED_CART_FORM_LAUNCH_DIALOG_SERVICE,
+    importPath: SPARTACUS_CART_SAVED_CART_COMPONENTS,
+    comment: `'${SAVED_CART_FORM_LAUNCH_DIALOG_SERVICE}' has been removed.' 'openDialog' method has been moved to 'LaunchDialogService'.`,
+  },
+  {
+    node: ANONYMOUS_CONSENT_LAUNCH_DIALOG_SERVICE,
+    importPath: SPARTACUS_STOREFRONTLIB,
+    comment: `'${ANONYMOUS_CONSENT_LAUNCH_DIALOG_SERVICE}' has been removed.' 'openDialog' method has been moved to 'LaunchDialogService'.`,
+  },
+  {
+    node: REPLENISHMENT_ORDER_CANCELLATION_LAUNCH_DIALOG_SERVICE,
+    importPath: SPARTACUS_STOREFRONTLIB,
+    comment: `'${REPLENISHMENT_ORDER_CANCELLATION_LAUNCH_DIALOG_SERVICE}' has been removed.' 'openDialog' method has been moved to 'LaunchDialogService'.`,
+  },
   // uncomment product variants deprecation on 4.0 migration works (#11391)
   // // projects/storefrontlib/src/cms-components/product/product-variants/product-variants.component.ts
   // {
