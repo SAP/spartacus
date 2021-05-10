e27194170aa1ca5f24a1b2ad0a1e3fa2d5ba7dc4
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-05-10 12:49:31 +0200

    exporting cds components and services

--- integration-libs/cds/src/profiletag/index.ts
+++ integration-libs/cds/src/profiletag/index.ts
@@ -1,4 +1,7 @@
+export * from './adapters/index';
 export * from './cms-components/index';
+export * from './http-interceptors/index';
 export * from './model/index';
 export * from './profile-tag.module';
 export * from './services/index';
+export * from './tracking/index';
