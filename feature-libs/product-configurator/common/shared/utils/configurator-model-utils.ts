b0b8bcc270b63eb0b817b0497f4cd980fff2ed61
d026976 <christoph.hinssen@sap.com>
2021-05-11 08:54:00 +0200

    GH-12288 Fix strict mode issues in connector

--- feature-libs/product-configurator/common/shared/utils/configurator-model-utils.ts
+++ feature-libs/product-configurator/common/shared/utils/configurator-model-utils.ts
@@ -33,7 +33,7 @@ export class ConfiguratorModelUtils {
    * @returns Initial owner
    */
   static createInitialOwner(): CommonConfigurator.Owner {
-    return { key: 'INITIAL' };
+    return { key: 'INITIAL', configuratorType: 'INITIAL' };
   }
 
   /**
