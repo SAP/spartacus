0cacdf244af632de13de0d2bf8ead5c6dc710ce5
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-05-11 08:06:16 +0200

    cr

--- integration-libs/cds/src/profiletag/index.ts
+++ integration-libs/cds/src/profiletag/index.ts
@@ -1,5 +1,6 @@
 export * from './adapters/index';
 export * from './cms-components/index';
+export * from './connectors/index';
 export * from './http-interceptors/index';
 export * from './model/index';
 export * from './profile-tag.module';
