64b706867ed4c4002b40c828c8c1bc8aa16b756b
dunqan <dunqan@w9.pl>
2021-05-12 08:55:22 +0200

    remove feature modules service

--- projects/storefrontlib/src/cms-structure/services/index.ts
+++ projects/storefrontlib/src/cms-structure/services/index.ts
@@ -1,6 +1,5 @@
 export * from './cms-guards.service';
 export * from './cms-i18n.service';
 export * from './cms-components.service';
-export * from './feature-modules.service';
 export * from './cms-features.service';
 export * from './cms-routes.service';
