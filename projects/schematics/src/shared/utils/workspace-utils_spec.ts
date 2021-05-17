3a783cec8621e42a5c16650e718cb800e31c3eee
Michał Gruca <pucek9@gmail.com>
2021-05-17 13:09:51 +0200

    add few tests

--- projects/schematics/src/shared/utils/workspace-utils_spec.ts
+++ projects/schematics/src/shared/utils/workspace-utils_spec.ts
@@ -1,3 +1,4 @@
+import { SchematicsException } from '@angular-devkit/schematics';
 import {
   SchematicTestRunner,
   UnitTestTree,
@@ -15,10 +16,17 @@ import * as path from 'path';
 import { Schema as SpartacusOptions } from '../../add-spartacus/schema';
 import {
   buildDefaultPath,
+  getDefaultProjectNameFromWorkspace,
+  // getProject,
   getProjectFromWorkspace,
+  getProjectTargets,
   getSourceRoot,
   getWorkspace,
+  isWorkspaceProject,
+  isWorkspaceSchema,
+  validateSpartacusInstallation,
 } from './workspace-utils';
+import { SPARTACUS_CORE } from '../../shared/constants';
 
 const collectionPath = path.join(__dirname, '../../collection.json');
 const schematicRunner = new SchematicTestRunner('schematics', collectionPath);
@@ -92,6 +100,34 @@ describe('Workspace utils', () => {
     });
   });
 
+  describe('getDefaultProjectNameFromWorkspace', () => {
+    it('should return default project name from workspace', () => {
+      const workspaceName = getDefaultProjectNameFromWorkspace(appTree);
+      expect(workspaceName).toEqual('schematics-test');
+    });
+  });
+
+  describe('getProjectTargets', () => {
+    it('should throw an error', () => {
+      expect(() =>
+        getProjectTargets({
+          projectType: ProjectType.Application,
+          root: 'root',
+          prefix: 'prefix',
+        })
+      ).toThrowError(new Error('Project target not found.'));
+    });
+
+    // it('should return project targets', () => {
+    //   const projectTargets = getProjectTargets({
+    //     projectType: ProjectType.Application,
+    //     root: 'root',
+    //     prefix: 'prefix',
+    //   });
+    //   expect(projectTargets).toEqual({});
+    // });
+  });
+
   describe('buildDefaultPath', () => {
     let project: WorkspaceProject;
     beforeEach(() => {
@@ -117,6 +153,76 @@ describe('Workspace utils', () => {
       project = { ...project, sourceRoot: 'foo/bar/custom' };
       const result = buildDefaultPath(project);
       expect(result).toEqual('/foo/bar/custom/app');
+    });
+  });
+
+  describe('getProject', () => {
+    it('should return project', () => {
+      // const project = getProject(
+      //   {
+      //     projectType: ProjectType.Application,
+      //     root: {},
+      //     prefix: 'prefix',
+      //   },
+      //   'name'
+      // );
+      // expect(project).toEqual({});
+    });
+  });
+
+  describe('isWorkspaceSchema', () => {
+    it('should return boolean', () => {
+      const project = isWorkspaceSchema({
+        projectType: ProjectType.Application,
+        root: 'root',
+        prefix: 'prefix',
+      });
+      expect(project).toEqual(false);
+    });
+  });
+
+  describe('isWorkspaceProject', () => {
+    it('should return true for a compatible type', () => {
+      expect(
+        isWorkspaceProject({
+          projectType: ProjectType.Application,
+        })
+      ).toEqual(true);
+      expect(
+        isWorkspaceProject({
+          projectType: ProjectType.Library,
+        })
+      ).toEqual(true);
+    });
+
+    it('should return false for missing type', () => {
+      expect(
+        isWorkspaceProject({
+          otherProperty: 'otherProperty',
+        })
+      ).toEqual(false);
+      expect(isWorkspaceProject(null)).toEqual(false);
+    });
+  });
+
+  describe('validateSpartacusInstallation', () => {
+    it('should throw an error if key is missing', () => {
+      expect(() =>
+        validateSpartacusInstallation({ dependencies: {} })
+      ).toThrowError(
+        new SchematicsException(
+          `Spartacus is not detected. Please first install Spartacus by running: 'ng add @spartacus/schematics'.
+    To see more options, please check our documentation.`
+        )
+      );
+    });
+
+    it('should not throw an error if key exists', () => {
+      expect(() =>
+        validateSpartacusInstallation({
+          dependencies: { [SPARTACUS_CORE]: '/..' },
+        })
+      ).not.toThrowError();
     });
   });
 });
