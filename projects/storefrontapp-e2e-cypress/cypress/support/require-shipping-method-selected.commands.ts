d5a9cea1655415c0abc9ee4d6336fb877dedcdf2
Weizheng Gao <weizheng.gao@sap.com>
2021-05-18 11:12:39 -0300

    feature/GH-12270:Remove hard-coded delivery mode Id in e2e tests

--- projects/storefrontapp-e2e-cypress/cypress/support/require-shipping-method-selected.commands.ts
+++ projects/storefrontapp-e2e-cypress/cypress/support/require-shipping-method-selected.commands.ts
@@ -1,9 +1,6 @@
-import { delivery } from '../sample-data/checkout-flow';
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      /**
+declare namespace Cypress {
+  interface Chainable {
+    /**
        * Make sure you have shipping method selected. Returns shipping method object.
        *
        * @memberof Cypress.Chainable
@@ -13,21 +10,34 @@ declare global {
         cy.requireShippingMethodSelected(auth);
         ```
        */
-      requireShippingMethodSelected: (auth: {}) => Cypress.Chainable<{}>;
-    }
+    requireShippingMethodSelected: (
+      auth: {},
+      cartId?: string
+    ) => Cypress.Chainable<{}>;
   }
 }
-Cypress.Commands.add('requireShippingMethodSelected', (auth) => {
-  function setShippingMethod() {
+
+Cypress.Commands.add('requireShippingMethodSelected', (auth, cartId) => {
+  const cartQueryValue = cartId || 'current';
+
+  function getDeliveryModes() {
+    return cy.request({
+      method: 'GET',
+      url: `${Cypress.env('API_URL')}${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+        'BASE_SITE'
+      )}/users/current/carts/${cartQueryValue}/deliverymodes`,
+      headers: {
+        Authorization: `bearer ${auth.access_token}`,
+      },
+    });
+  }
+
+  function setShippingMethod(deliveryMode) {
     return cy.request({
       method: 'PUT',
-      url: `${Cypress.env('API_URL')}/${Cypress.env(
-        'OCC_PREFIX'
-      )}/${Cypress.env(
+      url: `${Cypress.env('API_URL')}${Cypress.env('OCC_PREFIX')}/${Cypress.env(
         'BASE_SITE'
-      )}/users/current/carts/current/deliverymode?deliveryModeId=${
-        delivery.mode
-      }`,
+      )}/users/current/carts/${cartQueryValue}/deliverymode?deliveryModeId=${deliveryMode}`,
       form: false,
       headers: {
         Authorization: `bearer ${auth.access_token}`,
@@ -36,5 +46,9 @@ Cypress.Commands.add('requireShippingMethodSelected', (auth) => {
   }
 
   cy.server();
-  setShippingMethod().then((resp) => cy.wrap(resp));
+  getDeliveryModes().then((resp) =>
+    setShippingMethod(resp.body.deliveryModes[0].code).then((resp) =>
+      cy.wrap(resp)
+    )
+  );
 });
