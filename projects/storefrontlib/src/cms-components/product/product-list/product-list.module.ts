453a07dabaedf1d26a1ec319b4fd289e3489e8d4
Patryk Smul <psmul@divante.pl>
2021-05-19 11:42:51 +0200

    migration for product variants

--- projects/storefrontlib/src/cms-components/product/product-list/product-list.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-list/product-list.module.ts
@@ -22,7 +22,6 @@ import {
 import { AddToCartModule } from '../../cart/index';
 import { IconModule } from '../../misc/icon/index';
 import { defaultScrollConfig } from '../config/default-scroll-config';
-import { ProductVariantsModule } from '../product-variants/product-variants.module';
 import { ProductListComponent } from './container/product-list.component';
 import { ProductScrollComponent } from './container/product-scroll/product-scroll.component';
 import { ProductFacetNavigationComponent } from './product-facet-navigation/product-facet-navigation.component';
@@ -45,7 +44,6 @@ import { ProductViewComponent } from './product-view/product-view.component';
     SpinnerModule,
     InfiniteScrollModule,
     ViewConfigModule,
-    ProductVariantsModule,
     FeaturesConfigModule,
     OutletModule,
   ],
