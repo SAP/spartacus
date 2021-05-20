f66639bf5ed47a8f491fc039bed5df002c70f0f5
Saad Mohiuddin <saad.mohiuddin@sap.com>
2021-05-20 09:31:03 -0500

    add findStoreEntitiesById to state

--- feature-libs/storefinder/core/store/reducers/find-stores.reducer.ts
+++ feature-libs/storefinder/core/store/reducers/find-stores.reducer.ts
@@ -3,6 +3,7 @@ import { FindStoresState } from '../store-finder-state';
 
 export const initialState: FindStoresState = {
   findStoresEntities: {},
+  findStoreEntitiesById: {},
 };
 
 export function findStoresReducer(
@@ -10,11 +11,15 @@ export function findStoresReducer(
   action: StoreFinderActions.FindStoresAction
 ): FindStoresState {
   switch (action.type) {
-    case StoreFinderActions.FIND_STORES_SUCCESS:
-    case StoreFinderActions.FIND_STORE_BY_ID_SUCCESS: {
+    case StoreFinderActions.FIND_STORES_SUCCESS: {
       const findStoresEntities = action.payload;
 
       return { ...state, findStoresEntities };
+    }
+    case StoreFinderActions.FIND_STORE_BY_ID_SUCCESS: {
+      const findStoreEntitiesById = action.payload;
+
+      return { ...state, findStoreEntitiesById };
     }
   }
 
