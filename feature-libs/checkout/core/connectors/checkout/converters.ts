acae36e878be5214a20cb934dba2c77d75070135
Patrick Labadie <patrick.labadie@sap.com>
2021-05-21 12:27:25 -0400

    GH-12225: Leave POINT_OF_SERVICE_NORMALIZER ini @spartacus/core

--- feature-libs/checkout/core/connectors/checkout/converters.ts
+++ feature-libs/checkout/core/connectors/checkout/converters.ts
@@ -1,12 +1,6 @@
 import { InjectionToken } from '@angular/core';
-import { Order } from '@spartacus/core';
-import { PointOfService } from '@spartacus/core';
-import { Converter } from '@spartacus/core';
+import { Converter, Order } from '@spartacus/core';
 
 export const ORDER_NORMALIZER = new InjectionToken<Converter<any, Order>>(
   'OrderNormalizer'
 );
-
-export const POINT_OF_SERVICE_NORMALIZER = new InjectionToken<
-  Converter<any, PointOfService>
->('PointOfServiceNormalizer');
