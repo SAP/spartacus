9990ca1d54831f73bccf970bd83e940668b1a9f6
Michal Dydo <mdydo@divante.pl>
2021-05-25 09:37:12 +0200

    Fix for migration file

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/logout.guard.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/logout.guard.migration.ts
@@ -1,6 +1,12 @@
 import {
+  ANGULAR_ROUTER,
   AUTH_REDIRECT_SERVICE,
+  AUTH_SERVICE,
+  CMS_SERVICE,
   LOGOUT_GUARD,
+  PROTECTED_ROUTES_SERVICE,
+  ROUTER,
+  SEMANTIC_PATH_SERVICE,
   SPARTACUS_CORE,
   SPARTACUS_STOREFRONTLIB,
 } from '../../../../shared/constants';
@@ -10,6 +16,28 @@ export const LOGOUT_GUARD_CONSTRUCTOR_MIGRATION: ConstructorDeprecation = {
   // projects/storefrontlib/src/cms-components/user/logout/logout.guard.ts
   class: LOGOUT_GUARD,
   importPath: SPARTACUS_STOREFRONTLIB,
-  deprecatedParams: [],
+  deprecatedParams: [
+    {
+      className: AUTH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: CMS_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: SEMANTIC_PATH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: PROTECTED_ROUTES_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ROUTER,
+      importPath: ANGULAR_ROUTER,
+    },
+    { className: AUTH_REDIRECT_SERVICE, importPath: SPARTACUS_CORE },
+  ],
   addParams: [{ className: AUTH_REDIRECT_SERVICE, importPath: SPARTACUS_CORE }],
 };
