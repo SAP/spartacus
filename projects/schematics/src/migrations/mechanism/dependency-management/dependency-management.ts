6109e80a0a144b9fc80b1e56581ca1a21e05dc5c
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-05-26 17:04:30 +0200

    adding libs deps

--- projects/schematics/src/migrations/mechanism/dependency-management/dependency-management.ts
+++ projects/schematics/src/migrations/mechanism/dependency-management/dependency-management.ts
@@ -4,17 +4,59 @@ import {
   SchematicContext,
   Tree,
 } from '@angular-devkit/schematics';
+import { NodeDependency } from '@schematics/angular/utility/dependencies';
+import collectedDependencies from '../../../dependencies.json';
+import { SPARTACUS_SCOPE } from '../../../shared/constants';
 import {
   addPackageJsonDependencies,
   installPackageJsonDependencies,
 } from '../../../shared/utils/lib-utils';
-import { prepare3rdPartyDependencies } from '../../../shared/utils/package-utils';
+import {
+  CORE_SPARTACUS_SCOPES,
+  createDependencies,
+  prepare3rdPartyDependencies,
+  readPackageJson,
+} from '../../../shared/utils/package-utils';
 
 export function migrate(): Rule {
-  return (_tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, _context: SchematicContext) => {
+    const spartacusDependencies = collectSpartacusLibraryDependencies(
+      readPackageJson(tree)
+    );
+
+    const thirdPartyDependencies = prepare3rdPartyDependencies();
+    const libraryDependencies = createSpartacusLibraryDependencies(
+      spartacusDependencies
+    );
+
+    const dependencies = thirdPartyDependencies.concat(libraryDependencies);
     return chain([
-      addPackageJsonDependencies(prepare3rdPartyDependencies()),
+      addPackageJsonDependencies(dependencies),
       installPackageJsonDependencies(),
     ]);
   };
+}
+
+function collectSpartacusLibraryDependencies(packageJson: any): string[] {
+  const dependencies = packageJson.dependencies as Record<string, string>;
+  return Object.keys(dependencies)
+    .filter((d) => d.startsWith(SPARTACUS_SCOPE))
+    .filter((d) => !CORE_SPARTACUS_SCOPES.includes(d));
+}
+
+function createSpartacusLibraryDependencies(
+  spartacusDependencies: string[]
+): NodeDependency[] {
+  const dependenciesToAdd: NodeDependency[] = [];
+
+  for (const libraryName of spartacusDependencies) {
+    const spartacusLibrary = (collectedDependencies as Record<
+      string,
+      Record<string, string>
+    >)[libraryName];
+
+    dependenciesToAdd.push(...createDependencies(spartacusLibrary));
+  }
+
+  return dependenciesToAdd;
 }
