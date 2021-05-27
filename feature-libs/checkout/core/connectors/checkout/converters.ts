8b9a974925594383e744b7f98202c424ab355a3e
pla <plabadie@users.noreply.github.com>
2021-05-27 14:31:19 -0400

    GH-12225: Leave POINT_OF_SERVICE_NORMALIZER ini @spartacus/core (#12531)

--- feature-libs/checkout/core/connectors/checkout/converters.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { Converter, PointOfService } from '@spartacus/core';
-
-export const POINT_OF_SERVICE_NORMALIZER = new InjectionToken<
-  Converter<any, PointOfService>
->('PointOfServiceNormalizer');
