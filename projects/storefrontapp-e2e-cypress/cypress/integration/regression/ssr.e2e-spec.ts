cabab4271565fe8283bda761cac45ee3f74fb610
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-06-01 11:14:16 -0400

    Initial SSR e2e test

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/ssr.e2e-spec.ts
@@ -0,0 +1,36 @@
+describe('SSR', () => {
+  const plpUrl = '/Brands/Sony/c/brand_5';
+  const pdpUrl = '/product/3965240/np-fv-70';
+
+  beforeEach(() => {
+    cy.intercept({ method: 'GET', url: '*.js' }, {});
+  });
+
+  it('should render homepage', () => {
+    cy.visit('/');
+    cy.title().should('not.be.empty');
+    cy.get('cx-page-slot.SiteLogo').should('be.visible');
+    cy.get('head meta[name="robots"]')
+      .should('have.attr', 'content')
+      .and('contains', 'INDEX')
+      .and('contains', 'FOLLOW');
+  });
+
+  it('should render PLP', () => {
+    cy.visit(plpUrl);
+    cy.title().should('not.be.empty');
+    cy.get('head meta[name="robots"]')
+      .should('have.attr', 'content')
+      .and('contains', 'INDEX')
+      .and('contains', 'FOLLOW');
+  });
+
+  it('should render PDP', () => {
+    cy.visit(pdpUrl);
+    cy.title().should('not.be.empty');
+    cy.get('head meta[name="robots"]')
+      .should('have.attr', 'content')
+      .and('contains', 'INDEX')
+      .and('contains', 'FOLLOW');
+  });
+});
