daf8b9c089f5bc3456b42a526c58669418e7ae7c
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-06-01 17:33:03 -0400

    adding homepage header assertions

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/ssr/pages.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/ssr/pages.e2e-spec.ts
@@ -9,11 +9,15 @@ describe('SSR', () => {
   it('should render homepage', () => {
     cy.visit('/');
     cy.title().should('not.be.empty');
-    cy.get('cx-page-slot.SiteLogo').should('be.visible');
     cy.get('head meta[name="robots"]')
       .should('have.attr', 'content')
       .and('contains', 'INDEX')
       .and('contains', 'FOLLOW');
+    cy.get('.header').within(() => {
+      cy.get('cx-page-slot.SiteLogo').should('be.visible');
+      cy.get('.searchbox').should('be.visible');
+      cy.get('cx-mini-cart').should('be.visible');
+    });
   });
 
   it('should render PLP', () => {
