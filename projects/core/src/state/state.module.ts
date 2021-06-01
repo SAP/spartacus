3af6a0ea9d2bda7ae66b7b658c87bce6e61b974b
Marcin Lasak <lasakmarcin90@gmail.com>
2021-06-01 16:23:59 +0200

    refactor: Removal of storageSync mechanism (#12565)
    Closes #12563

--- projects/core/src/state/state.module.ts
+++ projects/core/src/state/state.module.ts
@@ -1,17 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { defaultStateConfig } from './config/default-state-config';
 import { stateMetaReducers } from './reducers/index';
-import { provideDefaultConfig } from '../config/config-providers';
 
 @NgModule({})
 export class StateModule {
   static forRoot(): ModuleWithProviders<StateModule> {
     return {
       ngModule: StateModule,
-      providers: [
-        ...stateMetaReducers,
-        provideDefaultConfig(defaultStateConfig),
-      ],
+      providers: [...stateMetaReducers],
     };
   }
 }
