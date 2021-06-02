bc2fc96e4a7b266af80242c581519b43269fe5a5
Larisa Staroverova <larisa.staroverova@sap.com>
2021-06-02 08:15:23 +0200

    Add missing deprecated params

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/configurator-form.component.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/configurator-form.component.migration.ts
@@ -9,6 +9,7 @@ export const CONFIGURATOR_FORM_COMPONENT_MIGRATION: ConstructorDeprecation = {
   //feature-libs/product-configurator/rulebased/components/form/configurator-form.component.ts
   class: CONFIGURATOR_FORM_COMPONENT,
   importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+  deprecatedParams: [],
   addParams: [
     {
       className: CONFIGURATOR_STOREFRONT_UTILS_SERVICE,
