059ff8ffdfd15ee474d27f9a3a036c211699d1a6
Patrick Labadie <patrick.labadie@sap.com>
2021-06-03 15:12:23 -0400

    GH-12649: Minor import change.

--- projects/core/src/occ/adapters/order/order-occ.module.ts
+++ projects/core/src/occ/adapters/order/order-occ.module.ts
@@ -2,10 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ORDER_NORMALIZER } from '../../../checkout/connectors/checkout/converters';
 import { REPLENISHMENT_ORDER_NORMALIZER } from '../../../checkout/connectors/replenishment-order/converters';
-import {
-  OccOrderNormalizer,
-  OccReplenishmentOrderNormalizer,
-} from './converters/index';
+import { OccOrderNormalizer } from './converters/occ-order-normalizer';
+import { OccReplenishmentOrderNormalizer } from './converters/occ-replenishment-order-normalizer';
 
 @NgModule({
   imports: [CommonModule],
