1c26c6cc39505a304d968939aeceb33006555ba0
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-06-04 15:00:58 -0400

    fix: Saved carts: can't delete description when editing

--- feature-libs/cart/saved-cart/components/saved-cart-form-dialog/saved-cart-form-dialog.component.ts
+++ feature-libs/cart/saved-cart/components/saved-cart-form-dialog/saved-cart-form-dialog.component.ts
@@ -11,8 +11,8 @@ import {
   DeleteSavedCartEvent,
   DeleteSavedCartFailEvent,
   DeleteSavedCartSuccessEvent,
-  SavedCartFormType,
   SavedCartFacade,
+  SavedCartFormType,
 } from '@spartacus/cart/saved-cart/root';
 import {
   Cart,
@@ -122,12 +122,16 @@ export class SavedCartFormDialogComponent implements OnInit, OnDestroy {
   }
 
   saveOrEditCart(cartId: string): void {
+    const name = this.form.get('name')?.value;
+    // TODO(#12660): Remove default value once backend is updated
+    const description = this.form.get('description')?.value || '-';
+
     switch (this.layoutOption) {
       case SavedCartFormType.SAVE: {
         this.savedCartService.saveCart({
           cartId,
-          saveCartName: this.form.get('name')?.value,
-          saveCartDescription: this.form.get('description')?.value,
+          saveCartName: name,
+          saveCartDescription: description,
         });
 
         break;
@@ -136,8 +140,8 @@ export class SavedCartFormDialogComponent implements OnInit, OnDestroy {
       case SavedCartFormType.EDIT: {
         this.savedCartService.editSavedCart({
           cartId,
-          saveCartName: this.form.get('name')?.value,
-          saveCartDescription: this.form.get('description')?.value,
+          saveCartName: name,
+          saveCartDescription: description,
         });
 
         break;
