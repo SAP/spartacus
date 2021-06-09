526b1a66bba0268604a98d62268176d443325c54
Larisa Staroverova <larisa.staroverova@sap.com>
2021-06-09 14:23:38 +0200

    Review feedback regarding schematics I

--- projects/schematics/src/migrations/4_0/methods-and-properties-deprecations/data/configurator-group-menu.component.migration.ts
+++ projects/schematics/src/migrations/4_0/methods-and-properties-deprecations/data/configurator-group-menu.component.migration.ts
@@ -2,7 +2,7 @@ import {
   CLICK_ON_ENTER,
   CONFIGURATOR_GROUP_MENU_COMPONENT,
   NAVIGATE_UP_ON_ENTER,
-  SPARTACUS_PRODUCT_CONFIGURATOR,
+  SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
   TODO_SPARTACUS,
 } from '../../../../shared/constants';
 
@@ -12,13 +12,13 @@ import { MethodPropertyDeprecation } from '../../../../shared/utils/file-utils';
 export const CONFIGURATOR_GROUP_MENU_COMPONENT_MIGRATION: MethodPropertyDeprecation[] = [
   {
     class: CONFIGURATOR_GROUP_MENU_COMPONENT,
-    importPath: SPARTACUS_PRODUCT_CONFIGURATOR,
+    importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
     deprecatedNode: CLICK_ON_ENTER,
     comment: `// ${TODO_SPARTACUS} Method '${CLICK_ON_ENTER}' was removed from '${CONFIGURATOR_GROUP_MENU_COMPONENT}'. It is no longer used.`,
   },
   {
     class: CONFIGURATOR_GROUP_MENU_COMPONENT,
-    importPath: SPARTACUS_PRODUCT_CONFIGURATOR,
+    importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
     deprecatedNode: NAVIGATE_UP_ON_ENTER,
     comment: `// ${TODO_SPARTACUS} Method '${NAVIGATE_UP_ON_ENTER}' was removed from '${CONFIGURATOR_GROUP_MENU_COMPONENT}'. It is no longer used.`,
   },
