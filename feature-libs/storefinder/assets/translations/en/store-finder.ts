bfd8a748646cdd617ef67639f20a37a263641c80
Jonathan Hsu <jonathan.hsu@sap.com>
2021-06-10 12:22:25 -0400

    change translation var make btn grow with text

--- feature-libs/storefinder/assets/translations/en/store-finder.ts
+++ feature-libs/storefinder/assets/translations/en/store-finder.ts
@@ -2,7 +2,7 @@ export const storeFinder = {
   storeFinder: {
     openUntil: 'Open until',
     closed: 'Closed',
-    clickBack: 'Back',
+    back: 'Back',
     call: 'Call',
     getDirections: 'Get Directions',
     listView: 'List View',
