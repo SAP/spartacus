9b88eea428691b84be16ae8d1514a318c01dcd91
Marcin Lasak <lasakmarcin90@gmail.com>
2021-06-10 18:12:01 +0200

    chor: Further TS improvements

--- projects/core/src/config/config-tokens.ts
+++ projects/core/src/config/config-tokens.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, InjectFlags, InjectionToken } from '@angular/core';
 import { deepMerge } from './utils/deep-merge';
 
-export function factory() {
+export function configFactory() {
   return deepMerge({}, inject(DefaultConfig), inject(RootConfig));
 }
 
@@ -10,11 +10,11 @@ export function factory() {
  */
 @Injectable({
   providedIn: 'root',
-  useFactory: factory,
+  useFactory: configFactory,
 })
 export abstract class Config {}
 
-export function defaultFactory() {
+export function defaultConfigFactory() {
   return deepMerge(
     {},
     ...(inject(DefaultConfigChunk, InjectFlags.Optional) ?? [])
@@ -25,10 +25,10 @@ export function defaultFactory() {
  */
 export const DefaultConfig = new InjectionToken('DefaultConfiguration', {
   providedIn: 'root',
-  factory: defaultFactory,
+  factory: defaultConfigFactory,
 });
 
-export function rootFactory() {
+export function rootConfigFactory() {
   return deepMerge({}, ...(inject(ConfigChunk, InjectFlags.Optional) ?? []));
 }
 
@@ -37,20 +37,20 @@ export function rootFactory() {
  */
 export const RootConfig = new InjectionToken('RootConfiguration', {
   providedIn: 'root',
-  factory: rootFactory,
+  factory: rootConfigFactory,
 });
 
 /**
  * Config chunk token, can be used to provide configuration chunk and contribute to the global configuration object.
  * Should not be used directly, use `provideConfig` or import `ConfigModule.withConfig` instead.
  */
-export const ConfigChunk = new InjectionToken<object[]>('ConfigurationChunk');
+export const ConfigChunk = new InjectionToken<Config[]>('ConfigurationChunk');
 /**
  * Config chunk token, can be used to provide configuration chunk and contribute to the default configuration.
  * Should not be used directly, use `provideDefaultConfig` or `provideDefaultConfigFactory` instead.
  *
  * General rule is, that all config provided in libraries should be provided as default config.
  */
-export const DefaultConfigChunk = new InjectionToken<object[]>(
+export const DefaultConfigChunk = new InjectionToken<Config[]>(
   'DefaultConfigurationChunk'
 );
