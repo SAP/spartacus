85bab05feb668344f18d5be0a7c0cd46a5abcfd8
Marcin Lasak <lasakmarcin90@gmail.com>
2021-06-11 11:35:46 +0200

    refactor: Interface for ProductConfigurator

--- feature-libs/product-configurator/rulebased/root/public_api.ts
+++ feature-libs/product-configurator/rulebased/root/public_api.ts
@@ -3,3 +3,7 @@
  */
 
 export * from './index';
+
+/** AUGMENTABLE_TYPES_START */
+export { ProductConfiguratorConfig } from './cpq/interceptor/cpq-configurator-auth.config';
+/** AUGMENTABLE_TYPES_END */
