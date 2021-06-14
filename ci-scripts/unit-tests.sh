1545958731d47b11b98ad2e9dbd55fcf7a01383f
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-06-14 11:14:59 -0400

    Sending core and lib tests to a separate job

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -16,25 +16,6 @@ echo "Running schematics unit tests and code coverage for CDS library"
 exec 5>&1
 output=$(yarn --cwd integration-libs/cds run test:schematics --coverage=true | tee /dev/fd/5)
 
-
-echo "Running unit tests and code coverage for core"
-exec 5>&1
-output=$(ng test core --watch=false --sourceMap --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
-
-echo "Running unit tests and code coverage for storefrontlib"
-exec 5>&1
-output=$(ng test storefrontlib --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
-
 echo "Running unit tests and code coverage for user"
 exec 5>&1
 output=$(ng test user --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
