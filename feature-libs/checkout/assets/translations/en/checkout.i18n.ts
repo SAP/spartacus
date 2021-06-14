9bb2dc59d1cb8937ab83caaddf819ca5230f4384
Patrick Labadie <patrick.labadie@sap.com>
2021-06-14 15:14:35 -0400

    GH-12462: Fix address verification labels

--- feature-libs/checkout/assets/translations/en/checkout.i18n.ts
+++ feature-libs/checkout/assets/translations/en/checkout.i18n.ts
@@ -3,16 +3,8 @@ export const checkout = {
     shippingAddress: 'Shipping Address',
     selectYourShippingAddress: 'Select your Shipping Address',
     defaultShippingAddress: 'Default Shipping Address',
-    verifyYourAddress: 'Verify your address',
-    ensureAccuracySuggestChange:
-      'To ensure delivery accuracy, we suggest the change selected below.',
-    chooseAddressToUse: 'Please choose which address you would like to use:',
-    suggestedAddress: 'Suggested address',
-    enteredAddress: 'Entered address',
     addNewAddress: 'Add New Address',
     shipToThisAddress: 'Ship to this address',
-    editAddress: 'Edit address',
-    saveAddress: 'Save address',
   },
   checkoutOrderConfirmation: {
     confirmationOfOrder: 'Confirmation of Order:',
