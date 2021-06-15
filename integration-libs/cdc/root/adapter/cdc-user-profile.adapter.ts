754086eab1165ae9556fb92df1dd74d051814962
Pratik Kumar <pratik.kumar@sap.com>
2021-06-15 18:19:01 +0530

    Fixed build errors

--- integration-libs/cdc/root/adapter/cdc-user-profile.adapter.ts
+++ integration-libs/cdc/root/adapter/cdc-user-profile.adapter.ts
@@ -1,19 +1,21 @@
-import { Injectable } from "@angular/core";
-import { normalizeHttpError, User } from "@spartacus/core";
-import { USER_PROFILE_SERIALIZER } from "@spartacus/user/profile/core";
-import { OccUserProfileAdapter } from "@spartacus/user/profile/occ";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { normalizeHttpError, User } from '@spartacus/core';
+import { USER_PROFILE_SERIALIZER } from '@spartacus/user/profile/core';
+import { OccUserProfileAdapter } from '@spartacus/user/profile/occ';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root'
-  })
-export class CdcUserProfileAdapter extends OccUserProfileAdapter{
-    update(userId: string, user: User): Observable<unknown> {
-        const url = this.occEndpoints.getUrl('users/${userId}', { userId });
-        user = this.converter.convert(user, USER_PROFILE_SERIALIZER);
-        return this.http
-          .patch(url, user)
-          .pipe(catchError((error) => throwError(normalizeHttpError(error))));
-      }
+  providedIn: 'root',
+})
+export class CdcUserProfileAdapter extends OccUserProfileAdapter {
+  update(userId: string, user: User): Observable<unknown> {
+    const url = this.occEndpoints.buildUrl('users/${userId}', {
+      urlParams: { userId },
+    });
+    user = this.converter.convert(user, USER_PROFILE_SERIALIZER);
+    return this.http
+      .patch(url, user)
+      .pipe(catchError((error) => throwError(normalizeHttpError(error))));
+  }
 }
