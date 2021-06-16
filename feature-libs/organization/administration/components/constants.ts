3e30a4f9a321504b02d0dc3feccd5c5c84e77a3c
Krzysztof Platis <platonn.git@gmail.com>
2021-06-16 16:43:33 +0200

    move routing config chunks in org lib to the root entry point

--- feature-libs/organization/administration/components/constants.ts
+++ feature-libs/organization/administration/components/constants.ts
@@ -1,13 +1,7 @@
-// This is not for the public API
-export const ROUTE_PARAMS = {
-  budgetCode: 'budgetCode',
-  unitCode: 'unitCode',
-  costCenterCode: 'costCenterCode',
-  userCode: 'userCode',
-  userGroupCode: 'userGroupCode',
-  permissionCode: 'permissionCode',
-  addressCode: 'addressId',
-};
+import { ROUTE_PARAMS as ROOT_ROUTE_PARAMS } from '@spartacus/organization/administration/root';
 
+export const ROUTE_PARAMS = ROOT_ROUTE_PARAMS; // SPIKE TODO REMOVE and use directly '@spartacus/organization/administration/root'
+
+// This is not for the public API
 // TODO:#my-account-architecture - Number.MAX_VALUE?
 export const MAX_OCC_INTEGER_VALUE = 2147483647;
