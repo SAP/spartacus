c4dc7cb924bc213159bc50b2933cfb056f3beab5
Michal Dydo <mdydo@divante.pl>
2021-06-17 08:42:49 +0200

    Fix e2e tests v2

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/product-list-component.service.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/product-list-component.service.migration.ts
@@ -1,5 +1,13 @@
 import {
+  ACTIVATED_ROUTE,
+  ANGULAR_ROUTER,
+  CURRENCY_SERVICE,
+  LANGUAGE_SERVICE,
   PRODUCT_LIST_COMPONENT_SERVICE,
+  PRODUCT_SEARCH_SERVICE,
+  ROUTER,
+  ROUTING_SERVICE,
+  SPARTACUS_CORE,
   SPARTACUS_STOREFRONTLIB,
   VIEW_CONFIG,
 } from '../../../../shared/constants';
@@ -9,7 +17,32 @@ export const PRODUCT_LIST_COMPONENT_SERVICE_MIGRATION: ConstructorDeprecation = 
   // projects/storefrontlib/src/cms-components/product/product-list/container/product-list-component.service.ts
   class: PRODUCT_LIST_COMPONENT_SERVICE,
   importPath: SPARTACUS_STOREFRONTLIB,
-  deprecatedParams: [],
+  deprecatedParams: [
+    {
+      className: PRODUCT_SEARCH_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ROUTING_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ACTIVATED_ROUTE,
+      importPath: ANGULAR_ROUTER,
+    },
+    {
+      className: CURRENCY_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: LANGUAGE_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: ROUTER,
+      importPath: ANGULAR_ROUTER,
+    },
+  ],
   addParams: [
     {
       className: VIEW_CONFIG,
