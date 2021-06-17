1c0d469e4ad0bcadb750bf6dc4e3dd5dfcbd5714
Thambirajah <renuchan.thambirajah@sap.com>
2021-06-17 12:40:54 -0400

    Removed commented code.

--- projects/schematics/src/migrations/4_0/removed-public-api-deprecations/removed-public-api-deprecation.ts
+++ projects/schematics/src/migrations/4_0/removed-public-api-deprecations/removed-public-api-deprecation.ts
@@ -135,37 +135,6 @@ import { DeprecatedNode } from '../../../shared/utils/file-utils';
 import { removedPublicApiDeprecation } from '../../mechanism/removed-public-api-deprecations/removed-public-api-deprecation';
 
 export const REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
-  // uncomment product variants deprecation on 4.0 migration works (#11391)
-  // // projects/storefrontlib/src/cms-components/product/product-variants/product-variants.component.ts
-  // {
-  //   node: PRODUCT_VARIANT_COMPONENT,
-  //   importPath: SPARTACUS_STOREFRONTLIB,
-  //   comment: `'${PRODUCT_VARIANT_COMPONENT}' was moved to @spartacus/product/variants.`,
-  // },
-  // // projects/storefrontlib/src/cms-components/product/product-variants/variant-color-selector/variant-color-selector.component.ts
-  // {
-  //   node: VARIANT_COLOR_SELECTOR_COMPONENT,
-  //   importPath: SPARTACUS_STOREFRONTLIB,
-  //   comment: `'${VARIANT_COLOR_SELECTOR_COMPONENT}' was moved to @spartacus/product/variants.`,
-  // },
-  // // projects/storefrontlib/src/cms-components/product/product-variants/variant-size-selector/variant-size-selector.component.ts
-  // {
-  //   node: VARIANT_SIZE_SELECTOR_COMPONENT,
-  //   importPath: SPARTACUS_STOREFRONTLIB,
-  //   comment: `'${VARIANT_SIZE_SELECTOR_COMPONENT}' was moved to @spartacus/product/variants.`,
-  // },
-  // // projects/storefrontlib/src/cms-components/product/product-variants/variant-style-icons/variant-style-icons.component.ts
-  // {
-  //   node: VARIANT_STYLE_ICONS_COMPONENT,
-  //   importPath: SPARTACUS_STOREFRONTLIB,
-  //   comment: `'${VARIANT_STYLE_ICONS_COMPONENT}' was moved to @spartacus/product/variants.`,
-  // },
-  // // projects/storefrontlib/src/cms-components/product/product-variants/variant-style-selector/variant-style-selector.component.ts
-  // {
-  //   node: VARIANT_STYLE_SELECTOR_COMPONENT,
-  //   importPath: SPARTACUS_STOREFRONTLIB,
-  //   comment: `'${VARIANT_STYLE_SELECTOR_COMPONENT}' was moved to @spartacus/product/variants.`,
-  // },
   //projects/core/src/occ/config-loader/occ-config-loader.module.ts
   {
     node: OCC_CONFIG_LOADER_MODULE,
