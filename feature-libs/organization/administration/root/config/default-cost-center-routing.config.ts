6340473821fe9529f5ab9eb0857238d853705d62
Krzysztof Platis <platonn.git@gmail.com>
2021-06-17 16:27:33 +0200

    fix routing config of cost centers - drop preceeding slash

--- feature-libs/organization/administration/root/config/default-cost-center-routing.config.ts
+++ feature-libs/organization/administration/root/config/default-cost-center-routing.config.ts
@@ -10,7 +10,7 @@ export const defaultCostCenterRoutingConfig: RoutingConfig = {
   routing: {
     routes: {
       orgCostCenter: {
-        paths: ['/organization/cost-centers'],
+        paths: ['organization/cost-centers'],
       },
       orgCostCenterCreate: {
         paths: ['organization/cost-centers/create'],
