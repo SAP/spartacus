ef15ed26250796fc5a17af68c2f13d49b79b5f46
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-06-17 16:01:03 +0200

    moved qualtrics

--- projects/schematics/src/migrations/4_0/rename-symbol/rename-symbol.ts
+++ projects/schematics/src/migrations/4_0/rename-symbol/rename-symbol.ts
@@ -1,8 +1,49 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
+import {
+  QUALTRICS_COMPONENT,
+  QUALTRICS_CONFIG,
+  QUALTRICS_EVENT_NAME,
+  QUALTRICS_LOADER_SERVICE,
+  QUALTRICS_MODULE,
+  SPARTACUS_QUALTRICS,
+  SPARTACUS_STOREFRONTLIB,
+} from '../../../shared/constants';
 import { RenamedSymbol } from '../../../shared/utils/file-utils';
 import { migrateRenamedSymbols } from '../../mechanism/rename-symbol/rename-symbol';
 
-export const RENAMED_SYMBOLS_DATA: RenamedSymbol[] = [];
+export const RENAMED_SYMBOLS_DATA: RenamedSymbol[] = [
+  // projects/storefrontlib/src/cms-components/misc/qualtrics/config/qualtrics-config.ts
+  {
+    previousNode: QUALTRICS_CONFIG,
+    previousImportPath: SPARTACUS_STOREFRONTLIB,
+    newImportPath: `${SPARTACUS_QUALTRICS}/components`,
+  },
+  // projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.ts
+  {
+    previousNode: QUALTRICS_EVENT_NAME,
+    previousImportPath: SPARTACUS_STOREFRONTLIB,
+    newImportPath: `${SPARTACUS_QUALTRICS}/components`,
+  },
+  // projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics-loader.service.ts
+  {
+    previousNode: QUALTRICS_LOADER_SERVICE,
+    previousImportPath: SPARTACUS_STOREFRONTLIB,
+    newImportPath: `${SPARTACUS_QUALTRICS}/components`,
+  },
+  // projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics.component.ts
+  {
+    previousNode: QUALTRICS_COMPONENT,
+    previousImportPath: SPARTACUS_STOREFRONTLIB,
+    newImportPath: `${SPARTACUS_QUALTRICS}/components`,
+  },
+  // projects/storefrontlib/src/cms-components/misc/qualtrics/qualtrics.module.ts
+  {
+    previousNode: QUALTRICS_MODULE,
+    previousImportPath: SPARTACUS_STOREFRONTLIB,
+    newNode: 'QualtricsComponentsModule',
+    newImportPath: `${SPARTACUS_QUALTRICS}/components`,
+  },
+];
 
 export function migrate(): Rule {
   return (tree: Tree) => {
