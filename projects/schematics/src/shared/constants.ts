9c524d5f23d973cd9975a651e78ed5e97bfaa719
Michal Dydo <mdydo@divante.pl>
2021-06-18 13:37:12 +0200

    Quick fix

--- projects/schematics/src/shared/constants.ts
+++ projects/schematics/src/shared/constants.ts
@@ -148,7 +148,6 @@ export const ORGANIZATION_PAGE_META_RESOLVER = 'OrganizationPageMetaResolver';
 export const PRODUCT_SEARCH_SERVICE = 'ProductSearchService';
 export const CHECKOUT_SERVICE = 'CheckoutService';
 export const CLEAR_CHECKOUT_SERVICE = 'ClearCheckoutService';
-export const CLEAR_CHECKOUT_SERVICE = 'ClearCheckoutService';
 export const CHECKOUT_FACADE = 'CheckoutFacade';
 export const CHECKOUT_CONNECTOR = 'CheckoutConnector';
 export const USER_ORDER_CONNECTOR = 'UserOrderConnector';
