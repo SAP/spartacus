9180b8defe745a76122c41447ecdecdd5abe2077
AshreeJay <jayashree.govindasamy@sap.com>
2021-06-18 13:20:43 +0530

    Update index.js

--- projects/storefrontapp-e2e-cypress/cypress/plugins/index.js
+++ projects/storefrontapp-e2e-cypress/cypress/plugins/index.js
@@ -2,7 +2,6 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
 module.exports = (on, config) => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
-
   /* Set exact timestamp to be shared in all spec files */
   config.env.TIMESTAMP = Date.now() - 1535535333333;
   return config;
