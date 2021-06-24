7beac47cf60e0c3cc2fcfab7e58fb0e7ac2851bc
Christoph Hinssen <christoph.hinssen@sap.com>
2021-06-24 14:52:27 +0200

    Add migrations

---
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
@@ -0,0 +1,18 @@
+import {
+  CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT, CONFIGURATOR_UI_SETTINGS_CONFIG,
+  SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED
+} from '../../../../shared/constants';
+import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
+
+export const CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT_MIGRATION: ConstructorDeprecation = {
+  // feature-libs/product-configurator/rulebased/components/attribute/types/numeric-input-field/configurator-attribute-numeric-input-field.component.ts
+  class: CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT,
+  importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+  deprecatedParams: [],
+  addParams: [
+    {
+      className: CONFIGURATOR_UI_SETTINGS_CONFIG,
+      importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+    },
+  ],
+};
