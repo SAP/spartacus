541823b1ab134a6460b5a11945c3444ade1ebe30
Marcin Lasak <lasakmarcin90@gmail.com>
2021-06-24 08:07:07 +0200

    chore: Type augmentation for occ endpoints

--- core-libs/setup/recipes/b2b/config/default-b2b-occ-config.ts
+++ core-libs/setup/recipes/b2b/config/default-b2b-occ-config.ts
@@ -1,17 +1,22 @@
+// We need hit import for augmentation of OccEndpoints to pick up
+import { CheckoutOccEndpoints } from '@spartacus/checkout/occ';
 import { OccConfig } from '@spartacus/core';
+
+const defaultB2bCheckoutOccEndpoints: CheckoutOccEndpoints = {
+  setDeliveryAddress: 'orgUsers/${userId}/carts/${cartId}/addresses/delivery',
+  placeOrder: 'orgUsers/${userId}/orders?fields=FULL',
+};
 
 export const defaultB2bOccConfig: OccConfig = {
   backend: {
     occ: {
       endpoints: {
+        ...defaultB2bCheckoutOccEndpoints,
         user: 'orgUsers/${userId}',
         userUpdateProfile: 'users/${userId}',
         userCloseAccount: 'users/${userId}',
         addEntries:
           'orgUsers/${userId}/carts/${cartId}/entries?quantity=${quantity}',
-        setDeliveryAddress:
-          'orgUsers/${userId}/carts/${cartId}/addresses/delivery',
-        placeOrder: 'orgUsers/${userId}/orders?fields=FULL',
         scheduleReplenishmentOrder:
           'orgUsers/${userId}/replenishmentOrders?fields=FULL,costCenter(FULL),purchaseOrderNumber,paymentType',
         replenishmentOrderDetails:
