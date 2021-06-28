7daadb03a8d75b309cef66d4dce66e4921376438
Christoph Hinssen <christoph.hinssen@sap.com>
2021-06-28 11:07:15 +0200

    Fix index.ts

--- feature-libs/product-configurator/rulebased/components/config/index.ts
+++ feature-libs/product-configurator/rulebased/components/config/index.ts
@@ -1,3 +1,3 @@
-export { ConfiguratorMessageConfig as MessageConfig } from './configurator-message.config';
+export { ConfiguratorMessageConfig } from './configurator-message.config';
 export { ConfiguratorUISettingsConfig } from './configurator-ui-settings.config';
 import './augmented-config.model';
