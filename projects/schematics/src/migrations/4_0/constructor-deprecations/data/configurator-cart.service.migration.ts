6a50757874d63dd5f2d9ea4490c1e6e77c7ed4bc
Christoph Hinssen <christoph.hinssen@sap.com>
2021-06-30 08:14:27 +0200

    Add migration with respect to utils service
    And remove checkout migration

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/configurator-cart.service.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/configurator-cart.service.migration.ts
@@ -1,32 +1,38 @@
+import { SPARTACUS_CHECKOUT_ROOT } from 'feature-libs/checkout/schematics/constants';
 import {
   ACTIVE_CART_SERVICE,
   CHECKOUT_FACADE,
-  CHECKOUT_SERVICE,
   COMMON_CONFIGURATOR_UTILS_SERVICE,
   CONFIGURATOR_CART_SERVICE,
+  CONFIGURATOR_UTILS_SERVICE,
   NGRX_STORE,
   SPARTACUS_CORE,
-  SPARTACUS_PRODUCT_CONFIGURATOR,
+  SPARTACUS_PRODUCT_CONFIGURATOR_COMMON,
+  SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
   STORE,
-  USER_ID_SERVICE,
+  USER_ID_SERVICE
 } from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
 export const CONFIGURATOR_CART_SERVICE_MIGRATION: ConstructorDeprecation = {
   // feature-libs/product-configurator/rulebased/core/facade/configurator-cart.service.ts
   class: CONFIGURATOR_CART_SERVICE,
-  importPath: SPARTACUS_PRODUCT_CONFIGURATOR,
+  importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
   deprecatedParams: [
     { className: STORE, importPath: NGRX_STORE },
     { className: STORE, importPath: NGRX_STORE },
     { className: ACTIVE_CART_SERVICE, importPath: SPARTACUS_CORE },
     {
       className: COMMON_CONFIGURATOR_UTILS_SERVICE,
-      importPath: SPARTACUS_PRODUCT_CONFIGURATOR,
+      importPath: SPARTACUS_PRODUCT_CONFIGURATOR_COMMON,
     },
-    { className: CHECKOUT_SERVICE, importPath: SPARTACUS_CORE },
+    { className: CHECKOUT_FACADE, importPath: SPARTACUS_CHECKOUT_ROOT },
     { className: USER_ID_SERVICE, importPath: SPARTACUS_CORE },
   ],
-  removeParams: [{ className: CHECKOUT_SERVICE, importPath: SPARTACUS_CORE }],
-  addParams: [{ className: CHECKOUT_FACADE, importPath: SPARTACUS_CORE }],
+  addParams: [
+    {
+      className: CONFIGURATOR_UTILS_SERVICE,
+      importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+    },
+  ],
 };
