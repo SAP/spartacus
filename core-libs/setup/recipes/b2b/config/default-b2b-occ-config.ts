51939e808637c8096cc0d18ec3f6011138b19d10
Mateusz <mateusz.kolasa@sap.com>
2021-06-30 09:31:54 +0200

    Add type augumentation

--- core-libs/setup/recipes/b2b/config/default-b2b-occ-config.ts
+++ core-libs/setup/recipes/b2b/config/default-b2b-occ-config.ts
@@ -1,9 +1,22 @@
+import { UserAccountOccEndpoints } from '@spartacus/user/account/occ';
+import { UserProfileOccEndpoints } from '@spartacus/user/profile/occ';
 import { OccConfig } from '@spartacus/core';
+
+const defaultB2bUserAccountOccEndpoints: UserAccountOccEndpoints = {
+  user: 'orgUsers/${userId}',
+};
+
+const defaultB2bUserProfileOccEndpoints: UserProfileOccEndpoints = {
+  userUpdateProfile: 'users/${userId}',
+  userCloseAccount: 'users/${userId}',
+};
 
 export const defaultB2bOccConfig: OccConfig = {
   backend: {
     occ: {
       endpoints: {
+        ...defaultB2bUserAccountOccEndpoints,
+        ...defaultB2bUserProfileOccEndpoints,
         addEntries:
           'orgUsers/${userId}/carts/${cartId}/entries?quantity=${quantity}',
         setDeliveryAddress:
