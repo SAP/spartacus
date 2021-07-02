eea99bbd0ae9233afeb3d2db0b08ea32e077a297
Mateusz <mateusz.kolasa@sap.com>
2021-07-02 10:08:31 +0200

    Update migration docs

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/unit-address-form.service.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/unit-address-form.service.migration.ts
@@ -3,6 +3,7 @@ import {
   SPARTACUS_ORGANIZATION_ADMINISTRATION_COMPONENTS,
   SPARTACUS_USER_PROFILE_ROOT,
   UNIT_ADDRESS_FORM_SERVICE,
+  USER_ADDRESS_SERVICE,
   USER_PROFILE_FACADE,
   USER_SERVICE,
 } from '../../../../shared/constants';
@@ -12,7 +13,11 @@ export const UNIT_ADDRESS_FORM_SERVICE_MIGRATION: ConstructorDeprecation = {
   // feature-libs/organization/administration/components/unit/links/addresses/form/unit-address-form.service.ts
   class: UNIT_ADDRESS_FORM_SERVICE,
   importPath: SPARTACUS_ORGANIZATION_ADMINISTRATION_COMPONENTS,
-  deprecatedParams: [{ className: USER_SERVICE, importPath: SPARTACUS_CORE }],
+  deprecatedParams: [
+    { className: USER_ADDRESS_SERVICE, importPath: SPARTACUS_CORE },
+    { className: USER_SERVICE, importPath: SPARTACUS_CORE },
+  ],
+  removeParams: [{ className: USER_SERVICE, importPath: SPARTACUS_CORE }],
   addParams: [
     { className: USER_PROFILE_FACADE, importPath: SPARTACUS_USER_PROFILE_ROOT },
   ],
