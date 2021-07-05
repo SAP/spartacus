80502afcc8da86e4b8fad9cf7268c16cacaf727d
Thambirajah <renuchan.thambirajah@sap.com>
2021-07-05 08:55:01 -0400

    Added new param in schematics.

--- projects/schematics/src/migrations/4_0/constructor-deprecations/data/navigation-ui.component.migration.ts
+++ projects/schematics/src/migrations/4_0/constructor-deprecations/data/navigation-ui.component.migration.ts
@@ -2,6 +2,7 @@ import {
   SPARTACUS_STOREFRONTLIB,
   HAMBURGER_MENU_SERVICE,
   NAVIGATION_UI_COMPONENT,
+  NAVIGATION_UI_CONFIG,
 } from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
@@ -11,6 +12,10 @@ export const NAVIGATION_UI_COMPONENT_MIGRATION: ConstructorDeprecation = {
   importPath: SPARTACUS_STOREFRONTLIB,
   deprecatedParams: [],
   addParams: [
+    {
+      className: NAVIGATION_UI_CONFIG,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
     {
       className: HAMBURGER_MENU_SERVICE,
       importPath: SPARTACUS_STOREFRONTLIB,
--- projects/schematics/src/shared/constants.ts
+++ projects/schematics/src/shared/constants.ts
@@ -760,6 +760,8 @@ export const GET_META_RESOLVER = 'getMetaResolver';
 export const FIND_COMPONENTS_BY_IDS_LEGACY = 'findComponentsByIdsLegacy';
 
 export const STOREFINDER_MODULE = 'StoreFinderModule';
+export const NAVIGATION_UI_CONFIG = 'NavigationUiConfig';
+
 /***** APIs end *****/
 
 /***** feature keys start *****/
