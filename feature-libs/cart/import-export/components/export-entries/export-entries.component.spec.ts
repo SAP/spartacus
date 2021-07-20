c6cf9d6c2e3f3b932c04633fc8c6e4c6843cdba9
Mateusz <mateusz.kolasa@sap.com>
2021-07-20 16:35:03 +0200

    Update unit tests

--- feature-libs/cart/import-export/components/export-entries/export-entries.component.spec.ts
+++ feature-libs/cart/import-export/components/export-entries/export-entries.component.spec.ts
@@ -1,22 +1,98 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { I18nTestingModule, RoutingService } from '@spartacus/core';
+import {
+  I18nTestingModule,
+  ImageType,
+  PriceType,
+  RoutingService,
+} from '@spartacus/core';
 import { ExportService } from '@spartacus/cart/import-export/core';
 import { ExportEntriesService } from './export-entries.service';
 import { of } from 'rxjs';
 import { StoreModule } from '@ngrx/store';
 import { ExportEntriesComponent } from './export-entries.component';
+import { OrderEntry } from './../../../../../projects/core/src/model/order.model';
+
+const entry: OrderEntry = {
+  basePrice: {
+    currencyIso: 'USD',
+    formattedValue: '$23.50',
+    priceType: PriceType.BUY,
+    value: 23.5,
+  },
+  cancellableQuantity: 0,
+  entryNumber: 1,
+  product: {
+    availableForPickup: false,
+    baseOptions: [],
+    categories: [
+      {
+        code: '1358',
+        name: 'Measuring & Layout Tools',
+        url: '/Open-Catalogue/Tools/Measuring-%26-Layout-Tools/c/1358',
+      },
+      {
+        code: 'brand_4434',
+        name: 'Ednet',
+        url: '/Brands/Ednet/c/brand_4434',
+      },
+    ],
+    code: '3803058',
+    images: {
+      PRIMARY: {
+        zoom: {
+          altText: 'PC Service Set Professional',
+          format: 'zoom',
+          imageType: ImageType.PRIMARY,
+          url: 'imageUrl',
+        },
+        product: {
+          altText: 'PC Service Set Professional',
+          format: 'product',
+          imageType: ImageType.PRIMARY,
+          url: 'imageUrl',
+        },
+        thumbnail: {
+          altText: 'PC Service Set Professional',
+          format: 'thumbnail',
+          imageType: ImageType.PRIMARY,
+          url: 'imageUrl',
+        },
+        cartIcon: {
+          altText: 'PC Service Set Professional',
+          format: 'cartIcon',
+          imageType: ImageType.PRIMARY,
+          url: 'imageUrl',
+        },
+      },
+    },
+    manufacturer: 'Ednet',
+    name: 'PC Service Set Professional',
+    purchasable: true,
+    stock: {
+      stockLevel: 365,
+      stockLevelStatus: 'inStock',
+    },
+    url:
+      '/Open-Catalogue/Tools/Measuring-%26-Layout-Tools/PC-Service-Set-Professional/p/3803058',
+    slug: 'pc-service-set-professional',
+    nameHtml: 'PC Service Set Professional',
+  },
+  quantity: 2,
+  returnableQuantity: 0,
+  totalPrice: {
+    currencyIso: 'USD',
+    formattedValue: '$47.00',
+    priceType: PriceType.BUY,
+    value: 47,
+  },
+  updateable: true,
+};
 
 class MockRoutingService {
   getRouterState() {
     return of();
-  }
-}
-
-class MockExportEntriesService {
-  getEntries() {
-    return of([]);
   }
 }
 
@@ -39,10 +115,6 @@ describe('ExportEntriesComponent', () => {
             provide: RoutingService,
             useClass: MockRoutingService,
           },
-          {
-            provide: ExportEntriesService,
-            useClass: MockExportEntriesService,
-          },
         ],
         declarations: [ExportEntriesComponent],
       }).compileComponents();
@@ -52,10 +124,9 @@ describe('ExportEntriesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExportEntriesComponent);
     component = fixture.componentInstance;
+
     exportService = TestBed.inject(ExportService);
     exportEntriesService = TestBed.inject(ExportEntriesService);
-
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -63,18 +134,32 @@ describe('ExportEntriesComponent', () => {
   });
 
   it('should display export to csv link and run downloadCsv on click', () => {
-    spyOn(exportService, 'dataToCsv').and.callThrough();
+    component.entries$ = of([entry]);
+    fixture.detectChanges();
 
+    const exportToCsvSpy = spyOn(component, 'exportToCsv').and.callThrough();
+    const downloadCsvSpy = spyOn(component, 'downloadCsv');
+    const dataToCsvSpy = spyOn(exportService, 'dataToCsv');
+    const exportEntriesSpy = spyOn(exportEntriesService, 'exportEntries');
     const btn = fixture.debugElement.query(By.css('button.cx-action-link'));
+
     expect(btn.nativeElement).toBeTruthy();
 
     btn.nativeElement.click();
-
     fixture.whenStable().then(() => {
-      expect(component.exportToCsv).toHaveBeenCalled();
-      expect(component.downloadCsv).toHaveBeenCalled();
-      expect(exportService.dataToCsv).toHaveBeenCalled();
-      expect(exportEntriesService.exportEntries).toHaveBeenCalled();
+      expect(exportToCsvSpy).toHaveBeenCalled();
+      expect(downloadCsvSpy).toHaveBeenCalled();
+      expect(dataToCsvSpy).toHaveBeenCalled();
+      expect(exportEntriesSpy).toHaveBeenCalled();
     });
+  });
+
+  it('should to not display button if no entries', () => {
+    component.entries$ = of([]);
+
+    const btn = fixture.debugElement.query(By.css('button.cx-action-link'));
+    fixture.detectChanges();
+
+    expect(btn).toBeNull();
   });
 });
