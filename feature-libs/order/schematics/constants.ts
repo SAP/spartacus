d645564557942965734636b7791d24d56f12f751
Weizheng Gao <weizheng.gao@sap.com>
2021-07-20 12:30:59 -0300

    feature/GH-13073: add schematics into order lib

---
+++ feature-libs/order/schematics/constants.ts
@@ -0,0 +1,13 @@
+import { SPARTACUS_ORDER } from '@spartacus/schematics';
+
+export const ORDER_FOLDER_NAME = 'order';
+export const ORDER_MODULE_NAME = 'order';
+export const SCSS_FILE_NAME = 'order.scss';
+
+export const ORDER_FEATURE_NAME_CONSTANT = 'ORDER_FEATURE';
+export const ORDER_MODULE = 'OrderModule';
+export const ORDER_ROOT_MODULE = 'OrderRootModule';
+export const SPARTACUS_ORDER_ROOT = `${SPARTACUS_ORDER}/root`;
+export const SPARTACUS_ORDER_ASSETS = `${SPARTACUS_ORDER}/assets`;
+export const ORDER_TRANSLATIONS = 'orderTranslations';
+export const ORDER_TRANSLATION_CHUNKS_CONFIG = 'orderTranslationChunksConfig';
