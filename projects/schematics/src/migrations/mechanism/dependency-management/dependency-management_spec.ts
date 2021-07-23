9c19ad06173be6ddfb64d1a35998307cb946a3ec
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-07-23 12:55:12 +0200

    improved the test

--- projects/schematics/src/migrations/mechanism/dependency-management/dependency-management_spec.ts
+++ projects/schematics/src/migrations/mechanism/dependency-management/dependency-management_spec.ts
@@ -173,6 +173,7 @@ describe('dependency management migrations', () => {
         expect(packageJson.dependencies['@spartacus/storefront']).toBeTruthy();
         expect(packageJson.dependencies['@spartacus/cart']).toBeTruthy();
         expect(packageJson.dependencies['@spartacus/checkout']).toBeTruthy();
+        expect(packageJson.dependencies['@spartacus/user']).toBeTruthy();
       });
     });
 
