16aa474de4af8706b2058a8ed23272394061090d
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-07-23 10:40:58 +0200

    added a log info about the new app structure

--- projects/schematics/src/migrations/mechanism/scaffold-app-structure/scaffold-app-structure.ts
+++ projects/schematics/src/migrations/mechanism/scaffold-app-structure/scaffold-app-structure.ts
@@ -1,10 +1,19 @@
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import {
   getDefaultProjectNameFromWorkspace,
   scaffoldStructure,
 } from '../../../shared/utils/workspace-utils';
 
-export function scaffoldAppStructure(tree: Tree): Rule {
+export function scaffoldAppStructure(
+  tree: Tree,
+  context: SchematicContext
+): Rule {
+  context.logger.info(
+    'Scaffolding the new app structure if not already present...'
+  );
+  context.logger.info(
+    'For more about the new app structure please visit: https://sap.github.io/spartacus-docs/reference-app-structure/'
+  );
   const project = getDefaultProjectNameFromWorkspace(tree);
   return scaffoldStructure({ project });
 }
