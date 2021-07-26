e2139f7c21d3c5b3a325a56673aa1be40abe34c6
Christoph Hinssen <christoph.hinssen@sap.com>
2021-07-26 09:54:09 +0200

    Create explicit event listener in dedicated package

--- feature-libs/product-configurator/rulebased/core/facade/index.ts
+++ feature-libs/product-configurator/rulebased/core/facade/index.ts
@@ -2,5 +2,4 @@ export * from './configurator-cart.service';
 export * from './configurator-commons.service';
 export * from './configurator-group-status.service';
 export * from './configurator-groups.service';
-export * from './configurator-place-order.service';
 export * from './utils/index';
--- feature-libs/product-configurator/rulebased/core/rulebased-configurator-core.module.ts
+++ feature-libs/product-configurator/rulebased/core/rulebased-configurator-core.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MODULE_INITIALIZER } from '@spartacus/core';
 import { RulebasedConfiguratorConnector } from './connectors/rulebased-configurator.connector';
-import { ConfiguratorPlaceOrderService } from './facade/configurator-place-order.service';
+import { RulebasedConfiguratorEventModule } from './events/rulebased-configurator-event.module';
 import { RulebasedConfiguratorStateModule } from './state/rulebased-configurator-state.module';
 
-function initEventListener(service: ConfiguratorPlaceOrderService): () => void {
-  const result = () => service.init();
-  return result;
-}
 /**
  * Exposes the rulebased configurator core entities.
  * Explicit providing of connector because otherwise lazy loading does not work
  */
 @NgModule({
-  imports: [RulebasedConfiguratorStateModule],
-  providers: [
-    RulebasedConfiguratorConnector,
-    {
-      provide: MODULE_INITIALIZER,
-      useFactory: initEventListener,
-      deps: [ConfiguratorPlaceOrderService],
-      multi: true,
-    },
-  ],
+  imports: [RulebasedConfiguratorStateModule, RulebasedConfiguratorEventModule],
+  providers: [RulebasedConfiguratorConnector],
 })
 export class RulebasedConfiguratorCoreModule {}
