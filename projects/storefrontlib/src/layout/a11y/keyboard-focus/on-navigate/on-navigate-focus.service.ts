ce25ae662123570644ba7cda62c8c2597fad21a5
Caine Rotherham <rotherham.c@gmail.com>
2021-07-29 10:23:54 +0200

    fix: Fix OnNavigateFocusService with SSR

--- projects/storefrontlib/src/layout/a11y/keyboard-focus/on-navigate/on-navigate-focus.service.ts
+++ projects/storefrontlib/src/layout/a11y/keyboard-focus/on-navigate/on-navigate-focus.service.ts
@@ -1,11 +1,11 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, OnDestroy } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { filter, take } from 'rxjs/operators';
 import { BREAKPOINT } from '../../../config';
 import { BreakpointService } from '../../../../layout/breakpoint/breakpoint.service';
 import { KeyboardFocusConfig } from '../config';
+import { DOCUMENT } from '@angular/common';
 
 /**
  * Shared service for keyboard focus features called when the browser navigates.
@@ -13,10 +13,23 @@ import { KeyboardFocusConfig } from '../config';
 @Injectable({
   providedIn: 'root',
 })
-export class OnNavigateFocusService {
+export class OnNavigateFocusService implements OnDestroy {
   protected resetFocusOnNavigate: Subscription;
   protected resetViewOnNavigate: Subscription;
 
+  /**
+   * @deprecated in 4.0
+   * Constructor will be migrated to...
+   *
+   * ```
+   * constructor(
+   *  protected config: KeyboardFocusConfig,
+   *  protected router: Router,
+   *  protected breakpointService: BreakpointService,
+   *  protected winRef: WindowRef
+   * ) {}
+   * ```
+   */
   constructor(
     protected config: KeyboardFocusConfig,
     protected router: Router,
@@ -24,10 +37,20 @@ export class OnNavigateFocusService {
     protected breakpointService: BreakpointService
   ) {}
 
+  ngOnDestroy(): void {
+    this.resetFocusOnNavigate?.unsubscribe();
+    this.resetViewOnNavigate?.unsubscribe();
+  }
+
   /**
    * Reads configuration and enables features based on flags set.
    */
   initializeWithConfig(): void {
+    // Do not initialize on server-side
+    if (typeof window === 'undefined') {
+      return;
+    }
+
     if (this.config?.keyboardFocus?.enableResetFocusOnNavigate) {
       this.setResetFocusOnNavigate(
         this.config.keyboardFocus.enableResetFocusOnNavigate
@@ -42,7 +65,7 @@ export class OnNavigateFocusService {
   }
 
   /**
-   * Resets focus back to root element `<cx-storefront>` in the DOM tree when a navigation is started.
+   * Resets focus back to body element in the DOM tree when a navigation is started.
    * @param enable Enable or disable this feature. Set this to an array of BREAKPOINTS to enable for specified screen widths.
    */
   setResetFocusOnNavigate(enable: boolean | BREAKPOINT[]): void {
@@ -57,18 +80,18 @@ export class OnNavigateFocusService {
               .pipe(take(1))
               .subscribe((breakpoint: BREAKPOINT) => {
                 if (enable.includes(breakpoint)) {
-                  this.getStorefrontElement().focus();
+                  this.document.body.focus();
                 }
               });
           } else if (typeof enable === 'boolean') {
-            this.getStorefrontElement().focus();
+            this.document.body.focus();
           }
         });
     }
   }
 
   /**
-   * Resets view back to root element `<cx-storefront>` in the DOM tree when a navigation is started.
+   * Resets view back to body element in the DOM tree when a navigation is started.
    * @param enable Enable or disable this feature. Set this to an array of BREAKPOINTS to enable for specified screen widths.
    */
   setResetViewOnNavigate(enable: boolean | BREAKPOINT[]): void {
@@ -83,20 +106,13 @@ export class OnNavigateFocusService {
               .pipe(take(1))
               .subscribe((breakpoint: BREAKPOINT) => {
                 if (enable.includes(breakpoint)) {
-                  this.getStorefrontElement().scrollIntoView();
+                  this.document.body.scrollIntoView();
                 }
               });
           } else if (typeof enable === 'boolean') {
-            this.getStorefrontElement().scrollIntoView();
+            this.document.body.scrollIntoView();
           }
         });
     }
-  }
-
-  /**
-   * Gets the root element `<cx-storefront>`.
-   */
-  private getStorefrontElement(): HTMLElement {
-    return this.document.getElementsByTagName('cx-storefront')[0];
   }
 }
