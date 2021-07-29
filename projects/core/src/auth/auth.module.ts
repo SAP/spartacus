8f9192a283e3fae049d086834edff427eb5f161e
LTiger14 <louis.pierrestiger@sap.com>
2021-07-29 08:10:56 -0400

    Add comment for context

--- projects/core/src/auth/auth.module.ts
+++ projects/core/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { ClientAuthModule } from './client-auth/client-auth.module';
 import { UserAuthModule } from './user-auth/user-auth.module';
 
 @NgModule({
+  // ClientAuthModule should always be imported after UserAuthModule to make sure client token expirations are correctly handled.
   imports: [CommonModule, UserAuthModule.forRoot(), ClientAuthModule.forRoot()],
 })
 export class AuthModule {
