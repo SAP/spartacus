796ac2377359a07c90f012775f719c32515c7244
Caine Rotherham <rotherham.c@gmail.com>
2021-08-11 08:12:07 +0200

    test: E2E test to verify file read/write

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/verify-read-write.e2e-spec.ts
@@ -0,0 +1,19 @@
+context('Verify Read/Write on Cypress', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  describe('Read/Write', () => {
+    it('should not initially exist from some leftover test run', () => {
+      cy.readFile('/tmp/test-file.txt').should('not.exist');
+    });
+
+    it('should write a file', () => {
+      cy.writeFile('/tmp/test-file.txt', 'This is a test file.');
+    });
+
+    it('should read file', () => {
+      cy.readFile('/tmp/test-file.txt').should('exist');
+    });
+  });
+});
