05a9733527504ba4e9e4490e16e840f7e1cfeee1
Caine Rotherham <rotherham.c@gmail.com>
2021-08-11 13:25:33 +0200

    Add comment about cleanup

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/verify-read-write.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/verify-read-write.e2e-spec.ts
@@ -1,15 +1,28 @@
+const DOWNLOADS_FOLDER = Cypress.config('downloadsFolder');
+const TEST_FILE = 'test-file.txt';
+const TEST_FILE_PATH = `${DOWNLOADS_FOLDER}/${TEST_FILE}`;
+
+/**
+ * Verifies that files are read and written correctly on the machine running tests.
+ * 
+ * NOTE: Clean up is not done correctly when using using `cy:open`. Delete 'cypress/downloads' 
+ * manually when running tests on a local machine.
+ * 
+ * Cypress issue regarding `cy:open`:
+ * https://github.com/cypress-io/cypress/issues/14870
+ */
 context('Verify Read/Write on Cypress', () => {
   describe('Read/Write', () => {
     it('should not initially exist from some leftover test run', () => {
-      cy.readFile('/tmp/test-file.txt').should('not.exist');
+      cy.readFile(TEST_FILE_PATH).should('not.exist');
     });
 
     it('should write a file', () => {
-      cy.writeFile('/tmp/test-file.txt', 'This is a test file.');
+      cy.writeFile(TEST_FILE_PATH, 'This is a test file.');
     });
 
     it('should read file', () => {
-      cy.readFile('/tmp/test-file.txt').should('exist');
+      cy.readFile(TEST_FILE_PATH).should('exist');
     });
   });
 });
