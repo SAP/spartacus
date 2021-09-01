1ee4f8acdee0b068ba2e316878a04fe9fe029ec9
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-09-01 14:59:07 -0400

    Adding URLs for PLP and PDP

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:4000/'],
+      url: ['http://localhost:4000/','http://localhost:4000/electronics-spa/en/USD/Brands/Canon/c/brand_10', 'http://localhost:4000/electronics-spa/en/USD/product/832386/ef-300mm-f-2.8l-is-usm'],
       startServerCommand: 'yarn serve:ssr',
     },
     upload: {
