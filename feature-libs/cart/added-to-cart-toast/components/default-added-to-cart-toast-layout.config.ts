dce8b6dd78cd7b84f06b054b4a6a3e6b5ab25f31
Jonathan Hsu <jonathan.hsu@sap.com>
2021-09-14 11:16:15 -0400

    feat: add components module (#13346)

---
+++ feature-libs/cart/added-to-cart-toast/components/default-added-to-cart-toast-layout.config.ts
@@ -0,0 +1,8 @@
+import { AddedToCartToastConfig } from '../added-to-cart-toast-config';
+
+export const defaultAddedToCartToastLayoutConfig: AddedToCartToastConfig = {
+  cartToast: {
+    enabled: true,
+    timeout: 5000,
+  },
+};
