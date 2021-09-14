ca8d9465a8f102cec5df03e53b54eb47fb6bc6f5
Michał Gruca <pucek9@gmail.com>
2021-09-14 13:40:44 +0200

    up

--- feature-libs/cart/import-export/root/import-export-root.module.ts
+++ feature-libs/cart/import-export/root/import-export-root.module.ts
@@ -14,6 +14,7 @@ import { CART_IMPORT_EXPORT_FEATURE } from './feature-name';
           cmsComponents: [
             'ExportOrderEntriesComponent',
             'ImportProductsComponent',
+            'CombinedImportExportComponent',
           ],
         },
       },
