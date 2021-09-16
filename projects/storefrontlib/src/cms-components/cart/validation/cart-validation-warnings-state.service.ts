8d10fbd8108742c8b08d8030f1aee2758f208b1b
Patryk Smul <psmul@divante.pl>
2021-09-16 12:34:29 +0200

    fix: proper result array clearing after two steps in routing

--- projects/storefrontlib/src/cms-components/cart/validation/cart-validation-warnings-state.service.ts
+++ projects/storefrontlib/src/cms-components/cart/validation/cart-validation-warnings-state.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { take, tap, withLatestFrom } from 'rxjs/operators';
+import { ReplaySubject, Subject } from 'rxjs';
+import { take, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
 import { RoutingService, CartModification } from '@spartacus/core';
 
 @Injectable({
@@ -16,22 +16,23 @@ export class CartValidationWarningsStateService {
       );
   }
   NAVIGATION_SKIPS = 2;
-  navigationIdCount: number;
+  navigationIdCount = 0;
 
+  shouldEndClear$ = new Subject<boolean>();
   cartValidationResult$ = new ReplaySubject<CartModification[]>(1);
 
   checkForValidationResultClear$ = this.routingService.getRouterState().pipe(
     withLatestFrom(this.cartValidationResult$),
-    take(this.NAVIGATION_SKIPS),
+    takeUntil(this.shouldEndClear$),
     tap(([routerState, cartModifications]) => {
       if (
         this.navigationIdCount + this.NAVIGATION_SKIPS <=
           routerState.navigationId &&
         cartModifications.length
       ) {
-        console.log('clearing results');
         this.cartValidationResult$.next([]);
         this.navigationIdCount = routerState.navigationId;
+        this.shouldEndClear$.next(true);
       }
     })
   );
