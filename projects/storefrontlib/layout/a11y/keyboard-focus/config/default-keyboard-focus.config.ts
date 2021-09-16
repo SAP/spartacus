2870b221a9d85c668104e17cc1da4c9d96fc0c18
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-09-16 17:26:05 -0400

    remove default scroll behavior

--- projects/storefrontlib/layout/a11y/keyboard-focus/config/default-keyboard-focus.config.ts
+++ projects/storefrontlib/layout/a11y/keyboard-focus/config/default-keyboard-focus.config.ts
@@ -5,7 +5,7 @@ import { KeyboardFocusConfig } from './keyboard-focus.config';
  */
 export const defaultKeyboardFocusConfig: KeyboardFocusConfig = {
   keyboardFocus: {
-    enableResetFocusOnNavigate: true,
-    enableResetViewOnNavigate: true,
+    enableResetFocusOnNavigate: false,
+    enableResetViewOnNavigate: false,
   },
 };
