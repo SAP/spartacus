4d4cd1b651d108fb53bbc8e81b5f34e0b6c77162
Weizheng Gao <weizheng.gao@sap.com>
2021-09-17 12:21:36 -0300

    feature/GH-13208: clear cart codes in storefrontlib

--- projects/storefrontapp/src/app/spartacus/features/cart-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cart-feature.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CartModule } from '@spartacus/cart/main';
-import { CartRootModule } from '@spartacus/cart/main/root';
+import { CartRootModule, CART_FEATURE } from '@spartacus/cart/main/root';
+import { provideConfig } from '@spartacus/core';
 
 @NgModule({
   imports: [CartRootModule, CartModule],
+  providers: [
+    provideConfig({
+      featureModules: {
+        [CART_FEATURE]: {
+          module: () =>
+            import('@spartacus/cart/main').then((m) => m.CartModule),
+        },
+      },
+    }),
+  ],
 })
 export class CartFeatureModule {}
