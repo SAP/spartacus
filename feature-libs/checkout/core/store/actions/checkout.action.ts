26eb0fc3744ea2ba3fc8c0b21b90ce15ad26d7e0
Marcin Lasak <lasakmarcin90@gmail.com>
2021-09-21 12:29:51 +0200

    chore: Revert not fully deprecated actions

--- feature-libs/checkout/core/store/actions/checkout.action.ts
+++ feature-libs/checkout/core/store/actions/checkout.action.ts
@@ -55,9 +55,6 @@ export const SET_DELIVERY_ADDRESS = '[Checkout] Set Delivery Address';
  * @deprecated since 4.3.0. Set delivery address is handled with command in CheckoutDeliveryService.
  */
 export const SET_DELIVERY_ADDRESS_FAIL = '[Checkout] Set Delivery Address Fail';
-/**
- * @deprecated since 4.3.0. Set delivery address is handled with command in CheckoutDeliveryService.
- */
 export const SET_DELIVERY_ADDRESS_SUCCESS =
   '[Checkout] Set Delivery Address Success';
 export const RESET_SET_DELIVERY_ADDRESS_PROCESS =
@@ -171,9 +168,6 @@ export class SetDeliveryAddressFail extends StateUtils.EntityFailAction {
   }
 }
 
-/**
- * @deprecated since 4.3.0. Set delivery address is handled with command in CheckoutDeliveryService.
- */
 export class SetDeliveryAddressSuccess extends StateUtils.EntitySuccessAction {
   readonly type = SET_DELIVERY_ADDRESS_SUCCESS;
   constructor(public payload: Address) {
