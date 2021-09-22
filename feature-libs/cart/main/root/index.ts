1f2bf81d9338459df533c3b2478a0c073bb6e8cb
Patrick Labadie <patrick.labadie@sap.com>
2021-09-22 16:39:06 -0400

    GH-13788: Add models to public api
    (cherry picked from commit 41059cbd1a3110da0c741ef97e5ca45cb02b0de4)

--- feature-libs/cart/main/root/index.ts
+++ feature-libs/cart/main/root/index.ts
@@ -2,3 +2,4 @@ export * from './cart-root.module';
 export * from './config/cart-config';
 export * from './events/cart.events';
 export * from './feature-name';
+export * from './models/cart.model';
