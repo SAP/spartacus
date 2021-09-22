39c5e85db17b68c4f017da0da6198b67ebf92c72
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-09-22 11:02:51 -0400

    Deprecate current scroll behavior (#13764)
    closes GH-13692

--- projects/storefrontlib/layout/a11y/keyboard-focus/config/default-keyboard-focus.config.ts
+++ projects/storefrontlib/layout/a11y/keyboard-focus/config/default-keyboard-focus.config.ts
@@ -1,5 +1,8 @@
 import { KeyboardFocusConfig } from './keyboard-focus.config';
 
+/**
+ * @deprecated since 4.2, refer to spartacus issues (#13762)
+ */
 export const defaultKeyboardFocusConfig: KeyboardFocusConfig = {
   keyboardFocus: {
     enableResetFocusOnNavigate: false,
