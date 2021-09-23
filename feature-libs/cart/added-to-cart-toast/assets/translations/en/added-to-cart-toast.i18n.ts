119faf0da146d26432fb622f64b2859a15ac609a
Jonathan Hsu <jonathan.hsu@sap.com>
2021-09-23 16:24:28 -0400

    feat: add translations #13346

---
+++ feature-libs/cart/added-to-cart-toast/assets/translations/en/added-to-cart-toast.i18n.ts
@@ -0,0 +1,6 @@
+export const addedToCartToast = {
+  entriesAdded: '{{ quantity }} items added',
+  entryAdded: '{{ quantity }} item added',
+  continueShopping: 'Continue Shopping',
+  goToCart: 'Go to Cart',
+};
--- feature-libs/cart/added-to-cart-toast/components/added-to-cart-toast-components.module.ts
+++ feature-libs/cart/added-to-cart-toast/components/added-to-cart-toast-components.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { UrlModule } from '@spartacus/core';
+import { I18nModule, UrlModule } from '@spartacus/core';
 import { MediaModule } from '@spartacus/storefront';
 import { AddedToCartToastComponent } from './added-to-cart-toast/added-to-cart-toast.component';
 
 @NgModule({
-  imports: [CommonModule, MediaModule, RouterModule, UrlModule],
+  imports: [CommonModule, MediaModule, RouterModule, UrlModule, I18nModule],
   declarations: [AddedToCartToastComponent],
 })
 export class AddedToCartToastComponentsModule {}
