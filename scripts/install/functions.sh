1e5b9e576fcfc4f0e7fef34f35885e9ca202833b
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-09-24 19:50:30 -0400

    attempting to build and install everything again

--- scripts/install/functions.sh
+++ scripts/install/functions.sh
@@ -201,8 +201,7 @@ function install_from_sources {
         pushd ../.. > /dev/null
         CLONE_DIR=`pwd`
         echo "CLONE DIR: ${CLONE_DIR}"
-#        yarn build:libs
-        ng build core --configuration production && ng build storefrontlib --configuration production 
+        yarn build:libs
         
         popd > /dev/null
     else
@@ -226,22 +225,22 @@ function install_from_sources {
     local dist_packages=(
         'core'
         'storefrontlib'
-#         'assets'
-#         'checkout'
-#         'product'
-#         'setup'
-#         'cart'
-#          'order'
-#         'asm'
-#         'user'
-#         'organization'
-#         'storefinder'
-#         'tracking'
-#         'qualtrics'
-#         'smartedit'
-#         'cds'
-#         'cdc'
-#         'product-configurator'
+        'assets'
+        'checkout'
+        'product'
+        'setup'
+        'cart'
+         'order'
+        'asm'
+        'user'
+        'organization'
+        'storefinder'
+        'tracking'
+        'qualtrics'
+        'smartedit'
+        'cds'
+        'cdc'
+        'product-configurator'
     )
 
     local packages=(
@@ -253,9 +252,9 @@ function install_from_sources {
         publish_dist_package ${package} 
     done
 
-    # for package in ${packages[@]}; do
-    #     publish_package ${package} 
-    # done
+    for package in ${packages[@]}; do
+        publish_package ${package} 
+    done
 
     create_apps
 
