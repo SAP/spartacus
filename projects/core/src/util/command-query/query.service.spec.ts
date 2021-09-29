44313d90de5613ff4ad380ceb71b522271d76205
Marcin Lasak <lasakmarcin90@gmail.com>
2021-09-29 14:42:19 +0200

    test: Unit tests for QueryService

--- projects/core/src/util/command-query/query.service.spec.ts
+++ projects/core/src/util/command-query/query.service.spec.ts
@@ -1,14 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Query, QueryService } from './query.service';
+import { Query, QueryService, QueryState } from './query.service';
 import { defer, of, Subject } from 'rxjs';
+import { skip, take } from 'rxjs/operators';
+import { CxEvent, EventService } from '@spartacus/core';
+
+class TestingEvent extends CxEvent {
+  static readonly type = 'TestingEvent';
+}
 
 describe('QueryService', () => {
   let service: QueryService;
+  let eventService: EventService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [EventService],
+    });
     service = TestBed.inject(QueryService);
+    eventService = TestBed.inject(EventService);
   });
 
   it('should be created', () => {
@@ -24,15 +34,25 @@ describe('QueryService', () => {
   describe('query', () => {
     let query: Query<string>;
     let loadingStream$: Subject<string>;
+    let resetTrigger$: Subject<boolean>;
     let loadingStreamAccessed: boolean;
+    let loaderFactoryCalls: number;
 
     beforeEach(() => {
       loadingStreamAccessed = false;
-      query = service.create(() =>
-        defer(() => {
-          loadingStreamAccessed = true;
-          return loadingStream$;
-        })
+      resetTrigger$ = new Subject<boolean>();
+      loaderFactoryCalls = 0;
+      query = service.create(
+        () =>
+          defer(() => {
+            loadingStreamAccessed = true;
+            loaderFactoryCalls++;
+            return loadingStream$.pipe(take(1));
+          }),
+        {
+          reloadOn: [TestingEvent],
+          resetOn: [resetTrigger$.asObservable()],
+        }
       );
       loadingStream$ = new Subject<string>();
     });
@@ -40,14 +60,153 @@ describe('QueryService', () => {
     it('should not load if not accessed', () => {
       const data = query.get();
       expect(data).toBeDefined();
-      expect(loadingStreamAccessed).toBeFalse();
+      expect(loadingStreamAccessed).toBeFalsy();
     });
 
     it('should load if first accessed', () => {
-      query.get().subscribe().unsubscribe();
       const data = query.get();
+      data.pipe(take(1)).subscribe();
       expect(data).toBeDefined();
-      expect(loadingStreamAccessed).toBeTrue();
+      expect(loadingStreamAccessed).toBeTruthy();
+    });
+
+    describe('getState', () => {
+      it('should emit loading state on first emission', (done) => {
+        const data = query.getState();
+        data.pipe(take(1)).subscribe((state) => {
+          expect(state).toEqual(
+            jasmine.objectContaining({
+              loading: true,
+              error: false,
+              data: undefined,
+            })
+          );
+          done();
+        });
+      });
+
+      it('should emit value returned by the loaderFactory', (done) => {
+        const data = query.getState();
+        data.pipe(take(2), skip(1)).subscribe((state) => {
+          expect(state).toEqual(
+            jasmine.objectContaining({
+              loading: false,
+              error: false,
+              data: 'test-value',
+            })
+          );
+          done();
+        });
+        loadingStream$.next('test-value');
+      });
+
+      it('should emit error state thrown by the loaderFactory', (done) => {
+        const data = query.getState();
+        data.pipe(take(2), skip(1)).subscribe((state) => {
+          expect(state).toEqual(
+            jasmine.objectContaining({
+              loading: false,
+              data: undefined,
+            })
+          );
+          expect((state.error as Error).message).toEqual('test-error');
+          done();
+        });
+        loadingStream$.error(new Error('test-error'));
+      });
+
+      it('should emit new value after reload trigger', (done) => {
+        const data = query.getState();
+        const states: QueryState<string>[] = [];
+        data.pipe(take(4), skip(2)).subscribe((state) => {
+          states.push(state);
+          if (states.length === 2) {
+            expect(states[0]).toEqual(
+              jasmine.objectContaining({
+                loading: true,
+                error: false,
+                data: 'test-value',
+              })
+            );
+            expect(states[1]).toEqual(
+              jasmine.objectContaining({
+                loading: false,
+                error: false,
+                data: 'test-value-2',
+              })
+            );
+            done();
+          }
+        });
+        loadingStream$.next('test-value');
+        eventService.dispatch(new TestingEvent());
+        loadingStream$.next('test-value-2');
+      });
+
+      it('should emit undefined on reset trigger', (done) => {
+        const data = query.getState();
+        const states: QueryState<string>[] = [];
+        data.pipe(take(5), skip(2)).subscribe((state) => {
+          states.push(state);
+          if (states.length === 3) {
+            expect(states[0]).toEqual(
+              jasmine.objectContaining({
+                loading: false,
+                error: false,
+                data: undefined,
+              })
+            );
+            expect(states[1]).toEqual(
+              jasmine.objectContaining({
+                loading: true,
+                error: false,
+                data: undefined,
+              })
+            );
+            expect(states[2]).toEqual(
+              jasmine.objectContaining({
+                loading: false,
+                error: false,
+                data: 'test-value-2',
+              })
+            );
+            done();
+          }
+        });
+        loadingStream$.next('test-value');
+        resetTrigger$.next(true);
+        loadingStream$.next('test-value-2');
+      });
+
+      it('should not call multiple times loaderFactory on multiple subscriptions', () => {
+        const data = query.getState();
+        data.pipe(take(2)).subscribe();
+        data.pipe(take(2)).subscribe();
+        loadingStream$.next('test-value');
+        expect(loaderFactoryCalls).toEqual(1);
+      });
+      it('should call loaderFactory when the query was unsubscribed during previous loading', () => {
+        const data = query.getState();
+        data.pipe(take(1)).subscribe();
+        data.pipe(take(1)).subscribe();
+        expect(loaderFactoryCalls).toEqual(2);
+      });
+    });
+
+    describe('get', () => {
+      it('should return value property from getState', (done) => {
+        const data = query.get();
+        const states: (string | undefined)[] = [];
+        data.pipe(take(2)).subscribe((state) => {
+          states.push(state);
+          if (states.length === 2) {
+            expect(states[0]).toEqual(undefined);
+            expect(states[1]).toEqual('test-value');
+            done();
+          }
+        });
+        loadingStream$.next('test-value');
+      });
     });
   });
 });
