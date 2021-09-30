0137080717c152a1869e55656ebb01c03ce8378b
Jonathan Hsu <jonathan.hsu@sap.com>
2021-09-30 16:53:47 -0400

    fix: translation object #13345

--- feature-libs/cart/added-to-cart-toast/assets/translations/en/added-to-cart-toast.i18n.ts
+++ feature-libs/cart/added-to-cart-toast/assets/translations/en/added-to-cart-toast.i18n.ts
@@ -1,8 +1,10 @@
 export const addedToCartToast = {
-  entriesAdded: '{{ quantity }} items added',
-  entryAdded: '{{ quantity }} item added',
-  continueShopping: 'Continue Shopping',
-  goToCart: 'Go to Cart',
-  quantity: 'Qty',
-  price: 'Price',
+  addedToCartToast: {
+    entriesAdded: '{{ quantity }} items added',
+    entryAdded: '{{ quantity }} item added',
+    continueShopping: 'Continue Shopping',
+    goToCart: 'Go to Cart',
+    quantity: 'Qty',
+    price: 'Price',
+  },
 };
