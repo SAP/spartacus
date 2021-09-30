4cb9269bdc7361f9fbb31750a2f574fe20277985
Jonathan Hsu <jonathan.hsu@sap.com>
2021-09-30 14:50:01 -0400

    feat: add index and ng-packagr files #13345

---
+++ feature-libs/cart/added-to-cart-toast/root/feature-name.ts
@@ -0,0 +1,1 @@
+export const ADDED_TO_CART_TOAST_FEATURE = 'addedToCartToast';
---
+++ feature-libs/cart/added-to-cart-toast/root/index.ts
@@ -0,0 +1,4 @@
+export * from './added-to-cart-toast-root.module';
+export * from './added-to-cart-toast-loader.module';
+export * from './services/added-to-cart-toast-enabler.service';
+export * from './feature-name';
