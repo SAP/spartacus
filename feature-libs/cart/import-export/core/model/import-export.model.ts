157633a086c9faa90f0278e555aab5b3a22efdd7
Michał Gruca <pucek9@gmail.com>
2021-10-05 01:36:08 +0200

    up

--- feature-libs/cart/import-export/core/model/import-export.model.ts
+++ feature-libs/cart/import-export/core/model/import-export.model.ts
@@ -1,9 +1,9 @@
 import { CmsComponent } from '@spartacus/core';
 
 export enum CartTypes {
-  NEW_SAVED_CART = 'savedCarts',
-  EXISTING_SAVED_CART = 'savedCartsDetails',
-  ACTIVE_CART = 'cart',
+  NEW_SAVED_CART,
+  EXISTING_SAVED_CART,
+  ACTIVE_CART,
 }
 
 export interface CmsImportExportComponent extends CmsComponent {
