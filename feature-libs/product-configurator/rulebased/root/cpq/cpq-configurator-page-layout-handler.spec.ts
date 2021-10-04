88bbbc5ce64df1163ea898d18cd47013752c983c
Christoph Hinssen <christoph.hinssen@sap.com>
2021-10-04 08:25:15 +0200

    GH-13807 Add test

--- feature-libs/product-configurator/rulebased/root/cpq/cpq-configurator-page-layout-handler.spec.ts
+++ feature-libs/product-configurator/rulebased/root/cpq/cpq-configurator-page-layout-handler.spec.ts
@@ -1,18 +1,56 @@
 import { Type } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  ConfiguratorModelUtils,
+  ConfiguratorRouter,
+  ConfiguratorRouterExtractorService,
+} from '@spartacus/product-configurator/common';
+import { cold } from 'jasmine-marbles';
 import { Observable, of } from 'rxjs';
 
 import { CpqConfiguratorPageLayoutHandler } from './cpq-configurator-page-layout-handler';
 
+const standardRouterData: ConfiguratorRouter.Data = {
+  owner: ConfiguratorModelUtils.createInitialOwner(),
+};
+let routerData: ConfiguratorRouter.Data;
+class MockRouterExtractorService {
+  extractRouterData(): Observable<ConfiguratorRouter.Data> {
+    return of(routerData);
+  }
+}
+const headerSlots = ['SiteLogo', 'MiniCart'];
+const headerSlotsIncludingPreHeader = ['PreHeader', 'SiteLogo', 'MiniCart'];
+const contentSlots = [
+  'CpqConfigHeader',
+  'CpqConfigBanner',
+  'CpqConfigMenu',
+  'CpqConfigContent',
+  'CpqConfigOverviewBanner',
+  'CpqConfigOverviewContent',
+  'CpqConfigBottombar',
+];
+const pageTemplateCpq = 'CpqConfigurationTemplate';
+const pageTemplateOther = 'OtherTemplate';
+const sectionHeader = 'header';
+const sectionContent = 'content';
+
 describe('CpqConfiguratorPageLayoutHandler', () => {
   let serviceUnderTest: CpqConfiguratorPageLayoutHandler;
-  let slots$: Observable<string[]> = of(['a']);
-  let pageTemplate = 'a';
-  let section = 'c';
 
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          {
+            provide: ConfiguratorRouterExtractorService,
+            useClass: MockRouterExtractorService,
+          },
+        ],
+      }).compileComponents();
+    })
+  );
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-
     serviceUnderTest = TestBed.inject(
       CpqConfiguratorPageLayoutHandler as Type<CpqConfiguratorPageLayoutHandler>
     );
@@ -22,17 +60,67 @@ describe('CpqConfiguratorPageLayoutHandler', () => {
     expect(serviceUnderTest).toBeDefined();
   });
 
-  it('should call getCpqAccessData endpoint', () => {
-    serviceUnderTest.handle(slots$, pageTemplate, section);
-    // serviceUnderTest.getCpqAccessData().subscribe();
-    // const mockReq = httpMock.expectOne((req) => {
-    //   return req.method === 'GET' && req.url === '/getCpqAccessData';
-    // });
-    // expect(occEnpointsService.buildUrl).toHaveBeenCalledWith(
-    //   'getCpqAccessData'
-    // );
-    // expect(mockReq.cancelled).toBeFalsy();
-    // expect(mockReq.request.responseType).toEqual('json');
-    // mockReq.flush(accessData);
+  it('should not touch slots for section different than header', () => {
+    let slots$ = cold('-a', {
+      a: contentSlots,
+    });
+    const handledSlots$ = serviceUnderTest.handle(
+      slots$,
+      pageTemplateCpq,
+      sectionContent
+    );
+    expect(handledSlots$).toBeObservable(slots$);
+  });
+
+  it('should change slots for header section in cpq template in case we are on configuration page', () => {
+    routerData = {
+      ...standardRouterData,
+      pageType: ConfiguratorRouter.PageType.CONFIGURATION,
+    };
+    let slots$ = cold('-a-a', {
+      a: headerSlots,
+    });
+    const handledSlots$ = serviceUnderTest.handle(
+      slots$,
+      pageTemplateCpq,
+      sectionHeader
+    );
+    expect(handledSlots$).toBeObservable(
+      cold('-a-a', {
+        a: headerSlotsIncludingPreHeader,
+      })
+    );
+  });
+
+  it('should not change slots for header section in cpq template in case we are on overview page', () => {
+    routerData = {
+      ...standardRouterData,
+      pageType: ConfiguratorRouter.PageType.OVERVIEW,
+    };
+    let slots$ = cold('-aaa', {
+      a: headerSlots,
+    });
+    const handledSlots$ = serviceUnderTest.handle(
+      slots$,
+      pageTemplateCpq,
+      sectionHeader
+    );
+    expect(handledSlots$).toBeObservable(slots$);
+  });
+
+  it('should not change slots for header section in other template', () => {
+    routerData = {
+      ...standardRouterData,
+      pageType: ConfiguratorRouter.PageType.CONFIGURATION,
+    };
+    let slots$ = cold('-a-a', {
+      a: headerSlots,
+    });
+    const handledSlots$ = serviceUnderTest.handle(
+      slots$,
+      pageTemplateOther,
+      sectionHeader
+    );
+    expect(handledSlots$).toBeObservable(slots$);
   });
 });
