022108bc986ac66f2485b1560dda3c9af868978e
Michał Gruca <pucek9@gmail.com>
2021-10-05 22:45:03 +0200

    cleanup

--- feature-libs/cart/import-export/core/model/import-export.model.ts
+++ feature-libs/cart/import-export/core/model/import-export.model.ts
@@ -1,14 +1,6 @@
 import { CmsComponent } from '@spartacus/core';
 
-export enum CartTypes {
-  NEW_SAVED_CART = 'NEW_SAVED_CART',
-  SAVED_CART = 'SAVED_CART',
-  ACTIVE_CART = 'ACTIVE_CART',
-  QUICK_ORDER = 'QUICK_ORDER',
-}
-
 export interface CmsImportExportComponent extends CmsComponent {
   importButtonDisplayRoutes: string[];
   exportButtonDisplayRoutes: string[];
-  routesCartMapping: { [route: string]: CartTypes };
 }
