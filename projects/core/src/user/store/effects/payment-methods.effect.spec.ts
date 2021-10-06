054ae78bd13416c8aa7af47a638beaec35629eeb
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-10-06 09:31:44 -0400

    test: refine test for payment-methods (#13917)

--- projects/core/src/user/store/effects/payment-methods.effect.spec.ts
+++ projects/core/src/user/store/effects/payment-methods.effect.spec.ts
@@ -2,50 +2,32 @@ import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { cold, hot } from 'jasmine-marbles';
 import { Observable, of } from 'rxjs';
-import { GlobalMessageService } from '../../../global-message/index';
+import {
+  GlobalMessageService,
+  GlobalMessageType,
+} from '../../../global-message/index';
 import { PaymentDetails } from '../../../model/cart.model';
-import { Address } from '../../../model/address.model';
+import { Translatable } from '@spartacus/core';
 import { OCC_USER_ID_CURRENT } from '../../../occ/utils/occ-constants';
 import { UserPaymentAdapter } from '../../connectors/payment/user-payment.adapter';
 import { UserPaymentConnector } from '../../connectors/payment/user-payment.connector';
 import { UserActions } from '../actions/index';
 import * as fromPaymentMethodsEffect from './payment-methods.effect';
 
-class MockGlobalMessageService {
-  add = jasmine.createSpy();
+class MockGlobalMessageService implements Partial<GlobalMessageService> {
+  add(
+    _text: string | Translatable,
+    _type: GlobalMessageType,
+    _timeout?: number
+  ): void {}
 }
 
-const mockAddress: Address = {
-  firstName: 'John',
-  lastName: 'Doe',
-  titleCode: 'mr',
-  line1: 'Toyosaki 2 create on cart',
-  line2: 'line2',
-  town: 'town',
-  region: { isocode: 'JP-27' },
-  postalCode: 'zip',
-  country: { isocode: 'JP' },
-};
-
 const mockPaymentMethods: PaymentDetails[] = [{ id: '3710178129845' }];
-const mockPaymentMethod: PaymentDetails = {
-  accountHolderName: 'John Smith',
-  billingAddress: mockAddress,
-  cardNumber: '1111222233334444',
-  cardType: {
-    code: 'visa',
-    name: 'Visa',
-  },
-  cvn: '111',
-  defaultPayment: true,
-  expiryMonth: '01',
-  expiryYear: '2030',
-  id: '1298453710178',
-};
 
 describe('Payment methods effect', () => {
   let userPaymentMethodsEffect: fromPaymentMethodsEffect.UserPaymentMethodsEffects;
   let userPaymentConnector: UserPaymentConnector;
+  let globalMessageService: GlobalMessageService;
   let actions$: Observable<any>;
 
   beforeEach(() => {
@@ -62,6 +44,7 @@ describe('Payment methods effect', () => {
       fromPaymentMethodsEffect.UserPaymentMethodsEffects
     );
     userPaymentConnector = TestBed.inject(UserPaymentConnector);
+    globalMessageService = TestBed.inject(GlobalMessageService);
 
     spyOn(userPaymentConnector, 'getAll').and.returnValue(
       of(mockPaymentMethods)
@@ -97,12 +80,15 @@ describe('Payment methods effect', () => {
 
       actions$ = hot('-a', { a: action });
       const expected = cold('-b', { b: completion });
-      expect(userPaymentMethodsEffect.setDefaultUserPaymentMethod$).toBeObservable(expected);
+      expect(
+        userPaymentMethodsEffect.setDefaultUserPaymentMethod$
+      ).toBeObservable(expected);
     });
   });
 
   describe('deleteUserPaymentMethod$', () => {
     it('should delete user payment method', () => {
+      spyOn(globalMessageService, 'add').and.callThrough();
       const action = new UserActions.DeleteUserPaymentMethod({
         userId: OCC_USER_ID_CURRENT,
         paymentMethodID: '3710178129845',
@@ -111,7 +97,13 @@ describe('Payment methods effect', () => {
       actions$ = hot('-a', { a: action });
 
       const expected = cold('-b', { b: completion });
-      expect(userPaymentMethodsEffect.deleteUserPaymentMethod$).toBeObservable(expected);
+      expect(userPaymentMethodsEffect.deleteUserPaymentMethod$).toBeObservable(
+        expected
+      );
+      expect(globalMessageService.add).toHaveBeenCalledWith(
+        { key: 'paymentCard.deletePaymentSuccess' },
+        GlobalMessageType.MSG_TYPE_CONFIRMATION
+      );
     });
   });
 });
