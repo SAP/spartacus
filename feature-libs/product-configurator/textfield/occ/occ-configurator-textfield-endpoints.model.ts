4fe7f67bb71e106e95699ff74efa7b38c4e6ca32
Christoph Hinssen <christoph.hinssen@sap.com>
2021-10-06 06:17:29 +0200

    GH-13939 Only extract new occ endpoint to feature lib

--- feature-libs/product-configurator/textfield/occ/occ-configurator-textfield-endpoints.model.ts
+++ feature-libs/product-configurator/textfield/occ/occ-configurator-textfield-endpoints.model.ts
@@ -1,29 +1,6 @@
 import { OccEndpoint } from '@spartacus/core';
 declare module '@spartacus/core' {
   interface OccEndpoints {
-    /**
-     * Endpoint for create configuration for the textfield configurator
-     *
-     * @member {string}
-     */
-    createTextfieldConfiguration?: string;
-    /**
-     * Endpoint for add textfield configuration to cart
-     *
-     * @member {string}
-     */
-    addTextfieldConfigurationToCart?: string;
-    /**
-     * Endpoint for reading textfield configuration attached to the cart entry
-     */
-    readTextfieldConfigurationForCartEntry?: string;
-    /**
-     * Endpoint for reading textfield configuration attached to the order entry
-     */
     readTextfieldConfigurationForOrderEntry?: string | OccEndpoint;
-    /**
-     * Endpoint for updating textfield configuration attached to the cart entry
-     */
-    updateTextfieldConfigurationForCartEntry?: string;
   }
 }
