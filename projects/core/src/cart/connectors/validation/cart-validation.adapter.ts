f8c95f4702f12537e8ac0c0bc20428fbbf5655ca
Patryk Smul <psmul@divante.pl>
2021-10-07 09:33:53 +0200

    Update projects/core/src/cart/connectors/validation/cart-validation.adapter.ts
    Co-authored-by: Marcin Lasak <lasakmarcin90@gmail.com>

--- projects/core/src/cart/connectors/validation/cart-validation.adapter.ts
+++ projects/core/src/cart/connectors/validation/cart-validation.adapter.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { CartModificationList } from '../../../model/cart.model';
 
 export abstract class CartValidationAdapter {
-  abstract load(
+  abstract validate(
     cartId: string,
     userId: string
   ): Observable<CartModificationList>;
