fe9022937267cb881f15b731a6987daec37dc504
Michał Gruca <pucek9@gmail.com>
2021-10-07 13:26:36 +0200

    try fix build

--- feature-libs/cart/import-export/root/import-export-root.module.ts
+++ feature-libs/cart/import-export/root/import-export-root.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { provideDefaultConfig } from '@spartacus/core';
-import { OrderDetailsService } from '@spartacus/order/components';
 import { CART_IMPORT_EXPORT_FEATURE } from './feature-name';
 
 @NgModule({
   providers: [
-    OrderDetailsService,
     provideDefaultConfig({
       featureModules: {
         [CART_IMPORT_EXPORT_FEATURE]: {
