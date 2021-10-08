d5ac24cb47bd509db3d461ac52390f68ba165b29
Patrick Labadie <patrick.labadie@sap.com>
2021-10-08 12:10:23 -0400

    GH-13909: Use CartActions from cart lib

--- feature-libs/checkout/core/store/effects/replenishment-order.effect.ts
+++ feature-libs/checkout/core/store/effects/replenishment-order.effect.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { CartActions, normalizeHttpError } from '@spartacus/core';
+import { CartActions } from '@spartacus/cart/main/core';
+import { normalizeHttpError } from '@spartacus/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
 import { CheckoutReplenishmentOrderConnector } from '../../connectors/index';
