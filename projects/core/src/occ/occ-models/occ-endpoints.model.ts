5f25f5b8d338481cd28bd0966ccf18d093019ecf
Weizheng Gao <weizheng.gao@sap.com>
2021-10-08 15:04:25 -0300

    feature/GH-13914: fix an issue in config augmentation

--- projects/core/src/occ/occ-models/occ-endpoints.model.ts
+++ projects/core/src/occ/occ-models/occ-endpoints.model.ts
@@ -68,54 +68,6 @@ export interface OccEndpoints {
    */
   page?: string | OccEndpoint;
   /**
-   * Get all carts
-   *
-   * @member {string} [carts]
-   */
-  carts?: string | OccEndpoint;
-  /**
-   * Get a cart with a given identifier
-   *
-   * @member {string} [cart]
-   */
-  cart?: string | OccEndpoint;
-  /**
-   * Creates or restore a cart for a user
-   *
-   * @member {string} [createCart]
-   */
-  createCart?: string | OccEndpoint;
-  /**
-   * Deletes a cart with a given cart id
-   *
-   * @member {string} [deleteCart]
-   */
-  deleteCart?: string | OccEndpoint;
-  /**
-   * Adds a product to the cart
-   *
-   * @member {string} [addEntries]
-   */
-  addEntries?: string | OccEndpoint;
-  /**
-   * Update quantity and store the details of a cart entry
-   *
-   * @member {string} [updateEntries]
-   */
-  updateEntries?: string | OccEndpoint;
-  /**
-   * Deletes cart entry
-   *
-   * @member {string} [removeEntries]
-   */
-  removeEntries?: string | OccEndpoint;
-  /**
-   * Assign email to cart
-   *
-   * @member {string} [addEmail]
-   */
-  addEmail?: string | OccEndpoint;
-  /**
    * Get a list of available languages
    *
    * @member {string}
@@ -287,12 +239,6 @@ export interface OccEndpoints {
    * @member {string}
    */
   consignmentTracking?: string | OccEndpoint;
-  /**
-   * Endpoint for cart voucher
-   *
-   * @member {string}
-   */
-  cartVoucher?: string | OccEndpoint;
   /**
    * Endpoint for coupons
    *
@@ -666,11 +612,6 @@ export interface OccEndpoints {
    * @member {string}
    */
   orderApprovalDecision?: string | OccEndpoint;
-  /**
-   * Explicitly saves a cart
-   *
-   * @member {string}
-   */
-  saveCart?: string | OccEndpoint;
+
   // DEPRECATION END
 }
