a7de451bb761e84d93ea03017e14561be0979a9c
Marcin Lasak <lasakmarcin90@gmail.com>
2021-10-13 19:43:40 +0200

    chore: Improve typing in CartValidationService

--- projects/core/src/cart/facade/cart-validation.service.ts
+++ projects/core/src/cart/facade/cart-validation.service.ts
@@ -1,31 +1,36 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, Observable } from 'rxjs';
-import { CartValidationConnector } from '../connectors/validation/cart-validation.connector';
-import { CartModificationList } from '../../model/cart.model';
 import { switchMap, take } from 'rxjs/operators';
-import { Command, CommandService, CommandStrategy } from '../../util/index';
 import { UserIdService } from '../../auth/user-auth/facade/user-id.service';
+import { CartModificationList } from '../../model/cart.model';
+import {
+  Command,
+  CommandService,
+  CommandStrategy,
+} from '../../util/command-query/command.service';
+import { CartValidationConnector } from '../connectors/validation/cart-validation.connector';
 import { ActiveCartService } from './active-cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartValidationService {
-  protected validateCartCommand: Command = this.command.create(
-    () =>
-      combineLatest([
-        this.activeCartService.getActiveCartId(),
-        this.userIdService.takeUserId(),
-      ]).pipe(
-        take(1),
-        switchMap(([cartId, userId]) =>
-          this.cartValidationConnector.validate(cartId, userId)
-        )
-      ),
-    {
-      strategy: CommandStrategy.CancelPrevious,
-    }
-  );
+  protected validateCartCommand: Command<void, CartModificationList> =
+    this.command.create(
+      () =>
+        combineLatest([
+          this.activeCartService.getActiveCartId(),
+          this.userIdService.takeUserId(),
+        ]).pipe(
+          take(1),
+          switchMap(([cartId, userId]) =>
+            this.cartValidationConnector.validate(cartId, userId)
+          )
+        ),
+      {
+        strategy: CommandStrategy.CancelPrevious,
+      }
+    );
 
   constructor(
     protected cartValidationConnector: CartValidationConnector,
@@ -41,6 +46,6 @@ export class CartValidationService {
    * @param userId
    */
   validateCart(): Observable<CartModificationList> {
-    return this.validateCartCommand.execute(undefined);
+    return this.validateCartCommand.execute();
   }
 }
