27c152c844a5179762c62f786f247feb7d6bd13b
Patrick Labadie <patrick.labadie@sap.com>
2021-10-15 13:03:04 -0400

    GH-13909: Move CART_VOUCHER_NORMALIZER in cart/main/root

--- feature-libs/cart/main/root/tokens/converters.ts
+++ feature-libs/cart/main/root/tokens/converters.ts
@@ -1,6 +1,11 @@
 import { InjectionToken } from '@angular/core';
 import { Converter } from '@spartacus/core';
-import { Cart, CartModification, SaveCartResult } from '../models/cart.model';
+import {
+  Cart,
+  CartModification,
+  SaveCartResult,
+  Voucher,
+} from '../models/cart.model';
 
 export const CART_NORMALIZER = new InjectionToken<Converter<any, Cart>>(
   'CartNormalizer'
@@ -13,3 +18,7 @@ export const CART_MODIFICATION_NORMALIZER = new InjectionToken<
 export const SAVE_CART_NORMALIZER = new InjectionToken<
   Converter<any, SaveCartResult>
 >('SaveCartNormalizer');
+
+export const CART_VOUCHER_NORMALIZER = new InjectionToken<
+  Converter<any, Voucher>
+>('CartVoucherNormalizer');
