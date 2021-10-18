cc1de5f8c7bc28686bb00af2029e69e9ada47afb
Michal Dydo <mdydo@divante.pl>
2021-10-18 09:30:11 +0200

    fix: Checkout fix after re-login during checkout for 3.x lib (#13489)

--- projects/storefrontlib/src/cms-components/checkout/services/checkout-details.service.ts
+++ projects/storefrontlib/src/cms-components/checkout/services/checkout-details.service.ts
@@ -10,7 +10,6 @@ import {
 } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import {
-  distinctUntilChanged,
   filter,
   map,
   shareReplay,
@@ -46,8 +45,11 @@ export class CheckoutDetailsService {
     );
 
     this.getCheckoutDetailsLoaded$ = this.cartId$.pipe(
-      distinctUntilChanged(),
-      tap((cartId) => this.checkoutService.loadCheckoutDetails(cartId)),
+      tap((cartId) => {
+        if (cartId) {
+          this.checkoutService.loadCheckoutDetails(cartId);
+        }
+      }),
       shareReplay(1),
       switchMap(() => this.checkoutService.getCheckoutDetailsLoaded()),
       skipWhile((loaded) => !loaded)
