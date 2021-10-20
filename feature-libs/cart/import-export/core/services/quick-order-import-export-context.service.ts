808a1e06680a4a353092324350e44aa91f87bb21
Michał Gruca <pucek9@gmail.com>
2021-10-20 18:24:37 +0200

    solve https://github.com/SAP/spartacus/pull/11794#discussion_r732639019

--- feature-libs/cart/import-export/core/services/quick-order-import-export-context.service.ts
+++ feature-libs/cart/import-export/core/services/quick-order-import-export-context.service.ts
@@ -23,38 +23,42 @@ export class QuickOrderImportExportContext implements ImportExportContext {
     protected productAdapter: ProductAdapter
   ) {}
 
-  results$ = new Subject<ProductImportInfo>();
-
   getEntries(): Observable<OrderEntry[]> {
     return this.quickOrderService.getEntries();
   }
 
   addEntries(products: ProductData[]): Observable<ProductImportInfo> {
+    const results$ = new Subject<ProductImportInfo>();
+
     products.forEach((productData) => {
       this.productAdapter
         .load(productData.productCode)
         .pipe(
           take(1),
           tap((product: Product) => {
-            this.handleResults(product, productData);
+            this.handleResults(product, productData, results$);
             this.quickOrderService.addProduct(product, productData.quantity);
           }),
           catchError((response: HttpErrorResponse) => {
-            this.handleErrors(response, productData.productCode);
+            this.handleErrors(response, productData.productCode, results$);
             return of(response);
           })
         )
         .subscribe();
     });
-    return this.results$.pipe(take(products.length));
+    return results$.pipe(take(products.length));
   }
 
-  protected handleResults(product: Product, productData: ProductData) {
+  protected handleResults(
+    product: Product,
+    productData: ProductData,
+    results$: Subject<ProductImportInfo>
+  ) {
     if (
       product.stock?.stockLevel &&
       productData.quantity >= product.stock.stockLevel
     ) {
-      this.results$.next({
+      results$.next({
         productCode: productData.productCode,
         productName: product?.name,
         statusCode: ProductImportStatus.LOW_STOCK,
@@ -62,22 +66,26 @@ export class QuickOrderImportExportContext implements ImportExportContext {
         quantityAdded: product.stock.stockLevel,
       });
     } else if (product.stock?.stockLevelStatus === 'outOfStock') {
-      this.results$.next({
+      results$.next({
         productCode: productData.productCode,
         statusCode: ProductImportStatus.NO_STOCK,
         productName: product?.name,
       });
     } else {
-      this.results$.next({
+      results$.next({
         productCode: productData.productCode,
         statusCode: ProductImportStatus.SUCCESS,
       });
     }
   }
 
-  protected handleErrors(response: HttpErrorResponse, productCode: string) {
+  protected handleErrors(
+    response: HttpErrorResponse,
+    productCode: string,
+    results$: Subject<ProductImportInfo>
+  ) {
     if (response?.error?.errors[0].type === 'UnknownIdentifierError') {
-      this.results$.next({
+      results$.next({
         productCode,
         statusCode: ProductImportStatus.UNKNOWN_IDENTIFIER,
       });
@@ -88,7 +96,7 @@ export class QuickOrderImportExportContext implements ImportExportContext {
           response
         );
       }
-      this.results$.next({
+      results$.next({
         productCode,
         statusCode: ProductImportStatus.UNKNOWN_ERROR,
       });
