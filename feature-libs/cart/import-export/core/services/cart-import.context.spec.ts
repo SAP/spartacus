051a2b1cb7cf7183d64536373f18bdb7063a09f5
Michał Gruca <pucek9@gmail.com>
2021-10-21 15:13:40 +0200

    move unit tests to proper place

--- feature-libs/cart/import-export/core/services/cart-import.context.spec.ts
+++ feature-libs/cart/import-export/core/services/cart-import.context.spec.ts
@@ -1,40 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { OrderEntry } from '@spartacus/core';
-import { ImportContext, ProductData } from '@spartacus/cart/import-export/core';
-import { Action, ActionsSubject } from '@ngrx/store';
-import { Observable, of, Subject } from 'rxjs';
-import { CartImportContext } from './cart-import.context';
-import { CartTypes } from '../model';
 import { Injectable } from '@angular/core';
-import createSpy = jasmine.createSpy;
+import { TestBed } from '@angular/core/testing';
+import { Action, ActionsSubject } from '@ngrx/store';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { CartActions } from '@spartacus/core';
+import {
+  ProductData,
+  ProductImportStatus,
+} from '@spartacus/cart/import-export/core';
+import { CartTypes } from '../model';
+import { CartImportContext } from './cart-import.context';
+import { ImportContext } from './import.context';
 
-const mockActionsSubject = new Subject<Action>();
+const mockActionsSubject = new BehaviorSubject<Action>(null);
 
 const mockCartId = '00004546';
-
-const mockProductsData: ProductData[] = [
-  {
-    productCode: '123456',
-    quantity: 2,
-  },
-  {
-    productCode: '789123',
-    quantity: 4,
-  },
-];
-
-const mockEntries: OrderEntry[] = [
-  {
-    quantity: 1,
-    product: { name: 'mockProduct', code: 'mockCode' },
-  },
-];
-
-class MockTestCartService {
-  addEntries = createSpy().and.callThrough();
-  getEntries = createSpy().and.returnValue(of(mockEntries));
-  getActiveCartId = createSpy().and.returnValue(of(mockCartId));
-}
 
 @Injectable({
   providedIn: 'root',
@@ -43,47 +22,25 @@ class TestCartImportExportContext
   extends CartImportContext
   implements ImportContext
 {
-  constructor(
-    protected actionsSubject: ActionsSubject,
-    protected testCartService: MockTestCartService
-  ) {
+  constructor(protected actionsSubject: ActionsSubject) {
     super(actionsSubject);
   }
 
   readonly type = 'TEST_CART' as CartTypes;
 
-  getEntries(): Observable<OrderEntry[]> {
-    return this.testCartService.getEntries();
-  }
-
   protected add(products: ProductData[]): Observable<string> {
-    this.testCartService.addEntries(this.mapProductsToOrderEntries(products));
-    return this.testCartService.getActiveCartId();
-  }
-
-  protected mapProductsToOrderEntries(products: ProductData[]): OrderEntry[] {
-    return products.map(
-      (product: { productCode: string; quantity: number }) => ({
-        product: { code: product.productCode },
-        quantity: product.quantity,
-      })
-    );
+    return of(mockCartId);
   }
 }
 
 describe('CartImportExportContext', () => {
   let service: TestCartImportExportContext;
-  let testCartService: MockTestCartService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        { useValue: mockActionsSubject, provide: ActionsSubject },
-        MockTestCartService,
-      ],
+      providers: [{ useValue: mockActionsSubject, provide: ActionsSubject }],
     });
     service = TestBed.inject(TestCartImportExportContext);
-    testCartService = TestBed.inject(MockTestCartService);
   });
 
   it('should be created', () => {
@@ -91,26 +48,117 @@ describe('CartImportExportContext', () => {
   });
 
   describe('addEntries', () => {
-    it('addEntries for test cart', () => {
-      service.addEntries(mockProductsData).subscribe().unsubscribe();
+    it('should return success action', () => {
+      let action;
+      service.addEntries(mockProductData).subscribe((data) => (action = data));
 
-      expect(testCartService.addEntries).toHaveBeenCalledWith(mockProductsData);
-      expect(testCartService.getActiveCartId).toHaveBeenCalledWith();
-    });
-  });
-
-  describe('getEntries', () => {
-    it('getEntries from test cart', () => {
-      let entries: OrderEntry[];
-      service
-        .getEntries()
-        .subscribe((result) => {
-          entries = result;
+      mockActionsSubject.next(
+        new CartActions.CartAddEntrySuccess({
+          userId: mockUserId,
+          cartId: mockCartId,
+          productCode: '693923',
+          quantity: 1,
+          entry: { product: { name: 'mockProduct1' } },
+          quantityAdded: 1,
+          statusCode: ProductImportStatus.SUCCESS,
         })
-        .unsubscribe();
+      );
 
-      expect(testCartService.getEntries).toHaveBeenCalledWith();
-      expect(entries).toEqual(mockEntries);
+      expect(action).toEqual({
+        productCode: '693923',
+        statusCode: ProductImportStatus.SUCCESS,
+        productName: 'mockProduct1',
+      });
+    });
+
+    it('should return low stock action', () => {
+      let action;
+      service.addEntries(mockProductData).subscribe((data) => (action = data));
+
+      mockActionsSubject.next(
+        new CartActions.CartAddEntrySuccess({
+          userId: mockUserId,
+          cartId: mockCartId,
+          productCode: '693923',
+          entry: { product: { name: 'mockProduct1' } },
+          quantity: 4,
+          quantityAdded: 1,
+          statusCode: ProductImportStatus.LOW_STOCK,
+        })
+      );
+
+      expect(action).toEqual({
+        productName: 'mockProduct1',
+        quantity: 4,
+        quantityAdded: 1,
+        productCode: '693923',
+        statusCode: ProductImportStatus.LOW_STOCK,
+      });
+    });
+
+    it('should return no stock action', () => {
+      let action;
+      service.addEntries(mockProductData).subscribe((data) => (action = data));
+
+      mockActionsSubject.next(
+        new CartActions.CartAddEntrySuccess({
+          userId: mockUserId,
+          cartId: mockCartId,
+          productCode: '693923',
+          entry: { product: { name: 'mockProduct1' } },
+          quantity: 4,
+          quantityAdded: 0,
+          statusCode: ProductImportStatus.NO_STOCK,
+        })
+      );
+
+      expect(action).toEqual({
+        productCode: '693923',
+        statusCode: ProductImportStatus.NO_STOCK,
+        productName: 'mockProduct1',
+      });
+    });
+
+    it('should return Unknown Identifier Error action', () => {
+      let action;
+      service.addEntries(mockProductData).subscribe((data) => (action = data));
+
+      mockActionsSubject.next(
+        new CartActions.CartAddEntryFail({
+          userId: mockUserId,
+          cartId: mockCartId,
+          productCode: '693923',
+          quantity: 1,
+          error: { details: [{ type: 'UnknownIdentifierError' }] },
+        })
+      );
+
+      expect(action).toEqual({
+        productCode: '693923',
+        statusCode: ProductImportStatus.UNKNOWN_IDENTIFIER,
+      });
+    });
+
+    it('should return unknown error action', () => {
+      let action;
+      service.addEntries(mockProductData).subscribe((data) => (action = data));
+
+      mockActionsSubject.next(
+        new CartActions.CartAddEntrySuccess({
+          userId: mockUserId,
+          cartId: mockCartId,
+          productCode: '693923',
+          entry: { product: { name: 'mockProduct1' } },
+          quantity: 4,
+          quantityAdded: 1,
+          statusCode: 'CODE_WHICH_WE_DIDNT_REGISTER',
+        })
+      );
+
+      expect(action).toEqual({
+        productCode: '693923',
+        statusCode: ProductImportStatus.UNKNOWN_ERROR,
+      });
     });
   });
 });
