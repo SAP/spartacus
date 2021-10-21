aa92175f207f057bff68e294a96c7281645bb81b
Michał Gruca <pucek9@gmail.com>
2021-10-21 23:12:06 +0200

    add unit test

--- feature-libs/cart/quick-order/core/services/quick-order.service.spec.ts
+++ feature-libs/cart/quick-order/core/services/quick-order.service.spec.ts
@@ -426,4 +426,22 @@ describe('QuickOrderService', () => {
         done();
       });
   });
+
+  it('should verify can add product', () => {
+    let result: boolean;
+    service.setListLimit(1);
+    service.addProduct(mockProduct1);
+
+    service.canAdd(mockProduct1Code).subscribe((canAdd) => (result = canAdd));
+    expect(result).toBe(true);
+  });
+
+  it('should verify can not add product', () => {
+    let result: boolean;
+    service.setListLimit(1);
+    service.addProduct(mockProduct1);
+
+    service.canAdd(mockProduct2Code).subscribe((canAdd) => (result = canAdd));
+    expect(result).toBe(false);
+  });
 });
