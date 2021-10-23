fc1668d91c630819d25fbdfe721211c3f7618e68
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-10-23 12:29:57 +0200

    fix: Provide delivery modes connector

--- feature-libs/checkout/core/checkout-core.module.ts
+++ feature-libs/checkout/core/checkout-core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PageMetaResolver } from '@spartacus/core';
 import { CheckoutConnector } from './connectors/checkout/checkout.connector';
+import { CheckoutDeliveryModesConnector } from './connectors/delivery-modes/checkout-delivery-modes.connector';
 import { CheckoutDeliveryConnector } from './connectors/delivery/checkout-delivery.connector';
 import { CheckoutPaymentConnector } from './connectors/payment/checkout-payment.connector';
 import { CheckoutEventModule } from './events/checkout-event.module';
@@ -12,6 +13,7 @@ import { CheckoutPageMetaResolver } from './services/checkout-page-meta.resolver
   providers: [
     ...facadeProviders,
     CheckoutDeliveryConnector,
+    CheckoutDeliveryModesConnector,
     CheckoutConnector,
     CheckoutPaymentConnector,
     CheckoutPageMetaResolver,
