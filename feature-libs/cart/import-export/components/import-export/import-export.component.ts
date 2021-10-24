b3cdb48d81974519788f9ddbc55d91bbc1fcc439
Michał Gruca <pucek9@gmail.com>
2021-10-24 02:52:53 +0200

    context must be public (used in html)

--- feature-libs/cart/import-export/components/import-export/import-export.component.ts
+++ feature-libs/cart/import-export/components/import-export/import-export.component.ts
@@ -15,7 +15,7 @@ import { map, shareReplay, switchMap } from 'rxjs/operators';
 export class ImportExportComponent {
   constructor(protected routingService: RoutingService) {}
 
-  protected context$: Observable<OrderEntriesContext | undefined> =
+  context$: Observable<OrderEntriesContext | undefined> =
     this.routingService
       .getContext<OrderEntriesContext>(ORDER_ENTRIES_CONTEXT)
       .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
