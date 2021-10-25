0ef1bde9f51a6e0b8b7ee1673125dd5ccfea5deb
Mateusz <mateusz.kolasa@sap.com>
2021-10-25 20:19:04 +0200

    fix typo

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `aPngular.json`.
+// The list of which env maps to which file can be found in `angular.json`.
 
 /*
  * For easier debugging in development mode, you can import the following file
