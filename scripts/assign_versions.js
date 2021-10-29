0e26ba3b39364a94f9fd7fe2cf1687cc137497fb
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-10-29 11:03:34 -0400

    Node script to assign versions to libs

---
+++ scripts/assign_versions.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+/**
+ * Script that traverses our libraries and assigns a version to each one (in package.json)
+ **/
+
+const fs = require('fs');
+
+const libs = [
+  'projects/core',
+  'projects/storefrontlib',
+  'projects/storefrontstyles',
+  'projects/assets',
+  'projects/schematics',
+  'core-libs/setup',
+  'feature-libs/asm',
+  'feature-libs/organization',
+  'feature-libs/storefinder',
+  'feature-libs/checkout',
+  'feature-libs/smartedit',
+  'feature-libs/product',
+  'feature-libs/product-configurator',
+  'feature-libs/qualtrics',
+  'feature-libs/cart',
+  'feature-libs/order',
+  'feature-libs/user',
+  'feature-libs/tracking',
+  'integration-libs/cds',
+  'integration-libs/cdc',
+];
+
+function assignVersion(lib) {
+  const filename = `${lib}/package.json`;
+  const fc = fs.readFileSync(filename);
+  const json = JSON.parse(fc);
+  json.version = version;
+  fs.writeFile(filename, JSON.stringify(json), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+const version = process.argv[2];
+
+console.log(`Assigning version ${version} to libs...`);
+
+if (!version) {
+  console.log('No version provided. Aborting.');
+  process.exit(1);
+}
+
+libs.forEach((lib) => assignVersion(lib));
