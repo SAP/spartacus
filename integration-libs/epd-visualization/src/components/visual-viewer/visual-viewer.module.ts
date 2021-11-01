e686f4a8252894c489c429957f5a5f41747bbc24
Sam Garland <52560012+sam-garland@users.noreply.github.com>
2021-11-01 13:09:33 +1300

    fix: reference npm package instead of using relative path. (#16)

--- integration-libs/epd-visualization/src/components/visual-viewer/visual-viewer.module.ts
+++ integration-libs/epd-visualization/src/components/visual-viewer/visual-viewer.module.ts
@@ -5,7 +5,7 @@ import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
 import { VisualViewerToolbarButtonModule } from './toolbar/visual-viewer-toolbar-button/visual-viewer-toolbar-button.module';
 import { VisualViewerAnimationSliderModule } from './toolbar/visual-viewer-animation-slider/visual-viewer-animation-slider.module';
 import { VisualViewerComponent } from './visual-viewer.component';
-import { SpinnerModule } from '../../../../../projects/storefrontlib/shared/components/spinner/spinner.module';
+import { SpinnerModule } from '@spartacus/storefront';
 
 @NgModule({
   imports: [
