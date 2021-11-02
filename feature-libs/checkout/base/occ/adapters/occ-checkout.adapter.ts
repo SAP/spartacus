7b129e1de79cd12d3fbfa110fba42a71bdee4f2b
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-11-02 18:58:04 +0100

    test: moved backoffJalo out of the file

--- feature-libs/checkout/base/occ/adapters/occ-checkout.adapter.ts
+++ feature-libs/checkout/base/occ/adapters/occ-checkout.adapter.ts
@@ -6,9 +6,10 @@ import {
 } from '@spartacus/checkout/base/core';
 import { CheckoutState } from '@spartacus/checkout/base/root';
 import {
+  backOff,
   ConverterService,
-  HttpErrorModel,
   InterceptorUtil,
+  isJaloError,
   normalizeHttpError,
   Occ,
   OccEndpointsService,
@@ -17,8 +18,8 @@ import {
   ORDER_NORMALIZER,
   USE_CLIENT_TOKEN,
 } from '@spartacus/core';
-import { Observable, of, pipe, range, throwError, timer, zip } from 'rxjs';
-import { catchError, map, mergeMap, retryWhen } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class OccCheckoutAdapter implements CheckoutAdapter {
@@ -27,29 +28,6 @@ export class OccCheckoutAdapter implements CheckoutAdapter {
     protected occEndpoints: OccEndpointsService,
     protected converter: ConverterService
   ) {}
-
-  protected getPlaceOrderEndpoint(
-    userId: string,
-    cartId: string,
-    termsChecked: string
-  ): string {
-    return this.occEndpoints.buildUrl('placeOrder', {
-      urlParams: { userId },
-      queryParams: { cartId, termsChecked },
-    });
-  }
-
-  protected getLoadCheckoutDetailsEndpoint(
-    userId: string,
-    cartId: string
-  ): string {
-    return this.occEndpoints.buildUrl('loadCheckoutDetails', {
-      urlParams: {
-        userId,
-        cartId,
-      },
-    });
-  }
 
   public placeOrder(
     userId: string,
@@ -76,41 +54,39 @@ export class OccCheckoutAdapter implements CheckoutAdapter {
       );
   }
 
+  protected getPlaceOrderEndpoint(
+    userId: string,
+    cartId: string,
+    termsChecked: string
+  ): string {
+    return this.occEndpoints.buildUrl('placeOrder', {
+      urlParams: { userId },
+      queryParams: { cartId, termsChecked },
+    });
+  }
+
   getCheckoutDetails(
     userId: string,
     cartId: string
   ): Observable<CheckoutState> {
     return this.http
-      .get<any>(
-        this.occEndpoints.buildUrl('getCheckoutDetails', {
-          urlParams: {
-            userId,
-            cartId,
-          },
-        })
-      )
+      .get<CheckoutState>(this.getGetCheckoutDetailsEndpoint(userId, cartId))
       .pipe(
         catchError((error) => throwError(normalizeHttpError(error))),
-        backoffJalo(3, 500),
+        backOff(isJaloError),
         this.converter.pipeable(CHECKOUT_NORMALIZER)
       );
   }
-}
 
-function backoffJalo<T>(maxTries: number, delay: number) {
-  return pipe(
-    retryWhen<T>((attempts) =>
-      zip(range(1, maxTries + 1), attempts).pipe(
-        mergeMap(([i, err]) =>
-          i > maxTries || !isJaloError(err) ? throwError(err) : of(i)
-        ),
-        map((i) => i * i),
-        mergeMap((v) => timer(v * delay))
-      )
-    )
-  );
-}
-
-function isJaloError(err: HttpErrorModel) {
-  return err.details?.[0]?.type === 'JaloObjectNoLongerValidError';
+  protected getGetCheckoutDetailsEndpoint(
+    userId: string,
+    cartId: string
+  ): string {
+    return this.occEndpoints.buildUrl('getCheckoutDetails', {
+      urlParams: {
+        userId,
+        cartId,
+      },
+    });
+  }
 }
