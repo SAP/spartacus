e7e556e2763ca7c73960c756c5fb9dbf5ff1b7b5
Kornkanok Changsuwan <kornkanok.changsuwan@sap.com>
2021-11-03 10:57:47 +0100

    GH-13806: add exit button to configurator in powertools

--- feature-libs/product-configurator/rulebased/root/cpq/cpq-configurator-layout.module.ts
+++ feature-libs/product-configurator/rulebased/root/cpq/cpq-configurator-layout.module.ts
@@ -16,10 +16,10 @@ import { CpqConfiguratorPageLayoutHandler } from './cpq-configurator-page-layout
         CpqConfigurationTemplate: {
           header: {
             md: {
-              slots: ['SiteLogo', 'MiniCart'],
+              slots: ['SiteLogo', 'VariantConfigExitButton', 'MiniCart'],
             },
             xs: {
-              slots: ['SiteLogo', 'MiniCart'],
+              slots: ['SiteLogo', 'VariantConfigExitButton', 'MiniCart'],
             },
           },
 
