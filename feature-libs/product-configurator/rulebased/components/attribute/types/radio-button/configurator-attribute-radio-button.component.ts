54022019371779bb366a5b78de4a2c866f03885f
Christoph Hinssen <christoph.hinssen@sap.com>
2021-11-12 07:01:14 +0100

    Directly call service from component

--- feature-libs/product-configurator/rulebased/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/radio-button/configurator-attribute-radio-button.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { InnerComponentsContext } from '@spartacus/storefront';
+import { ConfiguratorCommonsService } from '../../../../core/facade/configurator-commons.service';
 import { Configurator } from '../../../../core/model/configurator.model';
-import { ConfigFormUpdateEvent } from '../../../form/configurator-form.event';
 import { ConfiguratorAttributeQuantityService } from '../../quantity/configurator-attribute-quantity.service';
 import { ConfiguratorAttributeSingleSelectionBaseComponent } from '../base/configurator-attribute-single-selection-base.component';
 
@@ -21,13 +21,10 @@ export class ConfiguratorAttributeRadioButtonComponent
 
   constructor(
     protected quantityService: ConfiguratorAttributeQuantityService,
-    protected innerComponentsContext: InnerComponentsContext
+    protected innerComponentsContext: InnerComponentsContext,
+    protected configuratorCommonsService: ConfiguratorCommonsService
   ) {
     super(quantityService);
-    console.log(
-      'CHHI radio button component active for uiType: ' +
-        innerComponentsContext.context.attribute?.uiType
-    );
     this.attribute = innerComponentsContext.context.attribute;
     this.ownerKey = innerComponentsContext.context.configuration.owner.key;
   }
@@ -35,18 +32,23 @@ export class ConfiguratorAttributeRadioButtonComponent
   onSelect(value: string): void {
     this.loading$.next(true);
 
-    const event: ConfigFormUpdateEvent = {
-      changedAttribute: {
+    // const event: ConfigFormUpdateEvent = {
+    //   changedAttribute: {
+    //     ...this.attribute,
+    //     selectedSingleValue: value,
+    //   },
+    //   ownerKey: this.ownerKey,
+    //   updateType: Configurator.UpdateType.ATTRIBUTE,
+    // };
+
+    //this.selectionChange.emit(event);
+    this.configuratorCommonsService.updateConfiguration(
+      this.ownerKey,
+      {
         ...this.attribute,
         selectedSingleValue: value,
       },
-      ownerKey: this.ownerKey,
-      updateType: Configurator.UpdateType.ATTRIBUTE,
-    };
-
-    //this.selectionChange.emit(event);
-    this.innerComponentsContext.context.formComponentForUpdate.updateConfiguration(
-      event
+      Configurator.UpdateType.ATTRIBUTE
     );
   }
 
