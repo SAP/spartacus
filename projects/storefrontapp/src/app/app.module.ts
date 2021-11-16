a73f4d884edab61da0e148902f3bf59662daa902
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-11-16 14:40:47 +0100

    dummy

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { SpartacusModule } from './spartacus/spartacus.module';
 registerLocaleData(localeDe);
 registerLocaleData(localeJa);
 registerLocaleData(localeZh);
-console.log;
 
 const devImports = [];
 if (!environment.production) {
