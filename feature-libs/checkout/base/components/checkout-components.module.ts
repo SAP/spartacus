1cb4a681232152497d48d1ba23c6992b84ff7692
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-11-17 12:15:54 +0100

    chore: rename payment folder

--- feature-libs/checkout/base/components/checkout-components.module.ts
+++ feature-libs/checkout/base/components/checkout-components.module.ts
@@ -4,10 +4,10 @@ import { CheckoutDeliveryModeModule } from './components/checkout-delivery-mode/
 import { CheckoutLoginModule } from './components/checkout-login/checkout-login.module';
 import { CheckoutOrchestratorModule } from './components/checkout-orchestrator/checkout-orchestrator.module';
 import { CheckoutOrderSummaryModule } from './components/checkout-order-summary/checkout-order-summary.module';
+import { CheckoutPaymentMethodModule } from './components/checkout-payment-method/checkout-payment-method.module';
 import { CheckoutProgressMobileBottomModule } from './components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.module';
 import { CheckoutProgressMobileTopModule } from './components/checkout-progress/checkout-progress-mobile-top/checkout-progress-mobile-top.module';
 import { CheckoutProgressModule } from './components/checkout-progress/checkout-progress.module';
-import { CheckoutPaymentMethodModule } from './components/payment-method/checkout-payment-method.module';
 import { PlaceOrderModule } from './components/place-order/place-order.module';
 import { ReviewSubmitModule } from './components/review-submit/review-submit.module';
 import { ShippingAddressModule } from './components/shipping-address/shipping-address.module';
