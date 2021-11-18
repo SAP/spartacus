e5ca2b9afbfb627f8526d04bcd6d7dd5b9d31900
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-11-18 12:20:14 +0100

    removed b2b and repl routing configs

--- feature-libs/checkout/root/config/default-checkout-routing-config.ts
+++ feature-libs/checkout/root/config/default-checkout-routing-config.ts
@@ -5,13 +5,11 @@ export const defaultCheckoutRoutingConfig: RoutingConfig = {
     routes: {
       checkoutLogin: { paths: ['checkout-login'], authFlow: true },
       checkout: { paths: ['checkout'] },
-      checkoutPaymentType: { paths: ['checkout/payment-type'] },
       checkoutShippingAddress: { paths: ['checkout/shipping-address'] },
       checkoutDeliveryMode: { paths: ['checkout/delivery-mode'] },
       checkoutPaymentDetails: { paths: ['checkout/payment-details'] },
       checkoutReviewOrder: { paths: ['checkout/review-order'] },
       orderConfirmation: { paths: ['order-confirmation'] },
-      replenishmentConfirmation: { paths: ['replenishment/confirmation'] },
     },
   },
 };
