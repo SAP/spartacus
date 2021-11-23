696ff67b85643aeddfbf81fa450c88d1e1e479bb
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-11-23 03:52:32 -0500

    add first 2 fails, but 3rd success is weird to reproduce

--- projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-backoff.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-backoff.ts
@@ -1,21 +1,22 @@
 import * as sampleData from '../sample-data/checkout-flow';
 import { waitForPage } from './checkout-flow';
 
-export function waitForShippingAddressAndDeliveryModeSetdata() {
+export function waitForShippingAddressdata() {
   cy.window().then((win) => {
     const { token } = JSON.parse(win.localStorage.getItem('spartacus⚿⚿auth'));
 
     cy.requireShippingAddressAdded(sampleData.user.address, token);
-    cy.requireShippingMethodSelected(token);
   });
 }
 
-export function visitCheckoutPaymentDetailsPage() {
-  const paymentPage = waitForPage(
-    '/checkout/payment-details',
-    'getPaymentPage'
+export function visitCheckoutShippingAddressPage() {
+  const shippingAddressPage = waitForPage(
+    '/checkout/shipping-address',
+    'getShippingAddressPage'
   );
-  cy.visit('/checkout/payment-details');
-  cy.wait(`@${paymentPage}`).its('response.statusCode').should('eq', 200);
-  cy.get('cx-payment-method h2').should('contain', 'Payment');
+  cy.visit('/checkout/shipping-address');
+  cy.wait(`@${shippingAddressPage}`)
+    .its('response.statusCode')
+    .should('eq', 200);
+  cy.get('cx-shipping-address h2').should('contain', 'Shipping Address');
 }
---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-backoff.e2e-spec.ts
@@ -0,0 +1,74 @@
+import {
+  visitCartPage,
+  waitForCartPageData,
+} from '../../../helpers/b2b/b2b-saved-cart';
+import * as checkoutBackoff from '../../../helpers/checkout-backoff';
+import { viewportContext } from '../../../helpers/viewport-context';
+import * as sampleData from '../../../sample-data/checkout-flow';
+import { clearAllStorage } from '../../../support/utils/clear-all-storage';
+
+context('Checkout backoff test', () => {
+  viewportContext(['desktop'], () => {
+    before(() => {
+      clearAllStorage();
+    });
+
+    beforeEach(() => {
+      cy.requireLoggedIn();
+      waitForCartPageData(sampleData.product);
+      visitCartPage();
+
+      checkoutBackoff.waitForShippingAddressdata();
+      checkoutBackoff.visitCheckoutShippingAddressPage();
+    });
+
+    it('should verify backoff mechanism in checkout', () => {
+      // intercept for route
+      cy.intercept(
+        `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+          'BASE_SITE'
+        )}/users/current/carts/*?fields=deliveryAddress(FULL),deliveryMode(FULL),paymentInfo(FULL)&lang=en&curr=USD`,
+        { times: 1 },
+        (req) => {
+          req.reply({
+            statusCode: 400,
+            body: { details: [{ type: 'JaloObjectNoLongerValidError' }] },
+          });
+        }
+      ).as('test1err');
+
+      cy.intercept(
+        `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+          'BASE_SITE'
+        )}/users/current/carts/*?fields=deliveryAddress(FULL),deliveryMode(FULL),paymentInfo(FULL)&lang=en&curr=USD`,
+        { times: 1 },
+        (req) => {
+          req.reply({
+            statusCode: 400,
+            body: { details: [{ type: 'JaloObjectNoLongerValidError' }] },
+          });
+        }
+      ).as('test2err');
+
+      // cy.intercept(
+      //   `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+      //     'BASE_SITE'
+      //   )}/users/current/carts/*?fields=deliveryAddress(FULL),deliveryMode(FULL),paymentInfo(FULL)&lang=en&curr=USD`,
+      //   { delay: 5000 }
+      // ).as('test3successhopefully');
+
+      // click the card (can reproduce easily in the storefront)
+      cy.get('cx-shipping-address .cx-card-actions .cx-card-link').click({
+        force: true,
+      });
+
+      // do the 3 assert test (fail, fail, pass)
+      cy.wait(`@test1err`).its('response.statusCode').should('eq', 400);
+
+      cy.wait(`@test2err`).its('response.statusCode').should('eq', 400);
+      // cy.wait(`@test3successhopefully`)
+      //   .its('response.statusCode')
+      //   .should('eq', 200);
+    });
+  });
+});
