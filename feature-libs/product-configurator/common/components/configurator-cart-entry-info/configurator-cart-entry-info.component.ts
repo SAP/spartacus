c01f6f863c896f4bae1f77a5e7bdab81dfbe878b
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2021-11-23 14:44:52 +0100

    Enable screen reader for common components (#14218)
    * Enable screen reader for configurator-issues-notification and
    
    * Screen reader II
    
    * GH-14157: A11Y for textfield edit-/read-only-mode
    
    * GH-14157: Changed aria-label for textfield input
    
    * Fix failing karma tests
    
    * A11y: common components
    
    * Review feedback I
    
    * A11y: prepare common components for screen reader
    
    * Extend test coverage
    
    * Update feature-libs/product-configurator/common/components/configurator-cart-entry-bundle-info/configurator-cart-entry-bundle-info.component.spec.ts
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Update feature-libs/product-configurator/common/components/configurator-cart-entry-bundle-info/configurator-cart-entry-bundle-info.component.spec.ts
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Update feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Update feature-libs/product-configurator/common/components/configure-product/configure-product.component.spec.ts
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Update feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.html
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Update feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.html
    
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
    
    * Review feedback I
    
    * Review feedback II
    
    Co-authored-by: Sebastian Stein <sebastian.stein@sap.com>
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>

--- feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
+++ feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
@@ -60,4 +60,8 @@ export class ConfiguratorCartEntryInfoComponent {
       : false;
     return attributeBased ? attributeBased : false;
   }
+
+  getHiddenConfigurationInfoId(index: number): string {
+    return 'cx-configuration-hidden-info-' + index.toString();
+  }
 }
