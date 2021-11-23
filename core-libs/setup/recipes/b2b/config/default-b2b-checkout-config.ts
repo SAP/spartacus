75ce994c8f4f0bc1fe0df3fe51ae54ad55d41e70
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-11-23 14:52:19 +0100

    revert setup

---
+++ core-libs/setup/recipes/b2b/config/default-b2b-checkout-config.ts
@@ -0,0 +1,49 @@
+import {
+  CheckoutConfig,
+  CheckoutStepType,
+  DeliveryModePreferences,
+} from '@spartacus/checkout/root';
+
+/**
+ * Use 'defaultB2bCheckoutConfig' from '@spartacus/checkout/b2b/root' instead.
+ * @deprecated since version 5.0
+ */
+export const defaultB2bCheckoutConfig: CheckoutConfig = {
+  checkout: {
+    steps: [
+      {
+        id: 'paymentType',
+        name: 'checkoutProgress.methodOfPayment',
+        routeName: 'checkoutPaymentType',
+        type: [CheckoutStepType.PAYMENT_TYPE],
+      },
+      {
+        id: 'shippingAddress',
+        name: 'checkoutProgress.shippingAddress',
+        routeName: 'checkoutShippingAddress',
+        type: [CheckoutStepType.SHIPPING_ADDRESS],
+      },
+      {
+        id: 'deliveryMode',
+        name: 'checkoutProgress.deliveryMode',
+        routeName: 'checkoutDeliveryMode',
+        type: [CheckoutStepType.DELIVERY_MODE],
+      },
+      {
+        id: 'paymentDetails',
+        name: 'checkoutProgress.paymentDetails',
+        routeName: 'checkoutPaymentDetails',
+        type: [CheckoutStepType.PAYMENT_DETAILS],
+      },
+      {
+        id: 'reviewOrder',
+        name: 'checkoutProgress.reviewOrder',
+        routeName: 'checkoutReviewOrder',
+        type: [CheckoutStepType.REVIEW_ORDER],
+      },
+    ],
+    express: false,
+    defaultDeliveryMode: [DeliveryModePreferences.FREE],
+    guest: false,
+  },
+};
---
+++ core-libs/setup/recipes/b2b/config/default-b2b-occ-config.ts
@@ -0,0 +1,54 @@
+// We need this import for augmentation of OccEndpoints to pick up
+import { CheckoutOccEndpoints } from '@spartacus/checkout/occ';
+import { OccConfig } from '@spartacus/core';
+import { UserAccountOccEndpoints } from '@spartacus/user/account/occ';
+import { UserProfileOccEndpoints } from '@spartacus/user/profile/occ';
+
+// While it is not strictly required to define checkout endpoints in a separate `CheckoutOccEndpoints`
+// variable, type augmentation does require that this file imports `CheckoutOccEndpoints`.
+// A good way to make sure the `CheckoutOccEndpoints` import is not removed by mistake is to use
+// `CheckoutOccEndpoints` in the code.
+const defaultB2bCheckoutOccEndpoints: CheckoutOccEndpoints = {
+  setDeliveryAddress: 'orgUsers/${userId}/carts/${cartId}/addresses/delivery',
+  placeOrder: 'orgUsers/${userId}/orders?fields=FULL',
+};
+
+const defaultB2bUserAccountOccEndpoints: UserAccountOccEndpoints = {
+  user: 'orgUsers/${userId}',
+};
+
+const defaultB2bUserProfileOccEndpoints: UserProfileOccEndpoints = {
+  userUpdateProfile: 'users/${userId}',
+  userCloseAccount: 'users/${userId}',
+};
+
+/**
+ * Use 'defaultB2bOccConfig' from '@spartacus/checkout/b2b/root' instead.
+ * @deprecated since version 5.0
+ */
+export const defaultB2bOccConfig: OccConfig = {
+  backend: {
+    occ: {
+      endpoints: {
+        ...defaultB2bCheckoutOccEndpoints,
+        ...defaultB2bUserAccountOccEndpoints,
+        ...defaultB2bUserProfileOccEndpoints,
+        user: 'orgUsers/${userId}',
+        userUpdateProfile: 'users/${userId}',
+        userCloseAccount: 'users/${userId}',
+        addEntries:
+          'orgUsers/${userId}/carts/${cartId}/entries?quantity=${quantity}',
+        scheduleReplenishmentOrder:
+          'orgUsers/${userId}/replenishmentOrders?fields=FULL,costCenter(FULL),purchaseOrderNumber,paymentType',
+        replenishmentOrderDetails:
+          'users/${userId}/replenishmentOrders/${replenishmentOrderCode}?fields=FULL,costCenter(FULL),purchaseOrderNumber,paymentType,user',
+        replenishmentOrderDetailsHistory:
+          'users/${userId}/replenishmentOrders/${replenishmentOrderCode}/orders',
+        cancelReplenishmentOrder:
+          'users/${userId}/replenishmentOrders/${replenishmentOrderCode}?fields=FULL,costCenter(FULL),purchaseOrderNumber,paymentType,user',
+        replenishmentOrderHistory:
+          'users/${userId}/replenishmentOrders?fields=FULL,replenishmentOrders(FULL, purchaseOrderNumber)',
+      },
+    },
+  },
+};
