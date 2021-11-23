6c3bb244790d4d5df8ba3eca53f4e5a188dafe66
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2021-11-23 11:27:17 +0100

    Update feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
    Co-authored-by: Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>

--- feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
+++ feature-libs/product-configurator/common/components/configurator-cart-entry-info/configurator-cart-entry-info.component.ts
@@ -62,6 +62,6 @@ export class ConfiguratorCartEntryInfoComponent {
   }
 
   getHiddenConfigurationInfoId(index: number): string {
-    return 'cx-configuration-hidden-info-' + index?.toString();
+    return 'cx-configuration-hidden-info-' + index.toString();
   }
 }
