e49430874918984297da725f838fe1068d984e2d
Caine Rotherham <rotherham.c@gmail.com>
2021-11-25 12:44:37 +0100

    remove feature flags for tab paragraph component

--- projects/storefrontlib/cms-components/content/tab-paragraph-container/tab-paragraph-container.component.ts
+++ projects/storefrontlib/cms-components/content/tab-paragraph-container/tab-paragraph-container.component.ts
@@ -1,22 +1,8 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  OnDestroy,
-  OnInit,
-  QueryList,
-  ViewChildren,
-} from '@angular/core';
-import {
-  CmsService,
-  CMSTabParagraphContainer,
-  WindowRef,
-} from '@spartacus/core';
-import { combineLatest, Observable, Subscription } from 'rxjs';
-import { distinctUntilChanged, map, switchMap, take } from 'rxjs/operators';
-import { ComponentWrapperDirective } from '../../../cms-structure/page/component/component-wrapper.directive';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CmsService, CMSTabParagraphContainer } from '@spartacus/core';
+import { combineLatest, Observable } from 'rxjs';
+import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { CmsComponentData } from '../../../cms-structure/page/model/index';
-import { BreakpointService } from '../../../layout/breakpoint/breakpoint.service';
 import { BREAKPOINT } from '../../../layout/config/layout-config';
 import { Tab } from '../tab/Tab';
 
@@ -25,19 +11,7 @@ import { Tab } from '../tab/Tab';
   templateUrl: './tab-paragraph-container.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabParagraphContainerComponent
-  implements AfterViewInit, OnInit, OnDestroy
-{
-  activeTabNum = 0;
-
-  @ViewChildren(ComponentWrapperDirective)
-  children!: QueryList<ComponentWrapperDirective>;
-
-  tabTitleParams: (Observable<any> | null)[] = [];
-
-  // TODO: it is not used any more, so can be removed in 5.0
-  subscription: Subscription;
-
+export class TabParagraphContainerComponent {
   tabConfig = {
     label: 'Product Information',
     openTabs: [0],
@@ -46,9 +20,7 @@ export class TabParagraphContainerComponent
 
   constructor(
     public componentData: CmsComponentData<CMSTabParagraphContainer>,
-    protected cmsService: CmsService,
-    protected winRef: WindowRef,
-    protected breakpointService: BreakpointService
+    protected cmsService: CmsService
   ) {}
 
   components$: Observable<any[]> = this.componentData.data$.pipe(
@@ -89,55 +61,4 @@ export class TabParagraphContainerComponent
       }))
     )
   );
-
-  select(tabNum: number, event?: MouseEvent): void {
-    this.breakpointService
-      ?.isDown(BREAKPOINT.sm)
-      .pipe(take(1))
-      .subscribe((res) => {
-        if (res) {
-          this.activeTabNum = this.activeTabNum === tabNum ? -1 : tabNum;
-          if (event && event?.target) {
-            const target = event.target as HTMLElement;
-            const parentNode = target.parentNode as HTMLElement;
-            this.winRef?.nativeWindow?.scrollTo(0, parentNode.offsetTop);
-          }
-        } else {
-          this.activeTabNum = tabNum;
-        }
-      });
-  }
-
-  ngOnInit(): void {
-    this.activeTabNum =
-      this.winRef?.nativeWindow?.history?.state?.activeTab ?? this.activeTabNum;
-  }
-
-  ngAfterViewInit(): void {
-    // If the sub cms components data exist, the components created before ngAfterViewInit are called.
-    // In this case, the title parameters are directly pulled from them.
-    if (this.children.length > 0) {
-      this.getTitleParams(this.children);
-    }
-  }
-
-  tabCompLoaded(componentRef: any): void {
-    this.tabTitleParams.push(componentRef.instance.tabTitleParam$);
-  }
-
-  private getTitleParams(children: QueryList<ComponentWrapperDirective>) {
-    children.forEach((comp) => {
-      if (comp.cmpRef?.instance.tabTitleParam$) {
-        this.tabTitleParams.push(comp.cmpRef.instance.tabTitleParam$);
-      } else {
-        this.tabTitleParams.push(null);
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
 }
--- projects/storefrontlib/cms-components/content/tab-paragraph-container/tab-paragraph-container.module.ts
+++ projects/storefrontlib/cms-components/content/tab-paragraph-container/tab-paragraph-container.module.ts
@@ -1,25 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {
-  CmsConfig,
-  FeaturesConfigModule,
-  I18nModule,
-  provideDefaultConfig,
-} from '@spartacus/core';
-import { OutletModule } from '../../../cms-structure/outlet/outlet.module';
+import { CmsConfig, provideDefaultConfig } from '@spartacus/core';
 import { PageComponentModule } from '../../../cms-structure/page/component/page-component.module';
 import { TabModule } from '../tab/tab.module';
 import { TabParagraphContainerComponent } from './tab-paragraph-container.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    PageComponentModule,
-    OutletModule,
-    I18nModule,
-    TabModule,
-    FeaturesConfigModule,
-  ],
+  imports: [CommonModule, PageComponentModule, TabModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
