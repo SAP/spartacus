b7feb226b0e1715c5dfe593148bee23261df07db
Sam Garland <52560012+sam-garland@users.noreply.github.com>
2021-11-30 23:17:26 +1300

    chore: cherry-pick from epic/epd-visualization-maintenance (#62)
    * Moved styles and fixed lazy loading for schematic updates (#55)
    
    * chore: moved styles into epd-visualization library
    
    * fix: lazy loading wasn't working in schematic
    
    * fix: added missing peer dependencies.
    
    * chore: switched to a floating UI5 version (#59)
    
    * chore: change schematic propmt/default.
    Don't use ACC environment as default.
    
    * chore: ran prettier
    
    * fix: synchronise selection made before scene loaded (#57)

--- integration-libs/epd-visualization/root/config/epd-visualization-test-config.ts
+++ integration-libs/epd-visualization/root/config/epd-visualization-test-config.ts
@@ -10,8 +10,7 @@ export function getTestConfig(): EpdVisualizationConfig {
         'https://epd-acc-eu20-consumer.epdacc.cfapps.eu20.hana.ondemand.com',
     },
     ui5: {
-      bootstrapUrl:
-        'https://sapui5.hana.ondemand.com/1.96.0/resources/sap-ui-core.js',
+      bootstrapUrl: 'https://sapui5.hana.ondemand.com/resources/sap-ui-core.js',
     },
     usageIds: {
       folderUsageId: {
--- integration-libs/epd-visualization/schematics/constants.ts
+++ integration-libs/epd-visualization/schematics/constants.ts
@@ -1,10 +1,18 @@
+export const EPD_VISUALIZATION_FEATURE_NAME_CONSTANT =
+  'EPD_VISUALIZATION_FEATURE';
+
 export const EPD_VISUALIZATION_FOLDER_NAME = 'epd-visualization';
 export const EPD_VISUALIZATION_MODULE_NAME = 'EpdVisualization';
 export const EPD_VISUALIZATION_MODULE = 'EpdVisualizationModule';
+export const EPD_VISUALIZATION_ROOT_MODULE = 'EpdVisualizationRootModule';
+
 export const EPD_VISUALIZATION_TRANSLATIONS = 'epdVisualizationTranslations';
 export const EPD_VISUALIZATION_TRANSLATION_CHUNKS_CONFIG =
   'epdVisualizationTranslationChunksConfig';
 
-// Use the latest long term support version of UI5.
-// Synchronize this with the version of the @sapui5/ts-types in the package.json
-export const EPD_VISUALIZATION_UI5_VERSION = '1.96.0';
+export const SCSS_FILE_NAME = 'epd-visualization.scss';
+
+export const SPARTACUS_EPD_VISUALIZATION_ROOT =
+  '@spartacus/epd-visualization/root';
+export const SPARTACUS_EPD_VISUALIZATION_ASSETS =
+  '@spartacus/epd-visualization/assets';
