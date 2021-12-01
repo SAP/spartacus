a36e119ac567eb08391a766219edc82f4f704e56
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-12-01 10:19:09 -0500

    fix: include new required param in child class (#13700)

--- feature-libs/asm/root/services/asm-auth.service.ts
+++ feature-libs/asm/root/services/asm-auth.service.ts
@@ -39,7 +39,8 @@ export class AsmAuthService extends AuthService {
       oAuthLibWrapperService,
       authStorageService,
       authRedirectService,
-      routingService
+      routingService,
+      globalMessageService
     );
   }
 
