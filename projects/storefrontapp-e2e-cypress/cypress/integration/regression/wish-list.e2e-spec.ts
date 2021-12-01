6541be32566ef27b3963c4e330cbe919468772b5
BenjaminEmiliani <benjaminemi129@gmail.com>
2021-12-01 14:07:38 -0500

    test: remove redudant test case (#14131)

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/wish-list.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/wish-list.e2e-spec.ts
@@ -1,7 +1,7 @@
-import * as cart from '../../helpers/cart';
 import * as wishList from '../../helpers/wish-list';
 import * as login from '../../helpers/login';
 import { clearAllStorage } from '../../support/utils/clear-all-storage';
+import { visitHomePage } from '../../helpers/checkout-flow';
 
 describe('Wish list', () => {
   before(() => {
@@ -21,13 +21,14 @@ describe('Wish list', () => {
   });
 
   describe('Anonymous', () => {
-    it('should sign in to add to wish list from PDP', () => {
+    it('should register and sign in user to add product to wish list', () => {
       wishList.addToWishListAnonymous(wishList.products[0]);
     });
   });
 
   describe('Logged in', () => {
     beforeEach(() => {
+      visitHomePage();
       cy.get('cx-login').click();
       wishList.loginWishListUser();
     });
@@ -63,12 +64,21 @@ describe('Wish list', () => {
 
   describe('checkout', () => {
     beforeEach(() => {
+      visitHomePage();
       cy.get('cx-login').click();
       wishList.loginWishListUser();
     });
 
     it('should checkout with product added from wish list', () => {
       const currentRetry = cy.state('runnable')._currentRetry;
+
+      /**
+       * This test case performs a checkout. Checkout will occasioanlly (rare)
+       * freeze on a checkout step (Shipping -> Delivery -> Payment -> Review).
+       * If this occurs, spec will time out and restart from beginning. In this case
+       * we do not try to add products to wish list as they will still be in wish list
+       * from failed attempt.
+       */
       if (currentRetry === 0) {
         wishList.addToWishList(wishList.products[0]);
         wishList.addToWishList(wishList.products[1]);
@@ -77,6 +87,9 @@ describe('Wish list', () => {
       wishList.verifyProductInWishList(wishList.products[0]);
       wishList.verifyProductInWishList(wishList.products[1]);
 
+      /**
+       * Same as previously explained, only try to add products to cart on first attempt.
+       */
       if (currentRetry === 0) {
         wishList.addProductToCart(wishList.products[0]);
         wishList.addProductToCart(wishList.products[1]);
@@ -90,16 +103,6 @@ describe('Wish list', () => {
       wishList.goToWishList();
       wishList.removeProductFromWishListPage(wishList.products[0]);
       wishList.removeProductFromWishListPage(wishList.products[1]);
-    });
-
-    it('should add product to cart, to wish list and checkout', () => {
-      cy.visit(`/product/${cart.products[0].code}`);
-      cart.clickAddToCart();
-      cart.closeAddedToCartDialog();
-      wishList.addToWishList(wishList.products[2]);
-      wishList.verifyProductInWishList(wishList.products[2]);
-      wishList.addProductToCart(wishList.products[2]);
-      wishList.checkoutFromCart([cart.products[0], wishList.products[2]]);
     });
   });
 });
