7d8e569d7618dc621f179ad4b6658c737f87c722
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-01 13:10:49 +0100

    normalized subscriptions

--- feature-libs/checkout/base/components/checkout-shipping-address/checkout-shipping-address.component.ts
+++ feature-libs/checkout/base/components/checkout-shipping-address/checkout-shipping-address.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CheckoutDeliveryAddressFacade } from '@spartacus/checkout/base/root';
 import {
@@ -13,7 +8,7 @@ import {
   UserAddressService,
 } from '@spartacus/core';
 import { Card } from '@spartacus/storefront';
-import { combineLatest, Observable, Subscription } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
 import { CheckoutStepService } from '../services/checkout-step.service';
 
@@ -27,13 +22,11 @@ export interface CardWithAddress {
   templateUrl: './checkout-shipping-address.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckoutShippingAddressComponent implements OnInit, OnDestroy {
+export class CheckoutShippingAddressComponent implements OnInit {
   addressFormOpened = false;
   forceLoader = false; // this helps with smoother steps transition
   selectedAddress: Address | undefined;
   doneAutoSelect = false;
-
-  protected subscriptions = new Subscription();
 
   constructor(
     protected userAddressService: UserAddressService,
@@ -186,9 +179,5 @@ export class CheckoutShippingAddressComponent implements OnInit, OnDestroy {
 
   back(): void {
     this.checkoutStepService.back(this.activatedRoute);
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
   }
 }
