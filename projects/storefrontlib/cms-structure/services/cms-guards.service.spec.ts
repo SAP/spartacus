50b6dc1364d38ff8ab27a3027571ded1924dfcf7
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-03 11:12:56 +0100

    improved unit test

--- projects/storefrontlib/cms-structure/services/cms-guards.service.spec.ts
+++ projects/storefrontlib/cms-structure/services/cms-guards.service.spec.ts
@@ -7,15 +7,16 @@ import {
 } from '@angular/router';
 import { ConfigModule } from '@spartacus/core';
 import { of } from 'rxjs';
-import { CmsGuardsService } from './cms-guards.service';
+import { take } from 'rxjs/operators';
 import { CmsComponentsService } from './cms-components.service';
+import { CmsGuardsService } from './cms-guards.service';
 
 describe('CmsGuardsService', () => {
   let service: CmsGuardsService;
   let guards: any[];
   const mockUrlTree = new UrlTree();
 
-  class MockCmsComponentsService {
+  class MockCmsComponentsService implements Partial<CmsComponentsService> {
     getGuards = jasmine.createSpy('getGuards').and.returnValue(guards);
   }
 
@@ -82,6 +83,7 @@ describe('CmsGuardsService', () => {
           mockActivatedRouteSnapshot,
           mockRouterStateSnapshot
         )
+        .pipe(take(1))
         .subscribe((res) => (result = res));
       expect(result).toEqual(true);
     });
@@ -96,6 +98,7 @@ describe('CmsGuardsService', () => {
           mockActivatedRouteSnapshot,
           mockRouterStateSnapshot
         )
+        .pipe(take(1))
         .subscribe((res) => (result = res));
       expect(result).toEqual(true);
       expect(TestBed.inject(PositiveGuard).canActivate).toHaveBeenCalledWith(
@@ -114,6 +117,7 @@ describe('CmsGuardsService', () => {
           mockActivatedRouteSnapshot,
           mockRouterStateSnapshot
         )
+        .pipe(take(1))
         .subscribe((res) => (result = res));
       expect(result).toEqual(false);
     });
@@ -128,6 +132,7 @@ describe('CmsGuardsService', () => {
           mockActivatedRouteSnapshot,
           mockRouterStateSnapshot
         )
+        .pipe(take(1))
         .subscribe((res) => (result = res));
       expect(result).toEqual(mockUrlTree);
     });
