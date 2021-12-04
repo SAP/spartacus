8426eb54602e2b04acfc0ffb681efc166890d41b
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-04 17:36:34 +0100

    checked https://github.com/SAP/spartacus/pull/14003

--- checkout-git-check.sh
+++ checkout-git-check.sh
@@ -11,8 +11,6 @@
 
 # feature-libs/checkout/components/components/checkout-login/checkout-login.component.html
 # feature-libs/checkout/components/order-confirmation/components/guest-register-form/guest-register-form.component.html
-# feature-libs/checkout/components/components/checkout-login/checkout-login.component.html
-# feature-libs/checkout/components/order-confirmation/components/guest-register-form/guest-register-form.component.html
 # feature-libs/checkout/components/services/checkout-details.service.spec.ts
 # feature-libs/checkout/components/services/checkout-details.service.ts
 # feature-libs/checkout/components/components/checkout-progress/checkout-progress-mobile-bottom/checkout-progress-mobile-bottom.component.ts
