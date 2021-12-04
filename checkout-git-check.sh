e6dfe53082fe888611fc192360f085c5b780b544
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-04 14:02:48 +0100

    fix weird characters

--- checkout-git-check.sh
+++ checkout-git-check.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # =========================================================
 # Running instructions:
 # 1. Make sure you're on `develop` branch.
@@ -8,28 +10,28 @@
 # =========================================================
 # Display branch to be sure we're in the right place
 # =========================================================
-git branch
-​
+# git branch
+
 # =========================================================
 # Get the latest changes
 # =========================================================
 git pull
-​
-​
+
+
 # =========================================================
 # since_date: Update the date accordingly. How far back to check for changes
 # =========================================================
 since_date=2021-09-10
-​
+
 git log --since=${since_date} --name-only > git-log.txt
-​
-​
+
+
 echo "
 ==============================================
 Checkout lib file changes since ${since_date}
 ==============================================
 "
-​
+
 # =================================================================
 # paths_to_watch: List of files or folders to "monitor" for changes
 # =================================================================
@@ -41,12 +43,12 @@ paths_to_watch=(
 'feature-libs/checkout/root'
 'feature-libs/checkout/styles'
 )
-​
+
 for path in ${paths_to_watch[@]}; do
     # echo "grep [${path}] git-log.txt"
     grep "${path}" git-log.txt 
 done
-​
+
 echo "
 ==============================================
 Checkout lib file changes - end
