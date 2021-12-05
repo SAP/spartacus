8cde969e3fd892ee8f78845bcc7c48a8c3b5d0af
Sam Garland <52560012+sam-garland@users.noreply.github.com>
2021-12-05 21:37:34 +1300

    chore: removed currently unused type definitions (#14524)

--- integration-libs/epd-visualization/root/services/storage-api/storage-api.service.ts
+++ integration-libs/epd-visualization/root/services/storage-api/storage-api.service.ts
@@ -64,73 +64,9 @@ export interface NodesResponse {
   nodes?: TreeNode[];
 }
 
-export type TMatrix = number[];
-
-export enum NodeContentType {
-  UNKNOWN = 'UNKNOWN',
-  GEOMETRY = 'GEOMETRY',
-  PMI = 'PMI',
-  DRAWING = 'DRAWING',
-  ANNOTATION = 'ANNOTATION',
-  ANIMATION = 'ANIMATION',
-  VIEWPORT_GROUP = 'VIEWPORT_GROUP',
-  DETAIL_VIEWS = 'DETAIL_VIEWS',
-  CAMERA = 'CAMERA',
-  MODEL_VIEW = 'MODEL_VIEW',
-  SECTION_PLANE = 'SECTION_PLANE',
-  PARAMETRIC = 'PARAMETRIC',
-  PLAYBACK = 'PLAYBACK',
-  REFERENCE = 'REFERENCE',
-  MAX = 'MAX',
-}
-
-export enum RenderMethod {
-  DEFAULT = 'DEFAULT',
-  SOLID = 'SOLID',
-  TRANSPARENT = 'TRANSPARENT',
-  LINE_ILLUSTRATION = 'LINE_ILLUSTRATION',
-  SOLID_OUTLINE = 'SOLID_OUTLINE',
-  SHADED_ILLUSTRATION = 'SHADED_ILLUSTRATION',
-}
-
-export enum RenderStage {
-  UNDERLAY_2D = 'UNDERLAY_2D',
-  DEFAULT_3D = 'DEFAULT_3D',
-  OVERLAY_2D = 'OVERLAY_2D',
-}
-
-export enum TransformType {
-  NONE = 'NONE',
-  BILLBOARD_VIEW = 'BILLBOARD_VIEW',
-  ORTHO2D = 'ORTHO2D',
-}
-
 export interface TreeNode {
-  description?: string;
   sid: string;
-  parentSid?: string;
-  name?: string;
-  contentType?: NodeContentType;
-  instanceVeId?: string;
-  veId?: string;
-  transform?: TMatrix;
-  visible?: boolean;
-  suppressed?: boolean;
-  closed?: boolean;
-  phantom?: boolean;
-  opacity?: number;
-  materialId?: string;
-  meshId?: string;
-  entityId?: string;
-  children?: number[];
   metadata?: Metadatum[];
-  geometry?: string[];
-  renderOrder?: number;
-  renderMethod?: RenderMethod;
-  renderStage?: RenderStage;
-  transformType?: TransformType;
-  highlightStyleId?: string;
-  joints?: AnimationJoint[];
 }
 
 export enum MetadatumValueType {
@@ -148,52 +84,4 @@ export interface Metadatum {
   tag: string;
   value: string;
   valueType: MetadatumValueType;
-}
-
-export enum JointType {
-  relative = 'relative',
-  offset = 'offset',
-  orientation = 'orientation',
-  billboard = 'billboard',
-  link = 'link',
-}
-
-export type Vector = number[];
-
-export type Vector4 = number[];
-
-export enum UnitShortName {
-  nm = 'nm',
-  um = 'um',
-  mm = 'mm',
-  cm = 'cm',
-  m = 'm',
-  km = 'km',
-  in = 'in',
-  ft = 'ft',
-  mi = 'mi',
-  pnt = 'pnt',
-  pca = 'pca',
-}
-
-export interface AnimationJoint {
-  id?: string;
-  type?: JointType;
-  parentSid?: string;
-  childSid?: string;
-  parent?: number;
-  child?: number;
-  t?: Vector;
-  q?: Vector4;
-  s?: Vector;
-  aa?: Vector4;
-  euler?: Vector4;
-  pitch?: number;
-  yaw?: number;
-  roll?: number;
-  tw?: Vector;
-  sUnit?: UnitShortName;
-  tUnit?: UnitShortName;
-  action?: string;
-  suppressed?: boolean;
 }
