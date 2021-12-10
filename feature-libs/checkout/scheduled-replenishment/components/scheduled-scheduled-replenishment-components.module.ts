e29d1bc7ae6db4f2157d2be764c32e0cbb83185d
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-10 14:24:43 +0100

    renamed files

--- feature-libs/checkout/scheduled-replenishment/components/scheduled-scheduled-replenishment-components.module.ts
+++ feature-libs/checkout/scheduled-replenishment/components/scheduled-scheduled-replenishment-components.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CheckoutScheduledReplenishmentPlaceOrderModule } from './checkout-place-order/checkout-place-order.module';
-import { CheckoutScheduledReplenishmentOrderConfirmationModule } from './checkout-replenishment-order-confirmation.module';
 import { CheckoutScheduleReplenishmentOrderModule } from './checkout-schedule-replenishment-order/checkout-schedule-replenishment-order.module';
+import { CheckoutScheduledReplenishmentOrderConfirmationModule } from './checkout-scheduled-replenishment-order-confirmation.module';
 
 @NgModule({
   imports: [
