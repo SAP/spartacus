7423de1f4845137dedac9e08a06f0d692c2444ea
Sam Garland <sam.garland@sap.com>
2021-12-12 22:47:53 +1300

    chore: removed pointless code

--- integration-libs/epd-visualization/root/epd-visualization-root.module.ts
+++ integration-libs/epd-visualization/root/epd-visualization-root.module.ts
@@ -1,20 +1,11 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import {
   CmsConfig,
-  ConfigInitializerService,
   provideConfigValidator,
   provideDefaultConfigFactory,
 } from '@spartacus/core';
 import { epdVisualizationConfigValidator } from './config';
 import { EPD_VISUALIZATION_FEATURE } from './feature-name';
-
-export function epdVisualizationJsFactory(
-  _configInit: ConfigInitializerService
-) {
-  return () => {
-    return Promise.resolve();
-  };
-}
 
 export function defaultEpdVisualizationComponentsConfig(): CmsConfig {
   const config: CmsConfig = {
@@ -31,12 +22,6 @@ export function defaultEpdVisualizationComponentsConfig(): CmsConfig {
   providers: [
     provideDefaultConfigFactory(defaultEpdVisualizationComponentsConfig),
     provideConfigValidator(epdVisualizationConfigValidator),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: epdVisualizationJsFactory,
-      deps: [ConfigInitializerService],
-      multi: true,
-    },
   ],
 })
 export class EpdVisualizationRootModule {}
