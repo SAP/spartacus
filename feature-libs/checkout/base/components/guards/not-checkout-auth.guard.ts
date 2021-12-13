00ce96591f0b2fee42c44bbd3f41bdd90036bd50
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-12-13 02:16:16 -0500

    went through temp.md files + fixed express checkout

--- feature-libs/checkout/base/components/guards/not-checkout-auth.guard.ts
+++ feature-libs/checkout/base/components/guards/not-checkout-auth.guard.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { ActiveCartFacade } from '@spartacus/cart/main/root';
-import { AuthService, SemanticPathService } from '@spartacus/core';
+import {
+  AuthService,
+  getLastValueSync,
+  SemanticPathService,
+} from '@spartacus/core';
 import { Observable } from 'rxjs';
-import { map, withLatestFrom } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -18,11 +22,10 @@ export class NotCheckoutAuthGuard implements CanActivate {
 
   canActivate(): Observable<boolean | UrlTree> {
     return this.authService.isUserLoggedIn().pipe(
-      withLatestFrom(this.activeCartFacade.isGuestCart()),
-      map(([isLoggedIn, isGuestCart]) => {
+      map((isLoggedIn) => {
         if (isLoggedIn) {
           return this.router.parseUrl(this.semanticPathService.get('home'));
-        } else if (isGuestCart) {
+        } else if (!!getLastValueSync(this.activeCartFacade.isGuestCart())) {
           return this.router.parseUrl(this.semanticPathService.get('cart'));
         }
         return !isLoggedIn;
