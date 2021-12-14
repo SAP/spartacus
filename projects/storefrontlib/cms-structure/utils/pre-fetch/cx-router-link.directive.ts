ac66509313bef058472b2ac9a4c9e787257a40a9
Nikola Zarić <nikola.zaric.ns@gmail.com>
2021-12-14 11:43:24 +0100

    pre-loading product cms template

--- projects/storefrontlib/cms-structure/utils/pre-fetch/cx-router-link.directive.ts
+++ projects/storefrontlib/cms-structure/utils/pre-fetch/cx-router-link.directive.ts
@@ -1,11 +1,20 @@
 import { LocationStrategy } from '@angular/common';
-import { Directive, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Directive, HostListener, Input, OnDestroy } from '@angular/core';
 import {
   ActivatedRoute,
   Router,
   RouterLinkWithHref,
   UrlTree,
 } from '@angular/router';
+import {
+  CmsService,
+  PageContext,
+  PageType,
+  ProductService,
+  RoutingService,
+} from '@spartacus/core';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Directive({
   selector: '[cxRouterLink]',
@@ -13,16 +22,26 @@ import {
 })
 export class CxRouterLinkDirective
   extends RouterLinkWithHref
-  implements OnChanges, OnDestroy
+  implements OnDestroy
 {
-  private cxCommands: any[] | null = null;
-  private cxRouter: Router;
-  private cxRoute: ActivatedRoute;
+  @Input() cxRouterLinkData?: { type: 'product' | 'cms'; id: string };
+
+  protected cxCommands: any[] | null = null;
+  protected cxRouter: Router;
+  protected cxRoute: ActivatedRoute;
+  protected subscriptions = new Subscription();
 
   constructor(
     router: Router,
     route: ActivatedRoute,
-    locationStrategy: LocationStrategy
+    locationStrategy: LocationStrategy,
+
+    // products
+    protected productService: ProductService,
+
+    // cms
+    protected routingService: RoutingService,
+    protected cmsService: CmsService
   ) {
     super(router, route, locationStrategy);
     this.cxRouter = router;
@@ -42,8 +61,6 @@ export class CxRouterLinkDirective
   // @override
   get urlTree(): UrlTree {
     return this.cxRouter.createUrlTree(this.cxCommands ?? [], {
-      // If the `relativeTo` input is not defined, we want to use `this.route` by default.
-      // Otherwise, we should use the value provided by the user in the input.
       relativeTo:
         this.relativeTo !== undefined ? this.relativeTo : this.cxRoute,
       queryParams: this.queryParams,
@@ -51,6 +68,26 @@ export class CxRouterLinkDirective
       queryParamsHandling: this.queryParamsHandling,
       preserveFragment: attrBoolValue(this.preserveFragment),
     });
+  }
+
+  @HostListener('mouseenter') onHover() {
+    if (this.cxRouterLinkData?.type === 'product') {
+      const id = this.cxRouterLinkData.id;
+      console.log(this.cxRouterLinkData);
+      this.subscriptions.add(
+        this.productService.get(id).pipe(take(2)).subscribe()
+      );
+
+      const predictedContext: PageContext = { id, type: PageType.PRODUCT_PAGE };
+      this.subscriptions.add(
+        this.cmsService.getPage(predictedContext).subscribe()
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this.subscriptions.unsubscribe();
   }
 }
 
