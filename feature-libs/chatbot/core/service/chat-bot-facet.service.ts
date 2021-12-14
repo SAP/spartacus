3b58dcf1599dd8cec84c48b7d2baaeabfa0a6596
Caine Rotherham <rotherham.c@gmail.com>
2021-12-14 16:29:13 +0100

    improve adding and build query

--- feature-libs/chatbot/core/service/chat-bot-facet.service.ts
+++ feature-libs/chatbot/core/service/chat-bot-facet.service.ts
@@ -20,6 +20,24 @@ export class ChatBotFacetService {
     this.searchFacets();
   }
 
+  protected buildQueryFromSelectedFacets(): string {
+    const selectedFacets = this.selected$.value;
+
+    // Set start of query with first facet
+    let query: string = selectedFacets[0]?.query.query.value;
+
+    for (let i = 1; i < selectedFacets.length; i++) {
+      const facetQuerySplit = selectedFacets[i].query.query.value.split(':');
+      query +=
+        ':' +
+        facetQuerySplit[facetQuerySplit.length - 2] +
+        ':' +
+        facetQuerySplit[facetQuerySplit.length - 1];
+    }
+
+    return query;
+  }
+
   removeFacet(value) {
     this.selected$.next(
       this.selected$.value.filter((facet) => facet === value)
@@ -28,9 +46,9 @@ export class ChatBotFacetService {
   }
 
   searchFacets() {
-    console.log(this.selected$.value);
-    // this.productSearchService.search(
-    //   decodeURIComponent(facet.query.query.value)
-    // );
+    console.log(this.selected$.value, this.buildQueryFromSelectedFacets());
+    this.productSearchService.search(
+      decodeURIComponent(this.buildQueryFromSelectedFacets())
+    );
   }
 }
