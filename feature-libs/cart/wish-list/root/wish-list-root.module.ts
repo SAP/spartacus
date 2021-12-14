556ca4ebaa453e3cab24080b37379f641d8d2de6
Patrick Labadie <patrick.labadie@sap.com>
2021-12-14 10:12:51 -0500

    GH-14416: Optimize cart lib lazy loading - PDP

--- feature-libs/cart/wish-list/root/wish-list-root.module.ts
+++ feature-libs/cart/wish-list/root/wish-list-root.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CART_FEATURE } from '@spartacus/cart/main/root';
 import { provideDefaultConfigFactory } from '@spartacus/core';
+import { AddToWishListModule } from './components/add-to-wishlist/add-to-wish-list.module';
 import {
   CART_WISH_LIST_CORE_FEATURE,
   CART_WISH_LIST_FEATURE,
@@ -10,7 +11,7 @@ export function defaultCartWishListComponentsConfig() {
   const config = {
     featureModules: {
       [CART_WISH_LIST_FEATURE]: {
-        cmsComponents: ['WishListComponent', 'AddToWishListComponent'],
+        cmsComponents: ['WishListComponent'],
         dependencies: [CART_FEATURE],
       },
       // by default core is bundled together with components
@@ -21,7 +22,7 @@ export function defaultCartWishListComponentsConfig() {
 }
 
 @NgModule({
-  imports: [],
+  imports: [AddToWishListModule],
   providers: [provideDefaultConfigFactory(defaultCartWishListComponentsConfig)],
 })
 export class WishListRootModule {}
