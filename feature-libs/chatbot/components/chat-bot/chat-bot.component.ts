131b11efa5657f565125fe78fb720b60b2e9f265
Mateusz <mateusz.kolasa@sap.com>
2021-12-14 16:58:10 +0100

    Add recomendations component

--- feature-libs/chatbot/components/chat-bot/chat-bot.component.ts
+++ feature-libs/chatbot/components/chat-bot/chat-bot.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatBotConfig, ChatBotService } from '@spartacus/chatbot/core';
+import { ProductService, ProductScope } from '@spartacus/core';
 import { ICON_TYPE } from '@spartacus/storefront';
+import { of } from 'rxjs';
 import { defaultChatBotConfig } from '../../core/config/default-chat-bot-config';
 @Component({
   selector: 'cx-chat-bot',
@@ -9,10 +11,16 @@ import { defaultChatBotConfig } from '../../core/config/default-chat-bot-config'
 export class ChatBotComponent implements OnInit {
   constructor(
     protected config: ChatBotConfig,
-    protected service: ChatBotService
+    protected service: ChatBotService,
+    protected productService: ProductService
   ) {}
 
+  conversation$ = this.service.conversation$;
+
+  options$ = this.service.options$;
+
   TEMP_CONFIG = defaultChatBotConfig; // TODO: Remove and inject proper config
+
   closeIcon = ICON_TYPE.CLOSE;
 
   /**
@@ -20,14 +28,54 @@ export class ChatBotComponent implements OnInit {
    */
   isOpen = false;
 
+  /**
+   * Detemines if chatbot product recommendations component is in open state.
+   */
+  areRecommendationsOpen = true;
+
+  /**
+   * Observable with recommendations.
+   */
+  recommendations$: any;
+
+  /**
+   * Toggle chatbot component to be open or displayed as bot icon.
+   */
   toggle() {
     this.isOpen = !this.isOpen;
   }
 
-  ngOnInit() {
-    console.log(this.config.chatBot); // TODO: Config is not visible inside component
+  /**
+   * Displays results component.
+   */
+  displayRecommendations() {
+    this.areRecommendationsOpen = true;
   }
 
-  conversation$ = this.service.conversation$;
-  options$ = this.service.options$;
+  /**
+   * Hides results component.
+   */
+  hideRecommendations() {
+    this.areRecommendationsOpen = false;
+  }
+
+  ngOnInit() {
+    console.log(this.config.chatBot); // TODO: Config is not visible inside component
+
+    // TODO: Update recomendations based on choices
+    this.recommendations$ = of([
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+      this.productService.get('300938', ProductScope.LIST),
+      this.productService.get('358639', ProductScope.LIST),
+    ]);
+  }
 }
