f4cd2e1ad1024e7fcd29a97f3de5012bc76bce19
Sam Garland <sam.garland@sap.com>
2021-12-16 12:43:46 +1300

    chore: got rid of implicit any warning.

--- integration-libs/epd-visualization/components/visual-viewer/visual-viewer.service.ts
+++ integration-libs/epd-visualization/components/visual-viewer/visual-viewer.service.ts
@@ -1013,21 +1013,23 @@ export class VisualViewerService implements OnDestroy {
   }
 
   private initializeUi5(): Observable<void> {
-    return new Observable((subscriber) => {
-      const core: Core = this.getCore();
-      core.attachInit(() => {
-        const loadLibraryOptions = { async: true };
-        Promise.all([
-          core.loadLibrary('sap.m', loadLibraryOptions),
-          core.loadLibrary('sap.ui.layout', loadLibraryOptions),
-          core.loadLibrary('sap.ui.vk', loadLibraryOptions),
-          core.loadLibrary('sap.ui.richtexteditor', loadLibraryOptions),
-        ]).then(() => {
-          subscriber.next();
-          subscriber.complete();
+    return new Observable(
+      (subscriber: { next: () => void; complete: () => void }) => {
+        const core: Core = this.getCore();
+        core.attachInit(() => {
+          const loadLibraryOptions = { async: true };
+          Promise.all([
+            core.loadLibrary('sap.m', loadLibraryOptions),
+            core.loadLibrary('sap.ui.layout', loadLibraryOptions),
+            core.loadLibrary('sap.ui.vk', loadLibraryOptions),
+            core.loadLibrary('sap.ui.richtexteditor', loadLibraryOptions),
+          ]).then(() => {
+            subscriber.next();
+            subscriber.complete();
+          });
         });
-      });
-    });
+      }
+    );
   }
 
   private destroyViewportAssociations(viewport: Viewport): void {
