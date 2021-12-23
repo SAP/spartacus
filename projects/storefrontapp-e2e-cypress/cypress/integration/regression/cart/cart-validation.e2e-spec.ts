bf1f4f3725fd07cbcd6a2b1dc593c095e51bd9a7
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2021-12-23 09:52:10 -0500

    incorporate changes from feature/GH-14701

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart/cart-validation.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart/cart-validation.e2e-spec.ts
@@ -1,22 +1,46 @@
-import * as cartValidation from '../../helpers/cart-validation';
+import * as cartValidation from '../../../helpers/cart-validation';
+import { viewportContext } from '../../../helpers/viewport-context';
 import {
-  addMultipleProductsToCart,
-  removeItemAndCheckCartEntriesNumber,
-} from '../../helpers/cart-validation';
-import { viewportContext } from '../../helpers/viewport-context';
-import {
-  lowStockResponse,
   outOfStockResponse,
   PRODUCT_1,
   PRODUCT_2,
-} from '../../sample-data/cart-validation';
-import { clearAllStorage } from '../../support/utils/clear-all-storage';
+} from '../../../sample-data/cart-validation';
+import { clearAllStorage } from '../../../support/utils/clear-all-storage';
+import {
+  addMultipleProductsToCart,
+  removeItemAndCheckCartEntriesNumber,
+} from '../../../helpers/cart-validation';
 
 context('Cart validation', () => {
+  viewportContext(['mobile'], () => {
+    beforeEach(() => {
+      clearAllStorage();
+      cy.cxConfig({
+        cart: {
+          validation: {
+            enabled: true,
+          },
+        },
+      });
+    });
+    describe('As logged in', () => {
+      beforeEach(() => {
+        cy.restoreLocalStorage();
+        cy.requireLoggedIn();
+      });
+
+      afterEach(() => {
+        cy.saveLocalStorage();
+      });
+
+      // Core test. Check with mobile as well.
+      cartValidation.testReducedProductStockValidation();
+    });
+  });
+
   viewportContext(['mobile', 'desktop'], () => {
     beforeEach(() => {
       clearAllStorage();
-
       cy.cxConfig({
         cart: {
           validation: {
@@ -34,28 +58,6 @@ context('Cart validation', () => {
 
       afterEach(() => {
         cy.saveLocalStorage();
-      });
-
-      it('should display information about reduced stock for product in cart', () => {
-        addMultipleProductsToCart([PRODUCT_1, PRODUCT_2]);
-
-        cartValidation.validateStock(lowStockResponse);
-
-        cy.findByText(/proceed to checkout/i).click();
-        cy.wait(`@validate`);
-
-        cartValidation.checkProductAvailabilityMessage();
-
-        cy.get('cx-cart-item-list cx-cart-item-validation-warning div').should(
-          'have.length',
-          2
-        );
-
-        cartValidation.checkReducedQuantity(PRODUCT_1);
-        cartValidation.checkReducedQuantity(PRODUCT_2);
-
-        removeItemAndCheckCartEntriesNumber(PRODUCT_1, 1);
-        removeItemAndCheckCartEntriesNumber(PRODUCT_2, 0);
       });
 
       it('should display information about removed product from cart due to out of stock', () => {
