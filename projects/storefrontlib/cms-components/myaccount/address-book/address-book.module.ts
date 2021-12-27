7bb90f202fec7a1c6b52a204d4bf3d11945017f2
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-12-27 02:02:33 -0500

    test without an ll feature - it works ootb

--- projects/storefrontlib/cms-components/myaccount/address-book/address-book.module.ts
+++ projects/storefrontlib/cms-components/myaccount/address-book/address-book.module.ts
@@ -11,6 +11,7 @@ import { CardModule } from '../../../shared/components/card/card.module';
 import { SpinnerModule } from '../../../shared/components/spinner/spinner.module';
 import { AddressBookComponent } from './address-book.component';
 import { AddressFormModule } from './address-form/address-form.module';
+import { AddressBookAuthGuard } from './auth.guard';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { AddressFormModule } from './address-form/address-form.module';
       },
     }),
     UserAddressService,
+    { provide: AuthGuard, useExisting: AddressBookAuthGuard },
   ],
 })
 export class AddressBookModule {}
---
+++ projects/storefrontlib/cms-components/myaccount/address-book/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  AuthGuard,
+  AuthRedirectService,
+  AuthService,
+  SemanticPathService,
+} from '@spartacus/core';
+
+/**
+ * Checks if there is currently logged in user.
+ * Use to protect pages dedicated only for logged in users.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class AddressBookAuthGuard extends AuthGuard {
+  constructor(
+    protected authService: AuthService,
+    protected authRedirectService: AuthRedirectService,
+    protected router: Router,
+    protected semanticPathService: SemanticPathService
+  ) {
+    super(authService, authRedirectService, router, semanticPathService);
+    console.log('custom address guard');
+  }
+}
