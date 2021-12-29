ec736955086d4bd237d872aa1b37fed30b5da580
Kornkanok Changsuwan <kornkanok.changsuwan@sap.com>
2021-12-29 15:24:39 +0100

    GH-14721: add new method and new button name, adjust method

--- feature-libs/product-configurator/rulebased/components/exit-button/configurator-exit-button.component.ts
+++ feature-libs/product-configurator/rulebased/components/exit-button/configurator-exit-button.component.ts
@@ -5,12 +5,17 @@ import {
   RoutingService,
   WindowRef,
 } from '@spartacus/core';
-import { ConfiguratorRouterExtractorService } from '@spartacus/product-configurator/common';
+import {
+  CommonConfigurator,
+  ConfiguratorRouter,
+  ConfiguratorRouterExtractorService,
+} from '@spartacus/product-configurator/common';
 import { BREAKPOINT, BreakpointService } from '@spartacus/storefront';
 import { Observable } from 'rxjs';
-import { filter, switchMap, take } from 'rxjs/operators';
+import { filter, map, switchMap, take } from 'rxjs/operators';
 import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
 import { Configurator } from '../../core/model/configurator.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'cx-configurator-exit-button',
@@ -18,6 +23,30 @@ import { Configurator } from '../../core/model/configurator.model';
 })
 export class ConfiguratorExitButtonComponent {
   product$: Observable<Product>;
+  container$: Observable<{
+    routerData: ConfiguratorRouter.Data;
+    configuration: Configurator.Configuration;
+    hasPendingChanges: boolean;
+  }> = this.configRouterExtractorService.extractRouterData().pipe(
+    switchMap((routerData) =>
+      this.configuratorCommonsService
+        .getConfiguration(routerData.owner)
+        .pipe(map((configuration) => ({ routerData, configuration })))
+        .pipe(
+          switchMap((cont) =>
+            this.configuratorCommonsService
+              .hasPendingChanges(cont.configuration.owner)
+              .pipe(
+                map((hasPendingChanges) => ({
+                  routerData: cont.routerData,
+                  configuration: cont.configuration,
+                  hasPendingChanges,
+                }))
+              )
+          )
+        )
+    )
+  );
 
   constructor(
     protected productService: ProductService,
@@ -25,29 +54,43 @@ export class ConfiguratorExitButtonComponent {
     protected configRouterExtractorService: ConfiguratorRouterExtractorService,
     protected configuratorCommonsService: ConfiguratorCommonsService,
     protected breakpointService: BreakpointService,
-    protected windowRef: WindowRef
+    protected windowRef: WindowRef,
+    protected location: Location
   ) {}
+
+  protected navigateToCart(): void {
+    this.routingService.go('cart');
+  }
   /**
    * Navigates to the product detail page of the product that is being configured.
    */
   exitConfiguration() {
-    this.configRouterExtractorService
-      .extractRouterData()
-      .pipe(
-        switchMap((routerData) =>
-          this.configuratorCommonsService.getConfiguration(routerData.owner)
-        ),
-        switchMap((configuration: Configurator.Configuration) =>
-          this.productService.get(
-            configuration.productCode ? configuration.productCode : ''
+    this.container$.pipe(take(1)).subscribe((container) => {
+      if (
+        container.routerData.owner.type ===
+        CommonConfigurator.OwnerType.CART_ENTRY
+      ) {
+        this.navigateToCart();
+      } else {
+        this.configRouterExtractorService
+          .extractRouterData()
+          .pipe(
+            switchMap((routerData) =>
+              this.configuratorCommonsService.getConfiguration(routerData.owner)
+            ),
+            switchMap((configuration: Configurator.Configuration) =>
+              this.productService.get(
+                configuration.productCode ? configuration.productCode : ''
+              )
+            ),
+            filter((product) => product !== undefined),
+            take(1)
           )
-        ),
-        filter((product) => product !== undefined),
-        take(1)
-      )
-      .subscribe((product) =>
-        this.routingService.go({ cxRoute: 'product', params: product })
-      );
+          .subscribe((product) =>
+            this.routingService.go({ cxRoute: 'product', params: product })
+          );
+      }
+    });
   }
 
   /**
@@ -66,5 +109,23 @@ export class ConfiguratorExitButtonComponent {
    */
   isMobile(): Observable<boolean> {
     return this.breakpointService?.isDown(BREAKPOINT.sm);
+  }
+
+  /**
+   * Verifies whether user is navigated from cart.
+   *
+   * @returns {Observable<boolean>} - If the given breakpoint equals or is smaller than`BREAKPOINT.sm` returns `true`, otherwise `false`.
+   */
+  isNavigatedFromCart(): boolean {
+    let isNavigatedFromCart = false;
+    this.container$.pipe(take(1)).subscribe((container) => {
+      if (
+        container.routerData.owner.type ===
+        CommonConfigurator.OwnerType.CART_ENTRY
+      ) {
+        isNavigatedFromCart = true;
+      }
+    });
+    return isNavigatedFromCart;
   }
 }
