56056ca5ec71cbe01b26e185975669c4f390fa04
wangzixi-diablo <cle.ee@163.com>
2021-12-30 12:25:27 +0800

    use force:true to avoid detached from DOM error

--- projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-flow.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/checkout-flow.ts
@@ -187,7 +187,7 @@ export function verifyDeliveryMethod() {
     '/checkout/payment-details',
     'getPaymentPage'
   );
-  cy.get('.cx-checkout-btns button.btn-primary').click();
+  cy.get('.cx-checkout-btns button.btn-primary').click({ force: true });
   cy.wait(`@${paymentPage}`).its('response.statusCode').should('eq', 200);
 }
 
