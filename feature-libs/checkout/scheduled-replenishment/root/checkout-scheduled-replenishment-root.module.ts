cdb8bdc0798cf12a10138459a2318193ab180be4
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-12-30 06:01:10 -0500

    chore: Include order feature to load when on b2b/repl (#14724)

--- feature-libs/checkout/scheduled-replenishment/root/checkout-scheduled-replenishment-root.module.ts
+++ feature-libs/checkout/scheduled-replenishment/root/checkout-scheduled-replenishment-root.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CART_FEATURE } from '@spartacus/cart/main/root';
 import {
   CheckoutB2BRootModule,
   CHECKOUT_B2B_CMS_COMPONENTS,
@@ -9,6 +10,7 @@ import {
   provideDefaultConfig,
   provideDefaultConfigFactory,
 } from '@spartacus/core';
+import { ORDER_FEATURE } from '@spartacus/order/root';
 import { defaultCheckoutScheduledReplenishmentRoutingConfig } from './config/default-checkout-scheduled-replenishment-routing-config';
 import { CheckoutScheduledReplenishmentEventModule } from './events/checkout-scheduled-replenishment-event.module';
 import {
@@ -30,6 +32,8 @@ export function defaultCheckoutComponentsConfig() {
     featureModules: {
       [CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE]: {
         cmsComponents: CHECKOUT_SCHEDULED_REPLENISHMENT_CMS_COMPONENTS,
+        // TODO:#checkout - remove ORDER_FEATURE once we move the order placing functionality to the order lib
+        dependencies: [CART_FEATURE, ORDER_FEATURE],
       },
       [CHECKOUT_B2B_FEATURE]: CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE,
       // by default core is bundled together with components
