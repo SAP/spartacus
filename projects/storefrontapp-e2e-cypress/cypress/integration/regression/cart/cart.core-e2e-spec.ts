6fdce08e7596bc7837a97d7c30f9234631353dcf
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2022-01-04 11:17:50 -0500

    minor reduction in cart core tests

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart/cart.core-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/cart/cart.core-e2e-spec.ts
@@ -1,19 +1,31 @@
 import * as cart from '../../../helpers/cart';
 import { visitHomePage } from '../../../helpers/checkout-flow';
-import * as alerts from '../../../helpers/global-message';
 import { clickHamburger } from '../../../helpers/homepage';
 import { viewportContext } from '../../../helpers/viewport-context';
 import { login } from '../../../support/utils/login';
 
 // Slow test. Improve Execution in pipeline. 
 describe('Cart', () => {
-  viewportContext(['mobile'], () => {
+  viewportContext(['desktop'], () => {
     context('Anonymous user', () => {
       it('should add and remove products', () => {
         cart.checkBasicCart();
       });
-    });
 
+      it('should be unable to add out of stock products to cart', () => {
+        cart.outOfStock();
+      });
+
+      it('should keep cart on page refresh', () => {
+        cart.addProductAsAnonymous();
+        cy.reload();
+        cart.verifyCartNotEmpty();
+      });
+
+    });
+  });
+
+  viewportContext(['desktop'], () => {
     context('Registered user', () => {
       before(() => {
         cy.window().then((win) => win.sessionStorage.clear());
@@ -37,51 +49,6 @@ describe('Cart', () => {
 
       it('should add product and manipulate cart quantity', () => {
         cart.manipulateCartQuantity();
-      });
-    });
-  });
-
-  viewportContext(['desktop'], () => {
-    context('Anonymous user', () => {
-      it('should be unable to add out of stock products to cart', () => {
-        cart.outOfStock();
-      });
-
-      it('should keep cart on page refresh', () => {
-        cart.addProductAsAnonymous();
-        cy.reload();
-        cart.verifyCartNotEmpty();
-      });
-    });
-
-    context('Registered user', () => {
-      before(() => {
-        cy.window().then((win) => win.sessionStorage.clear());
-        cart.loginRegisteredUser();
-        visitHomePage();
-      });
-
-      it('should be loaded for authenticated user after "cart not found" error', () => {
-        cart.registerCreateCartRoute();
-        cart.registerSaveCartRoute();
-        cart.loginRegisteredUser();
-        cart.addProductWhenLoggedIn(false);
-        cy.window().then((window) => {
-          const storage = JSON.parse(
-            window.localStorage.getItem('spartacus⚿electronics-spa⚿cart')
-          );
-          const cartCode = storage.active;
-          storage.active = 'incorrect-code';
-          window.localStorage.setItem(
-            'spartacus⚿electronics-spa⚿cart',
-            JSON.stringify(storage)
-          );
-          cy.visit('/cart');
-          alerts.getErrorAlert().should('contain', 'Cart not found');
-          cy.get('.cart-details-wrapper .cx-total').contains(
-            `Cart #${cartCode}`
-          );
-        });
       });
 
       it('should be loaded after user login', () => {
@@ -107,80 +74,6 @@ describe('Cart', () => {
 
         // cleanup
         cart.removeCartItem(cart.products[0]);
-        cart.validateEmptyCart();
-      });
-
-      // will fail right now, as this is not implemented yet
-      it('should first try to load cart when adding first entry for logged user', () => {
-        cart.loginCartUser();
-
-        login(
-          cart.cartUser.registrationData.email,
-          cart.cartUser.registrationData.password,
-          false
-        ).then((res) => {
-          expect(res.status).to.eq(200);
-          // remove cart
-          cy.request({
-            method: 'DELETE',
-            url: `${Cypress.env('API_URL')}/${Cypress.env(
-              'OCC_PREFIX'
-            )}/${Cypress.env('BASE_SITE')}/users/current/carts/current`,
-            headers: {
-              Authorization: `bearer ${res.body.access_token}`,
-            },
-          }).then((response) => {
-            expect(response.status).to.eq(200);
-          });
-        });
-        cy.visit(`/product/${cart.products[0].code}`);
-        cy.get('cx-breadcrumb h1').contains(cart.products[0].name);
-        login(
-          cart.cartUser.registrationData.email,
-          cart.cartUser.registrationData.password,
-          false
-        ).then((res) => {
-          cy.request({
-            // create cart
-            method: 'POST',
-            url: `${Cypress.env('API_URL')}/${Cypress.env(
-              'OCC_PREFIX'
-            )}/${Cypress.env('BASE_SITE')}/users/current/carts`,
-            headers: {
-              Authorization: `bearer ${res.body.access_token}`,
-            },
-          }).then((response) => {
-            // add entry to cart
-            return cy.request({
-              method: 'POST',
-              url: `${Cypress.env('API_URL')}/${Cypress.env(
-                'OCC_PREFIX'
-              )}/${Cypress.env('BASE_SITE')}/users/current/carts/${
-                response.body.code
-              }/entries`,
-              headers: {
-                Authorization: `bearer ${res.body.access_token}`,
-              },
-              body: {
-                product: { code: cart.products[1].code, qty: 1 },
-              },
-            });
-          });
-        });
-
-        cart.clickAddToCart();
-        cart.checkAddedToCartDialog(2);
-        cy.visit('/cart');
-        cart.checkProductInCart(cart.products[0]);
-        cart.checkProductInCart(cart.products[1]);
-
-        cart.registerCartRefreshRoute();
-
-        cart.removeCartItem(cart.products[0]);
-
-        cy.wait('@refresh_cart');
-
-        cart.removeCartItem(cart.products[1]);
         cart.validateEmptyCart();
       });
 
