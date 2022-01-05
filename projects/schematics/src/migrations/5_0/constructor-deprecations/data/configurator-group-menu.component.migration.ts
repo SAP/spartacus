e5c42d1cea95178ffbb54ca45e9e8b7596192643
Larisa Staroverova <larisa.staroverova@sap.com>
2022-01-05 11:49:28 +0100

    Add missing constructor parameters

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-group-menu.component.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-group-menu.component.migration.ts
@@ -3,6 +3,14 @@ import {
   SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
   TRANSLATION_SERVICE,
   SPARTACUS_CORE,
+  CONFIGURATOR_COMMONS_SERVICE,
+  CONFIGURATOR_GROUPS_SERVICE,
+  HAMBURGER_MENU_SERVICE,
+  CONFIGURATOR_ROUTER_EXTRACTOR_SERVICE,
+  CONFIGURATOR_STOREFRONT_UTILS_SERVICE,
+  CONFIGURATOR_GROUP_MENU_SERVICE,
+  DIRECTION_SERVICE,
+  SPARTACUS_STOREFRONTLIB,
 } from '../../../../shared/constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
@@ -11,7 +19,36 @@ export const CONFIGURATOR_GROUP_MENU_COMPONENT_MIGRATION: ConstructorDeprecation
     //feature-libs/product-configurator/rulebased/components/group-menu/configurator-group-menu.component.ts
     class: CONFIGURATOR_GROUP_MENU_COMPONENT,
     importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
-    deprecatedParams: [],
+    deprecatedParams: [
+      {
+        className: CONFIGURATOR_COMMONS_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: CONFIGURATOR_GROUPS_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: HAMBURGER_MENU_SERVICE,
+        importPath: SPARTACUS_STOREFRONTLIB,
+      },
+      {
+        className: CONFIGURATOR_ROUTER_EXTRACTOR_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: CONFIGURATOR_STOREFRONT_UTILS_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: CONFIGURATOR_GROUP_MENU_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: DIRECTION_SERVICE,
+        importPath: SPARTACUS_STOREFRONTLIB,
+      },
+    ],
     addParams: [
       {
         className: TRANSLATION_SERVICE,
