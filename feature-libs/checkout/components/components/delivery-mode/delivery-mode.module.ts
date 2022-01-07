ba38f57e9eaea8a87fc9c9afdd97db6a167226f1
Jerry Wang <58975336+wangzixi-diablo@users.noreply.github.com>
2022-01-07 14:12:39 +0800

    fix: use spinner to hide delivery mode radio input when there's ongoing HTTP put request (#14714)
    Every time the delivery mode radio input is switched in shipping mode page during checkout process, there's a HTTP put request sent to backend to set the mode. If switching the mode rapidly in a short time, there will be multiple HTT put request sent parallelly and backend cannot handle this situation. In this fix, a spinner is introduced to hide the radio input when there's ongoing HTTP put request.
    
    BREAKING CHANGE: 
    
    - the condition to show a spinner was changed to: `supportedDeliveryModes$ | async)?.length && !(isSetDeliveryModeBusy$ | async) && !continueButtonPressed`.
    - the `*ngIf` directive of wrapper `ng-container` which holds `div.row.cx-checkout-btns` is removed.
    - the `disabled` property binding of continue `button` was changed to: `deliveryModeInvalid || continueButtonPressed || (isSetDeliveryModeBusy$ | async)`.
    
    Closes #14714

--- feature-libs/checkout/components/components/delivery-mode/delivery-mode.module.ts
+++ feature-libs/checkout/components/components/delivery-mode/delivery-mode.module.ts
@@ -1,25 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  CmsConfig,
-  FeaturesConfigModule,
-  I18nModule,
-  provideDefaultConfig,
-} from '@spartacus/core';
+import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
 import { SpinnerModule } from '@spartacus/storefront';
 import { CartNotEmptyGuard } from '../../guards/cart-not-empty.guard';
 import { CheckoutAuthGuard } from '../../guards/checkout-auth.guard';
 import { DeliveryModeComponent } from './delivery-mode.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    I18nModule,
-    SpinnerModule,
-    FeaturesConfigModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, I18nModule, SpinnerModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
