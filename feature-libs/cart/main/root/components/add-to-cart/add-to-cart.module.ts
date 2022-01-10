110a176e47071f81b9af23e5b2085c3bebc6a694
Patrick Labadie <patrick.labadie@sap.com>
2022-01-10 17:14:09 -0500

    GH-14416: Optimize cart lib lazy loading - PDP

--- feature-libs/cart/main/root/components/add-to-cart/add-to-cart.module.ts
+++ feature-libs/cart/main/root/components/add-to-cart/add-to-cart.module.ts
@@ -1,38 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import {
-  CmsConfig,
-  FeaturesConfigModule,
-  I18nModule,
-  provideDefaultConfig,
-  UrlModule,
-} from '@spartacus/core';
-import {
-  IconModule,
-  ItemCounterModule,
-  KeyboardFocusModule,
-  ModalModule,
-  PromotionsModule,
-  SpinnerModule,
-} from '@spartacus/storefront';
+import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
+import { ItemCounterModule, SpinnerModule } from '@spartacus/storefront';
 import { AddToCartComponent } from './add-to-cart.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule,
     SpinnerModule,
-    PromotionsModule,
-    FeaturesConfigModule,
-    UrlModule,
-    IconModule,
     I18nModule,
     ItemCounterModule,
-    KeyboardFocusModule,
-    ModalModule,
   ],
   providers: [
     provideDefaultConfig(<CmsConfig>{
