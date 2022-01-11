8149322b498dba6750a58ba538455439251a0778
Patrick Labadie <patrick.labadie@sap.com>
2022-01-11 15:17:26 -0500

    GH-14416: Optimize cart lib lazy loading - PDP

--- feature-libs/cart/main/components/cart/added-to-cart-dialog/added-to-cart-dialog.module.ts
+++ feature-libs/cart/main/components/cart/added-to-cart-dialog/added-to-cart-dialog.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { I18nModule } from '@spartacus/core';
+import { RouterModule } from '@angular/router';
+import { I18nModule, UrlModule } from '@spartacus/core';
 import {
   IconModule,
   ItemCounterModule,
@@ -17,8 +18,10 @@ import { AddedToCartDialogComponent } from './added-to-cart-dialog.component';
     CommonModule,
     ReactiveFormsModule,
     CartSharedModule,
+    RouterModule,
     SpinnerModule,
     PromotionsModule,
+    UrlModule,
     IconModule,
     I18nModule,
     ItemCounterModule,
