53915a5adaf59fe74af4217f80c066a9c748a387
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2022-01-13 12:33:26 -0500

    force rebuild

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -85,6 +85,7 @@ else
     echo '-----'
     echo "Running Cypress end to end tests"
 
+    # Run all tests
     #if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         yarn e2e:run:ci"${SUITE}"
     #else
