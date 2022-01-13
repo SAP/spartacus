d6eea2307235fa9e613db95a7918672f384847a7
Frederik Godejohann <88314710+fgn-sybit@users.noreply.github.com>
2022-01-13 18:45:38 +0100

    Export all initializers as DI-Tokens
    We overloaded LanguageService and had our own initialize in 3.4.2
    Now Upgrading to 4 you don't call langaugeService.initialize() anymore and suggest using the LanguageInitializer.
    Nice Idea ... so please export it 🤷‍♂️😁

--- projects/core/src/site-context/services/index.ts
+++ projects/core/src/site-context/services/index.ts
@@ -1,2 +1,5 @@
 export * from './site-context-params.service';
 export * from './site-context-url-serializer';
+export * from './language-initializer';
+export * from './currency-initializer';
+export * from './base-site-initializer';
