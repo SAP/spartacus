de7103ac453ba5c283e4b7385bfa1931874ce143
Michał Dydo <michal1.dydo@gmail.com>
2022-01-20 12:42:38 +0100

    Revert env

--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -15,8 +15,7 @@ import { Environment } from './models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  // occBaseUrl: buildProcess.env.CX_BASE_URL,
-  occBaseUrl: 'https://cx-platform-current.eastus.cloudapp.azure.com:9002',
+  occBaseUrl: buildProcess.env.CX_BASE_URL,
   occApiPrefix: '/occ/v2/',
   cds: buildProcess.env.CX_CDS ?? false,
   b2b: buildProcess.env.CX_B2B ?? false,
