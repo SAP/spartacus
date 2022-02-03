a8f08c3e00abdf1bd0445ea5b944b78d2c1b6331
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-02-03 12:50:54 +0100

    updated schematics to use one feature name

--- feature-libs/checkout/schematics/constants.ts
+++ feature-libs/checkout/schematics/constants.ts
@@ -9,15 +9,12 @@ export const CHECKOUT_BASE_TRANSLATIONS = 'checkoutTranslations';
 export const CHECKOUT_BASE_TRANSLATION_CHUNKS_CONFIG =
   'checkoutTranslationChunksConfig';
 
-export const CHECKOUT_B2B_FEATURE_NAME_CONSTANT = 'CHECKOUT_B2B_FEATURE';
 export const CHECKOUT_B2B_MODULE = 'CheckoutB2BModule';
 export const CHECKOUT_B2B_ROOT_MODULE = 'CheckoutB2BRootModule';
 export const CHECKOUT_B2B_TRANSLATIONS = 'checkoutB2BTranslations';
 export const CHECKOUT_B2B_TRANSLATION_CHUNKS_CONFIG =
   'checkoutB2BTranslationChunksConfig';
 
-export const CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE_NAME_CONSTANT =
-  'CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE';
 export const CHECKOUT_SCHEDULED_REPLENISHMENT_MODULE =
   'CheckoutScheduledReplenishmentModule';
 export const CHECKOUT_SCHEDULED_REPLENISHMENT_ROOT_MODULE =
