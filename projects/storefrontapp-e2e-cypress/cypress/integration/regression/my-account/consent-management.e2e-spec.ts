f69713e652cc0e4b0b10a5759eb146e7c5da5c96
Radhep Saba <radhep.saba@gmail.com>
2022-02-04 11:52:59 -0500

    refactor tests

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/consent-management.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { viewportContext } from '../../../helpers/viewport-context';
+import {
+  consentManagementTest,
+  verifyAsAnonymous,
+} from '../../../helpers/consent-management';
+import * as login from '../../../helpers/login';
+
+viewportContext(['mobile', 'desktop'], () => {
+  describe('Consent Management Page', () => {
+    before(() => {
+      cy.window().then((win) => win.sessionStorage.clear());
+    });
+
+    describe('consent management test for anonymous user', () => {
+      verifyAsAnonymous();
+    });
+
+    describe('consent management test for logged in user', () => {
+      before(() => {
+        cy.requireLoggedIn();
+        cy.reload();
+        cy.visit('/');
+        cy.selectUserMenuOption({
+          option: 'Consent Management',
+        });
+      });
+
+      beforeEach(() => {
+        cy.restoreLocalStorage();
+      });
+
+      consentManagementTest();
+      afterEach(() => {
+        cy.saveLocalStorage();
+      });
+
+      after(() => {
+        login.signOutUser();
+      });
+    });
+  });
+});
