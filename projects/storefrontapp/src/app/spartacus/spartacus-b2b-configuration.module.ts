5d05f48a550f5d3bbaf45d1a11450c1a37dd123c
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-02-04 10:54:16 +0100

    update b2b config modulde

--- projects/storefrontapp/src/app/spartacus/spartacus-b2b-configuration.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-b2b-configuration.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import {
-  defaultB2bCheckoutConfig,
-  defaultB2bOccConfig,
-} from '@spartacus/checkout/b2b/root';
+import { defaultB2bCheckoutConfig } from '@spartacus/checkout/b2b/root';
 import { provideConfig, SiteContextConfig } from '@spartacus/core';
 import {
   defaultB2bCheckoutConfig as oldDefaultB2bCheckoutConfig,
-  defaultB2bOccConfig as oldDefaultB2bOccConfig,
+  defaultB2bOccConfig,
 } from '@spartacus/setup';
 import {
   defaultCmsContentProviders,
@@ -17,11 +14,8 @@ import {
 import { environment } from '../../environments/environment';
 
 let checkoutConfig = defaultB2bCheckoutConfig;
-let checkoutOccConfig = defaultB2bOccConfig;
-
 if (environment.oldCheckout) {
   checkoutConfig = oldDefaultB2bCheckoutConfig;
-  checkoutOccConfig = oldDefaultB2bOccConfig;
 }
 
 @NgModule({
@@ -31,7 +25,7 @@ if (environment.oldCheckout) {
     provideConfig(mediaConfig),
     ...defaultCmsContentProviders,
     // b2b
-    provideConfig(checkoutOccConfig),
+    provideConfig(defaultB2bOccConfig),
     provideConfig(checkoutConfig),
     provideConfig(<SiteContextConfig>{
       context: {
