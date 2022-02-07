0647b33a3dac4dbb39ae84e238d36b6487cd4970
Radhep Saba <radhep.saba@gmail.com>
2022-02-07 11:50:03 -0500

    lint

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/product_configurator/product-configurator-mobile.core-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/product_configurator/product-configurator-mobile.core-e2e-spec.ts
@@ -12,21 +12,21 @@ const radioGroup = 'radioGroup';
 const CAMERA_DISPLAY = 'CAMERA_DISPLAY';
 const CAMERA_MODE = 'CAMERA_MODE';
 
-  viewportContext(['mobile'], () => {
-    describe('Group Handling', () => {
-      it('should navigate using the group menu in mobile resolution', () => {
-        cy.window().then((win) => win.sessionStorage.clear());
-        cy.visit('/');
-        clickAllowAllFromBanner();
-        configurationVc.goToConfigurationPage(electronicsShop, testProduct);
-        configuration.checkHamburgerDisplayed();
-        configuration.checkAttributeDisplayed(CAMERA_MODE, radioGroup);
+viewportContext(['mobile'], () => {
+  describe('Group Handling', () => {
+    it('should navigate using the group menu in mobile resolution', () => {
+      cy.window().then((win) => win.sessionStorage.clear());
+      cy.visit('/');
+      clickAllowAllFromBanner();
+      configurationVc.goToConfigurationPage(electronicsShop, testProduct);
+      configuration.checkHamburgerDisplayed();
+      configuration.checkAttributeDisplayed(CAMERA_MODE, radioGroup);
 
-        configuration.clickHamburger();
-        configuration.checkGroupMenuDisplayed();
+      configuration.clickHamburger();
+      configuration.checkGroupMenuDisplayed();
 
-        configurationVc.clickOnGroup(2);
-        configuration.checkAttributeDisplayed(CAMERA_DISPLAY, radioGroup);
-      });
+      configurationVc.clickOnGroup(2);
+      configuration.checkAttributeDisplayed(CAMERA_DISPLAY, radioGroup);
     });
+  });
 });
