1333001e44851427fb4a8ae0130f4a65a5d82bea
Michał Gruca <pucek9@gmail.com>
2022-02-10 14:08:57 +0100

    init

--- projects/storefrontlib/cms-components/product/product-intro/product-intro.component.ts
+++ projects/storefrontlib/cms-components/product/product-intro/product-intro.component.ts
@@ -37,6 +37,7 @@ export class ProductIntroComponent implements AfterContentChecked {
         const reviewsTab = this.getTabByLabel(reviewsTabLabel, tabsComponent);
         const reviewsComponent = this.getReviewsComponent();
         if (reviewsTab && reviewsComponent) {
+          reviewsTab.focus();
           this.clickTabIfInactive(reviewsTab);
           setTimeout(
             () => reviewsComponent.scrollIntoView({ behavior: 'smooth' }),
