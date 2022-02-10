fbd4d95f62e7943d71b1f4e0905592c2300564f5
pawelfras <fras.pawel@yahoo.com>
2022-02-10 14:25:30 +0100

    test: Refactor of coupon E2E tests to avoid unnecessary delays (#14500)

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/coupons/coupons.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/coupons/coupons.e2e-spec.ts
@@ -2,26 +2,6 @@ import * as cartCoupon from '../../../helpers/coupons/cart-coupon';
 import { viewportContext } from '../../../helpers/viewport-context';
 
 describe('Cart Coupon', () => {
-  viewportContext(['mobile'], () => {
-    beforeEach(() => {
-      cy.window().then((win) => win.sessionStorage.clear());
-      cy.requireLoggedIn();
-    });
-
-    // TODO. Core test. Move to helper after GH-14500
-    it('should apply cart coupon', () => {
-      const stateAuth = JSON.parse(localStorage.getItem('spartacus⚿⚿auth'));
-      cartCoupon.visitProductPage(cartCoupon.productCode1);
-
-      //TODO products can be added to cart asynchronously
-      cartCoupon.addProductToCart(cartCoupon.productCode1);
-      cartCoupon.applyCoupon(cartCoupon.couponForCart);
-      cartCoupon.placeOrder(stateAuth.token).then((orderData) => {
-        cartCoupon.verifyOrderHistory(orderData, cartCoupon.couponForCart);
-      });
-    });
-  });
-
   viewportContext(['mobile', 'desktop'], () => {
     beforeEach(() => {
       cy.window().then((win) => win.sessionStorage.clear());
@@ -38,8 +18,8 @@ describe('Cart Coupon', () => {
       cartCoupon.visitProductPage(cartCoupon.productCode2);
       cartCoupon.addProductToCart(cartCoupon.productCode2);
       cartCoupon.applyCoupon(cartCoupon.couponForProduct);
-      cartCoupon.placeOrder(stateAuth.token).then((orderData) => {
-        cartCoupon.verifyOrderHistory(orderData, cartCoupon.couponForProduct);
+      cartCoupon.goTroughCheckout(stateAuth.token).then(() => {
+        cartCoupon.verifyCouponInReviewOrder(cartCoupon.couponForProduct);
       });
     });
 
@@ -49,8 +29,8 @@ describe('Cart Coupon', () => {
       cartCoupon.addProductToCart(cartCoupon.productCode3);
       cartCoupon.applyCoupon(cartCoupon.freeGiftCoupon);
       cartCoupon.verifyGiftProductCoupon(cartCoupon.giftProductCode);
-      cartCoupon.placeOrder(stateAuth.token).then((orderData) => {
-        cartCoupon.verifyOrderHistory(orderData, cartCoupon.freeGiftCoupon);
+      cartCoupon.goTroughCheckout(stateAuth.token).then(() => {
+        cartCoupon.verifyCouponInReviewOrder(cartCoupon.freeGiftCoupon);
       });
     });
 
@@ -61,8 +41,8 @@ describe('Cart Coupon', () => {
       cartCoupon.applyCoupon(cartCoupon.couponForCart);
       cartCoupon.removeCoupon(cartCoupon.couponForCart);
 
-      cartCoupon.placeOrder(stateAuth.token).then((orderData) => {
-        cartCoupon.verifyOrderHistory(orderData);
+      cartCoupon.goTroughCheckout(stateAuth.token).then(() => {
+        cartCoupon.verifyCouponInReviewOrder();
       });
     });
   });
