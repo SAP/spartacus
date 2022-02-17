1f1232fcd972c66c075a7ebc50ff28fa78dc55e5
BenjaminEmiliani <benjaminemi129@gmail.com>
2022-02-17 11:19:41 -0500

    fix: use single input object in directive (#14997)

--- projects/storefrontlib/shared/components/bind-attributes/bind-attributes.directive.ts
+++ projects/storefrontlib/shared/components/bind-attributes/bind-attributes.directive.ts
@@ -13,25 +13,32 @@ export class BindAttributesDirective implements AfterViewInit {
   /**
    * Usage:
    * [cxBindAttributes]="{
-      'aria-label': selectedLabel,
-      'aria-controls': ariaControls
+      target: '[role=\'combobox\']',
+      attributes: {
+        'aria-label': selectedLabel,
+        'aria-controls': ariaControls
+      }
      }"
    */
-  @Input() cxBindAttributes: { [attribute: string]: any };
-
-  /* DOMString that matches the element to bind the attributes */
-  @Input() cxBindTarget: string;
+  @Input() cxBindAttributes: {
+    target: string /* DOMString that matches the element to bind the attributes */;
+    attributes: { [attribute: string]: any };
+  };
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngAfterViewInit(): void {
-    if (this.cxBindAttributes) {
-      for (const attributeName in this.cxBindAttributes) {
-        if (this.cxBindAttributes.hasOwnProperty(attributeName)) {
-          const attributeValue = this.cxBindAttributes[attributeName];
+    const attributes = this.cxBindAttributes.attributes;
+    const target = this.cxBindAttributes.target;
+
+    if (attributes) {
+      for (const attributeName in attributes) {
+        if (attributes.hasOwnProperty(attributeName)) {
+          const attributeValue = attributes[attributeName];
+
           if (attributeValue) {
             this.renderer.setAttribute(
-              this.elementRef.nativeElement.querySelector(this.cxBindTarget) ??
+              this.elementRef.nativeElement.querySelector(target) ??
                 this.elementRef.nativeElement,
               attributeName,
               attributeValue
