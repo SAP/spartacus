0da0dd70090615c672396aec0889d4fd06c880fa
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-02-17 12:02:37 +0100

    simplify condition

--- feature-libs/checkout/b2b/components/checkout-delivery-address/checkout-delivery-address.component.ts
+++ feature-libs/checkout/b2b/components/checkout-delivery-address/checkout-delivery-address.component.ts
@@ -117,8 +117,7 @@ export class B2BCheckoutDeliveryAddressComponent
   ): void {
     if (
       !this.doneAutoSelect &&
-      addresses &&
-      addresses.length &&
+      addresses?.length &&
       (!selected || Object.keys(selected).length === 0)
     ) {
       if (this.isAccountPayment) {
