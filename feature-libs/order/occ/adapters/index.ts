4a5d2228ec418e7f56fb2dd6e6576ea4621f2e68
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-02-22 23:30:55 -0500

    move adapters to order/occ/adapters

--- feature-libs/order/occ/adapters/index.ts
+++ feature-libs/order/occ/adapters/index.ts
@@ -1,3 +1,5 @@
 export * from './converters/index';
+export * from './occ-checkout-replenishment-order.adapter';
+export * from './occ-checkout.adapter';
 export * from './occ-order.adapter';
 export * from './occ-replenishment-order.adapter';
