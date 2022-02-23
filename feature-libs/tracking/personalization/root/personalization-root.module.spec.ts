5faedafcf95be3191101012773a487723e78cabd
Patryk Smul <psmul@divante.pl>
2022-02-23 09:54:53 +0100

    fix: prettier

--- feature-libs/tracking/personalization/root/personalization-root.module.spec.ts
+++ feature-libs/tracking/personalization/root/personalization-root.module.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { CmsConfig } from '@spartacus/core';
-import { defaultPersonalizationComponentsConfig, PersonalizationRootModule } from './personalization-root.module';
+import {
+  defaultPersonalizationComponentsConfig,
+  PersonalizationRootModule,
+} from './personalization-root.module';
 import { PERSONALIZATION_FEATURE } from './feature-name';
 
 const MockCmsConfig: CmsConfig = {
-	featureModules: {
-		[PERSONALIZATION_FEATURE]: {
-			cmsComponents: ['PersonalizationScriptComponent'],
-		},
-	},
+  featureModules: {
+    [PERSONALIZATION_FEATURE]: {
+      cmsComponents: ['PersonalizationScriptComponent'],
+    },
+  },
 };
 
 describe('FeatureModule', () => {
@@ -23,8 +26,8 @@ describe('FeatureModule', () => {
     expect(module).toBeTruthy();
   });
 
-	it('defaultPersonalizationComponentsConfig', () => {
-		const result = defaultPersonalizationComponentsConfig();
-		expect(result).toEqual(MockCmsConfig);
+  it('defaultPersonalizationComponentsConfig', () => {
+    const result = defaultPersonalizationComponentsConfig();
+    expect(result).toEqual(MockCmsConfig);
   });
 });
