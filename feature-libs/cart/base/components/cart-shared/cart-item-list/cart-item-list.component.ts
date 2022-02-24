2247f2aab9147970a15fb12e283b256b0f617fe7
Weizheng Gao <weizheng.gao@sap.com>
2022-02-24 17:14:32 -0400

    feature/GH-14957: format codes

--- feature-libs/cart/base/components/cart-shared/cart-item-list/cart-item-list.component.ts
+++ feature-libs/cart/base/components/cart-shared/cart-item-list/cart-item-list.component.ts
@@ -5,7 +5,7 @@ import {
   Input,
   OnDestroy,
   OnInit,
-  Optional
+  Optional,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import {
@@ -15,7 +15,7 @@ import {
   MultiCartFacade,
   OrderEntry,
   PromotionLocation,
-  SelectiveCartFacade
+  SelectiveCartFacade,
 } from '@spartacus/cart/base/root';
 import { UserIdService } from '@spartacus/core';
 import { OutletContextData } from '@spartacus/storefront';
