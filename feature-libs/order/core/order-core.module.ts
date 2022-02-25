09d6bddac0d7a42696133706c2bc07b4775e534a
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-02-25 00:54:39 -0500

    wire in the connector and adapters

--- feature-libs/order/core/order-core.module.ts
+++ feature-libs/order/core/order-core.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { OrderConnector } from './connectors/order.connector';
 import { ReplenishmentOrderConnector } from './connectors/replenishment-order.connector';
+import { UnnamedReplenishmentOrderConnector } from './connectors/unnamed-replenishment-order.connector';
+import { UnnamedConnector } from './connectors/unnamed.connector';
 import { facadeProviders } from './facade/facade-providers';
 import { OrderStoreModule } from './store/order-store.module';
 
 @NgModule({
   imports: [OrderStoreModule],
-  providers: [OrderConnector, ReplenishmentOrderConnector, ...facadeProviders],
+  providers: [
+    ...facadeProviders,
+    OrderConnector,
+    ReplenishmentOrderConnector,
+    UnnamedConnector,
+    UnnamedReplenishmentOrderConnector,
+  ],
 })
 export class OrderCoreModule {}
--- feature-libs/order/occ/order-occ.module.ts
+++ feature-libs/order/occ/order-occ.module.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { provideDefaultConfig } from '@spartacus/core';
-import { OrderAdapter, ReplenishmentOrderAdapter } from '@spartacus/order/core';
+import {
+  OrderAdapter,
+  ReplenishmentOrderAdapter,
+  UnnamedAdapter,
+  UnnamedReplenishmentOrderAdapter,
+} from '@spartacus/order/core';
 import {
   ORDER_NORMALIZER,
   ORDER_RETURN_REQUEST_NORMALIZER,
+  REPLENISHMENT_ORDER_FORM_SERIALIZER,
   REPLENISHMENT_ORDER_NORMALIZER,
 } from '@spartacus/order/root';
 import { OccOrderNormalizer } from './adapters/converters/occ-order-normalizer';
 import { OccReplenishmentOrderNormalizer } from './adapters/converters/occ-replenishment-order-normalizer';
 import { OccReturnRequestNormalizer } from './adapters/converters/occ-return-request-normalizer';
+import { OccUnnamedReplenishmentOrderFormSerializer } from './adapters/converters/occ-unnamed-replenishment-order-form-serializer';
+import { OccUnnamedAdapter } from './adapters/occ-checkout.adapter';
 import { OccOrderAdapter } from './adapters/occ-order.adapter';
 import { OccReplenishmentOrderAdapter } from './adapters/occ-replenishment-order.adapter';
 import { defaultOccOrderConfig } from './config/default-occ-order-config';
@@ -21,6 +29,14 @@ import { defaultOccOrderConfig } from './config/default-occ-order-config';
     { provide: OrderAdapter, useClass: OccOrderAdapter },
     {
       provide: ReplenishmentOrderAdapter,
+      useClass: OccReplenishmentOrderAdapter,
+    },
+    {
+      provide: UnnamedAdapter,
+      useClass: OccUnnamedAdapter,
+    },
+    {
+      provide: UnnamedReplenishmentOrderAdapter,
       useClass: OccReplenishmentOrderAdapter,
     },
     {
@@ -36,6 +52,11 @@ import { defaultOccOrderConfig } from './config/default-occ-order-config';
     {
       provide: REPLENISHMENT_ORDER_NORMALIZER,
       useExisting: OccReplenishmentOrderNormalizer,
+      multi: true,
+    },
+    {
+      provide: REPLENISHMENT_ORDER_FORM_SERIALIZER,
+      useExisting: OccUnnamedReplenishmentOrderFormSerializer,
       multi: true,
     },
   ],
