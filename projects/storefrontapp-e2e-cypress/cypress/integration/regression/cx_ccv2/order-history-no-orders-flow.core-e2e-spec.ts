95eb85390bf085e13c3df48bfcaf0d742cfe2c0e
Giancarlo Cordero Ortiz <46171897+giancorderoortiz@users.noreply.github.com>
2022-02-25 17:47:10 -0500

    select subset of core tests to run in cx ccv2

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/cx_ccv2/order-history-no-orders-flow.core-e2e-spec.ts
@@ -0,0 +1,23 @@
+import * as userAccountHelpers from '../../../helpers/login';
+import { clickHamburger } from '../../../helpers/homepage';
+import { orderHistoryTest } from '../../../helpers/order-history';
+import { verifyGlobalMessageAfterRegistration } from '../../../helpers/register';
+import { viewportContext } from '../../../helpers/viewport-context';
+
+describe('Order History with no orders', () => {
+  viewportContext(['desktop'], () => {
+    before(() => {
+      cy.window().then((win) => win.sessionStorage.clear());
+      cy.visit('/');
+      cy.onMobile(() => {
+        clickHamburger();
+      });
+      userAccountHelpers.registerUser();
+      verifyGlobalMessageAfterRegistration();
+    });
+
+    orderHistoryTest.checkRedirectNotLoggedInUser();
+    orderHistoryTest.checkRedirectLoggedInUser();
+    orderHistoryTest.checkStartShoppingButton();
+  });
+});
