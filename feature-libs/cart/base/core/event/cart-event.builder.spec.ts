a712ec80494a2a4872d3f0f3786568e1c6e7e5e2
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-02-25 14:09:35 -0500

    grouping test 'same' test cases in cart event builder

--- feature-libs/cart/base/core/event/cart-event.builder.spec.ts
+++ feature-libs/cart/base/core/event/cart-event.builder.spec.ts
@@ -132,221 +132,241 @@ describe('CartEventBuilder', () => {
       subscription.unsubscribe();
     });
 
-    it('CartAddEntryEvent', () => {
-      const eventData: CartAddEntryEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        productCode: 'productCode',
-        quantity: 123,
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
+    describe('Cart Add Entry Events', () => {
+      describe('CartAddEntryEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const eventData: CartAddEntryEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            productCode: 'productCode',
+            quantity: 123,
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
 
-      testActionToEventMapping({
-        event: createFrom(CartAddEntryEvent, eventData),
-        actionActive: new CartActions.CartAddEntry(eventData),
-        actionNotActive: new CartActions.CartAddEntry({
-          ...eventData,
-          ...MOCK_NOT_ACTIVE_CART_EVENT,
-        }),
+          testActionToEventMapping({
+            event: createFrom(CartAddEntryEvent, eventData),
+            actionActive: new CartActions.CartAddEntry(eventData),
+            actionNotActive: new CartActions.CartAddEntry({
+              ...eventData,
+              ...MOCK_NOT_ACTIVE_CART_EVENT,
+            }),
+          });
+        });
+      });
+
+      describe('CartAddEntrySuccessEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const eventData: CartAddEntrySuccessEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            productCode: 'productCode',
+            quantity: 123,
+            deliveryModeChanged: true,
+            entry: {},
+            quantityAdded: 1,
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
+
+          testActionToEventMapping({
+            event: createFrom(CartAddEntrySuccessEvent, eventData),
+            actionActive: new CartActions.CartAddEntrySuccess({
+              ...eventData,
+              statusCode: null,
+              statusMessage: null,
+            }),
+            actionNotActive: new CartActions.CartAddEntrySuccess({
+              ...eventData,
+              statusCode: null,
+              statusMessage: null,
+              ...MOCK_NOT_ACTIVE_CART_EVENT,
+            }),
+          });
+        });
+      });
+
+      describe('CartAddEntryFailEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const eventData: CartAddEntryFailEvent = {
+            productCode: 'productCode',
+            cartCode: MOCK_ACTIVE_CART.code,
+            quantity: 123,
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
+
+          testActionToEventMapping({
+            event: createFrom(CartAddEntryFailEvent, eventData),
+            actionActive: new CartActions.CartAddEntryFail({
+              ...eventData,
+              error: 'error',
+            }),
+            actionNotActive: new CartActions.CartAddEntryFail({
+              ...eventData,
+              error: 'error',
+              ...MOCK_NOT_ACTIVE_CART_EVENT,
+            }),
+          });
+        });
       });
     });
 
-    it('CartAddEntrySuccessEvent', () => {
-      const eventData: CartAddEntrySuccessEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        productCode: 'productCode',
-        quantity: 123,
-        deliveryModeChanged: true,
-        entry: {},
-        quantityAdded: 1,
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
+    describe('Cart Remove Entry Events', () => {
+      describe('CartRemoveEntrySuccessEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const firstEventData: CartRemoveEntrySuccessEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[0],
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
 
-      testActionToEventMapping({
-        event: createFrom(CartAddEntrySuccessEvent, eventData),
-        actionActive: new CartActions.CartAddEntrySuccess({
-          ...eventData,
-          statusCode: null,
-          statusMessage: null,
-        }),
-        actionNotActive: new CartActions.CartAddEntrySuccess({
-          ...eventData,
-          statusCode: null,
-          statusMessage: null,
-          ...MOCK_NOT_ACTIVE_CART_EVENT,
-        }),
+          const secondEventData: CartRemoveEntrySuccessEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[1],
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
+
+          const result = [];
+          const subscription = eventService
+            .get(CartRemoveEntrySuccessEvent)
+            .subscribe((e) => result.push(e));
+
+          actions$.next(
+            new CartActions.CartRemoveEntrySuccess({
+              entryNumber: '0',
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+          actions$.next(
+            new CartActions.CartRemoveEntrySuccess({
+              entryNumber: '0',
+              ...MOCK_NOT_ACTIVE_CART_EVENT,
+            })
+          );
+
+          actions$.next(
+            new CartActions.CartRemoveEntrySuccess({
+              entryNumber: '1',
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+
+          expect(result.length).toBe(2);
+          expect(result[0].constructor).toEqual(CartRemoveEntrySuccessEvent);
+          expect(result[0]).toEqual(jasmine.objectContaining(firstEventData));
+          expect(result[1]).toEqual(jasmine.objectContaining(secondEventData));
+
+          subscription.unsubscribe();
+        });
+      });
+
+      describe('CartRemoveEntryFailEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const firstEventData: CartRemoveEntryFailEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[0],
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
+
+          const secondEventData: CartRemoveEntryFailEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[1],
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
+
+          const results: CartRemoveEntryFailEvent[] = [];
+          const subscription = eventService
+            .get(CartRemoveEntryFailEvent)
+            .subscribe((e) => results.push(e));
+
+          actions$.next(
+            new CartActions.CartRemoveEntryFail({
+              error: 'remove failed',
+              entryNumber: '0',
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+          actions$.next(
+            new CartActions.CartRemoveEntryFail({
+              error: 'remove failed',
+              entryNumber: '0',
+              ...MOCK_NOT_ACTIVE_CART_EVENT,
+            })
+          );
+
+          actions$.next(
+            new CartActions.CartRemoveEntryFail({
+              error: 'remove failed',
+              entryNumber: '1',
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+
+          expect(results.length).toBe(2);
+          expect(results[0].constructor).toEqual(CartRemoveEntryFailEvent);
+          expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
+          expect(results[1]).toEqual(jasmine.objectContaining(secondEventData));
+
+          subscription.unsubscribe();
+        });
       });
     });
 
-    it('CartAddEntryFailEvent', () => {
-      const eventData: CartAddEntryFailEvent = {
-        productCode: 'productCode',
-        cartCode: MOCK_ACTIVE_CART.code,
-        quantity: 123,
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
+    describe('Cart Update Entry Events', () => {
+      describe('CartUpdateEntrySuccessEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const firstEventData: CartUpdateEntrySuccessEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[0],
+            quantity: 2,
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
 
-      testActionToEventMapping({
-        event: createFrom(CartAddEntryFailEvent, eventData),
-        actionActive: new CartActions.CartAddEntryFail({
-          ...eventData,
-          error: 'error',
-        }),
-        actionNotActive: new CartActions.CartAddEntryFail({
-          ...eventData,
-          error: 'error',
-          ...MOCK_NOT_ACTIVE_CART_EVENT,
-        }),
+          const results: CartUpdateEntrySuccessEvent[] = [];
+          const subscription = eventService
+            .get(CartUpdateEntrySuccessEvent)
+            .subscribe((e) => results.push(e));
+
+          actions$.next(
+            new CartActions.CartUpdateEntrySuccess({
+              entryNumber: '0',
+              quantity: 2,
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+          expect(results.length).toBe(1);
+          expect(results[0].constructor).toEqual(CartUpdateEntrySuccessEvent);
+          expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
+
+          subscription.unsubscribe();
+        });
       });
-    });
 
-    it('CartRemoveEntrySuccessEvent', () => {
-      const firstEventData: CartRemoveEntrySuccessEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[0],
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
+      describe('CartUpdateEntryFailEvent', () => {
+        it('should emit the event when the action is fired', () => {
+          const firstEventData: CartUpdateEntryFailEvent = {
+            cartCode: MOCK_ACTIVE_CART.code,
+            entry: MOCK_ACTIVE_CART.entries[0],
+            quantity: 2,
+            ...MOCK_ACTIVE_CART_EVENT,
+          };
 
-      const secondEventData: CartRemoveEntrySuccessEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[1],
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
+          const results: CartUpdateEntryFailEvent[] = [];
+          const subscription = eventService
+            .get(CartUpdateEntryFailEvent)
+            .subscribe((e) => results.push(e));
 
-      const result = [];
-      const subscription = eventService
-        .get(CartRemoveEntrySuccessEvent)
-        .subscribe((e) => result.push(e));
+          actions$.next(
+            new CartActions.CartUpdateEntryFail({
+              error: 'update failed',
+              entryNumber: '0',
+              quantity: 2,
+              ...MOCK_ACTIVE_CART_EVENT,
+            })
+          );
+          expect(results.length).toBe(1);
+          expect(results[0].constructor).toEqual(CartUpdateEntryFailEvent);
+          expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
 
-      actions$.next(
-        new CartActions.CartRemoveEntrySuccess({
-          entryNumber: '0',
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-      actions$.next(
-        new CartActions.CartRemoveEntrySuccess({
-          entryNumber: '0',
-          ...MOCK_NOT_ACTIVE_CART_EVENT,
-        })
-      );
-
-      actions$.next(
-        new CartActions.CartRemoveEntrySuccess({
-          entryNumber: '1',
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-
-      expect(result.length).toBe(2);
-      expect(result[0].constructor).toEqual(CartRemoveEntrySuccessEvent);
-      expect(result[0]).toEqual(jasmine.objectContaining(firstEventData));
-      expect(result[1]).toEqual(jasmine.objectContaining(secondEventData));
-
-      subscription.unsubscribe();
-    });
-
-    it('CartRemoveEntryFailEvent', () => {
-      const firstEventData: CartRemoveEntryFailEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[0],
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
-
-      const secondEventData: CartRemoveEntryFailEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[1],
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
-
-      const results: CartRemoveEntryFailEvent[] = [];
-      const subscription = eventService
-        .get(CartRemoveEntryFailEvent)
-        .subscribe((e) => results.push(e));
-
-      actions$.next(
-        new CartActions.CartRemoveEntryFail({
-          error: 'remove failed',
-          entryNumber: '0',
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-      actions$.next(
-        new CartActions.CartRemoveEntryFail({
-          error: 'remove failed',
-          entryNumber: '0',
-          ...MOCK_NOT_ACTIVE_CART_EVENT,
-        })
-      );
-
-      actions$.next(
-        new CartActions.CartRemoveEntryFail({
-          error: 'remove failed',
-          entryNumber: '1',
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-
-      expect(results.length).toBe(2);
-      expect(results[0].constructor).toEqual(CartRemoveEntryFailEvent);
-      expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
-      expect(results[1]).toEqual(jasmine.objectContaining(secondEventData));
-
-      subscription.unsubscribe();
-    });
-
-    it('CartUpdateEntrySuccessEvent', () => {
-      const firstEventData: CartUpdateEntrySuccessEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[0],
-        quantity: 2,
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
-
-      const results: CartUpdateEntrySuccessEvent[] = [];
-      const subscription = eventService
-        .get(CartUpdateEntrySuccessEvent)
-        .subscribe((e) => results.push(e));
-
-      actions$.next(
-        new CartActions.CartUpdateEntrySuccess({
-          entryNumber: '0',
-          quantity: 2,
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-      expect(results.length).toBe(1);
-      expect(results[0].constructor).toEqual(CartUpdateEntrySuccessEvent);
-      expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
-
-      subscription.unsubscribe();
-    });
-
-    it('CartUpdateEntryFailEvent', () => {
-      const firstEventData: CartUpdateEntryFailEvent = {
-        cartCode: MOCK_ACTIVE_CART.code,
-        entry: MOCK_ACTIVE_CART.entries[0],
-        quantity: 2,
-        ...MOCK_ACTIVE_CART_EVENT,
-      };
-
-      const results: CartUpdateEntryFailEvent[] = [];
-      const subscription = eventService
-        .get(CartUpdateEntryFailEvent)
-        .subscribe((e) => results.push(e));
-
-      actions$.next(
-        new CartActions.CartUpdateEntryFail({
-          error: 'update failed',
-          entryNumber: '0',
-          quantity: 2,
-          ...MOCK_ACTIVE_CART_EVENT,
-        })
-      );
-      expect(results.length).toBe(1);
-      expect(results[0].constructor).toEqual(CartUpdateEntryFailEvent);
-      expect(results[0]).toEqual(jasmine.objectContaining(firstEventData));
-
-      subscription.unsubscribe();
+          subscription.unsubscribe();
+        });
+      });
     });
 
     describe('Delete Cart Events', () => {
