f9d35a9b6da4e7291fa81cf54bc48a59d31942ca
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-02-28 12:53:57 -0500

    feat: Move the placeOrder() logic from the checkout library to the order library (#15206)
    Moves the placing/scheduling an order logic from checkout to order-lib for optimizing lazy loading of chunks, as we will programatically load order-lib upon placing an order.
    
    closes GH-14649

--- feature-libs/order/core/order-core.module.ts
+++ feature-libs/order/core/order-core.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+import { OrderHistoryConnector } from './connectors/order-history.connector';
 import { OrderConnector } from './connectors/order.connector';
-import { ReplenishmentOrderConnector } from './connectors/replenishment-order.connector';
+import { ReplenishmentOrderHistoryConnector } from './connectors/replenishment-order-history.connector';
+import { ScheduledReplenishmentOrderConnector } from './connectors/scheduled-replenishment-order.connector';
 import { facadeProviders } from './facade/facade-providers';
 import { OrderStoreModule } from './store/order-store.module';
 
 @NgModule({
   imports: [OrderStoreModule],
-  providers: [OrderConnector, ReplenishmentOrderConnector, ...facadeProviders],
+  providers: [
+    ...facadeProviders,
+    OrderHistoryConnector,
+    ReplenishmentOrderHistoryConnector,
+    OrderConnector,
+    ScheduledReplenishmentOrderConnector,
+  ],
 })
 export class OrderCoreModule {}
