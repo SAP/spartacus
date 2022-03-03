50d71ad8cf9976e7629310a3ae67abcc68a5bc5c
Krzysztof Platis <platonn.git@gmail.com>
2022-03-03 16:25:58 +0100

    change comment

--- projects/core/src/i18n/i18next/i18next-init.ts
+++ projects/core/src/i18n/i18next/i18next-init.ts
@@ -43,7 +43,7 @@ export function i18nextInit(
               loadPath,
               request: i18nextGetHttpClient(httpClient),
 
-              // Disable the periodical reloading, because the `setInterval()` used inside `i18next-http-backend` would prevent the SSR to finish
+              // Disable the periodical reloading. Otherwise the SSR would not finish due to the pending task `setInterval()`
               // See source code: https://github.com/i18next/i18next-http-backend/blob/00b7e8f67abf8372af17529b51190a7e8b17e3d8/i18nextHttpBackend.js#L94-L95
               reloadInterval: false,
             };
