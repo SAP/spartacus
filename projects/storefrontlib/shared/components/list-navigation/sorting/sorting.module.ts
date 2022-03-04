c26c3b89030ac793fe7bdb4c991909859ab2b660
BenjaminEmiliani <benjaminemi129@gmail.com>
2022-03-04 11:19:53 -0500

    fix: bind aria label to listbox (#14997)

--- projects/storefrontlib/shared/components/list-navigation/sorting/sorting.module.ts
+++ projects/storefrontlib/shared/components/list-navigation/sorting/sorting.module.ts
@@ -4,9 +4,16 @@ import { SortingComponent } from './sorting.component';
 import { CxNgSelectModule } from '../../ng-select/ng-select.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
+import { I18nModule } from '@spartacus/core';
 
 @NgModule({
-  imports: [CommonModule, NgSelectModule, FormsModule, CxNgSelectModule],
+  imports: [
+    CommonModule,
+    NgSelectModule,
+    FormsModule,
+    CxNgSelectModule,
+    I18nModule,
+  ],
   declarations: [SortingComponent],
   exports: [SortingComponent],
 })
--- projects/storefrontlib/shared/components/ng-select/ng-select.directive.ts
+++ projects/storefrontlib/shared/components/ng-select/ng-select.directive.ts
@@ -1,15 +1,24 @@
-import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[cxNgSelect]',
 })
 export class NgSelectDirective implements AfterViewInit {
+  @Input() cxNgSelect: string;
+
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngAfterViewInit(): void {
     const divCombobox =
       this.elementRef.nativeElement.querySelector('[role="combobox"]');
 
+    this.renderer.setAttribute(divCombobox, 'aria-label', this.cxNgSelect);
     this.renderer.setAttribute(divCombobox, 'role', 'listbox');
   }
 }
