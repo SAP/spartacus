d3973cd4dee25c778ab4f234010abcd8545ebedc
Zeyber <rotherham.c@gmail.com>
2022-03-09 12:34:43 +0100

    Use click event listener to handle internal links

--- projects/storefrontlib/cms-components/content/paragraph/paragraph.module.ts
+++ projects/storefrontlib/cms-components/content/paragraph/paragraph.module.ts
@@ -1,23 +1,11 @@
-import {
-  Compiler,
-  CompilerFactory,
-  COMPILER_OPTIONS,
-  NgModule,
-} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CmsConfig, provideDefaultConfig } from '@spartacus/core';
 import { SupplementHashAnchorsModule } from '../../../shared/pipes/suplement-hash-anchors/supplement-hash-anchors.module';
 import { ParagraphComponent } from './paragraph.component';
-import { HtmlOutletDirective } from './html-outlet';
-import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
-import { SupplementInternalLinksModule } from '../../../shared/pipes/suplement-internal-links/supplement-internal-links.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SupplementHashAnchorsModule,
-    SupplementInternalLinksModule,
-  ],
+  imports: [CommonModule, SupplementHashAnchorsModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
@@ -29,19 +17,8 @@ import { SupplementInternalLinksModule } from '../../../shared/pipes/suplement-i
         },
       },
     }),
-    { provide: COMPILER_OPTIONS, useValue: {}, multi: true },
-    {
-      provide: CompilerFactory,
-      useClass: JitCompilerFactory,
-      deps: [COMPILER_OPTIONS],
-    },
-    { provide: Compiler, useFactory: createCompiler, deps: [CompilerFactory] },
   ],
-  declarations: [ParagraphComponent, HtmlOutletDirective],
+  declarations: [ParagraphComponent],
   exports: [ParagraphComponent],
 })
 export class CmsParagraphModule {}
-
-export function createCompiler(compilerFactory: CompilerFactory) {
-  return compilerFactory.createCompiler();
-}
--- projects/storefrontlib/shared/pipes/suplement-internal-links/supplement-internal-links.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SupplementInternalLinksPipe } from './supplement-internal-links.pipe';
-
-@NgModule({
-  declarations: [SupplementInternalLinksPipe],
-  exports: [SupplementInternalLinksPipe],
-})
-export class SupplementInternalLinksModule {}
