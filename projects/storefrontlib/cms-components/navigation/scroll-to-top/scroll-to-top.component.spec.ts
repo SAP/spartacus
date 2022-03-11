c577bda89b8968a02e8fcdad0f6461e0846e81b1
BenjaminEmiliani <benjaminemi129@gmail.com>
2022-03-11 09:25:33 -0500

    test: add unit test for feature (#15356)

---
+++ projects/storefrontlib/cms-components/navigation/scroll-to-top/scroll-to-top.component.spec.ts
@@ -0,0 +1,69 @@
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Component, DebugElement } from '@angular/core';
+import { WindowRef } from '@spartacus/core';
+import { ScrollToTopModule } from '@spartacus/storefront';
+
+@Component({
+  template: `
+    <div style="height: 2000px;">
+      <h1>Test page</h1>
+      <cx-scroll-to-top></cx-scroll-to-top>
+    </div>
+  `,
+})
+class MockComponent {}
+
+// class MockScrollToTopComponent implements Partial<ScrollToTopComponent> {
+//   scrollToTop(): void {}
+// }
+
+fdescribe('ScrollToTopComponent', () => {
+  let component: MockComponent;
+  // let scrollToTopComponent: ScrollToTopComponent;
+  let fixture: ComponentFixture<MockComponent>;
+  let winRef: WindowRef;
+  let el: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent],
+      imports: [ScrollToTopModule],
+    }).compileComponents();
+
+    winRef = TestBed.inject(WindowRef);
+    fixture = TestBed.createComponent(MockComponent);
+
+    component = fixture.componentInstance;
+    el = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should not be visible at top of page', () => {
+    winRef.nativeWindow?.scrollTo(0, 0);
+    fixture.detectChanges();
+    expect(el.query(By.css('.scroll-to-top-btn'))).toBeNull();
+  });
+
+  it('should be visible scroll to top of page', fakeAsync(() => {
+    winRef.nativeWindow?.scrollTo(0, 200);
+    winRef.nativeWindow?.dispatchEvent(new Event('scroll'));
+
+    fixture.detectChanges();
+    const scrollBtn = el.query(By.css('.scroll-to-top-btn')).nativeElement;
+    expect(el.query(By.css('.scroll-to-top-btn'))).toBeTruthy();
+
+    scrollBtn.click();
+    tick(3000);
+    fixture.detectChanges();
+    expect(winRef.nativeWindow?.scrollY).toEqual(0);
+  }));
+});
