73261b57d89d47c85ce88fee5059c97e2c40cd2d
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-03-16 16:17:01 -0400

    backport consolidating cypress config files

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -46,6 +46,14 @@ done
 
 set -- "${POSITIONAL[@]}"
 
+if [ "$SUITE" == ":ccv2" ]; then
+    export SPA_ENV='ccv2,b2c'
+fi
+
+if [ "$SUITE" == ":ccv2-b2b" ]; then
+    export SPA_ENV='ccv2,b2b'
+fi
+
 echo '-----'
 echo "Building Spartacus libraries"
 
