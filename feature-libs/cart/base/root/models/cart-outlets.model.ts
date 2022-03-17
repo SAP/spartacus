3372560d1ff8f0abf5b4f199e9f5b8891b654437
Michał Gruca <pucek9@gmail.com>
2022-03-17 11:49:01 +0100

    UPDATE outlets

--- feature-libs/cart/base/root/models/cart-outlets.model.ts
+++ feature-libs/cart/base/root/models/cart-outlets.model.ts
@@ -3,6 +3,7 @@
  */
 export enum CartOutlets {
   ITEM = 'cx-cart-item',
+  LIST_ITEM = 'cx-cart-item-list-row',
   ITEM_DETAILS = 'cx-cart-item.details',
   ITEM_CONFIGURATOR_ISSUES = 'cx-configurator-issues-notification',
   ITEM_BUNDLE_DETAILS = 'cx-cart-item.bundle-details',
