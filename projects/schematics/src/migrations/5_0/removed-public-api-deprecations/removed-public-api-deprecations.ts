e0f47bf3f604466044a7e64d14599b4271d2d34a
Patrick Labadie <patrick.labadie@sap.com>
2022-03-17 17:01:22 -0400

    GH-15306: File rename.

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -1,6 +1,6 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { removedPublicApiDeprecation } from '../../mechanism/removed-public-api-deprecations/removed-public-api-deprecation';
-import { REMOVED_PUBLIC_API_DATA } from './cart-lib-checkout.migration';
+import { REMOVED_PUBLIC_API_DATA } from './removed-public-api.migration';
 
 export function migrate(): Rule {
   return (tree: Tree, context: SchematicContext) => {
