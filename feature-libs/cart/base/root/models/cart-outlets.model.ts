2fba236b2ab93ac62dd22bff3c90d6fac9c8e83e
Michał Gruca <pucek9@gmail.com>
2022-03-17 11:44:41 +0100

    UPDATE ConfiguratorIssuesNotificationComponent

--- feature-libs/cart/base/root/models/cart-outlets.model.ts
+++ feature-libs/cart/base/root/models/cart-outlets.model.ts
@@ -4,6 +4,7 @@
 export enum CartOutlets {
   ITEM = 'cx-cart-item',
   ITEM_DETAILS = 'cx-cart-item.details',
+  ITEM_CONFIGURATOR_ISSUES = 'cx-configurator-issues-notification',
   ITEM_BUNDLE_DETAILS = 'cx-cart-item.bundle-details',
   ORDER_SUMMARY = 'cx-order-summary',
   CART_ITEM_LIST = 'cx-cart-item-list',
