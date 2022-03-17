2259609267c795c75b88b32acacc6ba6f348474d
Patrick Labadie <patrick.labadie@sap.com>
2022-03-17 12:24:40 -0400

    GH-15306: Fix merge/conflict resolution.

--- projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
@@ -1,13 +1,15 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { migrateConstructorDeprecation } from '../../mechanism/constructor-deprecations/constructor-deprecations';
 import { CONSTRUCTOR_DEPRECATIONS_DATA } from './data/constructor-deprecations.migration';
+import { FORM_ERRORS_COMPONENT_MIGRATION } from './data/form-errors.component.migration';
+import { TAB_PARAGRAPH_CONTAINER_COMPONENT_MIGRATION } from './data/tab-paragraph-container.component.migration';
 
 export function migrate(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    return migrateConstructorDeprecation(
-      tree,
-      context,
-      CONSTRUCTOR_DEPRECATIONS_DATA
-    );
+    return migrateConstructorDeprecation(tree, context, [
+      ...CONSTRUCTOR_DEPRECATIONS_DATA,
+      FORM_ERRORS_COMPONENT_MIGRATION,
+      TAB_PARAGRAPH_CONTAINER_COMPONENT_MIGRATION,
+    ]);
   };
 }
