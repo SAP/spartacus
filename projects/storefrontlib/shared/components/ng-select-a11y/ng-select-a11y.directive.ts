395787f9aeb15be9a9f7db80db9021714065e50f
BenjaminEmiliani <benjaminemi129@gmail.com>
2022-03-18 12:36:06 -0400

    fix: make ariaControls optional with default value (#14988)

--- projects/storefrontlib/shared/components/ng-select-a11y/ng-select-a11y.directive.ts
+++ projects/storefrontlib/shared/components/ng-select-a11y/ng-select-a11y.directive.ts
@@ -12,7 +12,8 @@ import {
 export class NgSelectA11yDirective implements AfterViewInit {
   /**
    * Use directive to bind aria attribute to inner element of ng-select
-   * Angular component for accessibility compliance.
+   * Angular component for accessibility compliance. If ng-select controls itself
+   * ariaControls is not needed, instead bind a specific id to the <ng-select> element.
    */
   @Input() cxNgSelectA11y: { ariaLabel: string; ariaControls: string };
 
@@ -22,20 +23,16 @@ export class NgSelectA11yDirective implements AfterViewInit {
     const divCombobox =
       this.elementRef.nativeElement.querySelector('[role="combobox"]');
 
-    if (this.cxNgSelectA11y.ariaLabel) {
-      this.renderer.setAttribute(
-        divCombobox,
-        'aria-label',
-        this.cxNgSelectA11y.ariaLabel
-      );
+    const _ariaLabel = this.cxNgSelectA11y.ariaLabel;
+    const elementId = this.elementRef.nativeElement.id;
+    const _ariaControls = this.cxNgSelectA11y.ariaControls ?? elementId;
+
+    if (_ariaLabel) {
+      this.renderer.setAttribute(divCombobox, 'aria-label', _ariaLabel);
     }
 
-    if (this.cxNgSelectA11y.ariaControls) {
-      this.renderer.setAttribute(
-        divCombobox,
-        'aria-controls',
-        this.cxNgSelectA11y.ariaControls
-      );
+    if (_ariaControls) {
+      this.renderer.setAttribute(divCombobox, 'aria-controls', _ariaControls);
     }
   }
 }
