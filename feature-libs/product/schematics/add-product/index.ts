34b914b2f0b4e3fff632cceab60b4251db776cf1
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-03-22 10:39:04 +0100

    product done

--- feature-libs/product/schematics/add-product/index.ts
+++ feature-libs/product/schematics/add-product/index.ts
@@ -6,16 +6,12 @@ import {
   Tree,
 } from '@angular-devkit/schematics';
 import {
-  addLibraryFeature,
+  addFeatures,
   addPackageJsonDependenciesForLibrary,
+  analyzeCrossFeatureDependencies,
   CLI_PRODUCT_BULK_PRICING_FEATURE,
-  CLI_PRODUCT_IMAGE_ZOOM_FEATURE,
-  CLI_PRODUCT_VARIANTS_FEATURE,
   configureB2bFeatures,
   LibraryOptions as SpartacusProductOptions,
-  PRODUCT_BULK_PRICING_SCHEMATICS_CONFIG,
-  PRODUCT_IMAGE_ZOOM_SCHEMATICS_CONFIG,
-  PRODUCT_VARIANTS_SCHEMATICS_CONFIG,
   readPackageJson,
   shouldAddFeature,
   validateSpartacusInstallation,
@@ -27,35 +23,20 @@ export function addSpartacusProduct(options: SpartacusProductOptions): Rule {
     const packageJson = readPackageJson(tree);
     validateSpartacusInstallation(packageJson);
 
+    const features = analyzeCrossFeatureDependencies(options.features ?? []);
+
     return chain([
+      addFeatures(options, features),
+
+      shouldAddB2b(options)
+        ? configureB2bFeatures(options, packageJson)
+        : noop(),
+
       addPackageJsonDependenciesForLibrary(peerDependencies, options),
-
-      shouldAddFeature(CLI_PRODUCT_BULK_PRICING_FEATURE, options.features)
-        ? chain([
-            addBulkPricingFeature(options),
-            configureB2bFeatures(options, packageJson),
-          ])
-        : noop(),
-
-      shouldAddFeature(CLI_PRODUCT_IMAGE_ZOOM_FEATURE, options.features)
-        ? addImageZoom(options)
-        : noop(),
-
-      shouldAddFeature(CLI_PRODUCT_VARIANTS_FEATURE, options.features)
-        ? addVariantsFeature(options)
-        : noop(),
     ]);
   };
 }
 
-export function addBulkPricingFeature(options: SpartacusProductOptions): Rule {
-  return addLibraryFeature(options, PRODUCT_BULK_PRICING_SCHEMATICS_CONFIG);
-}
-
-export function addImageZoom(options: SpartacusProductOptions): Rule {
-  return addLibraryFeature(options, PRODUCT_IMAGE_ZOOM_SCHEMATICS_CONFIG);
-}
-
-export function addVariantsFeature(options: SpartacusProductOptions): Rule {
-  return addLibraryFeature(options, PRODUCT_VARIANTS_SCHEMATICS_CONFIG);
+function shouldAddB2b(options: SpartacusProductOptions): boolean {
+  return shouldAddFeature(CLI_PRODUCT_BULK_PRICING_FEATURE, options.features);
 }
