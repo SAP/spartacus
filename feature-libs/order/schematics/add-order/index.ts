6f185463b4d61ae11b4c75fe123fb5d7e24fb838
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-03-22 10:28:36 +0100

    order done

--- feature-libs/order/schematics/add-order/index.ts
+++ feature-libs/order/schematics/add-order/index.ts
@@ -1,18 +1,15 @@
 import {
   chain,
-  noop,
   Rule,
   SchematicContext,
   Tree,
 } from '@angular-devkit/schematics';
 import {
-  addLibraryFeature,
+  addFeatures,
   addPackageJsonDependenciesForLibrary,
-  CLI_ORDER_FEATURE,
+  analyzeCrossFeatureDependencies,
   LibraryOptions as SpartacusOrderOptions,
-  ORDER_SCHEMATICS_CONFIG,
   readPackageJson,
-  shouldAddFeature,
   validateSpartacusInstallation,
 } from '@spartacus/schematics';
 import { peerDependencies } from '../../package.json';
@@ -22,16 +19,11 @@ export function addOrderFeatures(options: SpartacusOrderOptions): Rule {
     const packageJson = readPackageJson(tree);
     validateSpartacusInstallation(packageJson);
 
-    return chain([
-      addPackageJsonDependenciesForLibrary(peerDependencies, options),
+    const features = analyzeCrossFeatureDependencies(options.features ?? []);
 
-      shouldAddFeature(CLI_ORDER_FEATURE, options.features)
-        ? addOrderFeature(options)
-        : noop(),
+    return chain([
+      addFeatures(options, features),
+      addPackageJsonDependenciesForLibrary(peerDependencies, options),
     ]);
   };
-}
-
-function addOrderFeature(options: SpartacusOrderOptions): Rule {
-  return addLibraryFeature(options, ORDER_SCHEMATICS_CONFIG);
 }
--- projects/schematics/src/shared/utils/test-utils.ts
+++ projects/schematics/src/shared/utils/test-utils.ts
@@ -8,6 +8,56 @@ import { findNodes } from '@schematics/angular/utility/ast-utils';
 import ts from 'typescript';
 import { findConstructor } from './file-utils';
 
+export const asmFeatureModulePath =
+  'src/app/spartacus/features/asm/asm-feature.module.ts';
+export const cartBaseFeatureModulePath =
+  'src/app/spartacus/features/cart/cart-base-feature.module.ts';
+export const importExportFeatureModulePath =
+  'src/app/spartacus/features/cart/cart-import-export-feature.module.ts';
+export const quickOrderFeatureModulePath =
+  'src/app/spartacus/features/cart/cart-quick-order-feature.module.ts';
+export const wishListFeatureModulePath =
+  'src/app/spartacus/features/cart/wish-list-feature.module.ts';
+export const savedCartFeatureModulePath =
+  'src/app/spartacus/features/cart/cart-saved-cart-feature.module.ts';
+export const checkoutFeatureModulePath =
+  'src/app/spartacus/features/checkout/checkout-feature.module.ts';
+export const orderFeatureModulePath =
+  'src/app/spartacus/features/order/order-feature.module.ts';
+export const organizationAdministrationFeatureModulePath =
+  'src/app/spartacus/features/organization/organization-administration-feature.module.ts';
+export const organizationOrderApprovalFeatureModulePath =
+  'src/app/spartacus/features/organization/organization-order-approval-feature.module.ts';
+export const productBulkPricingFeatureModulePath =
+  'src/app/spartacus/features/product/product-bulk-pricing-feature.module.ts';
+export const productImageZoomFeatureModulePath =
+  'src/app/spartacus/features/product/product-image-zoom-feature.module.ts';
+export const productVariantsFeatureModulePath =
+  'src/app/spartacus/features/product/product-variants-feature.module.ts';
+export const productConfiguratorFeatureModulePath =
+  'src/app/spartacus/features/product-configurator/product-configurator-feature.module.ts';
+export const qualtricsFeatureModulePath =
+  'src/app/spartacus/features/qualtrics/qualtrics-feature.module.ts';
+export const smartEditFeatureModulePath =
+  'src/app/spartacus/features/smartedit/smart-edit-feature.module.ts';
+export const storeFinderFeatureModulePath =
+  'src/app/spartacus/features/storefinder/store-finder-feature.module.ts';
+export const trackingPersonalizationFeatureModulePath =
+  'src/app/spartacus/features/tracking/personalization-feature.module.ts';
+export const trackingTagManagementFeatureModulePath =
+  'src/app/spartacus/features/tracking/tag-management-feature.module.ts';
+export const userFeatureModulePath =
+  'src/app/spartacus/features/user/user-feature.module.ts';
+
+export const cdcFeatureModulePath =
+  'src/app/spartacus/features/cdc/cdc-feature.module.ts';
+export const cdsFeatureModulePath =
+  'src/app/spartacus/features/cds/cds-feature.module.ts';
+export const digitalPaymentsFeatureModulePath =
+  'src/app/spartacus/features/digital-payments/digital-payments-feature.module.ts';
+export const epdFeatureModulePath =
+  'src/app/spartacus/features/epd-visualization/epd-visualization-feature.module.ts';
+
 export function writeFile(
   host: TempScopedNodeJsSyncHost,
   filePath: string,
