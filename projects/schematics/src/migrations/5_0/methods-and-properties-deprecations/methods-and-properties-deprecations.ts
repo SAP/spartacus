3814d95abdfd029af892a2ab6ca3e860a419fb20
Patrick Labadie <patrick.labadie@sap.com>
2022-03-23 14:01:41 -0400

    GH-15306: Handle duplicate migration.

--- projects/schematics/src/migrations/5_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
+++ projects/schematics/src/migrations/5_0/methods-and-properties-deprecations/methods-and-properties-deprecations.ts
@@ -1,13 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { migrateMethodPropertiesDeprecation } from '../../mechanism/methods-and-properties-deprecations/methods-and-properties-deprecations';
-import { CONFIGURATOR_ATTRIBUTE_SINGLE_SELECTION_BUNDLE_COMPONENT_MIGRATION } from './data/configurator-attribute-single-selection-bundle.component.migration';
 import { METHODS_AND_PROPERTIES_DEPRECATIONS_DATA } from './data/methods-and-properties.migration';
 
 export function migrate(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    return migrateMethodPropertiesDeprecation(tree, context, [
-      ...METHODS_AND_PROPERTIES_DEPRECATIONS_DATA,
-      ...CONFIGURATOR_ATTRIBUTE_SINGLE_SELECTION_BUNDLE_COMPONENT_MIGRATION,
-    ]);
+    return migrateMethodPropertiesDeprecation(
+      tree,
+      context,
+      METHODS_AND_PROPERTIES_DEPRECATIONS_DATA
+    );
   };
 }
