954bb9cd04e3fe79eb7d0a543a1871b56d3d8733
Krzysztof Platis <platonn.git@gmail.com>
2022-03-24 16:10:44 +0100

    format note about original code near MIT-licensed code

--- projects/schematics/src/shared/utils/load-esm-module.ts
+++ projects/schematics/src/shared/utils/load-esm-module.ts
@@ -13,9 +13,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  *
+ * ----------------------------------------------------------------------------------------
+ *
  * See original code: https://github.com/angular/angular/blob/e0015d3c456d584242269b0765878d598a550888/packages/core/schematics/utils/load_esm.ts#L11-L35
  *
- *
+ * --------------------------------------------------------------------------------
  * This uses a dynamic import to load a module which may be ESM.
  * CommonJS code can load ESM code via a dynamic import. Unfortunately, TypeScript
  * will currently, unconditionally downlevel dynamic import into a require call.
