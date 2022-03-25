ca1a8764dc3ea8867df10ccd70212ff15f129b4a
Radhep Saba <radhep.saba@gmail.com>
2022-03-25 16:05:37 -0400

    test

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -61,20 +61,6 @@ yarn install
 
 (cd projects/storefrontapp-e2e-cypress && yarn install)
 
-yarn build:libs 2>&1 | tee build.log
-
-results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
-if [[ -z "${results}" ]]; then
-    echo "Success: Spartacus production build was successful."
-    rm build.log
-else
-    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
-    rm build.log
-    exit 1
-fi
-echo '-----'
-echo "Building Spartacus storefrontapp"
-
 if [[ "${SSR}" = true ]]; then
     echo "Building Spartacus storefrontapp (SSR PROD mode)"
     yarn build:ssr:ci
