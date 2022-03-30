b94c49be902d0f7e7c3b6a18d8e267d676721e21
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-03-30 16:08:15 -0400

    chore: Ccv2 e2e support + backported consolidating cypress configs (#15400)
    Support ccv2 e2es and WTF framework
    
    Co-authored-by: Giancarlo Cordero Ortiz <46171897+giancorderoortiz@users.noreply.github.com>

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/cx_ccv2/regression/b2c/early-login.flaky-e2e-spec.ts
@@ -0,0 +1,69 @@
+import { RoutingConfig } from '@spartacus/core';
+import * as login from '../../../../helpers/login';
+
+// TODO. Fix Priority 2. Remove this line after this spec runs successfully with CCV2.
+
+const FAQ_HEADING = 'Frequently Asked Questions';
+
+function headingContains(text: string) {
+  cy.get('cx-breadcrumb h1').should('contain', text);
+}
+
+context('Early login', () => {
+  let config: RoutingConfig;
+
+  beforeEach(() => {
+    config = {
+      routing: {
+        protected: true, // enable routes protection
+        routes: {
+          faq: { paths: ['faq'] },
+        },
+      },
+    };
+  });
+
+  context('public page', () => {
+    beforeEach(() => {
+      config.routing.routes.faq.protected = false;
+      cy.cxConfig(config);
+      cy.visit('/faq');
+    });
+
+    it('should display', () => {
+      headingContains(FAQ_HEADING);
+    });
+  });
+
+  context('protected page', () => {
+    context('for unauthorized user', () => {
+      beforeEach(() => {
+        cy.cxConfig(config);
+        cy.visit('/faq');
+      });
+
+      it('should redirect to login page and redirect back after sign in', () => {
+        cy.url().should('contain', '/login');
+
+        login.registerUserFromLoginPage();
+        login.loginUser();
+        headingContains(FAQ_HEADING);
+      });
+    });
+
+    context('for authorized user', () => {
+      beforeEach(() => {
+        cy.cxConfig(config);
+
+        cy.visit('/');
+        cy.requireLoggedIn();
+
+        cy.visit('/faq');
+      });
+
+      it('should display', () => {
+        headingContains(FAQ_HEADING);
+      });
+    });
+  });
+});
