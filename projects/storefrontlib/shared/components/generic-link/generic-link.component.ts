fab2903d71d0691fe58620eb94656bc26f4318eb
Zeyber <rotherham.c@gmail.com>
2022-03-31 15:40:36 +0200

    fix: Change generic link component properties access to protected

--- projects/storefrontlib/shared/components/generic-link/generic-link.component.ts
+++ projects/storefrontlib/shared/components/generic-link/generic-link.component.ts
@@ -26,31 +26,31 @@ export class GenericLinkComponent implements OnChanges {
   /**
    * Pattern matching string starting with `http://` or `https://`.
    */
-  private readonly PROTOCOL_REGEX: RegExp = /^https?:\/\//i;
+  protected readonly PROTOCOL_REGEX: RegExp = /^https?:\/\//i;
 
   /**
    * Pattern matching string starting with `mailto:`.
    */
-  private readonly MAILTO_PROTOCOL_REGEX: RegExp = /^mailto:/i;
+  protected readonly MAILTO_PROTOCOL_REGEX: RegExp = /^mailto:/i;
 
   /**
    * Pattern matching string starting with `tel:`.
    */
-  private readonly TEL_PROTOCOL_REGEX: RegExp = /^tel:/i;
+  protected readonly TEL_PROTOCOL_REGEX: RegExp = /^tel:/i;
 
   /**
    * Used to split url into 2 parts:
    * 1. the path
    * 2. query params + hash fragment
    */
-  private readonly URL_SPLIT = /(^[^#?]*)(.*)/;
+  protected readonly URL_SPLIT = /(^[^#?]*)(.*)/;
 
   /**
    * Parsed parts of the @Input `url`, when it's a local URL.
    * It should not be used when the `url` is external.
    * @see `url`
    */
-  private routeParts: RouteParts = {};
+  protected routeParts: RouteParts = {};
 
   @Input() url: string | any[];
   @Input() target: string;
@@ -111,7 +111,7 @@ export class GenericLinkComponent implements OnChanges {
   /**
    * Parses the given url and sets the property `urlParts` accordingly.
    */
-  private setUrlParts(url: string | any[]) {
+  protected setUrlParts(url: string | any[]) {
     if (typeof url === 'string') {
       url = this.getAbsoluteUrl(url); // string links in CMS sometimes don't have the leading slash, so fix it here
       this.routeParts = this.splitUrl(url as string);
@@ -126,7 +126,7 @@ export class GenericLinkComponent implements OnChanges {
    * - query params (as an object)
    * - hash fragment (string)
    */
-  private splitUrl(url: string = ''): RouteParts {
+  protected splitUrl(url: string = ''): RouteParts {
     const { queryParams, fragment } = this.router.parseUrl(url);
     const [, path] = url.match(this.URL_SPLIT);
 
@@ -137,7 +137,7 @@ export class GenericLinkComponent implements OnChanges {
   /**
    * Prepends a leading slash to the given URL string, in case it doesn't have it.
    */
-  private getAbsoluteUrl(url: string): string {
+  protected getAbsoluteUrl(url: string): string {
     return url.startsWith('/') ? url : '/' + url;
   }
 }
