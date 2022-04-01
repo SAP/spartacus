331d2ed11642696c3f39116166e5f5b2e0769c88
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-01 12:28:21 -0400

    test

--- .github/api-extractor-action/src/setup.ts
+++ .github/api-extractor-action/src/setup.ts
@@ -35,39 +35,39 @@ export async function prepareRepositoryForApiExtractor(
   // Builded libraries are cached by `cache-builded-libs` action
   const paths = [BUILD_DIR];
   const key = `dist-${baseCommit}`;
-  let cacheKey;
+  const BUILD_COMMAND = 'build:libs';
+
+  await cache.restoreCache(paths, key, []);
+
+  core.warning('check fs 1');
+  await exec.exec('ls', ['-al']);
 
   try {
-    core.warning('test 1');
-    cacheKey = await cache.restoreCache(paths, key, []);
-    core.warning('test 2');
-  } catch {
-    core.warning('dist folder is not found');
-  }
-
-  if (cacheKey) {
-    core.warning('test 3');
     // Cache restores files in the same location, so we need to move them manually
     await io.cp(BUILD_DIR, `${BASE_BRANCH_DIR}/${BUILD_DIR}`, {
       recursive: true,
       force: false,
     });
-    core.warning('test 4');
     await io.rmRF(BUILD_DIR);
-  }
+  } catch {
+    core.warning('check fs 2');
+    await exec.exec('ls', ['-al']);
+    core.warning('dist folder not found in cache');
+    // If we didn't restored builded libs, we need to also build base branch
+    await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR]);
+    await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR, BUILD_COMMAND]);
 
-  core.warning('test 5');
+    core.warning('check fs 3');
+    await exec.exec('ls', ['-al']);
+  }
 
   // Build the libraries
   // TODO: We can parallel these builds, when schematics builds won't trigger yarn install
-  const BUILD_COMMAND = 'build:libs';
   await exec.exec('yarn');
   await exec.exec('yarn', [BUILD_COMMAND]);
-  // If we didn't restored builded libs, we need to also build base branch
-  if (!cacheKey) {
-    await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR]);
-    await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR, BUILD_COMMAND]);
-  }
+
+  core.warning('check fs 4');
+  await exec.exec('ls', ['-al']);
 
   core.endGroup();
 }
