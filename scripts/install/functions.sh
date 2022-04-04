c73c169a3f29770a789737da642b0314b5431c41
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-04 12:11:27 -0400

    test again.

--- scripts/install/functions.sh
+++ scripts/install/functions.sh
@@ -200,7 +200,7 @@ function publish_package {
 }
 
 function restore_clone {
-    if [ ${BRANCH} == 'develop' ]; then
+    if [ ${BRANCH} == 'feature/GH-15525' ]; then
         pushd ../.. > /dev/null
         for path in ${SPARTACUS_PROJECTS[@]}
         do
