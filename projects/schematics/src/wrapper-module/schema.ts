d28064e5f225cb57e244e00aaa9600b734aebe15
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-04-05 12:32:51 +0200

    change schema comments

--- projects/schematics/src/wrapper-module/schema.ts
+++ projects/schematics/src/wrapper-module/schema.ts
@@ -4,12 +4,12 @@ export interface Schema extends Partial<ExecutionOptions> {
   project: string;
   /**
    * The marker module for which
-   * to find or generate the wrapper module.
+   * to find or generate a new wrapper module.
    */
   markerModuleName: string;
   /**
    * Name of the feature module
-   * which to add to the wrapper module.
+   * to add to the wrapper module.
    */
   featureModuleName: string;
 }
