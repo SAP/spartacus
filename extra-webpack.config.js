7bfdc26ba2324ad235b89c23249102785b717506
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-06 11:37:55 -0400

    finalized fix? - will bring up to team on other 'potential' solutions

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -24,10 +24,7 @@ module.exports = {
   resolve: {
     alias: {
       '@spartacus/styles': path.join(__dirname, 'projects/storefrontstyles'),
-      '@spartacus/cart/base/styles': path.join(
-        __dirname,
-        'feature-libs/cart/base/styles'
-      ),
+      '@spartacus/cart': path.join(__dirname, 'feature-libs/cart'),
     },
   },
 };
