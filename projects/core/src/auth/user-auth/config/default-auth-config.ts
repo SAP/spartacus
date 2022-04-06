26e573796e9fbdf0d8d3ff80e57c939e0c5fad03
Zeyber <rotherham.c@gmail.com>
2022-04-06 10:59:27 +0200

    Revert "empty"
    This reverts commit 01c7d97e42610be164ef73192a2d713d5dc68284.

--- projects/core/src/auth/user-auth/config/default-auth-config.ts
+++ projects/core/src/auth/user-auth/config/default-auth-config.ts
@@ -1,26 +1,5 @@
 import { AuthConfig } from './auth-config';
 
-// export const defaultAuthConfig: AuthConfig = {
-//   authentication: {
-//     client_id: 'client4kyma',
-//     client_secret: 'secret',
-//     tokenEndpoint: '/oauth/token',
-//     revokeEndpoint: '/oauth/revoke',
-//     loginUrl: '/oauth/authorize',
-//     logoutUrl: '/oauth/logout',
-//     OAuthLibConfig: {
-//       responseType: 'code',
-//       scope: '',
-//       customTokenParameters: ['token_type'],
-//       strictDiscoveryDocumentValidation: false,
-//       skipIssuerCheck: true,
-//       disablePKCE: true,
-//       oidc: false,
-//       clearHashAfterLogin: false,
-//     },
-//   },
-// };
-
 export const defaultAuthConfig: AuthConfig = {
   authentication: {
     client_id: 'mobile_android',
