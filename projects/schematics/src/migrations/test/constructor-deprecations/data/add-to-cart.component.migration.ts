3e2f79b23d154d516ab02e6a2d5652095e331cb8
Patrick Labadie <patrick.labadie@sap.com>
2022-04-08 18:19:15 -0400

    GH-15581: Improve constructor migration schematic test coverage

--- projects/schematics/src/migrations/test/constructor-deprecations/data/add-to-cart.component.migration.ts
+++ projects/schematics/src/migrations/test/constructor-deprecations/data/add-to-cart.component.migration.ts
@@ -4,6 +4,7 @@ import {
   ANGULAR_CORE,
   CART_SERVICE,
   CHANGE_DETECTOR_REF,
+  CMS_COMPONENTS_SERVICE,
   CURRENT_PRODUCT_SERVICE,
   MODAL_SERVICE,
 } from '../../../../shared/constants';
@@ -45,6 +46,81 @@ export const ADD_TO_CART_COMPONENT_MIGRATION: ConstructorDeprecation = {
     {
       className: ACTIVE_CART_SERVICE,
       importPath: SPARTACUS_CORE,
+    },
+  ],
+};
+
+// Use case: delete all old params and use the complete new signature
+// in the "addParams" list.  Doing so should keep the correct param
+// order when the modified params aree not at the end.
+export const ADD_TO_CART_COMPONENT_MIGRATION_2: ConstructorDeprecation = {
+  // projects/storefrontlib/cms-components/cart/add-to-cart/add-to-cart.component.ts
+  class: ADD_TO_CART_COMPONENT,
+  importPath: SPARTACUS_STOREFRONTLIB,
+  deprecatedParams: [
+    {
+      className: CMS_COMPONENTS_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: MODAL_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CURRENT_PRODUCT_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CHANGE_DETECTOR_REF,
+      importPath: ANGULAR_CORE,
+    },
+  ],
+  removeParams: [
+    {
+      className: CMS_COMPONENTS_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: MODAL_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CURRENT_PRODUCT_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CHANGE_DETECTOR_REF,
+      importPath: ANGULAR_CORE,
+    },
+  ],
+  addParams: [
+    {
+      className: CMS_COMPONENTS_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: ACTIVE_CART_SERVICE,
+      importPath: SPARTACUS_CORE,
+    },
+    {
+      className: MODAL_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CURRENT_PRODUCT_SERVICE,
+      importPath: SPARTACUS_STOREFRONTLIB,
+    },
+    {
+      className: CHANGE_DETECTOR_REF,
+      importPath: ANGULAR_CORE,
     },
   ],
 };
