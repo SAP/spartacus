596c3783efa6f82dd6ebe4add302d44cf9d113ce
Zeyber <rotherham.c@gmail.com>
2022-04-08 10:37:31 +0200

    fix: Use router to navigate internal links on ParagraphComponent

--- projects/storefrontlib/cms-components/content/paragraph/paragraph.component.ts
+++ projects/storefrontlib/cms-components/content/paragraph/paragraph.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+} from '@angular/core';
+import { Router } from '@angular/router';
 import { CmsParagraphComponent } from '@spartacus/core';
 import { CmsComponentData } from '../../../cms-structure/page/model/cms-component-data';
 
@@ -8,5 +13,33 @@ import { CmsComponentData } from '../../../cms-structure/page/model/cms-componen
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ParagraphComponent {
-  constructor(public component: CmsComponentData<CmsParagraphComponent>) {}
+  @HostListener('click', ['$event'])
+  public handleClick(event: Event): void {
+    if (this.router && event.target instanceof HTMLAnchorElement) {
+      const element = event.target as HTMLAnchorElement;
+      const href = element?.getAttribute('href');
+
+      // Use router for internal link navigation
+      if (href?.indexOf('/') === 0) {
+        event.preventDefault();
+        this.router.navigate([`/${href}`]);
+      }
+    }
+  }
+
+  /**
+   * @deprecated since version 5.0
+   * Use the following constructor instead:
+   * ```
+   * constructor(
+   *   public component: CmsComponentData<CmsParagraphComponent>,
+   *   protected router: Router
+   * ) {}
+   * ```
+   */
+  constructor(component: CmsComponentData<CmsParagraphComponent>);
+  constructor(
+    public component: CmsComponentData<CmsParagraphComponent>,
+    protected router?: Router
+  ) {}
 }
