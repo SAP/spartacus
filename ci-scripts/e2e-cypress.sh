69e8fbdc39beab397be750a07b0e5fc957b8c8a4
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-10 23:09:26 -0400

    revert e2e script

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -54,6 +54,28 @@ if [ "$SUITE" == ":ccv2-b2b" ]; then
     export SPA_ENV='ccv2,b2b'
 fi
 
+echo '-----'
+echo "Building Spartacus libraries"
+
+yarn install
+
+(cd projects/storefrontapp-e2e-cypress && yarn install)
+
+yarn build:libs 2>&1 | tee build.log
+
+results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
+if [[ -z "${results}" ]]; then
+    echo "Success: Spartacus production build was successful."
+    rm build.log
+else
+    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
+    rm build.log
+    exit 1
+fi
+echo '-----'
+echo "Building Spartacus storefrontapp"
+yarn build
+
 if [[ "${SSR}" = true ]]; then
     echo "Building Spartacus storefrontapp (SSR PROD mode)"
     yarn build:ssr:ci
