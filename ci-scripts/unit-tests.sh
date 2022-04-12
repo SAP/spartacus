8c4e196ec4a187c248da968cbe14c7444a753883
Radhep Saba <radhep.saba@gmail.com>
2022-04-12 19:56:08 -0400

    remove libaries that dont exist

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -40,30 +40,6 @@ echo "Running schematics unit tests and code coverage for cdc library"
 exec 5>&1
 output=$(yarn --cwd integration-libs/cdc run test:schematics --coverage=true | tee /dev/fd/5)
 
-echo "Running unit tests and code coverage for Digital-Payments"
-exec 5>&1
-output=$(ng test digital-payments --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
-echo "Running schematics unit tests and code coverage for Digital-Payments library"
-exec 5>&1
-output=$(yarn --cwd integration-libs/digital-payments run test:schematics --coverage=true | tee /dev/fd/5)
-
-echo "Running unit tests and code coverage for EPD Visualization"
-exec 5>&1
-output=$(ng test epd-visualization --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
-coverage=$(echo $output | grep -i "does not meet global threshold" || true)
-if [[ -n "$coverage" ]]; then
-    echo "Error: Tests did not meet coverage expectations"
-    exit 1
-fi
-echo "Running schematics unit tests and code coverage for EPD Visualization library"
-exec 5>&1
-output=$(yarn --cwd integration-libs/epd-visualization run test:schematics --coverage=true | tee /dev/fd/5)
-
 echo "Running unit tests and code coverage for storefinder library"
 exec 5>&1
 output=$(ng test storefinder --sourceMap --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
