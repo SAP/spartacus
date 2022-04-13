a992ca8507d689736bed0e4f22e786bb6eb5436e
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-04-13 17:54:22 +0200

    prettier

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
@@ -1,8 +1,11 @@
 import {
   CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT,
-  TRANSLATION_SERVICE
+  TRANSLATION_SERVICE,
 } from '../../../../shared/constants';
-import { SPARTACUS_CORE, SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED } from '../../../../shared/libs-constants';
+import {
+  SPARTACUS_CORE,
+  SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+} from '../../../../shared/libs-constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
 export const CONFIGURATOR_ATTRIBUTE_INPUT_FIELD_COMPONENT_MIGRATION: ConstructorDeprecation =
