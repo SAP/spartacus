6a7b0ea5b595b3daad43646f027965208036698c
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-04-13 21:44:26 +0200

    simplified checkout lib schematic

--- feature-libs/checkout/schematics/add-checkout/index.ts
+++ feature-libs/checkout/schematics/add-checkout/index.ts
@@ -6,16 +6,10 @@ import {
 } from '@angular-devkit/schematics';
 import {
   addFeatures,
-  addFeatureTranslations,
   addPackageJsonDependenciesForLibrary,
   analyzeCrossFeatureDependencies,
-  CHECKOUT_B2B_SCHEMATICS_CONFIG,
-  CHECKOUT_SCHEDULED_REPLENISHMENT_SCHEMATICS_CONFIG,
-  CLI_CHECKOUT_B2B_FEATURE,
-  CLI_CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE,
   LibraryOptions as SpartacusCheckoutOptions,
   readPackageJson,
-  shouldAddFeature,
   validateSpartacusInstallation,
 } from '@spartacus/schematics';
 import { peerDependencies } from '../../package.json';
@@ -31,35 +25,7 @@ export function addCheckoutFeatures(options: SpartacusCheckoutOptions): Rule {
 
     return chain([
       addFeatures(options, features),
-      additionalCheckoutConfiguration(options),
       addPackageJsonDependenciesForLibrary(peerDependencies, options),
     ]);
   };
-}
-
-function additionalCheckoutConfiguration(
-  options: SpartacusCheckoutOptions
-): Rule {
-  const rules: Rule[] = [];
-
-  if (shouldAddFeature(CLI_CHECKOUT_B2B_FEATURE, options.features)) {
-    rules.push(addFeatureTranslations(options, CHECKOUT_B2B_SCHEMATICS_CONFIG));
-  }
-
-  if (
-    shouldAddFeature(
-      CLI_CHECKOUT_SCHEDULED_REPLENISHMENT_FEATURE,
-      options.features
-    )
-  ) {
-    rules.push(
-      addFeatureTranslations(options, CHECKOUT_B2B_SCHEMATICS_CONFIG),
-      addFeatureTranslations(
-        options,
-        CHECKOUT_SCHEDULED_REPLENISHMENT_SCHEMATICS_CONFIG
-      )
-    );
-  }
-
-  return chain(rules);
 }
