814edd9c68517a888e6da48a38c3beab0a51ba76
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-14 12:10:18 -0400

    determining e2e execution test

---
+++ ci-scripts/validate-e2e-execution.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+FILES=$(git diff --name-only origin/develop)
+
+RUN_E2E=
+
+for file in $FILES; do
+
+    case "$file" in
+    *.md | docs/** | tools/** | *.spec.ts )
+        ;;
+    * )
+        echo "here $file"
+        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we need to run e2es
+        RUN_E2E=true
+        ;;
+    esac
+done
