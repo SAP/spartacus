cd4b8b76e11cb3f82a75a4a5d7129efc3235fc31
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-14 01:34:23 -0400

    test

--- ci-scripts/verify-pull-request.sh
+++ ci-scripts/verify-pull-request.sh
@@ -4,6 +4,8 @@ set -o pipefail
 
 FILES=$(git diff --name-only origin/develop)
 
+OUTPUT_CONCLUSION=
+export OUTPUT_CONCLUSION_TEST=
 # echo $FILES
 
 for file in $FILES; do
@@ -11,8 +13,9 @@ for file in $FILES; do
 
     case "$file" in
     *.md | docs/** | tools/** | *.spec.ts )
-        echo "ignored files $file"
-        # do nothing
+        OUTPUT_CONCLUSION='something'
+        OUTPUT_CONCLUSION_TEST='something-test'
+        exit 1
         ;;
     * )
         #  set github actions for output to skip e2es. output will be used in the job with if outputs.skipe2e is true
