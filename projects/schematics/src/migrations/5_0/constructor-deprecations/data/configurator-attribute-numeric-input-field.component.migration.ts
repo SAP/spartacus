4a6a35bcc91912a0bc2ac030f125cc72ba7854b2
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-04-19 16:08:45 +0200

    fix: sync migration with implementation used for test

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
@@ -1,5 +1,7 @@
 import {
   CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT,
+  CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_SERVICE,
+  CONFIGURATOR_UI_SETTINGS_CONFIG,
   TRANSLATION_SERVICE,
 } from '../../../../shared/constants';
 import {
@@ -13,7 +15,16 @@ export const CONFIGURATOR_ATTRIBUTE_INPUT_FIELD_COMPONENT_MIGRATION: Constructor
     // feature-libs/product-configurator/rulebased/components/attribute/types/input-field/configurator-attribute-numeric-input-field.component.ts
     class: CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT,
     importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
-    deprecatedParams: [],
+    deprecatedParams: [
+      {
+        className: CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_SERVICE,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+      {
+        className: CONFIGURATOR_UI_SETTINGS_CONFIG,
+        importPath: SPARTACUS_PRODUCT_CONFIGURATOR_RULEBASED,
+      },
+    ],
     addParams: [
       {
         className: TRANSLATION_SERVICE,
