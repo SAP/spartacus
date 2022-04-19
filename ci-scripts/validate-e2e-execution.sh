042976e83d7ca77b915bd6415e4d2eb7c4393d98
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-19 14:26:15 -0400

    script cleanup

--- ci-scripts/validate-e2e-execution.sh
+++ ci-scripts/validate-e2e-execution.sh
@@ -4,18 +4,16 @@ set -o pipefail
 
 FILES=$(git diff --name-only origin/develop)
 
-RUN_E2E=
+RUN_E2E=false
 
 for file in $FILES; do
 
     case "$file" in
     *.md | docs/** | tools/** | *.spec.ts )
-        echo ""
         ;;
     * )
-        echo "here $file"
-        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we need to run e2es
-        RUN_E2E=false
+        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
+        RUN_E2E=true
         ;;
     esac
 done
