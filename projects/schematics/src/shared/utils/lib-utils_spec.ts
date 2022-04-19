25a72266d0bdb2501aec16cf356e06837f5fb587
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-04-19 17:26:05 +0200

    removed obsolete tests

--- projects/schematics/src/shared/utils/lib-utils_spec.ts
+++ projects/schematics/src/shared/utils/lib-utils_spec.ts
@@ -12,7 +12,6 @@ import {
   SPARTACUS_CART,
   SPARTACUS_CDS,
   SPARTACUS_CHECKOUT,
-  SPARTACUS_FEATURES_MODULE,
   SPARTACUS_ORDER,
 } from '../libs-constants';
 import {
@@ -20,13 +19,10 @@ import {
   addPackageJsonDependenciesForLibrary,
   FeatureConfig,
   LibraryOptions,
-  // TODO:#schematics - remove?
-  // orderInstalledFeatures,
   shouldAddFeature,
 } from './lib-utils';
 
 const xxxFeaturePath = `src/app/spartacus/features/xxx/xxx-feature.module.ts`;
-const spartacusFeaturesPath = `src/app/spartacus/${SPARTACUS_FEATURES_MODULE}.module.ts`;
 
 describe('Lib utils', () => {
   const schematicRunner = new SchematicTestRunner(
@@ -104,24 +100,6 @@ describe('Lib utils', () => {
     lazy: true,
   };
 
-  const DP_FEATURE_CONFIG: FeatureConfig = {
-    library: {
-      cli: 'dp',
-      mainScope: '@spartacus/digital-payments',
-    },
-    folderName: 'dp',
-    moduleName: 'DigitalPayments',
-    featureModule: {
-      name: 'DigitalPaymentsModule',
-      importPath: '@spartacus/digital-payments',
-    },
-  };
-  const DP_OPTIONS: LibraryOptions = {
-    project: 'schematics-test',
-    features: ['dp-cli'],
-    lazy: true,
-  };
-
   const CHECKOUT_FEATURE_CONFIG: FeatureConfig = {
     library: {
       cli: 'checkout',
@@ -142,73 +120,6 @@ describe('Lib utils', () => {
   const CHECKOUT_OPTIONS: LibraryOptions = {
     project: 'schematics-test',
     features: ['checkout-base-cli'],
-    lazy: true,
-  };
-
-  const CART_FEATURE_CONFIG: FeatureConfig = {
-    library: {
-      cli: 'cart',
-      mainScope: '@spartacus/cart',
-      featureScope: '@spartacus/cart/base',
-    },
-    folderName: 'cart',
-    moduleName: 'Cart',
-    featureModule: {
-      name: 'CartBaseModule',
-      importPath: '@spartacus/cart/base',
-    },
-    rootModule: {
-      name: 'CartBaseRootModule',
-      importPath: '@spartacus/cart/base/root',
-    },
-  };
-  const CART_OPTIONS: LibraryOptions = {
-    project: 'schematics-test',
-    features: ['cart-cli'],
-    lazy: true,
-  };
-
-  const USER_PROFILE_FEATURE_CONFIG: FeatureConfig = {
-    library: {
-      cli: 'user-profile',
-      mainScope: '@spartacus/user',
-    },
-    folderName: 'user',
-    moduleName: 'UserProfile',
-    featureModule: {
-      name: 'UserProfileModule',
-      importPath: '@spartacus/user',
-    },
-    rootModule: {
-      name: 'UserProfileRootModule',
-      importPath: '@spartacus/user/root',
-    },
-  };
-  const USER_PROFILE_OPTIONS: LibraryOptions = {
-    project: 'schematics-test',
-    features: ['user-profile-cli'],
-    lazy: true,
-  };
-
-  const ORDER_FEATURE_CONFIG: FeatureConfig = {
-    library: {
-      cli: 'order',
-      mainScope: '@spartacus/order',
-    },
-    folderName: 'order',
-    moduleName: 'Order',
-    featureModule: {
-      name: 'OrderModule',
-      importPath: '@spartacus/order',
-    },
-    rootModule: {
-      name: 'OrderRootModule',
-      importPath: '@spartacus/order/root',
-    },
-  };
-  const ORDER_OPTIONS: LibraryOptions = {
-    project: 'schematics-test',
-    features: ['order-cli'],
     lazy: true,
   };
 
@@ -437,49 +348,6 @@ describe('Lib utils', () => {
       expect(packageJson[SPARTACUS_ORDER]).toBeTruthy();
       expect(packageJson[SPARTACUS_CART]).toBeTruthy();
       expect(packageJson[SPARTACUS_CHECKOUT]).toBeTruthy();
-    });
-  });
-
-  describe('feature ordering', () => {
-    beforeEach(async () => {
-      appTree = await schematicRunner
-        .callRule(addLibraryFeature(BASE_OPTIONS, BASE_FEATURE_CONFIG), appTree)
-        .toPromise();
-      appTree = await schematicRunner
-        .callRule(addLibraryFeature(DP_OPTIONS, DP_FEATURE_CONFIG), appTree)
-        .toPromise();
-      appTree = await schematicRunner
-        .callRule(
-          addLibraryFeature(CHECKOUT_OPTIONS, CHECKOUT_FEATURE_CONFIG),
-          appTree
-        )
-        .toPromise();
-      appTree = await schematicRunner
-        .callRule(addLibraryFeature(CART_OPTIONS, CART_FEATURE_CONFIG), appTree)
-        .toPromise();
-      appTree = await schematicRunner
-        .callRule(
-          addLibraryFeature(USER_PROFILE_OPTIONS, USER_PROFILE_FEATURE_CONFIG),
-          appTree
-        )
-        .toPromise();
-      appTree = await schematicRunner
-        .callRule(
-          addLibraryFeature(ORDER_OPTIONS, ORDER_FEATURE_CONFIG),
-          appTree
-        )
-        .toPromise();
-
-      // TODO:#schematics - enable?
-      // tree = await schematicRunner
-      //   .callRule(orderInstalledFeatures(spartacusDefaultOptions), tree)
-      //   .toPromise();
-    });
-
-    it('should appropriately order the feature modules', () => {
-      expect(
-        appTree.read(spartacusFeaturesPath)?.toString(UTF_8)
-      ).toMatchSnapshot();
     });
   });
 });
