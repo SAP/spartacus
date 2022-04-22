f21dd1541d29955744002cde9958c2e8d0faa658
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-22 12:42:17 -0400

    skip unit test for testing purposes

--- ci-scripts/validate-e2e-execution.sh
+++ ci-scripts/validate-e2e-execution.sh
@@ -9,11 +9,11 @@ RUN_E2E=false
 for file in $FILES; do
 
     case "$file" in
-    *.md | docs/** | tools/** | *.spec.ts )
+    *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
         ;;
     * )
         # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
-        RUN_E2E=false
+        RUN_E2E=true
         ;;
     esac
 done
