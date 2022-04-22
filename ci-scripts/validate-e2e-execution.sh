6f914c74a5848bc8ea508493abc30bce87d7f7c8
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-22 13:36:28 -0400

    testepic to simulate PR as epic is protected now

---
+++ ci-scripts/validate-e2e-execution.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+FILES=$(git diff --name-only origin/develop)
+
+RUN_E2E=false
+
+for file in $FILES; do
+
+    case "$file" in
+    *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
+        ;;
+    * )
+        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
+        RUN_E2E=true
+        ;;
+    esac
+done
