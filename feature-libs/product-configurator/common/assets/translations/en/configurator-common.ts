45a66441a311591c8c92f61b6013bb06e486f4a7
Kornkanok Changsuwan (Sia) <Kornkanok.changsuwan@sap.com>
2022-04-25 17:33:16 +0200

    GH-15501: add aria-label

--- feature-libs/product-configurator/common/assets/translations/en/configurator-common.ts
+++ feature-libs/product-configurator/common/assets/translations/en/configurator-common.ts
@@ -173,6 +173,8 @@ export const configurator = {
       configurationPageLink: 'Navigate to configuration  page.',
       overviewPage: 'You are on the overview page.',
       overviewPageLink: 'Navigate to configuration overview page.',
+      navigateToConflict:
+        'Follow link to get more details on conflict for attribute {{ attribute }}.',
     },
   },
 };
