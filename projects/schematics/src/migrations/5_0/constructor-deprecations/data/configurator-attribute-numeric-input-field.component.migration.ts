8d50960072e2dc329aea2291d11a57f84d5294f9
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-04-25 16:26:22 +0200

    fix: review feedback rename to CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT_MIGRATION

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/configurator-attribute-numeric-input-field.component.migration.ts
@@ -10,7 +10,7 @@ import {
 } from '../../../../shared/libs-constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
 
-export const CONFIGURATOR_ATTRIBUTE_INPUT_FIELD_COMPONENT_MIGRATION: ConstructorDeprecation =
+export const CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT_MIGRATION: ConstructorDeprecation =
   {
     // feature-libs/product-configurator/rulebased/components/attribute/types/input-field/configurator-attribute-numeric-input-field.component.ts
     class: CONFIGURATOR_ATTRIBUTE_NUMERIC_INPUT_FIELD_COMPONENT,
