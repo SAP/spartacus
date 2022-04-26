9c53def6eacc897ddb88c35f72e9a666f4ad0166
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-26 15:32:42 -0400

    apply code review changes

--- ci-scripts/validate-e2e-execution.sh
+++ ci-scripts/validate-e2e-execution.sh
@@ -2,18 +2,20 @@
 set -e
 set -o pipefail
 
-FILES=$(git diff --name-only origin/develop)
-
 RUN_E2E=false
 
-for file in $FILES; do
+if [ ! -z "$GITHUB_BASE_REF" ]; then
+    FILES=`git diff --name-only origin/$GITHUB_BASE_REF`
 
-    case "$file" in
-    *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
-        ;;
-    * )
-        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
-        RUN_E2E=true
-        ;;
-    esac
-done
+    for file in $FILES; do
+
+        case "$file" in
+        *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
+            ;;
+        * )
+            # if anything other than `*.md | docs/** | tools/** | *.spec.ts | **/schematics/**` are found, then we should run e2es
+            RUN_E2E=true
+            ;;
+        esac
+    done
+fi
