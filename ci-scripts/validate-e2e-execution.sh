67cdc07f86ee9c632e1f0a1882fc3d38723c14fc
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-26 15:26:46 -0400

    test

--- ci-scripts/validate-e2e-execution.sh
+++ ci-scripts/validate-e2e-execution.sh
@@ -2,23 +2,24 @@
 set -e
 set -o pipefail
 
-FILES=$(git diff --name-only origin/testepic/simulate-branch-again)
-
 RUN_E2E=false
 
-echo $GITHUB_HEAD_REF
-echo "abc"
+if [ ! -z "$GITHUB_BASE_REF" ]; then
+    FILES=`git diff --name-only origin/$GITHUB_BASE_REF`
 
-for file in $FILES; do
+    echo $GITHUB_BASE_REF
 
-    case "$file" in
-    *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
-        echo "found file $file"
-        ;;
-    * )
-        echo "test here $file"
-        # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
-        RUN_E2E=true
-        ;;
-    esac
-done
+    for file in $FILES; do
+
+        case "$file" in
+        *.md | docs/** | tools/** | *.spec.ts | **/schematics/** )
+            echo "found file $file"
+            ;;
+        * )
+            echo "test here $file"
+            # if anything other than `*.md | docs/** | tools/** | *.spec.ts` are found, then we should e2es
+            RUN_E2E=true
+            ;;
+        esac
+    done
+fi
