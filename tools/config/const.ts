e1b4e160ff159ceee64f946249a3a032e4d0009a
Radhep Saba <radhep.saba@gmail.com>
2022-04-26 12:59:18 -0400

    revert

---
+++ tools/config/const.ts
@@ -0,0 +1,6 @@
+export const PACKAGE_JSON = 'package.json';
+export const NG_PACKAGE_JSON = 'ng-package.json';
+export const SPARTACUS_SCOPE = '@spartacus';
+export const SAP_SCOPE = 'sap';
+export const SAPUI5_TYPES = '@sapui5/ts-types-esm';
+export const SPARTACUS_SCHEMATICS = `${SPARTACUS_SCOPE}/schematics`;
