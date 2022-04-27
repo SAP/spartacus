2a0d042e5c1a88efce60f648df24fb8766d75bb2
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-27 01:16:16 -0400

    release draft workflow

---
+++ .github/publish-release-draft/src/functions.ts
@@ -0,0 +1,36 @@
+export async function publishReleaseDraft(
+  tag_name: string,
+  context: any,
+  octoKit: any,
+  body: string | undefined
+) {
+  const owner = context.payload.repository.owner.login;
+  const repo = context.payload.repository.name;
+
+  const releases = await octoKit.repos.listReleases({
+    owner,
+    repo,
+  });
+
+  const releaseDrafts = releases.data.filter(
+    (release: any) =>
+      release.draft && release.author.login === 'github-actions[bot]'
+  );
+
+  if (releaseDrafts && releaseDrafts.length) {
+    await octoKit.repos.deleteRelease({
+      owner,
+      repo,
+      release_id: releaseDrafts[0].id,
+    });
+  }
+
+  await octoKit.repos.createRelease({
+    tag_name,
+    owner,
+    repo,
+    name: tag_name,
+    body,
+    draft: true,
+  });
+}
---
+++ .github/publish-release-draft/src/index.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { publishReleaseDraft } from './functions';
+
+async function run() {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const CHANGELOG = process.env.CHANGELOG;
+  const TO_TAG = process.env.TO_TAG ?? '';
+
+  if (!GITHUB_TOKEN) {
+    throw new Error('No valid github token to perform this action');
+  }
+
+  if (!CHANGELOG) {
+    core.setFailed('Unable to execute action. CHANGELOG value is missing');
+  }
+
+  const octoKit = github.getOctokit(GITHUB_TOKEN);
+  const context = github.context;
+
+  await publishReleaseDraft(TO_TAG, context, octoKit, CHANGELOG);
+}
+
+run();
