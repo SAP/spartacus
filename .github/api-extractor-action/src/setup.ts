70a9733f27f22177c0ea5c06efddf202480600d5
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-28 14:36:39 -0400

    quick fix for api extractor that I broke...

--- .github/api-extractor-action/src/setup.ts
+++ .github/api-extractor-action/src/setup.ts
@@ -37,10 +37,19 @@ export async function prepareRepositoryForApiExtractor(
   const key = `dist-${baseCommit}`;
   const BUILD_COMMAND = 'build:libs';
 
-  await cache.restoreCache(paths, key, []);
+  try {
+    await cache.restoreCache(paths, key, []);
+    core.warning('check fs 1');
+    core.warning(key);
+    await exec.exec('ls', ['-al']);
+  } catch {
+    core.warning('dist folder not found in cache');
+    await exec.exec('ls', ['-al']);
+  }
 
   try {
     // Cache restores files in the same location, so we need to move them manually
+    core.warning('check fs 2');
     await io.cp(BUILD_DIR, `${BASE_BRANCH_DIR}/${BUILD_DIR}`, {
       recursive: true,
       force: false,
@@ -50,6 +59,7 @@ export async function prepareRepositoryForApiExtractor(
     core.warning(
       'dist folder not found as it failed to be restored from cache'
     );
+    await exec.exec('ls', ['-al']);
 
     // If we didn't restore builded libs from cache on the BASE branch, we need to also build base branch
     await exec.exec('yarn', ['--cwd', BASE_BRANCH_DIR]);
