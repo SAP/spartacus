75418cc48793e6aaac6800e0e8868f676071fa7a
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-04-28 02:27:01 -0400

    development 'style' aware using webpack to resolve paths

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -24,6 +24,37 @@ module.exports = {
   resolve: {
     alias: {
       '@spartacus/styles': path.join(__dirname, 'projects/storefrontstyles'),
+      '@spartacus/user': path.join(__dirname, 'feature-libs/user'),
+      '@spartacus/organization': path.join(
+        __dirname,
+        'feature-libs/organization'
+      ),
+      '@spartacus/product': path.join(__dirname, 'feature-libs/product'),
+      '@spartacus/product-configurator': path.join(
+        __dirname,
+        'feature-libs/product-configurator'
+      ),
+      '@spartacus/storefinder': path.join(
+        __dirname,
+        'feature-libs/storefinder'
+      ),
+      '@spartacus/checkout': path.join(__dirname, 'feature-libs/checkout'),
+      '@spartacus/asm': path.join(__dirname, 'feature-libs/asm'),
+      '@spartacus/smartedit': path.join(__dirname, 'feature-libs/smartedit'),
+      '@spartacus/qualtrics': path.join(__dirname, 'feature-libs/qualtrics'),
+      '@spartacus/cdc': path.join(__dirname, 'feature-libs/cdc'),
+      '@spartacus/digital-payments': path.join(
+        __dirname,
+        'feature-libs/digital-payments'
+      ),
+      '@spartacus/tracking': path.join(__dirname, 'feature-libs/tracking'),
+      '@spartacus/cart': path.join(__dirname, 'feature-libs/cart'),
+      '@spartacus/order': path.join(__dirname, 'feature-libs/order'),
+      '@spartacus/setup': path.join(__dirname, 'feature-libs/setup'),
+      '@spartacus/epd-visualization': path.join(
+        __dirname,
+        'integration-libs/epd-visualization'
+      ),
     },
   },
 };
