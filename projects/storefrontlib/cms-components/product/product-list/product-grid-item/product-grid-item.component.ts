881e7b1710863324397bdd7017ac6278a035f181
Weizheng Gao <weizheng.gao@sap.com>
2022-04-28 14:19:47 -0300

    feature/cxspa-299

--- projects/storefrontlib/cms-components/product/product-list/product-grid-item/product-grid-item.component.ts
+++ projects/storefrontlib/cms-components/product/product-list/product-grid-item/product-grid-item.component.ts
@@ -1,37 +1,12 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ProductListOutlets } from '../../product-outlets.model';
-import { ProductListItemContextSource } from '../model/product-list-item-context-source.model';
-import { ProductListItemContext } from '../model/product-list-item-context.model';
 
 @Component({
   selector: 'cx-product-grid-item',
   templateUrl: './product-grid-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    ProductListItemContextSource,
-    {
-      provide: ProductListItemContext,
-      useExisting: ProductListItemContextSource,
-    },
-  ],
 })
-export class ProductGridItemComponent implements OnChanges {
+export class ProductGridItemComponent {
   readonly ProductListOutlets = ProductListOutlets;
   @Input() product: any;
-
-  constructor(
-    protected productListItemContextSource: ProductListItemContextSource
-  ) {}
-
-  ngOnChanges(changes?: SimpleChanges): void {
-    if (changes?.product) {
-      this.productListItemContextSource.product$.next(this.product);
-    }
-  }
 }
--- projects/storefrontlib/cms-components/product/product-list/product-list-item/product-list-item.component.ts
+++ projects/storefrontlib/cms-components/product/product-list/product-list-item/product-list-item.component.ts
@@ -1,37 +1,12 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ProductListOutlets } from '../../product-outlets.model';
-import { ProductListItemContextSource } from '../model/product-list-item-context-source.model';
-import { ProductListItemContext } from '../model/product-list-item-context.model';
 
 @Component({
   selector: 'cx-product-list-item',
   templateUrl: './product-list-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    ProductListItemContextSource,
-    {
-      provide: ProductListItemContext,
-      useExisting: ProductListItemContextSource,
-    },
-  ],
 })
-export class ProductListItemComponent implements OnChanges {
+export class ProductListItemComponent {
   readonly ProductListOutlets = ProductListOutlets;
   @Input() product: any;
-
-  constructor(
-    protected productListItemContextSource: ProductListItemContextSource
-  ) {}
-
-  ngOnChanges(changes?: SimpleChanges): void {
-    if (changes?.product) {
-      this.productListItemContextSource.product$.next(this.product);
-    }
-  }
 }
