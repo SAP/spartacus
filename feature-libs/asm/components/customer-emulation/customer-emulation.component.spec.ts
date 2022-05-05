3423dcae87bab7599f0200302174c130e4e3da13
Christopher Manciero <cmanciero@gmail.com>
2022-05-05 08:34:50 -0400

    update unit tests

--- feature-libs/asm/components/customer-emulation/customer-emulation.component.spec.ts
+++ feature-libs/asm/components/customer-emulation/customer-emulation.component.spec.ts
@@ -1,7 +1,15 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { I18nTestingModule, User, UserService } from '@spartacus/core';
+import { OccAsmAdapter } from '@spartacus/asm/occ';
+import {
+  BaseSiteService,
+  I18nTestingModule,
+  User,
+  UserService,
+} from '@spartacus/core';
+import { ActiveCartFacade } from 'feature-libs/cart/base/root';
 import { Observable, of } from 'rxjs';
 import { AsmComponentService } from '../services/asm-component.service';
 import { CustomerEmulationComponent } from './customer-emulation.component';
@@ -9,6 +17,19 @@ import { CustomerEmulationComponent } from './customer-emulation.component';
 class MockUserService {
   get(): Observable<User> {
     return of({});
+  }
+}
+
+class MockActiveCartService {
+  getActiveCartId(): Observable<string> {
+    return of();
+  }
+}
+
+const baseSite = 'test-site';
+class MockBaseSiteService {
+  getActive(): Observable<string> {
+    return of(baseSite);
   }
 }
 
@@ -24,16 +45,21 @@ describe('CustomerEmulationComponent', () => {
   let fixture: ComponentFixture<CustomerEmulationComponent>;
   let userService: UserService;
   let asmComponentService: AsmComponentService;
+  let occAsmAdapter: OccAsmAdapter;
+  let activeCartFacade: ActiveCartFacade;
   let el: DebugElement;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [I18nTestingModule],
+        imports: [I18nTestingModule, HttpClientTestingModule],
         declarations: [CustomerEmulationComponent],
         providers: [
           { provide: UserService, useClass: MockUserService },
           { provide: AsmComponentService, useClass: MockAsmComponentService },
+          { provide: ActiveCartFacade, useClass: MockActiveCartService },
+          OccAsmAdapter,
+          { provide: BaseSiteService, useClass: MockBaseSiteService },
         ],
       }).compileComponents();
     })
@@ -45,6 +71,8 @@ describe('CustomerEmulationComponent', () => {
     fixture.detectChanges();
     userService = TestBed.inject(UserService);
     asmComponentService = TestBed.inject(AsmComponentService);
+    occAsmAdapter = TestBed.inject(OccAsmAdapter);
+    activeCartFacade = TestBed.inject(ActiveCartFacade);
     el = fixture.debugElement;
   });
 
@@ -59,9 +87,11 @@ describe('CustomerEmulationComponent', () => {
     fixture.detectChanges();
 
     expect(
-      el.query(By.css('input[formcontrolname="customer"]')).nativeElement
-        .placeholder
-    ).toEqual(`${testUser.name}, ${testUser.uid}`);
+      el.query(By.css('.customerInfo .name')).nativeElement.innerHTML
+    ).toEqual(`${testUser.name}`);
+    expect(
+      el.query(By.css('.customerInfo .uid')).nativeElement.innerHTML
+    ).toEqual(`${testUser.uid}`);
     expect(el.query(By.css('dev.fd-alert'))).toBeFalsy();
   });
 
@@ -74,11 +104,67 @@ describe('CustomerEmulationComponent', () => {
     fixture.detectChanges();
 
     //Click button
-    const endSessionButton = fixture.debugElement.query(By.css('button'));
+    const endSessionButton = fixture.debugElement.query(
+      By.css('button[formControlName="logoutCustomer"]')
+    );
     spyOn(asmComponentService, 'logoutCustomer').and.stub();
     endSessionButton.nativeElement.click();
 
     //assert
     expect(asmComponentService.logoutCustomer).toHaveBeenCalled();
+  });
+
+  it('should assign cart to customer', () => {
+    const testUser = { uid: 'user@test.com', name: 'Test User' } as User;
+    const prevActiveCartId = '00001122';
+    const testCartId = '00001234';
+    const assignCartToCustomerButton = fixture.debugElement.query(
+      By.css('button.assignCartToCustomer')
+    );
+    spyOn(userService, 'get').and.returnValue(of(testUser));
+
+    spyOn(occAsmAdapter, 'bindCart').and.returnValue(of());
+    spyOn(activeCartFacade, 'getActiveCartId').and.returnValue(
+      of(prevActiveCartId)
+    );
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    // check that cart id entered matches
+    expect(component.cartId.value).toEqual(prevActiveCartId);
+
+    // check that cart id exists
+    expect(component.cartIdExists).toBeTruthy();
+
+    // check that assign to cart button is enabled
+    expect(
+      assignCartToCustomerButton.nativeElement.getAttribute('disabled')
+    ).toBeFalsy();
+
+    // clear entered cart id
+    component.cartId.setValue('');
+
+    // check that cart id DOES NOT exists
+    expect(component.cartIdExists).toBeFalsy();
+
+    // check that assign to cart button is disabled
+    expect(
+      assignCartToCustomerButton.nativeElement.getAttribute('disabled')
+    ).toBeNull();
+
+    // set cart number to assign
+    component.cartId.setValue(testCartId);
+
+    // check that cart id exists
+    expect(component.cartIdExists).toBeTruthy();
+
+    // check that cart id entered matches
+    expect(component.cartId.value).toEqual(testCartId);
+
+    // check that assign to cart button is enabled
+    expect(
+      assignCartToCustomerButton.nativeElement.getAttribute('disabled')
+    ).toBeFalsy();
   });
 });
