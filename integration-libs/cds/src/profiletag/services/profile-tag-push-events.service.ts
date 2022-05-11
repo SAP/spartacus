58357f807fd7d9dee067f88a97f5b530f403fc4a
Shankari Giri <shankari.giri@sap.com>
2022-05-11 12:36:45 +0200

    CXCDS-3688: Backport cart-snapshot changes to 4.3.x

--- integration-libs/cds/src/profiletag/services/profile-tag-push-events.service.ts
+++ integration-libs/cds/src/profiletag/services/profile-tag-push-events.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
 import { OrderPlacedEvent } from '@spartacus/checkout/root';
 import {
+  ActiveCartFacade,
   CartAddEntrySuccessEvent,
   CartRemoveEntrySuccessEvent,
   CartUpdateEntrySuccessEvent,
@@ -24,10 +25,12 @@ import {
   mapTo,
   pairwise,
   startWith,
+  switchMapTo,
   withLatestFrom,
 } from 'rxjs/operators';
 import {
   AddedToCartPushEvent,
+  CartSnapshotPushEvent,
   CartViewPushEvent,
   CategoryViewPushEvent,
   HomePageViewPushEvent,
@@ -71,12 +74,30 @@ export class ProfileTagPushEventsService {
     this.orderConfirmationPageVisited(),
     this.addedToCart(),
     this.removedFromCart(),
-    this.modifiedCart()
+    this.modifiedCart(),
+    this.cartChangedEvent()
+  );
+
+  // TODO: Remove deprecated constructors
+  constructor(
+    protected eventService: EventService,
+    protected personalizationContextService: PersonalizationContextService,
+    // eslint-disable-next-line @typescript-eslint/unified-signatures
+    activeCartFacade?: ActiveCartFacade
+  );
+
+  /**
+   * @deprecated since 4.3
+   */
+  constructor(
+    protected eventService: EventService,
+    protected personalizationContextService: PersonalizationContextService
   );
 
   constructor(
     protected eventService: EventService,
-    protected personalizationContextService: PersonalizationContextService
+    protected personalizationContextService: PersonalizationContextService,
+    @Optional() protected activeCartFacade?: ActiveCartFacade
   ) {}
 
   /**
@@ -253,9 +274,9 @@ export class ProfileTagPushEventsService {
   }
 
   /**
-   * Listens to CartAddEntrySuccessEvent events, transforms them to AddedToCartPushEvent.
+   * Listens to @CartAddEntrySuccessEvent events, transforms them to @AddedToCartPushEvent .
    *
-   * @returns an observable emitting AddedToCartPushEvent events
+   * @returns an observable emitting @AddedToCartPushEvent events
    * @see CartAddEntrySuccessEvent
    * @see AddedToCartPushEvent
    */
@@ -321,8 +342,8 @@ export class ProfileTagPushEventsService {
   /**
    * Listens to @CartUpdateEntrySuccessEvent events, transforms them to @ModifiedCartPushEvent
    *
-   * @returns an observable emitting @RemovedFromCartPushEvent events
-   * @see CartRemoveEntrySuccessEvent
+   * @returns an observable emitting @ModifiedCartPushEvent events
+   * @see CartUpdateEntrySuccessEvent
    * @see ModifiedCartPushEvent
    */
   protected modifiedCart(): Observable<ProfileTagPushEvent> {
@@ -346,6 +367,32 @@ export class ProfileTagPushEventsService {
                   item.entry.product.categories.length - 1
                 ].code
               : undefined,
+          })
+      )
+    );
+  }
+
+  /**
+   * Listens to @CartAddEntrySuccessEvent , @CartUpdateEntrySuccessEvent and @CartRemoveEntrySuccessEvent events,
+   * transforms them to @CartSnapshotPushEvent whenever there is an activity on the cart.
+   *
+   * @returns an observable emitting @CartSnapshotPushEvent events
+   * @see CartAddEntrySuccessEvent
+   * @see CartUpdateEntrySuccessEvent
+   * @see CartRemoveEntrySuccessEvent
+   * @see CartSnapshotPushEvent
+   */
+  protected cartChangedEvent(): Observable<ProfileTagPushEvent> {
+    return merge(
+      this.eventService.get(CartAddEntrySuccessEvent),
+      this.eventService.get(CartUpdateEntrySuccessEvent),
+      this.eventService.get(CartRemoveEntrySuccessEvent)
+    ).pipe(
+      switchMapTo(this.activeCartFacade.takeActive()),
+      map(
+        (cart) =>
+          new CartSnapshotPushEvent({
+            cart,
           })
       )
     );
