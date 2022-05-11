3291b344d130e0da4d1083efe40a342d679d96dc
FollowTheFlo <florent.letendre01@sap.com>
2022-05-11 16:53:18 -0400

    feature/CXSPA-331:add unit tests

---
+++ projects/storefrontlib/cms-components/content/pdf/pdf.component.spec.ts
@@ -0,0 +1,128 @@
+import { Component, Input, Pipe, PipeTransform } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  CmsBannerComponentMedia,
+  CmsPDFDocumentComponent,
+} from '@spartacus/core';
+import { CmsComponentData, Media, MediaService } from '@spartacus/storefront';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { PDFComponent } from './pdf.component';
+
+@Pipe({
+  name: 'cxTranslate',
+})
+class MockTranslatePipe implements PipeTransform {
+  transform(value: string): any {
+    if (value === 'pdf.defaultTitle') return 'Document';
+    else return '';
+  }
+}
+
+class MockMediaService {
+  getMedia(media: any): Media {
+    return {
+      src: media ? media.url : undefined,
+      srcset: undefined,
+      alt: undefined,
+    };
+  }
+}
+
+@Component({
+  selector: 'cx-icon',
+  template: '',
+})
+class MockCxIconComponent {
+  @Input() type: any;
+}
+
+const mockCmsBannerComponentMedia: CmsBannerComponentMedia = {
+  altText: 'test alt text',
+  code: 'test code',
+  mime: 'test mime',
+  url: 'test url',
+};
+
+const mockComponentData: CmsPDFDocumentComponent = {
+  pdfFile: mockCmsBannerComponentMedia,
+  title: 'test title',
+  height: 256,
+};
+
+const data$: BehaviorSubject<CmsPDFDocumentComponent> = new BehaviorSubject(
+  mockComponentData
+);
+
+class MockCmsPDFDocumentComponentData {
+  get data$(): Observable<CmsPDFDocumentComponent> {
+    return data$.asObservable();
+  }
+}
+
+describe('PdfComponent', () => {
+  let pdfComponent: PDFComponent;
+  let fixture: ComponentFixture<PDFComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PDFComponent, MockTranslatePipe, MockCxIconComponent],
+      providers: [
+        {
+          provide: CmsComponentData,
+          useClass: MockCmsPDFDocumentComponentData,
+        },
+        { provide: MediaService, useClass: MockMediaService },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PDFComponent);
+    pdfComponent = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(pdfComponent).toBeTruthy();
+  });
+
+  describe('Title text', () => {
+    it("should set 'test title.pdf' when title is defined", () => {
+      data$.next({
+        ...mockComponentData,
+      });
+
+      const textElement = fixture.nativeElement.querySelector('a > span');
+      fixture.detectChanges();
+      expect(textElement.textContent).toContain('test title.pdf');
+    });
+    it('should display pdf altText when title is not defined', () => {
+      data$.next({ ...mockComponentData, title: undefined });
+
+      const textElement = fixture.nativeElement.querySelector('a > span');
+      fixture.detectChanges();
+      expect(textElement.textContent).toContain('test alt text.pdf');
+    });
+    it("should display 'Document.pdf' when title and altText are not defined", () => {
+      data$.next({
+        ...mockComponentData,
+        title: undefined,
+        pdfFile: { ...mockComponentData.pdfFile, altText: undefined },
+      });
+
+      const textElement = fixture.nativeElement.querySelector('a > span');
+      fixture.detectChanges();
+      expect(textElement.textContent).toContain('Document.pdf');
+    });
+    it("should display 'title.pdf' when title already ends with pdf extension", () => {
+      data$.next({
+        ...mockComponentData,
+        title: ' title.pdf ',
+      });
+
+      const textElement = fixture.nativeElement.querySelector('a > span');
+      fixture.detectChanges();
+      expect(textElement.textContent).toContain('title.pdf');
+    });
+  });
+});
