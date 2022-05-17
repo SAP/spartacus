266ca956b10ae01421444091bc0f22ed3a813e2e
Christopher Michael <chris.michael@sap.com>
2022-05-17 17:09:12 +0100

    Adds schematics

---
+++ feature-libs/pickup-in-store/schematics/constants.ts
@@ -0,0 +1,10 @@
+import { SPARTACUS_PICKUP_IN_STORE } from '@spartacus/schematics';
+
+export const SPARTACUS_PICKUP_IN_STORE_ASSETS = `${SPARTACUS_PICKUP_IN_STORE}/assets`;
+export const SPARTACUS_PICKUP_IN_STORE_ROOT = `${SPARTACUS_PICKUP_IN_STORE}/root`;
+export const PICKUP_IN_STORE_FEATURE_NAME_CONSTANT = 'PICKUP_IN_STORE_FEATURE';
+export const PICKUP_IN_STORE_FOLDER_NAME = 'pickup-in-store';
+export const PICKUP_IN_STORE_MODULE_NAME = 'PickupInStore';
+export const PICKUP_IN_STORE_TRANSLATIONS = 'pickupInStoreTranslations';
+export const PICKUP_IN_STORE_TRANSLATION_CHUNKS_CONFIG =
+  'pickupInStoreTranslationChunksConfig';
