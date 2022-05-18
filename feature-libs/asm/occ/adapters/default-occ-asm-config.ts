edc2d909a3cd4bf5538aeefc66ac7ad873fe3a28
Christopher Manciero <cmanciero@gmail.com>
2022-05-18 09:08:45 -0400

    update bind cart endpoint

--- feature-libs/asm/occ/adapters/default-occ-asm-config.ts
+++ feature-libs/asm/occ/adapters/default-occ-asm-config.ts
@@ -5,7 +5,7 @@ export const defaultOccAsmConfig: OccConfig = {
     occ: {
       endpoints: {
         asmCustomerSearch: '/assistedservicewebservices/customers/search',
-        asmBindCart: '/assistedservicewebservices/customers/search',
+        asmBindCart: '/assistedservicewebservices/bind-cart',
       },
     },
   },
