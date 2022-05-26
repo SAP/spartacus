14a9d8e43ce7724af8e00d81fc0f747bb5fa3a9c
I554698 <surya.singh@sap.com>
2022-05-26 12:20:55 +0100

    Test: Fixed test case for pick up in store

--- feature-libs/pickup-in-store/root/components/pickup-delivery-options/pickup-delivery-options.component.spec.ts
+++ feature-libs/pickup-in-store/root/components/pickup-delivery-options/pickup-delivery-options.component.spec.ts
@@ -1,11 +1,25 @@
+import { ElementRef, ViewContainerRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { I18nTestingModule } from '@spartacus/core';
 import { PickupInStoreFacade } from '@spartacus/pickup-in-store/root';
+import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
+import { of } from 'rxjs';
 import { PickupDeliveryOptionsComponent } from './pickup-delivery-options.component';
+
 import createSpy = jasmine.createSpy;
 
 class MockPickupInStoreFacade implements PickupInStoreFacade {
   getStore = createSpy();
+}
+
+class MockLaunchDialogService implements Partial<LaunchDialogService> {
+  openDialog(
+    _caller: LAUNCH_CALLER,
+    _openElement?: ElementRef,
+    _vcr?: ViewContainerRef
+  ) {
+    return of();
+  }
 }
 
 describe('PickupDeliveryOptionsComponent', () => {
@@ -22,6 +36,10 @@ describe('PickupDeliveryOptionsComponent', () => {
         {
           provide: PickupInStoreFacade,
           useClass: MockPickupInStoreFacade,
+        },
+        {
+          provide: LaunchDialogService,
+          useClass: MockLaunchDialogService,
         },
       ],
       declarations: [PickupDeliveryOptionsComponent],
