028ae1030449b59adc4a7604fe5b199461a9547d
KateChuen <kate.chuen.cheong@sap.com>
2022-06-09 11:26:39 -0400

    changes requested for pwd visibility

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/password-visibility/password-visibility.core-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/password-visibility/password-visibility.core-e2e-spec.ts
@@ -1,36 +1,57 @@
 import { viewportContext } from '../../../helpers/viewport-context';
 import { standardUser } from '../../../sample-data/shared-users';
 import { generateMail, randomString } from '../../../helpers/user';
+import { clearAllStorage } from '../../../support/utils/clear-all-storage';
+import { waitForPage } from '../../../helpers/checkout-flow';
 
 context('Password Visibility', () => {
   viewportContext(['mobile', 'desktop'], () => {
     beforeEach(() => {
       cy.window().then((win) => {
         win.sessionStorage.clear();
+        clearAllStorage();
       });
     });
 
     it('should hide password by default', () => {
+      const loginPage = waitForPage('/login', 'getLoginPage');
       cy.visit('/login');
+      cy.wait(`@${loginPage}`).its('response.statusCode').should('eq', 200);
+
       cy.get('button[aria-label="Show password"]').should('be.visible');
     });
 
     it('should show and hide password when toggled', () => {
+      const loginPage = waitForPage('/login', 'getLoginPage');
       cy.visit('/login');
+      cy.wait(`@${loginPage}`).its('response.statusCode').should('eq', 200);
+
       cy.get('button[aria-label="Show password"]').click();
+      cy.get('input[aria-label="Password"]').type('abc');
+      cy.get('input[aria-label="Password"]').should('have.value', 'abc');
 
       cy.get('button[aria-label="Hide password"]').click();
     });
 
-    it('should verify on other pages', () => {
-      //on registration page
+    it('should verify password is hidden by default on registration page', () => {
+      const registerPage = waitForPage('/login/register', 'getRegisterPage');
       cy.visit('/login/register');
-      cy.get('button[aria-label="Show password"]').should('have.length', 2);
+      cy.wait(`@${registerPage}`).its('response.statusCode').should('eq', 200);
 
-      //on my account update password page
-      standardUser.registrationData.email = generateMail(randomString(), true);
-      cy.requireLoggedIn(standardUser);
+      cy.get('button[aria-label="Show password"]').should('have.length', 2);
+    });
+
+    it('should verify password is hidden by default on my account password page', () => {
+      cy.requireLoggedIn();
+      const myAccountUpdatePasswordPage = waitForPage(
+        '/my-account/update-password',
+        'getMyAccountUpdatePasswordPage'
+      );
       cy.visit('/my-account/update-password');
+      cy.wait(`@${myAccountUpdatePasswordPage}`)
+        .its('response.statusCode')
+        .should('eq', 200);
+
       cy.get('button[aria-label="Show password"]').should('have.length', 3);
     });
   });
