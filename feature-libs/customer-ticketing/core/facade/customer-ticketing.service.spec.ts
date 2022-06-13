69745694582d3c105b63512c65559164d650c0d8
Manpreet Kaur <manpreet.kaur03@sap.com>
2022-06-13 10:04:08 -0400

    Fix integration test

--- feature-libs/customer-ticketing/core/facade/customer-ticketing.service.spec.ts
+++ feature-libs/customer-ticketing/core/facade/customer-ticketing.service.spec.ts
@@ -2,13 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { CustomerTicketingService } from './customer-ticketing.service';
 
 describe('CustomerTicketingService', () => {
-  let service: CustomerTicketingService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CustomerTicketingService],
     });
-
-    service = TestBed.inject(CustomerTicketingService);
   });
 });
