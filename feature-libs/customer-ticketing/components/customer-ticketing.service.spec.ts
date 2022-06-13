4bc7469e28a086c4da66e8f636c672f7bad3a064
Manpreet Kaur <manpreet.kaur03@sap.com>
2022-06-13 09:33:07 -0400

    Fix unit test

--- feature-libs/customer-ticketing/components/customer-ticketing.service.spec.ts
+++ feature-libs/customer-ticketing/components/customer-ticketing.service.spec.ts
@@ -1,14 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CustomerTicketingService } from './customer-ticketing.service';
-
 describe('CustomerTicketingService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: CustomerTicketingService = TestBed.get(
-      CustomerTicketingService
-    );
-    expect(service).toBeTruthy();
-  });
 });
