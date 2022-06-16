49875e6f8d96d6388c8e21377de91703586d9425
Sam Vendittelli <sam.vendittelli@sap.com>
2022-06-16 10:17:27 +0100

    feat: i18n for store list

--- feature-libs/pickup-in-store/components/store-search/store-search.module.ts
+++ feature-libs/pickup-in-store/components/store-search/store-search.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { I18nModule } from '@spartacus/core';
 import { StoreSearchComponent } from './store-search.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, I18nModule],
   exports: [StoreSearchComponent],
   declarations: [StoreSearchComponent],
 })
