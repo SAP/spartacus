56594caf9d41bb1b8b5e96da4a107ff6e378c857
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2022-06-17 16:07:30 +0200

    Adjust root module

--- integration-libs/s4om/root/s4om-root.module.ts
+++ integration-libs/s4om/root/s4om-root.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ScheduleLinesModule } from '@spartacus/s4om';
 
 @NgModule({
-  imports: [],
+  imports: [ScheduleLinesModule],
 })
 export class S4omModule {}
