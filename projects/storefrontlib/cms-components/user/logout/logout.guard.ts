cae156b2e7e6c6e5f5f20482f8843dd1942cb785
Jeremy Laverdiere <jeremy.laverdiere@sap.com>
2022-06-22 14:15:05 -0400

    Remove usage of removed method

--- projects/storefrontlib/cms-components/user/logout/logout.guard.ts
+++ projects/storefrontlib/cms-components/user/logout/logout.guard.ts
@@ -26,12 +26,10 @@ export class LogoutGuard implements CanActivate {
     protected cms: CmsService,
     protected semanticPathService: SemanticPathService,
     protected protectedRoutes: ProtectedRoutesService,
-    protected router: Router
+    protected router: Router,
   ) {}
 
   canActivate(): Observable<boolean | UrlTree> {
-    // Logout route should never be remembered as a redirect url after login (that would cause logout right after login).
-    this.authRedirectService.reportNotAuthGuard();
     /**
      * First we want to complete logout process before redirecting to logout page
      * We want to avoid errors like `token is no longer valid`
