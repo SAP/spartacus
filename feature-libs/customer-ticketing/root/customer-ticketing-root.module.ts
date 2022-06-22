973d668f7532302a6fb044ae83876d31bd1f2984
Manpreet Kaur <manpreet.kaur03@sap.com>
2022-06-22 09:43:49 -0400

    Add cms components

--- feature-libs/customer-ticketing/root/customer-ticketing-root.module.ts
+++ feature-libs/customer-ticketing/root/customer-ticketing-root.module.ts
@@ -1,5 +1,75 @@
 import { NgModule } from '@angular/core';
-import {} from '@spartacus/core';
+import { RouterModule } from '@angular/router';
+import {
+  AuthGuard,
+  CmsConfig,
+  provideDefaultConfig,
+  provideDefaultConfigFactory,
+  RoutingConfig,
+} from '@spartacus/core';
+import { CmsPageGuard, PageLayoutComponent } from '@spartacus/storefront';
+import {
+  CUSTOMER_TICKETING_CORE_FEATURE,
+  CUSTOMER_TICKETING_FEATURE,
+} from './feature-name';
 
-@NgModule({})
+export function defaultCustomerTicketingComponentsConfig(): CmsConfig {
+  const config: CmsConfig = {
+    featureModules: {
+      [CUSTOMER_TICKETING_FEATURE]: {
+        cmsComponents: [
+          'SupportTicketsHistoryComponent',
+          'AddSupportTicketComponent',
+          'UpdateSupportTicketComponent',
+          'SupportTicketDetailsComponent',
+        ],
+        dependencies: [CUSTOMER_TICKETING_FEATURE],
+      },
+
+      // by default core is bundled together with components
+      [CUSTOMER_TICKETING_CORE_FEATURE]: CUSTOMER_TICKETING_FEATURE,
+    },
+  };
+  return config;
+}
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        // @ts-ignore
+        path: null,
+        canActivate: [AuthGuard, CmsPageGuard],
+        component: PageLayoutComponent,
+        data: {
+          cxRoute: 'supportTicketDetails',
+        },
+      },
+      {
+        // @ts-ignore
+        path: null,
+        canActivate: [AuthGuard, CmsPageGuard],
+        component: PageLayoutComponent,
+        data: {
+          cxRoute: 'supportTickets',
+        },
+      },
+    ]),
+  ],
+  providers: [
+    provideDefaultConfigFactory(defaultCustomerTicketingComponentsConfig),
+    provideDefaultConfig(<RoutingConfig>{
+      routing: {
+        routes: {
+          supportTickets: {
+            paths: ['my-account/support-tickets'],
+          },
+          supportTicketDetails: {
+            paths: ['my-account/support-ticket/:ticketCode'],
+            paramsMapping: { ticketCode: 'ticketCode' },
+          },
+        },
+      },
+    }),
+  ],
+})
 export class CustomerTicketingRootModule {}
