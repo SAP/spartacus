4b242f752b28682fb1e22be120b1ff7bdf2933df
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2022-06-23 09:46:44 -0400

    Removing it blocks

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/billing-address.core-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/billing-address.core-e2e-spec.ts
@@ -40,77 +40,33 @@ context('Payment billing address', () => {
     verifyGlobalMessageAfterRegistration();
     checkout.goToCheapProductDetailsPage();
     checkout.addCheapProductToCartAndLogin();
-  });
-
-  it('should fill address form', () => {
     checkout.fillAddressFormWithCheapProduct();
-  });
-
-  it('should choose delivery', () => {
     checkout.verifyDeliveryMethod();
-  });
-
-  it('should fill in payment form with billing address same as shipping address', () => {
     checkout.fillPaymentFormWithCheapProduct();
-  });
-
-  it('should redirect to review order page', () => {
     checkout.verifyReviewOrderPage();
   });
 
-  it('should go to payment details', () => {
+  it('should add new payment forms', () => {
     checkout.goToPaymentDetails();
-  });
-
-  it('should open add new payment form', () => {
     checkout.clickAddNewPayment();
-  });
-
-  it('should fill in payment form with different billing address (same country)', () => {
     checkout.fillPaymentFormWithCheapProduct(user, {
       ...user,
       address: usaAddress,
     });
-  });
-
-  it('should redirect to review order page', () => {
     checkout.verifyReviewOrderPage();
-  });
-  it('should go to payment details', () => {
     checkout.goToPaymentDetails();
-  });
-
-  it('should open add new payment form', () => {
     checkout.clickAddNewPayment();
-  });
-
-  it('should fill in payment form with different billing address (country with states)', () => {
     checkout.fillPaymentFormWithCheapProduct(user, {
       ...user,
       address: canadaAddress,
     });
-  });
-
-  it('should redirect to review order page', () => {
     checkout.verifyReviewOrderPage();
-  });
-
-  it('should go to payment details', () => {
     checkout.goToPaymentDetails();
-  });
-
-  it('should open add new payment form', () => {
     checkout.clickAddNewPayment();
-  });
-
-  it('should fill in payment form with different billing address (country without states)', () => {
     checkout.fillPaymentFormWithCheapProduct(user, {
       ...user,
       address: polandAddress,
     });
-  });
-
-  it('should redirect to review order page', () => {
     checkout.verifyReviewOrderPage();
   });
 });
