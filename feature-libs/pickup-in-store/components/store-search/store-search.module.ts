0746a9532ab8bc58323c4e4c73eff8e2655d59f9
mechris3 <mechris3@hotmail.com>
2022-06-24 11:34:52 +0100

    feat: fix broken build

--- feature-libs/pickup-in-store/components/store-search/store-search.module.ts
+++ feature-libs/pickup-in-store/components/store-search/store-search.module.ts
@@ -1,19 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NgModule } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
 import { I18nModule } from '@spartacus/core';
 import { StoreSearchComponent } from './store-search.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    I18nModule,
-    HttpClientTestingModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-  ],
+  imports: [CommonModule, I18nModule],
   exports: [StoreSearchComponent],
   declarations: [StoreSearchComponent],
   providers: [],
