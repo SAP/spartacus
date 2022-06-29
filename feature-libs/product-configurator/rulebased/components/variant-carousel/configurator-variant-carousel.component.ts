41401121c6510ac3d893efc17073123084816020
Larisa Staroverova <larisa.staroverova@sap.com>
2022-06-29 13:33:40 +0200

    Refactoring I

--- feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.component.ts
+++ feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.component.ts
@@ -1,34 +1,41 @@
 import {
   CmsProductCarouselComponent,
+  Product,
   ProductService,
   TranslationService,
 } from '@spartacus/core';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { CmsComponentData } from '@spartacus/storefront';
-import { tap } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-configurator-variant-carousel',
   templateUrl: './configurator-variant-carousel.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfiguratorVariantCarouselComponent implements OnInit {
+export class ConfiguratorVariantCarouselComponent {
   constructor(
-    protected component: CmsComponentData<CmsProductCarouselComponent>,
     protected productService: ProductService,
     protected translation: TranslationService
   ) {}
 
-  protected setProductCodes(data: CmsProductCarouselComponent) {
-    data.productCodes = 'CONF_CAMERA_SL';
-  }
+  data: CmsProductCarouselComponent = {
+    productCodes:
+      'CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC ',
+  };
 
-  ngOnInit(): void {
-    this.component.data$.pipe(
-      tap((data) => {
-        data.title = 'TEST';
-        this.setProductCodes(data);
-      })
+  private componentData$: Observable<CmsProductCarouselComponent> =
+    new Observable((observer) => {
+      observer.next(this.data);
+    });
+
+  title$: Observable<string | undefined> = this.translation
+    .translate('configurator.variantCarousel.title')
+    .pipe(take(1));
+
+  items$: Observable<Observable<Product | undefined>[]> =
+    this.componentData$.pipe(
+      map((data) => data.productCodes?.trim().split(' ') ?? []),
+      map((codes) => codes.map((code) => this.productService.get(code)))
     );
-  }
 }
--- feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.module.ts
+++ feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
-import { ProductCarouselModule } from '@spartacus/storefront';
+import { CarouselModule, ProductCarouselModule } from '@spartacus/storefront';
 import { ConfiguratorVariantCarouselComponent } from './configurator-variant-carousel.component';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { ConfiguratorVariantCarouselComponent } from './configurator-variant-car
     ReactiveFormsModule,
     CommonModule,
     I18nModule,
+    CarouselModule,
     ProductCarouselModule,
   ],
   providers: [
@@ -18,12 +19,6 @@ import { ConfiguratorVariantCarouselComponent } from './configurator-variant-car
       cmsComponents: {
         ConfiguratorVariantCarousel: {
           component: ConfiguratorVariantCarouselComponent,
-          data: {
-            title: 'Pre-configured Versions',
-            productCodes:
-              'CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC',
-            scroll: 'ALLVISIBLE',
-          },
         },
       },
     }),
