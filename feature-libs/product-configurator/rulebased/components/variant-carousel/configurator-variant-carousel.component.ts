6b7734ecd60cfd0165266ddd05dfaaba12dc1c47
Larisa Staroverova <larisa.staroverova@sap.com>
2022-07-01 10:43:45 +0200

    Bind service implementation

--- feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.component.ts
+++ feature-libs/product-configurator/rulebased/components/variant-carousel/configurator-variant-carousel.component.ts
@@ -1,12 +1,10 @@
-import {
-  CmsProductCarouselComponent,
-  Product,
-  ProductService,
-  TranslationService,
-} from '@spartacus/core';
+import { Product, ProductService, TranslationService } from '@spartacus/core';
+import { ConfiguratorRouterExtractorService } from '@spartacus/product-configurator/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
+import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
+import { Configurator } from '../../core/model/configurator.model';
 
 @Component({
   selector: 'cx-configurator-variant-carousel',
@@ -14,28 +12,32 @@ import { map, take } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConfiguratorVariantCarouselComponent {
-  constructor(
-    protected productService: ProductService,
-    protected translation: TranslationService
-  ) {}
-
-  data: CmsProductCarouselComponent = {
-    productCodes:
-      'CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC CONF_CAMERA_SL-PROF-BLACK CONF_CAMERA_SL-PROF-METALLIC CONF_CAMERA_SL-STD-BLACK CONF_CAMERA_SL-STD-METALLIC ',
-  };
-
-  private componentData$: Observable<CmsProductCarouselComponent> =
-    new Observable((observer) => {
-      observer.next(this.data);
-    });
+  configuration$: Observable<Configurator.Configuration> =
+    this.configRouterExtractorService
+      .extractRouterData()
+      .pipe(
+        switchMap((routerData) =>
+          this.configuratorCommonsService.getConfiguration(routerData.owner)
+        )
+      );
 
   title$: Observable<string | undefined> = this.translation
     .translate('configurator.variantCarousel.title')
     .pipe(take(1));
 
   items$: Observable<Observable<Product | undefined>[]> =
-    this.componentData$.pipe(
-      map((data) => data.productCodes?.trim().split(' ') ?? []),
-      map((codes) => codes.map((code) => this.productService.get(code)))
+    this.configuration$.pipe(
+      map((configuration) => {
+        return configuration?.variants?.map((variant) =>
+          this.productService.get(variant.productCode)
+        );
+      })
     );
+
+  constructor(
+    protected productService: ProductService,
+    protected translation: TranslationService,
+    protected configRouterExtractorService: ConfiguratorRouterExtractorService,
+    protected configuratorCommonsService: ConfiguratorCommonsService
+  ) {}
 }
