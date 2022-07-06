b2a02393427e8a2bed64a9dcf0506d12a691e8d8
Christoph Hinssen <christoph.hinssen@sap.com>
2022-07-06 10:44:40 +0200

    Fix build issue (augmentation in one place)

--- feature-libs/product-configurator/rulebased/core/config/configurator-core.config.ts
+++ feature-libs/product-configurator/rulebased/core/config/configurator-core.config.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Config } from '@spartacus/core';
-import '@spartacus/product-configurator/common';
 
 export interface ProductConfiguratorCoreConfig {
   enableVariantSearch?: boolean;
@@ -14,6 +13,4 @@ export abstract class ConfiguratorCoreConfig {
   productConfigurator?: ProductConfiguratorCoreConfig;
 }
 
-declare module '@spartacus/product-configurator/common' {
-  interface ProductConfiguratorConfig extends ProductConfiguratorCoreConfig {}
-}
+ 
