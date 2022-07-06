a82dec3a4c6b04b6a450b8dddcbf2e35cb97051b
Patrick Labadie <patrick.labadie@sap.com>
2022-07-06 18:55:31 -0400

    CXSPA-711: Merge constructors - QuickOrderService

--- projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
@@ -4,13 +4,11 @@ import { migrateConstructorDeprecation } from '../../mechanism/constructor-depre
 import { FORM_ERRORS_COMPONENT_MIGRATION } from './data/form-errors.component.migration';
 import { GENERATED_CONSTRUCTOR_MIGRATIONS } from './data/generated-constructor.migration';
 import { QUICK_ORDER_FORM_COMPONENT_MIGRATION } from './data/quick-order-form.component.migration';
-import { QUICK_ORDER_SERVICE_MIGRATION } from './data/quick-order.service.migration';
 import { SHIPPING_ADDRESS_COMPONENT_MIGRATION } from './data/shipping-address.component.migration';
 import { TAB_PARAGRAPH_CONTAINER_COMPONENT_MIGRATION } from './data/tab-paragraph-container.component.migration';
 
 export const CONSTRUCTOR_DEPRECATIONS_DATA: ConstructorDeprecation[] = [
   ...GENERATED_CONSTRUCTOR_MIGRATIONS,
-  QUICK_ORDER_SERVICE_MIGRATION,
   QUICK_ORDER_FORM_COMPONENT_MIGRATION,
   SHIPPING_ADDRESS_COMPONENT_MIGRATION,
   FORM_ERRORS_COMPONENT_MIGRATION,
