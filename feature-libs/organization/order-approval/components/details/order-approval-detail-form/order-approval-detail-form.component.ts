b4af2f57c2a84568d8fc7e71f17482a7c7191ef7
FollowTheFlo <florent.letendre01@sap.com>
2022-07-06 12:42:27 -0400

    fix for Angular_Improper_Type_Pipe_Usage async pipe

--- feature-libs/organization/order-approval/components/details/order-approval-detail-form/order-approval-detail-form.component.ts
+++ feature-libs/organization/order-approval/components/details/order-approval-detail-form/order-approval-detail-form.component.ts
@@ -4,7 +4,7 @@ import { combineLatest, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import {
   OrderApproval,
-  OrderApprovalDecisionValue,
+  OrderApprovalDecisionValue
 } from '../../../core/model/order-approval.model';
 import { OrderApprovalService } from '../../../core/services/order-approval.service';
 import { OrderApprovalDetailService } from '../order-approval-detail.service';
@@ -34,7 +34,7 @@ export class OrderApprovalDetailFormComponent implements OnDestroy {
   protected decisionResultLoading$ =
     this.orderApprovalService.getMakeDecisionResultLoading();
 
-  loading$ = combineLatest([
+  loading$: Observable<boolean> = combineLatest([
     this.orderApprovalLoading$,
     this.decisionResultLoading$,
   ]).pipe(
