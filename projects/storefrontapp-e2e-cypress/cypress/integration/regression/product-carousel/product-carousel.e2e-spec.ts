8c5d5dcf5b5a1e7639f32a7b98a0610c0240f62d
Justin Lee <justin.lee01@sap.com>
2022-07-07 12:57:51 -0400

    Adding E2E test for checking that out of stock products do not have Add-to-Cart buttons for Product Carousel

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/product-carousel/product-carousel.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/product-carousel/product-carousel.e2e-spec.ts
@@ -1,3 +1,5 @@
+import { CyHttpMessages } from 'cypress/types/net-stubbing';
+
 import * as cart from '../../../helpers/cart';
 import { viewportContext } from '../../../helpers/viewport-context';
 
@@ -18,11 +20,12 @@ context('Product carousel', () => {
       });
 
       cy.window().then((win) => win.sessionStorage.clear());
-      cy.visit('/');
     });
 
     describe('Add to Cart', () => {
       it('should add a product to cart', () => {
+        visitHomepage();
+
         const addToCart = cy
           .get('cx-product-carousel cx-product-carousel-item cx-add-to-cart')
           .first();
@@ -34,6 +37,92 @@ context('Product carousel', () => {
 
         cart.checkAddedToCartDialog();
       });
+
+      it('should not show for out-of-stock products', () => {
+        const secureDigitalCard2gb = '872912';
+
+        function pageRequestHandler(
+          request: CyHttpMessages.IncomingHttpRequest
+        ): void {
+          request.continue((res: CyHttpMessages.IncomingHttpResponse) => {
+            const body = res.body;
+            const slot = body.contentSlots.contentSlot.find(
+              (contentSlot) => contentSlot.slotId === 'Section3Slot-Homepage'
+            );
+
+            const component = slot.components.component.find(
+              (cmp) => cmp.uid === 'ElectronicsHomepageProductCarouselComponent'
+            );
+
+            component.productCodes = `${component.productCodes} ${secureDigitalCard2gb}`;
+
+            res.send(body);
+          });
+        }
+
+        function secureDigitalCardRequestHandler(
+          request: CyHttpMessages.IncomingHttpRequest
+        ): void {
+          request.continue((res: CyHttpMessages.IncomingHttpResponse) => {
+            const body = res.body;
+
+            body.stock = {
+              ...body.stock,
+              stockLevelStatus: 'outOfStock',
+              stockLevel: 0,
+            };
+
+            res.send(body);
+          });
+        }
+
+        visitHomepage();
+
+        cy.intercept(
+          {
+            method: 'GET',
+            pathname: `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+              'BASE_SITE'
+            )}/cms/pages`,
+          },
+          pageRequestHandler
+        );
+
+        cy.intercept(
+          {
+            method: 'GET',
+            pathname: `${Cypress.env('OCC_PREFIX')}/${Cypress.env(
+              'BASE_SITE'
+            )}/products/${secureDigitalCard2gb}`,
+          },
+          secureDigitalCardRequestHandler
+        );
+
+        function clickNextButton(): void {
+          const cypressButton = cy.get(
+            'cx-product-carousel:first-of-type button.next'
+          );
+
+          cypressButton.then((button) => {
+            if (!button.is(':disabled')) {
+              cypressButton.click();
+              clickNextButton();
+            }
+          });
+        }
+
+        // Continue clicking next in the carousel until we are at the last slide.
+        clickNextButton();
+
+        const lastSlide = cy
+          .get('cx-product-carousel:first-of-type cx-product-carousel-item')
+          .last();
+        lastSlide.find('cx-add-to-cart button').should('not.exist');
+      });
     });
   });
+
+  function visitHomepage(): void {
+    cy.visit('/');
+  }
 });
