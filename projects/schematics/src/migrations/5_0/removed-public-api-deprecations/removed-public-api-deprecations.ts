1aa471ebc5f8e925234fc4c0102500a59e62e7f2
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-20 15:51:01 -0400

    migrate AddDeliveryAddressFail

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -134,7 +134,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'AddDeliveryAddressFail',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Class CheckoutActions.AddDeliveryAddressFail has been removed and is no longer part of the public API. ',
+      "Class CheckoutActions.AddDeliveryAddressFail has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can leverage the creation of delivery address 'state' using the returned observable from 'createAndSetAddress' in CheckoutDeliveryAddressFacade.",
   },
   {
     node: 'AddDeliveryAddressSuccess',
