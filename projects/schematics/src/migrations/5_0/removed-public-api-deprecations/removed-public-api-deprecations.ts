1cbbe1c9653ad285af5ce38527798c3134740328
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-20 17:07:16 -0400

    migrate CLEAR_CHECKOUT_DELIVERY_ADDRESS_FAIL

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -176,7 +176,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'CLEAR_CHECKOUT_DELIVERY_ADDRESS_FAIL',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Variable CheckoutActions.CLEAR_CHECKOUT_DELIVERY_ADDRESS_FAIL has been removed and is no longer part of the public API. ',
+      "Variable CheckoutActions.CLEAR_CHECKOUT_DELIVERY_ADDRESS_FAIL has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can leverage the clearing of delivery address 'state' using the returned observable from 'clearCheckoutDeliveryAddress' in CheckoutDeliveryAddressFacade.",
   },
   {
     node: 'CLEAR_CHECKOUT_DELIVERY_ADDRESS_SUCCESS',
