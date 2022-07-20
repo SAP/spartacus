7cb08acdd53c6dfc83d6f6029d9ca1bb25f04e3f
Sam Vendittelli <sam.vendittelli@sap.com>
2022-07-20 15:59:06 +0100

    centre globe on average of points

--- projects/storefrontlib/cms-components/content/globe/globe.component.ts
+++ projects/storefrontlib/cms-components/content/globe/globe.component.ts
@@ -13,65 +13,55 @@ export class GlobeComponent implements AfterViewInit {
   constructor(private readonly globeService: GlobeService) {}
 
   ngAfterViewInit(): void {
-    this.globeService.getSupplyChainForProduct('productCode').subscribe((supplyChain) => {
-      //   Globe()
-      //     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
-      //     .width(545)
-      //     .height(545)
-      //     .backgroundColor('#ffffff')
-      //     .labelsData(places.features)
-      //     .labelLat((d: any) => d.properties.latitude)
-      //     .labelLng((d: any) => d.properties.longitude)
-      //     .labelText((d: any) => d.properties.name)
-      //     .labelSize((d: any) => Math.sqrt(d.properties.pop_max) * 4e-4)
-      //     .labelDotRadius((d: any) => Math.sqrt(d.properties.pop_max) * 4e-4)
-      //     .labelColor(() => 'rgba(255, 165, 0, 0.75)')
-      //     .labelResolution(2)
-      //     .arcsData(arcsData)
-      //     .arcColor('color')
-      //     .arcDashLength(() => Math.random())
-      //     .arcDashGap(() => Math.random())
-      //     .arcDashAnimateTime(() => Math.random() * 4000 + 500)
-      //     .arcStroke(() => 1)
-      //     .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')(
-      //     this.globeViz.nativeElement
-      //   );
-      // });
+    this.globeService
+      .getSupplyChainForProduct('productCode')
+      .subscribe((supplyChain) => {
+        const arcsData = supplyChain.paths
+          .map((path) => {
+            const arcs = path.slice(0, -1).map(({ lat, long }, index) => ({
+              startLat: lat,
+              startLng: long,
+              endLat: path[index + 1].lat,
+              endLng: path[index + 1].long,
+            }));
+            return arcs;
+          })
+          .reduce((acc, val) => acc.concat(val), []);
 
-      const arcsData = supplyChain.paths
-        .map((path) => {
-          const arcs = path.slice(0, -1).map(({ lat, long }, index) => ({
-            startLat: lat,
-            startLng: long,
-            endLat: path[index + 1].lat,
-            endLng: path[index + 1].long,
-          }));
-          return arcs;
-        })
-        .reduce((acc, val) => acc.concat(val), []);
+        const { lat, lng } = supplyChain.labels.reduce(
+          (acc, val) => ({ lat: acc.lat + val.lat, lng: acc.lng + val.long }),
+          { lat: 0, lng: 0 }
+        );
+        const averageLocation = {
+          lat: lat / supplyChain.labels.length,
+          lng: lng / supplyChain.labels.length,
+          altitude: 2.5,
+        };
 
-      Globe()
-        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
-        .width(545)
-        .height(545)
-        .backgroundColor('rgba(0, 0, 0, 0)')
-        .arcsData(arcsData)
-        .arcColor(() => '#FE5757')
-        .arcDashLength(() => Math.random())
-        .arcDashGap(() => Math.random())
-        .arcDashAnimateTime(() => Math.random() * 4000 + 500)
-        .arcStroke(() => 1)
-        .labelsData(supplyChain.labels)
-        .labelLat((d: any) => d.lat)
-        .labelLng((d: any) => d.long)
-        .labelText((d: any) => d.label)
-        .labelSize(1.5)
-        .labelDotRadius(1)
-        .labelColor(() => 'rgba(255, 165, 0, 0.75)')
-        .labelResolution(2)
-        .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')(
-        this.globeViz.nativeElement
-      );
-    });
+        const myGlobe = Globe();
+        myGlobe
+          .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
+          .width(545)
+          .height(545)
+          .backgroundColor('rgba(0, 0, 0, 0)')
+          .arcsData(arcsData)
+          .arcColor(() => '#FE5757')
+          .arcDashLength(() => Math.random())
+          .arcDashGap(() => Math.random())
+          .arcDashAnimateTime(() => Math.random() * 4000 + 500)
+          .arcStroke(() => 1)
+          .labelsData(supplyChain.labels)
+          .labelLat((d: any) => d.lat)
+          .labelLng((d: any) => d.long)
+          .labelText((d: any) => d.label)
+          .labelSize(1.5)
+          .labelDotRadius(1)
+          .labelColor(() => 'rgba(255, 165, 0, 0.75)')
+          .labelResolution(2)
+          .pointOfView(averageLocation, 10)
+          .bumpImageUrl(
+            '//unpkg.com/three-globe/example/img/earth-topology.png'
+          )(this.globeViz.nativeElement);
+      });
   }
 }
