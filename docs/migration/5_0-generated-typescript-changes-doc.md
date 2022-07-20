e23da198c6a3141b6e49629cf301be583be76adc
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-20 15:52:31 -0400

    migrate AddDeliveryAddressSuccess

--- docs/migration/5_0-generated-typescript-changes-doc.md
+++ docs/migration/5_0-generated-typescript-changes-doc.md
@@ -2289,7 +2289,7 @@ It is no used anymore. Checkout library no longer uses NgRX to dispatch actions.
 
 
 Class CheckoutActions.AddDeliveryAddressSuccess has been removed and is no longer part of the public API.
-
+It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can use the EventService to listen for 'CheckoutDeliveryAddressCreatedEvent', which gets fired when the address has been successfully added.
 
 
 
--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -140,7 +140,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'AddDeliveryAddressSuccess',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Class CheckoutActions.AddDeliveryAddressSuccess has been removed and is no longer part of the public API. ',
+      "Class CheckoutActions.AddDeliveryAddressSuccess has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can use the EventService to listen for 'CheckoutDeliveryAddressCreatedEvent', which gets fired when the address has been successfully added.",
   },
   {
     node: 'CardTypesAction',
