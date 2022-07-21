c5748e62db8d77899297aff474dfcc4c571da249
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 04:12:14 -0400

    migrate ClearCheckoutData

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -236,7 +236,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'ClearCheckoutData',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Class CheckoutActions.ClearCheckoutData has been removed and is no longer part of the public API. ',
+      "Class CheckoutActions.ClearCheckoutData has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can use the EventService to dispatch an event to clear checkout data by using 'CheckoutQueryResetEvent'.",
   },
   {
     node: 'ClearCheckoutDeliveryAddress',
