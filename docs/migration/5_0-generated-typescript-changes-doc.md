e02f84ff0f2423dde0f21eadb8a432ec34af7abc
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 05:26:07 -0400

    migrate PaymentTypesAction

--- docs/migration/5_0-generated-typescript-changes-doc.md
+++ docs/migration/5_0-generated-typescript-changes-doc.md
@@ -2830,7 +2830,7 @@ It is no used anymore. Checkout library no longer uses NgRX to dispatch actions.
 
 
 TypeAlias CheckoutActions.PaymentTypesAction has been removed and is no longer part of the public API.
-
+It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries.
 
 
 
--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -500,7 +500,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'PaymentTypesAction',
     importPath: '@spartacus/checkout/core',
     comment:
-      'TypeAlias CheckoutActions.PaymentTypesAction has been removed and is no longer part of the public API. ',
+      'TypeAlias CheckoutActions.PaymentTypesAction has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries.',
   },
   {
     node: 'PLACE_ORDER_FAIL',
