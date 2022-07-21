4913da64f07dc5781919b0ec3bd381c7e5a2d95a
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 04:10:54 -0400

    migrate CLEAR_SUPPORTED_DELIVERY_MODES

--- docs/migration/5_0-generated-typescript-changes-doc.md
+++ docs/migration/5_0-generated-typescript-changes-doc.md
@@ -2424,7 +2424,7 @@ It is no used anymore. Checkout library no longer uses NgRX to dispatch actions.
 
 
 Variable CheckoutActions.CLEAR_SUPPORTED_DELIVERY_MODES has been removed and is no longer part of the public API.
-
+It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. Use 'clearCheckoutDeliveryMode' from CheckoutDeliveryModesFacade instead.
 
 
 
--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -230,7 +230,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'CLEAR_SUPPORTED_DELIVERY_MODES',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Variable CheckoutActions.CLEAR_SUPPORTED_DELIVERY_MODES has been removed and is no longer part of the public API. ',
+      "Variable CheckoutActions.CLEAR_SUPPORTED_DELIVERY_MODES has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. Use 'clearCheckoutDeliveryMode' from CheckoutDeliveryModesFacade instead.",
   },
   {
     node: 'ClearCheckoutData',
