5e4ad20b601cd1cf46bb5b644c36e2c0ec54d407
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 04:35:23 -0400

    migrate ClearSupportedDeliveryModes

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -296,7 +296,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'ClearSupportedDeliveryModes',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Class CheckoutActions.ClearSupportedDeliveryModes has been removed and is no longer part of the public API. ',
+      "Class CheckoutActions.ClearSupportedDeliveryModes has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. Use 'clearCheckoutDeliveryMode' from CheckoutDeliveryModesFacade instead.",
   },
   {
     node: 'CREATE_PAYMENT_DETAILS_FAIL',
