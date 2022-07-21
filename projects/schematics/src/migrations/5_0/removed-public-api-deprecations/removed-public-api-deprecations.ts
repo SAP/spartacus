a477ce359a4367f6f9509d67e96d82bb1b4d7493
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 05:18:06 -0400

    migrate OrderTypesActions

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -482,7 +482,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'OrderTypesActions',
     importPath: '@spartacus/checkout/core',
     comment:
-      'TypeAlias CheckoutActions.OrderTypesActions has been removed and is no longer part of the public API. ',
+      "TypeAlias CheckoutActions.OrderTypesActions has been removed and is no longer part of the public API. It is no used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can use 'setOrderType' from CheckoutReplenishmentFormService.",
   },
   {
     node: 'PAYMENT_PROCESS_SUCCESS',
