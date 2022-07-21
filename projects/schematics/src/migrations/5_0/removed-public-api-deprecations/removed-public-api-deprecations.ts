fc29a8205f63637404de02a56a02b6aa43ea2195
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-07-21 16:28:54 -0400

    migrate ResetLoadPaymentTypesProcess

--- projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
+++ projects/schematics/src/migrations/5_0/removed-public-api-deprecations/removed-public-api-deprecations.ts
@@ -584,7 +584,7 @@ export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
     node: 'ResetLoadPaymentTypesProcess',
     importPath: '@spartacus/checkout/core',
     comment:
-      'Class CheckoutActions.ResetLoadPaymentTypesProcess has been removed and is no longer part of the public API. ',
+      "Class CheckoutActions.ResetLoadPaymentTypesProcess has been removed and is no longer part of the public API. It is not used anymore. Checkout library no longer uses NgRX to dispatch actions. Please take a look at Commands & Queries documentation on how we handle state https://sap.github.io/spartacus-docs/commands-and-queries. You can use the EventService to dispatch an event to invalidate the payment types by using 'CheckoutPaymentTypesQueryResetEvent'.",
   },
   {
     node: 'ResetLoadSupportedDeliveryModesProcess',
