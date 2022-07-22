0ebc167d51bd0d3e3f7962b6f3198d26a1ba5904
mechris3 <mechris3@hotmail.com>
2022-07-22 15:39:32 +0100

    feat: Adds Cypress tests and some ids for elements

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/pickup-in-store/pickup-delivery-options-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/pickup-in-store/pickup-delivery-options-spec.ts
@@ -19,9 +19,16 @@ function mockLocation(
   };
 }
 
+const PICKUP_IN_STORE_MODAL = 'cx-delivery-pickup-options-dialog';
+const PICKUP_IN_STORE_RADIO_BUTTON = '#pickup';
+const HIDE_OUT_OF_STOCK_CHECK_BOX = '#chkHideOutOfStock';
+const SEARCH_LOCATION_TEXTBOX = '#txtFindAStore';
+const FIND_STORES_BUTTON = '#btnFindStores';
+const USE_MY_LOCATION = '#lnkUseMyLocation';
+
 describe('Pickup delivery options', () => {
   viewportContext(['desktop'], () => {
-    before(() => {
+    beforeEach(() => {
       cy.window().then((win) => win.sessionStorage.clear());
       cy.cxConfig({
         context: {
@@ -32,11 +39,52 @@ describe('Pickup delivery options', () => {
       cy.visit('/product/300310300', mockLocation(53, 0));
     });
 
-    it('should open the pickup locations dialog', () => {
+    xit('should open the pickup locations dialog, and dialog should be closeable', () => {
       cy.get('cx-pickup-delivery-options').should('exist');
       cy.get('#delivery').should('have.attr', 'aria-checked', 'true');
-      cy.get('#pickup').click();
-      cy.get('cx-delivery-pickup-options-dialog').should('exist');
+      cy.get(PICKUP_IN_STORE_RADIO_BUTTON).click();
+      cy.get(PICKUP_IN_STORE_MODAL).should('exist');
+      cy.get('button.cx-dialog-close').click();
+      cy.get(PICKUP_IN_STORE_MODAL).should('not.exist');
+    });
+
+    xit('should filter out stores with no stock when "Hide out of stock options" is checked', () => {
+      cy.get('cx-pickup-delivery-options').should('exist');
+      cy.get(PICKUP_IN_STORE_RADIO_BUTTON).click();
+      cy.get(PICKUP_IN_STORE_MODAL).should('exist');
+      cy.get('cx-store').should('have.length', 20);
+      cy.get(HIDE_OUT_OF_STOCK_CHECK_BOX).click();
+      cy.get('cx-store').should('have.length', 11);
+    });
+
+    xit('uses the search term entered if Find Stores button clicked ', () => {
+      cy.get('cx-pickup-delivery-options').should('exist');
+      cy.get(PICKUP_IN_STORE_RADIO_BUTTON).click();
+      cy.get(PICKUP_IN_STORE_MODAL).should('exist');
+      cy.get(SEARCH_LOCATION_TEXTBOX).type('Maidenhead');
+      cy.intercept({
+        method: 'GET',
+        url: '/occ/v2/apparel-uk-spa/products/300310300/stock*',
+      }).as('apiSearchStores');
+      cy.get(FIND_STORES_BUTTON).click();
+      cy.wait('@apiSearchStores').then((interception) => {
+        expect(interception.request.url).contain('location=Maidenhead');
+      });
+    });
+
+    it('uses the location if "Use My Location" link clicked ', () => {
+      cy.get('cx-pickup-delivery-options').should('exist');
+      cy.get(PICKUP_IN_STORE_RADIO_BUTTON).click();
+      cy.get(PICKUP_IN_STORE_MODAL).should('exist');
+
+      cy.intercept({
+        method: 'GET',
+        url: '/occ/v2/apparel-uk-spa/products/300310300/stock*',
+      }).as('apiSearchStores');
+      cy.get(USE_MY_LOCATION).click();
+      cy.wait('@apiSearchStores').then((interception) => {
+        expect(interception.request.url).contain('latitude=53&longitude=0');
+      });
     });
   });
 });
