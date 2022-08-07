2b815da287b696a64b425cfdf4e001139984d5db
Hakwoo Kim <hakwoo.kim@sap.com>
2022-08-06 23:44:50 -0400

    add missing module

--- feature-libs/asm/components/asm-customer-360/asm-customer-profile/asm-customer-profile.module.ts
+++ feature-libs/asm/components/asm-customer-360/asm-customer-profile/asm-customer-profile.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
+import { CardModule } from '@spartacus/storefront';
 import { AsmCustomerProfileComponent } from './asm-customer-profile.component';
 
 @NgModule({
-  imports: [CommonModule, I18nModule],
+  imports: [CardModule, CommonModule, I18nModule],
   declarations: [AsmCustomerProfileComponent],
   exports: [AsmCustomerProfileComponent],
 })
