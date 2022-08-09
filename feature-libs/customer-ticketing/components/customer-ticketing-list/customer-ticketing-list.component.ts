fad05c426675e81a5a81eb4d32bd256c7d20863d
Adolfo <adolfo.pereira@sap.com>
2022-08-09 17:38:46 -0400

    Adding tickets and data to the ticketing list

--- feature-libs/customer-ticketing/components/customer-ticketing-list/customer-ticketing-list.component.ts
+++ feature-libs/customer-ticketing/components/customer-ticketing-list/customer-ticketing-list.component.ts
@@ -1,11 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-customer-ticketing-list',
   templateUrl: './customer-ticketing-list.component.html',
 })
-export class CustomerTicketingListComponent implements OnInit {
+export class CustomerTicketingListComponent {
   constructor() {}
 
-  ngOnInit() {}
+  tickets$ = of({
+    pagination: {
+      currentPage: 0,
+      pageSize: 0,
+      sort: 'string',
+      totalPages: 0,
+      totalResults: 0,
+    },
+    sorts: [
+      {
+        code: 'string',
+        name: 'string',
+        selected: true,
+      },
+    ],
+    tickets: [
+      {
+        associatedTo: {
+          code: '00001000',
+          modifiedAt: '2021-01-13T10:06:57+0000',
+          type: 'Cart',
+        },
+        availableStatusTransitions: [
+          {
+            id: 'CLOSED',
+            name: 'Closed',
+          },
+        ],
+        createdAt: '2021-01-13T10:06:57+0000',
+        customerId: 1000001,
+        id: '0000001',
+        modifiedAt: '2021-01-13T10:06:57+0000',
+        status: {
+          id: 'CLOSED',
+          name: 'Closed',
+        },
+        subject: 'My drill is broken.',
+        ticketCategory: {
+          id: 'ENQUIRY',
+          name: 'Enquiry',
+        },
+        ticketEvents: [
+          {
+            addedByAgent: false,
+            author: 'Mark Rivers',
+            createdAt: '2021-01-13T10:06:57+0000',
+            message:
+              'It is broken when I receive it. Please send one replacement to me.',
+            toStatus: {
+              id: 'CLOSED',
+              name: 'Closed',
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  tickets = this.tickets$.pipe(
+    map((tickets) => {
+      return tickets.tickets.map((ticket) => {
+        return {
+          id: ticket.id,
+          subject: ticket.subject,
+          ticketCategory: ticket.ticketCategory.name,
+          status: ticket.status.name,
+          createdAt: ticket.createdAt,
+          modifiedAt: ticket.modifiedAt,
+        };
+      });
+    })
+  );
 }
