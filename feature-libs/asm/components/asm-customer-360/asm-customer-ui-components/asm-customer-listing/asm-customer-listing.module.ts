6050f9a98862c1bc421162143a252c75135f08f9
Jeremy Laverdiere <jeremy.laverdiere@sap.com>
2022-08-10 14:13:27 -0400

    Fix search functionality for customer coupons

--- feature-libs/asm/components/asm-customer-360/asm-customer-ui-components/asm-customer-listing/asm-customer-listing.module.ts
+++ feature-libs/asm/components/asm-customer-360/asm-customer-ui-components/asm-customer-listing/asm-customer-listing.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IconModule } from '@spartacus/storefront';
 import { AsmCustomerListingComponent } from './asm-customer-listing.component';
 
 @NgModule({
-  imports: [CommonModule, IconModule],
+  imports: [CommonModule, FormsModule, IconModule],
   declarations: [AsmCustomerListingComponent],
   exports: [AsmCustomerListingComponent],
 })
