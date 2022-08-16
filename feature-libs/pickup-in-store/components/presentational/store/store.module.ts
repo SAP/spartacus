ad6a9da62c532367248badf3beaad5b9fe1e8c3b
Sam Vendittelli <sam.vendittelli@sap.com>
2022-08-16 11:09:47 +0100

    fix: remove circular dependency

--- feature-libs/pickup-in-store/components/presentational/store/store.module.ts
+++ feature-libs/pickup-in-store/components/presentational/store/store.module.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
 import { IconModule, SpinnerModule } from '@spartacus/storefront';
-import { StoreComponent, StoreScheduleComponent } from './index';
+import { StoreScheduleComponent } from './store-schedule/index';
+import { StoreComponent } from './store.component';
 
 @NgModule({
   imports: [CommonModule, I18nModule, IconModule, SpinnerModule],
