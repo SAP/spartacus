ad6e5c8a96a6ee7f47ce78326d4bc138050a2967
Chris Kuhr <chris.kuhr@sap.com>
2022-08-16 13:42:26 -0400

    update error messages

--- feature-libs/organization/account-summary/assets/translations/en/account-summary.i18n.ts
+++ feature-libs/organization/account-summary/assets/translations/en/account-summary.i18n.ts
@@ -68,8 +68,8 @@ export const accountSummary = {
       clear: 'Clear All',
       search: 'Search',
       errors: {
-        toDateMustComeAfterFrom: 'End date must be later than start',
-        toAmountMustBeLargeThanFrom: 'To amount must be larger than from',
+        toDateMustComeAfterFrom: 'Choose an end date that\'s later than the start date.',
+        toAmountMustBeLargeThanFrom: 'Choose an end range value that\'s smaller than the start value.'
       }
     },
     hint: 'Account summaries allow you to review general information about a unit, including balances and aging summary of invoices. Here, you can also browse through a list of transaction documents for a unit.',
