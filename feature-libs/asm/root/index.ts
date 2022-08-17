15c8b62c775c8632a48e708c26ddd65c6a3b4bf9
Eric Polesky <eric.polesky@sap.com>
2022-08-17 14:48:45 -0400

    remove barrel from root of asm/root module

--- feature-libs/asm/root/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './asm-constants';
-export * from './asm-loader.module';
-export * from './asm-root.module';
-export * from './feature-name';
-export * from './facade/index';
-export * from './model/index';
-export * from './services/index';
