47ef6fcb659bb93a254b4dc189d2991f1ba3cc3a
I559579 <marcin.ganczorz@sap.com>
2022-08-19 00:34:02 +0200

    add missing unit tests

--- feature-libs/organization/unit-order/core/store/actions/unit-order.action.spec.ts
+++ feature-libs/organization/unit-order/core/store/actions/unit-order.action.spec.ts
@@ -1,1 +1,74 @@
-// TODO
+import { StateUtils } from '@spartacus/core';
+import { OrderHistoryList } from '@spartacus/order/root';
+import { UNIT_ORDERS } from '../unit-order-state';
+import { UnitOrderActions } from './index';
+
+describe('OrdersActions', () => {
+  describe('LoadUserOrders Actions', () => {
+    it('should create the action', () => {
+      const payload: {
+        userId: string;
+        pageSize?: number;
+        currentPage?: number;
+        sort?: string;
+      } = {
+        userId: 'test@sap.com',
+        pageSize: 5,
+        currentPage: 1,
+        sort: 'byDate',
+      };
+
+      const action = new UnitOrderActions.LoadUnitOrders(payload);
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_UNIT_ORDERS,
+        payload,
+        meta: StateUtils.loadMeta(UNIT_ORDERS),
+      });
+    });
+  });
+
+  describe('LoadUnitOrdersFail Action', () => {
+    it('should create the action', () => {
+      const error = 'mockError';
+      const action = new UnitOrderActions.LoadUnitOrdersFail(error);
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_UNIT_ORDERS_FAIL,
+        payload: error,
+        meta: StateUtils.failMeta(UNIT_ORDERS, error),
+      });
+    });
+  });
+
+  describe('LoadUnitOrdersSuccess Action', () => {
+    it('should create the action', () => {
+      const payload: OrderHistoryList = {
+        orders: [{ code: '01' }, { code: '02' }],
+        pagination: {
+          totalPages: 13,
+        },
+        sorts: [{ selected: true }, { selected: false }],
+      };
+
+      const action = new UnitOrderActions.LoadUnitOrdersSuccess(payload);
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_UNIT_ORDERS_SUCCESS,
+        payload,
+        meta: StateUtils.successMeta(UNIT_ORDERS),
+      });
+    });
+  });
+
+  describe('ClearUnitOrders Action', () => {
+    it('should create the action', () => {
+      const action = new UnitOrderActions.ClearUnitOrders();
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.CLEAR_UNIT_ORDERS,
+        meta: StateUtils.resetMeta(UNIT_ORDERS),
+      });
+    });
+  });
+});
--- feature-libs/organization/unit-order/occ/adapters/occ-unit-order.adapter.spec.ts
+++ feature-libs/organization/unit-order/occ/adapters/occ-unit-order.adapter.spec.ts
@@ -1,1 +1,97 @@
-// TODO
+import { HttpClientModule, HttpRequest } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { ConverterService, OccEndpointsService } from '@spartacus/core';
+import { ORDER_HISTORY_NORMALIZER } from '@spartacus/order/root';
+import { MockOccEndpointsService } from 'projects/core/src/occ/adapters/user/unit-test.helper';
+import { OccUnitOrderAdapter } from './occ-unit-order.adapter';
+
+const userId = '123';
+
+describe('OccUnitOrderAdapter', () => {
+  let occOrderHistoryAdapter: OccUnitOrderAdapter;
+  let httpMock: HttpTestingController;
+  let converter: ConverterService;
+  let occEnpointsService: OccEndpointsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [
+        OccUnitOrderAdapter,
+        {
+          provide: OccEndpointsService,
+          useClass: MockOccEndpointsService,
+        },
+      ],
+    });
+
+    occOrderHistoryAdapter = TestBed.inject(OccUnitOrderAdapter);
+    httpMock = TestBed.inject(HttpTestingController);
+    converter = TestBed.inject(ConverterService);
+    occEnpointsService = TestBed.inject(OccEndpointsService);
+    spyOn(converter, 'pipeable').and.callThrough();
+    spyOn(converter, 'convert').and.callThrough();
+    spyOn(occEnpointsService, 'buildUrl').and.callThrough();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('getUnitLevelOrders', () => {
+    it('should fetch unit Orders with default options', fakeAsync(() => {
+      const PAGE_SIZE = 5;
+      occOrderHistoryAdapter
+        .loadUnitOrderHistory(userId, PAGE_SIZE)
+        .subscribe();
+      httpMock.expectOne((req: HttpRequest<void>) => {
+        return req.method === 'GET';
+      }, `GET method and url`);
+      expect(occEnpointsService.buildUrl).toHaveBeenCalledWith(
+        'unitLevelOrderHistory',
+        {
+          urlParams: { userId },
+          queryParams: { pageSize: PAGE_SIZE.toString() },
+        }
+      );
+    }));
+
+    it('should fetch unit Orders with defined options', fakeAsync(() => {
+      const PAGE_SIZE = 5;
+      const currentPage = 1;
+      const sort = 'byDate';
+
+      occOrderHistoryAdapter
+        .loadUnitOrderHistory(userId, PAGE_SIZE, currentPage, sort)
+        .subscribe();
+      httpMock.expectOne((req: HttpRequest<void>) => {
+        return req.method === 'GET';
+      }, `GET method`);
+      expect(occEnpointsService.buildUrl).toHaveBeenCalledWith(
+        'unitLevelOrderHistory',
+        {
+          urlParams: { userId },
+          queryParams: {
+            pageSize: PAGE_SIZE.toString(),
+            currentPage: currentPage.toString(),
+            sort,
+          },
+        }
+      );
+    }));
+
+    it('should use converter', () => {
+      occOrderHistoryAdapter.loadUnitOrderHistory(userId).subscribe();
+      httpMock
+        .expectOne((req: HttpRequest<any>) => {
+          return req.method === 'GET';
+        }, `GET method`)
+        .flush({});
+      expect(converter.pipeable).toHaveBeenCalledWith(ORDER_HISTORY_NORMALIZER);
+    });
+  });
+});
