6cd7d211a633855ca829fdfe9f7c8b2cc846275c
Parthlakhani <parth.lakhani@sap.com>
2022-08-18 16:21:17 -0400

    remove comments

--- feature-libs/customer-ticketing/root/events/customer-ticketing.events.ts
+++ feature-libs/customer-ticketing/root/events/customer-ticketing.events.ts
@@ -1,15 +1,9 @@
 import { CxEvent } from '@spartacus/core';
 
 export class GetTicketQueryResetEvent extends CxEvent {
-  /**
-   * Event's type
-   */
   static readonly type = 'GetTicketQueryResetEvent';
 }
 
 export class GetTicketQueryReloadEvent extends CxEvent {
-  /**
-   * Event's type
-   */
   static readonly type = 'GetTicketQueryReloadEvent';
 }
