2c736394d566f49703e59b80178dd86fa2f38681
Lee <justin.lee01@sap.com>
2022-08-18 13:20:08 -0400

    Unit test for ASMContextService

--- projects/core/src/occ/services/asm-context.service.spec.ts
+++ projects/core/src/occ/services/asm-context.service.spec.ts
@@ -1,3 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { OCC_ASM_TOKEN } from '../utils';
+import { AsmContextService } from './asm-context.service';
+
 describe('AsmContextService', () => {
-  // TODO: pending decision whether this is a good idea or not
+  let service: AsmContextService;
+
+  beforeEach(() => {
+    service = TestBed.inject(AsmContextService);
+  });
+
+  it('should create a context', () => {
+    const arg = { sendUserIdAsHeader: true };
+    const context = service.createContext(arg);
+
+    expect(context.get(OCC_ASM_TOKEN)).toBe(arg);
+  });
 });
