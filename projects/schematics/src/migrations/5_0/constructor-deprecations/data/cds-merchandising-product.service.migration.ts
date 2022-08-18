e5a9d1f7dedfb25dd1731ff272d2f5c810364446
I554698 <surya.singh@sap.com>
2022-08-18 10:28:18 +0100

    chore: Adds header using prepend-license script

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/cds-merchandising-product.service.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/cds-merchandising-product.service.migration.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import {
   CDS_MERCHANDISING_PRODUCT_SERVICE,
   MERCHANDISING_STRATEGY_CONNECTOR,
