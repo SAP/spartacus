803b85a7021e8bb46881f1b665421427076cd399
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2022-08-22 21:46:53 +0200

    Service updates

--- feature-libs/product/future-stock/core/services/future-stock.service.ts
+++ feature-libs/product/future-stock/core/services/future-stock.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { FutureStockFacade } from '@spartacus/product/future-stock/root';
+import { Query, QueryService, RoutingService, UserIdService } from '@spartacus/core';
+import { switchMap, withLatestFrom } from 'rxjs/operators';
+import { FutureStockConnector } from '../connectors';
+import { ProductFutureStock } from '../model';
 
 @Injectable()
 export class FutureStockService implements FutureStockFacade {
+  protected readonly PRODUCT_KEY = 'productCode';
+
+  protected futureStockState$: Query<ProductFutureStock, unknown[]> =
+    this.queryService.create<ProductFutureStock>(
+      () => this.routingService.getRouterState().pipe(
+        withLatestFrom(this.userIdService.takeUserId()),
+        switchMap(([{ state }, userId]) =>
+          this.futureStockConnector.getFutureStock(state.params[this.PRODUCT_KEY], userId)
+      )), {
+      }
+    );
+
   /**
    * Get future stock
    */
-  getFutureStock(): Observable<any> {
-    return of({});
+  getFutureStock(): Observable<ProductFutureStock | undefined> {
+    return this.futureStockState$.get();
   }
+
+  constructor(
+    protected userIdService: UserIdService,
+    protected queryService: QueryService,
+    protected futureStockConnector: FutureStockConnector,
+    protected routingService: RoutingService,
+  ) {}
 }
