d4e41056fa7001fed720cf4e85693a4bb4716f27
Khadim Fall <khadim.fall@sap.com>
2022-08-24 15:10:33 -0400

    added 4.3 to sanity

--- scripts/install/functions.sh
+++ scripts/install/functions.sh
@@ -521,7 +521,7 @@ function run_sanity_check {
 
 function ng_sanity_check {
     printh "Run config sanity check"
-    if [ "$BRANCH" == "release/4.0.x" ]; then
+    if [ "$BRANCH" == "release/4.0.x" ] || [ "$BRANCH" == "release/4.3.x" ]; then
         local CLEAN_VERSION=$(echo "$ANGULAR_CLI_VERSION" | sed 's/[^0-9\.]//g')
         if [ $(version $CLEAN_VERSION) -ge $(version "13.0.0") ]; then
             echo "❗️ You are trying to install a release which seems to be uncompatible with AngularCLI 13.0.0 or higher."
