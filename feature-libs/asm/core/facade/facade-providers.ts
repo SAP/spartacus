bfb4e07820dd643b3b07b8e1ebf6605861b739d4
Hakwoo Kim <hakwoo.kim@sap.com>
2022-08-24 10:10:02 -0400

    Add missing license header to sources

--- feature-libs/asm/core/facade/facade-providers.ts
+++ feature-libs/asm/core/facade/facade-providers.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Provider } from '@angular/core';
 import { AsmBindCartFacade } from '@spartacus/asm/root';
 import { AsmBindCartService } from './asm-bind-cart.service';
