a7e07bbf7cd429c19146825033ecd59e46805e20
Parthlakhani <parth.lakhani@sap.com>
2022-08-25 17:08:57 -0400

    add unit tests

--- projects/storefrontlib/shared/components/chat-messaging/avatar/avatar.component.spec.ts
+++ projects/storefrontlib/shared/components/chat-messaging/avatar/avatar.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { I18nTestingModule } from '@spartacus/core';
+import { IconModule } from 'projects/storefrontlib/cms-components';
+import { MessageEvent } from '../messaging';
 import { AvatarComponent } from './avatar.component';
+
+const mockEvent: MessageEvent = {
+  author: 'Mark Rivers',
+  rightAlign: true,
+};
 
 describe('AvatarComponent', () => {
   let component: AvatarComponent;
@@ -8,6 +16,7 @@ describe('AvatarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [I18nTestingModule, IconModule],
       declarations: [AvatarComponent],
     }).compileComponents();
   });
@@ -15,10 +24,35 @@ describe('AvatarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AvatarComponent);
     component = fixture.componentInstance;
+    component.message = mockEvent;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return initials', () => {
+    let result = component.getInitials('Mark Rivers');
+
+    expect(result).toEqual('MR');
+  });
+
+  it('should display initials if the addedByAgent is falsy', () => {
+    mockEvent.rightAlign = false;
+    component.message = mockEvent;
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('h1')).nativeElement;
+
+    expect(element.innerText).toEqual('MR');
+  });
+
+  it('should display icon if addedByAgent is truthy', () => {
+    mockEvent.rightAlign = true;
+    component.message = mockEvent;
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('cx-icon')).nativeElement;
+
+    expect(element.tagName).toEqual('CX-ICON');
   });
 });
