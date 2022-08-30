9c1374d14e31e029fc43ed675c4693df5e1f8443
Lee <justin.lee01@sap.com>
2022-08-30 16:10:46 -0400

    WIP: Refactoring AsmCustomerListing component to have logic separate from its data. Search function is missing

--- feature-libs/asm/components/asm-customer-360/asm-customer-ui-components/asm-customer-listing/asm-customer-listing.model.ts
+++ feature-libs/asm/components/asm-customer-360/asm-customer-ui-components/asm-customer-listing/asm-customer-listing.model.ts
@@ -1,41 +1,19 @@
-import { Class, Fragment } from '../../asm-customer-360.model';
-
-export abstract class ListingFragment implements Fragment {
-  abstract type: string;
-  abstract text: string;
+export interface CustomerListingTab {
+  buttonConfig?: CustomerListingButton;
+  tabName: string;
   items: Array<ListingItem>;
-  emptyText?: string;
-  buttonAction?(item: ListingItem): void;
-
-  constructor(
-    rawItems: Array<RawListingItem>,
-    itemClass: Class<ListingItem, RawListingItem>
-  ) {
-    this.items = rawItems.map((item) => new itemClass(item));
-  }
+  emptyText: string;
 }
 
-export interface Searchable extends ListingFragment {
-  searchTerm: string;
-  searchAction(term: string): void;
+export interface CustomerListingButton {
+  applyText: string;
+  appliedText?: string;
+  removeText?: string;
+  isApplied?: (item: ListingItem) => boolean;
 }
 
-export interface Tabulated extends ListingFragment {
-  currentTab: number;
-  tabs: Array<{
-    tabName: string;
-    items: Array<ListingItem>;
-    emptyText: string;
-  }>;
-  setCurrentTab(tab: number): void;
-}
-
-export type ListingFragmentType = ListingFragment & Searchable & Tabulated;
-
-export class ListingItem {
-  [key: string]: string | boolean | undefined;
-}
-
-export interface RawListingItem {
-  [key: string]: string | boolean | number | undefined;
+export interface ListingItem {
+  title: string | undefined;
+  description: string | undefined;
+  applied: boolean;
 }
