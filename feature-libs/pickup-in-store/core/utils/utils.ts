dd203cfddf51b2b148e31c6be5261413dc2408a8
Sam Vendittelli <sam.vendittelli@sap.com>
2022-09-01 15:52:17 +0100

    chore: remove unused functions

--- feature-libs/pickup-in-store/core/utils/utils.ts
+++ feature-libs/pickup-in-store/core/utils/utils.ts
@@ -17,11 +17,3 @@ export function isInStock(stockInfo: Stock | undefined): boolean {
 export function storeHasStock({ stockInfo }: PointOfServiceStock): boolean {
   return isInStock(stockInfo);
 }
-
-export const insertItemAtIndex = <T>(item: T, index: number, array: T[]) => {
-  return [...array.slice(0, index), item, ...array.slice(index)];
-};
-
-export const removeItemAtIndex = <T>(index: number, array: T[]) => {
-  return [...array.slice(0, index), ...array.slice(index + 1)];
-};
