d40b6676a304e4399848073670e13dbea149d68e
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2022-09-04 14:51:29 +0200

    Adjust Future Stock Component to use queries

--- feature-libs/product/future-stock/components/future-stock/future-stock.component.ts
+++ feature-libs/product/future-stock/components/future-stock/future-stock.component.ts
@@ -1,40 +1,32 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { FutureStock } from '@spartacus/core';
-
+import { FutureStockFacade } from '@spartacus/product/future-stock/root';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'cx-future-stock',
   templateUrl: './future-stock.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FutureStockComponent implements OnInit {
+export class FutureStockComponent implements OnInit, OnDestroy {
+  futureStocks$ = this.futureStockService.getFutureStock();
   futureStocks: FutureStock[] | string;
 
+  futureStockSubscription: Subscription;
+
+  constructor(
+    protected futureStockService: FutureStockFacade
+  ) {}
+
   ngOnInit() {
-    // TODO: replace with get stocks from API from CXSPA-418 when this ticket is merged
-    if (Math.random() > 0.5) {
-      this.futureStocks = [
-        {
-          formattedDate: '10/11/2020',
-          stock: {
-            stockLevel: 15,
-          },
-        },
-        {
-          formattedDate: '11/11/2020',
-          stock: {
-            stockLevel: 20,
-          },
-        },
-        {
-          formattedDate: '12/11/2020',
-          stock: {
-            stockLevel: 25,
-          },
-        },
-      ];
-    } else {
-      this.futureStocks =
-        'This product has no future availability information.';
+
+    if(!this.futureStocks) {
+      this.futureStockSubscription = this.futureStocks$.subscribe((futureStocks) => {
+        this.futureStocks = (futureStocks && futureStocks.length !== 0) ? futureStocks : 'This product has no future availability information.';
+      });
     }
+  }
+
+  ngOnDestroy(): void {
+      this.futureStockSubscription.unsubscribe();
   }
 }
