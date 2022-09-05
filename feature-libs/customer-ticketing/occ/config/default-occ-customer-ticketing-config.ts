2f2136723014abcfdeba7aa0f3634862b5d088b0
Adolfo <adolfo.pereira@sap.com>
2022-09-05 13:30:47 -0400

    Adding new query to occ endpoints and config

--- feature-libs/customer-ticketing/occ/config/default-occ-customer-ticketing-config.ts
+++ feature-libs/customer-ticketing/occ/config/default-occ-customer-ticketing-config.ts
@@ -5,6 +5,7 @@ export const defaultOccCustomerTicketingConfig: OccConfig = {
     occ: {
       endpoints: {
         getTicket: 'users/${customerId}/tickets/${ticketId}',
+        getTickets: 'users/${customerId}/tickets',
       },
     },
   },
--- feature-libs/customer-ticketing/occ/model/occ-customer-ticketing-endpoints.model.ts
+++ feature-libs/customer-ticketing/occ/model/occ-customer-ticketing-endpoints.model.ts
@@ -4,9 +4,14 @@ declare module '@spartacus/core' {
   interface OccEndpoints {
     /**
      * Endpoint to get ticket details by ticket id
-     *
-     * * @member {string}
+     * @member {string}
      */
     getTicket?: string | OccEndpoint;
+
+    /**
+     * Endpoint to get ticket list by customer id
+     * @member {string}
+     */
+    getTickets?: string | OccEndpoint;
   }
 }
