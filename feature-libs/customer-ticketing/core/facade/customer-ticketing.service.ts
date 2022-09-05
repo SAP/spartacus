f2b230935bcedaf0176e08a2dcd15afa0d785219
Adolfo <adolfo.pereira@sap.com>
2022-09-05 13:29:09 -0400

    Adding new queries to services

--- feature-libs/customer-ticketing/core/facade/customer-ticketing.service.ts
+++ feature-libs/customer-ticketing/core/facade/customer-ticketing.service.ts
@@ -11,7 +11,10 @@ import {
   CustomerTicketingFacade,
   GetTicketQueryReloadEvent,
   GetTicketQueryResetEvent,
+  GetTicketsQueryReloadEvents,
+  GetTicketsQueryResetEvents,
   TicketDetails,
+  TicketList,
 } from '@spartacus/customer-ticketing/root';
 import { combineLatest, Observable } from 'rxjs';
 import { distinctUntilChanged, map, switchMap, take } from 'rxjs/operators';
@@ -32,6 +35,20 @@ export class CustomerTicketingService implements CustomerTicketingFacade {
     return [GetTicketQueryResetEvent];
   }
 
+  /**
+   * Returns the reload events for the getTickets query.
+   */
+  protected getTicketsQueryReloadEvents(): QueryNotifier[] {
+    return [GetTicketsQueryReloadEvents];
+  }
+
+  /**
+   * Returns the reset events for the getTickets query.
+   */
+  protected getTicketsQueryResetEvents(): QueryNotifier[] {
+    return [GetTicketsQueryResetEvents];
+  }
+
   protected getTicketQuery$: Query<TicketDetails | undefined> =
     this.queryService.create<TicketDetails | undefined>(
       () =>
@@ -43,6 +60,20 @@ export class CustomerTicketingService implements CustomerTicketingFacade {
       {
         reloadOn: this.getTicketQueryReloadEvents(),
         resetOn: this.getTicketQueryResetEvents(),
+      }
+    );
+
+  protected getTicketsQuery$: Query<TicketList | undefined> =
+    this.queryService.create<TicketList | undefined>(
+      () =>
+        this.customerTicketingListPreConditions().pipe(
+          switchMap(([customerId]) =>
+            this.customerTicketingConnector.getTickets(customerId)
+          )
+        ),
+      {
+        reloadOn: this.getTicketsQueryReloadEvents(),
+        resetOn: this.getTicketsQueryResetEvents(),
       }
     );
 
@@ -70,11 +101,32 @@ export class CustomerTicketingService implements CustomerTicketingFacade {
       })
     );
   }
+
+  protected customerTicketingListPreConditions(): Observable<string> {
+    return this.userIdService.getUserId().pipe(
+      take(1),
+      map((userId) => {
+        if (!userId) {
+          throw new Error('Customer ticketing list pre conditions not met');
+        }
+        return userId;
+      })
+    );
+  }
+
   getTicketState(): Observable<QueryState<TicketDetails | undefined>> {
     return this.getTicketQuery$.getState();
   }
 
   getTicket(): Observable<TicketDetails | undefined> {
     return this.getTicketState().pipe(map((state) => state.data));
+  }
+
+  getTicketsState(): Observable<QueryState<TicketList | undefined>> {
+    return this.getTicketsQuery$.getState();
+  }
+
+  getTickets(): Observable<TicketList | undefined> {
+    return this.getTicketsState().pipe(map((state) => state.data));
   }
 }
