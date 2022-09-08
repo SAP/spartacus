0d0861397b9e51c1aaa6fa6acbc4230492a4f62f
Adolfo <adolfo.pereira@sap.com>
2022-09-08 10:10:11 -0400

    Adding ticket list to converter

--- feature-libs/customer-ticketing/core/connectors/converters.ts
+++ feature-libs/customer-ticketing/core/connectors/converters.ts
@@ -1,7 +1,11 @@
 import { InjectionToken } from '@angular/core';
 import { Converter } from '@spartacus/core';
-import { TicketDetails } from '@spartacus/customer-ticketing/root';
+import { TicketDetails, TicketList } from '@spartacus/customer-ticketing/root';
 
 export const CUSTOMER_TICKETING_NORMALIZER = new InjectionToken<
   Converter<any, TicketDetails>
 >('CustomerTicketingNormalizer');
+
+export const CUSTOMER_TICKETING_LIST_NORMALIZER = new InjectionToken<
+  Converter<any, TicketList>
+>('CustomerTicketingListNormalizer');
