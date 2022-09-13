85c63d95bbfb07729f1c0871a5b77c73c8334905
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-09-13 01:55:35 -0400

    automation done, but needs further testing + updating the ccv2 global script too

--- ci-scripts/deploy-ccv2.sh
+++ ci-scripts/deploy-ccv2.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -xe
+
+B2C_STORE="spartacusstore"
+B2B_STORE="b2bspastore"
 
 echo "-----"
 echo "Verify source branch exist"
@@ -13,7 +17,7 @@ fi
 echo "-----"
 echo "Verify CCv2 branch exist"
 
-IS_BRANCH=`git ls-remote --heads https://${GHT_USER}:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
+IS_BRANCH=`git ls-remote --heads https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
 
 if [ -z "$IS_BRANCH" ]; then
     echo "Error downloading $CCV2_BRANCH zip/tar. Verify branch name exist on the ccv2 repository"
@@ -54,4 +58,40 @@ echo "Build CSR"
 yarn build
 
 echo "-----"
-echo "TBD today"
+echo "Clone ccv2 repository"
+
+git clone -b spa_p4_dist_test https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/$GHT_REPO.git
+
+echo "-----"
+echo "Updating js-storefront to adhere to the ccv2 dist strucutre"
+
+cd "$GHT_REPO/js-storefront"
+rm -rf $B2C_STORE
+rm -rf $B2B_STORE
+
+mkdir -p $B2C_STORE/dist/$B2C_STORE/browser
+mkdir -p $B2C_STORE/dist/$B2C_STORE/server
+
+# only b2c for now test
+# mkdir -p $B2B_STORE/dist/$B2B_STORE/browser
+# mkdir -p $B2B_STORE/dist/$B2B_STORE/server
+
+cd -
+
+echo "-----"
+echo "Copy server and browser files to js-storefront to adhere to the ccv2 dist structure"
+
+cp -R dist/storefrontapp/ $GHT_REPO/js-storefront/$B2C_STORE/dist/$B2C_STORE/browser/
+cp -R dist/storefrontapp-server/ $GHT_REPO/js-storefront/$B2C_STORE/dist/$B2C_STORE/server/
+
+echo "-----"
+echo "Push to remote repository"
+
+cd $GHT_REPO
+
+git config --global user.email louis.pierrestiger@sap.com
+git config --global user.name team-griffin-serviceuser
+
+git add .
+git commit -m "update from source"
+git push
