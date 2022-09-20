e08cb3ec6e42d156d41676dcde7b9ddf05bdf678
i822892 <arkadiy.sukharenko@sap.com>
2022-09-20 14:33:28 -0400

    Fix lint

--- feature-libs/checkout/b2b/checkout-b2b.module.ts
+++ feature-libs/checkout/b2b/checkout-b2b.module.ts
@@ -9,7 +9,7 @@ import { HttpErrorHandler } from '@spartacus/core';
 import { CheckoutB2BComponentsModule } from '@spartacus/checkout/b2b/components';
 import { CheckoutB2BCoreModule } from '@spartacus/checkout/b2b/core';
 import { CheckoutB2BOccModule } from '@spartacus/checkout/b2b/occ';
-import { BadCostCenterRequestHandler } from "./core/http-interceptors/bad-request/bad-cost-center-request.handler";
+import { BadCostCenterRequestHandler } from './core/http-interceptors/bad-request/bad-cost-center-request.handler';
 
 @NgModule({
   imports: [
@@ -23,6 +23,6 @@ import { BadCostCenterRequestHandler } from "./core/http-interceptors/bad-reques
       useExisting: BadCostCenterRequestHandler,
       multi: true,
     },
-  ]
+  ],
 })
 export class CheckoutB2BModule {}
