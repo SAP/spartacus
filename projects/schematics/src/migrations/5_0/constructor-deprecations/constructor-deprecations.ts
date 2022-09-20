7d9b39d23b1ad8ffe299e75ffbd2802aeb34689d
Patryk Smul <psmul@divante.pl>
2022-09-20 13:51:53 +0200

    feat: Remove NgBootstrap dependency from CouponCardComponent (#16149)
    Closes: CXSPA-1250

--- projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/constructor-deprecations.ts
@@ -10,6 +10,8 @@ import { SUGGESTED_ADDRESS_DIALOG_COMPONENT_CONSTRUCTOR_MIGRATION } from './data
 import { ADDRESS_FORM_COMPONENT_CONSTRUCTOR_MIGRATION } from './data/address-form.component.migration';
 import { CLOSE_ACCOUNT_MODAL_COMPONENT_CONSTRUCTOR_MIGRATION } from './data/close-account-modal.component.migration';
 import { CLOSE_ACCOUNT_COMPONENT_CONSTRUCTOR_MIGRATION } from './data/close-account.component.migration';
+import { COUPON_CARD_COMPONENT_CONSTRUCTOR_MIGRATION } from './data/coupon-card.component.migration';
+import { COUPON_DIALOG_COMPONENT_CONSTRUCTOR_MIGRATION } from './data/coupon-dialog.component.migration';
 
 export const CONSTRUCTOR_DEPRECATIONS_DATA: ConstructorDeprecation[] = [
   ...GENERATED_CONSTRUCTOR_MIGRATIONS,
@@ -21,6 +23,8 @@ export const CONSTRUCTOR_DEPRECATIONS_DATA: ConstructorDeprecation[] = [
   ADDRESS_FORM_COMPONENT_CONSTRUCTOR_MIGRATION,
   CLOSE_ACCOUNT_COMPONENT_CONSTRUCTOR_MIGRATION,
   CLOSE_ACCOUNT_MODAL_COMPONENT_CONSTRUCTOR_MIGRATION,
+  COUPON_CARD_COMPONENT_CONSTRUCTOR_MIGRATION,
+  COUPON_DIALOG_COMPONENT_CONSTRUCTOR_MIGRATION,
 ];
 
 export function migrate(): Rule {
