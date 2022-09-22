3babdcbeed7f6b11ae07b21efd82a11b4b806c99
Zeyber <rotherham.c@gmail.com>
2022-09-22 12:09:27 +0200

    small modal cleanup

--- feature-libs/cart/bundle/components/product-details-dialog/product-details-dialog.module.ts
+++ feature-libs/cart/bundle/components/product-details-dialog/product-details-dialog.module.ts
@@ -1,34 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import {
   IconModule,
-  ItemCounterModule,
   ModalModule,
   ProductImagesModule,
   ProductIntroModule,
   ProductSummaryModule,
   ProductTabsModule,
-  PromotionsModule,
   SpinnerModule,
 } from '@spartacus/storefront';
-import { CartSharedModule } from 'feature-libs/cart/base/components/cart-shared/cart-shared.module';
 import { ProductDetailsDialogComponent } from './product-details-dialog.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    CartSharedModule,
-    RouterModule,
     SpinnerModule,
-    PromotionsModule,
     UrlModule,
     IconModule,
     I18nModule,
-    ItemCounterModule,
     ModalModule,
     ProductSummaryModule,
     ProductIntroModule,
