275c6491a90e1e9ae92738feeedef1232b7c1a03
Radhep Saba <radhep.saba@gmail.com>
2022-09-22 13:58:40 -0400

    test

--- ci-scripts/publish-sample-data.sh
+++ ci-scripts/publish-sample-data.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-SAMPLE_DATA_NEW=release/2105/next
-
-SAMPLE_DATA_OLD=release/2105/4dot4
-
 TAG_NAME=sampledata
 SAMPLE_DATA_ASSETS_FOLDER=sample-data-assets
 IS_SAMPLE_DATA_BRANCH=
