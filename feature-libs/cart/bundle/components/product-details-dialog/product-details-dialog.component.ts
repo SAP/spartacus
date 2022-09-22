21ea2189f47bc8d207861759d371d2939439dc95
Zeyber <rotherham.c@gmail.com>
2022-09-22 11:47:06 +0200

    modal changes

--- feature-libs/cart/bundle/components/product-details-dialog/product-details-dialog.component.ts
+++ feature-libs/cart/bundle/components/product-details-dialog/product-details-dialog.component.ts
@@ -1,13 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import {
-  ActiveCartFacade,
-  Cart,
-  OrderEntry,
-  PromotionLocation,
-} from '@spartacus/cart/base/root';
+import { Component } from '@angular/core';
 import { Product } from '@spartacus/core';
 import { ICON_TYPE, ModalService } from '@spartacus/storefront';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-product-details-dialog',
@@ -16,24 +9,10 @@ import { Observable } from 'rxjs';
 export class ProductDetailsDialogComponent {
   iconTypes = ICON_TYPE;
 
-  entry$: Observable<OrderEntry | undefined>;
-  cart$: Observable<Cart> = this.activeCartFacade.getActive();
-  loaded$: Observable<boolean> = this.activeCartFacade.isStable();
-  addedEntryWasMerged$: Observable<boolean>;
-  promotionLocation: PromotionLocation = PromotionLocation.ActiveCart;
   product: Product;
   select: Function;
 
-  quantity = 0;
-  modalIsOpen = false;
-
-  @ViewChild('dialog', { read: ElementRef })
-  dialog: ElementRef;
-
-  constructor(
-    protected modalService: ModalService,
-    protected activeCartFacade: ActiveCartFacade
-  ) {}
+  constructor(protected modalService: ModalService) {}
 
   selectProduct() {
     this.select();
