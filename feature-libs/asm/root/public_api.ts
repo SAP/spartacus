ee12245379197c3a0e550eef5630e2b3be340769
Eric Polesky <eric.polesky@sap.com>
2022-09-27 17:33:57 -0400

    remove index.ts in asm/root package

--- feature-libs/asm/root/public_api.ts
+++ feature-libs/asm/root/public_api.ts
@@ -4,4 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './index';
+export * from './asm-constants';
+export * from './asm-loader.module';
+export * from './asm-root.module';
+export * from './facade/index';
+export * from './feature-name';
+export * from './model/index';
+export * from './services/index';
