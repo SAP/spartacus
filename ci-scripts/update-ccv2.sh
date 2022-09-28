37dc69015fc07fca96a78852f2f3acce93383fe2
I506207 <brian.gamboc-javiniar@sap.com>
2022-09-28 14:44:44 -0400

    use proper param 'var' in function

--- ci-scripts/update-ccv2.sh
+++ ci-scripts/update-ccv2.sh
@@ -14,8 +14,8 @@ SERVER_CONFIG_PATH="projects/storefrontapp/server.ts"
 function verify_branch_exist {
     IS_BRANCH=`git ls-remote --heads $1 $2`
 
-    if [ -z "$IS_BRANCH" ]; then
-        echo "Error. Can't finding the branch $2. Verify the branch name exist"
+    if [ -z "$2" ]; then
+        echo "Error. Can't find the branch $2. Verify the branch name exist"
         exit 1
     fi
 }
