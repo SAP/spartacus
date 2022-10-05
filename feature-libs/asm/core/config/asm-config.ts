aa2d23c7b6809cf967b1dd014c85a1c40021ec65
Lee <justin.lee01@sap.com>
2022-10-05 16:47:59 -0400

    Removing implementation of core/config/asm-config.ts, instead extending it to use the implementation of root/config/asm-config.ts

--- feature-libs/asm/core/config/asm-config.ts
+++ feature-libs/asm/core/config/asm-config.ts
@@ -5,38 +5,14 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Config, OccConfig } from '@spartacus/core';
+import { Config } from '@spartacus/core';
+import { AsmConfig as AsmConfigRoot } from '@spartacus/asm/root';
 
 @Injectable({
   providedIn: 'root',
   useExisting: Config,
 })
 /**
- * @deprecated. Please use ../../root/asm-config.ts
+ * @deprecated. To extend, please use ../../root/asm-config.ts
  */
-export abstract class AsmConfig extends OccConfig {
-  asm?: {
-    agentSessionTimer?: {
-      startingDelayInSeconds?: number;
-    };
-    customerSearch?: {
-      maxResults?: number;
-    };
-    userIdHttpHeaderInterceptor?: {
-      /**
-       * To fix certain features when a customer agent is emulating a user, we must send a header called
-       * "sap-commerce-cloud-user-id" to the backend fetching certain data as the user, rather than the
-       * agent. However, the header will break instances of Commerce Cloud who do not allow requests with
-       * this header. (For example, the configuration "corsfilter.commercewebservices.allowedHeaders" will
-       * need to allow it.)
-       *
-       * Enabling this feature will send the emulated user's IDs on requests that need it as context.
-       */
-      enable?: boolean;
-    };
-  };
-}
-
-declare module '@spartacus/core' {
-  interface Config extends AsmConfig {}
-}
+export abstract class AsmConfig extends AsmConfigRoot {}
