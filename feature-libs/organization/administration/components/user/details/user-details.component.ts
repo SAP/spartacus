43634a93accd3925821397ba8a16aa678873127c
I561291 <tobias.djermester@sap.com>
2022-10-07 09:42:03 +0200

    CXSPA-1350: Add rights information to user details view

--- feature-libs/organization/administration/components/user/details/user-details.component.ts
+++ feature-libs/organization/administration/components/user/details/user-details.component.ts
@@ -5,11 +5,12 @@
  */
 
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { B2BUser } from '@spartacus/core';
+import { B2BUser, B2BUserRole, B2BUserRight } from '@spartacus/core';
 import { Observable, Subscription } from 'rxjs';
 import { startWith, switchMap } from 'rxjs/operators';
 import { ItemService } from '../../shared/item.service';
 import { UserItemService } from '../services/user-item.service';
+import { B2BUserService } from '@spartacus/organization/administration/core';
 
 @Component({
   selector: 'cx-org-user-details',
@@ -31,5 +32,17 @@ export class UserDetailsComponent {
   );
   isInEditMode$ = this.itemService.isInEditMode$;
 
-  constructor(protected itemService: ItemService<B2BUser>) {}
+  availableRoles: B2BUserRole[] = this.b2bUserService.getAllRoles();
+  availableRights: B2BUserRight[] = this.b2bUserService.getAllRights();
+
+  constructor(
+    protected itemService: ItemService<B2BUser>,
+    protected b2bUserService: B2BUserService
+  ) {}
+
+  hasRight(model: B2BUser): boolean {
+    return model.roles.some((role: B2BUserRight) =>
+      this.availableRights.includes(role)
+    );
+  }
 }
