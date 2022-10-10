edac7c8f62fc7055d9f664cd5afe595efceee73c
pawelfras <fras.pawel@yahoo.com>
2022-10-10 08:14:59 +0200

    bugfix: CXSPA-1486 Added To Cart Dialog not closing after clicking outside

--- projects/schematics/src/migrations/5_0/constructor-deprecations/data/added-to-cart-dialog.component.migration.ts
+++ projects/schematics/src/migrations/5_0/constructor-deprecations/data/added-to-cart-dialog.component.migration.ts
@@ -1,23 +1,25 @@
 import {
   ACTIVE_CART_FACADE,
   ADDED_TO_CART_DIALOG_COMPONENT,
+  ANGULAR_CORE,
+  ELEMENT_REF,
   LAUNCH_DIALOG_SERVICE,
   MODAL_SERVICE,
   ROUTING_SERVICE,
 } from '../../../../shared/constants';
 import {
+  SPARTACUS_CART_BASE_COMPONENTS,
   SPARTACUS_CART_BASE_ROOT,
   SPARTACUS_CORE,
   SPARTACUS_STOREFRONTLIB,
 } from '../../../../shared/libs-constants';
 import { ConstructorDeprecation } from '../../../../shared/utils/file-utils';
-import { SPARTACUS_CART_BASE } from './../../../../shared/libs-constants';
 
 export const ADDED_TO_CART_DIALOG_COMPONENT_CONSTRUCTOR_MIGRATION: ConstructorDeprecation =
   {
     // feature-libs/cart/base/components/added-to-cart-dialog/added-to-cart-dialog.component.ts
     class: ADDED_TO_CART_DIALOG_COMPONENT,
-    importPath: SPARTACUS_CART_BASE,
+    importPath: SPARTACUS_CART_BASE_COMPONENTS,
     deprecatedParams: [
       { className: MODAL_SERVICE, importPath: SPARTACUS_STOREFRONTLIB },
       { className: ACTIVE_CART_FACADE, importPath: SPARTACUS_CART_BASE_ROOT },
@@ -36,6 +38,10 @@ export const ADDED_TO_CART_DIALOG_COMPONENT_CONSTRUCTOR_MIGRATION: ConstructorDe
       {
         className: ROUTING_SERVICE,
         importPath: SPARTACUS_CORE,
+      },
+      {
+        className: ELEMENT_REF,
+        importPath: ANGULAR_CORE,
       },
     ],
   };
