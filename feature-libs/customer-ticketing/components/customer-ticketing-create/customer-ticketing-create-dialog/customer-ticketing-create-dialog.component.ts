bb4f3a0f7c834c710447616e2ac0f83eaa891549
Manpreet Kaur <manpreet.kaur03@sap.com>
2022-10-12 12:56:26 -0400

    Address Review comments

--- feature-libs/customer-ticketing/components/customer-ticketing-create/customer-ticketing-create-dialog/customer-ticketing-create-dialog.component.ts
+++ feature-libs/customer-ticketing/components/customer-ticketing-create/customer-ticketing-create-dialog/customer-ticketing-create-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AssociatedObject, Category } from '@spartacus/customer-ticketing/root';
 import { FormUtils } from '@spartacus/storefront';
+import { Observable } from 'rxjs';
 import { CustomerTicketingDialogComponent } from '../../shared/customer-ticketing-dialog/customer-ticketing-dialog.component';
 @Component({
   selector: 'cx-customer-ticketing-create-dialog',
@@ -10,20 +11,12 @@ export class CustomerTicketingCreateDialogComponent
   extends CustomerTicketingDialogComponent
   implements OnInit
 {
-  ticketCategories: Array<Category>;
-  ticketAssociatedObjects: Array<AssociatedObject>;
+  ticketCategories$: Observable<Category[]> =
+    this.customerTicketingFacade.getTicketCategories();
+  ticketAssociatedObjects$: Observable<AssociatedObject[]> =
+    this.customerTicketingFacade.getTicketAssociatedObjects();
 
   ngOnInit(): void {
-    this.customerTicketingFacade
-      .getTicketCategories()
-      .subscribe((categories) => {
-        this.ticketCategories = categories;
-      });
-    this.customerTicketingFacade
-      .getTicketAssociatedObjects()
-      .subscribe((associatedObjects) => {
-        this.ticketAssociatedObjects = associatedObjects;
-      });
     this.buildForm();
   }
 
