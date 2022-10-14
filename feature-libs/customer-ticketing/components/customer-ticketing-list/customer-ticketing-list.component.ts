a70e9d79c12a30447654f5e6055c2b863e8f5710
Adolfo <adolfo.pereira@sap.com>
2022-10-14 10:55:58 -0400

    Adding fetch list when sorting and pagination

--- feature-libs/customer-ticketing/components/customer-ticketing-list/customer-ticketing-list.component.ts
+++ feature-libs/customer-ticketing/components/customer-ticketing-list/customer-ticketing-list.component.ts
@@ -20,108 +20,15 @@ export class CustomerTicketingListComponent {
     protected routing: RoutingService,
     protected translation: TranslationService
   ) {}
+  PAGE_SIZE = 5;
   sortType: string = 'byId';
   iconTypes = ICON_TYPE;
   customerTicketsFlag: boolean = true;
   tickets$: Observable<TicketList | undefined> =
-    this.customerTicketingFacade.getTickets();
+    this.customerTicketingFacade.getTickets(this.PAGE_SIZE);
   tickets: Observable<Array<TicketDetails>> = this.tickets$?.pipe(
     map((ticketList) => ticketList?.tickets)
   );
-
-  // tickets$ = of({
-  //   pagination: {
-  //     currentPage: 0,
-  //     pageSize: 5,
-  //     sort: 'byId',
-  //     totalPages: 2,
-  //     totalResults: 10,
-  //   },
-  //   sorts: [
-  //     { code: 'byId', selected: true },
-  //     { code: 'byChangedDate', selected: false },
-  //   ],
-  //   tickets: [
-  //     {
-  //       availableStatusTransitions: [
-  //         {
-  //           id: 'CLOSED',
-  //           name: 'Closed',
-  //         },
-  //       ],
-  //       id: '0000001',
-  //       createdAt: '2021-01-13T10:06:57+0000',
-  //       modifiedAt: '2021-01-13T10:06:57+0000',
-  //       status: {
-  //         id: 'CLOSED',
-  //         name: 'Closed',
-  //       },
-  //       subject: 'My drill is broken.',
-  //       ticketCategory: {
-  //         id: 'ENQUIRY',
-  //         name: 'Enquiry',
-  //       },
-  //       ticketEvents: [
-  //         {
-  //           author: 'Mark Rivers',
-  //           createdAt: '2021-01-13T10:06:57+0000',
-  //           message:
-  //             'It is broken when I receive it. Please send one replacement to me.',
-  //           toStatus: {
-  //             id: 'CLOSED',
-  //             name: 'Closed',
-  //           },
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       availableStatusTransitions: [
-  //         {
-  //           id: 'CLOSED',
-  //           name: 'Closed',
-  //         },
-  //       ],
-  //       id: '0000002',
-  //       createdAt: '2021-01-14T10:06:57+0000',
-  //       modifiedAt: '2021-01-14T10:06:57+0000',
-  //       status: {
-  //         id: 'OPEN',
-  //         name: 'Open',
-  //       },
-  //       subject: 'Need fix for my door',
-  //       ticketCategory: {
-  //         id: 'ENQUIRY',
-  //         name: 'Enquiry',
-  //       },
-  //       ticketEvents: [
-  //         {
-  //           author: 'Bob',
-  //           createdAt: '2021-01-14T10:06:57+0000',
-  //           message: 'Door received broken',
-  //           toStatus: {
-  //             id: 'OPEN',
-  //             name: 'Open',
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // });
-
-  // tickets = this.tickets$.pipe(
-  //   map((ticketsList) => {
-  //     return ticketsList.tickets.map((ticket) => {
-  //       return {
-  //         id: ticket.id,
-  //         subject: ticket.subject,
-  //         ticketCategory: ticket.ticketCategory.name,
-  //         status: ticket.status,
-  //         createdAt: ticket.createdAt,
-  //         modifiedAt: ticket.modifiedAt,
-  //       };
-  //     });
-  //   })
-  // );
 
   goToTicketDetail(ticketId: string): void {
     this.routing.go({
@@ -150,18 +57,32 @@ export class CustomerTicketingListComponent {
   }
 
   pageChange(page: number): void {
-    this.createTicketListEvent(this.sortType, page);
-    // this.fetchTicketList(event);
+    const ticketListParams = this.createTicketListEvent(this.sortType, page);
+    this.fetchTicketList(ticketListParams);
   }
 
   createTicketListEvent(
     sortCode: string,
     currentPage: number
-  ): { sortCode: string; currentPage: number } {
+  ): { currentPage: number; sortCode: string } {
     return {
-      sortCode: sortCode,
       currentPage: currentPage,
+      sortCode: sortCode,
     };
+  }
+
+  private fetchTicketList(ticketListParams: {
+    sortCode: string;
+    currentPage: number;
+  }): void {
+    this.tickets$ = this.customerTicketingFacade.getTickets(
+      this.PAGE_SIZE,
+      ticketListParams.currentPage,
+      ticketListParams.sortCode
+    );
+    this.tickets = this.tickets$?.pipe(
+      map((ticketList) => ticketList?.tickets)
+    );
   }
 
   getStatusClass = (status: string): string => {
