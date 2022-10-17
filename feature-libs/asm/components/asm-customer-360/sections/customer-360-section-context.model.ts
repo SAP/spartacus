738fc5dabb88748bba0e920835cccabd95a08500
Lee <justin.lee01@sap.com>
2022-10-17 11:32:36 -0400

    Using the SectionContext to pass navigate events upward to the Customer 360 component so the modal can be closed properly

--- feature-libs/asm/components/asm-customer-360/sections/customer-360-section-context.model.ts
+++ feature-libs/asm/components/asm-customer-360/sections/customer-360-section-context.model.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Customer360SectionConfig } from '@spartacus/asm/root';
-import { User } from '@spartacus/core';
-import { Observable } from 'rxjs';
+import { UrlCommand, User } from '@spartacus/core';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable()
 export abstract class Customer360SectionContext<Data> {
   readonly customer$: Observable<User>;
 
   readonly config$: Observable<Customer360SectionConfig>;
+
+  readonly navigate$: Observer<UrlCommand>;
 
   data$: Observable<Data>;
 }
