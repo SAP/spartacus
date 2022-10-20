a0f1042fc0213221c159e48f555b4a076d141c9a
Dalvir-Personal <40722374+Dalvir-Personal@users.noreply.github.com>
2022-10-20 15:52:48 -0400

    Updated test case

--- projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.spec.ts
+++ projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.spec.ts
@@ -1,34 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { BaseSite } from '../../../model/misc.model';
-import { JavaRegExpConverter } from '../../../util/java-reg-exp-converter/java-reg-exp-converter';
-import { WindowRef } from '../../../window/window-ref';
 import { BaseSiteService } from '../../../site-context/facade/base-site.service';
 import { SecurePortalConfigInitializer } from './secure-portal-config-initializer';
+import { SiteContextConfig } from '../../../site-context/config/site-context-config';
+import { ConfigInitializerService } from '../../../config';
 
-class MockWindowRef implements Partial<WindowRef> {
-  location = {
-    href: 'testUrl',
-  };
-}
-
-const mockBaseStore = {
-  languages: [{ isocode: 'de' }, { isocode: 'en' }],
-  defaultLanguage: { isocode: 'en' },
-  currencies: [{ isocode: 'EUR' }, { isocode: 'USD' }],
-  defaultCurrency: { isocode: 'EUR' },
-};
 
 const mockBaseSites = [
   {
     uid: 'test',
     urlPatterns: [''],
-    baseStore: mockBaseStore,
     urlEncodingAttributes: ['language', 'currency'],
     theme: 'test-theme',
     requiresAuthentication: true,
   },
 ];
+
+const mockSiteContextConfig: SiteContextConfig = {
+  context: {
+    baseSite: ['test'],
+  },
+};
+
+class MockConfigInitializerService
+  implements Partial<ConfigInitializerService>
+{
+  getStable = () => of(mockSiteContextConfig);
+}
 
 class MockBaseSiteService {
   getAll(): Observable<BaseSite> {
@@ -43,15 +42,11 @@ describe(`SecurePortalConfigInitializer`, () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        SecurePortalConfigInitializer,
         { provide: BaseSiteService, useClass: MockBaseSiteService },
-        { provide: WindowRef, useClass: MockWindowRef },
         {
-          provide: JavaRegExpConverter,
-          useValue: {
-            toJsRegExp: jasmine
-              .createSpy()
-              .and.callFake((x: any) => new RegExp(x)),
-          },
+          provide: ConfigInitializerService,
+          useClass: MockConfigInitializerService,
         },
       ],
     });
@@ -60,7 +55,7 @@ describe(`SecurePortalConfigInitializer`, () => {
     baseSiteService = TestBed.inject(BaseSiteService);
   });
 
-  describe(`resolveConfig - context was not already configured statically`, () => {
+  describe(`resolveConfig`, () => {
     it(`should throw error when the base sites loaded are undefined`, async () => {
       spyOn(baseSiteService, 'getAll').and.returnValue(of(undefined));
       let message = false;
@@ -83,9 +78,8 @@ describe(`SecurePortalConfigInitializer`, () => {
       expect(message).toBeTruthy();
     });
 
-    it(`should throw error when no url pattern of any base site matches the current url`, async () => {
-      initializer['isCurrentBaseSite'] = () => false;
-      spyOn(baseSiteService, 'getAll').and.returnValue(of(mockBaseSites));
+    it(`should throw error when the basesite is not found`, async () => {
+      spyOn(baseSiteService, 'getAll').and.returnValue(of([{ uid: 'test1'}]));
 
       let message = false;
       try {
@@ -97,7 +91,6 @@ describe(`SecurePortalConfigInitializer`, () => {
     });
 
     it(`should return routing config based on BaseSite.requiresAuthentication value`, async () => {
-      initializer['isCurrentBaseSite'] = () => true;
       spyOn(baseSiteService, 'getAll').and.returnValue(of(mockBaseSites));
 
       const result = await initializer.configFactory();
