91743ee53e01b1a3208fb022531b266c7dc4c149
Krzysztof Platis <platonn.git@gmail.com>
2022-10-22 00:19:53 +0200

    add ssr errors handler that embeds errors informations into the html

---
+++ core-libs/setup/recipes/server/error-handler/ssr-http-errors.interceptor.ts
@@ -0,0 +1,38 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { WindowRef } from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class SsrHttpErrorsInterceptor implements HttpInterceptor {
+  constructor(protected windowRef: WindowRef) {}
+
+  public readonly errors: any[] = [];
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // SPIKE TODO - UNCOMMENT IT:
+    // if (this.windowRef.isBrowser()) {
+    //   return next.handle(request);
+    // }
+
+    return next.handle(request).pipe(
+      catchError((error) => {
+        this.collectError(error);
+        throw error;
+      })
+    );
+  }
+
+  protected collectError(error: any) {
+    this.errors.push(error);
+  }
+}
--- core-libs/setup/recipes/server/spartacus-server.module.ts
+++ core-libs/setup/recipes/server/spartacus-server.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SsrBackendRequestTimeoutModule } from './backend-request-timeout/ssr-backend-request-timeout.module';
+import { SsrErrorHandlerModule } from './error-handler/ssr-error-handler.module';
 
 // SPIKE TODO: move everything related to this module to a different package than /recipes
 
 @NgModule({
-  imports: [SsrBackendRequestTimeoutModule],
+  imports: [SsrErrorHandlerModule, SsrBackendRequestTimeoutModule],
 })
 export class SpartacusServerModule {}
