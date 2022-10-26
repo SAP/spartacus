f5646cb3a9836ab9880d854f3be53e52d407e227
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-10-26 12:01:23 -0400

    chore: Running tests on CCv2 that are running unreleased Spartacus versions (#16372)
    closes https://jira.tools.sap/browse/CXSPA-1603

---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/cx_ccv2/regression/b2c/checkout-flow.e2e-spec.ts
@@ -0,0 +1,29 @@
+import * as checkout from '../../../../helpers/checkout-flow';
+import { viewportContext } from '../../../../helpers/viewport-context';
+import { getSampleUser } from '../../../../sample-data/checkout-flow';
+
+context('Checkout flow', () => {
+  viewportContext(['desktop'], () => {
+    beforeEach(() => {
+      cy.window().then((win) => {
+        win.sessionStorage.clear();
+      });
+    });
+
+    it('should perform checkout', () => {
+      const user = getSampleUser();
+      checkout.visitHomePage();
+
+      checkout.clickHamburger();
+
+      checkout.registerUser(false, user);
+      checkout.goToCheapProductDetailsPage();
+      checkout.addCheapProductToCartAndLogin(user);
+      checkout.fillAddressFormWithCheapProduct(user);
+      checkout.verifyDeliveryMethod();
+      checkout.fillPaymentFormWithCheapProduct(user);
+      checkout.placeOrderWithCheapProduct(user);
+      checkout.verifyOrderConfirmationPageWithCheapProduct(user);
+    });
+  });
+});
