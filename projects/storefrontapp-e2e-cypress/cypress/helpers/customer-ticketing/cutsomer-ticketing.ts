5712420e10f32ff5f160af7b05cb6d09869a20fc
i540872 <sushant.shrestha@sap.com>
2022-11-02 08:14:17 -0700

    Adding failure paths for create new ticket

--- projects/storefrontapp-e2e-cypress/cypress/helpers/customer-ticketing/cutsomer-ticketing.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/customer-ticketing/cutsomer-ticketing.ts
@@ -6,6 +6,20 @@ const CUSTOMER_SUPPORT_MENU_OPTION_INDEX = 14;
 const FIRST_TICKET_COLUMN_INDEX = 1;
 const FIRST_TICKET_ROW_INDEX = 1;
 
+export enum TestCategory {
+  enquiry = "Enquiry",
+  complaint = "Complaint",
+  problem = "Problem",
+}
+
+export interface TestTicketDetails {
+  subject: string;
+  message: string;
+  category: TestCategory;
+  associatedTo?: string;
+  filename?: string;
+}
+
 export function loginRegisteredUser() {
   standardUser.registrationData.email = generateMail(randomString(), true);
   cy.requireLoggedIn(standardUser);
@@ -39,4 +53,174 @@ export function clickFirstTicketFromTicketListing(){
 export function verifyTicketDetailsPageVisit(){
   cy.url().should('match',/http:\/\/.+\/my\-account\/support\-ticket\/[0-9]+/);
   cy.get('cx-customer-ticketing-messages').should('exist');
+}
+
+
+export function visitElectronicSupportTicketPage() {
+  cy.intercept('/my-account/support-tickets').as('getElectronicSupportTicketPage');
+  cy.visit('/my-account/support-tickets');
+  cy.wait(`@getElectronicSupportTicketPage`).its('response.statusCode').should('eq', 200);
+}
+
+
+export function openCreateTicketModal() {
+  cy.get('cx-customer-ticketing-list').should('exist');
+  cy.get('cx-customer-ticketing-create button').click();
+  cy.get('cx-customer-ticketing-create-dialog').should('exist');
+}
+
+export function fillTicketDetails(ticketDetails: TestTicketDetails){
+
+  // subject
+  cy.get('textarea').first().type(ticketDetails.subject);
+
+  // category dropdown
+  cy.contains('.cx-customer-ticket-label', 'Category').get('select').eq(2).select(ticketDetails.category); // TODO
+
+  // AssociatedTo dropdown
+  // cy.contains('.cx-customer-ticket-label', 'Associated To').get('select').eq(3).select('');
+
+  // Message
+  cy.get('textarea').last().type(ticketDetails.message);
+
+  // validation
+  cy.get('cx-form-errors').should('not.be.visible');
+}
+
+export function addFile(filename: string){
+  cy.get('cx-file-upload input[type="file"]').attachFile({filePath: '../helpers/customer-ticketing/files-to-upload/' + filename});
+  cy.get('p').contains(filename);
+}
+
+export function verifyFileAttachedToMessage(filename: string){
+  cy.get('cx-messaging').contains(filename);
+}
+
+export function clickSubmit(){
+  cy.contains('[type="button"]', 'Submit').click();
+}
+
+export function clickSubmitAndVerifyRequestCompleted(){
+  cy.contains('[type="button"]', 'Submit').click();
+  cy.get('cx-global-message').contains('Request Created');
+}
+
+export function clickCancel(){
+  cy.contains('[type="button"]', 'Cancel').click();
+}
+
+export function clickClose(){
+  cy.get('button[aria-label="Close"]').click();
+}
+
+export function verifyFileToLartErrorShown(){
+  cy.get('cx-form-errors').should('be.visible');
+  cy.get('cx-form-errors').get('p').contains('File size should not exceed 10 MB');
+
+}
+
+export function verifyCreatedTicketDetails(ticketDetails: TestTicketDetails) {
+   const row = cy.get('cx-customer-ticketing-list').find('tbody').get('tr').eq(1);
+   row.get('td').eq(1).contains(ticketDetails.subject);
+   row.get('td').eq(2).contains(ticketDetails.category);
+   row.get('td').eq(5).contains("Open");
+
+   row.click();
+   cy.wait(1000);
+   cy.get('cx-messaging').contains(ticketDetails.message);
+}
+
+
+export function isFieldValidationErrorShown(){
+  cy.get('cx-form-errors').should('be.visible');
+}
+
+
+export function verifyTicketDoesNotExist() {
+  cy.get('cx-customer-ticketing-list').find('h3').contains("You don't have any request");
+
+}
+
+export function visitApparelUKSupportPage(){
+  cy.intercept('/apparel-uk-spa/en/GBP/my-account/support-tickets').as('getApparelUKSupportPage');
+  cy.visit('/apparel-uk-spa/en/GBP/my-account/support-tickets');
+  cy.wait(`@getApparelUKSupportPage`).its('response.statusCode').should('eq', 200);
+}
+
+export function verifyCreateTicketPopupIsClosed(){
+  cy.get('cx-customer-ticketing-create-dialog').should('not.exist');
+}
+
+export function getTicketDetailsFromTicketListView(){
+  let ticketDetails = [];
+  const row = cy.get('cx-customer-ticketing-list').find('tbody').get('tr').eq(1);
+  row.get('td').eq(0).invoke('text').then((x) => ticketDetails.push(x));
+  row.get('td').eq(1).invoke('text').then((x) => ticketDetails.push(x));
+  row.get('td').eq(5).invoke('text').then((x) => ticketDetails.push(x));
+
+   return ticketDetails;
+}
+
+export function verifyTicketDetails(ticketDetails){
+  //assert title when available
+  cy.get(':nth-child(1) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains(ticketDetails[1]);
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains(ticketDetails[2]);
+}
+
+export function createNewTicket(){
+  const testTicketDetails: TestTicketDetails = {
+    subject: 'Entering a subject',
+    message: 'Typing a message',
+    category: TestCategory.complaint,
+  };
+
+  loginRegisteredUser();
+  visitElectronicSupportTicketPage();
+  openCreateTicketModal();
+  fillTicketDetails(testTicketDetails);
+  clickSubmitAndVerifyRequestCompleted();
+  verifyCreatedTicketDetails(testTicketDetails);
+}
+
+export function clickCloseRequestButton(){
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains("Open");
+  cy.get('.btn.btn-block.btn-action"').click();
+  cy.get('.cx-customer-ticket-form-body').should('exist');
+  cy.get('label > .form-control').last().type("Closing Ticket");
+  cy.get('.cx-customer-ticket-form-footer > .btn-primary').click();
+}
+
+export function verifyStatusOnListingPage(status){
+  const row = cy.get('cx-customer-ticketing-list').find('tbody').get('tr').eq(1);
+  row.get('td').eq(5).contains(status);
+}
+
+export function verifyMessageBoxExists(doesExist){
+  if(doesExist){
+    cy.get('.form-control').should('exist');
+  } else {
+    cy.get('.form-control').should('not.exist');
+  }
+}
+
+export function clickReopenRequestButton(){
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains("Closed");
+  cy.get('.btn.btn-block.btn-action"').click();
+  cy.get('.cx-customer-ticket-form-body').should('exist');
+  cy.get('label > .form-control').last().type("Reopening Ticket");
+  cy.get('.cx-customer-ticket-form-footer > .btn-primary').click();
+}
+
+export function verifyMessagesAreBeingPopulated(){
+  cy.get('.cx-message-left-align-text').should('exist');
+}
+
+export function addFilesInChatBox(){
+  cy.get('.cx-message-footer-text').attachFile({filePath: '../helpers/customer-ticketing/files-to-upload/test.docx'});
+  cy.get('p').contains("test.docx");
+}
+
+export function sendMessageAsUser(){
+  cy.get('.form-control').type("Update");
+  cy.get('.cx-message-input > .btn').click();
 }
---
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/customer-ticketing/create-ticket.e2e-spec.ts
@@ -0,0 +1,189 @@
+import { viewportContext } from "../../../helpers/viewport-context";
+import { loginRegisteredUser } from "../../../helpers/cart";
+import * as customerTicketing from '../../../helpers/customer-ticketing/cutsomer-ticketing';
+import {TestTicketDetails, TestCategory} from '../../../helpers/customer-ticketing/cutsomer-ticketing';
+
+
+
+describe('ticketing', () => {
+  viewportContext(['desktop', 'mobile'], () => {
+    context('Registered User', () => {
+      before(() => {
+        cy.window().then((win) => {
+          win.sessionStorage.clear();
+        });
+      });
+
+      it('clicking add should open create new ticket popup', () => {
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+      });
+
+      it('should be able to create ticket when filling the required form', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Entering a subject',
+          message: 'Typing a message',
+          category: TestCategory.complaint,
+        };
+
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickSubmitAndVerifyRequestCompleted();
+        customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
+      });
+
+      it('should be able to create a ticket with an attachment', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Testing uploading attachment',
+          message: 'Has the file been uploaded',
+          category: TestCategory.complaint,
+          filename: 'test.docx',
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.addFile(testTicketDetails.filename);
+        customerTicketing.clickSubmitAndVerifyRequestCompleted();
+        customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
+        customerTicketing.verifyFileAttachedToMessage(testTicketDetails.filename);
+      });
+
+      it('should not be able to create a ticket with an attachment larger than 10mb', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Testing uploading attachment',
+          message: 'Has the file been uploaded',
+          category: TestCategory.complaint,
+          filename: 'largeFile.txt',
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.addFile(testTicketDetails.filename);
+        customerTicketing.clickSubmit();
+        customerTicketing.verifyTicketDoesNotExist();
+      });
+
+      it('should not be able to create a ticket with an attachment that has an unsupported extension', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Testing uploading attachment',
+          message: 'Has the file been uploaded',
+          category: TestCategory.complaint,
+          filename: 'fileNotSupported.xls',
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.addFile(testTicketDetails.filename);
+        customerTicketing.clickSubmit();
+        customerTicketing.verifyTicketDoesNotExist(); // TODO check if any error is shown
+      });
+
+      it('should not be able to create a ticket with an empty attachment', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Testing uploading attachment',
+          message: 'Has the file been uploaded',
+          category: TestCategory.complaint,
+          filename: 'emptyFile.txt',
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.addFile(testTicketDetails.filename);
+         // TODO throws exception that isn't caught and handled
+      });
+
+      it('should not be able to see created ticket in other stores', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Entering a subject',
+          message: 'Typing a message',
+          category: TestCategory.complaint,
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickSubmitAndVerifyRequestCompleted();
+        customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
+        customerTicketing.visitApparelUKSupportPage();
+        customerTicketing.verifyTicketDoesNotExist();
+      });
+
+      it('should not allow ticket to be created if form not properly completed', () => {
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.clickSubmit();
+        customerTicketing.isFieldValidationErrorShown();
+      });
+
+      it('should not create tickets when cancelling or closing the modal', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Cancelling a ticketing creating',
+          message: 'Cancelled',
+          category: TestCategory.complaint,
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickCancel();
+        customerTicketing.verifyTicketDoesNotExist();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickClose();
+        customerTicketing.verifyTicketDoesNotExist();
+      });
+
+      it('should not create ticket if subject exceeds 255 character limit', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Exceeding character limit                                                                                                                                                                                                                                       <--Space',
+          message: 'Exceeding character limit',
+          category: TestCategory.complaint,
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickSubmit();
+        customerTicketing.isFieldValidationErrorShown();
+      });
+
+      it('should not create ticket if message exceeds 5000 character limit', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Exceeding character limit',
+          message: 'Exceeding character limit space-->                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               <--space',
+          category: TestCategory.complaint,
+        };
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickSubmit();
+        customerTicketing.isFieldValidationErrorShown();
+      });
+
+      it('should close create ticket popup upon submit', () => {
+        const testTicketDetails: TestTicketDetails = {
+          subject: 'Entering a subject',
+          message: 'Typing a message',
+          category: TestCategory.complaint,
+        };
+
+        loginRegisteredUser();
+        customerTicketing.visitElectronicSupportTicketPage();
+        customerTicketing.openCreateTicketModal();
+        customerTicketing.fillTicketDetails(testTicketDetails);
+        customerTicketing.clickSubmit();
+        customerTicketing.verifyCreateTicketPopupIsClosed();
+      });
+
+    });
+  });
+});
