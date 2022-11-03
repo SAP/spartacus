2c2c2d48612c3d718ce08d25d1ae46601527da8b
i540872 <sushant.shrestha@sap.com>
2022-11-03 11:56:13 -0700

    Adding failure path for Ticket Detailing

--- projects/storefrontapp-e2e-cypress/cypress/helpers/customer-ticketing/customer-ticketing.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/customer-ticketing/customer-ticketing.ts
@@ -149,3 +149,77 @@ export function verifyTicketDoesNotExist(ticketDetails: TestTicketDetails) {
 export function visitApparelUKTicketListingPage(){
   visitPage('apparel-uk-spa/en/GBP/my-account/support-tickets', 'apparelTicketListingPage');
 }
+
+export function getTicketDetailsFromTicketListView(){
+  let ticketDetails = [];
+  const row = cy.get('cx-customer-ticketing-list').find('tbody').get('tr').eq(1);
+  row.get('td').eq(0).invoke('text').then((x) => ticketDetails.push(x));
+  row.get('td').eq(1).invoke('text').then((x) => ticketDetails.push(x));
+  row.get('td').eq(5).invoke('text').then((x) => ticketDetails.push(x));
+
+   return ticketDetails;
+}
+
+export function verifyTicketDetails(ticketDetails){
+  //assert title when available
+  cy.get(':nth-child(1) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains(ticketDetails[1]);
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains(ticketDetails[2]);
+}
+
+export function createNewTicket(){
+  const testTicketDetails: TestTicketDetails = {
+    subject: 'Entering a subject',
+    message: 'Typing a message',
+    category: TestCategory.complaint,
+  };
+
+  loginRegisteredUser();
+  visitElectronicSupportTicketPage();
+  openCreateTicketModal();
+  fillTicketDetails(testTicketDetails);
+  clickSubmitAndVerifyRequestCompleted();
+  verifyCreatedTicketDetails(testTicketDetails);
+}
+
+export function clickCloseRequestButton(){
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains("Open");
+  cy.get('.btn.btn-block.btn-action"').click();
+  cy.get('.cx-customer-ticket-form-body').should('exist');
+  cy.get('label > .form-control').last().type("Closing Ticket");
+  cy.get('.cx-customer-ticket-form-footer > .btn-primary').click();
+}
+
+export function verifyStatusOnListingPage(status){
+  const row = cy.get('cx-customer-ticketing-list').find('tbody').get('tr').eq(1);
+  row.get('td').eq(5).contains(status);
+}
+
+export function verifyMessageBoxExists(doesExist){
+  if(doesExist){
+    cy.get('.form-control').should('exist');
+  } else {
+    cy.get('.form-control').should('not.exist');
+  }
+}
+
+export function clickReopenRequestButton(){
+  cy.get(':nth-child(4) > cx-card > .cx-card > .card-body > .cx-card-container > .cx-card-label-container > :nth-child(1) > .cx-card-label').contains("Closed");
+  cy.get('.btn.btn-block.btn-action"').click();
+  cy.get('.cx-customer-ticket-form-body').should('exist');
+  cy.get('label > .form-control').last().type("Reopening Ticket");
+  cy.get('.cx-customer-ticket-form-footer > .btn-primary').click();
+}
+
+export function verifyMessagesAreBeingPopulated(){
+  cy.get('.cx-message-left-align-text').should('exist');
+}
+
+export function addFilesInChatBox(){
+  cy.get('.cx-message-footer-text').attachFile({filePath: '../helpers/customer-ticketing/files-to-upload/test.docx'});
+  cy.get('p').contains("test.docx");
+}
+
+export function sendMessageAsUser(){
+  cy.get('.form-control').type("Update");
+  cy.get('.cx-message-input > .btn').click();
+}
--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/customer-ticketing/view-ticket-details.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/customer-ticketing/view-ticket-details.e2e-spec.ts
@@ -9,6 +9,7 @@ describe('ticketing', () => {
           win.sessionStorage.clear();
         });
       });
+
       it('should be able to view ticket details page for an existing ticket', () => {
         customerTicketing.loginRegisteredUser();
         customerTicketing.clickMyAccountMenuOption();
@@ -16,6 +17,74 @@ describe('ticketing', () => {
         customerTicketing.verifyTicketListingPageVisit();
         customerTicketing.clickFirstTicketFromTicketListing();
         customerTicketing.verifyTicketDetailsPageVisit();
+      });
+
+      it('clicking a ticket should open its corresponding ticket details', () => {
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        let ticketDetails = customerTicketing.getTicketDetailsFromTicketListView();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetails(ticketDetails);
+      });
+
+      it('closing a ticket should not let user interact with the ticket anymore', () => {
+
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetailsPageVisit();
+        customerTicketing.clickCloseRequestButton();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.verifyStatusOnListingPage(" Closed ");
+        customerTicketing.verifyMessageBoxExists(false);
+      });
+
+      it('reopening a ticket should let user make more interaction with the ticket', () => {
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetailsPageVisit();
+        customerTicketing.clickReopenRequestButton();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.verifyStatusOnListingPage(" Open ");
+        customerTicketing.verifyMessageBoxExists(true);
+      });
+
+      it('ticket should always have atleast one message in it', () => {
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetailsPageVisit();
+        customerTicketing.verifyMessagesAreBeingPopulated();
+      });
+
+      it('pressing send should publish message without attachment', () => {
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetailsPageVisit();
+        customerTicketing.sendMessageAsUser();
+      });
+
+      it('pressing send should publish message with attachment', () => {
+        customerTicketing.loginRegisteredUser();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
+        customerTicketing.clickFirstTicketFromTicketListing();
+        customerTicketing.verifyTicketDetailsPageVisit();
+        customerTicketing.addFilesInChatBox();
+        customerTicketing.sendMessageAsUser();
       });
     });
   });
