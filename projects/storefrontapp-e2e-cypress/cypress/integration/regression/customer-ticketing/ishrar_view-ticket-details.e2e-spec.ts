eff2d9475bf7e6ca20b4f4eae13fe327627aab39
Ishrar Hussain <ishrar.hussain@sap.com>
2022-11-07 09:49:59 -0500

    Updates to view ticket details e2e tests as per feedback

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/customer-ticketing/ishrar_view-ticket-details.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/customer-ticketing/ishrar_view-ticket-details.e2e-spec.ts
@@ -12,6 +12,22 @@ const NOT_EXISTENT_TICKET_ID = 'XYZ01234';
 const FIRST_TICKET_ROW_INDEX = 1;
 
 
+const NOT_EXISTENT_TICKET_ID = 'XYZ01234';
+
+function createTestTicket() {
+  const testTicketDetails: customerTicketing.TestTicketDetails = {
+    subject: 'A test subject',
+    message: 'A test message',
+    category: customerTicketing.TestCategory.complaint,
+  };
+  customerTicketing.loginRegisteredUser();
+  customerTicketing.visitElectronicTicketListingPage();
+  customerTicketing.openCreateTicketPopup();
+  customerTicketing.fillTicketDetails(testTicketDetails);
+  customerTicketing.clickSubmit();
+  customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
+}
+
 describe('ticketing', () => {
   viewportContext(['desktop', 'mobile'], () => {
     context('Registered User', () => {
@@ -41,7 +57,10 @@ describe('ticketing', () => {
         customerTicketing.visitTicketDetailsPageForTicketInPosition(FIRST_TICKET_ROW_INDEX);
       });
       it('should throw 404 error when trying to visit ticket details page for a non-existing ticket id via url', () => {
-        customerTicketing.loginRegisteredUser();
+        createTestTicket();
+        customerTicketing.clickMyAccountMenuOption();
+        customerTicketing.clickCustomerSupportMenuOption();
+        customerTicketing.verifyTicketListingPageVisit();
         customerTicketing.visitTicketDetailsPageForNonExistentTicket(NOT_EXISTENT_TICKET_ID);
       });
     });
