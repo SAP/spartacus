f8c884657013119e74bea07cfc9af6250461d56c
Kibriya Inamdar <kibriya.inamdar@sap.com>
2022-11-08 08:35:00 +0100

    add clear function

--- feature-libs/organization/unit-order/components/unit-level-order-history/filter/unit-level-order-history-filter.component.ts
+++ feature-libs/organization/unit-order/components/unit-level-order-history/filter/unit-level-order-history-filter.component.ts
@@ -1,7 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import { TranslationService } from '@spartacus/core';
+import {Component, EventEmitter, Output, ViewChild} from '@angular/core';
+import {TranslationService} from '@spartacus/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import { OrderHistoryQueryParams } from '../../../core/model/augmented-core.model';
+import {OrderHistoryQueryParams} from '../../../core/model/augmented-core.model';
 import {ICON_TYPE} from "@spartacus/storefront";
 
 @Component({
@@ -9,7 +9,7 @@ import {ICON_TYPE} from "@spartacus/storefront";
   templateUrl: './unit-level-order-history-filter.component.html',
   styleUrls: ['./unit-level-order-history-filter.component.css']
 })
-export class UnitLevelOrderHistoryFilterComponent{
+export class UnitLevelOrderHistoryFilterComponent {
 
   iconTypes = ICON_TYPE;
   encodedFilter: string;
@@ -24,8 +24,10 @@ export class UnitLevelOrderHistoryFilterComponent{
     inputFilterUnit: new FormControl(),
   });
 
-  // @ViewChild('queryInputUser') queryInputUser; // accessing the reference element
-  // @ViewChild('queryInputUnit') queryInputUnit; // accessing the reference element
+  @ViewChild('queryInputBuyer') queryInputBuyer: { nativeElement: { value: any; }; };
+  @ViewChild('queryInputUnit') queryInputUnit: { nativeElement: { value: any; }; };
+  @ViewChild('queryInputBuyerMobile') queryInputBuyerMobile: { nativeElement: { value: any; }; };
+  @ViewChild('queryInputUnitMobile') queryInputUnitMobile: { nativeElement: { value: any; }; };
 
 
   @Output()
@@ -33,12 +35,13 @@ export class UnitLevelOrderHistoryFilterComponent{
 
   constructor(
     protected translation: TranslationService
-  ) { }
+  ) {
+  }
 
   formSearch(): void {
     let filters: string[] = [];
-    let user = this.filterForm.get('userFilter')?.value;
-    let unit = this.filterForm.get('unitFilter')?.value;
+    let user = this.queryInputBuyer?.nativeElement?.value;
+    let unit = this.queryInputUnit?.nativeElement?.value;
 
     user?.length ? filters.push('user:' + user) : '';
     unit?.length ? filters.push('unit:' + unit) : '';
@@ -46,14 +49,15 @@ export class UnitLevelOrderHistoryFilterComponent{
     this.emitFilterEvent(filters);
   }
 
-  emitFilterEvent(filters: string[]){
+  emitFilterEvent(filters: string[]) {
     this.encodedFilter = filters.join(':');
 
     this.filterListEvent.emit({
-      currentPage : 0,
-      filters : this.encodedFilter,
+      currentPage: 0,
+      filters: this.encodedFilter,
     });
   }
+
   resetForm(): void {
     this.filterForm.reset();
     this.filterFormMobile.reset();
@@ -73,11 +77,10 @@ export class UnitLevelOrderHistoryFilterComponent{
     document.getElementById("cx-unit-level-order-history-filter-nav").style.width = "100%";
   }
 
-
   formSearchMobile(): void {
     let filters: string[] = [];
-    let user = this.filterFormMobile.get('inputFilterBuyer')?.value;
-    let unit = this.filterFormMobile.get('inputFilterUnit')?.value;
+    let user = this.queryInputBuyerMobile?.nativeElement?.value;
+    let unit = this.queryInputUnitMobile?.nativeElement?.value;
 
     user?.length ? filters.push('user:' + user) : '';
     unit?.length ? filters.push('unit:' + unit) : '';
@@ -88,9 +91,11 @@ export class UnitLevelOrderHistoryFilterComponent{
     document.getElementById("cx-unit-level-order-history-filter-nav-sub-buyer").style.width = "0";
     document.getElementById("cx-unit-level-order-history-filter-nav").style.width = "100%";
   }
+
   closeNav(): void {
     document.getElementById("cx-unit-level-order-history-filter-nav").style.width = "0";
   }
+
   closeSubNav(): void {
     document.getElementById("cx-unit-level-order-history-filter-nav-sub-unit").style.width = "0";
     document.getElementById("cx-unit-level-order-history-filter-nav-sub-buyer").style.width = "0";
@@ -101,16 +106,18 @@ export class UnitLevelOrderHistoryFilterComponent{
     document.getElementById("cx-unit-level-order-history-filter-nav-sub-buyer").style.width = "0";
     document.getElementById("cx-unit-level-order-history-filter-nav").style.width = "100%";
   }
+
   launchSubNav(option: string): void {
 
     document.getElementById("cx-unit-level-order-history-filter-nav").style.width = "0";
 
-    if(option === 'filterByUnit') {
+    if (option === 'filterByUnit') {
       document.getElementById("cx-unit-level-order-history-filter-nav-sub-unit").style.width = "100%";
-    }else if(option === 'filterByBuyer'){
+    } else if (option === 'filterByBuyer') {
       document.getElementById("cx-unit-level-order-history-filter-nav-sub-buyer").style.width = "100%";
     }
   }
+
   clear(el: HTMLInputElement): void {
     el.value = '';
     el.focus();
