7a9747ab3d83a608549f7bebe97b87d99752aee3
Ishrar <103441743+ishrarhussain@users.noreply.github.com>
2022-11-08 16:23:20 -0500

    CXEC-17550: Navigate to Ticket Listing in case of 404 Not Found Ticket Detail (#16405)
    * CXEC-17550: Navigate to Ticket Listing in case of 404 Not Found Ticket Detail
    
    Jira Issue # CXEC-17550 (https://jira.tools.sap/browse/CXEC-17550)
    
    * Simplify error details retrieval with constants
    
    + Added separate module for constants in error details strings
    
    Co-Authored-By: Christian Montoya <christian.montoya.b@gmail.com>
    
    * Added check on http status code
    
    * Added a new http-interceptor as per request on the PR review
    
    Co-authored-by: Christian Montoya <christian.montoya.b@gmail.com>

--- feature-libs/customer-ticketing/core/customer-ticketing-core.module.ts
+++ feature-libs/customer-ticketing/core/customer-ticketing-core.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HttpErrorHandler } from '@spartacus/core';
 import { CustomerTicketingConnector } from './connectors';
 import { facadeProviders } from './facade/facade-providers';
+import { BadTicketRequestHandler } from './http-interceptors/handlers/bad-ticket-request.handler';
 
 @NgModule({
-  providers: [...facadeProviders, CustomerTicketingConnector],
+  providers: [
+    ...facadeProviders,
+    {
+      provide: HttpErrorHandler,
+      useExisting: BadTicketRequestHandler,
+      multi: true,
+    },
+    CustomerTicketingConnector
+  ],
 })
 export class CustomerTicketingCoreModule {}
