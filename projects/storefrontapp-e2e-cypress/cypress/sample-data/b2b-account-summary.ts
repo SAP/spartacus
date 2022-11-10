a68b04a549f59a3859b14343a9a52539d7c59043
Chris Kuhr <chris.kuhr@sap.com>
2022-11-10 15:31:26 -0500

    Update Account Summary E2E tests

--- projects/storefrontapp-e2e-cypress/cypress/sample-data/b2b-account-summary.ts
+++ projects/storefrontapp-e2e-cypress/cypress/sample-data/b2b-account-summary.ts
@@ -19,19 +19,18 @@ export const notAuthorizedText =
 export const headerDetailTiles = [
   { title: 'Unit ID', value: 'Custom Retail' },
   { title: 'Unit Name', value: 'Custom Retail' },
-  { title: 'Address', value: 'Ms, Carla Torres' },
+  { title: 'Address', value: 'Ms., Carla Torres' },
   { title: 'Credit Rep', value: 'n/a' },
   { title: 'Credit Line', value: 'n/a' },
   { title: 'Current Balance', value: '$102,145,214.00' },
   { title: 'Open Balance', value: '$135,737,232.00' },
 ];
 
+// The dates have been omitted from the value checks because they are generated dynmically and will change from one run to the next.
 export const documentsDefault = [
   {
     documentNumber: 'CRNCR-0000001',
     documentType: 'Invoice',
-    createdOn: 'March 10, 2022',
-    dueOn: 'April 9, 2022',
     originalAmount: '$796,371.00',
     openAmount: '$796,371.00',
     status: 'Open',
@@ -39,7 +38,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000001',
     documentType: 'Purchase Order',
-    createdOn: 'March 10, 2022',
     originalAmount: '$7,851,558.00',
     openAmount: '$7,851,558.00',
     status: 'Open',
@@ -47,7 +45,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000002',
     documentType: 'Purchase Order',
-    createdOn: 'March 10, 2022',
     originalAmount: '$5,094,536.00',
     openAmount: '$5,094,536.00',
     status: 'Open',
@@ -55,7 +52,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000003',
     documentType: 'Purchase Order',
-    createdOn: 'March 10, 2022',
     originalAmount: '$8,200,511.00',
     openAmount: '$8,200,511.00',
     status: 'Open',
@@ -63,7 +59,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000004',
     documentType: 'Purchase Order',
-    createdOn: 'March 10, 2022',
     originalAmount: '$3,175,103.00',
     openAmount: '$3,175,103.00',
     status: 'Open',
@@ -71,7 +66,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000009',
     documentType: 'Purchase Order',
-    createdOn: 'March 18, 2022',
     originalAmount: '$3,754,263.00',
     openAmount: '$3,754,263.00',
     status: 'Open',
@@ -79,7 +73,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000010',
     documentType: 'Purchase Order',
-    createdOn: 'March 18, 2022',
     originalAmount: '$3,893,837.00',
     openAmount: '$3,893,837.00',
     status: 'Open',
@@ -87,7 +80,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000011',
     documentType: 'Purchase Order',
-    createdOn: 'March 18, 2022',
     originalAmount: '$2,537,717.00',
     openAmount: '$2,537,717.00',
     status: 'Open',
@@ -95,7 +87,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000012',
     documentType: 'Purchase Order',
-    createdOn: 'March 18, 2022',
     originalAmount: '$7,745,197.00',
     openAmount: '$7,745,197.00',
     status: 'Open',
@@ -103,7 +94,6 @@ export const documentsDefault = [
   {
     documentNumber: 'POCR-0000013',
     documentType: 'Purchase Order',
-    createdOn: 'March 18, 2022',
     originalAmount: '$7,624,105.00',
     openAmount: '$7,624,105.00',
     status: 'Open',
