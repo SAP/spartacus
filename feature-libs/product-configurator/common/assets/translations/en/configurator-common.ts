3627e70808802b879fd079047658fa18816542be
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-11-15 18:03:08 +0100

    fix: icons in group menu shall have a tooltip (CXEC-16787)

--- feature-libs/product-configurator/common/assets/translations/en/configurator-common.ts
+++ feature-libs/product-configurator/common/assets/translations/en/configurator-common.ts
@@ -76,6 +76,12 @@ export const configurator = {
       cancelConfiguration: 'Cancel Configuration',
       cancelConfigurationMobile: 'Cancel',
     },
+    icon: {
+      groupComplete: 'Complete',
+      groupIncomplete: 'Incomplete',
+      groupConflict: 'Conflict',
+      subgroup: 'Sub-group',
+    },
     priceSummary: {
       basePrice: 'Base Price',
       selectedOptions: 'Selected Options',
