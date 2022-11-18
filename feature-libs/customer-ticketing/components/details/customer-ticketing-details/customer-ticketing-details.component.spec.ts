fbead4731cc1aabd2cbabf4c3fd91f8766e62e61
Parthlakhani <parth.lakhani@sap.com>
2022-11-18 09:31:30 -0500

    CXEC-20931: fix data reloading issue (#16493)

--- feature-libs/customer-ticketing/components/details/customer-ticketing-details/customer-ticketing-details.component.spec.ts
+++ feature-libs/customer-ticketing/components/details/customer-ticketing-details/customer-ticketing-details.component.spec.ts
@@ -1,12 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { I18nTestingModule, TranslationService } from '@spartacus/core';
+import {
+  EventService,
+  I18nTestingModule,
+  RoutingService,
+  TranslationService,
+} from '@spartacus/core';
 import {
   CustomerTicketingFacade,
   STATUS,
   TicketDetails,
 } from '@spartacus/customer-ticketing/root';
 import { Card, CardModule } from '@spartacus/storefront';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { CustomerTicketingDetailsComponent } from './customer-ticketing-details.component';
 
@@ -16,6 +21,9 @@ const mockTicketDetails: TicketDetails = {
   subject: 'mockTicket',
   status: { id: 'OPEN', name: 'Open' },
 };
+const routerParam$: BehaviorSubject<{
+  [key: string]: string;
+}> = new BehaviorSubject({});
 
 class MockTranslationService {
   translate(text: string): Observable<string> {
@@ -29,9 +37,18 @@ class MockCustomerTicketingFacade implements Partial<CustomerTicketingFacade> {
   }
 }
 
+class MockRoutingService implements Partial<RoutingService> {
+  getParams = () => routerParam$.asObservable();
+}
+
+class MockEventService implements Partial<EventService> {
+  dispatch<T extends object>(_event: T): void {}
+}
+
 describe('CustomerTicketingDetailsComponent', () => {
   let component: CustomerTicketingDetailsComponent;
   let fixture: ComponentFixture<CustomerTicketingDetailsComponent>;
+  let eventService: EventService;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -43,8 +60,13 @@ describe('CustomerTicketingDetailsComponent', () => {
           provide: CustomerTicketingFacade,
           useClass: MockCustomerTicketingFacade,
         },
+        { provide: RoutingService, useClass: MockRoutingService },
+        { provide: EventService, useClass: MockEventService },
       ],
     }).compileComponents();
+    eventService = TestBed.inject(EventService);
+    spyOn(eventService, 'dispatch').and.callThrough();
+    routerParam$.next({ ticketCode: '1' });
   });
 
   beforeEach(() => {
@@ -96,5 +118,21 @@ describe('CustomerTicketingDetailsComponent', () => {
     it('should return empty if the id is undefined', () => {
       assertStatusClassByStatusId('', undefined);
     });
+  });
+
+  it('should reload data if the ticket code does not match with the url ticket code', () => {
+    const mockParams = { ticketCode: '11' };
+    routerParam$.next(mockParams);
+    component['reloadOnRedirection']();
+
+    expect(eventService.dispatch).toHaveBeenCalled();
+  });
+
+  it('should not reload data if the ticket code is matches with the url ticket code', () => {
+    const mockParams = { ticketCode: '1' };
+    routerParam$.next(mockParams);
+    component['reloadOnRedirection']();
+
+    expect(eventService.dispatch).not.toHaveBeenCalled();
   });
 });
