be2f7ee32b0b95b090093e2134142f0b623401a2
I559579 <marcin.ganczorz@sap.com>
2022-11-21 09:56:52 +0100

    CXSPA-1743: adjust order detail tests to use unitLevelOrderDetails endpoint

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/unit-level-order/b2b-unit-level-order.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/unit-level-order/b2b-unit-level-order.e2e-spec.ts
@@ -7,18 +7,22 @@ describe('B2B - Unit-Level Order Details Page', () => {
   });
 
   describe('Check order details page', () => {
-    it('should display buyer and unit on order details page for unit-level-viewer user', () => {
+    it('should display buyer and unit on order details page for user with right to view Unit-Level Orders', () => {
       unitLevelOrderDetails.loginB2bUnitOrderViewer();
       unitLevelOrderDetails.getStubbedUnitLevelOrderDetails();
-      cy.visit(`/my-account/order/${sampleData.ORDER_CODE}`);
+      cy.visit(`/my-account/unitLevelOrderDetails/${sampleData.ORDER_CODE}`);
       assertUnitLevelOrderDetails(sampleData.unitLevelOrder);
     });
 
-    it('should not display buyer and unit on order details page for ordinal user', () => {
+    it('should not display order details page for user without right to view Unit-Level Orders', () => {
       unitLevelOrderDetails.loginB2bCommonUser();
       unitLevelOrderDetails.getStubbedUnitLevelOrderDetails();
-      cy.visit(`/my-account/order/${sampleData.ORDER_CODE}`);
-      assertCommonUserOrderDetails(sampleData.unitLevelOrder);
+      cy.visit(`/my-account/unitLevelOrderDetails/${sampleData.ORDER_CODE}`);
+      assertExistenceOfNoPermissionMessage();
+      cy.location('pathname').should(
+        'not.contain',
+        '/my-account/unitLevelOrderDetails/'
+      );
     });
   });
 });
@@ -30,11 +34,11 @@ function assertUnitLevelOrderDetails(order) {
   assertOrderDetailsCard(4, order.orgUnit.name);
 }
 
-function assertCommonUserOrderDetails(order) {
-  assertCommonFieldsOfOrderDetails(order);
-  const buyerNameAndEmail = `${order.orgCustomer.name}  (${order.orgCustomer.uid})`;
-  cy.get('cx-order-overview cx-card').should('not.contain', buyerNameAndEmail);
-  cy.get('cx-order-overview cx-card').should('not.contain', order.orgUnit.name);
+function assertExistenceOfNoPermissionMessage() {
+  cy.get('cx-global-message').should(
+    'contain',
+    'No sufficient permissions to access this page'
+  );
 }
 
 function assertCommonFieldsOfOrderDetails(order) {
@@ -65,5 +69,7 @@ function assertCommonFieldsOfOrderDetails(order) {
 }
 
 function assertOrderDetailsCard(index: number, value: string) {
-  cy.get('cx-order-overview cx-card').eq(index).should('contain', value);
+  cy.get('cx-unit-level-order-overview cx-card')
+    .eq(index)
+    .should('contain', value);
 }
