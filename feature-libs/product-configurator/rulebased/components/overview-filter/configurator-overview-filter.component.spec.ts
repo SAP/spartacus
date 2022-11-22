7866a844324136ead1ef222e48186821f6aec5bf
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-11-22 20:14:12 +0100

    feat: render group filter options based on overview config

--- feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.spec.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.spec.ts
@@ -1,13 +1,61 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { I18nTestingModule } from '@spartacus/core';
+import { ConfiguratorRouterExtractorService } from 'feature-libs/product-configurator/common/components/service/configurator-router-extractor.service';
+import { CommonConfigurator } from 'feature-libs/product-configurator/common/core/model/common-configurator.model';
+import { CommonConfiguratorTestUtilsService } from 'feature-libs/product-configurator/common/testing/common-configurator-test-utils.service';
+import { NEVER, of } from 'rxjs';
+import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
+import { Configurator } from '../../core/model/configurator.model';
+import * as ConfigurationTestData from '../../testing/configurator-test-data';
+import { ConfiguratorTestUtils } from '../../testing/configurator-test-utils';
 import { ConfiguratorOverviewFilterComponent } from './configurator-overview-filter.component';
+
+const owner: CommonConfigurator.Owner =
+  ConfigurationTestData.productConfiguration.owner;
+const configId = '1234-56-7890';
 
 let component: ConfiguratorOverviewFilterComponent;
 let fixture: ComponentFixture<ConfiguratorOverviewFilterComponent>;
-//let htmlElem: HTMLElement;
+let htmlElem: HTMLElement;
 
-function initialize() {
+let mockConfigRouterService: ConfiguratorRouterExtractorService;
+let mockConfigCommonsService: ConfiguratorCommonsService;
+
+let config: Configurator.Configuration;
+let ovConfig: Configurator.Configuration;
+
+function asSpy(f: any) {
+  return <jasmine.Spy>f;
+}
+
+function initTestData() {
+  config = ConfiguratorTestUtils.createConfiguration(configId, owner);
+  ovConfig = {
+    ...config,
+    overview: ConfigurationTestData.productConfiguration.overview,
+  };
+}
+
+function initMocks() {
+  mockConfigRouterService = jasmine.createSpyObj(['extractRouterData']);
+  mockConfigCommonsService = jasmine.createSpyObj([
+    'getOrCreateConfiguration',
+    'getConfigurationWithOverview',
+  ]);
+  asSpy(mockConfigRouterService.extractRouterData).and.returnValue(
+    of(ConfigurationTestData.mockRouterState)
+  );
+  asSpy(mockConfigCommonsService.getOrCreateConfiguration).and.returnValue(
+    of(config)
+  );
+  asSpy(mockConfigCommonsService.getConfigurationWithOverview).and.returnValue(
+    of(ovConfig)
+  );
+}
+
+function initTestComponent() {
   fixture = TestBed.createComponent(ConfiguratorOverviewFilterComponent);
-  //htmlElem = fixture.nativeElement;
+  htmlElem = fixture.nativeElement;
   component = fixture.componentInstance;
   fixture.detectChanges();
 }
@@ -15,16 +63,84 @@ function initialize() {
 describe('ConfigurationOverviewFilterComponent', () => {
   beforeEach(
     waitForAsync(() => {
+      initMocks();
       TestBed.configureTestingModule({
+        imports: [I18nTestingModule],
         declarations: [ConfiguratorOverviewFilterComponent],
+        providers: [
+          {
+            provide: ConfiguratorRouterExtractorService,
+            useValue: mockConfigRouterService,
+          },
+          {
+            provide: ConfiguratorCommonsService,
+            useValue: mockConfigCommonsService,
+          },
+        ],
       }).compileComponents();
     })
   );
   beforeEach(() => {
-    initialize();
+    initTestData();
   });
 
   it('should create component', () => {
+    initTestComponent();
     expect(component).toBeDefined();
+  });
+
+  it('should render filter options', () => {
+    initTestComponent();
+    CommonConfiguratorTestUtilsService.expectNumberOfElementsPresent(
+      expect,
+      htmlElem,
+      '.cx-overview-filter-option',
+      4 //2 default + 2 groups
+    );
+  });
+
+  it('should render both filter headers', () => {
+    initTestComponent();
+    CommonConfiguratorTestUtilsService.expectNumberOfElementsPresent(
+      expect,
+      htmlElem,
+      '.cx-overview-filter-header',
+      2
+    );
+  });
+
+  it('should render always default options', () => {
+    if (ovConfig.overview) ovConfig.overview.groups = [];
+    initTestComponent();
+
+    CommonConfiguratorTestUtilsService.expectNumberOfElementsPresent(
+      expect,
+      htmlElem,
+      '.cx-overview-filter-option',
+      2 //2 default
+    );
+  });
+
+  it('should not render any options if overview (groups) is undefined', () => {
+    if (ovConfig.overview) ovConfig.overview.groups = undefined;
+    initTestComponent();
+
+    CommonConfiguratorTestUtilsService.expectElementNotPresent(
+      expect,
+      htmlElem,
+      '.cx-overview-filter-option, .cx-overview-filter-header'
+    );
+  });
+
+  it('should not render anything if nothing has been emitted', () => {
+    asSpy(mockConfigCommonsService.getOrCreateConfiguration).and.returnValue(
+      NEVER
+    );
+    initTestComponent();
+    CommonConfiguratorTestUtilsService.expectElementNotPresent(
+      expect,
+      htmlElem,
+      '.cx-overview-filter-option, .cx-overview-filter-header'
+    );
   });
 });
--- feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.ts
@@ -5,11 +5,46 @@
  */
 
 import { Component } from '@angular/core';
+import { ConfiguratorRouterExtractorService } from 'feature-libs/product-configurator/common/components/service/configurator-router-extractor.service';
+import { OperatorFunction } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import {
+  distinctUntilKeyChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs/operators';
+import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
+import { Configurator } from '../../core/model/configurator.model';
 
 @Component({
   selector: 'cx-configurator-overview-filter',
   templateUrl: './configurator-overview-filter.component.html',
 })
 export class ConfiguratorOverviewFilterComponent {
-  constructor() {}
+  constructor(
+    protected configuratorCommonsService: ConfiguratorCommonsService,
+    protected configRouterExtractorService: ConfiguratorRouterExtractorService
+  ) {}
+
+  ovGroups$: Observable<Configurator.GroupOverview[]> =
+    this.configRouterExtractorService.extractRouterData().pipe(
+      switchMap((routerData) =>
+        this.configuratorCommonsService.getOrCreateConfiguration(
+          routerData.owner
+        )
+      ),
+      distinctUntilKeyChanged('configId'),
+      switchMap((configuration) =>
+        this.configuratorCommonsService.getConfigurationWithOverview(
+          configuration
+        )
+      ),
+      map((configuration) => configuration.overview?.groups),
+      // filter nullish 'strict null checks' safe
+      filter((groups) => groups !== undefined) as OperatorFunction<
+        Configurator.GroupOverview[] | undefined,
+        Configurator.GroupOverview[]
+      >
+    );
 }
