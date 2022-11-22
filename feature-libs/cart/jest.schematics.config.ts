a66f3187c4eb3322f10e2e9888430ecb6a8f3e85
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-11-22 19:56:12 +0100

    cart jest

---
+++ feature-libs/cart/jest.schematics.config.ts
@@ -0,0 +1,34 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.schematics.json';
+
+const jestConfig: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+
+  testMatch: ['**/+(*_)+(spec).+(ts)'],
+  globals: {
+    'ts-jest': {
+      // our naming convention deviates from the standard "tsconfig.spec.json"
+      tsconfig: '<rootDir>/tsconfig.schematics.json',
+    },
+  },
+
+  collectCoverage: false,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: '<rootDir>/../../coverage/cart/schematics',
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+};
+
+export default jestConfig;
