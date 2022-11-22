c313f258c72fb7d6c8e39d7cdbc23888fcb36a70
I561291 <tobias.djermester@sap.com>
2022-11-22 09:54:22 +0100

    CXSPA-1301: Implement b2b unit-order test

--- projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/unit-level-order/b2b-unit-level-order.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/b2b/regression/unit-level-order/b2b-unit-level-order.e2e-spec.ts
@@ -1,9 +1,113 @@
 import * as unitLevelOrderDetails from '../../../../helpers/b2b/b2b-order-details';
 import * as sampleData from '../../../../sample-data/b2b-order-details';
+import * as unitLevelOrderHistory from '../../../../helpers/b2b/b2b-order-history';
+import { doPlaceB2BOrder } from '../../../../helpers/b2b/b2b-order-history';
+import { b2bProduct } from '../../../../sample-data/b2b-checkout';
 
-describe('B2B - Unit-Level Order Details Page', () => {
+describe('B2B - Unit-level order history page', () => {
+  let order0;
+  let order1;
+  let order2;
+
   before(() => {
     cy.window().then((win) => win.sessionStorage.clear());
+    // William Hunter - Custom Retail
+    unitLevelOrderHistory.loginB2bCommonUser();
+    doPlaceB2BOrder(b2bProduct).then((resp) => (order0 = resp.body));
+
+    // Gi Sun - Services West
+    unitLevelOrderHistory.loginB2bUnitOrderViewer();
+    doPlaceB2BOrder(b2bProduct).then((resp) => (order1 = resp.body));
+
+    // Mark Rivers - Custom Retail
+    unitLevelOrderHistory.loginB2bUnitOrderViewer2();
+    doPlaceB2BOrder(b2bProduct).then((resp) => (order2 = resp.body));
+
+    // Hanna Schmidt - Rustic
+    unitLevelOrderHistory.loginB2bUnitOrderViewerManager();
+
+    unitLevelOrderDetails.loginB2bUnitOrderViewer();
+    cy.visit('/my-account/unitLevelOrders');
+  });
+
+  describe('Check unit-level orders display', () => {
+    describe('User without right', () => {
+      it('is not allowed to access unit-level orders page', () => {
+        unitLevelOrderDetails.loginB2bCommonUser();
+
+        cy.visit('/my-account/unitLevelOrders');
+
+        cy.location('pathname').should(
+          'eq',
+          `/${Cypress.env('BASE_SITE')}/${Cypress.env(
+            'BASE_LANG'
+          )}/${Cypress.env('BASE_CURRENCY')}/`
+        );
+        cy.get('.alert')
+          .should('be.visible')
+          .and('contain.text', 'No sufficient permissions');
+      });
+    });
+
+    describe('User with right', () => {
+      beforeEach(() => {
+        cy.wrap(order0).as('order0');
+        cy.wrap(order1).as('order1');
+        cy.wrap(order2).as('order2');
+      });
+
+      it('can see own placed orders', () => {
+        unitLevelOrderHistory.loginB2bUnitOrderViewer();
+        cy.visit('/my-account/unitLevelOrders');
+
+        cy.get('@order0').then((order) => {
+          assertNoUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order1').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order2').then((order) => {
+          assertNoUnitLevelOrderDisplayed(order);
+        });
+      });
+
+      it('can see all placed orders from unit', () => {
+        unitLevelOrderHistory.loginB2bUnitOrderViewer2();
+        cy.visit('/my-account/unitLevelOrders');
+
+        cy.get('@order0').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order1').then((order) => {
+          assertNoUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order2').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+      });
+
+      it('can see all placed orders from subunits', () => {
+        unitLevelOrderHistory.loginB2bUnitOrderViewerManager();
+        cy.visit('/my-account/unitLevelOrders');
+
+        cy.get('@order0').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order1').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+        cy.get('@order2').then((order) => {
+          assertUnitLevelOrderDisplayed(order);
+        });
+      });
+
+      it('can go to order details page', () => {
+        unitLevelOrderHistory.loginB2bUnitOrderViewerManager();
+
+        cy.visit('/my-account/unitLevelOrders');
+        assertClickOrderDetailsPage(order0);
+      });
+    });
   });
 
   describe('Check order details page', () => {
@@ -28,6 +132,30 @@ function assertUnitLevelOrderDetails(order) {
   const buyerNameAndEmail = `${order.orgCustomer.name}  (${order.orgCustomer.uid})`;
   assertOrderDetailsCard(3, buyerNameAndEmail);
   assertOrderDetailsCard(4, order.orgUnit.name);
+}
+
+function assertUnitLevelOrderDisplayed(order) {
+  cy.get('#order-history-table')
+    .contains('tr', order.code)
+    .should('contain.text', order.orgCustomer.email)
+    .and('contain.text', order.orgUnit.name);
+}
+
+function assertNoUnitLevelOrderDisplayed(order) {
+  cy.get('#order-history-table tr')
+    .should('not.contain', order.code)
+    .and('not.contain', order.orgCustomer.email)
+    .and('not.contain', order.orgUnit.name);
+}
+
+function assertClickOrderDetailsPage(order) {
+  cy.get('td a').contains(order.code).click();
+  cy.get('cx-breadcrumb').should('contain.text', 'Unit-Level Order Details');
+  cy.location('pathname').should(
+    'contain',
+    `unitLevelOrderDetails/${order.code}`
+  );
+  cy.contains('Order Number').parent().should('contain', order.code);
 }
 
 function assertCommonUserOrderDetails(order) {
