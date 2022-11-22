f6883cd053ceb9012aa18762c3f62724452c5596
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2022-11-22 12:26:11 -0500

    prepend license

--- projects/storefrontapp/src/app/spartacus/features/user/user-account-wrapper.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/user/user-account-wrapper.module.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { NgModule, Type } from '@angular/core';
 import { CDCUserAccountModule } from '@spartacus/cdc/user-account';
 import { UserAccountModule } from '@spartacus/user/account';
