0e057ba39f54e3bf5c6a858d415cadcdd6ace5a2
justinlee01 <47992455+justinlee01@users.noreply.github.com>
2022-11-23 12:27:17 -0500

    CXSPA-1639: Cypress deleteFolder task mimics Unix rm -rf in that it does not error if the folder does not exist (#16435)
    * Cypress deleteFolder task mimics Unix rm -rf in that it does not error if the folder does not exist. Fixes many cart import tests which seem to impact one another https://nodejs.org/api/fs.html\#fspromisesrmdirpath-options
    
    * Adding 'setUpNodeEvents' to Cypress CI config (why aren't base configs inherited?...), and linting
    
    * cypress.config.ts: reverting non-CI API URL to the current develop server to match 'yarn start'
    
    * Save for now
    
    * Add license header
    
    * Undoing changes that should not have been committed
    
    * Excluding cx_ccv2 nightly tests from general CI build
    
    * CI build
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
@@ -30,6 +30,11 @@ export default defineConfig({
     OCC_PREFIX_ORDER_ENDPOINT: 'orders',
   },
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
     baseUrl: 'http://localhost:4200',
   },
 });
