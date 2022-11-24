e9977b733c4a7af9cdf3776f674c3d3ab8a3ac83
Dalvir-Personal <40722374+Dalvir-Personal@users.noreply.github.com>
2022-11-24 16:10:21 -0500

    CXSPA-1787 Updated basesites URL to get all fields

--- projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.ts
+++ projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.ts
@@ -6,7 +6,7 @@
 
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, switchMap, take } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
 import { ConfigInitializer } from '../../../config/config-initializer/config-initializer';
 import { ConfigInitializerService } from '../../../config/config-initializer/config-initializer.service';
 import { BaseSite } from '../../../model/misc.model';
@@ -33,27 +33,16 @@ export class SecurePortalConfigInitializer implements ConfigInitializer {
     return this.configInit.getStable('context').pipe(
       switchMap((config) => {
         const siteUid = config?.context?.[BASE_SITE_CONTEXT_ID]?.[0];
-        return this.baseSiteService.getAll().pipe(
-          map((baseSites) => baseSites?.find((site) => siteUid === site.uid)),
-          filter((baseSite: any) => {
-            if (!baseSite) {
-              throw new Error(
-                `Error: Cannot get base site config for ${siteUid}.`
-              );
-            }
-            return Boolean(baseSite);
-          }),
-          map((baseSite) => this.getRoutingConfig(baseSite)),
-          take(1)
-        );
+        return this.baseSiteService.get(siteUid)
+          .pipe(map((baseSite) => this.getRoutingConfig(baseSite)), take(1));
       })
     );
   }
 
-  protected getRoutingConfig(source: BaseSite): RoutingConfig {
+  protected getRoutingConfig(source: BaseSite | undefined): RoutingConfig {
     const result = {
       routing: {
-        protected: source.requiresAuthentication,
+        protected: source?.requiresAuthentication,
       },
     };
     return result;
