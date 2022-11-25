2f9bc4046d3295ebdf591b186e0417aeeb4bfca7
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-11-25 12:00:56 +0100

    feat: add filter-bar re-use component to filter-button

--- feature-libs/product-configurator/rulebased/components/overview-filter-button/configurator-overview-filter-button.component.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter-button/configurator-overview-filter-button.component.ts
@@ -6,14 +6,48 @@
 
 import { Component } from '@angular/core';
 import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
-import { take } from 'rxjs/operators';
+import { ConfiguratorRouterExtractorService } from 'feature-libs/product-configurator/common/components/service/configurator-router-extractor.service';
+import { Observable, OperatorFunction } from 'rxjs';
+import {
+  distinctUntilKeyChanged,
+  filter,
+  switchMap,
+  take,
+} from 'rxjs/operators';
+import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
+import { Configurator } from '../../core/model/configurator.model';
+import { ConfigurationNonNullOv } from '../overview-filter/configurator-overview-filter.component';
 
 @Component({
   selector: 'cx-configurator-overview-filter-button',
   templateUrl: './configurator-overview-filter-button.component.html',
 })
 export class ConfiguratorOverviewFilterButtonComponent {
-  constructor(protected launchDialogService: LaunchDialogService) {}
+  constructor(
+    protected launchDialogService: LaunchDialogService,
+    protected configuratorCommonsService: ConfiguratorCommonsService,
+    protected configRouterExtractorService: ConfiguratorRouterExtractorService
+  ) {}
+
+  config$: Observable<Configurator.Configuration> =
+    this.configRouterExtractorService.extractRouterData().pipe(
+      switchMap((routerData) =>
+        this.configuratorCommonsService.getOrCreateConfiguration(
+          routerData.owner
+        )
+      ),
+      distinctUntilKeyChanged('configId'),
+      switchMap((configuration) =>
+        this.configuratorCommonsService.getConfigurationWithOverview(
+          configuration
+        )
+      ),
+      // filter 'strict null check safe'
+      filter(
+        (configuration) => configuration.overview != null
+      ) as OperatorFunction<Configurator.Configuration, ConfigurationNonNullOv>
+    );
+
   openFilterModal() {
     const dialogData = {
       testData: 'test',
--- feature-libs/product-configurator/rulebased/components/overview-filter-button/configurator-overview-filter-button.module.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter-button/configurator-overview-filter-button.module.ts
@@ -7,10 +7,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CmsConfig, I18nModule, provideDefaultConfig } from '@spartacus/core';
+import { ConfiguratorOverviewFilterBarModule } from '../overview-filter-bar/configurator-overview-filter-bar.module';
 import { ConfiguratorOverviewFilterButtonComponent } from './configurator-overview-filter-button.component';
 
 @NgModule({
-  imports: [CommonModule, I18nModule],
+  imports: [CommonModule, I18nModule, ConfiguratorOverviewFilterBarModule],
   providers: [
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
