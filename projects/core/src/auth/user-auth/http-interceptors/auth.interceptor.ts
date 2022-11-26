3b0790aa0fedc4e0fce188fa41d0acbabb950e1a
Chris Kuhr <chris.kuhr@sap.com>
2022-11-25 21:21:31 -0500

    fix account summary attachment download timeout redirect bug

--- projects/core/src/auth/user-auth/http-interceptors/auth.interceptor.ts
+++ projects/core/src/auth/user-auth/http-interceptors/auth.interceptor.ts
@@ -12,6 +12,7 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FileReaderService } from 'projects/storefrontlib/shared/services/file/file-reader.service';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, switchMap, take } from 'rxjs/operators';
 import { AuthConfigService } from '../services/auth-config.service';
@@ -25,7 +26,8 @@ import { AuthHttpHeaderService } from '../services/auth-http-header.service';
 export class AuthInterceptor implements HttpInterceptor {
   constructor(
     protected authHttpHeaderService: AuthHttpHeaderService,
-    protected authConfigService: AuthConfigService
+    protected authConfigService: AuthConfigService,
+    protected fileReaderService: FileReaderService
   ) {}
 
   intercept(
@@ -51,6 +53,7 @@ export class AuthInterceptor implements HttpInterceptor {
     return requestAndToken$.pipe(
       switchMap(({ request, token }) =>
         next.handle(request).pipe(
+          catchError((errResponse: any) => this.handleBlobErrors(errResponse)),
           catchError((errResponse: any) => {
             if (errResponse instanceof HttpErrorResponse) {
               switch (errResponse.status) {
@@ -99,5 +102,30 @@ export class AuthInterceptor implements HttpInterceptor {
 
   protected isExpiredToken(resp: HttpErrorResponse): boolean {
     return resp.error?.errors?.[0]?.type === 'InvalidTokenError';
+  }
+
+  // If the response error is a blob of type HttpErrorResonse: extract the error from blob to JSON
+  // Otherwise: rethrow the error
+  protected handleBlobErrors(errResponse: any): Observable<never> {
+    if (
+      errResponse instanceof HttpErrorResponse &&
+      errResponse?.error instanceof Blob
+    ) {
+      const blob = new Blob([errResponse.error]) as File;
+      return this.fileReaderService.loadTextFile(blob).pipe(
+        switchMap((unparsedError: any) => {
+          const error = JSON.parse(unparsedError);
+          return throwError(
+            new HttpErrorResponse({
+              ...errResponse,
+              error,
+              url: errResponse.url ?? undefined,
+            })
+          );
+        })
+      );
+    } else {
+      return throwError(errResponse);
+    }
   }
 }
