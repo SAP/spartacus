a0db2e71d530435a5c2a377c5d56f359cd561971
Dalvir-Personal <40722374+Dalvir-Personal@users.noreply.github.com>
2022-11-28 16:03:04 -0500

    Updated code after review

--- projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.ts
+++ projects/core/src/routing/configurable-routes/secure-portal-config/secure-portal-config-initializer.ts
@@ -6,7 +6,7 @@
 
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, switchMap, take } from 'rxjs/operators';
+import { map, switchMap, take, tap } from 'rxjs/operators';
 import { ConfigInitializer } from '../../../config/config-initializer/config-initializer';
 import { ConfigInitializerService } from '../../../config/config-initializer/config-initializer.service';
 import { BaseSite } from '../../../model/misc.model';
@@ -34,13 +34,12 @@ export class SecurePortalConfigInitializer implements ConfigInitializer {
       switchMap((config) => {
         const siteUid = config?.context?.[BASE_SITE_CONTEXT_ID]?.[0];
         return this.baseSiteService.get(siteUid).pipe(
-          filter((baseSite: any) => {
+          tap((baseSite) => {
             if (!baseSite) {
               throw new Error(
                 `Error: Cannot get base site config for ${siteUid}.`
               );
             }
-            return Boolean(baseSite);
           }),
           map((baseSite) => this.getRoutingConfig(baseSite)),
           take(1)
