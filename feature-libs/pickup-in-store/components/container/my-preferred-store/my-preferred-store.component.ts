54b2c043530c69eada3f7875d4647c7dfbc5ec08
cawmichae <chris.michael@sap.com>
2022-11-28 15:25:21 +0000

    Add Preferred Store to Address Book

--- feature-libs/pickup-in-store/components/container/my-preferred-store/my-preferred-store.component.ts
+++ feature-libs/pickup-in-store/components/container/my-preferred-store/my-preferred-store.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PointOfService } from '@spartacus/core';
 import { PreferredStoreService } from '@spartacus/pickup-in-store/core';
+import { PickupLocationsSearchFacade } from '@spartacus/pickup-in-store/root';
+import { Observable } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-my-preferred-store',
@@ -7,6 +11,26 @@ import { PreferredStoreService } from '@spartacus/pickup-in-store/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyPreferredStoreComponent {
-  public storeSelected$ = this.preferredStoreService.getPreferredStore$();
-  constructor(private preferredStoreService: PreferredStoreService) {}
+  public preferredStore$: Observable<PointOfService>;
+  public content = { header: 'My Store' };
+
+  constructor(
+    private preferredStoreService: PreferredStoreService,
+    protected pickupLocationsSearchService: PickupLocationsSearchFacade
+  ) {
+    this.preferredStore$ = this.preferredStoreService.getPreferredStore$().pipe(
+      filter((preferredStore) => !!preferredStore?.name),
+      map((preferredStore) => preferredStore?.name),
+      tap((preferredStoreName) =>
+        this.pickupLocationsSearchService.loadStoreDetails(
+          preferredStoreName as string
+        )
+      ),
+      switchMap((preferredStoreName) =>
+        this.pickupLocationsSearchService.getStoreDetails(
+          preferredStoreName as string
+        )
+      )
+    );
+  }
 }
--- feature-libs/pickup-in-store/components/container/my-preferred-store/my-preferred-store.module.ts
+++ feature-libs/pickup-in-store/components/container/my-preferred-store/my-preferred-store.module.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CmsConfig, ConfigModule } from '@spartacus/core';
+import { CmsConfig, ConfigModule, I18nModule } from '@spartacus/core';
+import { CardModule } from '@spartacus/storefront';
+import { StoreModule } from '../../presentational/store';
 import { MyPreferredStoreComponent } from './my-preferred-store.component';
 @NgModule({
   imports: [
+    CardModule,
+    StoreModule,
     CommonModule,
+    I18nModule,
     ConfigModule.withConfig({
       cmsComponents: {
         MyPreferredStore: {
