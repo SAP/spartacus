8b903824fc09c00e1d666d068d15a26eb3444a61
MarcinGanczorz <102027317+MarcinGanczorz@users.noreply.github.com>
2022-11-29 15:32:51 +0100

    CXSPA-1882: improve unit test code coverage for unit-level order details implementation (#16558)

--- feature-libs/organization/unit-order/core/store/actions/unit-order.action.spec.ts
+++ feature-libs/organization/unit-order/core/store/actions/unit-order.action.spec.ts
@@ -1,6 +1,6 @@
 import { StateUtils } from '@spartacus/core';
-import { OrderHistoryList } from '@spartacus/order/root';
-import { UNIT_ORDERS } from '../unit-order-state';
+import { Order, OrderHistoryList } from '@spartacus/order/root';
+import { UNIT_ORDERS, UNIT_ORDER_DETAILS } from '../unit-order-state';
 import { UnitOrderActions } from './index';
 
 describe('OrdersActions', () => {
@@ -68,6 +68,67 @@ describe('OrdersActions', () => {
       expect({ ...action }).toEqual({
         type: UnitOrderActions.CLEAR_UNIT_ORDERS,
         meta: StateUtils.resetMeta(UNIT_ORDERS),
+      });
+    });
+  });
+});
+
+const mockOrderDetails: Order = { code: '123' };
+
+const mockOrderDetailsParams = {
+  userId: 'user15355363988711@ydev.hybris.com',
+  orderCode: '00000386',
+};
+
+describe('Order Details Actions', () => {
+  describe('LoadOrderDetails Action', () => {
+    it('should create the action', () => {
+      const action = new UnitOrderActions.LoadOrderDetails(
+        mockOrderDetailsParams
+      );
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_ORDER_DETAILS,
+        payload: mockOrderDetailsParams,
+        meta: StateUtils.loadMeta(UNIT_ORDER_DETAILS),
+      });
+    });
+  });
+
+  describe('LoadOrderDetailsFail Action', () => {
+    it('should create the action', () => {
+      const error = 'mockError';
+      const action = new UnitOrderActions.LoadOrderDetailsFail(error);
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_ORDER_DETAILS_FAIL,
+        payload: error,
+        meta: StateUtils.failMeta(UNIT_ORDER_DETAILS, error),
+      });
+    });
+  });
+
+  describe('LoadOrderDetailsSuccess Action', () => {
+    it('should create the action', () => {
+      const action = new UnitOrderActions.LoadOrderDetailsSuccess(
+        mockOrderDetails
+      );
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.LOAD_ORDER_DETAILS_SUCCESS,
+        payload: mockOrderDetails,
+        meta: StateUtils.successMeta(UNIT_ORDER_DETAILS),
+      });
+    });
+  });
+
+  describe('ClearOrderDetails Action', () => {
+    it('should create the action', () => {
+      const action = new UnitOrderActions.ClearOrderDetails();
+
+      expect({ ...action }).toEqual({
+        type: UnitOrderActions.CLEAR_ORDER_DETAILS,
+        meta: StateUtils.resetMeta(UNIT_ORDER_DETAILS),
       });
     });
   });
