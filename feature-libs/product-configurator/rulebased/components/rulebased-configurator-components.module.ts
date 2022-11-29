f337cd6efd30c61ffec59f8f494655755fa1cc73
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2022-11-29 19:28:11 +0100

    chore: organize imports

--- feature-libs/product-configurator/rulebased/components/rulebased-configurator-components.module.ts
+++ feature-libs/product-configurator/rulebased/components/rulebased-configurator-components.module.ts
@@ -6,16 +6,17 @@
 
 import { NgModule } from '@angular/core';
 import { ConfiguratorAddToCartButtonModule } from './add-to-cart-button/configurator-add-to-cart-button.module';
+import { ConfiguratorConflictAndErrorMessagesModule } from './configurator-conflict-and-error-messages/configurator-conflict-and-error-messages.module';
+import { ConfiguratorExitButtonModule } from './exit-button/configurator-exit-button.module';
 import { ConfiguratorFormModule } from './form/configurator-form.module';
 import { ConfiguratorGroupMenuModule } from './group-menu/configurator-group-menu.module';
 import { ConfiguratorGroupTitleModule } from './group-title/configurator-group-title.module';
 import { ConfiguratorOverviewAttributeModule } from './overview-attribute/configurator-overview-attribute.module';
-import { ConfiguratorOverviewFormModule } from './overview-form/configurator-overview-form.module';
-import { ConfiguratorConflictAndErrorMessagesModule } from './configurator-conflict-and-error-messages/configurator-conflict-and-error-messages.module';
-import { ConfiguratorExitButtonModule } from './exit-button/configurator-exit-button.module';
 import { ConfiguratorOverviewFilterButtonModule } from './overview-filter-button/configurator-overview-filter-button.module';
 import { ConfiguratorOverviewFilterDialogModule } from './overview-filter-dialog/configurator-overview-filter-dialog.module';
 import { ConfiguratorOverviewFilterModule } from './overview-filter/configurator-overview-filter.module';
+import { ConfiguratorOverviewFormModule } from './overview-form/configurator-overview-form.module';
+import { ConfiguratorOverviewMenuModule } from './overview-menu/configurator-overview-menu.module';
 import { ConfiguratorOverviewNotificationBannerModule } from './overview-notification-banner/configurator-overview-notification-banner.module';
 import { ConfiguratorPreviousNextButtonsModule } from './previous-next-buttons/configurator-previous-next-buttons.module';
 import { ConfiguratorPriceSummaryModule } from './price-summary/configurator-price-summary.module';
@@ -23,7 +24,6 @@ import { ConfiguratorProductTitleModule } from './product-title/configurator-pro
 import { ConfiguratorTabBarModule } from './tab-bar/configurator-tab-bar.module';
 import { ConfiguratorUpdateMessageModule } from './update-message/configurator-update-message.module';
 import { ConfiguratorVariantCarouselModule } from './variant-carousel/configurator-variant-carousel.module';
-import { ConfiguratorOverviewMenuModule } from './overview-menu/configurator-overview-menu.module';
 
 @NgModule({
   imports: [
