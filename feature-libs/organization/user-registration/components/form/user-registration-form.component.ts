54ca856adfdc0629261fbe9fcadd3eecd6edea7f
Mateusz Kolasa <mateusz.kolasa@sap.com>
2022-11-30 15:03:55 +0100

    feat: B2B User Registration
    Closes: CXSPA-215

---
+++ feature-libs/organization/user-registration/components/form/user-registration-form.component.ts
@@ -0,0 +1,57 @@
+/*
+ * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Country, Region } from '@spartacus/core';
+import { Title } from '@spartacus/user/profile/root';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { UserRegistrationFormService } from './user-registration-form.service';
+
+@Component({
+  selector: 'cx-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserRegistrationFormComponent implements OnDestroy {
+  titles$: Observable<Title[]> = this.userRegistrationFormService.getTitles();
+
+  countries$: Observable<Country[]> =
+    this.userRegistrationFormService.getCountries();
+
+  regions$: Observable<Region[]> =
+    this.userRegistrationFormService.getRegions();
+
+  registerForm: FormGroup = this.userRegistrationFormService.form;
+
+  isLoading$ = new BehaviorSubject(false);
+
+  protected subscriptions = new Subscription();
+
+  constructor(
+    protected userRegistrationFormService: UserRegistrationFormService
+  ) {}
+
+  submit(): void {
+    if (this.registerForm.valid) {
+      this.isLoading$.next(true);
+      this.subscriptions.add(
+        this.userRegistrationFormService
+          .registerUser(this.registerForm)
+          .subscribe({
+            complete: () => this.isLoading$.next(false),
+            error: () => this.isLoading$.next(false),
+          })
+      );
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
