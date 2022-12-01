4550495389b6ba152dd530651a8209c2da41832d
Eric Polesky <eric.polesky@sap.com>
2022-12-01 17:27:40 -0500

    chore: add error states to customer list dialog (#16441)
    Add error normalization to asm adapter customer search request.
    Now shows unique state for
     - empty lists request
     - failed lists request
     - failed customer page request
    Specify formatters in suggested workspace config

--- projects/assets/src/translations/translation-chunks-config.ts
+++ projects/assets/src/translations/translation-chunks-config.ts
@@ -24,6 +24,7 @@ export const translationChunksConfig: TranslationChunksConfig = {
     'carousel',
     'assistiveMessage',
     'passwordVisibility',
+    'generalErrors',
   ],
   address: [
     'addressForm',
