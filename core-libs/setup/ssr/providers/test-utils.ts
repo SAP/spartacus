bd2054bb06812edc27cae3f0fb4e610eeeef745d
Nikola Zarić <nikola.zaric.ns@gmail.com>
2022-12-01 10:56:32 +0100

    Revert "Add license header"
    This reverts commit 8c76e525147e36bcbbec28c3638dbf8bcb1b3144.

--- core-libs/setup/ssr/providers/test-utils.ts
+++ core-libs/setup/ssr/providers/test-utils.ts
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import { StaticProvider } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import {
