859183715d386907742befbfdafe086097db8d5d
Weizheng Gao <weizheng.gao@sap.com>
2022-12-06 11:53:59 -0400

    add storefrontstyle tests in CI

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -79,3 +79,7 @@ yarn --cwd feature-libs/tracking run test:schematics --coverage
 echo "Running unit tests and code coverage for schematics library"
 
 yarn --cwd projects/schematics run test --runInBand --coverage
+
+echo "Running unit tests and code coverage for storefrontstyle"
+
+yarn --cwd projects/storefrontstyles run test --runInBand --coverage
