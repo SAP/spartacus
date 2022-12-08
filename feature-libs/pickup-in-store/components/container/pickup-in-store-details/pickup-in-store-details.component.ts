cd2b03a364e17882635ec3ca26ce8f17f7963858
cawmichae <chris.michael@sap.com>
2022-12-08 16:46:48 +0000

    Show DeliverPointOfService and Items

--- feature-libs/pickup-in-store/components/container/pickup-in-store-details/pickup-in-store-details.component.ts
+++ feature-libs/pickup-in-store/components/container/pickup-in-store-details/pickup-in-store-details.component.ts
@@ -5,6 +5,9 @@
  */
 
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActiveCartFacade } from '@spartacus/cart/base/root';
+import { PickupLocationsSearchFacade } from '@spartacus/pickup-in-store/root';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-pick-up-in-store-details',
@@ -12,7 +15,46 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PickUpInStoreDetailsComponent implements OnInit {
-  constructor() {}
+  deliveryPointsOfService$ = this.activeCartFacade.getActive().pipe(
+    filter((cart) => !!cart.entries),
+    filter((cart) => !!(cart.entries as Array<any>).length),
+    map((cart) => {
+      const COPY = [...(cart.entries as Array<any>)];
+      COPY.sort((a, b) =>
+        a.deliveryPointOfService?.name?.localeCompare(
+          b.deliveryPointOfService?.name
+        )
+      );
+      return COPY;
+    }),
+    map((sortedArray) =>
+      sortedArray
+        .filter((entry) => !!entry.deliveryPointOfService)
+        .reduce((accumulator, value) => {
+          const DELIVERY_POINT_OF_SERVICE = value.deliveryPointOfService.name;
+          const existingValue = accumulator[DELIVERY_POINT_OF_SERVICE]
+            ? accumulator[DELIVERY_POINT_OF_SERVICE]
+            : [];
+          return {
+            ...accumulator,
+            [DELIVERY_POINT_OF_SERVICE]: [...existingValue, value],
+          };
+        }, {})
+    ),
+    map((deliveryPointOfServiceMap) =>
+      Object.keys(deliveryPointOfServiceMap).map((key) => ({
+        name: key,
+        value: deliveryPointOfServiceMap[key],
+      }))
+    ),
+    tap((d) => console.log('d', d))
+  );
+  constructor(
+    protected activeCartFacade: ActiveCartFacade,
+    protected pickupLocationsSearchService: PickupLocationsSearchFacade
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.deliveryPointsOfService$.subscribe((d) => console.log('d', d));
+  }
 }
