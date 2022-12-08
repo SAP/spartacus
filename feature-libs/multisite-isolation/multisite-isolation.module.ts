e9853cba950793295c3544613d52aec04c8209e0
Mateusz <mateusz.kolasa@sap.com>
2022-12-08 10:48:54 +0100

    Comment out schematics

--- feature-libs/multisite-isolation/multisite-isolation.module.ts
+++ feature-libs/multisite-isolation/multisite-isolation.module.ts
@@ -1,3 +1,4 @@
+import { MultisiteIsolationRootModule } from './root/multisite-isolation-root.module';
 /*
  * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
  *
@@ -7,7 +8,6 @@
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  // TODO: Import modules
-  imports: [],
+  imports: [MultisiteIsolationRootModule],
 })
 export class MultisiteIsolationModule {}
