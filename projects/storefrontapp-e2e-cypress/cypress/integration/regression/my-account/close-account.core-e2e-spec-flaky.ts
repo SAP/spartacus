c73b3110f061d70d897cddd381719ee08915b1d7
Zeyber <rotherham.c@gmail.com>
2022-12-14 12:15:00 +0100

    test: Test e2e builds

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/close-account.core-e2e-spec-flaky.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/my-account/close-account.core-e2e-spec-flaky.ts
@@ -6,85 +6,107 @@ import { standardUser } from '../../../sample-data/shared-users';
 
 const CLOSE_ACCOUNT_URL = '/my-account/close-account';
 
-describe('My Account - Close Account', () => {
-  viewportContext(['mobile', 'desktop'], () => {
-    before(() =>
-      cy.window().then((win) => {
-        win.sessionStorage.clear();
-      })
-    );
+let _counter = 0;
+let skip = false;
 
-    describe('Anonymous user', () => {
-      it('should redirect to login page', () => {
-        cy.visit(CLOSE_ACCOUNT_URL);
-        cy.location('pathname').should('contain', '/login');
+Cypress.on('fail', (error, test) => {
+  skip = false;
+  console.log('hello');
+  console.log(test);
+  if (_counter < 3) {
+    _counter++;
+    console.log(_counter);
+    // test.run(() => myTest());
+    cy.end();
+    myTest();
+  } else {
+    throw error; // behave as normal
+  }
+});
+
+// Cypress._.times(_runModeRetries, () => {
+const myTest = () =>
+  describe('My Account - Close Account', () => {
+    viewportContext(['mobile', 'desktop'], () => {
+      before(() =>
+        cy.window().then((win) => {
+          win.sessionStorage.clear();
+        })
+      );
+
+      describe('Anonymous user', () => {
+        it('should redirect to login page', () => {
+          cy.visit(CLOSE_ACCOUNT_URL);
+          cy.location('pathname').should('contain', '/login');
+        });
       });
-    });
 
-    describe('Logged in user', () => {
-      before(() => {
-        standardUser.registrationData.email = generateMail(
-          randomString(),
-          true
-        );
-        cy.requireLoggedIn(standardUser);
-        cy.visit('/');
-      });
-
-      beforeEach(() => {
-        cy.restoreLocalStorage();
-      });
-
-      it('should cancel and go back to the homepage', () => {
-        cy.selectUserMenuOption({
-          option: 'Close Account',
+      describe('Logged in user', () => {
+        before(() => {
+          standardUser.registrationData.email = generateMail(
+            randomString(),
+            true
+          );
+          cy.requireLoggedIn(standardUser);
+          cy.visit('/');
         });
 
-        cy.get('cx-close-account a').click({ force: true });
-        cy.location('pathname').should('contain', '/');
-      });
-
-      it('should close account', () => {
-        cy.selectUserMenuOption({
-          option: 'Close Account',
+        beforeEach(() => {
+          cy.restoreLocalStorage();
         });
 
-        cy.intercept(
-          'DELETE',
-          `${Cypress.env('OCC_PREFIX')}/${Cypress.env('BASE_SITE')}/users/*`
-        ).as('deleteQuery');
+        it('should cancel and go back to the homepage', () => {
+          cy.selectUserMenuOption({
+            option: 'Close Account',
+          });
 
-        cy.location('pathname').should('contain', CLOSE_ACCOUNT_URL);
+          cy.get('cx-close-account a').click({ force: true });
+          cy.location('pathname').should('contain', '/');
+        });
 
-        cy.get('cx-close-account button').click({ force: true });
+        it('should close account', () => {
+          cy.selectUserMenuOption({
+            option: 'Close Account',
+          });
 
-        cy.get(
-          'cx-close-account-modal .cx-close-account-modal-container .cx-close-account-modal-footer button:first-of-type'
-        ).click();
+          cy.intercept(
+            'DELETE',
+            `${Cypress.env('OCC_PREFIX')}/${Cypress.env('BASE_SITE')}/users/*`
+          ).as('deleteQuery');
 
-        cy.wait('@deleteQuery');
+          cy.location('pathname').should('contain', CLOSE_ACCOUNT_URL);
 
-        cy.location('pathname').should('contain', '/');
+          cy.get('cx-close-account button').click({ force: true });
 
-        alerts
-          .getSuccessAlert()
-          .should('contain', 'Account closed with success');
-      });
+          cy.get(
+            'cx-close-account-modal .cx-close-account-modal-container .cx-close-account-modal-footer button:first-of-type'
+          ).click();
 
-      it('should not login with a closed account credentials', () => {
-        cy.visit('/login');
-        login(
-          standardUser.registrationData.email,
-          standardUser.registrationData.password
-        );
+          cy.wait('@deleteQuery');
 
-        cy.location('pathname').should('contain', '/login');
-        alerts.getErrorAlert().should('contain', 'User is disabled');
-      });
+          cy.location('pathname').should('contain', '/');
 
-      afterEach(() => {
-        cy.saveLocalStorage();
+          alerts
+            .getSuccessAlert()
+            .should('contain', 'Account closed with success');
+        });
+
+        it('should not login with a closed account credentials', () => {
+          cy.visit('/login');
+          login(
+            standardUser.registrationData.email,
+            standardUser.registrationData.password
+          );
+
+          cy.location('pathname').should('contain', '/login');
+          alerts.getErrorAlert().should('contain', 'User is disabled');
+        });
+
+        afterEach(() => {
+          cy.saveLocalStorage();
+        });
       });
     });
   });
-});
+
+myTest();
