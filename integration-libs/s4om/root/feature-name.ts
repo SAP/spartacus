fe685d2aad00973fc8e34cb111eb23f0bd4d878e
Sherwin Varghese <sherwinvarghese11@gmail.com>
2022-12-14 15:26:26 +0530

    Updating schematics tests

--- integration-libs/s4om/root/feature-name.ts
+++ integration-libs/s4om/root/feature-name.ts
@@ -1,1 +1,1 @@
-export const S4OM_FEATURE = 's4om';
+export const S4OM_FEATURE = 'S4HANA-Order-Management';
--- projects/storefrontapp/src/app/spartacus/features/s4om/s4om-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/s4om/s4om-feature.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CmsConfig, I18nConfig, provideConfig } from '@spartacus/core';
+import { I18nConfig, provideConfig } from '@spartacus/core';
 import {
   s4omTranslationChunksConfig,
-  s4omTranslations,
+  s4omTranslations
 } from '@spartacus/s4om/assets';
-import { S4omRootModule, S4OM_FEATURE } from '@spartacus/s4om/root';
+import { S4omRootModule } from '@spartacus/s4om/root';
 
 @NgModule({
   imports: [S4omRootModule],
@@ -18,4 +18,4 @@ import { S4omRootModule, S4OM_FEATURE } from '@spartacus/s4om/root';
     }),
   ],
 })
-export class S4OMFeatureModule {}
+export class S4OMFeatureModule { }
