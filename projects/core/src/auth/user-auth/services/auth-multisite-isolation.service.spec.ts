e5b0b022cb9f03ca699ebefd7de11a556c52a1c7
Mateusz <mateusz.kolasa@sap.com>
2022-12-14 21:16:24 +0100

    Add unit tests

--- projects/core/src/auth/user-auth/services/auth-multisite-isolation.service.spec.ts
+++ projects/core/src/auth/user-auth/services/auth-multisite-isolation.service.spec.ts
@@ -2,15 +2,24 @@ import { inject, TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { AuthMultisiteIsolationService } from './auth-multisite-isolation.service';
 import { BaseSiteService } from '../../../site-context/facade/base-site.service';
+import { BaseSite } from '../../../model/misc.model';
+
+const mockBaseSite: BaseSite = {
+  uid: 'test-basesite',
+  isolated: true,
+};
+
+const mockDecorator = `|${mockBaseSite?.uid}`;
 
 class MockBaseSiteService {
-  get(): Observable<string> {
-    return of();
+  get(): Observable<BaseSite> {
+    return of(mockBaseSite);
   }
 }
 
 describe('AuthMultisiteIsolationService', () => {
   let service: AuthMultisiteIsolationService;
+  let baseSiteService: BaseSiteService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,6 +30,7 @@ describe('AuthMultisiteIsolationService', () => {
     });
 
     service = TestBed.inject(AuthMultisiteIsolationService);
+    baseSiteService = TestBed.inject(BaseSiteService);
   });
 
   it('should be injected', inject(
@@ -32,5 +42,23 @@ describe('AuthMultisiteIsolationService', () => {
 
   it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should getBaseSiteDecorator() return decorator if `isolated` property is `true`', () => {
+    spyOn(baseSiteService, 'get').and.returnValue(of(mockBaseSite));
+
+    const decorator = service.getBaseSiteDecorator();
+
+    expect(decorator).toBe(mockDecorator);
+  });
+
+  it('should getBaseSiteDecorator() return empty string decorator if `isolated` is `false`', () => {
+    spyOn(baseSiteService, 'get').and.returnValue(
+      of({ ...mockBaseSite, ...{ isolated: false } })
+    );
+
+    const decorator = service.getBaseSiteDecorator();
+
+    expect(decorator).toBe('');
   });
 });
