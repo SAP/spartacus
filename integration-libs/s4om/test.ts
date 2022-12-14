cdd144bd010da9cb812c3109d4dd408049fa2651
Krzysztof Platis <platonn.git@gmail.com>
2022-12-14 18:45:16 +0100

    (s4om configuration test) fix linting for SonarQube

--- integration-libs/s4om/test.ts
+++ integration-libs/s4om/test.ts
@@ -6,13 +6,13 @@
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js';
-import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js';
+import 'zone.js/testing';
 
 declare const require: {
   context(
@@ -34,4 +34,4 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context.keys().forEach(context);
