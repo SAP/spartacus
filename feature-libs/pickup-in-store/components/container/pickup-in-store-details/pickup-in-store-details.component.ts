89b3530fe376c86ecd6478a6e8bbedf18fbf940e
I554698 <surya.singh@sap.com>
2022-12-15 11:00:41 +0000

    feat(pickup in store on delivery mode page)

--- feature-libs/pickup-in-store/components/container/pickup-in-store-details/pickup-in-store-details.component.ts
+++ feature-libs/pickup-in-store/components/container/pickup-in-store-details/pickup-in-store-details.component.ts
@@ -6,8 +6,8 @@
 
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { PickupLocationsSearchFacade } from '@spartacus/pickup-in-store/root';
+import { ICON_TYPE } from '@spartacus/storefront';
 import { DeliveryPointsService } from '../../services/delivery-points.service';
-
 @Component({
   selector: 'cx-pick-up-in-store-details',
   templateUrl: 'pickup-in-store-details.component.html',
@@ -16,18 +16,27 @@ import { DeliveryPointsService } from '../../services/delivery-points.service';
 })
 export class PickUpInStoreDetailsComponent implements OnInit {
   // TODO Remove the pickup-in-store-details.component.scss and move to its proper place
+  readonly ICON_TYPE = ICON_TYPE;
   deliveryPointsOfService$ =
     this.deliveryPointsService.getDeliveryPointsOfService();
   compact = false;
+  openHoursOpen: Record<number, boolean> = {};
+
   constructor(
     protected deliveryPointsService: DeliveryPointsService,
     protected pickupLocationsSearchService: PickupLocationsSearchFacade
   ) {}
-
   ngOnInit(): void {
     this.deliveryPointsOfService$.subscribe((d) => {
       console.clear();
       console.log(d);
     });
+  }
+
+  toggleOpenHours(index: number): boolean {
+    this.openHoursOpen[index] = !(
+      this.openHoursOpen && this.openHoursOpen[index]
+    );
+    return false;
   }
 }
