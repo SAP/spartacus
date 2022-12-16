b60a3a8a7ba1eb3db5cf02c8b2d74e1b05e59f83
Christoph Hinssen <christoph.hinssen@sap.com>
2022-12-16 14:28:05 +0100

    fix: get rid of console output during test exec

--- feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.spec.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Directive, Input } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { UntypedFormControl } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -44,6 +45,20 @@ function initTestComponent() {
   fixture.detectChanges();
 }
 
+@Component({
+  selector: 'cx-configurator-overview-filter-bar',
+  template: '',
+})
+class MockConfiguratorOverviewFilterBarComponent {
+  @Input() config: Configurator.ConfigurationWithOverview;
+}
+
+@Directive({
+  selector: '[formControl]',
+})
+export class MockFormControlDirective {
+  @Input('formControl') protected config: any;
+}
 describe('ConfiguratorOverviewFilterComponent', () => {
   describe('in a component test environment', () => {
     beforeEach(
@@ -52,7 +67,11 @@ describe('ConfiguratorOverviewFilterComponent', () => {
         initMocks();
         TestBed.configureTestingModule({
           imports: [I18nTestingModule],
-          declarations: [ConfiguratorOverviewFilterComponent],
+          declarations: [
+            ConfiguratorOverviewFilterComponent,
+            MockConfiguratorOverviewFilterBarComponent,
+            MockFormControlDirective,
+          ],
           providers: [
             {
               provide: ConfiguratorCommonsService,
