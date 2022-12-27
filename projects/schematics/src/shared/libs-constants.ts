bb0d1260f990c10e495ed60fd897ab7e809cac7d
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2022-12-27 09:43:16 -0500

    test

--- projects/schematics/src/shared/libs-constants.ts
+++ projects/schematics/src/shared/libs-constants.ts
@@ -102,7 +102,7 @@ export const SPARTACUS_ORGANIZATION_USER_REGISTRATION_ROOT = `@spartacus/organiz
 export const SPARTACUS_ORGANIZATION_USER_REGISTRATION_ASSETS = `@spartacus/organization/user-registration/assets`;
 export const SPARTACUS_ORGANIZATION_UNIT_ORDER = `@spartacus/organization/unit-order`;
 export const SPARTACUS_ORGANIZATION_UNIT_ORDER_ROOT = `@spartacus/organization/unit-order/root`;
-export const SSPARTACUS_ORGANIZATION_UNIT_ORDER_ASSETS = `@spartacus/organization/unit-order/assets`;
+export const SPARTACUS_ORGANIZATION_UNIT_ORDER_ASSETS = `@spartacus/organization/unit-order/assets`;
 
 export const SPARTACUS_ORGANIZATION_ACCOUNT_SUMMARY = `@spartacus/organization/account-summary`;
 export const SPARTACUS_ORGANIZATION_ACCOUNT_SUMMARY_ROOT = `@spartacus/organization/account-summary/root`;
