ec68b6b677bfebf6bdac866503b636390c8c05c3
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2022-12-28 13:37:38 -0500

    update import

--- feature-libs/organization/unit-order/components/unit-order-components.module.ts
+++ feature-libs/organization/unit-order/components/unit-order-components.module.ts
@@ -7,7 +7,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UnitOrderDetailsOrderEntriesContextToken } from '@spartacus/organization/unit-order/root';
-import { UnitOrderDetailsOrderEntriesContext } from './page-context';
+import { UnitOrderDetailsOrderEntriesContext } from './page-context/unit-order-details-order-entries.context';
 import { UnitLevelOrderDetailModule } from './unit-level-order-detail/unit-level-order-detail.module';
 import { UnitLevelOrderHistoryModule } from './unit-level-order-history';
 
