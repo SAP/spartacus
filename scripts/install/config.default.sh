4c1e10e3e468938a6a3658336a4802d710692867
I506207 <brian.gamboc-javiniar@sap.com>
2022-12-30 02:17:40 -0500

    it was not running my branch fix all along................

--- scripts/install/config.default.sh
+++ scripts/install/config.default.sh
@@ -41,7 +41,7 @@ SPARTACUS_PROJECTS=(
         )
 
 SPARTACUS_REPO_URL="https://github.com/SAP/spartacus.git"
-BRANCH='develop'
+BRANCH='feature/CXSPA-2128'
 
 # custom location for the installation output
 # BASE_DIR='/tmp/'
