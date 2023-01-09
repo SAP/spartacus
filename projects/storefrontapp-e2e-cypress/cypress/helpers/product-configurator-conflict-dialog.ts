fefcc452ad35243b10c50342edcc4abb3838c818
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2023-01-09 17:43:02 +0100

    test: skeleton for immediate conflict resolution test

--- projects/storefrontapp-e2e-cypress/cypress/helpers/product-configurator-conflict-dialog.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/product-configurator-conflict-dialog.ts
@@ -6,10 +6,37 @@
 
 //import * as configurationVc from './product-configurator-vc';
 
-export function checkConflictDialogOpen() {
+export function checkIsOpen() {
   cy.get('cx-configurator-conflict-solver-dialog').should('be.visible');
 }
 
-export function checkConflictDialogClosed() {
+export function checkIsClosed() {
   cy.get('cx-configurator-conflict-solver-dialog').should('not.exist');
 }
+
+export function checkDisplayedConflict() {
+  checkHasTitle();
+  checkHasCloseButton();
+  checkHasResolveMessage();
+  checkHasDescription();
+}
+
+export function checkHasTitle() {
+  cy.get('.cx-dialog-title').should('be.visible');
+}
+
+export function checkHasCloseButton() {
+  cy.get('button.close').should('be.visible');
+}
+
+export function checkHasResolveMessage() {
+  cy.get('.cx-dialog-body .cx-msg-warning').should('be.visible');
+}
+
+export function checkHasDescription() {
+  cy.get('cx-configurator-conflict-description').should('be.visible');
+}
+
+
+
+
