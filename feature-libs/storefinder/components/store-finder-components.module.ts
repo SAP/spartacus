6612c8bfd60e4083f49268080ed706bb7f664a33
Hak Woo Kim <kimhakwoo@hotmail.com>
2023-01-10 14:02:58 -0500

    fix: update tab index for search icon (CXSPA-948) (#16750)
    * update tab index
    
    * update md file for changes
    
    * added deprecation messages
    
    Co-authored-by: Hakwoo Kim <hakwoo.kim@sap.com>

--- feature-libs/storefinder/components/store-finder-components.module.ts
+++ feature-libs/storefinder/components/store-finder-components.module.ts
@@ -8,7 +8,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { I18nModule, provideDefaultConfig, UrlModule } from '@spartacus/core';
+import {
+  FeaturesConfigModule,
+  I18nModule,
+  provideDefaultConfig,
+  UrlModule,
+} from '@spartacus/core';
 import { StoreFinderCoreModule } from '@spartacus/storefinder/core';
 import {
   IconModule,
@@ -39,6 +44,8 @@ import { StoreFinderComponent } from './store-finder/store-finder.component';
     StoreFinderCoreModule,
     I18nModule,
     IconModule,
+    // TODO:(CXSPA-1695) #deprecation for next major release remove below feature config
+    FeaturesConfigModule,
   ],
   providers: [
     provideDefaultConfig({
