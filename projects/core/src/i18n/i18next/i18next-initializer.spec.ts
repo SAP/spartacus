8052ff051129d6d459d05059a931b824b5e23101
Krzysztof Platis <platonn.git@gmail.com>
2023-01-10 18:36:49 +0100

    draft names of unit tests  of i18next initializer and suggest extracting a class for I18nextHttpClient

--- projects/core/src/i18n/i18next/i18next-initializer.spec.ts
+++ projects/core/src/i18n/i18next/i18next-initializer.spec.ts
@@ -1,2 +1,39 @@
 // SPIKE TODO: write unit tests - possibly reuse from `i18next-init.spec.ts`
-describe('I18nextInitializer', () => {});
+describe('I18nextInitializer', () => {
+  /*
+
+  // SPIKE TODO: possibly extract to separate Injectable class I18nextHttpClient
+  I18nextHttpClient
+
+    when config 'i18n.backend.loadPath' is set
+      should use angular http client for loading translations from backend
+      should use the loadPath for loading translations from backend
+
+      forwards success response to i18next callback
+      forwards failure response to i18next callback
+    
+      //SPIKE TODO: copy all unit tests of getLoadPath
+
+
+
+      when using i18next http backend
+        should set i18next config `backend.reloadInterval` to false
+
+  -----------------------------------------------------------------------------------
+
+  should set fallbackLang
+  should set debug flag
+  should set interpolation options
+  should set i18next config `ns` to empty array
+  should set i18next config `resources` to empty object
+
+  when i18n.backend.loadPath is provided
+    should set reloadInterval to false
+    should set the i18next http client
+    should set the loadPath
+
+  should populate i18next with static translations right after initialization
+
+  should change i18next language when language is changed
+  */
+});
