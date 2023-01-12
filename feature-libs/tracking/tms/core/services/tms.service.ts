ebce512de9e7571c6eeb2cb6ddbf7cdf957c2473
Zeyber <rotherham.c@gmail.com>
2023-01-12 17:09:40 +0100

    reduce tms complexity

--- feature-libs/tracking/tms/core/services/tms.service.ts
+++ feature-libs/tracking/tms/core/services/tms.service.ts
@@ -71,7 +71,7 @@ export class TmsService implements OnDestroy {
             );
           }
 
-          event = collector.map ? collector.map(event) : event;
+          event = collector.map?.(event) ?? event;
           collector.pushEvent(
             collectorConfig,
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
