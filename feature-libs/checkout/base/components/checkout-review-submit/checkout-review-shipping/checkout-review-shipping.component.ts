996865e170d2e0743f06fa26e193a1f4892ab8d1
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2023-01-13 11:01:14 -0500

    remove css

--- feature-libs/checkout/base/components/checkout-review-submit/checkout-review-shipping/checkout-review-shipping.component.ts
+++ feature-libs/checkout/base/components/checkout-review-submit/checkout-review-shipping/checkout-review-shipping.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cx-checkout-review-shipping',
   templateUrl: './checkout-review-shipping.component.html',
-  styleUrls: ['./checkout-review-shipping.component.css']
 })
 export class CheckoutReviewShippingComponent implements OnInit {
 
