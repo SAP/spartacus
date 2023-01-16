4a7f75071ab30e470d63bda879fe8ea1db39c934
anjana-bl <anjana.b.l@sap.com>
2023-01-16 21:39:43 +0530

    updating the config

---
+++ integration-libs/cdc/manage-users/public_api.ts
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export * from './manage-users-list/cdc-list.module';
+export * from './manage-users-list/cdc-list.component';
+export * from './manage-users-list/manage-users.service';
+export * from './user-details/cdc-user-details.component';
+export * from './user-details/cdc-user-details.module';
--- projects/storefrontapp/src/app/spartacus/features/organization/organization-administration-wrapper.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/organization/organization-administration-wrapper.module.ts
@@ -1,8 +1,13 @@
-import { CdcUserDetailsModule } from 'integration-libs/cdc/manage-users/user-details/cdc-user-details.module';
-import { CdcListModule } from 'integration-libs/cdc/manage-users/manage-users-list/cdc-list.module';
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { environment } from 'projects/storefrontapp/src/environments/environment';
 import { NgModule, Type } from '@angular/core';
 import { AdministrationModule } from '@spartacus/organization';
+import { CdcListModule, CdcUserDetailsModule } from '@spartacus/cdc/manage-users';
 
 const extensions: Type<any>[] = [];
 if (environment.cdc) {
