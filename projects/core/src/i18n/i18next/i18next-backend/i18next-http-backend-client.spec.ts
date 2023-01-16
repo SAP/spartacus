bc339d8822f1886063a38b018c1555ecf3d5b0c6
Krzysztof Platis <platonn.git@gmail.com>
2023-01-17 00:38:56 +0100

    test: i18next http backend client

--- projects/core/src/i18n/i18next/i18next-backend/i18next-http-backend-client.spec.ts
+++ projects/core/src/i18n/i18next/i18next-backend/i18next-http-backend-client.spec.ts
@@ -9,68 +9,81 @@ forwards success response to i18next callback
 forwards failure response to i18next callback
 */
 
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { RequestCallback } from 'i18next-http-backend';
+import { I18NEXT_HTTP_BACKEND_CLIENT } from './i18next-http-backend-client';
+
 // SPIKE TODO: fix the tests below:
 
 // imports: [
 //   HttpClientTestingModule,
 // ],
 
-// describe('i18nextGetHttpClient should return a http client that', () => {
-//   let httpMock: HttpTestingController;
-//   let httpClient: HttpClient;
-//   let req: TestRequest;
-//   let testCallback: RequestCallback;
+const testUrl = 'test/url';
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule],
-//     });
+fdescribe('I18NEXT_HTTP_BACKEND_CLIENT', () => {
+  let httpMock: HttpTestingController;
+  let req: TestRequest;
+  let testCallback: RequestCallback;
 
-//     httpMock = TestBed.inject(HttpTestingController);
-//     httpClient = TestBed.inject(HttpClient);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
 
-//     const func = i18nextGetHttpClient(httpClient);
-//     testCallback = jasmine.createSpy('testCallback');
-//     func({}, testUrl, {}, testCallback);
-//     req = httpMock.expectOne({ url: testUrl, method: 'GET' });
-//   });
+    httpMock = TestBed.inject(HttpTestingController);
 
-//   afterEach(() => {
-//     httpMock.verify();
-//   });
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const i18nextHttpBackendClient = TestBed.inject(
+      I18NEXT_HTTP_BACKEND_CLIENT
+    )!;
+    testCallback = jasmine.createSpy('testCallback');
+    i18nextHttpBackendClient({}, testUrl, {}, testCallback);
+    req = httpMock.expectOne({ url: testUrl, method: 'GET' });
+  });
 
-//   it('requests for responseType text', () => {
-//     expect(req.request.responseType).toBe('text');
-//   });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
-//   it('forwards success response to i18next callback', () => {
-//     req.flush('testResponse');
+  it('requests for responseType text', () => {
+    expect(req.request.responseType).toBe('text');
+  });
 
-//     expect(testCallback).toHaveBeenCalledWith(null, {
-//       status: 200,
-//       data: 'testResponse',
-//     });
-//   });
+  it('forwards success response to i18next callback', () => {
+    req.flush('testResponse');
 
-//   it('forwards failure response to i18next callback', () => {
-//     const error = 'test error message';
-//     const statusText = 'Not Found';
-//     const status = 404;
-//     const expectedHttpErrorResponse = new HttpErrorResponse({
-//       status,
-//       error,
-//       statusText,
-//       url: testUrl,
-//     });
+    expect(testCallback).toHaveBeenCalledWith(null, {
+      status: 200,
+      data: 'testResponse',
+    });
+  });
 
-//     req.flush(error, {
-//       status,
-//       statusText,
-//     });
-//     expect(testCallback).toHaveBeenCalledWith(expectedHttpErrorResponse, {
-//       status,
-//       // a workaround for https://github.com/i18next/i18next-http-backend/issues/82
-//       data: null as any,
-//     });
-//   });
-// });
+  it('forwards failure response to i18next callback', () => {
+    const error = 'test error message';
+    const statusText = 'Not Found';
+    const status = 404;
+    const expectedHttpErrorResponse = new HttpErrorResponse({
+      status,
+      error,
+      statusText,
+      url: testUrl,
+    });
+
+    req.flush(error, {
+      status,
+      statusText,
+    });
+    expect(testCallback).toHaveBeenCalledWith(expectedHttpErrorResponse, {
+      status,
+      // a workaround for https://github.com/i18next/i18next-http-backend/issues/82
+      data: null as any,
+    });
+  });
+});
