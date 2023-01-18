6de211ebef3c540f0675707a5baaae8e18b62085
Patryk Smul <psmul@divante.pl>
2023-01-18 15:23:29 +0100

    feat: provide unit tests for business logic

---
+++ integration-libs/opf/occ/adapters/occ-opf.adapter.spec.ts
@@ -0,0 +1,101 @@
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  BaseOccUrlProperties,
+  ConverterService,
+  DynamicAttributes,
+} from '@spartacus/core';
+import {
+  OpfEndpointsService,
+  OPF_ACTIVE_CONFIGURATION_NORMALIZER,
+} from '@spartacus/opf/core';
+import { ActiveConfiguration } from '@spartacus/opf/root';
+import { OccOpfAdapter } from './occ-opf.adapter';
+
+const mockResponse: ActiveConfiguration[] = [];
+
+export class MockOpfEndpointsService implements Partial<OpfEndpointsService> {
+  buildUrl(
+    endpoint: string,
+    _attributes?: DynamicAttributes,
+    _propertiesToOmit?: BaseOccUrlProperties
+  ) {
+    return this.getEndpoint(endpoint);
+  }
+  getEndpoint(endpoint: string) {
+    if (!endpoint.startsWith('/')) {
+      endpoint = '/' + endpoint;
+    }
+    return endpoint;
+  }
+  getBaseUrl() {
+    return '';
+  }
+  isConfigured() {
+    return true;
+  }
+}
+
+describe('OccOpfAdapter', () => {
+  let occOpfAdapter: OccOpfAdapter;
+  let httpMock: HttpTestingController;
+  let converter: ConverterService;
+  let opfEndpointsService: OpfEndpointsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        OccOpfAdapter,
+        {
+          provide: OpfEndpointsService,
+          useClass: MockOpfEndpointsService,
+        },
+      ],
+    });
+
+    occOpfAdapter = TestBed.inject(OccOpfAdapter);
+    httpMock = TestBed.inject(HttpTestingController);
+    converter = TestBed.inject(ConverterService);
+    opfEndpointsService = TestBed.inject(OpfEndpointsService);
+    spyOn(converter, 'convert').and.callThrough();
+    spyOn(converter, 'pipeable').and.callThrough();
+    spyOn(opfEndpointsService, 'buildUrl').and.callThrough();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should return cart modification list based on provided params', () => {
+    occOpfAdapter.getActiveConfigurations().subscribe();
+
+    const mockReq = httpMock.expectOne((req) => {
+      return req.method === 'GET';
+    });
+
+    expect(opfEndpointsService.buildUrl).toHaveBeenCalled();
+
+    expect(
+      mockReq.request.headers.has('sap-commerce-cloud-public-key')
+    ).toEqual(true);
+    expect(mockReq.cancelled).toBeFalsy();
+    expect(mockReq.request.responseType).toEqual('json');
+    mockReq.flush(mockResponse);
+  });
+
+  it('should use converter', () => {
+    occOpfAdapter.getActiveConfigurations().subscribe();
+    httpMock
+      .expectOne((req) => {
+        return req.method === 'GET';
+      })
+      .flush(mockResponse);
+    expect(converter.pipeable).toHaveBeenCalledWith(
+      OPF_ACTIVE_CONFIGURATION_NORMALIZER
+    );
+  });
+});
