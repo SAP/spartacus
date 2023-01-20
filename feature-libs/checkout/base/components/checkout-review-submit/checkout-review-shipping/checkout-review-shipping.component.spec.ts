0240725b482477f9bf3666ea21c96269f78d7e4e
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2023-01-19 22:04:52 -0500

    add unit tests

--- feature-libs/checkout/base/components/checkout-review-submit/checkout-review-shipping/checkout-review-shipping.component.spec.ts
+++ feature-libs/checkout/base/components/checkout-review-submit/checkout-review-shipping/checkout-review-shipping.component.spec.ts
@@ -1,6 +1,97 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CheckoutDeliveryAddressFacade, CheckoutDeliveryModesFacade, CheckoutStep, CheckoutStepType } from '@spartacus/checkout/base/root';
+import { Address, Country, I18nTestingModule } from '@spartacus/core';
+import { IconTestingModule } from 'projects/storefrontlib/cms-components/misc/icon/testing/icon-testing.module';
+import { of } from 'rxjs';
+import createSpy = jasmine.createSpy;
 
 import { CheckoutReviewShippingComponent } from './checkout-review-shipping.component';
+import { CheckoutStepService } from '../../services/checkout-step.service';
+import { ActiveCartFacade, DeliveryMode, OrderEntry } from '@spartacus/cart/base/root';
+import { Pipe, PipeTransform } from '@angular/core';
+
+const mockCheckoutStep: CheckoutStep = {
+  id: 'step',
+  name: 'name',
+  routeName: '/route',
+  type: [CheckoutStepType.DELIVERY_ADDRESS],
+};
+
+class MockCheckoutStepService {
+  steps$ = of([
+    {
+      id: 'step1',
+      name: 'step1',
+      routeName: 'route1',
+      type: [CheckoutStepType.DELIVERY_ADDRESS],
+    },
+    {
+      id: 'step2',
+      name: 'step2',
+      routeName: 'route2',
+      type: [CheckoutStepType.DELIVERY_MODE],
+    },
+  ]);
+  getCheckoutStep = createSpy().and.returnValue(mockCheckoutStep);
+}
+
+const mockCountry: Country = {
+  isocode: 'JP',
+  name: 'Japan',
+};
+const mockAddress: Address = {
+  firstName: 'John',
+  lastName: 'Doe',
+  titleCode: 'mr',
+  line1: 'Toyosaki 2 create on cart',
+  line2: 'line2',
+  town: 'town',
+  region: { isocode: 'JP-27' },
+  postalCode: 'zip',
+  country: mockCountry,
+};
+
+class MockCheckoutDeliveryAddressService
+  implements Partial<CheckoutDeliveryAddressFacade>
+{
+  getDeliveryAddressState = createSpy().and.returnValue(
+    of({ loading: false, error: false, data: mockAddress })
+  );
+}
+
+class MockCheckoutDeliveryModesService
+  implements Partial<CheckoutDeliveryModesFacade>
+{
+  getSupportedDeliveryModesState = createSpy().and.returnValue(
+    of({ loading: false, error: false, data: [] })
+  );
+  getSelectedDeliveryModeState = createSpy().and.returnValue(
+    of({
+      loading: false,
+      error: false,
+      data: mockDeliveryMode,
+    })
+  );
+}
+
+class MockActiveCartService implements Partial<ActiveCartFacade> {
+  getDeliveryEntries = createSpy().and.returnValue(of(mockEntries));
+}
+
+const mockDeliveryMode: DeliveryMode = {
+  name: 'standard-gross',
+  description: 'Delivery mode test description',
+};
+
+const mockEntries: OrderEntry[] = [{ entryNumber: 123, quantity: 2, totalPrice: { formattedValue: "$8.20" }}, { entryNumber: 456 }];
+
+@Pipe({
+  name: 'cxUrl',
+})
+class MockUrlPipe implements PipeTransform {
+  transform(): any {}
+}
 
 describe('CheckoutReviewShippingComponent', () => {
   let component: CheckoutReviewShippingComponent;
@@ -8,7 +99,27 @@ describe('CheckoutReviewShippingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CheckoutReviewShippingComponent],
+      imports: [
+        I18nTestingModule,
+        RouterTestingModule,
+        IconTestingModule,
+      ],
+      declarations: [CheckoutReviewShippingComponent, MockUrlPipe],
+      providers: [
+        {
+          provide: CheckoutDeliveryAddressFacade,
+          useClass: MockCheckoutDeliveryAddressService,
+        },
+        {
+          provide: CheckoutDeliveryModesFacade,
+          useClass: MockCheckoutDeliveryModesService,
+        },
+        {
+          provide: CheckoutStepService,
+          useClass: MockCheckoutStepService,
+        },
+        { provide: ActiveCartFacade, useClass: MockActiveCartService },
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CheckoutReviewShippingComponent);
@@ -18,5 +129,88 @@ describe('CheckoutReviewShippingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to get delivery entries', () => {
+    let entries: OrderEntry[] | undefined;
+    component.entries$.subscribe((data: OrderEntry[]) => {
+      entries = data;
+    });
+
+    expect(entries).toEqual(mockEntries);
+  });
+
+  it('should be able to get steps', () => {
+    let steps: CheckoutStep[] | undefined;
+    component.steps$.subscribe((data) => (steps = data));
+
+    expect(steps?.[0]).toEqual({
+      id: 'step1',
+      name: 'step1',
+      routeName: 'route1',
+      type: [CheckoutStepType.DELIVERY_ADDRESS],
+    });
+    expect(steps?.[1]).toEqual({
+      id: 'step2',
+      name: 'step2',
+      routeName: 'route2',
+      type: [CheckoutStepType.DELIVERY_MODE],
+    });
+  });
+
+  it('should be able to get deliveryAddress', () => {
+    let deliveryAddress: Address | undefined;
+    component.deliveryAddress$.subscribe((data) => {
+      deliveryAddress = data;
+    });
+
+    expect(deliveryAddress).toEqual(mockAddress);
+  });
+
+  it('should be able to get deliveryMode if a mode is selected', () => {
+    let deliveryMode: DeliveryMode | undefined;
+    component.deliveryMode$.subscribe((data) => {
+      deliveryMode = data;
+    });
+
+    expect(deliveryMode).toEqual(mockDeliveryMode);
+  });
+
+  it('should call getDeliveryAddressCard(deliveryAddress, countryName) to get address card data', () => {
+    component
+      .getDeliveryAddressCard(mockAddress, 'Canada')
+      .subscribe((card) => {
+        expect(card.title).toEqual('addressCard.shipTo');
+        expect(card.textBold).toEqual('John Doe');
+        expect(card.text).toEqual([
+          'Toyosaki 2 create on cart',
+          'line2',
+          'town, JP-27, Canada',
+          'zip',
+          undefined,
+        ]);
+      });
+  });
+
+  it('should call getDeliveryModeCard(deliveryMode) to get delivery mode card data', () => {
+    const selectedMode: DeliveryMode = {
+      code: 'standard-gross',
+      name: 'Standard gross',
+      description: 'Standard Delivery description',
+      deliveryCost: {
+        formattedValue: '$9.99',
+      },
+    };
+    component.getDeliveryModeCard(selectedMode).subscribe((card) => {
+      expect(card.title).toEqual('checkoutMode.deliveryMethod');
+      expect(card.textBold).toEqual('Standard gross');
+      expect(card.text).toEqual(['Standard Delivery description', '$9.99']);
+    });
+  });
+
+  it('should get checkout step url', () => {
+    expect(
+      component.getCheckoutStepUrl(CheckoutStepType.DELIVERY_ADDRESS)
+    ).toEqual(mockCheckoutStep.routeName);
   });
 });
