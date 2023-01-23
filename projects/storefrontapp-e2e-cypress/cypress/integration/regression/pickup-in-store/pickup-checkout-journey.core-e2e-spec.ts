ac96e9edcab6edd2485d30e4a87968a8e772d69f
cawmichae <50734654+cawmichae@users.noreply.github.com>
2023-01-23 15:59:39 +0000

    Test/cxspa 817 (#16804)
    * Finish Cypress Test for A guest user logs in while checking out with BOPIS

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/pickup-in-store/pickup-checkout-journey.core-e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/pickup-in-store/pickup-checkout-journey.core-e2e-spec.ts
@@ -21,15 +21,16 @@ import { viewportContext } from '../../../helpers/viewport-context';
 - From the cart, the user can change the location they wish to pick up the product from.
 - The user also add another item only for delivery.(Multiple items in cart)
 - The user decides to login so the order will show in the user's account.
-TODO:- The logged in user checks out.
-TODO:- During checkout, the user can change the pickup location. (WIP)
-TODO:- During the order review, the user can change the pickup location.
+- The logged in user checks out.
+- During checkout, the user cannot change the pickup location. (WIP)
+- During the order review, the user cannot change the pickup location.
 TODO:- The user completes checkout and sees the order details. On here they can see their pickup location for the picket item and the delivery for the delivered item. (WIP)
 */
 
 describe('Pickup Delivery Option - A guest user logs in while checking out with BOPIS', () => {
   viewportContext(['desktop'], () => {
     beforeEach(() => {
+      // A guest user navigates to a PDP wishing to buy the product.
       configureApparelProduct();
     });
 
@@ -58,15 +59,20 @@ describe('Pickup Delivery Option - A guest user logs in while checking out with 
         url,
       }).as('changePickupInStoreInOrderReview');
 
+      // The user has the choice of whether they want the product delivered (the default) or whether they want to pick it up in store.
       cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.visible');
       cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.visible');
       cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.checked');
 
+      // The user selects pickup in store.
       cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).click();
       cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.checked');
+
+      // The user selects which store they want to collect from (by default the last store they selected, falling back to the nearest store). (NEEDS TO BE CORRECTED)
       cy.get(L.USE_MY_LOCATION).click();
       cy.get(L.ACTIVE_PICK_UP_IN_STORE_BUTTON).first().click();
 
+      // The user adds the product to the cart. (The cart entries post call will have the "deliveryPointOfService" field).
       cy.get(L.ADD_TO_CART).click();
 
       cy.wait('@apiAddToCart').then((interception) => {
@@ -102,6 +108,7 @@ describe('Pickup Delivery Option - A guest user logs in while checking out with 
           'The user also add another item only for delivery. (Multiple items in cart)'
         );
 
+        // The user also add another item only for delivery.(Multiple items in cart)
         visitAlternativeProductPage();
         cy.get(L.ADD_TO_CART).click();
         cy.get(L.VIEW_CART).click();
@@ -116,12 +123,27 @@ describe('Pickup Delivery Option - A guest user logs in while checking out with 
         });
 
         cy.log('The logged in user checks out.');
+
+        // TODO
+        /*
+          Not ideal, but we need this to allow time for the event handlers to have been attached
+          After cypress version 6 we can use
+            cy.get('#element').listeners().then(events => {
+              expect(events.click).to.have.lengthOf(1);
+            });
+         */
+        cy.wait(1000);
+
         cy.get(L.MINI_CART_BUTTON).click();
         cy.get(L.PROCEED_TO_CHECKOUT_BUTTON).should('be.visible').click();
 
         cy.log('During checkout, the user fills address form.');
         fillAddressForm(defaultAddress);
         cy.get(L.CHECKOUT_ADDRESS_FORM_SUBMIT_BUTTON).click();
+
+        // During checkout, the user cannot change the pickup location. (WIP)
+        cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('not.exist');
+        cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('not.exist');
 
         cy.log('During checkout, the user selects delivery mode.');
         cy.get(L.CHECKOUT_DELIVERY_MODE_CONTINUE_BUTTON).click();
@@ -131,21 +153,14 @@ describe('Pickup Delivery Option - A guest user logs in while checking out with 
         cy.get(L.CHECKOUT_PAYMENT_FORM_CONTINUE_BUTTON).click();
 
         cy.log('order review');
-        cy.get(L.CHANGE_STORE_LINK).first().click();
-        cy.get(L.PICKUP_IN_STORE_MODAL).should('exist');
-        cy.get(L.USE_MY_LOCATION).click({ force: true });
 
-        cy.get(L.ACTIVE_PICK_UP_IN_STORE_BUTTON).last().click();
-
-        cy.wait('@changePickupInStoreInOrderReview').then((interception) => {
-          expect(interception.response.statusCode).to.equal(200);
-          expect(interception.request.body).to.have.property(
-            'deliveryPointOfService'
-          );
-        });
+        cy.get(
+          `cx-pickup-in-store-details-review ${L.PICKUP_OPTIONS_RADIO_DELIVERY}`
+        ).should('not.exist');
 
         cy.get(L.REVIEW_ORDER_TERM_CONDITION).click();
         cy.get(L.REVIEW_ORDER_SUBMIT).click();
+        cy.get(L.ORDER_CONFIRMATION).should('exist');
       });
     });
   });
