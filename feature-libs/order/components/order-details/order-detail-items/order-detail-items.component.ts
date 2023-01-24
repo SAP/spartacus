7772037da3d73c9c8e37e58316e4b0b295edbf0b
Weizheng Gao <weizheng.gao@sap.com>
2023-01-24 15:45:18 -0400

    feature/CXSPA-2200

--- feature-libs/order/components/order-details/order-detail-items/order-detail-items.component.ts
+++ feature-libs/order/components/order-details/order-detail-items/order-detail-items.component.ts
@@ -4,13 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Component, OnInit } from '@angular/core';
-import { CartOutlets, PromotionLocation } from '@spartacus/cart/base/root';
-import { CmsOrderDetailItemsComponent } from '@spartacus/core';
+import { Component } from '@angular/core';
+import {
+  CartOutlets,
+  DeliveryMode,
+  PromotionLocation,
+} from '@spartacus/cart/base/root';
+import {
+  Address,
+  CmsOrderDetailItemsComponent,
+  TranslationService,
+} from '@spartacus/core';
 import { Consignment, Order } from '@spartacus/order/root';
-import { CmsComponentData } from '@spartacus/storefront';
+import { Card, CmsComponentData } from '@spartacus/storefront';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { OrderDetailsService } from '../order-details.service';
 import {
   cancelledValues,
@@ -21,41 +29,53 @@ import {
   selector: 'cx-order-details-items',
   templateUrl: './order-detail-items.component.html',
 })
-export class OrderDetailItemsComponent implements OnInit {
-  constructor(
-    protected orderDetailsService: OrderDetailsService,
-    protected component: CmsComponentData<CmsOrderDetailItemsComponent>
-  ) {}
-
+export class OrderDetailItemsComponent {
   readonly CartOutlets = CartOutlets;
 
   promotionLocation: PromotionLocation = PromotionLocation.Order;
   order$: Observable<Order> = this.orderDetailsService.getOrderDetails();
-  consignments$: Observable<Consignment[] | undefined>;
 
   enableAddToCart$: Observable<boolean | undefined> = this.component.data$.pipe(
     map((data) => data.enableAddToCart)
   );
 
-  ngOnInit() {
-    this.consignments$ = this.order$.pipe(
-      map((order) => {
-        const grouped = order.consignments?.reduce((result, current) => {
-          const key = this.getStatusGroupKey(current.status || '');
-          result[key] = result[key] || [];
-          result[key].push(current);
-          return result;
-        }, {} as { [key: string]: Consignment[] });
+  pickupConsignments$ = this.order$.pipe(
+    map((order) =>
+      order.consignments?.filter(
+        (entry) => entry.deliveryPointOfService !== undefined
+      )
+    ),
+    map((conginments) => this.groupConsignments(conginments))
+  );
 
-        return grouped
-          ? [
-              ...(grouped[1] || []),
-              ...(grouped[0] || []),
-              ...(grouped[-1] || []),
-            ]
-          : undefined;
-      })
-    );
+  deliveryConsignments$ = this.order$.pipe(
+    map((order) =>
+      order.consignments?.filter(
+        (entry) => entry.deliveryPointOfService === undefined
+      )
+    ),
+    map((conginments) => this.groupConsignments(conginments))
+  );
+
+  constructor(
+    protected orderDetailsService: OrderDetailsService,
+    protected component: CmsComponentData<CmsOrderDetailItemsComponent>,
+    protected translation: TranslationService
+  ) {}
+
+  protected groupConsignments(
+    consignments: Consignment[] | undefined
+  ): Consignment[] | undefined {
+    const grouped = consignments?.reduce((result, current) => {
+      const key = this.getStatusGroupKey(current.status || '');
+      result[key] = result[key] || [];
+      result[key].push(current);
+      return result;
+    }, {} as { [key: string]: Consignment[] });
+
+    return grouped
+      ? [...(grouped[1] || []), ...(grouped[0] || []), ...(grouped[-1] || [])]
+      : undefined;
   }
 
   /**
@@ -71,5 +91,51 @@ export class OrderDetailItemsComponent implements OnInit {
       return -1;
     }
     return 1;
+  }
+
+  getAddressCardContent(deliveryAddress: Address): Observable<Card> {
+    return this.translation.translate('addressCard.shipTo').pipe(
+      filter(() => Boolean(deliveryAddress)),
+      map((textTitle) => {
+        const formattedAddress = this.normalizeFormattedAddress(
+          deliveryAddress.formattedAddress ?? ''
+        );
+
+        return {
+          title: textTitle,
+          textBold: `${deliveryAddress.firstName} ${deliveryAddress.lastName}`,
+          text: [formattedAddress, deliveryAddress.country?.name],
+        } as Card;
+      })
+    );
+  }
+
+  getDeliveryModeCardContent(deliveryMode: DeliveryMode): Observable<Card> {
+    return this.translation.translate('orderDetails.shippingMethod').pipe(
+      filter(() => Boolean(deliveryMode)),
+      map(
+        (textTitle) =>
+          ({
+            title: textTitle,
+            textBold: deliveryMode.name,
+            text: [
+              deliveryMode.description,
+              deliveryMode.deliveryCost?.formattedValue
+                ? deliveryMode.deliveryCost?.formattedValue
+                : '',
+            ],
+          } as Card)
+      )
+    );
+  }
+
+  private normalizeFormattedAddress(formattedAddress: string): string {
+    const addresses = formattedAddress
+      .split(',')
+      .map((address) => address.trim());
+
+    const newFormattedAddress = addresses.filter(Boolean).join(', ');
+
+    return newFormattedAddress;
   }
 }
