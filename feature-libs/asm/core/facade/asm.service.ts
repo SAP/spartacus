2573b1a3732f9b5713c5e7b1fc67cf7f08be4f65
Lee <justin.lee01@sap.com>
2023-01-25 16:17:28 -0500

    Removing unused methods in AsmService

--- feature-libs/asm/core/facade/asm.service.ts
+++ feature-libs/asm/core/facade/asm.service.ts
@@ -6,10 +6,6 @@
 
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import {
-  AsmCustomer360Request,
-  AsmCustomer360Response,
-} from '@spartacus/asm/root';
 import { Observable } from 'rxjs';
 import {
   AsmUi,
@@ -110,13 +106,5 @@ export class AsmService {
    */
   getAsmUiState(): Observable<AsmUi> {
     return this.store.pipe(select(AsmSelectors.getAsmUi));
-  }
-
-  fetchCustomer360Data(request: AsmCustomer360Request): void {
-    this.store.dispatch(new AsmActions.Customer360Get(request));
-  }
-
-  getCustomer360Data(): Observable<AsmCustomer360Response> {
-    return this.store.pipe(select(AsmSelectors.getCustomer360Data));
   }
 }
