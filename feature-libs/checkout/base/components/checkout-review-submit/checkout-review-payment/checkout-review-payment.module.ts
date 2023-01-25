cbafe2d9e3e45bf872138674e659dd1b86e769b0
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2023-01-25 09:49:57 -0500

    remove import

--- feature-libs/checkout/base/components/checkout-review-submit/checkout-review-payment/checkout-review-payment.module.ts
+++ feature-libs/checkout/base/components/checkout-review-submit/checkout-review-payment/checkout-review-payment.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutReviewPaymentComponent } from './checkout-review-payment.component';
-import { CardModule, IconModule, OutletModule } from '@spartacus/storefront';
+import { CardModule, IconModule } from '@spartacus/storefront';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import { RouterModule } from '@angular/router';
 
@@ -15,7 +15,6 @@ import { RouterModule } from '@angular/router';
     UrlModule,
     RouterModule,
     IconModule,
-    OutletModule,
   ],
 })
 export class CheckoutReviewPaymentModule {}
