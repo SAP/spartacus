896e9124ad479bdcb6c975353dc90c758c18d9b1
Stofftiger <moe@webmoe.com>
2023-01-25 18:48:37 +0100

    changes for accessibility

--- feature-libs/product-configurator/rulebased/components/attribute/types/read-only/configurator-attribute-read-only.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/read-only/configurator-attribute-read-only.component.ts
@@ -8,6 +8,9 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Configurator } from '../../../../core/model/configurator.model';
 import { ConfiguratorPriceComponentOptions } from '../../../price';
 import { ConfiguratorAttributeBaseComponent } from '../base/configurator-attribute-base.component';
+import { TranslationService } from '@spartacus/core';
+import { take } from 'rxjs/operators';
+
 @Component({
   selector: 'cx-configurator-attribute-read-only',
   templateUrl: './configurator-attribute-read-only.component.html',
@@ -17,6 +20,54 @@ export class ConfiguratorAttributeReadOnlyComponent extends ConfiguratorAttribut
   @Input() attribute: Configurator.Attribute;
   @Input() group: String;
   @Input() expMode: boolean;
+
+  constructor(protected translation: TranslationService) {
+    super();
+  }
+
+  getAriaLabel(
+    value: Configurator.Value,
+    attribute: Configurator.Attribute,
+    currentValue: String
+  ): string {
+    let ariaLabel = '';
+    if (value.valuePrice && value.valuePrice?.value !== 0) {
+      if (value.valuePriceTotal && value.valuePriceTotal?.value !== 0) {
+        this.translation
+          .translate(
+            'configurator.a11y.readOnlyValueOfAttributeFullWithPrice',
+            {
+              value: currentValue,
+              attribute: attribute.label,
+              price: value.valuePriceTotal.formattedValue,
+            }
+          )
+          .pipe(take(1))
+          .subscribe((text) => (ariaLabel = text));
+      } else {
+        this.translation
+          .translate(
+            'configurator.a11y.readOnlyValueOfAttributeFullWithPrice',
+            {
+              value: currentValue,
+              attribute: attribute.label,
+              price: value.valuePrice.formattedValue,
+            }
+          )
+          .pipe(take(1))
+          .subscribe((text) => (ariaLabel = text));
+      }
+    } else {
+      this.translation
+        .translate('configurator.a11y.readOnlyValueOfAttributeFull', {
+          value: currentValue,
+          attribute: attribute.label,
+        })
+        .pipe(take(1))
+        .subscribe((text) => (ariaLabel = text));
+    }
+    return ariaLabel;
+  }
 
   /**
    * Extract corresponding value price formula parameters.
