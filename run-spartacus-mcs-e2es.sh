2fec2f01043342dd03d02b9d78a27bf7647a2754
I506207 <brian.gamboc-javiniar@sap.com>
2023-01-27 13:40:05 -0500

    removed test values

--- run-spartacus-mcs-e2es.sh
+++ run-spartacus-mcs-e2es.sh
@@ -3,9 +3,6 @@
 # This is just for MCs. This file will be deleted in future updates, where we unify the scripts to take parameters and to run package scripts accordingly.
 # Remove all reference to MCs e2es/scripts from the repo when sample data from MCs and our core sample data aligns.
 
-export ENDPOINT_URL_PUBLIC_SPARTACUS="https://spartacusstore.cg79x9wuu9-ecdhcomme2-s1-public.model-t.myhybris.cloud"
-export ENDPOINT_URL_PUBLIC_API="https://api.cg79x9wuu9-ecdhcomme2-s1-public.model-t.myhybris.cloud"
-
 # install cypress standlone dependencies
 (cd projects/storefrontapp-e2e-cypress && yarn install)
 
