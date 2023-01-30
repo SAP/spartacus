15f0f451e187f7c429e9de0c17be39221267f42e
Michal Szczepaniak <michal.szczepaniak01@sap.com>
2023-01-30 12:39:02 +0100

    Refactor future stock service

--- feature-libs/product/future-stock/core/services/future-stock.service.ts
+++ feature-libs/product/future-stock/core/services/future-stock.service.ts
@@ -8,8 +8,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FutureStockFacade } from '@spartacus/product/future-stock/root';
 import {
-  Query,
-  QueryService,
   RoutingService,
   UserIdService,
 } from '@spartacus/core';
@@ -21,31 +19,25 @@ import { ProductFutureStock } from '../model';
 export class FutureStockService implements FutureStockFacade {
   protected readonly PRODUCT_KEY = 'productCode';
 
-  protected futureStockState$: Query<ProductFutureStock, unknown[]> =
-    this.queryService.create<ProductFutureStock>(
-      () =>
-        this.routingService.getRouterState().pipe(
-          withLatestFrom(this.userIdService.takeUserId()),
-          switchMap(([{ state }, userId]) => {
-            return this.futureStockConnector.getFutureStock(
-              userId,
-              state.params[this.PRODUCT_KEY]
-            );
-          })
-        ),
-      {}
+  protected futureStockState$: Observable<ProductFutureStock> =
+    this.routingService.getRouterState().pipe(
+      withLatestFrom(this.userIdService.takeUserId()),
+      switchMap(([{ state }, userId]) => {
+        return this.futureStockConnector.getFutureStock(
+          userId,
+          state.params[this.PRODUCT_KEY]
+        );
+      })
     );
-
   /**
    * Get future stock
    */
   getFutureStock(): Observable<ProductFutureStock | undefined> {
-    return this.futureStockState$.get();
+    return this.futureStockState$;
   }
 
   constructor(
     protected userIdService: UserIdService,
-    protected queryService: QueryService,
     protected futureStockConnector: FutureStockConnector,
     protected routingService: RoutingService
   ) {}
