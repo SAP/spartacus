c59c68d223e1ebe9dc21e6cf719bbe4e9adb9753
AshreeJay <52031769+AshreeJay@users.noreply.github.com>
2023-02-03 13:30:10 +0530

    created component my-account

---
+++ integration-libs/cdp/src/lib/my-account/my-account/my-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cx-my-account',
+  templateUrl: './my-account.component.html',
+  styleUrls: ['./my-account.component.css']
+})
+export class MyAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
