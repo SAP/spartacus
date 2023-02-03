f4ce0689e149c94974c87efa144af8bab442bf9f
AshreeJay <52031769+AshreeJay@users.noreply.github.com>
2023-02-03 12:11:35 +0530

    generated cdp library

---
+++ integration-libs/cdp/src/lib/cdp.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cx-cdp',
+  template: `
+    <p>
+      cdp works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CdpComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ integration-libs/cdp/src/lib/cdp.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CdpComponent } from './cdp.component';
+
+
+
+@NgModule({
+  declarations: [
+    CdpComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    CdpComponent
+  ]
+})
+export class CdpModule { }
+
+
+
