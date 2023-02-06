b55e8abae5280136fba65a8193ce56f2bc8e0521
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2023-02-06 10:58:08 +0100

    feat: enforce immediate conflict resolution for VC configurator (#15996)
    The backend can signal that for (AVC driven) configurations, conflicts must be immediately resolved. In that case, the conflict group will be rendered in a popup.
    
    Co-authored-by: Ulrich Stellmacher <ulrich.stellmacher@sap.com>
    Co-authored-by: Christoph Hinssen <christoph.hinssen@sap.com> 
    Co-authored-by: Sebastian Stein <sebastian.stein@sap.com>

--- feature-libs/product-configurator/rulebased/components/rulebased-configurator-components.module.ts
+++ feature-libs/product-configurator/rulebased/components/rulebased-configurator-components.module.ts
@@ -8,6 +8,7 @@ import { NgModule } from '@angular/core';
 import { ConfiguratorAddToCartButtonModule } from './add-to-cart-button/configurator-add-to-cart-button.module';
 import { ConfiguratorConflictAndErrorMessagesModule } from './configurator-conflict-and-error-messages/configurator-conflict-and-error-messages.module';
 import { ConfiguratorExitButtonModule } from './exit-button/configurator-exit-button.module';
+import { ConfiguratorGroupModule } from './group/configurator-group.module';
 import { ConfiguratorFormModule } from './form/configurator-form.module';
 import { ConfiguratorGroupMenuModule } from './group-menu/configurator-group-menu.module';
 import { ConfiguratorGroupTitleModule } from './group-title/configurator-group-title.module';
@@ -24,6 +25,7 @@ import { ConfiguratorProductTitleModule } from './product-title/configurator-pro
 import { ConfiguratorTabBarModule } from './tab-bar/configurator-tab-bar.module';
 import { ConfiguratorUpdateMessageModule } from './update-message/configurator-update-message.module';
 import { ConfiguratorVariantCarouselModule } from './variant-carousel/configurator-variant-carousel.module';
+import { ConfiguratorConflictSolverDialogModule } from './conflict-solver-dialog/configurator-conflict-solver-dialog.module';
 import { ConfiguratorOverviewSidebarModule } from './overview-sidebar/configurator-overview-sidebar.module';
 
 @NgModule({
@@ -33,6 +35,7 @@ import { ConfiguratorOverviewSidebarModule } from './overview-sidebar/configurat
     ConfiguratorGroupMenuModule,
     ConfiguratorProductTitleModule,
     ConfiguratorTabBarModule,
+    ConfiguratorGroupModule,
     ConfiguratorFormModule,
     ConfiguratorGroupTitleModule,
     ConfiguratorUpdateMessageModule,
@@ -48,6 +51,7 @@ import { ConfiguratorOverviewSidebarModule } from './overview-sidebar/configurat
     ConfiguratorOverviewFilterButtonModule,
     ConfiguratorOverviewFilterDialogModule,
     ConfiguratorOverviewSidebarModule,
+    ConfiguratorConflictSolverDialogModule,
   ],
 })
 export class RulebasedConfiguratorComponentsModule {}
