b5535d347e3f41f125dfbc81e1572b59a3fb3a85
Mateusz Kolasa <mateusz.kolasa@sap.com>
2023-02-09 14:51:19 +0100

    fix: flickering effect on the order details page
    Closes: CXSPA-1980

--- feature-libs/order/core/store/selectors/order-details.selectors.spec.ts
+++ feature-libs/order/core/store/selectors/order-details.selectors.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { select, Store, StoreModule } from '@ngrx/store';
 import { Order } from '@spartacus/order/root';
+import { OrderActions } from '../actions';
 import { ORDER_FEATURE, StateWithOrder } from '../order-state';
 import * as fromReducers from '../reducers';
 import { OrderSelectors } from './index';
@@ -19,6 +20,7 @@ describe('Order Details Selectors', () => {
     store = TestBed.inject(Store);
     spyOn(store, 'dispatch').and.callThrough();
   });
+
   describe('getOrderDetails', () => {
     it('should return the Order state from the store', () => {
       let result: Order;
@@ -26,6 +28,26 @@ describe('Order Details Selectors', () => {
         .pipe(select(OrderSelectors.getOrderDetails))
         .subscribe((value) => (result = value));
       expect(result).not.toBeNull();
+    });
+  });
+
+  describe('getOrderDetailsLoading', () => {
+    it('should return the boolean value from the loader state loading', () => {
+      store.dispatch(
+        new OrderActions.LoadOrderDetails({
+          userId: 'curent',
+          orderCode: '123',
+        })
+      );
+
+      let result = false;
+
+      store
+        .pipe(select(OrderSelectors.getOrderDetailsLoading))
+        .subscribe((value) => (result = value))
+        .unsubscribe();
+
+      expect(result).toEqual(true);
     });
   });
 });
