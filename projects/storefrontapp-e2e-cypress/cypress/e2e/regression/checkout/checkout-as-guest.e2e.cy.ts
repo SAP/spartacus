ad75ef62a1425a980539d925b3457f32807c301e
Florent Letendre <florent.letendre@hotmail.fr>
2023-02-16 17:02:45 -0500

    modify cache logic

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/checkout/checkout-as-guest.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/checkout/checkout-as-guest.e2e.cy.ts
@@ -10,12 +10,12 @@ import * as guestCheckout from '../../../helpers/checkout-as-guest';
 import * as checkout from '../../../helpers/checkout-flow';
 import { waitForPage } from '../../../helpers/checkout-flow';
 import * as loginHelper from '../../../helpers/login';
-import { clearCacheTestIsolationBeforeOnly } from '../../../helpers/utils-cypress-legacy';
+import { clearCacheTestIsolation } from '../../../helpers/utils-cypress-legacy';
 import { viewportContext } from '../../../helpers/viewport-context';
 import { cheapProduct } from '../../../sample-data/checkout-flow';
 context('Checkout as guest', { testIsolation: false }, () => {
   viewportContext(['mobile', 'desktop'], () => {
-    clearCacheTestIsolationBeforeOnly();
+    clearCacheTestIsolation();
     before(() => {
       cy.window().then((win) => win.sessionStorage.clear());
       guestCheckout.generateGuestUser();
