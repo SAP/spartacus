a9acb7091b556c83703990dd3e34b5be8e34cd7a
Levent Arman Özak <arman@armanozak.com>
2023-02-19 14:45:05 +0100

    AdministrationWrapperModule test & sample (#16911)
    * test: add test for administration wrapper module
    
    * chore: add CdcAdministrationModule to sample app

--- projects/storefrontapp/src/app/spartacus/features/organization/organization-administration-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/organization/organization-administration-feature.module.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { NgModule, Type } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CmsConfig, I18nConfig, provideConfig } from '@spartacus/core';
 import {
   organizationTranslationChunksConfig,
@@ -14,22 +14,16 @@ import {
   AdministrationRootModule,
   ORGANIZATION_ADMINISTRATION_FEATURE,
 } from '@spartacus/organization/administration/root';
-import { environment } from '../../../../environments/environment';
-import { CdcAdministrationModule } from 'integration-libs/cdc/organization/administration/public_api';
 
-const extensions: Type<any>[] = [];
-if (environment.cdc) {
-  extensions.push(CdcAdministrationModule);
-}
 @NgModule({
   declarations: [],
-  imports: [AdministrationRootModule, ...extensions],
+  imports: [AdministrationRootModule],
   providers: [
     provideConfig(<CmsConfig>{
       featureModules: {
         [ORGANIZATION_ADMINISTRATION_FEATURE]: {
           module: () =>
-            import('./organization-administration-wrapper.module').then(
+            import('./administration-wrapper.module').then(
               (m) => m.AdministrationWrapperModule
             ),
         },
