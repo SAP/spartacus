9d04ee8b74f1f345f793f06f52e764dc79cc70f4
Florent Letendre <florent.letendre@hotmail.fr>
2023-02-20 12:39:47 -0500

    add cypress legacy utils + config adjustment

--- projects/storefrontapp-e2e-cypress/cypress.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 10000,
@@ -28,8 +28,8 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:4200',
   },
-})
+});
---
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/utils-cypress-legacy.ts
@@ -0,0 +1,33 @@
+/*
+Cypress 12 has introduced Test isolation: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
+ By default, before each test this mechanism takes place:
+  - Clearing the dom state by visiting about:blank
+  - Clearing cookies in all domains
+  - Clearing localStorage in all domains
+  - Clearing sessionStorage in all domains
+For existing Sparatcus test not supporting TestIsolation, utils functions can simulates pre-Cy12 behavior:
+https://docs.cypress.io/guides/references/migration-guide#Simulating-Pre-Test-Isolation-Behavior
+*/
+
+function clearLocalStorageAndCookies() {
+  cy.clearLocalStorage();
+  cy.clearCookies();
+}
+
+export function isolateTests(): void {
+  before(() => {
+    clearLocalStorageAndCookies();
+  });
+  beforeEach(() => {
+    clearLocalStorageAndCookies();
+  });
+}
+
+export function isolateTestsBefore(): void {
+  before(() => {
+    clearLocalStorageAndCookies();
+  });
+  beforeEach(() => {
+    clearLocalStorageAndCookies();
+  });
+}
