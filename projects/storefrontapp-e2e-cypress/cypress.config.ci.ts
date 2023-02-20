b63e61bb1117c2c72ce982113e367b6b68238495
Florent Letendre <florent.letendre@hotmail.fr>
2023-02-20 18:26:34 -0500

    fix config

--- projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
@@ -1,12 +1,15 @@
 /*
+ * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
  * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { defineConfig } from 'cypress';
+
 export default defineConfig({
   defaultCommandTimeout: 30000,
-  baseUrl: 'http://localhost:4200',
+
   requestTimeout: 30000,
   projectId: 'k3nmep',
   numTestsKeptInMemory: 1,
@@ -26,5 +29,13 @@ export default defineConfig({
     OCC_PREFIX: '/occ/v2',
     OCC_PREFIX_USER_ENDPOINT: 'users',
     OCC_PREFIX_ORDER_ENDPOINT: 'orders',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:4200',
   },
 });
