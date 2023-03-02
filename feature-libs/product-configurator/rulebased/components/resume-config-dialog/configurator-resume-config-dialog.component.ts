e34d7e18ead829ea779ebae4a74221f9c8e4c928
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2023-03-02 10:25:18 +0100

    resume config dialog skeleton

--- feature-libs/product-configurator/rulebased/components/resume-config-dialog/configurator-resume-config-dialog.component.ts
+++ feature-libs/product-configurator/rulebased/components/resume-config-dialog/configurator-resume-config-dialog.component.ts
@@ -6,17 +6,26 @@
 
 import { Component, OnInit } from '@angular/core';
 import {
+  CommonConfigurator,
+  ConfiguratorModelUtils,
+  ConfiguratorType,
+} from '@spartacus/product-configurator/common';
+import {
   FocusConfig,
   ICON_TYPE,
   LaunchDialogService,
 } from '@spartacus/storefront';
+import { ConfiguratorCommonsService } from '../../core/facade/configurator-commons.service';
 
 @Component({
   selector: 'cx-configurator-resume-config-dialog',
   templateUrl: './configurator-resume-config-dialog.component.html',
 })
 export class ConfiguratorResumeConfigDialogComponent implements OnInit {
-  constructor(protected launchDialogService: LaunchDialogService) {}
+  constructor(
+    protected launchDialogService: LaunchDialogService,
+    protected configuratorCommonsService: ConfiguratorCommonsService
+  ) {}
 
   iconTypes = ICON_TYPE;
   focusConfig: FocusConfig = {
@@ -29,9 +38,28 @@ export class ConfiguratorResumeConfigDialogComponent implements OnInit {
   ngOnInit(): void {}
 
   /**
-   * closes the filter modal
+   * closes the  modal
    */
-  closeFilterModal(): void {
-    this.launchDialogService.closeDialog('Close Filtering');
+  closeModal(): void {
+    this.launchDialogService.closeDialog('Close Resume Config Modal');
+  }
+
+  /**
+   * closes the  modal
+   */
+  discardConfig(): void {
+    this.closeModal();
+    const owner: CommonConfigurator.Owner =
+      ConfiguratorModelUtils.createInitialOwner();
+    owner.key = 'CONF_CAMERA_SL';
+    owner.id = 'CONF_CAMERA_SL';
+    owner.configuratorType = ConfiguratorType.VARIANT;
+    console.log('discard');
+    this.configuratorCommonsService.removeProductBoundConfigurations();
+    this.configuratorCommonsService.getOrCreateConfiguration(
+      owner,
+      undefined,
+      true
+    );
   }
 }
