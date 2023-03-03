222ec8b40d252469bf95cf3f3f88a86968afc94d
Florent Letendre <florent.letendre01@sap.com>
2023-03-03 14:21:16 -0500

    chore: Cypress12 Upgrade

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/secure-portal/b2b-secure-portal.e2e.cy.ts
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { RoutingConfig } from '@spartacus/core';
+import * as securePortal from '../../../../helpers/b2b/b2b-secure-portal';
+
+context('B2B - Secure Portal', () => {
+  let config: RoutingConfig;
+
+  beforeEach(() => {
+    config = {
+      routing: {
+        protected: undefined,
+      },
+    };
+  });
+});
+
+describe('Secure Portal - Routing', () => {
+  describe('Routing restrictions', () => {
+    before(() => {
+      securePortal.getStubbedBasesites();
+    });
+
+    it('should redirect to the login page when accessing homepage', () => {
+      cy.visit('/');
+      cy.url().should('contain', '/login');
+    });
+  });
+});
