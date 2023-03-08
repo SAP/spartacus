54f4e1e2a87f0c99f08d6bd6b51a6e2e7cfd8249
cawmichael <mechris3@hotmail.com>
2023-03-08 12:06:03 +0000

    Add Cypress Test and minor change to template to output store value in an attribute

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/pickup-in-store/pdp-and-cart-logged-in-user-pickup-display.core-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/pickup-in-store/pdp-and-cart-logged-in-user-pickup-display.core-e2e.cy.ts
@@ -43,13 +43,13 @@ import {
     Display local preferred store as the elected pickup store
 
 */
-describe('Pickup Delivery Option - A logged in user which checkout with BOPIS', () => {
+describe('Check for presence or absence of displayed pick up store on PDP page', () => {
   viewportContext(['desktop'], () => {
     beforeEach(() => {
       configureApparelProduct();
     });
 
-    it("User has not selected a store for the product to be picked up from previously and doesn't have a preferred store. The user is on the PDP page. Don't display an elected pickup store", () => {
+    it.skip("User has not selected a store for the product to be picked up from previously and doesn't have a preferred store. The user is on the PDP page. Don't display an elected pickup store", () => {
       /*
     GIVEN
     The user has not selected a store for the product to be picked up from previously and doesn't have a preferred store
@@ -64,10 +64,29 @@ describe('Pickup Delivery Option - A logged in user which checkout with BOPIS', 
         url: '/authorizationserver/oauth/token',
       }).as('registerUser');
 
-      //   cy.intercept({
-      //     method: 'POST',
-      //     url: /users\/current\/carts\/[0-9a-zA-Z|-]*\/entries/,
-      //   }).as('apiAddToCart');
+      // A registered user who has logged in.
+      register();
+      cy.wait('@registerUser').then((_interception) => {
+        login();
+      });
+      cy.get(L.PICKUP_STORE_LOCATION).should('not.exist');
+      //
+    });
+
+    it.skip('User has selected a store for the product to be picked up from. The user is on the PDP page. Display the selected store as the elected pickup store', () => {
+      /*
+    GIVEN
+    The user has selected a store for the product to be picked up from
+    WHEN
+    The user is on the PDP and cart pages
+    THEN
+    Display the selected store as the elected pickup store
+      */
+
+      cy.intercept({
+        method: 'POST',
+        url: '/authorizationserver/oauth/token',
+      }).as('registerUser');
 
       // A registered user who has logged in.
       register();
@@ -75,84 +94,75 @@ describe('Pickup Delivery Option - A logged in user which checkout with BOPIS', 
         login();
       });
 
-      //   // The logged in user navigates to a PDP wishing to buy the product.
-      //   // The user has the choice of whether they want the product delivered (the default) or whether they want to pick it up in store.
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.visible');
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.visible');
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.checked');
+      // The user has the choice of whether they want the product delivered (the default) or whether they want to pick it up in store.
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.visible');
+      cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.visible');
+      cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.checked');
 
-      //   // The user selects pickup in store.
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).click();
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.checked');
-      //   cy.get(L.USE_MY_LOCATION).click();
+      // The user selects pickup in store.
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).click();
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.checked');
+      cy.get(L.USE_MY_LOCATION).click();
 
-      //   //The user selects which store they want to collect from (by default the last store they selected, falling back to the nearest store).
-      //   cy.get(L.ACTIVE_PICK_UP_IN_STORE_BUTTON).first().click();
-      //   cy.get(L.PICKUP_STORE_LOCATION).invoke('text').as('firstStoreName');
+      //The user selects which store they want to collect from (by default the last store they selected, falling back to the nearest store).
+      cy.get(L.ACTIVE_PICK_UP_IN_STORE_BUTTON).first().click();
+      cy.get(L.PICKUP_STORE_LOCATION).invoke('text').as('firstStoreName');
 
-      //   // The user adds the product to the cart. (The cart entries post call will have the "deliveryPointOfService" field).
-      //   cy.get(L.ADD_TO_CART).click();
-      //   cy.wait('@apiAddToCart').then((interception) => {
-      //     expect(interception.response.statusCode).to.equal(200);
-      //     expect(interception.request.body).to.have.property(
-      //       'deliveryPointOfService'
-      //     );
-      //     cy.get(L.PICKUP_STORE_LOCATION)
-      //       .invoke('text')
-      //       .should(
-      //         'be.equal',
-      //         interception.request.body.deliveryPointOfService.name
-      //       );
-      //   });
-      //   // From the cart, the user can change the location they wish to pick up the product from.
-      //   cy.get(L.VIEW_CART).click();
-      //   cy.url().should('include', '/cart');
-      //   cy.get(L.PICKUP_STORE_LOCATION).should('be.visible');
-      //   cy.get(L.CHANGE_STORE_LINK).click();
-      //   cy.get(L.PICKUP_IN_STORE_MODAL).should('exist');
-      //   cy.intercept({
-      //     method: 'GET',
-      //     url: /stores\/[0-9a-zA-Z|-]*?/,
-      //   }).as('getStores');
-      //   cy.get(L.ACTIVE_PICK_UP_IN_STORE_BUTTON).last().click();
-      //   cy.wait('@getStores').then((interception) => {
-      //     cy.get('@firstStoreName').then((firstStoreName) => {
-      //       cy.get(
-      //         L.PICKUP_STORE_LOCATION_WITH_VALUE(interception.response.body.name)
-      //       )
-      //         .invoke('text')
-      //         .should('not.equal', firstStoreName);
-      //     });
-      //   });
+      cy.get('@firstStoreName').then((firstStoreName) => {
+        cy.get(L.PICKUP_STORE_LOCATION_WITH_VALUE(firstStoreName)).should(
+          'exist'
+        );
+      });
 
-      //   cy.log('The logged in user checks out.');
-
-      //   cy.get(L.PROCEED_TO_CHECKOUT_BUTTON).click();
-      //   fillAddressForm(defaultAddress);
-      //   cy.get(L.CHECKOUT_ADDRESS_FORM_SUBMIT_BUTTON).click();
-      //   cy.get(L.CHECKOUT_DELIVERY_MODE_CONTINUE_BUTTON).click();
-
-      //   cy.log('During checkout, the user cannot change the pickup location');
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('not.exist');
-      //   cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('not.exist');
-
-      //   fillPaymentForm(defaultPaymentDetails);
-      //   cy.get(L.CHECKOUT_PAYMENT_FORM_CONTINUE_BUTTON).click();
-      //   cy.log(
-      //     'During the order review, the user cannot change the pickup location.'
-      //   );
-      //   cy.get(
-      //     `cx-pickup-in-store-details-review ${L.PICKUP_OPTIONS_RADIO_DELIVERY}`
-      //   ).should('not.exist');
-
-      //   cy.log(
-      //     '  - The user completes checkout and sees the order details. On here they can see their pickup location.'
-      //   );
-      //   cy.get(L.REVIEW_ORDER_TERM_CONDITION).click();
-      //   cy.get(L.REVIEW_ORDER_SUBMIT).click();
-      //   cy.get(L.ORDER_CONFIRMATION).should('exist');
+      //
     });
 
-    //
+    it('User has not selected a store for the product previously, but has a preferred store (Backend API). The user is on the PDP page. Display remote preferred store as the elected pickup store.', () => {
+      /*
+    GIVEN
+    The user has not selected a store for the product previously, but has a preferred store (Backend API)
+    WHEN
+    The user is on the PDP and cart pages
+    THEN
+    Display remote preferred store as the elected pickup store
+      */
+
+      cy.intercept({
+        method: 'POST',
+        url: '/authorizationserver/oauth/token',
+      }).as('registerUser');
+
+      // A registered user who has logged in.
+      register();
+      cy.wait('@registerUser').then((_interception) => {
+        login();
+      });
+      cy.get(L.PICKUP_STORE_LOCATION).should('not.exist');
+
+      // The user has the choice of whether they want the product delivered (the default) or whether they want to pick it up in store.
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.visible');
+      cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.visible');
+      cy.get(L.PICKUP_OPTIONS_RADIO_DELIVERY).should('be.checked');
+
+      // The user selects pickup in store.
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).click();
+      cy.get(L.PICKUP_OPTIONS_RADIO_PICKUP).should('be.checked');
+      cy.get(L.USE_MY_LOCATION).click();
+
+      cy.get(L.SET_PREFERRED_STORE)
+        .first()
+        .invoke('attr', 'data-preferred-store')
+        .then((firstStoreName) => cy.wrap(firstStoreName).as('firstStoreName'));
+
+      cy.get(L.SET_PREFERRED_STORE).first().click();
+
+      cy.get(L.DIALOG_CLOSE).click();
+
+      cy.get('@firstStoreName').then((firstStoreName) => {
+        cy.get(L.PICKUP_STORE_LOCATION_WITH_VALUE(firstStoreName)).should(
+          'exist'
+        );
+      });
+    });
   });
 });
