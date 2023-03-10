76c7f8e54c018f55efb8c42096cf784cde5382fc
Akash Srivastava <akash.srivastava05@sap.com>
2023-03-10 11:35:48 +0530

    Changes for image in order history

--- integration-libs/cdp/root/components/cdp-order/cdp-order.module.ts
+++ integration-libs/cdp/root/components/cdp-order/cdp-order.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ConfigModule, UrlModule} from '@spartacus/core';
+import { ConfigModule, RoutingModule, UrlModule} from '@spartacus/core';
 import { I18nModule} from '@spartacus/core';
 //import { OrderHistoryModule } from '@spartacus/order/components';
 import { OrderComponent } from './cdp-order.component';
 
 @NgModule({
   imports: [
-    CommonModule,BrowserModule,I18nModule,UrlModule,
+    CommonModule,BrowserModule,I18nModule,UrlModule,RoutingModule,
     ConfigModule.withConfig({
       cmsComponents: {
         AccountOrderHistoryComponent: {
