b05321f8a7276e95f1bdd756658f75077b8e0d91
Ulrich Stellmacher <ulrich.stellmacher@sap.com>
2023-03-14 09:53:51 +0100

    refactor: use constant, fix comments

--- projects/storefrontapp-e2e-cypress/cypress/helpers/product-configurator-restart-dialog.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/product-configurator-restart-dialog.ts
@@ -8,21 +8,21 @@ import * as configurationVc from './product-configurator-vc';
 
 const COMPONENT_SELECTOR = 'cx-configurator-restart-dialog';
 /**
- * Verifies that the restart dialog is currently opened.
+ * Verifies that the restart dialog is currently opened
  */
 export function checkIsOpen() {
-  cy.get('cx-configurator-restart-dialog').should('be.visible');
+  cy.get(COMPONENT_SELECTOR).should('be.visible');
 }
 
 /**
- * Verifies that the restart dialog is currently closed.
+ * Verifies that the restart dialog is currently closed
  */
 export function checkIsClosed() {
-  cy.get('cx-configurator-restart-dialog').should('not.exist');
+  cy.get(COMPONENT_SELECTOR).should('not.exist');
 }
 
 /**
- * Verifies the content of the restart dialog.
+ * Verifies the content of the restart dialog
  */
 export function checkDialog() {
   checkTitleDisplayed();
@@ -33,7 +33,7 @@ export function checkDialog() {
 }
 
 /**
- * Verifies that the restart dialog has a title.
+ * Verifies that the restart dialog has a title
  */
 export function checkTitleDisplayed() {
   cy.get(COMPONENT_SELECTOR).within(() =>
@@ -42,7 +42,7 @@ export function checkTitleDisplayed() {
 }
 
 /**
- * Verifies that the restart dialog has a close button.
+ * Verifies that the restart dialog has a close button
  */
 export function checkCloseButtonDisplayed() {
   cy.get(COMPONENT_SELECTOR).within(() =>
@@ -51,7 +51,7 @@ export function checkCloseButtonDisplayed() {
 }
 
 /**
- * Verifies that the restart dialog displays a description.
+ * Verifies that the restart dialog displays a description
  */
 export function checkDescriptionDisplayed() {
   cy.get('#cx-configurator-restart-dialog-description').should('be.visible');
@@ -76,17 +76,17 @@ export function checkRestartButtonDisplayed() {
 }
 
 /**
- * closes the restart dialog via the close button
+ * Clicks the close button
  */
 export function close(productId: string) {
   cy.get(COMPONENT_SELECTOR).within(() => cy.get('button.close').click());
-  const location = `/${Cypress.env('BASE_SITE')}/en/USD/product/${productId}/`;
-  cy.location('pathname').should('contain', location);
+  const pdpUrl = `/${Cypress.env('BASE_SITE')}/en/USD/product/${productId}/`;
+  cy.location('pathname').should('contain', pdpUrl);
   cy.get('.ProductDetailsPageTemplate').should('be.visible');
 }
 
 /**
- * click the resume button
+ * Clicks the resume button
  */
 export function resume() {
   cy.get(COMPONENT_SELECTOR).within(() => cy.get('button.btn-primary').click());
@@ -94,7 +94,7 @@ export function resume() {
 }
 
 /**
- * clicks the restart button
+ * Clicks the restart button
  */
 export function restart(isPricingEnabled?: boolean) {
   cy.get(COMPONENT_SELECTOR).within(() =>
