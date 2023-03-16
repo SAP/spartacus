5875161fde945607362360ae976d4724ff8681d0
AshreeJay <52031769+AshreeJay@users.noreply.github.com>
2023-03-16 17:22:08 +0530

    UPDATE

--- integration-libs/cdp/root/components/cdp-my-account/cdp-my-account.component.ts
+++ integration-libs/cdp/root/components/cdp-my-account/cdp-my-account.component.ts
@@ -1,8 +1,12 @@
-import {  Component, OnDestroy,  OnInit,  Optional } from '@angular/core';
-import { CxDatePipe, FeatureConfigService, isNotUndefined, OccEndpointsService, RoutingService, TranslationService, UserIdService} from '@spartacus/core';
-import { OrderHistoryFacade, ReplenishmentOrderHistoryFacade, OrderHistoryList, Order } from '@spartacus/order/root';
-import { Observable, combineLatest } from 'rxjs';
-import { tap, map, filter, take, switchMap } from 'rxjs/operators';
+import {  Component, OnInit } from '@angular/core';
+import { CxDatePipe, OccEndpointsService, RoutingService, UserIdService} from '@spartacus/core';
+import result from 'postcss/lib/result';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap, mergeMap } from 'rxjs/operators';
+import { finalOrder } from '../cdp-order/model/order/finalOrder';
+import { order } from '../cdp-order/model/orderDetail/order';
+import { product } from '../cdp-order/model/ImageDetail/product';
+import { cdpOrderAdapter } from '../cdp-order/adapter/cdp-order-adapter';
 
 @Component({
   selector: 'cx-cdp-body',
@@ -11,138 +15,98 @@ import { tap, map, filter, take, switchMap } from 'rxjs/operators';
   providers: [CxDatePipe],
 })
 
-export class CdpMyAccountComponent implements OnDestroy,OnInit {
-  // TODO(#630): make featureConfigService are required dependency and for major releases, remove featureConfigService
-  constructor(
-    routing: RoutingService,
-    orderHistoryFacade: OrderHistoryFacade,
-    translation: TranslationService,
-    replenishmentOrderHistoryFacade: ReplenishmentOrderHistoryFacade,
-    // eslint-disable-next-line @typescript-eslint/unified-signatures
-    featureConfigService: FeatureConfigService,
-    userIdService: UserIdService
-  );
-  /**
-   * @deprecated since 5.1
-   */
-  constructor(
-    routing: RoutingService,
-    orderHistoryFacade: OrderHistoryFacade,
-    translation: TranslationService,
-    replenishmentOrderHistoryFacade: ReplenishmentOrderHistoryFacade
-  );
-  constructor(
-    protected routing: RoutingService,
-    protected orderHistoryFacade: OrderHistoryFacade,
-    protected translation: TranslationService,
-    protected replenishmentOrderHistoryFacade: ReplenishmentOrderHistoryFacade,
-    @Optional() protected featureConfigService?: FeatureConfigService
-  );
-  
-  constructor(private userIdService: UserIdService,
-              private cdpOrderAdapter: cdpOrderAdapter,
-              protected datePipe: CxDatePipe,
-              protected routing: RoutingService,
-              protected occEndpointsService: OccEndpointsService){}
+export class CdpMyAccountComponent implements OnInit{
 
-  private PAGE_SIZE = 3;
-  sortType: string;
-  hasPONumber: boolean | undefined;
+  constructor(private userIdService: UserIdService,private cdpOrderAdapter: cdpOrderAdapter,protected datePipe: CxDatePipe,protected routing: RoutingService,protected occEndpointsService: OccEndpointsService){}
 
-  orders$: Observable<OrderHistoryList | undefined> = this.orderHistoryFacade
-    .getOrderHistoryList(this.PAGE_SIZE)
-    .pipe(
-      tap((orders: OrderHistoryList | undefined) => {
-        if (orders?.pagination?.sort) {
-          this.sortType = orders.pagination.sort;
-        }
-        // TODO(#630): remove featureConfigService for major releases
-        this.hasPONumber =
-          orders?.orders?.[0]?.purchaseOrderNumber !== undefined &&
-          this.featureConfigService?.isLevel('5.1');
-      })
-    );
- 
-    ngOnInit(): void {
-      this.getMyData(this.orders$);
+  result: finalOrder={orders:[]};
+  totalPrice: number=0;
+  totalItem: number[]=[];
+  orderDetail: Record<string,order>={};
+  orderedItems: Record<string,number>={};
+  i: number=0;
+  url: any;
+  output: result;
+  orderStatus: Record<string,Record<string,number>>={};
+  orderImage: Record<string,product[]>={};
+  userId: string;
+  tabTitleParam$=new BehaviorSubject(0);
+  public loading$ = new BehaviorSubject<boolean>(true);
+
+
+  orders$ = this.userIdService.takeUserId().pipe(switchMap((userId) => this.cdpOrderAdapter.getOrder(userId)));
+
+  ngOnInit(): void {
+    this.getMyData();
+  }
+
+  public getMyData(): void{
+
+  this.orders$.subscribe((res)=>{
+    this.result=res;
+    this.tabTitleParam$.next(res.orders.length-res.orders.length+2);
+    this.calculateTotalAmount(this.result);
+    this.getOrderedItems(this.result);
+  });
+
+  }
+
+  public calculateTotalAmount(finalResult: finalOrder): void{
+
+    for(var val of finalResult.orders)
+    {
+      this.totalPrice = val.total.value + this.totalPrice;
+      console.log(this.totalPrice);
     }
-
-    public getMyData(): Order{
-
-      const obser= this.userIdService.takeUserId().pipe(switchMap((userId) => this.cdpOrderAdapter.getOrder(userId)));
-   
-      obser.subscribe(res => {
-       this.result=res;
-       this.tabTitleParam$.next(res.orders.length);
-       this.calculateTotalAmount(this.result);
-       this.getItemCount(this.result);
-     });
-     }
-  hasReplenishmentOrder$: Observable<boolean> =
-    this.replenishmentOrderHistoryFacade
-      .getReplenishmentOrderDetails()
-      .pipe(map((order) => order && Object.keys(order).length !== 0));
-
-  isLoaded$: Observable<boolean> =
-    this.orderHistoryFacade.getOrderHistoryListLoaded();
-
-  /**
-   * When "Order Return" feature is enabled, this component becomes one tab in
-   * TabParagraphContainerComponent. This can be read from TabParagraphContainer.
-   */
-  tabTitleParam$: Observable<number> = this.orders$.pipe(
-    map((order) => order?.pagination?.totalResults),
-    filter(isNotUndefined),
-    take(1)
-  );
-
-  ngOnDestroy(): void {
-    this.orderHistoryFacade.clearOrderList();
   }
 
-  changeSortCode(sortCode: string): void {
-    const event: { sortCode: string; currentPage: number } = {
-      sortCode,
-      currentPage: 0,
-    };
-    this.sortType = sortCode;
-    this.fetchOrders(event);
+  public async getOrderedItems(finalResult: finalOrder): Promise<void>{
+
+    for(let order of finalResult.orders)
+    {
+      await this.userIdService.takeUserId().pipe(mergeMap((userId)=> this.cdpOrderAdapter.getOrderDetail(userId,order))).toPromise().then( data=>{
+        this.orderDetail[order.code]=data;
+        //orderDetail->order
+      });
+    }
+    this.getDetail();
   }
 
-  pageChange(page: number): void {
-    const event: { sortCode: string; currentPage: number } = {
-      sortCode: this.sortType,
-      currentPage: page,
-    };
-    this.fetchOrders(event);
-  }
+  public async getDetail() {
 
-  goToOrderDetail(order: Order): void {
-    this.routing.go({
-      cxRoute: 'orderDetails',
-      params: order,
-    });
-  }
+    this.loading$.next(true);
+    // eslint-disable-next-line guard-for-in
+    for (let orderCode in this.orderDetail) {
+      this.orderStatus[orderCode] ??= {};
+      this.orderImage[orderCode]??=[];
+      this.orderDetail[orderCode].consignments.forEach((ord) => {
+        this.orderStatus[orderCode][ord.status] ??= 0;
+        ord.entries.forEach((entr) => {
+          console.log(orderCode + ' status ' + ord.status + entr.quantity);
+          this.orderStatus[orderCode][ord.status] =
+            this.orderStatus[orderCode][ord.status] + entr.quantity;
+            if(entr.orderEntry.product && entr.orderEntry.product.images)
+            {
+            //   entr.orderEntry.product.images.forEach((img)=>{
+            //     img.url =
+            //             this.occEndpointsService.getBaseUrl({
+            //               prefix: false,
+            //               baseSite: false,
+            //             }) + img.url;
+            //   });
+            //   this.orderImage[orderCode].push(entr.orderEntry.product);
 
-  getSortLabels(): Observable<{ byDate: string; byOrderNumber: string }> {
-    return combineLatest([
-      this.translation.translate('sorting.date'),
-      this.translation.translate('sorting.orderNumber'),
-    ]).pipe(
-      map(([textByDate, textByOrderNumber]) => {
-        return {
-          byDate: textByDate,
-          byOrderNumber: textByOrderNumber,
-        };
-      })
-    );
-  }
-
-  private fetchOrders(event: { sortCode: string; currentPage: number }): void {
-    this.orderHistoryFacade.loadOrderList(
-      this.PAGE_SIZE,
-      event.currentPage,
-      event.sortCode
-    );
+            this.url = entr.orderEntry.product.images[0];
+            this.url = this.occEndpointsService.getBaseUrl({
+                            prefix: false,
+                            baseSite: false,
+                          }) + this.url;
+                          this.orderImage[orderCode].push(entr.orderEntry.product);
+            }
+        });
+      });
+      this.loading$.next(false);
+    }
+    console.log(this.orderImage);
   }
 }
