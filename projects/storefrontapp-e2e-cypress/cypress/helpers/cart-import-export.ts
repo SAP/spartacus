5c98339d43d2586e529c086a0d4275a93c509031
Hakwoo Kim <hakwoo.kim@sap.com>
2023-03-20 11:48:50 -0400

    add back label check for cart import export

--- projects/storefrontapp-e2e-cypress/cypress/helpers/cart-import-export.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/cart-import-export.ts
@@ -259,6 +259,7 @@ export function addProductToCart(productCode: string = cart.products[1].code) {
   cy.wait(['@refreshCart', '@addToCart']);
   // there is only one btn-primary which is "View Cart" button
   cy.get('cx-added-to-cart-dialog a.btn-primary')
+    .contains('view cart')
     .scrollIntoView()
     .should('be.visible');
 }
