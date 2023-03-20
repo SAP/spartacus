efa7dad52e8c0364e1f838340df169d66af01aee
Krzysztof Platis <platonn.git@gmail.com>
2023-03-20 14:15:28 +0100

    chore: try to mitigate error about unused dependency '18next-resources-to-backend'

--- projects/core/src/i18n/i18next/i18next-backend/i18next-resources-to-backend.initializer.ts
+++ projects/core/src/i18n/i18next/i18next-backend/i18next-resources-to-backend.initializer.ts
@@ -3,10 +3,10 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 // [CXSPA-2060]
 // To prepare for a future minor 6.x release, we intend to utilize 'i18next-resources-to-backend'.
 // However, we won't be able to introduce a new peer dependency in a minor release as it would constitute
 // a breaking change. Therefore, now in version 6.0, we simulate the use of this library,
 // but the actual implementation using it will occur at a later date (in 6.x).
-export * as I18nextResourcesToBackend from 'i18next-resources-to-backend'; // not exported in public API
+import I18nextResourcesToBackend from 'i18next-resources-to-backend'; // not exported in public API
+export { I18nextResourcesToBackend };
