5c4392cbcec4433c308a672d09ae70ce0a82f2d5
Krzysztof Platis <platonn.git@gmail.com>
2023-03-20 16:48:16 +0100

    refactor(i18next-initializer): add dependency 'i18next-resources-to-backend' and introduce 'Applicable' multi-providers for choosing i18next backend plugin (#17073)
    To implement in a future release the feature of [loading of i18n JSON files from filesystem (CXSPA-2060)](https://jira.tools.sap/browse/CXSPA-2060), we'll need to do a few breaking changes.
    
    However, according to _semver_, we shouldn' to introduce breaking changes in a minor release. So now in major release 6.0, we prepare for the future and introduce those breaking changes.
    
    BREAKING CHANGES:
    - add npm peer dependency `i18next-resources-to-backend` to `@spartacus/core`. 
    - add a mechanism for `Applicable` multi-provided strategies for choosing i18next backend plugin and initializing it. However behavior doesn't change - we still use `i18next-http-backend` behind the scenes.
    
    Note: eventually in the future we plan to provide a new `Applicable` strategy for initializing i18next backend, using the package `i18next-resources-to-backend`. Then customers will be able to configure which translations loading strategy they want: via raw http OR via (webpack) dynamic imports.
    
    fixes https://jira.tools.sap/browse/CXSPA-2866

---
+++ projects/core/src/i18n/i18next/i18next-backend/i18next-backend.providers.ts
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Provider } from '@angular/core';
+import { I18nextBackendInitializer } from './i18next-backend.initializer';
+import { I18nextHttpBackendInitializer } from './i18next-http-backend.initializer';
+
+export const i18nextBackendProviders: Provider[] = [
+  {
+    provide: I18nextBackendInitializer,
+    useExisting: I18nextHttpBackendInitializer,
+    multi: true,
+  },
+];
