bf2de7276384742334a596cd22e4cc9c14c041cc
anjana-bl <anjana.b.l@sap.com>
2023-03-22 10:46:10 +0530

    Update cdc-feature.module.ts

--- projects/storefrontapp/src/app/spartacus/features/cdc/cdc-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cdc/cdc-feature.module.ts
@@ -15,8 +15,7 @@ import { CmsConfig, provideConfig } from '@spartacus/core';
       cdc: [
         {
           baseSite: 'electronics-spa',
-          javascriptUrl:
-            'https://cdns.eu1.gigya.com/JS/gigya.js?apikey=3_k_wG-sllOhu2rjDEWHjG9-ncnnGAMHfkIcUKzl94weJU1Y18hITRgnTDp1LP8QdC',
+          javascriptUrl: 'JS_SDK_URL_PLACEHOLDER',
           sessionExpiration: 3600,
         },
         {
