5c003e11792d72413532d24f5df76b6b46852e1b
Patrick Labadie <patrick.labadie@sap.com>
2023-03-22 16:24:12 -0400

    CXSPA-2862: Generate migration assets. Update formatting

--- projects/schematics/src/migrations/6_0/rename-symbol/data/generated-rename-symbols.migration.ts
+++ projects/schematics/src/migrations/6_0/rename-symbol/data/generated-rename-symbols.migration.ts
@@ -9,24 +9,24 @@ import { RenamedSymbol } from '../../../../shared/utils/file-utils';
 // Generated file, don't update manually.
 //
 export const GENERATED_RENAMED_SYMBOLS_DATA: RenamedSymbol[] = [
-	{
-		previousNode: 'AsmConfig',
-		previousImportPath: '@spartacus/asm/core',
-		newImportPath: '@spartacus/asm/root'
-	},
-	{
-		previousNode: 'AsmUi',
-		previousImportPath: '@spartacus/asm/core',
-		newImportPath: '@spartacus/asm/root'
-	},
-	{
-		previousNode: 'CustomerSearchOptions',
-		previousImportPath: '@spartacus/asm/core',
-		newImportPath: '@spartacus/asm/root'
-	},
-	{
-		previousNode: 'CustomerSearchPage',
-		previousImportPath: '@spartacus/asm/core',
-		newImportPath: '@spartacus/asm/root'
-	}
+  {
+    previousNode: 'AsmConfig',
+    previousImportPath: '@spartacus/asm/core',
+    newImportPath: '@spartacus/asm/root',
+  },
+  {
+    previousNode: 'AsmUi',
+    previousImportPath: '@spartacus/asm/core',
+    newImportPath: '@spartacus/asm/root',
+  },
+  {
+    previousNode: 'CustomerSearchOptions',
+    previousImportPath: '@spartacus/asm/core',
+    newImportPath: '@spartacus/asm/root',
+  },
+  {
+    previousNode: 'CustomerSearchPage',
+    previousImportPath: '@spartacus/asm/core',
+    newImportPath: '@spartacus/asm/root',
+  },
 ];
