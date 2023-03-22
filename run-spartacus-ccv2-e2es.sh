f4776a1489119add9ae968555d8db9a61254f109
Florent Letendre <florent.letendre@hotmail.fr>
2023-03-22 16:04:40 -0400

    paths on on 1 line

--- run-spartacus-ccv2-e2es.sh
+++ run-spartacus-ccv2-e2es.sh
@@ -12,8 +12,7 @@ CYPRESS_SSR_FOLDER="$CYPRESS_ROOT_FOLDER/ssr"
 CYPRESS_VENDOR_FOLDER="$CYPRESS_ROOT_FOLDER/vendor"
 
 
-export E2ES_TO_RUN="$CYPRESS_B2C_FOLDER/checkout/checkout-flow.core-e2e.cy.ts,
-$CYPRESS_B2C_FOLDER/homepage/homepage.core-e2e.cy.ts"
+export E2ES_TO_RUN="$CYPRESS_B2C_FOLDER/checkout/checkout-flow.core-e2e.cy.ts,$CYPRESS_B2C_FOLDER/homepage/homepage.core-e2e.cy.ts"
 
 # install cypress standlone dependencies
 (cd projects/storefrontapp-e2e-cypress && npm install)
