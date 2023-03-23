20089e194061425adafbbca4714de54fc3d16751
AshreeJay <52031769+AshreeJay@users.noreply.github.com>
2023-03-23 13:12:20 +0530

    Update cdp.ts

--- integration-libs/cdp/assets/translations/en/cdp.ts
+++ integration-libs/cdp/assets/translations/en/cdp.ts
@@ -5,30 +5,7 @@
  */
 
 export const cdp = {
-  loginForm: {
-    forgotPassword: 'Forgot password?',
-    signIn: 'Sign In',
-    register: 'Register',
-    dontHaveAccount: 'Don’t have an account?',
-    guestCheckout: 'Guest Checkout',
-    emailAddress: {
-      label: 'Email address',
-      placeholder: 'Enter email',
-    },
-    password: {
-      label: 'Password',
-      placeholder: 'Password',
-    },
-    wrongEmailFormat: 'This is not a valid email format.',
-  },
-  miniLogin: {
-    userGreeting: 'Hi, {{name}}',
-    signInRegister: 'Sign In / Register',
-    signOut: 'Sign Out',
-  },
-  myAccount: {
-    // label: 'My Account',
-  },
+
   cdpOrderHistory: {
     orderInformation: 'Order Information',
     accountInformation: 'Account Information',
@@ -57,29 +34,5 @@ export const cdp = {
     paymentDetails: 'Payment Details',
     communications: 'Communications',
     settingAndPrivacy: 'Settings And Privacy',
-    orderId: 'Order #',
-    emptyPurchaseOrderId: 'None',
-    date: 'Date',
-    status: 'Status',
-    PONumber: 'P.O. Number',
-    total: 'Total',
-    noOrders: 'We have no order records for this account.',
-    noReplenishmentOrders:
-      'We have no replenishment order records for this account.',
-    startShopping: 'Start Shopping',
-    sortBy: 'Sort by',
-    sortOrders: 'Sort orders',
-    replenishmentOrderHistory: 'Replenishment Order History',
-    replenishmentOrderId: 'Replenishment #',
-    purchaseOrderNumber: 'PO #',
-    costCenter: 'Cost Center',
-    startOn: 'Start On',
-    frequency: 'Frequency',
-    nextOrderDate: 'Next Order Date',
-    cancel: 'Cancel',
-    cancelled: 'Cancelled',
-    replenishmentHistory: 'Replenishment History',
-    notFound: 'No Orders Found',
-    actions: 'Actions',
   },
 };
