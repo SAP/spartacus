993c3d76a2eaedb8fc595f5a34cf3e29d161d3da
Patryk Smul <psmul@divante.pl>
2023-03-23 08:20:09 +0100

    chore: webpack config merge with develop update

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -49,17 +49,16 @@ module.exports = {
         __dirname,
         'integration-libs/epd-visualization'
       ),
-      ),
       '@spartacus/customer-ticketing': path.join(
         __dirname,
         'feature-libs/customer-ticketing'
       ),
-      '@spartacus/pickup-in-store': path.join(__dirname, 'feature-libs/pickup-in-store'),
-      '@spartacus/s4om': path.join(__dirname, 'integration-libs/s4om'),
-      '@spartacus/opf': path.join(
+      '@spartacus/pickup-in-store': path.join(
         __dirname,
-        'integration-libs/opf'
-      )
+        'feature-libs/pickup-in-store'
+      ),
+      '@spartacus/s4om': path.join(__dirname, 'integration-libs/s4om'),
+      '@spartacus/opf': path.join(__dirname, 'integration-libs/opf'),
     },
   },
 };
