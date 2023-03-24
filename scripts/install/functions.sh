32e9b7b85f1b9b28e3a3a8fad09f2dc1f0d03c64
Sam Vendittelli <sam.vendittelli@sap.com>
2023-03-24 16:13:59 +0000

    fix: remove pickup-in-store from add_feature_libs

--- scripts/install/functions.sh
+++ scripts/install/functions.sh
@@ -132,7 +132,6 @@ function add_feature_libs {
   ng add @spartacus/tracking --skip-confirmation --no-interactive --features "TMS-GTM" --features "TMS-AEPL"
   ng add @spartacus/qualtrics@${SPARTACUS_VERSION} --skip-confirmation --no-interactive
   ng add @spartacus/customer-ticketing --skip-confirmation --no-interactive
-  ng add @spartacus/pickup-in-store --skip-confirmation --no-interactive
 }
 
 function add_spartacus_csr {
