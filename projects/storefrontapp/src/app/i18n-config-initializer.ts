ecbefb947f959631b1f48c416b53031397dfe970
Krzysztof Platis <platonn.git@gmail.com>
2023-03-27 12:37:15 +0200

    fix: initialize 'i18next' only after the config 'i18n' is stable

---
+++ projects/storefrontapp/src/app/i18n-config-initializer.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  Config,
+  ConfigInitializer,
+  ConfigInitializerService,
+  I18nConfig,
+  OccEndpointsService,
+} from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+/**
+ * Inspired by https://stackoverflow.com/a/67588414
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomI18nConfigInitializer implements ConfigInitializer {
+  readonly scopes = ['i18n.backend.loadPath']; // declare config key that you will resolve
+  readonly configFactory = () => this.resolveConfig().toPromise();
+
+  constructor(
+    protected configInit: ConfigInitializerService,
+    private occEndpoints: OccEndpointsService
+  ) {}
+
+  protected resolveConfig(): Observable<I18nConfig> {
+    return this.configInit.getStable('context.baseSite').pipe(
+      take(1),
+      map(() => {
+        // eslint-disable-next-line no-console
+        console.log(
+          'i18n config initializer, load path: ' +
+            this.occEndpoints.buildUrl('i18n')
+        );
+        return {
+          i18n: {
+            backend: {
+              loadPath: this.occEndpoints.buildUrl('i18n'),
+            },
+          },
+        } as Config;
+      })
+    );
+  }
+}
