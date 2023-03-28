0d12a8da0eb3a4dd921b564d48d19b798677097b
Krzysztof Platis <platonn.git@gmail.com>
2023-03-28 16:09:13 +0200

    fix: initialize 'i18next' only after the config 'i18n' is stable [backport to 5.2.x] (#17145)
    In PR #16662 we've refactored the logic of the initialization of `i18next`. The refactor shouldn't have introduced breaking changes. However that refactor unintentionally removed the logic responsible for waiting for the `i18n` config to be stable before we initialize `i18next` (see diff: https://github.com/SAP/spartacus/pull/16662/files#diff-b9c2f7d32db998292ce5b32da446437ad64755d84817c3261fdbc89f6438856aL28-L30). This mistake was a behavioral breaking change and caused problems for customized code (see the bug ticket:  https://github.com/SAP/spartacus/issues/17124).
    
    In this PR we bring back the logic of waiting for `i18n` config to be stable before we initialize `i18next` library
    
    fixes #17124
    fixes https://jira.tools.sap/browse/CXSPA-2955

--- projects/core/src/i18n/i18next/i18next-providers.ts
+++ projects/core/src/i18n/i18next/i18next-providers.ts
@@ -5,14 +5,20 @@
  */
 
 import { APP_INITIALIZER, inject, Provider } from '@angular/core';
+import { ConfigInitializerService } from '../../config/config-initializer/config-initializer.service';
 import { I18nextInitializer } from './i18next-initializer';
 
 export const i18nextProviders: Provider[] = [
   {
     provide: APP_INITIALIZER,
     useFactory: () => {
+      const configInitializerService = inject(ConfigInitializerService);
       const i18nextInitializer = inject(I18nextInitializer);
-      return () => i18nextInitializer.initialize();
+      return () =>
+        configInitializerService
+          .getStable('i18n')
+          .toPromise()
+          .then(() => i18nextInitializer.initialize());
     },
     multi: true,
   },
