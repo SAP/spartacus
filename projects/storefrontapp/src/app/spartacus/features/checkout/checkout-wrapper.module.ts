6b9dfb4d40db9fe354ece75af48313d245725bd9
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2023-04-04 17:11:21 -0400

    Removing more storefrontapp references

--- projects/storefrontapp/src/app/spartacus/features/checkout/checkout-wrapper.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/checkout/checkout-wrapper.module.ts
@@ -8,17 +8,12 @@ import { NgModule, Type } from '@angular/core';
 import { CheckoutB2BModule } from '@spartacus/checkout/b2b';
 import { CheckoutModule } from '@spartacus/checkout/base';
 import { CheckoutScheduledReplenishmentModule } from '@spartacus/checkout/scheduled-replenishment';
-import { DigitalPaymentsModule } from '@spartacus/digital-payments';
 import { environment } from '../../../../environments/environment';
 
 const extensions: Type<any>[] = [];
 
 if (environment.b2b) {
   extensions.push(CheckoutB2BModule, CheckoutScheduledReplenishmentModule);
-}
-
-if (environment.digitalPayments) {
-  extensions.push(DigitalPaymentsModule);
 }
 
 @NgModule({
