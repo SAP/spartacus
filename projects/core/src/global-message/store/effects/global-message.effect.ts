df245df345de3fd05daa208dcd9db4871cbbcc19
Eric Polesky <eric.polesky@sap.com>
2023-04-05 14:28:12 -0400

    remove deprecated mapTo()

--- projects/core/src/global-message/store/effects/global-message.effect.ts
+++ projects/core/src/global-message/store/effects/global-message.effect.ts
@@ -7,7 +7,7 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { select, Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { EMPTY, Observable, of } from 'rxjs';
 import {
   concatMap,
@@ -22,7 +22,6 @@ import { Translatable } from '../../../i18n/translatable';
 import { ObjectComparisonUtils } from '../../../util/object-comparison-utils';
 import { isNotUndefined } from '../../../util/type-guards';
 import { GlobalMessageConfig } from '../../config/global-message-config';
-import { GlobalMessage } from '../../models/global-message.model';
 import { GlobalMessageActions } from '../actions/index';
 import { StateWithGlobalMessage } from '../global-message-state';
 import { GlobalMessageSelectors } from '../selectors/index';
@@ -33,7 +32,7 @@ export class GlobalMessageEffect {
     createEffect(() =>
       this.actions$.pipe(
         ofType(GlobalMessageActions.ADD_MESSAGE),
-        map((x) => (x as any).payload as GlobalMessage),
+        map((x) => (x as GlobalMessageActions.AddMessage).payload),
         switchMap((message) =>
           of(message.text).pipe(
             withLatestFrom(
@@ -74,7 +73,7 @@ export class GlobalMessageEffect {
     isPlatformBrowser(this.platformId) // we don't want to run this logic when doing SSR
       ? this.actions$.pipe(
           ofType(GlobalMessageActions.ADD_MESSAGE),
-          map((x) => (x as any).payload as GlobalMessage),
+          map((x) => (x as GlobalMessageActions.AddMessage).payload),
           concatMap((message) => {
             const config = this.config.globalMessages?.[message.type];
             return this.store.pipe(
