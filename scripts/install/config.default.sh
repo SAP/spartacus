0e82546163a8b321706fc0bdfd592a15a618a93e
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2023-04-05 12:12:00 -0400

    push changes

--- scripts/install/config.default.sh
+++ scripts/install/config.default.sh
@@ -43,7 +43,7 @@ SPARTACUS_PROJECTS=(
         )
 
 SPARTACUS_REPO_URL="https://github.com/SAP/spartacus.git"
-BRANCH='develop'
+BRANCH='task/asm-360-rebase-develop'
 
 # custom location for the installation output
 # BASE_DIR='/tmp/'
