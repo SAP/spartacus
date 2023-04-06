f72a1e2b911cfcd18483ec6c54acc44915b353af
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2023-04-06 15:52:31 -0400

    Splitting unit tests suite into two to benchmark

--- ci-scripts/unit-tests-core-lib.sh
+++ ci-scripts/unit-tests-core-lib.sh
@@ -17,40 +17,7 @@ echo "Running unit tests and code coverage for cart lib"
 ng test cart --source-map --no-watch --code-coverage --browsers ChromeHeadless
 npm --prefix feature-libs/cart run test:schematics -- --coverage
 
-echo "Running unit tests and code coverage for order lib"
-
-ng test order --source-map --no-watch --code-coverage --browsers ChromeHeadless
-npm --prefix feature-libs/order run test:schematics -- --coverage
-
-echo "Running unit tests and code coverage for user lib"
-
-ng test user --source-map --no-watch --code-coverage --browsers ChromeHeadless
-npm --prefix feature-libs/user run test:schematics -- --coverage
-
 echo "Running unit tests and code coverage for checkout lib"
 
 ng test checkout --source-map --no-watch --code-coverage --browsers ChromeHeadless
 npm --prefix feature-libs/checkout run test:schematics -- --coverage
-
-echo "Running unit tests and code coverage for product library"
-
-ng test product --source-map --no-watch --code-coverage --browsers ChromeHeadless
-npm --prefix feature-libs/product run test:schematics -- --coverage
-
-echo "Running unit tests and code coverage for organization library"
-
-ng test organization --source-map --no-watch --code-coverage --browsers ChromeHeadless
-npm --prefix feature-libs/organization run test:schematics -- --coverage
-
-echo "Running unit tests and code coverage for smartedit library"
-
-ng test smartedit --source-map --no-watch --code-coverage --browsers ChromeHeadless
-npm --prefix feature-libs/smartedit run test:schematics -- --coverage
-
-echo "Running unit tests and code coverage for setup lib"
-
-npm --prefix core-libs/setup run test --runInBand -- --coverage
-
-echo "Running unit tests for storefrontstyles lib"
-
-npm --prefix projects/storefrontstyles run test --runInBand
