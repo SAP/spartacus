bda749d17a5987b7def3d22e95d98a55f8dabc9a
Patrick Labadie <patrick.labadie@sap.com>
2023-04-11 15:50:06 -0400

    CXSPA-2950: Epic branches run all the e2e test suite.

--- ci-scripts/e2e-cypress-merge-check.sh
+++ ci-scripts/e2e-cypress-merge-check.sh
@@ -43,7 +43,27 @@ done
 
 set -- "${POSITIONAL[@]}"
 
-buildLibsAndStorefront
+echo '-----'
+echo "Building Spartacus libraries"
+
+npm ci
+
+(cd projects/storefrontapp-e2e-cypress && npm ci)
+
+npm run build:libs 2>&1 | tee build.log
+
+results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
+if [[ -z "${results}" ]]; then
+    echo "Success: Spartacus production build was successful."
+    rm build.log
+else
+    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
+    rm build.log
+    exit 1
+fi
+echo '-----'
+echo "Building Spartacus storefrontapp"
+npm run build
 
 echo '-----'
 echo "Running Cypress end to end tests for branch [${BRANCH}]"
