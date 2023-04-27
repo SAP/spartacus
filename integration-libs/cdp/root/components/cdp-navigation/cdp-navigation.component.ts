34479aa5a501acaa7097de2f67cd920a6724f661
AshreeJay <52031769+AshreeJay@users.noreply.github.com>
2023-04-27 10:41:13 +0530

    update

--- integration-libs/cdp/root/components/cdp-navigation/cdp-navigation.component.ts
+++ integration-libs/cdp/root/components/cdp-navigation/cdp-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CmsNavigationComponent } from '@spartacus/core';
+import { NavigationNode, CmsComponentData, NavigationService } from '@spartacus/storefront';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-cdp-navigation',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./cdp-navigation.component.css']
 })
 export class CdpNavigationComponent {
+  node$: Observable<NavigationNode> = this.service.getNavigationNode(
+    this.componentData.data$
+  );
 
+  data$: Observable<CmsNavigationComponent> = this.componentData.data$;
+
+
+  constructor(
+    protected componentData: CmsComponentData<CmsNavigationComponent>,
+    protected service: NavigationService
+  ) {}
 }
+
--- projects/storefrontapp/src/environments/environment.ts
+++ projects/storefrontapp/src/environments/environment.ts
@@ -22,8 +22,9 @@ import { Environment } from './models/environment.model';
 export const environment: Environment = {
   production: false,
   occBaseUrl:
-    // 'https://localhost:9002',
-  'https://api.cg79x9wuu9-eccommerc1-s5-public.model-t.myhybris.cloud',
+  // 'http://10.237.103.83:4200',
+    'https://localhost:9002',
+  // 'https://api.cg79x9wuu9-eccommerc1-s5-public.model-t.myhybris.cloud',
   occApiPrefix: '/occ/v2/',
   cds: buildProcess.env.CX_CDS ?? false,
   b2b: buildProcess.env.CX_B2B ?? false,
