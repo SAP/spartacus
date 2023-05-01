1d7338719eb99705df5ccd645366b1a495db6d26
Radhep Sabapathipillai <radhep.sabapathipillai@mail.concordia.ca>
2023-05-01 15:50:14 -0400

    add epic number to tests

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/customer360-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/customer360-e2e.cy.ts
@@ -27,28 +27,28 @@ context('Assisted Service Module', () => {
       cy.saveLocalStorage();
     });
 
-    it('should contain all activities ', () => {
+    it('should contain all activities (CXSPA-700)', () => {
       cy.get('.cx-asm-customer-table').contains('Cart');
       cy.get('.cx-asm-customer-table').contains('Order');
       cy.get('.cx-asm-customer-table').contains('Ticket');
       cy.get('.cx-asm-customer-table').contains('Saved Cart');
     });
 
-    it('should redirect to the cart page', () => {
+    it('should redirect to the cart page (CXSPA-700)', () => {
       const cartPage = waitForPage('/cart', 'getCartPage');
       customer360.redirect('Cart');
       cy.wait(`@${cartPage}`).its('response.statusCode').should('eq', 200);
       cy.get('h1').contains('Your Shopping Cart');
     });
 
-    it('should redirect to the order details page', () => {
+    it('should redirect to the order details page (CXSPA-700)', () => {
       const orderPage = waitForPage('/my-account/order/*', 'getOrderPage');
       customer360.redirect('Order');
       cy.wait(`@${orderPage}`).its('response.statusCode').should('eq', 200);
       cy.get('h1').contains('Order Details');
     });
 
-    it('should redirect to the saved cart page', () => {
+    it('should redirect to the saved cart page (CXSPA-700)', () => {
       const savedCartPage = waitForPage(
         '/my-account/saved-cart/*',
         'getSavedCartPage'
@@ -58,7 +58,7 @@ context('Assisted Service Module', () => {
       cy.get('h1').contains('Saved Cart Details');
     });
 
-    it('should redirect to the support tickets page', () => {
+    it('should redirect to the support tickets page (CXSPA-700)', () => {
       const supportTicketsPage = waitForPage(
         '/my-account/support-ticket/*',
         'getSupportTicketsPage'
