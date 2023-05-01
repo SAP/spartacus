807b0bb6d1c0222f6032237619800054c14cfa99
I506207 <brian.gamboc-javiniar@sap.com>
2023-05-01 11:59:36 -0400

    test

--- feature-libs/checkout/base/components/guards/checkout.guard.spec.ts
+++ feature-libs/checkout/base/components/guards/checkout.guard.spec.ts
@@ -142,7 +142,7 @@ describe(`CheckoutGuard`, () => {
       .unsubscribe();
   });
 
-  it(`should redirect to refview order`, (done) => {
+  it(`should redirect to refviasdew order`, (done) => {
     expressCheckoutService.trySetDefaultCheckoutDetails =
       createSpy().and.returnValue(of(true));
 
