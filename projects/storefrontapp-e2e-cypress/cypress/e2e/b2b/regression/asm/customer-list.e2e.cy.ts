ef490c48d94a1a27c0cbb225c0bce8dac2d89de1
Grace <gracedong1102@163.com>
2023-05-03 17:05:03 +0800

    CXSPA-2109: add e2e test

--- projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/asm/customer-list.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/asm/customer-list.e2e.cy.ts
@@ -23,5 +23,15 @@ context('Assisted Service Module', () => {
       asm.asmB2bCustomerLists();
       asm.agentSignOut();
     });
+
+    it('checking pagination', () => {
+      checkout.visitHomePage('asm=true');
+      cy.get('cx-asm-main-ui').should('exist');
+      cy.get('cx-asm-main-ui').should('be.visible');
+
+      asm.agentLogin('jules.hasson@acme.com', 'pw4all');
+      asm.asmB2bCustomerListPagination();
+      asm.agentSignOut();
+    });
   });
 });
--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/customer-list.e2e-flaky.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/customer-list.e2e-flaky.cy.ts
@@ -23,5 +23,15 @@ context('Assisted Service Module', () => {
       asm.asmCustomerLists();
       asm.agentSignOut();
     });
+
+    it('checking pagination', () => {
+      checkout.visitHomePage('asm=true');
+      cy.get('cx-asm-main-ui').should('exist');
+      cy.get('cx-asm-main-ui').should('be.visible');
+
+      asm.agentLogin('asagent', 'pw4all');
+      asm.asmCustomerListPagination();
+      asm.agentSignOut();
+    });
   });
 });
