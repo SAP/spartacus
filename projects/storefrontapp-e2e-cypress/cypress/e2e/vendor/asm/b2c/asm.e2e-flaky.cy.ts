36ad48d6aaeda23449392144d66e87c94297c857
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2023-05-03 09:29:57 -0400

    run prettier fix

--- projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/asm/b2c/asm.e2e-flaky.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/asm/b2c/asm.e2e-flaky.cy.ts
@@ -9,7 +9,10 @@ import { login } from '../../../../helpers/auth-forms';
 import * as checkout from '../../../../helpers/checkout-flow';
 import { ELECTRONICS_BASESITE } from '../../../../helpers/checkout-flow';
 import { getErrorAlert } from '../../../../helpers/global-message';
-import { navigateToCategory, waitForPage } from '../../../../helpers/navigation';
+import {
+  navigateToCategory,
+  waitForPage,
+} from '../../../../helpers/navigation';
 import { APPAREL_BASESITE } from '../../../../helpers/variants/apparel-checkout-flow';
 import { getSampleUser } from '../../../../sample-data/checkout-flow';
 import { clearAllStorage } from '../../../../support/utils/clear-all-storage';
