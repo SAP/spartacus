7e7559808b12278d72091da38863730eb9c70c7d
Sam Garland <sam.garland@sap.com>
2023-05-04 13:45:42 +1200

    fix: expose missing services in public API
    Expose the following in `@spartacus/epd-visualization/components`:
    
    - `VisualPickingTabService`
    - `VisualPickingProductFilterService`
    - `VisualPickingProductListService`
    - `VisualViewerAnimationSliderService`
    
    JIRA: https://jira.tools.sap/browse/CXSPA-3342

--- integration-libs/epd-visualization/components/visual-picking/visual-picking-tab/product-list/index.ts
+++ integration-libs/epd-visualization/components/visual-picking/visual-picking-tab/product-list/index.ts
@@ -8,3 +8,4 @@ export * from './compact-add-to-cart/index';
 export * from './model/index';
 export * from './visual-picking-product-list.component';
 export * from './visual-picking-product-list.module';
+export * from './visual-picking-product-list.service';
