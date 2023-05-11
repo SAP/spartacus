7b3a35b582012a5904da28584aa14bc81de9a500
Mateusz Kolasa <mateusz.kolasa@sap.com>
2023-05-11 12:54:10 +0200

    Update loading resources method

--- integration-libs/opf/core/services/opf-resource-loader.service.ts
+++ integration-libs/opf/core/services/opf-resource-loader.service.ts
@@ -4,10 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, isPlatformServer } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ScriptLoader } from '@spartacus/core';
-import { PaymentDynamicScriptResource } from '@spartacus/opf/root';
+import {
+  PaymentDynamicScriptResource,
+  PaymentDynamicScriptResourceType,
+} from '@spartacus/opf/root';
 import { throwError } from 'rxjs';
 
 @Injectable({
@@ -21,8 +24,100 @@ export class OpfResourceLoaderService extends ScriptLoader {
     super(document, platformId);
   }
 
-  hasScript(src?: string): boolean {
+  protected loadedResources: PaymentDynamicScriptResource[] = [];
+
+  protected embedStyles(embedOptions: {
+    src: string;
+    callback?: EventListener;
+    errorCallback?: EventListener;
+  }): void {
+    const { src, callback, errorCallback } = embedOptions;
+
+    const isSSR = isPlatformServer(this.platformId);
+    if ((callback || errorCallback) && isSSR) {
+      return;
+    }
+
+    if (!isSSR && this.hasStyles(src)) {
+      return;
+    }
+
+    const link: HTMLLinkElement = this.document.createElement('link');
+    link.href = src;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+
+    if (callback) {
+      link.addEventListener('load', callback);
+    }
+
+    if (errorCallback) {
+      link.addEventListener('error', errorCallback);
+    }
+
+    this.document.head.appendChild(link);
+  }
+
+  protected hasStyles(src?: string): boolean {
+    return !!this.document.querySelector(`link[href="${src}"]`);
+  }
+
+  protected hasScript(src?: string): boolean {
     return super.hasScript(src);
+  }
+
+  protected handleLoadingResourceError(src?: string) {
+    return throwError(`Error while loading external ${src} resource.`);
+  }
+
+  protected isResourceLoadingCompleted(
+    resources: PaymentDynamicScriptResource[]
+  ) {
+    return resources.length === this.loadedResources.length;
+  }
+
+  protected markResourceAsLoaded(
+    resource: PaymentDynamicScriptResource,
+    resources: PaymentDynamicScriptResource[],
+    resolve: (value: void | PromiseLike<void>) => void
+  ) {
+    this.loadedResources.push(resource);
+    if (this.isResourceLoadingCompleted(resources)) {
+      resolve();
+    }
+  }
+
+  protected loadScript(
+    resource: PaymentDynamicScriptResource,
+    resources: PaymentDynamicScriptResource[],
+    resolve: (value: void | PromiseLike<void>) => void
+  ) {
+    if (resource.url && !this.hasScript(resource.url)) {
+      super.embedScript({
+        src: resource.url,
+        attributes: { type: 'text/javascript' },
+        callback: () => this.markResourceAsLoaded(resource, resources, resolve),
+        errorCallback: () => this.handleLoadingResourceError(resource.url),
+      });
+    } else {
+      this.markResourceAsLoaded(resource, resources, resolve);
+    }
+  }
+
+  protected loadStyles(
+    resource: PaymentDynamicScriptResource,
+    resources: PaymentDynamicScriptResource[],
+    resolve: (value: void | PromiseLike<void>) => void
+  ) {
+    if (resource.url && !this.hasStyles(resource.url)) {
+      this.embedStyles({
+        src: resource.url,
+        callback: () => this.markResourceAsLoaded(resource, resources, resolve),
+        errorCallback: () => this.handleLoadingResourceError(resource.url),
+      });
+    } else {
+      this.markResourceAsLoaded(resource, resources, resolve);
+    }
   }
 
   executeScriptFromHtml(html: string | undefined) {
@@ -33,34 +128,37 @@ export class OpfResourceLoaderService extends ScriptLoader {
     }
   }
 
-  loadProviderScripts(
-    scripts: PaymentDynamicScriptResource[] | undefined
+  loadProviderResources(
+    scripts: PaymentDynamicScriptResource[] = [],
+    styles: PaymentDynamicScriptResource[] = []
   ): Promise<void> {
-    return new Promise((resolve) => {
-      let loaded = 0;
+    const resources: PaymentDynamicScriptResource[] = [
+      ...scripts.map((script) => ({
+        ...script,
+        type: PaymentDynamicScriptResourceType.SCRIPT,
+      })),
+      ...styles.map((style) => ({
+        ...style,
+        type: PaymentDynamicScriptResourceType.STYLES,
+      })),
+    ];
 
-      scripts?.forEach((script: PaymentDynamicScriptResource) => {
-        if (script.url) {
-          if (this.hasScript(script.url)) {
-            loaded++;
-            if (loaded === scripts?.length) {
-              resolve();
-            }
-          } else {
-            super.embedScript({
-              src: script.url,
-              attributes: { type: 'text/javascript' },
-              callback: () => {
-                loaded++;
-                if (loaded === scripts?.length) {
-                  resolve();
-                }
-              },
-              errorCallback: () =>
-                throwError(
-                  `Error while loading external ${script.url} script.`
-                ),
-            });
+    return new Promise((resolve) => {
+      this.loadedResources = [];
+
+      resources.forEach((resource: PaymentDynamicScriptResource) => {
+        if (!resource.url) {
+          this.markResourceAsLoaded(resource, resources, resolve);
+        } else {
+          switch (resource.type) {
+            case PaymentDynamicScriptResourceType.SCRIPT:
+              this.loadScript(resource, resources, resolve);
+              break;
+            case PaymentDynamicScriptResourceType.STYLES:
+              this.loadStyles(resource, resources, resolve);
+              break;
+            default:
+              break;
           }
         }
       });
