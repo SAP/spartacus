4a90c7ac6f324a2b324c262581034e9b358aaac7
Florent Letendre <florent.letendre@hotmail.fr>
2023-05-12 11:29:52 -0400

    remove ternary operator

--- integration-libs/opf/occ/utils/opf-occ-http-error-handlers.ts
+++ integration-libs/opf/occ/utils/opf-occ-http-error-handlers.ts
@@ -7,5 +7,5 @@
 import { HttpErrorModel } from '@spartacus/core';
 
 export function isHttp500Error(err: HttpErrorModel): boolean {
-  return err?.status && err.status >= 500 ? true : false;
+  return !!err?.status && err.status >= 500;
 }
