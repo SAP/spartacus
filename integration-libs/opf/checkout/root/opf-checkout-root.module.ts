6a3f70cd3d4c7d7937791af7aa0481f3cb242282
Florent Letendre <florent.letendre@hotmail.fr>
2023-05-18 13:41:05 -0400

    remove verif cpnt from Checkout lib

--- integration-libs/opf/checkout/root/opf-checkout-root.module.ts
+++ integration-libs/opf/checkout/root/opf-checkout-root.module.ts
@@ -5,42 +5,17 @@
  */
 
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { provideConfigValidator, provideDefaultConfig } from '@spartacus/core';
-import { OpfPaymentVerificationComponent } from './components/opf-payment-verification';
-import { defaultOpfRoutingConfig } from './config';
 import { defaultOPFCheckoutConfig } from './config/default-opf-checkout-config';
 import { defaultOpfConfig } from './config/default-opf-config';
 import { opfConfidValidator } from './config/opf-config-validator';
 import { OpfEventModule } from './events/opf-event.module';
 
 @NgModule({
-  imports: [
-    OpfEventModule,
-    RouterModule.forChild([
-      {
-        // @ts-ignore
-        path: null,
-        component: OpfPaymentVerificationComponent,
-        data: {
-          cxRoute: 'paymentVerificationResult',
-        },
-      },
-      {
-        // @ts-ignore
-        path: null,
-        component: OpfPaymentVerificationComponent,
-        data: {
-          cxRoute: 'paymentVerificationCancel',
-        },
-      },
-    ]),
-  ],
+  imports: [OpfEventModule],
   providers: [
     provideDefaultConfig(defaultOpfConfig),
     provideDefaultConfig(defaultOPFCheckoutConfig),
-    // TODO OPF: uncomment once proper type and routing is set up
-    provideDefaultConfig(defaultOpfRoutingConfig),
     provideConfigValidator(opfConfidValidator),
   ],
 })
