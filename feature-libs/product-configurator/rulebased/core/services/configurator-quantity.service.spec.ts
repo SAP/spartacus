58575cb5b6af1e66d91b3161d2977eee41f49b4d
Christoph Hinssen <christoph.hinssen@sap.com>
2023-05-23 14:11:20 +0200

    rephrase test

--- feature-libs/product-configurator/rulebased/core/services/configurator-quantity.service.spec.ts
+++ feature-libs/product-configurator/rulebased/core/services/configurator-quantity.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { take } from 'rxjs/operators';
 import { ConfiguratorQuantityService } from './configurator-quantity.service';
+import { cold } from 'jasmine-marbles';
 
 describe('ConfiguratorQuantityService', () => {
   let classUnderTest: ConfiguratorQuantityService;
@@ -14,14 +15,7 @@ describe('ConfiguratorQuantityService', () => {
 
   describe('getQuantity', () => {
     it('should not emit anything until it will be initialized from outside', () => {
-      let result;
-      classUnderTest
-        .getQuantity()
-        .subscribe((quantity) => {
-          result = quantity;
-        })
-        .unsubscribe();
-      expect(result).toBeUndefined();
+      expect(classUnderTest.getQuantity()).toBeObservable(cold(''));
     });
 
     it('should return value that was set with setQuantity', (done) => {
