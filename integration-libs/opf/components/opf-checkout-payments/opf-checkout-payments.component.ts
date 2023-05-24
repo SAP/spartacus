c0498bf0be10e4e6db9a8397c13eb16dcef13009
Matejk00 <mateusz.kolasa@sap.com>
2023-05-24 11:19:27 +0200

    feat: add error handling for active configurations endpoint

--- integration-libs/opf/components/opf-checkout-payments/opf-checkout-payments.component.ts
+++ integration-libs/opf/components/opf-checkout-payments/opf-checkout-payments.component.ts
@@ -11,6 +11,7 @@ import {
   OnDestroy,
   OnInit,
 } from '@angular/core';
+import { GlobalMessageService, GlobalMessageType } from '@spartacus/core';
 import { OpfService } from '@spartacus/opf/core';
 import {
   ActiveConfiguration,
@@ -19,7 +20,7 @@ import {
   OpfUi,
 } from '@spartacus/opf/root';
 import { Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-opf-checkout-payments',
@@ -32,8 +33,14 @@ export class OpfCheckoutPaymentsComponent implements OnInit, OnDestroy {
   activeConfiguratons$ = this.opfCheckoutService
     .getActiveConfigurationsState()
     .pipe(
-      filter((state) => !state.loading),
-      map((state) => state.data)
+      tap((state) => {
+        if (state?.error) {
+          this.globalMessageService.add(
+            { key: 'opf.checkout.errors.loadActiveConfigurations' },
+            GlobalMessageType.MSG_TYPE_ERROR
+          );
+        }
+      })
     );
 
   @Input()
@@ -44,7 +51,8 @@ export class OpfCheckoutPaymentsComponent implements OnInit, OnDestroy {
   constructor(
     protected opfCheckoutService: OpfCheckoutFacade,
     protected opfOtpService: OpfOtpFacade,
-    protected opfService: OpfService
+    protected opfService: OpfService,
+    protected globalMessageService: GlobalMessageService
   ) {}
 
   /**
--- integration-libs/opf/components/opf-checkout-payments/opf-checkout-payments.module.ts
+++ integration-libs/opf/components/opf-checkout-payments/opf-checkout-payments.module.ts
@@ -7,12 +7,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { I18nModule } from '@spartacus/core';
+import { SpinnerModule } from '@spartacus/storefront';
 import { OpfCheckoutPaymentWrapperModule } from '../opf-checkout-payment-wrapper';
 import { OpfCheckoutPaymentsComponent } from './opf-checkout-payments.component';
 
 @NgModule({
   declarations: [OpfCheckoutPaymentsComponent],
   exports: [OpfCheckoutPaymentsComponent],
-  imports: [CommonModule, I18nModule, OpfCheckoutPaymentWrapperModule],
+  imports: [
+    CommonModule,
+    I18nModule,
+    OpfCheckoutPaymentWrapperModule,
+    SpinnerModule,
+  ],
 })
 export class OpfCheckoutPaymentsModule {}
