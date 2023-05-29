31010f1c421e65fd53c0877096ec4a2bba91f091
Hakwoo Kim <hakwoo.kim@sap.com>
2023-05-29 10:32:01 -0400

    clean up endpoints

--- feature-libs/asm/occ/model/occ-asm-endpoints.model.ts
+++ feature-libs/asm/occ/model/occ-asm-endpoints.model.ts
@@ -16,7 +16,6 @@ declare module '@spartacus/core' {
     asmCustomerSearch?: string | OccEndpoint;
     asmCustomerLists?: string | OccEndpoint;
     asmBindCart?: string | OccEndpoint;
-    asmCustomer360?: string | OccEndpoint;
     asmCreateCustomer?: string | OccEndpoint;
   }
 }
