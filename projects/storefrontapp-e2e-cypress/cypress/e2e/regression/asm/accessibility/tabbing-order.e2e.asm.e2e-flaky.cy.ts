11063c1fba879686430cc1b7df310d80ab78c703
niehuayang <126219214+niehuayang@users.noreply.github.com>
2023-05-30 13:42:40 +0800

    fix: Fix ASM e2e accessibility tests which have been commented out (#17453)
    Closes: https://jira.tools.sap/browse/CXSPA-3249

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/accessibility/tabbing-order.e2e.asm.e2e-flaky.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/asm/accessibility/tabbing-order.e2e.asm.e2e-flaky.cy.ts
@@ -6,9 +6,12 @@
 
 import { tabbingOrderConfig as config } from '../../../../helpers/accessibility/tabbing-order.config';
 import {
+  asmTabbingOrderNoSelectedUser,
+  asmTabbingOrderNotLoggedIn,
   asmTabbingOrderWithCreateCustomerForm,
   asmTabbingOrderWithCustomerList,
   asmTabbingOrderWithSaveInactiveCartDialog,
+  asmTabbingOrderWithSelectedUser,
 } from '../../../../helpers/accessibility/tabbing-order/asm';
 
 describe('Tabbing order for ASM', () => {
@@ -17,6 +20,18 @@ describe('Tabbing order for ASM', () => {
   });
 
   context('ASM', () => {
+    it('should allow to navigate with tab key (not logged in)', () => {
+      asmTabbingOrderNotLoggedIn(config.asmNotLoggedIn);
+    });
+
+    it('should allow to navigate with tab key (no user selected)', () => {
+      asmTabbingOrderNoSelectedUser(config.asmNoSelectedUser);
+    });
+
+    it('should allow to navigate with tab key (with user selected)', () => {
+      asmTabbingOrderWithSelectedUser(config.asmWithSelectedUser);
+    });
+
     it('should allow to navigate with tab key for customer List （CXSPA-1595）', () => {
       asmTabbingOrderWithCustomerList(config.asmWithCustomerLists, 'asagent');
     });
