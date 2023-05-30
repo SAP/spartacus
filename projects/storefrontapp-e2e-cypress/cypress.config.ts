cdedfd5af91c0a8f76b78568560a374cb5993895
KateChuen <kate.chuen.cheong@sap.com>
2023-05-30 12:44:32 -0400

    running 6.1 e2e against 2205

--- projects/storefrontapp-e2e-cypress/cypress.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ts
@@ -16,14 +16,14 @@ export default defineConfig({
   env: {
     CLIENT_ID: 'mobile_android',
     CLIENT_SECRET: 'secret',
-    API_URL: 'https://40.76.109.9:9002',
+    API_URL: 'https://20.62.192.238:9002',
     BASE_SITE: 'electronics-spa',
     BASE_LANG: 'en',
     BASE_CURRENCY: 'USD',
     OCC_PREFIX: '/occ/v2',
     OCC_PREFIX_USER_ENDPOINT: 'users',
     OCC_PREFIX_ORDER_ENDPOINT: 'orders',
-    BACK_OFFICE_URL: 'https://40.76.109.9:9002',
+    BACK_OFFICE_URL: 'https://20.62.192.238:9002',
     BACK_OFFICE_PREFIX: '/backoffice',
   },
   e2e: {
