5b0dcbfb125593555be15c3a505efe42967e3c7f
Ulrich Stellmacher <Ulrich.Stellmacher@sap.com>
2023-06-27 13:42:15 +0200

    update module declarations

---
+++ feature-libs/product-configurator/rulebased/cpq/common/cpq-configurator-common.modules.ts
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CpqConfiguratorNormalizer } from '../common/converters/cpq-configurator-normalizer';
+import { CpqConfiguratorOverviewNormalizer } from '../common/converters/cpq-configurator-overview-normalizer';
+import { CpqConfiguratorSerializer } from '../common/converters/cpq-configurator-serializer';
+import { CpqConfiguratorValueSerializer } from '../common/converters/cpq-configurator-value-serializer';
+import {
+    CPQ_CONFIGURATOR_NORMALIZER,
+    CPQ_CONFIGURATOR_OVERVIEW_NORMALIZER,
+    CPQ_CONFIGURATOR_QUANTITY_SERIALIZER,
+    CPQ_CONFIGURATOR_SERIALIZER
+} from '../common/converters/cpq-configurator.converters';
+
+@NgModule({
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: CPQ_CONFIGURATOR_NORMALIZER,
+      useClass: CpqConfiguratorNormalizer,
+      multi: true,
+    },
+    {
+      provide: CPQ_CONFIGURATOR_SERIALIZER,
+      useClass: CpqConfiguratorSerializer,
+      multi: true,
+    },
+    {
+      provide: CPQ_CONFIGURATOR_QUANTITY_SERIALIZER,
+      useClass: CpqConfiguratorValueSerializer,
+      multi: true,
+    },
+    {
+      provide: CPQ_CONFIGURATOR_OVERVIEW_NORMALIZER,
+      useClass: CpqConfiguratorOverviewNormalizer,
+      multi: true,
+    },
+  ],
+})
+export class CpqConfiguratorCommonModule {}
