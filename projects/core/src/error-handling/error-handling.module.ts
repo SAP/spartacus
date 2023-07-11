b9654e3e29406a93da4fc5205c6419ec2613c65d
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-07-11 13:33:22 +0200

    poc effect error handling

--- projects/core/src/error-handling/error-handling.module.ts
+++ projects/core/src/error-handling/error-handling.module.ts
@@ -6,6 +6,24 @@
 
 import { ErrorHandler, ModuleWithProviders, NgModule } from '@angular/core';
 import { CxErrorHandler } from './cx-error-handler';
+import { EFFECTS_ERROR_HANDLER } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export function cxEffectsErrorHandler<T extends Action>(
+  observable$: Observable<T>,
+  errorHandler: ErrorHandler
+): Observable<T> {
+  return observable$.pipe(
+    // @ts-ignore
+    catchError((error) => {
+      if (errorHandler) {
+        errorHandler.handleError(error);
+      }
+    })
+  );
+}
 
 @NgModule()
 export class ErrorHandlingModule {
@@ -16,6 +34,10 @@ export class ErrorHandlingModule {
         {
           provide: ErrorHandler,
           useClass: CxErrorHandler,
+        },
+        {
+          provide: EFFECTS_ERROR_HANDLER,
+          useValue: cxEffectsErrorHandler,
         },
       ],
     };
--- projects/core/src/product/store/effects/product.effect.ts
+++ projects/core/src/product/store/effects/product.effect.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { inject, Injectable } from '@angular/core';
+import { ErrorHandler, inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { merge, Observable, of } from 'rxjs';
@@ -12,12 +12,21 @@ import { catchError, map, mergeMap } from 'rxjs/operators';
 import { AuthActions } from '../../../auth/user-auth/store/actions';
 import { LoggerService } from '../../../logger';
 import { SiteContextActions } from '../../../site-context/store/actions/index';
-import { normalizeHttpError } from '../../../util/normalize-http-error';
 import { bufferDebounceTime } from '../../../util/rxjs/buffer-debounce-time';
 import { withdrawOn } from '../../../util/rxjs/withdraw-on';
 import { ProductConnector } from '../../connectors/product/product.connector';
 import { ScopedProductData } from '../../connectors/product/scoped-product-data';
 import { ProductActions } from '../actions/index';
+import { EntityFailAction } from '../../../state/utils/entity-loader';
+import { EntityScopedLoaderActions } from '../../../state/utils/scoped-loader/entity-scoped-loader.actions';
+import { LoadProductFail } from '../actions/product.action';
+import { HttpErrorResponse } from '@angular/common/http';
+import EntityScopedFailAction = EntityScopedLoaderActions.EntityScopedFailAction;
+
+interface CxEffectError<T extends EntityFailAction | EntityScopedFailAction> {
+  action: T;
+  error: string | HttpErrorResponse;
+}
 
 @Injectable()
 export class ProductEffects {
@@ -71,13 +80,23 @@ export class ProductEffects {
             )
         ),
         catchError((error) => {
-          return of(
-            new ProductActions.LoadProductFail(
+          const action = new ProductActions.LoadProductFail(
               productLoad.code,
-              normalizeHttpError(error, this.logger),
+              error, // ?
               productLoad.scope
-            )
-          );
+            ),
+            effectError: CxEffectError<LoadProductFail> = {
+              action,
+              error,
+            };
+          return this.getAndHandleFailAction(effectError);
+          // return of(
+          //   new ProductActions.LoadProductFail(
+          //     productLoad.code,
+          //     normalizeHttpError(error, this.logger),
+          //     productLoad.scope
+          //   )
+          // );
         })
       ) ??
       of(
@@ -98,8 +117,23 @@ export class ProductEffects {
       )
     );
 
+  // service
+  // - logger
+  // - handler
+  private getAndHandleFailAction(effectError: CxEffectError<any>) {
+    if (effectError.error instanceof HttpErrorResponse) {
+      return of('');
+    }
+
+    // error handler
+    this.errorHandler.handleError(effectError.error);
+
+    return of(effectError.action);
+  }
+
   constructor(
     private actions$: Actions,
-    private productConnector: ProductConnector
+    private productConnector: ProductConnector,
+    private errorHandler: ErrorHandler
   ) {}
 }
