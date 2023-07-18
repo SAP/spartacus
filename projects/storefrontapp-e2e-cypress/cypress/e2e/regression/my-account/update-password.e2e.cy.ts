3095f300d2e8216ce5a7a6d6b1bbbbbd62e48fe2
evy <evy.yang@sap.com>
2023-07-18 10:50:44 +0800

    Update update-password.e2e.cy.ts
    CXSPA-3399, add e2e test case for it.

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/update-password.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/update-password.e2e.cy.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as asm from '../../../helpers/asm';
+import * as checkout from '../../../helpers/checkout-flow';
 import * as alerts from '../../../helpers/global-message';
 import { signOutUser } from '../../../helpers/login';
 import * as updatePassword from '../../../helpers/update-password';
@@ -11,7 +13,6 @@ import { generateMail, randomString } from '../../../helpers/user';
 import { viewportContext } from '../../../helpers/viewport-context';
 import { standardUser } from '../../../sample-data/shared-users';
 import { isolateTests } from '../../../support/utils/test-isolation';
-
 describe('My Account - Update Password', () => {
   viewportContext(['mobile'], () => {
     before(() =>
@@ -87,5 +88,77 @@ describe('My Account - Update Password', () => {
         });
       }
     );
+
+    describe('update password by agent', { testIsolation: false }, () => {
+      isolateTests();
+      before(() => {
+        standardUser.registrationData.email = generateMail(
+          randomString(),
+          true
+        );
+        cy.requireLoggedIn(standardUser);
+        cy.visit('/');
+      });
+
+      it('should display server error if agent try to modify customer password', () => {
+        cy.log('--> Agent logging in');
+        checkout.visitHomePage('asm=true');
+        cy.get('cx-asm-main-ui').should('exist');
+        cy.get('cx-asm-main-ui').should('be.visible');
+        asm.agentLogin('asagent', 'pw4all');
+        cy.log('--> Starting customer emulation');
+        startCustomerEmulation(standardUser.registrationData);
+
+        cy.selectUserMenuOption({
+          option: 'Password',
+        });
+
+        alerts.getErrorAlert().should('not.exist');
+        cy.get('[formcontrolname="oldPassword"]').type(
+          standardUser.registrationData.password
+        );
+        cy.get('[formcontrolname="newPassword"]').type(
+          updatePassword.newPassword
+        );
+        cy.get('[formcontrolname="newPasswordConfirm"]').type(
+          updatePassword.newPassword
+        );
+        cy.get('cx-update-password button.btn-primary').click();
+        cy.url().should('contain', updatePassword.PAGE_URL_UPDATE_PASSWORD);
+        alerts.getErrorAlert().should('contain', 'Access is denied');
+      });
+
+      after(() => {
+        signOutUser();
+      });
+    });
   });
 });
+
+export function startCustomerEmulation(customer, b2b = false): void {
+  const customerSearchRequestAlias = asm.listenForCustomerSearchRequest();
+  const userDetailsRequestAlias = asm.listenForUserDetailsRequest(b2b);
+
+  cy.get('cx-csagent-login-form').should('not.exist');
+  cy.get('cx-customer-selection').should('exist');
+  cy.get('cx-customer-selection form').within(() => {
+    cy.get('[formcontrolname="searchTerm"]')
+      .should('not.be.disabled')
+      .type(customer.email);
+    cy.get('[formcontrolname="searchTerm"]').should(
+      'have.value',
+      `${customer.email}`
+    );
+  });
+  cy.wait(customerSearchRequestAlias)
+    .its('response.statusCode')
+    .should('eq', 200);
+
+  cy.get('cx-customer-selection div.asm-results button').click();
+  cy.get('cx-customer-selection button[type="submit"]').click();
+
+  cy.wait(userDetailsRequestAlias).its('response.statusCode').should('eq', 200);
+  cy.get('cx-csagent-login-form').should('not.exist');
+  cy.get('cx-customer-selection').should('not.exist');
+  cy.get('cx-customer-emulation').should('be.visible');
+}
