e1b4716b5132bce3a1fa478b210b916c87ad6197
Christoph Hinssen <christoph.hinssen@sap.com>
2023-07-28 08:36:41 +0200

    Fix 'new cart'

--- feature-libs/quote/components/quote-action-links/quote-action-links.service.spec.ts
+++ feature-libs/quote/components/quote-action-links/quote-action-links.service.spec.ts
@@ -1,27 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import {
-  ActiveCartFacade,
-  MultiCartFacade,
-  DeleteCartSuccessEvent as ClearActiveCartSuccessEvent,
-} from '@spartacus/cart/base/root';
+import { Cart, MultiCartFacade } from '@spartacus/cart/base/root';
 import { Observable, of } from 'rxjs';
-import { UserIdService, EventService, RoutingService } from '@spartacus/core';
+import { UserIdService, RoutingService } from '@spartacus/core';
 import { QuoteActionLinksService } from './quote-action-links.service';
 import createSpy = jasmine.createSpy;
 
-class MockActiveCartFacade implements Partial<ActiveCartFacade> {
-  getActiveCartId(): Observable<string> {
-    return of();
-  }
-}
+const newCart: Cart = {};
 
 class MockMultiCartFacade implements Partial<MultiCartFacade> {
-  deleteCart(): void {}
-}
-
-class MockEventService implements Partial<EventService> {
-  get(): Observable<any> {
-    return of();
+  createCart(): Observable<Cart> {
+    return of(newCart);
   }
 }
 
@@ -37,52 +25,42 @@ class MockRoutingService implements Partial<RoutingService> {
 
 describe('QuoteActionLinksService', () => {
   let service: QuoteActionLinksService;
-  let activeCartFacade: ActiveCartFacade;
-  let multiCartFacade: MultiCartFacade;
-  let eventService: EventService;
   let userIdService: UserIdService;
   let routingService: RoutingService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: ActiveCartFacade, useClass: MockActiveCartFacade },
         { provide: MultiCartFacade, useClass: MockMultiCartFacade },
-        { provide: EventService, useClass: MockEventService },
         { provide: UserIdService, useClass: MockUserIdService },
         { provide: RoutingService, useClass: MockRoutingService },
       ],
     }).compileComponents();
 
     service = TestBed.inject(QuoteActionLinksService);
-    eventService = TestBed.inject(EventService);
     userIdService = TestBed.inject(UserIdService);
-    activeCartFacade = TestBed.inject(ActiveCartFacade);
-    multiCartFacade = TestBed.inject(MultiCartFacade);
     routingService = TestBed.inject(RoutingService);
+    spyOn(userIdService, 'getUserId').and.returnValue(of('current'));
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should clear the active cart', () => {
-    spyOn(eventService, 'get').and.returnValue(of(ClearActiveCartSuccessEvent));
-    spyOn(multiCartFacade, 'deleteCart').and.callThrough();
-    spyOn(userIdService, 'getUserId').and.returnValue(of('current'));
-    spyOn(activeCartFacade, 'getActiveCartId').and.returnValue(of('00001122'));
-
-    service.clearCart().subscribe().unsubscribe();
-
-    expect(eventService.get).toHaveBeenCalled();
-    expect(multiCartFacade.deleteCart).toHaveBeenCalled();
-    expect(userIdService.getUserId).toHaveBeenCalled();
+  describe('createNewCart', () => {
+    it('should create a new cart ', (done) => {
+      service['createNewCart']().subscribe((cart) => {
+        expect(cart).toBe(newCart);
+        expect(userIdService.getUserId).toHaveBeenCalled();
+        done();
+      });
+    });
   });
 
-  it('should redirect to the cart page', () => {
-    spyOn(service, 'clearCart').and.returnValue(of(true));
-    service.goToNewCart();
-
-    expect(routingService.go).toHaveBeenCalledWith({ cxRoute: 'cart' });
+  describe('goToNewCart', () => {
+    it('should redirect to the cart page', () => {
+      service.goToNewCart();
+      expect(routingService.go).toHaveBeenCalledWith({ cxRoute: 'cart' });
+    });
   });
 });
--- feature-libs/quote/components/quote-action-links/quote-action-links.service.ts
+++ feature-libs/quote/components/quote-action-links/quote-action-links.service.ts
@@ -5,55 +5,43 @@
  */
 
 import { Injectable } from '@angular/core';
-import {
-  ActiveCartFacade,
-  DeleteCartFailEvent,
-  DeleteCartSuccessEvent,
-  MultiCartFacade,
-} from '@spartacus/cart/base/root';
-import { EventService, RoutingService, UserIdService } from '@spartacus/core';
+import { Cart, MultiCartFacade } from '@spartacus/cart/base/root';
+import { RoutingService, UserIdService } from '@spartacus/core';
 import { QuoteCartService } from '@spartacus/quote/root';
-import { Observable, merge } from 'rxjs';
-import { tap, take, withLatestFrom, switchMap, mapTo } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { tap, take, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuoteActionLinksService {
   constructor(
-    protected activeCartFacade: ActiveCartFacade,
     protected userIdService: UserIdService,
     protected multiCartFacade: MultiCartFacade,
     protected routingService: RoutingService,
-    protected eventService: EventService,
     protected quoteCartService: QuoteCartService
   ) {}
 
-  clearCart(): Observable<boolean> {
-    return this.activeCartFacade.getActiveCartId().pipe(
-      withLatestFrom(this.userIdService.getUserId()),
+  protected createNewCart(): Observable<Cart> {
+    return this.userIdService.getUserId().pipe(
       take(1),
-      //TODO switch to create cart , in tap only disable quote cart
-      tap(([cartId, userId]) => {
-        this.multiCartFacade.createCart({userId, oldCartId:undefined, toMergeCartGuid:undefined,extraData:{active:true}});
-        console.log("CHHI old id: "+ cartId);
-        //this.multiCartFacade.deleteCart(cartId, userId);
+      switchMap((userId) =>
+        this.multiCartFacade.createCart({
+          userId,
+          oldCartId: undefined,
+          toMergeCartGuid: undefined,
+          extraData: { active: true },
+        })
+      ),
+      tap(() => {
         this.quoteCartService.setQuoteCartActive(false);
-      }),
-      switchMap(() =>
-        merge(
-          this.eventService.get(DeleteCartSuccessEvent).pipe(mapTo(true)),
-          this.eventService.get(DeleteCartFailEvent).pipe(mapTo(false))
-        ).pipe(take(1))
-      )
+      })
     );
   }
 
   goToNewCart(): void {
-    this.clearCart().subscribe((isCartEmpty: boolean) => {
-      if (isCartEmpty) {
-        this.routingService.go({ cxRoute: 'cart' });
-      }
+    this.createNewCart().subscribe(() => {
+      this.routingService.go({ cxRoute: 'cart' });
     });
   }
 }
