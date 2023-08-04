ecfe23617e23b7846bdc15e6de63d6625fb6ae50
Caine Rotherham <rotherham.c@gmail.com>
2023-08-04 15:11:38 +0200

    chore: Release 6.3.0 (#17729)

--- tools/config/const.ts
+++ tools/config/const.ts
@@ -10,4 +10,4 @@ export const SPARTACUS_SCOPE = '@spartacus';
 export const SAP_SCOPE = 'sap';
 export const SAPUI5_TYPES = '@sapui5/ts-types-esm';
 export const SPARTACUS_SCHEMATICS = `${SPARTACUS_SCOPE}/schematics`;
-export const PUBLISHING_VERSION = '6.3.0-1';
+export const PUBLISHING_VERSION = '6.3.0';
