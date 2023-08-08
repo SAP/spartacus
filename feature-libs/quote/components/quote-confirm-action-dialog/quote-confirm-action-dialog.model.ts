94b64cfcde81a33da08e5766b6958c4102f7e416
Ulrich Stellmacher <Ulrich.Stellmacher@sap.com>
2023-08-08 19:03:46 +0200

    pretty print

--- feature-libs/quote/components/quote-confirm-action-dialog/quote-confirm-action-dialog.model.ts
+++ feature-libs/quote/components/quote-confirm-action-dialog/quote-confirm-action-dialog.model.ts
@@ -4,16 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Quote } from "@spartacus/quote/root";
+import { Quote } from '@spartacus/quote/root';
 
 export interface ConfirmationContext {
-    quote: Quote;
-    title: string;
-    confirmNote: string;
-    warningNote?: string;
-    validity?: string;
-    successMessage?: string;
-  }
+  quote: Quote;
+  title: string;
+  confirmNote: string;
+  warningNote?: string;
+  validity?: string;
+  successMessage?: string;
+}
 
 export type ConfirmActionDialogConfig = {
   i18nKey: string;
