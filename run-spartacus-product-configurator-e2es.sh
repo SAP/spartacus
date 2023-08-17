31cdca6d69050b787b9e31d6df77c5eb55b02781
Alexander Pilarczyk <54265591+alepil@users.noreply.github.com>
2023-08-17 11:36:59 +0200

    Update run-spartacus-product-configurator-e2es.sh

--- run-spartacus-product-configurator-e2es.sh
+++ run-spartacus-product-configurator-e2es.sh
@@ -1,1 +1,7 @@
-product-configurator
+#!/bin/bash
+
+# install cypress standlone dependencies
+(cd projects/storefrontapp-e2e-cypress && npm install)
+
+# run spartacus ccv2 e2es for product-configurator
+npm run e2e:run:ci:ccv2-product-configurator
