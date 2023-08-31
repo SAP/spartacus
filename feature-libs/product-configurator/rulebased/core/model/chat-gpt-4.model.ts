452ec998e063474c5492dd7437343865a33b1f71
Ulrich Stellmacher <Ulrich.Stellmacher@sap.com>
2023-08-31 17:15:47 +0200

    add models for request and response

--- feature-libs/product-configurator/rulebased/core/model/chat-gpt-4.model.ts
+++ feature-libs/product-configurator/rulebased/core/model/chat-gpt-4.model.ts
@@ -4,6 +4,30 @@ export namespace ChatGPT4 {
     content: string;
   }
 
+  export interface Request {
+    deployment_id: string;
+    messages: Message[];
+  }
+
+  export interface Response {
+    choices: Choice[];
+    created: Date;
+    id: string;
+    usage: Usage;
+  }
+
+  export interface Choice {
+    finish_reason: string;
+    index: number;
+    message: Message;
+  }
+
+  export interface Usage {
+    completion_tokens: number;
+    prompt_tokens: number;
+    total_tokens: Message;
+  }
+
   export enum Role {
     USER = 'user',
     ASSISTANT = 'assistant',
