bada5a23b6dd9e70250ebd42810f11b08601aba0
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-09-06 13:27:17 +0200

    Update rendering-strategy-resolver.ts

--- core-libs/setup/ssr/optimized-engine/rendering-strategy-resolver.ts
+++ core-libs/setup/ssr/optimized-engine/rendering-strategy-resolver.ts
@@ -48,11 +48,10 @@ const shouldFallbackToCsr = (
  * Creates a rendering strategy resolver function with the specified options.
  *
  * @function
- * @param {RenderingStrategyResolverOptions} options - The options to configure the rendering strategy resolver.
- * @param {string[]} [options.excludedUrls] - An optional array of URLs for which server-side rendering (SSR) should be disabled.
- * @param {string[]} [options.excludedParams] - An optional array of Query parameters for which SSR should be disabled.
- * @param {Request} request - The request object for which the rendering strategy should be determined.
- * @returns {function(request:Request): RenderingStrategy} - A rendering strategy resolver function that takes a Request object
+ * @param  options - The options to configure the rendering strategy resolver.
+ * @param [options.excludedUrls] - An optional array of URLs for which server-side rendering (SSR) should be disabled.
+ * @param [options.excludedParams] - An optional array of Query parameters for which SSR should be disabled.
+ * @returns A rendering strategy resolver function that takes a Request object
  * as a parameter and returns the rendering strategy to be applied for the request, which can be either
  * `RenderingStrategy.ALWAYS_CSR` or `RenderingStrategy.DEFAULT`.
  */
