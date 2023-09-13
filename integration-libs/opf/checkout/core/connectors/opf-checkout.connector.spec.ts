5edbc54a0744d7aa02573bf0b1a2a3f3282f09f5
Mateusz Kolasa <mateusz.kolasa@sap.com>
2023-09-13 10:23:04 +0200

    fix: Update unit tests coverage
    Closes: CXSPA-4621

--- integration-libs/opf/checkout/core/connectors/opf-checkout.connector.spec.ts
+++ integration-libs/opf/checkout/core/connectors/opf-checkout.connector.spec.ts
@@ -1,40 +1,55 @@
-// TODO: Add unit tests
+import { TestBed } from '@angular/core/testing';
+import { ActiveConfiguration } from '@spartacus/opf/checkout/root';
+import { EMPTY, Observable } from 'rxjs';
+import { PaymentInitiationConfig, PaymentSessionData } from '../../root/model';
+import { OpfCheckoutConnector } from './opf-checkout.connector';
+import { OpfAdapter } from './opf.adapter';
 
-// import { TestBed } from '@angular/core/testing';
-// import { ActiveConfiguration } from '@spartacus/opf/root';
-// import { EMPTY, Observable } from 'rxjs';
-// import { OpfCheckoutConnector } from './opf-checkout.connector';
-// import { OpfAdapter } from './opf.adapter';
+class MockOpfAdapter implements Partial<OpfAdapter> {
+  getActiveConfigurations(): Observable<ActiveConfiguration[]> {
+    return EMPTY;
+  }
+  initiatePayment(
+    _paymentConfig: PaymentInitiationConfig
+  ): Observable<PaymentSessionData> {
+    return EMPTY;
+  }
+}
 
-// class MockOpfAdapter implements Partial<OpfAdapter> {
-//   getActiveConfigurations(): Observable<ActiveConfiguration[]> {
-//     return EMPTY;
-//   }
-// }
+describe('OpfCheckoutConnector', () => {
+  let service: OpfCheckoutConnector;
+  let adapter: OpfAdapter;
 
-// describe('OpfCheckoutConnector', () => {
-//   let service: OpfCheckoutConnector;
-//   let adapter: OpfAdapter;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        OpfCheckoutConnector,
+        { provide: OpfAdapter, useClass: MockOpfAdapter },
+      ],
+    });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         OpfCheckoutConnector,
-//         { provide: OpfAdapter, useClass: MockOpfAdapter },
-//       ],
-//     });
+    service = TestBed.inject(OpfCheckoutConnector);
+    adapter = TestBed.inject(OpfAdapter);
+  });
 
-//     service = TestBed.inject(OpfCheckoutConnector);
-//     adapter = TestBed.inject(OpfAdapter);
-//   });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
+  it('getActiveConfigurations should call adapter', () => {
+    spyOn(adapter, 'getActiveConfigurations').and.stub();
+    service.getActiveConfigurations();
+    expect(adapter.getActiveConfigurations).toHaveBeenCalled();
+  });
 
-//   it('getActiveConfigurations should call adapter', () => {
-//     spyOn(adapter, 'getActiveConfigurations').and.stub();
-//     service.getActiveConfigurations();
-//     expect(adapter.getActiveConfigurations).toHaveBeenCalled();
-//   });
-// });
+  it('initiatePayment should call adapter', () => {
+    const paymentConfig: PaymentInitiationConfig = {
+      otpKey: 'test',
+      config: {},
+    };
+    spyOn(adapter, 'initiatePayment').and.stub();
+    service.initiatePayment(paymentConfig);
+
+    expect(adapter.initiatePayment).toHaveBeenCalledWith(paymentConfig);
+  });
+});
