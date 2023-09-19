73d66d8d6db5f1f18911ca74cc83ac030a3f2886
Melody <Melody.zhou@sap.com>
2023-09-19 14:33:50 +0800

    New Notification Preference Component
    As a customer, I want to view and update my preferred notification channels

---
+++ projects/storefrontlib/cms-components/myaccount/new-notification-preference/new-notification-preference.module.ts
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  AuthGuard,
+  I18nModule,
+} from '@spartacus/core';
+import { CmsPageGuard } from '../../../cms-structure/guards/cms-page.guard';
+import { PageLayoutComponent } from '../../../cms-structure/page/page-layout/page-layout.component';
+import { SpinnerModule } from '../../../shared/components/spinner/spinner.module';
+import { NewNotificationPreferenceComponent } from './new-notification-preference.component';
+
+@NgModule({
+  declarations: [NewNotificationPreferenceComponent],
+  imports: [
+    CommonModule,
+    SpinnerModule,
+    I18nModule,
+    RouterModule.forChild([
+      {
+        // @ts-ignore
+        path: null,
+        canActivate: [AuthGuard, CmsPageGuard],
+        component: PageLayoutComponent,
+        data: { cxRoute: 'notificationPreference' },
+      },
+    ]),
+  ],
+  exports: [NewNotificationPreferenceComponent],
+})
+export class NewNotificationPreferenceModule {}
