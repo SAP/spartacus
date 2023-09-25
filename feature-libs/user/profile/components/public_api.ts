bcc8b8fb725dd2110a5c80e5ecb63f6133aa7d0c
evy <evy.yang@sap.com>
2023-09-25 17:40:29 +0800

    remove uselss line

--- feature-libs/user/profile/components/public_api.ts
+++ feature-libs/user/profile/components/public_api.ts
@@ -6,12 +6,10 @@
 
 export * from './close-account/index';
 export * from './forgot-password/index';
+export * from './new-password/index';
 export * from './register/index';
 export * from './reset-password/index';
 export * from './update-email/index';
 export * from './update-password/index';
 export * from './update-profile/index';
 export * from './user-profile-components.module';
-export * from './new-password/index';
-
-
