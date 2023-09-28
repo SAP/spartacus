12cce4082617771af38c49d6168d600578733bff
Matejk00 <mateusz.kolasa@sap.com>
2023-09-28 13:43:05 +0200

    Updates

--- feature-libs/checkout/base/root/config/default-checkout-config.ts
+++ feature-libs/checkout/base/root/config/default-checkout-config.ts
@@ -7,46 +7,36 @@
 import { CheckoutStepType } from '../model/checkout-step.model';
 import { CheckoutConfig, DeliveryModePreferences } from './checkout-config';
 
-/**
- * TODO: Verify this change before the final review,
- * as it could potentially be considered as a breaking change.
- * This modification was made for testing purposes
- * to allow switching between flows on the epic/opf branch.
- */
 export const defaultCheckoutConfig: CheckoutConfig = {
   checkout: {
-    flows: {
-      mockup: {
-        steps: [
-          {
-            id: 'deliveryAddress',
-            name: 'checkoutProgress.deliveryAddress',
-            routeName: 'checkoutDeliveryAddress',
-            type: [CheckoutStepType.DELIVERY_ADDRESS],
-          },
-          {
-            id: 'deliveryMode',
-            name: 'checkoutProgress.deliveryMode',
-            routeName: 'checkoutDeliveryMode',
-            type: [CheckoutStepType.DELIVERY_MODE],
-          },
-          {
-            id: 'paymentDetails',
-            name: 'checkoutProgress.paymentDetails',
-            routeName: 'checkoutPaymentDetails',
-            type: [CheckoutStepType.PAYMENT_DETAILS],
-          },
-          {
-            id: 'reviewOrder',
-            name: 'checkoutProgress.reviewOrder',
-            routeName: 'checkoutReviewOrder',
-            type: [CheckoutStepType.REVIEW_ORDER],
-          },
-        ],
-        express: false,
-        defaultDeliveryMode: [DeliveryModePreferences.FREE],
-        guest: false,
+    steps: [
+      {
+        id: 'deliveryAddress',
+        name: 'checkoutProgress.deliveryAddress',
+        routeName: 'checkoutDeliveryAddress',
+        type: [CheckoutStepType.DELIVERY_ADDRESS],
       },
-    },
+      {
+        id: 'deliveryMode',
+        name: 'checkoutProgress.deliveryMode',
+        routeName: 'checkoutDeliveryMode',
+        type: [CheckoutStepType.DELIVERY_MODE],
+      },
+      {
+        id: 'paymentDetails',
+        name: 'checkoutProgress.paymentDetails',
+        routeName: 'checkoutPaymentDetails',
+        type: [CheckoutStepType.PAYMENT_DETAILS],
+      },
+      {
+        id: 'reviewOrder',
+        name: 'checkoutProgress.reviewOrder',
+        routeName: 'checkoutReviewOrder',
+        type: [CheckoutStepType.REVIEW_ORDER],
+      },
+    ],
+    express: false,
+    defaultDeliveryMode: [DeliveryModePreferences.FREE],
+    guest: false,
   },
 };
