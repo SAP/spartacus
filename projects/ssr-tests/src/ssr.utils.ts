dddf50666aabe730652d6e0f796b6bf3acf0588d
Caine Rotherham <rotherham.c@gmail.com>
2023-10-02 13:24:59 +0200

    remove debug code and leave verbose

--- projects/ssr-tests/src/ssr.utils.ts
+++ projects/ssr-tests/src/ssr.utils.ts
@@ -20,10 +20,7 @@ export async function startSsrServer(port = 4000) {
   childProcess.exec(
     `NODE_TLS_REJECT_UNAUTHORIZED=0 PORT=${port} npm run serve:ssr --prefix ../../> ssr.log`
   );
-  console.log('starting...');
   await Log.waitUntilLogContainsText(`Node Express server listening on `);
-  console.log('STARTED');
-  console.log(Log.getLogText());
 }
 
 /**
