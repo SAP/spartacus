d303be11e25218aa819d95e03b98eb689f8e5d6a
anjana-bl <anjana.b.l@sap.com>
2023-10-03 21:07:29 +0530

    Update index_spec.ts

--- integration-libs/cdc/schematics/add-cdc/index_spec.ts
+++ integration-libs/cdc/schematics/add-cdc/index_spec.ts
@@ -167,7 +167,7 @@ describe('Spartacus CDC schematics: ng-add', () => {
           ) {
             continue;
           }
-          // TODO: after 4.0: use this test, as we'll have synced versions between lib's and root package.json
+          // CXSPA-4872: after 4.0: use this test, as we'll have synced versions between lib's and root package.json
           // const expectedVersion = (peerDependencies as Record<
           //   string,
           //   string
