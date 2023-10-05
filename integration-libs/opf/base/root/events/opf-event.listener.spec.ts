549d6db24c6b9ea45c93d4a4474dec0115acb839
Roman Chygryn <129765378+rmch91@users.noreply.github.com>
2023-10-05 17:47:36 +0200

    Fix coverage for some opf tests  (#17914)

--- integration-libs/opf/base/root/events/opf-event.listener.spec.ts
+++ integration-libs/opf/base/root/events/opf-event.listener.spec.ts
@@ -1,62 +1,71 @@
-// import { TestBed } from '@angular/core/testing';
-// import { CreateCartEvent } from '@spartacus/cart/base/root';
-// import { CxEvent, EventService, LoginEvent } from '@spartacus/core';
-// import { OrderPlacedEvent } from '@spartacus/order/root';
-// import { Subject } from 'rxjs';
-// import { OpfService } from '../services';
-// import { OpfEventListenerService } from './opf-event.listener';
+import { TestBed } from '@angular/core/testing';
+import { CreateCartEvent } from '@spartacus/cart/base/root';
+import { CxEvent, EventService, LoginEvent } from '@spartacus/core';
+import { OrderPlacedEvent } from '@spartacus/order/root';
+import { Subject } from 'rxjs';
+import { OpfService } from '../services';
+import { OpfEventListenerService } from './opf-event.listener';
 
-// import createSpy = jasmine.createSpy;
+import createSpy = jasmine.createSpy;
 
-// const mockEventStream$ = new Subject<CxEvent>();
+const mockEventStream$ = new Subject<CxEvent>();
 
-// class MockOpfService implements Partial<OpfService> {
-//   clearOpfMetadataState = createSpy();
-// }
+class MockOpfService implements Partial<OpfService> {
+  clearOpfMetadataState = createSpy();
+}
 
-// class MockEventService implements Partial<EventService> {
-//   get = createSpy().and.returnValue(mockEventStream$.asObservable());
-// }
+class MockEventService implements Partial<EventService> {
+  get = createSpy().and.returnValue(mockEventStream$.asObservable());
+}
 
-// describe(`OpfEventListenerService`, () => {
-//   let opfService: OpfService;
+describe(`OpfEventListenerService`, () => {
+  let opfService: OpfService;
+  let service: OpfEventListenerService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         OpfEventListenerService,
-//         {
-//           provide: EventService,
-//           useClass: MockEventService,
-//         },
-//         {
-//           provide: OpfService,
-//           useClass: MockOpfService,
-//         },
-//       ],
-//     });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        OpfEventListenerService,
+        {
+          provide: EventService,
+          useClass: MockEventService,
+        },
+        {
+          provide: OpfService,
+          useClass: MockOpfService,
+        },
+      ],
+    });
 
-//     TestBed.inject(OpfEventListenerService);
-//     opfService = TestBed.inject(OpfService);
-//   });
+    service = TestBed.inject(OpfEventListenerService);
+    opfService = TestBed.inject(OpfService);
+  });
 
-//   describe(`onOpfPaymentMetadataResetConditionsMet`, () => {
-//     it(`LoginEvent should call clearOpfMetadataState() method`, () => {
-//       mockEventStream$.next(new LoginEvent());
+  describe(`onOpfPaymentMetadataResetConditionsMet`, () => {
+    it(`LoginEvent should call clearOpfMetadataState() method`, () => {
+      mockEventStream$.next(new LoginEvent());
 
-//       expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
-//     });
+      expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
+    });
 
-//     it(`OrderPlacedEvent should call clearOpfMetadataState() method`, () => {
-//       mockEventStream$.next(new OrderPlacedEvent());
+    it(`OrderPlacedEvent should call clearOpfMetadataState() method`, () => {
+      mockEventStream$.next(new OrderPlacedEvent());
 
-//       expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
-//     });
+      expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
+    });
 
-//     it(`CreateCartEvent should call clearOpfMetadataState() method`, () => {
-//       mockEventStream$.next(new CreateCartEvent());
+    it(`CreateCartEvent should call clearOpfMetadataState() method`, () => {
+      mockEventStream$.next(new CreateCartEvent());
 
-//       expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
-//     });
-//   });
-// });
+      expect(opfService.clearOpfMetadataState).toHaveBeenCalled();
+    });
+
+    it('should unsubscribe from subscriptions on destroy', () => {
+      spyOn(service['subscriptions'], 'unsubscribe');
+
+      service.ngOnDestroy();
+
+      expect(service['subscriptions'].unsubscribe).toHaveBeenCalled();
+    });
+  });
+});
