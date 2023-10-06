0d36166b26cf7146f2f19d0d380c965758761ba7
Florent Letendre <florent.letendre01@sap.com>
2023-10-06 10:39:33 -0400

    fix: component with restriction is not displayed after being changed in SmartEdit (#17893)
    CXSPA-3495
    
    SmartEdit Interceptor modified to improve productList request. This fix is effective with a server enhancement described on CCMTD-46304.

--- feature-libs/smartedit/root/http-interceptors/cms-ticket.interceptor.spec.ts
+++ feature-libs/smartedit/root/http-interceptors/cms-ticket.interceptor.spec.ts
@@ -1,10 +1,17 @@
-import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { inject, TestBed } from '@angular/core/testing';
-import { defaultOccConfig } from '@spartacus/core';
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  FeatureConfigService,
+  PageContext,
+  PageType,
+  RoutingService,
+  defaultOccConfig,
+} from '@spartacus/core';
+import { Observable, of } from 'rxjs';
 import { SmartEditLauncherService } from '../services/smart-edit-launcher.service';
 import { CmsTicketInterceptor } from './cms-ticket.interceptor';
 
@@ -16,10 +23,21 @@ class MockSmartEditLauncherService {
     return this._cmsTicketId;
   }
 }
+const mockPageContext = {
+  id: 'mockCode',
+  type: PageType.CATEGORY_PAGE,
+};
+class MockRoutingService implements Partial<RoutingService> {
+  getPageContext(): Observable<PageContext> {
+    return of(mockPageContext);
+  }
+}
 
 describe('CmsTicketInterceptor', () => {
   let httpMock: HttpTestingController;
   let service: SmartEditLauncherService;
+  let routingService: RoutingService;
+  let featureConfig: FeatureConfigService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,7 +47,10 @@ describe('CmsTicketInterceptor', () => {
           provide: SmartEditLauncherService,
           useClass: MockSmartEditLauncherService,
         },
-
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService,
+        },
         {
           provide: HTTP_INTERCEPTORS,
           useClass: CmsTicketInterceptor,
@@ -40,6 +61,8 @@ describe('CmsTicketInterceptor', () => {
 
     httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(SmartEditLauncherService);
+    routingService = TestBed.inject(RoutingService);
+    featureConfig = TestBed.inject(FeatureConfigService);
   });
 
   afterEach(() => {
@@ -119,6 +142,63 @@ describe('CmsTicketInterceptor', () => {
       expect(mockReq.request.params.get('cmsTicketId')).toEqual(
         'mockCmsTicketId'
       );
+      mockReq.flush('somedata');
+    }
+  ));
+
+  it('should add parameters for productList requests: cmsTicketId, pageType, code', inject(
+    [HttpClient],
+    (http: HttpClient) => {
+      spyOn(featureConfig, 'isLevel').and.returnValue(true);
+      spyOnProperty(service, 'cmsTicketId', 'get').and.returnValue(
+        'mockCmsTicketId'
+      );
+
+      http.get(`${OccUrl}/productList`).subscribe((result) => {
+        expect(result).toBeTruthy();
+      });
+      const mockReq = httpMock.expectOne((req) => {
+        return req.method === 'GET';
+      });
+
+      expect(mockReq.request.params.get('cmsTicketId')).toEqual(
+        'mockCmsTicketId'
+      );
+      expect(mockReq.request.params.get('pageType')).toEqual(
+        PageType.CATEGORY_PAGE
+      );
+      expect(mockReq.request.params.get('code')).toEqual('mockCode');
+      mockReq.flush('somedata');
+    }
+  ));
+
+  it('should add only one parameter for productList requests when pageContext is partial: cmsTicketId', inject(
+    [HttpClient],
+    (http: HttpClient) => {
+      spyOn(featureConfig, 'isLevel').and.returnValue(true);
+      spyOn(routingService, 'getPageContext').and.returnValue(
+        of({
+          ...mockPageContext,
+          id: '',
+        })
+      );
+
+      spyOnProperty(service, 'cmsTicketId', 'get').and.returnValue(
+        'mockCmsTicketId'
+      );
+
+      http.get(`${OccUrl}/productList`).subscribe((result) => {
+        expect(result).toBeTruthy();
+      });
+      const mockReq = httpMock.expectOne((req) => {
+        return req.method === 'GET';
+      });
+
+      expect(mockReq.request.params.get('cmsTicketId')).toEqual(
+        'mockCmsTicketId'
+      );
+      expect(mockReq.request.params.get('pageType')).toBeFalsy();
+      expect(mockReq.request.params.get('code')).toBeFalsy();
       mockReq.flush('somedata');
     }
   ));
