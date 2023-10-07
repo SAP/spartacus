89bd1386344b28ff13f47c3db17baf06f473bdfd
Jayashree <ayashree.govindasamy@sap.com>
2023-10-07 15:50:45 +0530

    update view name component

--- feature-libs/user/account/components/myaccount-view-name/myaccount-view-name.component.ts
+++ feature-libs/user/account/components/myaccount-view-name/myaccount-view-name.component.ts
@@ -4,34 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Component, OnInit } from '@angular/core';
-import { User, AuthService } from '@spartacus/core';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { UserAccountFacade } from '../../root/facade';
+import { Component } from '@angular/core';
+import { LoginComponent } from '../login';
 
 @Component({
   selector: 'cx-myaccount-view-name',
   templateUrl: './myaccount-view-name.component.html',
-  styleUrls: ['./myaccount-view-name.component.scss']
+  styleUrls: ['./myaccount-view-name.component.scss'],
 })
-export class MyaccountViewNameComponent implements OnInit {
-  user$: Observable<User | undefined>;
-
-  constructor(
-    protected auth: AuthService,
-    protected userAccount: UserAccountFacade
-  ) {}
-
-  ngOnInit(): void {
-    this.user$ = this.auth.isUserLoggedIn().pipe(
-      switchMap((isUserLoggedIn) => {
-        if (isUserLoggedIn) {
-          return this.userAccount.get();
-        } else {
-          return of(undefined);
-        }
-      })
-    );
-  }
-}
+export class MyaccountViewNameComponent extends LoginComponent {}
