182dfa5833541b9b74f2ba35be6a6f4593a34d16
Jayashree <ayashree.govindasamy@sap.com>
2023-10-07 20:57:14 +0530

    Update myaccount-view-side-navigation.component.ts

--- projects/storefrontlib/cms-components/myaccount/myaccount-view-side-navigation/myaccount-view-side-navigation.component.ts
+++ projects/storefrontlib/cms-components/myaccount/myaccount-view-side-navigation/myaccount-view-side-navigation.component.ts
@@ -12,17 +12,4 @@ import { NavigationComponent } from '../../navigation';
   templateUrl: './myaccount-view-side-navigation.component.html',
 })
 export class MyaccountViewSideNavigationComponent extends NavigationComponent{
-  // user$: Observable<User | undefined>;
-  // node$: Observable<NavigationNode> = this.service.getNavigationNode(
-  //   this.componentData.data$
-  // );
-
-  // styleClass$: Observable<string | undefined> = this.componentData.data$.pipe(
-  //   map((d) => d?.styleClass)
-  // );
-
-  // constructor(
-  //   protected componentData: CmsComponentData<CmsNavigationComponent>,
-  //   protected service: NavigationService
-  // ) {}
 }
