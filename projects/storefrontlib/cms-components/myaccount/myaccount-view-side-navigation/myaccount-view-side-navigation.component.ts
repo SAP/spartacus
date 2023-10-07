ff696b2a35ca95ff7cf7db4a2135824011614403
Jayashree <ayashree.govindasamy@sap.com>
2023-10-07 17:35:21 +0530

    plain navigation component

--- projects/storefrontlib/cms-components/myaccount/myaccount-view-side-navigation/myaccount-view-side-navigation.component.ts
+++ projects/storefrontlib/cms-components/myaccount/myaccount-view-side-navigation/myaccount-view-side-navigation.component.ts
@@ -5,29 +5,24 @@
  */
 
 import { Component } from '@angular/core';
-import { User, CmsNavigationComponent } from '@spartacus/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { NavigationNode, NavigationService } from '../../navigation';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { CmsComponentData } from '@spartacus/storefront';
+import { NavigationComponent } from '../../navigation';
 
 @Component({
   selector: 'cx-myaccount-view-side-navigation',
   templateUrl: './myaccount-view-side-navigation.component.html',
 })
-export class MyaccountViewSideNavigationComponent{
-  user$: Observable<User | undefined>;
-  node$: Observable<NavigationNode> = this.service.getNavigationNode(
-    this.componentData.data$
-  );
+export class MyaccountViewSideNavigationComponent extends NavigationComponent{
+  // user$: Observable<User | undefined>;
+  // node$: Observable<NavigationNode> = this.service.getNavigationNode(
+  //   this.componentData.data$
+  // );
 
-  styleClass$: Observable<string | undefined> = this.componentData.data$.pipe(
-    map((d) => d?.styleClass)
-  );
+  // styleClass$: Observable<string | undefined> = this.componentData.data$.pipe(
+  //   map((d) => d?.styleClass)
+  // );
 
-  constructor(
-    protected componentData: CmsComponentData<CmsNavigationComponent>,
-    protected service: NavigationService
-  ) {}
+  // constructor(
+  //   protected componentData: CmsComponentData<CmsNavigationComponent>,
+  //   protected service: NavigationService
+  // ) {}
 }
