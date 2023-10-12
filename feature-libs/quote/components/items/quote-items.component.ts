ca4cc6a83a05957bfc6e5b1e77d9d7acc3af5528
Christoph Hinssen <christoph.hinssen@sap.com>
2023-10-12 16:37:33 +0200

    CXSPA-4968 load quote cart if deviating from session cart / part 1

--- feature-libs/quote/components/items/quote-items.component.ts
+++ feature-libs/quote/components/items/quote-items.component.ts
@@ -5,11 +5,18 @@
  */
 
 import { Component, inject } from '@angular/core';
-import { CartOutlets, ActiveCartFacade, Cart } from '@spartacus/cart/base/root';
+import {
+  CartOutlets,
+  ActiveCartFacade,
+  Cart,
+  MultiCartFacade,
+} from '@spartacus/cart/base/root';
 import { Quote, QuoteFacade } from '@spartacus/quote/root';
 import { ICON_TYPE } from '@spartacus/storefront';
-import { Observable, Subscription } from 'rxjs';
+import { NEVER, Observable, Subscription, of, zip } from 'rxjs';
 import { QuoteItemsComponentService } from './quote-items.component.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { UserIdService } from '@spartacus/core';
 
 @Component({
   selector: 'cx-quote-items',
@@ -18,9 +25,29 @@ import { QuoteItemsComponentService } from './quote-items.component.service';
 export class QuoteItemsComponent {
   protected quoteFacade = inject(QuoteFacade);
   protected activeCartFacade = inject(ActiveCartFacade);
+  protected multiCartFacade = inject(MultiCartFacade);
   protected quoteItemsService = inject(QuoteItemsComponentService);
+  protected userIdService = inject(UserIdService);
 
   quoteDetails$: Observable<Quote> = this.quoteFacade.getQuoteDetails();
+  cartReadOnlyDetails$: Observable<Cart> = this.userIdService.takeUserId().pipe(
+    switchMap((userId) => zip(this.quoteDetails$, of(userId))),
+    tap(([quote, userId]) => {
+      if (quote.cartId !== undefined) {
+        this.multiCartFacade.loadCart({
+          userId,
+          cartId: quote.cartId,
+          extraData: { active: false },
+        });
+      }
+    }),
+    switchMap(([quote, _userId]) => {
+      return quote.cartId !== undefined
+        ? this.multiCartFacade.getCart(quote.cartId)
+        : NEVER;
+    })
+  );
+
   cartDetails$: Observable<Cart> = this.activeCartFacade.getActive();
   showCart$ = this.quoteItemsService.getQuoteEntriesExpanded();
   iconTypes = ICON_TYPE;
@@ -31,3 +58,4 @@ export class QuoteItemsComponent {
     this.quoteItemsService.setQuoteEntriesExpanded(!showCart);
   }
 }
+
