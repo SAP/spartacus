bbf4d6fffd94588217fe0841356c2c2c25bfc615
Matejk00 <mateusz.kolasa@sap.com>
2023-10-12 11:35:25 +0200

    Update OPF schematics unit tests

--- integration-libs/opf/schematics/add-opf/index_spec.ts
+++ integration-libs/opf/schematics/add-opf/index_spec.ts
@@ -1,224 +1,212 @@
-// /// <reference types="jest" />
+/// <reference types="jest" />
 
-// import { RunSchematicTaskOptions } from '@angular-devkit/schematics/tasks/run-schematic/options';
-// import {
-//   SchematicTestRunner,
-//   UnitTestTree,
-// } from '@angular-devkit/schematics/testing';
-// import {
-//   Schema as ApplicationOptions,
-//   Style,
-// } from '@schematics/angular/application/schema';
-// import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
-// import {
-//   opfFeatureModulePath,
-//   OPF_FEATURE_NAME,
-//   LibraryOptions as SpartacusOpfOptions,
-//   SpartacusOptions,
-//   SPARTACUS_OPF,
-//   SPARTACUS_SCHEMATICS,
-//   CHECKOUT_BASE_FEATURE_NAME,
-//   SPARTACUS_CHECKOUT,
-//   checkoutWrapperModulePath,
-// } from '@spartacus/schematics';
-// import * as path from 'path';
-// import { peerDependencies } from '../../package.json';
+import { RunSchematicTaskOptions } from '@angular-devkit/schematics/tasks/run-schematic/options';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import {
+  Schema as ApplicationOptions,
+  Style,
+} from '@schematics/angular/application/schema';
+import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
+import {
+  OPF_BASE_FEATURE_NAME,
+  OPF_CHECKOUT_FEATURE_NAME,
+  SPARTACUS_CHECKOUT,
+  SPARTACUS_OPF,
+  SPARTACUS_SCHEMATICS,
+  LibraryOptions as SpartacusOpfOptions,
+  SpartacusOptions,
+  checkoutWrapperModulePath,
+  opfFeatureModulePath,
+} from '@spartacus/schematics';
+import * as path from 'path';
+import { peerDependencies } from '../../package.json';
+const collectionPath = path.join(__dirname, '../collection.json');
+const scssFilePath = 'src/styles/spartacus/opf.scss';
 
-// const collectionPath = path.join(__dirname, '../collection.json');
-// const scssFilePath = 'src/styles/spartacus/opf.scss';
+describe('Spartacus SAP OPF integration schematics: ng-add', () => {
+  const schematicRunner = new SchematicTestRunner(
+    SPARTACUS_OPF,
+    collectionPath
+  );
 
-// describe('Spartacus SAP OPF integration schematics: ng-add', () => {
-//   const schematicRunner = new SchematicTestRunner(
-//     SPARTACUS_OPF,
-//     collectionPath
-//   );
+  let appTree: UnitTestTree;
 
-//   let appTree: UnitTestTree;
+  const workspaceOptions: WorkspaceOptions = {
+    name: 'workspace',
+    version: '0.5.0',
+  };
 
-//   const workspaceOptions: WorkspaceOptions = {
-//     name: 'workspace',
-//     version: '0.5.0',
-//   };
+  const appOptions: ApplicationOptions = {
+    name: 'schematics-test',
+    inlineStyle: false,
+    inlineTemplate: false,
+    routing: false,
+    style: Style.Scss,
+    skipTests: false,
+    projectRoot: '',
+  };
 
-//   const appOptions: ApplicationOptions = {
-//     name: 'schematics-test',
-//     inlineStyle: false,
-//     inlineTemplate: false,
-//     routing: false,
-//     style: Style.Scss,
-//     skipTests: false,
-//     projectRoot: '',
-//   };
+  const spartacusDefaultOptions: SpartacusOptions = {
+    project: 'schematics-test',
+    lazy: true,
+    features: [],
+  };
 
-//   const spartacusDefaultOptions: SpartacusOptions = {
-//     project: 'schematics-test',
-//     lazy: true,
-//     features: [],
-//   };
+  const libraryNoFeaturesOptions: SpartacusOpfOptions = {
+    project: 'schematics-test',
+    lazy: true,
+    features: [],
+  };
 
-//   const libraryNoFeaturesOptions: SpartacusOpfOptions = {
-//     project: 'schematics-test',
-//     lazy: true,
-//     features: [],
-//   };
+  const checkoutFeatureOptions: SpartacusOpfOptions = {
+    ...libraryNoFeaturesOptions,
+    features: [OPF_CHECKOUT_FEATURE_NAME],
+  };
 
-//   const checkoutFeatureOptions: SpartacusOpfOptions = {
-//     ...libraryNoFeaturesOptions,
-//     features: [CHECKOUT_BASE_FEATURE_NAME],
-//   };
+  const opfFeatureOptions: SpartacusOpfOptions = {
+    ...libraryNoFeaturesOptions,
+    features: [OPF_BASE_FEATURE_NAME],
+  };
 
-//   const opfFeatureOptions: SpartacusOpfOptions = {
-//     ...libraryNoFeaturesOptions,
-//     features: [OPF_FEATURE_NAME],
-//   };
+  beforeEach(async () => {
+    schematicRunner.registerCollection(
+      SPARTACUS_SCHEMATICS,
+      path.join(
+        __dirname,
+        '../../../../projects/schematics/src/collection.json'
+      )
+    );
 
-//   beforeEach(async () => {
-//     schematicRunner.registerCollection(
-//       SPARTACUS_SCHEMATICS,
-//       path.join(
-//         __dirname,
-//         '../../../../projects/schematics/src/collection.json'
-//       )
-//     );
+    schematicRunner.registerCollection(
+      SPARTACUS_CHECKOUT,
+      path.join(
+        __dirname,
+        '../../../../feature-libs/checkout/schematics/collection.json'
+      )
+    );
 
-//     schematicRunner.registerCollection(
-//       SPARTACUS_CHECKOUT,
-//       path.join(
-//         __dirname,
-//         '../../../../feature-libs/checkout/schematics/collection.json'
-//       )
-//     );
+    appTree = await schematicRunner.runExternalSchematic(
+      '@schematics/angular',
+      'workspace',
+      workspaceOptions
+    );
 
-//     appTree = await schematicRunner
-//       .runExternalSchematicAsync(
-//         '@schematics/angular',
-//         'workspace',
-//         workspaceOptions
-//       )
-//       .toPromise();
-//     appTree = await schematicRunner
-//       .runExternalSchematicAsync(
-//         '@schematics/angular',
-//         'application',
-//         appOptions,
-//         appTree
-//       )
-//       .toPromise();
-//     appTree = await schematicRunner
-//       .runExternalSchematicAsync(
-//         SPARTACUS_SCHEMATICS,
-//         'ng-add',
-//         { ...spartacusDefaultOptions, name: 'schematics-test' },
-//         appTree
-//       )
-//       .toPromise();
-//   });
+    appTree = await schematicRunner.runExternalSchematic(
+      '@schematics/angular',
+      'application',
+      appOptions,
+      appTree
+    );
 
-//   describe('Without features', () => {
-//     beforeEach(async () => {
-//       appTree = await schematicRunner
-//         .runSchematicAsync('ng-add', libraryNoFeaturesOptions, appTree)
-//         .toPromise();
-//     });
+    appTree = await schematicRunner.runExternalSchematic(
+      SPARTACUS_SCHEMATICS,
+      'ng-add',
+      { ...spartacusDefaultOptions, name: 'schematics-test' },
+      appTree
+    );
+  });
 
-//     it('should not create any of the feature modules', () => {
-//       expect(appTree.exists(opfFeatureModulePath)).toBeFalsy();
-//     });
-//   });
+  describe('Without features', () => {
+    beforeEach(async () => {
+      appTree = await schematicRunner.runSchematic(
+        'ng-add',
+        { ...libraryNoFeaturesOptions, features: [] },
+        appTree
+      );
+    });
 
-//   describe('SAP OPF feature', () => {
-//     describe('general setup', () => {
-//       beforeEach(async () => {
-//         appTree = await schematicRunner
-//           .runSchematicAsync('ng-add', checkoutFeatureOptions, appTree)
-//           .toPromise();
-//         appTree = await schematicRunner
-//           .runSchematicAsync('ng-add', opfFeatureOptions, appTree)
-//           .toPromise();
-//       });
+    it('should not create any of the feature modules', () => {
+      expect(appTree.exists(opfFeatureModulePath)).toBeFalsy();
+    });
+  });
 
-//       it('should add the feature using the lazy loading syntax', async () => {
-//         const module = appTree.readContent(opfFeatureModulePath);
-//         expect(module).toMatchSnapshot();
-//       });
+  describe('SAP OPF feature', () => {
+    describe('general setup', () => {
+      beforeEach(async () => {
+        appTree = await schematicRunner.runSchematic(
+          'ng-add',
+          { ...checkoutFeatureOptions, ...opfFeatureOptions },
+          appTree
+        );
+      });
 
-//       describe('styling', () => {
-//         it('should create a proper scss file', () => {
-//           const scssContent = appTree.readContent(scssFilePath);
-//           expect(scssContent).toMatchSnapshot();
-//         });
+      it('should install necessary Spartacus libraries', () => {
+        const packageJson = JSON.parse(appTree.readContent('package.json'));
+        let dependencies: Record<string, string> = {};
+        dependencies = { ...packageJson.dependencies };
+        dependencies = { ...dependencies, ...packageJson.devDependencies };
 
-//         it('should update angular.json', async () => {
-//           const content = appTree.readContent('/angular.json');
-//           expect(content).toMatchSnapshot();
-//         });
-//       });
+        for (const toAdd in peerDependencies) {
+          if (
+            !peerDependencies.hasOwnProperty(toAdd) ||
+            toAdd === SPARTACUS_SCHEMATICS
+          ) {
+            continue;
+          }
+          const expectedDependency = dependencies[toAdd];
+          expect(expectedDependency).toBeTruthy();
+        }
+      });
 
-//       it('should install necessary Spartacus libraries', () => {
-//         const packageJson = JSON.parse(appTree.readContent('package.json'));
-//         let dependencies: Record<string, string> = {};
-//         dependencies = { ...packageJson.dependencies };
-//         dependencies = { ...dependencies, ...packageJson.devDependencies };
+      it('should add the feature using the lazy loading syntax', async () => {
+        const module = appTree.readContent(opfFeatureModulePath);
+        expect(module).toMatchSnapshot();
+      });
 
-//         for (const toAdd in peerDependencies) {
-//           if (
-//             !peerDependencies.hasOwnProperty(toAdd) ||
-//             toAdd === SPARTACUS_SCHEMATICS
-//           ) {
-//             continue;
-//           }
-//           const expectedDependency = dependencies[toAdd];
-//           expect(expectedDependency).toBeTruthy();
-//         }
-//       });
+      describe('styling', () => {
+        it('should create a proper scss file', () => {
+          const scssContent = appTree.readContent(scssFilePath);
+          expect(scssContent).toMatchSnapshot();
+        });
 
-//       it('should run the proper installation tasks', async () => {
-//         const tasks = schematicRunner.tasks.filter(
-//           (task) =>
-//             task.name === 'run-schematic' &&
-//             (task.options as RunSchematicTaskOptions<{}>).collection ===
-//               '@sap/opf'
-//         );
+        it('should update angular.json', async () => {
+          const content = appTree.readContent('/angular.json');
+          expect(content).toMatchSnapshot();
+        });
+      });
 
-//         expect(tasks.length).toEqual(0);
-//       });
+      it('should install necessary Spartacus libraries', () => {
+        const packageJson = JSON.parse(appTree.readContent('package.json'));
+        let dependencies: Record<string, string> = {};
+        dependencies = { ...packageJson.dependencies };
+        dependencies = { ...dependencies, ...packageJson.devDependencies };
 
-//       it('should add the feature using the lazy loading syntax', async () => {
-//         const module = appTree.readContent(opfFeatureModulePath);
-//         expect(module).toMatchSnapshot();
+        for (const toAdd in peerDependencies) {
+          if (
+            !peerDependencies.hasOwnProperty(toAdd) ||
+            toAdd === SPARTACUS_SCHEMATICS
+          ) {
+            continue;
+          }
+          const expectedDependency = dependencies[toAdd];
+          expect(expectedDependency).toBeTruthy();
+        }
+      });
 
-//         const wrapperModule = appTree.readContent(checkoutWrapperModulePath);
-//         expect(wrapperModule).toMatchSnapshot();
-//       });
-//     });
+      it('should run the proper installation tasks', async () => {
+        const tasks = schematicRunner.tasks.filter(
+          (task) =>
+            task.name === 'run-schematic' &&
+            (task.options as RunSchematicTaskOptions<{}>).collection ===
+              '@sap/opf'
+        );
 
-//     describe('eager loading', () => {
-//       beforeEach(async () => {
-//         appTree = await schematicRunner
-//           .runSchematicAsync(
-//             'ng-add',
-//             { ...checkoutFeatureOptions, lazy: false },
-//             appTree
-//           )
-//           .toPromise();
-//         appTree = await schematicRunner
-//           .runSchematicAsync(
-//             'ng-add',
-//             { ...opfFeatureOptions, lazy: false },
-//             appTree
-//           )
-//           .toPromise();
-//       });
+        expect(tasks.length).toEqual(0);
+      });
 
-//       it('should import appropriate modules', async () => {
-//         const module = appTree.readContent(opfFeatureModulePath);
-//         expect(module).toMatchSnapshot();
+      it('should add the feature using the lazy loading syntax', async () => {
+        const module = appTree.readContent(opfFeatureModulePath);
+        expect(module).toMatchSnapshot();
 
-//         expect(appTree.readContent(checkoutWrapperModulePath)).toBeFalsy();
-//       });
-//     });
-//   });
-// });
+        const wrapperModule = appTree.readContent(checkoutWrapperModulePath);
+        expect(wrapperModule).toMatchSnapshot();
+      });
+    });
+  });
+});
 
 it('should pass schematics tests', async () => {
   expect(true).toEqual(true);
