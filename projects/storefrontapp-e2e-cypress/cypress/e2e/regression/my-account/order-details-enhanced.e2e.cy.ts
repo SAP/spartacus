57d0b304ab73e0195672ee3feb8b097c2a50738e
sisodiyam <mohit.sisodiya@sap.com>
2023-10-16 21:01:59 +0530

    CXINT-2317: refactored the code

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/order-details-enhanced.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/order-details-enhanced.e2e.cy.ts
@@ -16,35 +16,38 @@ describe('Order Details', { testIsolation: false }, () => {
       cy.visit('/');
     });
 
-      beforeEach(() => {
-        cy.restoreLocalStorage();
-      });
+    beforeEach(() => {
+      cy.restoreLocalStorage();
+    });
 
     it('should navigate to login page and SignIn with user details', () => {
-        cy.findByText(/Sign in \/ Register/i).click();
-        fillLoginForm({ username: 'cdp.user@sap.com', password: 'Test@1' });
+      cy.findByText(/Sign in \/ Register/i).click();
+      fillLoginForm({ username: 'cdp.user@sap.com', password: 'Test@1' });
     });
-    
+
     it('should navigate to Order History page', () => {
-        let totalCount;
-        cy.findByText(/My Account/i).click();
-        cy.findByText(/Order History/i).click();
-        cy.get('h2').contains('All Orders (2)');
-        cy.get('.cx-order-history-extended-body > .cx-each-order').then((value) => {
-            totalCount = 2;
-            expect(value).to.have.length(totalCount);
-        });
+      let totalCount;
+      cy.findByText(/My Account/i).click();
+      cy.findByText(/Order History/i).click();
+      cy.get('h2').contains('All Orders');
+      cy.get('.cx-myaccount-v2-order-history-body > .cx-each-order').then(
+        (value) => {
+          totalCount = Cypress.$(value).length;
+          expect(value).to.have.length(totalCount);
+        }
+      );
     });
 
     it('should navigate to view details of first order', () => {
-        cy.get('.cx-enhanced-ui-order-history-code > a').first().click();
-        cy.findByText(/Download Invoices/i).click();
-        cy.get('.cx-modal-content').contains('Download Invoices');
-        cy.get('.close').click();
+      cy.get('.cx-myaccount-v2-order-history-code > a').first().click();
+      cy.get('cx-media > img').should('exist');
+      cy.findByText(/Download Invoices/i).click();
+      cy.get('.cx-modal-content').contains('Download Invoices');
+      cy.get('.close').click();
     });
 
     afterEach(() => {
-        cy.saveLocalStorage();
+      cy.saveLocalStorage();
     });
   });
 });
