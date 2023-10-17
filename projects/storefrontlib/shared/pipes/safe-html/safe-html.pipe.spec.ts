3fdbcb4c88f2a261de1e54741bec7399757b571b
rmch91 <r.chygryn@sap.com>
2023-10-17 15:52:13 +0200

    changes

--- projects/storefrontlib/shared/pipes/safe-html/safe-html.pipe.spec.ts
+++ projects/storefrontlib/shared/pipes/safe-html/safe-html.pipe.spec.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { TestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SafeHtmlPipe } from './safe-html.pipe';
