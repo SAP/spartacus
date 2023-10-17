86cc8897d8776ee37417f102bbac2c0737810eca
rmch91 <r.chygryn@sap.com>
2023-10-17 14:48:07 +0200

    fix: Allow bypassing stylings for paragraph component

--- feature-libs/smartedit/root/config/default-smart-edit-config.ts
+++ feature-libs/smartedit/root/config/default-smart-edit-config.ts
@@ -9,6 +9,6 @@ import { SmartEditConfig } from './smart-edit-config';
 export const defaultSmartEditConfig: SmartEditConfig = {
   smartEdit: {
     storefrontPreviewRoute: 'cx-preview',
-    allowOrigin: 'localhost:9002',
+    allowOrigin: '40.76.109.9:9002',
   },
 };
