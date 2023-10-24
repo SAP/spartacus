9a66e6d0e0734e6dc24227b2698d18fafa8edb1c
Caine Rotherham <rotherham.c@gmail.com>
2023-10-24 11:20:09 +0200

    fix: Load product list from transfer state in SSR

--- projects/core/src/product/facade/product-search.service.ts
+++ projects/core/src/product/facade/product-search.service.ts
@@ -4,19 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, merge, Subject } from 'rxjs';
 import { ProductSearchPage } from '../../model/product-search.model';
 import { SearchConfig } from '../model/search-config';
 import { ProductActions } from '../store/actions/index';
-import { StateWithProduct } from '../store/product-state';
+import { PRODUCT_FEATURE, StateWithProduct } from '../store/product-state';
 import { ProductSelectors } from '../store/selectors/index';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductSearchService {
+  transferState = inject(TransferState, { optional: true });
+
   constructor(protected store: Store<StateWithProduct>) {}
 
   search(query: string | undefined, searchConfig?: SearchConfig): void {
@@ -31,6 +34,15 @@ export class ProductSearchService {
   }
 
   getResults(): Observable<ProductSearchPage> {
+    const results = this.getResultsFromTransferState();
+
+    if (results) {
+      return merge(
+        this.store.pipe(select(ProductSelectors.getSearchResults)),
+        results
+      );
+    }
+
     return this.store.pipe(select(ProductSelectors.getSearchResults));
   }
 
@@ -40,5 +52,20 @@ export class ProductSearchService {
         clearPageResults: true,
       })
     );
+  }
+
+  protected getResultsFromTransferState():
+    | Observable<ProductSearchPage>
+    | undefined {
+    if (this.transferState) {
+      const subject = new Subject<ProductSearchPage>();
+      const CX_KEY = makeStateKey<string>('cx-state');
+      const state = this.transferState.get(CX_KEY, <any>{});
+      const results = state[PRODUCT_FEATURE]?.search?.results;
+      subject.next(results);
+
+      return results ? subject.asObservable() : undefined;
+    }
+    return undefined;
   }
 }
