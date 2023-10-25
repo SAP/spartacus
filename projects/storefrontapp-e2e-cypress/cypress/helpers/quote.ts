09ce46b99364d7bb6eeb96a4eab5ee6d66e1308c
Moritz Schaefer <Moritz.Schaefer@sap.com>
2023-10-25 17:14:35 +0200

    add import

--- projects/storefrontapp-e2e-cypress/cypress/helpers/quote.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/quote.ts
@@ -7,6 +7,7 @@
 import * as authentication from './auth-forms';
 import * as common from './common';
 import * as cart from './cart';
+import { scrollToTopOfPage } from './infinite-scroll';
 
 /** Aliases for Quote Routes */
 export const GET_QUOTE_ALIAS = '@GET_QUOTE';
