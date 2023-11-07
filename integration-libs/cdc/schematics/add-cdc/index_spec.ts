61fec6415a687c21724b52891cb93f0312132077
anjana-bl <84384970+anjana-bl@users.noreply.github.com>
2023-11-07 14:58:35 +0530

    Revert "cdc fix - upport to 6.6  (#17918)"
    This reverts commit b57713ea104575bf402b1ea01e02360632571cd7.

--- integration-libs/cdc/schematics/add-cdc/index_spec.ts
+++ integration-libs/cdc/schematics/add-cdc/index_spec.ts
@@ -1,4 +1,5 @@
 /// <reference types="jest" />
+
 import {
   SchematicTestRunner,
   UnitTestTree,
@@ -10,7 +11,6 @@ import {
 import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
 import {
   cdcFeatureModulePath,
-  CDC_B2B_FEATURE_NAME,
   CDC_FEATURE_NAME,
   LibraryOptions as SpartacusCdcOptions,
   organizationAdministrationWrapperModulePath,
@@ -29,17 +29,22 @@ import {
 } from '@spartacus/schematics';
 import * as path from 'path';
 import { peerDependencies } from '../../package.json';
+
 const collectionPath = path.join(__dirname, '../collection.json');
+
 describe('Spartacus CDC schematics: ng-add', () => {
   const schematicRunner = new SchematicTestRunner(
     SPARTACUS_CDC,
     collectionPath
   );
+
   let appTree: UnitTestTree;
+
   const workspaceOptions: WorkspaceOptions = {
     name: 'workspace',
     version: '0.5.0',
   };
+
   const appOptions: ApplicationOptions = {
     name: 'schematics-test',
     inlineStyle: false,
@@ -49,24 +54,24 @@ describe('Spartacus CDC schematics: ng-add', () => {
     skipTests: false,
     projectRoot: '',
   };
+
   const spartacusDefaultOptions: SpartacusOptions = {
     project: 'schematics-test',
     lazy: true,
     features: [],
   };
+
   const libraryNoFeaturesOptions: SpartacusCdcOptions = {
     project: 'schematics-test',
     lazy: true,
     features: [],
   };
+
   const cdcFeatureOptions: SpartacusCdcOptions = {
     ...libraryNoFeaturesOptions,
     features: [CDC_FEATURE_NAME],
   };
-  const cdcB2bFeatureOptions: SpartacusCdcOptions = {
-    ...libraryNoFeaturesOptions,
-    features: [CDC_B2B_FEATURE_NAME],
-  };
+
   beforeEach(async () => {
     schematicRunner.registerCollection(
       SPARTACUS_SCHEMATICS,
@@ -86,17 +91,20 @@ describe('Spartacus CDC schematics: ng-add', () => {
         '../../../../feature-libs/user/schematics/collection.json'
       )
     );
+
     appTree = await schematicRunner.runExternalSchematic(
       '@schematics/angular',
       'workspace',
       workspaceOptions
     );
+
     appTree = await schematicRunner.runExternalSchematic(
       '@schematics/angular',
       'application',
       appOptions,
       appTree
     );
+
     appTree = await schematicRunner.runExternalSchematic(
       SPARTACUS_SCHEMATICS,
       'ng-add',
@@ -104,6 +112,7 @@ describe('Spartacus CDC schematics: ng-add', () => {
       appTree
     );
   });
+
   describe('Without features', () => {
     beforeEach(async () => {
       appTree = await schematicRunner.runSchematic(
@@ -112,164 +121,71 @@ describe('Spartacus CDC schematics: ng-add', () => {
         appTree
       );
     });
+
     it('should not create any of the feature modules', () => {
       expect(appTree.exists(cdcFeatureModulePath)).toBeFalsy();
     });
   });
-  describe('CDC-B2C feature', () => {
+
+  describe('CDC feature', () => {
     describe('validation of jsSDKUrl', () => {
       beforeEach(async () => {
         appTree = await schematicRunner.runSchematic(
           'ng-add',
           {
             ...cdcFeatureOptions,
-            features: [USER_ACCOUNT_FEATURE_NAME, USER_PROFILE_FEATURE_NAME],
+            features: [
+              USER_ACCOUNT_FEATURE_NAME,
+              USER_PROFILE_FEATURE_NAME,
+              ORGANIZATION_ADMINISTRATION_FEATURE_NAME,
+              ORGANIZATION_USER_REGISTRATION_FEATURE_NAME,
+            ],
           },
           appTree
         );
+
         appTree = await schematicRunner.runSchematic(
           'ng-add',
           { ...cdcFeatureOptions, javascriptUrl: '<dc>.gigya.com/<api-key>' },
           appTree
         );
       });
+
       it('should set the given javascriptUrl', async () => {
         const featureModule = appTree.readContent(cdcFeatureModulePath);
         expect(featureModule).toMatchSnapshot();
       });
     });
+
     describe('general setup', () => {
       beforeEach(async () => {
         appTree = await schematicRunner.runSchematic(
           'ng-add',
           {
             ...cdcFeatureOptions,
-            features: [USER_ACCOUNT_FEATURE_NAME, USER_PROFILE_FEATURE_NAME],
+            features: [
+              USER_ACCOUNT_FEATURE_NAME,
+              USER_PROFILE_FEATURE_NAME,
+              ORGANIZATION_ADMINISTRATION_FEATURE_NAME,
+              ORGANIZATION_USER_REGISTRATION_FEATURE_NAME,
+            ],
           },
           appTree
         );
+
         appTree = await schematicRunner.runSchematic(
           'ng-add',
           cdcFeatureOptions,
           appTree
         );
       });
+
       it('should install necessary Spartacus libraries', () => {
         const packageJson = JSON.parse(appTree.readContent('package.json'));
         let dependencies: Record<string, string> = {};
         dependencies = { ...packageJson.dependencies };
         dependencies = { ...dependencies, ...packageJson.devDependencies };
-        for (const toAdd in peerDependencies) {
-          // skip the SPARTACUS_SCHEMATICS, as those are added only when running by the Angular CLI, and not in the testing environment
-          if (
-            !peerDependencies.hasOwnProperty(toAdd) ||
-            toAdd === SPARTACUS_SCHEMATICS
-          ) {
-            continue;
-          }
-          // CXSPA-4872: after 4.0: use this test, as we'll have synced versions between lib's and root package.json
-          // const expectedVersion = (peerDependencies as Record<
-          //   string,
-          //   string
-          // >)[toAdd];
-          const expectedDependency = dependencies[toAdd];
-          expect(expectedDependency).toBeTruthy();
-          // expect(expectedDependency).toEqual(expectedVersion);
-        }
-      });
-      it('should add the feature using the lazy loading syntax', async () => {
-        const featureModule = appTree.readContent(cdcFeatureModulePath);
-        expect(featureModule).toMatchSnapshot();
-      });
-      it('should install the appropriate dependencies', async () => {
-        const userAccountWrapperModule = appTree.readContent(
-          userAccountWrapperModulePath
-        );
-        expect(userAccountWrapperModule).toMatchSnapshot();
-        const userProfileWrapperModule = appTree.readContent(
-          userProfileWrapperModulePath
-        );
-        expect(userProfileWrapperModule).toMatchSnapshot();
-      });
-    });
-    describe('eager loading', () => {
-      beforeEach(async () => {
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          {
-            ...cdcFeatureOptions,
-            features: [USER_ACCOUNT_FEATURE_NAME, USER_PROFILE_FEATURE_NAME],
-          },
-          appTree
-        );
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          { ...cdcFeatureOptions, lazy: false },
-          appTree
-        );
-      });
-      it('should import appropriate modules', async () => {
-        const featureModule = appTree.readContent(cdcFeatureModulePath);
-        expect(featureModule).toMatchSnapshot();
-      });
-    });
-  });
-  describe('CDC-B2B feature', () => {
-    describe('validation of jsSDKUrl', () => {
-      beforeEach(async () => {
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          {
-            ...cdcB2bFeatureOptions,
-            features: [
-              USER_ACCOUNT_FEATURE_NAME,
-              USER_PROFILE_FEATURE_NAME,
-              ORGANIZATION_ADMINISTRATION_FEATURE_NAME,
-              ORGANIZATION_USER_REGISTRATION_FEATURE_NAME,
-            ],
-          },
-          appTree
-        );
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          {
-            ...cdcB2bFeatureOptions,
-            javascriptUrl: '<dc>.gigya.com/<api-key>',
-          },
-          appTree
-        );
-      });
-      it('should set the given javascriptUrl', async () => {
-        const featureModule = appTree.readContent(cdcFeatureModulePath);
-        expect(featureModule).toMatchSnapshot();
-      });
-    });
-    describe('general setup', () => {
-      beforeEach(async () => {
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          {
-            ...cdcB2bFeatureOptions,
-            features: [
-              USER_ACCOUNT_FEATURE_NAME,
-              USER_PROFILE_FEATURE_NAME,
-              ORGANIZATION_ADMINISTRATION_FEATURE_NAME,
-              ORGANIZATION_USER_REGISTRATION_FEATURE_NAME,
-            ],
-          },
-          appTree
-        );
-        appTree = await schematicRunner.runSchematic(
-          'ng-add',
-          cdcB2bFeatureOptions,
-          appTree
-        );
-      });
-      it('should install necessary Spartacus libraries', () => {
-        const packageJson = JSON.parse(appTree.readContent('package.json'));
-        let dependencies: Record<string, string> = {};
-        dependencies = { ...packageJson.dependencies };
-        dependencies = { ...dependencies, ...packageJson.devDependencies };
+
         for (const toAdd in peerDependencies) {
           // skip the SPARTACUS_SCHEMATICS, as those are added only when running by the Angular CLI, and not in the testing environment
           if (
@@ -288,35 +204,41 @@ describe('Spartacus CDC schematics: ng-add', () => {
           // expect(expectedDependency).toEqual(expectedVersion);
         }
       });
+
       it('should add the feature using the lazy loading syntax', async () => {
         const featureModule = appTree.readContent(cdcFeatureModulePath);
         expect(featureModule).toMatchSnapshot();
       });
+
       it('should install the appropriate dependencies', async () => {
         const userAccountWrapperModule = appTree.readContent(
           userAccountWrapperModulePath
         );
         expect(userAccountWrapperModule).toMatchSnapshot();
+
         const userProfileWrapperModule = appTree.readContent(
           userProfileWrapperModulePath
         );
         expect(userProfileWrapperModule).toMatchSnapshot();
+
         const administrationWrapperModule = appTree.readContent(
           organizationAdministrationWrapperModulePath
         );
         expect(administrationWrapperModule).toMatchSnapshot();
+
         const organizationUserRegistrationWrapperModule = appTree.readContent(
           organizationUserRegistrationWrapperModulePath
         );
         expect(organizationUserRegistrationWrapperModule).toMatchSnapshot();
       });
     });
+
     describe('eager loading', () => {
       beforeEach(async () => {
         appTree = await schematicRunner.runSchematic(
           'ng-add',
           {
-            ...cdcB2bFeatureOptions,
+            ...cdcFeatureOptions,
             features: [
               USER_ACCOUNT_FEATURE_NAME,
               USER_PROFILE_FEATURE_NAME,
@@ -326,12 +248,14 @@ describe('Spartacus CDC schematics: ng-add', () => {
           },
           appTree
         );
+
         appTree = await schematicRunner.runSchematic(
           'ng-add',
-          { ...cdcB2bFeatureOptions, lazy: false },
+          { ...cdcFeatureOptions, lazy: false },
           appTree
         );
       });
+
       it('should import appropriate modules', async () => {
         const featureModule = appTree.readContent(cdcFeatureModulePath);
         expect(featureModule).toMatchSnapshot();
