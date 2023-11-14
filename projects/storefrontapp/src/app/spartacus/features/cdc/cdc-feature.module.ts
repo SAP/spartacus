7613a815c573df7501642ee2cc38a801f75204e9
anjana-bl <anjana.b.l@sap.com>
2023-11-14 16:22:33 +0530

    test case change

--- projects/storefrontapp/src/app/spartacus/features/cdc/cdc-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cdc/cdc-feature.module.ts
@@ -18,7 +18,8 @@ import {
       cdc: [
         {
           baseSite: 'electronics-spa',
-          javascriptUrl: 'JS_SDK_URL_PLACEHOLDER',
+          javascriptUrl:
+            'https://cdns.eu1.gigya.com/JS/gigya.js?apikey=3_k_wG-sllOhu2rjDEWHjG9-ncnnGAMHfkIcUKzl94weJU1Y18hITRgnTDp1LP8QdC',
           sessionExpiration: 3600,
         },
         {
