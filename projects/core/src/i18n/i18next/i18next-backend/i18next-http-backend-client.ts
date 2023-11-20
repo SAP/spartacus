0a93289de31034324ad3a1ebd6b8e394d3b0c52b
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-11-20 13:13:46 +0100

    Update i18next-http-backend-client.ts

--- projects/core/src/i18n/i18next/i18next-backend/i18next-http-backend-client.ts
+++ projects/core/src/i18n/i18next/i18next-backend/i18next-http-backend-client.ts
@@ -6,7 +6,10 @@
 
 import { HttpClient } from '@angular/common/http';
 import { inject, InjectionToken } from '@angular/core';
-import type { HttpBackendOptions, RequestCallback } from 'i18next-http-backend/cjs';
+import type {
+  HttpBackendOptions,
+  RequestCallback,
+} from 'i18next-http-backend/cjs';
 
 export type I18nextHttpBackendClient = HttpBackendOptions['request'];
 
@@ -24,7 +27,12 @@ export const I18NEXT_HTTP_BACKEND_CLIENT = new InjectionToken<
     // See docs for `i18next-http-backend`: https://github.com/i18next/i18next-http-backend#backend-options
     //
     // It uses Angular HttpClient under the hood, so it works in SSR.
-    return (_options: HttpBackendOptions, url: string, _payload: string | object, callback: RequestCallback) => {
+    return (
+      _options: HttpBackendOptions,
+      url: string,
+      _payload: string | object,
+      callback: RequestCallback
+    ) => {
       httpClient.get(url, { responseType: 'text' }).subscribe({
         next: (data) => callback(null, { status: 200, data }),
         error: (error) =>
