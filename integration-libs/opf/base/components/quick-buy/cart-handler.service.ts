e14e6ba796fcb68feeefaa13d8eb14f126b174c2
Matejk00 <mateusz.kolasa@sap.com>
2023-11-24 16:54:27 +0100

    Add shipping address and delivery mode handling

---
+++ integration-libs/opf/base/components/quick-buy/cart-handler.service.ts
@@ -0,0 +1,164 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Injectable } from '@angular/core';
+import {
+  ActiveCartFacade,
+  DeleteCartFailEvent,
+  DeleteCartSuccessEvent,
+  MultiCartFacade,
+} from '@spartacus/cart/base/root';
+import {
+  CheckoutDeliveryAddressFacade,
+  CheckoutDeliveryModesFacade,
+} from '@spartacus/checkout/base/root';
+import {
+  Address,
+  EventService,
+  UserAddressService,
+  UserIdService,
+} from '@spartacus/core';
+import { merge, throwError } from 'rxjs';
+import {
+  filter,
+  map,
+  switchMap,
+  take,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartHandlerService {
+  constructor(
+    protected activeCartFacade: ActiveCartFacade,
+    protected checkoutDeliveryModesFacade: CheckoutDeliveryModesFacade,
+    protected checkoutDeliveryAddressFacade: CheckoutDeliveryAddressFacade,
+    protected userAddressService: UserAddressService,
+    protected multiCartFacade: MultiCartFacade,
+    protected userIdService: UserIdService,
+    protected eventService: EventService
+  ) {}
+
+  addProductTocart(
+    productCode: string,
+    quantity: number,
+    pickupStore?: string | undefined
+  ) {
+    this.activeCartFacade.addEntry(productCode, quantity, pickupStore);
+
+    return this.checkStableCart();
+  }
+
+  removeProductFromCart(productId: string) {
+    if (!productId) {
+      return throwError('');
+    }
+    if (productId) {
+      return (
+        this.activeCartFacade.getLastEntry(productId).pipe(take(1)),
+        tap((entry) => {
+          if (entry) {
+            this.activeCartFacade.removeEntry(entry);
+          }
+        }),
+        switchMap(() => {
+          return this.checkStableCart();
+        })
+      );
+    }
+  }
+
+  checkStableCart() {
+    return this.activeCartFacade.isStable().pipe(
+      filter((isStable) => !!isStable),
+      take(1)
+    );
+  }
+
+  getDeliveryModes() {
+    return this.checkoutDeliveryModesFacade.getSupportedDeliveryModes();
+  }
+
+  setDeliveryAddress(address: Address) {
+    return this.checkoutDeliveryAddressFacade
+      .createAndSetAddress(address)
+      .pipe(switchMap(() => this.checkStableCart()));
+  }
+
+  getDeliveryAddress() {
+    return this.checkoutDeliveryAddressFacade.getDeliveryAddressState().pipe(
+      filter((state) => !state.loading),
+      take(1),
+      map((state) => {
+        return state.data;
+      })
+    );
+  }
+
+  updateCurrentCartDeliveryAddress(newAddress: Address) {
+    return this.getDeliveryAddress().pipe(
+      switchMap((address) => {
+        if (!address?.id) {
+          return this.setDeliveryAddress(newAddress);
+        }
+        return this.updateDeliveryAddress(address?.id as string, newAddress);
+      })
+    );
+  }
+
+  getCurrentCart() {
+    return this.activeCartFacade.getActive();
+  }
+
+  setDeliveryMode(mode: string) {
+    return this.checkoutDeliveryModesFacade.setDeliveryMode(mode).pipe(
+      switchMap(() =>
+        this.checkoutDeliveryModesFacade.getSelectedDeliveryModeState()
+      ),
+      filter((state) => !state.error && !state.loading),
+      take(1),
+      map((state) => state.data)
+    );
+  }
+
+  getSelectedDeliveryMode() {
+    this.checkoutDeliveryModesFacade.getSelectedDeliveryModeState().pipe(
+      filter((state) => !state.error && !state.loading),
+      take(1),
+      map((state) => state.data)
+    );
+  }
+
+  updateDeliveryAddress(targetId: string, address: Address) {
+    this.userAddressService.updateUserAddress(targetId, address);
+    return this.checkoutDeliveryAddressFacade.getDeliveryAddressState().pipe(
+      filter((state) => !state.error && !state.loading),
+      take(1),
+      map((state) => {
+        !!state.data;
+      })
+    );
+  }
+
+  deleteCurrentCart() {
+    return this.activeCartFacade.getActiveCartId().pipe(
+      withLatestFrom(this.userIdService.getUserId()),
+      take(1),
+      tap(([cartId, userId]) => {
+        this.multiCartFacade.deleteCart(cartId, userId);
+      }),
+      switchMap(() =>
+        merge(
+          this.eventService.get(DeleteCartSuccessEvent).pipe(map(() => true)),
+          this.eventService.get(DeleteCartFailEvent).pipe(map(() => false))
+        ).pipe(take(1))
+      )
+    );
+  }
+}
--- integration-libs/opf/base/components/quick-buy/google-pay/google-pay.service.ts
+++ integration-libs/opf/base/components/quick-buy/google-pay/google-pay.service.ts
@@ -5,12 +5,16 @@
  */
 
 import { ElementRef, Injectable, inject } from '@angular/core';
+import { Product } from '@spartacus/core';
 import { OpfResourceLoaderService } from '@spartacus/opf/base/root';
 import {
   CurrentProductService,
   ItemCounterService,
 } from '@spartacus/storefront';
-import { take } from 'rxjs/operators';
+
+import { Observable } from 'rxjs';
+import { map, switchMap, take, tap } from 'rxjs/operators';
+import { CartHandlerService } from '../cart-handler.service';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +23,7 @@ export class OpfGooglePayService {
   protected opfResourceLoaderService = inject(OpfResourceLoaderService);
   protected itemCounterService = inject(ItemCounterService);
   protected currentProductService = inject(CurrentProductService);
+  protected cartHandlerService = inject(CartHandlerService);
 
   protected readonly GOOGLE_PAY_JS_URL =
     'https://pay.google.com/gp/p/js/pay.js';
@@ -28,6 +33,50 @@ export class OpfGooglePayService {
   protected googlePaymentClientOptions: google.payments.api.PaymentOptions = {
     environment: 'TEST',
     paymentDataCallbacks: this.handlePaymentCallbacks(),
+  };
+
+  protected googlePaymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    callbackIntents: [
+      'SHIPPING_ADDRESS',
+      'SHIPPING_OPTION',
+      'PAYMENT_AUTHORIZATION',
+    ],
+    allowedPaymentMethods: [
+      {
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: [
+            'AMEX',
+            'DISCOVER',
+            'INTERAC',
+            'JCB',
+            'MASTERCARD',
+            'VISA',
+          ],
+        },
+        tokenizationSpecification: {
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId',
+          },
+          type: 'PAYMENT_GATEWAY',
+        },
+        type: 'CARD',
+      },
+    ],
+    // @ts-ignore
+    merchantInfo: {
+      // merchantId: 'spartacusStorefront',
+      merchantName: 'Spartacus Storefront',
+    },
+    shippingOptionRequired: true,
+    shippingAddressRequired: true,
+    // @ts-ignore
+    shippingAddressParameters: {
+      phoneNumberRequired: true,
+    },
   };
 
   loadProviderResources(): Promise<void> {
@@ -46,51 +95,73 @@ export class OpfGooglePayService {
     return this.googlePaymentClient;
   }
 
+  isReadyToPay(
+    request: google.payments.api.IsReadyToPayRequest
+  ): Promise<google.payments.api.IsReadyToPayResponse> {
+    return this.googlePaymentClient.isReadyToPay(request);
+  }
+
+  updateTransactionInfo(transactionInfo: google.payments.api.TransactionInfo) {
+    this.googlePaymentRequest.transactionInfo = transactionInfo;
+  }
+
+  updateShippingOptionParameters() {
+    return this.cartHandlerService.getDeliveryModes().pipe(
+      take(1),
+      map((modes) => {
+        this.googlePaymentRequest.shippingOptionParameters = {
+          defaultSelectedOptionId: modes[0].code,
+          shippingOptions: modes.map((mode) => ({
+            id: mode.code,
+            label: mode.name,
+            description: mode.description,
+          })),
+        };
+      })
+    );
+  }
+
+  setDeliveryAddress(
+    address: google.payments.api.IntermediateAddress | undefined
+  ): Observable<boolean> {
+    return this.cartHandlerService.setDeliveryAddress({
+      firstName: 'Test',
+      lastName: 'Test',
+      country: {
+        isocode: address?.countryCode,
+      },
+      town: address?.locality,
+      district: address?.administrativeArea,
+      postalCode: address?.postalCode,
+      line1: 'Test',
+    });
+  }
+
   initTransaction(): void {
     this.currentProductService
-      ?.getProduct()
-      .pipe(take(1))
-      .subscribe((product) => {
-        const paymentDataRequest: google.payments.api.PaymentDataRequest = {
-          allowedPaymentMethods: [
-            {
-              parameters: {
-                allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
-                allowedCardNetworks: [
-                  'AMEX',
-                  'DISCOVER',
-                  'INTERAC',
-                  'JCB',
-                  'MASTERCARD',
-                  'VISA',
-                ],
-              },
-              tokenizationSpecification: {
-                parameters: {
-                  gateway: 'example',
-                  gatewayMerchantId: 'exampleGatewayMerchantId',
-                },
-                type: 'PAYMENT_GATEWAY',
-              },
-              type: 'CARD',
-            },
-          ],
-          apiVersion: 2,
-          apiVersionMinor: 0,
-          callbackIntents: ['PAYMENT_AUTHORIZATION'],
-          // @ts-ignore
-          merchantInfo: {
-            // merchantId: 'spartacusStorefront',
-            merchantName: 'Spartacus Storefront',
-          },
-          transactionInfo: {
-            currencyCode: product?.price?.currencyIso || '',
-            totalPrice: this.estimateTotalPrice(product?.price?.value || 1),
-            totalPriceStatus: 'ESTIMATED',
-          },
-        };
-
-        this.googlePaymentClient.loadPaymentData(paymentDataRequest);
+      .getProduct()
+      .pipe(
+        take(1),
+        switchMap((product: Product | null) => {
+          return this.cartHandlerService.deleteCurrentCart().pipe(
+            switchMap(() => {
+              return this.cartHandlerService.addProductTocart(
+                product?.code || '',
+                this.itemCounterService.getCounter()
+              );
+            }),
+            tap(() => {
+              this.updateTransactionInfo({
+                totalPrice: this.estimateTotalPrice(product?.price?.value),
+                currencyCode: product?.price?.currencyIso,
+                totalPriceStatus: 'ESTIMATED',
+              });
+            })
+          );
+        })
+      )
+      .subscribe(() => {
+        this.googlePaymentClient.loadPaymentData(this.googlePaymentRequest);
       });
   }
 
@@ -111,10 +182,42 @@ export class OpfGooglePayService {
           resolve({ transactionState: 'SUCCESS' });
         });
       },
+      onPaymentDataChanged: (intermediatePaymentData) => {
+        // TODO: WiP
+        let self = this;
+        return new Promise(function (resolve) {
+          let paymentDataRequestUpdate = {};
+
+          if (intermediatePaymentData.callbackTrigger === 'INITIALIZE') {
+            console.log(intermediatePaymentData, self);
+            self
+              .setDeliveryAddress(intermediatePaymentData.shippingAddress)
+              .subscribe((result) => {
+                console.log('Updated address? ' + result);
+
+                resolve(paymentDataRequestUpdate);
+              });
+            console.log('SHIPPING TRIGGER!');
+          }
+
+          if (intermediatePaymentData.callbackTrigger === 'SHIPPING_ADDRESS') {
+            console.log(intermediatePaymentData, self);
+            self.updateShippingOptionParameters().subscribe(() => {
+              resolve(paymentDataRequestUpdate);
+            });
+            self
+              .setDeliveryAddress(intermediatePaymentData.shippingAddress)
+              .subscribe((result) => {
+                console.log('Updated address? ' + result);
+              });
+            console.log('SHIPPING TRIGGER!');
+          }
+        });
+      },
     };
   }
 
-  protected estimateTotalPrice(productPrice: number): string {
+  protected estimateTotalPrice(productPrice: number = 0): string {
     return (this.itemCounterService.getCounter() * productPrice).toString();
   }
 }
