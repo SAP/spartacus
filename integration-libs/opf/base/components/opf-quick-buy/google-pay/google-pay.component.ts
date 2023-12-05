4ae404aea904a8622279ed454eed3ccb9f90dc40
Matejk00 <mateusz.kolasa@sap.com>
2023-12-05 06:19:20 +0100

    Do not render google pay container if not supported

--- integration-libs/opf/base/components/opf-quick-buy/google-pay/google-pay.component.ts
+++ integration-libs/opf/base/components/opf-quick-buy/google-pay/google-pay.component.ts
@@ -6,6 +6,7 @@
 
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ElementRef,
   Input,
@@ -14,6 +15,7 @@ import {
   inject,
 } from '@angular/core';
 import { ActiveConfiguration } from '@spartacus/opf/base/root';
+import { BehaviorSubject } from 'rxjs';
 import { OpfGooglePayService } from './google-pay.service';
 
 @Component({
@@ -23,20 +25,26 @@ import { OpfGooglePayService } from './google-pay.service';
 })
 export class OpfGooglePayComponent implements OnInit {
   protected opfGooglePayService = inject(OpfGooglePayService);
+  protected changeDetectionRef = inject(ChangeDetectorRef);
 
   @Input() activeConfiguration: ActiveConfiguration;
 
   @ViewChild('googlePayButtonContainer') googlePayButtonContainer: ElementRef;
 
+  isReadyToPayState$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   ngOnInit(): void {
     this.opfGooglePayService.loadProviderResources().then(() => {
       this.opfGooglePayService.initClient(this.activeConfiguration);
       this.opfGooglePayService.isReadyToPay().then((response) => {
-        console.log('isGooglePaySupported', response.result);
+        this.isReadyToPayState$.next(response.result);
+        this.changeDetectionRef.detectChanges();
         if (response.result) {
-          this.opfGooglePayService.renderPaymentButton(
-            this.googlePayButtonContainer
-          );
+          if (this.googlePayButtonContainer) {
+            this.opfGooglePayService.renderPaymentButton(
+              this.googlePayButtonContainer
+            );
+          }
         }
       });
     });
