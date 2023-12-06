37bb320feabc2762a3b37e4db9210a24c813b4a6
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-12-06 09:57:00 +0100

    fix

--- projects/storefrontapp/src/app/spartacus/features/cds/cds-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cds/cds-feature.module.ts
@@ -6,17 +6,13 @@
 
 import { NgModule } from '@angular/core';
 import { CdsConfig, CdsModule } from '@spartacus/cds';
-import {
-  provideConfig,
-  provideConfigFactory,
-  WindowRef,
-} from '@spartacus/core';
+import { provideConfigFactory, WindowRef } from '@spartacus/core';
 
 const cds1: CdsConfig = {
   cds: {
     site: 'electronics-spa',
     tenant: 'E_SPA_TENANT',
-    baseUrl:'E_SPA_BASE_URL',
+    baseUrl: 'E_SPA_BASE_URL',
     endpoints: {
       strategyProducts: '/strategy/${tenant}/strategies/${strategyId}/products',
     },
@@ -24,10 +20,8 @@ const cds1: CdsConfig = {
       defaultCarouselViewportThreshold: 80,
     },
     profileTag: {
-      javascriptUrl:
-        'E_SPA_JS_URL',
-      configUrl:
-        'E_SPA_CONFIG_URL',
+      javascriptUrl: 'E_SPA_JS_URL',
+      configUrl: 'E_SPA_CONFIG_URL',
       allowInsecureCookies: true,
     },
   },
@@ -45,19 +39,17 @@ const cds2 = {
       defaultCarouselViewportThreshold: 80,
     },
     profileTag: {
-      javascriptUrl:
-        'A_SPA_JS_URL',
-      configUrl:
-        'A_SPA_CONFIG_URL',
+      javascriptUrl: 'A_SPA_JS_URL',
+      configUrl: 'A_SPA_CONFIG_URL',
       allowInsecureCookies: true,
-    }
-  }
+    },
+  },
 };
 const cdsArray = [cds1, cds2];
 
 const cdsConfig = (windowRef: WindowRef): CdsConfig => {
   if (!windowRef.isBrowser()) {
-    return;
+    return cdsArray[0];
   }
   const cds = cdsArray.filter((_cds: CdsConfig) =>
     windowRef.location.href.includes(_cds.cds.site)
@@ -67,11 +59,6 @@ const cdsConfig = (windowRef: WindowRef): CdsConfig => {
 
 @NgModule({
   imports: [CdsModule.forRoot()],
-  providers: [
-    provideConfigFactory(cdsConfig, [WindowRef]),
-    provideConfig(<CdsConfig>{
-      cds: cds1,
-    }),
-  ],
+  providers: [provideConfigFactory(cdsConfig, [WindowRef])],
 })
 export class CdsFeatureModule {}
