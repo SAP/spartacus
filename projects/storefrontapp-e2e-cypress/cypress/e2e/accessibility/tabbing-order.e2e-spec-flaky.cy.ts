e13925a2c4a1b5f1b81dfdcf14ab9ef075c2b15f
evy-yang01 <evy.yang@sap.com>
2023-12-12 10:54:06 +0800

    test:CXSPA-4736, Develop E2E and accessibility tests for Update Password view with Cumulus traceability (#18227)

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/tabbing-order.e2e-spec-flaky.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/tabbing-order.e2e-spec-flaky.cy.ts
@@ -37,6 +37,7 @@ import { changePasswordTabbingOrder } from '../../helpers/accessibility/tabbing-
 import { closeAccountTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/close-account';
 import { consentManagementTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/consent-management';
 import { myAccountV2consentManagementTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/my-account-v2-consent-management';
+import { myAccountV2PasswordTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/my-account-v2-password';
 import { checkoutMyCouponsTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/my-coupons';
 import { myInterestTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/my-interests';
 import { notificationPreferenceTabbingOrder } from '../../helpers/accessibility/tabbing-order/my-account/notification-preference';
@@ -412,6 +413,13 @@ describe('Tabbing order - tests do require user to be logged in', () => {
     it('should allow to navigate with tab key', () => {
       cy.requireLoggedIn();
       myAccountV2consentManagementTabbingOrder(config.consentManagementV2);
+    });
+  });
+
+  context('My Account V2 Password(CXSPA-4455)', () => {
+    it('should allow to navigate with tab key', () => {
+      cy.requireLoggedIn();
+      myAccountV2PasswordTabbingOrder(config.myAccountV2Password);
     });
   });
 
