bc5362f7821fbb26313047b14193e02096b27c7a
Christoph Hinssen <33626130+ChristophHi@users.noreply.github.com>
2023-12-15 07:46:30 +0100

    feat: CXSPA-5638 Adjust error handling (#18253)

--- feature-libs/quote/core/public_api.ts
+++ feature-libs/quote/core/public_api.ts
@@ -4,10 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './quote-core.module';
-export * from './connectors/index';
-export * from './facade/index';
 export * from './config/index';
-export * from './services/index';
-export * from './http-interceptors/index';
+export * from './connectors/index';
 export * from './event/index';
+export * from './facade/index';
+export * from './quote-core.module';
+export * from './services/index';
