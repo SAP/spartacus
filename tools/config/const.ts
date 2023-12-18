5ccb0850758d49e4d7ee534a9c097dbac0ee90e2
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-12-18 16:17:07 +0100

    chore: release 6.8.0-1 (#18262)

--- tools/config/const.ts
+++ tools/config/const.ts
@@ -10,4 +10,4 @@ export const SPARTACUS_SCOPE = '@spartacus';
 export const SAP_SCOPE = 'sap';
 export const SAPUI5_TYPES = '@sapui5/ts-types-esm';
 export const SPARTACUS_SCHEMATICS = `${SPARTACUS_SCOPE}/schematics`;
-export const PUBLISHING_VERSION = '6.7.0';
+export const PUBLISHING_VERSION = '6.8.0-1';
