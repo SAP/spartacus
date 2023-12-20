ab226722ea8aa32d574d0b8573902a1f7e583cdf
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2023-12-20 15:08:31 +0100

    Update cds-feature.module.ts

--- projects/storefrontapp/src/app/spartacus/features/cds/cds-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cds/cds-feature.module.ts
@@ -8,11 +8,31 @@ import { NgModule } from '@angular/core';
 import { CdsConfig, CdsModule } from '@spartacus/cds';
 import { provideConfig } from '@spartacus/core';
 
+const additionalCdsConfig = {
+  cds: {
+    site: 'A_CDS_SITE',
+    tenant: 'A_CDS_TENANT',
+    baseUrl: 'A_CDS_BASE_URL',
+    endpoints: {
+      strategyProducts: '/strategy/${tenant}/strategies/${strategyId}/products',
+    },
+    merchandising: {
+      defaultCarouselViewportThreshold: 80,
+    },
+    profileTag: {
+      javascriptUrl: 'A_CDS_PROFILE_TAG_LOAD_URL',
+      configUrl: 'A_CDS_PROFILE_TAG_CONFIG_URL',
+      allowInsecureCookies: true,
+    },
+  },
+}
+
 @NgModule({
   imports: [CdsModule.forRoot()],
   providers: [
     provideConfig(<CdsConfig>{
       cds: {
+        site: 'CDS_SITE',
         tenant: 'argotest',
         baseUrl: 'https://api.stage.context.cloud.sap',
         endpoints: {
@@ -30,6 +50,7 @@ import { provideConfig } from '@spartacus/core';
           allowInsecureCookies: true,
         },
       },
+      cdsConfigs: [additionalCdsConfig]
     }),
   ],
 })
