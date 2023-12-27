460cb6d695b798c4f7d14fd39583902d0cf5a9a7
rmch91 <r.chygryn@sap.com>
2023-12-27 13:30:38 +0100

    release-6.8.0-2

--- projects/schematics/src/shared/utils/package-utils_spec.ts
+++ projects/schematics/src/shared/utils/package-utils_spec.ts
@@ -101,8 +101,8 @@ describe('Package utils', () => {
       const version = getSpartacusSchematicsVersion();
       const featureLevel = getSpartacusCurrentFeatureLevel();
       expect(featureLevel).toBeTruthy();
-      expect(featureLevel.length).toEqual(7);
-      expect(featureLevel).toEqual(version.substring(0, 7));
+      expect(featureLevel.length).toEqual(3);
+      expect(featureLevel).toEqual(version.substring(0, 3));
     });
   });
 });
