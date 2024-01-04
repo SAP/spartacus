349f6a09a1eafe86da05ed85b0fece6c750a6d2d
Piotr Bartkowiak <pbartkowiak@divante.pl>
2024-01-04 17:32:16 +0100

    e2e: added keyboard navigation test

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/keyboard-navigation.e2e.cy.ts
@@ -0,0 +1,36 @@
+describe('Kayboard navigation', () => {
+  context('Navigation UI component', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.get('cx-navigation-ui [aria-haspopup="true"]').as('firstDropdown');
+    });
+    it('opens a node after pressing space', () => {
+      cy.get('@firstDropdown').each((dropdownButton) => {
+        cy.wrap(dropdownButton).as('dropdownButton');
+        cy.get('@dropdownButton').focus().type(' ').next().should('be.visible');
+      });
+    });
+    it('navigates node children with downArrow key', () => {
+      cy.get('@firstDropdown').first().focus().type(' ');
+      cy.contains(' Cameras ').type('{downArrow}');
+      cy.contains(' Canon ').should('have.focus').type('{downArrow}');
+      cy.contains(' Sony ').should('have.focus').type('{downArrow}');
+      cy.contains(' Kodak ').should('have.focus').type('{downArrow}');
+      cy.contains(' Samsung ').should('have.focus').type('{downArrow}');
+      cy.contains(' Toshiba ').should('have.focus').type('{downArrow}');
+      cy.contains(' Fujifilm ').should('have.focus').type('{downArrow}');
+      cy.contains(' Fujifilm ').should('have.focus');
+    });
+
+    it('navigates node children with upArrow key', () => {
+      cy.get('@firstDropdown').first().focus().type(' ');
+      cy.contains(' Fujifilm ').focus().type('{upArrow}');
+      cy.contains(' Toshiba ').should('have.focus').type('{upArrow}');
+      cy.contains(' Samsung ').should('have.focus').type('{upArrow}');
+      cy.contains(' Kodak ').should('have.focus').type('{upArrow}');
+      cy.contains(' Sony ').should('have.focus').type('{upArrow}');
+      cy.contains(' Canon ').should('have.focus').type('{upArrow}');
+      cy.contains(' Canon ').should('have.focus');
+    });
+  });
+});
