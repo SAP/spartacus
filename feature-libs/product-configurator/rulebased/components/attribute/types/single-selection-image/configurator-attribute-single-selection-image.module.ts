ad92fc899bbf40c9a6c3804c8bb8791c566ce2a7
Larisa Staroverova <larisa.staroverova@sap.com>
2024-01-25 16:00:43 +0100

    Refactoring III

--- feature-libs/product-configurator/rulebased/components/attribute/types/single-selection-image/configurator-attribute-single-selection-image.module.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/single-selection-image/configurator-attribute-single-selection-image.module.ts
@@ -28,6 +28,8 @@ import { ConfiguratorAttributeCompositionConfig } from '../../composition/config
         assignment: {
           AttributeType_single_selection_image:
             ConfiguratorAttributeSingleSelectionImageComponent,
+          AttributeType_read_only_single_selection_image:
+            ConfiguratorAttributeSingleSelectionImageComponent,
         },
       },
     }),
