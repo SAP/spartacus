b519e376457988e44e473b48e6e70ec0a47ad594
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-02-01 16:10:25 +0100

    update migration.json

---
+++ projects/schematics/src/migrations/2211_0/ssr/ssr.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+export function migrate(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  };
+}
