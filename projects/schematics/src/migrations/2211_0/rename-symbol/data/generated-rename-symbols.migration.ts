02e7b7cd2146804cf56b061889fc6a99a406d3f9
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-02-01 14:34:10 +0100

    generate breaking changes migrations

---
+++ projects/schematics/src/migrations/2211_0/rename-symbol/data/generated-rename-symbols.migration.ts
@@ -0,0 +1,46 @@
+import { RenamedSymbol } from '../../../../shared/utils/file-utils';
+//
+// Generated file, don't update manually.
+//
+export const GENERATED_RENAMED_SYMBOLS_DATA: RenamedSymbol[] = [
+	{
+		previousNode: 'CardType',
+		previousImportPath: '@spartacus/cart/base/root',
+		newImportPath: '@spartacus/core'
+	},
+	{
+		previousNode: 'PaymentDetails',
+		previousImportPath: '@spartacus/cart/base/root',
+		newImportPath: '@spartacus/core'
+	},
+	{
+		previousNode: 'AddressBookComponent',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	},
+	{
+		previousNode: 'AddressBookComponentService',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	},
+	{
+		previousNode: 'AddressBookModule',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	},
+	{
+		previousNode: 'AddressFormComponent',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	},
+	{
+		previousNode: 'AddressFormModule',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	},
+	{
+		previousNode: 'SuggestedAddressDialogComponent',
+		previousImportPath: '@spartacus/storefront',
+		newImportPath: '@spartacus/user/profile/components'
+	}
+];
