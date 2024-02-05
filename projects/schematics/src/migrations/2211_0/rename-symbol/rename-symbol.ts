3ca40bd73361789d00c6f0a22da19ece10f3b009
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-02-05 15:20:58 +0100

    prettier fix

--- projects/schematics/src/migrations/2211_0/rename-symbol/rename-symbol.ts
+++ projects/schematics/src/migrations/2211_0/rename-symbol/rename-symbol.ts
@@ -6,7 +6,7 @@
 
 import { Rule, Tree } from '@angular-devkit/schematics';
 import { migrateRenamedSymbols } from '../../mechanism/rename-symbol/rename-symbol';
-import { GENERATED_RENAMED_SYMBOLS_DATA } from "./data/generated-rename-symbols.migration";
+import { GENERATED_RENAMED_SYMBOLS_DATA } from './data/generated-rename-symbols.migration';
 
 export function migrate(): Rule {
   return (tree: Tree) => {
