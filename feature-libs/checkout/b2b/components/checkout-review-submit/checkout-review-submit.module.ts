c17f7536b1a273635b2f2a0f9026691e8ac0ebf8
Krzysztof Platis <platonn.git@gmail.com>
2024-02-09 12:11:56 +0100

    un-import leftover FeaturesConfigModule from other NgModules

--- feature-libs/checkout/b2b/components/checkout-review-submit/checkout-review-submit.module.ts
+++ feature-libs/checkout/b2b/components/checkout-review-submit/checkout-review-submit.module.ts
@@ -13,10 +13,9 @@ import {
 } from '@spartacus/checkout/base/components';
 import {
   CmsConfig,
-  FeaturesConfigModule,
   I18nModule,
-  provideDefaultConfig,
   UrlModule,
+  provideDefaultConfig,
 } from '@spartacus/core';
 import {
   CardModule,
@@ -36,7 +35,6 @@ import { B2BCheckoutReviewSubmitComponent } from './checkout-review-submit.compo
     PromotionsModule,
     IconModule,
     OutletModule,
-    FeaturesConfigModule,
   ],
   providers: [
     provideDefaultConfig(<CmsConfig>{
