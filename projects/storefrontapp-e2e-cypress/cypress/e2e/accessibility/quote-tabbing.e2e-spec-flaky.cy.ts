8c988d28fbff1e293fd8441ea0d75be49b826846
Christoph Hinssen <christoph.hinssen@sap.com>
2024-02-13 16:38:54 +0100

    Update quote-tabbing.e2e-spec-flaky.cy.ts

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/quote-tabbing.e2e-spec-flaky.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/quote-tabbing.e2e-spec-flaky.cy.ts
@@ -1,14 +1,7 @@
-/*
- * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
- * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { verifyTabbingOrder } from '../../helpers/accessibility/tabbing-order';
-import * as quote from '../../helpers/b2b/b2b-quote';
-import { clickAllowAllFromBanner } from '../../helpers/anonymous-consents';
 import { tabbingOrderConfig } from '../../helpers/accessibility/b2b/tabbing-order.config';
+import { verifyTabbingOrder } from '../../helpers/accessibility/tabbing-order';
+import { clickAllowAllFromBanner } from '../../helpers/anonymous-consents';
+import * as quote from '../../helpers/b2b/b2b-quote';
 
 const containerSelectorQuoteDetails = 'main';
 const TEST_PRODUCT_HAMMER_DRILLING_ID = '3887130';
