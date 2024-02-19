1e1c51efe537dc699c954da7915025c3a6675114
Piotr Bartkowiak <pbartkowiak@divante.pl>
2024-02-19 17:06:44 +0100

    Proposed keyboard controls for organization tables

--- feature-libs/organization/administration/components/unit/list/toggle-link/toggle-link-cell.component.ts
+++ feature-libs/organization/administration/components/unit/list/toggle-link/toggle-link-cell.component.ts
@@ -4,13 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  Optional,
+} from '@angular/core';
 import { B2BUnit } from '@spartacus/core';
 import { B2BUnitTreeNode } from '@spartacus/organization/administration/core';
 import {
   OutletContextData,
   TableDataOutletContext,
 } from '@spartacus/storefront';
+import { take } from 'rxjs';
 import { CellComponent } from '../../../shared/table/cell.component';
 import { UnitTreeService } from '../../services/unit-tree.service';
 
@@ -27,17 +34,14 @@ export class ToggleLinkCellComponent extends CellComponent {
 
   constructor(
     protected outlet: OutletContextData<TableDataOutletContext>,
-    protected unitTreeService: UnitTreeService
+    protected unitTreeService: UnitTreeService,
+    @Optional() private elementRef: ElementRef
   ) {
     super(outlet);
   }
 
   get combinedName() {
     return this.property ? `${this.property} (${this.count})` : '';
-  }
-
-  get tabIndex() {
-    return 0;
   }
 
   get expanded() {
@@ -78,5 +82,87 @@ export class ToggleLinkCellComponent extends CellComponent {
     }
     const { _field, _options, _type, _i18nRoot, ...all } = this.outlet.context;
     return all as B2BUnit;
+  }
+
+  onFocus(event: FocusEvent) {
+    if (this.expanded || !this.isSwitchable) {
+      return;
+    }
+
+    const focusedElementId = document.activeElement?.id || '';
+    this.toggleItem(event);
+    this.unitTreeService.treeToggle$.pipe(take(1)).subscribe(() => {
+      setTimeout(() => {
+        document.getElementById(focusedElementId)?.focus();
+      }, 0);
+    });
+  }
+
+  onKeydown(event: KeyboardEvent) {
+    const tableElement = this.elementRef.nativeElement.closest('table');
+    const focusedDepth = this.model.depthLevel;
+    const siblingElements = tableElement.querySelectorAll(
+      `[data-depth="${focusedDepth}"]`
+    );
+    const currentSelectedIndex = Array.from(siblingElements).findIndex(
+      (element) => {
+        return element === event.target;
+      }
+    );
+
+    this.handleTab(event, focusedDepth, tableElement);
+    this.handleArrowDown(event, currentSelectedIndex, siblingElements);
+    this.handleArrowUp(event, currentSelectedIndex, siblingElements);
+  }
+
+  handleTab(
+    event: KeyboardEvent,
+    focusedDepth: number,
+    tableElement: HTMLElement
+  ): void {
+    if (!(event.key === 'Tab' && this.isSwitchable && !event.shiftKey)) {
+      return;
+    }
+    const firstChild = Array.from(
+      tableElement.querySelectorAll(`[data-depth="${focusedDepth + 1}"]`)
+    ).find((element) => {
+      return this.model.children
+        .map((child: TableDataOutletContext) => child.id)
+        .includes((element as HTMLElement).id);
+    }) as HTMLElement;
+
+    event.preventDefault();
+    firstChild.focus();
+  }
+
+  handleArrowDown(
+    event: KeyboardEvent,
+    currentSelectedIndex: number,
+    siblingElements: Element[]
+  ): void {
+    if (
+      event.key === 'ArrowDown' &&
+      currentSelectedIndex < siblingElements.length - 1
+    ) {
+      if (this.expanded) {
+        this.toggleItem(event);
+      }
+      event.preventDefault();
+      (siblingElements[currentSelectedIndex + 1] as HTMLElement)?.focus();
+    }
+  }
+
+  handleArrowUp(
+    event: KeyboardEvent,
+    currentSelectedIndex: number,
+    siblingElements: Element[]
+  ): void {
+    if (event.key === 'ArrowUp' && currentSelectedIndex > 0) {
+      if (this.expanded) {
+        this.toggleItem(event);
+      }
+      event.preventDefault();
+      (siblingElements[currentSelectedIndex + -1] as HTMLElement)?.focus();
+    }
   }
 }
