5ed15eb76e5923f424b2225d82a386b80d73805c
Alexander Pilarczyk <alexander.pilarczyk@sap.com>
2024-02-26 13:28:19 +0100

    Update tests for b2c/b2b or cpq test environment

--- run-spartacus-product-configurator-e2es.sh
+++ run-spartacus-product-configurator-e2es.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+# This script is invoked from https://github.tools.sap/cx-commerce/wonderful-testing-framework/blob/main/resources/wtf/config.yaml
+# By default, the e2es for b2c product-configurator will be executed without parameters. 
+# Supported options include "cpq", "b2c" and "b2c_flaky"
+
+param=${1:-"b2c"} # Default to "b2c" if no parameter provided
+
+# Update variables based on the provided parameter
+case "$param" in
+  "cpq")
+    export E2E_BASE_SITE="powertools-spa"
+    export ENDPOINT_URL_PUBLIC_SPARTACUS=$ENDPOINT_URL_PUBLIC_SPA_POWERTOOLS 
+    export E2ES_TO_RUN="'cypress/e2e/**/cpq-configuration.ccv2-e2e.cy.ts'"
+    ;;
+  "b2c")
+    export E2E_BASE_SITE="electronics-spa"
+    export E2ES_TO_RUN="'cypress/e2e/**/product_configurator/*-configurator*e2e.cy.ts'"
+    ;;
+  "b2c_flaky")
+    export E2E_BASE_SITE="electronics-spa"
+    export E2ES_TO_RUN="'cypress/e2e/**/*-configurator*e2e*flaky.cy.ts'"
+    ;;
+  *)
+    echo "Invalid parameter. Please provide a valid parameter."
+    exit 1
+    ;;
+esac
+
+
 # Install cypress standalone dependencies
 (cd projects/storefrontapp-e2e-cypress && npm install)
 
