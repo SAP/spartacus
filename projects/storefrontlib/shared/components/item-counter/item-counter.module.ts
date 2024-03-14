d84ae2818f853fbd07ae92cbd3fa378d0042ddca
Piotr Bartkowiak <pbartkowiak@divante.pl>
2024-03-14 18:12:21 +0100

    removed useless module

--- projects/storefrontlib/shared/components/item-counter/item-counter.module.ts
+++ projects/storefrontlib/shared/components/item-counter/item-counter.module.ts
@@ -7,16 +7,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FeaturesConfigModule, I18nModule } from '@spartacus/core';
+import { I18nModule } from '@spartacus/core';
 import { ItemCounterComponent } from './item-counter.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    I18nModule,
-    FeaturesConfigModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, I18nModule],
   declarations: [ItemCounterComponent],
   exports: [ItemCounterComponent],
 })
