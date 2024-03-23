fa3b8df978e72db83da30332193c453f173ecfed
Akshay Tiwari <akshay.tiwari@sap.com>
2024-03-23 13:48:42 +0530

    POC for OMSA-ETA UI

--- integration-libs/s4om/root/model/augmented-core.model.ts
+++ integration-libs/s4om/root/model/augmented-core.model.ts
@@ -1,11 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 import '@spartacus/cart/base/root';
-import { ScheduleLine } from './schedule-line.model';
+import { arrivalSlots } from './schedule-line.model';
 
 declare module '@spartacus/cart/base/root' {
   interface OrderEntry {
