752037375564f9761c61f9d006090de261164e3f
anjana-bl <anjana.b.l@sap.com>
2024-04-03 15:28:55 +0530

    Update ai-prompts.ts

--- projects/core/src/product/facade/ai-prompts.ts
+++ projects/core/src/product/facade/ai-prompts.ts
@@ -1,12 +1,12 @@
 export const SUMMARY_PROMPT =
   'Generate a review summary based on the following reviews';
 export const POSITIVE_SENTIMENTS_PROMPT =
-  'Maximum 3 tags in separate lines with positive sentiment, ignoring spam reviews';
+  'Based on the provided reviews please suggest up to three positive sentiment tags that are meaningful and non-spammy, in separate lines.';
 export const NEGATIVE_SENTIMENTS_PROMPT =
-  'Maximum 3 tags in separate lines with negative sentiment, ignoring spam reviews';
+'Based on the provided reviews please suggest up to three negative sentiment that are meaningful and non-spammy, in separate lines.';
 export const COMMENT_PROMPT =
   'Generate a detailed customer review comment with approximately 30 words based on product details, review title and rating (out of 5) provided by customer';
 export const SENTIMENTS_PERCENTAGE_PROMPT =
-  'Give an array where first element is positive sentiment percentage and second element is negative sentiment percentage';
+  'Provide an array with two elements representing percentages of positive and negative sentiments, ensuring that their sum equals 100';
 export const FETCH_SPAM_PROMPT =
-  'Give an array of position of reviews which can be considered spam. A review can be considered spam if it includes irrelevant content, repetitive phrases, suspiciously positive or negative sentiments, or promotional language';
+  'Provide the output as an array containing the positions of reviews that could be considered spam based on the following criteria: irrelevant content';
