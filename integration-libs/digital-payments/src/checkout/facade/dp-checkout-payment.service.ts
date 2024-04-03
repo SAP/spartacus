8eadf0bb8e155663f74e90f54a2a47353b7d6e54
anjana-bl <anjana.b.l@sap.com>
2024-04-03 11:32:54 +0530

    code changes - part 1

--- integration-libs/digital-payments/src/checkout/facade/dp-checkout-payment.service.ts
+++ integration-libs/digital-payments/src/checkout/facade/dp-checkout-payment.service.ts
@@ -4,20 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {
   Command,
   CommandService,
   CommandStrategy,
+  OCC_USER_ID_ANONYMOUS,
   PaymentDetails,
   Query,
   QueryService,
   UserIdService,
 } from '@spartacus/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { DigitalPaymentsAdapter } from '../adapters/digital-payments.adapter';
 import { DpPaymentRequest } from '../models/dp-checkout.model';
+import { ActiveCartService } from '@spartacus/cart/base/core';
 @Injectable({
   providedIn: 'root',
 })
@@ -28,12 +30,21 @@ export class DpCheckoutPaymentService {
     protected query: QueryService,
     protected userIdService: UserIdService
   ) {}
+  protected activeCartService = inject(ActiveCartService);
 
-  protected RequestUrlQuery: Query<DpPaymentRequest> = this.query.create(() =>
-    this.userIdService
-      .takeUserId()
-      .pipe(switchMap((userId) => this.dpAdapter.createPaymentRequest(userId)))
-  );
+  protected RequestUrlQuery: Query<DpPaymentRequest> = this.query.create(() => {
+    return combineLatest([
+      this.userIdService.takeUserId(),
+      this.activeCartService.getActive(),
+    ]).pipe(
+      switchMap(([userId, cart]) => {
+        if (userId === OCC_USER_ID_ANONYMOUS) {
+          return this.dpAdapter.createPaymentRequest(userId, cart.guid);
+        }
+        return this.dpAdapter.createPaymentRequest(userId);
+      })
+    );
+  });
 
   getCardRegistrationDetails(): Observable<DpPaymentRequest | undefined> {
     return this.RequestUrlQuery.get();
@@ -47,17 +58,27 @@ export class DpCheckoutPaymentService {
     PaymentDetails
   > = this.command.create(
     (payload) =>
-      this.userIdService
-        .takeUserId()
-        .pipe(
-          switchMap((userId) =>
-            this.dpAdapter.createPaymentDetails(
+      combineLatest([
+        this.userIdService.takeUserId(),
+        this.activeCartService.getActive(),
+      ]).pipe(
+        switchMap(([userId, cart]) => {
+          if (userId === OCC_USER_ID_ANONYMOUS) {
+            return this.dpAdapter.createPaymentDetails(
+              payload.sessionId,
+              payload.signature,
+              userId,
+              cart.guid
+            );
+          } else {
+            return this.dpAdapter.createPaymentDetails(
               payload.sessionId,
               payload.signature,
               userId
-            )
-          )
-        ),
+            );
+          }
+        })
+      ),
     {
       strategy: CommandStrategy.Queue,
     }
