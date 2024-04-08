bdfe80bcd2e629edb2388e2fcdc3c15e13ba440b
Krzysztof Platis <platonn.git@gmail.com>
2024-04-08 18:08:43 +0200

    test: feature-toggles-providers

---
+++ projects/core/src/features-config/feature-toggles/feature-toggles-providers.spec.ts
@@ -0,0 +1,72 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  provideDefaultFeatureToggles,
+  provideDefaultFeatureTogglesFactory,
+  provideFeatureToggles,
+  provideFeatureTogglesFactory,
+} from './feature-toggles-providers';
+import {
+  DefaultFeatureTogglesChunk,
+  FeatureTogglesChunk,
+} from './feature-toggles-tokens';
+
+declare module './feature-toggles-tokens' {
+  interface FeatureToggles {
+    test?: boolean;
+  }
+}
+
+describe('FeatureToggles providers', () => {
+  describe(`provideFeatureToggles`, () => {
+    it('should provide FeatureTogglesChunk with the given toggles', () => {
+      const toggles = { test: true };
+      const provider = provideFeatureToggles(toggles);
+      expect(provider).toEqual({
+        provide: FeatureTogglesChunk,
+        useValue: toggles,
+        multi: true,
+      });
+    });
+  });
+
+  describe(`provideDefaultFeatureToggles`, () => {
+    it('should provide DefaultFeatureTogglesChunk with the given toggles', () => {
+      const toggles = { test: true };
+      const provider = provideDefaultFeatureToggles(toggles);
+      expect(provider).toEqual({
+        provide: DefaultFeatureTogglesChunk,
+        useValue: toggles,
+        multi: true,
+      });
+    });
+  });
+
+  describe(`provideFeatureTogglesFactory`, () => {
+    it('should provide FeatureTogglesChunk with the given factory', () => {
+      const factory = () => ({ test: true });
+      const provider = provideFeatureTogglesFactory(factory);
+      expect(provider).toEqual({
+        provide: FeatureTogglesChunk,
+        useFactory: factory,
+        multi: true,
+      });
+    });
+  });
+
+  describe(`provideDefaultFeatureTogglesFactory`, () => {
+    it('should provide DefaultFeatureTogglesChunk with the given factory', () => {
+      const factory = () => ({ test: true });
+      const provider = provideDefaultFeatureTogglesFactory(factory);
+      expect(provider).toEqual({
+        provide: DefaultFeatureTogglesChunk,
+        useFactory: factory,
+        multi: true,
+      });
+    });
+  });
+});
