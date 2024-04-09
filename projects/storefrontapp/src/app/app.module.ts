9371196749abec7e673a0bdc0e062e414fac7ec3
PioBar <72926984+Pio-Bar@users.noreply.github.com>
2024-04-09 14:46:53 +0200

    fix: (CXSPA-2798) - Show assistive message when active view changes (#18609)
    Co-authored-by: Piotr Bartkowiak <pbartkowiak@divante.pl>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -97,6 +97,7 @@ if (!environment.production) {
         a11yNavigationUiKeyboardControls: true,
         a11yOrderConfirmationHeadingOrder: true,
         a11yStarRating: true,
+        a11yViewChangeAssistiveMessage: true,
         a11yReorderDialog: true,
         a11yPopoverFocus: true,
         a11yScheduleReplenishment: true,
