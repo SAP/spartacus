62e321cee7f731db53fc186b3698e21a84c227a6
Paweł Fraś <fras.pawel@yahoo.com>
2024-04-09 10:54:12 +0200

    fix: CXSPA-6581 Race condition in OpenID code flow causes redirect loop or double redirect on login (#18706)

--- projects/core/src/auth/user-auth/user-auth.module.ts
+++ projects/core/src/auth/user-auth/user-auth.module.ts
@@ -6,10 +6,10 @@
 
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import {
-  APP_INITIALIZER,
   ModuleWithProviders,
   NgModule,
   PLATFORM_ID,
+  inject,
 } from '@angular/core';
 import { OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
 import { lastValueFrom } from 'rxjs';
@@ -24,6 +24,7 @@ import { AuthService } from './facade/auth.service';
 import { interceptors } from './http-interceptors/index';
 import { AuthStatePersistenceService } from './services/auth-state-persistence.service';
 import { AuthStorageService } from './services/auth-storage.service';
+import { LOCATION_INITIALIZED_MULTI } from '../../routing/location-initialized-multi/location-initialized-multi';
 
 /**
  * Initialize the check for `token` or `code` in the url returned from the OAuth server.
@@ -54,6 +55,23 @@ export function authStatePersistenceFactory(
 }
 
 /**
+ * Factory to ensure that auth is initialized and its state is synced between
+ * browser storage and in-memory storage.
+ * This is required to handle the OAuth callback.
+ */
+const authInitializedFactory = () => {
+  const authService = inject(AuthService);
+  const configInit = inject(ConfigInitializerService);
+  const platformId = inject(PLATFORM_ID);
+  const authStatePersistenceService = inject(AuthStatePersistenceService);
+
+  //We need to call it before checkOAuthParamsInUrl to ensure that in-memory storage is in sync with browser storage.
+  //In other case "nonce" value used in Implicit Flow will be undefined causing the error.
+  authStatePersistenceFactory(authStatePersistenceService)();
+  return checkOAuthParamsInUrl(authService, configInit, platformId);
+};
+
+/**
  * Authentication module for a user. Handlers requests for logged in users,
  * provides authorization services and storage for tokens.
  */
@@ -73,15 +91,8 @@ export class UserAuthModule {
           useExisting: AuthStorageService,
         },
         {
-          provide: APP_INITIALIZER,
-          useFactory: authStatePersistenceFactory,
-          deps: [AuthStatePersistenceService],
-          multi: true,
-        },
-        {
-          provide: APP_INITIALIZER,
-          useFactory: checkOAuthParamsInUrl,
-          deps: [AuthService, ConfigInitializerService, PLATFORM_ID],
+          provide: LOCATION_INITIALIZED_MULTI,
+          useFactory: authInitializedFactory,
           multi: true,
         },
       ],
