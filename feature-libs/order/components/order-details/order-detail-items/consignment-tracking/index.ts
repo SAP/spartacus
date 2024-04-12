d91afb9723dfc6748cc79e0c2eced54a865a62c7
Krzysztof Platis <platonn.git@gmail.com>
2024-04-12 16:10:51 +0200

    revert unnecessary changes in barrel files in order feature

--- feature-libs/order/components/order-details/order-detail-items/consignment-tracking/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './consignment-tracking.component';
-export * from './tracking-events/tracking-events.component';
