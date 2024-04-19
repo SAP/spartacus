dbd9c97ffaa2a0e24822d261b67e990ee8956c31
Christoph Hinssen <christoph.hinssen@sap.com>
2024-04-19 08:46:44 +0200

    a11y support

--- feature-libs/product-configurator/rulebased/components/attribute/types/date-input-field/configurator-attribute-date-input-field.component.spec.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/date-input-field/configurator-attribute-date-input-field.component.spec.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormControl } from '@angular/forms';
-import { ConfiguratorStorefrontUtilsService } from '@spartacus/product-configurator/rulebased';
+import { I18nTestingModule } from '@spartacus/core';
+import {
+  Configurator,
+  ConfiguratorStorefrontUtilsService,
+} from '@spartacus/product-configurator/rulebased';
 import { Observable, of } from 'rxjs';
+import { CommonConfiguratorTestUtilsService } from '../../../../../common/testing/common-configurator-test-utils.service';
 import { ConfiguratorCommonsService } from '../../../../core/facade/configurator-commons.service';
 import { ConfiguratorTestUtils } from '../../../../testing/configurator-test-utils';
 import { ConfiguratorAttributeCompositionContext } from '../../composition/configurator-attribute-composition.model';
 import { ConfiguratorAttributeDateInputFieldComponent } from './configurator-attribute-date-input-field.component';
 
+const attributeName = 'attributeName';
+const groupId = 'groupId';
 class MockConfiguratorCommonsService {
   updateConfiguration(): void {}
 }
@@ -33,6 +40,7 @@ class MockConfigUtilsService {
 describe('ConfiguratorAttributeDateInputFieldComponent', () => {
   let component: ConfiguratorAttributeDateInputFieldComponent;
   let fixture: ComponentFixture<ConfiguratorAttributeDateInputFieldComponent>;
+  let htmlElem: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
@@ -41,6 +49,7 @@ describe('ConfiguratorAttributeDateInputFieldComponent', () => {
           ConfiguratorAttributeDateInputFieldComponent,
           MockDatePickerComponent,
         ],
+        imports: [I18nTestingModule],
         providers: [
           {
             provide: ConfiguratorAttributeCompositionContext,
@@ -55,7 +64,13 @@ describe('ConfiguratorAttributeDateInputFieldComponent', () => {
             useClass: MockConfigUtilsService,
           },
         ],
-      }).compileComponents();
+      })
+        .overrideComponent(ConfiguratorAttributeDateInputFieldComponent, {
+          set: {
+            changeDetection: ChangeDetectionStrategy.Default,
+          },
+        })
+        .compileComponents();
     })
   );
 
@@ -64,7 +79,17 @@ describe('ConfiguratorAttributeDateInputFieldComponent', () => {
       ConfiguratorAttributeDateInputFieldComponent
     );
     component = fixture.componentInstance;
+    component.attribute = {
+      name: attributeName,
+      label: attributeName,
+      uiType: Configurator.UiType.SAP_DATE,
+      userInput: undefined,
+      required: true,
+      incomplete: true,
+      groupId: groupId,
+    };
     fixture.detectChanges();
+    htmlElem = fixture.nativeElement;
   });
 
   it('should create', () => {
@@ -72,7 +97,39 @@ describe('ConfiguratorAttributeDateInputFieldComponent', () => {
   });
 
   it('should render date picker', () => {
-    const htmlElem: HTMLElement = fixture.nativeElement;
     expect(htmlElem.querySelector('cx-date-picker')).toBeTruthy();
+  });
+
+  describe('Accessibility support', () => {
+    it('should render proper aria element if no attribute value present ', () => {
+      CommonConfiguratorTestUtilsService.expectElementContainsA11y(
+        expect,
+        htmlElem,
+        'span',
+        undefined,
+        0,
+        'aria-label',
+        'configurator.a11y.valueOfAttributeBlank attribute:' +
+          component.attribute.label
+      );
+    });
+
+    it('should render proper aria element including value if attribute value is present ', () => {
+      component.attribute.userInput = '2025-02-01';
+      fixture.detectChanges();
+
+      CommonConfiguratorTestUtilsService.expectElementContainsA11y(
+        expect,
+        htmlElem,
+        'span',
+        undefined,
+        0,
+        'aria-label',
+        'configurator.a11y.valueOfAttributeFull attribute:' +
+          component.attribute.label +
+          ' value:' +
+          component.attribute.userInput
+      );
+    });
   });
 });
