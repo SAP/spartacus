c4f43637188b2743f29cc801696e214db8c009ab
Krzysztof Platis <platonn.git@gmail.com>
2024-04-19 18:05:32 +0200

    remove unnecessary param from script copy-feature-toggles

--- projects/schematics/copy-feature-toggles.js
+++ projects/schematics/copy-feature-toggles.js
@@ -1,18 +1,10 @@
-const fs = require('fs');
-
 /**
  * This script copies the feature-toggles.ts file from the Spartacus core project to schematics project.
- * 
- * Usage:
- * `node copy-feature-toggles.ts copy`
- */
+*/
 
+const fs = require('fs');
 const SOURCE =
   '../../projects/core/src/features-config/feature-toggles/config/feature-toggles.ts';
 const DESTINATION = './src/feature-toggles.copied-from-core-lib.ts';
 
-const action = process.argv[2];
-
-if (action === 'copy') {
-  fs.copyFileSync(SOURCE, DESTINATION);
-}
+fs.copyFileSync(SOURCE, DESTINATION);
