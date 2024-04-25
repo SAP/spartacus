0b7d289c6d123a3cbd452cb9471cb3e4c7196adb
PioBar <72926984+Pio-Bar@users.noreply.github.com>
2024-04-25 13:31:14 +0200

    feature: (CXSPA-2190) - Keyboard controls for organization tables (#18514)
    Co-authored-by: Piotr Bartkowiak <pbartkowiak@divante.pl>

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -154,6 +154,13 @@ export interface FeatureTogglesInterface {
   a11yCartSummaryHeadingOrder?: boolean;
 
   /**
+   * Allows users to navigate through the list of units using the arrow keys.
+   * Enables keyboard controls inside 'ToggleLinkCellComponent' and
+   * adjusts 'ListComponent' styles to accomodate.
+   */
+  a11yUnitsListKeyboardControls?: boolean;
+
+  /**
    * When set to `true`, product titles in `CartItemComponent`, `QuickOrderItemComponent`, `WishListItemComponent`
    * adopt a more link-like style, appearing blue with an underline. This enhances visual cues for clickable elements,
    * providing a more intuitive user experience.
@@ -188,5 +195,6 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   a11yListOversizedFocus: false,
   a11yStoreFinderOverflow: false,
   a11yCartSummaryHeadingOrder: false,
+  a11yUnitsListKeyboardControls: false,
   a11yCartItemsLinksStyles: false,
 };
