4f1f7ca28080c18d611ed85c7153acfb676a1518
Pawel Fras <fras.pawel@yahoo.com>
2024-05-06 09:43:07 +0200

    chore: CXSPA-6960 Remove 2023 license headers

--- projects/core/src/error-handling/http-error-handler/http-error-handler.interceptor.ts
+++ projects/core/src/error-handling/http-error-handler/http-error-handler.interceptor.ts
@@ -1,11 +1,9 @@
 /*
- * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
  * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ErrorHandler, Injectable } from '@angular/core';
 import {
   HttpErrorResponse,
   HttpEvent,
@@ -13,6 +11,7 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
