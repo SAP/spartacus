9aa7b8d50d97a4cffd86bc04edf550834845d3cf
Christoph Hinssen <christoph.hinssen@sap.com>
2024-05-08 08:50:40 +0200

    Pass event observable to addedToCartDialog

--- feature-libs/cart/base/components/added-to-cart-dialog/added-to-cart-dialog-event.listener.spec.ts
+++ feature-libs/cart/base/components/added-to-cart-dialog/added-to-cart-dialog-event.listener.spec.ts
@@ -8,7 +8,6 @@ import {
 import {
   CxEvent,
   EventService,
-  FeatureConfigService,
   PointOfService,
 } from '@spartacus/core';
 import { LAUNCH_CALLER, LaunchDialogService } from '@spartacus/storefront';
@@ -68,7 +67,6 @@ mockFailEvent.error = {};
 describe('AddedToCartDialogEventListener', () => {
   let listener: AddedToCartDialogEventListener;
   let launchDialogService: LaunchDialogService;
-  let featureConfigService: FeatureConfigService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -85,48 +83,20 @@ describe('AddedToCartDialogEventListener', () => {
       ],
     });
 
-    featureConfigService = TestBed.inject(FeatureConfigService);
     launchDialogService = TestBed.inject(LaunchDialogService);
     entry.deliveryPointOfService = deliveryPointOfService;
   });
 
   describe('onAddToCart', () => {
-    it('should open modal on event CartAddEntrySuccessEvent in case toggle addedToCartDialogDrivenBySuccessEvent is active', () => {
-      spyOn(featureConfigService, 'isEnabled').and.returnValue(true);
-      listener = TestBed.inject(AddedToCartDialogEventListener);
-      spyOn(listener as any, 'openModalAfterSuccess').and.stub();
-      mockEventSuccessStream$.next(mockSuccessEvent);
-      expect(listener['openModalAfterSuccess']).toHaveBeenCalledWith(
-        mockSuccessEvent
-      );
-    });
-
-    it('should not open modal on event CartAddEntrySuccessEvent in case toggle addedToCartDialogDrivenBySuccessEvent is inactive', () => {
-      spyOn(featureConfigService, 'isEnabled').and.returnValue(false);
-      listener = TestBed.inject(AddedToCartDialogEventListener);
-      spyOn(listener as any, 'openModalAfterSuccess').and.stub();
-      mockEventSuccessStream$.next(mockSuccessEvent);
-      expect(listener['openModalAfterSuccess']).not.toHaveBeenCalled();
-    });
-
-    it('should open modal on event CartUiEventAddToCart in case toggle addedToCartDialogDrivenBySuccessEvent is inactive', () => {
-      spyOn(featureConfigService, 'isEnabled').and.returnValue(false);
+    it('should open modal on event CartUiEventAddToCart', () => {
       listener = TestBed.inject(AddedToCartDialogEventListener);
       spyOn(listener as any, 'openModal').and.stub();
       mockEventStream$.next(mockEvent);
       expect(listener['openModal']).toHaveBeenCalledWith(mockEvent);
     });
 
-    it('should not open modal on event CartUiEventAddToCart in case toggle addedToCartDialogDrivenBySuccessEvent is active', () => {
-      spyOn(featureConfigService, 'isEnabled').and.returnValue(true);
-      listener = TestBed.inject(AddedToCartDialogEventListener);
-      spyOn(listener as any, 'openModal').and.stub();
-      mockEventStream$.next(mockEvent);
-      expect(listener['openModal']).not.toHaveBeenCalled();
-    });
 
-    it('should close modal on fail event in case toggle is inactive', () => {
-      spyOn(featureConfigService, 'isEnabled').and.returnValue(false);
+    it('should close modal on fail event', () => {
       listener = TestBed.inject(AddedToCartDialogEventListener);
       spyOn(listener as any, 'closeModal').and.stub();
       mockEventStream$.next(mockFailEvent);
@@ -143,75 +113,12 @@ describe('AddedToCartDialogEventListener', () => {
     });
   });
 
-  describe('openModalAfterSuccess', () => {
-    beforeEach(() => {
-      listener = TestBed.inject(AddedToCartDialogEventListener);
-      spyOn(launchDialogService, 'openDialog').and.callThrough();
-    });
-
-    it('should retrieve pickup store name from point of service of new entry added to the cart', () => {
-      listener['openModalAfterSuccess'](mockSuccessEvent);
-      expect(launchDialogService.openDialog).toHaveBeenCalledWith(
-        LAUNCH_CALLER.ADDED_TO_CART,
-        undefined,
-        undefined,
-        {
-          productCode: PRODUCT_CODE,
-          quantity: QUANTITY,
-          pickupStoreName: STORE_NAME_FROM_POS,
-          addedEntryWasMerged: true,
-        }
-      );
-    });
-
-    it('should forward pickup store name as undefined in case no point of service provided in success event', () => {
-      entry.deliveryPointOfService = undefined;
-      listener['openModalAfterSuccess'](mockSuccessEvent);
-      expect(launchDialogService.openDialog).toHaveBeenCalledWith(
-        LAUNCH_CALLER.ADDED_TO_CART,
-        undefined,
-        undefined,
-        {
-          productCode: PRODUCT_CODE,
-          quantity: QUANTITY,
-          pickupStoreName: undefined,
-          addedEntryWasMerged: true,
-        }
-      );
-    });
-  });
-
   describe('closeModal', () => {
     it('should close the add to cart dialog', () => {
       listener = TestBed.inject(AddedToCartDialogEventListener);
       spyOn(launchDialogService, 'closeDialog').and.stub();
       listener['closeModal']('reason');
       expect(launchDialogService.closeDialog).toHaveBeenCalledWith('reason');
-    });
-  });
-
-  describe('calculateEntryWasMerged', () => {
-    it('should return true in case no quantityAdded is present (which happens if no stock is available)', () => {
-      mockSuccessEvent.quantityAdded = undefined;
-      expect(listener['calculateEntryWasMerged'](mockSuccessEvent)).toBe(true);
-    });
-
-    it('should return true in case the resulting entries quantity exceeds the quantity that was added to the cart', () => {
-      mockSuccessEvent.quantityAdded = 1;
-      entry.quantity = 2;
-      expect(listener['calculateEntryWasMerged'](mockSuccessEvent)).toBe(true);
-    });
-
-    it('should return false in case the resulting entries quantity equals the quantity that was added to the cart', () => {
-      mockSuccessEvent.quantityAdded = 3;
-      entry.quantity = 3;
-      expect(listener['calculateEntryWasMerged'](mockSuccessEvent)).toBe(false);
-    });
-
-    it('should return false in case the resulting entries quantity is undefined (which can happen only in exceptional situations) ', () => {
-      mockSuccessEvent.quantityAdded = 1;
-      entry.quantity = undefined;
-      expect(listener['calculateEntryWasMerged'](mockSuccessEvent)).toBe(false);
     });
   });
 });
