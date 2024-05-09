9d5fef2d0cf2adba01e86f6567e11c831b5e905b
Krzysztof Platis <platonn.git@gmail.com>
2024-05-09 17:28:21 +0200

    feat: create `BEFORE_CMS_PAGE_GUARD` injection token allowing for custom pre-`CmsPageGuard` logic (#18781)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: anjana-bl <anjana.b.l@sap.com>
    Co-authored-by: Paweł Fraś <fras.pawel@yahoo.com>

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -290,6 +290,7 @@ if (environment.requestedDeliveryDate) {
         a11yUnitsListKeyboardControls: true,
         a11yCartItemsLinksStyles: true,
         a11yFocusableCarouselControls: true,
+        cmsGuardsServiceUseGuardsComposer: true,
         cartQuickOrderRemoveListeningToFailEvent: true,
       };
       return appFeatureToggles;
