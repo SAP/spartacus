92dd7fc0dda74c2fa2a5cd77cfd4cf429f5eeb8c
Akshay Tiwari <tiwariakshay0@gmail.com>
2024-05-15 10:55:27 +0530

    Revert "chore: Revert "CXSPA-6816 : Estimated delivery date UI" (#18845)"
    This reverts commit 2e318c3490142ffec2d1dfce0729806bb93de4a4.

---
+++ feature-libs/estimated-delivery-date/assets/translations/en/index.ts
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import estimatedDeliveryDate from './estimatedDeliveryDate.json';
+
+export const en = {
+  estimatedDeliveryDate,
+};
