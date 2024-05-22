e1aa80f36355e0be475f7aa1ffe94b005377cdc1
LindaSAP <wpbaobao@gmail.com>
2024-05-22 15:06:36 +0800

    add code

--- run-spartacus-cust-e2es.sh
+++ run-spartacus-cust-e2es.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # uncomment below for local testing
-# export ENDPOINT_URL_PUBLIC_SPARTACUS=https://spartacusstore.cg79x9wuu9-eccommerc1-p1-public.model-t.myhybris.cloud
-# export ENDPOINT_URL_PUBLIC_API=https://api.cg79x9wuu9-eccommerc1-p1-public.model-t.myhybris.cloud
+export ENDPOINT_URL_PUBLIC_SPARTACUS=https://spartacusstore.cg79x9wuu9-eccommerc1-d3-public.model-t.myhybris.cloud
+export ENDPOINT_URL_PUBLIC_API=https://api.cg79x9wuu9-eccommerc1-d3-public.model-t.myhybris.cloud
 
 CYPRESS_ROOT_FOLDER="cypress/e2e"
 CYPRESS_OTP_FOLDER="$CYPRESS_ROOT_FOLDER/regression/user_access"
 CYPRESS_ACCESSIBILITY_FOLDER="$CYPRESS_ROOT_FOLDER/accessibility"
 
-export E2ES_TO_RUN="$CYPRESS_CUST_FOLDER/otp-login.e2e-flaky.cy.ts,$CYPRESS_B2C_FOLDER/homepage/homepage.core-e2e.cy.ts"
+export E2ES_TO_RUN="$CYPRESS_OTP_FOLDER/otp-login.e2e-flaky.cy.ts"
 
 # install cypress standlone dependencies
 (cd projects/storefrontapp-e2e-cypress && npm install)
