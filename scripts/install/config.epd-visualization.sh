913636b2aed6c694c9683d97a72b6dd7bc8e64cf
Sam Garland <sam.garland@sap.com>
2024-05-30 10:45:09 +1200

    Revert "Use a working CCv2 URL for epd-visualization"
    The CCv2 server managed by PLM visualization
    is working again.
    
    This reverts commit 637aecfae18b596471d3c615a3e8d17aba60ccd5.

--- scripts/install/config.epd-visualization.sh
+++ scripts/install/config.epd-visualization.sh
@@ -4,7 +4,7 @@ ADD_EPD_VISUALIZATION=true
 
 # This CCv2 server has been initialized with the 'epdvisualizationspartacussampledata' addon
 # which creates 'electronics-epdvisualization-spa' and 'powertools-epdvisualization-spa' sites.
-BACKEND_URL=https://spa-epd-visualization.demo.hybris.com
+BACKEND_URL=https://api.cp96avkh5f-integrati1-d1-public.model-t.cc.commerce.ondemand.com
 
 # Only powertools-epdvisualization-spa contains @spartacus/epd-visualization sample data at this point.
 # Sample data may be added to electronics-epdvisualization-spa in the future.
