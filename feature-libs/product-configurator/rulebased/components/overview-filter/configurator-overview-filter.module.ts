d40c31412f2c9fa06ef87e2860d8c05c54267264
Larisa Staroverova <larisa.staroverova@sap.com>
2024-05-29 16:28:44 +0200

    Remove unneccessary import

--- feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.module.ts
+++ feature-libs/product-configurator/rulebased/components/overview-filter/configurator-overview-filter.module.ts
@@ -7,7 +7,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FeaturesConfigModule, I18nModule } from '@spartacus/core';
+import { I18nModule } from '@spartacus/core';
 import { ConfiguratorOverviewFilterBarModule } from '../overview-filter-bar/configurator-overview-filter-bar.module';
 import { ConfiguratorOverviewFilterComponent } from './configurator-overview-filter.component';
 
@@ -18,7 +18,6 @@ import { ConfiguratorOverviewFilterComponent } from './configurator-overview-fil
     FormsModule,
     ReactiveFormsModule,
     ConfiguratorOverviewFilterBarModule,
-    FeaturesConfigModule,
   ],
   declarations: [ConfiguratorOverviewFilterComponent],
   exports: [ConfiguratorOverviewFilterComponent],
