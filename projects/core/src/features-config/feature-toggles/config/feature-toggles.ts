b73feab1addcf34318f14e12b22d3e7e7fe6c644
Radhep Sabapathipillai <34665674+RadhepS@users.noreply.github.com>
2024-06-17 12:24:17 -0400

    feat: remove add to cart button when price is not defined (#18928)
    closes: https://jira.tools.sap/browse/CXSPA-7284

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -10,6 +10,12 @@
 // Thanks to that, customers using a property that was recently removed, will know they have to adapt their code.
 export interface FeatureTogglesInterface {
   /**
+   * In 'ProductListItemComponent' and 'ProductGridItemComponent', it hides the 'Add to cart' button
+   * when a product does not have a defined price or its purchasable field is set to false
+   */
+  shouldHideAddToCartForUnpurchasableProducts?: boolean;
+
+  /**
    * In `FormErrorsComponent` it uses more descriptive validation error messages
    * in all UI form fields existing before v2211.25.
    *
@@ -333,6 +339,7 @@ export interface FeatureTogglesInterface {
 
 export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   formErrorsDescriptiveMessages: true,
+  shouldHideAddToCartForUnpurchasableProducts: false,
   useExtractedBillingAddressComponent: false,
   showBillingAddressInDigitalPayments: false,
   showDownloadProposalButton: false,
