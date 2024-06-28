58c3a7d71eff5e81a312d04a1aceccb253a91874
Krzysztof Platis <platonn.git@gmail.com>
2024-06-28 22:37:32 +0200

    fix(UpdatePriceSummaryFail): constructors in wrong order, using null | undefinied instead of any in deprecated constructor

--- feature-libs/product-configurator/rulebased/core/state/actions/configurator.action.ts
+++ feature-libs/product-configurator/rulebased/core/state/actions/configurator.action.ts
@@ -223,19 +223,17 @@ export class UpdatePriceSummary extends StateUtils.EntityLoadAction {
 export class UpdatePriceSummaryFail extends StateUtils.EntityFailAction {
   readonly type = UPDATE_PRICE_SUMMARY_FAIL;
 
+  constructor(payload: { ownerKey: string; error: ActionErrorProperty });
   /**
    * @deprecated Please use the `error` parameter with a non-null, non-undefined value.
    *             Support for `null` or `undefined` will be removed in future versions,
    *             along with the feature toggle `ssrStrictErrorHandlingForHttpAndNgrx`.
    */
-  constructor(payload: { ownerKey: string; error: null | undefined });
   constructor(
     // eslint-disable-next-line @typescript-eslint/unified-signatures -- needed to deprecate only the old constructor
-    payload: {
-      ownerKey: string;
-      error: ActionErrorProperty;
-    }
+    payload: { ownerKey: string; error: any }
   );
+
   constructor(public payload: { ownerKey: string; error: any }) {
     super(CONFIGURATOR_DATA, payload.ownerKey, payload.error);
   }
