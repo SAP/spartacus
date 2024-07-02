b7f17a67275cf08e1e946cc0918c9c768fd5f28b
Paweł Fraś <fras.pawel@yahoo.com>
2024-07-02 08:40:00 +0200

    Update core-libs/setup/ssr/error-handling/multi-error-handlers/propagating-to-server-error-handler.ts
    Co-authored-by: Krzysztof Platis <platonn.git@gmail.com>

--- core-libs/setup/ssr/error-handling/multi-error-handlers/propagating-to-server-error-handler.ts
+++ core-libs/setup/ssr/error-handling/multi-error-handlers/propagating-to-server-error-handler.ts
@@ -9,7 +9,16 @@ import { MultiErrorHandler } from '@spartacus/core';
 import { PROPAGATE_ERROR_TO_SERVER } from '../error-response/propagate-error-response';
 
 /**
- * Error handler responsible for sending an error to the incoming request in the server.
+ * Propagates the given error object to the higher layer in the server.
+ *
+ * It's meant to propagate errors for example to ExpressJS layer when using SSR
+ * or to a Prerendering Worker when using Server Prerendering.
+ * Currently, it's provided OOTB only in SSR (not prerendering), in the `CxCommonEngine` class.
+ *
+ * Note: We need it until Angular implements a proper propagation of async errors 
+ * from an app to the the higher layer in the server.
+ * For more, see the Angular issue https://github.com/angular/angular/issues/33642
+ *
  * Intended to be used as part of a multi-error handler strategy.
  *
  * @see MultiErrorHandler
