dcacc99724be6516c16b90910892ab44853dfb0c
Artur Lasocha <artur.lasocha@sap.com>
2024-07-05 13:49:08 +0200

    CXCDS-12024 fix imports

--- integration-libs/cds/assets/index.ts
+++ integration-libs/cds/assets/index.ts
@@ -4,4 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './translations/translations';
+import { TranslationChunksConfig, TranslationResources } from '@spartacus/core';
+import {
+  cdsTranslations as originaCdsTranslations,
+  cdsTranslationChunksConfig as originalCdsTranslationChunksConfig,
+} from '@spartacus/cds/assets';
+
+/** @deprecated  Use @spartacus/cds/assets instead */
+export const cdsTranslations: TranslationResources = originaCdsTranslations;
+
+/** @deprecated  Use @spartacus/cds/assets instead */
+export const cdsTranslationChunksConfig: TranslationChunksConfig =
+  originalCdsTranslationChunksConfig;
