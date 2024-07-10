849ee94d94f93c6525f6b2234cc70f156a956be7
Hakwoo Kim <hakwoo.kim@sap.com>
2024-07-10 07:48:13 -0400

    update index

--- projects/core/src/site-theme/index.ts
+++ projects/core/src/site-theme/index.ts
@@ -7,10 +7,4 @@
 export * from './site-theme.module';
 export * from './config/site-theme-config';
 export * from './config/default-site-theme-config';
-//export * from './connectors/index';
 export * from './facade/index';
-//export * from './http-interceptors/anonymous-consents-interceptor';
-// export * from './services/index';
-// export * from './store/actions/index';
-// export * from './store/anonymous-consents-state';
-// export * from './store/selectors/index';
