34b9cc44bdb4c20c7b5aebe053ad0ba682fee6b8
PioBar <72926984+Pio-Bar@users.noreply.github.com>
2024-07-15 11:12:56 +0200

    fix: (CXSPA-1094) - Store finder alerts (#18961)
    Co-authored-by: Miguel Estrada <15113219+developpeurweb@users.noreply.github.com>

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -330,12 +330,12 @@ if (environment.cpq) {
         a11yDisabledCouponAndQuickOrderActionButtonsInsteadOfRequiredFields:
           true,
         a11yFacetsDialogFocusHandling: true,
+        a11yStoreFinderAlerts: true,
         a11yFormErrorMuteIcon: true,
         a11yCxMessageFocus: true,
         occCartNameAndDescriptionInHttpRequestBody: true,
         a11yLinkBtnsToTertiaryBtns: true,
         cmsBottomHeaderSlotUsingFlexStyles: true,
-        a11yStoreFinderSearchboxBloat: true,
       };
       return appFeatureToggles;
     }),
