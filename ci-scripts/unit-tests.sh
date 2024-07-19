26abcc5fab52380d54ad921654fe51e9564b92c9
Radhep Sabapathipillai <radhep.sabapathipillai@sap.com>
2024-07-19 09:14:51 -0400

    update

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -15,20 +15,34 @@ function run_affected_unit_tests {
     npx nx affected --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
 }
 
-function run_all_unit_tests {
+function run_jasmine_tests {
     echo "Running JASMINE unit tests and code coverage for ALL libraries"
     npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JEST" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+}
 
+function run_jest_tests {
     echo "Running JEST (mostly schematics) unit tests and code coverage for ALL libraries"
     npx nx run-many --all --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
 }
 
-if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
-    if [[ "${GITHUB_HEAD_REF}" == epic/* ]]; then
+case $1 in
+  --jest)
+    run_jest_tests
+    ;;
+  --jasmine)
+    run_jasmine_tests
+    ;;
+  *)
+    if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+        if [[ "${GITHUB_HEAD_REF}" == epic/* ]]; then
+            run_all_unit_tests
+        else 
+            run_affected_unit_tests
+        fi
+    elif [[ $1 == 'build' ]]; then
+        build_libs
+    else
         run_all_unit_tests
-    else 
-        run_affected_unit_tests
     fi
-else
-    run_all_unit_tests
-fi
+    ;;
+esac
