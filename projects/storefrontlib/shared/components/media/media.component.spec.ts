02f8ac5c92acc20baa897859915576c250a87ccd
rmch91 <r.chygryn@sap.com>
2024-07-25 14:44:38 +0200

    unit tests for media component

--- projects/storefrontlib/shared/components/media/media.component.spec.ts
+++ projects/storefrontlib/shared/components/media/media.component.spec.ts
@@ -1,12 +1,48 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {
+  Directive,
+  Input,
+  Pipe,
+  PipeTransform,
+  TemplateRef,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+
+import { InjectionToken } from '@angular/core';
 import { MediaComponent } from './media.component';
 import { ImageLoadingStrategy, Media } from './media.model';
 import { MediaService } from './media.service';
 import { USE_LEGACY_MEDIA_COMPONENT } from './media.token';
 
+export const IS_CONFIGURABLE_MEDIA_COMPONENT = new InjectionToken<boolean>(
+  'IS_CONFIGURABLE_MEDIA_COMPONENT'
+);
+
 const mediaUrl = 'mockProductImageUrl.jpg';
+
+@Directive({
+  selector: '[cxFeature]',
+})
+export class MockFeatureDirective {
+  protected templateRef = inject(TemplateRef<any>);
+  protected viewContainer = inject(ViewContainerRef);
+  protected isConfigurableMediaComponent = inject(
+    IS_CONFIGURABLE_MEDIA_COMPONENT
+  );
+
+  @Input() set cxFeature(_feature: string) {
+    const featureIncludesExclamation = _feature.toString().includes('!');
+    const shouldCreateView =
+      (featureIncludesExclamation && !this.isConfigurableMediaComponent) ||
+      (!featureIncludesExclamation && this.isConfigurableMediaComponent);
+
+    if (shouldCreateView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+  }
+}
 
 @Pipe({
   name: 'cxMediaSources',
@@ -48,6 +84,19 @@ class MockMediaService {
       alt: undefined,
     };
   }
+  getMediaForPictureElement(media: any): Media | undefined {
+    return {
+      src: media ? media.product.url : undefined,
+      srcset: this.srcset,
+      alt: undefined,
+      sources: [
+        {
+          srcset: 'test.url',
+          media: '',
+        },
+      ],
+    };
+  }
   getMissingImage() {
     return {
       src: 'missing.jpg',
@@ -66,34 +115,92 @@ const mockMissingImageContainer = undefined;
 
 function configureTestingModule(
   mockMediaService: MockMediaService,
-  isLegacy: boolean = true
+  isLegacy: boolean = true,
+  isConfigurableMediaComponent = false
 ): void {
   TestBed.configureTestingModule({
-    declarations: [MediaComponent, MockMediaSourcesPipe],
+    declarations: [MediaComponent, MockMediaSourcesPipe, MockFeatureDirective],
     providers: [
       { provide: MediaService, useValue: mockMediaService },
       {
         provide: USE_LEGACY_MEDIA_COMPONENT,
         useValue: isLegacy,
       },
+      {
+        provide: IS_CONFIGURABLE_MEDIA_COMPONENT,
+        useValue: isConfigurableMediaComponent,
+      },
     ],
   }).compileComponents();
 }
 
-function createComponent() {
+function createComponent(usePictureElement = false) {
   const service = TestBed.inject(MediaService);
   const fixture = TestBed.createComponent(MediaComponent);
   const component = fixture.componentInstance;
+  const getMediaSpy = spyOn(service, 'getMedia').and.callThrough();
+  const getMediaForPictureElementSpy = spyOn(
+    service,
+    'getMediaForPictureElement'
+  ).and.callThrough();
 
   component.container = mockImageContainer;
+
+  if (usePictureElement) {
+    component.usePictureElement = usePictureElement;
+  }
 
   component.ngOnChanges();
   fixture.detectChanges();
 
-  return { service, fixture, component };
+  return {
+    service,
+    fixture,
+    component,
+    getMediaSpy,
+    getMediaForPictureElementSpy,
+  };
 }
 
 describe('MediaComponent', () => {
+  describe('with enabled useMediaComponentWithConfigurableMediaQueries', () => {
+    it('should have picture element if usePictureElement is true', () => {
+      configureTestingModule(new MockMediaService('srcset'), false, true);
+      const { fixture } = createComponent(true);
+
+      const picture = fixture.debugElement.query(By.css('picture'));
+
+      expect(picture).not.toBeNull();
+    });
+
+    it('should not have picture element if usePictureElement is false', () => {
+      configureTestingModule(new MockMediaService('srcset'), false, true);
+      const { fixture } = createComponent(false);
+
+      const picture = fixture.debugElement.query(By.css('picture'));
+
+      expect(picture).toBeNull();
+    });
+
+    it('should call getMedia() method from service if usePictureElement is false', () => {
+      configureTestingModule(new MockMediaService('srcset'), false, true);
+      const { getMediaSpy, getMediaForPictureElementSpy } =
+        createComponent(false);
+
+      expect(getMediaForPictureElementSpy).not.toHaveBeenCalled();
+      expect(getMediaSpy).toHaveBeenCalled();
+    });
+
+    it('should call getMediaForPictureElement() method from service if usePictureElement is true', () => {
+      configureTestingModule(new MockMediaService('srcset'), false, true);
+      const { getMediaForPictureElementSpy, getMediaSpy } =
+        createComponent(true);
+
+      expect(getMediaForPictureElementSpy).toHaveBeenCalled();
+      expect(getMediaSpy).not.toHaveBeenCalled();
+    });
+  });
+
   it('should create', () => {
     configureTestingModule(new MockMediaService());
     const { component } = createComponent();
@@ -177,14 +284,14 @@ describe('MediaComponent', () => {
 
   it('should have is-missing class when there is no image', () => {
     configureTestingModule(new MockMediaService());
-    const { service, fixture, component } = createComponent();
+    const { fixture, component, getMediaSpy } = createComponent();
 
     component.container = mockImageContainer;
 
     component.ngOnChanges();
     fixture.detectChanges();
 
-    spyOn(service, 'getMedia').and.returnValue(null);
+    getMediaSpy.and.returnValue(null);
     component.container = mockMissingImageContainer;
 
     component.ngOnChanges();
