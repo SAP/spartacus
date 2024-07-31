85a7f1e71b6786e382634e86399c0b82037671a7
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-07-31 13:26:08 +0200

    Update product-multi-dimensional-selector-root.module.ts

--- feature-libs/product-multi-dimensional/selector/root/product-multi-dimensional-selector-root.module.ts
+++ feature-libs/product-multi-dimensional/selector/root/product-multi-dimensional-selector-root.module.ts
@@ -6,12 +6,12 @@
 
 import { NgModule } from '@angular/core';
 import { provideDefaultConfigFactory } from '@spartacus/core';
-import { PRODUCT_MULTI_DIMENSIONAL_FEATURE } from './feature-name';
+import { PRODUCT_MULTI_DIMENSIONAL_SELECTOR_FEATURE } from './feature-name';
 
 export function defaultProductMultiDimensionalSelectorComponentsConfig() {
   return {
     featureModules: {
-      [PRODUCT_MULTI_DIMENSIONAL_FEATURE]: {
+      [PRODUCT_MULTI_DIMENSIONAL_SELECTOR_FEATURE]: {
         cmsComponents: [
           'ProductVariantSelectorComponent', // TBD trigger
           'ProductVariantMultiDimensionalSelectorComponent',
