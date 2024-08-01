b252110ce5ee0af4ba1f947dc0537732e1c021fd
Grace Dong <gracedong1102@163.com>
2024-08-01 17:19:52 +0800

    CXMC-13000: draft

--- feature-libs/user/account/components/login-form/auth.config.ts
+++ feature-libs/user/account/components/login-form/auth.config.ts
@@ -7,7 +7,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'https://fidm.eu1.gigya.com/oidc/op/v1.0/4_l3dMCBwsQX6XopODQlWG7A',
+  issuer: 'https://fidm.eu1.gigya.com/oidc/op/v1.0/4_haAyXsKhFEupcUCQ9UPizw',
   redirectUri: window.location.origin + '/electronics-spa/en/USD/login',
   clientId: 'FeWB0V0Opi2hEL-T21DlUuEO',
   responseType: 'code',
--- feature-libs/user/account/components/login-form/login-form.component.ts
+++ feature-libs/user/account/components/login-form/login-form.component.ts
@@ -4,27 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { filter, map } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { LoginFormComponentService } from './login-form-component.service';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { authCodeFlowConfig } from './auth.config';
 import { AuthService, RoutingService } from '@spartacus/core';
-import { Location } from '@angular/common';
 
-declare var gigya: any;
 @Component({
   selector: 'cx-login-form',
-  templateUrl: './login-form.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent implements OnInit{
+  protected LoggedIn: boolean = false;
+// export class LoginFormComponent{
   constructor(
     protected authService: AuthService,
     protected routingService: RoutingService,
     protected service: LoginFormComponentService,
-    protected location: Location,
-    private oauthService: OAuthService
+    private oauthService: OAuthService,
+    protected routing: RoutingService,
   ) {
     this.oauthService.configure(authCodeFlowConfig);
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
@@ -34,30 +33,18 @@ export class LoginFormComponent implements OnInit{
         this.oauthService.loadUserProfile();
         this.service.updateToken(this.oauthService.getAccessToken());
       });
-
-    gigya.accounts.addEventHandlers({
-      onLogin: (account: any) => {
-        this.service.onCDCLoginSuccess(account);
-      },
-    });
   }
-
   ngOnInit(): void {
-    this.authService
-      .isUserLoggedIn()
-      .pipe(
-        map((isLoggedIn) => {
-          if (isLoggedIn) {
-            this.routingService.go({ cxRoute: 'home' });
-            // TBD
-            // this.location.back();
-          } else {
-            this.startPKCEflow();
-            // go back to the previous page after getting the token
-            // this.location.back();
-          }
-        })
-      ).subscribe();
+      this.authService.isUserLoggedIn().subscribe(isLoggedIn => {
+        if(isLoggedIn){
+          console.log('User is logged in: '+isLoggedIn);
+          // this.routing.go('/');
+        }
+        else{
+          console.log('User is logged in: '+isLoggedIn);
+          // this.startPKCEflow();
+        }
+      });
   }
 
   startPKCEflow(): void {
