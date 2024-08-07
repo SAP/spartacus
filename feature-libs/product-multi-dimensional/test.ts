8cb2a3cd9c3294e7c452d735514a1919b64d65c2
kpawelczak <42094017+kpawelczak@users.noreply.github.com>
2024-08-07 10:53:13 +0200

    Update test.ts

--- feature-libs/product-multi-dimensional/test.ts
+++ feature-libs/product-multi-dimensional/test.ts
@@ -3,3 +3,17 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: false },
+  }
+);
