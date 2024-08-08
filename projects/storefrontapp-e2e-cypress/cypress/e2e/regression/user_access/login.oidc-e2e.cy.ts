81a33338b42bf707a29811967b5718681b88cfef
Grace Dong <gracedong1102@163.com>
2024-08-08 12:55:51 +0800

    CXMC-13000:add e2e test with oidc login flow

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/user_access/login.oidc-e2e.cy.ts
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: 2024 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as checkout from '../../../helpers/checkout-flow';
+
+describe('Login', () => {
+  const gigyaBaseUrl = 'https://616654787756.eu1.my.gigya-ext.com';
+
+  it('should login successfully and redirect to spatarcus storefront home page', () => {
+    cy.visit('/login');
+    cy.get('.cx-spinner').should('be.visible');
+    cy.origin(gigyaBaseUrl, () => {
+      cy.get('[placeholder="Email *"]').type('grace.dongcdc9@sap.com');
+      cy.get('[placeholder="Password *"]').type('Pw4all.');
+      cy.get('[type="submit"][data-screenset-roles="instance"]').click();
+    }).then(() => {
+      cy.url().should('include', '/login');
+      cy.get('.cx-spinner').should('be.visible');
+      cy.url().should('not.include', '/login');
+      cy.get('.cx-login-greet').should('be.visible');
+      cy.get('.cx-login-greet').should('contain', 'Hi');
+      cy.get(
+        '[title="Save Big on Select Camera Accessories & Supplies"]'
+      ).should('be.visible');
+    });
+  });
+
+  it('should login successfully and redirect to previous storefront product details page', () => {
+    checkout.goToProductDetailsPage();
+    cy.visit('/login');
+    cy.get('.cx-spinner').should('be.visible');
+    cy.origin(gigyaBaseUrl, () => {
+      cy.get('[placeholder="Email *"]').type('grace.dongcdc9@sap.com');
+      cy.get('[placeholder="Password *"]').type('Pw4all.');
+      cy.get('[type="submit"][data-screenset-roles="instance"]').click();
+    }).then(() => {
+      cy.url().should('include', '/login');
+      cy.get('.cx-spinner').should('be.visible');
+      cy.get('.cx-login-greet').should('be.visible');
+      cy.get('.cx-login-greet').should('contain', 'Hi');
+      cy.get('cx-product-intro').should('be.visible');
+    });
+  });
+});
