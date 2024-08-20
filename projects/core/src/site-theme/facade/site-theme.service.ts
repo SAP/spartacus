526df602b811c5eede6da1115b1de85ed05c4ad5
Hakwoo Kim <hakwoo.kim@sap.com>
2024-08-20 08:50:44 -0400

    revert change for subscription in siteThemeService

--- projects/core/src/site-theme/facade/site-theme.service.ts
+++ projects/core/src/site-theme/facade/site-theme.service.ts
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable, OnDestroy, inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { filter, map, take, tap } from 'rxjs/operators';
 
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { SiteTheme } from '../../model/misc.model';
 import { SiteThemeConfig } from '../config/site-theme-config';
@@ -18,15 +18,10 @@ import { SiteThemeSelectors } from '../store/selectors';
 import { SiteThemeActions } from '../store/actions';
 
 @Injectable()
-export class SiteThemeService implements OnDestroy {
+export class SiteThemeService {
   private _isInitialized = false;
   protected store = inject(Store<StateWithSiteTheme>);
   protected config = inject(SiteThemeConfig);
-  protected subscription = new Subscription();
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 
   getAll(): Observable<SiteTheme[]> {
     return this.store.pipe(
@@ -55,7 +50,7 @@ export class SiteThemeService implements OnDestroy {
    */
   // TODO(#8153): If we move this._isInitialized into an RxJS stream, it may create a race condition between SiteThemePersistenceService:onRead and SiteThemeInitializer:this.setFallbackValue().
   setActive(className: string): void {
-    this.subscription = this.isValidTheme(className)
+    this.isValidTheme(className)
       .pipe(filter(Boolean), take(1))
       .subscribe(() => {
         this.store.dispatch(new SiteThemeActions.SetActiveSiteTheme(className));
