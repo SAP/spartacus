4f1bd46aaddf893689a062a5f60df280a390f65d
Grace Dong <gracedong1102@163.com>
2024-08-28 17:34:56 +0800

    CXMC-8184: test

--- feature-libs/user/account/root/configs/oidc-config-initializer.ts
+++ feature-libs/user/account/root/configs/oidc-config-initializer.ts
@@ -4,27 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {
   AuthConfig,
-  AuthLibConfig,
-  BASE_SITE_CONTEXT_ID,
   BaseSiteService,
   ConfigInitializer,
-  ConfigInitializerService,
 } from '@spartacus/core';
-import { Observable, lastValueFrom } from 'rxjs';
-import { map, switchMap, take, tap } from 'rxjs/operators';
+import { defaultAuthConfig } from 'projects/core/src/auth/user-auth/config/default-auth-config';
+import { Observable, from, lastValueFrom } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class CdcConfigInitializer implements ConfigInitializer {
   readonly scopes = ['authentication'];
   readonly configFactory = () => lastValueFrom(this.resolveConfig());
-
-  constructor(
-    protected baseSiteService: BaseSiteService,
-    protected configInit: ConfigInitializerService
-  ) {}
+  protected baseSiteService = inject(BaseSiteService);
 
   /**
    * Emits the Auth config basing on the current base site data.
@@ -32,45 +26,34 @@ export class CdcConfigInitializer implements ConfigInitializer {
    * Completes after emitting the value.
    */
   protected resolveConfig(): Observable<AuthConfig> {
-    return this.configInit.getStable('context').pipe(
-      switchMap((config) => {
-        // todo: get uid for current site
-        const siteUid = config?.context?.[BASE_SITE_CONTEXT_ID]?.[0];
-
-        return this.baseSiteService.get(siteUid).pipe(
-          tap((baseSite) => {
-            if (!baseSite) {
-              throw new Error(
-                `Error: Cannot get base site config for ${siteUid}.`
-              );
-            }
-          }),
-          // todo: get auth config by site id
-          map(() => this.getAuthConfig()),
-          take(1)
-        );
+    return from(this.baseSiteService.get()).pipe(
+      take(1),
+      map((site) => {
+        if (
+          site?.cdcSiteConfig &&
+          site?.uid &&
+          site.baseStore?.defaultCurrency &&
+          site.defaultLanguage
+        ) {
+          const defaultCurrency = site.baseStore.defaultCurrency.isocode;
+          const defaultLanguage = site.defaultLanguage.isocode;
+          const result: AuthConfig = {
+            authentication: {
+              client_id: site.cdcSiteConfig.oidcRpClientId,
+              OAuthLibConfig: {
+                issuer: site.cdcSiteConfig.oidcOpIssuerURI,
+                redirectUri: `${window.location.origin}/${site.uid}/${defaultLanguage}/${defaultCurrency}/login`,
+                scope: site.cdcSiteConfig.scopes.join(' '),
+                disablePKCE: false,
+                responseType: 'code',
+              },
+            },
+          };
+          return result;
+        } else {
+          return defaultAuthConfig;
+        }
       })
     );
-  }
-
-  protected getAuthConfig(): AuthConfig {
-    // todo: get auth config from site
-    const result = {
-      authentication: {
-        clientId: 'T8ONdZ84OQW6Kud0LwM9pFC1',
-        tokenEndpoint:
-          'https://auth.pilot-demo6-p1-public.model-t.myhybris.cloud/oidc/op/v1.0/4__GcGe4z3mp-6QicQgo0Y_Q/token',
-        OAuthLibConfig: <AuthLibConfig>{
-          clientId: 'T8ONdZ84OQW6Kud0LwM9pFC1',
-          issuer:
-            'https://auth.pilot-demo6-p1-public.model-t.myhybris.cloud/oidc/op/v1.0/4__GcGe4z3mp-6QicQgo0Y_Q',
-          redirectUri: 'http://localhost:4200/electronics-spa/en/USD/login',
-          scope: 'openid profile email uid bpDisplayId bpId isB2BCustomer',
-          responseType: 'code',
-          disablePKCE: false,
-        },
-      },
-    };
-    return result;
   }
 }
