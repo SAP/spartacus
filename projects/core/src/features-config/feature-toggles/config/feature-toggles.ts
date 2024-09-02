2096708640a1d2387f31edab58a565de82b377ef
LarisaStar <61147963+Larisa-Staroverova@users.noreply.github.com>
2024-09-02 14:07:01 +0200

    chore: set feature toggle storeFrontLibCardParagraphTruncated to true (#19195)

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -504,7 +504,7 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   showPromotionsInPDP: false,
   recentSearches: false,
   pdfInvoicesSortByInvoiceDate: false,
-  storeFrontLibCardParagraphTruncated: false,
+  storeFrontLibCardParagraphTruncated: true,
   useProductCarouselBatchApi: false,
   productConfiguratorAttributeTypesV2: false,
   productConfiguratorDeltaRendering: false,
