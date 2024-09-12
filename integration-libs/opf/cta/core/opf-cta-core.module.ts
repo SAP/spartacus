fce86262f2c4c7afec5a8ae440da586d4e4d37db
Matejk00 <mateusz.kolasa@sap.com>
2024-09-12 12:56:23 +0200

    CR update

--- integration-libs/opf/cta/core/opf-cta-core.module.ts
+++ integration-libs/opf/cta/core/opf-cta-core.module.ts
@@ -5,12 +5,10 @@
  */
 
 import { NgModule } from '@angular/core';
-import { OpfCtaComponentsModule } from './components/opf-cta-components.module';
 import { OpfCtaConnector } from './connectors';
 import { facadeProviders } from './facade/facade-providers';
 
 @NgModule({
-  imports: [OpfCtaComponentsModule],
   providers: [...facadeProviders, OpfCtaConnector],
 })
 export class OpfCtaCoreModule {}
--- integration-libs/opf/cta/opf-cta.module.ts
+++ integration-libs/opf/cta/opf-cta.module.ts
@@ -5,10 +5,11 @@
  */
 
 import { NgModule } from '@angular/core';
+import { OpfCtaComponentsModule } from '@spartacus/opf/cta/components';
 import { OpfCtaCoreModule } from '@spartacus/opf/cta/core';
 import { OpfCtaOccModule } from '@spartacus/opf/cta/occ';
 
 @NgModule({
-  imports: [OpfCtaCoreModule, OpfCtaOccModule],
+  imports: [OpfCtaCoreModule, OpfCtaOccModule, OpfCtaComponentsModule],
 })
 export class OpfCtaModule {}
