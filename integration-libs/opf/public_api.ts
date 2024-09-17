afa23134d0d565399657e2fd5f072d6f603fd7ba
Matejk00 <mateusz.kolasa@sap.com>
2024-09-17 10:56:10 +0200

    Remove libs export in main lib

--- integration-libs/opf/public_api.ts
+++ integration-libs/opf/public_api.ts
@@ -4,8 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from '@spartacus/opf/base';
-export * from '@spartacus/opf/checkout';
-export * from '@spartacus/opf/cta';
-export * from '@spartacus/opf/global-functions';
-export * from '@spartacus/opf/payment';
+export {};
