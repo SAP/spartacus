a998e8aa3623dfbca67860916b2d94801679a327
KeerthiSap <81921271+KeerthiSap@users.noreply.github.com>
2024-09-18 20:33:24 +0530

    Update feature-toggles.ts

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -494,7 +494,7 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   shouldHideAddToCartForUnpurchasableProducts: false,
   useExtractedBillingAddressComponent: false,
   showBillingAddressInDigitalPayments: false,
-  showDownloadProposalButton: false,
+  showDownloadProposalButton: true,
   showPromotionsInPDP: false,
   recentSearches: false,
   pdfInvoicesSortByInvoiceDate: false,
