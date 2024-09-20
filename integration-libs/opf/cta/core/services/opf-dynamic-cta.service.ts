7c0ddface7850c970b8ce0ade28b109b854ddbcd
Florent Letendre <florent.letendre@hotmail.fr>
2024-09-20 14:02:07 -0400

    to be reverted: needed to run on vm

--- integration-libs/opf/cta/core/services/opf-dynamic-cta.service.ts
+++ integration-libs/opf/cta/core/services/opf-dynamic-cta.service.ts
@@ -113,10 +113,11 @@ export class OpfDynamicCtaService {
     }).pipe(
       take(1),
       map(({ language, currency }) => {
+        console.log('language', language);
         return [
           {
             key: 'locale',
-            value: language,
+            value: 'en-US',
           },
           {
             key: 'currency',
