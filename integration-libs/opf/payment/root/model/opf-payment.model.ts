c086948214981807b80819800b0889cd5e2797a1
Florent Letendre <florent.letendre@hotmail.fr>
2024-09-24 16:00:28 -0400

    remove def

--- integration-libs/opf/payment/root/model/opf-payment.model.ts
+++ integration-libs/opf/payment/root/model/opf-payment.model.ts
@@ -42,7 +42,6 @@ export interface GlobalOpfPaymentMethods {
   throwPaymentError?(errorOptions?: ErrorDialogOptions): void;
   startLoadIndicator?(): void;
   stopLoadIndicator?(): void;
-  scriptReady?(scriptIdentifier: string): void;
 }
 
 export interface PaymentBrowserInfo {
