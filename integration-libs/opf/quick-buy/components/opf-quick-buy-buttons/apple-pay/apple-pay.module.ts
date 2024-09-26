f8db9bf78c30ce76ced733dea6656422a4a80ad0
Florent Letendre <florent.letendre@hotmail.fr>
2024-09-26 13:51:05 -0400

    add injector

--- integration-libs/opf/quick-buy/components/opf-quick-buy-buttons/apple-pay/apple-pay.module.ts
+++ integration-libs/opf/quick-buy/components/opf-quick-buy-buttons/apple-pay/apple-pay.module.ts
@@ -7,11 +7,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { OpfQuickBuyTransactionService } from '@spartacus/opf/quick-buy/core';
 import { ApplePayComponent } from './apple-pay.component';
+import { ApplePayService } from './apple-pay.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ApplePayComponent],
   exports: [ApplePayComponent],
+  providers: [ApplePayService, OpfQuickBuyTransactionService],
 })
 export class OpfApplePayModule {}
