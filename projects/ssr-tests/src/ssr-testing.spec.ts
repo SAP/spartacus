7c37dc426f66352ece10a08fe125c45f9568ab9f
Krzysztof Platis <platonn.git@gmail.com>
2024-09-26 15:41:12 +0200

    chore: attach SSR logs to Error messages (eg. failed expect() assertions) - to improve troubleshooting of failed tests

--- projects/ssr-tests/src/ssr-testing.spec.ts
+++ projects/ssr-tests/src/ssr-testing.spec.ts
@@ -4,7 +4,6 @@ import * as HttpUtils from './utils/http.utils';
 import * as LogUtils from './utils/log.utils';
 import * as ProxyUtils from './utils/proxy.utils';
 import * as SsrUtils from './utils/ssr.utils';
-
 const BACKEND_BASE_URL: string = process.env.CX_BASE_URL || '';
 
 jest.setTimeout(SsrUtils.DEFAULT_SSR_TIMEOUT); // set timeout to at least 1x DEFAULT_SSR_TIMEOUT seconds for each test in this file to increase stability of the tests
@@ -12,10 +11,6 @@ jest.setTimeout(SsrUtils.DEFAULT_SSR_TIMEOUT); // set timeout to at least 1x DEF
 describe('SSR E2E', () => {
   let backendProxy: Server;
   const REQUEST_PATH = '/contact'; // path to the page that is less "busy" than the homepage
-
-  beforeEach(() => {
-    LogUtils.clearSsrLogFile();
-  });
 
   afterEach(async () => {
     backendProxy.close();
@@ -28,55 +23,67 @@ describe('SSR E2E', () => {
         await SsrUtils.startSsrServer();
       });
 
-      it('should receive success response with request', async () => {
-        backendProxy = await ProxyUtils.startBackendProxyServer({
-          target: BACKEND_BASE_URL,
-        });
-        const response: any =
-          await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
-        expect(response.statusCode).toEqual(200);
+      it(
+        'should receive success response with request',
+        LogUtils.attachLogsToErrors(async () => {
+          backendProxy = await ProxyUtils.startBackendProxyServer({
+            target: BACKEND_BASE_URL,
+          });
+          const response: any =
+            await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
+          expect(response.statusCode).toEqual(200);
 
-        expectLogMessages().toContainLogs([
-          `Rendering started (${REQUEST_PATH})`,
-          `Request is waiting for the SSR rendering to complete (${REQUEST_PATH})`,
-        ]);
-      });
+          expectLogMessages().toContainLogs([
+            `Rendering started (${REQUEST_PATH})`,
+            `Request is waiting for the SSR rendering to complete (${REQUEST_PATH})`,
+          ]);
+        })
+      );
 
-      it('should receive response with 404 when page does not exist', async () => {
-        backendProxy = await ProxyUtils.startBackendProxyServer({
-          target: BACKEND_BASE_URL,
-        });
-        const response = await HttpUtils.sendRequestToSsrServer(
-          REQUEST_PATH + 'not-existing-page'
-        );
-        expect(response.statusCode).toEqual(404);
-      });
+      it(
+        'should receive response with 404 when page does not exist',
+        LogUtils.attachLogsToErrors(async () => {
+          backendProxy = await ProxyUtils.startBackendProxyServer({
+            target: BACKEND_BASE_URL,
+          });
+          const response = await HttpUtils.sendRequestToSsrServer(
+            REQUEST_PATH + 'not-existing-page'
+          );
+          expect(response.statusCode).toEqual(404);
+        })
+      );
 
-      it('should receive response with status 404 if HTTP error occurred when calling cms/pages API URL', async () => {
-        backendProxy = await ProxyUtils.startBackendProxyServer({
-          target: BACKEND_BASE_URL,
-          callback: (proxyRes, req) => {
-            if (req.url?.includes('cms/pages')) {
-              proxyRes.statusCode = 404;
-            }
-          },
-        });
-        const response = await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
-        expect(response.statusCode).toEqual(404);
-      });
+      it(
+        'should receive response with status 404 if HTTP error occurred when calling cms/pages API URL',
+        LogUtils.attachLogsToErrors(async () => {
+          backendProxy = await ProxyUtils.startBackendProxyServer({
+            target: BACKEND_BASE_URL,
+            callback: (proxyRes, req) => {
+              if (req.url?.includes('cms/pages')) {
+                proxyRes.statusCode = 404;
+              }
+            },
+          });
+          const response = await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
+          expect(response.statusCode).toEqual(404);
+        })
+      );
 
-      it('should receive response with status 500 if HTTP error occurred when calling other than cms/pages API URL', async () => {
-        backendProxy = await ProxyUtils.startBackendProxyServer({
-          target: BACKEND_BASE_URL,
-          callback: (proxyRes, req) => {
-            if (req.url?.includes('cms/components')) {
-              proxyRes.statusCode = 404;
-            }
-          },
-        });
-        const response = await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
-        expect(response.statusCode).toEqual(500);
-      });
+      it(
+        'should receive response with status 500 if HTTP error occurred when calling other than cms/pages API URL',
+        LogUtils.attachLogsToErrors(async () => {
+          backendProxy = await ProxyUtils.startBackendProxyServer({
+            target: BACKEND_BASE_URL,
+            callback: (proxyRes, req) => {
+              if (req.url?.includes('cms/components')) {
+                proxyRes.statusCode = 404;
+              }
+            },
+          });
+          const response = await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
+          expect(response.statusCode).toEqual(500);
+        })
+      );
     });
 
     describe('With caching enabled', () => {
@@ -86,7 +93,7 @@ describe('SSR E2E', () => {
 
       it(
         'should take the response from cache for the next request if previous render succeeded',
-        async () => {
+        LogUtils.attachLogsToErrors(async () => {
           backendProxy = await ProxyUtils.startBackendProxyServer({
             target: BACKEND_BASE_URL,
           });
@@ -102,13 +109,13 @@ describe('SSR E2E', () => {
           response = await HttpUtils.sendRequestToSsrServer(REQUEST_PATH);
           expect(response.statusCode).toEqual(200);
           expectLogMessages().toContain(`Render from cache (${REQUEST_PATH})`);
-        },
+        }),
         2 * SsrUtils.DEFAULT_SSR_TIMEOUT // increase timeout for this test as it calls the SSR server twice
       );
 
       it(
         'should render for the next request if previous render failed',
-        async () => {
+        LogUtils.attachLogsToErrors(async () => {
           backendProxy = await ProxyUtils.startBackendProxyServer({
             target: BACKEND_BASE_URL,
             callback: (proxyRes, req) => {
@@ -126,7 +133,7 @@ describe('SSR E2E', () => {
           expectLogMessages().not.toContain(
             `Render from cache (${REQUEST_PATH})`
           );
-        },
+        }),
         2 * SsrUtils.DEFAULT_SSR_TIMEOUT // increase timeout for this test as it calls the SSR server twice
       );
     });
--- projects/ssr-tests/src/utils/log.utils.ts
+++ projects/ssr-tests/src/utils/log.utils.ts
@@ -32,9 +32,9 @@ export function getLogText(): string {
 }
 
 /**
- * Reads log and returns messages as string array.
+ * Reads log and returns them as string array.
  */
-export function getLogMessages(): string[] {
+export function getLogObjects(): object[] {
   const data = fs.readFileSync(SSR_LOG_PATH).toString();
   return (
     data
@@ -44,9 +44,16 @@ export function getLogMessages(): string[] {
       // We ignore plain text logs coming from other sources, like `Node Express server listening on http://localhost:4200`
       .filter((text: string) => text.charAt(0) === '{')
       .map((text: any) => {
-        return JSON.parse(text).message;
+        return JSON.parse(text);
       })
   );
+}
+
+/**
+ * Reads log and returns messages as string array.
+ */
+export function getLogMessages(): string[] {
+  return getLogObjects().map((log) => (log as any).message);
 }
 
 /**
@@ -74,4 +81,46 @@ export async function waitUntilLogContainsText(
 export function doesLogContainText(text: string): boolean {
   const data = fs.readFileSync(SSR_LOG_PATH).toString();
   return data.includes(text);
+}
+
+import { inspect } from 'util';
+import * as LogUtils from './log.utils';
+
+/**
+ * A higher-order function that wraps a test callback and includes SSR logs
+ * in any error thrown during the test execution. The logs are put into the `cause`
+ * property of the Error.
+ *
+ * @param testFn - The original test function to be wrapped.
+ * @returns A new function that can be passed to Jest's `it()` or `test()`.
+ *
+ * @example
+ * it('should perform SSR correctly', attachLogsToErrors(async () => {
+ *   // Your test code here
+ * }));
+ */
+export function attachLogsToErrors(
+  testFn: () => Promise<void> | void
+): () => Promise<void> {
+  return async () => {
+    try {
+      await testFn();
+    } catch (error: unknown) {
+      const readableLogs = inspect(LogUtils.getLogObjects(), { depth: null });
+      const ssrLogs = `(more context below)\n--- SSR LOGS START ---\n${readableLogs}\n--- SSR LOGS END ---`;
+
+      if (error instanceof Error) {
+        // Error's `cause` property is the only property printed by Jest
+        // besides `message` that we can utilize for attaching logs.
+        // No other custom properties are printed by Jest.
+        // See their source code of their function `formatExecError`:
+        // https://github.com/jestjs/jest/blob/bd1c6db7c15c23788ca3e09c919138e48dd3b28a/packages/jest-message-util/src/index.ts#L235
+        error.cause = ssrLogs;
+      } else {
+        throw new Error(error as string, { cause: ssrLogs });
+      }
+
+      throw error;
+    }
+  };
 }
