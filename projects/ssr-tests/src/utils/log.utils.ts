267231983dddbeef941f6a31edbb52c6ea35e896
Krzysztof Platis <platonn.git@gmail.com>
2024-09-27 15:38:09 +0200

    test: print SSR Logs as a context for failed tests to improve troubleshooting of SSR e2e (#19296)
    Wrapped each test function inside `it()` within an util function `LogUtils.attachLogsToErrors()`.
    Whenever an error happens inside such a function (e.g. due to a failing `expect()`, the Error is intercepted (via try-catch) and the SSR logs are attached to this error's `cause` property. Then the error is re-thrown. Thanks to this, JestJS will report this `cause` along with the main error message.
    
    Btw. made a refactor in various places related to logs:
    - removed custom function `expectLogMessages()` and custom matcher `.toContainLogs` with regular `expect(logMessages).toContain()` - to not obfuscate under the hood what is being expected and how
    - moved `LogUtils.clearSsrLogFile();` from top-level `beforeEach` to inside the util `SsrUtils.startSsrServer()` - to be less prone to refactoring mistake of re-ordering the execution of those events (i.e. clear log file VS. start new ssr writing to this file)
    - created separate functions to get logs:
       - `getRawLogs(): string[]` - gets raw content of the log file (both JSONs and raw strings like 'Node is listening on port 4000'.
       - `getRawLogsPretty(): string[]` - similar as above, but it parses single line JSON strings and pretty-prints them (Note: it's used in `LogUtils.attachLogsToErrors()` for making the logs more readable when we attach them as a context of the error).
       - `getLogsObjects(): object[]` - gets only JSON logs and parses them to JS objects. Raw strings from logs are skipped
       - `getLogsMessages(): string[]` - gets only JSON logs' `message` properties. Raw strings from logs are skipped
       
    **QA steps:**
    1. Deliberately make one of the tests fail - e.g. change the expected HTTP status from `200` to `123`, e.g. in line 33, in the test `should receive success response with request`
    1. Build libs, app and SSR (with localhost backend):
       `npm run build:libs && npm run build && build:ssr:local-http-backend`
    2. Run ssr-tests:
       `npm run test:ssr`
    3. Verify that the Error message printed by JestJS contains SSR logs as a context:
    ![image](https://github.com/user-attachments/assets/4c12588b-5ad1-4f99-a56c-67a213ff687b)
    ![image](https://github.com/user-attachments/assets/d7ae2d01-f853-49b2-8935-3ced99d4b8dd)
    
    
    fixes https://jira.tools.sap/browse/CXSPA-8564

--- projects/ssr-tests/src/utils/log.utils.ts
+++ projects/ssr-tests/src/utils/log.utils.ts
@@ -11,6 +11,7 @@
  */
 
 import * as fs from 'fs';
+import { inspect } from 'util';
 
 /**
  * Path where SSR log file from server will be generated and read from.
@@ -25,28 +26,86 @@ export function clearSsrLogFile(): void {
 }
 
 /**
- * Returns all text in the log as a single string.
+ * Validates that all lines starting with `{` are valid JSON objects.
+ * Otherwise it throws an error.
+ *
+ * Note: multi-line JSONs (printed by SSR in dev mode) cannot be parsed by `JSON.parse`.
+ *       That's why we need to to run SSR in prod mode to get single line JSON logs.
  */
-export function getLogText(): string {
-  return fs.readFileSync(SSR_LOG_PATH).toString();
+function validateJsonsInLogs(logs: string[]): void {
+  logs.forEach((text) => {
+    if (text.charAt(0) === '{') {
+      try {
+        JSON.parse(text);
+      } catch (error) {
+        throw new Error(
+          `Encountered in SSR Logs a line starting with \`{\` that could not be parsed as JSON.
+          Perhaps its a multi-line JSON log from SSR dev mode.
+          Please make sure to build Spartacus SSR in prod mode - to get single line JSONs that can be parsed in tests.`
+        );
+      }
+    }
+  });
 }
 
 /**
- * Reads log and returns messages as string array.
+ * Returns raw logs as an array of strings.
+ *
+ * Note: Non-JSON log entries are also included in the returned array.
+ *
+ * It also validates whether each line starting with `{` is a valid JSON object.
+ * Otherwise it throws an error.
  */
-export function getLogMessages(): string[] {
+export function getRawLogs(): string[] {
   const data = fs.readFileSync(SSR_LOG_PATH).toString();
-  return (
-    data
-      .toString()
-      .split('\n')
-      // We're interested only in JSON logs from Spartacus SSR app.
-      // We ignore plain text logs coming from other sources, like `Node Express server listening on http://localhost:4200`
-      .filter((text: string) => text.charAt(0) === '{')
-      .map((text: any) => {
-        return JSON.parse(text).message;
-      })
-  );
+  const logs = data.toString().split('\n');
+  validateJsonsInLogs(logs);
+  return logs;
+}
+
+/**
+ * Returns raw logs as an array of strings, with JSON objects pretty-printed.
+ *
+ * Note: Non-JSON log entries are also included in the returned array.
+ */
+export function getRawLogsPretty(): string[] {
+  return getRawLogs().map((line) => {
+    try {
+      const object = JSON.parse(line);
+      return inspect(object, { depth: null });
+    } catch (_e) {
+      // If the line is not a valid JSON, return it as a string
+      return line;
+    }
+  });
+}
+
+/**
+ * Returns logs as an array of objects, parsed from JSON log entries.
+ *
+ * Note: Non-JSON log entries are skipped (e.g. 'Node is running on port 4000').
+ */
+export function getLogsObjects(): object[] {
+  return getRawLogs()
+    .map((log) => {
+      try {
+        return JSON.parse(log);
+      } catch (_e) {
+        return undefined;
+      }
+    })
+    .filter((x): x is object => x !== undefined);
+}
+
+/**
+ * Returns logs as an array of strings, being the `message` field of each parsed JSON log entry.
+ *
+ * Note: Non-JSON log entries are skipped (e.g. 'Node is running on port 4000').
+ */
+export function getLogsMessages(): string[] {
+  return getLogsObjects().map((log) => {
+    return (log as { message: string }).message;
+  });
 }
 
 /**
@@ -58,7 +117,7 @@ export async function waitUntilLogContainsText(
   checkInterval = 500
 ): Promise<true> {
   return new Promise((resolve) => {
-    if (doesLogContainText(text)) {
+    if (getRawLogs().some((log) => log.includes(text))) {
       return resolve(true);
     }
     return setTimeout(
@@ -69,9 +128,41 @@ export async function waitUntilLogContainsText(
 }
 
 /**
- * Returns true if log contains string.
+ * A higher-order function that wraps a test callback and includes SSR logs
+ * in any error thrown during the test execution. The logs are put into the `cause`
+ * property of the Error.
+ *
+ * @param testFn - The original test function to be wrapped.
+ * @returns A new function that can be passed to Jest's `it()` or `test()`.
+ *
+ * @example
+ * it('should perform SSR correctly', attachLogsToErrors(async () => {
+ *   // Your test code here
+ * }));
  */
-export function doesLogContainText(text: string): boolean {
-  const data = fs.readFileSync(SSR_LOG_PATH).toString();
-  return data.includes(text);
+export function attachLogsToErrors(
+  testFn: () => Promise<void> | void
+): () => Promise<void> {
+  return async () => {
+    try {
+      await testFn();
+    } catch (error: unknown) {
+      const readableLogs = getRawLogsPretty().join('\n');
+      const ssrLogs = `(more context below)\n--- SSR LOGS (with JSONs pretty-printed) ---\n${readableLogs}\n--- SSR LOGS END ---`;
+
+      if (error instanceof Error) {
+        // Error's `cause` property is the only property printed by Jest
+        // besides `message` that we can utilize for attaching logs.
+        // No other custom properties are printed by Jest.
+        // See their source code of their function `formatExecError`:
+        // https://github.com/jestjs/jest/blob/bd1c6db7c15c23788ca3e09c919138e48dd3b28a/packages/jest-message-util/src/index.ts#L436
+
+        error.cause = ssrLogs;
+      } else {
+        throw new Error(error as string, { cause: ssrLogs });
+      }
+
+      throw error;
+    }
+  };
 }
