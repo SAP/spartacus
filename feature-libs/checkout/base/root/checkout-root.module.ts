c656ed56d7b2d1a93947e67524a64aeeaadfe566
Krzysztof Platis <platonn.git@gmail.com>
2024-10-02 11:28:27 +0200

    PoC: support guest checkout with external login

--- feature-libs/checkout/base/root/checkout-root.module.ts
+++ feature-libs/checkout/base/root/checkout-root.module.ts
@@ -5,12 +5,14 @@
  */
 
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CART_BASE_FEATURE } from '@spartacus/cart/base/root';
 import {
   CmsConfig,
   provideDefaultConfig,
   provideDefaultConfigFactory,
 } from '@spartacus/core';
+import { CmsPageGuard, PageLayoutComponent } from '@spartacus/storefront';
 import { defaultCheckoutConfig } from './config/default-checkout-config';
 import { defaultCheckoutRoutingConfig } from './config/default-checkout-routing-config';
 import { CheckoutEventModule } from './events/checkout-event.module';
@@ -49,8 +51,43 @@ export function defaultCheckoutComponentsConfig() {
 }
 
 @NgModule({
-  imports: [CheckoutEventModule],
+  imports: [
+    CheckoutEventModule,
+
+    // We need to add a new route.
+    // Normally, I'd define a new ContentPage in CMS and not use `RouterModule.forChild()`.
+    // But JUST FOR DEMONSTRATION PURPOSES, I took the shortcut here.
+    // I've added a new route (from the Spartacus point of view), but under the hood I reuse
+    // an existing CMS page content (so I don't need to define a new page in the CMS sample data now).
+    // See the mapping below: `pageLabel: '/login'`
+    // In other words, when entering the route `/checkout-require-auth`,
+    // it will not trigger a regular `LoginGuard` (which would redirect to an EXTERNAL login page).
+    // but instead it will show us a page content of the old login page.
+    RouterModule.forChild([
+      {
+        // @ts-ignore
+        path: null,
+        canActivate: [CmsPageGuard],
+        component: PageLayoutComponent,
+        data: {
+          cxRoute: 'checkoutRequireAuth',
+          pageLabel: '/login',
+        },
+      },
+    ]),
+  ],
   providers: [
+    provideDefaultConfig({
+      routing: {
+        routes: {
+          checkoutRequireAuth: {
+            paths: ['checkout-require-auth'],
+            authFlow: true,
+          },
+        },
+      },
+    }),
+
     ...interceptors,
     provideDefaultConfig(defaultCheckoutRoutingConfig),
     provideDefaultConfig(defaultCheckoutConfig),
