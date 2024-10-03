f3275741b751679728ea925aa586bf120c74eac8
Krzysztof Platis <platonn.git@gmail.com>
2024-10-03 10:01:34 +0200

    temp: write custom-test-environment.ts using import/export, but not require/module.export

--- projects/ssr-tests/src/environments/custom-test-environment.js
+++ projects/ssr-tests/src/environments/custom-test-environment.js
@@ -4,13 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const NodeEnvironment = require('jest-environment-node').TestEnvironment;
+import { TestEnvironment as NodeEnvironment } from 'jest-environment-node';
 
 /**
  * This is a custom Jest environment that adds the SSR logs to the test errors.
  * The logs are added to the error `cause` property for failed tests,
  * so those logs will be printed by Jest along with the test error.
  */
-class CustomTestEnvironment extends NodeEnvironment {}
-
-module.exports = CustomTestEnvironment;
+export default class CustomTestEnvironment extends NodeEnvironment {}
