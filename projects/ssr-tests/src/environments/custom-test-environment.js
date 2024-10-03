95c9714bce27b6a2afed6f8e92b8c481a49f503d
Krzysztof Platis <platonn.git@gmail.com>
2024-10-03 10:23:16 +0200

    temp: use require/module.export instead of ESM import/export

--- projects/ssr-tests/src/environments/custom-test-environment.js
+++ projects/ssr-tests/src/environments/custom-test-environment.js
@@ -4,11 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { TestEnvironment as NodeEnvironment } from 'jest-environment-node';
+const NodeEnvironment = require('jest-environment-node').TestEnvironment;
 
 /**
  * This is a custom Jest environment that adds the SSR logs to the test errors.
  * The logs are added to the error `cause` property for failed tests,
  * so those logs will be printed by Jest along with the test error.
  */
-export default class CustomTestEnvironment extends NodeEnvironment {}
+class CustomTestEnvironment extends NodeEnvironment {}
+
+module.exports = CustomTestEnvironment;
