1ba263f8235a7586151834f6178757a77d6335b7
anjana-bl <anjana.b.l@sap.com>
2024-10-07 14:33:55 +0530

    Update subscription-product.module.ts

--- integration-libs/subscription-billing/root/product/subscription-product.module.ts
+++ integration-libs/subscription-billing/root/product/subscription-product.module.ts
@@ -17,8 +17,6 @@ import { SubscriptionProductUsageChargeComponent } from './product-price/subscri
 import { CommonModule } from '@angular/common';
 import { defaultOccSubscriptionBillingConfig } from './occ/config/default-occ-subscription-billing-config';
 import { CurrentSubscriptionProductService } from './services/current-subscription-product.service';
-import { MockInterceptor } from '../dummy/dummy-http-interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 const componentList = [
   SubscriptionProductPriceComponent,
@@ -37,11 +35,6 @@ const componentList = [
     {
       provide: CurrentProductService,
       useExisting: CurrentSubscriptionProductService,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useExisting: MockInterceptor,
-      multi: true,
     },
   ],
   exports: componentList,
