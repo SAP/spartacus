e372230678d8d6c1b61e776d21e33b0c3ec67f3c
Larisa Staroverova <larisa.staroverova@sap.com>
2024-10-09 08:15:39 +0200

    Fix focus after clicking on hamburger menu

--- projects/storefrontlib/layout/main/storefront.component.ts
+++ projects/storefrontlib/layout/main/storefront.component.ts
@@ -120,8 +120,9 @@ export class StorefrontComponent implements OnInit, OnDestroy {
   }
 
   protected focusOnFirstNavigationItem() {
-    const closestNavigationUi =
-      this.elementRef.nativeElement.querySelector('cx-navigation-ui');
+    const closestNavigationUi = this.elementRef.nativeElement.querySelector(
+      '.NavigationBar cx-navigation-ui'
+    );
     const focusable = closestNavigationUi?.querySelector<HTMLElement>(
       'li:not(.back) button, [tabindex="0"]'
     );
