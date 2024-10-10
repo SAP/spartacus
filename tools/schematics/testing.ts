2588fc815c890615e823ef6cedf1a3a8f74b0f13
Krzysztof Platis <platonn.git@gmail.com>
2024-10-10 11:26:34 +0200

    refactor: rename variables

--- tools/schematics/testing.ts
+++ tools/schematics/testing.ts
@@ -83,9 +83,10 @@ const commands = [
 type Command = (typeof commands)[number];
 
 const buildLibRegEx = new RegExp('build (.*?)/schematics');
-const verdaccioUrl = 'http://localhost:4873/';
-
-const npmUrl = execSync('npm config get @spartacus:registry').toString().trim();
+const verdaccioRegistryUrl = 'http://localhost:4873/';
+const originalRegistryUrl = execSync('npm config get @spartacus:registry')
+  .toString()
+  .trim();
 
 function startVerdaccio(): ChildProcess {
   execSync('rm -rf ./scripts/install/storage');
@@ -93,7 +94,7 @@ function startVerdaccio(): ChildProcess {
   console.log('Waiting for verdaccio to boot...');
   const res = exec('verdaccio --config ./scripts/install/config.yaml');
   try {
-    execSync(`npx wait-on ${verdaccioUrl} --timeout 10000`);
+    execSync(`npx wait-on ${verdaccioRegistryUrl} --timeout 10000`);
   } catch (_e) {
     console.log(
       chalk.red(
@@ -103,13 +104,13 @@ function startVerdaccio(): ChildProcess {
     process.exit(1);
   }
   console.log('Pointing npm to verdaccio');
-  execSync(`npm config set @spartacus:registry ${verdaccioUrl}`);
+  execSync(`npm config set @spartacus:registry ${verdaccioRegistryUrl}`);
   return res;
 }
 
 function beforeExit(): void {
   console.log('Setting npm back to npmjs.org');
-  execSync(`npm config set @spartacus:registry ${npmUrl}`);
+  execSync(`npm config set @spartacus:registry ${originalRegistryUrl}`);
   if (verdaccioProcess) {
     try {
       console.log('Killing verdaccio');
@@ -141,7 +142,7 @@ function publishLibs(): void {
     const dir = path.dirname(packagePath);
     console.log(`\nPublishing ${content.name}`);
     execSync(
-      `cd ${dir} && npm publish --registry=${verdaccioUrl} --no-git-tag-version`,
+      `cd ${dir} && npm publish --registry=${verdaccioRegistryUrl} --no-git-tag-version`,
       { stdio: 'inherit' }
     );
   });
