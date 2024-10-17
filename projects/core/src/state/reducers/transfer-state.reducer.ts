991f2655a9f7dad279538bcfa3e443ff84863b69
Krzysztof Platis <platonn.git@gmail.com>
2024-10-17 22:51:35 +0200

    feat: disable transfer state

--- projects/core/src/state/reducers/transfer-state.reducer.ts
+++ projects/core/src/state/reducers/transfer-state.reducer.ts
@@ -6,12 +6,12 @@
 
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
+import { StateKey, TransferState, makeStateKey } from '@angular/core';
 import { INIT } from '@ngrx/store';
 import { AuthStatePersistenceService } from '../../auth/user-auth/services/auth-state-persistence.service';
 import { deepMerge } from '../../config/utils/deep-merge';
 import { StateConfig, StateTransferType } from '../config/state-config';
 import { filterKeysByType, getStateSlice } from '../utils/get-state-slice';
-import { makeStateKey, StateKey, TransferState } from '@angular/core';
 
 export const CX_KEY: StateKey<string> = makeStateKey<string>('cx-state');
 
@@ -53,7 +53,11 @@ export function getServerTransferStateReducer(
       const newState = reducer(state, action);
       if (newState) {
         const stateSlice = getStateSlice(transferStateKeys, [], newState);
-        transferState.set(CX_KEY, stateSlice);
+
+        // SPIKE TODO REVERT - disabling rendering transfer state in SSR
+        if (false) {
+          transferState.set(CX_KEY, stateSlice);
+        }
       }
 
       return newState;
