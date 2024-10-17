b6b0302ac5655fc87e24e9e75b16b008e6c3d291
Krzysztof Platis <platonn.git@gmail.com>
2024-10-17 10:32:39 +0200

    temp: reduce number of unused features in css and JS - use only features from a fresh OOTB app

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -29,7 +29,6 @@ import {
   FooterNavigationModule,
   HamburgerMenuModule,
   HomePageEventModule,
-  JsonLdBuilderModule,
   LinkModule,
   LoginRouteModule,
   LogoutModule,
@@ -56,7 +55,6 @@ import {
   ScrollToTopModule,
   SearchBoxModule,
   SiteContextSelectorModule,
-  SiteThemeSwitcherModule,
   StockNotificationModule,
   TabParagraphContainerModule,
   USE_MY_ACCOUNT_V2_CONSENT,
@@ -71,106 +69,184 @@ import { ImportExportFeatureModule } from './features/cart/cart-import-export-fe
 import { QuickOrderFeatureModule } from './features/cart/cart-quick-order-feature.module';
 import { SavedCartFeatureModule } from './features/cart/cart-saved-cart-feature.module';
 import { WishListFeatureModule } from './features/cart/wish-list-feature.module';
-import { CdcFeatureModule } from './features/cdc/cdc-feature.module';
-import { CdsFeatureModule } from './features/cds/cds-feature.module';
 import { CheckoutFeatureModule } from './features/checkout/checkout-feature.module';
-import { CpqQuoteFeatureModule } from './features/cpq-quote/cpq-quote-feature.module';
-import { CustomerTicketingFeatureModule } from './features/customer-ticketing/customer-ticketing-feature.module';
-import { DigitalPaymentsFeatureModule } from './features/digital-payments/digital-payments-feature.module';
-import { EpdVisualizationFeatureModule } from './features/epd-visualization/epd-visualization-feature.module';
-import { EstimatedDeliveryDateFeatureModule } from './features/estimated-delivery-date/estimated-delivery-date-feature.module';
-import { OmfFeatureModule } from './features/omf/omf-feature.module';
-import { OppsFeatureModule } from './features/opps/opps-feature.module';
 import { OrderFeatureModule } from './features/order/order-feature.module';
-import { AccountSummaryFeatureModule } from './features/organization/organization-account-summary-feature.module';
-import { AdministrationFeatureModule } from './features/organization/organization-administration-feature.module';
-import { OrderApprovalFeatureModule } from './features/organization/organization-order-approval-feature.module';
-import { UnitOrderFeatureModule } from './features/organization/organization-unit-order-feature.module';
-import { PDFInvoicesFeatureModule } from './features/pdf-invoices/pdf-invoices-feature.module';
-import { PickupInStoreFeatureModule } from './features/pickup-in-store/pickup-in-store-feature.module';
-import { ProductConfiguratorRulebasedFeatureModule } from './features/product-configurator/product-configurator-rulebased-feature.module';
-import { ProductConfiguratorTextfieldFeatureModule } from './features/product-configurator/product-configurator-textfield-feature.module';
-import { ProductMultiDimensionalListFeatureModule } from './features/product-multi-dimensional/product-multi-dimensional-list-feature.module';
-import { ProductMultiDimensionalSelectorFeatureModule } from './features/product-multi-dimensional/product-multi-dimensional-selector-feature.module';
-import { BulkPricingFeatureModule } from './features/product/product-bulk-pricing-feature.module';
-import { FutureStockFeatureModule } from './features/product/product-future-stock-feature.module';
-import { ImageZoomFeatureModule } from './features/product/product-image-zoom-feature.module';
 import { VariantsFeatureModule } from './features/product/product-variants-feature.module';
-import { QualtricsFeatureModule } from './features/qualtrics/qualtrics-feature.module';
-import { QuoteFeatureModule } from './features/quote-feature.module';
-import { OrganizationUserRegistrationFeatureModule } from './features/registration-feature.module';
-import { RequestedDeliveryDateFeatureModule } from './features/requested-delivery-date/requested-delivery-date-feature.module';
-import { S4ServiceFeatureModule } from './features/s4-service/s4-service-feature.module';
-import { S4OMFeatureModule } from './features/s4om/s4om-feature.module';
-import { SegmentRefsFeatureModule } from './features/segment-refs/segment-refs-feature.module';
 import { SmartEditFeatureModule } from './features/smartedit/smartedit-feature.module';
 import { StorefinderFeatureModule } from './features/storefinder/storefinder-feature.module';
 import { TrackingFeatureModule } from './features/tracking/tracking-feature.module';
 import { UserFeatureModule } from './features/user/user-feature.module';
 
-const featureModules = [];
+// const featureModules = [];
 
-if (environment.b2b) {
-  featureModules.push(
-    AdministrationFeatureModule,
-    AccountSummaryFeatureModule,
-    BulkPricingFeatureModule,
-    OrderApprovalFeatureModule,
-    OrganizationUserRegistrationFeatureModule,
-    UnitOrderFeatureModule,
-    FutureStockFeatureModule
-  );
-} else {
-  featureModules.push(PickupInStoreFeatureModule);
-}
+// if (environment.b2b) {
+//   featureModules.push(
+//     AdministrationFeatureModule,
+//     AccountSummaryFeatureModule,
+//     BulkPricingFeatureModule,
+//     OrderApprovalFeatureModule,
+//     OrganizationUserRegistrationFeatureModule,
+//     UnitOrderFeatureModule,
+//     FutureStockFeatureModule
+//   );
+// } else {
+//   featureModules.push(PickupInStoreFeatureModule);
+// }
 
-if (environment.cdc) {
-  featureModules.push(CdcFeatureModule);
-}
-if (environment.s4Service) {
-  featureModules.push(S4ServiceFeatureModule);
-}
-if (environment.cds) {
-  featureModules.push(CdsFeatureModule);
-}
-if (environment.digitalPayments) {
-  featureModules.push(DigitalPaymentsFeatureModule);
-}
-if (environment.epdVisualization) {
-  featureModules.push(EpdVisualizationFeatureModule);
-}
-if (environment.pdfInvoices) {
-  featureModules.push(PDFInvoicesFeatureModule);
-}
-if (environment.opps) {
-  featureModules.push(OppsFeatureModule);
-}
-if (environment.s4om) {
-  featureModules.push(S4OMFeatureModule);
-}
-if (environment.segmentRefs) {
-  featureModules.push(SegmentRefsFeatureModule);
-}
-if (environment.requestedDeliveryDate) {
-  featureModules.push(RequestedDeliveryDateFeatureModule);
-}
-if (environment.estimatedDeliveryDate) {
-  featureModules.push(EstimatedDeliveryDateFeatureModule);
-}
-if (environment.omf) {
-  featureModules.push(OmfFeatureModule);
-}
-if (environment.cpq) {
-  featureModules.push(CpqQuoteFeatureModule);
-}
+// if (environment.cdc) {
+//   featureModules.push(CdcFeatureModule);
+// }
+// if (environment.s4Service) {
+//   featureModules.push(S4ServiceFeatureModule);
+// }
+// if (environment.cds) {
+//   featureModules.push(CdsFeatureModule);
+// }
+// if (environment.digitalPayments) {
+//   featureModules.push(DigitalPaymentsFeatureModule);
+// }
+// if (environment.epdVisualization) {
+//   featureModules.push(EpdVisualizationFeatureModule);
+// }
+// if (environment.pdfInvoices) {
+//   featureModules.push(PDFInvoicesFeatureModule);
+// }
+// if (environment.opps) {
+//   featureModules.push(OppsFeatureModule);
+// }
+// if (environment.s4om) {
+//   featureModules.push(S4OMFeatureModule);
+// }
+// if (environment.segmentRefs) {
+//   featureModules.push(SegmentRefsFeatureModule);
+// }
+// if (environment.requestedDeliveryDate) {
+//   featureModules.push(RequestedDeliveryDateFeatureModule);
+// }
+// if (environment.estimatedDeliveryDate) {
+//   featureModules.push(EstimatedDeliveryDateFeatureModule);
+// }
+// if (environment.omf) {
+//   featureModules.push(OmfFeatureModule);
+// }
+// if (environment.cpq) {
+//   featureModules.push(CpqQuoteFeatureModule);
+// }
 @NgModule({
   imports: [
-    // Auth Core
-    AuthModule.forRoot(),
-    LogoutModule, // will be come part of auth package
-    LoginRouteModule, // will be come part of auth package
+    // SPIKE - OLD, ORIGINAL:
 
-    // Basic Cms Components
+    // // Auth Core
+    // AuthModule.forRoot(),
+    // LogoutModule, // will be come part of auth package
+    // LoginRouteModule, // will be come part of auth package
+
+    // // Basic Cms Components
+    // HamburgerMenuModule,
+    // SiteContextSelectorModule,
+    // LinkModule,
+    // BannerModule,
+    // CmsParagraphModule,
+    // TabParagraphContainerModule,
+    // BannerCarouselModule,
+    // CategoryNavigationModule,
+    // NavigationModule,
+    // FooterNavigationModule,
+    // PageTitleModule,
+    // BreadcrumbModule,
+    // PDFModule,
+    // ScrollToTopModule,
+    // VideoModule,
+    // SiteThemeSwitcherModule,
+
+    // // User Core
+    // UserModule,
+    // UserOccModule,
+    // // User UI
+    // PaymentMethodsModule,
+    // NotificationPreferenceModule,
+    // MyInterestsModule,
+    // MyAccountV2Module,
+    // StockNotificationModule,
+    // ConsentManagementModule,
+    // MyCouponsModule,
+
+    // // Anonymous Consents Core
+    // AnonymousConsentsModule.forRoot(),
+    // // Anonymous Consents UI
+    // AnonymousConsentsDialogModule,
+    // AnonymousConsentManagementBannerModule,
+
+    // // Product Core
+    // ProductModule.forRoot(),
+    // ProductOccModule,
+
+    // // Product UI
+    // ProductDetailsPageModule,
+    // ProductListingPageModule,
+    // ProductListModule,
+    // SearchBoxModule,
+    // ProductFacetNavigationModule,
+    // ProductTabsModule,
+    // ProductCarouselModule,
+    // ProductReferencesModule,
+    // ProductImagesModule,
+    // ProductSummaryModule,
+    // ProductIntroModule,
+
+    // // Cost Center
+    // CostCenterOccModule,
+
+    // // Page Events
+    // NavigationEventModule,
+    // HomePageEventModule,
+    // ProductPageEventModule,
+
+    // /************************* Opt-in features *************************/
+
+    // ExternalRoutesModule.forRoot(), // to opt-in explicitly, is added by default schematics
+    // JsonLdBuilderModule,
+
+    // /************************* Feature libraries *************************/
+    // UserFeatureModule,
+
+    // CartBaseFeatureModule,
+    // WishListFeatureModule,
+    // SavedCartFeatureModule,
+    // QuickOrderFeatureModule,
+    // ImportExportFeatureModule,
+
+    // OrderFeatureModule,
+
+    // CheckoutFeatureModule,
+
+    // TrackingFeatureModule,
+
+    // AsmFeatureModule,
+    // AsmCustomer360FeatureModule,
+
+    // StorefinderFeatureModule,
+
+    // QualtricsFeatureModule,
+
+    // SmartEditFeatureModule,
+
+    // VariantsFeatureModule,
+    // ProductMultiDimensionalSelectorFeatureModule,
+    // ProductMultiDimensionalListFeatureModule,
+    // ImageZoomFeatureModule,
+
+    // QuoteFeatureModule,
+    // CustomerTicketingFeatureModule,
+
+    // ProductConfiguratorTextfieldFeatureModule,
+    // ProductConfiguratorRulebasedFeatureModule,
+    // ...featureModules,
+
+    // SPIKE NEW - COPIED FROM A FRESH APP:
+    AuthModule.forRoot(),
+    LogoutModule,
+    LoginRouteModule,
     HamburgerMenuModule,
     SiteContextSelectorModule,
     LinkModule,
@@ -181,17 +257,13 @@ if (environment.cpq) {
     CategoryNavigationModule,
     NavigationModule,
     FooterNavigationModule,
-    PageTitleModule,
     BreadcrumbModule,
-    PDFModule,
     ScrollToTopModule,
+    PageTitleModule,
     VideoModule,
-    SiteThemeSwitcherModule,
-
-    // User Core
+    PDFModule,
     UserModule,
     UserOccModule,
-    // User UI
     PaymentMethodsModule,
     NotificationPreferenceModule,
     MyInterestsModule,
@@ -199,18 +271,11 @@ if (environment.cpq) {
     StockNotificationModule,
     ConsentManagementModule,
     MyCouponsModule,
-
-    // Anonymous Consents Core
     AnonymousConsentsModule.forRoot(),
-    // Anonymous Consents UI
     AnonymousConsentsDialogModule,
     AnonymousConsentManagementBannerModule,
-
-    // Product Core
     ProductModule.forRoot(),
     ProductOccModule,
-
-    // Product UI
     ProductDetailsPageModule,
     ProductListingPageModule,
     ProductListModule,
@@ -222,55 +287,43 @@ if (environment.cpq) {
     ProductImagesModule,
     ProductSummaryModule,
     ProductIntroModule,
-
-    // Cost Center
     CostCenterOccModule,
-
-    // Page Events
     NavigationEventModule,
     HomePageEventModule,
     ProductPageEventModule,
-
-    /************************* Opt-in features *************************/
-
-    ExternalRoutesModule.forRoot(), // to opt-in explicitly, is added by default schematics
-    JsonLdBuilderModule,
-
-    /************************* Feature libraries *************************/
+    ExternalRoutesModule.forRoot(),
     UserFeatureModule,
-
     CartBaseFeatureModule,
+
+    // CartSavedCartFeatureModule,
+    SavedCartFeatureModule, /// our replacement for a module from fresh app
+
     WishListFeatureModule,
-    SavedCartFeatureModule,
-    QuickOrderFeatureModule,
-    ImportExportFeatureModule,
+
+    // CartQuickOrderFeatureModule,
+    QuickOrderFeatureModule, /// our replacement for a module from fresh app
+
+    // CartImportExportFeatureModule,
+    ImportExportFeatureModule, /// our replacement for a module from fresh app
 
     OrderFeatureModule,
-
     CheckoutFeatureModule,
 
-    TrackingFeatureModule,
+    // PersonalizationFeatureModule,
+    TrackingFeatureModule, /// our replacement for a module from fresh app
+
+    // StoreFinderFeatureModule,
+    StorefinderFeatureModule,
 
     AsmFeatureModule,
     AsmCustomer360FeatureModule,
-
-    StorefinderFeatureModule,
-
-    QualtricsFeatureModule,
-
     SmartEditFeatureModule,
 
-    VariantsFeatureModule,
-    ProductMultiDimensionalSelectorFeatureModule,
-    ProductMultiDimensionalListFeatureModule,
-    ImageZoomFeatureModule,
+    // ProductVariantsFeatureModule,
+    VariantsFeatureModule, /// our replacement for a module from fresh app
 
-    QuoteFeatureModule,
-    CustomerTicketingFeatureModule,
-
-    ProductConfiguratorTextfieldFeatureModule,
-    ProductConfiguratorRulebasedFeatureModule,
-    ...featureModules,
+    // ProductImageZoomFeatureModule,
+    AsmFeatureModule, /// our replacement for a module from fresh app
   ],
   providers: [
     // Adding the provider here because consents feature is not code-splitted to separate library and not lazy-loaded
