722b49830ad5e23ca25a33a9b78132ee073bd6f9
Akshay Tiwari <tiwariakshay0@gmail.com>
2024-10-21 18:47:16 +0530

    CXSPA-8577: Fix failing test

--- integration-libs/epd-visualization/root/util/url-utils.spec.ts
+++ integration-libs/epd-visualization/root/util/url-utils.spec.ts
@@ -21,7 +21,7 @@ describe('UrlUtils', () => {
 
     it('should return false for data: protocol', () => {
       expect(
-        isHttpOrHttps(new URL('data://data:text/plain,some%20data/'))
+        isHttpOrHttps(new URL('data:text/plain,some%20data'))
       ).toBeFalsy();
     });
   });
