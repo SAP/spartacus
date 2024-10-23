054ec7cf0436c6d675cfbb09b9366f10a5bb2c83
Larisa Staroverova <larisa.staroverova@sap.com>
2024-10-23 07:49:39 +0200

    Update karma.conf.js file

--- feature-libs/product-configurator/karma.conf.js
+++ feature-libs/product-configurator/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
       require('karma-junit-reporter'),
     ],
     client: {
-      clearContext: true, // close Jasmine Spec Runner output in browser to avoid 'Some of your tests did a full page reload!' error when '--no-watch' is active
+      clearContext: false, // close Jasmine Spec Runner output in browser to avoid 'Some of your tests did a full page reload!' error when '--no-watch' is active
       jasmine: {
         random: false,
       },
