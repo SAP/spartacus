f6780faaf7c5f616767074b33b63bc2d24a6df28
petarmarkov9449 <166535589+petarmarkov9449@users.noreply.github.com>
2024-10-23 20:39:46 +0200

    (CXSPA-8024) - Focus issue on remove button at count 2 (#19423)

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -376,6 +376,7 @@ if (environment.cpq) {
         a11yDialogTriggerRefocus: true,
         a11yAddToWishlistFocus: true,
         a11ySearchBoxFocusOnEscape: true,
+        a11yItemCounterFocus: true,
         a11yScrollToReviewByShowReview: true,
         a11yViewHoursButtonIconContrast: true,
         cmsBottomHeaderSlotUsingFlexStyles: true,
