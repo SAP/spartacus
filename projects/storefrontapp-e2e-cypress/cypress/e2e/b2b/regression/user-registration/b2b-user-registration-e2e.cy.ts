56bebbb06e6167a380906eb472dc571345f3080d
Florent Letendre <florent.letendre@hotmail.fr>
2025-01-14 14:10:42 -0500

    add fix

--- projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/user-registration/b2b-user-registration-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/user-registration/b2b-user-registration-e2e.cy.ts
@@ -75,7 +75,7 @@ context('B2B - User Registration', () => {
           verifyFormErrors();
         });
 
-        it('should display error global message if user exists (CXSPA-215)', () => {
+        it('should no throw error if user already exists (CXSPA-215)', () => {
           let user = getSampleUser();
           user.email = myCompanyAdminUser.registrationData?.email;
 
