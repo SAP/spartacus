c637b6f388d37626c572ae1dad8706a59982a779
johannathalmann-SAP <johanna.thalmann@sap.com>
2025-01-16 10:43:24 +0100

    CXCDS-13558: move evaluation of sciEnabled to cds-feature.module

--- integration-libs/cds/src/config/default-cds-config.ts
+++ integration-libs/cds/src/config/default-cds-config.ts
@@ -4,30 +4,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { environment } from 'projects/storefrontapp/src/environments/environment';
 import { CdsConfig } from './cds-config';
 
 export function defaultCdsConfigFactory(): CdsConfig {
-  const sciEnabled = environment.sciEnabled;
-
   return {
     cds: {
       tenant: '',
       baseUrl: '',
-      endpoints: {
-        strategyProducts: sciEnabled
-          ? '/strategy/v1/sites/${baseSite}/strategies/${strategyId}/products'
-          : '/strategy/${tenant}/strategies/${strategyId}/products',
-        searchIntelligence:
-          '/search-intelligence/v1/sites/${cdsSiteId}/trendingSearches',
-      },
       merchandising: {
         defaultCarouselViewportThreshold: 80,
       },
       consentTemplateId: 'PROFILE',
       profileTag: {
         allowInsecureCookies: false,
-        sciEnabled: sciEnabled,
       },
     },
   };
