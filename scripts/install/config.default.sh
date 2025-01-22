09a9240fc3a5aeb4de6e3ae6fca5e34cfd5e9de3
Pawel Fras <fras.pawel@yahoo.com>
2025-01-22 15:34:22 +0100

    chore: adjust config for installation script
    - use 'develop' branch
    - add comment to switch to Angular 19 when PR with Angualr 19 is ready

--- scripts/install/config.default.sh
+++ scripts/install/config.default.sh
@@ -51,8 +51,7 @@ SPARTACUS_PROJECTS=(
         )
 
 SPARTACUS_REPO_URL="https://github.com/SAP/spartacus.git"
-# TODO: CXSPA-9115 change to `develop` after the February release 
-BRANCH='develop-next-major' 
+BRANCH='develop' 
 
 # custom location for the installation output
 # BASE_DIR='/tmp/'
@@ -62,6 +61,7 @@ CLONE_DIR="clone"
 INSTALLATION_DIR="apps"
 E2E_TEST_DIR=${CLONE_DIR}/projects/storefrontapp-e2e-cypress
 
+# TODO: CXSPA-9115 change to `^19.0.4` after merging https://github.com/SAP/spartacus/pull/19772
 ANGULAR_CLI_VERSION='^18.2.9'
 SPARTACUS_VERSION='latest'
 
