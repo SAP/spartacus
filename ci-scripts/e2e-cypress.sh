05545cb90764d5b320e383da4ac151c69faf987d
KateChuen <kate.chuen.cheong@sap.com>
2025-02-25 13:57:25 -0500

    chore:running spa 2503 against cx 2211.32

--- ci-scripts/e2e-cypress.sh
+++ ci-scripts/e2e-cypress.sh
@@ -103,13 +103,13 @@ else
     echo '-----'
     echo "Running Cypress end to end tests"
 
-    if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
-      if [[ "${GITHUB_HEAD_REF}" == epic/* && "${GITHUB_HEAD_REF}" != renovate/* && "${GITHUB_HEAD_REF}" != dependabot/* ]]; then
+#    if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+#      if [[ "${GITHUB_HEAD_REF}" == epic/* && "${GITHUB_HEAD_REF}" != renovate/* && "${GITHUB_HEAD_REF}" != dependabot/* ]]; then
+#        npm run e2e:run:ci"${SUITE}"
+#      else
+#        npm run e2e:run:ci:core"${SUITE}"
+#      fi
+#    else
         npm run e2e:run:ci"${SUITE}"
-      else
-        npm run e2e:run:ci:core"${SUITE}"
-      fi
-    else
-        npm run e2e:run:ci"${SUITE}"
-    fi
+#    fi
 fi
