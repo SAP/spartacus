ec65898927d23c14c04180aacc9578c83b812364
Florent Letendre <florent.letendre@hotmail.fr>
2025-03-04 15:57:47 -0500

    use enum to fix sonar

--- integration-libs/punchout/root/model/punchout.model.ts
+++ integration-libs/punchout/root/model/punchout.model.ts
@@ -6,11 +6,25 @@
 
 export const PUNCHOUT_SESSION_KEY = 'sid';
 
+export enum PunchOutLevel {
+  STORE = 'store',
+  PRODUCT = 'product',
+  AISLE = 'aisle',
+  SHAELF = 'shelf',
+}
+
+export enum PunchOutOperation {
+  CREATE = 'create',
+  EDIT = 'edit',
+  INSPECT = 'inspect',
+  SOURCE = 'source',
+}
+
 export interface PunchoutSession {
   customerId: string;
   cartId: string;
-  punchOutLevel: 'store' | 'product' | 'aisle' | 'shelf';
-  punchOutOperation: 'create' | 'edit' | 'inspect' | 'source';
+  punchOutLevel: PunchOutLevel;
+  punchOutOperation: PunchOutOperation;
   selectedItem: string;
   token: {
     accessToken: string;
