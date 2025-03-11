55a85ad18021f4cfc3962dc27a8d30166d6f539c
PioBar <bartkowiakpiotr0@gmail.com>
2025-03-11 13:23:39 +0100

    Widescreen layout improvements

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -970,6 +970,12 @@ export interface FeatureTogglesInterface {
   a11yWrapReviewOrderInSection?: boolean;
 
   /**
+   * Improves wide viewport layout issues.
+   * Affects the styles of: Order confirmation page, product configurator.
+   */
+  a11yWideScreenImprovements?: boolean;
+
+  /**
    * Enables the product carousel to include products based on specified category codes.
    *
    * - When this feature is enabled, the carousel will fetch and display products
@@ -1136,6 +1142,7 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   useExtendedMediaComponentConfiguration: false,
   showRealTimeStockInPDP: false,
   a11yScrollToTopPositioning: false,
+  a11yWideScreenImprovements: false,
   enableSecurePasswordValidation: false,
   enableCarouselCategoryProducts: false,
   enableClaimCustomerCouponWithCodeInRequestBody: false,
