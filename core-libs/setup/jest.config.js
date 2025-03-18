e58542b5223d59ebe41bea1266cdfe6c4552a047
andreighervan7 <andrei.ghervan@sap.com>
2025-03-18 10:57:45 +0200

    fix unit testing

--- core-libs/setup/jest.config.js
+++ core-libs/setup/jest.config.js
@@ -1,13 +1,19 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.spec.json';
-import { createJestPreset } from 'jest-preset-angular/presets';
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.spec.json');
+const { defaultTransformerOptions } = require('jest-preset-angular/presets');
 
-export default {
-  ...createJestPreset(), // Use the new recommended preset method
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
   moduleNameMapper: {
     ...pathsToModuleNameMapper(compilerOptions.paths || {}, {
       prefix: '<rootDir>/',
     }),
+    // mapping required to use `beasties` from node modules that has proper ES module format
+    // instead of the version internalized by the Angular Team which file format is not supported
+    // by Jest.
+    // for more, see: https://github.com/angular/angular-cli/pull/28228
+    // and: https://github.com/angular/angular-cli/pull/28726
     '^../third_party/beasties/index.js$':
       '<rootDir>/../../node_modules/beasties',
   },
@@ -16,8 +22,7 @@ export default {
     '^.+\\.(ts|js|mjs|html|svg)$': [
       'jest-preset-angular',
       {
-        stringifyContentPathRegex: '\\.html$',
-        tsconfig: '<rootDir>/tsconfig.spec.json',
+        ...defaultTransformerOptions,
       },
     ],
   },
