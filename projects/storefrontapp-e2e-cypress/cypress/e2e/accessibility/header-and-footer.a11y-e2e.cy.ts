a4e599385b998e39e8f35dc964d4432e57dfcb9a
PioBar <bartkowiakpiotr0@gmail.com>
2025-04-07 12:56:57 +0200

    CR changes

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/header-and-footer.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/header-and-footer.a11y-e2e.cy.ts
@@ -4,27 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { viewportContext } from '../../helpers/viewport-context';
 import { standardUser } from '../../sample-data/shared-users';
 
 describe('Header and Footer Continuum tests', () => {
   beforeEach(() => {
     cy.a11yContinuumSetup();
     cy.visit('/');
-    cy.get('[section="header"]').as('header');
-    cy.get('[section="footer"]').as('footer');
   });
   context('Header', () => {
     it('Main header body', () => {
-      cy.get('@header').get('a').contains('Brands');
-      cy.get('@header').get('nav button[aria-label="brands"]').click();
-      cy.get('@header').a11yRunContinuumTest();
+      cy.get('header').get('a').contains('Brands');
+      cy.get('header').get('nav button[aria-label="Brands"]').click();
+      cy.get('header').a11yRunContinuumTest();
     });
 
     it('My Account dropdown', () => {
       cy.requireLoggedIn(standardUser);
       cy.reload();
-      cy.get('@header').get('.accNavComponent button').click();
-      cy.get('@header')
+      cy.get('header').get('.accNavComponent button').click();
+      cy.get('header')
         .get('nav[aria-label="My Account"]')
         .a11yRunContinuumTest();
     });
@@ -32,13 +31,29 @@ describe('Header and Footer Continuum tests', () => {
 
   context('Footer', () => {
     it('Main footer body', () => {
-      cy.get('@footer').a11yRunContinuumTest();
+      cy.get('footer').a11yRunContinuumTest();
     });
 
     it('Consent Management dialog', () => {
-      cy.get('@footer').get('button').contains(' Consent Management').click();
+      cy.get('footer').get('button').contains(' Consent Management').click();
       cy.get('.modal-dialog').contains(' Select all ').click();
       cy.get('.modal-dialog').a11yRunContinuumTest();
+    });
+  });
+
+  viewportContext(['mobile'], () => {
+    it('Hamburger menu', () => {
+      cy.get('cx-hamburger-menu button').click();
+      cy.get('a').contains('Brands');
+      cy.get('header').a11yRunContinuumTest();
+
+      cy.get('button[aria-label="Brands"]').click();
+      cy.get('button').contains('Cameras');
+      cy.get('nav[aria-label="Category menu"]').a11yRunContinuumTest();
+
+      cy.get('button').contains('Cameras').click();
+      cy.get('a').contains('Canon');
+      cy.get('nav[aria-label="Category menu"]').a11yRunContinuumTest();
     });
   });
 });
