1b08ec0f6d589e7ff25ebca854e097fbba6d2704
Uros Lates <u.lates@sap.com>
2025-04-07 11:34:36 +0200

    fix: fixing PR comments

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/add-to-cart-dialog.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/add-to-cart-dialog.a11y-e2e.cy.ts
@@ -4,41 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { checkA11yConcerns } from '../../support/utils/a11y-continuum.utils';
 import * as siteContextSelector from '../../helpers/site-context-selector';
 
 /**
  * This test checks accessibility concerns on the add to cart modal (PLP Page) using Access Continuum
  */
-context('Add to Cart Modal Accessibility', () => {
-  before(() => {
+describe('Add to Cart Modal Accessibility', () => {
+  beforeEach(() => {
     cy.a11yContinuumSetup();
   });
 
-  describe('Add to Cart', () => {
-    before(() => {
-      const productDetailsPath = siteContextSelector.PRODUCT_PATH_2;
-      cy.visit(productDetailsPath);
-      cy.get('cx-add-to-cart button[type=submit]').click().wait(3000);
-      cy.get('cx-added-to-cart-dialog').within(() => {
-        //check for initial default values
-        cy.get('.cx-quantity cx-item-counter input').should('have.value', '1');
-        cy.get('.cx-dialog-total').should('contain', '1 item');
-        // cy.get('[aria-label="Close Modal"]').click();
-      });
+  context('Add to Cart (PDP)', () => {
+    it('Item Added to Cart Dialog', () => {
+      cy.visit(siteContextSelector.PRODUCT_PATH_2);
+      cy.get('cx-add-to-cart button[type=submit]').click();
+      cy.get('cx-added-to-cart-dialog')
+        .get('.cx-modal-content')
+        .a11yRunContinuumTest();
     });
-
-    checkA11yConcerns();
   });
 
-  // describe('Cart with Products', () => {
-  //   before(() => {
-  //     cart.addProducts();
-  //     cy.visit('/cart').wait(3000);
-  //     cy.get('cx-breadcrumb h1').should('contain', 'Your Shopping Cart');
-  //   });
-
-  //   // Run accessibility tests but don't fail the test if concerns are found
-  //   checkA11yConcerns(false);
-  // });
+  context('Add to Cart (PLP)', () => {
+    it('Item Added to Cart Dialog', () => {
+      cy.visit(siteContextSelector.PRODUCT_SEARCH_PATH);
+      cy.get('cx-add-to-cart button[type=submit]:first').click();
+      cy.get('cx-added-to-cart-dialog')
+        .get('.cx-modal-content')
+        .a11yRunContinuumTest();
+    });
+  });
 });
