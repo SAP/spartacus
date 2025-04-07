9e8bbe2cdb42ee8901296648ac26dc6dcb47a9e4
Stanislav Sukhanov <sukhanov.stas@gmail.com>
2025-04-07 22:44:31 +0200

    fix: (CXSPA-9649) Continuum E2Es - Order History
    * introduced a new test suite for checking accessibility concerns for order history
    * adressed accessibility concerns found by a test.
    
    * add unit tests;
    
    * closes: https://jira.tools.sap/browse/CXSPA-9649

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/order-history.e2e.cy.ts
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { viewportContext } from '../../helpers/viewport-context';
+
+describe('Order History Page accessibility', { testIsolation: false }, () => {
+  viewportContext(['mobile', 'desktop'], () => {
+    before(() => {
+      cy.a11yContinuumSetup();
+      cy.login('test-user-with-orders@sap.cx.com', 'pw4all');
+      cy.visit('/my-account/orders');
+    });
+
+    context('Order list', () => {
+      it('Page loaded', () => {
+        cy.get('main').a11yRunContinuumTest();
+      });
+    });
+
+    context('Order details', () => {
+      it('Page loaded', () => {
+        cy.get('.cx-order-history-code > .cx-order-history-value')
+          .first()
+          .click();
+
+        cy.get('main').a11yRunContinuumTest();
+      });
+    });
+  });
+});
