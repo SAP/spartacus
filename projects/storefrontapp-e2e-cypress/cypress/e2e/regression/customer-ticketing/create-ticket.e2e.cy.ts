cc563725fd644aa1ab5611f0c273c04bcf247e6c
i306028 <katarina.gladisova.kopunekova@sap.com>
2025-04-08 14:48:21 +0200

    chore:CXSPA-9749 Optimize e2e/regression/customer-ticketing/create-ticket.e2e.cy.ts Description: Refactor from 12 to 6 tests

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/customer-ticketing/create-ticket.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/customer-ticketing/create-ticket.e2e.cy.ts
@@ -6,7 +6,7 @@
 
 import * as customerTicketing from '../../../helpers/customer-ticketing/customer-ticketing';
 
-describe('ticketing', () => {
+describe('Ticketing', () => {
   context('Registered User', () => {
     before(() => {
       cy.window().then((win) => {
@@ -14,13 +14,9 @@ describe('ticketing', () => {
       });
     });
 
-    it('should open create new ticket popup when clicking add button (CXSPA-470)', () => {
-      customerTicketing.loginRegisteredUser();
-      customerTicketing.visitElectronicTicketListingPage();
-      customerTicketing.openCreateTicketPopup();
-    });
-
-    it('should be able to create ticket when filling the required form (CXSPA-470)', () => {
+    //refactored within CXSPA-9749
+    //includes the original tests 1, 2, 7 and 11
+    it('should be able to open the form and create ticket when filling the form properly (CXSPA-470)', () => {
       const testTicketDetails: customerTicketing.TestTicketDetails = {
         subject: 'Entering a subject',
         message: 'Typing a message',
@@ -33,8 +29,17 @@ describe('ticketing', () => {
       customerTicketing.loginRegisteredUser();
       customerTicketing.visitElectronicTicketListingPage();
       customerTicketing.openCreateTicketPopup();
+
+      cy.log('DO NOT CREATE A TICKET IF FORM NOT PROPERLY COMPLETED:');
+      customerTicketing.clickSubmit();
+      customerTicketing.verifyFieldValidationErrorShown();
+
+      cy.log(
+        'CREATE A TICKET ONCE FORM PROPERLY COMPLETED, VERIFY FORM CLOSE AND CREATION MESSAGE:'
+      );
       customerTicketing.fillTicketDetails(testTicketDetails);
       customerTicketing.clickSubmit();
+      customerTicketing.verifyCreateTicketPopupIsClosed();
       customerTicketing.verifyGlobalMessage();
       customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
     });
@@ -98,7 +103,8 @@ describe('ticketing', () => {
       customerTicketing.verifyTicketDoesNotExist(testTicketDetails);
     });
 
-    it('should be able to create a ticket with an empty attachment (CXSPA-470)', () => {
+    //why do we support empty attachments? is there any use case behind this scenario? it doesn't make sense to me
+    it.skip('should be able to create a ticket with an empty attachment (CXSPA-470)', () => {
       const testTicketDetails: customerTicketing.TestTicketDetails = {
         subject: 'Testing uploading attachment',
         message: 'Has the file been uploaded',
@@ -117,14 +123,6 @@ describe('ticketing', () => {
       customerTicketing.verifyGlobalMessage();
       customerTicketing.verifyCreatedTicketDetails(testTicketDetails);
       customerTicketing.verifyFileAttachedToMessage(testTicketDetails.filename);
-    });
-
-    it('should not allow ticket to be created if form not properly completed (CXSPA-470)', () => {
-      customerTicketing.loginRegisteredUser();
-      customerTicketing.visitElectronicTicketListingPage();
-      customerTicketing.openCreateTicketPopup();
-      customerTicketing.clickSubmit();
-      customerTicketing.verifyFieldValidationErrorShown();
     });
 
     it('should not create tickets when cancelling or closing the modal (CXSPA-470)', () => {
@@ -148,30 +146,13 @@ describe('ticketing', () => {
       customerTicketing.verifyTicketDoesNotExist(testTicketDetails);
     });
 
-    it('should not let subject exceeds 255 character limit (CXSPA-470)', () => {
+    it('should not let subject exceed 255 character limit and message exceed 5000 character limit (CXSPA-470)', () => {
       const TICKET_SUBJECT_MAX_LENGTH = 255;
+      const TICKET_MESSAGE_MAX_LENGTH = 5000;
       const testTicketDetails: customerTicketing.TestTicketDetails = {
         subject: customerTicketing.generateDummyStringOfLength(
           TICKET_SUBJECT_MAX_LENGTH + 1
         ),
-        message: 'Exceeding character limit',
-        ticketCategory: {
-          id: customerTicketing.TestCategory.complaint.toUpperCase(),
-          name: customerTicketing.TestCategory.complaint,
-        },
-      };
-      customerTicketing.loginRegisteredUser();
-      customerTicketing.visitElectronicTicketListingPage();
-      customerTicketing.openCreateTicketPopup();
-      customerTicketing.fillTicketDetails(testTicketDetails);
-      customerTicketing.clickSubmit();
-      customerTicketing.verifyTicketSubjectAndMessageDoNotExceedCharacterLimit();
-    });
-
-    it('should not let message exceeds 5000 character limit (CXSPA-470)', () => {
-      const TICKET_MESSAGE_MAX_LENGTH = 5000;
-      const testTicketDetails: customerTicketing.TestTicketDetails = {
-        subject: 'Exceeding character limit',
         message: customerTicketing.generateDummyStringOfLength(
           TICKET_MESSAGE_MAX_LENGTH + 1
         ),
@@ -188,25 +169,8 @@ describe('ticketing', () => {
       customerTicketing.verifyTicketSubjectAndMessageDoNotExceedCharacterLimit();
     });
 
-    it('should close create ticket popup upon submit (CXSPA-470)', () => {
-      const testTicketDetails: customerTicketing.TestTicketDetails = {
-        subject: 'Entering a subject',
-        message: 'Typing a message',
-        ticketCategory: {
-          id: customerTicketing.TestCategory.complaint.toUpperCase(),
-          name: customerTicketing.TestCategory.complaint,
-        },
-      };
-
-      customerTicketing.loginRegisteredUser();
-      customerTicketing.visitElectronicTicketListingPage();
-      customerTicketing.openCreateTicketPopup();
-      customerTicketing.fillTicketDetails(testTicketDetails);
-      customerTicketing.clickSubmit();
-      customerTicketing.verifyCreateTicketPopupIsClosed();
-    });
-
-    it('should not be able to see created ticket in other stores (CXSPA-470)', () => {
+    //what's the use case behind this test? sounds like backend related issue if a ticket is saved in such way - should be included in backend tests rather than the frontend ones
+    it.skip('should not be able to see created ticket in other stores (CXSPA-470)', () => {
       const testTicketDetails: customerTicketing.TestTicketDetails = {
         subject: 'Entering a subject',
         message: 'Typing a message',
