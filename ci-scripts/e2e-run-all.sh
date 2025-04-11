9195b8990193cdc0335f66e194d1386e31138f89
andreighervan7 <andrei.ghervan@sap.com>
2025-04-11 15:00:58 +0300

    update ci

--- ci-scripts/e2e-run-all.sh
+++ ci-scripts/e2e-run-all.sh
@@ -2,7 +2,57 @@
 set -e
 set -o pipefail
 
-echo "-----"
+POSITIONAL=()
+
+readonly help_display="Usage: $0 [ command_options ] [ param ]
+
+    command options:
+        --suite, -s                             e2e suite to run (b2c, b2b, cds, flaky). Default: b2c
+        --environment, --env                    [ 2005 | 2011 | ccv2]. Default: 2005
+        --help, -h                              show help
+        --ssr                                   Run ssr smoke test
+"
+
+while [ "${1:0:1}" == "-" ]; do
+    case "$1" in
+    '--suite' | '-s')
+        SUITE=":$2"
+        shift
+        shift
+        ;;
+    '--environment' | '--env')
+        CI_ENV=":$2"
+        shift
+        shift
+        ;;
+    '--ssr')
+        SSR=true
+        shift
+        ;;
+    '--help' | '-h')
+        echo "$help_display"
+        exit 0
+        ;;
+    *)
+        POSITIONAL+=("$1")
+        shift
+
+        echo "Error: unknown option: ${POSITIONAL}"
+        exit 1
+        ;;
+    esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+if [ "$SUITE" == ":ccv2" ]; then
+    export SPA_ENV='ccv2,b2c'
+fi
+
+if [ "$SUITE" == ":ccv2-b2b" ]; then
+    export SPA_ENV='ccv2,b2b'
+fi
+
 echo '-----'
 echo "Building Spartacus libraries"
 
@@ -40,9 +90,7 @@ is_bot_commit() {
     fi
 }
 
-echo "Running ALL E2E tests sequentially"
-
-npm run e2e:run
-
-echo "-----"
-echo "All E2E tests completed!"
+echo '-----'
+echo "Running full Cypress end-to-end tests"
+npm run e2e:run:ci
+   
