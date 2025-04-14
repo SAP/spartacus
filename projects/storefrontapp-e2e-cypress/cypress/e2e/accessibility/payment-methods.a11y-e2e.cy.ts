9827ef9768e021301a4059ff0c0491fe48dfd323
Stanislav Sukhanov <sukhanov.stas@gmail.com>
2025-04-14 13:49:00 +0200

    fix: (CXSPA-9651) Continuum E2Es - Payment Methods
    * introduced a new test suite for checking accessibility concerns for order history
    * addressed accessibility concerns found by a test.
    
    * closes: https://jira.tools.sap/browse/CXSPA-9651

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/payment-methods.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/payment-methods.a11y-e2e.cy.ts
@@ -4,29 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { viewportContext } from '../../helpers/viewport-context';
-
 describe('Payment Methods Page accessibility', { testIsolation: false }, () => {
-  viewportContext(['desktop'], () => {
-    before(() => {
-      cy.a11yContinuumSetup();
-      cy.login('test-user-with-orders@sap.cx.com', 'pw4all');
-      cy.visit('/my-account/payment-details');
-    });
+  before(() => {
+    cy.a11yContinuumSetup();
+    cy.login('test-user-with-orders@sap.cx.com', 'pw4all');
+    cy.visit('/my-account/payment-details');
+  });
 
-    it('Page loaded', () => {
-      cy.get('.cx-card');
-      cy.get('main').a11yRunContinuumTest();
-    });
+  it('Page loaded', () => {
+    cy.get('.cx-card');
+    cy.get('main').a11yRunContinuumTest();
+  });
 
-    it('Delete payment method', () => {
-      cy.get('cx-card').first().find('button').click();
-      cy.get('cx-card').first().a11yRunContinuumTest();
-    });
+  it('Delete payment method', () => {
+    cy.get('cx-card').first().find('button').click();
+    cy.get('cx-card').first().a11yRunContinuumTest();
+  });
 
-    it('Set as default', () => {
-      cy.get('cx-card').eq(1).find('button').first().click();
-      cy.get('.alert-success').a11yRunContinuumTest();
-    });
+  it('Set as default', () => {
+    cy.get('cx-card').eq(1).find('button').first().click();
+    cy.get('.alert-success').a11yRunContinuumTest();
   });
 });
