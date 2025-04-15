9565167df811d14624be818f8ff73200c4450f33
Petar Markov <petarmarkov9449@gmail.com>
2025-04-15 13:56:11 +0200

    remove comments

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/login-register-forgot-password.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/login-register-forgot-password.a11y-e2e.cy.ts
@@ -12,46 +12,37 @@ describe(
   { testIsolation: false },
   () => {
     beforeEach(() => {
-      cy.a11yContinuumSetup(); // Setup a11y scanning tool
+      cy.a11yContinuumSetup();
     });
 
     it('should pass a11y scan for the Login Page', () => {
       cy.visit('/login');
 
-      // Focus on the first input field to trigger validation (e.g., email field)
       cy.get('input[formControlName="userId"]').focus();
 
-      // Submit the form to trigger validation and validation hints
-      cy.get('form').submit(); // Trigger validation on form fields
+      cy.get('form').submit();
 
-      // Run full accessibility scan for the Login Page
-      cy.a11yRunContinuumTest(); // This will scan the whole page for a11y issues
+      cy.a11yRunContinuumTest();
     });
 
     it('should pass a11y scan for the Forgot Password Page', () => {
       cy.visit('/login/forgot-password');
 
-      // Focus on the email input to trigger validation and hints
       cy.get('input[formControlName="userEmail"]').focus();
 
-      // Submit the form to trigger validation and validation hints
-      cy.get('form').submit(); // Trigger validation on form fields
+      cy.get('form').submit();
 
-      // Run full accessibility scan for the Forgot Password Page
-      cy.a11yRunContinuumTest(); // This will scan the whole page for a11y issues
+      cy.a11yRunContinuumTest();
     });
 
     it('should pass a11y scan for the Register Page', () => {
       cy.visit('/login/register');
 
-      // Focus on the first input field (e.g., email field) to trigger validation
       cy.get('input[formControlName="email"]').focus();
 
-      // Submit the form to trigger validation and validation hints
-      cy.get('form').submit(); // Trigger validation on form fields
+      cy.get('form').submit();
 
-      // Run full accessibility scan for the Register Page
-      cy.a11yRunContinuumTest(); // This will scan the whole page for a11y issues
+      cy.a11yRunContinuumTest();
     });
   }
 );
