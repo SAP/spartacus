7a48e80e9a30840ee76d38d24cd8d8ea359d6bfd
Bose <saptarshi.bose01@sap.com>
2025-04-15 17:51:30 +0530

    updated condition for displaying cart-item-price-body component

--- feature-libs/cart/base/components/cart-shared/cart-item-list-row/cart-item-list-row.component.ts
+++ feature-libs/cart/base/components/cart-shared/cart-item-list-row/cart-item-list-row.component.ts
@@ -4,12 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Component, inject } from '@angular/core';
-import { CartItemContext } from '@spartacus/cart/base/root';
+import { Component, computed, inject, Signal } from '@angular/core';
+import { ActiveCartFacade, Cart, CartItemContext } from '@spartacus/cart/base/root';
 import { useFeatureStyles } from '@spartacus/core';
 import { CartItemComponent } from '../cart-item/cart-item.component';
 import { CartItemContextSource } from '../cart-item/model/cart-item-context-source.model';
 import { CartItemListComponentService } from './cart-item-list-row.component.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: '[cx-cart-item-list-row], cx-cart-item-list-row',
@@ -22,9 +23,17 @@ import { CartItemListComponentService } from './cart-item-list-row.component.ser
 })
 export class CartItemListRowComponent extends CartItemComponent {
   protected componentService = inject(CartItemListComponentService);
+  protected activeCart = inject(ActiveCartFacade);
   isFlagQuote = this.componentService.showBasePriceWithDiscount();
   constructor(cartItemContextSource: CartItemContextSource) {
     super(cartItemContextSource);
     useFeatureStyles('a11yQTY2Quantity');
   }
+  cart  = toSignal<Cart>(this.activeCart.getActive());
+  containsSubscriptions: Signal<boolean> = computed(() => {
+    return this.cart()?.entries?.some((entry) => {
+      return entry.product?.productTypes === 'SUBSCRIPTION';
+    }
+    ) ?? false;
+  });
 }
