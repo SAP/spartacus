9aa6a233419fcdcf0cea4b3097d100f2ac2d3add
andreighervan7 <andrei.ghervan@sap.com>
2025-04-18 09:25:25 +0300

    update ci

--- projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/segment-refs/segment-refs.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/segment-refs/segment-refs.e2e.cy.ts
@@ -8,21 +8,26 @@ import { interceptGet } from '../../../support/utils/intercept';
 
 export const APPAREL_BASESITE = 'apparel-uk-spa';
 export const APPAREL_CURRENCY = 'EUR';
+
 describe('Segment Reference', () => {
   beforeEach(() => {
     cy.restoreLocalStorage();
     Cypress.env('BASE_SITE', APPAREL_BASESITE);
     Cypress.env('BASE_CURRENCY', APPAREL_CURRENCY);
+
+    interceptGet('segmentRefApi', '**/cms/pages**');
   });
+
   it('should fetch appropriate banner customization', () => {
-    interceptGet('segmentRefApi', '/cms/pages*');
     cy.visit(
       `${Cypress.env('BASE_SITE')}/${Cypress.env('BASE_LANG')}/${Cypress.env(
         'BASE_CURRENCY'
       )}/?segmentrefs=footwear`
     );
-    cy.wait('@segmentRefApi').then((xhr) => {
+    cy.wait('@segmentRefApi', { timeout: 60000 }).then((xhr) => {
       expect(xhr.request.headers).to.have.property('segmentrefs', 'footwear');
+      expect(xhr.response.statusCode).to.eq(200); // Extra verification
     });
   });
 });
+
