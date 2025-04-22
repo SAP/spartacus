2e24c8b345b5d91c5f733b7d563c8f7a34ab4180
Florent Letendre <florent.letendre@hotmail.fr>
2025-04-22 15:35:31 -0400

    remove unused code

--- integration-libs/punchout/components/punchout-inspect-cart/punchout-inspect-cart.component.ts
+++ integration-libs/punchout/components/punchout-inspect-cart/punchout-inspect-cart.component.ts
@@ -10,16 +10,9 @@ import {
   inject,
   OnInit,
 } from '@angular/core';
-import { CartConfigService } from '@spartacus/cart/base/core';
-import {
-  ActiveCartFacade,
-  Cart,
-  OrderEntry,
-  PromotionLocation,
-  SelectiveCartFacade,
-} from '@spartacus/cart/base/root';
-import { AuthService, RoutingService, useFeatureStyles } from '@spartacus/core';
-import { combineLatest, filter, map, Observable, of, tap } from 'rxjs';
+import { ActiveCartFacade, Cart, OrderEntry } from '@spartacus/cart/base/root';
+import { AuthService, useFeatureStyles } from '@spartacus/core';
+import { combineLatest, filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'cx-punchout-inspect-cart',
@@ -31,15 +24,9 @@ export class PunchoutInspectCartComponent implements OnInit {
   cart$: Observable<Cart>;
   entries$: Observable<OrderEntry[]>;
   cartLoaded$: Observable<boolean>;
-  loggedIn = false;
-  promotionLocation: PromotionLocation = PromotionLocation.ActiveCart;
-  selectiveCartEnabled: boolean;
 
   protected activeCartService = inject(ActiveCartFacade);
-  protected selectiveCartService = inject(SelectiveCartFacade);
   protected authService = inject(AuthService);
-  protected routingService = inject(RoutingService);
-  protected cartConfig = inject(CartConfigService);
 
   constructor() {
     useFeatureStyles('a11yQTY2Quantity');
@@ -53,17 +40,7 @@ export class PunchoutInspectCartComponent implements OnInit {
 
     this.cartLoaded$ = combineLatest([
       this.activeCartService.isStable(),
-      this.selectiveCartEnabled
-        ? this.selectiveCartService.isStable()
-        : of(false),
       this.authService.isUserLoggedIn(),
-    ]).pipe(
-      tap(([, , loggedIn]) => (this.loggedIn = loggedIn)),
-      map(([cartLoaded, sflLoaded, loggedIn]) =>
-        loggedIn && this.selectiveCartEnabled
-          ? cartLoaded && sflLoaded
-          : cartLoaded
-      )
-    );
+    ]).pipe(map(([cartLoaded, loggedIn]) => loggedIn && cartLoaded));
   }
 }
