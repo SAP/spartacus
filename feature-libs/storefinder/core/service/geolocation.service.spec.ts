485d3bced2f4bea228d93dbb570c863d3e6c5f5b
Pucek9 <pucek9@gmail.com>
2025-04-23 18:46:15 +0200

    fix/CXSPA-9546: fix ci

--- feature-libs/storefinder/core/service/geolocation.service.spec.ts
+++ feature-libs/storefinder/core/service/geolocation.service.spec.ts
@@ -9,39 +9,38 @@ const location: PointOfService = {
   },
 };
 
-describe('StoreFinderService', () => {
+describe('GeolocationService', () => {
   let service: GeolocationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [GeolocationService],
     });
-
     service = TestBed.inject(GeolocationService);
+  });
 
-    it('should inject GeolocationService', inject(
-      [GeolocationService],
-      (geolocationService: GeolocationService) => {
-        expect(geolocationService).toBeTruthy();
-      }
-    ));
+  it('should inject GeolocationService', inject(
+    [GeolocationService],
+    (geolocationService: GeolocationService) => {
+      expect(geolocationService).toBeTruthy();
+    }
+  ));
 
-    it('should create', () => {
-      expect(service).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
 
-    it('should return store latitude', () => {
-      expect(service.getStoreLatitude(location)).toBe(35.528984);
-    });
+  it('should return store latitude', () => {
+    expect(service.getStoreLatitude(location)).toBe(35.528984);
+  });
 
-    it('should return store longitude', () => {
-      expect(service.getStoreLongitude(location)).toBe(139.700168);
-    });
+  it('should return store longitude', () => {
+    expect(service.getStoreLongitude(location)).toBe(139.700168);
+  });
 
-    it('should return url for navigation', () => {
-      expect(service.getDirections(location)).toBe(
-        'https://www.google.com/maps/dir/Current+Location/35.528984,139.700168'
-      );
-    });
+  it('should return url for navigation', () => {
+    expect(service.getDirections(location)).toBe(
+      'https://www.google.com/maps/dir/Current+Location/35.528984,139.700168'
+    );
   });
 });
