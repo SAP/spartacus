f9c9bdfbb6d4dcb2b281f9360c067bef06968b0e
Jedrychowski Emil <emil.jedrychowski@sap.com>
2025-04-23 17:20:18 +0200

    CXIEP-7764: Remove renamed files

--- feature-libs/order/components/order-details/order-detail-attachments/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './order-detail-attachments.component';
-export * from './attachments-dialog/order-detail-attachments-dialog.component';
