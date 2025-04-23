6a307c7ffe129f7d5ec8749719a812d1ca98496d
Caine Rotherham <rotherham.c@gmail.com>
2025-04-23 15:38:33 +0200

    prettier

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/add-to-cart-dialog.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/add-to-cart-dialog.a11y-e2e.cy.ts
@@ -19,7 +19,7 @@ describe('Add to Cart Modal Accessibility', () => {
       cy.visit(siteContextSelector.PRODUCT_PATH_2);
       cy.get('cx-add-to-cart button[type=submit]').click();
       cy.get('cx-added-to-cart-dialog .cx-modal-content cx-cart-item');
-      cy.get('cx-added-to-cart-dialog').a11yRunContinuumTest()
+      cy.get('cx-added-to-cart-dialog').a11yRunContinuumTest();
     });
   });
 });
