d254745357c15438fe6aca9c64c861662b0f9641
PioBar <bartkowiakpiotr0@gmail.com>
2025-04-24 11:04:44 +0200

    ASM continuum tests

--- projects/storefrontlib/shared/components/ng-select-a11y/ng-select-a11y.directive.ts
+++ projects/storefrontlib/shared/components/ng-select-a11y/ng-select-a11y.directive.ts
@@ -115,6 +115,19 @@ export class NgSelectA11yDirective implements AfterViewInit {
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((state) => {
         this.renderer.setAttribute(inputCombobox, 'aria-expanded', state);
+        if (
+          ariaLabel &&
+          this.featureConfigService.isEnabled('a11yNgSelectAriaControls')
+        ) {
+          // Delay execution to come after the ng-select's own 'aria-controls' logic
+          setTimeout(() => {
+            this.renderer.setAttribute(
+              inputCombobox,
+              'aria-controls',
+              ariaControls
+            );
+          });
+        }
       });
 
     const ariaLabel = this.cxNgSelectA11y.ariaLabel;
@@ -125,7 +138,10 @@ export class NgSelectA11yDirective implements AfterViewInit {
       this.renderer.setAttribute(inputCombobox, ARIA_LABEL, ariaLabel);
     }
 
-    if (ariaControls) {
+    if (
+      ariaControls &&
+      !this.featureConfigService.isEnabled('a11yNgSelectAriaControls')
+    ) {
       this.renderer.setAttribute(inputCombobox, 'aria-controls', ariaControls);
     }
 
