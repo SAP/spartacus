22a99353fd85e2bb9c3b57f4d218b3d5472ba6ec
andreighervan7 <andrei.ghervan@sap.com>
2025-04-30 15:21:23 +0300

    update ci

---
+++ ci-scripts/e2e-cypress-vendor.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+POSITIONAL=()
+
+readonly help_display="Usage: $0 [ command_options ] [ param ]
+
+    command options:
+        --suite, -s                             e2e suite to run (b2c, b2b, cds, flaky, a11y). Default: b2c
+        --environment, --env                    [ 2005 | 2011 | ccv2]. Default: 2005
+        --help, -h                              show help
+        --ssr                                   Run ssr smoke test
+"
+
+while [ "${1:0:1}" == "-" ]; do
+    case "$1" in
+    '--suite' | '-s')
+        SUITE=":$2"
+        shift
+        shift
+        ;;
+    '--environment' | '--env')
+        CI_ENV=":$2"
+        shift
+        shift
+        ;;
+    '--ssr')
+        SSR=true
+        shift
+        ;;
+    '--help' | '-h')
+        echo "$help_display"
+        exit 0
+        ;;
+    *)
+        POSITIONAL+=("$1")
+        shift
+
+        echo "Error: unknown option: ${POSITIONAL}"
+        exit 1
+        ;;
+    esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+# if [ "$SUITE" == ":opf" ]; then
+#     export SPA_ENV='opf'
+# fi
+
+echo '-----'
+echo "Building Spartacus libraries"
+
+export NODE_OPTIONS=--dns-result-order=ipv4first
+
+npm ci
+
+(cd projects/storefrontapp-e2e-cypress && npm ci)
+
+npm run build:libs 2>&1 | tee build.log
+
+results=$(grep "Warning: Can't resolve all parameters for" build.log || true)
+if [[ -z "${results}" ]]; then
+    echo "Success: Spartacus production build was successful."
+    rm build.log
+else
+    echo "ERROR: Spartacus production build failed. Check the import statements. 'Warning: Can't resolve all parameters for ...' found in the build log."
+    rm build.log
+    exit 1
+fi
+echo '-----'
+echo "Building Spartacus storefrontapp"
+npm run build
+
+npm run start:pwa &
+
+echo '-----'
+echo "Running Cypress end to end tests"
+
+if [ "$SUITE" == ":opf" ]; then
+    echo "Running Cypress end to end tests for pull request"
+
+    npm run e2e:run:ci:opf
+elif [ "$SUITE" == ":cdc" ]; then
+    echo "Running Cypress end to end tests for CDC"
+
+    npm run e2e:run:ci:cdc
+elif [ "$SUITE" == ":cdc-b2b" ]; then
+    echo "Running Cypress end to end tests for CDC B2B"
+
+    npm run e2e:run:ci:cdc-b2b
+else
+    echo "Running vendor cypress tests"
+
+    npm run e2e:run:ci:vendor
+fi
