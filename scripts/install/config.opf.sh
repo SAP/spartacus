0feb71382da31b6d2584ba3964bd9d72ea5a39f5
Mateusz Kolasa <mateusz.kolasa@sap.com>
2025-05-02 14:00:21 +0200

    Update schematics

--- scripts/install/config.opf.sh
+++ scripts/install/config.opf.sh
@@ -4,7 +4,7 @@ ADD_OPF=true
 
 BACKEND_URL="https://api.cp96avkh5f-integrati2-d2-public.model-t.cc.commerce.ondemand.com"
 BASE_SITE="electronics-spa-standalone"
-
+ADD_B2B_LIBS=true
 # The base URL and public key values are required for connection to Cloud Commerce Adapter (OPF)
 OPF_BASE_URL="https://cp96avkh5f-integrati2-d2.opf.commerce.stage.context.cloud.sap/commerce-cloud-adapter-stage/storefront"
 OPF_CLIENT_PUBLIC_KEY="k2N3m3TJPLragwia5ZUvS/qkIPVQoy5qjUkOAB6Db+U="
