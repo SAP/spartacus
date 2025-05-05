81c3cde19c6c5c4622fc7d21526b31f93af654b4
Krzysztof Platis <platonn.git@gmail.com>
2025-05-05 13:38:33 +0200

    fix: add missing public api exports

--- core-libs/setup/ssr/optimized-engine/index.ts
+++ core-libs/setup/ssr/optimized-engine/index.ts
@@ -4,8 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export * from './convert-to-bytes';
+export * from './default-cache-entry-size-calculator';
 export * from './optimized-ssr-engine';
 export * from './rendering-cache';
+export * from './rendering-cache-size-manager';
 export * from './rendering-cache.model';
 export * from './rendering-strategy-resolver';
 export * from './rendering-strategy-resolver-options';
