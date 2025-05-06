bd0342ddd52bdbf21c7796be2b6246193f197b5f
rmch91 <r.chygryn@sap.com>
2025-05-06 14:56:14 +0200

    bugfix: Demo ci checks

---
+++ feature-libs/ci-check-test/public_api.ts
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * Public API Surface of ci-check-test
+ */
+export * from './store-finder.module';
---
+++ feature-libs/ci-check-test/test.ts
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+// Patching Object.defineProperty unlocks frozen JS symbols and makes possible to mock them.
+// Should be used with caution, and only if there is no other way to mock stuff (eg. by DI)
+// Has to be imported just after zone.js imports.
+import 'testing/patch-object-define-property';
+
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: false },
+  }
+);
