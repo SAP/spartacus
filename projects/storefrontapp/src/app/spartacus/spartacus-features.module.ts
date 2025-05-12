aff2bedc27b5981ebb8140b98c8f1fe9fb49b7a1
Eric Polesky <eric.polesky@sap.com>
2025-05-12 13:51:01 -0400

    chore: add explicit injector config to USE_AUTHORIZATION_CODE_FLOW_BY_DEFAULT
    InjectionTokens will implicitly use `providedIn: 'root'` when given a factory.
    To avoid confusion, we now specify this explicitly.

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -15,7 +15,6 @@ import {
   ProductOccModule,
   UserModule,
   UserOccModule,
-  provideAuthorizationCodeFlowByDefault,
   provideFeatureTogglesFactory,
 } from '@spartacus/core';
 import {
@@ -287,7 +286,6 @@ if (environment.cpq) {
       provide: USE_MY_ACCOUNT_V2_NOTIFICATION_PREFERENCE,
       useValue: environment.myAccountV2,
     },
-    provideAuthorizationCodeFlowByDefault(false),
     provideFeatureTogglesFactory(() => {
       const appFeatureToggles: Required<FeatureToggles> = {
         showDeliveryOptionsTranslation: true,
