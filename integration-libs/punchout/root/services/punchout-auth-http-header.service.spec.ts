2babbd8c72c29538c38fc0ad2a4169d8ff44da39
Florent Letendre <florent.letendre@hotmail.fr>
2025-05-12 16:35:53 -0400

    add unit tests

--- integration-libs/punchout/root/services/punchout-auth-http-header.service.spec.ts
+++ integration-libs/punchout/root/services/punchout-auth-http-header.service.spec.ts
@@ -1,4 +1,3 @@
-import { HttpHandler, HttpRequest } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import {
   AuthRedirectService,
@@ -12,12 +11,7 @@ import {
 } from '@spartacus/core';
 import { of } from 'rxjs';
 import { PunchoutFacade } from '../facade';
-import {
-  PUNCHOUT_SESSION_KEY,
-  PunchOutLevel,
-  PunchOutOperation,
-  PunchoutSession,
-} from '../model';
+import { PunchOutLevel, PunchOutOperation, PunchoutSession } from '../model';
 import { PunchoutAuthHttpHeaderService } from './punchout-auth-http-header.service';
 import { PunchoutDetectionService } from './punchout-detection.service';
 import { PunchoutStoreService } from './punchout-store.service';
@@ -98,8 +92,6 @@ describe('PunchoutAuthHttpHeaderService', () => {
   let service: PunchoutAuthHttpHeaderService;
   let authService: AuthService;
   let punchoutDetectionService: PunchoutDetectionService;
-  let globalMessageService: GlobalMessageService;
-  let routingService: RoutingService;
   let punchoutStoreService: PunchoutStoreService;
   let punchoutfacade: PunchoutFacade;
   beforeEach(() => {
@@ -126,8 +118,6 @@ describe('PunchoutAuthHttpHeaderService', () => {
     service = TestBed.inject(PunchoutAuthHttpHeaderService);
     authService = TestBed.inject(AuthService);
     punchoutDetectionService = TestBed.inject(PunchoutDetectionService);
-    globalMessageService = TestBed.inject(GlobalMessageService);
-    routingService = TestBed.inject(RoutingService);
     punchoutStoreService = TestBed.inject(PunchoutStoreService);
     punchoutfacade = TestBed.inject(PunchoutFacade);
   });
@@ -138,17 +128,18 @@ describe('PunchoutAuthHttpHeaderService', () => {
 
   it('should handleExpiredRefreshToken call super coreLogout when not Punchout Session page', async () => {
     spyOn(authService, 'coreLogout').and.callThrough();
+    spyOn(punchoutfacade, 'logoutPunchoutUser').and.callThrough();
     spyOn(punchoutDetectionService, 'isPunchoutSessionPage').and.returnValue(
       false
     );
     spyOn(punchoutStoreService, 'getPunchoutSessionId').and.returnValue('');
-    spyOn(globalMessageService, 'remove').and.callThrough();
+    spyOn(punchoutfacade, 'endPunchoutSession').and.callThrough();
 
     service.handleExpiredRefreshToken();
     await Promise.resolve();
-
     expect(authService.coreLogout).toHaveBeenCalled();
-    expect(globalMessageService.remove).not.toHaveBeenCalled();
+    expect(punchoutfacade.logoutPunchoutUser).not.toHaveBeenCalled();
+    expect(punchoutfacade.endPunchoutSession).not.toHaveBeenCalled();
   });
 
   it('should handleExpiredRefreshToken silently logout when Punchout Session page', async () => {
@@ -158,56 +149,30 @@ describe('PunchoutAuthHttpHeaderService', () => {
     );
     spyOn(punchoutDetectionService, 'isPunchoutSession').and.returnValue(true);
     spyOn(punchoutfacade, 'endPunchoutSession').and.callThrough();
+    spyOn(punchoutfacade, 'logoutPunchoutUser').and.callThrough();
 
     service.handleExpiredRefreshToken();
     await Promise.resolve();
 
-    expect(punchoutfacade.endPunchoutSession).toHaveBeenCalledWith();
+    expect(punchoutfacade.logoutPunchoutUser).toHaveBeenCalledWith();
+    expect(punchoutfacade.endPunchoutSession).not.toHaveBeenCalledWith();
+    expect(authService.coreLogout).not.toHaveBeenCalled();
   });
 
-  it('should handleExpiredAccessToken without occ punchout request not redirect to any page', async () => {
-    const initialToken: AuthToken = {
-      access_token: `old_token`,
-      access_token_stored_at: '123',
-      refresh_token: 'ref_token',
-    };
-    const handler = (a: any) => of(a);
-    spyOn(routingService, 'goByUrl').and.callThrough();
-    spyOn(punchoutStoreService, 'getPunchoutSessionId').and.returnValue('');
-
-    await service.handleExpiredAccessToken(
-      new HttpRequest(
-        'GET',
-        `some-server/occ/test?${PUNCHOUT_SESSION_KEY}${mockSessionId}`
-      ),
-      { handle: handler } as HttpHandler,
-      initialToken
-    );
-
-    expect(routingService.goByUrl).not.toHaveBeenCalled();
-  });
-
-  it('should handleExpiredAccessToken in active punchout session to call logout', async () => {
-    const initialToken: AuthToken = {
-      access_token: `old_token`,
-      access_token_stored_at: '123',
-      refresh_token: 'ref_token',
-    };
-    const handler = (a: any) => of(a);
-
+  it('should handleExpiredRefreshToken endPunchoutSession when Punchout Session already exists', async () => {
+    spyOn(authService, 'coreLogout').and.callThrough();
     spyOn(punchoutDetectionService, 'isPunchoutSessionPage').and.returnValue(
-      true
+      false
     );
     spyOn(punchoutDetectionService, 'isPunchoutSession').and.returnValue(true);
     spyOn(punchoutfacade, 'endPunchoutSession').and.callThrough();
+    spyOn(punchoutfacade, 'logoutPunchoutUser').and.callThrough();
 
-    await service.handleExpiredAccessToken(
-      new HttpRequest('GET', `some-server`),
-      { handle: handler } as HttpHandler,
-      initialToken
-    );
+    service.handleExpiredRefreshToken();
+    await Promise.resolve();
 
-    // navigate with empty sessionId then punchout facade will take care of error handling
+    expect(punchoutfacade.logoutPunchoutUser).not.toHaveBeenCalledWith();
     expect(punchoutfacade.endPunchoutSession).toHaveBeenCalledWith();
+    expect(authService.coreLogout).not.toHaveBeenCalled();
   });
 });
