92764148d11d927004fd6de2b0314053a3349044
Hak Woo Kim <kimhakwoo@hotmail.com>
2025-05-13 12:15:26 -0400

    feat: added global messages for loading status (CXSPA-10017) (#20299)
    Co-authored-by: Hakwoo Kim <hakwoo.kim@sap.com>

--- integration-libs/punchout/components/punchout-session/punchout-session.component.ts
+++ integration-libs/punchout/components/punchout-session/punchout-session.component.ts
@@ -11,29 +11,41 @@ import {
   OnInit,
 } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { GlobalMessageService, GlobalMessageType } from '@spartacus/core';
 import { PUNCHOUT_SESSION_KEY, PunchoutFacade } from '@spartacus/punchout/root';
 import { switchMap, take } from 'rxjs';
 @Component({
   selector: 'cx-punchout-session',
-  template: `<p>Punchout session loading</p> `,
+  template: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
 export class PunchoutSessionComponent implements OnInit {
   protected activatedRoute = inject(ActivatedRoute);
   protected punchoutFacade = inject(PunchoutFacade);
+  protected globalMessageService = inject(GlobalMessageService);
 
   ngOnInit(): void {
     this.activatedRoute.queryParams
       .pipe(
         take(1),
-        switchMap((param: Params) =>
-          this.punchoutFacade.getPunchoutSession({
+        switchMap((param: Params) => {
+          this.globalMessageService.add(
+            {
+              key: 'punchout.initiatingUserSession',
+            },
+            GlobalMessageType.MSG_TYPE_INFO
+          );
+          return this.punchoutFacade.getPunchoutSession({
             punchoutSessionId: param?.[PUNCHOUT_SESSION_KEY],
-          })
-        ),
+          });
+        }),
         take(1)
       )
-      .subscribe();
+      .subscribe({
+        complete: () => {
+          this.globalMessageService.remove(GlobalMessageType.MSG_TYPE_INFO);
+        },
+      });
   }
 }
