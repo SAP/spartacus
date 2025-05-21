f465aa4f9d1962cd2e6f0173a543a1498da78fdd
Anjana BL <anjana.b.l@sap.com>
2025-05-21 15:31:39 +0530

    Update spartacus-features.module.ts

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -298,7 +298,6 @@ if (environment.cpq) {
         showStyleChangesInASM: false,
         shouldHideAddToCartForUnpurchasableProducts: false,
         showOrderQuoteLink: true,
-        showPromotionsInPDP: true,
         searchBoxV2: false,
         trendingSearches: false,
         useProductCarouselBatchApi: true,
