2c681d0c221f6f55bd990d5ad0f5d2517cf679e5
Florent Letendre <florent.letendre01@sap.com>
2025-05-21 17:02:45 +0200

    fix: Skip page redirection when initial requisition is fetched (#20346)
    CXSPA-10019
    Page redirection is skipped when initial requisition is fetched.

--- integration-libs/punchout/core/facade/punchout.service.ts
+++ integration-libs/punchout/core/facade/punchout.service.ts
@@ -114,14 +114,15 @@ export class PunchoutService implements PunchoutFacade {
    * getPunchoutRequisition workflow:
    * Ensure user is logged-in
    * get punchoutSessionId from PunchoutState
+   * Redirect user to homePage if method is called from Requisition page manual navigation
    * Get PunchoutSessionRequisition from  occ api OR from PunchoutState
    * Redirect to Punchout Error page if error occurs
    */
 
   protected getPunchoutRequisitionCommand: Command<
-    undefined,
+    boolean,
     PunchoutRequisition
-  > = this.commandService.create(() => {
+  > = this.commandService.create((isInitialRequisition: boolean) => {
     return this.punchoutAuthService.isUserLoggedIn().pipe(
       switchMap((isLoggedIn) => {
         return isLoggedIn
@@ -131,7 +132,10 @@ export class PunchoutService implements PunchoutFacade {
       take(1),
       switchMap((punchoutState: PunchoutState) => {
         // prevent manual navigation to requisition page
+        // isInitialRequisition handles use case where this method is called out of Requisition page,
+        // when fetching the initial requisition object from setPunchoutInitialRequisition().
         if (
+          !isInitialRequisition &&
           punchoutState?.closePunchoutSession === undefined &&
           punchoutState?.cancelRequisition === undefined
         ) {
@@ -268,8 +272,10 @@ export class PunchoutService implements PunchoutFacade {
     return this.getPunchoutSessionCommand.execute(punchoutSessionInput);
   }
 
-  getPunchoutSessionRequisition(): Observable<PunchoutRequisition | undefined> {
-    return this.getPunchoutRequisitionCommand.execute(undefined);
+  getPunchoutSessionRequisition(
+    isInitialRequisition = false
+  ): Observable<PunchoutRequisition | undefined> {
+    return this.getPunchoutRequisitionCommand.execute(isInitialRequisition);
   }
 
   logoutPunchoutUser(): Observable<boolean> {
@@ -330,8 +336,17 @@ export class PunchoutService implements PunchoutFacade {
     );
   }
 
+  /**
+   * Take a snapshot of initial requisition and store it in the state.
+   * This is used to send the initial cart snapshot in CXML when the user
+   * closes the punchout session via 'Close Session' button.
+   *
+   * This method is called only in EDIT mode.
+   * @returns
+   */
   protected setPunchoutInitialRequisition(): void {
-    this.getPunchoutSessionRequisition()
+    // isInitialRequisition is set to true as hitting initial Requisition use case.
+    this.getPunchoutSessionRequisition(true)
       .pipe(take(1))
       .subscribe({
         next: (punchoutRequisition) => {
