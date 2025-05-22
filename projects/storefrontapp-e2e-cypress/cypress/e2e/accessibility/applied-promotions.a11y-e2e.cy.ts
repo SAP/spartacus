8ee639729c2165ead978a9e8f56902250879b00a
Petar Markov <petarmarkov9449@gmail.com>
2025-05-22 13:35:25 +0200

    remove unncessary test case for PDP

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/applied-promotions.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/applied-promotions.a11y-e2e.cy.ts
@@ -11,24 +11,14 @@ describe('Applied Promotions Accessibility', { testIsolation: false }, () => {
     cy.requireLoggedIn();
   });
 
-  it('should show promotions in Add to Cart modal', () => {
+  it('should show all applied promotions on Cart page', () => {
     cy.visit(`/product/${ProductCode}`);
+
     cy.get('cx-add-to-cart')
       .findByText(/Add To Cart/i)
       .click();
-
-    cy.get('.cx-promotions')
-      .filter(':visible')
-      .each(($el) => {
-        if ($el.text().trim().length > 0) {
-          cy.wrap($el).a11yRunContinuumTest();
-        }
-      });
-
     cy.get('cx-added-to-cart-dialog button.close').click({ force: true });
-  });
 
-  it('should show all applied promotions on Cart page', () => {
     cy.get('cx-mini-cart').click();
     cy.get('.cart-details-wrapper');
 
