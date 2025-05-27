d0959617113597df545327d16436c289df933090
Michał Gruca <pucek9@gmail.com>
2025-05-27 18:37:22 +0200

    feat/CXSPA-10240 revert to use allowedUrls

--- integration-libs/punchout/root/config/default-punchout-guard-config.ts
+++ integration-libs/punchout/root/config/default-punchout-guard-config.ts
@@ -7,6 +7,13 @@
 import { PunchoutNavigationGuardConfig } from './punchout-guard-config';
 import { PunchOutOperation } from '../model';
 
+const allowedUrlsForAll = [
+  'punchout/cxml/session',
+  'punchout/cxml/requisition',
+];
+
+const allowedUrlsForInspect = ['punchout/cxml/inspect'];
+
 const allowedCxRoutesForEditCreate: string[] = [
   'category',
   'brand',
@@ -21,29 +28,17 @@ export const defaultPunchoutNavigationGuardConfig: PunchoutNavigationGuardConfig
   {
     punchoutNavigation: {
       [PunchOutOperation.INSPECT]: {
-        allowedCxRoutes: [
-          'punchoutSession',
-          'punchoutRequisition',
-          'punchoutInspect',
-        ],
+        allowedUrls: [...allowedUrlsForAll, ...allowedUrlsForInspect],
         redirectPage: { cxRoute: 'punchoutInspect' },
       },
       [PunchOutOperation.EDIT]: {
-        allowedUrls: ['/'],
-        allowedCxRoutes: [
-          ...allowedCxRoutesForEditCreate,
-          'punchoutSession',
-          'punchoutRequisition',
-        ],
+        allowedUrls: [...allowedUrlsForAll, '/'],
+        allowedCxRoutes: [...allowedCxRoutesForEditCreate],
         redirectPage: { cxRoute: 'home' },
       },
       [PunchOutOperation.CREATE]: {
-        allowedUrls: ['/'],
-        allowedCxRoutes: [
-          ...allowedCxRoutesForEditCreate,
-          'punchoutSession',
-          'punchoutRequisition',
-        ],
+        allowedUrls: [...allowedUrlsForAll, '/'],
+        allowedCxRoutes: [...allowedCxRoutesForEditCreate],
         redirectPage: { cxRoute: 'home' },
       },
     },
