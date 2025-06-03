f18584e3ab3975fdec0c8f725a2aff0c45294e3a
Petar Markov <petarmarkov9449@gmail.com>
2025-06-03 10:31:34 +0200

    check if theme applied correctly

--- projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/site-theme.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/accessibility/site-theme.a11y-e2e.cy.ts
@@ -26,16 +26,21 @@ context('Continuum Theme Switching Context', { testIsolation: false }, () => {
 
   it('should scan storefront after changing theme to HC-Dark', () => {
     cy.get('cx-site-theme-switcher select').select('HC-Dark');
+    cy.get('cx-storefront.cx-theme-high-contrast-dark');
     cy.get('main').a11yRunContinuumTest();
   });
 
   it('should scan storefront after changing theme to HC-Light', () => {
     cy.get('cx-site-theme-switcher select').select('HC-Light');
+    cy.get('cx-storefront.cx-theme-high-contrast-light');
     cy.get('main').a11yRunContinuumTest();
   });
 
   it('should scan storefront after reverting theme to Default', () => {
     cy.get('cx-site-theme-switcher select').select('Default');
+    cy.get(
+      'cx-storefront:not(.cx-theme-high-contrast-dark):not(.cx-theme-high-contrast-light)'
+    );
     cy.get('main').a11yRunContinuumTest();
   });
 });
