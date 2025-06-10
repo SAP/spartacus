6c571e7a02719020095cfab16f7dbb059edf23b1
Petar Markov <petarmarkov9449@gmail.com>
2025-06-10 13:55:26 +0200

    a11yCxMessageFocus

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -327,7 +327,6 @@ if (environment.cpq) {
         headerLayoutForSmallerViewports: true,
         a11yImprovedErrorMessage: true,
         a11yStoreFinderLabel: true,
-        a11yCxMessageFocus: true,
         occCartNameAndDescriptionInHttpRequestBody: true,
         a11yLinkBtnsToTertiaryBtns: true,
         a11yRepeatedPageTitleFix: true,
