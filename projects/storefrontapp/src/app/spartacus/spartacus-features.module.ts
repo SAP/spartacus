3d3678eec7f8efafc0e5a293d9958e1e25522881
Grace Dong <127385561+i53577@users.noreply.github.com>
2025-06-10 16:25:16 +0800

    feat: Remove feature toggle showSearchingCustomerByOrderInASM and showStyleChangesInASM (#20427)

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -294,8 +294,6 @@ if (environment.cpq) {
       const appFeatureToggles: Required<FeatureToggles> = {
         showDeliveryOptionsTranslation: true,
         formErrorsDescriptiveMessages: true,
-        showSearchingCustomerByOrderInASM: true,
-        showStyleChangesInASM: false,
         shouldHideAddToCartForUnpurchasableProducts: false,
         searchBoxV2: false,
         trendingSearches: false,
