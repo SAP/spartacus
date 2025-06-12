0894843eee979b3d508929485e455aad8284eab9
Norbert Papp <norbert.papp02@sap.com>
2025-06-12 12:04:15 +0200

    feat: replace withHttpTransferCacheOptions with withNoHttpTransferCache in client hydration provider

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -18,7 +18,7 @@ import {
   BrowserModule,
   provideClientHydration,
   withEventReplay,
-  withHttpTransferCacheOptions,
+  withNoHttpTransferCache,
 } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -67,12 +67,7 @@ if (!environment.production) {
   ],
   providers: [
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
-    provideClientHydration(
-      withEventReplay(),
-      withHttpTransferCacheOptions({
-        includeHeaders: ['X-Anonymous-Consents'],
-      })
-    ),
+    provideClientHydration(withEventReplay(), withNoHttpTransferCache()),
     provideConfig(<OccConfig>{
       backend: {
         occ: {
