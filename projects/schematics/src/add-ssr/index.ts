169c31fec00a681ce56791d851b1a12242f7c78b
Norbert Papp <norbert.papp02@sap.com>
2025-06-13 12:24:34 +0200

    feat: reformat code to make it more readable

--- projects/schematics/src/add-ssr/index.ts
+++ projects/schematics/src/add-ssr/index.ts
@@ -354,7 +354,9 @@ function useNoSsrConfigurationInNgServe(
 /**
  * Http Transfer Cache is temporarily disabled; https://jira.tools.sap/browse/CXSPA-10430
  */
-export function addWithNoHttpTransferCacheToAppModule(spartacusOptions: SpartacusOptions): Rule {
+export function addWithNoHttpTransferCacheToAppModule(
+  spartacusOptions: SpartacusOptions
+): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const appModulePath = getPathResultsForFile(
       tree,
@@ -378,9 +380,7 @@ export function addWithNoHttpTransferCacheToAppModule(spartacusOptions: Spartacu
 
     const fileBuffer = tree.read(appModulePath);
     if (!fileBuffer) {
-      throw new SchematicsException(
-        `Could not read file at ${appModulePath}`
-      );
+      throw new SchematicsException(`Could not read file at ${appModulePath}`);
     }
     const fileContent = fileBuffer.toString();
 
@@ -393,27 +393,27 @@ export function addWithNoHttpTransferCacheToAppModule(spartacusOptions: Spartacu
 
       const argList = args
         .split(',')
-        .map(a => a.trim())
-        .filter(a => a.length > 0);
+        .map((a) => a.trim())
+        .filter((a) => a.length > 0);
 
-        const idx = argList.findIndex(a => a.startsWith('withEventReplay'));
-        if (idx !== -1) {
-          argList[idx] = 'withEventReplay()';
-          argList.splice(idx + 1, 0, 'withNoHttpTransferCache()');
-        } else {
-          argList.push('withNoHttpTransferCache()');
-        }
-        const newArgs = argList.join(', ');
+      const idx = argList.findIndex((a) => a.startsWith('withEventReplay'));
+      if (idx !== -1) {
+        argList[idx] = 'withEventReplay()';
+        argList.splice(idx + 1, 0, 'withNoHttpTransferCache()');
+      } else {
+        argList.push('withNoHttpTransferCache()');
+      }
+      const newArgs = argList.join(', ');
 
-        const updatedContent = fileContent.replace(
-          hydrationRegex,
-          (_, _args) => `provideClientHydration(${newArgs}`
+      const updatedContent = fileContent.replace(
+        hydrationRegex,
+        (_, _args) => `provideClientHydration(${newArgs}`
+      );
+      tree.overwrite(appModulePath, updatedContent);
+      if (spartacusOptions.debug) {
+        context.logger.info(
+          '✅ Added withNoHttpTransferCache() next to withEventReplay() in the parameter list of provideClientHydration'
         );
-        tree.overwrite(appModulePath, updatedContent);
-        if (spartacusOptions.debug) {
-          context.logger.info(
-            '✅ Added withNoHttpTransferCache() next to withEventReplay() in the parameter list of provideClientHydration'
-          );
       }
     }
     commitChanges(tree, appModulePath, [importChange]);
