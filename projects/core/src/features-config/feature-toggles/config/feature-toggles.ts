8aa52f5c60b9846bddfaba725ccbd723729d3b31
Sergiej Drozd <siarhei.drozd@sap.com>
2025-06-16 11:27:04 +0200

    Revert "a11yNgSelectMobileReadout"
    This reverts commit 47f5a7ad04f77ded78623fc4d9ead70ceab8eb00.

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -283,6 +283,11 @@ export interface FeatureTogglesInterface {
   a11yAddedToCartActiveDialog?: boolean;
 
   /**
+   * Modifies the 'NgSelectA11yDirective' to improve the sorting dropdown screen reader experience on mobile devices.
+   */
+  a11yNgSelectMobileReadout?: boolean;
+
+  /**
    * When enabled, the form in 'PickupOptionsComponent' will be wrapped in a fieldset and contain a legend.
    */
   a11yDeliveryMethodFieldset?: boolean;
@@ -769,6 +774,7 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   a11yNgSelectAriaLabelDropdownCustomized: true,
   a11yRepeatedCancelOrderError: true,
   a11yAddedToCartActiveDialog: true,
+  a11yNgSelectMobileReadout: true,
   a11yDeliveryMethodFieldset: true,
   a11yShowMoreReviewsBtnFocus: true,
   a11yQuickOrderAriaControls: true,
