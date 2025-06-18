846e85be566ddcdda96a61ba97c007fabbc1a8e5
Eric Polesky <eric.polesky@sap.com>
2025-06-18 10:41:17 -0400

    adjust legacy e2e file pattern to avoid running by ci script

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/user_access/login.legacy-core-e2e.cy.ts
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as login from '../../../helpers/login';
+import { viewportContext } from '../../../helpers/viewport-context';
+
+describe('Legacy Login', () => {
+  viewportContext(['mobile'], () => {
+    before(() => {
+      cy.visit('/login');
+      login.registerUserFromLoginPage();
+    });
+
+    it('should login and logout successfully', () => {
+      login.legacyLogin();
+
+      const tokenRevocationRequestAlias =
+        login.listenForTokenRevocationRequest();
+      login.signOutUser();
+      cy.wait(tokenRevocationRequestAlias);
+    });
+
+    it('should not login with wrong password', () => {
+      cy.visit('/login');
+      login.loginWithBadCredentialsFromLoginPage();
+    });
+  });
+});
