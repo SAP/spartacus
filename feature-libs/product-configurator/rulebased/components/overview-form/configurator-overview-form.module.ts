98cf9381be6664377446b75ea7f460030509b830
Michael Schmut <m.schmut@sap.com>
2025-06-18 12:59:54 +0200

    styling fix

--- feature-libs/product-configurator/rulebased/components/overview-form/configurator-overview-form.module.ts
+++ feature-libs/product-configurator/rulebased/components/overview-form/configurator-overview-form.module.ts
@@ -6,7 +6,12 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CmsConfig, FeaturesConfigModule, I18nModule, provideDefaultConfig } from '@spartacus/core';
+import {
+  CmsConfig,
+  FeaturesConfigModule,
+  I18nModule,
+  provideDefaultConfig,
+} from '@spartacus/core';
 import { ConfiguratorOverviewAttributeModule } from '../overview-attribute/configurator-overview-attribute.module';
 import { ConfiguratorOverviewBundleAttributeModule } from '../overview-bundle-attribute/configurator-overview-bundle-attribute.module';
 import { ConfiguratorOverviewFormComponent } from './configurator-overview-form.component';
