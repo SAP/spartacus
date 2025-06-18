4ab0a7806ea77fc9da12d9cbc34c1d11c839293c
Norbert Papp <norbert.papp02@sap.com>
2025-06-18 16:47:52 +0200

    fix: format host property in carousel component for consistency

--- projects/storefrontlib/shared/components/carousel/carousel.component.ts
+++ projects/storefrontlib/shared/components/carousel/carousel.component.ts
@@ -42,7 +42,7 @@ import { CarouselService } from './carousel.service';
   selector: 'cx-carousel',
   templateUrl: './carousel.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {ngSkipHydration: 'true'},
+  host: { ngSkipHydration: 'true' },
   standalone: false,
 })
 export class CarouselComponent implements OnInit, OnChanges {
