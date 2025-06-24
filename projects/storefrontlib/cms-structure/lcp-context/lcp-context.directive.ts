e55ae6fb89c5c8cfac5019563f640add65d71e8f
Krzysztof Platis <platonn.git@gmail.com>
2025-06-24 11:46:39 +0200

    refactor: LcpContextDirective - become structural directive exposing context via `let`

--- projects/storefrontlib/cms-structure/lcp-context/lcp-context.directive.ts
+++ projects/storefrontlib/cms-structure/lcp-context/lcp-context.directive.ts
@@ -1,20 +1,50 @@
-import { Directive, inject } from '@angular/core';
-import { map } from 'rxjs';
-import { LCP_CONTEXT } from './lcp-context.model';
+import {
+  Directive,
+  inject,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ImageFetchPriority } from '../shared/components/media/media.model';
+import { LCP_CONTEXT, LcpContext } from './lcp-context.model';
 import { LcpToFetchPriorityMappingService } from './lcp-to-fetch-priority-mapping.service';
+
+interface LcpContextDirectiveTemplateContext {
+  $implicit: {
+    fetchPriority$: Observable<ImageFetchPriority | null | undefined>;
+    lcpContext$: Observable<LcpContext>;
+  };
+}
 
 @Directive({
   selector: '[cxLcpContext]',
-  exportAs: 'cxLcpContext',
   standalone: false,
 })
 export class LcpContextDirective {
   protected readonly lcpToFetchPriorityService = inject(
     LcpToFetchPriorityMappingService
   );
-
   readonly lcpContext$ = inject(LCP_CONTEXT);
   readonly fetchPriority$ = this.lcpContext$.pipe(
-    map((lcpContext) => this.lcpToFetchPriorityService.map(lcpContext))
+    map((lcpContext) => this.lcpToFetchPriorityService.map(lcpContext) ?? null)
   );
+
+  constructor(
+    private templateRef: TemplateRef<LcpContextDirectiveTemplateContext>,
+    private viewContainer: ViewContainerRef
+  ) {
+    this.viewContainer.createEmbeddedView(this.templateRef, {
+      $implicit: {
+        fetchPriority$: this.fetchPriority$,
+        lcpContext$: this.lcpContext$,
+      },
+    });
+  }
+
+  static ngTemplateContextGuard(
+    _dir: LcpContextDirective,
+    _ctx: unknown
+  ): _ctx is LcpContextDirectiveTemplateContext {
+    return true;
+  }
 }
