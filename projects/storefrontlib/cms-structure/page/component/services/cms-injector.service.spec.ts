c97fa5d8c6f95385674cec76f7a28a596a93ad51
Krzysztof Platis <platonn.git@gmail.com>
2025-06-25 18:30:51 +0200

    test: test providing LCP_CONTEXT by CmsInjectorService

--- projects/storefrontlib/cms-structure/page/component/services/cms-injector.service.spec.ts
+++ projects/storefrontlib/cms-structure/page/component/services/cms-injector.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CmsService } from '@spartacus/core';
-import { CmsComponentData } from '@spartacus/storefront';
+import { CmsComponentData, LCP_CONTEXT } from '@spartacus/storefront';
+import { DEFAULT_LCP_CONTEXT } from 'projects/storefrontlib/shared/directives/lcp-context/lcp-context.token';
 import { of } from 'rxjs';
 import { CmsComponentsService } from '../../../services/cms-components.service';
 import { CmsInjectorService } from './cms-injector.service';
@@ -48,6 +49,13 @@ describe('CmsInjectorService', () => {
     it('should call getMapping from CmsComponentsService', () => {
       service.getInjector('aaa', 'sampleUid');
       expect(mockCmsComponentsService.getMapping).toHaveBeenCalled();
+    });
+
+    it('should provide LCP_CONTEXT from the injector', () => {
+      const injector = service.getInjector('aaa', 'sampleUid');
+      const lcpContext = injector.get(LCP_CONTEXT);
+      expect(lcpContext).toBeDefined();
+      expect(lcpContext).not.toEqual(DEFAULT_LCP_CONTEXT);
     });
   });
 });
