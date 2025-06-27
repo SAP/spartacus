4ea72e1c49c9d90c9399a701678350e6f64900a0
Hakwoo Kim <hakwoo.kim@sap.com>
2025-06-27 10:53:41 -0400

    update after code review

--- projects/storefrontlib/layout/main/storefront.component.ts
+++ projects/storefrontlib/layout/main/storefront.component.ts
@@ -92,7 +92,6 @@ export class StorefrontComponent implements OnInit, OnDestroy {
     useFeatureStyles('a11yKeyboardFocusInSearchBox');
     useFeatureStyles('a11yNgSelectLayering');
     useFeatureStyles('topProgressBarUseTransformAnimation');
-    useFeatureStyles('reserveSpaceForImagesOnPdpAndPlp');
   }
 
   ngOnInit(): void {
