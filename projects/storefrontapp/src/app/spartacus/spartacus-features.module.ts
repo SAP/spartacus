8ed8a5e79713d6737dfecc218bdf68b41e406a4e
keerthi <keerthi.sattaluri@sap.com>
2025-07-01 21:26:01 +0200

    update jdk21 oauth configuration

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -16,6 +16,7 @@ import {
   UserModule,
   UserOccModule,
   provideFeatureTogglesFactory,
+  provideAuthorizationCodeFlowByDefault
 } from '@spartacus/core';
 import {
   AnonymousConsentManagementBannerModule,
@@ -419,6 +420,7 @@ if (environment.cpq) {
       };
       return appFeatureToggles;
     }),
+    provideAuthorizationCodeFlowByDefault(),
   ],
 })
 export class SpartacusFeaturesModule {}
