12854b56572f32c50637ba1d01576fcae1d9a1d7
Sergiej Drozd <siarhei.drozd@sap.com>
2025-07-01 12:55:24 +0200

    testing cypress-split

--- projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
@@ -7,6 +7,9 @@
 
 import { defineConfig } from 'cypress';
 
+// cypress-split for parallel test distribution
+const cypressSplit = require('cypress-split');
+
 export default defineConfig({
   defaultCommandTimeout: 30000,
 
@@ -36,7 +39,14 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      // Add cypress-split for parallel test distribution
+      cypressSplit(on, config);
+
+      // Call the existing plugins
+      const existingConfig = require('./cypress/plugins/index.js')(on, config);
+
+      // IMPORTANT: return the config object with any modifications from cypress-split
+      return existingConfig;
     },
     baseUrl: 'http://localhost:4200',
     excludeSpecPattern: '**/*.example-e2e.cy.ts',
