2e229448087727a6e68f863ac3ed770915425e5e
Krzysztof Platis <platonn.git@gmail.com>
2025-07-01 17:48:05 +0200

    feat: use new carousel in product-images component

--- projects/storefrontlib/cms-components/product/product-images/product-images.component.ts
+++ projects/storefrontlib/cms-components/product/product-images/product-images.component.ts
@@ -4,7 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  TrackByFunction,
+} from '@angular/core';
 import {
   ImageGroup,
   Product,
@@ -45,6 +49,7 @@ export class ProductImagesComponent {
 
   constructor(protected currentProductService: CurrentProductService) {
     useFeatureStyles('a11yDifferentiateFocusedAndSelected');
+    useFeatureStyles('productCarouselScrolling');
   }
 
   openImage(item: any): void {
@@ -100,4 +105,7 @@ export class ProductImagesComponent {
 
     return (<any[]>product.images.GALLERY).map((c) => of({ container: c }));
   }
+
+  trackByFn: TrackByFunction<Product> = (_index: number, item: Product) =>
+    item?.code;
 }
