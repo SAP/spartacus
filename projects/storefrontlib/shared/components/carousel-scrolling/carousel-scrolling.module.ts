e816f887a0b669c80585951e19ade2fa91d648ab
Krzysztof Platis <platonn.git@gmail.com>
2025-07-01 15:48:32 +0200

    refactor: remove HorizontalScrollingPositionDirective declaration from the CarouselScrollingModule

--- projects/storefrontlib/shared/components/carousel-scrolling/carousel-scrolling.module.ts
+++ projects/storefrontlib/shared/components/carousel-scrolling/carousel-scrolling.module.ts
@@ -8,10 +8,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { I18nModule, UrlModule } from '@spartacus/core';
+import { HorizontalScrollingPositionDirectiveModule } from '@spartacus/storefront';
 import { IconModule } from '../../../cms-components/misc/icon/index';
 import { MediaModule } from '../media/media.module';
 import { CarouselScrollingComponent } from './carousel-scrolling.component';
-import { HorizontalScrollingPositionDirective } from './horizontal-scrolling-position.directive';
 
 @NgModule({
   imports: [
@@ -21,12 +21,9 @@ import { HorizontalScrollingPositionDirective } from './horizontal-scrolling-pos
     MediaModule,
     UrlModule,
     I18nModule,
+    HorizontalScrollingPositionDirectiveModule,
   ],
-  // SPIKE TODO: move scrolling state to separate module
-  declarations: [
-    CarouselScrollingComponent,
-    HorizontalScrollingPositionDirective,
-  ],
-  exports: [CarouselScrollingComponent, HorizontalScrollingPositionDirective],
+  declarations: [CarouselScrollingComponent],
+  exports: [CarouselScrollingComponent],
 })
 export class CarouselScrollingModule {}
