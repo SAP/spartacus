545423cc54a6908c9283a62fafe5cb2db140f737
Kar <j.kar@sap.com>
2025-07-23 15:52:10 +0530

    on action reload details

--- feature-libs/subscription-billing/components/details/subscription-details.component.ts
+++ feature-libs/subscription-billing/components/details/subscription-details.component.ts
@@ -158,22 +158,12 @@ export class SubscriptionDetailsComponent implements OnDestroy, OnInit {
       .pipe(
         take(1),
         switchMap(([code, subscription]) => {
-          // if (!code || !subscription) {
-          //   this.globalMessageService.add(
-          //     { key: 'cancelSubscription.missingCode' },
-          //     GlobalMessageType.MSG_TYPE_ERROR
-          //   );
-          //   return throwError(() => new Error('Missing subscription code'));
-          // }
-
           const payload: withdrawal = {
             subscriptionId: subscription?.id,
             version: subscription?.version,
             withdrawnAt: subscription?.withdrawnAt,
             withdrawalPeriodEndDate: subscription?.withdrawalPeriodEndDate,
           };
-
-          console.log('[withdrawal] Using payload:', payload);
 
           return this.subscriptionCancelFacade.withdrawal(payload, code).pipe(
             catchError((err) => {
@@ -202,19 +192,6 @@ export class SubscriptionDetailsComponent implements OnDestroy, OnInit {
       .pipe(
         take(1),
         switchMap(([code]) => {
-          // if (!code || !subscription) {
-          //   this.globalMessageService.add(
-          //     { key: 'cancelSubscription.missingCode' },
-          //     GlobalMessageType.MSG_TYPE_ERROR
-          //   );
-          //   return throwError(() => new Error('Missing subscription code'));
-          // }
-
-          // const payload: reverseCancellation = {
-          //   subscriptionId: subscription?.id,
-          //   version: subscription?.version,
-          // };
-
           return this.subscriptionCancelFacade.reverseCancellation(code).pipe(
             catchError((err) => {
               this.onError();
