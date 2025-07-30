8e875bae630cf44f9a69c44d0101e08d9bb1177e
katechuen <kate.chuen.cheong@sap.com>
2025-07-30 10:14:37 -0400

    chore: fixing order-history-no-orders-flow.e2e

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/order-history-no-orders-flow.e2e.cy.ts
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { checkBanner } from '../../../helpers/homepage';
+import { waitForPage } from '../../../helpers/navigation';
+import { viewportContext } from '../../../helpers/viewport-context';
+import { isolateTests } from '../../../support/utils/test-isolation';
+
+describe('Order History with no orders', { testIsolation: false }, () => {
+  viewportContext(['mobile', 'desktop'], () => {
+    isolateTests();
+    before(() => {
+      cy.window().then((win) => win.sessionStorage.clear());
+    });
+
+    describe('Order History for anonymous user', () => {
+      it('should redirect to login page for anonymous user', () => {
+        cy.visit('/my-account/orders');
+        cy.location('pathname').should('contain', '/login');
+      });
+    });
+
+    describe('Order History for logged in user', () => {
+      before(() => {
+        cy.visit('/');
+        cy.requireLoggedIn();
+        cy.reload();
+        cy.visit('/');
+        cy.selectUserMenuOption({
+          option: 'Order History',
+        });
+      });
+
+      beforeEach(() => {
+        cy.restoreLocalStorage();
+      });
+
+      afterEach(() => {
+        cy.saveLocalStorage();
+      });
+
+      it('should display order history page and should be able to start shopping', () => {
+        const homePage = waitForPage('homepage', 'getHomePage');
+        cy.get('.cx-order-history-header h2').should(
+          'contain',
+          'Order history'
+        );
+        cy.get('.cx-order-history-no-order').should('exist');
+
+        cy.get('.btn.btn-primary.btn-block.active')
+          .findByText('Start Shopping')
+          .click();
+
+        cy.wait(`@${homePage}`).its('response.statusCode').should('eq', 200);
+        checkBanner();
+      });
+    });
+  });
+});
