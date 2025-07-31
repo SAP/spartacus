cd664ca3e6b04f19ba1281db2acd63fbe5699338
katechuen <kate.chuen.cheong@sap.com>
2025-07-31 12:38:11 -0400

    test: fixing update-email e2e jdk21

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/update-email.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/update-email.e2e.cy.ts
@@ -5,14 +5,14 @@
  */
 
 import { login } from '../../../helpers/auth-forms';
-import * as alerts from '../../../helpers/global-message';
 import { checkBanner } from '../../../helpers/homepage';
 import { signOut } from '../../../helpers/register';
 import * as updateEmail from '../../../helpers/update-email';
 import { registerAndLogin } from '../../../helpers/update-email';
 import { viewportContext } from '../../../helpers/viewport-context';
 import { standardUser } from '../../../sample-data/shared-users';
-import { isolateTests } from '../../../support/utils/test-isolation';
+import * as alerts from '../../../helpers/global-message';
+
 
 describe('My Account - Update Email', () => {
   viewportContext(['mobile', 'desktop'], () => {
@@ -27,15 +27,12 @@ describe('My Account - Update Email', () => {
       });
     });
 
-    describe('Logged in user', { testIsolation: false }, () => {
-      isolateTests();
-      before(() => {
-        registerAndLogin();
-        cy.visit('/');
-      });
-
+    describe('Logged in user', () => {
       beforeEach(() => {
-        cy.restoreLocalStorage();
+        cy.visit('/');
+        registerAndLogin();
+        cy.reload();
+        cy.visit('/');
         cy.selectUserMenuOption({
           option: 'Email Address',
         });
@@ -44,26 +41,24 @@ describe('My Account - Update Email', () => {
       it('should click cancel update email and go back to the homepage', () => {
         cy.get('cx-update-email a.btn-secondary').click();
         checkBanner();
-
         cy.location('pathname').should('contain', '/');
       });
 
       // Core e2e test. Check with different view port.
-      updateEmail.testUpdateEmailAndLogin();
-
-      // Below test depends on core test for setup.
-      it('should not allow login with old email address', () => {
+      it('should update email and able to login with new and not with old email', () => {
+        updateEmail.testUpdateEmailAndLogin();
         signOut();
         cy.visit('/login');
         login(
           standardUser.registrationData.email,
           standardUser.registrationData.password
         );
-        alerts.getErrorAlert().should('contain', 'Bad credentials');
-      });
-
-      afterEach(() => {
-        cy.saveLocalStorage();
+        cy.whenJDK17(() => {
+          alerts.getErrorAlert().should('contain', 'Bad credentials');
+        });
+        cy.whenJDK21(() => {
+          cy.get('.fd-message-strip__text').should('contain','Bad credentials.');
+        });
       });
     });
   });
