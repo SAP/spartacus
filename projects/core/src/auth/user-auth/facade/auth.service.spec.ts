30597e7b6e05ef9ff7f9adb3512cbadb4cbc4922
Melody-zhou-512 <140146828+Melody-zhou-512@users.noreply.github.com>
2025-08-06 18:10:01 +0800

    fix code review comments
    fix code review comments

--- projects/core/src/auth/user-auth/facade/auth.service.spec.ts
+++ projects/core/src/auth/user-auth/facade/auth.service.spec.ts
@@ -134,6 +134,19 @@ describe('AuthService', () => {
       expect(store.dispatch).toHaveBeenCalledWith(new AuthActions.Login());
     });
 
+    it('when customer emulated in asm page', async () => {
+      spyOn(authStorageService, 'getItem').and.returnValue('token');
+      spyOn(userIdService, 'setUserId').and.callThrough();
+
+      spyOn(userIdService, 'isEmulated').and.returnValue(of(true));
+
+      await service.checkOAuthParamsInUrl();
+
+      expect(userIdService.setUserId).not.toHaveBeenCalledWith(
+        OCC_USER_ID_CURRENT
+      );
+    });
+
     describe('when the token is received', () => {
       it('should redirect', async () => {
         spyOn(authRedirectService, 'redirect').and.callThrough();
