fb9dbc400782c2523d2c641116462e29ab006bbc
Saptarshi <saptarshi.bose01@sap.com>
2025-08-18 14:34:50 +0530

    added buttons for cancel and returning physical products in S4ServiceOrderDetailActionsComponent

--- integration-libs/s4-service/order/components/s4-service-order-detail-actions/s4-service-order-detail-actions.component.ts
+++ integration-libs/s4-service/order/components/s4-service-order-detail-actions/s4-service-order-detail-actions.component.ts
@@ -4,12 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { GlobalMessageService, GlobalMessageType } from '@spartacus/core';
 import { OrderDetailActionsComponent } from '@spartacus/order/components';
 import { Order } from '@spartacus/order/root';
 import { CheckoutServiceSchedulePickerService } from '@spartacus/s4-service/root';
-import { map, Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'cx-s4-service-order-detail-actions',
@@ -17,16 +22,30 @@ import { map, Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
-export class S4ServiceOrderDetailActionsComponent extends OrderDetailActionsComponent {
+export class S4ServiceOrderDetailActionsComponent
+  extends OrderDetailActionsComponent
+  implements OnInit
+{
   protected checkoutServiceSchedulePickerService = inject(
     CheckoutServiceSchedulePickerService
   );
   protected globalMessageService = inject(GlobalMessageService);
 
+  /**
+   * @deprecated since 2211.44 - Displaying the individual action buttons will depend on their boolean flags from API response.
+   */
   displayActions$: Observable<boolean> = this.order$.pipe(
     map((order) => this.checkServiceStatus(order))
   );
 
+  ngOnInit(): void {
+    this.order$.pipe(tap((order) => this.displayServiceMessage(order)));
+  }
+
+  /**
+   * @deprecated since 2211.44 - Displaying the individual action buttons will depend on their boolean flags from API response.
+   * Displaying notification for a service not amendable will be carried out by 'displayServiceMessage' instead.
+   */
   protected checkServiceStatus(order: Order): boolean {
     if (order && order.status === 'CANCELLED') {
       return false;
@@ -46,5 +65,20 @@ export class S4ServiceOrderDetailActionsComponent extends OrderDetailActionsComp
       }
     }
     return true;
+  }
+
+  protected displayServiceMessage(order: Order): void {
+    if (order.status !== 'CANCELLED' && !!order.servicedAt) {
+      const hoursFromSchedule =
+        this.checkoutServiceSchedulePickerService.getHoursFromServiceSchedule(
+          order.servicedAt
+        );
+      if (hoursFromSchedule > 0 && hoursFromSchedule <= 24) {
+        this.globalMessageService.add(
+          { key: 'rescheduleService.serviceNotAmendable' },
+          GlobalMessageType.MSG_TYPE_INFO
+        );
+      }
+    }
   }
 }
