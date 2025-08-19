40d529e6977a3e81b8209fe2b230256576efdbd5
Anjana BL <anjana.b.l@sap.com>
2025-08-19 20:16:51 +0530

    Update cdc-login-form.module.ts

--- integration-libs/cdc/user-account/login-form/cdc-login-form.module.ts
+++ integration-libs/cdc/user-account/login-form/cdc-login-form.module.ts
@@ -14,6 +14,7 @@ import {
   CmsConfig,
   GlobalMessageService,
   I18nModule,
+  NotAuthGuard,
   provideConfig,
   provideDefaultConfig,
   UrlModule,
@@ -50,6 +51,7 @@ import { CdcJsService } from '@spartacus/cdc/root';
       cmsComponents: {
         ReturningCustomerLoginComponent: {
           component: LoginFormComponent,
+          guards: [NotAuthGuard],
           providers: [
             {
               provide: LoginFormComponentService,
