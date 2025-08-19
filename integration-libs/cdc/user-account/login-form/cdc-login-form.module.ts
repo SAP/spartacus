260c75ccbf0975a76d02c4193137d2cf50ecc051
Anjana BL <anjana.b.l@sap.com>
2025-08-19 18:38:11 +0530

    Update cdc-login-form.module.ts

--- integration-libs/cdc/user-account/login-form/cdc-login-form.module.ts
+++ integration-libs/cdc/user-account/login-form/cdc-login-form.module.ts
@@ -9,11 +9,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import {
+  AuthConfig,
   AuthService,
   CmsConfig,
   GlobalMessageService,
   I18nModule,
-  NotAuthGuard,
+  provideConfig,
   provideDefaultConfig,
   UrlModule,
   WindowRef,
@@ -40,11 +41,15 @@ import { CdcJsService } from '@spartacus/cdc/root';
     CdcReconsentModule,
   ],
   providers: [
+    provideConfig(<AuthConfig>{
+      authentication: {
+        customLoginPage: undefined,
+      },
+    }),
     provideDefaultConfig(<CmsConfig>{
       cmsComponents: {
         ReturningCustomerLoginComponent: {
           component: LoginFormComponent,
-          guards: [NotAuthGuard], //CSRF handling skipped for CDC flow.
           providers: [
             {
               provide: LoginFormComponentService,
