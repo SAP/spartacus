77abfc793b53db291973a67f4205739da9d39f8d
evy-yang01 <evy.yang@sap.com>
2025-09-01 11:41:14 +0800

    feat: adapt my account v2 test case to support otp login (#20696)

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/my-account-v2-consent-management.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/my-account/my-account-v2-consent-management.e2e.cy.ts
@@ -7,28 +7,47 @@
 import {
   myAccountV2consentManagementTest,
   verifyAsAnonymous,
-} from '../../../helpers/consent-management';
+} from '../../../helpers/my-account-v2/my-account-v2-consent-management';
 import * as login from '../../../helpers/login';
+import { generateMail, randomString } from '../../../helpers/user';
+import * as loginHelper from '../../../helpers/my-account-v2/my-account-v2-login-helper';
 import { viewportContext } from '../../../helpers/viewport-context';
+import { standardUser } from '../../../sample-data/shared-users';
 import { isolateTests } from '../../../support/utils/test-isolation';
 
-viewportContext(['mobile', 'desktop'], () => {
-  describe('My Account - Consent Management', () => {
+describe('My Account - Consent Management(CXSPA-10780)', () => {
+  viewportContext(['mobile', 'desktop'], () => {
     before(() => {
       cy.window().then((win) => win.sessionStorage.clear());
     });
-    describe('consent management test for anonymous user(CXSPA-4491)', () => {
+
+    describe('consent management test for anonymous user(CXSPA-10780)', () => {
       verifyAsAnonymous();
     });
+  });
+
+  viewportContext(['desktop'], () => {
+    before(() =>
+      cy.window().then((win) => {
+        win.sessionStorage.clear();
+      })
+    );
 
     describe(
-      'consent management test for logged in user(CXSPA-4491)',
+      'consent management test for logged in user(CXSPA-10780)',
       { testIsolation: false },
       () => {
         isolateTests();
         before(() => {
-          cy.requireLoggedIn();
-          cy.reload();
+          standardUser.registrationData.email = generateMail(
+            randomString(),
+            true
+          );
+          loginHelper.registerAndLogin(
+            standardUser.registrationData.email,
+            standardUser.registrationData.password
+          );
+
           cy.visit('/');
           cy.selectUserMenuOption({
             option: 'Consent Management',
