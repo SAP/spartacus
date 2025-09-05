30a8b4f6c50498dbefe246bbaa03692ccd98d0ec
Hakwoo Kim <hakwoo.kim@sap.com>
2025-09-05 11:50:05 -0400

    chore: remove useSiteThemeService feature toggle

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -364,7 +364,6 @@ if (environment.cpq) {
         a11yRepeatingButtonsUniqueLabels: true,
         a11yHighContrastBorders: true,
         a11yRegionAssociatedHeaders: true,
-        useSiteThemeService: true,
         enableConsecutiveCharactersPasswordRequirement: true,
         enablePasswordsCannotMatchInPasswordUpdateForm: true,
         dispatchLoginActionOnlyWhenTokenReceived: true,
