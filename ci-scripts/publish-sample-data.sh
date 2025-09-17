bc527c3f22169f36c3cc496a855191be234d37ce
rmch91 <r.chygryn@sap.com>
2025-09-17 07:53:25 -0400

    changes

--- ci-scripts/publish-sample-data.sh
+++ ci-scripts/publish-sample-data.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 TAG_NAME="sampledata"
-SAMPLE_DATA_OLD_BRANCH_JDK_17="release/2211.x"
-SAMPLE_DATA_UNRELEASED_BRANCH_JDK21="release/221121.x"
-OLD_SPARTACUS_VERSION_NAME_JDK17="spartacussampledata-version-2211-x"
-UNRELEASED_SPARTACUS_VERSION_NAME_JDK21="spartacussampledata-version-221121-x"
+SAMPLE_DATA_UNRELEASED_BRANCH="release/221121.x"
+UNRELEASED_SPARTACUS_VERSION_NAME="spartacussampledata-version-221121-x"
 
 echo "Downloading current sample data for 2211.x"
 
@@ -13,8 +11,7 @@ function download_sample_data_from_spartacussample_repo {
     curl -H "Authorization: token $GHT_PRIVATE_REPO_TOKEN" -L "https://github.tools.sap/cx-commerce/spartacussampledata/archive/$1.tar.gz" -o "$2.tar.gz"
 }
 
-download_sample_data_from_spartacussample_repo $SAMPLE_DATA_OLD_BRANCH_JDK_17 $OLD_SPARTACUS_VERSION_NAME_JDK17
-download_sample_data_from_spartacussample_repo $SAMPLE_DATA_UNRELEASED_BRANCH_JDK21 $UNRELEASED_SPARTACUS_VERSION_NAME_JDK21
+download_sample_data_from_spartacussample_repo $SAMPLE_DATA_UNRELEASED_BRANCH $UNRELEASED_SPARTACUS_VERSION_NAME
 
 echo "-----"
 echo "Deleting existing tag (if any)"
@@ -27,11 +24,11 @@ echo "Creating a new release"
 gh release create "$TAG_NAME" \
   --repo SAP-samples/cloud-commerce-sample-setup \
   --title "Spartacus Sample Data" \
-  --notes "Spartacus sample data releases: 221121-x (current release JDK 21) and 2211-x (old release JDK 17)." \
+  --notes "Spartacus sample data releases: 221121-x (current release JDK 21)" \
 
 echo "-----"
 echo "Uploading assets to the release"
 
-gh release upload "$TAG_NAME" "$UNRELEASED_SPARTACUS_VERSION_NAME_JDK21.zip" "$UNRELEASED_SPARTACUS_VERSION_NAME_JDK21.tar.gz" "$OLD_SPARTACUS_VERSION_NAME_JDK17.zip" "$OLD_SPARTACUS_VERSION_NAME_JDK17.tar.gz" \
+gh release upload "$TAG_NAME" "$UNRELEASED_SPARTACUS_VERSION_NAME.zip" "$UNRELEASED_SPARTACUS_VERSION_NAME.tar.gz" \
   --repo SAP-samples/cloud-commerce-sample-setup \
   --clobber
