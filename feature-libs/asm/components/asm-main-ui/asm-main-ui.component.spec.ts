ab7573ecd8c62ecab0b1c92fa2ec8f65c639be17
scar.ai@sap.com <scar.ai@sap.com>
2025-09-22 16:03:56 +0800

    fix:  Fix UT

--- feature-libs/asm/components/asm-main-ui/asm-main-ui.component.spec.ts
+++ feature-libs/asm/components/asm-main-ui/asm-main-ui.component.spec.ts
@@ -42,6 +42,10 @@ class MockAuthService implements Partial<AuthService> {
   isUserLoggedIn(): Observable<boolean> {
     return of(false);
   }
+
+  updateIsUsingASMClient(_isUsing: boolean) {
+        return of(false);
+  }
 }
 
 @Component({
@@ -492,6 +496,7 @@ describe('AsmMainUiComponent', () => {
   it('should hide the UI when the Close Asm button is clicked', () => {
     component.ngOnInit();
     fixture.detectChanges();
+    spyOn(authService, 'updateIsUsingASMClient')
     const submitBtn = fixture.debugElement.query(
       By.css('button[title="asm.hideUi"]')
     );
