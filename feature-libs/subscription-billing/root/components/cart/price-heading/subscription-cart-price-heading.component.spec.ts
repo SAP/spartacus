be23529282d4777002412f25adbb9b5e289bd48a
Saptarshi <saptarshi.bose01@sap.com>
2025-09-26 17:51:37 +0530

    Unit tests for SubscriptionCartPriceHeadingComponent

--- feature-libs/subscription-billing/root/components/cart/price-heading/subscription-cart-price-heading.component.spec.ts
+++ feature-libs/subscription-billing/root/components/cart/price-heading/subscription-cart-price-heading.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubscriptionCartPriceHeadingComponent } from './subscription-cart-price-heading.component';
+import { TranslationService } from '@spartacus/core';
+import { Observable, of } from 'rxjs';
 
+class MockTranslateService implements Partial<TranslationService> {
+  translate(
+    _key: string | string[],
+    _options?: any,
+    _whitespaceUntilLoaded?: boolean
+  ): Observable<string> {
+    return of('');
+  }
+}
 describe('SubscriptionCartPriceHeadingComponent', () => {
   let component: SubscriptionCartPriceHeadingComponent;
   let fixture: ComponentFixture<SubscriptionCartPriceHeadingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SubscriptionCartPriceHeadingComponent]
+      imports: [SubscriptionCartPriceHeadingComponent],
+      providers: [{ provide: TranslationService, useClass: MockTranslateService }],
     })
     .compileComponents();
 
