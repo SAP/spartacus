5798eca7bdb2f60d27f4c35084b431ab5a72aeda
Siarhei Drozd <siarhei.drozd@sap.com>
2025-10-02 10:51:47 +0200

    Complete refactor fo b2b checkout

--- projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2b/b2b.checkout.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2b/b2b.checkout.a11y-e2e.cy.ts
@@ -4,133 +4,124 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { loginB2bUser } from '../helpers/a11y-b2b.checkout';
-import {
-  fillPaymentDetails,
-  fillShippingAddress,
-} from '../../../helpers/checkout-forms';
-import { user } from '../../../sample-data/checkout-flow';
 import * as b2bCheckout from '../../../helpers/b2b/b2b-checkout';
-import { products } from '../../../sample-data/b2b-checkout';
-import { viewportContext } from '../../../helpers/viewport-context';
-
-function proceedToCheckout(): void {
-  cy.visit(`product/${products[0].code}`);
-  cy.get('button').contains(' Add to cart ').click();
-  cy.get('.cx-dialog-buttons button').contains(' view cart ').click();
-  cy.get('cx-cart-details');
-  cy.get('main').a11yRunContinuumTest();
-  cy.get('button').contains(' Proceed to Checkout ').click();
-}
+import * as checkout from '../../../helpers/checkout-flow';
+import {
+  b2bAccountShipToUser,
+  b2bProduct,
+  cartWithB2bProduct,
+  cartWithB2bProductAndPremiumShipping,
+  order_type,
+  POWERTOOLS_BASESITE,
+} from '../../../sample-data/b2b-checkout';
+import { user } from '../../../sample-data/checkout-flow';
 
 for (let i = 0; i < 20; i++) {
-  describe('B2B checkout accessibility', { testIsolation: false }, () => {
-    viewportContext(['desktop', 'mobile'], () => {
-      before(() => {
-        cy.window().then((win) => {
-          win.sessionStorage.clear();
-          win.localStorage.clear();
-        });
-        cy.a11yContinuumSetup();
-        loginB2bUser();
+  describe('B2B Accessibility Checkout', () => {
+    before(() => {
+      Cypress.env('BASE_SITE', POWERTOOLS_BASESITE);
+      cy.a11yContinuumSetup();
+    });
+
+    it('should checkout using an account payment type', () => {
+      b2bCheckout.loginB2bUser();
+
+      b2bCheckout.addB2bProductToCartAndCheckout();
+      cy.get('main').a11yRunContinuumTest();
+
+      b2bCheckout.enterPONumber();
+      cy.get('cx-payment-type').a11yRunContinuumTest();
+
+      b2bCheckout.selectAccountPayment();
+      cy.get('.cx-checkout-title').should('contain', 'Shipping Address');
+      cy.get('main').a11yRunContinuumTest();
+
+      b2bCheckout.selectAccountShippingAddress();
+      cy.get('.cx-checkout-title').should('contain', 'Delivery Options');
+      cy.get('cx-delivery-mode').a11yRunContinuumTest();
+
+      b2bCheckout.selectAccountDeliveryMode();
+
+      b2bCheckout.reviewB2bReviewOrderPage(
+        b2bAccountShipToUser,
+        cartWithB2bProductAndPremiumShipping,
+        true,
+        order_type.PLACE_ORDER
+      );
+      cy.get('main').a11yRunContinuumTest();
+
+      b2bCheckout.placeOrder('/order-confirmation');
+      cy.get('main').a11yRunContinuumTest();
+
+      b2bCheckout.reviewB2bOrderConfirmation(
+        b2bAccountShipToUser,
+        b2bProduct,
+        cartWithB2bProductAndPremiumShipping
+      );
+      cy.get('cx-order-confirmation-thank-you-message').a11yRunContinuumTest();
+    });
+
+    it('should checkout using a credit card', () => {
+      b2bCheckout.loginB2bUser();
+
+      b2bCheckout.addB2bProductToCartAndCheckout();
+      cy.get('main').a11yRunContinuumTest();
+
+      b2bCheckout.enterPONumber();
+      cy.get('cx-payment-type').a11yRunContinuumTest();
+
+      b2bCheckout.selectCreditCardPayment();
+      cy.get('main').a11yRunContinuumTest();
+
+      checkout.checkSummaryAmount(cartWithB2bProduct);
+      cy.get('cx-order-summary').a11yRunContinuumTest();
+
+      checkout.proceedWithEmptyShippingAdressForm();
+      cy.get('cx-delivery-address').a11yRunContinuumTest();
+
+      checkout.proceedWithIncorrectShippingAddressForm({
+        ...user,
+        firstName: '',
       });
+      cy.get('cx-delivery-address').a11yRunContinuumTest();
 
-      describe('Checkout with account', () => {
-        it('Payment Type', () => {
-          proceedToCheckout();
-          cy.get('cx-payment-type').within(() => {
-            cy.findByText('Account').click();
-          });
-          cy.get('cx-payment-type button').contains(' Continue ');
-          cy.get('main').a11yRunContinuumTest();
-        });
+      checkout.fillAddressFormWithCheapProduct({ firstName: user.firstName });
+      cy.get('.cx-checkout-title').should('contain', 'Delivery Options');
 
-        it('Delivery address', () => {
-          cy.get('cx-payment-type button').contains(' Continue ').click();
+      checkout.verifyDeliveryOptions();
+      cy.get('cx-delivery-mode').a11yRunContinuumTest();
 
-          cy.get('.cx-delivery-address-card');
-          cy.get('main').a11yRunContinuumTest();
-        });
+      checkout.proceedWithEmptyPaymentForm();
+      cy.get('cx-payment-method').a11yRunContinuumTest();
 
-        it('Delivery mode', () => {
-          cy.get('cx-delivery-address button.btn-primary')
-            .contains(' Continue ')
-            .click();
-
-          cy.get('cx-delivery-mode input');
-          cy.get('main').a11yRunContinuumTest();
-        });
-
-        it('Review', () => {
-          cy.get('cx-delivery-mode button.btn-primary')
-            .contains(' Continue ')
-            .click();
-
-          cy.get('input[formcontrolname="termsAndConditions"]');
-          cy.get('main').a11yRunContinuumTest();
-        });
-
-        it('Order Confirmation', () => {
-          cy.get('input[formcontrolname="termsAndConditions"]').check();
-          cy.get('cx-place-order button').contains(' Place Order ').click();
-
-          cy.get('cx-order-confirmation-thank-you-message');
-          cy.get('main').a11yRunContinuumTest();
-        });
+      checkout.proceedWithIncorrectPaymentForm({
+        ...user,
+        payment: { ...user.payment, number: '' },
       });
+      cy.get('cx-payment-method').a11yRunContinuumTest();
 
-      describe('Checkout With Card', () => {
-        it('Payment Type', () => {
-          proceedToCheckout();
-          b2bCheckout.enterPONumber();
-          cy.get('cx-payment-type').within(() => {
-            cy.findByText('Credit Card').click();
-          });
-          cy.get('cx-payment-type button').contains(' Continue ').click();
-        });
+      checkout.fillPaymentFormWithCheapProduct(
+        { payment: { number: user.payment.number } },
+        undefined
+      );
+      b2bCheckout.reviewB2bReviewOrderPage(
+        user,
+        cartWithB2bProduct,
+        false,
+        order_type.PLACE_ORDER
+      );
+      cy.get('main').a11yRunContinuumTest();
 
-        it('Shipping Address', () => {
-          cy.get('.cx-address-form-btns button').contains(' Continue ').click();
-          cy.get('cx-delivery-address').a11yRunContinuumTest();
-          fillShippingAddress(user, false);
-          cy.get('cx-delivery-address').a11yRunContinuumTest();
-          cy.get('cx-delivery-address button.btn-primary')
-            .contains(' Continue ')
-            .click();
-          cy.get('cx-global-message').a11yRunContinuumTest();
+      b2bCheckout.placeOrder('/order-confirmation');
+      cy.get('main').a11yRunContinuumTest();
 
-          // skipping delivery mode since it's been tested in previous suite
-          cy.get('cx-delivery-mode button.btn-primary')
-            .contains(' Continue ')
-            .click();
-        });
-
-        it('Payment Type', () => {
-          cy.get('cx-payment-method button').contains(' Continue ').click();
-          cy.get('cx-payment-method').a11yRunContinuumTest();
-
-          fillPaymentDetails(user, undefined, false);
-          cy.get('cx-payment-method').a11yRunContinuumTest();
-
-          cy.get('cx-payment-form').within(() => {
-            cy.wait(3000);
-            cy.get('button.btn.btn-block.btn-primary')
-              .should('be.enabled')
-              .contains('Continue')
-              .click();
-          });
-
-          cy.get('input[formcontrolname="termsAndConditions"]', {
-            timeout: 10000,
-          }).should('be.visible');
-          cy.get('cx-global-message').a11yRunContinuumTest();
-        });
-
-        it('Order Review', () => {
-          cy.get('input[formcontrolname="termsAndConditions"]');
-          cy.get('main').a11yRunContinuumTest();
-        });
-      });
+      b2bCheckout.reviewB2bOrderConfirmation(
+        user,
+        b2bProduct,
+        cartWithB2bProduct,
+        false
+      );
+      cy.get('cx-order-confirmation-thank-you-message').a11yRunContinuumTest();
     });
   });
 }
