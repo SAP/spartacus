1dd4ddec976e69e829a221bf9cf6bce061d65c5d
Anjana BL <84384970+anjana-bl@users.noreply.github.com>
2025-10-03 18:45:02 +0530

    feat: Subscription Billing - PDP, List, Details (#20763)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ feature-libs/subscription-billing/assets/translations/translations.ts
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { TranslationChunksConfig } from '@spartacus/core';
+
+export { en as subscriptionBillingTranslationsEn } from './en/index';
+
+export const subscriptionBillingTranslationChunksConfig: TranslationChunksConfig =
+  {
+    subscriptionBilling: [
+      'subscriptionProduct',
+      'subscriptionList',
+      'subscriptionDetails',
+    ],
+  };
