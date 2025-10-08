6ea545b1c7d3dca71b473a87a9f586c7810ef673
ijitendrasap <jitendra.kumar.shah@sap>
2025-10-08 16:43:47 +0530

    addressing review comments

--- integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.spec.ts
+++ integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.spec.ts
@@ -118,7 +118,7 @@ describe('OpfCheckoutPaymentsComponent', () => {
       of(mockOpfMetadata)
     );
     mockBillingAddressFormService = {
-      paymentOptionsDisabled$: of(false), // Mock the observable
+      paymentOptionsDisabled$: of(false),
     };
 
     await TestBed.configureTestingModule({
