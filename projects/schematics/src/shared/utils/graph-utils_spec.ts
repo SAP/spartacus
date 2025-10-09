c9ac70fe93aa213a575c86255699f6368a27bf8d
ijitendrasap <jitendra.kumar.shah@sap>
2025-10-09 11:50:29 +0530

    fix dependencies error

--- projects/schematics/src/shared/utils/graph-utils_spec.ts
+++ projects/schematics/src/shared/utils/graph-utils_spec.ts
@@ -202,6 +202,7 @@ describe('Graph utils', () => {
           "TMS-GTM",
           "PDF-Invoices",
           "Requested-Delivery-Date",
+          "Pickup-In-Store",
           "Customer-Ticketing",
           "Organization-User-Registration",
           "Administration",
@@ -237,7 +238,6 @@ describe('Graph utils', () => {
           "Product-Variants",
           "Image-Zoom",
           "Bulk-Pricing",
-          "Pickup-In-Store",
           "Quote",
         ]
       `);
