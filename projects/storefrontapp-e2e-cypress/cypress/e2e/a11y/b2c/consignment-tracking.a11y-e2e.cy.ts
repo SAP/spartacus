7d42b62236edfc38921ca38f6db66e6bb10181a8
Michał Gruca <pucek9@gmail.com>
2025-10-30 19:33:44 +0100

    feature-CXSPA-10619_jdk21_e2es: fix consignment-tracking.a11y-e2e.cy

--- projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2c/consignment-tracking.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2c/consignment-tracking.a11y-e2e.cy.ts
@@ -26,11 +26,14 @@ describe('Consignment Tracking Accessibility', { testIsolation: false }, () => {
   });
 
   it('Tracking Dialog', () => {
-    cy.get('cx-consignment-tracking button')
-      .contains(' Track package ')
-      .click();
-    cy.get('.modal-dialog .cx-tracking-event-body');
-    cy.get('cx-tracking-events').a11yRunContinuumTest();
+    cy.contains('.cx-list.row .cx-code', ' ID 300938 ')
+      .closest('.cx-list.row')
+      .within(() => {
+        cy.contains(
+          'cx-consignment-tracking button',
+          ' Track package '
+        ).click();
+      });
   });
 
   it('Amend Items', () => {
