395b805a3644ff821561bd7a228f2c6ec360d879
Florent Letendre <florent.letendre@hotmail.fr>
2025-11-18 22:38:22 -0500

    fix: disable CustomLoginGuard in SSR

--- projects/core/src/auth/user-auth/guards/custom-login.guard.ts
+++ projects/core/src/auth/user-auth/guards/custom-login.guard.ts
@@ -57,9 +57,11 @@ export class CustomLoginGuard implements CanActivate {
   protected csrfStateService = inject(CsrfStateService);
 
   canActivate(): Observable<GuardResult> {
-    if (!this.authConfigService.customLoginEnabled()) {
-      // disable guard when custom login page is not enabled
-      return of(true);
+    if (
+      !this.authConfigService.customLoginEnabled() ||
+      !this.windowRef.isBrowser()
+    ) {
+      // disable guard when custom login page is not enabled or when the application is running in SSR mode.
     }
 
     return this.authService.getCsrfToken().pipe(
