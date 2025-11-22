b0eed56944fb11e3484106347d622d1a22958133
Iustin Serian <serianiustin@gmail.com>
2025-11-22 18:52:13 +0200

    remove order-history mocks and use real backend flow

--- projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2c/order-history.a11y-e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/a11y/b2c/order-history.a11y-e2e.cy.ts
@@ -4,45 +4,42 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  mockOrderDetails,
-  mockOrdersListEN,
-} from '../../../helpers/mock-order-history';
-import { doPlaceOrder } from '../../../helpers/order-history';
 import { viewportContext } from '../../../helpers/viewport-context';
+import { doPlaceOrder } from '../../../helpers/order-history';
 
 describe('Order History Page accessibility', { testIsolation: false }, () => {
   viewportContext(['mobile', 'desktop'], () => {
     before(() => {
       cy.a11yContinuumSetup();
       cy.requireLoggedIn();
-      doPlaceOrder().then(() => {
-        cy.intercept('GET', /\/users\/current\/orders.*curr=USD.*/, {
-          fixture: 'orders/orders-list-en.json',
-        }).as('ordersEN');
+      doPlaceOrder().then((orderData: any) => {
+        cy.waitForOrderToBePlacedRequest(
+          undefined,
+          undefined,
+          orderData.body.code
+        );
         cy.visit('/my-account/orders');
       });
     });
 
     it('Order list', () => {
-      cy.wait('@ordersEN');
       cy.get('#order-history-table'); // wait until content is loaded
       cy.get('main').a11yRunContinuumTest();
     });
 
     it('Order details', () => {
-      mockOrderDetails();
       cy.get('.cx-order-history-code > .cx-order-history-value')
         .first()
         .click();
-      cy.wait('@orderDetails');
 
       cy.get('.cx-order-details-cards'); // wait until content is loaded
       cy.get('main').a11yRunContinuumTest();
     });
 
     it('Cancel order', () => {
-      cy.get('cx-order-details-actions a.btn-secondary').click();
+      cy.get('cx-order-details-actions .btn-secondary')
+        .contains(' Cancel Items ')
+        .click();
       cy.get('.cx-amend-order-items'); // wait until content is loaded
       cy.get('main').a11yRunContinuumTest();
     });
