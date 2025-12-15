734080a0c9f3e870b73fcfaaf91a213394c910b7
Pawel Fras <fras.pawel@yahoo.com>
2025-12-15 13:42:19 +0100

    feat: CXSPA-11613 Migration - removal of obsolate 'index' property from 'angular.json'

---
+++ projects/schematics/src/migrations/221121_6/angular-config/index.ts
@@ -0,0 +1,70 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserBuilderOptions } from '@angular-devkit/build-angular';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  getDefaultProjectNameFromWorkspace,
+  getWorkspace,
+} from '../../../shared/utils/workspace-utils';
+
+/**
+ * Removes the obsolete `index` property from angular.json build configuration.
+ *
+ * The `index` property is no longer needed in Angular 20+ with the application builder.
+ * This migration removes it from the build options to clean up the configuration.
+ * For more, see: https://github.com/angular/angular-cli/commit/901ab60d9f63fcff17213dbf7fe17e4a46835974
+ */
+export function migrate(): Rule {
+  return (tree: Tree, context: SchematicContext): Tree => {
+    context.logger.info(
+      '\n⌛️ Removing obsolete "index" property from angular.json...'
+    );
+
+    const { path, workspace: angularJson } = getWorkspace(tree);
+    const projectName = getDefaultProjectNameFromWorkspace(tree);
+
+    const project = angularJson.projects[projectName];
+    const architect = project.architect;
+    const build = architect?.build;
+    const options = build?.options as BrowserBuilderOptions;
+
+    if (!options?.index) {
+      context.logger.info('  ↳ No "index" property found to remove');
+      return tree;
+    }
+
+    context.logger.info('  ↳ Removing "index" property from build options');
+
+    const updatedAngularJson = {
+      ...angularJson,
+      projects: {
+        ...angularJson.projects,
+        [projectName]: {
+          ...project,
+          architect: {
+            ...architect,
+            build: {
+              ...build,
+              options: {
+                ...options,
+                index: undefined,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    tree.overwrite(path, JSON.stringify(updatedAngularJson, null, 2));
+
+    context.logger.info(
+      '✅ Removed obsolete "index" property from angular.json'
+    );
+
+    return tree;
+  };
+}
