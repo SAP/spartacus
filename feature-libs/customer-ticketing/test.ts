f370152df3169fd9613633c2a2e5f270747005c4
Sergiej Drozd <siarhei.drozd@sap.com>
2025-12-17 08:50:15 +0100

    docs(testing): add comments explaining ZoneChangeDetectionModule workaround

--- feature-libs/customer-ticketing/test.ts
+++ feature-libs/customer-ticketing/test.ts
@@ -13,6 +13,10 @@ import {
   platformBrowserTesting,
 } from '@angular/platform-browser/testing';
 
+// Angular 21 introduced a change that causes NG0100 errors in Karma tests.
+// See: https://github.com/angular/angular-cli/issues/32047
+// Angular fixed this for built-in test.ts: https://github.com/angular/angular-cli/pull/32049
+// Since we use a custom test.ts, we must manually provide zone change detection.
 @NgModule({
   providers: [provideZoneChangeDetection()],
 })
--- projects/storefrontapp/src/test.ts
+++ projects/storefrontapp/src/test.ts
@@ -28,6 +28,10 @@ __karma__.loaded = function () {
   // Intentional empty function
 };
 
+// Angular 21 introduced a change that causes NG0100 errors in Karma tests.
+// See: https://github.com/angular/angular-cli/issues/32047
+// Angular fixed this for built-in test.ts: https://github.com/angular/angular-cli/pull/32049
+// Since we use a custom test.ts, we must manually provide zone change detection.
 @NgModule({
   providers: [provideZoneChangeDetection()],
 })
