429516bf3bda53ec1ef1e7460dd84ccddb5922c5
Cris Li <cris.li01@sap.com>
2026-01-05 16:55:06 +0800

    Revert "Add license header"
    This reverts commit 5829fcaec51bb2377fbe8b782bb5aa26753ac1c6.

--- feature-libs/subscription-billing/components/actions-modal/subscription-actions-popup.config.ts
+++ feature-libs/subscription-billing/components/actions-modal/subscription-actions-popup.config.ts
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2025 SAP SE or an SAP affiliate company. All rights reserved.
  * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
- * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
--- projects/core/src/util/glob-utils.ts
+++ projects/core/src/util/glob-utils.ts
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2010-2019 Google LLC. http://angular.io/license
  * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
- * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
