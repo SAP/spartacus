0f9e19413ca39887795a7bbc744cf8940cd2f488
Pawel Fras <pawel.fras@sap.com>
2026-01-07 13:07:02 +0100

    chore: Update license header for declaration merging builder

--- tools/build-lib/declaration-merging/index.js
+++ tools/build-lib/declaration-merging/index.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
- * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
