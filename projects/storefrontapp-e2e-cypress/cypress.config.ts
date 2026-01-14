7020c75d111bbb5ac74da83f67fcc5049c0af5da
katechuen <kate.chuen.cheong@sap.com>
2026-01-14 11:19:48 -0500

    chore: run 2602 jdk21 e2es ng21 upgrade

--- projects/storefrontapp-e2e-cypress/cypress.config.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ts
@@ -16,9 +16,9 @@ export default defineConfig({
   },
   videoUploadOnPasses: false,
   env: {
-    CLIENT_ID: 'mobile_android',
+    CLIENT_ID: 'mobile_android_public',
     CLIENT_SECRET: 'secret',
-    API_URL: 'https://40.76.109.9:9002',
+    API_URL: 'https://spartacus-test.eastus.cloudapp.azure.com:8443',
     BASE_SITE: 'electronics-spa',
     BASE_LANG: 'en',
     BASE_CURRENCY: 'USD',
@@ -27,7 +27,7 @@ export default defineConfig({
     OCC_PREFIX_ORDER_ENDPOINT: 'orders',
     MAIL_CCV2_URL: 'https://mailhog-poc.westeurope.cloudapp.azure.com',
     MAIL_CCV2_PREFIX: '/api/v1',
-    JDK_VERSION: 'JDK17',
+    JDK_VERSION: 'JDK21',
   },
   e2e: {
     // We've imported your old cypress plugins here.
