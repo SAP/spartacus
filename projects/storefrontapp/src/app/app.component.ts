181b7958e09295674985875e56876d241a4df97e
Krzysztof Platis <krzysztof.platis@sap.com>
2026-01-14 22:04:23 +0100

    doc: add jsdoc to app component

--- projects/storefrontapp/src/app/app.component.ts
+++ projects/storefrontapp/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+/**
+ * Root component that belongs to the example app, not Spartacus libraries.
+ * In customers' applications, the analogical root AppComponent belongs to the custom app.
+ */
 @Component({
   selector: 'app-root',
   template: `<cx-storefront></cx-storefront>`,
+
+  // eslint-disable-next-line @angular-eslint/prefer-standalone -- This component must be non-standalone to support NgModule.bootstrap[] in the AppModule
   standalone: false,
 })
 export class AppComponent {}
