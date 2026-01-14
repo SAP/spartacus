733a02b0575495ef2b26d0e59f18fee299fc7c93
sdrozdsap <163305268+sdrozdsap@users.noreply.github.com>
2026-01-14 13:07:55 +0100

    chore: Upgrade to Angular 21 (#20946)
    Nx migration:
    - "@nx/angular" bumped to 22.3.1
    - "@nx/devkit" bumped to 22.3.1
    - "@nx/eslint-plugin" bumped to 22.3.1
    - "@nx/jest" bumped to 22.3.1
    - "@nx/workspace" bumped to 22.3.1
    - "nx" bumped to 22.3.1
    
    Angular migration:
    - "@angular/core" bumped to 21.0.8
    - "@angular/animations" bumped to 21.0.8
    - "@angular/common" bumped to 21.0.8
    - "@angular/compiler" bumped to 21.0.8
    - "@angular/forms" bumped to 21.0.8
    - "@angular/platform-browser" bumped to 21.0.8
    - "@angular/platform-browser-dynamic" bumped to 21.0.8
    - "@angular/platform-server" bumped to 21.0.8
    - "@angular/router" bumped to 21.0.8
    - "@angular/service-worker" bumped to 21.0.8
    - "@angular/cli" bumped to 21.0.5
    - "@angular/compiler-cli" bumped to 21.0.8
    - "@angular/language-service" bumped to 21.0.8
    - "@angular/ssr" bumped to 21.0.5
    - "@angular/pwa" bumped to 21.0.5
    - "@angular/build" bumped to 21.0.5
    - "@angular-devkit/build-angular" bumped to 21.0.5
    - "@angular-devkit/core" bumped to 21.0.5
    - "@angular-devkit/schematics" bumped to 21.0.5
    - "@schematics/angular" bumped to 21.0.5
    - "@angular-builders/custom-esbuild" bumped to 21.0.0-beta.1
    - "ng-packagr" bumped to 21.0.1
    - "zone.js" bumped to 0.16.0
    
    Angular ESLint:
    - "@angular-eslint/builder" bumped to 21.1.0
    - "@angular-eslint/eslint-plugin" bumped to 21.1.0
    - "@angular-eslint/eslint-plugin-template" bumped to 21.1.0
    - "@angular-eslint/schematics" bumped to 21.1.0
    - "@angular-eslint/template-parser" bumped to 21.1.0
    - "@angular-eslint/test-utils" bumped to 21.1.0
    - "@angular-eslint/utils" bumped to 21.1.0
    
    NgRx:
    - "@ngrx/effects" bumped to 21.0.1
    - "@ngrx/operators" bumped to 21.0.1
    - "@ngrx/router-store" bumped to 21.0.1
    - "@ngrx/store" bumped to 21.0.1
    - "@ngrx/store-devtools" bumped to 21.0.1
    
    Other dependencies:
    - "@ng-select/ng-select" bumped to 21.1.4
    - "ngx-infinite-scroll" bumped to 21.0.0
    - "jest-preset-angular" bumped to 16.0.0
    
    Build issues:
    - added global type augmentations for `@HostListener` decorators with platform event syntax
      see: https://github.com/SAP/spartacus/pull/20967
    - fixed declaration-merging builder for ng-packagr v21 output structure change
    - updated tsconfig files: changed `module` from `es2022` to `preserve`, added `moduleResolution: bundler`
    - removed `lib: ["es2022", "dom"]` from tsconfig.lib.json files
    - removed `isolatedModules` from tsconfig.spec.json files
    - disabled `prefer-control-flow` ESLint rule
    
    Unit tests:
    - replaced `BrowserDynamicTestingModule`/`platformBrowserDynamicTesting` with `BrowserTestingModule`/`platformBrowserTesting`
    - added `provideZoneChangeDetection()` via custom `ZoneChangeDetectionModule` in all test config files
    - updated snapshots across multiple libraries
    - fixed failing unit tests (checkout components, HorizontalScrollingPositionDirective, guest registration form)
    - added `transformIgnorePatterns` to jest schematics configs to support ESM modules
    
    Schematics:
    - Installation and migration docs adjustments 
    see: https://github.com/SAP/spartacus/pull/20970
    - Installation script update 
    see: https://github.com/SAP/spartacus/pull/20987
    - Remove redundant 'applyClassicNamingConvention' from schematics 
    see: https://github.com/SAP/spartacus/pull/20986
    - add missing semicolon in the generated app server.ts 
    see: https://github.com/SAP/spartacus/pull/20988
    - Updated schematics snapshots
    
    **SSR:**
    - Issues with SSR Cypress E2Es 
    see: https://github.com/SAP/spartacus/pull/20996

--- feature-libs/product-configurator/rulebased/components/conflict-suggestion/configurator-conflict-suggestion.component.ts
+++ feature-libs/product-configurator/rulebased/components/conflict-suggestion/configurator-conflict-suggestion.component.ts
@@ -26,7 +26,7 @@ export class ConfiguratorConflictSuggestionComponent {
   groupType = Configurator.GroupType;
 
   @HostBinding('tabindex') tabindex = '0';
-  @HostBinding('role') role = 'note';
+  @HostBinding('attr.role') role = 'note';
 
   constructor() {
     // Intentional empty constructor
