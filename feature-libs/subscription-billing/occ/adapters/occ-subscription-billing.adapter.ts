f6c92904c6d2c6e989a282554d7faa12e6e6e8f3
Chandan <chandan.kumar.shaw@sap.com>
2026-01-16 12:19:39 +0530

    feat: add view bill colum in the billing list table and implement api call methods

--- feature-libs/subscription-billing/occ/adapters/occ-subscription-billing.adapter.ts
+++ feature-libs/subscription-billing/occ/adapters/occ-subscription-billing.adapter.ts
@@ -5,20 +5,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { LoggerService, OccEndpointsService, tryNormalizeHttpError } from '@spartacus/core';
 import { SubscriptionBillingAdapter } from '@spartacus/subscription-billing/core';
 import { SubscriptionBill, SubscriptionBillsList } from '@spartacus/subscription-billing/root';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable()
 export class OccSubscriptionBillingAdapter implements SubscriptionBillingAdapter {
-    
+    protected logger = inject(LoggerService);
+    protected http = inject(HttpClient);
+    protected occEndpoints = inject(OccEndpointsService);
+
     getSubscriptionBillsList(userId: string, pageSize?: number, currentPage?: number, sort?: string, filter?: string): Observable<SubscriptionBillsList> {
-        throw new Error('Method not implemented.');
+        const url = this.occEndpoints.buildUrl('subscriptionBillsList', {
+            urlParams: {
+                userId,
+            },
+            queryParams: {
+                pageSize,
+                currentPage,
+                sort,
+                filter,
+            }
+        });
+        return this.http.get<SubscriptionBillsList>(url).pipe(
+            catchError(error => {
+                throw tryNormalizeHttpError(error, this.logger);
+            })
+        );
     }
 
-    getSubscriptionBillByCode(userId: string, billCode: string): Observable<SubscriptionBill> {
-        throw new Error('Method not implemented.');
+    getSubscriptionBillByCode(userId: string, billId: string): Observable<SubscriptionBill> {
+        const url = this.occEndpoints.buildUrl('subscriptionBillsList', {
+            urlParams: {
+                userId,
+                billId
+            }
+        });
+        return this.http.get<SubscriptionBill>(url).pipe(
+            catchError(error => {
+                throw tryNormalizeHttpError(error, this.logger);
+            })
+        );
     }
 
 }
