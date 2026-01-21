f6372aebc2b88f3caf868c73d95740372e682740
sdrozdsap <163305268+sdrozdsap@users.noreply.github.com>
2026-01-21 06:27:43 +0100

    fix: Future stock accordion breaks unique ID rule (#21033)

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -307,6 +307,7 @@ if (environment.cpq) {
         productReviewCharactersLeft: true,
         a11yNgSelectAriaControls: true,
         a11yConfiguratorOverviewHeaderVPC: true,
+        a11yFutureStockAccordionAriaControls: true,
         enableReadDomainValuesOnDemand: true,
         opfEnablePreventingFromCheckoutWithoutEmail: true,
         storeFinderFacadeCleanup: true,
