30c2d0d960f5a27577ef074d06b1e3ce7bc54fc3
Krzysztof Platis <krzysztof.platis@sap.com>
2026-01-26 10:08:02 +0100

    test: move tests of `removeImportUsingTsMorph` to file-utils_spec.ts

--- projects/schematics/src/shared/utils/file-utils_spec.ts
+++ projects/schematics/src/shared/utils/file-utils_spec.ts
@@ -16,6 +16,7 @@ import {
 } from '@schematics/angular/utility/change';
 import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
 import * as path from 'path';
+import { Project } from 'ts-morph';
 import ts from 'typescript';
 import { COMPONENT_DEPRECATION_DATA } from '../../migrations/test/component-deprecations/component-deprecations';
 import {
@@ -55,6 +56,7 @@ import {
   isCandidateForConstructorDeprecation,
   isInheriting,
   removeConstructorParam,
+  removeImportUsingTsMorph,
   removeInjectImports,
   renameIdentifierNode,
   shouldRemoveDecorator,
@@ -1065,6 +1067,124 @@ describe('File utils', () => {
         expect(changes).toEqual([
           new ReplaceChange(filePath, 274, oldName, newName),
         ]);
+      });
+    });
+  });
+
+  describe('No Workspace', () => {
+    describe('removeImportUsingTsMorph', () => {
+      let project: Project;
+
+      beforeEach(() => {
+        project = new Project();
+      });
+
+      it('should remove a named import from import declaration', () => {
+        const sourceFile = project.createSourceFile(
+          'test.ts',
+          `
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+export class TestClass implements OnInit, OnDestroy {
+  ngOnInit() {}
+  ngOnDestroy() {}
+}
+      `
+        );
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: '@angular/core',
+          importName: 'OnDestroy',
+        });
+
+        const result = sourceFile.getText();
+        expect(result).toMatchSnapshot();
+      });
+
+      it('should remove entire import declaration if last named import is removed', () => {
+        const sourceFile = project.createSourceFile(
+          'test.ts',
+          `
+import { OnlyImport } from './some-module';
+import { Component } from '@angular/core';
+
+export class TestClass {}
+      `
+        );
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: './some-module',
+          importName: 'OnlyImport',
+        });
+
+        const result = sourceFile.getText();
+        expect(result).toMatchSnapshot();
+      });
+
+      it('should do nothing if import path does not exist', () => {
+        const sourceFile = project.createSourceFile(
+          'test.ts',
+          `
+import { Component } from '@angular/core';
+
+export class TestClass {}
+      `
+        );
+
+        const originalText = sourceFile.getText();
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: './non-existent-module',
+          importName: 'SomeThing',
+        });
+
+        const result = sourceFile.getText();
+        expect(result).toBe(originalText);
+      });
+
+      it('should do nothing if import name does not exist in the declaration', () => {
+        const sourceFile = project.createSourceFile(
+          'test.ts',
+          `
+import { Component, OnInit } from '@angular/core';
+
+export class TestClass implements OnInit {}
+      `
+        );
+
+        const originalText = sourceFile.getText();
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: '@angular/core',
+          importName: 'NonExistent',
+        });
+
+        const result = sourceFile.getText();
+        expect(result).toBe(originalText);
+      });
+
+      it('should allow for being called multiple times on the same file', () => {
+        const sourceFile = project.createSourceFile(
+          'test.ts',
+          `
+import { A, B, C, D } from './module';
+
+export class TestClass {}
+      `
+        );
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: './module',
+          importName: 'B',
+        });
+
+        removeImportUsingTsMorph(sourceFile, {
+          importPath: './module',
+          importName: 'D',
+        });
+
+        const result = sourceFile.getText();
+        expect(result).toMatchSnapshot();
       });
     });
   });
