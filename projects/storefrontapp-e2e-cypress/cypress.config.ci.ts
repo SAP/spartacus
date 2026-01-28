12eab244802a127179f375a6e73ab4e196cb6c02
katechuen <kate.chuen.cheong@sap.com>
2026-01-28 12:17:27 -0500

    chore: run spa 2602 against cx 2211-jdk21.4

--- projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
+++ projects/storefrontapp-e2e-cypress/cypress.config.ci.ts
@@ -18,18 +18,18 @@ export default defineConfig({
   },
   videoUploadOnPasses: false,
   env: {
-    API_URL: 'https://20.83.184.244:9002',
+    API_URL: 'https://spartacus-test.eastus.cloudapp.azure.com:8443',
     BASE_SITE: 'electronics-spa',
     BASE_LANG: 'en',
     BASE_CURRENCY: 'USD',
-    CLIENT_ID: 'mobile_android',
+    CLIENT_ID: 'mobile_android_public',
     CLIENT_SECRET: 'secret',
     OCC_PREFIX: '/occ/v2',
     OCC_PREFIX_USER_ENDPOINT: 'users',
     OCC_PREFIX_ORDER_ENDPOINT: 'orders',
     MAIL_CCV2_URL: 'https://mailhog-poc.westeurope.cloudapp.azure.com',
     MAIL_CCV2_PREFIX: '/api/v1',
-    JDK_VERSION: 'JDK17',
+    JDK_VERSION: 'JDK21',
   },
   e2e: {
     // We've imported your old cypress plugins here.
--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -323,7 +323,7 @@ if (environment.cpq) {
         reserveSpaceForImagesOnPdpAndPlp: true,
         lazyLoadImagesByDefault: true,
         incrementProcessesCountForMergeCart: true,
-        authorizationCodeFlowByDefault: false,
+        authorizationCodeFlowByDefault: true,
         defaultLayoutConfigWithoutPageFold: true,
         navigationMenuCloseOnSameLinkClick: true,
         enablePasswordExpiredErrorTranslation: true,
