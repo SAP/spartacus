ba1b7c39875e8aa9181b9c3a0ab39cdf2f6ea5e5
Chandan <chandan.kumar.shaw@sap.com>
2026-02-02 12:32:07 +0530

    chore: refactor cypress test to include billing related in the subscription test file

--- projects/storefrontapp-e2e-cypress/cypress/helpers/vendor/subscription-billing/subscription.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/vendor/subscription-billing/subscription.ts
@@ -384,3 +384,34 @@ export function validateSubscriptionCharges() {
     .should('contain.text', 'monthly payment')
     .and('contain.text', ' pay on checkout ');
 }
+
+export function validateSubscriptionBillingList() {
+  cy.visit('/powertools-spa/en/USD/my-account/subscription-bills');
+
+  // table headers
+  cy.get('.cx-billing-list-table .cx-billing-list-thead')
+    .should('exist')
+    .within(() => {
+      cy.contains('.cx-billing-list-table-header', 'Bill ID/Number').should(
+        'exist'
+      );
+      cy.contains('.cx-billing-list-table-header', 'Billing Date').should(
+        'exist'
+      );
+      cy.contains('.cx-billing-list-table-header', 'Subscriptions').should(
+        'exist'
+      );
+      cy.contains('.cx-billing-list-table-header', 'Total').should('exist');
+    });
+
+  // first row data
+  cy.get('.cx-billing-list-table tbody tr')
+    .eq(0)
+    .should('exist')
+    .within(() => {
+      cy.get('td').eq(0).should('exist'); // Bill ID/Number value
+      cy.get('td').eq(1).should('exist'); // Billing Date value
+      cy.get('td').eq(2).should('exist'); // Subscriptions value
+      cy.get('td').eq(3).should('exist'); // Total value
+    });
+}
