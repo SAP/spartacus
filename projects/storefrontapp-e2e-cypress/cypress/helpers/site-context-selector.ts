1ca325bd8bd8e1cd50f3ec974cdc9174ee4cda25
Florent Letendre <florent.letendre01@sap.com>
2026-02-01 22:30:50 -0500

    fix language test

--- projects/storefrontapp-e2e-cypress/cypress/helpers/site-context-selector.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/site-context-selector.ts
@@ -56,9 +56,7 @@ export const PRODUCT_NAME_DETAILS_DE = 'Stativ mit Fernbedienung';
 export const PRODUCT_NAME_SEARCH_DE =
   'FUN Einwegkamera mit Blitz, 27+12 Bilder';
 export const TITLE_DE = 'Herr';
-export const MONTH_DE = new Date().toLocaleDateString('de-DE', {
-  month: 'long',
-});
+export const MONTH_DE_JANUARY = 'Januar';
 export const EDIT_DE = 'Bearbeiten';
 
 export const PRODUCT_PATH_1 = `/product/${PRODUCT_ID_1}`;
@@ -201,7 +199,7 @@ export function verifySiteContextChangeUrl(
 export function testLangSwitchOrderPage() {
   describe('order page', () => {
     const orderPath = ORDER_PATH;
-    const deutschName = MONTH_DE;
+    const deutschName = MONTH_DE_JANUARY;
 
     before(() => {
       goToOrderHistoryWithConsignedOrder();
