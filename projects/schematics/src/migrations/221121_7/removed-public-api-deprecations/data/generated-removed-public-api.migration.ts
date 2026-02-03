fad5e2019c1329876e63cdcd978066ee024ae51c
Michał Gruca <michal.gruca@sap.com>
2026-02-03 12:33:47 +0100

    feature/CXSPA-11884: add generated migration files for renamed symbols, removed public API, and constructor deprecations

---
+++ projects/schematics/src/migrations/221121_7/removed-public-api-deprecations/data/generated-removed-public-api.migration.ts
@@ -0,0 +1,36 @@
+import { DeprecatedNode } from '../../../../shared/utils/file-utils';
+//
+// Generated file, don't update manually.
+//
+export const GENERATED_REMOVED_PUBLIC_API_DATA: DeprecatedNode[] = [
+	{
+		node: 'CDC_USER_PREFERENCE_SERIALIZER',
+		importPath: '@spartacus/cdc/root',
+		comment: '// TODO:Spartacus - Variable CDC_USER_PREFERENCE_SERIALIZER has been removed and is no longer part of the public API. '
+	},
+	{
+		node: 'CdcUserPreferenceSerializer',
+		importPath: '@spartacus/cdc/root',
+		comment: '// TODO:Spartacus - Class CdcUserPreferenceSerializer has been removed and is no longer part of the public API. '
+	},
+	{
+		node: 'defaultUserCmsModuleConfig',
+		importPath: '@spartacus/core',
+		comment: '// TODO:Spartacus - Variable defaultUserCmsModuleConfig has been removed and is no longer part of the public API. '
+	},
+	{
+		node: 'SMART_EDIT_DUMMY_COMPONENT_TYPE',
+		importPath: '@spartacus/core',
+		comment: '// TODO:Spartacus - Variable SMART_EDIT_DUMMY_COMPONENT_TYPE has been removed and is no longer part of the public API. '
+	},
+	{
+		node: 'USER_CMS_ENDPOINTS',
+		importPath: '@spartacus/core',
+		comment: '// TODO:Spartacus - Variable USER_CMS_ENDPOINTS has been removed and is no longer part of the public API. '
+	},
+	{
+		node: 'USE_LEGACY_MEDIA_COMPONENT',
+		importPath: '@spartacus/storefront',
+		comment: '// TODO:Spartacus - Variable USE_LEGACY_MEDIA_COMPONENT has been removed and is no longer part of the public API. '
+	}
+];
