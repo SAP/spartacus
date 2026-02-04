c9f3b44ac57d233519268473c2f09b9ea37b19d3
Krzysztof Platis <krzysztof.platis@sap.com>
2026-02-04 16:08:17 +0100

    refactor: move OCC backend configuration to app.module.ts

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,7 +8,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { provideConfig } from '@spartacus/core';
 import { AppRoutingModule } from '@spartacus/storefront';
+import { environment } from '../environments/environment';
 import { privateProviders } from './private/private.providers';
 import { SpartacusModule } from './spartacus/spartacus.module';
 
@@ -20,6 +22,17 @@ import { SpartacusModule } from './spartacus/spartacus.module';
     EffectsModule.forRoot([]),
     SpartacusModule,
   ],
-  providers: [privateProviders],
+  providers: [
+    provideConfig({
+      // Custom OCC backend configuration:
+      backend: {
+        occ: {
+          baseUrl: environment.occBaseUrl,
+          prefix: environment.occApiPrefix,
+        },
+      },
+    }),
+    privateProviders,
+  ],
 })
 export class AppModule {}
--- projects/storefrontapp/src/app/spartacus/spartacus-configuration.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-configuration.module.ts
@@ -47,15 +47,6 @@ let spartacusChannelSpecificConfigurationProviders = environment.b2b
     provideConfig(mediaConfig),
     ...defaultCmsContentProviders,
     provideConfig({
-      // Custom OCC backend configuration:
-      backend: {
-        occ: {
-          baseUrl: environment.occBaseUrl,
-          prefix: environment.occApiPrefix,
-        },
-      },
-    }),
-    provideConfig({
       pwa: {
         enabled: true,
         addToHomeScreen: true,
