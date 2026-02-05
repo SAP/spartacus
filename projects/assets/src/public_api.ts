6e30604f321941c685a8d7a83a0766cd9bc7bc1b
Stanislav Sukhanov <sukhanov.stas@gmail.com>
2026-02-05 11:17:55 +0100

    refactor: (CXSPA-12031) Remove deprecated big translations objects in favor of language-specific one (#21119)
    Co-authored-by: Stanislav Sukhanov <stanislav.sukhanov@sap.com>
    Co-authored-by: Michał Gruca <pucek9@gmail.com>

--- projects/assets/src/public_api.ts
+++ projects/assets/src/public_api.ts
@@ -8,20 +8,5 @@
  * Public API Surface of assets
  */
 
-import { translationsEn } from './translations/translations';
 export * from './translations/translation-chunks-config';
 export * from './translations/translations';
-
-/**
- * @deprecated Please use **specific language** translations (suffixed with language code) instead,
- * like in the following example:
- *             ```diff
- *               i18n: {
- *             -   resources: translations
- *             +   resources: { en: translationsEn }
- *               }
- *             ```
- */
-export const translations = {
-  en: translationsEn,
-};
