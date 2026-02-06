22e8339c3f516e549e56634e10741bcb9986846c
Chandan <chandan.kumar.shaw@sap.com>
2026-02-07 00:15:10 +0530

    test: add bill details rendering test

--- feature-libs/subscription-billing/components/billing-details/subscription-billing-details.component.spec.ts
+++ feature-libs/subscription-billing/components/billing-details/subscription-billing-details.component.spec.ts
@@ -4,6 +4,7 @@ import { SubscriptionBillingDetailsComponent } from './subscription-billing-deta
 import {
   EventService,
   I18nTestingModule,
+  LanguageService,
   TranslationService,
 } from '@spartacus/core';
 import {
@@ -12,12 +13,34 @@ import {
 } from '@spartacus/subscription-billing/root';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Pipe, PipeTransform } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 const routerParam$: BehaviorSubject<{
   [key: string]: string;
 }> = new BehaviorSubject({});
 const mockSubBills: SubscriptionBill = {
   id: 's1',
+  documentNumber: '5776',
+  billAt: '2026-04-11T00:00:00+0000',
+  periodStartAt: '2026-01-09T00:00:00+0000',
+  periodEndAt: '2026-04-09T00:00:00+0000',
+  netAmount: 'USD 50.00',
+  items: [
+    {
+      productCode: 'SAPCPQ_EDITRATIO_FORMAT_TIERS_cpq',
+      productName: 'SAPCPQ_EDITRATIO_FORMAT_TIERS',
+      subscriptionDocumentNumber: '807',
+      subscriptionId: '86B01278-B670-4812-B69C-55E41439D59E',
+      netAmount: 'USD 50.00',
+      usageCharges: [
+        {
+          typeName: 'Charge',
+          netAmount: 'USD 50.00',
+        },
+      ],
+    },
+  ],
 };
 class MockSubscriptionBillingFacade
   implements Partial<SubscriptionBillingFacade>
@@ -44,6 +67,22 @@ class MockUrlPipe implements PipeTransform {
   transform(): any {}
 }
 
+class MockLanguageService {
+  isocode = new BehaviorSubject('');
+
+  getActive(): Observable<string> {
+    return this.isocode;
+  }
+
+  setActive(isocode: string) {
+    this.isocode.next(isocode);
+  }
+}
+
+class MockActivatedRoute {
+  constructor(public snapshot: any) {}
+}
+
 describe('SubscriptionBillingDetailsComponent', () => {
   let component: SubscriptionBillingDetailsComponent;
   let fixture: ComponentFixture<SubscriptionBillingDetailsComponent>;
@@ -63,6 +102,8 @@ describe('SubscriptionBillingDetailsComponent', () => {
           provide: SubscriptionBillingFacade,
           useClass: MockSubscriptionBillingFacade,
         },
+        { provide: ActivatedRoute, useValue: new MockActivatedRoute({}) },
+        { provide: LanguageService, useClass: MockLanguageService },
       ],
     }).compileComponents();
 
@@ -78,6 +119,36 @@ describe('SubscriptionBillingDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should render the bill details correctly', () => {
+    fixture.detectChanges();
+    const idEl = fixture.debugElement.query(By.css('.subscription-id'));
+    expect(idEl.nativeElement.textContent).toContain('5776');
+
+    const netAmountEl = fixture.debugElement.query(
+      By.css('.subscription-bill-item-total')
+    );
+    expect(netAmountEl).toBeDefined();
+
+    expect(
+      fixture.debugElement.query(By.css('.subscription-bill-item-body'))
+    ).toBeDefined();
+
+    const billDetailEl = fixture.debugElement.queryAll(
+      By.css('.subscription-bill-item-details')
+    );
+    expect(billDetailEl.length).toEqual(1);
+    expect(billDetailEl[0].children.length).toBe(4);
+    expect(billDetailEl[0].children[0].nativeElement.textContent).toContain(
+      'SAPCPQ_EDITRATIO_FORMAT_TIERS'
+    );
+    expect(billDetailEl[0].children[1].nativeElement.textContent).toContain(
+      'USD 50.00'
+    );
+    expect(billDetailEl[0].children[2].nativeElement.textContent).toContain(
+      'USD 50.00'
+    );
   });
 
   it('should reload data', () => {
