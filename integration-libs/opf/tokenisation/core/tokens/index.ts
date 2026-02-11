4c8e29588ef742b44ba2103c77e652c77288486a
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-02-11 10:44:42 +0530

    Update index.ts

--- integration-libs/opf/tokenisation/core/tokens/index.ts
+++ integration-libs/opf/tokenisation/core/tokens/index.ts
@@ -5,5 +5,3 @@
  */
 
 // export * from './tokens';
-// The above export is commented out to avoid circular dependency issues. It can be uncommented once the circular dependency is resolved.
-export {};
