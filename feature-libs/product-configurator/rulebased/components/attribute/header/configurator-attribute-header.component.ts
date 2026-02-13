2e5bb1878e3ffcafd148505e4c08ea75ba963089
steinsebastian <54309425+steinsebastian@users.noreply.github.com>
2026-02-13 15:22:46 +0100

    feat: CXSPA-11888: Handle non navigable conflicts (#21143)

--- feature-libs/product-configurator/rulebased/components/attribute/header/configurator-attribute-header.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/header/configurator-attribute-header.component.ts
@@ -180,11 +180,19 @@ export class ConfiguratorAttributeHeaderComponent
    * @return {string} - the conflict link key
    */
   getConflictMessageKey(): string {
-    return this.groupType === Configurator.GroupType.CONFLICT_GROUP
-      ? 'configurator.conflict.viewConfigurationDetails'
-      : this.isNavigationToConflictEnabled()
-        ? 'configurator.conflict.viewConflictDetails'
-        : 'configurator.conflict.conflictDetected';
+    if (this.groupType === Configurator.GroupType.CONFLICT_GROUP) {
+      return 'configurator.conflict.viewConfigurationDetails';
+    }
+
+    if (this.attribute.hasNonNavigableConflict) {
+      return 'configurator.conflict.nonNavigableConflict';
+    }
+
+    if (this.isNavigationToConflictEnabled()) {
+      return 'configurator.conflict.viewConflictDetails';
+    }
+
+    return 'configurator.conflict.conflictDetected';
   }
 
   /**
