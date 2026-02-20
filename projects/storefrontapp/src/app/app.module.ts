eea385b3ab63c1cbd4241d10c768cd3c836bcc0f
Krzysztof Platis <krzysztof.platis@sap.com>
2026-02-20 19:22:49 +0100

    Revert "fix: move back storefrontapp occ config to 'app.module.ts', and restore b2b- and b2c-configuration.module.ts (#21120)"
    This reverts commit 70bc432142621a74b3509c1c7900b323fc658095.

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,9 +8,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { provideConfig } from '@spartacus/core';
 import { AppRoutingModule } from '@spartacus/storefront';
-import { environment } from '../environments/environment';
 import { privateProviders } from './private/private.providers';
 import { SpartacusModule } from './spartacus/spartacus.module';
 
@@ -22,18 +20,6 @@ import { SpartacusModule } from './spartacus/spartacus.module';
     EffectsModule.forRoot([]),
     SpartacusModule,
   ],
-  providers: [
-    provideConfig({
-      backend: {
-        occ: {
-          // Note: The next line is edited by our internal script in the `ec-automate-pipelines` repo. Don't move it to other file.
-          baseUrl: environment.occBaseUrl,
-          prefix: environment.occApiPrefix,
-        },
-      },
-    }),
-
-    privateProviders,
-  ],
+  providers: [privateProviders],
 })
 export class AppModule {}
