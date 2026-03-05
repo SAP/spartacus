80e54e107d1027869115f8eb1cfa4bde91604734
SadhanBose <sadhanbose.s01@sap.com>
2026-03-05 15:02:35 +0530

    Update app.module.ts

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { provideConfig } from '@spartacus/core';
+import { CmsConfig, provideConfig } from '@spartacus/core';
 import { AppRoutingModule } from '@spartacus/storefront';
 import { environment } from '../environments/environment';
 import { privateProviders } from './private/private.providers';
