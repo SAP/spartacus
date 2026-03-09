f2016a4edc427364a3a2237ea8be88099fafb067
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-03-09 16:39:55 +0530

    OpfTokenisationService test cases

--- integration-libs/opf/tokenisation/core/facade/opf-tokenisation.service.spec.ts
+++ integration-libs/opf/tokenisation/core/facade/opf-tokenisation.service.spec.ts
@@ -3,7 +3,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { UserPaymentService } from '@spartacus/core';
@@ -14,33 +13,27 @@ class MockUserPaymentService implements Partial<UserPaymentService> {
   getPaymentMethods() {
     return of([]);
   }
-
   getPaymentMethodsLoading() {
     return of(false);
   }
-
   loadPaymentMethods() {}
-
   deletePaymentMethod(_paymentMethodId: string) {}
-
-  setPaymentMethodAsDefault(_paymentMethodId: string) {}
 }
 
 describe('OpfTokenisationService', () => {
   let service: OpfTokenisationService;
-  let mockUserPaymentService: MockUserPaymentService;
+  let userPaymentService: UserPaymentService;
 
   beforeEach(() => {
-    mockUserPaymentService = new MockUserPaymentService();
-
     TestBed.configureTestingModule({
       providers: [
         OpfTokenisationService,
-        { provide: UserPaymentService, useValue: mockUserPaymentService },
+        { provide: UserPaymentService, useClass: MockUserPaymentService },
       ],
     });
 
     service = TestBed.inject(OpfTokenisationService);
+    userPaymentService = TestBed.inject(UserPaymentService);
   });
 
   it('should be created', () => {
@@ -48,127 +41,56 @@ describe('OpfTokenisationService', () => {
   });
 
   describe('getPaymentMethods', () => {
-    it('should delegate to UserPaymentService.getPaymentMethods()', () => {
+    it('should delegate to UserPaymentService.getPaymentMethods', (done) => {
       const mockPayments: OpfPaymentDetails[] = [
-        { id: 'method1' } as any,
-        { id: 'method2' } as any,
+        { id: 'method1' } as OpfPaymentDetails,
+        { id: 'method2' } as OpfPaymentDetails,
       ];
-      spyOn(mockUserPaymentService, 'getPaymentMethods').and.returnValue(
+      spyOn(userPaymentService, 'getPaymentMethods').and.returnValue(
         of(mockPayments)
       );
 
-      let result: OpfPaymentDetails[] = [];
-      service
-        .getPaymentMethods()
-        .subscribe((data) => {
-          result = data;
-        })
-        .unsubscribe();
-
-      expect(mockUserPaymentService.getPaymentMethods).toHaveBeenCalled();
-      expect(result).toEqual(mockPayments);
-    });
-
-    it('should return empty array when no payment methods exist', () => {
-      spyOn(mockUserPaymentService, 'getPaymentMethods').and.returnValue(
-        of([])
-      );
-
-      let result: OpfPaymentDetails[] = [];
-      service
-        .getPaymentMethods()
-        .subscribe((data) => {
-          result = data;
-        })
-        .unsubscribe();
-
-      expect(result).toEqual([]);
+      service.getPaymentMethods().subscribe((result) => {
+        expect(userPaymentService.getPaymentMethods).toHaveBeenCalled();
+        expect(result).toEqual(mockPayments);
+        done();
+      });
     });
   });
 
   describe('getPaymentMethodsLoading', () => {
-    it('should delegate to UserPaymentService.getPaymentMethodsLoading()', () => {
-      spyOn(mockUserPaymentService, 'getPaymentMethodsLoading').and.returnValue(
+    it('should delegate to UserPaymentService.getPaymentMethodsLoading', (done) => {
+      spyOn(userPaymentService, 'getPaymentMethodsLoading').and.returnValue(
         of(true)
       );
 
-      let result: boolean = false;
-      service
-        .getPaymentMethodsLoading()
-        .subscribe((data) => {
-          result = data;
-        })
-        .unsubscribe();
-
-      expect(
-        mockUserPaymentService.getPaymentMethodsLoading
-      ).toHaveBeenCalled();
-      expect(result).toEqual(true);
-    });
-
-    it('should return false when not loading', () => {
-      spyOn(mockUserPaymentService, 'getPaymentMethodsLoading').and.returnValue(
-        of(false)
-      );
-
-      let result: boolean = true;
-      service
-        .getPaymentMethodsLoading()
-        .subscribe((data) => {
-          result = data;
-        })
-        .unsubscribe();
-
-      expect(result).toEqual(false);
+      service.getPaymentMethodsLoading().subscribe((result) => {
+        expect(userPaymentService.getPaymentMethodsLoading).toHaveBeenCalled();
+        expect(result).toBe(true);
+        done();
+      });
     });
   });
 
   describe('loadPaymentMethods', () => {
-    it('should delegate to UserPaymentService.loadPaymentMethods()', () => {
-      spyOn(mockUserPaymentService, 'loadPaymentMethods');
+    it('should delegate to UserPaymentService.loadPaymentMethods', () => {
+      spyOn(userPaymentService, 'loadPaymentMethods');
 
       service.loadPaymentMethods();
 
-      expect(mockUserPaymentService.loadPaymentMethods).toHaveBeenCalled();
+      expect(userPaymentService.loadPaymentMethods).toHaveBeenCalled();
     });
   });
 
   describe('deletePaymentMethod', () => {
-    it('should delegate to UserPaymentService.deletePaymentMethod() with paymentMethodId', () => {
-      spyOn(mockUserPaymentService, 'deletePaymentMethod');
-      const paymentMethodId = 'method-123';
+    it('should delegate to UserPaymentService.deletePaymentMethod with correct id', () => {
+      spyOn(userPaymentService, 'deletePaymentMethod');
 
-      service.deletePaymentMethod(paymentMethodId);
+      service.deletePaymentMethod('method-123');
 
-      expect(mockUserPaymentService.deletePaymentMethod).toHaveBeenCalledWith(
-        paymentMethodId
+      expect(userPaymentService.deletePaymentMethod).toHaveBeenCalledWith(
+        'method-123'
       );
-    });
-
-    it('should handle deletion of multiple payment methods', () => {
-      spyOn(mockUserPaymentService, 'deletePaymentMethod');
-
-      service.deletePaymentMethod('method-1');
-      service.deletePaymentMethod('method-2');
-
-      expect(mockUserPaymentService.deletePaymentMethod).toHaveBeenCalledWith(
-        'method-1'
-      );
-      expect(mockUserPaymentService.deletePaymentMethod).toHaveBeenCalledWith(
-        'method-2'
-      );
-      expect(mockUserPaymentService.deletePaymentMethod).toHaveBeenCalledTimes(
-        2
-      );
-    });
-  });
-
-  describe('facade contract compliance', () => {
-    it('should implement all OpfTokenisationFacade methods', () => {
-      expect(typeof service.getPaymentMethods).toBe('function');
-      expect(typeof service.getPaymentMethodsLoading).toBe('function');
-      expect(typeof service.loadPaymentMethods).toBe('function');
-      expect(typeof service.deletePaymentMethod).toBe('function');
     });
   });
 });
