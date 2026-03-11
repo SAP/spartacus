3fe3a9d65a725a43fdb246938ba07e01df7594e8
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-03-11 16:00:46 +0530

    Update opf-checkout-payments.component.ts

--- integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
+++ integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
@@ -293,6 +293,11 @@ export class OpfCheckoutPaymentsComponent implements OnInit, OnDestroy {
     );
   }
 
+  onSavedCardsSelected(): void {
+    this.selectedPaymentId = this.SAVED_CARDS_ID;
+    this.isSavedCardsSelected = true;
+  }
+
   changePayment(payment: OpfActiveConfiguration): void {
     if (this.isSavedCardsSelected) {
       this.isSavedCardsSelected = false;
