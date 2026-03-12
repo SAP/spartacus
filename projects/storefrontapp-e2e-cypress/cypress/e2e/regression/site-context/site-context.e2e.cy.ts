c90fd2260894333495b9e780992225b5b4812b33
BenjaminEmiliani <benjamin.emiliani.arango@sap.com>
2026-03-12 15:33:06 -0400

    fix: (CXSPA-11223) Enhance e2e test for bug fix

--- projects/storefrontapp-e2e-cypress/cypress/e2e/regression/site-context/site-context.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/regression/site-context/site-context.e2e.cy.ts
@@ -8,47 +8,42 @@ import { login } from '../../../helpers/auth-forms';
 import * as siteContextSelector from '../../../helpers/site-context-selector';
 import * as merchandisingCarousel from '../../../helpers/vendor/cds/merchandising-carousel';
 import { switchSiteContext } from '../../../support/utils/switch-site-context';
-import { isolateTests } from '../../../support/utils/test-isolation';
+import { isolateTestsBefore } from '../../../support/utils/test-isolation';
+import { myCompanyAdminUser } from '../../../sample-data/shared-users';
 
 context(
-  'Site Context changes made on login page should persist when redirecting on login',
+  'Site Context changes made on login page',
   { testIsolation: false },
   () => {
-    const filmCamerasPath = '/c/574';
-    isolateTests();
-    before(() => {
-      cy.visit('/');
-    });
+    isolateTestsBefore();
+    const registrationData = myCompanyAdminUser.registrationData;
+    const filmCamerasCategoryUrl =
+      '/c/' + merchandisingCarousel.filmCamerasCategoryCode;
 
     it('should navigate to film cameras category page to save redirect url', () => {
+      cy.visit('/');
       merchandisingCarousel.navigateToCategory(
         merchandisingCarousel.filmCamerasCategoryName,
         merchandisingCarousel.filmCamerasCategoryCode
       );
+      cy.url().should('include', filmCamerasCategoryUrl);
     });
 
     it('should navigate to login page without submitting the form', () => {
-      // loginHelper.navigateToLoginPage();
       cy.visit('/login');
     });
 
     it('should change language and currency site context', () => {
-      cy.wait(3000);
       switchSiteContext(
         siteContextSelector.LANGUAGE_DE,
         siteContextSelector.LANGUAGE_LABEL
       );
-      // switchSiteContext(
-      //   siteContextSelector.CURRENCY_JPY,
-      //   siteContextSelector.CURRENCY_LABEL
-      // );
     });
 
     it('should login and redirect to film cameras category page with the updated language and currency context', () => {
-      login('mark.rivers@rustic-hw.com', 'pw4all');
-      cy.reload();
+      login(registrationData.email, registrationData.password);
       siteContextSelector.assertSiteContextChange(
-        siteContextSelector.FULL_BASE_URL_DE + filmCamerasPath
+        siteContextSelector.FULL_BASE_URL_DE_USD + filmCamerasCategoryUrl
       );
     });
   }
