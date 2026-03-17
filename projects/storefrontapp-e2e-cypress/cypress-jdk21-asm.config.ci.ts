b49fbfb8b03a195ece57629d54e14f6d843bcbef
Florent Letendre <florent.letendre01@sap.com>
2026-03-17 10:08:58 -0400

    switch to dev2

--- projects/storefrontapp-e2e-cypress/cypress-jdk21-asm.config.ci.ts
+++ projects/storefrontapp-e2e-cypress/cypress-jdk21-asm.config.ci.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   env: {
     ...baseConfig.env,
     CLIENT_ID: 'asm_client',
-    API_URL: 'https://spartacus-colosseum2.eastus.cloudapp.azure.com:8443',
+    API_URL: 'https://spartacus-dev-backend2.eastus.cloudapp.azure.com:8443',
     JDK_VERSION: 'JDK21',
   },
 });
