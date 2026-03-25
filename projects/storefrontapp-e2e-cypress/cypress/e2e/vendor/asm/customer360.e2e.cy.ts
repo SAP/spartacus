5d43ac6780ba12db4e262599a3db2ec6e3155d17
Florent Letendre <florent.letendre01@sap.com>
2026-03-25 17:48:35 -0400

    fix e2e

--- projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/asm/customer360.e2e.cy.ts
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/vendor/asm/customer360.e2e.cy.ts
@@ -238,13 +238,9 @@ context('Assisted Service Module', () => {
           cy.wait('@applyCoupon').its('response.statusCode').should('eq', 200);
           cy.get('button').should('not.contain', 'Apply to Cart');
           cy.get('button').contains('Remove').should('be.visible');
-          cy.intercept('POST', /\.*\/removeVoucher(\?.*)?$/).as('removeCoupon');
+          cy.intercept('POST', '**/removeVoucher').as('removeCoupon');
           cy.get('button').contains('Remove').click();
-          cy.whenJDK17(() => {
-            cy.wait('@removeCoupon')
-              .its('response.statusCode')
-              .should('be.oneOf', [200, 204]);
-          });
+          cy.wait('@removeCoupon').its('response.statusCode').should('eq', 204);
           cy.get('button').should('not.contain', 'Remove');
           cy.get('button').contains('Apply to Cart').should('be.visible');
         });
