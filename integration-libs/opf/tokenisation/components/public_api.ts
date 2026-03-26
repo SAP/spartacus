da87b7a85c4dc3327dd4bd5c3e4c92da94dcd3ab
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-03-26 17:59:44 +0530

    removed redundant files

--- integration-libs/opf/tokenisation/components/public_api.ts
+++ integration-libs/opf/tokenisation/components/public_api.ts
@@ -7,4 +7,3 @@
 export * from './opf-tokenisation-components.module';
 export * from './opf-tokenisation-account-payment-methods/opf-tokenisation-account-payment-methods.component';
 export * from './opf-tokenisation-payment-method/opf-tokenisation-payment-method.component';
-export * from './opf-tokenisation-saved-cards-toggle/opf-tokenisation-saved-cards-toggle.component';
