a99d6a5839ac49230ad7b54677984d8975e33ed0
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-03-26 16:27:01 +0530

    test cases for core dir

--- integration-libs/opf/tokenisation/core/connectors/opf-tokenisation.connector.ts
+++ integration-libs/opf/tokenisation/core/connectors/opf-tokenisation.connector.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@angular/core';
-
 /*
  * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+import { Injectable } from '@angular/core';
 
 /**
  * OPF Tokenisation Connector
