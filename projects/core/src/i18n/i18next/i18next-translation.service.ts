3d2327e11bb1d9e5a43b1aa72517805986c606b9
Norbert Papp <norbert.papp02@sap.com>
2026-03-30 09:44:11 +0200

    fix: update i18next translation service to use 'any' type for options

--- projects/core/src/i18n/i18next/i18next-translation.service.ts
+++ projects/core/src/i18n/i18next/i18next-translation.service.ts
@@ -5,7 +5,7 @@
  */
 
 import { Inject, inject, Injectable, isDevMode } from '@angular/core';
-import { i18n, TOptions } from 'i18next';
+import { i18n } from 'i18next';
 import { Observable } from 'rxjs';
 import { LoggerService } from '../../logger';
 import { I18nConfig } from '../config/i18n-config';
@@ -69,7 +69,7 @@ export class I18nextTranslationService implements TranslationService {
             namespacesLoaded = true;
             if (this.i18next.exists(namespacedKeys, options)) {
               subscriber.next(
-                this.i18next.t(namespacedKeys, options as TOptions)
+                this.i18next.t(namespacedKeys, options as any) as string
               );
             } else {
               this.reportMissingKey(chunkNamesByKeys);
