85f7d462835bee1da83d5c8f275cd8ff9a8b5a12
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-04-03 02:25:11 +0530

    Update opf-tokenisation-saved-cards-toggle.component.ts

--- integration-libs/opf/tokenisation/root/components/opf-tokenisation-saved-cards-toggle/opf-tokenisation-saved-cards-toggle.component.ts
+++ integration-libs/opf/tokenisation/root/components/opf-tokenisation-saved-cards-toggle/opf-tokenisation-saved-cards-toggle.component.ts
@@ -19,7 +19,7 @@ import { OpfSavedCardsToggleContext } from '../../model';
 import {
   OpfTokenisationSavedCardsService,
   SAVED_CARDS_ID,
-} from '@spartacus/opf/tokenisation/core';
+} from '@spartacus/opf/tokenisation/root';
 
 @Component({
   selector: 'cx-opf-tokenisation-saved-cards-toggle',
