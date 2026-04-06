0318b86c9f6a356593fdb6c446b264991dcfcba5
Florent Letendre <florent.letendre01@sap.com>
2026-04-06 09:46:13 -0400

    revert ts file

--- feature-libs/cart/base/components/cart-details/cart-details.component.ts
+++ feature-libs/cart/base/components/cart-details/cart-details.component.ts
@@ -51,7 +51,6 @@ export class CartDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // test CXSPA-12325 modifying code related file
     this.cart$ = this.activeCartService.getActive();
 
     this.entries$ = this.activeCartService
