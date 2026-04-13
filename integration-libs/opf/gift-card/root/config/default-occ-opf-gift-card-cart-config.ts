6eba6fc0fe3be320edd1a882fa63d62182f3a6a4
ijitendrasap <jitendra.kumar.shah@sap.com>
2026-04-13 15:16:24 +0530

    fix: gc-occ endpoints config (#21362)

--- integration-libs/opf/gift-card/root/config/default-occ-opf-gift-card-cart-config.ts
+++ integration-libs/opf/gift-card/root/config/default-occ-opf-gift-card-cart-config.ts
@@ -3,7 +3,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-import '../model/occ-opf-gift-card-endpoints.model';
 
 import { CartOccEndpoints } from '@spartacus/cart/base/occ';
 import { OccConfig } from '@spartacus/core';
@@ -14,14 +13,11 @@ const giftCardCartEndpoints: CartOccEndpoints = {
   cart: 'users/${userId}/carts/${cartId}?fields=DEFAULT,sapGiftCards(FULL,balance(value,formattedValue),remainingBalance(value,formattedValue), appliedAmount(value,formattedValue)),sapGiftCardSummary(FULL,totalAppliedAmount(value,formattedValue),totalRemainingBalance(value,formattedValue), totalBalance(value,formattedValue)),potentialProductPromotions,appliedProductPromotions,potentialOrderPromotions,appliedOrderPromotions,entries(totalPrice(formattedValue),product(images(FULL),stock(FULL)),basePrice(formattedValue,value),updateable),totalPrice(formattedValue),totalItems,totalPriceWithTax(formattedValue),totalDiscounts(value,formattedValue),subTotal(formattedValue),totalUnitCount,deliveryItemsQuantity,deliveryCost(formattedValue),totalTax(formattedValue, value),pickupItemsQuantity,net,appliedVouchers,productDiscounts(formattedValue),user,saveTime,name,description,_availableOperations',
 };
 
-export const defaultGiftCardCartOccEndpointsConfig: OccConfig = {
+export const defaultOccOpfGiftCardCartEndpointsConfig: OccConfig = {
   backend: {
     occ: {
       endpoints: {
         ...giftCardCartEndpoints,
-        applyGiftCard: 'users/${userId}/carts/${cartId}/giftCards',
-        removeGiftCard:
-          'users/${userId}/carts/${cartId}/giftCards/${giftCardId}',
       },
     },
   },
