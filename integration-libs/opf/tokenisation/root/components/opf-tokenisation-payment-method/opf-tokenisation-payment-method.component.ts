7ba1f17fb75dd3a445522ebf7c72a823ea16f4be
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-04-22 11:00:31 +0530

    fix

--- integration-libs/opf/tokenisation/root/components/opf-tokenisation-payment-method/opf-tokenisation-payment-method.component.ts
+++ integration-libs/opf/tokenisation/root/components/opf-tokenisation-payment-method/opf-tokenisation-payment-method.component.ts
@@ -60,6 +60,16 @@ export class OpfTokenisationPaymentMethodComponent
     );
   }
 
+  onCardClick(event: MouseEvent, paymentDetails: PaymentDetails): void {
+    const target = event.target as HTMLElement | null;
+
+    if (target?.closest('button, a, cx-generic-link')) {
+      return;
+    }
+
+    this.selectPaymentMethod(paymentDetails);
+  }
+
   setDefaultPaymentMethod(paymentDetails: PaymentDetails): void {
     this.OpfTokenisationPaymentMethodService.setDefaultPaymentMethod(
       paymentDetails
