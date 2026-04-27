f4e7c7a05b128341381c6d160726dba3b88dc739
Florent Letendre <florent.letendre01@sap.com>
2026-04-27 09:55:31 -0400

    fix: add user currency logic

--- feature-libs/user/account/root/events/user-account-event.listener.spec.ts
+++ feature-libs/user/account/root/events/user-account-event.listener.spec.ts
@@ -1,19 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import {
+  AuthService,
+  CurrencyService,
+  CurrencySetEvent,
   CxEvent,
   EventService,
   GlobalMessageService,
   GlobalMessageType,
+  LoginEvent,
   LogoutEvent,
+  WindowRef,
 } from '@spartacus/core';
-import { Subject } from 'rxjs';
+import { User, UserAccountFacade } from '@spartacus/user/account/root';
+import { UserProfileFacade } from '@spartacus/user/profile/root';
+import { filter, of, Subject } from 'rxjs';
 import { UserAccountEventListener } from './user-account-event.listener';
 import createSpy = jasmine.createSpy;
 
 const mockEventStream$ = new Subject<CxEvent>();
 
 class MockEventService implements Partial<EventService> {
-  get = createSpy().and.returnValue(mockEventStream$.asObservable());
+  get = createSpy().and.callFake((eventType: new () => CxEvent) =>
+    mockEventStream$
+      .asObservable()
+      .pipe(filter((event) => event instanceof eventType))
+  );
   dispatch = createSpy();
 }
 
@@ -21,8 +32,39 @@ class MockGlobalMessageService implements Partial<GlobalMessageService> {
   add = createSpy();
 }
 
+class MockUserAccountFacade implements Partial<UserAccountFacade> {
+  get = createSpy().and.returnValue(
+    of({ currency: { isocode: 'USD' } } as User)
+  );
+}
+
+class MockCurrencyService implements Partial<CurrencyService> {
+  setActive = createSpy();
+}
+
+class MockAuthService implements Partial<AuthService> {
+  isUserLoggedIn = createSpy().and.returnValue(of(true));
+}
+
+class MockUserProfileFacade implements Partial<UserProfileFacade> {
+  update = createSpy().and.returnValue(of(undefined));
+}
+
+class MockWindowRef implements Partial<WindowRef> {
+  localStorage = {
+    getItem: createSpy('getItem').and.returnValue('"EUR"'),
+    setItem: createSpy('setItem'),
+    removeItem: createSpy('removeItem'),
+  } as unknown as Storage;
+}
+
 describe(`UserAccountEventListener`, () => {
   let globalMessageService: GlobalMessageService;
+  let userAccountFacade: UserAccountFacade;
+  let currencyService: CurrencyService;
+  let authService: AuthService;
+  let userProfileFacade: UserProfileFacade;
+  let winRef: WindowRef;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -36,11 +78,36 @@ describe(`UserAccountEventListener`, () => {
           provide: GlobalMessageService,
           useClass: MockGlobalMessageService,
         },
+        {
+          provide: UserAccountFacade,
+          useClass: MockUserAccountFacade,
+        },
+        {
+          provide: CurrencyService,
+          useClass: MockCurrencyService,
+        },
+        {
+          provide: AuthService,
+          useClass: MockAuthService,
+        },
+        {
+          provide: UserProfileFacade,
+          useClass: MockUserProfileFacade,
+        },
+        {
+          provide: WindowRef,
+          useClass: MockWindowRef,
+        },
       ],
     });
 
     TestBed.inject(UserAccountEventListener);
     globalMessageService = TestBed.inject(GlobalMessageService);
+    userAccountFacade = TestBed.inject(UserAccountFacade);
+    currencyService = TestBed.inject(CurrencyService);
+    authService = TestBed.inject(AuthService);
+    userProfileFacade = TestBed.inject(UserProfileFacade);
+    winRef = TestBed.inject(WindowRef);
   });
 
   describe(`onAuth`, () => {
@@ -51,6 +118,81 @@ describe(`UserAccountEventListener`, () => {
         { key: 'authMessages.signedOutSuccessfully' },
         GlobalMessageType.MSG_TYPE_CONFIRMATION
       );
+    });
+
+    it('LoginEvent should set active currency from the OCC user response', () => {
+      mockEventStream$.next(new LoginEvent());
+
+      expect(userAccountFacade.get).toHaveBeenCalled();
+      expect(currencyService.setActive).toHaveBeenCalledWith('USD');
+      expect(winRef.localStorage?.getItem).toHaveBeenCalledWith(
+        'spartacus⚿⚿currency'
+      );
+      expect(winRef.localStorage?.setItem).toHaveBeenCalledWith(
+        'spartacus⚿⚿currency',
+        '"EUR"'
+      );
+    });
+
+    it('LoginEvent should not set active currency when OCC user has no currency', () => {
+      (userAccountFacade.get as jasmine.Spy).and.returnValue(of({}));
+
+      mockEventStream$.next(new LoginEvent());
+
+      expect(currencyService.setActive).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('onCurrencyChange', () => {
+    it('should persist currency update for logged-in users when currency changes', () => {
+      const event = new CurrencySetEvent();
+      event.activeCurrency = 'EUR';
+
+      mockEventStream$.next(event);
+
+      expect(authService.isUserLoggedIn).toHaveBeenCalled();
+      expect(winRef.localStorage?.setItem).toHaveBeenCalledWith(
+        'spartacus⚿⚿currency',
+        '"EUR"'
+      );
+      expect(userProfileFacade.update).toHaveBeenCalledWith({
+        currency: { isocode: 'EUR' },
+      });
+    });
+
+    it('should not persist currency update for anonymous users', () => {
+      (authService.isUserLoggedIn as jasmine.Spy).and.returnValue(of(false));
+      const event = new CurrencySetEvent();
+      event.activeCurrency = 'EUR';
+
+      mockEventStream$.next(event);
+
+      expect(winRef.localStorage?.setItem).not.toHaveBeenCalled();
+      expect(userProfileFacade.update).not.toHaveBeenCalled();
+    });
+
+    it('should not persist currency update if user already has the same currency', () => {
+      const event = new CurrencySetEvent();
+      event.activeCurrency = 'USD';
+
+      mockEventStream$.next(event);
+
+      expect(userProfileFacade.update).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('logout currency restoration', () => {
+    it('should restore anonymous currency on logout', () => {
+      mockEventStream$.next(new LoginEvent());
+      (currencyService.setActive as jasmine.Spy).calls.reset();
+
+      mockEventStream$.next(new LogoutEvent());
+
+      expect(winRef.localStorage?.setItem).toHaveBeenCalledWith(
+        'spartacus⚿⚿currency',
+        '"EUR"'
+      );
+      expect(currencyService.setActive).toHaveBeenCalledWith('EUR');
     });
   });
 });
--- feature-libs/user/account/root/events/user-account-event.listener.ts
+++ feature-libs/user/account/root/events/user-account-event.listener.ts
@@ -6,12 +6,23 @@
 
 import { Injectable, OnDestroy } from '@angular/core';
 import {
+  AuthService,
+  CurrencyService,
+  CurrencySetEvent,
   EventService,
   GlobalMessageService,
   GlobalMessageType,
+  isNotNullable,
+  LoginEvent,
   LogoutEvent,
+  WindowRef,
 } from '@spartacus/core';
+import { UserAccountFacade } from '@spartacus/user/account/root';
+import { UserProfileFacade } from '@spartacus/user/profile/root';
 import { Subscription } from 'rxjs';
+import { filter, map, switchMap, take, tap } from 'rxjs/operators';
+
+const CURRENCY_STORAGE_KEY = 'spartacus⚿⚿currency';
 
 /**
  * User account event listener.
@@ -21,12 +32,19 @@ import { Subscription } from 'rxjs';
 })
 export class UserAccountEventListener implements OnDestroy {
   protected subscriptions = new Subscription();
+  protected anonymousCurrencyStorageValue?: string | null;
 
   constructor(
     protected eventService: EventService,
-    protected globalMessageService: GlobalMessageService
+    protected globalMessageService: GlobalMessageService,
+    protected userAccountFacade: UserAccountFacade,
+    protected currencyService: CurrencyService,
+    protected authService: AuthService,
+    protected userProfileFacade: UserProfileFacade,
+    protected winRef: WindowRef
   ) {
     this.onAuth();
+    this.onCurrencyChange();
   }
 
   /**
@@ -35,12 +53,107 @@ export class UserAccountEventListener implements OnDestroy {
   protected onAuth(): void {
     this.subscriptions.add(
       this.eventService.get(LogoutEvent).subscribe(() => {
+        this.restoreAnonymousCurrency();
         this.globalMessageService.add(
           { key: 'authMessages.signedOutSuccessfully' },
           GlobalMessageType.MSG_TYPE_CONFIRMATION
         );
       })
     );
+
+    this.subscriptions.add(
+      this.eventService
+        .get(LoginEvent)
+        .pipe(
+          switchMap(() =>
+            this.userAccountFacade.get().pipe(filter(isNotNullable), take(1))
+          ),
+          map((user) => user?.currency?.isocode),
+          filter(isNotNullable)
+        )
+        .subscribe((currencyIsocode) => {
+          this.storeAnonymousCurrencySnapshot();
+          this.currencyService.setActive(currencyIsocode);
+          this.restoreAnonymousCurrencyInStorage();
+        })
+    );
+  }
+
+  /**
+   * Persists active currency for logged-in users when it changes in FE.
+   */
+  protected onCurrencyChange(): void {
+    this.subscriptions.add(
+      this.eventService
+        .get(CurrencySetEvent)
+        .pipe(
+          switchMap((event) =>
+            this.authService.isUserLoggedIn().pipe(
+              filter((isLoggedIn) => isLoggedIn),
+              map(() => event.activeCurrency)
+            )
+          ),
+          tap(() => this.restoreAnonymousCurrencyInStorage()),
+          switchMap((activeCurrency) =>
+            this.userAccountFacade.get().pipe(
+              filter(isNotNullable),
+              take(1),
+              filter((user) => user?.currency?.isocode !== activeCurrency),
+              map(() => activeCurrency)
+            )
+          ),
+          switchMap((activeCurrency) =>
+            this.userProfileFacade.update({
+              currency: { isocode: activeCurrency },
+            })
+          )
+        )
+        .subscribe()
+    );
+  }
+
+  protected storeAnonymousCurrencySnapshot(): void {
+    this.anonymousCurrencyStorageValue =
+      this.winRef.localStorage?.getItem(CURRENCY_STORAGE_KEY) ?? null;
+  }
+
+  protected restoreAnonymousCurrencyInStorage(): void {
+    if (this.anonymousCurrencyStorageValue === undefined) {
+      return;
+    }
+
+    if (this.anonymousCurrencyStorageValue === null) {
+      this.winRef.localStorage?.removeItem(CURRENCY_STORAGE_KEY);
+      return;
+    }
+
+    console.log(
+      '[localStorage.setItem]',
+      CURRENCY_STORAGE_KEY,
+      this.anonymousCurrencyStorageValue
+    );
+    console.trace('[localStorage.setItem] stack');
+    this.winRef.localStorage?.setItem(
+      CURRENCY_STORAGE_KEY,
+      this.anonymousCurrencyStorageValue
+    );
+  }
+
+  protected restoreAnonymousCurrency(): void {
+    this.restoreAnonymousCurrencyInStorage();
+
+    const anonymousCurrency = this.getAnonymousCurrencyFromStorage();
+    if (anonymousCurrency) {
+      this.currencyService.setActive(anonymousCurrency);
+    }
+  }
+
+  protected getAnonymousCurrencyFromStorage(): string | undefined {
+    if (!this.anonymousCurrencyStorageValue) {
+      return undefined;
+    }
+
+    return JSON.parse(this.anonymousCurrencyStorageValue) as string;
   }
 
   ngOnDestroy(): void {
