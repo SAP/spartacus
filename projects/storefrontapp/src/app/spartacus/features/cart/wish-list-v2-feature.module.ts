6e32bf06221674fa426d315a05e486d3122f15ea
Melody-zhou-512 <140146828+Melody-zhou-512@users.noreply.github.com>
2026-04-27 09:54:14 +0800

    add an inline comment
    add an inline comment

--- projects/storefrontapp/src/app/spartacus/features/cart/wish-list-v2-feature.module.ts
+++ projects/storefrontapp/src/app/spartacus/features/cart/wish-list-v2-feature.module.ts
@@ -9,6 +9,11 @@ import { CART_WISH_LIST_FEATURE } from '@spartacus/cart/wish-list/root';
 import { provideConfig } from '@spartacus/core';
 import { WishListFeatureModule } from './wish-list-feature.module';
 
+// TODO: CXSPA-12962
+// This module is not yet used in our storefrontapp because the WishList V2
+// feature requires a dedicated `/wishlists` OCC API endpoint that is not yet
+// enabled on our CI dev server. It will be integrated into storefrontapp and
+// accompanied by schematics for customer opt-in installation as part of that ticket.
 @NgModule({
   imports: [WishListFeatureModule],
   providers: [
