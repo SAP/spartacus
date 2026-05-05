566db2b9d05115466beddac1d8eb17ae69328064
BenjaminEmiliani <benjamin.emiliani@sap.com>
2026-05-05 15:56:43 -0400

    fix: (CXSPA-12798) Focus h2 title for vocalization

--- feature-libs/customer-ticketing/components/list/customer-ticketing-list/customer-ticketing-list.component.ts
+++ feature-libs/customer-ticketing/components/list/customer-ticketing-list/customer-ticketing-list.component.ts
@@ -5,7 +5,7 @@
  */
 
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {
   CxDatePipe,
@@ -50,6 +50,15 @@ import { CustomerTicketingCreateComponent } from '../customer-ticketing-create/c
   ],
 })
 export class CustomerTicketingListComponent {
+  private titleFocused = false;
+
+  @ViewChild('pageTitle') set pageTitleRef(el: ElementRef<HTMLElement>) {
+    if (el && !this.titleFocused) {
+      this.titleFocused = true;
+      el.nativeElement.focus();
+    }
+  }
+
   constructor(
     protected customerTicketingFacade: CustomerTicketingFacade,
     protected routingService: RoutingService,
