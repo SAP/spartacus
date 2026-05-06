108b7ba0a02319f9dce8cb34552f0b22fb93b17f
Raja Singh <59006156+rjsinghraj@users.noreply.github.com>
2026-05-06 18:47:51 +0530

    Update app.module.ts

--- projects/storefrontapp/src/app/app.module.ts
+++ projects/storefrontapp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { CmsConfig, provideConfig } from '@spartacus/core';
 import { AppRoutingModule } from '@spartacus/storefront';
 import { privateProviders } from './private/private.providers';
 import { SpartacusModule } from './spartacus/spartacus.module';
@@ -20,6 +21,17 @@ import { SpartacusModule } from './spartacus/spartacus.module';
     EffectsModule.forRoot([]),
     SpartacusModule,
   ],
-  providers: [privateProviders],
+  providers: [
+    privateProviders,
+    provideConfig(<CmsConfig>{
+      cmsComponents: {
+        ProductAddToCartComponent: {
+          data: {
+            inventoryDisplay: true,
+          },
+        },
+      },
+    }),
+  ],
 })
 export class AppModule {}
