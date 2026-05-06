89fd43495667d9474c78323e46e3ee938347b193
Florent Letendre <florent.letendre01@sap.com>
2026-05-06 13:20:35 -0400

    enable jdk21 login

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -322,7 +322,7 @@ if (environment.cpq) {
         reserveSpaceForImagesOnPdpAndPlp: true,
         lazyLoadImagesByDefault: true,
         incrementProcessesCountForMergeCart: true,
-        authorizationCodeFlowByDefault: false,
+        authorizationCodeFlowByDefault: true,
         defaultLayoutConfigWithoutPageFold: true,
         navigationMenuCloseOnSameLinkClick: true,
         enablePasswordExpiredErrorTranslation: true,
