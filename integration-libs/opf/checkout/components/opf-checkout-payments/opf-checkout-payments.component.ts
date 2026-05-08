73f659a01d472c41fa1a91dbe168c61e586b5efc
Diksha-28 <139218905+Diksha-28@users.noreply.github.com>
2026-05-08 14:07:23 +0530

    fix: CXOPF-5209 : Pagination for saved card in Checkout Page and other fixes (#21443)

--- integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
+++ integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
@@ -264,10 +264,15 @@ export class OpfCheckoutPaymentsComponent implements OnInit, OnDestroy {
             (state.termsAndConditionsChecked ||
               !this.explicitTermsAndConditions)
           ) {
-            isPreselected = true;
-            this.selectedPaymentId =
+            const resolvedId =
               state.selectedPaymentOptionId ??
               state.defaultSelectedPaymentOptionId;
+
+            if (resolvedId !== undefined) {
+              isPreselected = true;
+            }
+
+            this.selectedPaymentId = resolvedId;
             this.opfMetadataStoreService.updateOpfMetadata({
               selectedPaymentOptionId: this.selectedPaymentId,
             });
