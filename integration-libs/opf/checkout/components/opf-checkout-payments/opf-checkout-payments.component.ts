d1e70272baf1d2c689499c4aeaaea8b7db655cf4
Diksha Rahul Bhatkar <diksha.bhatkar@sap.com>
2026-05-11 13:33:38 +0530

    changes

--- integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
+++ integration-libs/opf/checkout/components/opf-checkout-payments/opf-checkout-payments.component.ts
@@ -25,7 +25,6 @@ import {
   TranslationService,
   UserPaymentService,
 } from '@spartacus/core';
-import { CheckoutPaymentTypeFacade } from '@spartacus/checkout/b2b/root';
 import {
   ICON_TYPE,
   IconComponent,
@@ -80,7 +79,6 @@ export class OpfCheckoutPaymentsComponent implements OnInit, OnDestroy {
   );
   protected opfPaymentEventsService = inject(OpfPaymentEventsService);
   protected userPaymentService = inject(UserPaymentService);
-  readonly isB2B = !!inject(CheckoutPaymentTypeFacade, { optional: true });
 
   protected subscription = new Subscription();
 
