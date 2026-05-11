38b7da87311b2e79a4ed522bb7d3b2c2d593f31c
Benjamin Emiliani <49131189+BenjaminEmiliani@users.noreply.github.com>
2026-05-11 10:11:34 -0400

    fix: (CXSPA-12671 ) Incorporate cx-read-more directive in ProductList… (#21429)
    Closes: https://jira.tools.sap/browse/CXSPA-12671

--- projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ projects/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -44,6 +44,12 @@ export interface FeatureTogglesInterface {
   readMoreDirective?: boolean;
 
   /**
+   * Introduces the read more directive in product list item summary
+   * Affects: ProductListItemComponent
+   */
+  productListItemSummaryReadMore?: boolean;
+
+  /**
    * Introduces characters left for product review form elements.
    * Affects: ProductReviewsComponent
    */
@@ -493,6 +499,7 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   a11yNgSelectUnicodeCarets: false,
   a11yPreventWindowsHighContrastOverride: false,
   readMoreDirective: true,
+  productListItemSummaryReadMore: false,
   productReviewCharactersLeft: true,
   a11yConfiguratorOverviewHeaderVPC: true,
   a11yFutureStockAccordionAriaControls: false,
--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -304,6 +304,7 @@ if (environment.cpq) {
         dispatchLoginActionOnlyWhenTokenReceived: true,
         a11yNgSelectUnicodeCarets: true,
         readMoreDirective: true,
+        productListItemSummaryReadMore: true,
         productReviewCharactersLeft: true,
         a11yConfiguratorOverviewHeaderVPC: true,
         a11yFutureStockAccordionAriaControls: true,
