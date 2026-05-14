6299d416e77ae83dd758284c264c1479b87fdadc
Florent Letendre <florent.letendre01@sap.com>
2026-05-14 09:08:17 -0400

    remove directive deps

--- projects/storefrontlib/layout/a11y/index.ts
+++ projects/storefrontlib/layout/a11y/index.ts
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './keyboard-focus/index';
-export * from './skip-link/index';
 export * from './btn-like-link';
+export * from './keyboard-focus/index';
 export * from './on-dom-change';
-export * from './responsive-aria-label';
+export * from './skip-link/index';
