f2de65b0505e14d48706d339584303a158d3cbcf
SouhaibKhadraoui <souhaib.khadraoui@sap.com>
2026-05-19 18:18:37 -0400

    fix: (CXSPA - 13024) RefreshCart instead of ResetCart on context change (#21475)
    Co-authored-by: Florent Letendre <florent.letendre01@sap.com>

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -354,6 +354,7 @@ if (environment.cpq) {
         showSortFieldsOnlyAtTop: true,
         showRequiredAsterisks: true,
         enableExpiredRefreshTokenHandlers: true,
+        enableCartReloadOnContextChange: true,
         opfPaymentVerificationCheckProcessingCartOnErrorOnly: true,
       };
       return appFeatureToggles;
