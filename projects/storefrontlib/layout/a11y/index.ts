991c0dbde2cc0fa198865e26cd36b0606599b07a
Florent Letendre <florent.letendre01@sap.com>
2026-05-19 10:42:24 -0400

    fix: improve a11y of unit tree list rows (#21463)
    CXSPA-12737

--- projects/storefrontlib/layout/a11y/index.ts
+++ projects/storefrontlib/layout/a11y/index.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './keyboard-focus/index';
-export * from './skip-link/index';
 export * from './btn-like-link';
+export * from './keyboard-focus/index';
 export * from './on-dom-change';
+export * from './skip-link/index';
