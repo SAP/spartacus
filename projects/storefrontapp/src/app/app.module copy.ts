ef5e5ca280469a27e65a728308e279ea2214f57e
Roman <r.chygryn@sap.com>
2026-05-26 12:40:09 +0200

    test

---
+++ projects/storefrontapp/src/app/app.module copy.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from '@spartacus/storefront';
+import { privateProviders } from './private/private.providers';
+import { SpartacusModule } from './spartacus/spartacus.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}),
+    AppRoutingModule,
+    EffectsModule.forRoot([]),
+    SpartacusModule,
+  ],
+  providers: [privateProviders],
+})
+export class AppModule {}
