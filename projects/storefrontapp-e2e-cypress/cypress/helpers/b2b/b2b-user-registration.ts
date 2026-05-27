505f1a112ab01f0ec28a9c74c623895372c41e6c
Florent Letendre <florent.letendre01@sap.com>
2026-05-27 18:34:52 -0400

    fix

--- projects/storefrontapp-e2e-cypress/cypress/helpers/b2b/b2b-user-registration.ts
+++ projects/storefrontapp-e2e-cypress/cypress/helpers/b2b/b2b-user-registration.ts
@@ -29,7 +29,7 @@ export function navigateToOrganizationUserRegisterPage() {
   clickHamburger();
   whenJDK17(() => {
     cy.getLoginRegisterLink().click();
-    cy.get('cx-login-register button').click();
+    cy.get('cx-login-register button.btn-register').click();
   });
   whenJDK21(() => {
     const registerPage = waitForPage('/login/register', 'getRegisterPage');
