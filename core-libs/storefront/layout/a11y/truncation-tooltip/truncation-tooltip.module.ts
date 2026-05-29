6fa4d615888a4d6727c2814db7fcc77f239fd5ff
Florent Letendre <florent.letendre01@sap.com>
2026-05-29 16:26:00 -0400

    fix: a11y - truncate long customerId with ellipsis in profile forms (#21529)
    CXSPA-12703
    
    Added cxTruncationTooltip directive which add ellipsis and tooltip on input field.

---
+++ core-libs/storefront/layout/a11y/truncation-tooltip/truncation-tooltip.module.ts
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { NgModule } from '@angular/core';
+import { TruncationTooltipDirective } from './truncation-tooltip.directive';
+
+@NgModule({
+  imports: [TruncationTooltipDirective],
+  exports: [TruncationTooltipDirective],
+})
+export class TruncationTooltipModule {}
