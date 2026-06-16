405f6bfc89b801ff3cc2741e667684259b377e66
zeyber <rotherham.c@gmail.com>
2026-06-16 14:13:31 +0200

    fix feature toggle usage

--- core-libs/storefront/cms-components/misc/site-context-selector/site-context-selector.component.ts
+++ core-libs/storefront/cms-components/misc/site-context-selector/site-context-selector.component.ts
@@ -11,7 +11,7 @@ import {
   inject,
   Input,
 } from '@angular/core';
-import { I18nModule, SiteContext, TranslationService, useFeatureStyles } from '@spartacus/core';
+import { FeatureDirective, I18nModule, SiteContext, TranslationService, useFeatureStyles } from '@spartacus/core';
 import { map, Observable } from 'rxjs';
 import { IconComponent } from '../icon/icon.component';
 import { ICON_TYPE } from '../icon/icon.model';
@@ -22,7 +22,7 @@ import { SiteContextType } from './site-context.model';
   selector: 'cx-site-context-selector',
   templateUrl: './site-context-selector.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgIf, NgFor, IconComponent, AsyncPipe, I18nModule],
+  imports: [NgIf, NgFor, IconComponent, AsyncPipe, I18nModule, FeatureDirective],
 })
 export class SiteContextSelectorComponent {
   /**
