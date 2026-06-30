525e120de1d933113b1730b50abd44b3f3e888a6
Stanislav Sukhanov <stanislav.sukhanov@sap.com>
2026-06-30 16:05:25 +0200

    feat: CXSPA-13564 auto-add feature toggle migration entry on release
      * fix sonar
    
    close: https://jira.tools.sap/browse/CXSPA-13564

--- core-libs/schematics/copy-feature-toggles.js
+++ core-libs/schematics/copy-feature-toggles.js
@@ -2,7 +2,7 @@
  * This script copies the feature-toggles.ts file from the Spartacus core project to schematics project.
 */
 
-const fs = require('fs');
+const fs = require('node:fs');
 const SOURCE =
   '../../core-libs/core/src/features-config/feature-toggles/config/feature-toggles.ts';
 const DESTINATION = './src/feature-toggles.copied-from-core-lib.ts';
