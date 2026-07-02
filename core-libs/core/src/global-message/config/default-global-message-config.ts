0a3efdecd8a224e5ffa1ffa263d41793afc6e4a7
BenjaminEmiliani <benjamin.emiliani@sap.com>
2026-07-02 08:41:42 -0400

    fix: CXSPA-12683 increase contrast close global msg

--- core-libs/core/src/global-message/config/default-global-message-config.ts
+++ core-libs/core/src/global-message/config/default-global-message-config.ts
@@ -10,10 +10,10 @@ import { GlobalMessageConfig } from './global-message-config';
 export const defaultGlobalMessageConfig: GlobalMessageConfig = {
   globalMessages: {
     [GlobalMessageType.MSG_TYPE_CONFIRMATION]: {
-      timeout: 3000,
+      timeout: 30000,
     },
     [GlobalMessageType.MSG_TYPE_INFO]: {
-      timeout: 3000,
+      timeout: 30000,
     },
     [GlobalMessageType.MSG_TYPE_ERROR]: {
       timeout: 7000,
