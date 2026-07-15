4413eb4957d7cdb4a078b4491245e84bca83a9c9
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-07-15 17:20:11 -0400

    feat: (CXSPA-13905) - initial config files for core-libs/storefront

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -3,14 +3,15 @@ set -e
 set -o pipefail
 
 EXCLUDE_APPLICATIONS=storefrontapp,ssr-tests
-EXCLUDE_JASMINE=storefrontstyles,schematics,setup,core
+EXCLUDE_JASMINE=storefrontstyles,schematics,setup,core,storefrontlib
 EXCLUDE_JEST=storefrontstyles,schematics,setup
 
 echo "-----"
 
 function run_affected_unit_tests {
-    echo "Running VITEST unit tests and code coverage for core"
+    echo "Running VITEST unit tests and code coverage for core and storefrontlib"
     npx nx run core:test -- --coverage
+    npx nx run storefrontlib:test -- --coverage
 
     echo "Running JASMINE unit tests and code coverage for AFFECTED libraries"
     npx nx affected --target=test --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JASMINE" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
@@ -23,8 +24,9 @@ function run_all_unit_tests {
     if [[ -n "$UNIT_TEST_GROUP_PROJECTS" ]]; then
         echo "🔀 Running only selected projects: $UNIT_TEST_GROUP_PROJECTS"
 
-        echo "Running VITEST unit tests and code coverage for core"
+        echo "Running VITEST unit tests and code coverage for core and storefrontlib"
         npx nx run core:test -- --coverage
+        npx nx run storefrontlib:test -- --coverage
 
         echo "Running JASMINE unit tests for selected projects"
         npx nx run-many --target=test --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JASMINE" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
@@ -32,8 +34,9 @@ function run_all_unit_tests {
         echo "Running JEST unit tests for selected projects"
         npx nx run-many --target=test-jest --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
     else
-        echo "Running VITEST unit tests and code coverage for core"
+        echo "Running VITEST unit tests and code coverage for core and storefrontlib"
         npx nx run core:test -- --coverage
+        npx nx run storefrontlib:test -- --coverage
 
         echo "Running ALL JASMINE unit tests"
         npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JASMINE" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
---
+++ core-libs/storefront/setup-test.ts
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '@angular/compiler';
+import 'zone.js';
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
