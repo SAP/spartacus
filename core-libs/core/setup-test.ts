1f4bf59cda40dcf5dc6fbbc6e8580a881d4e2192
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-07-17 11:25:51 -0400

    feat: (CXSPA-13905) - removal of temporary file + setup update to remove deprecations

--- core-libs/core/setup-test.ts
+++ core-libs/core/setup-test.ts
@@ -5,15 +5,12 @@
  */
 
 import '@angular/compiler';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 import 'zone.js';
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
 
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  BrowserTestingModule,
+  platformBrowserTesting()
 );
