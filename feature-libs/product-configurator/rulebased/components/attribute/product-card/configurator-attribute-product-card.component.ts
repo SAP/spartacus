6a7f87cde633f75bbeb19a13728bf9866ccd2807
Larisa Staroverova <larisa.staroverova@sap.com>
2026-07-20 08:21:49 +0200

    CXSPA-13057: Review feedback

--- feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/product-card/configurator-attribute-product-card.component.ts
@@ -47,10 +47,6 @@ import { ConfiguratorAttributeBaseComponent } from '../types/base/configurator-a
 export interface ConfiguratorAttributeProductCardComponentOptions {
   /**
    * If set to `true`, all action buttons will be disabled.
-   *
-   * @deprecated Only used while the `productConfiguratorConsolidatedButtonDisabling`
-   * feature toggle is off. With the toggle on, the action buttons rely on
-   * `disableActions$` instead, which already accounts for the parent loading state.
    */
   disableAllButtons?: boolean;
   /** If set to `true`, the remove/deselect button won't be available. Useful for required attributes,
