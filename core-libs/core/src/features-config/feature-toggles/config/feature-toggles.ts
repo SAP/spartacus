f102ae04775fae2a04c3908762151ce7eae23a45
Benjamin Emiliani <49131189+BenjaminEmiliani@users.noreply.github.com>
2026-07-24 13:21:38 -0400

    fix: CXSPA-12839 add initial focus (#21782)

--- core-libs/core/src/features-config/feature-toggles/config/feature-toggles.ts
+++ core-libs/core/src/features-config/feature-toggles/config/feature-toggles.ts
@@ -611,6 +611,14 @@ export interface FeatureTogglesInterface {
    * Affects: `FormErrorsComponent`
    */
   a11yFormErrorIconContrast?: boolean;
+
+  /**
+   * When enabled, the address form applies the `cxFocus` directive with autofocus
+   * to manage initial keyboard focus.
+   *
+   * Affects: `AddressFormComponent`
+   */
+  a11yAddressFormInitialFocus?: boolean;
 }
 
 export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
@@ -693,4 +701,5 @@ export const defaultFeatureToggles: Required<FeatureTogglesInterface> = {
   improvedTabStyling: false,
   productConfiguratorConsolidatedButtonDisabling: false,
   a11yFormErrorIconContrast: false,
+  a11yAddressFormInitialFocus: false,
 };
