fe008b1b498e48edc18a38ad20b7e8027d27acc7
Benjamin Emiliani <49131189+BenjaminEmiliani@users.noreply.github.com>
2026-07-29 15:54:26 -0400

    fix: CXSPA-13837 merge guest cart (#21775)

--- feature-libs/cart/base/core/services/index.ts
+++ feature-libs/cart/base/core/services/index.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export * from './active-cart-state-persistence.service';
 export * from './cart-config.service';
 export * from './cart-validation-state.service';
 export * from './multi-cart-state-persistence.service';
