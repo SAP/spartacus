57343655fc7f4a8901a904f523e509a2d4ec31a1
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-07-30 16:37:53 -0400

    feat: (CXSPA-13730) - make sure unit_test.sh reflects the vitest migrated folders and does not run jasmine on vitest

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -2,10 +2,16 @@
 set -e
 set -o pipefail
 
+VITEST_MIGRATED_FOLDERS=core
 EXCLUDE_APPLICATIONS=storefrontapp,ssr-tests
-EXCLUDE_JEST=storefrontstyles,schematics,setup
+EXCLUDE_JEST=storefrontstyles,schematics,setup,$VITEST_MIGRATED_FOLDERS
 
 echo "-----"
+
+function run_vitest_migrated_folders {
+    echo "Running VITEST unit tests for migrated libraries"
+    npx nx run-many --target=test --projects="$VITEST_MIGRATED_FOLDERS"
+}
 
 function run_affected_unit_tests {
     echo "Running JASMINE unit tests and code coverage for AFFECTED libraries"
@@ -13,6 +19,8 @@ function run_affected_unit_tests {
 
     echo "Running JEST (mostly schematics) unit tests and code coverage for AFFECTED libraries"
     npx nx affected --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
+
+    run_vitest_migrated_folders
 }
 
 function run_all_unit_tests {
@@ -31,6 +39,8 @@ function run_all_unit_tests {
         echo "Running ALL JEST unit tests"
         npx nx run-many --all --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
     fi
+
+    run_vitest_migrated_folders
 }
 
 if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
