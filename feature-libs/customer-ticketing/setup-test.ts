599c2a983342f38676d60e6e08740ebf50046673
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-03 12:47:14 -0400

    feat: (CXSPA-13733) - prettier lint failure correction

--- feature-libs/customer-ticketing/setup-test.ts
+++ feature-libs/customer-ticketing/setup-test.ts
@@ -6,8 +6,14 @@
 
 import '@angular/compiler';
 import { getTestBed } from '@angular/core/testing';
-import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
 import 'zone.js';
 import 'zone.js/testing';
 
-getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting()
+);
