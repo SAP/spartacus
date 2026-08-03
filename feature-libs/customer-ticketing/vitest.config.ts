cdbb128dca0c362f13f38da407eb087034a49b4e
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-03 12:53:58 -0400

    feat: (CXSPA-13733) - remove karma files and update paths resolutions

--- feature-libs/customer-ticketing/vitest.config.ts
+++ feature-libs/customer-ticketing/vitest.config.ts
@@ -8,9 +8,16 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vitest/config';
 import angular from '@analogjs/vite-plugin-angular';
 
+const root = `${import.meta.dirname}/../..`;
+
 export default defineConfig({
   root: import.meta.dirname,
   plugins: [angular(), nxViteTsPaths()],
+  resolve: {
+    alias: {
+      '@spartacus/core/feature-toggles/testing': `${root}/core-libs/core/src/features-config/feature-toggles/testing/index.ts`,
+    },
+  },
   test: {
     pool: 'forks',
     watch: false,
