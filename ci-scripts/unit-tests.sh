b972b1c76ed01d50e72f7d2d60ce67562c7abb67
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-03 16:25:04 -0400

    chore: fixed the comments of the PR review

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -2,26 +2,33 @@
 set -e
 set -o pipefail
 
-VITEST_MIGRATED_FOLDERS=core
+VITEST_PROJECTS=core
 EXCLUDE_APPLICATIONS=storefrontapp,ssr-tests
-EXCLUDE_JEST=storefrontstyles,schematics,setup,$VITEST_MIGRATED_FOLDERS
+JEST_PROJECTS=storefrontstyles,schematics,setup
 
 echo "-----"
 
-function run_vitest_migrated_folders {
+function run_vitest_migrated_tests {
+    local projects="${1:-$VITEST_PROJECTS}"
     echo "Running VITEST unit tests for migrated libraries"
-    npx nx run-many --target=test --projects="$VITEST_MIGRATED_FOLDERS"
+    npx nx run-many --target=test --projects="$projects"
+    return 0
+}
+
+function run_vitest_affected_tests {
+    echo "Running VITEST unit tests for affected migrated libraries"
+    npx nx affected --target=test --projects="$VITEST_PROJECTS"
     return 0
 }
 
 function run_affected_unit_tests {
     echo "Running JASMINE unit tests and code coverage for AFFECTED libraries"
-    npx nx affected --target=test --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JEST" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+    npx nx affected --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
     echo "Running JEST (mostly schematics) unit tests and code coverage for AFFECTED libraries"
     npx nx affected --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
 
-    run_vitest_migrated_folders
+    run_vitest_affected_tests
 }
 
 function run_all_unit_tests {
@@ -29,19 +36,21 @@ function run_all_unit_tests {
         echo "🔀 Running only selected projects: $UNIT_TEST_GROUP_PROJECTS"
 
         echo "Running JASMINE unit tests for selected projects"
-        npx nx run-many --target=test --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JEST" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+        npx nx run-many --target=test --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
         echo "Running JEST unit tests for selected projects"
         npx nx run-many --target=test-jest --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
+
+        run_vitest_migrated_tests "$UNIT_TEST_GROUP_PROJECTS"
     else
         echo "Running ALL JASMINE unit tests"
-        npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$EXCLUDE_JEST" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+        npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
         echo "Running ALL JEST unit tests"
         npx nx run-many --all --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
-    fi
 
-    run_vitest_migrated_folders
+        run_vitest_migrated_tests
+    fi
 }
 
 if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
