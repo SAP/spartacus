bb81c8b89b25f148931673fc8c17329b68a82ca0
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-04 10:39:43 -0400

    chore: changed target name from test to test-vitest

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -2,28 +2,26 @@
 set -e
 set -o pipefail
 
-VITEST_PROJECTS=core
 EXCLUDE_APPLICATIONS=storefrontapp,ssr-tests
 JEST_PROJECTS=storefrontstyles,schematics,setup
 
 echo "-----"
 
 function run_vitest_migrated_tests {
-    local projects="${1:-$VITEST_PROJECTS}"
     echo "Running VITEST unit tests for migrated libraries"
-    npx nx run-many --target=test --projects="$projects"
+    npx nx run-many --all --target=test-vitest
     return 0
 }
 
 function run_vitest_affected_tests {
     echo "Running VITEST unit tests for affected migrated libraries"
-    npx nx affected --target=test --projects="$VITEST_PROJECTS"
+    npx nx affected --target=test-vitest
     return 0
 }
 
 function run_affected_unit_tests {
     echo "Running JASMINE unit tests and code coverage for AFFECTED libraries"
-    npx nx affected --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+    npx nx affected --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
     echo "Running JEST (mostly schematics) unit tests and code coverage for AFFECTED libraries"
     npx nx affected --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
@@ -36,15 +34,15 @@ function run_all_unit_tests {
         echo "🔀 Running only selected projects: $UNIT_TEST_GROUP_PROJECTS"
 
         echo "Running JASMINE unit tests for selected projects"
-        npx nx run-many --target=test --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+        npx nx run-many --target=test --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
         echo "Running JEST unit tests for selected projects"
         npx nx run-many --target=test-jest --projects="$UNIT_TEST_GROUP_PROJECTS" --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
 
-        run_vitest_migrated_tests "$UNIT_TEST_GROUP_PROJECTS"
+        run_vitest_migrated_tests
     else
         echo "Running ALL JASMINE unit tests"
-        npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS,$VITEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
+        npx nx run-many --all --target=test --exclude="$EXCLUDE_APPLICATIONS,$JEST_PROJECTS" -- --no-watch --source-map --code-coverage --browsers ChromeHeadless
 
         echo "Running ALL JEST unit tests"
         npx nx run-many --all --target=test-jest --exclude="$EXCLUDE_APPLICATIONS" -- --coverage --runInBand
