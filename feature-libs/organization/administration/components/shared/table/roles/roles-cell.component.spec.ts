a7622779392b25433350d075e83a3f5176e8c489
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-05 16:44:04 -0400

    chore: all 14 remaining feature-libs now fully migrated to vitest

--- feature-libs/organization/administration/components/shared/table/roles/roles-cell.component.spec.ts
+++ feature-libs/organization/administration/components/shared/table/roles/roles-cell.component.spec.ts
@@ -38,11 +38,10 @@ describe('RolesCellComponent', () => {
   it('should render roles', () => {
     const el = fixture.debugElement.queryAll(By.css('ul.text li'));
     expect(el.length).toEqual(2);
-
-    expect((el[0].nativeElement as HTMLElement).textContent?.trim()).toEqual(
+    expect((el[0].nativeElement as HTMLElement).innerText).toEqual(
       'organization.userRoles.approver'
     );
-    expect((el[1].nativeElement as HTMLElement).textContent?.trim()).toEqual(
+    expect((el[1].nativeElement as HTMLElement).innerText).toEqual(
       'organization.userRoles.worker'
     );
   });
