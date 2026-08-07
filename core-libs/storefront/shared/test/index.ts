38db4ef6fde7d4830e6097a0ec7fffc4f4237a47
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-08-07 15:19:50 -0400

    chore: removal of unnecessary index file

--- core-libs/storefront/shared/test/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2026 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './mock-feature-directive';
-export * from './mock-feature-directives.module';
-export * from './mock-feature-level-directive';
