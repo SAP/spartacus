4898fb19780df909016977bdefe8d47ba2cb0b7b
Eric Polesky <eric.polesky@sap.com>
2026-08-07 12:15:56 -0400

    fix lint issues

--- projects/storefrontapp/src/app/ssr-base-url.provider.ts
+++ projects/storefrontapp/src/app/ssr-base-url.provider.ts
@@ -21,7 +21,6 @@ import { baseUrlMap, fallbackBaseUrl } from './ssr-baser-url-mappings';
  * Allows for server-side assignment of OCC base URL without exposing url mappings.
  */
 export const ssrBaseUrl = (): OccConfig => {
-  console.log('ssrBaseUrl');
   const meta = inject(Meta);
   const windowRef = inject(WindowRef);
   const logger = inject(LoggerService);
@@ -30,13 +29,11 @@ export const ssrBaseUrl = (): OccConfig => {
   /** Will be the value injected at pod startup */
   const initialBaseUrl =
     occServerConfigFromMetaTagFactory(meta).backend?.occ?.baseUrl;
-  console.log('initialBaseUrl', initialBaseUrl);
   logger.error(`initialBaseUrl: ${initialBaseUrl}`);
 
   /* read request data */
   const originUrl = new URL(windowRef.location.origin as string);
   // const hrefUrl = new URL(windowRef.location.href as string); // If we need to read request path
-  console.log('windowRef', JSON.stringify(windowRef.location, undefined, 2));
   logger.error(
     `windowRef: ${JSON.stringify(windowRef.location, undefined, 2)}`
   );
@@ -48,14 +45,12 @@ export const ssrBaseUrl = (): OccConfig => {
 
   /* Set meta tag for CSR configuration */
   meta.updateTag({ name: OCC_BASE_URL_META_TAG_NAME, content: baseUrl });
-  console.log('set meta tag to', baseUrl);
   logger.error(`set meta tag to: ${baseUrl}`);
 
   /* Return config partial for SSR configuration */
   const config = {
     backend: { occ: { baseUrl } },
   } satisfies OccConfig;
-  console.log('adding config', JSON.stringify(config, undefined, 2));
   logger.error(`adding config: ${JSON.stringify(config, undefined, 2)}`);
   return config;
 };
