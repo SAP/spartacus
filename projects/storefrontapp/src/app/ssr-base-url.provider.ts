2fe5a30340422ddfdae99168c702d5dba163167d
Eric Polesky <eric.polesky@sap.com>
2026-08-07 10:13:30 -0400

    add s2 to base url mapping

--- projects/storefrontapp/src/app/ssr-base-url.provider.ts
+++ projects/storefrontapp/src/app/ssr-base-url.provider.ts
@@ -1,12 +1,13 @@
 import { inject } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import {
+  LoggerService,
   OCC_BASE_URL_META_TAG_NAME,
   OccConfig,
   occServerConfigFromMetaTagFactory,
   WindowRef,
 } from '@spartacus/core';
-import { baseUrlMap } from './ssr-baser-url-mappings';
+import { baseUrlMap, fallbackBaseUrl } from './ssr-baser-url-mappings';
 
 /**
  * Determine an OCC Base URL to use in SSR processing and set it to the OCC Base URL Meta tag.
@@ -17,28 +18,38 @@ export const ssrBaseUrl = (): OccConfig => {
   console.log('ssrBaseUrl');
   const meta = inject(Meta);
   const windowRef = inject(WindowRef);
+  const logger = inject(LoggerService);
 
+  /* read injected data */
   /** Will be the value injected at pod startup */
   const initialBaseUrl =
     occServerConfigFromMetaTagFactory(meta).backend?.occ?.baseUrl;
   console.log('initialBaseUrl', initialBaseUrl);
+  logger.error(`initialBaseUrl: ${initialBaseUrl}`);
 
-  // read request data
+  /* read request data */
   const originUrl = new URL(windowRef.location.origin as string);
   // const hrefUrl = new URL(windowRef.location.href as string); // If we need to read request path
   console.log('windowRef', JSON.stringify(windowRef.location, undefined, 2));
+  logger.error(
+    `windowRef: ${JSON.stringify(windowRef.location, undefined, 2)}`
+  );
 
-  // Business logic to determine baseUrl
-  const baseUrl = baseUrlMap[originUrl?.hostname] ?? initialBaseUrl ?? 'UH-OH!'; // Uses a simple map
+  /* Business logic to determine baseUrl */
+  // Uses a simple map, fallback to injected value, then hard-coded fallback
+  const baseUrl =
+    baseUrlMap[originUrl?.hostname] ?? initialBaseUrl ?? fallbackBaseUrl;
 
-  // Set meta tag for CSR requests
+  /* Set meta tag for CSR configuration */
   meta.updateTag({ name: OCC_BASE_URL_META_TAG_NAME, content: baseUrl });
   console.log('set meta tag to', baseUrl);
+  logger.error(`set meta tag to: ${baseUrl}`);
 
-  // Return config partial for SSR requests
+  /* Return config partial for SSR configuration */
   const config = {
     backend: { occ: { baseUrl } },
   } satisfies OccConfig;
   console.log('adding config', JSON.stringify(config, undefined, 2));
+  logger.error(`adding config: ${JSON.stringify(config, undefined, 2)}`);
   return config;
 };
--- projects/storefrontapp/src/app/ssr-baser-url-mappings.ts
+++ projects/storefrontapp/src/app/ssr-baser-url-mappings.ts
@@ -1,3 +1,5 @@
+export const fallbackBaseUrl = 'https://40.76.109.9:9002';
+
 export const baseUrlMap: Record<string, string> = {
   'apparel-storefront.com': 'https://api.apparel-storefront.com',
 
@@ -5,6 +7,12 @@ export const baseUrlMap: Record<string, string> = {
   localhost: 'https://40.76.109.9:9002',
 
   'spartacusstore.c432wmya2v-teamspart3-s3-public.model-t.myhybris.cloud':
+    'https://api.c432wmya2v-teamspart3-s3-public.model-t.myhybris.cloud',
+
+  'spartacusstore.c432wmya2v-teamspart3-s2-public.model-t.myhybris.cloud':
+    'https://api.c432wmya2v-teamspart3-s2-public.model-t.myhybris.cloud',
+
+  'b2bspastore.c432wmya2v-teamspart3-s2-public.model-t.myhybris.cloud':
     'https://api.c432wmya2v-teamspart3-s3-public.model-t.myhybris.cloud',
 
   'electronics-storefront.com': 'https://20.83.184.244:9002',
