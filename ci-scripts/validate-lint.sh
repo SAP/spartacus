9fbdf4e067ce25bba369f105e62712266fd8c588
Benjamin Emiliani <49131189+BenjaminEmiliani@users.noreply.github.com>
2026-08-14 12:43:22 -0400

    chore: fix multiple CVEs (#21872)
    Co-authored-by: Giancarlo Cordero Ortiz <46171897+giancorderoortiz@users.noreply.github.com>

--- ci-scripts/validate-lint.sh
+++ ci-scripts/validate-lint.sh
@@ -11,14 +11,13 @@ function validateStylesLint {
 function validateTsConfigFile {
     echo "Validating ${TSCONFIGFILE_TO_VALIDATE} integrity"
     LOCAL_ENV_LIB_PATH_OCCURENCES=$(grep -c "core-libs/storefront/public_api" ${TSCONFIGFILE_TO_VALIDATE} || true)
-    if [ $LOCAL_ENV_LIB_PATH_OCCURENCES \> 0 ];
-    then
-        echo "ERROR: ${TSCONFIGFILE_TO_VALIDATE} file is invalid. Found [${LOCAL_ENV_LIB_PATH}].";
+    if [ $LOCAL_ENV_LIB_PATH_OCCURENCES \> 0 ]; then
+        echo "ERROR: ${TSCONFIGFILE_TO_VALIDATE} file is invalid. Found [${LOCAL_ENV_LIB_PATH}]."
         echo "A proper ng-packager build in /dist should be used in this context."
         exit 1
     else
-        echo "${TSCONFIGFILE_TO_VALIDATE} file is valid.";
-    fi;
+        echo "${TSCONFIGFILE_TO_VALIDATE} file is valid."
+    fi
 }
 
 function validateNoHardCodedText {
@@ -83,12 +82,12 @@ fi
 validateStylesLint
 
 echo "Validating code linting"
-node --max_old_space_size=3584 ./node_modules/nx/bin/nx run-many --all --target=lint
+node --max_old_space_size=3584 ./node_modules/nx/dist/bin/nx.js run-many --all --target=lint
 
 echo "-----"
 
 echo "Validating code formatting (using prettier)"
-npm run prettier 2>&1 |  tee prettier.log
+npm run prettier 2>&1 | tee prettier.log
 results=$(tail -1 prettier.log | grep projects || true)
 if [[ -z "$results" ]]; then
     echo "Success: Codebase has been prettified correctly"
