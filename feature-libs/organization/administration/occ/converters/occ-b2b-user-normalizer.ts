465205661f99892dc025d0f31fc68a7fbaa4ba66
scar.ai@sap.com <scar.ai@sap.com>
2026-08-26 15:08:33 +0800

    revert: remove customerId workaround from occ-b2b-user-normalizer

--- feature-libs/organization/administration/occ/converters/occ-b2b-user-normalizer.ts
+++ feature-libs/organization/administration/occ/converters/occ-b2b-user-normalizer.ts
@@ -20,9 +20,6 @@ export class OccB2BUserNormalizer implements Converter<Occ.B2BUser, B2BUser> {
       target = { ...(source as any) } as B2BUser;
     }
     target.email = source?.uid;
-    if (!target.customerId) {
-      target.customerId = source?.uid;
-    }
 
     return target;
   }
