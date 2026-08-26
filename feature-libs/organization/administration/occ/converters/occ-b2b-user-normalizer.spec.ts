70ad240734340d906fbc93f4a97a9fdc52791811
scar.ai@sap.com <scar.ai@sap.com>
2026-08-26 15:08:17 +0800

    revert: remove customerId workaround from occ-b2b-user-normalizer spec

--- feature-libs/organization/administration/occ/converters/occ-b2b-user-normalizer.spec.ts
+++ feature-libs/organization/administration/occ/converters/occ-b2b-user-normalizer.spec.ts
@@ -23,7 +23,6 @@ describe('B2BUserNormalizer', () => {
     active: true,
     uid: orgCustomerId,
     email: orgCustomerId,
-    customerId: orgCustomerId,
   };
 
   beforeEach(() => {
@@ -49,22 +48,8 @@ describe('B2BUserNormalizer', () => {
     expect(result).toEqual(convertedOrgCustomer);
   });
 
-  it('should use uid as customerId fallback when customerId is missing', () => {
-    const result = service.convert({ uid: orgCustomerId });
-    expect(result.customerId).toEqual(orgCustomerId);
-  });
-
-  it('should not override customerId when already present', () => {
-    const existingCustomerId = 'existing-customer-id';
-    const result = service.convert({
-      uid: orgCustomerId,
-      customerId: existingCustomerId,
-    } as Occ.B2BUser);
-    expect(result.customerId).toEqual(existingCustomerId);
-  });
-
   it('should convert B2B User with applied target', () => {
     const result = service.convert(orgCustomer, {});
-    expect(result).toEqual({ email: orgCustomerId, customerId: orgCustomerId });
+    expect(result).toEqual({ email: orgCustomerId });
   });
 });
