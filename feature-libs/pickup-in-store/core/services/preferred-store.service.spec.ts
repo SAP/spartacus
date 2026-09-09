dce16c3e234ca7aaa70a208fbfd392dfd3bb0812
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-09-09 11:11:03 -0400

    chore: pickup-instore changes and product-configurator changes

--- feature-libs/pickup-in-store/core/services/preferred-store.service.spec.ts
+++ feature-libs/pickup-in-store/core/services/preferred-store.service.spec.ts
@@ -60,8 +60,11 @@ describe('PreferredStoreService', () => {
   let pickupLocationSearchService: PickupLocationsSearchFacade;
   let store: Store<StateWithPickupLocations>;
 
-  const configureTestingModule = (withConfig = true, localStorage = true) => {
-    TestBed.configureTestingModule({
+  const configureTestingModule = async (
+    withConfig = true,
+    localStorage = true
+  ) => {
+    await TestBed.configureTestingModule({
       imports: [
         StoreModule.forRoot({}),
         StoreModule.forFeature('pickup-option', fromReducers.getReducers()),
@@ -72,7 +75,12 @@ describe('PreferredStoreService', () => {
           provide: PickupInStoreConfig,
           useValue: MockPickupInStoreConfig(withConfig),
         },
-        { provide: WindowRef, useValue: localStorage ? MockWindowRef() : {} },
+        {
+          provide: WindowRef,
+          useFactory: () => {
+            return localStorage ? MockWindowRef() : {};
+          },
+        },
         {
           provide: PickupLocationsSearchFacade,
           useClass: MockPickupLocationsSearchService,
@@ -90,8 +98,8 @@ describe('PreferredStoreService', () => {
   };
 
   describe('with pickup in store config', () => {
-    beforeEach(() => {
-      configureTestingModule();
+    beforeEach(async () => {
+      await configureTestingModule();
     });
 
     it('should be created', () => {
