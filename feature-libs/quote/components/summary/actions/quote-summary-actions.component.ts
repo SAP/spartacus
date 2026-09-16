a1c5eb4ee0726137931da1bdf2accdb6cd2a8532
Chandan <chandan.kumar.shaw@sap.com>
2026-09-16 08:49:56 +0530

    chore: remove dead code

--- feature-libs/quote/components/summary/actions/quote-summary-actions.component.ts
+++ feature-libs/quote/components/summary/actions/quote-summary-actions.component.ts
@@ -255,20 +255,6 @@ export class QuoteSummaryActionsComponent
 
   protected performAction(action: QuoteActionType, quote: Quote) {
     if (action === QuoteActionType.REQUOTE) {
-      if (
-        this.featureToggle.showWarningMessageOnRequoteButtonClick &&
-        !this.isThresholdReached(quote)
-      ) {
-        this.globalMessageService.add(
-          {
-            key: 'quote.commons.minRequestInitiationNote',
-            params: {
-              minValue: quote.threshold,
-            },
-          },
-          GlobalMessageType.MSG_TYPE_WARNING
-        );
-      }
       this.requote(quote.code);
     } else {
       this.quoteFacade.performQuoteAction(quote, action);
