59a81afb658df9ca8a8c4883def86f2fdef1b1b5
FollowTheFlo <florent.letendre@hotmail.fr>
2026-09-16 17:38:21 -0400

    remove logs

--- core-libs/storefront/cms-components/user/logout/logout.guard.ts
+++ core-libs/storefront/cms-components/user/logout/logout.guard.ts
@@ -73,9 +73,7 @@ export class LogoutGuard {
       map(() => {
         const redirectUrl = this.getRedirectUrl();
         const logoutUrl = this.router.parseUrl(
-          this.semanticPathService.get('logout') ?? '/logout' );
-        console.log('flo redirectUrl',redirectUrl.toString());
-        console.log('flo logoutUrl',logoutUrl.toString());
+          this.semanticPathService.get('logout') ?? '/logout');
         // If the configured redirect destination is the logout page itself, keep the user on the current route so the CMS logout page renders.
         if (redirectUrl.toString() === logoutUrl.toString()) {
           return true
