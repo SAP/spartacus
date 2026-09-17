9421bea5419fe85ac7692017ec88501d9fc0a740
Souhaib Khadraoui <souhaib.khadraoui@sap.com>
2026-09-17 13:50:07 -0400

    fix: revert changes on core-libs' fixtures

--- core-libs/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.spec.ts
+++ core-libs/core/src/occ/adapters/product/converters/occ-product-search-page-normalizer.spec.ts
@@ -74,16 +74,6 @@ describe('OccProductSearchPageNormalizer', () => {
   let normalizer: OccProductSearchPageNormalizer;
 
   beforeEach(() => {
-    mockPlpWithUselessFacets.facets = [
-      {
-        name: 'useless-facet',
-        values: [{ count: 2 }, { count: 2 }],
-      },
-      {
-        name: 'useful-facet',
-        values: [{ count: 1 }, { count: 2 }, { count: 1 }],
-      },
-    ] as Occ.Facet[];
     TestBed.configureTestingModule({
       providers: [
         {
