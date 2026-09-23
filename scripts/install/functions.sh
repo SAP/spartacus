1988a6784ac1f98f31f0ce17b7209f041581a246
FollowTheFlo <florent.letendre@hotmail.fr>
2026-09-23 18:01:00 -0400

    fix

--- scripts/install/functions.sh
+++ scripts/install/functions.sh
@@ -100,7 +100,7 @@ function create_shell_app {
     local current_npm_version
     current_npm_version=$(npm --version)
     if [[ "$(compareSemver "$current_npm_version" "11.0.0")" -lt 0 ]]; then
-        echo "ERROR: npm version $current_npm_version is too old. Please install npm@11 or higher: npm install -g npm@11" >&2
+        echo "ERROR: npm version $current_npm_version is too old. Please install npm@11 or higher" >&2
         exit 1
     fi
 
