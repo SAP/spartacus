b869e49b14a3928348737a99a47fad99a9035716
BenjaminEmiliani <benjamin.emiliani@sap.com>
2026-09-24 12:08:45 -0400

    Apply formatter

--- feature-libs/user/profile/components/update-email/my-account-v2-email.component.spec.ts
+++ feature-libs/user/profile/components/update-email/my-account-v2-email.component.spec.ts
@@ -1,4 +1,3 @@
-
 vi.mock('@spartacus/storefront', async (importActual) => {
   const actual = await importActual<typeof import('@spartacus/storefront')>();
   const { filter, map } = await import('rxjs/operators');
